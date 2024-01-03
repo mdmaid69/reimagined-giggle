5a5bf42ce58e685ee6de14537af6f7233ad33236
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:31 +0000

    dick forward rain foreign combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
