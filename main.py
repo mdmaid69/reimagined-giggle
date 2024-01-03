a8b64c181e66868ad99bfbe630b2d77b5039a172
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:39:40 +0000

    fair month tomorrow care voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
