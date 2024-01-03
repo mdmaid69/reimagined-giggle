63967e446be6e50edc1e01a2dc7e8ad111a19d70
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:55:56 +0000

    protest taste search achievement thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_typecode(array):
+        return array.typecode
