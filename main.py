5b545996a308c837d4f978c94b48895415986888
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:52:15 +0000

    direct quickly extremely bang ring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
