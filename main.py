240235dceaba9becca3626f1c5cc4b2f6b47f0ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:35:05 +0000

    shelter kick keepoff stiff open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Square numbers:", [x**2 for x in range(n)])
