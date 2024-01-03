838270747b7fe70916cc7965e246d3fd07f01ff0
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:21:29 +0000

    innocent personal fit body calm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
