00076108760044f5155312830515f2ace10a295b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:07:04 +0000

    getoff experiment sail 569enjoy emergency

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
