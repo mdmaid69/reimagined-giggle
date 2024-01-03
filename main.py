617f4a7d1c1a5c94ef4beb00836225d0821c82e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:08:09 +0000

    typical promise pump plan none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
