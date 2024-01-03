a35d02e47576e0afc0f47479bfc257fe72989324
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:04:34 +0000

    treat season case final movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_work(force, distance):
+        return force * distance
