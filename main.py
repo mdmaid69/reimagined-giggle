9cce960ac42032185e1ac314e0bbe8e83243b94b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:16:36 +0000

    mistake very pleasant might left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
