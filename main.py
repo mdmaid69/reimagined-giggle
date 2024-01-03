d1c7f36fdf0b5c52b54c5440b3b6a3300180cb96
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:12:58 +0000

    thought combine small project method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
