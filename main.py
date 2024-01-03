70d26f4bcb53faf177b4536f5cf89867a3630806
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:04:26 +0000

    discovery news scream illness except

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
