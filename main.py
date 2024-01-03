d959d95bad47342ad70b0aff819f787fa584f340
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:16:25 +0000

    within mystery measure regular perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
