3b3056709aada9652b068853fb2b7e25b149917b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:53 +0000

    bush miss repeat collection last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
