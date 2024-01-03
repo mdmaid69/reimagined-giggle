6eb50c11b3eabb4db466bf741130f1abd253e1e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:03:36 +0000

    sort turnout runout gift workout

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
