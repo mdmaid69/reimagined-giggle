f6a4c3eac1a73ca4f858cca9a20101b125aa642b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:26:10 +0000

    manner people deliberately wide own

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
