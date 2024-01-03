c3786ec9fb34f041e9bde5dd3f6c0241cdf4f983
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:13:43 +0000

    accidentally pollution exploration degree connect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def subtract_numbers(x, y):
+        return x - y
