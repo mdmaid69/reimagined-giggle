b4c87000f8c54754954d42aa1b14bb151befa34d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:22:07 +0000

    performance prepare lookaround turndown worry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
