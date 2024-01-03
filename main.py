121d005be1177ce1a6a6b70c3361133154e6dc1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:30:32 +0000

    odd patience ashamed impatient else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
