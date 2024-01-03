3e01bae4cdac54e34d66a8799027141a96a261a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:42:39 +0000

    lift lookafter keepfrom company suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
