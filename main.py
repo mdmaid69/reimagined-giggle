d9dd4aa779f83341e04f6bb65255e2e9b0555110
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:42:12 +0000

    getaway charge bye meal area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
