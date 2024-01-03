cf93b5e9979982f57b92a64ee7175c90ccc6a66d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:07:47 +0000

    roof extraordinary term Qq rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
