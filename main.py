fce8b18681156322259a2d68c986acd6210bc89f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:57:57 +0000

    fear prevent themselves ball nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
