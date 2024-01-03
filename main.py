fd211262e8ea5d49d2c0b704e0e109cb92c84aa2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:31:37 +0000

    hard beautiful anything earth our

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
