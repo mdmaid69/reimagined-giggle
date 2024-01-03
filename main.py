c15314cfc003d0a56d28dafb0bc17872bcb82250
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:59 +0000

    neither respect argument exact risk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
