8bb020d8964daad8c72257ec51898e618a709f39
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:11:01 +0000

    small spend short complaint accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
