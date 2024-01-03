01c20054e272a2c8eb0d93d51de5207db0e5d0a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:51:24 +0000

    pronunciation turnover communication cheap nice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
