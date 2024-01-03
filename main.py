d3774674837b95fffd3efd7cb9521a46a134b6f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:11 +0000

    idea main spite language greeting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def multiply_numbers(x, y):
+        return x * y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
