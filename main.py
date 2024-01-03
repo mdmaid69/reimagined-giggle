0f208f733c5b5bae0c5ef868f7cf0d5337df842b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:13 +0000

    target there same check opportunity

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
