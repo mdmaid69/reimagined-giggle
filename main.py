b09cd883bff521eb85c3f124618e1e6708b42c50
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:53:14 +0000

    indeed fetch fire motion whole

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_dict():
+        return collections.UserDict()
