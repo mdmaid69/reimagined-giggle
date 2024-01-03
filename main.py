a77f6459c71ae227025159f5b56d1b3bdb5bbdcb
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:11 +0000

    soon breakinto nearly there desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sys
-def print_python_version():
-        return sys.version
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
