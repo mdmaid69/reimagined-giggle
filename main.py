a45baba205a5c0c9916bec43269c7f69548d51e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:28:54 +0000

    unlucky involve closely lock difference

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def convert_array_to_string(array):
+        return array.tostring()
