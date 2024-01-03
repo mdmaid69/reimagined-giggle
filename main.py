5d5588fc503746bef2fcca62e362acefa8d6e9af
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:20:53 +0000

    even top central now well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
