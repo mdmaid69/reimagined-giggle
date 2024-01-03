fce987e91ac28fb60bf4a82fd4ff0f68ffe0e098
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:23:45 +0000

    uncomfortable duty bored friendly lock

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_density(mass, volume):
+        return mass / volume
