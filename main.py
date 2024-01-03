70fa147002bcbdae8f8136a5a33e609c47f517ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:06 +0000

    compete yes tooth their noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
