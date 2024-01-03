bef42fc427a2358ca4012ca50c467762b5505c43
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:35 +0000

    pupil judge object mother declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
