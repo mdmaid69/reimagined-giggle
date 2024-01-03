8db08cf703076b0c1405c6feee9b92a1715495c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:52:06 +0000

    discuss cinema plan chase cheek

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
