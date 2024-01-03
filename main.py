2693b59d7947f346cc145d49d36bec58e37a2c3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:11 +0000

    lift compete statement preparation pupil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
