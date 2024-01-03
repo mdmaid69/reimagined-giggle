a827357c6009752ff6ce688b2998e605778b8eb1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:50:57 +0000

    Gg anywhere show plant also

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import re
+print(re.match("h.*o", "hello world"))
