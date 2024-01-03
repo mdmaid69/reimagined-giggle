44f22f4bc068cc3c4e7e6407d90a943108256f1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:19:54 +0000

    mind coat goaway case right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
