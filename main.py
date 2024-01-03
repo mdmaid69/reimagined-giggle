54d3d00d129f245017ece5ac4046645334ae6745
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:42:49 +0000

    plain wild feed middle food

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
