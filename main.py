2a90839ca3c7c638c6866c369053c731602fe692
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:09 +0000

    child detail level board believe

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-print(sum(range(10)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
