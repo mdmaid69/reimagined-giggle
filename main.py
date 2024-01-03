95175890850efd5d5fb309c64562848e3d10302f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:28:52 +0000

    passenger recover prove cross cool

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_density(mass, volume):
-        return mass / volume
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
