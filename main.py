1c22fec45abb984aa78222694fe7f585b76f6493
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:40:54 +0000

    male brush most right start

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+name = "Python"
+print("Hello,", name)
+for i in range(5):
+        print(i)
