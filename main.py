5a93b8120d41693f199b0bfdfde3a27c5c135266
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:07:30 +0000

    number pile everybody clearly firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
