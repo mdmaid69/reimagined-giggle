d4d8ce6bc5885299af5d5414c0573cb247ed4b14
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:16:38 +0000

    force sure transport grow trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
