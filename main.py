83070c054842c686819dbd00524bb25ed0da977d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:28:29 +0000

    brush watch hurry takeover already

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
