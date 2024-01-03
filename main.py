afd306f3d513fdfcf6a03b3811877db82b20c2f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:45:57 +0000

    cut so total look fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
