3233da6cb26d88cf65ab17a1475317f13798730e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:39:45 +0000

    original press long force alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
