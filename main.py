03101a459809d943acc2a1d653287b7f7a9a7f99
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:32:16 +0000

    life ground course skin disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def is_even(n):
-        return n % 2 == 0
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
