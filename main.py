104854b74f9f20245694b3ee4e3e61e81e56c6a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:13 +0000

    invite post each believe impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def delete_file(file_name):
+        os.remove(file_name)
