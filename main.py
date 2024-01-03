91228f8c18cc99c66765cfbec0c5d44e182ee270
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:21:30 +0000

    entrance ocean appeal comment afterwards

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import sys
-def print_python_version():
-        return sys.version
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
