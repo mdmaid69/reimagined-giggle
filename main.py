6f3c698f1118ee51395ec029c02e39c143bbbf6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:35:03 +0000

    now geton back together track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_pressure(force, area):
+        return force / area
