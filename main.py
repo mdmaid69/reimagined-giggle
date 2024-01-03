0deafbc865b1b6d7fadf22afb2d2cf0759b710ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:55:34 +0000

    hardly help giveout firm cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_binary(n):
-        return bin(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
