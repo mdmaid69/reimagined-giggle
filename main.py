ce47db0f82cede1c1643e12990755cebc50a3be2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:08:51 +0000

    borrow disturb obvious comefrom face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
