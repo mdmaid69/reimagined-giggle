28d1c46aca7b41fd91a0efbefaae9c859e08ad41
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:02 +0000

    polite comeup mess breakin Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_length(array):
+        return len(array)
