8e042373d5fa9a41c7181dfd0b7f37835b949492
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:51:47 +0000

    both anything message bring them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import time
+def get_current_time():
+        return time.ctime()
