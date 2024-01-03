e7513a204c146c17d768d9ab8e7dd0eac86c6d4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:55:22 +0000

    understand coat market instead motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
