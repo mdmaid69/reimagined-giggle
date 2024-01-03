a1487416c9b2ee55a654c7968712dc82c3c27a23
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:03:08 +0000

    low block quarter floor burst

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
