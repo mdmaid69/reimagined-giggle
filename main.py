177c2ad74f0644dc0afab335d4248926c7c97676
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:48 +0000

    little begin afford responsible recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_bytes_from_array(array):
+        return array.tobytes()
