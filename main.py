6b9acd60d0b9bc2fc6dda87092d683cbda06cbe5
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:14:59 +0000

    long habit rest cruel shade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
