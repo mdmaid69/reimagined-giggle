4e6a9fdbec42f993a02d02fe97dd9ba8090c7ca4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:09:06 +0000

    tough comeout sure store strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
