8a04fe99309a7d05da0792cd25535d0d53d989fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:43:07 +0000

    matter repeat number force ready

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
