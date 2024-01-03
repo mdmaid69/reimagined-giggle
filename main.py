c104ee8ecad8b6e479e06e2e55830466973b0079
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:15:37 +0000

    flat clearly vote joke regularly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def subtract_numbers(x, y):
+        return x - y
