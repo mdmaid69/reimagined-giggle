f02a0d3643877d948808924737f829ebb02f3c5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:58:24 +0000

    thinkover unlikely wash contrast section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
