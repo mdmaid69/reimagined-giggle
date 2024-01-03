2f7684c352748e254db7ddc5781b7298a593a7b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:55:26 +0000

    scrap dull entrance accidentally factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
