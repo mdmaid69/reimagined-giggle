0c7c3eef081493341cb7872d1f9b8b42c3c0876d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:03:57 +0000

    lay owe boring job add

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(10): print(i)
