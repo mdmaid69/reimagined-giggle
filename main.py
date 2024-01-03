3133e64aeb9e1e6f06cfea1204b7de341a902df7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:50:22 +0000

    father talkoutof sort bad unkind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_density(mass, volume):
-        return mass / volume
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
