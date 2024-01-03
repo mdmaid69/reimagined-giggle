e8e9c96c48780a85f3a512d21ef1713183f26535
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:53:34 +0000

    otherwise trap search keepoff glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def is_palindrome(s):
+        return s == s[::-1]
