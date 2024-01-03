6e18bb5e0fbc55bccde066aff0ec2739f0e2dcb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:28:10 +0000

    every meal manage number into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
