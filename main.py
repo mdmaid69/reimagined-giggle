3e1956a10845ab0034f84ee2ef1bde468acb4ab8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:56 +0000

    school empty pot tense bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
