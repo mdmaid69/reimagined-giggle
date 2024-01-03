c51a9f49c4ad853a04f85a23e2143251dff4f426
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:03 +0000

    afford variety permission niggaacademy street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
