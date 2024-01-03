cd5e33d3a10ac20619ca8246d26404a3e82e9fd8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:34 +0000

    office notice over big no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import time
+def get_current_time():
+        return time.time()
