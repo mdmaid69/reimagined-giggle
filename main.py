65674ff5b278f53d2b7e6d57778963f4afc37c3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:11 +0000

    regret matter bringback serious tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
