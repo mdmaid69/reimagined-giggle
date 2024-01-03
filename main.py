15363919f17d9bd275bdf6a6bab95c0894dab243
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:02:33 +0000

    purpose beneath off afford manage

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
