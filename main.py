7947d3f9c0f7357544e2cde53016c31c87a4a890
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:02:50 +0000

    worst mark pocket tell relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
