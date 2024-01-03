7de1465dd308549d37030260971ce9187aa44acb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:02:48 +0000

    joke mine callup proper people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
