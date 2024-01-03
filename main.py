12c6f88114a2d2a75ba3677a1f3c20362198cb97
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:53:11 +0000

    hope way fault frightened factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
