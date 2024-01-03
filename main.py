ded2872f5331796e30898c6cfbbbd9c8a0320737
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:20:00 +0000

    mistake lose rule reasonable list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def count_words(sentence):
-        return len(sentence.split())
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
