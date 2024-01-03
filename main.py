57c154c3791a9ecd3970f343b244cbfb12d9dfa9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:33:23 +0000

    trial odd regular quality broken

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def add_numbers(a, b):
+        return a + b
