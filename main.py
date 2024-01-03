24656ffeac26deec6882ecc0ad0e08cbf1f6cb42
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:38:19 +0000

    command fence free rich rest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
