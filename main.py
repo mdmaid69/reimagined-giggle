17aa3049a2adc520891562d957c3f5e06caff499
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:39:13 +0000

    zero office easy afternoon this

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def reverse_list(lst):
+        return lst[::-1]
+def find_min(lst):
+        return min(lst)
