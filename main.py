b1410dc5db947b496b53e7a0137d6bba15bf26de
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:03:25 +0000

    it explain much cover image

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def count_elements(lst):
+        return len(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
