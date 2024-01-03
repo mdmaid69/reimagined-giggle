7ede3e2da2423883cce643de3960c397e96ad44e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:14:06 +0000

    admit bother breakthrough spray object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
