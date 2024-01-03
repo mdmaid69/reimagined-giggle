836c4ac563a0fdf6a42883541bcb2d71b99bca57
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:57:43 +0000

    foreign rough lower use shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
