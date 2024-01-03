9b14b547fdc9a9a59f4c997456d428d2792be878
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:11:37 +0000

    praise calloff crazy post pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def square_number(x):
+        return x**2
+import collections
+def create_counter():
+        return collections.Counter()
