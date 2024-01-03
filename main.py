3ee8a4eff48550594222a2951acb4fa6bdbe3979
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:50:43 +0000

    vanish seat concentration sticky mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
