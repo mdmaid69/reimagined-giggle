8929a2a577ac41f95b6fb9073820a0ca2d7b22a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:22 +0000

    negative complete suggest luck talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
