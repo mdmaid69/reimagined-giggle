8f44924cb4c9acbf6d7fb92569d4d36ad01a125e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:55:15 +0000

    guide influence bridge wear disagree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
