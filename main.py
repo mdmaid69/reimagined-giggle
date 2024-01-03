916d3f6f77dc54c96443b0cfc82539d12466253a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:55:55 +0000

    probably every uncertain translate around

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
