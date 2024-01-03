1833565d8eb9f38e5a5852eda912f7573073d9d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:56:42 +0000

    mark apart something practice sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
