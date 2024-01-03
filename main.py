6324391c2d4bf5b8d598282c433ee62c426e6d4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:18 +0000

    uniform relax tape breakfast straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
