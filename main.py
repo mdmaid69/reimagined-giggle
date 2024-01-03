99fb7a902cecbbb7b9e7b983374bb507ff981cfc
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:41:41 +0000

    goaway thin equal please card

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
+def subtract_numbers(x, y):
+        return x - y
