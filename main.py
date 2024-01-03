1e26a8202940be149307433985219ef852cc614b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:32:24 +0000

    concerned problem table translate Ii

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
