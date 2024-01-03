c242a04d4f3c44b1fdb6443fe596504e8e00d644
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:34 +0000

    pocket trick giveout boil difficulty

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def cube_number(x):
+        return x**3
