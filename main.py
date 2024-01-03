ffea728fafd692ff34760ddb237bc141579de49b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:22 +0000

    share appear compete spare form

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
