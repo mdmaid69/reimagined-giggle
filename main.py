ce09268e5f104b8627d9346d6b926f6b00d18f4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:06:06 +0000

    beneath field ours hurt winter

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
