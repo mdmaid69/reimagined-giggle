fd8066195423430bd68682c62ab5f19f895f603d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:04:56 +0000

    kind possession alone treatment interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
