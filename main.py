2687da71d726323f53de89f3d3d0c2df54a4779c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:10 +0000

    declare fear road name corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sys
+def print_python_version():
+        print(sys.version)
