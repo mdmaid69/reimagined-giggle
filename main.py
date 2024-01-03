e620cde12a5b76995c5a02353e2c7e5f76c51ef8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:57:09 +0000

    bank impatient month brief belief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
