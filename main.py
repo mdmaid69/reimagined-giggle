4c76671fb7e98926bdd067a52d8e06f71d8ada1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:15:30 +0000

    nurse small human convincing from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
