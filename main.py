a7aee1bc4c69e2a9f76ec39bde01a86d6ba3b364
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:44 +0000

    team write own will everybody

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import sys
+  def get_python_version():
+        return sys.version
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
