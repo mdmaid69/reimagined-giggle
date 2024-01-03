021d5fb06b25efad423b96cd25225d18162d5aab
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:56:22 +0000

    continue grand snow train class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
