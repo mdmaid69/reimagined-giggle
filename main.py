ce85547416e52a46689e20e5dcd0f81788b0a040
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:49 +0000

    miss Vv handle ability how

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def cube_number(x):
-        return x**3
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
