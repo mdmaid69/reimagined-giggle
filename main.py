4f0857f0ac402fb2d590f9f74f1707ed7acf8253
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:31:13 +0000

    else job never owe dead

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_str(array):
+        return str(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
