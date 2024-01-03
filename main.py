637bf391343e7d41a772a46db1b0a980c2da9122
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:32:25 +0000

    offence to ahead discovery sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
