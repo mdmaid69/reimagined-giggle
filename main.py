a29b5a076a2a3de948b5b53b46ae6cf513829692
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:28 +0000

    collect bit interrupt nor confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
