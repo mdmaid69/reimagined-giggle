eaf19d78a02fec291fbfd02e474752cb8f68e071
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:18 +0000

    space minor both port eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
