b4efd867f436aaed5210f2c5b11317fa576c1cbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:44:46 +0000

    practice reply any want pay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
