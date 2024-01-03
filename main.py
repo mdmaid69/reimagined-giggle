d33115a54dece774f858546888ee142701e3ee32
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:15:02 +0000

    stranger difficult now depend little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
