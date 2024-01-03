9d1a56a38c972c5e2c3a819fbdea564f84f836a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:19:06 +0000

    order accordingto active process hollow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def remove_duplicates(lst):
-        return list(set(lst))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_item(array, i):
+        return array[i]
