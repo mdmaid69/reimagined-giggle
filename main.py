39bc3e42d567620fbc288139663a3d4988cc981c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:20:15 +0000

    exact tall whenever moon here

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
