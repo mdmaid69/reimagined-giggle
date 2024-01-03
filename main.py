b74c06188093abc84e1509f599fb45cb70c785b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:11 +0000

    crash care cool personal gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
