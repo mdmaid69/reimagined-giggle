a5d91c26ff8a26785a54015c346aa269ad116f4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:10:30 +0000

    probably flow touch afterwards off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
