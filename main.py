54158af7bde3f5cadb733cf24771c808c3154cfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:20:45 +0000

    hardly board funny badly during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
