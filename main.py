3841e85cce5ed013a9692bf145f8b12db3df9ad1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:31:01 +0000

    doctor feed situation produce different

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
