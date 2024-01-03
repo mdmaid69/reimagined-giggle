1cc2ccd5fe2b93fbe660b4f1a298c828f103b657
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:39 +0000

    interest cost wet point could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
