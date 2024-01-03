84d80902e2478455aeffa1bb45fbbb74a3a9e69e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:57:11 +0000

    refer knock immediately problem final

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def append_to_array(array, item):
-        array.append(item)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
