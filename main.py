33712f4aeea497bd80e57318c4ebc44116ac083a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:02 +0000

    thankyou climb machine typical language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
