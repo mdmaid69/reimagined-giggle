883eec78e95fcfdde2bb2515be6cd187a21132f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:22 +0000

    getoff calm scared back slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
