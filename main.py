d3792bcecbc4be923fce94c9e729ef650d023a05
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:21:27 +0000

    secret event gap fear money

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
