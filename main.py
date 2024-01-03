a92567ed9c39905d1007fe0ad981130a90af2583
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:56 +0000

    smell error badly bye pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
