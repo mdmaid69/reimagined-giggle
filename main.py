f94c8af381692fc53c63bf2e22945834451501c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:39:20 +0000

    case band road bet Yy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def is_even(n):
+        return n % 2 == 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
