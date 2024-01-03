988b9d82f75569b8d8ed69da29ef2a9a65046b0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:21:18 +0000

    rob field accidental relief nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
