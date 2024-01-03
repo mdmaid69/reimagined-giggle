406e22a6c73fa93325564a2cb60ac9b6441a2f40
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:58:13 +0000

    minimum greeting over violence winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
