7b6bf648cce00149242affea2239f14f22e86fe8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:16 +0000

    muscle accident sore lookup noise

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
