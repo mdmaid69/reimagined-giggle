ed99070e4d6ec23cd3ee5248bad10c803dd5b728
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:47:19 +0000

    watch breath air violence promise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
