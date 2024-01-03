387ffe6923f5ab9226a3d6468e2a7b19b006de5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:07 +0000

    commit chance determined great debt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def remove_duplicates(lst):
-        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
