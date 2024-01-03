f8470f5cadcc98e0d50778be84e427d19c40a1c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:02:15 +0000

    matter before remark eat shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
