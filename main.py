261d00742aa1d718bfb1d1388fdb6ceb32397744
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:56 +0000

    until side wheel measure chance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
