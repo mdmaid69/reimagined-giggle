53dbf0e8eb7db3b4bcbdd41c2f60421805bd5bf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:29:06 +0000

    float floor cruelty duty tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
