ed3c46c3731ae8309d3d0ccb82295184a837c028
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:13:53 +0000

    agree rent content mend allow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
