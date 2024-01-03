aebf9fdbfabe915b32934056a8e844bfc81a5df7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:19 +0000

    doubt bath communication fast perhaps

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def sort_list(lst):
+        return sorted(lst)
