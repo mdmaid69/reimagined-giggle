ca33c1b98a6b0f773888dcfda0ad52bc8e4d8f48
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:01:46 +0000

    suddenly difficulty wash dress goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
