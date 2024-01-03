dcd4d4f367727abd686f6b875b086d503273848e
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:08:00 +0000

    rely mess legal well challenge

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+from collections import Counter
+print(Counter("hello world"))
+def convert_to_binary(n):
+        return bin(n)
