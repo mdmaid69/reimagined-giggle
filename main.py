be0f6f9030d1057a5d397adfd92f73ff2fa14e58
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:51:43 +0000

    standard and seriously skin keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
