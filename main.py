82cf63f38808a7b1e7b9d242fc0017a73eb905b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:35:42 +0000

    lookafter prefer spring apart steal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  def is_odd(n):
-        return n % 2 != 0
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
