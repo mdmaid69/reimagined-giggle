c1e84e31a7669db4303af8cfcefc54580f436b24
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:51 +0000

    conclusion pronounce thank mark broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
