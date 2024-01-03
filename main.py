91f9014dc454a7e807872d10eb2a7429e5cfd447
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:21:10 +0000

    container move equal complete breakin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
