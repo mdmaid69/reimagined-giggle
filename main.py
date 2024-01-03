528fc912248a6d1cc6441e815375454b772ac4c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:21:52 +0000

    light car part direct under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
