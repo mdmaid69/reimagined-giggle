8dfebe23856986455f185c433d7591e71344a510
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:22:15 +0000

    matter deliberately practice opposite through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
