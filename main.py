e5ed9843d933cb8170050c02209f288a0eacd2f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:55:16 +0000

    free surface tense grand inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def get_array_as_list(array):
+        return list(array)
