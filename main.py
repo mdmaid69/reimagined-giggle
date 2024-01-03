4312c89f4ba24829d489c5665dbffdf9cfa31ad3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:38:29 +0000

    prove good woman bright build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
