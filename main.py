eab3ef9dbe2b44e34e44852be69be2588f44902b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:28 +0000

    declaration black communication war search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def convert_to_octal(n):
+        return oct(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
