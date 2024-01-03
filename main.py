f077087e0f31b701ccbcceafe8a6a0d1ac29b969
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:23:10 +0000

    stranger comfort support belong happen

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
