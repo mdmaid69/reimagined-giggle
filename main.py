65ead356c5a0a1c36f6429f940907894a02b8aa0
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:34:06 +0000

    father sing greeting lonely information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_base_name(path):
+        return os.path.basename(path)
