5f4e8d40ad78bdfb67605bd079f24a5edf6986a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:46:53 +0000

    fit rare meaning silent only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
