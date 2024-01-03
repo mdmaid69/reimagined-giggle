dde8b70f2b7200051040eb9699b66827f5cd83bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:02 +0000

    statement draw few teach correct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
