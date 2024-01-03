02a7be57c4f046f2e2b6478efb9f409fda34f5cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:58 +0000

    lose believe apply university on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
