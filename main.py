49299f5f69428f1abdf910d8dc97fbb3302d91fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:21 +0000

    tend forever pay unlikely up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
