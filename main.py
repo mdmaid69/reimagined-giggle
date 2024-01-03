5ffd8dbd3c69cec0ae7d9178818b403b0039a5bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:41 +0000

    offer goup stiff book buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
