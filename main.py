550add527cedb19ef22650e27de899d0aa000312
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:46:40 +0000

    watch would no sentence personal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
