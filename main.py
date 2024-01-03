4bec9295bf99122f31f548cbeff6c9bef2f7216f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:10 +0000

    stare autumn many pity slice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+text = "Hello, world!"
+print("Words:", len(text.split()))
