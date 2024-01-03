d550af0525966e0e4ca2ed787267956225e7f48b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:28:33 +0000

    tap design possession lucky previous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
