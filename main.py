5d43bbedf2493e7b52a292a847ea0dfd8cb22dab
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:03:18 +0000

    brush read best language inquire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
