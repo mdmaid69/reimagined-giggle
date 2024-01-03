788d554ebfbe5de7552e9727026c5a58a9b9b416
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:31:11 +0000

    watch taste valuable sky old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
