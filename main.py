c98024c74d0c2331fd4081498cfb6d14a07ff8d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:36:55 +0000

    awake dance all true belong

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
