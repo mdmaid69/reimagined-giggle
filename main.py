fb60ed390ceacae27ebb8bc8ceb9d8e05e83ddb1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:13:34 +0000

    swing callup grateful raise substance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
