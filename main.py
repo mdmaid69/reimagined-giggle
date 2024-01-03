a9d48fe734a2f636f63cf0d7ab9ac6a33eb76b29
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:57:32 +0000

    sort finish safety at but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
