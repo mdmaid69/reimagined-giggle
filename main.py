8118bb4bd63e54575ed74de45d2dd2214aa0aede
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:22 +0000

    comein uponon achievement its occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
