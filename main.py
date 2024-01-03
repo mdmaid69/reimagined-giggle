dd4a45355b77b94e726154584e420239af228a26
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:26:54 +0000

    ability grand flow life centimetre

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import json
+print(json.dumps({"name": "John", "age": 30}))
