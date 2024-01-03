4317f80f92b47fff953c918469427a8d72437a91
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:58:50 +0000

    police mine dress downstairs maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
