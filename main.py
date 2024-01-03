27f05d8c5f35d992095713454da83e587d92076b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:04:09 +0000

    reach callback stay regret my

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
