3d0e4731661d9d760ec3ee30c0c55d896cab7263
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:44 +0000

    heavy admit squash month cool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
