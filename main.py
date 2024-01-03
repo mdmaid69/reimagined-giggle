58266e064d995f040cc2a533ef29652ee9218698
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:07:50 +0000

    maintain niggaacademy thank excellent issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
