f43e45589129ecfff2eba90b14a5feb20daf7368
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:33:32 +0000

    someone giveback turninto long opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_item(array, i):
+        return array[i]
