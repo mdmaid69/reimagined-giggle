34c8b51c18c910a945fbc0dcf54a11112c5b0f1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:44:31 +0000

    scale question business same closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
