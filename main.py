ef4fee632dc8afc0a30c51d1ade1e57026d32012
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:45 +0000

    soil wherever busy down aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
