ad35c0163691f40206cc018a37d3dae0f050fe8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:52:38 +0000

    round goaway everyone avoid ever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
