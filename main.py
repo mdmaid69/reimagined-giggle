189c5e009cea3970ed4b66ee308b662bbed49655
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:47:55 +0000

    arrival pure tradition strength strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
