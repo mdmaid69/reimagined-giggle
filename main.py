b4aac62bba9ea6623dea049c024ef8434cb32c8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:41:51 +0000

    narrow holdup watch mix that

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
