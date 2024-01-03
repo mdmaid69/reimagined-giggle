2075703b1a556a191372c9533c18dffa69fca912
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:48 +0000

    murder politics neat importance bringdown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
