6096c8c4491af924eacc69d4f38dbf07b496e945
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:06:16 +0000

    distance middle willing hold serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
