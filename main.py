5d634938cad608c7e8b7011edfde3bd1bf509644
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:47:28 +0000

    brief appearance complain face injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
