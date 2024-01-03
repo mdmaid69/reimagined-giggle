f17fa7d25f7525382f083e821c01134594084b01
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:21:54 +0000

    leave ocean afraid noone indeed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
