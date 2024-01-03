de8c9bc64d185d0f070d0cbfc36f8e46a633e603
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:26:59 +0000

    ashamed regular report interest issue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
