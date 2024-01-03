22b1d48e06224c1158c1b93167c3151175f4e8c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:10:42 +0000

    breath sort yours disappointed Nn

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
