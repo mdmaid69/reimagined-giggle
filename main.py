219e4b088d22999a06f6c105d1475c9aec8cf156
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:55:48 +0000

    damage track within course Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
