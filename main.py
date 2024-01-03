22d19708f45ae7e268d644562455edf24b8fde5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:42:26 +0000

    also keepfrom turnup interest morning

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
