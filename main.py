5ee33dc5f3bbf5113c86c231f6d3d58a654dee68
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:04:55 +0000

    police Jj comedown that possibility

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
