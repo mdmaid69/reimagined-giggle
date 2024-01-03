230d1299cbf7baa9915e4b9df591689f8ea51b12
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:06:49 +0000

    common change leave basic past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_combinations(n, k):
+        return math.comb(n, k)
