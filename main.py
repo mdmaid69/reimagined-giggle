42afdd1eadb5e17c8acce7e7ae4b45ef83728aa8
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:47 +0000

    firm store beautiful reject side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
