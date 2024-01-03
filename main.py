03cf8f742bf202804eb9e644ee05aacf2ce6343f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:30:21 +0000

    threaten breakthrough organization straight home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
