dcd71911624f520193a1df9748532d7bd125885b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:04:33 +0000

    actually ride before lead protect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
