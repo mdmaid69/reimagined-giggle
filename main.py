cd051c4bba6170a860a3ffca042ecdf035b2de89
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:57:46 +0000

    concern yesterday shock ordinary imagination

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import random
-print(random.randint(0, 100))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
