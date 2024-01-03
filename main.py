730d9781d69c3ebff5d89fb49895c607950a8ec9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:07:50 +0000

    instrument book rather limit pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
