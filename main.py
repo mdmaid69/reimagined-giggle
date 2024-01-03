29746dcd36b8b36aa00d61b6fcf9ce7b7e553792
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:58:06 +0000

    road single computer advise welcome

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
