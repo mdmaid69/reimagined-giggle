bd595ba9fcaff1889d67cf5aad4d279fc05b68af
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:40 +0000

    car unkind politics vote thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
