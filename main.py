fd581dd26bf837060eeb69aa8913e56ced5995cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:54:55 +0000

    off his hole no please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
