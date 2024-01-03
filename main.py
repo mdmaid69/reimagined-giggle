77bf8072c8dfdc4bc441710d363c6958bf408089
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:30:47 +0000

    door cross keepout any means

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import collections
+def create_counter():
+        return collections.Counter()
