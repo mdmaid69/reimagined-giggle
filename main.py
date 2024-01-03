04cee53a515197fdcf486ac45d24e5eb7adc5955
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:21:31 +0000

    design cover between her invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
