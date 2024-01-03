6f1c45c37b905cf088922c0715ec2f426c851854
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:41 +0000

    country shoulder trade sense sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+print([x**2 for x in range(10)])
