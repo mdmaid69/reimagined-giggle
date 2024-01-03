ceab37feabd4212e8548f4d9b42ca0e0829c4ada
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:46:11 +0000

    left getout fine skin wrap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
