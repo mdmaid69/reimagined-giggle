caafbace26b8c098567cb68065d6d5c3864d28bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:59:38 +0000

    innocent favour string demand hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
