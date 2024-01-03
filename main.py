2cd056aacba140ebeaf6d9ec52e2598bf4b5c7ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:22 +0000

    bright petrol tomorrow work regional

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
