86b0d24498f4e4474dab6c4fabb2ecc6f77182c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:32 +0000

    to upstairs poison strict move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_as_complex(array):
+        return complex(array[0])
