af871e652198dee7663051bd50af7fe691f8acd2
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:20 +0000

    constantly brave want tip trick

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
