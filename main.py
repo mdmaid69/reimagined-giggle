e16909afc91a999790e32f116332770679ac297a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:06 +0000

    repeat trip discover miss degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
