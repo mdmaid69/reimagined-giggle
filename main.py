6e2170db2258438bf0ba779e684aa095de87fc36
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:41 +0000

    big content pronunciation store up

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
