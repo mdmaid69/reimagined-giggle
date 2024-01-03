6b11e468d277ef609cc10d8c947f107c3f0c5c4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:35:50 +0000

    powerful unit want frame pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
