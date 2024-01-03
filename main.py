0f30f39f82a236fe311dc81bb6653c00b8ba13db
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:03 +0000

    claim piece flat completely claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
