c07a8a38b24cf578f9b188953820c032ed13882e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:37:32 +0000

    successful when as no contain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
