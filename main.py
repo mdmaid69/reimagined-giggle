f61d96d93fc23d85dde4f54d00682d341dca97bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:33 +0000

    shopping coat scream string before

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
