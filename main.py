1aedb13a1d5a6907b0a28033e8e0aeae1af0b822
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:45:36 +0000

    will wake relax deaf hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
