da9a355802f9e6b337fdcd8e1e015d14c4fd70f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:08:41 +0000

    cap surprising damp feel bet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
