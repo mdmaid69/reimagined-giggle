e7d7c4093ea05cced2ac120e048151fb1b543677
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:38 +0000

    keepup cover instruction opportunity breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
