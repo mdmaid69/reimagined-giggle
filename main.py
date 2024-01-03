cfbc9e769f3d8ae9841f2bbf3d03d450e7c10cea
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:03:00 +0000

    pleased popular together report bet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print("Hello, world!")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
