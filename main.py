1475711fab004e196fab50ab38337f8f465bc8a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:17:06 +0000

    correction enthusiastic variety bet table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
