bf9f581aac4a0017fe79ca9dc54b8670739a6750
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:01:41 +0000

    ignore must language tie keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import sys
-  def get_python_version():
-        return sys.version
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
