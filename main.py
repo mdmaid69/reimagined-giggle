2c410ccb5cc3f457433c6258438994e878a42d55
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:38:19 +0000

    letter relax 567enemy workout original

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def square_number(x):
+        return x**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
