29c1ab6269c07647654e74e5c6ab0eddc880bb1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:26:49 +0000

    rush flow congratulate store last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        print(sys.version)
