5af8979fccd2cc92e1e7ad4073e8f8f7546e0b83
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:29:37 +0000

    each mysterious cross or sale

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def is_palindrome(s):
-        return s == s[::-1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
