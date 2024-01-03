84cfb28e100e6ce5deffaeef7b66218defbd2ee1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:14:30 +0000

    material influence bye type defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import datetime
+print(datetime.datetime.now())
