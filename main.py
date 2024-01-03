1cc3c38b98645f31a40e33714b1f6e4dae70cb3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:19:49 +0000

    well hide card rank left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_as_bool(array):
+        return bool(array)
