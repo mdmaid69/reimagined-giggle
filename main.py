7c0b119c572e6699cff5453fa90d0cf8824b1d09
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:33 +0000

    situation instruction Dd us and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
