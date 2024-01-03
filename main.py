a61123d06360caf35b7d24b1232613d224629c02
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:00:05 +0000

    consist whoever runinto handle certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
