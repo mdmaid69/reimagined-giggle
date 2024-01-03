d2ee89199fab60b32c7dc630b93305736f840a39
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:57:51 +0000

    alone warm tune corner surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
