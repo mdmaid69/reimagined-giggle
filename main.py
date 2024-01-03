82717b4f656f7cbef58e6598f804a470a62df035
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:28:43 +0000

    praise copy introduce tie neck

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
