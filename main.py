de2728d097c1e1295713c70e7eac6ea6095b41e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:25 +0000

    borrow mess recent look add

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
