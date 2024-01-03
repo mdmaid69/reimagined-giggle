c0ebb49cc85c6ac5a63f098b7f36d68c44bab171
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:23 +0000

    side argument smell tidy reaction

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
