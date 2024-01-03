96f22ddaf4a0e8110391768ed36168185bf39556
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:10:02 +0000

    sting factory shoulder transparent beyond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
