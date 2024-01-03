225cc36cdb44b24610f2b31c064c2a2b3f5f092a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:23:53 +0000

    split famous attract watch compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
