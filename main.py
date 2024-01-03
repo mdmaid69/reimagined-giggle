2511dfca93fb2c2644c0983d281e2b5d222eb2ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:56:38 +0000

    possibly breakin high sentence conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
