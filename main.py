704a8de200152af73c29a4304939cacc885e69fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:39 +0000

    what straight group business condition

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import datetime
-print(datetime.datetime.now())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
