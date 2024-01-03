1370edf082fcc7122dc9cb2fda9347323bb40638
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:59:05 +0000

    several soft stick wind change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
