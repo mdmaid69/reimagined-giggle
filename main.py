103fcfb695f0ac0d66995cc2cf85a274651975fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:16:16 +0000

    dangerous fall legal loose born

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
