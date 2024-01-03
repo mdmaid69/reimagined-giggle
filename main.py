925c8e4e159359b62e714fb7ac05c1631ea14699
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:07:17 +0000

    study sweet blame goout care

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
