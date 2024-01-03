1f6f2da62f2041525e59f593af8d5d11e3091090
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:40:18 +0000

    connected getaway blow burst frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_itemsize(array):
+        return array.itemsize
