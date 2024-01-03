b1d99e7cec04f9027ff8fc60d48946435b89f2c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:41 +0000

    chief miss 569enjoy deserve live

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
