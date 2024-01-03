991438220470b3008fe4836e989ea5af85ca501b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:09 +0000

    guess keepoff polite breakup bringback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def count_characters(sentence):
+        return len(sentence)
