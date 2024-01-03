653c4c85122239f9bd131c19eb6ef52b32c040cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:55 +0000

    marry communication between friend arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
