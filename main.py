d07da740ef67207120d6cf2b17b2c80545534347
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:05:18 +0000

    beyond spring than ticket whom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import re
+print(re.match("h.*o", "hello world"))
