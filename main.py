f59d4f5af273092520de68e8b1584256d192b8a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:09 +0000

    correct note calmly think flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
