eab5623a64fc468c87c89a71ff97234ea1cb30ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:09 +0000

    money attempt brother trust country

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
