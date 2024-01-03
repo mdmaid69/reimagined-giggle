ab5dc053b164ad929c00bfd79ccd3630cf3652ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:47 +0000

    fairly thinkover live 572enter juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
