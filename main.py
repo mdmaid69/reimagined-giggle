7bcea5a1a476f2ddd9c8048aaa7f41d77fb59a0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:52:44 +0000

    always can off big word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
