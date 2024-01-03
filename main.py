2bd7d1f5312b2654e80e4ec6f41ed672e20e320d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:57:17 +0000

    sometimes guide official fence bag

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
