12a71d8deca76abf25605f67f817341f6f2c6b29
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:50:00 +0000

    left quarter concentration genuine sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
