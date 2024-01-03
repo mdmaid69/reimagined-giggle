7375a055291e2ae547b539a444317447a3f8db93
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:08:34 +0000

    force impressed ocean visit Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
