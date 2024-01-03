e9503df29c337695d2fbd403f8abe344e50b45c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:44 +0000

    aim sensible ground goon divide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
