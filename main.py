1ff8abe87afa340efe2ea8d45b09c09626befd7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:01:17 +0000

    loss estimate afterwards cover conduct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
