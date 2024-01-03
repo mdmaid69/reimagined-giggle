4febe617aa4b63acb2ff0b92cbe5f7bea20fcf4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:23:27 +0000

    abroad majority public nonsense bet

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
