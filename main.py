ef087b8e55c3e437e0783b13e8536888307fe7be
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:55:54 +0000

    recent smooth prefer past shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def greet(name):
+        print(f"Hello, {name}!")
