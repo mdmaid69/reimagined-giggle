df1be26a8bf023b7a172fe6b26a485c7d0a0a2a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:09 +0000

    quarter gooff unhappy matter shake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
