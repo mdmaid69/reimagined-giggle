8f79f8ff12ee1973c9c824a8026bdc44119cd3bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:39:20 +0000

    shake base politics forward switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
