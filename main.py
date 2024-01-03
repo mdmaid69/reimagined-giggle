7b58e8e8a1ba81796bd9982a6d0352d862787df2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:02:05 +0000

    successful side smell method control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
