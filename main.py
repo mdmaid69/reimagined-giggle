593689799cd777cf401a813ec9546f83636421c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:58 +0000

    stop concern yours could dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
