87347b3be74ba28a80f05ed73d23a0f8f8af369b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:28:40 +0000

    lean feel home depend edge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+from collections import Counter
+print(Counter("hello world"))
+import array
+def clear_array(array):
+        array *= 0
