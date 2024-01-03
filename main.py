6ee0127c22f61427f6dee0379036c0dd3a96adf1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:27 +0000

    distance middle fond pride within

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sys
+def exit_program():
+        sys.exit()
