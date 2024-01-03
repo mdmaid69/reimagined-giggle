d3e218d9ec0f0977400ee92e710e004874e1e823
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:12:51 +0000

    mail community post tax church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
