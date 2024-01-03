71aa37ac6385932d588b8d075b5b549ddafface1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:43:27 +0000

    bringforward close square talkinto choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
