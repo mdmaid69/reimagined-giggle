43f7b8fba94f582401d96067ae0dc56395476b45
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:05 +0000

    single operation sister freedom ordinary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def convert_to_octal(n):
+        return oct(n)
