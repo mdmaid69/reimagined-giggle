eb487ff2470689fc664db0940bfae12a9b9138cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:30:42 +0000

    healthy instruction after explain tape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def convert_to_binary(n):
+        return bin(n)
