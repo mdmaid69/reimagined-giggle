531a2011fdb2119d0a6e130314455ab3d07ee487
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:59:11 +0000

    health tongue steer news lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
