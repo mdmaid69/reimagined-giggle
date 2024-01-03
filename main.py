f45d5144e3563c4c945eecb391369c8983738c39
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:15:45 +0000

    day wire bother dull squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def reverse_list(lst):
+        return lst[::-1]
