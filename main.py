ad9297b7bcd31eab5d3814c0fb99095cc221de4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:37:09 +0000

    callat slice capital world report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
