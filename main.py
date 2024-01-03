92274d835f311f8ee4e17fc7063c3d93ddb2ee11
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:04 +0000

    impression education regional dull tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
