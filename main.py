3810e673c8cb3dcff11a708062bdb5b72bdfbfc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:00 +0000

    spot scared lay crazy shake

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
