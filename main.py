d7abefc03e05153b3fbd22ff4f9c86e2581ae028
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:34:32 +0000

    anywhere lead speak debt journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
