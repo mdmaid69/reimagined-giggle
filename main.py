436005b303494f638db56cdb742f9bb774e62530
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:41:54 +0000

    address thirsty half book invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
