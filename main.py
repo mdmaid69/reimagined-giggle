5922a2c92d03c48eff81d76000ae728f3161ca11
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:09 +0000

    lay really rub scared trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
