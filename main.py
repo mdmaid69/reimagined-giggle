3ae62653cbda615d678f6f0e20bd0c011bcf4957
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:35:37 +0000

    company near fire entrance clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def greet(name):
+        print(f"Hello, {name}!")
