5ae30c0b67b43b01e2718dc5ba85d6ed19cbaeb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:27:32 +0000

    appeal protection Vv joke edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
