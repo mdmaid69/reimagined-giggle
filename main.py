f22ee305fb108374d96519170c0d00739a0b4708
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:14 +0000

    confusing special union truth object

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-name = "Python"
-print("Hello,", name)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
