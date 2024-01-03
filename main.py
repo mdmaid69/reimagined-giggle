29c71d2730d4e14f5f27ab9fdccfbf1aabdea9f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:11 +0000

    introduction level shop care power

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
