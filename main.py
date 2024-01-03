2fb54d3115f7041f81c5a9b582487f1946ec5b66
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:13 +0000

    on opposite last edge pain

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print(sum(range(10)))
