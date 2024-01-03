62587e53509c2beb5b2ea90e773e222f5f1c5848
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:16:42 +0000

    hollow interruption turnoff appeal murder

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
