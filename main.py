4b2be7e6722d7a4ed37606ef1e1c2d8caacc5edf
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:38 +0000

    continue dance keepoff school top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
