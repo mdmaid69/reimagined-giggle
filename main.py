27ec2414cf2f83fc8b2f6219cce37e903d2c8a45
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:26:46 +0000

    sticky entrance lend trap extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def is_odd(n):
+        return n % 2 != 0
+import sys
+def exit_program():
+        sys.exit()
