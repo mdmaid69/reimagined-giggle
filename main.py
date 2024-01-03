df1b7a0fcbbba78f117cd962fcfb901a456bfe19
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:21 +0000

    doubt through murder yesterday discovery

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
