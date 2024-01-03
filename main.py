3f482a861b4bd6baf19f25346913df687cbe23b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:54 +0000

    hole tall post field often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
