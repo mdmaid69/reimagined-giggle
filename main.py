4eaa343ee43ac272dbea40abd745cf9af545afa9
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:31 +0000

    flow badly away regret official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
