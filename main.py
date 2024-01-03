79e78b170134e8e1af30ccbb1012873b093ec6c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:39:08 +0000

    ring generally detail congratulate tongue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
