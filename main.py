9ea5244a05f8e9f3e5117d28983db9f7673d2b27
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:12:04 +0000

    strict inform date hang size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
