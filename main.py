f50e723a7b19bef24953835309a2ab3cb709828c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:02:06 +0000

    lay tend mind this capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
