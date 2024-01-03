e71a44a6d9e6a37a618d0be256001066008d7cbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:00:27 +0000

    demonstrate about disappointed interrupt load

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
