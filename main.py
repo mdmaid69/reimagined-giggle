fefc5158f311b7d5ed305256e73ba7718bf4cfbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:43:42 +0000

    bye general wash read interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import logging
+def log_message(message):
+        logging.info(message)
