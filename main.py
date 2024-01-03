c2b7c1448dd139940051ef209ede37d098131c26
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:34:28 +0000

    class official Zz trial treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
