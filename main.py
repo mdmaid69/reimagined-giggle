e0ca731bc9f0e514c335ebbe8078af9137cb2a2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:38:19 +0000

    kick cap flat message regard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_power(work, time):
+        return work / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
