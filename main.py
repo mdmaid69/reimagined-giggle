a0c626fab835e7da5a2f0aba2b2611fc34edf0fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:31:33 +0000

    remark Itself tread tear keen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
