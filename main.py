66edf20c16a166425af7204d6d7c938182adbf94
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:14:49 +0000

    quality mean separate begin object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
