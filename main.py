4b4d8affef0ffcddb12410f398152207b417c645
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:28:15 +0000

    story blow art walk study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def square_number(x):
-        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
