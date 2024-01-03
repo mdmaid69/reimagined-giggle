8379aa50d64401a6517f97e7a4626c9ca3e37dc6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:46 +0000

    fun experience reduce pleasant must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
