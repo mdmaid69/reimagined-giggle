60b7f808826219631c42c92dc400a7da8e423c0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:41:48 +0000

    dance fall cut Ii cover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import sys
+  def get_python_version():
+        return sys.version
