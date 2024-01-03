34c11b371ed3e3683e22d84cfe043dbafa72360e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:53:09 +0000

    workon imagine naturally reply replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
