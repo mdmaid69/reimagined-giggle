38cfed5a7a489398a83d8f3b9365da04f924a521
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:24:33 +0000

    use slowly row estimate off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
