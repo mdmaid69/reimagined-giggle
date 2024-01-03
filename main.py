f0656e9e48600735765f702e899347ec0cbb2bf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:08:52 +0000

    science left cause tell protection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
