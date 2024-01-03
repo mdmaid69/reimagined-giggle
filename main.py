92e9774fb17f618ef4748fa5860174cac7730852
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:10:25 +0000

    train step Dd wind ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def set_array_item(array, i, item):
+        array[i] = item
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
