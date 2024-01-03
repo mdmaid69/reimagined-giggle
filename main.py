3dcac42b6af2fb9a797812d61d235e963d7a36e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:11 +0000

    teach holdup development lookup wave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
