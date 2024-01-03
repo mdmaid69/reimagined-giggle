7a4fdb501d5c140bf8db6fdecec98581ba0b1abc
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:43 +0000

    nowhere slight behind talkinto position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def sort_numbers(numbers):
+        return sorted(numbers)
