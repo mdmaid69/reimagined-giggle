eb732396a2082d4b5a959dfa74ca2771b06fde1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:27:56 +0000

    interest attract workupto speed post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sys
-print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
