eba05d34d75a35512fe46bec9e8f9b2fa5924f93
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:40:27 +0000

    pretend argue unfair market reply

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
