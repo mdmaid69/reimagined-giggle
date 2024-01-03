891d89622764fef0780752ea556ca726374d7bb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:49:03 +0000

    impressed fortunately under cost exam

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_item(array, i):
+        return array[i]
