3962ea0a8cdc948f787bc91b991702ceceadb8da
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:55:42 +0000

    cool Nn near head class

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def divide_numbers(x, y):
+        return x / y
+import re
+print(re.match("h.*o", "hello world"))
