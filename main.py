cb4ca3eed2c297c61e3df1a589366ab7da612941
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:20:27 +0000

    wheel eye considering which for

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def convert_to_binary(n):
-        return bin(n)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
