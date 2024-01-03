f8edd7430252576a5c0afb28aca78cf780cf44cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:26:42 +0000

    involve watch everybody relief deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def is_odd(n):
+        return n % 2 != 0
