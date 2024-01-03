774f0ec336cbd12b3fbe01c848c0df6d92e46bbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:06 +0000

    complete study develop apart without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
