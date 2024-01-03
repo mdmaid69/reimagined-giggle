a87b5defb664811e394483e26c4b325ffc558b04
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:52:01 +0000

    repeat see drop breakdown little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
