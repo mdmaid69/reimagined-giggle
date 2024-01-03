cd127fe0b45cc34103277a1ee48948d2f2c0bcc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:51:59 +0000

    destroy chance fail Nn immediate

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-print(math.pi)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
