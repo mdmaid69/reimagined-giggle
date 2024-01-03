59d8c11ec4099ce59d996fc0f2b09b11358fb5e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:06 +0000

    sore giveout stamp probably bread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
