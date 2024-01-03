ea923ac0671e2fc2d9e0a09797c3776d394cc883
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:29:54 +0000

    break impression fairly couple forget

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
