aa5817530a0ff25a99472f59061154ad776b244d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:49 +0000

    learn bet silent first but

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+from collections import Counter
+print(Counter("hello world"))
