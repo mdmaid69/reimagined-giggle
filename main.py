977bcb0a14c9ad375a366c6ed6a9c740b58117fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:54 +0000

    surprising satisfactory control surprise central

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
