197238f6d149da0efcf29fa9fc5e28bd3b76c40f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:08:06 +0000

    to wide such fair new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def count_elements(lst):
+        return len(lst)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
