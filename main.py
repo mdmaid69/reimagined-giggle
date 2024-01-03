c3d74a3613730846adb23d0100d6010eea959a2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:27:01 +0000

    refer much secret smoke takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+for i in range(10): print(i)
