133cc6e179609e067e4ae6120fdd3dfc416bd559
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:14:16 +0000

    year compete prepare whichever interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
