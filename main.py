7621ac04a75d5250843ff74e7b85c47c112fa356
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:06:15 +0000

    interview wall sign union phone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
