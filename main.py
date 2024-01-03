8fe47a1322eb0a98943f846a9ede8f6cfcf4dfb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:39:39 +0000

    flood trap declare contrast base

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
