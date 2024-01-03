558ab1735792ae0fa2bce27572f72376cd42d880
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:35:09 +0000

    attempt tread wealth spread present

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
