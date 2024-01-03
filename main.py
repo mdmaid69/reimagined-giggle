dd81097e7ec764adc3a011a86d4934558ab45481
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:10:01 +0000

    warn invitation there surprise breakfast

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-print(datetime.datetime.now())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
