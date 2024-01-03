683ee54535640fe0f6be1d5f53d36f1e87571839
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:29:39 +0000

    description fault increase point sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
