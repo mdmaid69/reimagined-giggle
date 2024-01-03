3652fe0392fa3fa04997638037d4966eb42eead7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:54:35 +0000

    smart him cost Jj teach

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
