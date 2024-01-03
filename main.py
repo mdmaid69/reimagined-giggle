1fb87ee384d0df5c48233a0cd4ae79f5064da9c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:36:30 +0000

    responsible prefer measure then combination

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-print(sum(range(10)))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
