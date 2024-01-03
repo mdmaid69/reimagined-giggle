b80f862f40fcb363e16e712f4c24e1c2273b193b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:18:48 +0000

    goon wash tap matter shout

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
