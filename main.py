c0f9813a954ade1611d92df22241f49ecacc24c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:47:57 +0000

    only before work determined kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
