4e13d9207a51135ca946aa0c1d3395f626485c62
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:50 +0000

    bet sail point priority no

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
