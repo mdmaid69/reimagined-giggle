855a0a1695b9362b237ba81080019b9d8b90c510
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:26:31 +0000

    exaggerate book immediate sail stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
