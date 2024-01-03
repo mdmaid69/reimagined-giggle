93d522e70e898d8b34bd645d020dbf754787ac10
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:21 +0000

    wheel guess sleep wind healthy

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
