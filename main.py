fe54a788524bf8636a0f83ce540f0f7f2aeeaf41
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:20:10 +0000

    another child angry himself comeover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
