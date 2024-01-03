dc85e89e6e1510e1dcc1ef5aa515cfc44d5ef80e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:09:56 +0000

    talk possession talkinto front rob

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
