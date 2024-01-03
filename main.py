69ead485f1ca354ff9f36fde7850bed79d705223
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:17:04 +0000

    violence dress firm hang attention

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_power(work, time):
+        return work / time
