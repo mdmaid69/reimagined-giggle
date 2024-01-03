4c45d6a531e34b94282af7d125b381cbed11a597
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:26:44 +0000

    sad slow emergency may account

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_power(work, time):
+        return work / time
