0ce322e9c8f4321eacf7f0742d5d9c2116f6665e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:51:59 +0000

    worry your wave defeat pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
