f527501634394ec80eb342887ab3a65e25e642df
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:41:54 +0000

    deaf criticize fold deep behind

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
