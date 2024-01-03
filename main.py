b3fe9c99cd2bdb4ef03c904381cb62181243e78f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:35 +0000

    flow beat friendly explanation class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
