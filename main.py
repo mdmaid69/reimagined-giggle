0442646733d5de0ffcd07c6ed6a254f3c21d2120
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:33 +0000

    professional centimetre church enthusiastic store

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def subtract_numbers(x, y):
-        return x - y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
