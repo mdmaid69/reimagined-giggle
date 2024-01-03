2a56f82b85aaa434bed921780c23a34acaf2e2bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:26:37 +0000

    collection pool fix charge sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def cube_number(x):
+        return x**3
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
