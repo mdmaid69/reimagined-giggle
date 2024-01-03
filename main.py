fd05d9b1f5cd93d8b05027fb7affee24fae0e495
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:18:32 +0000

    tired opposite her disapprove suck

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
