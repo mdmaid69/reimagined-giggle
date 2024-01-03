6fe8bf2aa6886d672cbde967e07a495de105453d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:48:55 +0000

    head step consult bang room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_str(array):
+        return str(array)
