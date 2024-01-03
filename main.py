77e82f9021ac5a30ca64f474b4c0f6fe421ca550
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:35 +0000

    prevent shrink split phone satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_binary(n):
-        return bin(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
