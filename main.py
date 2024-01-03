2764add2b1a58a4bcb8614849640e090ee540ec1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:27:32 +0000

    rubbish exaggerate mean various work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+def calculate_power(work, time):
+        return work / time
   import os
   def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
