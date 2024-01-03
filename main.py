9bf26bd113d57e7a21c4785e00b7f3200fd892a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:30 +0000

    laugh exaggerate movement provide breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_max(numbers):
+        return max(numbers)
