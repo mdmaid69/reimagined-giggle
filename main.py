8ea7e7622dedd3b6f15dfb3ee915af5a86bc9b0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:48 +0000

    loose basis rank commit cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
