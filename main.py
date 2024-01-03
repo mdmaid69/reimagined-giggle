8a10aa4a3458b05e6645935ec67525c53f8f80dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:01 +0000

    fall skirt settle however takeback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
