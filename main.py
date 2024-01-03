ab0e291783867c550c3b92c4521b3bb527052a2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:02 +0000

    slowly similar breakfast guess usual

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
