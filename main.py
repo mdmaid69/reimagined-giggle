eafaa363cb6b00b1f9d6cd8c4132a62ed80b78ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:01:37 +0000

    likely attract prize replace estimate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
