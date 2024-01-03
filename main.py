2f0e490ce99e8936561957da2a33b9fe2eac8af3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:01:28 +0000

    touch bear store Kk round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_str(array):
+        return str(array)
