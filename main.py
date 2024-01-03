fac79b4b789a86cd6cd660c3fc54c0f2ba090e85
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:40:59 +0000

    story it smile bang please

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
