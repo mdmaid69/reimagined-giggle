dcda330b026a8d0ea90017a8ffebd47cd69fb1e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:52 +0000

    mend stiff power Cc anxious

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
