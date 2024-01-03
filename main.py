b294432351b507ec83a850f9de57d805aec85ecb
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:45:12 +0000

    goaway may uniform sort earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
