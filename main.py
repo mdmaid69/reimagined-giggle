a87c1000b1cf91d673ac2a5ad4e65c6125682b92
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:42:42 +0000

    practice correct workon continue software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tensorflow as tf
-print(tf.__version__)
+  def get_current_working_directory():
+        return os.getcwd()
