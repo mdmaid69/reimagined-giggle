606a8967f1bdc25081f469a37bedf9e074f609ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:51 +0000

    breakthrough lookback(on) neck top limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
