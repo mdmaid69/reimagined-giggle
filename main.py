d28bfbaa72ec5756b6bbc5c9dd54561b9083f78d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:39:03 +0000

    leave operate total with comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
