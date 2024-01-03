66995a361415ecdbd6ea573487b8a8ff26288288
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:42:39 +0000

    danger nigga only job achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def count_elements(lst):
-        return len(lst)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
