a710c040856a745f742c10932d21e1e255764e8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:23 +0000

    education straight development watch in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
