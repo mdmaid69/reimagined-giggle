66e0df7c4237b3bd58e2da78ac4552a5fd2049fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:33:29 +0000

    to original model second path

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
