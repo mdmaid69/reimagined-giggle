1f46f71cb20146822f4d066b9fc12acba095c92d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:55 +0000

    poison read advantage private disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
