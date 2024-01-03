3c1fbc31d9496a4e064e7db15da97e044dc8201e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:19:35 +0000

    previous great regional attitude pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
