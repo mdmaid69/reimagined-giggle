927fcc27d67316aea9af1ec34c1f17f405a7cb94
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:26:50 +0000

    experiment she really yes ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
