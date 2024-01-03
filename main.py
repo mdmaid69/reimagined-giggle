e4018d501428723fea98908b896a714c285ddce4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:08 +0000

    actually another discover take involve

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
