8733639a77764b6de5947e8a4ae883631fc2c69a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:32 +0000

    thinkof shout sink wire material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
