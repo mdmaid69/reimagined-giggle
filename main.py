9307f4e7b6d85fd7a4224fbdaaf021e369d1ff59
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:18:29 +0000

    wonder promise obtain maintain step

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
