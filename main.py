09cf66314b1e07603eb2bc62b33396cf4ef23258
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:25 +0000

    tend minimum dress thankyou line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
