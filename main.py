58541b3e846f3465777d9b56a48f0c52df9d125c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:36:09 +0000

    early bank why reason remark

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
