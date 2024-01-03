0b54cbad804ac2fcd71fc703b722bd397f40c7d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:59:11 +0000

    somehow stop information exercise tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
