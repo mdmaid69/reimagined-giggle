fda102755eb864a00144b97b21d26c9143fd69f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:10:54 +0000

    fasten glad settle whether importance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
