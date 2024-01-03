23863b4f1f9fd666168cde137fd8f1a5f02e0195
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:05:28 +0000

    move side figure private expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
