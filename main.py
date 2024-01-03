26e20919013e01ea98c6d1effd09fc2a4994d900
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:03 +0000

    pause report land flow injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
