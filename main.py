44712c5b22059881e7cf20ae3a6800564bb14270
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:15:30 +0000

    until generous niggaacademy wind pass

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
