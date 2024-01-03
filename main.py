2a4dcd8d07c00f20a417fe7db5e55ee747e8365f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:15 +0000

    rescue rather light sort convenient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-print(os.getcwd())
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
