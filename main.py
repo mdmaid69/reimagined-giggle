c2627922e80e359af18454e0326387f879cb251d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:42 +0000

    refer visit bored point poor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
