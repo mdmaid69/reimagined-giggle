3a91a5652bd23386311a90d9c1c15dec0b4f97c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:13:06 +0000

    slight talkback understand situation where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(lst):
-        return max(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
