2323c044bd0642ebb85c8402c10206f496a70e9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:29:06 +0000

    do task speak slice feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
