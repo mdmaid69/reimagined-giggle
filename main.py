2745655b49699e7c567672bf90548b4cbb42147f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:23:46 +0000

    you exit plenty bye confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
