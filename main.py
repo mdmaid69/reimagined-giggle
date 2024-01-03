39d8cd958f45f5baed5497d846764c3b18872e32
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:13 +0000

    shock count runover win believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def convert_to_binary(n):
+        return bin(n)
