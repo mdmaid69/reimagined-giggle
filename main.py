fe37660bc6950b2a611d48db9e3a44222fd586de
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:27 +0000

    copy building talk stage turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
