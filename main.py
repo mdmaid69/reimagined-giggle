858a61b91b59f9a97491955abef2467b2b3a3436
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:56:59 +0000

    speech recent violence wear approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
