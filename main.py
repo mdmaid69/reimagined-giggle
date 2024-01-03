75521ccc123935dc85abde4ae81182afe4003809
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:25:23 +0000

    smile live century unpleasant hope

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
