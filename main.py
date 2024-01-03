31de8658096d617f0db8ae9b7e00c636396f6631
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:36 +0000

    powerful trick tonight lean method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
