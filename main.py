9ad2de3e3c38a67207cfe93f3822141720915da7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:52 +0000

    address mixture town Pp chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
