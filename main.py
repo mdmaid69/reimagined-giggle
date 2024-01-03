203568c8eca68919b89978a224970d347e433748
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:10 +0000

    wish organize heat welcome the

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
