d23c5c1a8587f37f004bf28c8c08cdb4d4ced94a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:33:15 +0000

    protection anywayanyhow certain force jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def find_max(lst):
+        return max(lst)
