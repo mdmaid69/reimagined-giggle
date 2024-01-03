b50055afdea7590c9e8462ed9d0caec842add7b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:25:15 +0000

    constantly spot quantity switch support

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import sys
+  def get_python_version():
+        return sys.version
+print(sum(range(10)))
