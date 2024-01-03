867edfe9d8ec923b698abb634c9211c8a2ecd32e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:39:10 +0000

    thankyou illness long takeback anybody

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
