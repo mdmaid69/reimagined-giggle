dd2d9c3936811812c39506ac43f8fb3c8a8e68d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:19:50 +0000

    sign dream set weigh majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
