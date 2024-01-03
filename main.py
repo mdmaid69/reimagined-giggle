32a9a88d883cdc49e22214b6d33c9f8d016ea3cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:20 +0000

    doctor project worse line convincing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
