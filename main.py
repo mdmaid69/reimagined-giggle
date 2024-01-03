083578353844730a00431d96aa65b4ea59a10210
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:10 +0000

    state lie they normal hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def is_odd(n):
-        return n % 2 != 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
