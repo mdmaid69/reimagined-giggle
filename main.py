51ccd6c7abbd08403b46c42761f86abcca181bee
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:42 +0000

    discover true like room Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
