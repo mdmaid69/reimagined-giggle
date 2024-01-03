019d22692e03e78d3fb048e9e886a5f4bd77eb5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:44:37 +0000

    attention proof walk report turnaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
+def find_min(lst):
+        return min(lst)
