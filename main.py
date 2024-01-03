ea7fe8307667059ba2e61da7ce38ec33624992e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:34:09 +0000

    closed matter mistake completely cheap

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
