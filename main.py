f042db0db5c3b1916cbafcc0cda7ce6a5e5637f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:46:30 +0000

    none easy tomorrow goback(to) face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
