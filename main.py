00555de1bfe5a4caf33377aeec77d129dd50e154
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:52 +0000

    eager paper every forward cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
