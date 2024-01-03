6e7ebd9f183ffd7a8505898942c3ad15ea51f3bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:02 +0000

    myself mysterious will thorough change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
