b081e3cd91561a856bb6614ef314a348238a367f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:05:01 +0000

    house exist train exercise marry

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-print("Hello, world!")
+def find_max(lst):
+        return max(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
