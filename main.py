6d58398d0d9521d00917cf6e06ce934ac8a09224
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:21:53 +0000

    consult ticket instrument goon sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
