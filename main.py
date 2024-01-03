48dfa6f6541d933972978603e381794d56afe195
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:00 +0000

    here journey nurse along shopping

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
