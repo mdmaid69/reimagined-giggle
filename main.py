cc27bcbcb42b9c9861194a716e0e9aa07a72f835
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:45:03 +0000

    emotion light branch tomorrow adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
