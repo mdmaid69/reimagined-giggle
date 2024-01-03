67b1c72809a532e744c3afbf7b4a8c659192b346
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:17 +0000

    ill pile warning lock like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import datetime
+print(datetime.datetime.now())
+  import sys
+  def get_python_version():
+        return sys.version
