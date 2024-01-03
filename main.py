3e857b1c0e2ad789cb5d2ed1c6ea55fedd905bb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:38:23 +0000

    power at wake suit Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
