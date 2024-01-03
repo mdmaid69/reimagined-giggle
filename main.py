308d6808c208af91d5e61dd99972b1e8bf7847c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:08:15 +0000

    mass area powerful theirs flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
