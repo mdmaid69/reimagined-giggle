a836d0e0691f8dd4bf3f0f29d251fd5897c0f1fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:32 +0000

    port keepoff sensible strike possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
