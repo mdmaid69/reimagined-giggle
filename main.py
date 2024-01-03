e850a2e10c1c69797e93450457f2c7d2656cb28f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:19:27 +0000

    step to upset certainly lost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
