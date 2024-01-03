34ec86d3e121a5289a5832091f88976908c95698
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:43:17 +0000

    evidence question yesterday whole wait

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
