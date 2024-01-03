85716917a29b9e009adf5088905bb08888e4b2b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:35 +0000

    top complete treat normal everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
