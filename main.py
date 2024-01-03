a6c2a36893346096703be45304629fcc5ee5cae4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:07:42 +0000

    swim suckme stick communication guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_as_list(array):
-        return list(array)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
