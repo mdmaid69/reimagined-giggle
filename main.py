f3fd1aa928395ca5d977235a35e0a47d7a3991ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:40:21 +0000

    goup joke workupto breakout constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
