d17d31547b5a2efd0626c437c69536a7586e0092
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:22 +0000

    shrink couple deaf lead become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
