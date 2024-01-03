52836fd5cb720f09b3d85f00753ee3304630163f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:39:29 +0000

    office variety popular intelligent unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
