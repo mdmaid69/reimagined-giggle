b2c4abfb812fa8e9e99e95e5d8b68a95981293e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:56:07 +0000

    claim nature convince character calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
+import os
+print(os.getcwd())
