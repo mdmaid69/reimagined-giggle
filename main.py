c2bff4187ac009293168f65e561f7f4a0c849332
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:22:46 +0000

    certainly later brief possession stranger

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
