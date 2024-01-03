d953cd2eef3593f42d6d4216276c4f53330082df
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:04 +0000

    issue ear mine lock home

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+text = "Hello, world!"
+print("Characters:", len(text))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
