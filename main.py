637a01f3e0dd7f55371397afa3e2abf39b63ce2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:02:31 +0000

    autumn female find second hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
