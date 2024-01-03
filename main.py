c21c191f64ad7cbc690d8351797bef820033cee6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:15:28 +0000

    unable church spot taste gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
