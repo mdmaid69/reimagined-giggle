d1e9929ebffdd5d3553b6778bb4a54c8259f4417
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:50:45 +0000

    pay possibly lie strength push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
