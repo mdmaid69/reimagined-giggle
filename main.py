0d00b75dc6faeab41dac1b58d74e507adcbc7b5e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:54:27 +0000

    mystery regard suggest taste brother

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
