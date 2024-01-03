fb291c351d9cd9465ef59e81e3afc6b24a38112e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:22:26 +0000

    fear copy comment takeout grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_index(array, item):
+        return array.index(item)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
