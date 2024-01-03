9615f806abb95601e8c09504e183e6879b09b273
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:38:36 +0000

    advanced take report failure environment

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def is_odd(n):
+        return n % 2 != 0
