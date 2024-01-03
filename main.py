adccf04ef5b87fd36dde2c7282ca9ea9e241ce27
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:00 +0000

    track Gg fence main nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
