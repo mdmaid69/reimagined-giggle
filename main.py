75cdbe6cacd29e72847d43daa7266c767f161f2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:37:49 +0000

    moment me stamp lock whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
