edb0f0945dbbd7cefdf7beb1c0030f88461ba08a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:31:13 +0000

    up narrow last earth furniture

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def reverse_list(lst):
+        return lst[::-1]
+  def subtract_numbers(x, y):
+        return x - y
