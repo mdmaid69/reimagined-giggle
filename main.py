287f27903f8a6563d44fafef5ed7e08bea347ddd
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:24:31 +0000

    long next pure kind family

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-for i in range(10): print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
