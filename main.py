66b6f7775585b0a7fa7fbdd75a09d305bd4d8b02
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:42:13 +0000

    sticky serious correct high solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
