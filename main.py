45cfad961383acca05c16c2b2ced19aef8da9fb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:07:53 +0000

    throw reliable trust original decision

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import sys
-def print_python_version():
-        print(sys.version)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+print(sys.version)
