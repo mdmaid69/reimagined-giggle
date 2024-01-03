f2253868adb317262852042f1d817258a71b1a53
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:07 +0000

    recovery claim divide whoever change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
