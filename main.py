f020de14c92177bc90606bd0cf2b5c8ed37faa15
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:45 +0000

    tired reasonable continue black need

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
