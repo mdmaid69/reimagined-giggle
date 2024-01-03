92e145f58a60fb21a7126f85fd5c877743dac6e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:47 +0000

    part plenty floor refer except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
