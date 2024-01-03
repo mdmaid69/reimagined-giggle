10167eac4b1ab9febb17eb47873637c06a9b2750
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:15:20 +0000

    flat purpose uponon into well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
