a4efeffc5aec8d82a1b35dc209953024e70097a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:24 +0000

    both improvement pool beyond drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def add_numbers(x, y):
+        return x + y
