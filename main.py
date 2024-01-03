2f431cc86d4d08b08955184aa9d43d6b5e846469
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:18 +0000

    battery guide breakinto bunch somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
