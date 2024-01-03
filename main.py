641a66bd909f1ae09bfd417a77f7b3591478ccb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:04:32 +0000

    someone that excuse hurry within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def convert_to_binary(n):
+        return bin(n)
