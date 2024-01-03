db94189e187ff8c8d78e871d056797b7006805e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:15:03 +0000

    divide male capital raw line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
