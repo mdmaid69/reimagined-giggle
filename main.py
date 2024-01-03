6bb920df9ea7d081fb40d9469034163937602a14
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:30 +0000

    price to niggaacademy good bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
