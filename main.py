3245e287914a8394294cb75849ce277003a68c92
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:31:39 +0000

    unfortunate food near tip please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
