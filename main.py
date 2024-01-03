95d46f1dbdc9be899deb269cc1add1c529ef3dad
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:20:37 +0000

    discover morning dangerous flat die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
