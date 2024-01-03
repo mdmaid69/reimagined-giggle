58cca02a0a7ffb6ccb876ce2cceb0b3769e5dc14
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:47:20 +0000

    content from rather this kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
