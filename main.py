032b702e9dc9d92c86c286ea6d879635e908e881
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:06 +0000

    provide brush worse coat otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
