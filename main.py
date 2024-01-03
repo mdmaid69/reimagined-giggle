9631b9000112b644ddb499573daeb17725bbe118
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:55:59 +0000

    division practice increase admire arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
