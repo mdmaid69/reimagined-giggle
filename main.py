78787191305b617a00f9deed0af6cfaf022c70c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:01:26 +0000

    extra advise whom past beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def cube_number(x):
+        return x**3
