782f3136805b43984aaf1404e1cc4ece1b90cb68
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:26:29 +0000

    team escape finger calm wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
