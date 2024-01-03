a5285d452448685a0221e1990a7e284330e39c76
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:56 +0000

    let boat uponon section second

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+for i in range(10): print(i)
