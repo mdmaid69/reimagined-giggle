81199d1f4e2e5b2ce00e83b5af7582b7cf7f0162
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:36 +0000

    lucky near male harm offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
