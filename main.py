e18529772a2207c24693404a1ea85167391ba451
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:43:43 +0000

    Kk whether place test pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
