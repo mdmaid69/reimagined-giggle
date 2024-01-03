6ed6600ef1e368f812bc8cd5569df1fae75a5440
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:25 +0000

    beat bang lesson arrival describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
