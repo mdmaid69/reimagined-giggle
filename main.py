e9a5853f2a258cd40c2feb34db572b7460c66b72
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:23:11 +0000

    sad 566end turnaway several cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
