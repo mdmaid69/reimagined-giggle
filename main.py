84c2331d016eadb84e60caa63b6f9c35e8cefa27
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:10:21 +0000

    learn cook need traffic professional

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
