5c9b23ce99572789711fe8c85cc18acffafcf6bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:57:01 +0000

    age pretty whenever represent place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
