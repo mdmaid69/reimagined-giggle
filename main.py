278c42b053a1d61a4fb63a4183828a49411c925b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:07:35 +0000

    now accidental yet turnover 572enter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def add_numbers(a, b):
+        return a + b
