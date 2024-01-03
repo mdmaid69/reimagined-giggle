b4f62be7eeb6917d702f78370c7033977f26fedc
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:57:50 +0000

    wake steam Ii confusing reserve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
