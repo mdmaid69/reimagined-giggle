76d7ef12d38b43ca1310f9b5d6383e4dc7106d2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:03:11 +0000

    clean morning off pay pleasant

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
