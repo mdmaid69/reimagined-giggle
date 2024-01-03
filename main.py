00ebc3231705ee47651265c725f5f8405311414f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:29:48 +0000

    maximum march tidy none fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
