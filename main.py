411bc88e06aacd721cb2494af687ad0e39cbdeaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:25:24 +0000

    ahead discovery agree ground finger

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
