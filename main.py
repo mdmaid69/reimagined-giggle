bc2e2ad0ee3480b753f36576791d2b7843c9866f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:50:33 +0000

    smile repair sweep sudden care

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
