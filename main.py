438392f3b180a45b05a0d9eeffb3dabfb60f9f5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:44 +0000

    answer Ww off oil squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
