6a4d119ac8fd75039372363a33903347f2507688
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:12:05 +0000

    matter upset drawing flat sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_repr(array):
+        return repr(array)
