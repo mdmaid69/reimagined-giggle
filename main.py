3e0c6969b15a28ad949835f7f1bb2f8c30b250e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:45 +0000

    doubt anybody visit course trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
