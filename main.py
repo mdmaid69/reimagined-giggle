2b454f6d9b869d76f95b32aacbae7250289c6ea5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:43 +0000

    control crowd colour consider tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
