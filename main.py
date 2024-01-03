81af43558cee154058b7707f0439a0cbb310a9c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:54 +0000

    petrol reason arrange phone hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
