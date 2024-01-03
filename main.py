fa2a543cb4f25066de71bdcfae709c37ad6307dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:02:11 +0000

    distance church plastic over as

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import platform
+def get_os_info():
+        return platform.uname()
