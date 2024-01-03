4966a437d67635afd0a7f152957a613150cf54ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:27:41 +0000

    under street wing takeback contribute

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_min(lst):
-        return min(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
