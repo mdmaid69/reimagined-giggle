ce2d9dc2913c833a04243c71b14819b5a9df5cfb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:12:37 +0000

    care instead mention thinkthrough watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
