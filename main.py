63f5aab5a2864e24acb61090af17168b01d302c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:32:13 +0000

    like measure search hurt after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
