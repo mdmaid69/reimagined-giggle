5c9d35357ccd3ee529c7d258baca8e92b30c4bef
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:28:19 +0000

    geton pile sensitive game fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
