4edcd293299406a46b0d47a18e89676e2ed1c672
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:07:03 +0000

    sit continue supply careful apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
