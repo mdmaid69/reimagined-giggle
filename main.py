4d4a7e3aa0dd30ec235210a257fd67b0056892f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:13:53 +0000

    tread win suffer free usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
