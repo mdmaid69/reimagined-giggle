9b799f39009789a274f71d260887120471cad599
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:27 +0000

    which today direction expensive bath

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
