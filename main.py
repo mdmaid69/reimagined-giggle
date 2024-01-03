f927198f9a4e92a7ff128c31be6d56ce31cf84f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:25:35 +0000

    tip insist defend unkind pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
