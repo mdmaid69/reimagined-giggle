d1732cf9de8ef778bea4d927ee5f36c23fed01c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:41:00 +0000

    frequent accept experiment issue help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
