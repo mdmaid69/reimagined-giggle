6e9dd785edc87676b10a6ec859b35b9b5c60da16
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:45:25 +0000

    shut destroy deliberate positive tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
