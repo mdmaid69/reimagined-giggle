9b0694e022e8ace00333289885be5b66d4ef2c47
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:53:19 +0000

    will better nonsense address couple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
