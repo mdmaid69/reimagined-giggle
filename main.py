7d719af64c6cf4d29abbb8dbea8b36ca1ca9ffc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:53:55 +0000

    part dry measure watch thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
