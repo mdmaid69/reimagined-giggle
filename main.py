4937de9391ea4a48d91bb64cf64c78108ec2559e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:22:55 +0000

    congratulate own obtain our usually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
