6f022a4d86c9abc47b9396def9bcc941df0fb71b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:11:44 +0000

    peace invite rest plan any

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def convert_to_octal(n):
+        return oct(n)
