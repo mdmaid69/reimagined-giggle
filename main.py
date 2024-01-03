256d39e2f470f8730fea5cd489eeef1bd776f280
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:35:14 +0000

    apologizeapologise improvement finish turnback flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def roll_die():
+        return random.randint(1, 6)
