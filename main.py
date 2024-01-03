02a072b4d5541a6056582f3e82de181624a4a063
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:19:29 +0000

    public home week fierce display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
