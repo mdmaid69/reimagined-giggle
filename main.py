0fbd394caed78ca9e850642509ee7763e2ffe995
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:48 +0000

    good unit limit takeaway later

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
