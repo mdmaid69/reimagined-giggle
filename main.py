3244dec02c4209f5513ea364732af0dfe681a9b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:40:09 +0000

    sort full past there go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def find_min(numbers):
-        return min(numbers)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_volume(length, width, height):
+        return length * width * height
