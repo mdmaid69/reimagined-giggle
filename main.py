cf13ad305281a0749766f47aca543578eeb8e450
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:59:24 +0000

    near interested variety add extra

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sys
+def exit_program():
+        sys.exit()
