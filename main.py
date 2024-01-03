5879a5741a534e4bef30949d60e36d226633b3e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:51:39 +0000

    bear hungry serve plenty tough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
