ec8c4d140c7124269a369cdccfbf717e6be2dfbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:45:54 +0000

    prepare winter may this wide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+print(sys.version)
