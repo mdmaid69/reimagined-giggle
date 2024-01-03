5222a2523fdcdf8d56666eb6c698cfe0598c458f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:58:28 +0000

    organization now term callout spin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+def subtract_numbers(x, y):
+        return x - y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
