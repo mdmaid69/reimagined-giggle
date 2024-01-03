75e6c13a428f23b1987e163176c31012d174d836
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:01:28 +0000

    suit cover embarrassing trust nothing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_acceleration(speed, time):
+        return speed / time
