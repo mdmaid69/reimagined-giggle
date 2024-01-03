bed541dbd0427d2d41697271e8b8e790e9777834
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:02:57 +0000

    straight board patient speech emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
