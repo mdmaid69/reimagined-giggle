51bb793e5826283ff40ac329c7cefb22f86343fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:39 +0000

    detail imagine plenty circumstance OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
