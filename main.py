6c2267f1ee2c7bf7ab738a6916f946a6bec9e473
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:38 +0000

    apologizeapologise holiday trip dark die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-for i in range(5):
-        print(i)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
