9dbe8df1652ae71501f0d77c084bb3f68c112fc9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:54 +0000

    consist view send knock violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
