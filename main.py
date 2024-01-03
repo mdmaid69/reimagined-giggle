9fdc7262abf20f8c21f128c96fa4c1dc76b4358d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:07 +0000

    chase up very sink its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
