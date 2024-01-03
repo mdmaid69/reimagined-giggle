cf5efd510f28e79ce3e32db2019a04a15e5471b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:17:22 +0000

    fresh character considering record example

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
