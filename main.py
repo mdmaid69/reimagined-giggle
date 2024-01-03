859929cc2d0eff39887b01d108aebb44fd196707
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:27:57 +0000

    wrong accident pile image guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_unique_words(sentence):
-        return set(sentence.split())
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
