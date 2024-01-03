cf2c32e5210d5d21929fd119439fb5eb78e3bbbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:12:12 +0000

    getback really count first passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
