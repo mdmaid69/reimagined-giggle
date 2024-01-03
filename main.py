fd374e63f57c97af83cfe00e863016beaad7c726
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:13 +0000

    circumstance straight communicate hit weekend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
