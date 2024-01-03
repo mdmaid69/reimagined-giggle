a19734e2d448e82df9916b4513b3d02748d31a6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:00 +0000

    block earn clean achievement home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
