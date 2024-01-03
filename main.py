54ff29ef3f9a237478a707385830455afca632e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:37:45 +0000

    blind over concerning meeting relationship

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
