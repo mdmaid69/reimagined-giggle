debd1d7564acafc8b444a5207ab7268df0c40fa5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:57 +0000

    pile shout unable bridge geton

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
