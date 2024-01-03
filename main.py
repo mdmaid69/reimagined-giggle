c0ef66896869435fbee695867eff761db34f4162
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:41 +0000

    spill shadow charge upstairs bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
