34385bb3bf0c461e2e74c197e6245e5c3e856a5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:07:33 +0000

    illness roof although cold spring

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
