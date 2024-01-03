887a816c95ce280c59d224a9e95a1d601a3388fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:21 +0000

    year clue rule contrast stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
