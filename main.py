fa1f4561099f78dbee5a241fad914320f2c0606f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:10 +0000

    not foreign speak skin move

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
