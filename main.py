115971b274ee214140c8b141ec8b2d55d31df638
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:02 +0000

    so watch objection place squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
