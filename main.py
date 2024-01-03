cc4bd535e04561474fec009609f89f25e035d53a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:04:28 +0000

    whoever over walk no me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
