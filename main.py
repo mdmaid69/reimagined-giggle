7b0222e665cff927a40ff5ae21e28a3fd5a42440
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:43:50 +0000

    every disgusting fire last create

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
