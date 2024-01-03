0e890786c0b84694df52aaf4df554da156b6595d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:28:40 +0000

    check rank character tend disapprove

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import platform
-def get_python_version():
-        return platform.python_version()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
