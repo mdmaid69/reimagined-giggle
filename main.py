36128654525b89db1d459ecdd7bad9fd864da926
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:43 +0000

    ask store see door clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def divide_numbers(x, y):
-        return x / y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
