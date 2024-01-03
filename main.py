0a551cdcf192a42092353d88ec30e24729a8a87c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:51 +0000

    apart joke Vv own later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
