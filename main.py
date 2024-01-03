d0f4a570fd4690b6b9abb6683e40211fc1c69bc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:50:26 +0000

    neither exercise market future treat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def convert_to_binary(n):
-        return bin(n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
