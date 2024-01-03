207f395172fafac661122342d3ab9671c2e6857a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:11:38 +0000

    recover bottom can say butter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_density(mass, volume):
+        return mass / volume
