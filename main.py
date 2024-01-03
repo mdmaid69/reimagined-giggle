5da106eb0b87656bd9dc62d77572b4094e95695d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:12 +0000

    pump unhappy reliable 567enemy provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
