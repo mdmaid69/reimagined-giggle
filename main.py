3d1018578d3bb7ebb38e7298235b34bdef207cfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:07:32 +0000

    share general noisy pleased stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 def reverse_list(lst):
         return lst[::-1]
