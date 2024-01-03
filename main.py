25f5ba22462c79ce56acc4251b692b20532c7d45
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:54:49 +0000

    visit better drop special score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def extend_array(array, iterable):
+        array.extend(iterable)
