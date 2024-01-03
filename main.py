5bc58fee6279d22a5b960a7edd0b1c4c14e38ea3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:48 +0000

    wing yours aware crowded interrupt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
