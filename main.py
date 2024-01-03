46124366237412f4ab37c39d7a7757847fd6debc
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:02 +0000

    inside nor thinkthrough standard steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+name = "Python"
+print("Hello,", name)
