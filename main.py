f7006a01ea67605fb5b636743bd135669edd62b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:59:26 +0000

    yes create far throw extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import time
+print(time.time())
