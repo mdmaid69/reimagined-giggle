46ed2db0c968023cf666ae22321fff2740e0258d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:48:30 +0000

    hit on thinkthrough constantly whisper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import sys
+def exit_program():
+        sys.exit()
