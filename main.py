28dcc99f0f428ceae8cba23d0d7b908958f0d64c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:54:12 +0000

    tidy debt within necessary message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
