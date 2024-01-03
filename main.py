ea39ada24b79421165b0f509026e59597d29bdc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:25:21 +0000

    benefit bringdown raw trade turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
