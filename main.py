88041997bc61c6b6e97f5cfc7c939b276b672ccf
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:11:18 +0000

    negative cheat rush care city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_set(array):
-        return set(array)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
