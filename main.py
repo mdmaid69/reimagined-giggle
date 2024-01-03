69bc904430171507c19ae52226fea5eb631d06f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:22:23 +0000

    still opponent 572enter far manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def find_min(numbers):
-        return min(numbers)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
