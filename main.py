47cf1ef8524467781f6c02962ecff9555dd742b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:03:09 +0000

    general last turnover there difference

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
