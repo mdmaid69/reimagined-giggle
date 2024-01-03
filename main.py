db4f38e176afca6756ea3c0d512dea8ed7d916f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:25:43 +0000

    fast date play estimate nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
