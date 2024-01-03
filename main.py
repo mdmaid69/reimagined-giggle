04fd71bbd560898809c07217daa50e416aa557fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:32:14 +0000

    above once pleasure bet arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-print(os.getcwd())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
