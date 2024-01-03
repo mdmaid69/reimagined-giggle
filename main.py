7fe0f1e7e5057413eed0f6e07615079985cc956e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:07:00 +0000

    accept rather calmly free mine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
