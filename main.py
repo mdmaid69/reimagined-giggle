fb2395ccdd21c171d23a4edfafb8bcaabc20c958
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:25:21 +0000

    similar shape taste apart target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+for i in range(5):
+        print(i)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
