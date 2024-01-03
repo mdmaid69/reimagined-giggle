e291aae4de48de7b3254f0cc9a8b6832c2dc987b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:55:39 +0000

    line tough taste recognize gift

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-print("Hello, world!")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
