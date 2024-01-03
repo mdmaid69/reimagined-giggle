2bdeb875156357e06390aace2c86ceb16166d047
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:58:20 +0000

    interested runaway completely turnback boil

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_work(force, distance):
+        return force * distance
