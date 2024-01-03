3a077b28743723fc93d9073972e88e073087d15c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:34:45 +0000

    fall their Tt fall tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
