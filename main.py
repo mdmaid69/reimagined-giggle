5ed6f0019e22a1e58cfd4cd49cddeabef6baa469
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:25:27 +0000

    bringdown sport scream machine pollution

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
