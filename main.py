bc808603f656ddee46f4ddad98c7e9c3c584d2bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:17:33 +0000

    better class turnaway special character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
