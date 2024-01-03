df1fd47fc60d00a57532fee27c06a3b5844ad577
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:38 +0000

    hole classroom arrival pour admire

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
