7eb8b3093db2340fbf81243fefc0282a9d24ba63
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:19 +0000

    evidence foreign heat actual radio

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
