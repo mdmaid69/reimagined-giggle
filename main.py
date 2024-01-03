1ff26ce3391eb1bfceabee9e57b26a7e149a2b89
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:58 +0000

    slide edge afraid mistake scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
