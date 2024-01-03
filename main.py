03290d57f65ca5f18f52e6e6107a8d1f2bdc10e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:49:52 +0000

    dull gap roll turninto healthy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
