dc11c9d05d0f7168a3b2ef8c96bed2d7309324d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:16 +0000

    reaction building holiday list pass

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
