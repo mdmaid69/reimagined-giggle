5f8b60d547fafd413e60367b8a5d65a76760fb56
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:43:25 +0000

    sudden season goon factory hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
