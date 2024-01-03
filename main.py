f6f907b665d80a0c603b14c8b207ef08b1d258d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:03:33 +0000

    swing interested moral contain last

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
