9362857b8ce4cef7a53951ccb6d5ca10f12640dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:52:09 +0000

    theirs squeeze character cry suck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
