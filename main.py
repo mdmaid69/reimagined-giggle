292a43185b7c98ebca67e3f0b19477de0772a3f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:55:28 +0000

    naturally flat bend float report

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def convert_dict_to_json(d):
         return json.dumps(d)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
