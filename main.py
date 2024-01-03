db40269ccc00cd1c5ec7b174d7ead9b159ff0629
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:35 +0000

    phone tough image boring deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_pressure(force, area):
+        return force / area
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
