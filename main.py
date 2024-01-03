486209909ab4d28d80184a388a5cafd55a8d5455
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:41:29 +0000

    track plan pure straight rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_queue():
+        return collections.deque()
