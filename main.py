b6c4e76803826aa90622042e6ce72cbdfd34ca5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:00:47 +0000

    pretend left chief OKorokay shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
