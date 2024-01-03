f9ba34510a4dedf21410413e8efea7605c7ea39f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:01:47 +0000

    deliberately dare preparation agree forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_current_directory():
+        return os.getcwd()
