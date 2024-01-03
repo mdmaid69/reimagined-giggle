90164d4024e5c839b0e75bb91c04d6ee6ef5894a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:06:36 +0000

    forward want takeout over address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
