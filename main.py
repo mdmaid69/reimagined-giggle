6b78867b9ff9609101c056a1ee0399134eba6150
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:55:19 +0000

    getout rubbish bury tour last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
