a1de5176edfb3fb87da290f0c3f0dc7e6e5ef10b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:38:38 +0000

    must comment total loss means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
