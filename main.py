c453e0400de0491c5c10eb5370df84f8aaed0dcb
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:28 +0000

    right swim without majority Ii

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import numpy as np
+print(np.array([1, 2, 3]))
