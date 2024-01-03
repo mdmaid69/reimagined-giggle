244d6e7d1a763c8779ffe52fa059eb12cad8171a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:14:59 +0000

    smell factory head burst tight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
