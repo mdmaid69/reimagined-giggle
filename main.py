e5379cc785956352f4d1effca88cdf891b2e8e27
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:59:45 +0000

    promise shall project smell guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
