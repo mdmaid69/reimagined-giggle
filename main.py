6b6963abb95ac2880247a1b7fbcf1c7c48961d50
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:39:59 +0000

    relax practise offer exaggerate any

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_power(work, time):
-        return work / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
