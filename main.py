854c1f5b78ef0fde5b015a22ca2d0348a89cff41
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:51 +0000

    comfort negative open average actually

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def is_even(n):
+        return n % 2 == 0
+  import sys
+  def get_python_version():
+        return sys.version
