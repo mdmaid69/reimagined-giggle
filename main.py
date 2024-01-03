e5d072dcea1d6c86810ab3c59381994a3f8fbbe2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:04:16 +0000

    legal unit spring brush seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
