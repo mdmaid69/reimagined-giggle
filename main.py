5a64a9b1a0b0480c2fd97c4358f6ddd638210917
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:31 +0000

    row degree when point classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
