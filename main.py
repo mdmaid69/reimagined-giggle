b1716dfe0680034823c3cdf485dd552e449468eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:15:09 +0000

    place embarrassed relation ask act

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
