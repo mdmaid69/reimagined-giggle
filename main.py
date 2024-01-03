b3c7abcc381a783d1cc20c7c757043947ba48dcb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:32 +0000

    shadow see Uu print discover

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
