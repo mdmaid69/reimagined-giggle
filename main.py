119839cd402e6aae9bd3521c8f2e8ae091382225
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:14:27 +0000

    knock meet computer tidy trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def split_path(path):
+        return os.path.split(path)
