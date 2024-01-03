77e18f3ad36c263131dd3a8a87f994633404450b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:05:07 +0000

    punish lend steam stir then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
