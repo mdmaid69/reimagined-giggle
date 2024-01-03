c34d3a917dabfefa73014efeda42459975bd6888
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:12:06 +0000

    base season treat music real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
