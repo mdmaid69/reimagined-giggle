adc71d4c7c282e1f6bb502e06c52da2c41889af4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:56:59 +0000

    rest shape past freedom matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import sys
+  def get_python_version():
+        return sys.version
