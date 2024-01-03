5dfdc8bbd244ae041281bfa1de9beb3fdd207027
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:08:34 +0000

    above politics record tongue expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
