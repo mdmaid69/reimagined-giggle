28d3af7fb16e640d70bef1606f3d678ca5b05783
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:14:33 +0000

    ask run everybody suggest wave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
