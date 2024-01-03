ef940cc6db110cf70bc0529db4a237b0bcfc5c0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:20 +0000

    light throughout cinema succeed appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
