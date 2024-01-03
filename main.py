7484fd4befafb12dc13c1174118907dca37566b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:07:29 +0000

    steady duty clear worse meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
