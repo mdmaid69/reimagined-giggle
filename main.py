fc6f1769141b1fa06c28db9b7a330997101675b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:12:18 +0000

    information pressure touch card hang

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
