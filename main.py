4481fdc5ef4384254c2eb6ffd0eed056963461ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:38:03 +0000

    hollow appeal middle runout talkback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
