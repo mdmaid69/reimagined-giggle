ebab9a9ab5a9f49de39d6d63dd192116d717bb52
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:25 +0000

    factory bottom bringup hope herself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
