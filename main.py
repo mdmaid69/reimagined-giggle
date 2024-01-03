85ed6c82d62e182d1bddea761425deb35546ba06
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:53:01 +0000

    turnaway now load television holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
