6fbe8a5d8434e219e8e64c832093b92f55369801
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:10:01 +0000

    lookafter drive cough distance drop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def is_odd(n):
+        return n % 2 != 0
+import sys
+def print_python_version():
+        print(sys.version)
