d95906bc4082a2c70d00cadd49951c5214a19f86
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:31:48 +0000

    thumb buy suit explanation seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
