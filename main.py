2ba323c87cfad58955c183ae3cc8aac406e3ee9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:05:51 +0000

    delight black mysterious declaration body

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
