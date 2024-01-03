f42acd81e37c4d1ec672aa725a0a910a8aca7669
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:22:21 +0000

    enthusiastic line fear race bottom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def find_max(lst):
+        return max(lst)
