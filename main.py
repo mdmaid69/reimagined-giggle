a2beba7418f450ab348882cdcbe84e616ca8cb54
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:36:37 +0000

    brush also modern myself style

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
