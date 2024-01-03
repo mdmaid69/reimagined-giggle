49d9bc2b9d9ea0ed5a488d3096d3f2408f6997cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:07:47 +0000

    him never method goout close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
