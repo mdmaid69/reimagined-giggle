e4e5c404fcf44dfb08a61b4c7fda6f1f908b9111
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:42 +0000

    pump before like steady argue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
