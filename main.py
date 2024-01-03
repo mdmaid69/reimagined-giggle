03f33b1f8471ec665a38841c29dc86671712f9bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:42:59 +0000

    direct whoever correct return pile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_list_from_array(array):
+        return array.tolist()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
