d8933f96c3e6cc1defd8e13cabad2166aadd1051
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:57:01 +0000

    spring depend everybody no set

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def get_array_as_int(array):
+        return int(array[0])
