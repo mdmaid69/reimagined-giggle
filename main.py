dff78c8e04e6bb3e3f1fa59a1d83287d7834fba0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:12:45 +0000

    lookafter match upset knowledge comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
