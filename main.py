99280b14950c55df8cc3d1a2213eaddf3e00d539
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:05:17 +0000

    ought sad fashion whistle mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def add_numbers(x, y):
+        return x + y
