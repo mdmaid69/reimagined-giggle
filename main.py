42bf020e050e4712fc292eed20a0a46eec36c102
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:17:26 +0000

    dig fall paper surface market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def count_elements(lst):
-        return len(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
