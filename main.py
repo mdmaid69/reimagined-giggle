ba8092d601fcb53626861eb479e4f62ef433ba46
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:37 +0000

    plate slow method pleasant often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
