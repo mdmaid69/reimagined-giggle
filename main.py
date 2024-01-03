44c1aded0ac74438456e5ab12e74f84547cff883
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:39 +0000

    confuse attract broken snow lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
