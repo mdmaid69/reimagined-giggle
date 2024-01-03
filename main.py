62777f3c18859fec1f9f27c61f5a1f681ceec7e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:07 +0000

    drop add safe strong refuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
