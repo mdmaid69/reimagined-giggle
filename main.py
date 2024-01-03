aecfa0771bab3ed3cb2a9ee5fa022d4aa43c73a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:41 +0000

    clue act flat his refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
