34e98057a09a23c2d58525d8351255f4bfec3504
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:23:36 +0000

    weigh section container bit grand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def sort_list(lst):
+        return sorted(lst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
