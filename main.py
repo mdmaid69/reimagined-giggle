88e4baaacc8faee81e9e8758601b54f202977e15
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:58 +0000

    programme suckme smart connect invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
