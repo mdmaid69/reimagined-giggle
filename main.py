fa865e8ea7d854b58f06d2f39d90a1e5ed4591a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:44:27 +0000

    blame left short voice downstairs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def greet(name):
+        print(f"Hello, {name}!")
+  def convert_to_hex(n):
+        return hex(n)
