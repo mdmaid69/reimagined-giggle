c92916d0ab242b6f308b70d874695c2f7f75114e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:42 +0000

    reserve purpose unlucky always importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
