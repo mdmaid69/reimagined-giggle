34ed84c18e755cf3793264b6cc7ea5b375b70f52
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:54 +0000

    seat mess inform regard thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
