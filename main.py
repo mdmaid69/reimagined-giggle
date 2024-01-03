56016846236387e65fcfecd8c66ad79676fc8bc5
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:18:02 +0000

    tap wrong everyone rich decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
