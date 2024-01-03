c2792e59fbd3202d370500ea258ee946c4622d81
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:09:59 +0000

    breath tip fire sign shall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def convert_to_octal(n):
-        return oct(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
