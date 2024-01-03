8e847189629dbc49de69f2faf76157641ef5774c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:05:10 +0000

    seriously ignore ear single contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
 import math
 def calculate_floor(x):
         return math.floor(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
