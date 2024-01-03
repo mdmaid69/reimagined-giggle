d956c36d602d57b6e11ea8c05fedc7b9d267a2e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:46 +0000

    defeat air runaway couple communication

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+for i in range(5):
+        print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
