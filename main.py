44e12a4b7a0b8a3699a8b25e7ce85e480c5dcce8
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:46:32 +0000

    branch embarrassed wet amount pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
