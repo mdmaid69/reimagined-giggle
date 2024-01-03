e94b6a00d9e0844a0eb8191d68c9d310b632a054
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:23 +0000

    make admit pure appearance fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
