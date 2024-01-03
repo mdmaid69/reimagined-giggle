efe656ea10c15d6ad621e3dbaae811614e3a7a9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:50:51 +0000

    hair rich second ticket advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
