087fd9f63e5285095fac2c6891512e90179b1f66
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:39:00 +0000

    prize little high concentrate tap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
