09b9571b927323f904b91ccf85c32af199a90124
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:19 +0000

    increase object top central lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import random
-print(random.randint(0, 100))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
