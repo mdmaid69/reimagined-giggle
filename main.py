e6d6d7c12126b867f5f4a844656784440bcbac77
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:01:06 +0000

    takeout town disgusting excellent commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
