6ddc3924dd1e63526515cb5447da30b60eb56bc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:56:11 +0000

    basic picture direction arrange without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
