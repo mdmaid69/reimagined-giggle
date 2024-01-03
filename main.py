19354517e671d6e1a7bee06ddf41a34926480d10
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:37:13 +0000

    beneath edge born myself connect

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
