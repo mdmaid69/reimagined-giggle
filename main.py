376f57f7c4c7f276855b03c62aed251eb5a10dd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:56 +0000

    Jj petrol gather wise meeting

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def get_array_as_tuple(array):
+        return tuple(array)
+  def is_even(n):
+        return n % 2 == 0
