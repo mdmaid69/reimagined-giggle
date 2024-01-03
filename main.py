92e6d692b13dad59863f0e1a7235bb5fa5e5cb6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:04:47 +0000

    fall exist fall copy swing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
