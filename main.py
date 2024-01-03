ec52e97bfe3681e2e33e368161a0c04a06c370fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:01 +0000

    card quick mysterious tell transport

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
