826418a6371b62aeda8a7646db2018be7dd6a87d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:14:53 +0000

    problem tongue waste importance drive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
