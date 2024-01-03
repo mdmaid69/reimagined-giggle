1c6a4fdaae6a5b96ca4a90cb71a329947d2cc6e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:32 +0000

    hot bus tonight game forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
