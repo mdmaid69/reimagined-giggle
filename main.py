2f36fa2e70b6a917875d0c7436f935bac3f3b912
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:59 +0000

    mistake exaggerate probably hurt extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
