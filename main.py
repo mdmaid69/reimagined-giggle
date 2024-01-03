beed3baf42f2370ce3a0a86d4ebb108aa9ff2197
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:43 +0000

    shallow breakin mine tend lose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+print([x**2 for x in range(10)])
