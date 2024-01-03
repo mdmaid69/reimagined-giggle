70b9fec102032af2fd2bcce2e581fccd966f948f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:30:07 +0000

    anxious practise separate yourself secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def square_number(x):
+        return x**2
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
