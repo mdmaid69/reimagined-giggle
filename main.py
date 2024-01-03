b39eb71d3e43ac03d8a2a0f88289803f8d9105ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:24:46 +0000

    dull identify project one collection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
