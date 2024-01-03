b88fd422bae45d8a0aa7d2c2a76c557e142efdc9
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:06:33 +0000

    each 567enemy across century list

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+print([x**2 for x in range(10)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
