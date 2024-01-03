b226364c629deeecdb43b72a7141aa607eb4bdc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:21 +0000

    bunch image news sticky once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_directory_name(path):
+        return os.path.dirname(path)
