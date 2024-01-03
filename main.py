6b14df609fdd404d16882887168db39bf7c5a944
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:01:32 +0000

    style discussion record convincing against

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
