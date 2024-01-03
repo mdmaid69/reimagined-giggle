df47db70f868dc27383935b68fff65aed34e4f64
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:49:18 +0000

    worth understand public late medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
