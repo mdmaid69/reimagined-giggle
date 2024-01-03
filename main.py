615ff826c8183f84a610224e4f4bba0ad94b507e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:24:43 +0000

    content shock Bb coffee successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
