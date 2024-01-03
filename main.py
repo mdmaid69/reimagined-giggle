c59f6d2e2ec60c2a0d19fb6e3d7415f342a58e52
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:25:51 +0000

    phone someone try mess miss

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def get_array_as_str(array):
-        return str(array)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def convert_array_to_bytes(array):
+        return array.tobytes()
