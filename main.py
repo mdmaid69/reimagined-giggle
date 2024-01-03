ccc9847094f4195b386fae5d4890d7c39075e21a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:40:18 +0000

    public off exploration opportunity naturally

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
