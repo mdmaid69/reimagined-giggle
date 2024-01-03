488db9728cd613671503368418dfd1303b6ecb67
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:14:37 +0000

    near store must wherever vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
