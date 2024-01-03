386a7ef5ec1c827cd5cc9614e15fd32aeaa673cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:11:20 +0000

    friend but development suggest trip

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
