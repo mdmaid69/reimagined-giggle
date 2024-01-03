5e18a4e71f66e9c485b492cdfa8490a76c472d9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:55:20 +0000

    soft good although trip interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
