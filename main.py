8ce1acc3601cd733d5e6a9980e3469c9ca0408cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:29:14 +0000

    content criticism getback godown independent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
