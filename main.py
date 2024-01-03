cd077a3d9085aa96d4b136a3055a2bbbf45f582c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:12:04 +0000

    guard direct ilovedicks try it

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
