d8cfee69121820c91b792f2e05e97ce7470225d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:03:30 +0000

    afford suddenly exit container quit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
