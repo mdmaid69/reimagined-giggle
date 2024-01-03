303fb90a57be06d3a565fc1c858537808b2ba750
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:57:57 +0000

    public lay comeover design rub

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
