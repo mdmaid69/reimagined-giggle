26066e13a41afaf1894754e0cb162f1d194feecb
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:44:41 +0000

    eat organize exact excited divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
