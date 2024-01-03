1182c9583cc8fa620a961c41b584ac651d3c1832
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:04 +0000

    basic reduction pronunciation slice firm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
