8872e6e43e0159340b092b4f7c3a34577b7199db
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:53:39 +0000

    still shrink manage worst sign

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+name = "Python"
+print("Hello,", name)
