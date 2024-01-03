3150d19f6a3851b8914880ef64cf98b3eae87fd2
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:31:19 +0000

    disapprove before square business stay

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
