6fcf1dfe09db9a7aeeedcbd7111068333c270a4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:49:08 +0000

    book place scale study bet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   def reverse_list(lst):
         return lst[::-1]
