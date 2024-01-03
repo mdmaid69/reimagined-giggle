835fb7a7d387367e501ebd8666e7d467c59ace56
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:41 +0000

    used trousers plain lock sore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
