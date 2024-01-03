6ac1bcbc1f66b83469f55e2cebfcde06b3aa538f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:46:31 +0000

    cope the work ocean connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_binary(n):
-        return bin(n)
+def convert_to_hex(n):
+        return hex(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
