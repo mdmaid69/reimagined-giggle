ef34649509e5f0c0c576c9a9febd8eb75540abd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:01:59 +0000

    carry hello defend path account

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
