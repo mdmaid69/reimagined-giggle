8bdc11dffb3feeab5c7be947c88f7fb57bade691
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:23 +0000

    complaint degree bill announce help

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import time
+def get_current_time():
+        return time.ctime()
