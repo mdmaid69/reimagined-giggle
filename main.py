0d9dff926922352ba450f3fcbc67fac61993580d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:24:43 +0000

    appearance ordinary unlikely what grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
