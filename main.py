692a7925224b88490e403330d644c5a444a2ee74
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:46:29 +0000

    ring quiet lift mistake regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
