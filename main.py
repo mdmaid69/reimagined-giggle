62c30517cc5e139e5a124304acb43288a5cc48c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:23:29 +0000

    colour crash upstairs path move

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import collections
+def create_priority_queue():
+        return collections.deque()
