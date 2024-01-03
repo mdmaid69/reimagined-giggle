a737cc12d4404a01228b7e1323c20f7a71ebd34b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:49:56 +0000

    square stare politics eye needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
