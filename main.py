63e7623dfdbdfb9a89726c6bccefd94693cac014
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:29 +0000

    save careless should unless dance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
