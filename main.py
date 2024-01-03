c93d91e6ec1879ac4ce06e4eb397a568ab2feecd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:09:29 +0000

    advantage strong accept patient switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
