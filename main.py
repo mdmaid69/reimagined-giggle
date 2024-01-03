996fbdd1717351b3fb73ca74755785db93e2fd29
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:31:51 +0000

    floor free bright wild loose

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def set_array_item(array, i, item):
+        array[i] = item
