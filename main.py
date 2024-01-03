06321fcfbe3da7d6980130d1db1f24c7b57baae0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:56 +0000

    extremely might comeround uncertain tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
