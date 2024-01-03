e3d8dccd6d5d754a7d088ae1c5a0d5417c1adf35
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:57 +0000

    home unhappy trousers wise birth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-import random
-print(random.randint(0, 100))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
