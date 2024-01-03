a5031ee5702507a1c6009008e6799e225983752c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:25:59 +0000

    used should design earth later

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
