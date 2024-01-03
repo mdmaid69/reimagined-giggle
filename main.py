76e7d3ed5cf4f3b9673a87a18416d2d10aa40027
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:05 +0000

    deliberate last mark last but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
