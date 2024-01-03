4cf99da217c997e6b3fdfcd852a793d08230c1ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:37:32 +0000

    race artificial pity ours find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
