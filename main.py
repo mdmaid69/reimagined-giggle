f9afcc2b0989ce11eedc3438040a9deb3dfdc875
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:38:52 +0000

    weekend either bush old most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
