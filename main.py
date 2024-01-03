54edc5d02df900da3775a63d56c3680a39366537
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:41:46 +0000

    really argue event community snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
