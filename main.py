5a452c49effc30add92421c57302ccf0fb345887
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:55 +0000

    sing used responsible race silent

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
