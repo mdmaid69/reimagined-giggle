f2efbcd75c3e0e638efad0089a4e447923b05210
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:11:15 +0000

    guess pass sure shrink welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def convert_to_octal(n):
+        return oct(n)
