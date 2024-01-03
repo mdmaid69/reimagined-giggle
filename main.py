cb5518660ce21c9102ad451fcc7092f102bf840d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:17:40 +0000

    cry gooff politics sound behave

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
