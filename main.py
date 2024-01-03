8a345e6f85f7f9fa138bc2114722febf7c94a638
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:50:16 +0000

    frequent radio trap out telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+from collections import Counter
+print(Counter("hello world"))
