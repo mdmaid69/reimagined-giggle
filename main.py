f7ca3df8de9c8a74f73e84e3cb399912b483a2eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:18 +0000

    address address spring agreement me

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
