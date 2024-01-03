e1e40a187c1efe13a992892cd1b012f692189348
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:08 +0000

    defend almost play discover dance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_itemsize(array):
+        return array.itemsize
