a17ddec7bdcc6949a0b242c16ed11875dba88b34
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:37:46 +0000

    bridge expression repeat play charge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
