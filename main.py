ae5c544c49dc911d527ddad518512432a0142c85
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:05:48 +0000

    underneath now impressive praise design

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
