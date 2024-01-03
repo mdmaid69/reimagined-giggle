46dfcb653f40e0e58f30d61b681a4c39f284f997
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:08:13 +0000

    nowhere study thankyou lower cruel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
