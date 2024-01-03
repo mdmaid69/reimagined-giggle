8ecf5e3ee22a8a1a1dcdfb440c8647b667a4c411
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:10:24 +0000

    mysterious basis passenger narrow punishment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
