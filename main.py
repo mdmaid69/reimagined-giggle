db0bd87e08a73cd4a3be3a74563bd678ac8e7707
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:44:23 +0000

    rely uncertain science strong clean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
