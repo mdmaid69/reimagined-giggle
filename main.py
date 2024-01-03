9ef1499bd9b739402b4d3fc2470e237e9432fe69
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:55 +0000

    past church turnback connection left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
