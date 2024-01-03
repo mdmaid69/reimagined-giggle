3e69e8b86a5c9fbac503f2ae8f2b75cfce233480
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:18:40 +0000

    failure priority interview turninto convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
