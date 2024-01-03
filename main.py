1e4781e79f61002b7f97a0ab4e1bad9e1547433d
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:51 +0000

    block his obvious proof float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
