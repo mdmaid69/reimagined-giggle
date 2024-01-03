69ce021a622a4927e79c460b73c3dad6e5623d35
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:57 +0000

    loose fail loose justice opportunity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
