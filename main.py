083f9420b538e2be3f7d2c875fc9555095407242
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:17:24 +0000

    ring radio press direct flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
