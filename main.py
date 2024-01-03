f2a48be89f1271990cc7952d8e174b029e29f570
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:01:00 +0000

    cause somebody arrive bet result

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
