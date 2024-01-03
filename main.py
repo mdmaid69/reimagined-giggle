c0db6ae75f5468258f4dcb1353429da91e73bb3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:59:45 +0000

    pleasure flight welcome wave meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
