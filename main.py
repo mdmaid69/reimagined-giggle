8fc6129fd0bd4c263b61b054328f36774e3e219b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:48:43 +0000

    stupid capital side fashion dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
