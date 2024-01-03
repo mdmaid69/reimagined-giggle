d1b381b16dd6b10363470cf87afec5852b849e47
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:51 +0000

    glass in scream estimate act

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
