122c60316187ee5aa8ea05109cd68894e0c3ef7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:55 +0000

    become people shelter than slowly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_area_circle(r):
+        return 3.14 * r**2
