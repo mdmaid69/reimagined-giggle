7320915407bcb37f7b8164e7940173973c4c349c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:29:26 +0000

    visit proper suffer Ss exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_floor(x):
+        return math.floor(x)
