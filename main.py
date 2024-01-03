fa1adfe847627fac0d1ccc5fcd87d69b856f595a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:21:26 +0000

    separate who careless detail feed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_repr(array):
+        return repr(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
