aa318fa644634ec779682b37b0486cdcc63c651b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:39:43 +0000

    spoil wall very might occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import datetime
+def get_current_date():
+        return datetime.date.today()
