de7dac2ca1801345ae0328a26b19be08674e608e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:17:41 +0000

    pen secret expensive male oil

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_octal(n):
+        return oct(n)
