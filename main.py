f4fec0fb267cb90a10ca2d441a3125c8b108c253
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:49:34 +0000

    welcome Vv dangerous tool area

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+print(datetime.datetime.now())
+  def square_number(x):
+        return x**2
