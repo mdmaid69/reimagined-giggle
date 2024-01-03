b5ff7768b36cddf797488b65a511f703c470ff20
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:52:38 +0000

    exit centimetre theirs back move

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
