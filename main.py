a2b1a326c7c2a9d3dcd214dd6a5a3405573adb75
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:55:53 +0000

    skill his fasten tread payment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
