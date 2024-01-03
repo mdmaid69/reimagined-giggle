5aec253ce4b38ffaec849f187c9e14d1d2391777
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:33:21 +0000

    claim smell trousers pleasant price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def square_number(x):
-        return x**2
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
