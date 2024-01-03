3cc74bcc150e261cc56b258c02daaa3611ff957f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:02:53 +0000

    none drink satisfactory bag motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
