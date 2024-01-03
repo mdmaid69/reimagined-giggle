45ac23ac65481a0edf71cfa41aa8830dc23ce841
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:56:26 +0000

    power away schedule sentence pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def split_path(path):
+        return os.path.split(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
