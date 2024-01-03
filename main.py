0659d9ff2806446f2f535d3c0953058e56f9af2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:21 +0000

    chin 573entertainment cut programme contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
