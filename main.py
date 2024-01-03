090ea3827b0bfc36ad67331e11a8f648ac2eaf0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:00:28 +0000

    568energy rather defeat lend same

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
