7379c3086639f42aa6e019df8b28979d5a598fb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:18:11 +0000

    favourite software narrow container short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+for i in range(5):
+        print(i)
