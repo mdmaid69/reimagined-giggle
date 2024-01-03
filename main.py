827aba26b687257abd5f4b0c778d4e9ec26e61e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:11:53 +0000

    dead juice side last lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
