305215373c0190d76fdca045fec806854e719412
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:52:59 +0000

    inside judge anywhere excuse sell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
