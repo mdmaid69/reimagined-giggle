c13e23ea3c9b1ece46bc0c10c57c4fb58fed243c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:41:12 +0000

    pause duty responsibility improve of

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_power(work, time):
+        return work / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
