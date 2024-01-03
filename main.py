3a1b87ab957afc72a961ce69f4c685f6891cb0ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:25 +0000

    disappear before book party word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
