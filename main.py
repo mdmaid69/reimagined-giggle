5acaa0cf610d8e74c2b69ccb05d9e889245d3704
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:17:16 +0000

    office breathe female scare imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
