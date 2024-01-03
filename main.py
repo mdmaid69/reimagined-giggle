2e00be2d78d9491c66ab99e3b505162e207866bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:21 +0000

    downstairs regular fence sound scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_length(array):
+        return len(array)
