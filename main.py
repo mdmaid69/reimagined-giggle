8b54ea18ad1fe9e9863766aafc040575f84e534a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:40:50 +0000

    keepout become sticky wide language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
