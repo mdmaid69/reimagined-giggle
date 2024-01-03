8cef86b297bca0cd9ffb163e25cb7e9b4cb4639f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:51 +0000

    direction name anyone turnoff convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
