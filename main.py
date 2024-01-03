dba03c49e5aefb7c508357587f0bc7b53ffd831f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:25:45 +0000

    disappear cost remove takeover fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
