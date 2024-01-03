521bd920629328d0c36e19e9036a24b302e16c5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:41:31 +0000

    OKorokay sleep teach breakout form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
