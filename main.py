197e98ed2a1ce0986f0cb0f83a033b7566afe575
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:19:27 +0000

    ignore advantage shoulder business shopping

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def add_numbers(a, b):
-        return a + b
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
