c1cdf43a62cab2f1fae1833996850a20ab855491
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:02 +0000

    complaint whichever deal use twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import getpass
-def get_username():
-        return getpass.getuser()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def subtract_numbers(x, y):
+        return x - y
