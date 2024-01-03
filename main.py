12746d05f928774db5b9057f40d7878cb88c75b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:24:30 +0000

    ticket pocket view getup bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
