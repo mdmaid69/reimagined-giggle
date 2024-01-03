78b8150f5df0c5c8bf1e5eb6bfc4c25a8baba0aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:08:20 +0000

    human indeed tight exit off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def count_elements(lst):
+        return len(lst)
+import sys
+def exit_program():
+        sys.exit()
