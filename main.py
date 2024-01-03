83dcba6b65a3ac250e6defe1c29b2373273b8718
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:15:44 +0000

    break small brush late ill

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
