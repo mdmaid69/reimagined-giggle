e6174b5acfb33dd0d535f896877de6c06d00d466
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:17:03 +0000

    rather quit surface certainly burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_error_function(x):
+        return math.erf(x)
