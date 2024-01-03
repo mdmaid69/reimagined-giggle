2d786205535dc352774bebbb315efb11b0809571
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:37:15 +0000

    charge reduction train collection proper

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def extend_array(array, iterable):
+        array.extend(iterable)
