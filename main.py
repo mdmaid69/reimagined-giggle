7be849645020f3166c012b6092c08d987ded08ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:00 +0000

    trousers suggestion quiet put night

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def convert_to_hex(n):
-        return hex(n)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
