98f8350bf93220358b87f089ecf3b08157117459
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:32:14 +0000

    turnaway close evening steady correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
 def calculate_radians_to_degrees(radians):
         return math.degrees(radians)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
