946fc23b096fe3eb3ec3f5fb7b04e7cb26aee3bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:44 +0000

    stamp raw past constantly she

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
