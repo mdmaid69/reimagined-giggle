5296511ac82c43f317109cfed423a8037f686d85
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:07:12 +0000

    total under basically tool thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def square_number(x):
-        return x**2
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def is_odd(n):
+        return n % 2 != 0
