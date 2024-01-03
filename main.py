9e4c17585f4b0329fafed3e38b441dc3d250583c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:16:00 +0000

    pretend arrangement flood bringdown refuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
