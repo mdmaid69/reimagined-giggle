6dfeaa8b0729559bdb14df2d54232ad0676965cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:11:55 +0000

    tough damp trade mad ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
