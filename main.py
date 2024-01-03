373274400f990909edc8a114eaf10ecae8a03ec8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:05:16 +0000

    dream murder adjective awake church

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
