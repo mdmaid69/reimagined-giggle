2ba6f43a1e6634576a56e567c812babcfdfcbc7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:20:38 +0000

    Zz sound danger sign mine

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Hello, world!")
