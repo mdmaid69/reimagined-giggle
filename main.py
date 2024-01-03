b6cfa2ab98828e5910e184141afc4bb99e518774
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:34:21 +0000

    praise stick consult develop ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_str(array):
-        return str(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
