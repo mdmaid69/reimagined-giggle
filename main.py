40073751f1bf5bedfea362c93b9fde40fb649b66
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:14 +0000

    still during country ourselves pick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
