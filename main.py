7d869c821e8455c669a1c5bab2624f4e697d2a14
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:47:56 +0000

    preparation rate that prefer him

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
