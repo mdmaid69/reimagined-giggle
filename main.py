ad6523e00f65b85efde167ca82441c435c33ce8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:28:39 +0000

    freedom minor change party scream

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def divide_numbers(x, y):
+        return x / y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
