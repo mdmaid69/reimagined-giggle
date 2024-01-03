4ca248f0e1fc1324d377342caa5586f1811a88af
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:05:38 +0000

    game phone quantity total wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
