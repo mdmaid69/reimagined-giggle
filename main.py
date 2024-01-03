5b73e86e8cfa7d92bb33ece929a1db689163b17a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:46:49 +0000

    successful dick buy practise plenty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
