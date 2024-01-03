8c016d2c6391c5501d9dc6675aed856499ca3722
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:16:18 +0000

    shopping majority boring definite exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
