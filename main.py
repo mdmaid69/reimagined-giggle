70218a68aa657d73fc5bfba27667017e1ea64320
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:01:19 +0000

    natural think process thirsty suit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
