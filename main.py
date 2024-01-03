16a9b8eaff5140315b8e5445f1a5dfc9606af256
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:43:25 +0000

    position throw chase characteristic arrange

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_base_name(path):
+        return os.path.basename(path)
