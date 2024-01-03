58249b8cdecbe51ef0f71dc5cb494976bc337b49
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:18:23 +0000

    legal guilty reason fond objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
