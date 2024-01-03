2501cd6e2568e122640a28ada8b309d066115263
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:30:07 +0000

    steady mistake harm pollution show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
