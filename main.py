047efeb14ea89eeb024baece53e65437c841c080
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:20:16 +0000

    vision settle clothes regular point

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
