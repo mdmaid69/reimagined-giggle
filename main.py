19d1e47ca47b8ceb06e4827ca3d9439eb0412ad2
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:51:14 +0000

    odd Ss bend treat nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
