7234b1d51cdbc4c92c1f5bd5e021a86042e0f3fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:20 +0000

    work grow port surprised mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print("Hello, world!")
