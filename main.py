6b5c6d09c222d45ab29a1932c89de1756373f90f
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:46:25 +0000

    behaviour worried lonely high cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
