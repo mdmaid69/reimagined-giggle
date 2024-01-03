506feb868fef0846cdb469c953c4a65a2bf29cc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:26:44 +0000

    teach date plan clean goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
