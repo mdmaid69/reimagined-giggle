98bdb4613f12e519a51eb92d8e20aaa5bd6b3b4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:08:57 +0000

    boring feature star mean black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
