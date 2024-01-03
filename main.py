3b5d4377d7622266070f500468e552eb8caaa42c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:22:52 +0000

    stir reserve goon breakfast able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
