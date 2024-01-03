98e474767bde1da2cb7b97acda8814b64c3667b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:40:06 +0000

    spot plate repeat tidy allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
