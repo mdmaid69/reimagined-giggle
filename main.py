7e2975c12508bd7c2a7f8b492289ab98464af826
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:10:11 +0000

    happen real hold congratulate throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
