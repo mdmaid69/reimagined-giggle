e827487dc47b4a247224d3ce4bdd84b4a57a34e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:49:44 +0000

    discussion suit brush seriously generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+x = 10
+y = 20
+print("Sum:", x + y)
