d8e84351eeb2a8dea4ffcf1c91e81bc279d48906
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:43:40 +0000

    burn boring smell frequent show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
