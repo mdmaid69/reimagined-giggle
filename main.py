2c0f16dfc268163cf338a53a63402e321a7fd590
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:15:11 +0000

    contain force flow buy pass

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print(sum(range(10)))
+  def is_odd(n):
+        return n % 2 != 0
