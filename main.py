02793d1ee3d57c69318530deac782e861ca39e76
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:17 +0000

    distance nothing take typical present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
