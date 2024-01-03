9980fe4ae44ec6afe917d09189bf1ce8cf00898f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:45 +0000

    ride opponent measure into stretch

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-def divide_numbers(x, y):
-        return x / y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
