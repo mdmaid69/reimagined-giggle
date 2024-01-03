9f138bd50aed17ede803f4af3cae572fe005a1d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:10:43 +0000

    freedom pleasure afternoon wonder knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
