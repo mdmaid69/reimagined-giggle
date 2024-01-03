59165fe19b81c99fb4ad97f8fc9c052fde6e683d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:24:25 +0000

    damp combine flight corner regular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
