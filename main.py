eba4ed87f751e25f7c379e2c2651012adf33032a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:26:57 +0000

    scream bill buy language goon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def multiply_numbers(x, y):
+        return x * y
