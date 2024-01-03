a03346afea6c44e50ee60ebf52c27bd7b828f510
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:06 +0000

    language its mind brother gift

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
