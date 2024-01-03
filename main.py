ee60cf703c0ed5c660f148e78ad4479a6e67dbf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:05:00 +0000

    work anyone real skill loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
