8fa5e42ad4a94303c6d5c3bc4b4fd8e42cb66633
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:36 +0000

    delicate exhibition pick area raw

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
