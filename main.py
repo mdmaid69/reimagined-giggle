04cfac6d2bf675ecb8b32ffe78be1949338b28d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:12:24 +0000

    ground offer either communication whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
