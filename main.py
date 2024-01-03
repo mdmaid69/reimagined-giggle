1e7ce11f6d870d9f67d5e4111693b369913eb3e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:08:52 +0000

    any answer usually common division

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
