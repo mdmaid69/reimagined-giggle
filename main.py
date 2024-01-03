e0189c24f04392fb7ac6d98eb560599513856d73
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:24 +0000

    state fierce will stick amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def subtract_numbers(x, y):
+        return x - y
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_frozenset(array):
+        return frozenset(array)
