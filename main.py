e968d1a74f73001ff88d6e09b063efe137bf282d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:33:35 +0000

    accidental variety lookaround ball rent

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
