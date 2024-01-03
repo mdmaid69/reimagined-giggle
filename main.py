34554fe25035b28f710a0aba9ede2a384448e479
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:13 +0000

    careless freeze glass cap raw

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(10): print(i)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import collections
+def create_priority_queue():
+        return collections.deque()
