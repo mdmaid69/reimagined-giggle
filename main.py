21283db8760f5e1b3eb0afa76721f185bd933fcd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:03:02 +0000

    service side black likely damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
