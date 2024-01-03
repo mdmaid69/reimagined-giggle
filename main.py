655cd5bcc5122c8e32dec41a8bb13d3c5c5e113e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:21:36 +0000

    will turnout walk empty regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
