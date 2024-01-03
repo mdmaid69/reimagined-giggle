a10b16faa762b0a2bdf3ef8a589fbf246a8094b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:57:27 +0000

    waste quiet mother middle time

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
