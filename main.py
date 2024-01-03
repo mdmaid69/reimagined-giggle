10d83fe511ed7ca80fa95cdf74f3b3a2f35e5ab5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:55:47 +0000

    national from foot along genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
