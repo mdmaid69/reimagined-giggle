4bde5f35e89d938333d0aeb8f249966e8af7d472
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:41 +0000

    tear zero again dry hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
