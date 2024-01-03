c59cc70a3595244903ed3753133a828150f1c476
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:26:14 +0000

    cum another get other trousers

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_current_date():
+        return datetime.date.today()
