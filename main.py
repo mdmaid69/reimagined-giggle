c2ef257f12b9203ed1fdf6ef1ac3feccb44e3f98
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:57:53 +0000

    wrap attach wrong past few

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
