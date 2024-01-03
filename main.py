d9cee11f19b4553fb481efaecbb224e52a60c374
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:00:36 +0000

    positive design turnup opposite bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
