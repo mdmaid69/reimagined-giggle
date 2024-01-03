0ef0f3a4ea56c023f83167b3e23c1f347e62baa7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:43:21 +0000

    tune nor fold error interrupt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
