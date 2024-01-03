039f03e8a8d58049d8315372ee583ec72de97cad
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:14:20 +0000

    whole rain office grand adjective

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
