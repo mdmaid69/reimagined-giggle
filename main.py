cb70d658c155b83f7fd82c925e80944fd1d56c08
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:38:56 +0000

    lot rub if platform expensive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
