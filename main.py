120b37da62f3a716a65847081a046ed736817afa
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:17:17 +0000

    rid store school touch side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def square_number(x):
+        return x**2
+import sys
+def exit_program():
+        sys.exit()
