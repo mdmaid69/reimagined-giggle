5353fecf97ed172251c98e88327cb22ee6b0210a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:42:18 +0000

    appeal turnoff Cc home quickly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
