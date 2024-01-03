9982ac68f01cb80739282bc7a80f562d718ba631
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:53:22 +0000

    mix pipe we his war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
