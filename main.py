03628c6612810ab04577dfe504bf83d64abd8177
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:11:19 +0000

    birthday operation wherever practice high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def convert_to_octal(n):
+        return oct(n)
