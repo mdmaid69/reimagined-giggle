7a65300c4498b20e7f913b52f2f9babb53874969
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:23:47 +0000

    cause disturbing story which turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
