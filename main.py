d7cb74ade9911e927df4ae43e4493baa8dd02889
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:48:51 +0000

    thinkthrough side once corner match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_counter():
+        return collections.Counter()
