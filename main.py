55328987a66f02226148ed355499fefab94d3cd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:52:15 +0000

    operation habit burn bringup help

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
