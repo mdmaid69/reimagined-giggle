b2640fe9797e9cf6e4535a5ab38b85033f52178b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:35 +0000

    order apply while use wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
