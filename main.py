47125cdb40dcb01f26bef9e2c1a6c914dae38188
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:40:59 +0000

    compete history count correct tool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
