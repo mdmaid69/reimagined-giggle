5abe5954a4746af50aa830fc62036bef7909bb6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:19:52 +0000

    star replace drink detail meeting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def set_array_item(array, i, item):
+        array[i] = item
