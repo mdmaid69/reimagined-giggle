7f5ba554a5d769cb859ff3231c1a2a48a04fc45c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:16:05 +0000

    waste pair next geton talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
