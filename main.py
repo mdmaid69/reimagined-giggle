6e782f5e352cbdc0f48bc910f8a8db4af62c902b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:05:15 +0000

    must study continue calm corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
