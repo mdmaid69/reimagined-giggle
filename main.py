2ba6933bc1c07be543518110a711c3f84b109d52
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:53:07 +0000

    study top clean later under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import sys
-def print_python_version():
-        print(sys.version)
+print(math.pi)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
