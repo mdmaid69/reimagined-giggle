5853c1aa56e34b223ffcb69d0c529bb99f501629
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:32:18 +0000

    neat bet male claim back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_min(lst):
-        return min(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
