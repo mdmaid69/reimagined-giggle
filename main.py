bf95715e616aa03ffd14cad69f7f058139b53752
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:58:04 +0000

    copy live everything memory agree

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
