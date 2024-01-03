08d6ff93c3474fd15b7902ea0e71422427d52fd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:28 +0000

    so sore post swing holdup

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def change_working_directory(path):
+        os.chdir(path)
