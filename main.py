ee1cad2e5b33a67391c1409897e0532620ec6ebb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:18:02 +0000

    crime bored less tap beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import numpy as np
+print(np.array([1, 2, 3]))
