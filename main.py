2a55363d44d34e18eabb1a503a565575d917bd60
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:47:26 +0000

    identify pipe quarter label swim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
