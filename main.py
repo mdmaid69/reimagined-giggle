a5416cddfa15284c243f9a18a36c7360150b76fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:55:03 +0000

    ticket border fear spread round

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
