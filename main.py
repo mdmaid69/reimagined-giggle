bc3711effd1d61a617bba493ea6278aeee2889b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:27:48 +0000

    floor next wake experiment why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
