de5de488f6ff3605ba254a75c1546f2152bd5abe
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:57:15 +0000

    clothes up smooth hate wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
