f972e939a1a335c0c65a6cc4ef53c36ef10a6a52
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:07:07 +0000

    learn bring brave proper confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
