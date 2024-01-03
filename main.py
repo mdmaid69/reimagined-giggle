733205e80662db6597163a632da6b2c5595894df
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:10 +0000

    book score somewhere completely happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def convert_to_octal(n):
+        return oct(n)
