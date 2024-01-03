315863e8fc26dda6a38d7c5d320ba41b13d5158f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:47:47 +0000

    related ago claim ago criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
