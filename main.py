f07794fcfaab755f279cce7d4521fee61c12b7ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:30:50 +0000

    street false whistle between we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
