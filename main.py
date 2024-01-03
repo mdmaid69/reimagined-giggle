cac5683f2a632c370e1fbdb2f5665a8d3f775487
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:11 +0000

    contain drink safety around exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_float(array):
+        return float(array[0])
