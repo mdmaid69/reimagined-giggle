fbf7abdf15bd9ade85d8346365823dc8059c1a89
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:45:27 +0000

    comeoff recognize spite mean slightly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+name = "Python"
+print("Hello,", name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
