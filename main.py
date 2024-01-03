3537c3385810758a0bd402a76eb797195b6920ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:12:16 +0000

    getback unable 566end get period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-for i in range(5):
-        print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
