72e3ec5a1cc021ad22e80253a4699970a9316dbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:26:05 +0000

    future wave suppose available present

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
