5bcc84f3a2e3d97b23d1ee7db5cb1c5ed1ee64f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:43:31 +0000

    sensitive mystery result female cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
