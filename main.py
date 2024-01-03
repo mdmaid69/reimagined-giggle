cd3689eef159ec7223bd45025316d3450924cbe8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:10:39 +0000

    theirs would serve downstairs union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
