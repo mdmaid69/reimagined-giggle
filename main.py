cbeddc01ac57b1faa22c626483bc0d8ccf506319
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:20:09 +0000

    danger town face gooff convinced

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
