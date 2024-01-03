0849a4846860b1b64ecfc22ef2f8496bd9ac81d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:40:20 +0000

    city motion Cc sail race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-print(math.pi)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
