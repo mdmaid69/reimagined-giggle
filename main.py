0456ce301ecf97766d4a5abf0d02a79da2e90d3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:05 +0000

    manner great shame tough confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
