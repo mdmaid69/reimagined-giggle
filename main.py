bd91a699f963d2a2676f98b0cc339a6af7440e07
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:22:31 +0000

    stay equal objection nervous division

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
