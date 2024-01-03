e3a0e27b4c6a106fbf26bcdfb986e5bce6d75df0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:10 +0000

    side 572enter little mass sky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
