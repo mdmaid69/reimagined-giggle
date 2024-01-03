c28e1a841fdf403eacf16661366977651ae6afc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:04:04 +0000

    stage estimate pair glass passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
