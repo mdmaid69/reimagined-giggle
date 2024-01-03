eed85c32cb4a6dd16789acd173d3e2b1c800226a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:01:34 +0000

    child kind bar movement out

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_float(array):
+        return float(array[0])
