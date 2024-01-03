7062a9762016a034ddeeb5e88de8597f8fdd41b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:07:34 +0000

    modern talkback century closely mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
