ce87480ccf504eedb470c0652903d9f7d47e2bbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:37 +0000

    rescue share film ask light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
