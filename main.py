467a3a9456061ccd3c71e50c419e38d654157a80
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:08:37 +0000

    community sight pot when callup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print("Hello, world!")
