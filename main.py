8a0eeb4aa5918388dd76abfe4d842a53946c9ac3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:02:14 +0000

    along try water judge enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def multiply_numbers(x, y):
+        return x * y
