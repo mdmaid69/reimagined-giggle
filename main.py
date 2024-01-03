883b40c21dc072d8465234633cec7c94605e635f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:10:17 +0000

    floor much union suggest do

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
