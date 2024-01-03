d03ef2ba0d981eda78a7d39d61e5c6cee8abbf2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:06 +0000

    measure warm wish slowly guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
