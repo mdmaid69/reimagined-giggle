747987d8cc7866c7b0043552280f68f5a3f6ae5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:22 +0000

    stay act marry plain callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
