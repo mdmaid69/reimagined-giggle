ec4a6f29422a227bacc1b47637aa461f6304acfe
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:09:08 +0000

    repair language lift translate reduction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_volume(length, width, height):
+        return length * width * height
