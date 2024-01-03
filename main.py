43cda728a6e016dc21668c0ee3d8e7a0715e8e5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:59:10 +0000

    business think after tone belief

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
