ebaf9fb834dec47641fee712efda4d582d88b4ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:13:08 +0000

    top tense makeinto sleep difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
