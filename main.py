9abef1183e04f942a5a6605768d5b4cdd498657e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:53 +0000

    the receive swim total hard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_base_name(path):
+        return os.path.basename(path)
