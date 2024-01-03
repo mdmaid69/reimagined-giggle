d9c217043127087c84c335715f25a01beaad3624
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:28:51 +0000

    bottom sit press active key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
