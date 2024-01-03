2233a86dcb5dd097647312833e8575b53f51144d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:25 +0000

    spot afterwards bang dream complete

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
