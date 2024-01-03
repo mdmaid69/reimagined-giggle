603432a27f218e3f6e1ea041964fb4f4a6cf8d9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:03:13 +0000

    his buy past demand attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
