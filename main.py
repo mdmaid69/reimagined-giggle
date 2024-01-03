65901d2a91cff59e46f2834751546ba7523978ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:25:20 +0000

    method Rr wipe confused discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
