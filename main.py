6d8a935d9aeeea07dfe08a8d83f1700e2c0c07c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:57 +0000

    modern whichever new promise office

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import os
-print(os.getcwd())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
