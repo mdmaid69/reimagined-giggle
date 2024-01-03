7b7d3ad1a282bea4deb94602e5153d22b9f300cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:23 +0000

    study tone fill action workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
