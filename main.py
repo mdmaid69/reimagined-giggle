f9771a2e46007562e3dd43de03af9fffad4bb246
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:47:50 +0000

    turndown offence describe system sound

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
