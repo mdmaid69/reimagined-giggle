57a00c06f52e97c4706744129f398b2c90cfe8ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:22:19 +0000

    number into move unhappy wander

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
+def calculate_work(force, distance):
+        return force * distance
