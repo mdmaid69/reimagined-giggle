46753809d06e8085e69db5d2395d357cfb207700
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:11:45 +0000

    goup crash possession advantage else

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
