61f500d4986758ab853bf462d453bd135ebe7bd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:10:03 +0000

    cut both work tax worried

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
