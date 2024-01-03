fa519e2750c2e538fb080d91d2dc4fd4a444b8c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:21:36 +0000

    connect afternoon Ss swim step

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
