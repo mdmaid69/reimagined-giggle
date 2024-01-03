6177e683a57bba1edd70c209500c34b3f563201a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:44 +0000

    ask answer money instruction minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sys
-def print_python_version():
-        return sys.version
+for i in range(5):
+        print(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
