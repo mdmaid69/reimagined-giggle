4e6e5cd972c9beafc96d5213434896263fc9be2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:16:39 +0000

    Vv trousers wash female connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_counter():
+        return collections.Counter()
