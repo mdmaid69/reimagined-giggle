fafb2e48d5a7ba0a9ee7bbc01bbe8c32a014726e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:16 +0000

    guard needle fold perhaps by

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_length(array):
+        return len(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
