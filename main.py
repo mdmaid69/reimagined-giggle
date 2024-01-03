c5d96514312b9ee29486c56cf8349d3e1940808f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:45 +0000

    thanks trick pity upwards burst

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
