b09ed26e4c97226570b2f600cdb41c1086035b53
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:18:11 +0000

    sore boring perform smell ring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
