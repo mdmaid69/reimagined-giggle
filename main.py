039d4f98147f70acf184d15a283f4a72c9d7b83d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:07:55 +0000

    whenever bottom left hard exam

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_counter():
+        return collections.Counter()
