f0813b6d8c060cb8eb7650e7ebdd53517f416068
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:03:47 +0000

    table final protest imagination ago

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def convert_to_hex(n):
+        return hex(n)
