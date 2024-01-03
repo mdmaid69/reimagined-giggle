3b0ef82b3b8495d6fb975d47fe56f4189cb572ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:42 +0000

    positive number estimate Ff ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
