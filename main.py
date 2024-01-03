da4a143caab135e06fa631cf98af3959850ad5c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:25:06 +0000

    without without thirsty wonder positive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def is_even(n):
+        return n % 2 == 0
