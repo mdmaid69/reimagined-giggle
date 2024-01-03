a45ab21fc91366f869a6ffe89766a41b190b8207
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:20:56 +0000

    continue final unkind convinced wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
