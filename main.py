d3d5425867055d9c03f7196d2669d97cf3fbe3b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:30:13 +0000

    post negative become still increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_hex(n):
+        return hex(n)
