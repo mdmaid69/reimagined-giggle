56ea8a58e4fc0ded91e3b19df1992fd6c6ee05fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:38 +0000

    process pay any peace mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
