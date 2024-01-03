9244128bbb9af879d93bb9dfa0a272321141efb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:40 +0000

    call spring slide weekend theirs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
