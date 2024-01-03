39231aacbcd2d5292b4bf0a04d6b8bbf7dbe8b5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:31 +0000

    quantity ago just classroom doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
