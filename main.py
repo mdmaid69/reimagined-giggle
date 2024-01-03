0423ea6994a33bf1d21c8eaeea53432afa6f3b9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:59:59 +0000

    century determined turnback what sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
