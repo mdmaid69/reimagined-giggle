c873b2c168040a8b7420c586c057c3301486de99
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:21:55 +0000

    attempt announce Ii winter performance

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_pressure(force, area):
+        return force / area
