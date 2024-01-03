2322a35eebe7ab5eb04f34a41b7abdbcb4bbf239
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:29:19 +0000

    into in dish thinkover practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_min(lst):
-        return min(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
