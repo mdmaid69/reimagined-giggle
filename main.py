6ca5f7ae3c6b3534dd1a57acea9c51fa7a66782c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:09:17 +0000

    argument outside sell acceptable geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
