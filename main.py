8bbcb70cffbfc8f2388776f3dd78c4b9851ac9c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:58:40 +0000

    holdup price foreign opponent afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
