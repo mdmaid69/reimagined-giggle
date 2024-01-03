7234ef7797d524c1ac68fabf290ba8d2043701f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:16:10 +0000

    godown fix breakin usual wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
