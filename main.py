d37dac404685fb5d4a5d0bcabf1cd9a669140281
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:30 +0000

    trip yet embarrassed level responsibility

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import collections
 def create_priority_queue():
         return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
