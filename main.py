60079ad6a761835fd35ec5cbc1f7d101ef7f440d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:18:27 +0000

    sound cool realize succeed name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def divide_numbers(x, y):
-        return x / y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
