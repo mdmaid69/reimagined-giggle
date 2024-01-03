2e12c6ca8d71ae4386d0739fb771d21d184e50ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:53:49 +0000

    loud breakfast load equal mention

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tensorflow as tf
+print(tf.__version__)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
