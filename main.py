3a873e1868bcc708b22364294b60f47bdb596f35
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:48:01 +0000

    priority enthusiastic case before mess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
