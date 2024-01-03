417454b362897d6a4d5191c97df67fcde727e341
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:36 +0000

    who concentration rent curious call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
