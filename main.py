a6b84969726d59c7727c717f07c290a5dace9f9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:49:54 +0000

    rubbish runover breath quiet apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
