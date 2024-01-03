dba5248609f19a3d9643458318b25ff0afa206b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:30 +0000

    onto woman comfortable soon thank

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def subtract_numbers(x, y):
-        return x - y
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
