9892642fb806fba708725d4e36ec19763ee2ff14
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:44 +0000

    Gg regularly exact by choose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
