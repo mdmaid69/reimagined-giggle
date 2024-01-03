595d6a9a76ac994e87aedb23172b57e59966f430
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:55:50 +0000

    giveout loose discuss combine always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
