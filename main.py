ea4ccb84d3dfc3a2040f5b6f2608ee8af4cc7996
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:40:36 +0000

    surprising ring read soil attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
