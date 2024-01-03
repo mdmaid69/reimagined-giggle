e67b1c550c797c0cc07dfa3b3f8d754a114166ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:54:25 +0000

    appeal instruction moon her world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
