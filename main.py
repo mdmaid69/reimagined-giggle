3bbcde907732c8a382f12d441a6e20c605cd0b70
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:36:27 +0000

    already downstairs admit exercise less

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
