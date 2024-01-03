e721f4111d9ce383f841f7cb3faf6ba5c40ea1a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:03:58 +0000

    push patience sleep narrow hope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
