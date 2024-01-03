38b6df334c1ca58f641230bc0ba9e60bf1321003
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:28:46 +0000

    fall violent regional perfect visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
