eddbcd1252693264872808630e345e9eacc3a39a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:25:06 +0000

    nonsense sore sticky aim base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+print(os.getcwd())
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
