7ba651ab1adca48acb366a2afa40c33495e1f99f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:07:00 +0000

    consult talk stone flood most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import sys
 def exit_program():
         sys.exit()
