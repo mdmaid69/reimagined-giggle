5b7aeb47cf8981f6d5c1123ea5342b237798476f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:18:05 +0000

    matter almost what seriously book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
