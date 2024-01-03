47cafaa5ae106e3191f26be080324de01fbeac5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:52:13 +0000

    snow tall emphasis something zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
