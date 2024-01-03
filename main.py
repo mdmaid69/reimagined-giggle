f8e9d8941446acadf5866b14a6bc5b0d5bc05883
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:55:15 +0000

    Tt gradually aim yesterday obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
