052d5836b93644e992db118054aa6d2585e02e08
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:15:34 +0000

    hand slowly freeze like mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
