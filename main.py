ca27b5914ab362dba47b81910f1c0550d36ea161
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:52 +0000

    wet foreign keepup degree stranger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print("Hello, world!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
