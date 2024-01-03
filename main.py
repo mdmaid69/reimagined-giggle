3d08a31485c877b6f2c70061a06fe65ce1196af7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:41 +0000

    scream sink crash maintain climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
