f6f67941c1492f909aa1cc8a214808f193a5a0d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:24:51 +0000

    reject spring instead seat adjective

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+  def convert_to_binary(n):
+        return bin(n)
