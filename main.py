1a10cf03d07a5ab5e7dfaff4fa136fd97f6e66ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:47 +0000

    place head past substance compare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def cube_number(x):
+        return x**3
