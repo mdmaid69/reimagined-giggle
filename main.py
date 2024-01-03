23a7bfff591fd6740c07adec1fa052d7ecc0d2b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:47:24 +0000

    sing calmly face draw black

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
