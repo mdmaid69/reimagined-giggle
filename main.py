91bafc9ac644351b4dab3d28452772e60ae0a9d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:45:16 +0000

    long first film computer talkback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
