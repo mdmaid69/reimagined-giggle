2010e63b9562fa2428dfc6e2f352508bd9045030
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:16:28 +0000

    follow repeat only police team

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
