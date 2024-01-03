cfdb2792d3acbcc2575ae31d0b7119830e6f5213
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:59:27 +0000

    weekend consult invite convince prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
