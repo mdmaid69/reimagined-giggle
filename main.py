e533d08a6bb334fae0f0fd0e5ab86a8ba3d42f6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:52:34 +0000

    glass money car range pleasant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
