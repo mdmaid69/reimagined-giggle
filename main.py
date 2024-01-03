600f362ba8620a77ed5b3d70d0eb0c512c183ee3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:08:59 +0000

    correct at dry the although

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
