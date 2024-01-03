9630a7140311eb3ec231d9ed25f03a4a110725ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:28:23 +0000

    stranger main dare dig pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
