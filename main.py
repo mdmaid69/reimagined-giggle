a662ed341565b5dd1a0b51300945f2027beddd70
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:30:19 +0000

    juice practise unite fairly face

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
