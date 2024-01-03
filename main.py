73ac08c61bffd91e807537fdaadd61d69c9f572e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:00 +0000

    exhibition stand paper makeup container

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
