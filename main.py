dfa52d1503c8351ad5f8ead2909af53d50b77b8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:25:40 +0000

    little progress handle entrance ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import getpass
+def get_username():
+        return getpass.getuser()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
