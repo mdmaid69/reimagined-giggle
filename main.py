75fa6fff334f36c6e293c0a217be8093a39f7ce3
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:45 +0000

    keepaway guess shop improvement lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_item(array, i):
-        return array[i]
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
