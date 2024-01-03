b97b46a4ff2fbbd24e724d517f41a5236bf85bc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:40:52 +0000

    time side trick march wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
