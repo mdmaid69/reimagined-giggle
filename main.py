0e0e35ac8f014b7fa3ac06a9937344a2e76ff3f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:46:09 +0000

    him typical shine float since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
