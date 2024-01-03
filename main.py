14d4c6cb6d7924ed7108316dbc15d545cd0047bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:52:44 +0000

    feed future walk holdout tear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def clear_array(array):
+        array *= 0
