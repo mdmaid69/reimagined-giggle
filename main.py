4deaf520fae5f7587abfdfe9319192e19bf1e35a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:08:38 +0000

    takeoff freeze official develop street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
