bd0faaf385c04d63f69674ab804aa8b57520ab31
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:16:31 +0000

    word reaction rank thinkthrough vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
