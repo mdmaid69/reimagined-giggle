e81c47b1e8bd24c10773bdd4a18f0dec1c79f153
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:22:54 +0000

    scale habit makeup keepoff bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
