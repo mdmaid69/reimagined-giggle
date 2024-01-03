f31471bedfdf2c8c2c9c6d52fff5c625119709b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:10 +0000

    back why Yy spray cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def count_elements(lst):
+        return len(lst)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_list_from_array(array):
+        return array.tolist()
