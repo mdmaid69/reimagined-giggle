ca89c89fe377463b1089e5082fad9d48480732c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:27:43 +0000

    legal concerned attract congratulations plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def divide_numbers(x, y):
+        return x / y
