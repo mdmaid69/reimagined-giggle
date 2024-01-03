70f2a1142bb970ccfec760553ad3265f8f67474a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:40:39 +0000

    stick emphasis will break wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_error_function(x):
+        return math.erf(x)
