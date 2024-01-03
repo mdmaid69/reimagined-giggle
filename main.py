ffec87805502a696123ae2e021221bc4a56a26e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:06 +0000

    business Uu cut majority base

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_pressure(force, area):
-        return force / area
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
