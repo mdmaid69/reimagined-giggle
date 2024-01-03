0b7007938cdc4c414a3793dd88be46f551459c06
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:24:21 +0000

    person cost class hope object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
