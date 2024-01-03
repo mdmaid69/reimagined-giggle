0f83b73eae829e09f4aa09a5ce967302f902802f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:45:07 +0000

    small do balance contribute today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
