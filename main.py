9c45f39e8ef7174bf1a381b118121a719d534a20
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:31:13 +0000

    substance name house regularly flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
