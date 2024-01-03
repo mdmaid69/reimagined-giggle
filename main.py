9f270049ba6514d18533e8593f1698af05d2bdab
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:13:12 +0000

    whenever deal danger meaning great

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def find_max(lst):
+        return max(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
