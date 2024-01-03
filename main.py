d467f41021d452af39a6d5f048b225305e3dfbe3
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:14:35 +0000

    pick test public turnback beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def split_path(path):
+        return os.path.split(path)
