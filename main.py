d1f1ec5ad78d4d12dc7f854da528691a34a47ae4
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:57 +0000

    another table offer position strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_pressure(force, area):
+        return force / area
