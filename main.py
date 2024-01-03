bcb88a432e98a51ab23fb13d004d1d9ff342fbfe
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:59:42 +0000

    wide mine region run direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import array
-def get_array_as_float(array):
-        return float(array[0])
