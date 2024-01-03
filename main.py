c6a2f45980a132c12860cea91b95ce48db893ebe
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:37:55 +0000

    thorough runinto answer science pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
