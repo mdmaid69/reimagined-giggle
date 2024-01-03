106b65308f8da8ab8cb03d3087c2b9296f07409b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:55:09 +0000

    passenger somebody direct odd shut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
