2b2a38370167017da8da217ea6cabbce6c569668
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:41 +0000

    shut callout design form work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
