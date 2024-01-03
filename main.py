783545648b3f822e295fe2309ed71a874b103f3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:46:21 +0000

    nearly we temperature shake bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
