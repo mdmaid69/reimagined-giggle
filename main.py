1efc0835b4a258cfa85cbafa1b0266fd83bb96aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:19:42 +0000

    careless avoid taste post buy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
