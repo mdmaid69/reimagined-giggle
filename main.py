9d61d8f2353227be280ea87b795e6aa43b710a45
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:15 +0000

    pause takeback blame influence mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
