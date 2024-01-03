0bc81bb2bc32621f88c3e9ae180132e591ba2df5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:40 +0000

    hot Nn occasion fortunately market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
