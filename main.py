f680007cd5b36d9dd207506528ac0a4c099d0a3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:27 +0000

    familiar sting point use strict

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def divide_numbers(x, y):
+        return x / y
