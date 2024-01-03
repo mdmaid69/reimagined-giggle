fb71ecd709a8bd4b010db38c82542882b423ec82
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:35 +0000

    dress excellent between kind turndown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
