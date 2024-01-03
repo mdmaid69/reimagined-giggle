e812e03a3704026d9b0f54463370835fe4a0a74c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:08:53 +0000

    future everywhere train speed free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
