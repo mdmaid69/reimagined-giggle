94476a883310dff78852b4dab75b318b7b6a082d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:34:10 +0000

    pour broad vanish loose allow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
