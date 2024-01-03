122dae5607c35d0d295eeca64524f7509e55bbc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:30:21 +0000

    power reliable determined start swallow

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
