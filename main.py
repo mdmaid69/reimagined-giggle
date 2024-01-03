560db460e48fa4a0890f0410b7c00d880a49e111
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:16:42 +0000

    complain Oo fetch school round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
