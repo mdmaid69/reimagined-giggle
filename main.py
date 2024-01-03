a239ba2c855ce20e177fe2098afd6df715301871
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:44:20 +0000

    young rate famous 569enjoy talkback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
