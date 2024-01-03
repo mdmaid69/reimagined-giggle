e1b6bbe8607db1f09968ffdf35e1510cdff69058
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:29 +0000

    card ought bite silent environment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
