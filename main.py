bc610b412d3ea7029959435a8d07a09cae0d3a92
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:52 +0000

    scrap content estimate scrape shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
