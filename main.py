820be0cbb8ad8d5106b884fc32be3f6ce96206e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:42:32 +0000

    Kk mark above schedule straight

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
