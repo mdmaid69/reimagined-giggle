dd25a688fe8ebb3e46cee23c7fc07cd79e4f0db2
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:16:43 +0000

    care left level proof here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
