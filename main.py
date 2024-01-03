d068f863b1744e4cf1579e49eaa20c920cb2c253
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:09:03 +0000

    smell least base gift freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
