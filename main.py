11a4c63e2d9ee1b61136e40bff677d20dda95775
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:02 +0000

    myself unlucky Kk frightened standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
