b803a0c6eb1b8f9c58c80051de660e25650b517a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:39:38 +0000

    wire brave careful emphasis 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def get_bytes_from_array(array):
+        return array.tobytes()
