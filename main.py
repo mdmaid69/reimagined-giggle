6898e29ffa979c57e9cb4221df809e17c7f691a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:56 +0000

    scheme contact classroom substance maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
