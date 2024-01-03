ffb8b1ae8cd4e7a5776dddf00db96c68dcbb3ae6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:47:06 +0000

    constantly concern stare shut emotion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
