42445acffacff44fb11f1ab8e3740d9ddb413ce3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:19:44 +0000

    never since quit complain face

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+print(json.dumps({"name": "John", "age": 30}))
