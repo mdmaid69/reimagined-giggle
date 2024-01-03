18047efc2d5d068e61f4a6a82b5563c4a831e895
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:09 +0000

    police edge post insist happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_max(lst):
+        return max(lst)
