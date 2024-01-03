7da76a77f67041cba8705468eb9316e201efca62
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:20:17 +0000

    system excited serious please improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
