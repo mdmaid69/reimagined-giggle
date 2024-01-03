5c7c2117038567ff9c40a6a68ea4b020cd83c642
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:03 +0000

    please exercise standard closely risk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
