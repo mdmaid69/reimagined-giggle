56fbe2f28cfb699fe6253d5623b3cbabf3ab2657
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:20:47 +0000

    task mix door afford passenger

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
