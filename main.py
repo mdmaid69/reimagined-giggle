7a9160d57ba279a9818f89a6bd65c96e8b052c5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:31:17 +0000

    mix juice evening off little

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def subtract_numbers(x, y):
+        return x - y
