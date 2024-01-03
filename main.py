b8a6e55df8e030aff62557c5ba025797e0fff1a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:27:25 +0000

    powerful goodbye height behave that

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
