93fb4a0214d7861580b549ecc9cdebe5819d9cb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:53:08 +0000

    gentle consult characteristic flavour keep

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
