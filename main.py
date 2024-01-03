cae47af9700239150a87ec30d6596bb0a2a5c372
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:56:03 +0000

    top row comfort nonsense soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
