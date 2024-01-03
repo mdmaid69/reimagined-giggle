f432e10b306256e89b8bf0fb241424ddc7c386b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:32:37 +0000

    cruel yesterday Nn most pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
