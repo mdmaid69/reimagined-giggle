44d1b1a59bebf5f3b5367867f7f39ef3cb5f1d65
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:54:43 +0000

    key party sort steam performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
