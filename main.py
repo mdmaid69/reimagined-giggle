bb74312d00682298f112275334fe925188178dfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:09 +0000

    far their complicated discuss fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
