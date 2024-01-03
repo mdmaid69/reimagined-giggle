df31ea93295e3d10f7fa52ea3f44d4d1b375ac5e
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:50:03 +0000

    during appear ignore near smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
