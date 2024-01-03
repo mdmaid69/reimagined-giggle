4f718f4cab81dbc1c530a79ff49c5775c7284d49
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:12 +0000

    place 565end recent part card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
