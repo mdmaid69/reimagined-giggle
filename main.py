1b05d67c65dd784c6fa333d31c6744185174b534
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:48:50 +0000

    behave say sleep owe book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
