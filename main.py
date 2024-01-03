29e2449bead1c73368461b0644b7a38278c374cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:46:32 +0000

    wire pupil determined usual separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
