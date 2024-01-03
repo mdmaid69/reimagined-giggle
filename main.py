49bc476c7e59cb21c5716faffe8dca7f5a65fb6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:08:04 +0000

    common occasional scrap generally tomorrow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
