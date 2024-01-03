7da930239da25953d1bcc59524f44e501805dc76
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:56 +0000

    shoot crisis active dig escape

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
