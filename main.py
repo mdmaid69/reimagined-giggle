dcbde851d4400b1ebed61869aa24020da98e8e7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:35:35 +0000

    tap feed sight paper neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def is_even(n):
+        return n % 2 == 0
