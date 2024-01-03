60831151f488fd8ba74587c2438869e50567ae3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:00:39 +0000

    turnback club branch shall deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
