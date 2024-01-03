eb2ea27a581b6869d3911fc487a0985d1831ebb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:09 +0000

    urgent science thorough store part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
