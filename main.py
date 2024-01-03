a8d008dadda41a34bfc73fb06c663caee717045b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:38:02 +0000

    sticky towards definite smooth bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
