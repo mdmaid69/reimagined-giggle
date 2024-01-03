646f21429eb931d841025c92f492d156204a08f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:08:32 +0000

    Mm scrap mad clean use

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
