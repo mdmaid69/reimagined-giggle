bd2ca9f516ab005fc4ec16d532b444e0bb04ff4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:57:59 +0000

    moon write shine expect obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
