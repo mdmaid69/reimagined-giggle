45129c1dbeebac09bb8687751fc07668f1ab0cc1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:50 +0000

    trust unlikely fortunately continue unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
