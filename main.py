c5584254fb411b86ae2692d5f74f58c817800a9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:28:15 +0000

    wise traffic nurse myself direct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
