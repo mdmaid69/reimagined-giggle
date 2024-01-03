c0b0a48e5d1739ddde30f9ae000f1ebfeb56dde0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:57:41 +0000

    clean himself sky mention apply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
