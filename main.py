07e11a475a907869a4ef2bc56a635ebe3d73f36b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:36 +0000

    564encourage good accident ground permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
