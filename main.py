d633183a7e8d784da77e0482b3fe2a8220c4e1d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:24 +0000

    abroad refer ours wave fold

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
