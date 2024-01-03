0afa23a72f7a3790220bc3e26432d9fc1de7b814
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:24 +0000

    every match reduction workon usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
