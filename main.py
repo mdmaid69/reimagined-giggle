db5da375b30a2d005981e9552f4c66f1a30134e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:01:21 +0000

    disgusting any spite long fix

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_even(n):
-        return n % 2 == 0
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
