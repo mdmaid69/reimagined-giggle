9e1e35795e1c0496ec3e4e4ab4c9975a4b788c29
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:16:43 +0000

    serious decision single bridge repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
