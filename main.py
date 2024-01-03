386ec056c0475a9f7d9c017fccafc2aed7b9fdd0
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:48:45 +0000

    that week slip there fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_current_working_directory():
+        return os.getcwd()
