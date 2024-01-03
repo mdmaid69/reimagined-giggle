e94246ca9b5d0f8bed5bc6766a1521d962491dad
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:34:26 +0000

    star worst Bb thirsty sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_as_list(array):
-        return list(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
