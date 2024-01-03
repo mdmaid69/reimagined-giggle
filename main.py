ed2b8ff364ea0b831e1eafe73567ff0d64b923dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:06:02 +0000

    develop yourself react wire window

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
