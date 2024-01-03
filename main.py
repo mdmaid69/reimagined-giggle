c1efa32a90e62727100c6d28753e564d3ca86aee
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:16:37 +0000

    familiar equal loan lean pleasure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_length(array):
+        return len(array)
