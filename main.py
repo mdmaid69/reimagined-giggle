f2c72a5a1bc1612666bd06a3c6f3a965229b44ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:24:31 +0000

    no impressive round workout judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
