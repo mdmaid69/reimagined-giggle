ab9bb87180ec3493371fbb7523fb379efd1772be
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:58:57 +0000

    choice substance major interfere way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
