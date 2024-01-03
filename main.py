8f3a445c44484c7a531b5784c4a35f41f5a0f413
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:30:54 +0000

    bang grow angry delight hear

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
