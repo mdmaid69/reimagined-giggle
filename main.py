2e3b0fe1063357ce23d51937f162aa0a9da938ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:05:47 +0000

    straight better advanced on use

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
