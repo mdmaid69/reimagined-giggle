9b02bd32b9c6cf0790fd1c58358e0afed31981db
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:53:45 +0000

    face steal tip grateful polite

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-print(sum(range(10)))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
