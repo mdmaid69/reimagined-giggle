43e52941484a1fff42272d2e432c8f690b3318c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:42 +0000

    contact cost doctor failure line

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-print(time.time())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
