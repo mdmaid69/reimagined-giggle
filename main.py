deaba50985f8bccb7875b1276fb0eb175e31d4d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:58:35 +0000

    them circumstance police sight matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
