e8088a8d5c7fad11232e6ee937dee918064e6c36
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:06:23 +0000

    thumb direct search will time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def count_elements(lst):
-        return len(lst)
+def get_today_date():
+        return datetime.date.today()
