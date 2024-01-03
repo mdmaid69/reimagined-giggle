027e776ed4f108a43acc219e456ddac6d52548f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:45:17 +0000

    inside might trust bath sensitive

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
