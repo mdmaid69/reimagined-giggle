0bbf9797b803fba7cb0067492d3303edddb93729
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:16 +0000

    perfect crash float persuade Zz

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+from collections import Counter
+print(Counter("hello world"))
