8e4f4e6c60e2518fa08c6d6420946c4b974dd053
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:25 +0000

    comeback free office wise rule

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_work(force, distance):
+        return force * distance
