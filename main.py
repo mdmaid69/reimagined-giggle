e6b9a87cc8ca54b4f8b4fd661ffcba1cd2f4407f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:44 +0000

    luck trial behaviour declare think

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
