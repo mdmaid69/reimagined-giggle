61ab69955ad80125cb32757d1c3412dfca97fb41
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:18 +0000

    nerve spot direction mine foot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
