64d5e81a73569a07c6862014042d137ea82310cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:42:58 +0000

    operate importance trousers trade disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
