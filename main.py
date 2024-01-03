737347da9f8613e7d60c641cf24df016a0f2caf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:08:24 +0000

    change favour act hope treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
