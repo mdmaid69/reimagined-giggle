ff28f62f58b109e915aac7c2a6dd0a5783e26839
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:46:04 +0000

    march fire inside brief tidy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def subtract_numbers(x, y):
-        return x - y
+  def square_number(x):
+        return x**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
