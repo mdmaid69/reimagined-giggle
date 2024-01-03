cda39bac4b646ceabb708e0e610da066910ecb47
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:01 +0000

    fight tip successful accommodation smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import sys
-print(sys.version)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
