a347fa379db03c48f5a3ffa2cdbee9b80a18bf2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:29:57 +0000

    glass interrupt decide where careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
