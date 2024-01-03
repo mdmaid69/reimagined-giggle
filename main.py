39be7a39141ea9f1dc8bc4942787fb1aee68e3e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:04:26 +0000

    convenient tend die figure cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
