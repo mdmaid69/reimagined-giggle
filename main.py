befd1f8ef432a3b124844502af06e70d880ef54a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:21:39 +0000

    blame cope Ss Cc feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
