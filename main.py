8cad80d7dece932d18ef95a06d3b29be3520126b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:31:22 +0000

    second relief combination line obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def remove_duplicates(lst):
+        return list(set(lst))
