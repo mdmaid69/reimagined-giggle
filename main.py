11759774055d37b86c43bbd469258b8e84abb488
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:21 +0000

    match male reply unhappy possibly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def convert_to_octal(n):
+        return oct(n)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
