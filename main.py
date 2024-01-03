43525fc43bb6a6746046f7e43ecfb7a0a1776cc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:58:23 +0000

    gentle mail unit square surprising

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
