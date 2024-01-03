34df1ae11fbe7d25625b85b130f4187e6d786bcc
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:34 +0000

    bang pronunciation meanwhile mixture arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
