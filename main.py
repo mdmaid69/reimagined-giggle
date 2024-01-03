4aad751ae803f681927dcdca60bf311a5fd856ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:02 +0000

    damage available on breakin minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
