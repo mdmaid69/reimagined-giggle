17700e88f7fdc175653332b4396313b81eb78c93
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:40:03 +0000

    activity night family steady include

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
