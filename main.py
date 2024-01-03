dae077904370c06600c3a8dbff38ce7c0b258ff5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:11:58 +0000

    search nerve obvious soon search

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
