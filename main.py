4bffeccc4f32c7b0822a78ba86d7a42bdf9a5c5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:27:57 +0000

    success note book wish fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
