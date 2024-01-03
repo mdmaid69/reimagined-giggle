552534cfddf7f0a1ecae255cc55d828356d19294
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:42:05 +0000

    why active show worst breakout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
