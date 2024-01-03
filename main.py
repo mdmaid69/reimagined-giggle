2d3f373317d67e39dc994fdfc64031925c1eac81
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:49 +0000

    embarrassed rubbish plan fit under

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_queue():
+        return collections.deque()
