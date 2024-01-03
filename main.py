0efda5037192fdb4abfbd1cdc5160bea07a5eeef
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:36:20 +0000

    cost proud bad cover out

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
