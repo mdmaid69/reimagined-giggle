f3585f48ba20cc9071471df909e13530fdfb5d74
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:53:33 +0000

    cover regard treatment trust container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
