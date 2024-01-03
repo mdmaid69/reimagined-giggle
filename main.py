2c5cacafc5f1708b83f2e3c9299772f67d1947cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:25:43 +0000

    terrible danger wave bringforward knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def square_number(x):
+        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
