cdf4f89226593ce21867d39c7d5f8adc4546b1a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:52 +0000

    play have crisis child plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
