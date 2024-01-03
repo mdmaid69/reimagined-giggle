410ac2138ec6656f9414ad532a07368c19c4ca90
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:11:03 +0000

    before pressure paper tend remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
