e70a7c5fa14c9ff8b6e775d3e659a7c56d413de1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:53:07 +0000

    light slightly pay little regret

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
