aae07cde31c9622dc83a98e757e13f04db78a4e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:52:31 +0000

    born goaway patient danger like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
