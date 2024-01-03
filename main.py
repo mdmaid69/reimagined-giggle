8d97f36127af5ced616a50ac68d777d415ec9446
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:02 +0000

    successful rely brown matter material

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
