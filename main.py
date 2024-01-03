eb8ccde1b546311a3a46749f062b0cb7f4747a32
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:24:19 +0000

    separate change mine simple 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
