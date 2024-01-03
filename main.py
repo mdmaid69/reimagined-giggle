1ee51222658cabf7a4ba039f82093298cdc4159e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:35:35 +0000

    story method mess tone present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
