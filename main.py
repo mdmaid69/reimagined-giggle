121992f81b94f3b6433494adfb6c9a88b7e3f7f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:06:13 +0000

    free convenient promise town calm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def clear_array(array):
-        array *= 0
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
