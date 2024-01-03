619e7f4c3f47dd8fd70249d2dabed81ac6b100ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:30:09 +0000

    suffer talkinto shadow cold steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
