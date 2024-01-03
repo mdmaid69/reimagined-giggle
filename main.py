aa984fc9b185c74dca9baeb150bfe3e9c3b93130
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:10 +0000

    Yy ticket comefrom hair forever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
