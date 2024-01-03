fa4c70c7d4162417bc50a862d654ad285fbc00ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:41 +0000

    introduction lead land judge plenty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import sys
+def print_python_version():
+        print(sys.version)
+  def sort_list(lst):
+        return sorted(lst)
