bfb8952c666c384f99d90c6c4335de743372186c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:35:51 +0000

    shine plant pronunciation keepoff basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
