fd00aa386b1c7818d65465bafd74850d2645d52a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:59 +0000

    service come trap limit thinkof

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
