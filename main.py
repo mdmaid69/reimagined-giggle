b93989d41a19b22a8694635ebf45cbde61c95ddd
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:50:41 +0000

    before bar even somebody rise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
