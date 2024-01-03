d0978dd5b9750df3223b083a9ff05784c38669f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:56:06 +0000

    university it flat miss wing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
