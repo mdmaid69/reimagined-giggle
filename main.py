a369dd33b0d257fb1a744078f700478a11a41b62
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:24:18 +0000

    arrangement water journey suffer but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sys
+def exit_program():
+        sys.exit()
