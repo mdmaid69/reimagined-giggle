a65ad0ea1edef96387fd5fd6ec242fd80ebe251b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:28:25 +0000

    day design learn getoff secret

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def square_number(x):
+        return x**2
