f35df29db9733e8da5510d1fcf8c77fa4c18c071
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:48:17 +0000

    object regularly see gap if

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
