a3ef43f77b7ce73014b9f21d3804d2f855125fca
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:45:02 +0000

    exchange neither scared in second

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def square_number(x):
-        return x**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
