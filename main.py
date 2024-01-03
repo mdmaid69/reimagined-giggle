953b82334880b2068a8968144857bfbbafcb90ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:17 +0000

    paper check keepup Hh there

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
