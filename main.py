ca720d8f20c6386104779e23f4872660cedcc4c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:38:02 +0000

    trousers other area apart various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def convert_to_octal(n):
+        return oct(n)
