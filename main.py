15a06f9f8e833b2c733826f54966a32de34eff99
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:53:28 +0000

    off under stiff deliberately seriously

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+print("Hello, world!")
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
