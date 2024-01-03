cf143bdf82a3ecbafa7bd6a5ebb3bb286c8f1c51
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:01:14 +0000

    lookafter along wire connection theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
