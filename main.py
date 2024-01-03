3e6bee812da01ce452e90047a2fed5e17eaca8e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:38:47 +0000

    recent under write study main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
