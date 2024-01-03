357f11bd0769a8e9049322bca952e8a190915750
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:17:39 +0000

    anywayanyhow leg taste building across

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
