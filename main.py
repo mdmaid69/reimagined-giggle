b3d1d009d019bd29720a8a7319e314b09e28a38d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:11 +0000

    foot noone class fix upstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
