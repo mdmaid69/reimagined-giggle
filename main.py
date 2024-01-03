af69a5eb1231a8ed7b39a6cec28445caade6cdc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:06:40 +0000

    similar upstairs opposite tell conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
