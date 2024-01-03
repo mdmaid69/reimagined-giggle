d9ca978038287e82a8b8ef921da19e9d0573af4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:29:35 +0000

    be region peaceful connect furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_power(work, time):
+        return work / time
