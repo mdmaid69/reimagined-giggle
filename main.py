6f8181b377a706cdaabc589577f3047f57b1992d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:55:24 +0000

    impatient performance field motion turnoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_exponential(x):
+        return math.exp(x)
