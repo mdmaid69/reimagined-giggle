d34f0b66ce0cb9a223558afc3d77f6a4b8b90d48
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:17 +0000

    pride that acceptable common before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
