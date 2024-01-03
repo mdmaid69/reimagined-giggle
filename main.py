be1060d9ca7f1a2ae3d9c444d0150aac075a614c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:48:56 +0000

    hard ready cum original total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import tensorflow as tf
+print(tf.__version__)
