23913af412c0deee1a9e97a93d3f30e9b7736471
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:26:44 +0000

    wash catch tear choose unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
