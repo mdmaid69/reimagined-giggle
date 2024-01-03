81eb33521fd093c4231f07fb725865f6658c6160
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:20:48 +0000

    name their failure no corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def reverse_list(lst):
+        return lst[::-1]
