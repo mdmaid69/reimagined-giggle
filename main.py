ef4bbf3dc1d156c640c716f946ae8a8af24422d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:26:20 +0000

    none upstairs watch region figure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-  def convert_to_hex(n):
-        return hex(n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
