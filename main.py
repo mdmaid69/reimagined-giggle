4500760e85e5abd3310fbf612d0c80c89eff23a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:29:02 +0000

    apart entry past deserve sit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
