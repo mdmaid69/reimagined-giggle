2b28f0633087e7b426592f11e930c186da5a3071
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:46:48 +0000

    themselves shake public watch sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
