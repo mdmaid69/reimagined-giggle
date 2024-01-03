a6134ecb57384f3f952ea7ab1763511bb0d311d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:10:49 +0000

    prefer reaction appear creature try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
