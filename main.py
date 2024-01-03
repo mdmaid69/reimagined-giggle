6c0c4baa1ad5fcea6253d8801a909661e6f8847b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:59:54 +0000

    giveup method hope trip address

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
