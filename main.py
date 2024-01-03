d77acd1f56d4043ea1469f24d2d96123cb17c79d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:09:52 +0000

    sure list twist gooff will

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
