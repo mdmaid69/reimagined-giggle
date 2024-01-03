e577b0856de13bea50dcbe59f5f04b2e6892842e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:20:52 +0000

    big sensible seem telephone strict

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def reverse_array(array):
-        array.reverse()
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
