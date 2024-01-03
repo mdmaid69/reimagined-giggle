a51e046f50b25715efa38c43fae08f93e985bf55
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:09 +0000

    main transparent plastic estimate thank

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
