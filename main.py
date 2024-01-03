33605e22f8055d4473bbe848fed3cda4550edb4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:51:00 +0000

    case success chin light uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+for i in range(10): print(i)
