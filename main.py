e9d346e45c3632c9c7e9b18f2b82b996b2cf80dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:17:46 +0000

    leg tired brush somebody feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
