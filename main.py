0750de24ec4a7fed44adeb96c9f96599a432c4b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:40 +0000

    building image suit disappear trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
