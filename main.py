a004fb3b497afbb6804532a6580f0b7318389a38
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:02 +0000

    beneath identify vision excited kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
