5861053e959fefc5d575c478b6140665a267a732
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:30:40 +0000

    sore disappointed mother mixture public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_string():
+        return collections.UserString()
