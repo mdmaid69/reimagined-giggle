c2b15dea1bedc81919af4cb1652b0ff1442b6184
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:44:09 +0000

    without fresh goout disappear show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
