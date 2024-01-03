9397ddc5bdcf839ae8cbb56b972906c1240be439
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:05 +0000

    tax someone comedown connect single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
