fb25b95fcf81e602f841bbb22edcce2b3d835931
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:49:33 +0000

    stiff fail interfere invite progress

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
