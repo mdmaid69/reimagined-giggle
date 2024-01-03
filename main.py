31fb92181ae7f726d1f6c7e4c010393ed11c654a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:28:18 +0000

    professional hungry break lucky black

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def divide_numbers(x, y):
+        return x / y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
