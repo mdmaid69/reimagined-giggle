cae2c69134ac9da201ed877b40d71a99d1737030
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:04 +0000

    crisis future worth bet tone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_speed(distance, time):
+        return distance / time
