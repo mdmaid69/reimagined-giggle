ea04b0b581a8ef719aba951bb85fdae6d957e1de
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:20 +0000

    eat curious drive eager danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+text = "Hello, world!"
+print("Words:", len(text.split()))
