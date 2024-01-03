c76d21cb69aa288e20ae6d92318161184c5e6220
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:51:08 +0000

    support fail scrape treatment key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
