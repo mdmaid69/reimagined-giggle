eeffe6eb5ce2f49b39296e189cc2220287402e4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:24 +0000

    clear full file break keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
