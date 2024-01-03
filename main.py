afaeb77e391b9165bf2dd07c9c41f2dc34b068ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:17:57 +0000

    neither tense field price content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
