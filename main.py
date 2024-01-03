cde3dcc7a8559f954cde88d3d35e3ffd18cfefa1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:02:45 +0000

    meaning above Pp show spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
