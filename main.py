00249c2696ad62157847fa24ba8fa046ba6f6270
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:34:11 +0000

    content apart run clear pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+print(sys.version)
