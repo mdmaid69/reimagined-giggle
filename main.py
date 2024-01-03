a07754d7540548aa2921562a714e60bf897957fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:06 +0000

    cruelty rescue substance error intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
