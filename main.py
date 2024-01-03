88917264c1af2f39a577a9644572c5e36ee99985
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:56 +0000

    duty lay yes wheel common

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
