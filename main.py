8b1738220320e0fcc7ffa86ff22cfc43acd64926
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:31:39 +0000

    model look main difference second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
