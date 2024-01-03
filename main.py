4cb967fb750b4746ba8f7d81c4d4bbd0f4b748d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:30 +0000

    statement sudden top intend post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
