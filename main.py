c21555bd1e2949804fd91e601717f4400e467e17
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:09 +0000

    string doubt instant may strength

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import sys
+  def get_python_version():
+        return sys.version
