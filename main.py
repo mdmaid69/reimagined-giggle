4f12b001edd1237da30bdf4153f8443bb2d656ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:21:19 +0000

    there will care to experience

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
