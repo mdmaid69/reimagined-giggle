2f8dfeb1b9681393640d089ac3c68ff2f01d8fd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:14:49 +0000

    take around trade shock real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
