f57383de96e9c39a2ad721b976b3ec7173f5ea60
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:27:23 +0000

    female once your appeal hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
