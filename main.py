0ff7a849106566f057cfd2a78e75d079c270734f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:45:27 +0000

    commit dry shade risk persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
