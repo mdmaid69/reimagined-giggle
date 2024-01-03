ee4b97e0488e12023906ce5048426f0ee31f81f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:30:11 +0000

    project average welcome careful afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
