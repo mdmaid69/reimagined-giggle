6975a6278820b9f4bc8f1f02111bede533735a70
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:28 +0000

    arrange make concentrate right date

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
