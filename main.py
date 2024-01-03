9d2cf963be1859d8fd4e79594daf759dd099173b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:52 +0000

    yourself fat or stamp pin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
