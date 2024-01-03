1a4b96af3d27326a24d72c250869253ff7b37115
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:20:32 +0000

    later invite hurt ours dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
