1b1a5a4965d5844997fccea23d45310824f34768
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:18 +0000

    cover get platform responsible least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_power(work, time):
+        return work / time
