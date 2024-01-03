b937b3dfa5b29c8937d3336120cc72973823a1a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:13:25 +0000

    roof general nurse wonder journey

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
