3f88ac43e99952ce2ef820439ab6c6fe54cbe283
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:28:31 +0000

    profit person lose tooth far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
