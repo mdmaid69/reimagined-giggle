36fcfd4dd0b4d725a24e122e009f29526bf94858
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:06 +0000

    the fly law opposite vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
