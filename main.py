8f9dd64100999a529e0b465354ae87bd3f4e32ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:09:00 +0000

    centre repeat cut but tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def cube_number(x):
+        return x**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
