6cf0890f5a2d0a08081fb9892e137c65cdab078d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:47 +0000

    manage tell advice realize anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
