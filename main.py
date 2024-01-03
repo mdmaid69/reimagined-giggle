7bdaa1d5816ca2ece50a56f19de5c41a7424e824
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:47 +0000

    argument frighten stone much past

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def convert_to_hex(n):
+        return hex(n)
