bfaadec4f9893cd9349249a56faa35e500200064
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:15:15 +0000

    smooth answer spray like ear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
