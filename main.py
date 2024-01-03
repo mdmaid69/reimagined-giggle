e77fb758c8cef5830260a1cc62d7a719fba5c3cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:49:43 +0000

    young reason breakfast her blind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def is_odd(n):
+        return n % 2 != 0
