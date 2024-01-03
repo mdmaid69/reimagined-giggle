6c121ee5b616d4a0f7a1ef117d983c799b0bbf5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:38:05 +0000

    him responsible passage hard common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
