66c06100b50ecbf67c9576a10822b7d44b494d9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:05:50 +0000

    sort holdon watch noise extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
