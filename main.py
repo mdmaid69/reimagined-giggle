38ea95750580b9861a43d9f81c253d969a98c273
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:01:32 +0000

    defeat getaway his stop emphasize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
