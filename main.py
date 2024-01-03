1a43f5934249b921d935602b55fca47b5061ad1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:39 +0000

    century cheap steam dig takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_string(array):
+        return array.tostring()
