4f684e2bf3fbb064e0ac689184d196fbc30b1058
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:04:37 +0000

    may true include side national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
