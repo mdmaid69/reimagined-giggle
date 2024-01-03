702ca85bf993003646b1899deadb8b948860747d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:22:57 +0000

    case sad twist show measure

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+print(sys.version)
