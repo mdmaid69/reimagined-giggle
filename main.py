1e342ce6405e1e2a8030fd64fc17c9bce4774390
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:42 +0000

    way male opposite add present

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def reverse_array(array):
+        array.reverse()
