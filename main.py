c71114bc8969300d2c68339f9e1c086812986a22
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:25:48 +0000

    beginning unpleasant will level Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_speed(distance, time):
+        return distance / time
