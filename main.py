1cb099f65b0a38541ef5b7c4481a3a862f8f1679
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:55:37 +0000

    advantage gate hope shape spoil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
