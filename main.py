41ca3ab11129d21ab2ba4be377487fc2083ab5db
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:52:05 +0000

    advice conduct bright reason start

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def reverse_list(lst):
-        return lst[::-1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
