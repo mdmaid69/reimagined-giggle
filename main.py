2b6b03500d27138dd0cea3a3c1ca02bdaf4dbc55
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:51:32 +0000

    beside miss earth apologizeapologise giveout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def square_number(x):
+        return x**2
