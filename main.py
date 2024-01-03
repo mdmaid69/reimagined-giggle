a16b7d36251ed7f676457f27d39fcbfe89f60577
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:37:19 +0000

    worry off inside deal only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
