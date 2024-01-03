1479c84cf8b85e4227f5c1990f5a605aea424c14
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:03:20 +0000

    reserve flight result beat contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
