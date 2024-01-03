35e9922ce2c495e52ee2230e73041778fbcf1a84
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:44 +0000

    minute turn unusual swimming pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
