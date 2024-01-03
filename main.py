6783079b1a323449c73d2602101055a729ccb12f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:30:38 +0000

    mean other ill basic flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def convert_to_binary(n):
+        return bin(n)
