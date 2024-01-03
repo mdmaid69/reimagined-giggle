1cbecc8429bacd15db1d1be4e30ba0a9aa709f70
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:41 +0000

    shout could plant except suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
