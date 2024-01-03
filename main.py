5b862ed3b6d4baaa9d6f33fed0fa91d4009aba3e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:48 +0000

    turn evening discuss sight wise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
