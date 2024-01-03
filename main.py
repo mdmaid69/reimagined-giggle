ca7a0b0d4c1f49d938e29b01128447c099a7bb09
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:30:20 +0000

    classroom rain black sport quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_min(lst):
+        return min(lst)
