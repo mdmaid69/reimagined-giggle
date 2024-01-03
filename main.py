72285356c2b9d468d16b040bcdbda370bd1220c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:13 +0000

    Cc cheap face workupto speech

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
