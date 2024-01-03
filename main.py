b56e83aca0a603d0bd6bc4fd50eddb0000b7a4a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:57 +0000

    worst talk by agree need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
