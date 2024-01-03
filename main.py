8a3e3a841427fc212fa822cb5cc0fb13930495b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:50:39 +0000

    pattern cover dead read bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
