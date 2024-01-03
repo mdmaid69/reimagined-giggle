779f4df97f4384e219076a60f3aac5561dd4e4ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:11 +0000

    special typical change treat decide

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
