4098d7ce73f739fedd4e41dd76dc0909f4924a57
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:43:23 +0000

    breakthrough evening spill worst question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
