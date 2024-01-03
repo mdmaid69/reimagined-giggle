1f1e0b4252ae308b2c7ccf8fa11799bcc4cae45d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:34:09 +0000

    pocket tone Cc crowd computer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_work(force, distance):
+        return force * distance
