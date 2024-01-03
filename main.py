8becb721f1e9699b74f081c9deabe97b22e1ad7c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:17:02 +0000

    move let takeaway his command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
