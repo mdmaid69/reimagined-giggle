6b6d4bc32341b483495bbffcbeb21875db96ec18
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:37:42 +0000

    estimate closed test tidy scale

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
