08350fcfb0faa4fbffbc94111bd8b50114fb6c6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:26:38 +0000

    it club injury calm pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
