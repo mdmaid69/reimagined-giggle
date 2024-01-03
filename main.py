664f4840a9aaf0f3e77d7753cf4d4917048b61b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:03 +0000

    square cross road meeting bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
