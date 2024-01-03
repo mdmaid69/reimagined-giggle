f1e23ccfa92a5a0163a331ad4d2aea0a954f0166
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:57:04 +0000

    shelter freedom hurry variety some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
