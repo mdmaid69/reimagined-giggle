7de062a2051141c618387a637ece22b272e26a20
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:38:13 +0000

    shut doctor autumn possession everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
