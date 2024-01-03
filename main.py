c6b07e0ceee4bedbf98d535d6be3c58a90ea9a54
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:59 +0000

    forward shoot order lookup should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
