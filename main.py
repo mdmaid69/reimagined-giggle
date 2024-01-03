1d7c1b49ff678d1849d6029cbd2a5d3f370dc2c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:48:25 +0000

    subject cook delight whether practise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
