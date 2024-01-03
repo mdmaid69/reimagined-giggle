2d8ce84d09aaea27e7f2424c1958f70c429555af
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:35:01 +0000

    go recommend exhibition experiment increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def convert_to_binary(n):
+        return bin(n)
