b9dfbc53e0802c4a82e48b1538f494d66aec00c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:18 +0000

    form fat raw pour pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import time
-def get_time_since_epoch():
-        return time.time()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
