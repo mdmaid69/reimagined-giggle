ac77b5481d470c34c8da0395b5c1a6acc763b022
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:24:04 +0000

    feed traffic disapprove beautiful care

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
