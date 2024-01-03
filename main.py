21408a1efe07e707a87f8b3bf8102523428e562e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:23:06 +0000

    dare deliver accidental rest hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
