eb42e39e4be27ff28c681bac0894e10a32e18868
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:19:59 +0000

    bet official answer circle communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def reverse_string(s):
+        return s[::-1]
