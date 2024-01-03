e8c78f400f85152c8ce6b4b9fc2f52c5e033b532
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:09:18 +0000

    bet beneath first imagine mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
