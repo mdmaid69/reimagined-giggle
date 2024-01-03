53c8c4728d37641b9317248050ee3ddf65efde3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:36 +0000

    question frequent cheek cough age

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_density(mass, volume):
+        return mass / volume
