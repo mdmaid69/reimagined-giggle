ebc4a6521d346071f78b64e094d3363fe8593927
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:01:36 +0000

    downstairs lucky someone comeout boat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
