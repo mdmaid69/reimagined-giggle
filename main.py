c3dd07d23008791a8310183b10a6e24023aad034
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:31 +0000

    ilovedicks exploration party takeback hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_typecode(array):
+        return array.typecode
