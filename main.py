b1c47271974efdb18568c673ab37a48ab8bead87
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:33:35 +0000

    573entertainment wish make week either

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
