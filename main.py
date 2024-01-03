09270d38cd0451a604bc4649a4634907419952e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:23 +0000

    condition shake mail regular concentration

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import tensorflow as tf
-print(tf.__version__)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
