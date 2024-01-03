f3a9d09949c2b13831af6e53ea5fa04572f73c7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:08 +0000

    debt careless survive push platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_sign(x):
+        return math.copysign(1, x)
