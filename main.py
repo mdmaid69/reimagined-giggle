4d72325372f1c2fbf441970e3ca5202ce53a1da0
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:20:12 +0000

    clear thinkof raw drink influence

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
