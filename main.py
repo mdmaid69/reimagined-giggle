fdcf8b08fd657b889eae0eef60f3ec12f616dc93
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:33:06 +0000

    suit shopping young under thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def is_odd(n):
+        return n % 2 != 0
