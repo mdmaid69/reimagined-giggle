950399a6d02066a5304e88c27c90ffbb2d81e4ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:26:33 +0000

    make inquire behind least record

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import time
+print(time.time())
