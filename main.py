4f949ba3580639cd587d0bef79a504a3fc01e8d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:57:13 +0000

    mind accidental afraid often for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
