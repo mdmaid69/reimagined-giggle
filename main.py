2890e7d6f6aefe612b6dad52bc3c060d6d48c6a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:09:21 +0000

    impressed both impossible wise success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
