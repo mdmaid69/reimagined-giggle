cc9d6798d1bbb7807d67569019f12d6a5b267626
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:54:56 +0000

    wrong hollow nothing custom grateful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def divide_numbers(x, y):
+        return x / y
