7c257c5be82e5fde98dfc8aee529657edab2aca5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:02:56 +0000

    try present university stupid day

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
