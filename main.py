f8b83b9b3b12f072ccf78028a835d3098e6f3d3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:28 +0000

    special narrow stay serve behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
