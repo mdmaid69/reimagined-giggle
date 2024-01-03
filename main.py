edfcb4d2f835161d70dd7383a8cbfd19644accc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:00:23 +0000

    official you stupid innocent concerned

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
