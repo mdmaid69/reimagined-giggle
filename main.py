dedf9f47ac524626b8476b6e4c86b7472e6ee0a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:27:39 +0000

    might live beginning confidence distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
