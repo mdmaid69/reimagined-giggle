cfdb7e3f6f2203d79f5e0146cb1675758dfd69c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:52:05 +0000

    mail lonely possibly tune getback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
