ae3fbe991a6c5d092597b71cb932b1ebc5cf3681
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:10 +0000

    Gg worth along willing promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
