d45cf477defa54cc4cb5418080bab41fc30232b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:36:31 +0000

    desperate ill spring spring them

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
