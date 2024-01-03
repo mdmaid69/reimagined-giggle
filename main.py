a6ffc872da48508bb5e311e6014abe6022f0d814
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:36:53 +0000

    frequent pattern breath cause side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import collections
+def create_user_list():
+        return collections.UserList()
