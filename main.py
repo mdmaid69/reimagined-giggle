9f8285370d0ce22d44aa1acedf1f26d9112103b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:54:27 +0000

    miss Kk world service key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
