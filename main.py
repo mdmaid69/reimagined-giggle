962f6a8a775719baa388af8899c2daf5285126cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:34:17 +0000

    usual recovery temper opportunity dance

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def square_number(x):
+        return x**2
+def sort_list(lst):
+        return sorted(lst)
