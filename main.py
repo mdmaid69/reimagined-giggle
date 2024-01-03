5b426c9ed7cd17903d859bd4475b87d82f69378a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:09 +0000

    wide unfortunate deserve talk birthday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
