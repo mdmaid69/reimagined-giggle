027b09809f2d65373919905fb5822e7875f2b681
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:34:29 +0000

    shoulder something read view mysterious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
