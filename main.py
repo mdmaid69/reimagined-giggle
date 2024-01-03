345a089b22ea3c3c356b44b184ffe40ec6e69062
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:19:05 +0000

    benefit serve survive recommend accept

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
