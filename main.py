7d2f3513a8ba917942372c82c09774a401690a28
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:26:45 +0000

    shine thinkof before hold different

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def reverse_array(array):
-        array.reverse()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import tensorflow as tf
+print(tf.__version__)
