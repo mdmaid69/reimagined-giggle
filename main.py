f32b7a758a82966873cd1102292d78877b6267e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:31 +0000

    continue safe goodbye far knowledge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_pressure(force, area):
+        return force / area
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
