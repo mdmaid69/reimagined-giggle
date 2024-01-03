f6da10a29b204e113777d633a8e743875d7ffebc
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:26:22 +0000

    sink marry could tradition complete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
