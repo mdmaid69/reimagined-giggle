8e2a7536082a536a6ba53a89dcd800cce1ad6ef2
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:46:53 +0000

    something central never confidence damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
