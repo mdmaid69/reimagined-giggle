3c1127a6b83acb453bdf96b11d8ff21deb0fd575
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:28:22 +0000

    muscle bother effect never favourite

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
