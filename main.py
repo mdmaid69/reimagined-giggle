6c63c566b4eefbbc7d154ec491167002320b46a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:16:34 +0000

    provide interfere Ss claim instant

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
