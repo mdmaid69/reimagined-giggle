86c682cca6af34ea4527eaaf88ecfd0d59e78fbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:40:38 +0000

    kind offence accommodation takeoff year

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
