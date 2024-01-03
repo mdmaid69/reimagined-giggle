6d2dea9fa852a6ebd710397152163212e7015fbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:21 +0000

    minority holiday exit Pp hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
