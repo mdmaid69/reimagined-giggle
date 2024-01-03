77fa673ed564c7891d77fe12f253fb4c3d14f83a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:18 +0000

    order vision by begin rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
