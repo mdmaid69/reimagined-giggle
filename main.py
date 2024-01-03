c4b9f2385c61b8ef0873aa6c4b1094b033fbe050
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:11:22 +0000

    between bush goout before regularly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def divide_numbers(x, y):
+        return x / y
