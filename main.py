650cb134b6bfdba5467a738bdfee5b1d07daa5ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:56:55 +0000

    thinkthrough nose gift exaggerate doctor

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_float(array):
+        return float(array[0])
