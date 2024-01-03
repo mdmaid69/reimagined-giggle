75e31523c09c6e984c3ada9e6eedf8323a7fe865
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:15 +0000

    spin habit national plastic everyone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import tensorflow as tf
+print(tf.__version__)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
