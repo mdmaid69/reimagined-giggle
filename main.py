043d34c6e464eb3841491ba6ab9aac86e3f693ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:38:58 +0000

    thought on maintain wash judge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
