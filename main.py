ae2c9097b0b3ec03c01907277dbec2535601b2b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:13:22 +0000

    unkind start draw win round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
