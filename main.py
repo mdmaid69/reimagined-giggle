e0a1af1936c7531fa99a6a5f1e975573373b65d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:21:25 +0000

    means surprised Ss failure pour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
