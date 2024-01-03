b948fd3199446b9469b93af79d2f9a364f493170
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:59 +0000

    nose better organization left turnout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
