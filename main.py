c0598486262ded11f5bdf7dd63a067e4b03b218f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:43 +0000

    argument stupid butter schedule profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import datetime
+print(datetime.datetime.now())
