73d3db1adfe1e9f14d2f3649760f816b8d637614
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:20 +0000

    intelligent crash some friendly harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
