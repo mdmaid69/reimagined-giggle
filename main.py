f0e4d2c84bcee1169f4596ce9ad451a8ae482827
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:24 +0000

    comeoff often borrow swing stranger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+print(math.pi)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
