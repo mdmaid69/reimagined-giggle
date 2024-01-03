c0af72dba5914620ded9d00e766784bce4500cf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:51:05 +0000

    takeaway want band day pay

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_list_from_array(array):
+        return array.tolist()
