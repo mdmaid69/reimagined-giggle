5edc15c43b3cd576816e8fdeeb81a188566d343e
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:54:26 +0000

    godown couple stay slice nice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def split_path(path):
+        return os.path.split(path)
