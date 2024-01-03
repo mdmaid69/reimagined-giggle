cd9ac75dacbfb73ef162a0bb951f0068a69c103b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:40:40 +0000

    juice pronounce whisper number announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
