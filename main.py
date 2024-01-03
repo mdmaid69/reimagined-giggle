eae2a99a14ef5857b7512b442603654b3ce2d660
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:10 +0000

    while flat business manner Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
