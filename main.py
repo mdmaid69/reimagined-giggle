6b3d85a5ec8b72fba53253ffe2a01d56ac6f265c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:01 +0000

    lead age somehow problem round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
