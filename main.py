58c6ed2ef4e61d18a3542e5ba61900d467c50488
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:30:05 +0000

    me right unfair serve good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
