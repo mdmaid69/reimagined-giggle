deaafc19ca260fa22fa7868074aedac82512b913
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:04:14 +0000

    medium nerve performance custom fortunate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
