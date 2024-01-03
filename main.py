d4df503f72a3d696c338242413dac39d52ee4cd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:13:41 +0000

    comfortable wire peaceful confident interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
