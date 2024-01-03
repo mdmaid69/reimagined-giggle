c824b4112ec4a02d5e04649c031eac58d53aecd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:26 +0000

    coat track new price equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
