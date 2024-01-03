d4ab36d514853484ce13b5403e2b76efe648d052
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:19 +0000

    whole question original pile ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
