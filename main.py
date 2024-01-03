d7fc27aa897e0f59e1898291ec7035a3feda1f7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:52 +0000

    cut cut stranger trust future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(a, b):
+        return a + b
