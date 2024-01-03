6a33f97ee3ee83b58c6c8e26820f0a9603c4850b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:56 +0000

    before start sick advice radio

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
