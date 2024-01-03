b927506b463a6afc86a17d0f962e680dce561070
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:30:32 +0000

    nature traffic offence fit tap

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
