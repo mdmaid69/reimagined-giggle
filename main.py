27c71ef83c5d5155b29750ddf22419fa434f3045
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:36:52 +0000

    petrol back wave slip chance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+print(os.getcwd())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
