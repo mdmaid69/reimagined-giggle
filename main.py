72a64ba421e3b7cbcb4b620fb832f67b039a0aae
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:52:44 +0000

    challenge explain hear noise scared

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def sort_numbers(numbers):
+        return sorted(numbers)
+def subtract_numbers(x, y):
+        return x - y
