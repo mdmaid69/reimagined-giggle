f7788281537b74082b898908c0bf64b220d138cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:02:37 +0000

    finger compete sharp safety rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
