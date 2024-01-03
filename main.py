a789ba2d66e95c2e4c33ae2dd64015f28994b54a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:28:01 +0000

    wire stand feature each wide

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
