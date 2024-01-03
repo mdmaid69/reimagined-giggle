35c0781a6551c2cb68d6a718ad3f85cd36e3fbe6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:03:12 +0000

    busy glad join delicate careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def remove_duplicates(lst):
+        return list(set(lst))
