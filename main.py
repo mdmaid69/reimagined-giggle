b1c2ca1a2761dbcfdfe2ad7dbbf5a6dd136f88d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:32 +0000

    attempt practise seat process stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
