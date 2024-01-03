c2a29abf705e2f99b192babd0649e025ed58ee91
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:25:52 +0000

    Tt without mystery separate school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
