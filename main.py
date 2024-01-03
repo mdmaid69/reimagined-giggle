5157f8151a53779c280cf814478d6e1281f57b5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:01:04 +0000

    possibly spite sight operation grow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
