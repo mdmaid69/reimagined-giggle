03d925995d283f6207b04446ffd9ff4a611ba062
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:53:20 +0000

    necessary region runinto window thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
