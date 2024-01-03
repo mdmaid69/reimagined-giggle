941c08e67a96eecc48bca30d8dd1fbc22d9df0f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:07:11 +0000

    congratulate job whistle year ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
