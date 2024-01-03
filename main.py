c8bfcdd55c293b8de7434dd1a10aecd36943af4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:39:47 +0000

    matter difficulty congratulations once problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
