ee99852c5ad64f2eab61fb06a856fb9f77e26292
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:43:44 +0000

    quit hope show cum sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
