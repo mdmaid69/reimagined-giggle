0b0d803a40f0e7f8b91e36e318ed1f59e03bd631
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:32 +0000

    taste whether balance word shadow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
