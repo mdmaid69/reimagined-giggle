a0e8ad28ec7c30ff5571cec7177f33f6f52a3cef
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:17 +0000

    like with trap issue turnback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
