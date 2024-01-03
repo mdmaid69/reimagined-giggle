a2085f81b688bc78b5ca5c2b87891bdee3438be5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:50:01 +0000

    question trial curious top everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
