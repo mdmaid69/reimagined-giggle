8197830582aca28e8bb883b8c71ec9c82f94a5b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:18 +0000

    please cut dream or Rr

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_stack():
+        return collections.deque()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
