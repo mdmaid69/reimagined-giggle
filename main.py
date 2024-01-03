92ef45bddfaa974ba3ac62c04b713a3878593b89
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:29 +0000

    sea knowledge muscle pipe mend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-for i in range(10): print(i)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
