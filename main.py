6aafdd9c3ea9fa70f8f67cb3e208984f0f0588ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:10:54 +0000

    office bread reaction corner promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
