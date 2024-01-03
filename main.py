7b683ae404f1fcc80c48f5e2b608c0baa48d7f7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:20 +0000

    change breakdown hurry suddenly afraid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import sys
-print(sys.version)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
