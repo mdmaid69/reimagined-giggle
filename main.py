8fea1ab02ad32d72b136c99f0db6db18a9b56166
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:49:35 +0000

    yourself mind classroom welcome fall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-print(math.pi)
+import collections
+def create_user_list():
+        return collections.UserList()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
