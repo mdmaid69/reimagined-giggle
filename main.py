a9db0fa85995b3500650a7bb3561f203f786559a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:27:11 +0000

    cost close square his public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        return sys.version
