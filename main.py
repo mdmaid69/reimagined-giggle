ec6a36344201695bf0a829842ccb30f60883f078
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:22:36 +0000

    live government innocent soil onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
