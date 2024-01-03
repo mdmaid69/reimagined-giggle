cfbbadef137335257121f5fa77a76e2908e7658b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:16:03 +0000

    container popular workupto back scene

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
