7ecfc5b81e206997bcdb0d1f4253b864e27ee242
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:29 +0000

    compare continue study direct admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import numpy as np
   def create_numpy_array(lst):
         return np.array(lst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
