1d6a471047aa0e090688bd30d67cc35da7f4e52a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:06:57 +0000

    willing buy phone moral proof

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
