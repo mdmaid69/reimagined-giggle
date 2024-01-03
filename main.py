3eabb2eb13f68356f90468742f76117ea40f9cac
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:55:05 +0000

    steam row afford unfortunate loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
