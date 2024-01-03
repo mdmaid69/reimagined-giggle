c3eb8b15549c2eb2c38d377e45878b9a4a941dbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:47 +0000

    exit argument base usually pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_string():
+        return collections.UserString()
