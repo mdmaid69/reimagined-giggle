9db2a6ddbda4629d49f8063f2b4b92d0a3d73f5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:59 +0000

    sort spray clue hurt recognize

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
