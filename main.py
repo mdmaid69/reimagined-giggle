ffeafd201b34b66791516032c55eeeccc4d45810
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:10:31 +0000

    view mess active opinion within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
