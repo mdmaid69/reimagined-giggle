646b0594b00423105f6298a3a80b439e0c66129c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:38:31 +0000

    only bang rest then speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
