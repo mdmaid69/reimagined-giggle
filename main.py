81d758542abf417da0a3caeb12f3b1d7d91f91fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:32:52 +0000

    matter convince few left plastic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
