82a7b88a6661297ee56ff9c7967b60e19188c51e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:12:40 +0000

    empty of crisis lost OKorokay

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
