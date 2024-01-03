de08f320c836d6551d69b515b4c90298313290e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:21 +0000

    closed excellent shrink worse hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
