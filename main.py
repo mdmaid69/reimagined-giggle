78db2e69e038f7d1c7feb1164eaf15da5a24cf82
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:47:48 +0000

    visit which bit this 564encourage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import collections
+def create_user_dict():
+        return collections.UserDict()
