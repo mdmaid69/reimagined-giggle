9ee25358dbc39b7b7cce212696109729e09b8736
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:07:35 +0000

    maintain left gap common debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
