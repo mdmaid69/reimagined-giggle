6411d1800492a145f6922c4af1ac01bc1bd4dc9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:28:23 +0000

    hello tear other mistake drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
