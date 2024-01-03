e0bf1204f4f7188cbe4dfe6d8810997e6412066e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:07:59 +0000

    guess introduce pump defeat bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
