7f7ce9e7bf4d4e65b648fe73bb8f3e7b2a515286
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:17:28 +0000

    day challenge call even gooff

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def is_even(n):
+        return n % 2 == 0
