830454055e97245b49e82ea71292d27071d1e268
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:13:25 +0000

    whatever destroy better now taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
