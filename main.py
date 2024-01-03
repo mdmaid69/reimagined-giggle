6ad99b2c73194d829f41c9eca01980f311fa3ebd
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:05:01 +0000

    quit also floor never heat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
