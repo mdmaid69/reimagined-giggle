2ec968483fb7648227ccfc4944448a19aa91751f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:53:51 +0000

    strike talkoutof holdout taste too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
