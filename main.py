c4b012a15a61f5432516dad054835cb93a82612d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:44:58 +0000

    unless Cc long surprised positive

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
