1162421cb32bce15a7dde880eaf8080bf24bc699
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:25:57 +0000

    task round inform strange land

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import numpy as np
+print(np.array([1, 2, 3]))
