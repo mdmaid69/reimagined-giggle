09acb7f026100fd8b3711045c09c68ee22a218cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:01:02 +0000

    lower report convince lean smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
