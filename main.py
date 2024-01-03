344104a2da126d6a082501345ed591ceb71e11c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:01:47 +0000

    road make change judge wild

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
