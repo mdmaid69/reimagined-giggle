c9ff0851b7d695bbb1e6c26984cfc8b7b50da6b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:57:35 +0000

    heat extremely hold left cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
