78e1eb5305059c291030f655759aff8f4918cea9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:42:20 +0000

    spring seem behind night write

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
