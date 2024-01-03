1872aca1bcfcc04d232c30404c82e83bab8523b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:01 +0000

    person strange among nature last

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def convert_to_binary(n):
+        return bin(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
