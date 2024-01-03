8cdd7f22498d4fe6225f1d181d293d93836b3042
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:40:19 +0000

    day pollution land which machine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def is_odd(n):
-        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+text = "Hello, world!"
+print("Characters:", len(text))
