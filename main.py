4273dbc01c05fa78f6aa313f929e1d6d25e1e916
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:50:13 +0000

    hollow afternoon favour comeback extremely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
