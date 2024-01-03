c062be4cb8769eed6e50ed3fb7143969f0c868d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:53 +0000

    Hh properly environment damp moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
