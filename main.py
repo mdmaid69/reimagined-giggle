b47014159cff68c824def4fb2db7e4080ac4bc93
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:31 +0000

    tragedy chance convince identify platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_array_itemsize(array):
+        return array.itemsize
