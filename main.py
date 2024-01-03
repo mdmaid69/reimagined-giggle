0a3ac8bc614414a983defc3f38be8f94e962bb0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:20:58 +0000

    school preparation mark comment thank

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
