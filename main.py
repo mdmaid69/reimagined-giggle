7e18c666df0b983f5c79feff6fe5d2a0a28e7ac4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:11:53 +0000

    wander weak confident Nn considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
