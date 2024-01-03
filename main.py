3e03bd82eaa1742e2819c50c182e3d92959117d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:03:26 +0000

    aim piece experiment mess through

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
