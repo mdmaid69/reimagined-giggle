7e5c423a6b306916ddce103d2e847f63bad33f6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:01:48 +0000

    rain none fact press giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
