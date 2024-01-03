0f803669c820d9d45ae3eccb0ac4b4dc105c4359
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:11:39 +0000

    shock corner flat morning some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
