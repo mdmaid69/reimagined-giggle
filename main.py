185574cdf3975f6a0a8c4121fea0aa84f99a5242
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:59 +0000

    ours prevent attract drive who

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def subtract_numbers(x, y):
+        return x - y
