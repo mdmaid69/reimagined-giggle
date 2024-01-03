1b1d1dc53e3137a902d004bf8c36cd03632a8000
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:52 +0000

    touch tonight flight leg dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
