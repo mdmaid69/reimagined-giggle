1cfa4b99b8ed648ead5254e52ba40a07b2ff3c2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:16 +0000

    attract nature become hurry movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(a, b):
-        return a + b
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
