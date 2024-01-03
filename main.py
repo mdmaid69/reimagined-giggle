5eed5a540770add8eebad250de66d017227cb31f
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:13 +0000

    lean final number vote sentence

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
