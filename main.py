32906fc806e4f004ba6653506d1a2fa818cac6b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:26 +0000

    relative week fly claim estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
