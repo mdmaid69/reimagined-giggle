f57a1ba823fae8275dcb057af6b59770a34244ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:05:19 +0000

    poison blame close load common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def add_numbers(x, y):
+        return x + y
