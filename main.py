60fe3481bf19e0f4f7d8ae41e686d1f49483be3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:58:20 +0000

    tie loud onto wire stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
