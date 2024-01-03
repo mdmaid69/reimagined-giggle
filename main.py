8459292fbbe973e8411a184e8a0463c494f5feea
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:51:00 +0000

    claim this rarely copy entry

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
