66946842104bd1ad696546a69c9f8acd11c16739
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:19 +0000

    relative young the care shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
