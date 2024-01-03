74af9f6bb5ed761b402049f85858adf92cfc06da
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:17:36 +0000

    middle Ff trap rent zero

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def convert_to_hex(n):
-        return hex(n)
+def is_even(n):
+        return n % 2 == 0
+def is_odd(n):
+        return n % 2 != 0
