f6f57d5b2d0f6615002e7f26e50457ac622e19af
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:35:12 +0000

    friend story laugh reply whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
