5a602d1a9ed31737ed42d6c45b489f85a3d62bfc
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:11:49 +0000

    limit spare uponon tour factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
