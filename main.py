45a78001b41ae45b915e67636a81a216fbe398b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:38 +0000

    phone fortunately say deliberate rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def divide_numbers(x, y):
+        return x / y
