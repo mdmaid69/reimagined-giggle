96a95810e1737c8ec7984954c8ddc3754aaeddda
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:36:24 +0000

    road leave value makeinto male

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
