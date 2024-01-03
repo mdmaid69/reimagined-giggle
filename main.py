16454c2c4f550cdd60f0ff9bbf7441214e14f4cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:36:55 +0000

    describe himself level dress swimming

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def convert_to_binary(n):
+        return bin(n)
