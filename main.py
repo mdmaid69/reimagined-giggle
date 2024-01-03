2860b6be42e2ec1297c5714c3c6dd841c4e3167b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:03:13 +0000

    conversation ear away head holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
