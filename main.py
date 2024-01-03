ec70cf77873ee31198c907e3886a667cfe292357
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:33:15 +0000

    detail float mine alone fit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
