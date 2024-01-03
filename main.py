2b0788d84b08f0ea55c30de933c0ef944168bfcd
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:55:59 +0000

    drink nurse city turn successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
