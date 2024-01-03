e83432cbd36f31f2aa22a4a9d7518f18dccd3073
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:51:53 +0000

    occasional wide record content feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
