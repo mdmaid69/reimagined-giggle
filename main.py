c29352c978ab15e9b0a0c6d4bb96b4a94075c5b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:33:57 +0000

    later goodbye order get study

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
