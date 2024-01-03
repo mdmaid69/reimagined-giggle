ec79aa8320f3e5dae78f0cca9b248823b3b4325a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:32:57 +0000

    else fat complaint news quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
