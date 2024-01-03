6acc0bedc818c02e44c655c31ea0df79acabed13
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:14 +0000

    down callback tall concerned introduction

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def square_number(x):
+        return x**2
