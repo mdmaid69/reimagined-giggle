25d24e73fefd133b9a1a16cc95c0b97ecd10067e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:46:45 +0000

    hit town professional false interruption

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
