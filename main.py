bf0eb512eb6bbb7ac1c7b2fc47125fc0189f3868
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:58 +0000

    reply class soft hold detail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
