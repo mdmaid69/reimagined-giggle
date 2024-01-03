ee68882c03ea4ad79d339ebe4d7071c70c0a88fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:58:15 +0000

    sign responsibility hear clue speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
