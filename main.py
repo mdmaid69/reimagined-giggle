d788270a678d79d98fdca6e53a740e23cabbd53b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:09:06 +0000

    separate dance tip become thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def append_to_array(array, item):
-        array.append(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_itemsize(array):
+        return array.itemsize
