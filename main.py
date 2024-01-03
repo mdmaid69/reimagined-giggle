9b5d42eed6cce538f9f6ae87b8b1df395a6c223a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:07:04 +0000

    talkinto instrument dinner available protest

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
