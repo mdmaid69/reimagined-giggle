ecf391d90beb062de8772ef1ac685a7ef89cee25
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:33 +0000

    communication emotion like test 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
