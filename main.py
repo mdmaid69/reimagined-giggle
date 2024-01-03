8dfa7d1ec00864f10b027b27234708ef4beff189
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:49:34 +0000

    tidy bringup unlikely limit quickly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
