149bf43cf4f21f0067baadc73ce427778e4c61c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:13:20 +0000

    sign many bush flow offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
