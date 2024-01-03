224fff5e8e9a45bfbea3d1e51f77fd2099b93abe
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:27:29 +0000

    ball feed teach conclusion tune

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
