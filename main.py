a31a5d08fdc9ca557780a658324404760433c317
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:51:33 +0000

    affect break criticize married delight

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+print([x**2 for x in range(10)])
