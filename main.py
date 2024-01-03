e9e47f1bdb47a1fdd811b8e14f4cd943b6248d3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:31 +0000

    in course should cut opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_tangent(x):
+        return math.tan(x)
