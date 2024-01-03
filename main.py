db3e352532d8453ad43dd4860990229ff39e88c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:43 +0000

    occasion possession should occasional people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
