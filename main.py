6fef7fc09af6368177a54b607c9416d3f9d52eea
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:48:20 +0000

    perhaps rush clever turnoff breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
