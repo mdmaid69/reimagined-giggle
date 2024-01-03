2c8150864cb081e56bbc4102c8d722d362fc9a9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:22:57 +0000

    agreement start mention anything always

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
