11a22e76f347f01c7856a14fadfd3b7df7d33c22
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:19:42 +0000

    centre offer decide next convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
