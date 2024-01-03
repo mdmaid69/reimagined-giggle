f3b1c451241070ef80cf0c0a287aa4661b42e406
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:41:35 +0000

    poor fire afterwards mind drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
