bdf5946b3426cc2ad07ed7c9ff556c2bf74a38d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:10:39 +0000

    risk pleasure key that sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def is_odd(n):
+        return n % 2 != 0
