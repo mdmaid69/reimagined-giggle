d1fb888de7b3176c925bf2cb7052a11843b719b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:51 +0000

    wing phone noone decide although

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def greet(name):
+        print(f"Hello, {name}!")
