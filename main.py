f726016e21b7cfc03f18291e31380bfd28bfe30c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:44:59 +0000

    top every announce claim spray

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
