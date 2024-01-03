19fc463bd151a11da818d66cae4167b4fca52f79
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:05 +0000

    tap alive best classroom excited

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import re
+print(re.match("h.*o", "hello world"))
+def find_min(lst):
+        return min(lst)
