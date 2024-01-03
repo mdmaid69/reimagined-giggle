a4728e56aaa69acc74a4624e95bf8ecc1b1f3420
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:48:43 +0000

    comment black so soil rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
