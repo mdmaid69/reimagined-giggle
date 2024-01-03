0e381bd18c6a8df603dee00b5c4abd4463680499
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:09 +0000

    bother impressive feed extra possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
