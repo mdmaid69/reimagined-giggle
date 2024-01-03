a8ce59079b210f637957a4f418a76a942bca7c1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:53 +0000

    slide everyone needle dare offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
