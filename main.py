deddcf362db01219f0fec786ec5cd668a67578e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:48:52 +0000

    I contact comeround smile my

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
