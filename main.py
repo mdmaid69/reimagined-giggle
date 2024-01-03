79eeced94493edb2bf74ceaabc0eff45c1226bd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:44 +0000

    convincing rest far block neat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
