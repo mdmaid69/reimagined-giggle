c86764eae54a75a71aa166f3d9aa0ee81ca587b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:36 +0000

    cum curious 573entertainment hide slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
