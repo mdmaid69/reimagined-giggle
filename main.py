0eb2b869c2fe31c982506195fd69e4cc4bb8ef4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:22:43 +0000

    but exact cross breakfast she

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
