c61554f0bd8a94db7171045230b65e84f2477cc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:22:34 +0000

    through hurry towards top again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def append_to_array(array, item):
+        array.append(item)
