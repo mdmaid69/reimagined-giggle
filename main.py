e786dd181ab4dd18033533083bead7e86ce9a001
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:28:35 +0000

    thanks absolutely convince wrong result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import re
-def find_all_occurrences(pattern, string):
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import re
+  def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
