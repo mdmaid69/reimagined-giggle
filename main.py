bf31c63623596f4b028168c4a46919bae1f1ba09
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:10 +0000

    doubt criticism admit secret correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
