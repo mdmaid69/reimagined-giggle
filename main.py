4a99e2abde563c0eaf6df8a59375508e2388992c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:39 +0000

    payment like flow shopping unite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
