5ab69b59d6047f292c0317b50f8abf2a22137890
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:49 +0000

    intelligent fond achievement argument busy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
