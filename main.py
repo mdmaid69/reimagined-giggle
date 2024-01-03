0d73828abea4c4b911426c158518164505eb7aaf
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:12:38 +0000

    dangerous base boil ashamed obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
