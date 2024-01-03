c7c5b8bce215b0f0a1ab93af3dc2bdec86df2524
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:50:28 +0000

    spring central match music wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
