0a7479029ee612944f451e694631aa2302182938
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:16 +0000

    react shelter exchange factory makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
