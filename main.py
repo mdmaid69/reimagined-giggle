89d987864c6de9a1a9af6a0467775e1b4c8d2768
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:51:40 +0000

    spin sail lead attempt money

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
