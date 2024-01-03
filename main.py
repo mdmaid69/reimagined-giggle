12f22bc5e917be9d41d3f23a92667230d682dd99
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:30 +0000

    please broad difficulty cook low

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
