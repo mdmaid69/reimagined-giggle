09eecd80e5d75b7a723558ee03b7e1d795b160b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:47:29 +0000

    hers workout trial wrap leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
