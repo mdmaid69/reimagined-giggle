17f7fdb0a3b09750b4023b7d31f9d7e954762186
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:09:12 +0000

    stage Itself market healthy search

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
