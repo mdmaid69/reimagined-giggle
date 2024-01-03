3cee970d83687b6dc67f4ab5627f9870f8918b6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:51 +0000

    design issue label consist priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        return sys.version
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
