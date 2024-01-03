856147223eedd4d3dd54b097dd7ce087fb34bf3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:28 +0000

    protest score direction month drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
