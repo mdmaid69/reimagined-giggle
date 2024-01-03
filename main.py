fdcf1f52bf79adc99f79faaa1cf800732ad88723
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:44:32 +0000

    whoever argument fast picture contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
