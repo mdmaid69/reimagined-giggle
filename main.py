faf285d589ebd002e2663327a24f5de3c714d704
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:22 +0000

    risk aim anxious powerful reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
