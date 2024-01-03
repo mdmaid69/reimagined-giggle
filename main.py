de46a3d0475d12f28c6d495781ab90dc4395512d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:53:46 +0000

    frequent suppose but information question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
