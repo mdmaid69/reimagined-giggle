1eff20cd5c80396e5e7b8c88fdc4ba2310ce762b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:56:33 +0000

    record actually soak strength sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
