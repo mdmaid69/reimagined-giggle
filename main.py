6b16d96d553ef05237b4b21ae5eda501a8274068
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:29:09 +0000

    search law congratulate obvious car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
