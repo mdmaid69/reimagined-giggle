3155fba6cf8e2083b47bf9dd9e884b88d36c6c6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:57:48 +0000

    ought rush attract spread claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_square_root(x):
+        return math.sqrt(x)
