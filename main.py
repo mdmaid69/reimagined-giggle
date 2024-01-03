8da5fbc329239c60d5c8ea46ffb238d348fc418d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:07:40 +0000

    takeaway read design how shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def reverse_array(array):
+        array.reverse()
