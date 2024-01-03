e85aae14ea90f437feaa2445ecc275f297c06f41
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:00:44 +0000

    especially plate plenty port deaf

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
