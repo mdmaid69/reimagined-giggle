064f28f1685f355b84da6654be0914383f9ba640
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:32:57 +0000

    borrow heart occasional operate because

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
