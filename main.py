8a4bbf60a79f8555e35248a7ff3755d252cffedb
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:12 +0000

    hardly she general mistake move

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
