d4dd2b3404c8e80c6f1c20a83988f5e738a05ea0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:49 +0000

    star comeout prize strike 573entertainment

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
