0e89d66a2afe60f2d5b9a96a83608316baa724c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:54 +0000

    gradually often tip again sort

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
