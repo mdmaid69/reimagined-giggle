43109f65edb2e6dcf83dbc3e2e13dfd1fb118a93
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:53 +0000

    objection tidy continue call danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def multiply_numbers(x, y):
-        return x * y
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
