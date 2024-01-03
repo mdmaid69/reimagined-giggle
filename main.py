556610329f456a1fdbc545fb74af8d180878a5bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:29 +0000

    loan doubt definite praise thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
