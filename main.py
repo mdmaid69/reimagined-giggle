bd496eccba8bdb9287a1c2487bf5e56db5561c52
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:32 +0000

    goon fix range train sure

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
