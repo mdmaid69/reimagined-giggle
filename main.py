2f986dfb81a69eb44f7dedbcd6e2bf2bd1a0e1a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:51:24 +0000

    group hard other bye shadow

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_queue():
+        return collections.deque()
