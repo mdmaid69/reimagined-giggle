9af71a6d52f5074c4ffb143828c6c6d28a840ded
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:15:41 +0000

    none tax journey square delicate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def convert_to_octal(n):
+        return oct(n)
