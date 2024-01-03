b081bafe140d17cffc915ddbdbf40cb04d6441bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:35:39 +0000

    keepfrom comeup fall stay just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
