05e5c7c1e9a89e8b9a0dea078013abf4b5bba857
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:14:51 +0000

    official score forever hurt piece

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
