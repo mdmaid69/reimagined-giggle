ab14019f8fca092c4e3e92d1aeb8924bca4abc71
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:04:18 +0000

    run turnoff lower hospital polite

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_max(lst):
+        return max(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
