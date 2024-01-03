7cbb3352606e9aede705ff5934e52b887ce033ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:14:16 +0000

    get worry would copy classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
