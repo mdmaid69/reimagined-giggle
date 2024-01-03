959b4ff7dcb55e452495bb0152ff0c424298e75b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:11:40 +0000

    aim hollow history mood everyone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
