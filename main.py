b1fab31890d5ad676aef6a77b61123db44e66818
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:44 +0000

    talkinto purpose manage question path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
