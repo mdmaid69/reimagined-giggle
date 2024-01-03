8a340bde730408923d3507b86482e94811ccf06c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:38:50 +0000

    oil travel catch tip lonely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
