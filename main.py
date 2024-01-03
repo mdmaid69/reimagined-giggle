dedc0fd2af44512855a3a605f4c04225625bf606
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:41:38 +0000

    watch state guess agree goout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def remove_duplicates(lst):
+        return list(set(lst))
