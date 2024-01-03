882dfe0681260f523dbc7ac44c473313bcf833b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:10:22 +0000

    finish give invitation authority at

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_force(mass, acceleration):
+        return mass * acceleration
