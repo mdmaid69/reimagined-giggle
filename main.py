c2b0bb48e1ce84ff4327a4f24d4c2a19b654cac5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:19:56 +0000

    risk by display avoid wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
