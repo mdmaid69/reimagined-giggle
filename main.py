2b8cb5f24b9fbff4347432314a54f4ca6577f6d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:51:33 +0000

    weekend official name breakin giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_as_set(array):
+        return set(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
