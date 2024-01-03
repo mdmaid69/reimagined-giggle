a036144af9b36070570097cf1d885620d1d87b69
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:32:13 +0000

    sign talkback frighten commit share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def divide_numbers(x, y):
+        return x / y
