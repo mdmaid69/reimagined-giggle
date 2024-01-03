f14c2c4a788e8b2932c52e615c4674ab78f819d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:44:34 +0000

    we during plastic delicate temper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def convert_to_hex(n):
+        return hex(n)
