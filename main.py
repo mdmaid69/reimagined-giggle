7d8e28573de088c7cccb1f9d824ecd27edf94732
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:07:10 +0000

    copy long commit rare himself

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def reverse_list(lst):
+        return lst[::-1]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
