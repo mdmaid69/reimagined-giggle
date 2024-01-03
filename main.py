f6e697371a40d7645c510daf77b2b503e7617b6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:14:28 +0000

    base brush yesterday comefrom convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_item(array, i):
+        return array[i]
