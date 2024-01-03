8887eeecc7f3fd4c15334ac367d60af4e479140f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:50:55 +0000

    light pass top payment find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
