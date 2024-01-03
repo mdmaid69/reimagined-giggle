c95164b11a2fa65ea8fb8e291279f33dc4b0feb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:29:19 +0000

    side truth holdup bank beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
