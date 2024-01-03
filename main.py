e938342f7baaf61a0c68a981c9220714d9f5688c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:17:27 +0000

    search wide dry treat when

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
