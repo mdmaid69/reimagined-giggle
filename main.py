3fe4a4ddcd77ee7121d64e71d97ba8a1c4e71900
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:43:49 +0000

    nose hang variety dead wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_binary(n):
-        return bin(n)
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_as_repr(array):
+        return repr(array)
