6487ce68897c6f802d2047f4460f180751f5c8de
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:12:51 +0000

    shine split although uncomfortable less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def get_array_as_set(array):
-        return set(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
