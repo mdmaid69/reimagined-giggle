7c8769d6ba6106383261fe225aed6684cfa82e5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:51 +0000

    software straight gate use accidentally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
