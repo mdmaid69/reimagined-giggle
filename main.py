f74acee9e80badaf73a850d920b521895a3c062a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:21:09 +0000

    use weekend average fresh responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
