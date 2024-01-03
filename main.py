7751c60adf017ba039cf399e09b233db7986c963
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:05:11 +0000

    computer organization low pronounce professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def count_characters(sentence):
+        return len(sentence)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_typecode(array):
+        return array.typecode
