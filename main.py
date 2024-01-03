ba68f89a41c3122f9b98374122b9fd7a1722e543
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:57 +0000

    hello switch building thumb tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
