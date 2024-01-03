22062e1953bc8be2b673b4930bab845609ea3cf3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:22:44 +0000

    grow mail takeoff tomorrow thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
