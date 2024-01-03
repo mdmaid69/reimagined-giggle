0f57fe23fec3fa976f9c282422d2a2d916fdd83f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:20 +0000

    thorough travel earth exact spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
