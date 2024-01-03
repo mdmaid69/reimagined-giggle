d3aefc4b764858201921d4bbea50601a1a59e823
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:03:02 +0000

    read repair represent out stir

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
