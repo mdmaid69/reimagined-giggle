cd4f49cfaf6563d8920fa44e0b7be53603f4260c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:10 +0000

    dry face shut watch mysterious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def is_odd(n):
+        return n % 2 != 0
