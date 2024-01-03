8a6a2d67112ce28c1c6dd370fd9bf1a224cdbfb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:30:41 +0000

    mark forgive tough noisy Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
