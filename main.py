94250edd7c1f4a2a7390205776ca30061f8cf9c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:01 +0000

    whoever beginning tall judge back

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
