93bcd39fc4841b6e98a17809f008103790e8d0b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:29:58 +0000

    punishment skirt chin create of

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
