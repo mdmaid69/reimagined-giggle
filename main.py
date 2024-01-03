a889cd9a77bf8502bb7f56c1de9d59d79f1e74ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:54 +0000

    suffer result sure moon pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_str(array):
+        return str(array)
