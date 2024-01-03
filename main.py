5ee9cacf2c819ea7c7e6f6d289803d21a546733d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:45 +0000

    keepout our thinkover eager whenever

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
