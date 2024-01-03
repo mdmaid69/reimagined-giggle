bc8756af1fa73cfc7a6e6cf167adeea12079f9eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:25 +0000

    describe drink suckme upstairs advise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
