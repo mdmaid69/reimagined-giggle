e7844a3a8cf738617a5508439c87d216c8657c71
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:05:52 +0000

    stop bringforward centimetre passage drink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
