e8e8364e707332a005acc727496dfbb9269e4e71
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:49:58 +0000

    giveaway quick behind thin 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
