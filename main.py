54dc215e5e0a6eb58a1f6521e123e7d6f13a0e7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:16:05 +0000

    recovery chance remove bound method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
