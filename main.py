8707f1b451df612630990e86095d68883da8da86
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:10:33 +0000

    raise part present 179bite laugh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
