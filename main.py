ef80c61b81800730cc45eda2963cc5e5e3ed829c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:45 +0000

    little often round hair after

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
