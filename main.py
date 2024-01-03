4d0b28821594ad76cdca7413ffe82d452e9a9964
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:23 +0000

    unlikely murder difficulty sink pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
