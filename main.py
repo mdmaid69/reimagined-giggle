8eac05035d029821866191e064ba66cef1e47c78
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:07:38 +0000

    uncertain yet about child runout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
