f1ba7d5b215ce46f6fd151496897029ff18d5051
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:53:18 +0000

    image ride danger thanks nowhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
