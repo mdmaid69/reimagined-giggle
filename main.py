c2d35975586f77ffd55f306ec052d38c77d0fe5e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:09:04 +0000

    badly top practise woman introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def append_to_array(array, item):
+        array.append(item)
