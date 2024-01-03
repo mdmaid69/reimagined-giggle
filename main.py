d83176378c3de6ef45f08d386ce9109c27cd78ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:21:54 +0000

    friend design thirsty rob personal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
