ce5c01196f050b0b73e11c46f275fc93905e0681
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:02:02 +0000

    position appear runover level favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
