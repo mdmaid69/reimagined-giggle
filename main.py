555ce694b3a075343ec2dfa8784e4bbe86a3a6eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:36:54 +0000

    dream describe moral swim this

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import os
-print(os.getcwd())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
