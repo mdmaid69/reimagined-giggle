80d62e5156656a97e12387a4070dcf4c4ecfd073
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:38 +0000

    finger target track hospital foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
