20018cc2f768c2043381419e5c5afe7f0ace57cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:59:30 +0000

    cry present fortunate disagree month

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import os
+def get_current_working_directory():
+        return os.getcwd()
