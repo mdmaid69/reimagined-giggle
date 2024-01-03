6828a7802f385750d5ad09c5aabc676b5cc43198
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:30:27 +0000

    necessary experience single bother short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
