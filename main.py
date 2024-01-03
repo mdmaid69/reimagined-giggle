44689217fdd0ee34247f64113b452fa14d342ea3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:28:50 +0000

    question dress return error none

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
