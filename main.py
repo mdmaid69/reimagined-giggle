acc7cd6662253874adbbefd529982896bb572146
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:50:39 +0000

    proper beginning account objection door

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
