35e4af68ac82406aa956e8d4aaad8b5020daa4c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:51 +0000

    comeover difficult friendly declare influence

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+print(sum(range(10)))
