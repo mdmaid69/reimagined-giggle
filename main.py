ed2079d3c0b74e7ab0c2ece89c96136e2c314871
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:13:08 +0000

    suppose upwards emphasis play try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def add_numbers(x, y):
+        return x + y
+import array
+def reverse_array(array):
+        array.reverse()
