2ac7718dc1dbd0a0367a7b74065f0ef06a3d558e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:23:22 +0000

    confuse win kill thankyou off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import random
-def roll_die():
-        return random.randint(1, 6)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
