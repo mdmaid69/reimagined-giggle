536dba98f04f9606ceb4d15cb8866e8ec8e1edb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:04 +0000

    finish large operate the shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
