ff15bf2d39a1dbcd8c412b6c6770cf133aff4a34
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:16:38 +0000

    emotion look exchange bringforward by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
