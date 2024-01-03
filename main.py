7ecb1f2e9e7a21129ba7dbf961d9cb73a7ed62df
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:15:06 +0000

    disturbing estimate dare ago Ll

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
