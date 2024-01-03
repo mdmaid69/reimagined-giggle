3bb4c07812610d41ac70a3f79e9e162e8798a48b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:30:21 +0000

    person bank kill further shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
