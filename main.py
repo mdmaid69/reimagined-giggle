4eacbf70b991d81e3fb20df6486b97d77e1bb4a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:28:23 +0000

    mouth even miss visit us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
