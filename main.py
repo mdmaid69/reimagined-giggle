0182079a4df52241739ed0b90dd9730803c3faf5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:19:48 +0000

    call goout smell left control

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
