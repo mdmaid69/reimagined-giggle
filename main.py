b57d8b5940f0262cd46e7dd2fcd3a7d5f6a15263
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:48:31 +0000

    search ring similar before drive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
