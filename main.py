9f5769838c770e1ce8298ca1b9b6047938369fe1
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:46 +0000

    hope order stamp waste lay

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def reverse_array(array):
+        array.reverse()
