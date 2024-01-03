c423a12372afb73d1a8f45c59ba074ec0474024e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:14:36 +0000

    law escape beyond exchange comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
