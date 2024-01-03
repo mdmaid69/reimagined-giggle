8002d4434d932265cd8cd65db7af98d7a8c20218
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:18:56 +0000

    school win hair calmly belong

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
