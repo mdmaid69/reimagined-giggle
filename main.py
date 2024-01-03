20fefc879ec32aad26dfd6cc146a84e15ead4eec
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:16 +0000

    hole dear another speed knock

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_floor(x):
+        return math.floor(x)
