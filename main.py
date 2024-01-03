c9c6d400eb6fe2e138aabfce54b9b38475ebafce
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:05:44 +0000

    condition degree loss freedom temper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
