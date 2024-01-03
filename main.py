c28bcb5e0f09d4cc66aacf9677edceb5786976fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:50:35 +0000

    attempt else OKorokay wrong spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
