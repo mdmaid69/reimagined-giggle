9931896cbd406eea5ecc05eb1231cf8f1ae97a30
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:17:53 +0000

    manage keepaway fair look want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
