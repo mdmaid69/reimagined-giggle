d233733c4bb2a7538fe5537a0f63b6e5c3331a3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:25:37 +0000

    development platform crime hide fine

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def count_characters(sentence):
+        return len(sentence)
+text = "Hello, world!"
+print("Characters:", len(text))
