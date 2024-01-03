e2d263ce1230e5065c6e391eb04e9870040d63fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:41:48 +0000

    loose cough general criticize sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def clear_array(array):
+        array *= 0
