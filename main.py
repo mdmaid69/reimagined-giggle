1864e9887927aab7e269e7e2ef1e53beceb8d73d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:00 +0000

    exist every exam sit unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def subtract_numbers(x, y):
-        return x - y
+import tensorflow as tf
+print(tf.__version__)
+def calculate_area_rectangle(l, w):
+        return l * w
