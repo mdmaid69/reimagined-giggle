e760ba6a98e1eedace514dd86ee0d837fb1f4bf1
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:11:13 +0000

    lucky spray remove arrangement holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
