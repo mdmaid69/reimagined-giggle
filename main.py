175d2325b4a56b117803d989112b9797e368e55f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:23 +0000

    guilty operation prevent tidy cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
