8f0f726318474b2623a7a3c308dbb311a6aa5b9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:32 +0000

    any problem set name concerning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
