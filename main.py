43ccdddfb8250a3762e15bf6bee63988a82d902a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:54 +0000

    introduce pronounce natural break score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
