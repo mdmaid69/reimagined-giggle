d072c71354cf9ba8911af0ef20fa06431840aaa4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:25:35 +0000

    culture brother why forever spray

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+import time
+print(time.time())
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
