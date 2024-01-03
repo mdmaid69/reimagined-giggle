377418bb808ac112bd88a9dd177709dfe03cc3f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:35:46 +0000

    possibly breathe legal goout recover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
