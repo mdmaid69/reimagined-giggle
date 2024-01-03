e2a9a41d9ca4271ddab285f2100a9a84cd7826e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:26:07 +0000

    suddenly start bottom judge tragedy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
