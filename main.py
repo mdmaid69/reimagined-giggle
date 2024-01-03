8fae27bc2eb15377ef5ba0130a21a1e2ad341238
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:37:44 +0000

    goback(to) condition force behind payment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
