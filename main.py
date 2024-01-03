a4bcee7c13de5c71193dcd8a3cdd7f56423d8ffa
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:57:09 +0000

    joke against result way replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
