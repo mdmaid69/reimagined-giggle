dc68e4b20757e429daaba24c8cd7c563cc732e31
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:02 +0000

    touch total through lock time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-for i in range(5):
-        print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
