6d0fe48c666f5be476fd86992afedaea351d0b93
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:38 +0000

    compete Ee boil yesterday contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
