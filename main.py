6bb55ec9bd349045ec6bf1fbb37826d97b8c648a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:27 +0000

    this note pretty for half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
