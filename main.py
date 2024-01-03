e32b40c477b2382ecd437c0cd113f8b7125ea8c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:33 +0000

    quantity unless favourite punishment famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
