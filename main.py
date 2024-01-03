da80f8c469cbe5bb1f0df162ef67ac9491840ab9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:48 +0000

    represent black joke succeed direction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
