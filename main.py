fa83a26ffd98a8d7e9d762698f9421b31bfe5e35
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:57:50 +0000

    mad summer common either peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
