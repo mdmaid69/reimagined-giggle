59fd717a66958147780e5e685b69f4f58f71904a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:37:56 +0000

    shout carry grade rather oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
