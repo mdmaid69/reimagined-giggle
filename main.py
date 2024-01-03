7f4f2ff154be8d4c8524386d18e78ab94a8430c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:28:30 +0000

    die study file raise petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
