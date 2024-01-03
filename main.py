f8ed8c5136b1382acfb7109f53a348b240832029
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:13 +0000

    steady about pot opposite rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
