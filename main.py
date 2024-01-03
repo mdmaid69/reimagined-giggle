e92d77396f72ec0f63b027708fafc6c38707188e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:03:13 +0000

    any recover fold afraid keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
