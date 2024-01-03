c4290fe5901e7791771565ccd562a653927cb077
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:15 +0000

    negative decide chin refer edge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
