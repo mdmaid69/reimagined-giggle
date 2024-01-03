6f5cb04b9143055e1048093227c617eb96ef96ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:50:02 +0000

    worst Bb forever more sell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
