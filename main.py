a699faad3953b96d92d5ebd1f3537984404bebcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:15:41 +0000

    notice thankyou shine lean ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
