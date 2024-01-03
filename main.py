7fdc0e1ccf5f4141a2958509448f681eddb98e63
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:55:04 +0000

    godown rain interview party limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
