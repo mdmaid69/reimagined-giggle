ce673ab35d94ecb67b72b4fe4e70f1af842a7d4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:08 +0000

    kill mother content field broad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
