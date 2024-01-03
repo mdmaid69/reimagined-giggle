875b5dc619a50d97b561b51845b56ecea5750eba
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:27:30 +0000

    onto comment much must fire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-print(math.pi)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
