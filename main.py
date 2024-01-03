16807dcbd08999953b9c08ac6f171b34a5bf919e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:54 +0000

    mean noise laugh present later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
