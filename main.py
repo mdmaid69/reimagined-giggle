887676722d4e886cf6082384a1f3fe07635e3b38
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:42 +0000

    properly height bottom admire impressive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
