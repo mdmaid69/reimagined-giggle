77ce57b3ab49d62edaeebf21d6771410972c1dc5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:32 +0000

    scale ball care whoever flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
