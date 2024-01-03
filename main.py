e0dbcecd6ae6eb5a90659bd81d86af82f4e956b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:55:30 +0000

    result plenty low summer last

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
