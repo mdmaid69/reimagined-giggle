096ac299aface9c6c91cc866d0abcc5defb42ccf
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:34:41 +0000

    fit catch another sport calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
