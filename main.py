080b044c93b0a079c74f4a9403c47f3afad952e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:45:17 +0000

    favourite workout build smile address

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def is_palindrome(s):
+        return s == s[::-1]
