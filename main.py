c85b6ead3ad8288e0199392eddc9749dc31411e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:19:22 +0000

    ago present walk handle few

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
