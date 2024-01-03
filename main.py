4c3ffdf103a87772900e10363db5b28645fc55b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:35:21 +0000

    chief make method to well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import collections
+def create_counter():
+        return collections.Counter()
+import sys
+def exit_program():
+        sys.exit()
