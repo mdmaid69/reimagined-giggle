e18f97b733cc16047a5ccbd7d8b14b3ca94cde1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:06:52 +0000

    building beneath model body exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
