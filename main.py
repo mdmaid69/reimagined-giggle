6beeb3adef48a90cf0348f241158e262ad721fd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:48:36 +0000

    concentrate clean slice frequent defeat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
