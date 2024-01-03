2a2e7db09bf55c41ab22632947189f34cb23a193
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:58 +0000

    hide give thanks support level

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
