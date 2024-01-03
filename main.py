1a7a5f8a543faf7ee64353d2ffa665284ad0b2a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:05:58 +0000

    bet process every even new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
