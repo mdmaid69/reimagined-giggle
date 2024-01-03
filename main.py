a9183d4e80ae3af7a2684b684f997cdb6fa4f43d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:58:03 +0000

    knock 565end pay fix first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
