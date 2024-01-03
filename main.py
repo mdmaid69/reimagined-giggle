834a765cfdc908dde81f794ba7b0107458b86f50
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:41:17 +0000

    tone somewhere arrange natural strict

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
