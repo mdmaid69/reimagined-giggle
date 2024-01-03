43d67ecb6c3cb7aa880105ca40dffe099d8a222e
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:23:22 +0000

    spring question guess private fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
