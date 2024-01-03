b237d40cb91f65bd9791f12c11af0766b0f4bb3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:46 +0000

    practise rest number carry private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_length(array):
+        return len(array)
