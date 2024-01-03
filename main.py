ef18c86de8325b3aa48e34aba2d84b37126140b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:19:15 +0000

    special deliberately conclusion pronounce flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
