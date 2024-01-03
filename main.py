56d9f3b943dc612c1e6ee646f693374def59b373
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:08:53 +0000

    secret settle 567enemy turnout bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
