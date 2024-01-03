40affcdd342e7b9d4bdef79b9e230622c361d653
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:26:52 +0000

    near top past except hand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
