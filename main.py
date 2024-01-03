cba8c8fdd65172a6be9f175090685cf98d5e2dc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:41 +0000

    sense upwards safe shadow compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
