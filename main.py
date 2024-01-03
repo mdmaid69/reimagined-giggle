d4d93226263c7e75c1f6adda45f006eb50929c56
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:38 +0000

    step just their family sorry

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
