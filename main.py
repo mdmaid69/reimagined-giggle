8738350ab356e26a6ec513b7f208e197690687c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:39:53 +0000

    machine squeeze creature rate satisfactory

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
