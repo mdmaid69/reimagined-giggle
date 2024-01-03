c6fc3d51633470aad648bf2b016605fc93a34426
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:23 +0000

    thank nose prove drink position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
