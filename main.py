cd0f39f86f401e2d0d93ff16f3e48ae7858bf5ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:06:24 +0000

    goon telephone Ll sound sea

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
