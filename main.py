9e9df697eaf5dee668dd52d862aa793a0b310c4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:50:26 +0000

    regional quick waste something custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
