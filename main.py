49c2712f7495ed66051fe9499dcfb5c4bbbf9e4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:35 +0000

    die hard style related steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
