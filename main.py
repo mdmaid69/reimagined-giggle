c5d55cd229d47b0284d0616cd629ca7b241f554d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:12:36 +0000

    worry train bye part absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
