ebca83663c9d4bdf5c15f8713485887ba1a6ed2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:27:36 +0000

    wish sweet contact hurt spin

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def is_even(n):
-        return n % 2 == 0
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
