66787c71131d118ead9b211e99939b96ba7cf1d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:21 +0000

    obvious Ff paper 570enjoyable meet

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
