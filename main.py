09debda2f9e43a808cf2498205192b375ead1dbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:35:44 +0000

    right stone arrange test many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
