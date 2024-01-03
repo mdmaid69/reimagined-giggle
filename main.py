ead27ae9f5e255f5e9f0f119813223a999aa28af
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:49 +0000

    guard tax really although right

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
