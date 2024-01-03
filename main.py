2badc7e076bc9d48dae92cc00ccd6c6fc654ccd2
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:45 +0000

    draw needle willing comeround it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def reverse_string(s):
+        return s[::-1]
