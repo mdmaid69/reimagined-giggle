c841e6295fedcc066b5e0bc902f707a6d1c4ab71
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:43:31 +0000

    half confident maximum something onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
