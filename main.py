2c20b5e0087ddb531750186d55b156ed8f2a0de7
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:36:37 +0000

    reject other yourself bad holiday

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
