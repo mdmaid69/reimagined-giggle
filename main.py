f4a8453f34ae308e891cdec99cd962d776316080
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:30:31 +0000

    surprise that among telephone dear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_python_version():
-        return platform.python_version()
+def find_min(lst):
+        return min(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
