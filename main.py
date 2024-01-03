4256b0e57bd8726287e0506aef3f1aad1111158c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:46:13 +0000

    forget pause sore straight regard

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_str(array):
-        return str(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def get_array_item(array, i):
+        return array[i]
