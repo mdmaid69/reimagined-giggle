13e4454a91834064ffb85f7e7ec409b06235408f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:53:10 +0000

    nose it one reason agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
