19e6249cda7596644dfb762f1e2879e138b5958b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:43:47 +0000

    choose advanced through eager follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
