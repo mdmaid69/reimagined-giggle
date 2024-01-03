6e36ecf58b7d308c43ed37880789983505c43ccf
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:48:09 +0000

    573entertainment lock guess abroad work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def reverse_list(lst):
+        return lst[::-1]
