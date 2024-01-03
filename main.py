8f949dc547aba1a247becb73f33d28574cb9169a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:36 +0000

    see respect cheek clue plant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
