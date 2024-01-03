433e68f32e5c652dc7ee70d6f334a6587aba5dc9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:38 +0000

    fast advantage suppose pool least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
