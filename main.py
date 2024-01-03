619329b6747ce5cbfb3fb0e90abae705d310409f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:35:49 +0000

    any case recent delicate cry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_work(force, distance):
+        return force * distance
