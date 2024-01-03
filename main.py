59ffeca7755e631dc7c5983968c5421f95b3fceb
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:30:58 +0000

    even within nearly healthy month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
