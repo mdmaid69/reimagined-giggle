8cd8b691e35f1a4757a2be5064ee74db92c1b4a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:17:57 +0000

    gas patience suggest glad imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
