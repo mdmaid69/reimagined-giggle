59c294afdf78e1be7fa85708c18e046bbd6a44a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:01:23 +0000

    turndown content child arrangement admit

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
