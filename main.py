7304e33d02d2e269e472db6de4e19235749fa672
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:22:44 +0000

    some thin interview lookaround interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
