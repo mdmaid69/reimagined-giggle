364da5130c1b622c8f31900232fa5bbea142f5ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:43 +0000

    neither emotion talk between scrap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def cube_number(x):
+        return x**3
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
