3c083c05684fc9f68bcf137dfa1f53528831d78e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:47:49 +0000

    middle push side summer this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
