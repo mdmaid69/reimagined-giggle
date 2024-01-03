cfbd28d1ba5b42dba9d9e0e5e7232532bee909a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:50:12 +0000

    where pile correction foot fat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
