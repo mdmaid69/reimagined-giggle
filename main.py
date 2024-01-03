474fec8b3ff75f91c791f52ad2ca68b32004b6a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:47:20 +0000

    bound under possibly uncertain lose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
