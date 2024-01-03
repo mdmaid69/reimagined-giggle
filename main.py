3c99e4239ba35356c384ad8b1fbf8e81c5f472d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:06:53 +0000

    lookback(on) jump accidentally slightly final

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
