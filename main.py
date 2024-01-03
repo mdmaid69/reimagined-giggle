b70229733c7aba804caf6546e3dec41c38641f03
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:29:52 +0000

    line weather prefer telephone convinced

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def find_max(numbers):
+        return max(numbers)
