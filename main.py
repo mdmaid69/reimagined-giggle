77f6d11eda13a6165ec22beb378379d1e1e7ef8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:37:14 +0000

    shock agreement certainly sometimes them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
