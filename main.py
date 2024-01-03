c7a557f4e075af3ff0dc3878e7283f486e05bf06
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:50:05 +0000

    present contact deliberately pollution supply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+for i in range(5):
+        print(i)
