379195dfa8323595b15a0bfc6ab5bb3c4847465c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:03:47 +0000

    point mistake exam ball add

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
