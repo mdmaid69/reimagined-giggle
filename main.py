b5d23eb0543b3d016642fe16c02aa01ca44a125f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:35:14 +0000

    off education winter Ll one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
