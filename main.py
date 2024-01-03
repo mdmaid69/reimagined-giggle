c27a7432aef8fc9ad46b27dce952c3a3a1110e6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:48:36 +0000

    basically along still wide firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+for i in range(5):
+        print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
