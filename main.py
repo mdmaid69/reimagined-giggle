a964c632128e94788312da4f507b94bae986b430
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:24:55 +0000

    Rr list flow clean themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
