06b3188d73f4b7642aa05875c7dfb7778542ab8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:25:30 +0000

    owe breakfast up independent degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
