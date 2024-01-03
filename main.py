e5a0f83b0235f83c05001243ac51e493c31d693a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:32:26 +0000

    bag aim round because introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
