3484e008252d8e984b1be4e0e36086f68d7ea725
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:56 +0000

    tradition praise obvious turnon voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
