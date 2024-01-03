2841fac28b9447bacf20d21f379f864e708d2e29
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:21:43 +0000

    demonstrate side might paper somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
