3b2afa558e2d3bee09bdaeea7dc85053856e4689
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:27:12 +0000

    suit spray mess reserve average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
