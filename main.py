d3ac801f5d983807010c1707c72a2ded279a6ae8
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:59:59 +0000

    official satisfied charge stupid opposite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
