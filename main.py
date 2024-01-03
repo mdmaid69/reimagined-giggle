c2b6faa26f3dd2a19a98f30cce49822ed14fdbc5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:00:01 +0000

    seem education failure sting kill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
