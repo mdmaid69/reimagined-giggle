773508942c1207aa1d9173a605479d2e4fecd654
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:46:03 +0000

    Oo visit might cover intelligent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
