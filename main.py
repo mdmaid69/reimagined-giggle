75fc28948091d039c06e7b11db4c5f1df4744fcd
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:18:41 +0000

    up please tragedy outside like

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
