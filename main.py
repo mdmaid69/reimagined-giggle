649edab27a8589d2e683ef4c6bf1472ebe3e3600
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:42:57 +0000

    wave like crowd group muscle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
