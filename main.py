0446a1b8a6faf41f0c3c2f2a6c41e661e59a1a75
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:55 +0000

    line protect dick breakthrough concerned

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_area_circle(r):
+        return 3.14 * r**2
