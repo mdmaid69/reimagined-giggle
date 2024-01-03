5c5d7d9310740304b5de71284498237580b7feb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:14 +0000

    handle opposite identify pride lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
