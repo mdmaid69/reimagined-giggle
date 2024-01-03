4faf684f2b764301e3d91642726ab390ddd5e9e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:53:09 +0000

    swim star song somehow properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
