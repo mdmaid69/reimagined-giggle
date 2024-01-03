814e06182441fb6d0abc355279cbfa7cb9d3f738
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:31 +0000

    slice use shrink please wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
