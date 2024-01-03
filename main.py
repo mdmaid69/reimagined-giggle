18c7bf75fcc6448d98a1bd4d95495c0d0938ea09
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:52 +0000

    present factory failure solve serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def append_to_array(array, item):
+        array.append(item)
