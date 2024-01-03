8d5aff7e32c77fecbc20859fcb4f3b823410d96e
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:15:50 +0000

    as receive today talkoutof bin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
