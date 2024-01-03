6f99a17a32be1ac37ca40e55317ebe1a5696408e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:29 +0000

    talkback want basic once offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
