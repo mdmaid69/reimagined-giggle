592d76292866f47b3016b8f02acbed27980b054e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:31 +0000

    understand trap clothes ticket shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
