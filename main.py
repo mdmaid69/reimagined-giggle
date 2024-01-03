f9e3119d64d5e64e09105e6387736c9fab8f07b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:03:39 +0000

    science steal summer suddenly read

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import random
-print(random.randint(0, 100))
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
