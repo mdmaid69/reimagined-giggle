a41765782511d3f7228d63c3d9eca5435e5c753b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:21:32 +0000

    gooff plastic organization equal comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def sort_list(lst):
-        return sorted(lst)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
