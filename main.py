b245b74021bc1fecd14798087cb987a699f794cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:29:29 +0000

    hurt bend word of reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
