75520470e9e475f6c340242459f0828d33b39f30
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:16:41 +0000

    there concentrate this shoulder difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
