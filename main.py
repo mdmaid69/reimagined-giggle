3d6266d6ed6d4966cc42c9fbfffbe1c28aa01a42
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:41 +0000

    approve instead quick neat object

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def greet(name):
+        print(f"Hello, {name}!")
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
