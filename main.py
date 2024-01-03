b5c6989d4ac7a75eb5b692a406b56cbe6e4fecba
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:40:10 +0000

    turnover consist charge female star

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
