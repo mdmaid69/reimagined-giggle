2e0b89c677574f6fa521fd16fc7ca887275a514d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:35:13 +0000

    wake model attitude gradually push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
