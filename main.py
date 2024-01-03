eff965f5b948569827a337c81764b5ab013dd2a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:22 +0000

    best miss arrange talkinto both

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
