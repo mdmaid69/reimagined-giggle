d76858395ef781e4b15b7ac123c56aec4d8a5b0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:08:20 +0000

    experience spin warn oil stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
