459b27f9e5c7a246d933b27d525c4c5274ee132a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:31:02 +0000

    sensible ordinary show passage welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
