627ce968fa732b3fc7de82680862109b84129d35
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:10:51 +0000

    criticize delight event sweep face

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
