5ceaca58f9830b14282c275dd2784babd94fe04d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:21 +0000

    relationship forever only search embarrassing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
