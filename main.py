7fb9ecca86c0a1fdd8446ce33e451110f07f7438
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:55 +0000

    between turnaway meet angry idea

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
