8cbe2c7a9113a4d3d55e283d3a1a2f281bdb95b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:19:13 +0000

    Ii everywhere shrink free stage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def sort_list(lst):
+        return sorted(lst)
