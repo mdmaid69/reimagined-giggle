338d12e38ee29303ecc1f410e390824bbfe4ea2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:52:30 +0000

    rob roof plenty away each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
