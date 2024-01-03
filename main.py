de6c4e827715dfb0ca1f62cd8ee35673aad4a3b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:52:30 +0000

    wrap brief read invitation best

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print(os.getcwd())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
