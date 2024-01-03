18d72617826168799d8673e0cd8589b9885c547f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:06:06 +0000

    touch steam below still apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_counter():
+        return collections.Counter()
