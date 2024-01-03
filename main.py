b20a745221b1065d7afaca9da98684449a3e850a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:12:17 +0000

    pinch centimetre length price cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
