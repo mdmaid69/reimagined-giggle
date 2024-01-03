2490a82a3431010ecc816bc9a4a21fad2d13d13d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:18:55 +0000

    pay thumb production print recently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
