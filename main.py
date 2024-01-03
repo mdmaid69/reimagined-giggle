ddd6f9fe04f04fea6543fc8da62e4858ec10e23e
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:01:00 +0000

    half cost post finger comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_error_function(x):
+        return math.erf(x)
