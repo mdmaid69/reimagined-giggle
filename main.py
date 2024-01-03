58f3f77f8ca30ce5d78270ae4cfe097eac4cf11e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:41:37 +0000

    cost public through that yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
