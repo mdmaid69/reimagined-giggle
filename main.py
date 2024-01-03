654954746a6ac942385a6ce607586210a60e296a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:27:12 +0000

    blame score not dress radio

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
