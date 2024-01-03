643a765b24c3b85593f155cc2beefc592301a3bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:19 +0000

    fairly slowly even tonight finger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def find_min(lst):
+        return min(lst)
