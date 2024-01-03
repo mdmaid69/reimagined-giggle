68a8748f332022df02e681362a6d82949c2599d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:14:51 +0000

    period perform glad pressure thing

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_speed(distance, time):
-        return distance / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def square_number(x):
+        return x**2
