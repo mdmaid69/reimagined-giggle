14076a75a3fb80d5f6f4464dcc09c3dee07f5f60
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:53 +0000

    lock like persuade vision honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
