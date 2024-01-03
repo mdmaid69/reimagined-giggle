4d75c5aaf21e2a4aa05c83ed9bfe10a910edf1c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:16:48 +0000

    bored tone improve drive Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(numbers):
-        return min(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
