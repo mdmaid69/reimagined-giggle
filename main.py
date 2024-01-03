62a8a345cfc7ce94c3a80ed692cc16775e0c6784
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:01 +0000

    hide game wing responsible risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
