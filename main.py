80e847d57fc4d3e39c3773f6bb5e0908e66bdb66
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:24 +0000

    school war Itself turnon over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_power(work, time):
+        return work / time
