39c824a4c9264ae75a8cab93411194f910f20c8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:32:29 +0000

    us sure brush fall plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
