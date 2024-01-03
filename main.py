da059e64b5d81b93c628f9b345a3bac3c5348c41
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:23:58 +0000

    pattern design scale wish independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
