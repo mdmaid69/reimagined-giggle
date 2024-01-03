b76f6aa1d2250241d0da82c8aaa12313ce8bb3dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:14 +0000

    immediate class watch port bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
