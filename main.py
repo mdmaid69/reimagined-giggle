0800be0bb34d7c7d3d0ef8d32fd889e6103fce32
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:33:33 +0000

    turnup disapprove lookup peaceful nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
