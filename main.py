849414171e0186827ca171fc3d260a5a0aed693b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:24:45 +0000

    inside go turnup wish agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
