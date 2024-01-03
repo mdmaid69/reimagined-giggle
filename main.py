a52ebb48ac1d8a371a4c5c46aaf9ff2fc06ba3d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:16:50 +0000

    seem holdup risk listenfor come

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
