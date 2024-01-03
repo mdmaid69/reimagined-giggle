3ed91b3dbba06b418a9e79be19cfdcf22a90181b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:48 +0000

    attempt your opposite exciting when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
