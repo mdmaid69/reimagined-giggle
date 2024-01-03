63d216252eb26017087ae6e991f9ff1f0842ad9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:43:22 +0000

    hardly sensible air separate wind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
