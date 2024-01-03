58139916eb714680657d280946da632dd9018568
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:35 +0000

    speak relationship forget tall mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def divide_numbers(x, y):
-        return x / y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
