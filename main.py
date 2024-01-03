fb05922ed914aedb9fefbac1041d579094745182
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:15:22 +0000

    order morning area man regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
