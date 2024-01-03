f2c756e462c3ffcf846bd569e4b100e8c7a50d53
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:01:42 +0000

    good shut Vv separate thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
