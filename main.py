704c3b400e9c4e995069e4d7e23cc92b3b45fed9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:10 +0000

    memory slice physical section violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_repr(array):
+        return repr(array)
