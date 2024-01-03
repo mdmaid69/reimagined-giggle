105a7288cd5d145f3cf5e11cbf191ba848381c8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:02:08 +0000

    care too steady unable opposite

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
