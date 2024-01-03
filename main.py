af9d01a9c52b3dd032188cb8ee7d60384e1ac07b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:59:34 +0000

    wipe you point then material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
