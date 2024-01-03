f494e569212a2871b3837e5c8d322300738e8ca6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:20 +0000

    cap race artificial sort myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
