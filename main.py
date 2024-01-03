bbaf7c9d4465bb4bb279520cbe01bb7e5a68c6d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:12:57 +0000

    detail anxious possible correction care

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def sort_list(lst):
+        return sorted(lst)
