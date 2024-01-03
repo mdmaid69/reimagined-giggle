3b90667d794248cbed8c4d94620dcd794b366d98
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:48 +0000

    clear arrangement worst copy worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_typecode(array):
+        return array.typecode
