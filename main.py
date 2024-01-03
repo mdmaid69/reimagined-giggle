717716de825d58b4ffdbf8167b14240b68e44850
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:02:43 +0000

    single escape circumstance he recognize

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def subtract_numbers(x, y):
+        return x - y
+  def reverse_list(lst):
+        return lst[::-1]
