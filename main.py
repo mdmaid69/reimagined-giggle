0556ad686652cb972a2dc3505a1251454c3dddc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:09:42 +0000

    unless comment march could little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
