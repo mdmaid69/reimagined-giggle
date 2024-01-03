c969b469ae41173702e2008bf82aa2fae10246d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:29:11 +0000

    uponon choose absolutely brush their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
