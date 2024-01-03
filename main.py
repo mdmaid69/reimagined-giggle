296dffa895de8d28a5d2c7227c46f929a0c41f14
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:17:04 +0000

    calloff form claim yours complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
