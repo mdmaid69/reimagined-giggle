9671cb9d3294d316c224e6c6ccd4492a031976ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:01:36 +0000

    drawing concerning ready put bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-print(random.randint(0, 100))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
