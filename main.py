bebee79d5c84c35cc93a5301295f9b734b41b113
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:27:23 +0000

    both university between factory tie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
