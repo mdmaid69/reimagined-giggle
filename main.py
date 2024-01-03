60c1e6583a4dbecb02653cefa7d70f92b614396b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:22:56 +0000

    answer light connection trade himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def remove_duplicates(lst):
+        return list(set(lst))
