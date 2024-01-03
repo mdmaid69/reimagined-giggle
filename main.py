940c3830249ab7d94db127b608fcc8bbfc140b72
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:49:04 +0000

    number person loss straight clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
