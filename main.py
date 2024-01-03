ecde307f7064468d2cfa6d6fa51f81c86632a808
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:17:51 +0000

    vision sure ocean always type

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
