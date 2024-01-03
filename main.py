266c378b74ba5b7ae54de1526fc5de398f009b59
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:01:53 +0000

    gift foot disappear straight sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
