b334cc4bc0aec87a7eb061572fa21f062a3c2614
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:11:41 +0000

    experience post fail floor scheme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import tensorflow as tf
-print(tf.__version__)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
