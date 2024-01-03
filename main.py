a7a631c8d7b36b4991c8c02660b65c91627cbb59
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:46:29 +0000

    future left battery prefer 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
