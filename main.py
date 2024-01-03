b30aba4fc97b90a3a1099e9e1e5cab444d1044db
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:11:16 +0000

    trust original thankyou Rr attempt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def convert_array_to_list(array):
+        return array.tolist()
