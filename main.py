8c003e5ccd6676624a92dbbeb3090dd6e121cbb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:35:40 +0000

    illness quickly ability area plant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
