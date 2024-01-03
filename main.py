e6f1b274d71ca91920a3d9990b1470ba49645817
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:16:00 +0000

    slight uncomfortable simple switch chin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
