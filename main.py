e7e5c1ade61faa3b4ca24cdc91c9d9f62dece1a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:31:43 +0000

    beyond fast challenge deep busy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
+n = 10
+a, b = 0, 1
+while a < n:
         print(a, end=" ")
         a, b = b, a+b
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
