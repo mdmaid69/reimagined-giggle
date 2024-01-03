788189e1abc95e8313b5fba1a9a209b1d540fac8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:18:27 +0000

    brain neither might float impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_speed(distance, time):
+        return distance / time
+import getpass
+def get_username():
+        return getpass.getuser()
