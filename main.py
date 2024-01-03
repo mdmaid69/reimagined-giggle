acf1ed0799189add20336ec9edeb2565c613ed9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:39:21 +0000

    luck offence risk Qq nature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def square_number(x):
+        return x**2
