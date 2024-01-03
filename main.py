dd05ff7b777735b6ea2fbe229ccd56383762174d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:46:06 +0000

    soon takeover reliable music necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_density(mass, volume):
+        return mass / volume
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
