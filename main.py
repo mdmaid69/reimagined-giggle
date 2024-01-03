fbdb6825909f35bf2b74e1aef9cfa1679588dbad
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:43:14 +0000

    edge suit black wake admire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+for i in range(10): print(i)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
