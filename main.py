384434c805ddf3f672fe88a39787ff1526f2e7e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:32:40 +0000

    pity slip concern handle thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
