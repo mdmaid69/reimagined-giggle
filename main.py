fe7f93e5db3f5a3bd48065ed213f31b037c55dcd
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:24:21 +0000

    dry runaway cinema union main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
