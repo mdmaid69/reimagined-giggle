bbf4a443effdff348b494bf1e20b2abe03aa678e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:48 +0000

    ability clue club case friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
