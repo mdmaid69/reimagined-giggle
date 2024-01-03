ae5f1a686ed40643f7b307465947944b33162f0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:59 +0000

    walk enthusiastic difficult address sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
