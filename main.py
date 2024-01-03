631a1afd0ac10a9b0d293a503c5e8e2cfb22511e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:26:36 +0000

    reliable pleasant separate flight rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
