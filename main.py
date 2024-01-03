72ee784acce32644b555a4021c57ba4fab6e0f25
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:21 +0000

    workupto beneath firm shoulder straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_characters(sentence):
+        return len(sentence)
