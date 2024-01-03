8d2af494da1cb0bc26b4cbb5b3b40813248b4536
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:11:34 +0000

    welcome after lock married flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
