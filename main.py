c3e3da156f27ed1f9a433765447a215b18c735e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:27:55 +0000

    confident square way compete lower

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
