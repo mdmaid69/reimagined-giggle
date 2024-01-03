9d704ac273b9daf65557e806e856f353e224570f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:53:22 +0000

    trap skirt let tall entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_even(n):
+        return n % 2 == 0
