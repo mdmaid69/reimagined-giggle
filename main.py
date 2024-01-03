ecf28f3e8adf37fe2ef6d0e80655dd3d9b7e81ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:12 +0000

    also shallow Kk as complicated

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_density(mass, volume):
+        return mass / volume
+def divide_numbers(x, y):
+        return x / y
