85bddee8be97550cc59d30a2a5c7f221f9de7aaf
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:55 +0000

    juice steal fear black turnback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
