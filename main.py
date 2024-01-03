9207d821229e6cf10091b9a94ebc2d1761144012
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:24:32 +0000

    heavy so survive may lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
