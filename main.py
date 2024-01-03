914416a140c09716d1db1db34c91c83d7bf2cb5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:36 +0000

    record argument shopping song much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_directory():
+        return os.getcwd()
+def count_characters(sentence):
+        return len(sentence)
