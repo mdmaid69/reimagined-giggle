6133762ad98240075d3020592d098caf90a007e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:46:57 +0000

    plenty uponon account whisper not

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_as_list(array):
+        return list(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
