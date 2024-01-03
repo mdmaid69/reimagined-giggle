4157aa03a22d6bbb56b3b4ba49e2a96c1dab2411
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:58 +0000

    hold top tear object sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
