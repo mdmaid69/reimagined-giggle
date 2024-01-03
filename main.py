46e172c1e1cdd951fa66353dd1b228def83cc457
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:42:13 +0000

    image temper thinkover snow doctor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
