912d9569f5c31361abd1689a733a76c0b972ccf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:53:04 +0000

    bury vision happy push control

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def print_python_version():
+        return sys.version
+  def convert_to_octal(n):
+        return oct(n)
