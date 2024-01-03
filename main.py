047f0f6286be22b67755a111d52f2b889c941295
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:42 +0000

    neither slight cheap exactly offer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_to_hex(n):
+        return hex(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
