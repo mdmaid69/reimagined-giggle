b5c6814b94a8db3e871d2c99313d99e642a6627a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:09 +0000

    steam main sail swim repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
