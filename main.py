3716b9b8b9de016dc5d508b9b49d99075ed5ed9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:35 +0000

    left interview yesterday runaway meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
