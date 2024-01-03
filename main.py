c6b55f577460004f0a301452c0a895cda03c148f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:03 +0000

    first result no runinto the

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_pressure(force, area):
+        return force / area
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
