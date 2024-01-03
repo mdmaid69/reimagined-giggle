6c8621bd6846797233cf12f24838cf7e61eb7f79
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:02:20 +0000

    demand workout fortunately knowledge constantly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+name = "Python"
+print("Hello,", name)
