96acdbb3cbb389b5fa9e29f577190989c9b422ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:52:46 +0000

    relationship her dry office hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
