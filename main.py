4ebf1a443e00104bf19267c0d5cbb52ea3fc3b0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:15:50 +0000

    comeback damage delicate matter wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
