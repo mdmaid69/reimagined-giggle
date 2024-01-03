73d2dfa15993628a30d06e72978134f1c8c7f697
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:10:33 +0000

    switch remark worried behind accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
