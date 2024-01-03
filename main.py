170aa7553dd7ccc8d893c6c0a2bcd0b71d2b8fbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:58:38 +0000

    fence apply lend attempt later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
