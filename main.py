cb96debb23099c626eac51957e1d2077c59d6bb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:55:32 +0000

    illness money lean go protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
