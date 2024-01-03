4f5796e7f989db206fd4acf4102dd61c15a1862e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:06:18 +0000

    subject stage second quarter need

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
