dac39b7799f2df9800c80c023e621340320f73e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:20:02 +0000

    reason part case considering what

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
