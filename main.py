37f7eed326608f99c81c9e94a908f1f93208646b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:19:16 +0000

    demand practice undo save wide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
