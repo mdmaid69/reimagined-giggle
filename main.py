2a1feff23f4bdb70003a7d1ac6212b3b56c6e153
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:53:58 +0000

    list shall criticize unusual loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
