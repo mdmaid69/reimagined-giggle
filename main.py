b1b1760ed4ded80cc0a4b740077fe305f3bfcbf2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:30:02 +0000

    Itself cope often decision remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
