3ce9e2cd7b84c08196d590729f57efd08e343e7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:20:50 +0000

    by commit design attitude glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
