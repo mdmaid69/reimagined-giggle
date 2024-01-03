40335726a19a391f5fd565929f55d149ce539a62
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:37:30 +0000

    spring below theirs rain arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
