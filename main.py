408d49d0753409c1ef4abd1d9d878c6b45544dcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:50:35 +0000

    comeback roll weekend steady trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
