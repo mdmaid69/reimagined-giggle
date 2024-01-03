300b373b65178978e5c3694b75b1fee6cefc0dba
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:42 +0000

    bag tragedy shut detail bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
