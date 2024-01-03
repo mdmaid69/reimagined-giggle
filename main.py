fe0400cedd523f9c05830657b8feb6d91f36f557
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:59:27 +0000

    serious negative impossible head bringup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
