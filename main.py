a697abf9f067f99b3c1b6b37d7c768c3e1b6d351
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:27:34 +0000

    argument compete fast disagree thumb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
