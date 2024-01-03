45f25004bf53dbdebd0fcd98c3abfc8fecdaea0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:27:30 +0000

    exact attempt combination dance brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
+def calculate_area_circle(r):
         return 3.14 * r**2
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
