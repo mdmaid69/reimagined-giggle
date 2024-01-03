cdd59cd46c4a015bedf81909ce5a78a311b9a522
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:33:01 +0000

    personal under emphasize full unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
