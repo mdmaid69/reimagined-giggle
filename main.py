bd4638db782228181e4f31ba5fab9beb0efff41d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:16:25 +0000

    finish train say warning gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
