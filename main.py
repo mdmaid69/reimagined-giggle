4461f83a2744d5524800e9eb9f5c93e81d8cb9e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:48:38 +0000

    trap beneath hollow takeover want

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
