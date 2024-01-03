3bdce0a54500b423e3a3166c753afabd9e80386a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:58:24 +0000

    practice thirsty greeting difference fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
