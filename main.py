2655a17c5e1fa1b54038f412df25e43a5aea0e81
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:50:14 +0000

    hurt there workon show trial

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def count_characters(sentence):
+        return len(sentence)
