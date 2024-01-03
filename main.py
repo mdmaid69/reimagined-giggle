a6975a93189333435015dea73ebd5a6cac9b3c55
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:15 +0000

    begin little dead greeting week

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def square_number(x):
-        return x**2
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_str(array):
+        return str(array)
