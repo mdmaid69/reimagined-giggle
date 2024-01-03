20b70a7c0b7d70807ee09eb366f0a20548ffce24
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:10:52 +0000

    without pencil recognize minor scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
