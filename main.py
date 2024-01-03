4c65140c93d41d92a00fe8a183bfd6e9ccdbf09c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:19:03 +0000

    month increase entry plenty impatient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
