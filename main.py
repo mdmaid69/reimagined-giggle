e1eec233f7c23648fb204b4d59c1e0db37a95ac3
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:21:15 +0000

    have show blind below chance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
