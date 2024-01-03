a823b410d8837f5cb6b8849701567aa5326d42e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:35 +0000

    bus range could target imagination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_work(force, distance):
+        return force * distance
