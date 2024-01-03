668c34e59a3d05ef3535af764351e3c6dc191888
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:31:11 +0000

    pool return turn system drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
