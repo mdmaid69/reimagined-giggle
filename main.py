9b55f5b373b2cbe95aa99392accea141290286bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:16:15 +0000

    equal nigga purpose again thank

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
