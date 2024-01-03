91754299d7016ceca6f41ce5dc6e2ea1e2647f9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:48:37 +0000

    under case birthday how impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_typecode(array):
+        return array.typecode
+def convert_to_octal(n):
+        return oct(n)
