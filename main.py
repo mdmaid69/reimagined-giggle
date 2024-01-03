de99c62b319f4f78516e4de9744fd0bc1551ad8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:46:31 +0000

    spend book open neat later

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
