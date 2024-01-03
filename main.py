4a4f4b964bedea1a7b725045bd657c616496153a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:20 +0000

    thing theirs talk cause positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
