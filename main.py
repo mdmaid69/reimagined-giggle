d7e43ced6bfb1ac874bf33cc522ee189ae8cc86c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:04:41 +0000

    attach community danger nor regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-print(os.getcwd())
+  def delete_file(file_name):
+        os.remove(file_name)
