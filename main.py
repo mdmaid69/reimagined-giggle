f646c265d4e5a12db832c13d5884ab4bf24ab343
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:24:56 +0000

    petrol shopping fresh breakinto use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
