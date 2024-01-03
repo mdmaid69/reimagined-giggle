03f827396ba5a2436170bdaff55699f83c35a8a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:30:09 +0000

    wall badly there trade education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
