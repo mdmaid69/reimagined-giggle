f2257f79f01a8fa369b757cd7a1cc6094152e7bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:15 +0000

    estimate walk debt warning change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
