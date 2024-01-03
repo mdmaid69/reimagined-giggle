317a9173cb648f75da0a8308d0dc677b3077dd9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:01 +0000

    belief thinkof each contact violent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_string(s):
-        return s[::-1]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
