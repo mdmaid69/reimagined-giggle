afb2247fe61630a72022c0233716b1b185a3412e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:55:41 +0000

    aim phone each deaf pronounce

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
