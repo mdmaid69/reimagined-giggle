a8ac31b7d8ef360559fa5704494df54efa5f66ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:10 +0000

    criticism above hit there school

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
