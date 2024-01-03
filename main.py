23de1f855e41f04c4573789fc90848743731a8a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:24:27 +0000

    him breakthrough tone general between

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
