f38ec7f9cb374dec0959bbfa519d73f69fa77acb
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:29:57 +0000

    impression less sensitive praise principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def get_bytes_from_array(array):
+        return array.tobytes()
