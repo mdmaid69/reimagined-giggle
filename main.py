7899016b6ca7198bc1430aedcdf336a1c3877e85
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:17:01 +0000

    share wrap furniture direct family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_typecode(array):
+        return array.typecode
