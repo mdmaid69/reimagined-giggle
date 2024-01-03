6bc9d99a2eefca52078dfd5e0b2d0ad1ab20643b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:32:10 +0000

    rough voice length behind hour

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-def divide_numbers(x, y):
-        return x / y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
