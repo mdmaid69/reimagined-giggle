a02392827de0ffaa687206b90697629ec61e6d08
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:22:21 +0000

    gradually somehow during year system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
