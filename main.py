eaaaba256fdbbbb63fa407705a82568dfea84db8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:18 +0000

    cut giveback please bend keepup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-for i in range(5):
-        print(i)
+def append_to_array(array, item):
+        array.append(item)
