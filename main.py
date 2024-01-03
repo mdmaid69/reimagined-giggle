da67c352cee610fae38a77a30f6b438368a230d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:12:40 +0000

    regret could sing once creature

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
