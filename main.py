7c42cf12e672ac4e1654116bd8697a5a82249740
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:48:58 +0000

    exercise turnaway centre onto smooth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
