bd5421e8fcbd8fb9cf9a98a2bd7a8914e2fb0b31
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:52:39 +0000

    tend related plenty either return

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_density(mass, volume):
+        return mass / volume
