b9e310dea7d74dc4ca977fe7f91ee9db6196dfa5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:48:31 +0000

    uniform sick normal dare possibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
