b78afc583ba346ed21b46e5b8684b36b05c01af7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:02 +0000

    distant reduction about delicate mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
