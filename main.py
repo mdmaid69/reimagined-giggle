bb5751add02546629eb450d6fadeea39069cb42c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:42:11 +0000

    pure reason injury familiar sharp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
