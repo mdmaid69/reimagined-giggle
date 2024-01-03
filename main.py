d16d03c33edc3772d6ff8ea53117cc6333f7e77d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:02:05 +0000

    better song flood under fast

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
