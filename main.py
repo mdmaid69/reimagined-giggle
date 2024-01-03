4fac63ef0d85c1f945a0d69b34a2fd9b72066dcd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:06:55 +0000

    573entertainment count popular with field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
