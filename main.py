98cdab25fef642175845a4d89ad5594429cfa6a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:41:42 +0000

    dinner knowledge describe splash little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
