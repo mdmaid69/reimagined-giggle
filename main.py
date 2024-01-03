f7d18e7bf3b96ad15a4b9b0fdc41089e8f873c61
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:31:58 +0000

    joke inside point risk acceptable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print(sum(range(10)))
