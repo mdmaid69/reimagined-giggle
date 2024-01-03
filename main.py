c99e61c26df034d290a039ab452f410ff6105c83
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:44:56 +0000

    dinner free chin fast truth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
