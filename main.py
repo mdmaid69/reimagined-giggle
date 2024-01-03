f63339bba11fa5ff1e59e45219ef127097ae3914
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:16:48 +0000

    declaration culture runout than complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
