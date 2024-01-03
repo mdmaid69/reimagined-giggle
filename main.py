fa99cba978293e3d2f5e2fa483379f35dae03492
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:46:17 +0000

    yet immediately frightened special correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
