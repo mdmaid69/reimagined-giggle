6888bfa2366395a26f60216f438cd954476c362e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:49 +0000

    bringup money road dry squeeze

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
