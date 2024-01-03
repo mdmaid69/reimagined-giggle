778a3702c4e2e918b4cee793ba867da010d140f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:16 +0000

    maintain getup come getback place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
