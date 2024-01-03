3dd494cc93de7897346b6bbabe7e73e3a380b16e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:43:20 +0000

    distant door herself happy comment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
