1e5dfbdeff56590de681a635221a3584505eb862
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:57:07 +0000

    uniform count remark Ii correction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
