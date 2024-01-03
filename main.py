b78e1d2ea458bc4fce892e72f37d424b127ff30e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:30:04 +0000

    breakdown people neck brave fun

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
