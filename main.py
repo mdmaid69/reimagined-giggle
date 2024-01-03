7d46ca8752ba07ed47bdf5db11f6e5a4206fb83b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:56:52 +0000

    behave hot attract ready increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def count_elements(lst):
-        return len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
