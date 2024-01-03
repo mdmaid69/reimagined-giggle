26262220136258e4369dcb0f61b3e67efe9672d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:22:02 +0000

    relation excuse start report march

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
