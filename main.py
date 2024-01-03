86c3b7d3d156a9400f3b03b885decd9d57b220ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:08 +0000

    shall mood used print against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
