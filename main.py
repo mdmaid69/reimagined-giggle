8377cd2c182c1e9cfa6b2750600b2a14f41bc78c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:22:38 +0000

    rain remember smell extra square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def square_number(x):
+        return x**2
