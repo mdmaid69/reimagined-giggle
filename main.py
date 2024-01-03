9d4b7273c2dae5fba5d6416c7c65a4e5d7acd19e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:34:07 +0000

    cruelty flood to instrument cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def convert_to_octal(n):
+        return oct(n)
