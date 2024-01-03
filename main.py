b5d55459f24988de101e2fdf38843e910ce175c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:01 +0000

    add couple call match pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
