320094c727e906908caffa69bb241dfe1f111409
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:53 +0000

    ocean kind strange tired design

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
