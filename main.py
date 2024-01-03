2962ab506ffc84a22317023ac3d5d9b10d0616ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:51 +0000

    attempt handle terrible guard break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
