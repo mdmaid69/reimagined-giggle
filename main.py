59fa4ecf50e9ddf6d15583ec39fed9379093df0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:39:33 +0000

    floor here narrow sort sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
