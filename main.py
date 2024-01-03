e2d1ebddf84727303a92e9c3c9f06f454a4085a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:44:53 +0000

    furniture rule spite hospital tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
