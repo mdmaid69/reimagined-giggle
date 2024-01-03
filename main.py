9d3262cd195d03208111bee66a4a562a01206326
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:07 +0000

    noise few fortunately attract lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
