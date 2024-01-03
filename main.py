94ad484feb7a504dc11a70b0bf2989706b339379
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:36:03 +0000

    time comedown getback foreign freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
