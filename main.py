845bc676db32f8e11357e800d6bacd22905cb238
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:02:11 +0000

    turnback surprising hair please expression

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
