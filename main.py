00bad1b8ccc5309398dcfff6b3fa851362cd54dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:37 +0000

    board your manage smell behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
