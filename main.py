ded18878886f0a5b75e5b68769573d0cf96e51c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:07 +0000

    safety attempt comedown closed always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-print(os.getcwd())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
