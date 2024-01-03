deddd9aef6d28dbc27b75b4c5abfd830dde15fcd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:55:49 +0000

    unhappy fear classroom remember thorough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
