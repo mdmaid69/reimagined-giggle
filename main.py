1b101c0c736ee206d33885d13a1c71ba2317bbf6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:54:20 +0000

    health polite feeling shoulder matter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
