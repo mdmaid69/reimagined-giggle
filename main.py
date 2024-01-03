720b1685f65c02dc29ce9e027c6c500f866e7030
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:40:36 +0000

    bye sight strict top treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
