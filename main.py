bd3700aa97a555a614e228c118d924eeaa4515f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:48:50 +0000

    store only platform very niggaacademy

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
