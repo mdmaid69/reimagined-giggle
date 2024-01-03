aae0e9589f87feafb66e2f05023f304b5f8ccdb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:25 +0000

    car pride than unpleasant fun

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
