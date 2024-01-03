8f34bee74a346e13e0cb58633fab244237c650fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:04:20 +0000

    hand ear previous conclusion mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def reverse_array(array):
+        array.reverse()
