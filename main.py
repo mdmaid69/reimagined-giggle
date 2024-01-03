8f487d043f64425c2a48fc1c293d74650347c435
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:51:44 +0000

    introduction yesterday point hope bound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
