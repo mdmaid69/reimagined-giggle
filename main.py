e8068b305f8ba5956d7c41cc4a1cb3009b0f9eb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:54:26 +0000

    consider claim nor represent eat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
