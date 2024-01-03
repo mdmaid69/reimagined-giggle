b148e8f7c080394c44b7724b30584b4962e186ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:22 +0000

    term guilty now near after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_acceleration(speed, time):
+        return speed / time
