4098a10e697c506facd97936fc72cd1e641cfa4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:10 +0000

    holiday reduce whether point hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
