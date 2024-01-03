30c1a84eb36241f1c99f3c3f924611d2ee3ebb01
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:41:15 +0000

    reserve state show of tidy

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
