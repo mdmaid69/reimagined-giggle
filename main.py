b7a9a8d71c91cc4c39efbe46682d7283c31b2116
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:52:07 +0000

    thick lost dress cost pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
