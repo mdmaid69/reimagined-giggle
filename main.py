aa81669ec6e392355011e8003abf377c858d2e36
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:22:08 +0000

    breathe leg deep minority empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
