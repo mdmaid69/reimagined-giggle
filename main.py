8710409655c0b182df4a11b9e1312edbd64bb5ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:25:20 +0000

    rush where dangerous bringdown fall

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
