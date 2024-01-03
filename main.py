e3946c47d49b50de543214ac838f8df2505a9a72
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:28:01 +0000

    possibly number develop thankyou question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
