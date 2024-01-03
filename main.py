853aa4b0607e8147efbe632472d7ebfcabb08fc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:40:13 +0000

    trust line colour seriously please

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
