bceadaeace799dc69a77ed93d296cb37424194a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:46:50 +0000

    declare acceptable moral priority uponon

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
