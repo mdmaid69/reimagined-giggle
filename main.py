4d652a5f3d1ddf4b7683bce68cab50b028f1f19c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:03:37 +0000

    cheek pair workout century note

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_list(array):
-        return list(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import random
+print(random.randint(0, 100))
