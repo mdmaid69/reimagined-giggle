ed0936d78eaf241c3b9d3e513f24df7a11b5fad4
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:45:48 +0000

    replace conversation train intend distance

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
