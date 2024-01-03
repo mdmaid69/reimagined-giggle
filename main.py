2eb0c1410e72bf9f8a02b0704783d24875cd5b78
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:42:46 +0000

    add compete love pool claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
