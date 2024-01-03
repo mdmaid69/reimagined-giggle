cfed5be81075f421b830bb99e938617ab9c9a5a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:27:04 +0000

    excuse active software separate quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
