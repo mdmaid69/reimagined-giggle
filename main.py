2f7d000712fd536fa2309b2b775fcde4d06289c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:42:28 +0000

    tax treat Uu wide male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
