e8c5cea117307f7fcb2261368fcb30da2d2bb79a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:20:30 +0000

    to match bus usual business

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import collections
-def create_user_string():
-        return collections.UserString()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
