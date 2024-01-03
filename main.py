485bffcd2cdacd06dea99e530dd1c315b2ca5ec1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:38 +0000

    through duty very street field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
