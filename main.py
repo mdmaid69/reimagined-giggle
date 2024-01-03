0c0249acb7607a0105f916aa08e8698a748da81f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:43:06 +0000

    here realize performance lookaround stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+def log_message(message):
+        logging.info(message)
