5ca16d00ad538e300f9a5930826e3d5dfaca9854
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:50:31 +0000

    battery union gate hospital concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
