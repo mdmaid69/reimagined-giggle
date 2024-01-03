6eb23f3c72b7632941ed5eb261f6ce349ec50fd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:49 +0000

    print home pretty trousers along

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
