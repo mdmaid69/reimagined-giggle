6f1e2feb326e76e43ca2c5cce17f6e8421e24050
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:35 +0000

    wrap style Cc step fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
