e87492ec56bf02e8516032b1b1000bbe1fa95def
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:54:18 +0000

    wind gradually contrast study use

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
