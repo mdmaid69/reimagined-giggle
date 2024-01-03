033b643c30c2b9124245838b5578592d6aa16934
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:26:32 +0000

    slight key shock cinema score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_current_directory():
+        return os.getcwd()
+import tensorflow as tf
+print(tf.__version__)
