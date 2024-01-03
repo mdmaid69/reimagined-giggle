7f8e23bd555f54df32bdcb94e4ef2214a802ac4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:07:28 +0000

    winter soak however from common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
