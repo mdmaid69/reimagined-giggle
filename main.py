4ade28c2537df22d9a716fb21e4da7c997f9d88b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:04:18 +0000

    central sore easy feel habit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
