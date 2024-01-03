a0b0166acc55534d5de27dee9e9a35e3968752ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:34:12 +0000

    trap belief plant absolutely nature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
