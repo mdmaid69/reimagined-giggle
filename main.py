c58bd38ef8d7322e83b47f72b217a950668dbb36
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:01:03 +0000

    church contrast sight give style

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(numbers):
+        return min(numbers)
