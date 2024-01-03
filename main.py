3f21a1e5f154d1f44cb7b51e610f773d5b4d57c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:21:33 +0000

    estimate learn measure precise difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
