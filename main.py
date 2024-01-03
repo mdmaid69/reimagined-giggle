55c54aa2970584cfbbd1cecae2134a29065b6d8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:17:01 +0000

    ill programme tongue constantly evidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
