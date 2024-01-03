5eff896be356bf75cf77dca3de9654d614ef2600
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:58:50 +0000

    accommodation far holiday hurry care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
