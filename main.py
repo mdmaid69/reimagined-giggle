f45734e22adb0a442b1951bb474b2648556c1176
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:27:24 +0000

    tall evening marry fall now

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_density(mass, volume):
+        return mass / volume
