42c018560eda3a76d623bfab3cb062daea830bdc
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:04 +0000

    recently fast shade address herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
