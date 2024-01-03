fe89804ff9270533c07802d87dc11a590a1a8259
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:08:54 +0000

    personal reach in undo sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
