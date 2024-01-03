4a8bd89c0cf62cd2bd64313aa4c4e20a3d63b4fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:52:00 +0000

    plain well precise train class

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
