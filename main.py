2a1f5d98124c53839aa6fd322a2a123ada78bf54
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:04 +0000

    Ww range autumn bad generous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
