b08e56f274383f02fab267ea62f38673b7a2b82a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:20 +0000

    downstairs advanced uponon attach print

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
