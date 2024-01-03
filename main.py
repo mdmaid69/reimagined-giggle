a859bc7898c5ff29191652bf81f9bfbbebf3950e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:50:51 +0000

    drink tie manner before front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
