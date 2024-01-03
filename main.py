5943a1883e2842913a0db0e865467dc32bea2362
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:32:50 +0000

    touch keepaway radio top match

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
