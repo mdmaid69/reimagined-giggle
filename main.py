ca4e703e9615cfcd01ab672ba69d513b53e8a962
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:08:57 +0000

    draw shrink physical turnover relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
