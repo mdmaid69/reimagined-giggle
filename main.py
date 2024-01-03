565d3dd3e30c0276558f76b8a33454f702b120a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:31:25 +0000

    correction importance pretty any now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
