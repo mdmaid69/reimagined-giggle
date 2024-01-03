4a0e8b82568b411c0c95d53be8f35c02e2b076b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:46:23 +0000

    with piece your ask uniform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def count_elements(lst):
+        return len(lst)
