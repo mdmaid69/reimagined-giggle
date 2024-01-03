bb6dbdbf1828c52d65c3057943ca6ca549cb9244
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:13:04 +0000

    ought at cheat usual position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
