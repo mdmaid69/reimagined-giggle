40469bcb561d7a7e8b821d47315aa240e8fa4bb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:13 +0000

    able busy development path squash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
