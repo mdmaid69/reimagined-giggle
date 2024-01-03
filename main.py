dfe4c081e49f4d5cabf5a92f819b5f813ca2acdb
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:22:24 +0000

    manner thirsty would Tt room

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-print(sum(range(10)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
