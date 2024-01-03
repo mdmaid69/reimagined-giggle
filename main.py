a14fd8e4d13757dc0654fe22dbdb0f1fdb0e867e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:47:03 +0000

    affect deliberate Rr class job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def pop_from_array(array, i=-1):
+        return array.pop(i)
