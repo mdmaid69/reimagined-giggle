8418703db7c402653e7691cc207d4ca17e0362a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:32 +0000

    culture takeover holdout foot spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
