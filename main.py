67f5467aba4708ea18e8be6072a41a2107f0da05
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:09 +0000

    mass waste stamp object change

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_str(array):
+        return str(array)
