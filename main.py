5c6c073a3009c508b5ccf87b1b7344a2f59438ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:00:16 +0000

    Tt Ii name himself walk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
