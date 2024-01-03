475e9f536489c16182b730b0cdab1d938d95b49c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:33:10 +0000

    wind statement pinch fond easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
