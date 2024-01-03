860643ad66de63b6c61057eb5fc54fb751252bfd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:39:41 +0000

    odd once recent long practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
