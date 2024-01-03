ceef0a45bd231d2f50037fe14ee68b19674db9ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:01:50 +0000

    listenfor that accidental tie want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
