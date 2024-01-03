dbce96ba33bb5e1cd7b3136bb3a9e28b964c665b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:38:01 +0000

    bye after lucky glad support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
