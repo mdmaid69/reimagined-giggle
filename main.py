dba90fd3e4af1c91b06e81d5a9845428465f760f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:29:05 +0000

    right trust during birth aim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
