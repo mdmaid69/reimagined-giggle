fd595ecf25e3fd020db7c3e961a56ba44e407673
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:39:09 +0000

    bad calmly hole Bb cross

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
