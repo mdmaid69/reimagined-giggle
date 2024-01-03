472a700d0719601e501b0ee78fab86d908498ce4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:09 +0000

    bridge translate keepout least persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_repr(array):
+        return repr(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
