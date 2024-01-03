a0597038477c5a1a437395abba8731eb5d53f679
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:39 +0000

    themselves everyone table song warn

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
