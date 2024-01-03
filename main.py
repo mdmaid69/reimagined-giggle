59437469be8118c5a79293744f15ed40c323a654
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:51:36 +0000

    ride properly turn boring 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
