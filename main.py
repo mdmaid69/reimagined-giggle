3bf61fa6516f1c5e0f3cb32a986b33a68c93ef68
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:28 +0000

    lead calm passage splash 572enter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def count_words(sentence):
+        return len(sentence.split())
