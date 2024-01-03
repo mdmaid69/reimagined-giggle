c9632002101221dd471b803a8685e60214720454
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:10:24 +0000

    both concentrate bottom reliable spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_speed(distance, time):
+        return distance / time
