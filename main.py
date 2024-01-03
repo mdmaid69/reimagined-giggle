f55eb05dc223bfc79cf4048bb70014bc8ad87d6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:08:14 +0000

    platform habit someone cut accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def get_current_time():
+        return time.time()
