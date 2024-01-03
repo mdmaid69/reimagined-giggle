7ea8a5dfbb535f0669a534c6174fe1b44271f00d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:53:14 +0000

    case innocent maybe tall beginning

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
