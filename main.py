52a1c1f1c6709eebf57c477b79f87b95a50f208e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:47 +0000

    centimetre suit this seriously long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_memoryview(array):
+        return memoryview(array)
