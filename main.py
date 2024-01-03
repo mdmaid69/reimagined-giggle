1867e4081516e564997b48f7b94382956e1f9bf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:39:37 +0000

    quarter glad unfair over week

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
