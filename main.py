3b312015f9397ca13b9ebecd2b8af719d046e8c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:26:19 +0000

    knock pretend total degree central

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
