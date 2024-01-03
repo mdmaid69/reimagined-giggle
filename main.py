e17419da4a30b2b6aad6f85642182b404957d7c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:06:00 +0000

    development mood cheque takeaway regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-print(os.getcwd())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
