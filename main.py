dd83459eab6398db9f1b7228c34505ca87af6fda
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:36 +0000

    argue ball Yy minority exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
