e25d825472a3ec66784254e7b101ae0a463ae915
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:53:19 +0000

    complaint raw goout shoulder please

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(lst):
-        return min(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
