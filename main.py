11e87922b53912669e4ae64e8807db88db4386e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:47:54 +0000

    card container sting likely second

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
