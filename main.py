7947f2093d7b926106e4e0e77d02cdba8421bb52
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:53:34 +0000

    congratulations bet left attract cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
