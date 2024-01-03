6c5484cbb7fe75bd977feb59c8e1115d18454c48
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:47:15 +0000

    ask accommodation mess cover used

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
