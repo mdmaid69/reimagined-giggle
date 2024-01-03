ca38d317e78a41e0cca66794a638b74e0e0f2d1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:19:56 +0000

    satisfied knock pot style stick

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
