204f9151e9e58faa34c48837dabeb4db2ae634e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:38:45 +0000

    still foot lead immediately grade

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+def exit_program():
+        sys.exit()
+def is_odd(n):
+        return n % 2 != 0
