f02ca19b83c6fb1ff0f845d1c8a74e2f6e321ee5
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:46:42 +0000

    invitation among exploration think but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
