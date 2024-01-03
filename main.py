3d5bf44fcd3e730d66859c08a5b60580963e8aa1
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:25:22 +0000

    train translate work this comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import numpy as np
+print(np.array([1, 2, 3]))
+import collections
+def create_counter():
+        return collections.Counter()
