8dd0eb810b77a0c2ef65da2f17472a8b0e1def03
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:05:30 +0000

    interested mention touch threaten during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
