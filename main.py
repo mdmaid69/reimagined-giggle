5e1483f588462a75dc2bda530e2e842fd80c3007
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:17:38 +0000

    around discover operation recommend step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
