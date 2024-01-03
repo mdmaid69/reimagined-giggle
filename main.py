d4d09aa378a5861ae2ab2a4f72b96810e1a4834d
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:09:21 +0000

    mention left appearance contain embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
