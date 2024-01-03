f25a4c762cbf8d63f714348a26f5207cd6dd5405
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:56:18 +0000

    before instead forgive slice expect

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
