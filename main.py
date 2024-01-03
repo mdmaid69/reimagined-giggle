6057f61467f5b7924a883841ace2bdf27f1795e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:50 +0000

    breathe fortunate fetch search keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_error_function(x):
+        return math.erf(x)
