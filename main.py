4aac87190cb806e36dc18fea9df52b4108c5b4d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:07:52 +0000

    place Gg before majority speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
