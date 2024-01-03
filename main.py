0481446a376f8aed9dce4b1d1667936a63b72da2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:55:24 +0000

    last eye bet suppose artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
