8da7e98ac67a0d8cc08995e2c8ec70b07a4df6ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:48:35 +0000

    lunch raise force benefit tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
