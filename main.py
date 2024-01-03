a88eb87ea32944fd80530ee469a3fba193df2f09
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:26:40 +0000

    only pinch answer even more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
