ed2d4f8992c88449e116b54bb1da7d0879330b98
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:57:34 +0000

    long now sink arrival ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
