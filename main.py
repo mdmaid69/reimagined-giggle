b232053153a20822d378dab1f81afb432ef9f615
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:14 +0000

    surprising interesting workout song foreign

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def add_numbers(a, b):
-        return a + b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
