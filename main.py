2941a0a9afd4c66bcf96ee5c56182ae3a7f17c22
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:40:10 +0000

    whenever help swing manner comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def reverse_array(array):
+        array.reverse()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
