e6e7892e2a13b4e9770955ca8723dc5668c25e5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:09:40 +0000

    show wear immediately holiday where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_list(array):
+        return list(array)
