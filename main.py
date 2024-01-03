10572eea816f7b83a93a581909301f3eecee5a08
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:57:17 +0000

    motion move answer brush complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-print(math.pi)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
