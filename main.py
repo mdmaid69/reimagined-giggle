37bb59eebfa28cc3386a7b295bd28680642e2418
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:26:47 +0000

    borrow fun wrong black comeoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
