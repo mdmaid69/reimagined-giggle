bf29f4333406d392ce0c01186dd8e78276852510
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:07:58 +0000

    software regular mine time trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
