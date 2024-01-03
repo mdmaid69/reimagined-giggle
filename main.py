82165f734603fc745ae29d472c05a5034aa3739a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:07:49 +0000

    stiff there whoever create main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def square_number(x):
+        return x**2
