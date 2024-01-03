760c372fe18b15b4b5652d4f172d66ff127905e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:59:36 +0000

    listenfor breakin muscle further though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
