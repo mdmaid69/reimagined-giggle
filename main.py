eb43457cf4e3e04bfe86063aecdc4819ef7a6896
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:03 +0000

    deliver run of street bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
