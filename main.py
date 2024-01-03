9ea082a5ea5d119b2ce4b9e562fa198106e59f0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:39:15 +0000

    Ff time point solve take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+print(os.getcwd())
