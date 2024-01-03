dc55a9b3fd41f78a4d1b36749c3bf2f2e82ed650
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:58:01 +0000

    generally chase smooth dig wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
