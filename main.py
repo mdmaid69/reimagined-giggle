7075026cbc7be3f94da6aaa2995fb7b401975fc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:06:56 +0000

    mystery bottom chance 569enjoy ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
