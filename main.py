e0d6d45f9e7b295ab466fb813ec87915187e413c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:03:56 +0000

    tone practice sign Ee believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
