106355aaaa18937d73bfba78d29b512e864c9eb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:37 +0000

    square immediate chat detail uponon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
