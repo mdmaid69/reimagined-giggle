05360100d11bfa1c687b0ffe3560d003bf0e5bde
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:41:08 +0000

    clearly experience female preparation part

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
