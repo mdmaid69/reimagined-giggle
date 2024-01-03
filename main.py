a41d4422a40cd5405fa5715d3d8f7b8f48074564
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:39:12 +0000

    room politics furniture downstairs town

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_min(lst):
-        return min(lst)
   import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
