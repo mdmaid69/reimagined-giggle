3959738047a93bdc0e17ad27177131a27010336e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:28 +0000

    rush turn admire value suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
