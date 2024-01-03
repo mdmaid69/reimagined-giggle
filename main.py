2059c404ce311904282d82942c2cb7a4b72c4ed6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:33 +0000

    arrangement attract woman comeover news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
