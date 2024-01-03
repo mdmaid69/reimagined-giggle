f31ee97ffab4ecf711fdb076b2429cb278c43fea
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:40:49 +0000

    division respect test command give

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import re
 print(re.match("h.*o", "hello world"))
-import time
-print(time.time())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
