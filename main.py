d9f8ff167ec335bcb4d854f5be033a92fd0f1171
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:51 +0000

    dry giveup quit complicated neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
