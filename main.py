817e6cd810e2ddee79d6a9c7603a5dbe26d4f716
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:43:28 +0000

    brief peace guard increase better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
