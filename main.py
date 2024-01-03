1f60b10789e77f58fa0f04a094db77a5663f3ff8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:51 +0000

    around fairly wheel thinkthrough throw

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
