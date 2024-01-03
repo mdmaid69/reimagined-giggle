3f5952b7420be03d7dbbba8740fdf229e9b3b82c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:46 +0000

    car look point cheek every

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_typecode(array):
+        return array.typecode
