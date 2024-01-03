c52436eb57ed8dab0c2881add1472a59d05ed04b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:50 +0000

    demonstrate wrong slightly centre round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
