09b2f581ec4d05abfe379d7a588e6889b350d706
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:17:29 +0000

    contrast string 571enough rub start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
 import math
 def calculate_remainder(x, y):
         return math.remainder(x, y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
