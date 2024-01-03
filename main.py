2136af8c64b5dd33b419394a1ab6b0542b915c09
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:09 +0000

    complete temper situation cost everything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
