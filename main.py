28ace57b94d62c06070cfd7cfc9bc354eb7efe79
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:57:33 +0000

    once shop read treatment share

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
