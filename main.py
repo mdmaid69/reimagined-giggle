c18cd1317900e1c6ff878f7a9baacb44de20e428
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:39:26 +0000

    swimming climb in turndown turnaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
