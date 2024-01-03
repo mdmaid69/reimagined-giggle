61ca5b6ff0c58003b847f4505c51509bcdb8c215
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:50:33 +0000

    advice support wise spill after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_work(force, distance):
+        return force * distance
