3091fbe1b8c27de4284bff0b284e0c4510b87498
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:36:01 +0000

    far heavy seem exercise business

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_floor(x):
+        return math.floor(x)
