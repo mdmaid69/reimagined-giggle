2d1f5553dcdd532cc7e949566455c3edd5ed4340
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:27:22 +0000

    trial formal plastic goout cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
