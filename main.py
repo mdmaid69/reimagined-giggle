c988f24dd72395d19e5320102f52c4e66238442e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:17:20 +0000

    only old inside stamp listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_item(array, i):
+        return array[i]
