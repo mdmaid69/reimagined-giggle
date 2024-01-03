9e96445e98f4d4b423dea67d177b7e577c473eda
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:14:43 +0000

    crazy exit taste callat mysterious

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+for i in range(10): print(i)
