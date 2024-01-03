d989172f9ac94389724a1d87eb6b35764b48bac8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:34 +0000

    certain turnoff turndown storm send

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
