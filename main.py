e1f78c8b1c48be9656cb77adcec39cc50b570b22
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:04:24 +0000

    comein getback Yy your meaning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
