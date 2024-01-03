9c76990f7540db67d4b62343ba72448f9f23b3f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:36:46 +0000

    perhaps rich throw open or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def get_array_item_count(array, item):
+        return array.count(item)
