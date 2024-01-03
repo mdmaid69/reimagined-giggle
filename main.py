4a8aa4265aaf2168c8cbc90792bba29259a1e94e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:48:13 +0000

    feature juice personal at sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
