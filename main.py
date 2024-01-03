cfcf8c5f0816a55f796ebdb71abd36089ade9668
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:22:52 +0000

    splash later spin top prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-print(math.pi)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_cosine(x):
+        return math.cos(x)
