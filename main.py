ba45fdcd93b37fc9ecba78a109acbf2fb40bb11b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:13 +0000

    respect shine level intelligent muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
