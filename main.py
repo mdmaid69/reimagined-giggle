9ce5b9a1af15937a9b5bebac3b9f555697d50e40
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:53:15 +0000

    basic winter start calloff abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
