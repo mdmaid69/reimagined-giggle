bb6f866a11018bfa4b7870135fd8984fd5ef51d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:18 +0000

    Ll style example tape unfair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print([x**2 for x in range(10)])
