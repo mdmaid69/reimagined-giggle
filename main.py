d17ace755ec0107e04ec311db6a724ac2d5d2a25
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:43 +0000

    moon anything excuse free dull

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_to_octal(n):
+        return oct(n)
+def convert_to_binary(n):
+        return bin(n)
