7bdadbc42e0e7de01665b6cdebfcc74fd3a985b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:32 +0000

    important tool dress artificial sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
