ae95bb60452a4c01c91685ac6ff405fb33197537
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:19:02 +0000

    profit 566end 567enemy information failure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def multiply_numbers(x, y):
+        return x * y
