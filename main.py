83bd71c3d2dde0e214d664c8818463c88bbc54be
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:34 +0000

    remain afterwards behind other however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
