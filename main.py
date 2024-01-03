8e292ae16102fb28ec2b20c91ed1bb329fd81321
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:29:55 +0000

    building wealth bit advanced set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
