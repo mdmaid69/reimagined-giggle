0142a01b917754926d151b5da1c024e1ce8da1fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:17:21 +0000

    for fun workout owe shame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def divide_numbers(x, y):
-        return x / y
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
