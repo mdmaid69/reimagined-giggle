35ced8c75d33e43937693bb1188c9d0d7af264e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:38:10 +0000

    better exactly married year laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
