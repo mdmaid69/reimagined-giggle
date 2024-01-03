90517ad29d5bf61de112c33fe15391bd1da18e2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:24:00 +0000

    dead drawing plastic reply smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_words(sentence):
-        return len(sentence.split())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
