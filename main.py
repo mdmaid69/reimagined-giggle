42aefe199e48e2adb7f199fcec7dc6b98980fd20
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:01:36 +0000

    comfortable stop rare OKorokay congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def clear_array(array):
+        array *= 0
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
