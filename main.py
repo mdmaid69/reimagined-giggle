51032a2c57a487d583bc8e91deaadb803110ff2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:39:32 +0000

    string interfere bringback giveout they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
