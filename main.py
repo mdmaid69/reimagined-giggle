84d9c306cfc184b9ce099c951785e99321032c83
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:11:02 +0000

    Cc push task minute goon

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
