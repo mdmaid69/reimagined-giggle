e51309022ed0591a6837b08056633284775a722a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:21:05 +0000

    block limit them properly shopping

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
