4cd8f9f5fba6985172d92086f087df2ea3431f93
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:25:46 +0000

    fall interruption scene look but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+name = "Python"
+print("Hello,", name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
