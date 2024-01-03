fee6cba17cbe2c005efe75ad775a733e78626241
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:07:49 +0000

    better either buy happen slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def remove_duplicates(lst):
+        return list(set(lst))
+import platform
+def get_python_version():
+        return platform.python_version()
