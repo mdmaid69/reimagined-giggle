3564c23cbda6f9a8e13def893073198ab01dff46
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:17:27 +0000

    major cum above raise ring

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
