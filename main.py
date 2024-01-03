f5ec6b2dd4009973296d36ed212aa0e563c2aa8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:13 +0000

    spin turndown here little tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
