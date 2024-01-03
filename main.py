e58d0cd6a424139a41e9994382fc5d5389dff95d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:35 +0000

    loss declare may charge stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
