f5965a437929286bcb900a4e9cf902fa95139c3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:07 +0000

    speed gate label mad either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
