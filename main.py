88005e91a45719e6178950086a66c56742f4f4e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:46:22 +0000

    win whoever tone live fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_work(force, distance):
+        return force * distance
