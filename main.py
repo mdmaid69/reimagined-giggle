ece1e9e136c0f5d85d62262cdb24de21e5816d18
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:33:52 +0000

    accordingto contact someone thumb down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_length(array):
+        return len(array)
+import tensorflow as tf
+print(tf.__version__)
