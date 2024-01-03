bfbd90f6ebe762adb7dae60ed31c8be5bada3c5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:47:50 +0000

    disgusting turnout feeling Dd mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
