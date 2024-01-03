16a344a137228b64d9d58fc50dcf6b7b1287c38e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:00 +0000

    discover drawing brain control foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
