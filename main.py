efb93bd5c5d8b9e85dadfaf2236627e8815035b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:06:08 +0000

    way look write sting dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+def convert_to_hex(n):
+        return hex(n)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
