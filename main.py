83bd4bd591fdf17fa3c9775d1ea1c2de542db3d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:03:13 +0000

    cause noone dress thorough closely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
