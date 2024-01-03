eff8182ef4c6116cac01398c2fe5346903c5ed08
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:57 +0000

    image path passenger plant quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
