990c1a4ded2419b174c38641211c144bbff7d63a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:56:27 +0000

    benefit total sure prevent base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
