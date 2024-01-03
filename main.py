2317118d369d7db108fcb494f7fc20c1d506bee4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:02 +0000

    use OKorokay recover furniture ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
