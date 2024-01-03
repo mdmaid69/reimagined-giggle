ae9759faa168ca55547efce004d1dd71ef2188c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:31 +0000

    them friend release keepoff sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
+def convert_to_binary(n):
+        return bin(n)
