f4fee323d623d6f33a21b2dd75ee667e9bfe30fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:32:04 +0000

    lonely somebody everywhere goaway its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
