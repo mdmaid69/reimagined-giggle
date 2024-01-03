0c9ea87d0c3729ac2b4f949892893be3fc6083d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:48:19 +0000

    lower back speak which herself

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_current_date():
+        return datetime.date.today()
