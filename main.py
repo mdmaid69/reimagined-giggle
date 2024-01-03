063d9a33c9974ffdea494b4e28a7e38940dcda03
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:52:44 +0000

    anything him spring community spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
