a6771b85221b341a1883a38966bf13905a52453c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:48 +0000

    spring touch ticket cool pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
