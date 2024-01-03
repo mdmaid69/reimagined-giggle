c5f1e905d5f68033b6f4670039f701b5d8a7a3c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:35 +0000

    shelter callat inform judge little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
