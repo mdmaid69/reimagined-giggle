eb2bfb55aeea5570800b75981ce1e87bc0a18677
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:15:24 +0000

    properly beside beginning original black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_work(force, distance):
+        return force * distance
