ccedb07c8191ae59b6bdc444c05a1c6138836dfc
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:39:12 +0000

    why its unlikely coat payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
