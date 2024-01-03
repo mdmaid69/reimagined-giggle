4b452c6720266b9fedf3b3ae22ae0ee2e2885973
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:17 +0000

    preparation neck professional information spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
