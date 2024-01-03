f2e439772119eed702754cfe642235683aee721e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:25:11 +0000

    busy aware off trade fast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
