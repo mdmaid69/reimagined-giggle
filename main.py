8acc92142caaa30c16ab82462bf88c7e09b641be
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:59:46 +0000

    might negative turnoff skill pattern

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
