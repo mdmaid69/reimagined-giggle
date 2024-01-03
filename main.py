ac26716947bc3042c2b16de062e208160eadc8b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:58 +0000

    opponent cause our give dead

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def count_characters(sentence):
-        return len(sentence)
+def calculate_speed(distance, time):
+        return distance / time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
