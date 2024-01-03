f9850092f348e892b81fb9d339c664682eeb3ee9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:07:30 +0000

    statement goout fill telephone acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import datetime
+def get_today_date():
+        return datetime.date.today()
