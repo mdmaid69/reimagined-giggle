b08055a46ba24eeb3ea369fb11060ebfe0bb82ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:35:37 +0000

    broad fashion spring suppose content

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
