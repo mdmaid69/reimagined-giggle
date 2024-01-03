eadedf9869e4110dad6f6534b279851fccd8871b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:21:33 +0000

    change even or suffer treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def convert_to_octal(n):
-        return oct(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
