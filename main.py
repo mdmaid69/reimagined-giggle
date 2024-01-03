79a354f3a845bd7e25b9c2ea071608fcfc1b71af
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:27 +0000

    pipe ball border cause aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
