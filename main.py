aa18d7d5bacb38eb8a92902a42ff1aff160cc2e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:11:43 +0000

    plain game gift care kind

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def reverse_list(lst):
+        return lst[::-1]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
