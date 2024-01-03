0e4703cd93f5723aa4a6c210e4b9b74c07de5155
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:30:09 +0000

    uncertain up juice persuade ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
