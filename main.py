2931d76ef8a5806b92dd7e864e6eaa5c3a2bceee
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:10:09 +0000

    modern software ilovedicks ride frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_interest(principal, rate, time):
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
