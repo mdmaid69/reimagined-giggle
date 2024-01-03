2f37ad7cd53816591120d3ea44437bdf191a7edb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:18:48 +0000

    attractive church together sail danger

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
