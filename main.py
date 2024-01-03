c22440de78c07df494fc2aecaa94df99fed34cab
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:18 +0000

    draw sting copy scream except

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import random
+print(random.randint(0, 100))
+def calculate_pressure(force, area):
+        return force / area
