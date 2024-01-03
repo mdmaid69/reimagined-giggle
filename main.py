85f3d7fb4f002778c5017b9aa28d3efcb1139228
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:45:20 +0000

    work meeting disturbing statement swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
