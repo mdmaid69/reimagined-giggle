fa538dfa70f9d97fb4f69ed98e03181652eb5da5
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:30:41 +0000

    company slowly joke limit stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
