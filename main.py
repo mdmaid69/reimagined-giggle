b8fc8f638b20025c488ed15b7ffa8e9671a2a376
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:27:47 +0000

    phone however naturally money vanish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
