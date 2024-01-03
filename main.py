4acf31f1e8ff8f89058670dde77debceb03c1daa
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:26:50 +0000

    passage relief surprised experiment truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
