cfb8e9b76152cbc764ddca4790b69dcf1d0c4743
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:58 +0000

    quality on protest thing vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import os
+def change_working_directory(path):
+        os.chdir(path)
