18976d1a3ac4a898521359ca9d770f4f77c42b7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:01:11 +0000

    another light weigh future reduction

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import queue
+from flask import Flask
+app = Flask(__name__)
 
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def find_min(lst):
-        return min(lst)
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
