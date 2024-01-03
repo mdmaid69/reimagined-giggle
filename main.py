45f7aebb4133992e017b57e9533431aab587cc14
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:57:59 +0000

    unhappy sting trick pipe send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def append_to_array(array, item):
+        array.append(item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
