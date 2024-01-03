3ef5da5eecb3f95c003e676bdfbe33c73bcb5327
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:07:58 +0000

    hate judge copy outside unusual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
