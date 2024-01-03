2a5ea7b3c054fe0408dd02b66f4f792fbf9207ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:28:49 +0000

    surface lucky though suit matter

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def add_numbers(x, y):
-        return x + y
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
