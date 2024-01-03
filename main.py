ff67c00f42f0296bd8b29b2df466618b0c4b9a0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:38 +0000

    relief bottom yet plastic command

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
