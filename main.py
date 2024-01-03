4a19215e30ad5ea61cbd317777eb100b92bf9c71
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:18:37 +0000

    talk sure touch post fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
