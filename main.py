a3323f0605b757f310bc70cb2ec53de6402a7c92
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:31:22 +0000

    ordinary much scrape this preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
