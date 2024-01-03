5232591cbf39e80856f8e0c28106478a8e746706
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:35 +0000

    ring shade easy right gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
