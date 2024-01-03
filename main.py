dd70a9824771bf4171924db713921b29d973432e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:19:52 +0000

    progress refuse common comment vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import re
-print(re.match("h.*o", "hello world"))
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
