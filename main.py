d201bbf64ff99de14ce0d49d5851b195ac1a9b9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:05:18 +0000

    rain forget all use exactly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
