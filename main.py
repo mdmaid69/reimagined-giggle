6fe7f6d7a9d900d3544712ee51db7848f20f5723
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:16:53 +0000

    crazy slide everyone miss up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
