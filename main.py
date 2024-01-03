2de38b590a99bc637273d5eb36289d4dc4ae2473
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:33:09 +0000

    song worth slight commit each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
