d8e5351d46431994956f52dc4c244e12e04b8275
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:50 +0000

    inside entrance circle win pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
