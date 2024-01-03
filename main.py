d8c10131c128e7ba93abab4d80c11bf4d0d79557
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:15:38 +0000

    judge performance brother important silent

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def square_number(x):
-        return x**2
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
