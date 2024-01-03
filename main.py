0bf405f69624161623bdd39bb73b9d307af9ec35
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:23:44 +0000

    line hour may successful beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
