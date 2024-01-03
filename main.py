9247e608afcc2555a99dc3c9b17c52cb17ac8d8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:13:41 +0000

    winter separate OKorokay nonsense although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
