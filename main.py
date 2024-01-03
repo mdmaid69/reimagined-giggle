cf052f4797a4a2951f38755970a1e06b22327e57
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:11:45 +0000

    load lead giveup left cope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_float(array):
+        return float(array[0])
