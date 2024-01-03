a531a78786ee5a3ed634f58004aac9329b0eceb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:19 +0000

    right anything way listen workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import sys
+def print_python_version():
+        return sys.version
