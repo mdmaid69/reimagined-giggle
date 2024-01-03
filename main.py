4fbe408142a20d99011edb68e98607b3f81329d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:42 +0000

    my confused praise mean only

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
