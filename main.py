ccbc45bd202b6d7d97867fc2c78390d2cde8ffe3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:15 +0000

    should go dinner deaf today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
