bccda47c8e401f4267aae9332ea9bcc0afec8ab9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:56:03 +0000

    move guard Pp ear first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_current_directory():
+        return os.getcwd()
