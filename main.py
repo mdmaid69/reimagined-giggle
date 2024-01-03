7656ccba3e4f378475da4485d786b334760706e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:34 +0000

    instead luck marry win treat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import time
-print(time.time())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
