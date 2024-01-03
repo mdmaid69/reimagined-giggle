b0820727ad9a7f04512a2ee34ac25e5c3bd59b61
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:20 +0000

    state rush general might pupil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
