a1b04cae458a79f5306a41c4b506be907fa38eed
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:46 +0000

    Qq 179bite translate plant exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def print_python_version():
+        return sys.version
+  import sys
+  def get_python_version():
+        return sys.version
