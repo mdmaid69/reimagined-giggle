68e27e05cec9ba494bdaec7af8ee87e29345c680
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:10:39 +0000

    base failure scene scare survive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
