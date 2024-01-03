6e553d8855c4e9e107815deae6282d9ede8cb494
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:21 +0000

    record score value total much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def multiply_numbers(x, y):
+        return x * y
