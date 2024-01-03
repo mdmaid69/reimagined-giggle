c7d0e027a58d4c1c7cb097c19929804ea8b2f198
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:49:36 +0000

    sink eye happy pleased pinch

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
