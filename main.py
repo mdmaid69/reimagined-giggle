263c74425e147a2026fffc636adca7473b76e0c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:17 +0000

    aim spill treat tip lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
