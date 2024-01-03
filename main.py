78a9a62297442e6743e4f5ce6cf21c7d6bb15f97
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:03:52 +0000

    so injury game settle trust

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
