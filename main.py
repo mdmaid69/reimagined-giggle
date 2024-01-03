4cc228f02a8e09e8c0bf12565b473cc9a5eedb09
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:56 +0000

    demonstrate perform cut store punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+def reverse_string(s):
+        return s[::-1]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
