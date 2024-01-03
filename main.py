1df7d9d406c0a264cce68e1fee6b5c01df5aeb97
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:00 +0000

    passenger object rise ticket store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_work(force, distance):
+        return force * distance
