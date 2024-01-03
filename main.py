36a82e071b703728d72c93b006d7681cc77725d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:08:22 +0000

    top comeround reach wire hers

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_set(array):
+        return set(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
