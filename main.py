0a8e28266e4e8c23549beaa2402329e4e0cf0868
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:52:48 +0000

    takeoff clear it pure mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
