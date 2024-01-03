e53bd1d29c36b1975f5de2eca4b837a5e1720cf5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:38:42 +0000

    comefrom Tt yesterday use flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
