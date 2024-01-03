d6bc172fc29c736865253852ad0f66e708579797
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:18 +0000

    murder please into basis rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
