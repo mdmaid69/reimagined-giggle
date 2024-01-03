513cec3ef31a4b4d992aefd5b7553dc766fee5c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:55:09 +0000

    name like deaf fail seriously

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
