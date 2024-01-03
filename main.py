2959a990fd6b59361e98145cb7202e9fcd4df9eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:17 +0000

    authority entry recent crime wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
