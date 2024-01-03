98e32bd3e37a5a41ac73a39b8d73fc2fddddbb91
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:43:26 +0000

    direction people travel along scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
