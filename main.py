446516db5f1c269c8eda484592a67ca28dfbbee2
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:49:30 +0000

    sweep unkind he obtain system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def append_to_array(array, item):
-        array.append(item)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
