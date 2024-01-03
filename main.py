7ae1752792d1e09feb95f43ea2396b4bbfa8dee5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:06:32 +0000

    ourselves unlikely 570enjoyable course determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
