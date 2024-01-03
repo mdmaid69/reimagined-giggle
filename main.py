d248834c5e697622dd47387631db5d8b47a200cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:56 +0000

    woman every town offer goback(to)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import random
+def roll_die():
+        return random.randint(1, 6)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
