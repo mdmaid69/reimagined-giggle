cc27a1f5a0b9da29d0b7849ad36ba069cb1b9563
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:46:58 +0000

    step disturbing design excuse shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
