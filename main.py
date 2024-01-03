3ed1ee63eb8095f4a292801e409292e460192d04
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:41:26 +0000

    single sit uniform sign scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
