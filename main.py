ed647c4b17010de7500e5ab96a30bc10608e259a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:30:19 +0000

    task performance memory bath hair

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
