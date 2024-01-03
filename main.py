66577f2ed8292aba721af6ace6289f66eb4a7e6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:12 +0000

    bound move arrive float after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-print(sum(range(10)))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import collections
+def create_stack():
+        return collections.deque()
