d355c9272445631f537c12204261578357cee8f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:27 +0000

    uniform upwards steady curious completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
