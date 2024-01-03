24c1916eb432d51c3fcdb2d1334c1cf61e2fba00
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:35 +0000

    song what gather future season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
