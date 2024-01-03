c554b0659b6c7ba361b7f709e7aaf5648ceb0b4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:56 +0000

    fix turnup wait thanks remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_time_since_epoch():
+        return time.time()
