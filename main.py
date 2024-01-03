b22c1a3ef9579bb5ccf554372e3d6f2d8dc004c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:35:51 +0000

    sound spring now join exactly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import tensorflow as tf
+print(tf.__version__)
