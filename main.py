c2b37ebbe8466c66f1539116f484c85de3cc2cc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:26:53 +0000

    hold fair test Kk closely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def is_odd(n):
+        return n % 2 != 0
