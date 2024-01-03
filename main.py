3ad93cdcdee3b58111f7ee85cb5a30fd365a3631
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:38:44 +0000

    suckme whisper object confuse silent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
