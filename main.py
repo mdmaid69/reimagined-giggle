1f4365aaa2edb5440dc029d6c9eab565336c5347
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:37 +0000

    walk exit pronounce love question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
