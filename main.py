2b316a2eef9fb623260352e4bd50ac553807827a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:51 +0000

    comeoff somewhere meaning comment block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
