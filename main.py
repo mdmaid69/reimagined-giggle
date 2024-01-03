beb6051f20eb6dc369746c63152631ff238e1075
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:03:56 +0000

    exploration choice dream tie crash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+print(datetime.datetime.now())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
