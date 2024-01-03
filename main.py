125e19cf563ebcd3011dd7c80b7b432f4377c03a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:43:08 +0000

    announce successful why fit goon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print(sum(range(10)))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import collections
+def create_user_dict():
+        return collections.UserDict()
