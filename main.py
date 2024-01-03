2be236cec2d7f0fb206415b3bcc2466117d3b9b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:26:38 +0000

    machine equal urgent steady negative

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
