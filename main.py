9fd5fb9661b846372a021cbc8b772973712be25d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:31:32 +0000

    book concerning violence nervous will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
