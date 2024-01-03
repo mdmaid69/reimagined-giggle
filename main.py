01992222463189ce3960b71af71a8e7274948ca2
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:19:58 +0000

    approval team face emphasis light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def square_number(x):
+        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
