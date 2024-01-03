0085c891937584aa9fb7709fd489a1c69b2613b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:25:19 +0000

    bright increase person foreign deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
