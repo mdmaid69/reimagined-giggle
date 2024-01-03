be03c0d55ef0aa60562a33b7cee153abfddf53b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:01 +0000

    check difficulty course hand charge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+x = 10
+y = 20
+print("Sum:", x + y)
