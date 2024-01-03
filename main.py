3d04f8cf7b816ea2c2e97f88dc9371f5d052f88d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:33 +0000

    illness cover either possibility repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
