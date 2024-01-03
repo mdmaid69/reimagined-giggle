8d95053ea35c8abdf175439a6369cc288b91f2ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:23 +0000

    house steady child film downstairs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
