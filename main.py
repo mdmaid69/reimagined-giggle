36ad539ec377871a09f01204ce731a075fae0bff
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:09:05 +0000

    scream black shelter congratulations track

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
