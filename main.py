e9b686ceaf95508d5e9013036c3e692746f4da0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:28:28 +0000

    pick obtain congratulate game approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_bytes_from_array(array):
+        return array.tobytes()
