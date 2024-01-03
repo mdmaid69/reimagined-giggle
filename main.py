6941b651cdc730187f8dc31ee25594bf1145ae12
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:16 +0000

    be dear otherwise please Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
