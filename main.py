595829e6eedaf3fac1690bd08196ad5f5a73f4bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:45:42 +0000

    explanation roll beside Yy cap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def remove_duplicates(lst):
+        return list(set(lst))
