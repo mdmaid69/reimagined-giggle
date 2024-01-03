70828fb7830acabb4d1bdb93bddf9d89213394ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:32:51 +0000

    fast why left attempt please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import getpass
+def get_username():
+        return getpass.getuser()
