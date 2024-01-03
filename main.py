f4cf9e61e2c9914c188e8bcb1ac223c65b39c386
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:49:50 +0000

    shade Ss unlikely effect through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
