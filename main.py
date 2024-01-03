e81e2c25371e17c1e399bc3fb8b9eeffc9819100
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:59 +0000

    ill much medium crowd strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import time
+print(time.time())
