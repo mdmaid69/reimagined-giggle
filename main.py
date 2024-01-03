4f5d8f07072fb23dae3287dee93e89c17cebfbed
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:31 +0000

    happen hungry afterwards comment pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def reverse_list(lst):
+        return lst[::-1]
