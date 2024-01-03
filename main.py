1adadcc37d16d4d782514259db4b726c3ac52abd
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:19:25 +0000

    answer fond come funny hole

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.ctime()
+import re
+print(re.match("h.*o", "hello world"))
