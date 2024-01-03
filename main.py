0bd284fe04082c08e531ebf7ed1f6918e9e45d71
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:27 +0000

    burst school study track weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
