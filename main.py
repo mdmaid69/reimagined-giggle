85d180ce52e64f9ba99ad9a518dfeef3e3989582
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:54:17 +0000

    lesson transparent borrow test second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
