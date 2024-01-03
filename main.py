4448083258ca563d3a174e80d7adb6f38d644c79
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:31 +0000

    desperate introduce frame certain thirsty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
