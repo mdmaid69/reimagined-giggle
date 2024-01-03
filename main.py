4877b59d3c4033790f678d25264f1901cf719a48
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:20 +0000

    apart recognize from simple interest

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def append_to_array(array, item):
-        array.append(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
