07b6dfa75f62c458050af70f71831718688be0c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:10 +0000

    ground calloff sleep imagination he

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_binary(n):
+  def convert_to_binary(n):
         return bin(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
