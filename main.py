91f69e71053ba754389c5317e92eede7795e1ed6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:19:20 +0000

    calmly finger before machine demonstrate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def set_array_item(array, i, item):
+        array[i] = item
