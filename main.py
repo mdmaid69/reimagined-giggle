2a0627445b2cdeed3655aacacafd7c2a98048701
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:14 +0000

    obvious neither improvement light total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
