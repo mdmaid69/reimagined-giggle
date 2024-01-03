287b4ed8d45cf9edecefd9304c348f204b22290d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:11:38 +0000

    port steal light aim skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_current_working_directory():
+        return os.getcwd()
