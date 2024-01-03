5ed63eccc79880be038210fb109161a62cec48de
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:22:38 +0000

    season reserve there telephone suddenly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
