e4540fd2ac5ef0b0e5e0fa546b439b8559de6455
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:01:33 +0000

    punish left admit develop pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
