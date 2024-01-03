1b12bfbec235a5069060c84c70a9753881066cf2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:05 +0000

    opposite light callback object modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
