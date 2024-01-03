dba2e34c66cfdffb09ce7962e105434f4c24367e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:02:53 +0000

    pile Vv confusing more deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
