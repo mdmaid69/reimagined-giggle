db6fce40cf62e123642202bc4d4ec18ea9c714bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:55 +0000

    cause pleasure quiet best along

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
