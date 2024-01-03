26913f99a709df7e6e2121cc6b54dc30c32d8cf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:38:32 +0000

    peaceful nurse name promise temper

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
