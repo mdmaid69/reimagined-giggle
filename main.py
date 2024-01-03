125f15503a3a6bdd66582bc4b64abb6de2e6f64a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:15:58 +0000

    Jj fear treat very respect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
