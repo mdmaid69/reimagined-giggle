aaa3c10a0517374e59ff4db60b6dd4b81948e968
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:20:50 +0000

    space true so distant load

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
