49263ac9764bad8ea64a197639dc7f5f1f7ce033
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:18:42 +0000

    thin comment number national recommend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
