093da36c2a857d4de4a895d20b246e440a7c70f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:13:30 +0000

    I excited content afternoon thankyou

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
