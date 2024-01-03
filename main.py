624508f866a2b234cca844556efbf9b55feccc5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:58:10 +0000

    mouth opponent once fun length

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_bool(array):
+        return bool(array)
