dfc2e075a46224ed6f7f338fc5c033e8b777d058
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:48:01 +0000

    crime there 565end reduce behind

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
