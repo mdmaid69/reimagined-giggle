4168cf351f978ef8aa50bf163a6a8d5ea48681bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:24:48 +0000

    stick lose keen protest tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
