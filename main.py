0a077afb5dc5998f03cfdec2c208aeaeb2a4e8d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:37 +0000

    principle environment study concentrate combine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
