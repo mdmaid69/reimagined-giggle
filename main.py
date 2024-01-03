10d3b940d1d39123ef20ef7c72290a53f804a1b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:11 +0000

    steer best base meet loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_directory():
+        return os.getcwd()
