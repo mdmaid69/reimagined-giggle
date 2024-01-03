e0fd189890e1d4f2cfc9f14fa7ecb915c24346b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:44 +0000

    powerful tone attitude thin home

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
