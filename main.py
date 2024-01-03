237c2f4d8f222fc948d204092b99bef59d72769d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:47:09 +0000

    pupil shout feature holiday minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
