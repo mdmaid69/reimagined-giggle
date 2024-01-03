33b1d0046d174c6b9ce4160a983662f3347d1669
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:36:37 +0000

    open business considering preparation evidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
