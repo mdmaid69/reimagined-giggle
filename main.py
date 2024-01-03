ec093e84ac94858e39d532c5ea4257cae778b0e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:48 +0000

    trap apologizeapologise suddenly Ss bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
