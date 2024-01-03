08aefe7ffa64b36a9aed7306d92fc0bbaadbccd8
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:59:56 +0000

    slice cook fear breakinto software

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import sys
+print(sys.version)
