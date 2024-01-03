48f9b0a18d600bb243ed171a8b22c13711addd84
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:36:31 +0000

    before wind day capital bag

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def square_number(x):
-        return x**2
-for i in range(10): print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
