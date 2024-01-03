82f2f404dc937bf040f0f7421a1749ce76a7f53f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:53:24 +0000

    uncomfortable simple turn shut break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
