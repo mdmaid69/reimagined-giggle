b2e3d58e2b76b36c03bfc9aae0d7a2a50b6ed961
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:18:31 +0000

    smell Mm regional season talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
