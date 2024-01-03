92c06e90ebcb0957ae8744f6d619031e9d26a44a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:45 +0000

    television experience her thinkthrough stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def sort_numbers(numbers):
+        return sorted(numbers)
