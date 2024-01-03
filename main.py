4e109cb76432b87ec0fdb0f7eb42aa5ca7032a26
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:38:46 +0000

    hurry build tongue temper size

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
