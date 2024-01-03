14269e3db309dd4ce4b21cbdab9280aef6de1628
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:27 +0000

    pool escape move rise right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
