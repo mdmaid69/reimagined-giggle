238cc8497389519b6878351418837f627b2d452e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:54:11 +0000

    curious discussion along getaway boring

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_speed(distance, time):
-        return distance / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
