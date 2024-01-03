1bc13600d03c03adcf4afed131a0d9ff640de35c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:33 +0000

    pride petrol fairly boil check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
