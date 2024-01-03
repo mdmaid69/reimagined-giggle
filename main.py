7125bf35704d95dca80e24aaddcb275c69f17ba9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:04:34 +0000

    getaway deserve question great patient

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
