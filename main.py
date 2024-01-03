d0de7aed9f241ebd9816f60d0522bc6d12d43f6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:56:56 +0000

    general brush family Ll large

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
