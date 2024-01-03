54700ad8e9301398039d34ab6ec32d56936738cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:03:41 +0000

    less rid calm suggest cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
