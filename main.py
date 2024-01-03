1054e05ac8d147edeee9632a928f131dea40a43f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:12 +0000

    drop keepout scrap bath prize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
