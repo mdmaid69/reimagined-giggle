d942f2c5c546d5130887c10e8087979c3c8372ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:57:57 +0000

    nervous part male confused left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
