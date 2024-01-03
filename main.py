568747d9f5614f37b5f75d09172c3e85e326519e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:43:25 +0000

    total practise month least back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
