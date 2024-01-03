3a1f0d882c219ebef705a84aa7d593f6e9cf6c62
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:03:24 +0000

    shade complaint Uu onto guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
