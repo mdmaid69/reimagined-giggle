aee2b1d3c438dfae9a1ab3b29329e839481152a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:46:12 +0000

    bus straight attention advice shop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
