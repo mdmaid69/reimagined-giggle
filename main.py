51e6c1c9a821099ee87afd55b82b7f02d374cf74
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:19:21 +0000

    report correct score normal petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
