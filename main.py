81ee544f3b5ad3453f66bc5fc9a86006c7793723
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:37:16 +0000

    mind bus we neat firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def sort_list(lst):
+        return sorted(lst)
