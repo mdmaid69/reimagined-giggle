be03cf9b244f566283a94963b74aab2a8b732817
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:05:19 +0000

    exactly left possibility key advantage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def reverse_string(s):
+        return s[::-1]
+def calculate_density(mass, volume):
+        return mass / volume
