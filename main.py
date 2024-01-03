1790314fa9da3ba589c7c70e45f682e27ebe10ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:09:52 +0000

    than shadow goback(to) past flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
