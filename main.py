b2527b24c933abeb7aaf2b126c2f570564fce066
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:03:06 +0000

    save turn neither roof justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
