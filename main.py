b22e6c1a65d5f177303c166e6ab829758e62de74
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:23:28 +0000

    can Qq second distance floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import datetime
-print(datetime.datetime.now())
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
