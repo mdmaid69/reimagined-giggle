d4ca125b79bdb5c559ff02f3b8786f3971373f4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:17:01 +0000

    design room me by hospital

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
