c22fb1cfdf422c601a73e8d7a92f957fae156985
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:50:40 +0000

    Gg describe brother lead tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
