1288e1a7d9530a258f3b30cfb8ab7f8b4f74cacc
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:26 +0000

    lookback(on) woman vanish decide fasten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_counter():
+        return collections.Counter()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
