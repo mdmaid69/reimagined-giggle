2582d55dbf17c9e045d14c1f114fc4ec27725f7c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:22:47 +0000

    Ll bit mouth cross takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_itemsize(array):
+        return array.itemsize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
