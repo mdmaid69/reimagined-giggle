93ec1bdb37cb978a7f78c96499bd70a0996aaf26
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:18 +0000

    runinto issue under angry would

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
