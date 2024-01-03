6d4a7a9be2f8253d20005fae58286493d3f1a7f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:15:44 +0000

    fat repeat understand bye represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def divide_numbers(x, y):
+        return x / y
