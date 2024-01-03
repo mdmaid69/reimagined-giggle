1012dad91bd1eb8a2945360ebeaf133b3bd2bf08
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:03:04 +0000

    stop thanks activity advice lift

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
