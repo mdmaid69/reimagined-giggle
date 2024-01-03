0ec143c603004232c6bfac95f79b0758334d8c2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:06 +0000

    hurry ask obtain light same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
