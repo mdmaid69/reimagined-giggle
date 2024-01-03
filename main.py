4cca713d83807bb0c9ef6106f89e50ea1527059b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:06:34 +0000

    it raise interested pollution ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
