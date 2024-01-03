7808ece5a8aad707bb652c68f22922961555b7bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:48:12 +0000

    land much feeling tip television

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def square_number(x):
+        return x**2
