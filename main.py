ceb4ee30b8155a0f69143dc84caedce96ddb2537
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:36:45 +0000

    forever line section on principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
