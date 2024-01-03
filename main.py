06ec53ff7f1f2af4167abc200002ca38459e1690
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:02 +0000

    study ilovedicks hurt deliberately course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
