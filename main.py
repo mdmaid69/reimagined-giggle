5d2c0211e64a52351e99a2ad988a37c9eaf17c4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:57:05 +0000

    shut complete head address few

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def get_array_item(array, i):
-        return array[i]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def get_array_as_list(array):
+        return list(array)
