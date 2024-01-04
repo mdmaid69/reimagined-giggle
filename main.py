00f00de3875871f6fd7c6fe76674b5909fa91222
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:04 +0000

    bet life complete add sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
