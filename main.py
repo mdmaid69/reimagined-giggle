9f1bc5e18ce67c6155d365d67d3b66a780baadb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:32:15 +0000

    pretty view over aim future

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
