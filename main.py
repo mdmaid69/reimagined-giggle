eab77a2312b5b7b16ad32b3facc6760c6f56d6d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:30:26 +0000

    illness accommodation neat important nature

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
