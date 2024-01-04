9d6e444c656c81d866fdcd0c47361cd4388140e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:29:10 +0000

    add ball afford dead lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
