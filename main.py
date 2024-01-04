bdd55205f5a7c8d992e80a1747df2c736cc4fe0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:50:24 +0000

    soil ask soft tap independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
