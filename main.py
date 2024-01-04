e6c314268b557ad5016aeb16b84cf846e87a66af
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:27:04 +0000

    position position how side cost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def count_elements(lst):
+        return len(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
