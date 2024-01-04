edd8c9ea92e5ca092b3764e36a6b09bd2e1e9346
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:35:57 +0000

    waste cap joke breakfast base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
