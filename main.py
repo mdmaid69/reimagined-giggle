a3a8338b802746482469e3fef4e97b7acb5b91e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:55:45 +0000

    shake show safe little bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
