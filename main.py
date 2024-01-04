bc628f6240cd4f5179c626155dccda1a360570ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:48:33 +0000

    minor surprise breakup invitation wise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
