2cf4b0efdbcd416b8e1ee792b1d85f8be87b102c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:11:18 +0000

    unite fat cause pause remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def sort_list(lst):
+        return sorted(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
