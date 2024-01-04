fd4a8f32ae05a37ccb628b779e7808ac02f2a4f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:46:01 +0000

    introduce expensive defeat error week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
