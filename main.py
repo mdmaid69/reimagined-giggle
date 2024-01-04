72040949cf6ac868c3a208218c5c81205bd7f281
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:33:45 +0000

    once turninto man path frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
