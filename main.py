4c813c553954139addee73fee805b03afea4cd2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:33 +0000

    sensitive turnon introduce fasten punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
