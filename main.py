3a628abd6e5ac1772812bc53ea40d3c6d357ee14
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:13:03 +0000

    inside warn reply stand absolutely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
