b05dfa8360b580440e119810dbf5f4f959b46693
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:03 +0000

    achieve will survive section steady

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_power(work, time):
+        return work / time
