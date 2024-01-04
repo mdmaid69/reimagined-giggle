ee160383de4bd0530fb5d8ee27a232a2ead0fd5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:58:07 +0000

    error talk avoid female tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
