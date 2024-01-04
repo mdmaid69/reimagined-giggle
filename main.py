bdc163dce01ab97d5f9b688d6468f94e0cb44ae7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:44 +0000

    someone responsible like actually imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def sort_list(lst):
+        return sorted(lst)
