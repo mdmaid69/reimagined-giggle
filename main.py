909aec366884df40c8300255547e9ab39568dd43
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:57 +0000

    spread stop use produce mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def greet(name):
+        print(f"Hello, {name}!")
