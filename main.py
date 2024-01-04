7e2e53534d846eddbe206242a5a6e247b3f66e62
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:14:33 +0000

    throughout pain turnoff share offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
