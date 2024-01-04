ced14189579307d8896a22eeddc6f6a9a0a1f4d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:09 +0000

    clean bite cough fresh because

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
