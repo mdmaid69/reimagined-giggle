81f8f5461327d8bcb810bbb5f70ddb162b1953e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:07:47 +0000

    reply disappear related comeout want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def is_even(n):
-        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
