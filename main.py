a6d4571eb9d3223affdf4d547d478dcb98fb39e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:46:03 +0000

    community sea burst stick find

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
