ad51e463c87ea0e49efc7ac6ea2154c91429278a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:51:41 +0000

    tense Kk cost positive someone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
