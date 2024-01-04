6e8bee814fc3b6274292195fa170e609f027190a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:39:38 +0000

    famous square control performance prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
