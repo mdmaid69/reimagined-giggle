fce026cb2a0b75428e4b5885666b9277d7718eb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:09:23 +0000

    stupid worst quality surprising mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
