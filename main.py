0827da505819f5be8fbe71cb8b136a17178695f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:29:33 +0000

    steer hello strange needle education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
