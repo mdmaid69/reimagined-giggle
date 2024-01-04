120470806f3bebd9933bfac52bf3442daa1e7a04
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:03:22 +0000

    life risk sensitive situation stand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
