7c9cd2f409eaf090e20124b406e49034083a5cd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:23 +0000

    success custom experiment workupto pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
