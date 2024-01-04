59b0ff4e1c564d4f011dff73fe9a9fe5c4f5f973
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:07:47 +0000

    comment offence send fasten want

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
