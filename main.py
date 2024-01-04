6900b630dad9d0cb45019ddbd7b8a9440a3df3d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:17:57 +0000

    crash comein score successful involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
