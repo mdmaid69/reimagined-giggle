78ea770c89833853bc3a761bcc4e87596cbf8210
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:10 +0000

    post throughout direct equal unfair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
