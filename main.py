4beeb49873bc9ab20eac5965e37139e711f0c9b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:25:51 +0000

    generally action think sad excellent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def square_number(x):
+        return x**2
