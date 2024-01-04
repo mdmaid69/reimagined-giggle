976c52effe00cddf360c281c5a0d5c3221818a3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:53:42 +0000

    work trust tidy dream switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
