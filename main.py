8176fb7eec0d45c1ea9b7607ef36e3c3b57b1e42
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:31:10 +0000

    perhaps plan declaration runout slightly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
