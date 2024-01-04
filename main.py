6ef9115cdbc9998cc5c199ec795bf25e829e034f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:01:53 +0000

    each chance stamp dull play

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
