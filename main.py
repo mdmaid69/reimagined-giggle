76a7abf9d512c45d7040e306c41426f391201041
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:27:53 +0000

    whoever whistle accept live add

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+print("Hello, world!")
