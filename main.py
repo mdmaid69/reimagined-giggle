5dbec649b684339f9b5a4cb362210a8acfa82a57
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:25:01 +0000

    immediate fear edge nor art

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
