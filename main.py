3ec16b3874e3df3f0af00a3eb15447ede49f8b7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:22:34 +0000

    judge calloff thanks sink or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_logarithm_base_e(x):
+        return math.log(x)
