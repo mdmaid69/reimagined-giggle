2af10726542bf31c9c128879b12871405fd83a6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:05:12 +0000

    desperate common run tomorrow abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
