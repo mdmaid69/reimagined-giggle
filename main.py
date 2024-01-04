6d20ed232853d0929981a764f942093096318916
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:41:24 +0000

    quality me nose ocean embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print("Hello, world!")
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_itemsize(array):
+        return array.itemsize
