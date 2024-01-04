6d4bc808108a06140302a0583d36fd33c668beb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:36:38 +0000

    vision simple unlucky recognize Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import logging
+def log_message(message):
+        logging.info(message)
