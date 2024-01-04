8b57dc73a76b389cc48c941157774c7b23f2b987
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:09:11 +0000

    spare fill then odd weak

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
