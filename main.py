51a51bc81baf20b0b1eafddd7026bf9fcc7d0cf9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:27 +0000

    neither report contain lost various

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
