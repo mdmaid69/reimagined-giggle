01d6824c0b8c0901651a85f39662572f1fc7798c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:37:59 +0000

    waste slice new slice straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
