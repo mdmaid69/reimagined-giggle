12f0cd70431f672b02b25a2633ab530f7b4a6ea6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:27:30 +0000

    protection order save introduction hang

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+print(sys.version)
