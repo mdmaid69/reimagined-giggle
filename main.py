ff0320e40412f928dc0bd55236975ad8ed71635c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:19:25 +0000

    copy unkind unkind getback capital

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
