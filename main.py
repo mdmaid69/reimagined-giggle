7a6886cd76a8752920b0e90d97ed4b4f93b07a92
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:09:16 +0000

    failure own pupil temper angry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def subtract_numbers(x, y):
+        return x - y
