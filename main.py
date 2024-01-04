d15b9314aa73163624d740b9f790d5bc7dc3a6d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:23:01 +0000

    bright pleasure city drink ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
