74dca47ae4a5a88c4ed60bf28ef9cef7f3286c49
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:58:38 +0000

    complaint sign slice reply complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
