14147a89d635fa2d1764fe612d9ce266a82e553b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:51 +0000

    fairly matter match study conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
