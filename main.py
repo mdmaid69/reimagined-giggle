15da162fc4a4a4641402c22959281994239a28cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:19:20 +0000

    black say run rise stop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def convert_to_binary(n):
+        return bin(n)
