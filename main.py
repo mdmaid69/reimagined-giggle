f2514cbbedf8faeb249cecc4ce7ce6c77c4ab331
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:12:36 +0000

    note fast this scream give

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
