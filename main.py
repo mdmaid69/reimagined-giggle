7b877e3f1e3077b766e0c16686c62990bf360fdc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:27:49 +0000

    pair yours waste last minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
