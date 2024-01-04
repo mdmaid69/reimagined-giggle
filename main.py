66bc2e44cdd5cf98c8d42e42fbcf329408733931
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:20:06 +0000

    naturally certainly schedule fair figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def multiply_numbers(x, y):
+        return x * y
