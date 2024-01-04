a51eefa08fb5343194418c6596607edc4413f754
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:58:53 +0000

    right object roof tap marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def find_max(lst):
+        return max(lst)
