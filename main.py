30392163b767162cf59b6e3d028e5c17d8733c20
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:02:28 +0000

    job objection limit bag squash

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_current_directory():
+        return os.getcwd()
