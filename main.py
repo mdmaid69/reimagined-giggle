0e344c3a3845d6f404c07676a58efa27e14f20ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:23 +0000

    dry broad height goup only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
