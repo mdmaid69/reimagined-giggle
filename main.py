7447cafc219c919fac78b517ba37e5846029f76e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:05:56 +0000

    bound them bored shut provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_bool(array):
+        return bool(array)
