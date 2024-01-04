2b04c68c1ba12ebc6b43ec021bd20df2f5698863
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:50:17 +0000

    belief 571enough Ii hold vote

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def reverse_string(s):
-        return s[::-1]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
