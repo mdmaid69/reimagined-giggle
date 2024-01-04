c835ca593665d7bbe0010b0d8433ebced38ac125
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:44:17 +0000

    closed by sail giveout guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
