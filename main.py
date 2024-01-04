78dbaa14b07916515c17b4077e43ed5f263f8e76
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:01:58 +0000

    purpose loan translate confused buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def multiply_numbers(x, y):
+        return x * y
