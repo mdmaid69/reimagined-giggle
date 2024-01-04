5b218d5c13a93bd860f7b5ba82106ba307d32ac8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:40 +0000

    crowd attractive miss plan since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
