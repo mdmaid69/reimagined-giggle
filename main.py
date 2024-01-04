7444603a9c7e4e728ae135d5d58312c92a538393
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:19:21 +0000

    feature flight breakin invitation special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
