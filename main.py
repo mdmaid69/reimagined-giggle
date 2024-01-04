9dd9d80c59359cb68615cb3e1f63ed8f4c39aa95
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:51:28 +0000

    hour to already machine 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def convert_to_hex(n):
+        return hex(n)
