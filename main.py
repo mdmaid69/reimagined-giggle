737cfa74519f47e96fb5fcfdc7ddba5da9549d79
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:43:19 +0000

    steal chin obtain behind row

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
