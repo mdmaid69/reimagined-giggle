e43e4b84b84c234bd975af3ca951a0b38cf95c48
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:20:34 +0000

    field ticket general proof return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
