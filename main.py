1c84d693f2042fd4e482461e90fc02dae4fa6635
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:14:49 +0000

    wall kind central wall ourselves

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+for i in range(10): print(i)
