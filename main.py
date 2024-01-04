554b95662414567cb35fb02fd2372ed8f3256f8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:51:58 +0000

    reply tap money sometimes decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
