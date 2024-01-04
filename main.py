2b175631eee132b26201af7e9b195ddfc709b88e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:41:06 +0000

    its check raw connected attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
