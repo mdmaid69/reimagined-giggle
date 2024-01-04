33451718816819172d72bef9775b4865ea391b87
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:05:10 +0000

    simple accident gift tread file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_work(force, distance):
+        return force * distance
