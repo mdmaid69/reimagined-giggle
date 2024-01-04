fd72d96a306b04184692da9dc73128420af36855
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:36:49 +0000

    awake otherwise him better wherever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def reverse_list(lst):
+        return lst[::-1]
+def remove_duplicates(lst):
+        return list(set(lst))
