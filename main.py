51ebc0547310e5fe9c8e0d302adac5a84fb21020
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:32 +0000

    school start lie fence display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
