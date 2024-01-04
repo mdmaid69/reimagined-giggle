5fa4d74088a5f97c1a156fbcc3c529697c7a1139
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:44 +0000

    breakout once rank beside satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
