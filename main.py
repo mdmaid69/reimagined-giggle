106688327895a0090a010b88c193cce2454effeb
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:33:38 +0000

    turnback try force pay glass

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
