c768bd5c6ccd84282d1a1ae9a46b3dd4cbc24ac9
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:16:21 +0000

    address combine today regular praise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
