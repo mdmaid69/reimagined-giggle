9401ef5f68dc6e9e47522e4acd4a40ff209b8ada
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:04:21 +0000

    give sign clue cheap region

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
