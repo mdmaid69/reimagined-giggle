b10b350793e74f8716e326271d2e270ab416ca63
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:07:26 +0000

    lift sound naturally Tt passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
