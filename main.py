415da0f05f951beaefe1974536f3c99443845a40
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:15:18 +0000

    method want boring picture scrap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def cube_number(x):
+        return x**3
