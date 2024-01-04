fb5132d67d12a941a5019739c5f8c8892a8808c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:14:42 +0000

    what anxious after show sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
