cf9e003a87735203d3f76b9f361aaef4084c5d84
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:11 +0000

    hungry sleep moon murder rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
