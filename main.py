6e99303a8261577fa18e4ca33a96e7e259726e49
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:21:40 +0000

    further fear tax especially later

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
