fdbe038268e63cf42d03d10ab2169222c1dffd92
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:05:02 +0000

    capital long tired to crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
