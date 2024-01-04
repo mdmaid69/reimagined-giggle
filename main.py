b8c7e4a6703b9fe93495ceb2e1d9bac0f10fbdc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:49 +0000

    bang situation explanation we her

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
