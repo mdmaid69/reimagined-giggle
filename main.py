3af9f8ba186fb96ef1e153b1c4b9348c3efe60ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:35:06 +0000

    near unlikely voice makeinto bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
