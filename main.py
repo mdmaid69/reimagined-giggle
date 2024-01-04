6ad87d5ec5521926013d6528bb6ab27073bf4c0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:20:56 +0000

    become education war ought stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
