cb98f647feb55734197031c68a6e276d5a4890d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:29:46 +0000

    maximum except left or emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.ctime()
