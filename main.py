bf9ca06ee1a25d6e2a98d0b5375c1ced4bdf2b7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:10:41 +0000

    deliver sink include state beginning

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
