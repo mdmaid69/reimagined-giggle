44138977280c793c9dee65b2a6e549fb3acc0201
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:22:41 +0000

    design quick deliberately war through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
+def remove_duplicates(lst):
+        return list(set(lst))
