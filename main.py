6bbd92fbc58092acc9c01de8ef4207ccf35fdc86
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:28:44 +0000

    concentrate badly home sport yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
