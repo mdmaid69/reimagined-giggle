30c53951adaa5831fa5644e2a64e366815fea3cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:29:46 +0000

    happy commit opinion comfortable practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
