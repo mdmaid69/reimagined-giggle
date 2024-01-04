e16a3c40a51c46c2d9500bc09294977877092f88
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:47 +0000

    emphasize plenty play track whistle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
