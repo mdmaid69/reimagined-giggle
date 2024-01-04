d2889fb6c507602641a466f2f19e8dd197ccf28b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:59:14 +0000

    government weak off serve gift

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-name = "Python"
-print("Hello,", name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
