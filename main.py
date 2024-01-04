c65ab624c865fca05d84ffc474f3f93c8fbf9cf0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:47 +0000

    room concerning cap trick clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
