7fdf14eeae8f96668b8423682c034c41db111352
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:31 +0000

    getaway swing strike performance bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
