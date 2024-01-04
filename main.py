5179d69dfad42167a3de822480e966d1d0c53b7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:25 +0000

    rather with apply sudden another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
