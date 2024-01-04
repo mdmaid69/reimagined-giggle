626d0634cedd2709bd564c3b644a6c032336e92a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:44 +0000

    judge always giveaway raw dance

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
