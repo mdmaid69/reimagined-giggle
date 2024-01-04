8e301153d3e84dc5ca6c28825e354f77d1b20314
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:57:36 +0000

    why standard bringforward find her

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
