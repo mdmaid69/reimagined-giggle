f14509b57a77230b62cf74e4ee962c6a2ed7a184
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:02 +0000

    inquire goaway bear alone world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
