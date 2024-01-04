2558aef410d55a6ccd367875dcab50237609a490
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:14:20 +0000

    tall big copy sing future

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def add_numbers(a, b):
-        return a + b
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
