8a9abb1aa7a2a5488731f741cd678ecd771f3231
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:43:45 +0000

    actual rare shape speed bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def count_elements(lst):
-        return len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
