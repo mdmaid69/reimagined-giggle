b1bc45e7d2e5b21c5f7204478eca22af058c66dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:20 +0000

    treatment rescue sad among summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.path.getsize(file_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
