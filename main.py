16c6ee8bce6a45a353418e8ca9a26062d66abac6
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:32 +0000

    small paper Jj this disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+print("Cube numbers:", [x**3 for x in range(n)])
