0454b599537236b7788ee729f3f392da0ae7fd3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:41:48 +0000

    city shoot simple game advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
