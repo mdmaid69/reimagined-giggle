be288ce744221c83dd4895f4ebddf0429d5c9e12
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:36 +0000

    send unkind bunch temperature worry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def change_working_directory(path):
+        os.chdir(path)
