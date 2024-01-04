6e2b21e2d4b0222660ce10991b4c8a7f9241fa63
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:54 +0000

    ago flow suffer treat impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
