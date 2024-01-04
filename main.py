af436cdcdb0dfed177353a43206f934ff04589cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:49 +0000

    finger doubt chat bother control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
