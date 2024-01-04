05f2076de5cf3cbd60224641811221a9af1c34a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:08:34 +0000

    confusing arm threaten trick name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import sys
+def print_python_version():
+        return sys.version
