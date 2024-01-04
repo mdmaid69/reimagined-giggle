caeb8e57cbdfcb7cd339dc526f3f77d38293171b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:07 +0000

    set Rr tidy there difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
