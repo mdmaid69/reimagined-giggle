ce470eeb56df8249b152c1b9b1ec73946dfcac0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:37:55 +0000

    past war age impressed determined

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
