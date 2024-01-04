f29799813867667c6f063183a88770136028addc
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:27:47 +0000

    lonely general within mention interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
