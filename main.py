aca7ca929ba38dfbfb7c8290535a7d26b93c46dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:23:03 +0000

    form disagree appeal must cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
 import array
 def get_array_as_int(array):
         return int(array[0])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
