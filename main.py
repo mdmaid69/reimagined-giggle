053eb0720bebdc1192986a708b83a7dc99254ec3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:09:24 +0000

    wish ticket entrance help sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
