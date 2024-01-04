569c40a5098f78c20e3da17c873d0cfb78e47a24
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:24:56 +0000

    share address cross wise unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
