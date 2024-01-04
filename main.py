2f012f3c739f2f452b3a7518307ce54cae4abd5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:47:35 +0000

    confident art poison each tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
