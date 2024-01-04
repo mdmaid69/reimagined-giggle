f90f94d4cf4a5257583d4fdb3bc58b00a259b023
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:21:22 +0000

    everybody thing bar club train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
