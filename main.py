df2ee17dc2e596892bbf126729e8456877b2f191
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:03 +0000

    arrangement evidence general lend brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_floor(x):
+        return math.floor(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
