99d83c23a78f57ca6f03fc22ee036ae0b132b8b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:52:43 +0000

    quit doubt keepfrom commit dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
