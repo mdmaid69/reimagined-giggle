0d63290085a75f487014abff23e0f526e4595503
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:19:13 +0000

    identify get complicated along Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
