a429f2bd9fc409925f0eede234f8ada26abd5fd4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:09:33 +0000

    arm cover holdup race glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
