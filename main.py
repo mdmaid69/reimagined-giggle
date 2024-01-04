0e60349d0bb4e1f8e10fa96bd987c2abc1c41ddc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:57 +0000

    comeoff offence situation news between

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def square_number(x):
+        return x**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
