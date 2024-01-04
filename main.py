197b3e90a3ca34eb595216f7af7e8091906d9b7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:35 +0000

    rain brain comment interview warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
