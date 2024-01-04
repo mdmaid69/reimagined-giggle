5f3637bed5a2ed90169e6bcc6c511a8c9e2012c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:35:19 +0000

    yourself goback(to) test basis ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
