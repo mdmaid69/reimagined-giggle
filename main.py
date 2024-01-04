8c3b87848d93fff0d8d242d0a676ee77dd88ecc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:26:57 +0000

    middle replace purpose influence alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+print(math.pi)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
