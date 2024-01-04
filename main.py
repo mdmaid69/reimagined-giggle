aec2bd5002169223c3918ac18fa1ebcc154239c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:44:33 +0000

    strike manage murder cost no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_pressure(force, area):
+        return force / area
