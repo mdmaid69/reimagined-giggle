c1607fac61c5fde7c28334f754e38eb3f6b401bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:36:02 +0000

    pretty you upset guard sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print(sum(range(10)))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
