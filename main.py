4d99f16af1bb865c7a0f53fc38dc128ceacf32c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:20 +0000

    564encourage neither love loan before

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import sys
-def exit_program():
-        sys.exit()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
