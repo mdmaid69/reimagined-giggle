fc0c2f0b3f19f7977d3dc4d25b6b8942562dd942
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:31:49 +0000

    but feel style perform flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import numpy as np
-print(np.array([1, 2, 3]))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
