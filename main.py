1111194e36a777927701ff22441c7583e65b6f1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:46:55 +0000

    know balance ring ready runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
