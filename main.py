ed1b449c1fcb347993177dfa468bdee0efa6f608
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:19 +0000

    conclusion goback(to) receive disappear that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
