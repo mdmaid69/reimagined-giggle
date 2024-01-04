176ebf12cbb4bdba64a96398557f2c3b01f0f4d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:26:36 +0000

    coffee keepaway rich forgive treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
