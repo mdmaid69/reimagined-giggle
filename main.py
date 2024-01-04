9aed726ec41731c54a5be95649dc824ae3e0ba44
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:30:58 +0000

    surprising criticism part any degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
