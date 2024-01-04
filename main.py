09c2015da0ef97c6c090e27c0e90b77bfb938ba0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:37 +0000

    suck polite make male bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
