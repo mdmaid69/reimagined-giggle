7d00d741673ba26dab8b25175b050867ece16d7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:54 +0000

    new declaration only then but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
