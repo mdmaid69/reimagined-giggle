625802206e9b160dd3d56fc3d0146170c421855f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:28:54 +0000

    except holdon dance worried also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
