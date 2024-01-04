10f00b11404130aad782fe49fd4d7124834568b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:21:22 +0000

    under quarter runaway country respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
