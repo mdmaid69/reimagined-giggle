cb4a2dc5e527dd2eeba40fb072f3a6e6c6f34992
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:50:01 +0000

    one slightly dark honest fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
