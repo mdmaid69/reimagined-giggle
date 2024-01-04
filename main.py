b6044874fee6e60ff65dbed4d071379ef4a16aae
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:10:05 +0000

    finger belief temper callback foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_user_dict():
+        return collections.UserDict()
