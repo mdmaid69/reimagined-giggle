314c4d918e83f9eb299c06673e1a8065fef3fcf3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:52:22 +0000

    save dance cover marry hate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_array_length(array):
+        return len(array)
