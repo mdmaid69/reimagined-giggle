11eb685dba81a85647b6c826e2d7a5b8ae7dfa4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:00:46 +0000

    wrong surprising avoid describe skirt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
