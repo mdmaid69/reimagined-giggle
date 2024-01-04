d6fd60a7e9cb126f5ac7ff9e1fa62df91810f446
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:43:35 +0000

    wander order workout geton separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
