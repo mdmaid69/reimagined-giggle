4627596ec5b2558167e22b01fe40fbcd2fc88bb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:20:15 +0000

    whether instant switch run thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
