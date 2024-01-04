f2b0449f2b52c2f12055dc42dd6b6cf7812bc183
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:00:35 +0000

    course play Gg worst money

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
