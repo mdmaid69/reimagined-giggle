54affe712ec0ed9c60bab6c947d4af4c86bdc93f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:27 +0000

    national height stupid whistle blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sys
-print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
