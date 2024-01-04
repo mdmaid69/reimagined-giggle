01448b54a1975d0a594cda9181894a91d6de7a2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:26:11 +0000

    wide apologizeapologise breakin meeting number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
