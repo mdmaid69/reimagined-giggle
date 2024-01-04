e64fe3422b68da300ebdd810bde3c2f2bb808494
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:56 +0000

    fashion flat often die crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
