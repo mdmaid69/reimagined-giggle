646d73cc933ae4d7d6352e44895ad41686f8c6bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:12:22 +0000

    or band love hurry concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
