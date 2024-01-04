6b67a476725250fe1a20ba7f60ca1e2310de31af
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:30:27 +0000

    splash this either plan measure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
