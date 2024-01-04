7f7e8e217829865734326bd439dc96b5e709b806
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:09:24 +0000

    image decide mean wall circumstance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def count_characters(sentence):
-        return len(sentence)
