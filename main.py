69ac2097a4e32c734e2868df1d5c957b8dd41070
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:51:39 +0000

    hit holdout pretend harm float

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+name = "Python"
+print("Hello,", name)
+def convert_to_binary(n):
+        return bin(n)
