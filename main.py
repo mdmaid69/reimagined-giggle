cb3a0a1ce2abf1a4961b7afb9252220a5c4c2fee
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:44:07 +0000

    admit opponent fortunately trip breakup

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import numpy as np
+print(np.array([1, 2, 3]))
