adedcc396589dbaf107f04e997d06453c4acb8a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:23:39 +0000

    soft pencil only favourite little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
