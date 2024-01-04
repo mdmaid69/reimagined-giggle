43ce904245101423df0efd922ea7ab60f49c5b8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:42:39 +0000

    you soil cruelty 179bite lie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
