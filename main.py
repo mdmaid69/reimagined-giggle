5abfcd65cc39922a56fc9c3db9dc14304a1f582c
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:36:11 +0000

    clearly guilty backwards Pp open

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
