33110a9af72254dd4e181a9ebd530863b0a3f02c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:02 +0000

    marry vision consist rain heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
