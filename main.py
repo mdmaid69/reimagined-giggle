2cb518b509daf632963e2c77eb35285bfc4f4078
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:40 +0000

    branch colour shape pot turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
