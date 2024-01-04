fdb869b40b5c360444f7cead74a91c67303f1a4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:42:14 +0000

    forget fold coat level sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
