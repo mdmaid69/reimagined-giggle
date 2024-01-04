b2d63cd0ff4dde6b19cbedde96bee344c3bc908b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:24:09 +0000

    closed speak aware marry confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
