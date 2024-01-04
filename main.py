72cc24fbc81993c9f785a638906dc2b18a2a1ddf
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:24:38 +0000

    turnoff settle also bath break

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
