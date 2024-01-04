605335d20dfb175edcdf6e31035d1e28f69f5337
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:32:48 +0000

    creature natural opportunity climb sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
