a1a99bc547a284ba1c9731069b61a12ac5accb01
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:14:56 +0000

    lonely plant alive music cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def get_array_buffer_info(array):
+        return array.buffer_info()
