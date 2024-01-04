57b92b4910785439443d150e5c30401a76d5e574
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:41 +0000

    seriously worst first flavour crowded

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_as_repr(array):
+        return repr(array)
