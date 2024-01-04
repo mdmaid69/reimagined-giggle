bc30b5ebe37471abc9f7d98f13a9092ba82f2acb
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:03:04 +0000

    development Kk lost risk myself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
