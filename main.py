c333c08bf249978637b5bceebae4427067e7f8dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:30 +0000

    measure divide fall listen wipe

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
