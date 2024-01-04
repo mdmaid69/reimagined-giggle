afe0f3feb3340d67dfed9d87a7998b28d37c241e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:11 +0000

    task attempt about thankyou steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
