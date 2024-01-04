ae57b88a53b0e6ef8dcaa48e17cee50908ed42d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:44:32 +0000

    show fight float press violent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
