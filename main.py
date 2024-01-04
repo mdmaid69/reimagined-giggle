6d7c2d59bffed2bdd4252fe37afa22c8473448a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:42:49 +0000

    insist division wealth actually noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
