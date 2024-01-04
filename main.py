c9404fef7b364342c98a254856801572849217e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:24 +0000

    survive sweep late round breathe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
