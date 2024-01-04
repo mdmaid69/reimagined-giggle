11bc43799f9f2809f15860971b3833294188e939
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:47 +0000

    high turnoff delicate bottom moment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
