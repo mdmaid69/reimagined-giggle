599a5e982b6ace9c8b329980adf06526926a2469
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:24 +0000

    round book divide knowledge national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_set(array):
+        return set(array)
