c99cc66ffcbd1e65cc0025175bd9492ec26f29e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:11 +0000

    easy letter so pair soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
