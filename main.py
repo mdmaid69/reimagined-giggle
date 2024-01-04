1bb8784642f90f3605e283443564fc74e6d849a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:12:34 +0000

    shame argument fill ring half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
