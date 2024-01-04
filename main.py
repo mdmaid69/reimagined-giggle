ea9ef6c83c6ea312b935f83b30114379a61a5958
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:21:09 +0000

    damp person just book shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
