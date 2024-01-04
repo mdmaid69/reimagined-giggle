18d109f903d8020a8a1a08928697b5f8d1913dca
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:59:52 +0000

    detail software kind Oo set

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
