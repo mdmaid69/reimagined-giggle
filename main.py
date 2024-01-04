3737aeaf566f5ac79f3003937158892ed050f8c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:33:58 +0000

    angry manner regularly attempt complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
