c326c967b737cd38d25e9e083f83c3e0133ca1fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:12:30 +0000

    powerful geton cry plenty tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
