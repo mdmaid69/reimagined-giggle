df34c6421d38dfbf7ba155ae258cfe1ea664175c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:42 +0000

    interest clear pick immediately base

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
