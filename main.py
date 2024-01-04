e1c6afe14ea57a13fe9ca8f0b329bfcdd6d5d9fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:24:49 +0000

    feel dick opposite bang actually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def remove_from_array(array, item):
+        array.remove(item)
