54ef3e41043f81c3914903e6cabc908a7c262c64
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:59 +0000

    vanish century wish wide trap

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
