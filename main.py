8c14bf3e474e9cebea455ec0ff23ababa995f473
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:18 +0000

    geton blind yesterday exam bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
