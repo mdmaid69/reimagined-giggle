1ef118ba44da35580de6b967df8ce5416c8c5f4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:22:12 +0000

    support fine comefrom like confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
