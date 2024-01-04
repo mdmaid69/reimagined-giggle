d559866480a0a8698f99d7e232d59da3d8619bc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:58:19 +0000

    born 572enter protection lay urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_user_string():
+        return collections.UserString()
