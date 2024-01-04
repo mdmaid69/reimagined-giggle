0caa2afe246be3fa7d29d8efe47e9b4f09be916b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:18:36 +0000

    exchange spare admit odd criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
