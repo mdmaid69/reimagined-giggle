1972f020d4fcd5978aaac60f8905677c4aec1bcf
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:12:57 +0000

    drink recover tear service know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_power(work, time):
+        return work / time
