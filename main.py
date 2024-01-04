b3527514362c7cd90d27c3108be13cecc9cd5bb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:21 +0000

    line discovery regard ask quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
