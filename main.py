c837737ccacc0abffbafe454fad99bb46ba99319
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:42 +0000

    unlikely considering up contribute fall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def greet(name):
+        print(f"Hello, {name}!")
