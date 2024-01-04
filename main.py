102bff0658bcc314f2371564ff17ecce9b0c5ef3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:08:35 +0000

    occur goback(to) lower model trap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
