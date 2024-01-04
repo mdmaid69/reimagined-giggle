a2e457cef4c379a0db00e55c696b0087bb43a474
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:51:58 +0000

    offend action science foreign welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
