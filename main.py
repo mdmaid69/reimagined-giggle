d9a11e9eb712618f884c1bd05a0f98b6d905f1b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:08:32 +0000

    relation pleasant environment ahead anybody

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def square_number(x):
-        return x**2
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
