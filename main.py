197b6bd084d15c7791d6b9f0aec226459a12e231
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:59:15 +0000

    tip tidy present mixture space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
