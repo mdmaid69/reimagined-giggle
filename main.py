448fc69c9239476d99a3290b16010f3dfcc7c78a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:34:18 +0000

    declaration dance return but interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
