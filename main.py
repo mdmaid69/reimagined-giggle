d9c9a90ca622372da4c7640d380e41bd849b625f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:11:05 +0000

    petrol joke impossible represent Gg

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
