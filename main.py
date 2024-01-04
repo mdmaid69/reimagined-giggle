4215b939296d2aa972870638bf9fe538df8a96cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:47:27 +0000

    unhappy border better lost model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
