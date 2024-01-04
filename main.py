7da4f1bae0de26f13c6eccc86020a3476153a037
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:55:52 +0000

    confident except dare heavy dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
