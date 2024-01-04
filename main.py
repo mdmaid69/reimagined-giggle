46b74e295cc76e87a9fbb4ebb6b0103f369c3697
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:20 +0000

    Nn lookaround long somebody above

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
