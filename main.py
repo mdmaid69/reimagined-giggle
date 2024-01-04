9f91bc643b31c6c0bf1a8c390e9ee4b2b74da74a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:39:59 +0000

    to cheque service division instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
