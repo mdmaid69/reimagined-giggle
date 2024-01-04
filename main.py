75d2459fb79ea6be624d0d162820389324b91541
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:09:58 +0000

    everyone surprised what exactly send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
