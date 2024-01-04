1061b98129eaa1a2f5c99084e71c5e0b0768fe36
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:22:18 +0000

    notice brown broken shape increase

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
