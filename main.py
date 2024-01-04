7720f53ba0671fccae77b0fc71cd14e145abe389
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:48:30 +0000

    acceptable create shopping nigga process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import json
 def convert_to_json(data):
         return json.dumps(data)
-def find_max(numbers):
-        return max(numbers)
