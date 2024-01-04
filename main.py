7a72106538eb09e77adb98830adea433acef20a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:51:47 +0000

    single apply well born below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
