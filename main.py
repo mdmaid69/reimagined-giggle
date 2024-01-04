ccc0a4fb5cec3c56dbbdafbb0fd47dc9904c38ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:20:33 +0000

    tie table wander port finger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
