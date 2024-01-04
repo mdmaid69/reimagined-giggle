2c537d767fd4b390351805ec8693c50d6451f70e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:52:09 +0000

    rule pin kick comfortable letter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
