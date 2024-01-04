579a9094627a98ad45c44fd35125accc1de2d04e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:32 +0000

    hour off recovery rise nearly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
