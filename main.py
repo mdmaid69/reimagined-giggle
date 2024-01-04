d59da517980528a53093355245b13de7a4150826
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:48:25 +0000

    week style progress crash circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
