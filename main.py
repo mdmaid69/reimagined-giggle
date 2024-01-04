bcecff705ad60495c79e8eb76bb127071b80d4bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:22 +0000

    able form side unlikely 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
