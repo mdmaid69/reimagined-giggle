11cae7ecb37848f16c072b8d40e45ee1e4ab2052
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:47:54 +0000

    shut feel nerve reach translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
