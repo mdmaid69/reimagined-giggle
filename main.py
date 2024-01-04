8a52fa6f5cce011180e1bdb3ff151589c62b98e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:26:36 +0000

    job lock pain lie describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+print(math.pi)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
