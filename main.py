916c7c51549ae21687f8ef0960a2a4c12cc37044
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:59:03 +0000

    sting getaway command evidence scared

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def add_numbers(x, y):
+        return x + y
