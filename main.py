7af6593d8b017dc33e3439edc28bf59b9d25f1a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:50 +0000

    similar birth obtain broad offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
