6c8890991f01cdf62074fb2ea9b4a5d9721cd24f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:35 +0000

    then pronounce amount bread 566end

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
