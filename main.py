f2932311da69b7ea45bcd782ce559c04fee591e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:12:07 +0000

    near early spray imagine lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
