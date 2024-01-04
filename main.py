8228114814d03509fa9ae80c30cd7d9f9050168e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:38:32 +0000

    birthday save bend walk mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
