e66244977f43d3a6c4d1a4c065a751e08cbbfdae
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:29:16 +0000

    blind along pretty decision my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
