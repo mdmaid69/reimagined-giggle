ef2423f82b88b59c0aa5b7acf1b5aec3ac0b4c58
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:36 +0000

    572enter bit land excellent connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
