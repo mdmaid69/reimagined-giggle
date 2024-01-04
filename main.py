8b183684ba8c8111c249363596aa7634ca8cac57
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:29:17 +0000

    check vote accident explain across

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
