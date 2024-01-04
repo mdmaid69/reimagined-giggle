d751768c8db91a4ce091183a7451d3ee0c374ac0
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:18:07 +0000

    back talkoutof warning compare such

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
