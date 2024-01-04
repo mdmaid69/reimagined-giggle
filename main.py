7a4e8cc5dbad19c0f5ae486b70af79944be4372d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:34:21 +0000

    label suckme recovery black harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
