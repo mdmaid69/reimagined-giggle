3247c5aaf6ab8e5f93257da336ec15ce48fc2a83
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:48:51 +0000

    eat sail increase shock wire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
