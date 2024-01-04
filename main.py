898d0f3ffeb4b1222ab5262054b660618ef8028e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:20:22 +0000

    body affect basically indeed charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
