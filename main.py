25513b5343dccc75c849ed8df4b7355f1e8903ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:53:05 +0000

    discovery runout dry comeback secret

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def add_numbers(x, y):
+        return x + y
