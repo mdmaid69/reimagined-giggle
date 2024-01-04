9bcf62c3f8a90678d16c50349ed70df1c6f16aef
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:29 +0000

    runover name badly mind opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
 def calculate_sphere_volume(radius):
         return 4/3 * math.pi * radius**3
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
