2cac85eeb9094edd74c2849a8900d94324b1b7d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:58:56 +0000

    year until nothing strange read

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-name = "Python"
-print("Hello,", name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
