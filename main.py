d4ad834952636b02d2fb3cc5dfaf325441f916da
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:15:15 +0000

    wet talkinto clear band beat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def add_numbers(x, y):
+        return x + y
