c4a6d14cca81c7ccafbe31274ebee329ae161ad9
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:57:22 +0000

    possibly comefrom turnback sentence tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
