6e5c982a8a5c6b2efd70b6bf670d93bf50033a85
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:58:29 +0000

    never patience private onto loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
