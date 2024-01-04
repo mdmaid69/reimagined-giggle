d11d673630039bc202c258640b27dbde829e2d80
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:32:33 +0000

    pay sentence fire obvious surprising

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
