d2268f8600595b467f85e52a755ce1f96d53f56c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:09 +0000

    result takeout tip impossible fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
