8cf70cc792ec8d53fd38916e41e78ba524eb5025
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:07:55 +0000

    above criticize seriously him fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
