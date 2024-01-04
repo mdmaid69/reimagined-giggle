fa4920e26f7b4a0062d4998b79a1b5b0fd48796c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:05 +0000

    cool backwards everywhere minority apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
