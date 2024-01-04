ecb54a3050962827d319459fb64ce74e428dbb6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:52:05 +0000

    mass complaint control careless forgive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
