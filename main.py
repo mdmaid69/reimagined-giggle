e576044d5d8839bc931ebf7618af6d69e600b82b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:41:29 +0000

    apparently combine bin immediately report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
