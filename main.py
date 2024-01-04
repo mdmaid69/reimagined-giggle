da508fc858758c5b827380978cb0c73e761decfb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:58 +0000

    she forgive heavy firm strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_itemsize(array):
+        return array.itemsize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
