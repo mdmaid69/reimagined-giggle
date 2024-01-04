1fa0140ab527e42e6b5d9a7cec269f97da25f41f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:31 +0000

    mind anywhere home common cross

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
