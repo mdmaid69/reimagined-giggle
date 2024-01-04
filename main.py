e4f132de6b54e88fb175cc73d8e7e8e1c3acf5a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:08 +0000

    bury trip summer choice geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+def print_python_version():
+        print(sys.version)
