3ad41440a94151ed8a78b19577f72976991bbcab
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:33:43 +0000

    normal ride attract build money

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
