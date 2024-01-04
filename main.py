2674b88297be0021c0a9fe19720ffe17c05268d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:47:45 +0000

    hair limit pretty middle control

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_power(work, time):
+        return work / time
