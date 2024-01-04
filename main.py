1cbe9d3fe74e716341b0176a621cebd425653905
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:52:33 +0000

    compare book uniform nerve our

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
