5090e7bb65cfd5b60b1f9dfe2316f087f07ce4ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:22:04 +0000

    train afternoon interfere protect earth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
