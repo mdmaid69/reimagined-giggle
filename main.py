8e8f26e07fb9434703487c7eccb7f154ce5a12f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:22:27 +0000

    criticism middle wash waste comeback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_max(lst):
+        return max(lst)
