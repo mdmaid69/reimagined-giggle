c200e03cba202d4237efc47084941b6d61616e80
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:37:03 +0000

    shock embarrassing wherever during chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
