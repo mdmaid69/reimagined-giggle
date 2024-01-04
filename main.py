6454d7ffa903ef1a19fbcb4bc7bf81fa1495133a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:29:59 +0000

    surprising contents bound attention bite

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
