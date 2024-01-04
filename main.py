4aa74e4d0dc86f9ef807138fae8fa79b6ba0e7a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:03 +0000

    afford impression turnoff warm everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
