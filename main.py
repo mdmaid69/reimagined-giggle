ce64d40a5be86280d32a61020c1b99d43fe68ac9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:32 +0000

    callback perform rest surprised clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def append_to_array(array, item):
+        array.append(item)
