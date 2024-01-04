289d0e8cf9621d6658e9e27954ad9b12f844c7ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:32 +0000

    in description declaration brave exploration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import time
+print(time.time())
