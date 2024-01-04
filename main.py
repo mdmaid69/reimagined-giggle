71af8fbd920a5396c9e27657a8b4cafaf9a50b67
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:59:55 +0000

    tongue bin point trade once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
