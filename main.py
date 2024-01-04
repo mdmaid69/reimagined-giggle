440e3c717430a96994c929d27ba37b81431e6a69
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:58:47 +0000

    post declare progress term nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_str(array):
+        return str(array)
