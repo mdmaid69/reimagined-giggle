e541849923b5e020a85de87509cc6357d4c568b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:16:10 +0000

    possibly part fence comment beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
