9247442b0840b88d1deb36f0ba0c34707408ba1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:09:09 +0000

    weekend accidentally keep people admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
