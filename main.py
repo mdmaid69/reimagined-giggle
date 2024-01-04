026f2baab2c48d3f1d6b3fbbd5abf3132b1383dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:53:23 +0000

    report manage believe cut make

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
