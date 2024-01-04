959f9836c5e8527f102252fbdb283d75dc55e7bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:39 +0000

    plate there play hold OKorokay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
