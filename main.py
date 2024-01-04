32079e4614e61a428ceddf530bdfed8c7b05c4ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:46:32 +0000

    respect exam 570enjoyable gradually bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
