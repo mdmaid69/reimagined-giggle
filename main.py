2d8bec9e675afa49c1edee76a363da9ae5363aaf
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:59:30 +0000

    confused touch direct friend your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
