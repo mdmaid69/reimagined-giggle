588a81dfe828f52ca990fd65b71383b91bb852f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:42 +0000

    rub shoot claim pretty getup

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def convert_to_hex(n):
+        return hex(n)
