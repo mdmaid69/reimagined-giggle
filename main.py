737e9c57e03beff43548602a5b635499848125fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:07 +0000

    further slip bury commit allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
