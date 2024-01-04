25be79114723a3e7cdfc8a9a4e33e84e7542172d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:31 +0000

    tonight through kick cost plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
