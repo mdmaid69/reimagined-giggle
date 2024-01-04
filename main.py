8ac7274f5f851d961cda3ed3cf6c195da90e60be
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:22:05 +0000

    until turn regional education hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def square_number(x):
-        return x**2
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
