3a1cd7575f706c7f92d9b246b48b902dfee65fa2
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:31 +0000

    cut our show will edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def get_current_working_directory():
+        return os.getcwd()
