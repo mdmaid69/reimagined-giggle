397c7b329ae6e33196dab0ee7a19ee4543aaca86
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:15:21 +0000

    start people action watch after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
