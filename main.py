9bd3f3d491ffa6dc1eecd05b51bb4d7ab14b57b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:17:55 +0000

    project flat whistle cum dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
