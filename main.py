2bd012d349b4a0a1f190759535af1bfe501a7e35
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:05:50 +0000

    together regional wash congratulate phone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+def is_even(n):
+        return n % 2 == 0
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
