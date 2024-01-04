5285bcf588576c0df8aae5d90ddb94ec02a4b221
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:20:53 +0000

    together plan blind shallow brush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
