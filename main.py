4fd007ed3d3f204751bb6dd894e74d137b0b0787
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:39:33 +0000

    hospital their fierce return lookafter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
