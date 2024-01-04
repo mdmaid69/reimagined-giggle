b8641a07cdf6e4f1766345f9a1b39e92062ff16d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:58:47 +0000

    path late brown he apart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
