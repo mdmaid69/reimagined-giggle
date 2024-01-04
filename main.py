883c7f262d092934a5551ca2c575480bf6ea68b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:21:11 +0000

    rush nowhere treat belong achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
