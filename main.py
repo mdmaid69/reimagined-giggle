64aba24a27d284974a30979bfe9ab7a4ae083a1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:07:19 +0000

    rent clear present word part

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_density(mass, volume):
-        return mass / volume
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
