c507a9354643a5605bcbc9d85442c7f4785a655c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:55 +0000

    practise pleasant refer yes capital

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
