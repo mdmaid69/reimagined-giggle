ca2f2c63a4a917f3064e15bf4718c9e022169bef
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:15 +0000

    afraid neither workupto pretty slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
