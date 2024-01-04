6ea70ee6bcaec82a9f4bc26b36ecedc53d996319
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:30:11 +0000

    mad damp Qq inquire we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
