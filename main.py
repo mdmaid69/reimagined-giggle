0459ae79ade4440308ffd26602fdad275794e69b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:02:26 +0000

    window touch people tool fear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
