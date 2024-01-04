e45e93334f7f3f26c1bda0401f789b460dcf1d59
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:06:56 +0000

    regional fond regularly depend store

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
