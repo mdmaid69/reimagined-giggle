6e4588447d43674920bc958db3ff15171be9db26
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:13:26 +0000

    flavour run shut stamp odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
