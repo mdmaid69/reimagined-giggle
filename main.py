0fbc898fb6b1b1b881f44a5f7269df6ac5c31bec
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:29:47 +0000

    bring entrance direction petrol naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
