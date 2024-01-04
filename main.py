4d36dec1b271d4db4b21a279e613268273774649
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:48 +0000

    add major get fold Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
