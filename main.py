ae70a0100032a78f2235af4b66dbe8d490099ad3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:14 +0000

    lose task study holdon surprise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
