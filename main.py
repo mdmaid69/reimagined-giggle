107135773aa809566307a3186a223ed323a6b50f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:10:01 +0000

    sense professional answer poor every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
