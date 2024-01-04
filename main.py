70c28cd8687d3f92538cb1590ee6424022bcf918
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:33 +0000

    spend sense prevent trick arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
