4aa3a472c70aa0dc20e3a08067bb85b529ebd289
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:06:27 +0000

    operation skill sudden spring aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
