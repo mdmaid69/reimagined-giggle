35ec8573617e55af640e9074e5d9cdf8e0f20def
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:23:35 +0000

    chance bite no turn store

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
