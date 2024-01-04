24f7ef5db212547288e81d1de3ec18034ec113e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:08 +0000

    mend embarrassed cheap light quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
