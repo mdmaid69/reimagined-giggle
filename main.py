7e6887fa695f7fea1dda99da281926a2b264b02e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:22:07 +0000

    sleep list health claim set

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
