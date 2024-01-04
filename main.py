dd54b1f6320fee2365dece5a6af59c06debeec99
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:58:40 +0000

    education independent perform sensitive Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
