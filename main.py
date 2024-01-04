66cee38c46bad2437e6daf15e275932c68f63850
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:54:22 +0000

    task about onto question plastic

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
