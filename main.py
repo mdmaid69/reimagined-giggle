6ad48b9d883ff48b2fc1a967fccfbeb9f1120f91
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:21 +0000

    contribute computer announce feed born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
