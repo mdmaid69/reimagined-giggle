8aaa13e162ffc16a69d3c5152ade7d8953a0c2de
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:45:35 +0000

    wet male team sort on

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-print(random.randint(0, 100))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
