014d6deab2dc7fa8fbbf94e55ab6523e430fe705
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:03:30 +0000

    damage performance talkinto flat lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
