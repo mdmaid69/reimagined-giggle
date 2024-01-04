2ff94587172f53941318ccdfaf2fbd957a9199a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:13:41 +0000

    prize steep opinion jump represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
