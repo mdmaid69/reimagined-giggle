475ce3e9279f42871fdeb64d342bda3300102bfd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:09:25 +0000

    arrival careful process account Kk

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def convert_to_octal(n):
+        return oct(n)
