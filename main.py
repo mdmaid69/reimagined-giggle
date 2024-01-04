5f71a95b91e078636350e92e5488d861d910487b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:54:09 +0000

    wet centimetre way off manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
