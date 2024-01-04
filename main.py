47bf5d0b2f931f6b388e83a72889d5fafbb7039a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:13:29 +0000

    rare fear congratulations keep circle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_min(lst):
+        return min(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
