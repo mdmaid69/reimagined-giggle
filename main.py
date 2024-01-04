c882354d20e368b9545cda37e344a97008f92ad3
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:36 +0000

    birthday except beside heat match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
