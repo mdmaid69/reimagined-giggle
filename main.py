5c31ef837e794c284aaee2cfaffddf1283f86ba4
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:30:03 +0000

    agree weak traffic goon silent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print("Hello, world!")
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_user_dict():
+        return collections.UserDict()
