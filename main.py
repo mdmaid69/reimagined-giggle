a32fe12fcc8d2f7e29e507d1f973ba9b87900439
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:47:42 +0000

    interested air trick lean lucky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
