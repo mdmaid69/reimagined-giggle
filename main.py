4845a553051f4df7e801734e4b1c5d590c6f66ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:57:08 +0000

    impression number fail getback check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
