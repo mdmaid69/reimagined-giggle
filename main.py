91bfb3526671350842e89a10a82e7f59ea9064e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:16:28 +0000

    cry deep delight fear ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
