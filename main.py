f7f622a0beca7354abdc8a136869e87da4cf24dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:19:16 +0000

    thanks impressive trouble approve stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-print(sum(range(10)))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
