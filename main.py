13f5a93c0cba5d403f7cd6d478345667f8810845
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:19 +0000

    visit count emphasize all wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
