bb3896d18163abfbeb9b7433f0d33be037727ce4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:47 +0000

    freedom will emphasis there hold

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def exit_program():
+        sys.exit()
