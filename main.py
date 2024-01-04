4cd4ffa9f17bf45f722cd2f3a9d7e1a6b491d10a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:28 +0000

    challenge convince callup watch nearly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
