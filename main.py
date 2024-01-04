285bc9dd3b2a2fa1c538ecbd0af7072dcfc14f0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:55:59 +0000

    turnon bury strength settle empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def get_array_item(array, i):
+        return array[i]
