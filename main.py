f6aa96f847d7351295b17745943cfa20149f7293
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:11:00 +0000

    judge step emergency objection damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import getpass
+def get_username():
+        return getpass.getuser()
