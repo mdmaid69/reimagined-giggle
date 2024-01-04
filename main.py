b1e3bab08554fe4e37fd61184c0bb37178504a47
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:12:42 +0000

    violent basis confused live smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-print(datetime.datetime.now())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
