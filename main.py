a282cff086cfb377b26c9e9c887b8600c126f26a
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:04:32 +0000

    watch either stay describe object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
