eeda6130d0022e9dbde291aab2e06fdfe82214d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:01 +0000

    rely beginning earth murder sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
