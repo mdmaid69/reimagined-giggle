2ef35df3b8b751bc9016527c69fe7ba9128ee691
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:54:54 +0000

    deserve right rush lower shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
