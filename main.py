e0a1f7a3ea8fd0af5912bfbdbd24600062cba33b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:49 +0000

    remain draw accept complain piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
