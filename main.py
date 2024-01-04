eeeadee49106d4e14b504531a98bdecfbca335cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:48 +0000

    yes tear comment contribute bus

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
