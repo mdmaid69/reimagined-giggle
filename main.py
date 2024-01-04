3c54903bd7e1027b8914c966e09c9a2f82e3c76c
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:58:59 +0000

    university account second tune repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
