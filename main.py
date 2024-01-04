8235ca164fd40cc3c2f494b9b336646131cbabbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:30:11 +0000

    advanced left fortunate reject must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
