eee7ae49ed28f5ea520067a639e91399fe39765c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:39:58 +0000

    tap spray whichever copy quit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
