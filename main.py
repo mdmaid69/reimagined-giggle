563c6c6126831de42d329ba535dc1c0c81a7a0ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:01 +0000

    born confusing tap put month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
