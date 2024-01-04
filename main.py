5c72fc0506ae45959b28968c053a1b7c3bdf91cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:45:49 +0000

    now past evening idea feel

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_work(force, distance):
-        return force * distance
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
