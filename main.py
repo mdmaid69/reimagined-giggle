0b236f64966d5ee16c641eb6467fe95db2595bfb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:32:50 +0000

    introduce basic morning least wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import time
+def get_time_since_epoch():
+        return time.time()
