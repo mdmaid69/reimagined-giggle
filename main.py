b8e7d8ea4bcd8b30b9bef89774ca5f50cca00944
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:40:32 +0000

    second telephone boring difference there

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def is_even(n):
+        return n % 2 == 0
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
