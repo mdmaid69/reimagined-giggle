ce8a24cd667747a4a5c268fa5ce4f1112b56b1a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:15:47 +0000

    whenever coat entry can mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
