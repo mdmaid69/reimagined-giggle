41682ed3d38971463c107da7a9ef46b199a8d452
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:21 +0000

    knock reply pride equipment waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
