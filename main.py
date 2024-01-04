3c753bba00a944c77fff8ba1ea612cadcb232116
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:03 +0000

    hurry natural there fond difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
