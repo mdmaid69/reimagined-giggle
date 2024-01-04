9e0e5ec7b64f73b6698901ca94603f2a6b4b7d45
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:43:14 +0000

    remark consist evidence cost moon

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
