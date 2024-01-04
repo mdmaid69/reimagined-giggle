c3ed4a64694d29fe6f148439961460b02f2d3c69
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:38:56 +0000

    lie total holdout keepaway rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
