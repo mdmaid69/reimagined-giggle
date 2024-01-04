e1289fbdb3b7cdf546cf7dd3d5677dc9e3701f79
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:29:50 +0000

    pair Pp cruel beside fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
