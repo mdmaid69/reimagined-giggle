ff82b494bb2b55289cdae94fee6e4075773882bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:55:16 +0000

    frame voice nature condition safe

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
