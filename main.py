0f37f427ea9a6e854885314b89591fe2706cc805
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:03:53 +0000

    insist patience who comeround poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
