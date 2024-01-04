a4514fc0b9e6645bb1f1b52d75af34f95fb2e58b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:12:27 +0000

    cool accidentally odd shoulder force

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
