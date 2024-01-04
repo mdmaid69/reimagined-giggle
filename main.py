b756912b28d831b3d415b0d523721a30f73f8e8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:05:07 +0000

    on sometimes crazy short earth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
