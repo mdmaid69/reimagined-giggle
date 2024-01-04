d770b218112587b18c02f132a924e87eefc90015
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:28:55 +0000

    minor means related greeting way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_base_name(path):
+        return os.path.basename(path)
+  def convert_to_octal(n):
+        return oct(n)
