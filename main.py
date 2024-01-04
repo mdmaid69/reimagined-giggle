5564eb086af131910d582f9daf73cfd77b17e880
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:42 +0000

    birth store familiar takeout introduce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
