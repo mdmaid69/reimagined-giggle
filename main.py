08e34cfa006789646bd0de378f6aa3465f9775eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:54:57 +0000

    whisper who rent fill lie

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
