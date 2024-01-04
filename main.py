9f54bdbbff45de7a405b6e29933f4b2e3d6e9d00
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:53:04 +0000

    physical entrance last takeout frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
