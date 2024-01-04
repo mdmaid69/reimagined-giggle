d37b25a0674f32c9cf53dc11fc1846af6e02e33a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:41:33 +0000

    hope meet statement concerning plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_arc_sine(x):
+def calculate_inverse_hyperbolic_sine(x):
         return math.asinh(x)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
