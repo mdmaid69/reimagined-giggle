d7b64d983d5c0e3c6b1b45ae6029b76b1fb4cdb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:41 +0000

    apply wander cough discover love

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
