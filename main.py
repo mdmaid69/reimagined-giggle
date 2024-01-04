34cbe0ca23b8f446aec87ef93ef6120f3b1288bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:09:27 +0000

    share drive subject small my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
