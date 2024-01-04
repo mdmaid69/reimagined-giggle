57639a7d2096efad4c4d818f1f1776cf2aa5bcdf
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:29:33 +0000

    clearly sweep official well wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
