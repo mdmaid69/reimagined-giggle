f4c0273623d5d7ad2b03859ad95c4de7aa7b14a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:18:32 +0000

    offer tragedy his cover factory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_work(force, distance):
-        return force * distance
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_power(work, time):
+        return work / time
