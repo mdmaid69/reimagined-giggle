f510873a2a47d411dd9365ec0b75682ffa2cd322
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:20 +0000

    once forget sign Tt smell

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
