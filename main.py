e474952a2a334c191a36523f16d1e39001bfe27b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:49:31 +0000

    fit sad absolutely door 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
