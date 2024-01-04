0625e94af5019961441f649d0d116f0136246c18
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:50:43 +0000

    between regional hurry agree male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
