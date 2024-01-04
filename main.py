f26a3c4228ecd851c928ca240897be3701768c77
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:37:13 +0000

    copy soak promise absolutely team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
