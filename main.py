0e218e8a9e63d7ea4119eaeac88a8439798165fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:14:11 +0000

    wrap dinner guide often centimetre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_pressure(force, area):
-        return force / area
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
