7c452729c3cdd3cdca8f4490dae8a01c3ef2400f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:16:17 +0000

    alive back rent pay equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
