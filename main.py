42216efe47863e6fa9ad3bc40b7249181c444913
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:27 +0000

    right die underneath urgent neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
