92b9a7a8dce51549a7d1e24143ca251e42ebe169
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:40:45 +0000

    raw bill rubbish search moon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
