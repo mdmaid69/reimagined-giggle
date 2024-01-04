acb2d6c9e876d23115f03a9b7d309dfa671d54ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:01:09 +0000

    trip supply rain exaggerate temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
