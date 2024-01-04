d32d20fd932b8b14e852706461348197cbd7475f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:59 +0000

    step slightly nor breakdown desperate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_octal(n):
-        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
