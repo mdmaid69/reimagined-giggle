9816164a9d0f0f225d70abc5915137037869184e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:30:15 +0000

    town aim march mix ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_current_directory():
+        return os.getcwd()
