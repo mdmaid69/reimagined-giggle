742158367a068a7b8d34b21db40b5c42a727e126
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:54 +0000

    surface brain ilovedicks hide hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
