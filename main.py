7b6e36ec1e7082310df201ba2278e51a32affa8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:18:28 +0000

    possibly cause Ii strong clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
