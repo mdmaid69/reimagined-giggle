e8ed0c6b682a3432d397261d245dd1992ed1af95
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:02 +0000

    may lead instant long crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import getpass
-def get_username():
-        return getpass.getuser()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
