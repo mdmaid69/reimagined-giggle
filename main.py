8c3ac721026ab9f89aa58408012325cd87f96d53
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:14:12 +0000

    accordingto Ii weigh when deliberately

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_circle(r):
+        return 3.14 * r**2
