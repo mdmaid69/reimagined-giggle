d8b962e5cd764d705102ceaf489b323930ad5c06
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:00 +0000

    debt innocent stranger spite month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
