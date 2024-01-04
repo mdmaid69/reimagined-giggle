d6f703eee8c5b139c5fabe8615bba61f702b3cf0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:30:00 +0000

    strike when offer invitation suckme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
