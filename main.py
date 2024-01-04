30056a94492378ea543a986a07bb536e13b0c2a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:18:07 +0000

    heavy bringforward giveout score set

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
