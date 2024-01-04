1c00fc2d8c34e849ecda94e44ae88d9882ceda39
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:10:06 +0000

    tool sit shallow we take

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
