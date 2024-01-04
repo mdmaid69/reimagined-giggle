7159d9e6f1cefdcf823ba6bc6ababd7f8732d4db
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:37 +0000

    pull deaf now mail deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
