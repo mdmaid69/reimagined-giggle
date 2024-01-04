491b80eccf66416ec9b2d5dc59fdc73882892116
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:13:00 +0000

    walk send another body unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
