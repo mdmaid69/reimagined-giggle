bec674aa30a6601629e710d6597ed3fb4d9a6faf
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:53:45 +0000

    nervous sudden smooth actually slip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
