3540997c657da5e0d15d6ad5ff17701cfd3124cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:56:32 +0000

    hear demonstrate justice think should

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
