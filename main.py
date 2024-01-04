94b58e833cc17a054c2136c1c126b06aacb4c1f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:15 +0000

    male report anywhere true consider

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import platform
+def get_os_info():
+        return platform.uname()
