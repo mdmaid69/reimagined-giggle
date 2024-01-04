31f495e08803a9631f30779217f8c1fbffa4d8ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:29:32 +0000

    spread cruel damp standard exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_current_time():
-        return time.time()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
