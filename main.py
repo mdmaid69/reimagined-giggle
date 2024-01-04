ea879a289e140fca42dc3710eda068824106858e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:26 +0000

    keepout pass best young whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def subtract_numbers(x, y):
+        return x - y
