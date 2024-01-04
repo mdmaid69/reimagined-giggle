ea5fb58926969ec7ef875c983801c2fa609ec53b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:20:48 +0000

    deal nerve stamp find among

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def append_to_array(array, item):
+        array.append(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
