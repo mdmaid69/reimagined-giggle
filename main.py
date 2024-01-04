a4121579ec6a2d194495126f8acb6b403b5378c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:52:29 +0000

    classroom pretty born search Kk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
