d03d088459f933bf286bd3dd9bb70e2b6b83b68f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:32 +0000

    male outside direction circle hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_work(force, distance):
+        return force * distance
