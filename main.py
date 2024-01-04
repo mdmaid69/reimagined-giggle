8d51718b39326e97afd09563186247404a6e3e82
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:30:43 +0000

    mixture appearance bush glad Uu

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def cube_number(x):
+        return x**3
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
