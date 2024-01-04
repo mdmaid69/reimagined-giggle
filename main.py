fc9ed8355d303812976c7cc44b79f68475a04260
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:31:14 +0000

    platform scheme cheat sink embarrassing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
