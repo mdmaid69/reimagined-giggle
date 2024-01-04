fe346e6bfd073f8e2d13d8459e25c4e46d0bf564
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:13:40 +0000

    willing ready important flavour than

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def count_words(sentence):
+        return len(sentence.split())
