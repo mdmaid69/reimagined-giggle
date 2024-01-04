fe90862f0ee5870a40c9789360eba4e0b8a60b95
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:15:52 +0000

    rate scared hurt emergency shadow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def sort_list(lst):
+        return sorted(lst)
