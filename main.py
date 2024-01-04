e9df48b81da9076d0bb21a6c64d9cb8ba01a98ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:44:26 +0000

    sorry train lesson sign food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
