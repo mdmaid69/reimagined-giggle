ffe2bf814f3aa2440002c6496b239e29c8fd543d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:26 +0000

    better base runout popular declare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(a, b):
+        return a + b
