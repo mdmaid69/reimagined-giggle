653701b3813a2a5da482a62b708ae23b07c4c932
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:38 +0000

    importance go finish stay back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print("Hello, world!")
