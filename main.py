1716cf7ca04746b7af447fe41473d5f7b69d2222
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:08 +0000

    once slight be buy dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
