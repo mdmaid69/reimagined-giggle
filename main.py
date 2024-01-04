85fa1a068dac6392e90f49782d670544d38a59e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:54 +0000

    tense direct form or damp

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
