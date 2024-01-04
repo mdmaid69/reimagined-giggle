70a87471c1d33bd023b7d6582603733901bbdd91
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:03:48 +0000

    responsibility book compare uponon wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
