e6bbe0951639d3e694b0cedbbbc3471e6607e5c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:33:24 +0000

    within guide set operation suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
