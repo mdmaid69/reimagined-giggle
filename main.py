100d0ce7d722ad33861b3a9e9a7fbcc8a9a32511
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:09:21 +0000

    female smell shock another maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
