6ffb3bfc8b8caa243a01e4e4a49d8793d0e1a830
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:35 +0000

    cover rain why means dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
