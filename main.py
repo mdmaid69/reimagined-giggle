c05ae44f144bddef1ac7565502b986f2f8b95511
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:02 +0000

    crash phone beneath mail lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
