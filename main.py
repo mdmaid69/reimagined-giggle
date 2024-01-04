289ca51ee6754c71731f6a1b25f5adda5e4fdb5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:53 +0000

    reasonable right period issue ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
