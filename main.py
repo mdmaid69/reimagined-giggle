dcc68188de18f02f60ab03c8f096e1878f8b74ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:41 +0000

    somebody dull exactly absolutely runover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
+import os
+def change_working_directory(path):
+        os.chdir(path)
