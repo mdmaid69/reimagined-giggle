bb7f73cd2b7b32a72b5ad3f00987189622dd4363
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:25:43 +0000

    fortunately store this little train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
