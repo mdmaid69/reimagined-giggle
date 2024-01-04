feb34b1709bf0c507e801dc7f2d2e49ad097af8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:49 +0000

    collection basis edge quiet generally

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
