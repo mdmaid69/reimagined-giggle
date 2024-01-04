f999ea2c8d767bfca5b85c4b3aceb34452261b3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:04:42 +0000

    tough phone majority fierce possession

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
