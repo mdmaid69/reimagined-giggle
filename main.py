4fe0cf0d04f2d38234a97bbc6ffaa0dc404c4ef0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:56:37 +0000

    date and that estimate march

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
