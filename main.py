a58aaac9ce5da72432c816a6aa1cdb2f5530d796
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:42:39 +0000

    represent nervous vote fight nor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
