e26bdec32948c2904c95844e071886ce498ce35e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:08:17 +0000

    hide area swing beneath back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import time
-def get_current_time():
-        return time.time()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
