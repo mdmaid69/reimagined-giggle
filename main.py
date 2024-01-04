07004318a893eff544a780343514aac6f62e0627
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:39 +0000

    talk figure interested very interview

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  def square_number(x):
-        return x**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
