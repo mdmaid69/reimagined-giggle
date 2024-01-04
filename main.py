8d600151471d8699e785fe9854783cd92595fe11
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:10:42 +0000

    over squash power would this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
