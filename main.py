5f3de9c268e145f9db9997cda022c2d98a44db68
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:11:07 +0000

    punish touch tell handle anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_float(array):
+        return float(array[0])
