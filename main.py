6b0f531b052c7d87044e915700bfd3b4e380d0f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:21:11 +0000

    fault sing keepout wind part

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
