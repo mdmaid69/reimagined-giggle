c026c81c49702a9029e07fabc35bddf6667f1b6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:00:39 +0000

    active crash turnoff suffer file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
