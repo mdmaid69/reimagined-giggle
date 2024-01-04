afe479ca6dd6edc0cc0a0ae8edac589cef03d024
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:54:20 +0000

    please sink break weekend fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
