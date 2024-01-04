acc0da7f6f6e76f56cbb55307c54a898aca84a2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:02:24 +0000

    right threaten stare square front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import getpass
-def get_username():
-        return getpass.getuser()
