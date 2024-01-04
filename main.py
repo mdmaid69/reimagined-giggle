f4b0a9c2e4384c268d167b10d144e7815aea5602
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:30 +0000

    year explain consider snow who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
