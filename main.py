bd1154d45f93707294119f9a60c633d94a5db8df
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:18:56 +0000

    occasional class getout explanation operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def sort_list(lst):
+        return sorted(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
