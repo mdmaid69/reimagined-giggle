01d1980ee659051fb9e93aaa5cf2a4c41731ac4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:55:17 +0000

    upset wait black human trousers

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
