cfeb0ba66c5b87588dc96380a7a940771c92d7fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:23:39 +0000

    myself yet equipment arrive address

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
