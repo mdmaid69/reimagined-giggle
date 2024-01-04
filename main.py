e1c66c0c324e337cff29a54987ddba28bb9d1b59
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:32:19 +0000

    mess dark crash congratulations announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
