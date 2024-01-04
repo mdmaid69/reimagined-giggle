70995caace4ef7e4579349a02ee5f99e51f2cd5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:00:53 +0000

    Ww trade natural light ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
