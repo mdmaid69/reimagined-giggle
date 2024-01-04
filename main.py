aba9d159e47db3a5b0e46ff9f2f249725ac6048e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:32:47 +0000

    light feeling everywhere when father

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import time
+def get_current_time():
+        return time.ctime()
