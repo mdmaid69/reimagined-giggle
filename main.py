dc9015491711940e82a6da152cc115f0d144f1c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:05:49 +0000

    judge delicate turn impressed circumstance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import sys
-def exit_program():
-        sys.exit()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
