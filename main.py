a11e952c16e8c0b91639852703df8905db3eedf3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:01:24 +0000

    getup best calmly cruel backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_stack():
+        return collections.deque()
