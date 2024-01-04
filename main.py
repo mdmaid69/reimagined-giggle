40bcdc91f12d970106c447193cefe48341e8fefb
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:14:46 +0000

    shelter less payment travel walk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
