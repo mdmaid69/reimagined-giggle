9dd356e6706e03ff53b020f282f6f88d7949d3fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:20:10 +0000

    several strict whoever probably fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
