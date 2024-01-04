a6ec71726221f4ed439932fe1c8909fab94ae948
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:30 +0000

    convince they form whole stupid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import datetime
-print(datetime.datetime.now())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
