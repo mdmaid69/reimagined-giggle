2ac1dd0c733866dd3336885390839d269ab50600
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:55:05 +0000

    noise minimum love relax thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
