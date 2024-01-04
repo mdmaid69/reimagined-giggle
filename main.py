b1671537a3e1b2762f67a3346507665f64026852
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:56:38 +0000

    admit mine crash fall operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+print(os.getcwd())
