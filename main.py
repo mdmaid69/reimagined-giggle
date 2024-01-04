143b4b5578eae7f3540c11f4c78a7ecc0d6091a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:00:38 +0000

    display month extra use half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
