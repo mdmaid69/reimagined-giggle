a075534ef282540dfb203ab375f092d6938f82c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:33:43 +0000

    tomorrow wind partner waste forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
