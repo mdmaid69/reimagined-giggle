4ecb55422af61398b0faa91d2d3754b2662d5171
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:26:22 +0000

    discuss error fashion represent force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
   def get_file_modification_time(file_name):
         return os.path.getmtime(file_name)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
