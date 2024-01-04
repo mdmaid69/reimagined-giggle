537ad0f9106beefca47d03b560fba3dc58a4ddf0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:14:39 +0000

    nowhere thinkof usual effort long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 def count_words(sentence):
         return len(sentence.split())
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
