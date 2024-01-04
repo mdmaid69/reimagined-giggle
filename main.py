0f6f0ab688382b7de6c6f7382fe49f93b2e27ba4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:37:37 +0000

    responsible runover sad care recovery

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
