18c0c4f67cacc9d7330f32c5dbffa1b1f974709f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:57:06 +0000

    wipe bringforward stupid expression brown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
