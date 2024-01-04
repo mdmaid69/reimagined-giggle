8cab2b8831bad2c1f73a8576b82eeb81695df73e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:10:55 +0000

    oil smart Vv rest war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_list(array):
+        return list(array)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+print(math.pi)
