a342697121505f4ca47144ed4a099977cc59023f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:58:56 +0000

    skin level taste split plenty

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+print("Hello, world!")
+def find_max(lst):
+        return max(lst)
