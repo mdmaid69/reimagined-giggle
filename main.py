32ba9a6497d1521b58bc7310c8fe74d73258f25b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:28 +0000

    dance goon blame chat lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
