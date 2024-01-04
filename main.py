d7a6e373d7d779158c4e0f5ede5a6040f7abd4bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:46:44 +0000

    medium both settle there hurry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
