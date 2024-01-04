2c17f5acc1d57b3fe6be7d9075568d50d2b649d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:13:20 +0000

    count pile rest formal best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def square_number(x):
+        return x**2
