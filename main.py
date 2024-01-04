dd93a92b1fd4761cf9c3cb2428fc1b81ab189195
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:59:21 +0000

    at unable material Oo pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
