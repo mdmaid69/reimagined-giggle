ac6f0f64e02d5c02af28d48e917179fb21115c9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:19:59 +0000

    blame strong think appear everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import logging
+def log_message(message):
+        logging.info(message)
