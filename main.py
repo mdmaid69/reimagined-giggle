732402a65fd1a5cb5becd3f1e0fda9ae87e96a88
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:32 +0000

    oil explain owe correction somehow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
