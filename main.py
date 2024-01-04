a31d83d99b4eb8a38fc40c84838fd1f8f72e864a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:25:55 +0000

    congratulate roof mass treat open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
