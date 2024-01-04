fab6bdd28ce56b78fd0970d4f9416f4b3b260e12
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:30:06 +0000

    round contribute medium bath suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
