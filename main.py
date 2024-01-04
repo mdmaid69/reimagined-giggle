681767a528760088d37d795215bfeab24ddab31b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:45:02 +0000

    office develop organize community cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
