49db12e4b1735f75c598f8ec8b09f6b509f5af1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:42:06 +0000

    foot declaration pick approval matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import os
+def change_working_directory(path):
+        os.chdir(path)
