a01b70a8297833ef3fb4bc21b7a7ad1a5ad7d4e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:50:30 +0000

    handle effort hear count cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_typecode(array):
-        return array.typecode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
