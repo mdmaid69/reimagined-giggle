bf81281f991a6fa569c67df665cc09d9c96ca654
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:58:20 +0000

    principle with gas nerve low

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
