881042f707eb71acf2af5f396e1c12b40c8606e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:37:43 +0000

    free branch friendly delight petrol

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
