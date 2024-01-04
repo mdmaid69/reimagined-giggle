833f583c2344d24e524cdbfb4e16856afb183041
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:52:30 +0000

    introduction sleep through size dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
