057edfceb467cefbcdd58d291cab08fd88eba7df
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:16:05 +0000

    unkind cut difficult choice shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
