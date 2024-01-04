7c622431c8581289512cb6b13d4e438443301d06
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:22 +0000

    definite tongue complete finally understand

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
