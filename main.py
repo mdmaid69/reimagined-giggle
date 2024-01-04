eb0210aab6e495a865413a934e13ca131b0868dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:55 +0000

    whistle damage hang never while

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
