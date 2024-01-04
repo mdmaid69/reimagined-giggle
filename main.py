69bea8274beafcd36c69fed58ca17c507652eb95
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:56 +0000

    afraid probably run motion bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
