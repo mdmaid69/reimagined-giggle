236971fb929f24f75125b789ed9998c7b4253ce4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:06 +0000

    string whichever combination contact professional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_priority_queue():
-        return collections.deque()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
