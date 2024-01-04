fdba1ad8c0771e3fe2b3555c96952118e8c110bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:10:19 +0000

    fasten better bringdown false tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
