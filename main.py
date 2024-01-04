9284312551f09fd072c317ef8e370f7589b1f387
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:27:09 +0000

    takeout dangerous hour comeback appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
