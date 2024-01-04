0ca26a792f97b7fff5ea6d60fed24df1121f2a9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:44 +0000

    damp brave comedown begin rest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
