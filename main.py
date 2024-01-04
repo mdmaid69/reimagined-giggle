a947ec4c4ad150f5d948f7e1e95f5dd5804b7c0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:52 +0000

    about clearly offend public scared

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
