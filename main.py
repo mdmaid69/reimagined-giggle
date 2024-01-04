e51ae6f6f44c2278701bb9585e6a3f12c29e4ebb
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:35 +0000

    chin turnaway tear deserve upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def cube_number(x):
-        return x**3
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
