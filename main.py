42c9ac0619be4af1b509e067d8afc61dd16eaa05
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:32:23 +0000

    hard convincing show wet term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_rectangle(l, w):
+        return l * w
