8b62c31bd05c33c1c08a5b038b6b05c7b7cdda26
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:32 +0000

    nor left corner style quantity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
