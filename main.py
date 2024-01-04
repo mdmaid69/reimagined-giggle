0b9f5ff49ceefb0e068229f08e0c09d8e2d44596
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:26:57 +0000

    trade everything rescue basically remember

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
