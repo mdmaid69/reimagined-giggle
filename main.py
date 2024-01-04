89827cb6ab7efd2f7a51f23a5de6fab9a9fc3d68
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:47 +0000

    safety compare almost beginning certain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
