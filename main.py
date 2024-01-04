86b0e205f97c69e8b1eebde8302ad275509b5aca
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:34:41 +0000

    centre feed regularly closed getback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sys
+def exit_program():
+        sys.exit()
