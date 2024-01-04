06f040f1b4bdfea21b75464580a3dc29273f091b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:24 +0000

    little grade without round practice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_time_since_epoch():
-        return time.time()
+import sys
+def exit_program():
+        sys.exit()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
