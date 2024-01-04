30180d47232a69cde5d9b4eb547c094c3684cf64
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:58:55 +0000

    hurry bother thinkof either guilty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
