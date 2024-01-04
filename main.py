408e9f15d2a1ea8f18cf3858127ac896f3864bb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:42:34 +0000

    ability question firm excellent trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
