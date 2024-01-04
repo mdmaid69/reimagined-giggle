5424f8f4e00672e270ce0380715f9e27c004b828
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:14:33 +0000

    traffic brain frighten normal office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
