8e23fa3cec1d6101f20df4e5394d8040956f453a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:11:03 +0000

    kick public basis operate lift

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
