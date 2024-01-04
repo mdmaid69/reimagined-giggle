7e0be03383af32565e79cebd81f019607d97e7b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:00:40 +0000

    regard break move breakthrough book

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
