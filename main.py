b25c13d233161e4db8bd96b6b2abe1a14438c1e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:32:07 +0000

    crazy 570enjoyable rich shall sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
