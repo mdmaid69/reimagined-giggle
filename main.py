6b39f16173c96a6fdbd3179c38ca63ec0c42675a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:13:27 +0000

    study declare arrange job dish

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
