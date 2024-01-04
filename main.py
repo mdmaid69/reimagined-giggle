f4c3e64cb953d6855a6f6722a9a63ed684693ba4
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:48:54 +0000

    male everybody right home world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
