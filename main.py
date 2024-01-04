9fc137e1b641b966137a8f85116751bb21a06a4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:36:54 +0000

    permission risk thankyou hot exam

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_list(array):
-        return list(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
