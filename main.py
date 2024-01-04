c4aa81b1b6dbcab87fc5899842b75801b1f53d52
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:45 +0000

    account failure uncertain frequent impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
