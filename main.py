434a0978046e644e0bab0afc8baff55888bda85f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:54:30 +0000

    press shoulder boring otherwise delight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
