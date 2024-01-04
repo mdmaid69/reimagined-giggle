b43576dbf4069dad2aa2057e9a422e9ff9e70d83
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:44:15 +0000

    call middle dress opposite direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
