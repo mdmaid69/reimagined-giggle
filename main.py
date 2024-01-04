5197386c0858dbc6681a45ab68d8ba2959dafef4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:22:01 +0000

    climb bush substance her even

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
