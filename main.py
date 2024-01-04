d0e052af2731eae251206c3eb9d9b971fcbcd5ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:11:11 +0000

    dick formal bright burst address

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def greet(name):
-        print(f"Hello, {name}!")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+i = 0
+while i < 5:
+        print(i)
+        i += 1
