69ed15db910ac1cacb245ab64f88f6cd5772e5e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:33:32 +0000

    Tt tie cope their pretend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_logarithm_base_e(x):
+        return math.log(x)
