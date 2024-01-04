782fe1f524e33de8c60acca420864637a1899092
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:55:39 +0000

    short ask fat information mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
