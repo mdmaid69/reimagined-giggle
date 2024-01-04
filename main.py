b66512b460bd657faaa0989ef7e036895c85551e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:35:38 +0000

    off world wherever smoke burn

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
