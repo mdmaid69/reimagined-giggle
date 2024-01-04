f65fcfb7dccb37cc5d046a751a7329cbe77c8caf
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:00:28 +0000

    government shoulder come we ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
