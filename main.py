568596cbb357b630249dc3fed4ddd902d6feb5b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:39:41 +0000

    circle matter frame workon matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
