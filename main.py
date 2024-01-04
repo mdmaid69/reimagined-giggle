3a6474049aef940c9d96d9c295d42fe7bced43fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:47:43 +0000

    imagine show estimate public rather

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
