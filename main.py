f762a0447e9c63e80c0703554f11c27808fbe681
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:39 +0000

    across like any understand back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def get_array_as_str(array):
+        return str(array)
