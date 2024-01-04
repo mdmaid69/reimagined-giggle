90653a16aaf70a785b8de80ffc0079a7d73b2f6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:35:37 +0000

    fair dull meet last pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
