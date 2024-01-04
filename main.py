0ae628abd0a40141ccd9f3f5e837b0d287e7581a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:41:18 +0000

    polite size tell various Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
