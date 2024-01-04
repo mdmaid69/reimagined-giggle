dd398f8ac465ef57553bec3821b3d447ddb3d60f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:47 +0000

    spray deaf trick far birthday

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
