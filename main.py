f4e748e5824b160dbf9359d0c9afdc31a6a35f2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:47:41 +0000

    especially perform become excuse introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import sys
+def exit_program():
+        sys.exit()
