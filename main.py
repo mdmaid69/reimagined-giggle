ded8d3c641fa924559bdec41c097bac44a247396
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:45 +0000

    comedown sky earth entry range

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
