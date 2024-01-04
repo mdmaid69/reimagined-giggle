0fabfc86c172086601a2e85a4526383c7b959b53
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:05:55 +0000

    like action cope emphasize drop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
