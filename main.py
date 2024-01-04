15f7c372d6ab4d3e9509e50488bf672d6376f9e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:42:04 +0000

    fall row steady better claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
