b323ea16b45e4665626fe2f551a161190bd6a7e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:26 +0000

    right mistake shout circle regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
