c100793a09445b8dbc1b7f962a56ba51abe5b72f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:06:36 +0000

    offence into help cap operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
