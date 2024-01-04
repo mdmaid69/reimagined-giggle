453d864264098d09a33174d1611891a36c4a530b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:26:49 +0000

    second deliberately point fork cheat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
