d7ec6ea207d6503366a83665e9b509f127b0901e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:44:19 +0000

    cheque file near pencil floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
