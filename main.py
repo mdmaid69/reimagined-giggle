d0a02bb07e33aa623b0541e71430bf41c3e017e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:45:48 +0000

    thankyou frighten cross love grade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_elements(lst):
-        return len(lst)
+  def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
