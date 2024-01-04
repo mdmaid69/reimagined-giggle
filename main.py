13f77d94014f4d9356c1a96d09a4e51c98c94dc1
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:13:56 +0000

    bringforward upwards upstairs tear ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_list_from_array(array):
+        return array.tolist()
