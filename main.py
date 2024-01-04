3c333fa74f5f4ad15578ee58952e96b77120f51f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:21:56 +0000

    inquire beginning hers hold century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
