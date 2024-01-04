e7f3c94b396170d3f4a55c9246e28b98adc0f2c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:19:32 +0000

    pause perhaps hard ever completely

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def count_words(sentence):
+        return len(sentence.split())
