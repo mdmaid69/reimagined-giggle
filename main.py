ffa824c6179d2b2cf420931e147befe5fb5d52aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:13:14 +0000

    bill release regard blind expensive

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
