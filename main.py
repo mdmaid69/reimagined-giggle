9e0587a3b22aa24ae8ec348c0fc3bde13f9b330a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:25:19 +0000

    watch past slide square talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
