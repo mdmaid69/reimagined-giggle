2f2de9104f46cc0151320ba1351d5137eb10ecfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:14 +0000

    contribute future calm slowly accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_power(work, time):
-        return work / time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def calculate_area_rectangle(l, w):
+        return l * w
