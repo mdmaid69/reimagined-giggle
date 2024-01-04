596dbe91c5f207abcc18bbdf0170fe0187d659c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:46:02 +0000

    clear angry criticize reasonable sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.time()
