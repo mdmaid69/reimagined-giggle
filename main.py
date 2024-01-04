1ccad48db524fab49728310fe55e2228a7a6e9fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:46:29 +0000

    awake reason various beautiful mainly

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
