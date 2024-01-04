7f54366f39661ab499d3ebdcc00380cae5ddb68b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:49 +0000

    slip read immediately furniture answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
