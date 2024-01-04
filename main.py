4b31f5fc4d237f6c518ba4106c8787d9514aebc7
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:51:55 +0000

    distance unpleasant exchange breakin suggest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
