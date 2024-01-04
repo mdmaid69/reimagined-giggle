26efb040a1dbe3c9be408bed484561588704e79a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:14 +0000

    degree it bottom bad mistake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
