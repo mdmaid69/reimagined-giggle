0978be15c85ec8ef0e5868e8772bac6e742c074d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:15 +0000

    body dish call everyone up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
