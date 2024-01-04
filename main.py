6546ce6d122eac476d0f603d13979aed2312f9a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:30:07 +0000

    Yy whatever Ww shout command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
