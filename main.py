1b4ba0567e0f120781ab19e8867273357405a4da
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:31 +0000

    little pollution steer after comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
