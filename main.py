3428d9c08ce8e53e87746242861a04eeffe5bf63
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:43:57 +0000

    understand fit protect 570enjoyable narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
