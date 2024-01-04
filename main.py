363d9adea3965277a23cad4e3910ba3c45945f93
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:51 +0000

    stiff Zz justice splash idea

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
