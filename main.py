da8cf3abe9105c537832f037da524d5fe1c4aff2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:54 +0000

    task several visit to fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def is_even(n):
-        return n % 2 == 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
