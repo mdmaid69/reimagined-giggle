50ff05ec9c834b2dce2004077a837ebe35e156b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:23 +0000

    fast touch question dance closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
