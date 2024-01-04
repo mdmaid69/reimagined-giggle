45593354e67a53137f093ea586a140284fc0efcf
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:05:24 +0000

    warn bet drink callup noise

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
