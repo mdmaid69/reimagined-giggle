a878b39fe35af2b750650c5cb8415f503f605e42
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:56:36 +0000

    borrow pure sink expression accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
