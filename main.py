fb4fba3abc45df08727c056040af9546e6cccb79
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:43:41 +0000

    through politics deaf after disappear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
