8963e1328d4c784cc437b92ad1f0757c957bbf01
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:36:23 +0000

    sea deliberately genuine simple win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
