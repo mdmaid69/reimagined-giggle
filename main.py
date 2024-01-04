acf1128bec9b3cd296a1b1567ff54814089d85c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:11:15 +0000

    process question Mm damage bringback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def square_number(x):
-        return x**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
