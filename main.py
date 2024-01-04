a0e192a4d84fa7b0b5f9f4243ece6a5c3be23e5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:26:33 +0000

    amount this control rob count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
