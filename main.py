fb4f9a01b1f36df826af2741625a9ce5c67998c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:37 +0000

    body mend under live concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_priority_queue():
+        return collections.deque()
