8da886d503cd5ba2e3e2e4f2a1f25be0de995e0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:18:26 +0000

    petrol his track trial of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
