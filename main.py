97140ee8e7660e415d2b4181f8c3fa3833f9b577
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:57:27 +0000

    secret close actually ever minimum

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
