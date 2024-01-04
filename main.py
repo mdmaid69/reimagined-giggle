3518347d5ef495ff2a99dcfd36c2b68eff250ed4
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:45:04 +0000

    miss cum score secret completely

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def get_current_time():
+        return time.ctime()
