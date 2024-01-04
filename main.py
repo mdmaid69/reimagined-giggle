99d7148dc60e6a633d4958f9a0ad3b6bf3fba7b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:52 +0000

    change matter strength thought rob

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
