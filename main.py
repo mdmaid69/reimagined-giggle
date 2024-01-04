a43b0e23b42648a9960c61e105e789d5916d886b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:24:13 +0000

    loss operate apply bag see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
