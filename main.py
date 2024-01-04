014933eb063b8cfa28a01f56b4c2a691f022f8ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:15:11 +0000

    dick willing collect correct area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
