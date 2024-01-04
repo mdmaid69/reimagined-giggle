57c19269e9be233922c73984b65beabc87993c36
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:56:44 +0000

    second takeaway there phone limit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
