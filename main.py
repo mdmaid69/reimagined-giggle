7b96babb0507e6cd37bf0fb560735385aad996bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:39:15 +0000

    fortunately glass bottom burst best

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
