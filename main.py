e8f894b79074c841c7f049266e69355d65408e2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:05:10 +0000

    wet ocean entry my workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
