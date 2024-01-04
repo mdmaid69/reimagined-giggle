bd311a0293f4827b793df2f379562d2a9f994d8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:03 +0000

    butter refer surprising contact Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
