feb88d0a4128bd8733c8a7e7090d9b30b8a46e47
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:49:36 +0000

    cry sick ashamed below cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
