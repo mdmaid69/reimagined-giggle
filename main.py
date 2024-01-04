5250629106c7b4788b6213f91cc40a25d87219a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:13 +0000

    happy tomorrow watch left sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
