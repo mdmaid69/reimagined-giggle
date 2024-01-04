9bf1dcdbabe8ca7dc4fd21e193d7694642c97d21
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:42:20 +0000

    careless raw when ever divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import time
-def get_time_since_epoch():
+def get_current_time():
         return time.time()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
