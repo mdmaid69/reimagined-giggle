a76ca008dfbeff2625542693fe951de3755b14e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:20:51 +0000

    spare method place very pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
