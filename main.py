12d422457f77aea1c8d2957504fbf54076aa764f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:04 +0000

    somewhere instrument face hungry large

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
