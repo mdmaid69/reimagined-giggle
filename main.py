bb091acdd383367443fd17eb4baf161ecd890da2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:40:59 +0000

    their believe arrange split operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
