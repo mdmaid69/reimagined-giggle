6f4b90335478886a94960470eba0361182b46c49
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:30:27 +0000

    pile head sentence charge tired

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
+def add_numbers(a, b):
+        return a + b
