babace2661a71260c7da863fc7fb1a2e230b42a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:17 +0000

    boat scrape another build side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import sys
-  def get_python_version():
-        return sys.version
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
