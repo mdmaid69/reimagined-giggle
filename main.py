11a8d5ebbc1524ab6efb274486d0199f3ffb2f13
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:07:42 +0000

    every development paper future disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
