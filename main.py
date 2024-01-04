ff991aa780cfacb3dc248ea00b7ae536627ef1f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:35:43 +0000

    food find welcome shape convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import sys
+def exit_program():
+        sys.exit()
