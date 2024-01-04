86c9ea9bd21c4d48dce1a80fa70bda170f018435
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:15:02 +0000

    write generous push guilty needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
