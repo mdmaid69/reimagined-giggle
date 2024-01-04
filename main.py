46a10ce4737dc465fbd9be5cf1e9686a37148687
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:03 +0000

    nearly still do hungry respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
