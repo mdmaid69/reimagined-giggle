04e1d4c670c3d48f6a4ac49dd3e5ed2da048a932
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:54:38 +0000

    holdout office stranger image move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
