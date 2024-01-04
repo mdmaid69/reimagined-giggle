47242a4520f004c603f174d2866053d29a53a575
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:57:32 +0000

    long interested development Qq accept

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
