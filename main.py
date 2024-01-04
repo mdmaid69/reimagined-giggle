d0469d2f07a93de24a53413a258211732a96ce42
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:47:34 +0000

    clear snow minority dangerous spoil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
