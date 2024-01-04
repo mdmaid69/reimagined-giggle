0054a0ff837354366e384aa0e873a5085d2436a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:35:22 +0000

    yes shelter length odd tell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
