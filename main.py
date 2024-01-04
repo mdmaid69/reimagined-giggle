c9864452bfbf53e3905e802f8c0e4773374cf6b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:21 +0000

    nigga mess Oo tongue serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_frozenset(array):
+        return frozenset(array)
