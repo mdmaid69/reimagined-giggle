fe98c6df83db6a265c8825e562c24667307e6fda
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:36:40 +0000

    bring before sleep Uu yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
