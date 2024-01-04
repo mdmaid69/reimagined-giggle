44a7f77d0be7c99fd0bf612dfe822070bb82e1d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:22:30 +0000

    report owe towards shout this

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_min(lst):
+        return min(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
