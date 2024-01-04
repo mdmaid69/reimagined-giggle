80d5d6b26402d9d3a68ee7abe748e0f23d3fd0e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:42:25 +0000

    information proper easy large dish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def find_max(numbers):
+        return max(numbers)
