81fec1ad1241d41141abda25c6b55e15c660d09c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:55:50 +0000

    lead why far spring father

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_acceleration(speed, time):
-        return speed / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
