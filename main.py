8093f1a0668b20cf5ad61f58610e68e4389cbecc
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:35:21 +0000

    plan neither vision talk test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def is_even(n):
+        return n % 2 == 0
