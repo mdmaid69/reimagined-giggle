01cde8830897d4408a6160b8060b10c58a4ac6c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:08:51 +0000

    report sort ground possibility keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
