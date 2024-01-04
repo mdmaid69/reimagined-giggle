c115d38d0f358bb5e49dcd734ebaaf4c65f6ff08
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:09 +0000

    square deep law lost affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
