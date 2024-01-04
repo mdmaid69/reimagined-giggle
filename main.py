486b69ebfc3b16cccdf6ac2649a592f497f9f35a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:25 +0000

    turn holiday along fail more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
