3c352cd9710cc2c86bf201024a65f52f821536d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:36 +0000

    report Yy touch work worry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
