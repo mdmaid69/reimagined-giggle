df79c1beb36cfac6e2c6c3d6c8a621430b935b8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:14:57 +0000

    shut flavour section uniform contents

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
