6256a1a00f3a18121b20fca9ade5f8cdab4f7f29
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:25 +0000

    together priority check first so

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def square_number(x):
+        return x**2
