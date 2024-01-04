75e669d7ff5d410eacf4a5b81582fd14807fc289
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:00 +0000

    transparent compete closed act window

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
