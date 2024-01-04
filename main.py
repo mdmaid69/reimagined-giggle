39f274e5f94805468a36865a258d48d255dc4ebe
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:40:51 +0000

    turn apparently such either hide

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
