3a6832d961457706c780f6ab4ce182b9639e509b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:31 +0000

    luck which she geton various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
