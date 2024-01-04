c04df80604584d96158935f42c06377501c5c41c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:25:48 +0000

    organization kind famous rate maintain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-for i in range(5):
-        print(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
