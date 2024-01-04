7bc93b1589e1128812676e9c4d83b3adfa965b51
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:34:21 +0000

    hard club no work callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
