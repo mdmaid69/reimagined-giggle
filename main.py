7cc4bd17151aebe874d7c22f93f37a96a01a6b55
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:49:21 +0000

    already probably port street fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
