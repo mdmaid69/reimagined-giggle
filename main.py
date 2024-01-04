07a88d4682b45eef28f0c4e6bac195f695020f68
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:47:50 +0000

    model peaceful respect direct admit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
