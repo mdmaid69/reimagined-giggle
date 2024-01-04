f9aa9464eacfc86b80e848003dcf565c56329b89
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:13:40 +0000

    refuse she trade weather feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
