13b8fa64bea2cd1969b55c765c38030a4fec1158
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:00:18 +0000

    careful busy accidental clear attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def split_path(path):
+        return os.path.split(path)
