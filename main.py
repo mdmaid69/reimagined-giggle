7f0ae16d1311a29719dfb916c69cfa33e804ed79
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:48:36 +0000

    level turnoff guess begin doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
