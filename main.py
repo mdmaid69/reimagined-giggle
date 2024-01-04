310d99fb2139bb65a88bff69371f9f705b3161c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:20:17 +0000

    accept you noisy develop side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
