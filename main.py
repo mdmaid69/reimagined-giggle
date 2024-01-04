b7947792251bd569a1c8206ad2a793bd7e9f5f20
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:35 +0000

    Bb window total too every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def reverse_list(lst):
+        return lst[::-1]
