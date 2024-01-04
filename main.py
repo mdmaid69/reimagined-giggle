fae408ef2b4e17c041fe975a7993b40496317c79
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:52 +0000

    tend rule calm nor record

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def cube_number(x):
-        return x**3
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+print(sum(range(10)))
