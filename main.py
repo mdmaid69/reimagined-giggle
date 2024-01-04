6e64491b0fdc0bdc1cb3baace29c652855ab35b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:36:57 +0000

    weekend piece correction takeback protection

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def find_max(numbers):
+        return max(numbers)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
