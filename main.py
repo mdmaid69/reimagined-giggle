ef58090342dcc7776fd3271d711f761cfe32929e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:33:29 +0000

    hospital fierce fact crowd worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   def divide_numbers(x, y):
         return x / y if y != 0 else "Cannot divide by zero"
