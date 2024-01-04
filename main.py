6fc9fa022c46d7607096d50b4c4bb937d74739cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:35:07 +0000

    block motion he I callout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
