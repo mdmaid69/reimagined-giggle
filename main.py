95f2cf7654275b29c3dedc57dc6ea26ced7b64bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:16 +0000

    crash content convince ashamed much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
