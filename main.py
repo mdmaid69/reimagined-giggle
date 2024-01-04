092ed5c62f23bb826a9fd07fc909a121d7f2dff3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:18:17 +0000

    spray joke note back Cc

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
