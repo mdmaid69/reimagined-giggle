7d00a821891004058ba9affc1d42b24f04143168
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:59:44 +0000

    spin luck pot cut correct

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+print("Hello, world!")
