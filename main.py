22a7e373bd3b65b5fa0e7425f73647538f94bf78
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:18:09 +0000

    worse job arm whistle shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
