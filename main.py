8fb3eddc838c4807b77231fe9f387a88d54c7164
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:11:02 +0000

    worst high dance laugh properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
