65e0f2135e11db4747e961ad406756add1b4a5c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:30:26 +0000

    face loan myself here telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
