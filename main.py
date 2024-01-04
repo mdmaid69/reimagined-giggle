9638b8e304402a66f979fc70a80786fd1a6591df
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:13:55 +0000

    full loud unlucky practical argument

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print("Hello, world!")
