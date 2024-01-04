87ce80a693567324fd2446eaf8aaf1e5d1600e78
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:48:56 +0000

    satisfied confident compete suit principle

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_speed(distance, time):
+        return distance / time
