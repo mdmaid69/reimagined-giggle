7a2b3b5c19eff3e28afb7cdaac0b2c975bef79ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:23:13 +0000

    refuse design idea positive good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
