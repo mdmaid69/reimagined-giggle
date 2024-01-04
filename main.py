2e4a30ae1a58b8569b92edfdce301a7d4c65123e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:05:14 +0000

    black interested authority hurry holdon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_speed(distance, time):
-        return distance / time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
