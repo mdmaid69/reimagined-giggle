7abbcc2bcebd9a3544bac29cf48572aaea532e80
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:23 +0000

    polite information uncomfortable let as

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
