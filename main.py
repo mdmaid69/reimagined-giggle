aa0da7075ee9343b569f4b2059ea11d870a084df
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:25 +0000

    565end minor survive save preparation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
