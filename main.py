60a84c6db14689689e80d75d605003d8aab5b0cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:02:27 +0000

    condition image trade 573entertainment interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
