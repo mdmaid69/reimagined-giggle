38c64e1d7eac02eeb6ef6f1611bfaf1d604036bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:52 +0000

    through they benefit yet congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
