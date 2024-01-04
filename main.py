cd7cd900bce8e7f333806e1bd1aca10f4b6d554b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:13:44 +0000

    old dish drop cheek formal

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
