480a540050793b55d30c531a448ef2ae2f45459d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:55:31 +0000

    back escape solve train attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
