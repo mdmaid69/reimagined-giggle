36636899dfd2aeca4ec713c779cb43037735e249
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:18 +0000

    off children steam should near

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
