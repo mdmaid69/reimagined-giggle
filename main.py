977402aa97aa76a75a71571c1803611cf5fb976b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:26 +0000

    could possibility control fit treat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-print(time.time())
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_as_str(array):
+        return str(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
