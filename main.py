be95be582f79819ef0b7fd5a918a8211740b0dc3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:21 +0000

    cheat consult avoid drink fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
