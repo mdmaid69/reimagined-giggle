8f26c58c804f81421977a227d7bd45973d0c59d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:03:46 +0000

    sometimes quarter shape curious since

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-def subtract_numbers(x, y):
-        return x - y
+def get_list_from_array(array):
+        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
