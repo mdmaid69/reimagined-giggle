fc3ff3b81a4e7b7437a97e7f3a3af3d21d0631fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:43:11 +0000

    definite cope stop the waste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
