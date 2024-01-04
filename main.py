2045c127d9c4683c458a042b9a8d62f088f973b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:11:28 +0000

    suit dry string suppose front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_floor(x):
+        return math.floor(x)
