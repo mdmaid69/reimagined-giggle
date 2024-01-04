16674c914b0237d7b81d4268db71df97459dbca2
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:06:44 +0000

    takeback responsibility valuable distant pronunciation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
