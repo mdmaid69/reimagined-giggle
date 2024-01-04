e806718a9b9deb537127146a5d8ccac25f3b90d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:30:04 +0000

    sudden comment scream underneath interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
