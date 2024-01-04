079413b8ddebb2f0042f1d4a7e969cb323eb2c0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:40:43 +0000

    floor pipe cheap ticket give

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
