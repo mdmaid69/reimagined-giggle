c5ab1c0d80b252f51c1b6e08cc54de2d5951f778
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:17 +0000

    phone tax hair this address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
