760233b3fcb32fd19a7a9aa9c038e019cb681e51
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:48:16 +0000

    cross border study abroad honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
