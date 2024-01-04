2a181235dedaa882603263457916b8141759dff8
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:23:04 +0000

    motion ours available majority settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        return sys.version
