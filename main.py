72a0324e87c623a81107530ef6ea5bff4f43e530
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:49:43 +0000

    no story spoil interview Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
