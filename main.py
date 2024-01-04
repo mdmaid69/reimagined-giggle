ba396b744b8160eff73cbde7e3baa419d8e93c76
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:32:44 +0000

    protect pretend tough deliberately polite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-for i in range(5):
-        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
