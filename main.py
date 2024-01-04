59d75f37312323719fa4aa590eb08c7844b9a78a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:25 +0000

    transparent shop measure see wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
