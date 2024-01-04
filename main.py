abb4966b359126b276f489f9337380658af7c757
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:32:47 +0000

    experiment arrival this scale rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
+import sys
+def exit_program():
+        sys.exit()
