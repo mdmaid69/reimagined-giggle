bd297ffa58daaa59c7dd38507244356f9338c570
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:30:03 +0000

    worried Oo clever that defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
