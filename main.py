326fd115c4d2e2ccd9be12ea9789ff907388d350
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:16 +0000

    detail last become impressive bag

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
+def count_words(sentence):
+        return len(sentence.split())
