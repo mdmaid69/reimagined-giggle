7e67bc6f3cf0208f517f2a84269d08395f85daa9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:07:45 +0000

    still obvious trouble advise bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
