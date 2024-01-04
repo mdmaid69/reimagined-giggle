600fd804ad3f2df7e5b70fe251c0717884430a23
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:09:10 +0000

    think reply wet Dd Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_max(lst):
+        return max(lst)
