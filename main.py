f3941e8a178825b8bec7d96d01fbfbd540e097c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:11:20 +0000

    promise hungry married delicate such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
