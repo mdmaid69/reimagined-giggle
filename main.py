2d5cbcb549a1804afc6dca62100fecee4892b968
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:08 +0000

    available punishment loud sad eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
