b76ec46e99dfe33a454e9a6248c55238522fb3f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:45:06 +0000

    smoke platform board talkoutof spray

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
