4838516bad8529c463eb3f6fc297fb670e21f41b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:44:30 +0000

    feed contact offer performance stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
