d2d68603ccf8909967a2338c61150a2abdc3f83d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:16 +0000

    organization well where little equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
