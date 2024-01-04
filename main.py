8346f69fa2b39b3a1479eae57e1ae742d6c689c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:46:29 +0000

    steep occur workon nature official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
