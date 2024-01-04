77eee91857ab1cadfd1d43aa9a615ccadeb4e8ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:28:15 +0000

    fold brave special rain problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
