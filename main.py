478541bb2bc1d3754bf0a98efeca1402c0cf4080
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:32:28 +0000

    cruel anything consider summer software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
