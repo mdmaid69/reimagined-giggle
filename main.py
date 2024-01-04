9247cfd76ff86234cd32e5a4f4ed92fb07498c09
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:52 +0000

    generous please argument minor final

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from collections import Counter
+print(Counter("hello world"))
+import tensorflow as tf
+print(tf.__version__)
