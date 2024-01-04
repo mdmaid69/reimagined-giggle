1aa946ce6fd0f243bc3a5576a53db55bf710f947
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:29 +0000

    various cut tonight wash part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import sys
-print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
