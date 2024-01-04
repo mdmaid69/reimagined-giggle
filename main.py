27e9fbd80ad20998672e12ef2e590eae2827161e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:01:41 +0000

    past exist stand anywhere card

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
