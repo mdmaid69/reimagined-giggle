370dd427feceacb508d3da8b9ebc9ab5873e21e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:53:22 +0000

    nose set suggest new below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
