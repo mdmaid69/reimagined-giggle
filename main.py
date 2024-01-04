4496c13c7c3ad734b63dfd40b12c8317bb40fbb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:14:22 +0000

    challenge conclusion succeed rule mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import time
-def get_time_since_epoch():
-        return time.time()
+def count_characters(sentence):
+        return len(sentence)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
