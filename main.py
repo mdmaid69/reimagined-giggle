a8f6e9a08789242c9fa10845ae2a851ff5f25421
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:50:28 +0000

    disgusting fond survive trick Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
