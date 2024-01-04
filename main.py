06114a3fc9ef96d54f6ffc043e83bb2696cd1867
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:37:23 +0000

    hit float protect negative comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
