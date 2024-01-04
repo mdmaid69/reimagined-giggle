f17c706e9d777d9abed3bae57434101d543f2b43
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:51 +0000

    note rough protection approve advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
