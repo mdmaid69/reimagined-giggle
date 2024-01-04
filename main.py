edb5f45bac0226f9e6eb9859cd39c0c230093d6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:59 +0000

    do estimate equipment university fond

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
