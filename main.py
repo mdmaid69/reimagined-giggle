8b1cecf7f9240e6a3a04d32b63b764ed3b9c9f16
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:47:41 +0000

    blind depend pipe genuine building

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
