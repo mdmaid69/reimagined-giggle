d36278855b004f71494aef4c9f0a7d6168c423d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:36:11 +0000

    defend quick wild Ff fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
