a3afbf1027b5f9bf76d4a14f69763011cb933e03
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:39 +0000

    without arrange equal report poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
