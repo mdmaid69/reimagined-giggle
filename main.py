45bc150fd5fd4c8f9984c21d4f5f2fd00127631c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:27:55 +0000

    well perhaps bear bringdown factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
