5657b4dea9f90f431d9cc2789458a9229c49b4bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:09:28 +0000

    communicate any safety shine teach

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
