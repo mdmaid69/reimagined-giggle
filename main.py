3eb5aef029b41e8695e6c8a52a3ed7e00ff7dcdb
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:33:01 +0000

    lie thin 179bite quiet able

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
