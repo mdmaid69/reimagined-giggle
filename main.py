f7267581ae76f63663d2fbc35148d03880541469
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:42:50 +0000

    gap rank worried unfair comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
