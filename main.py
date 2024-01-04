e503ab4536d1e33503abb5bbf6adccfcfc5163eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:18 +0000

    use skill fat until matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Uppercase:", text.upper())
