8cbfaa458f011a9ba415ea417d0eaecdd0ad77df
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:43 +0000

    especially watch mystery his sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
