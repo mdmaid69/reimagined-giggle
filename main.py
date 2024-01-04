7d8be4733eb6a833bec355dbecdc7a6973ee5663
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:47 +0000

    match flight number might neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
