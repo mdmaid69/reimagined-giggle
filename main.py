db196732c54598d35e9478ebd8210425dd6dbbda
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:17:54 +0000

    direct copy everything increase shop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
