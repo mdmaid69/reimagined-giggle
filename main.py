a7e5405d0da60fcbe694b204f88029e9469d3279
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:13:44 +0000

    block 568energy lost niggaacademy fair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def sort_numbers(numbers):
-        return sorted(numbers)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
