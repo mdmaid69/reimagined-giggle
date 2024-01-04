6b51efc72b2e447126436d289225174ff5f7ea6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:53 +0000

    honest favourite blind task line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
