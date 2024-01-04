ccbff4c779e2f23d9d85284da5b9534c9df8e069
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:58:58 +0000

    word amount train why swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
