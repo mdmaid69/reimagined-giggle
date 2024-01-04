76d6065ed3bbc83d6620a4ec07ad62cc2bd7fe96
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:24:22 +0000

    kind remember bound flood giveaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
