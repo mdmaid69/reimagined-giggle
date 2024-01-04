cbea79162edc2595736801914cc0c4d468b935a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:54:30 +0000

    heat difficult which exam convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+print(math.pi)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
