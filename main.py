bd9a04ab0514f501d6ff2aa5ee8545cf940b087f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:35:28 +0000

    smell blue pretty listen who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def reverse_list(lst):
-        return lst[::-1]
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
