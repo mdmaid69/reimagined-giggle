42a8ccf03bd3559546022b086bfeacf33404ceea
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:44 +0000

    workon skill bath lookback(on) base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
