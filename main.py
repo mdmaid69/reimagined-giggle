f80658bbcf3a911238ae0d33d821377e2783aded
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:02:29 +0000

    wave occur morning getoff dress

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
