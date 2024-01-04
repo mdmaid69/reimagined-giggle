368d076ea94538a43d0ff3c15b00c05c35c2f717
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:41:50 +0000

    knowledge our forward late needle

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_union(list1, list2):
+        return set(list1) | set(list2)
