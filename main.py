6fdda7413126336c087edf8f65836bb646e98671
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:05:38 +0000

    test fold under justice awake

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
