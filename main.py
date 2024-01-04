beeb4be178e213b816080f82c859bab765ccc1b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:47:27 +0000

    pinch stamp 566end present tax

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
