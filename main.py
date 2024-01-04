fc24a39dcce83dc5bf915279fdcaf894f1df0996
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:40:11 +0000

    club vote side standard each

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
