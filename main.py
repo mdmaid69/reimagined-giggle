021774b2da364b02f51d02b34a43c2673590d560
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:12:11 +0000

    wander worth false beneath way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import collections
+def create_queue():
+        return collections.deque()
