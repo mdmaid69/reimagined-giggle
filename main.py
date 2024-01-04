2abd0ebeda59b61b26c67d8f12a0a606fa584af2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:30:52 +0000

    become than wherever effort sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-print(math.pi)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
