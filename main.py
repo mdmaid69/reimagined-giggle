19b1fa3e4f2a2b76ca849bb13bf63b31d283b9dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:18 +0000

    time jump block memory production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
