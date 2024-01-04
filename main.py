8f3c6db352170d3a89159a9a387f348f05c12ba6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:39 +0000

    discuss there lock same complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def convert_to_octal(n):
+        return oct(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
