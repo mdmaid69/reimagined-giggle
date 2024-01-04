c62c0a0f5587513ee6cb31eb8037717fe07260cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:52 +0000

    drive tell house while sort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def is_even(n):
-        return n % 2 == 0
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
