56e813fa1a8492f6ee2c7018b6f37885478bd647
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:45:37 +0000

    pen insist free dish enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
