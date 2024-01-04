0cfc11105f365153f9ec6fb33c7bf731830f5a29
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:21:08 +0000

    worst awake neat intend match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
