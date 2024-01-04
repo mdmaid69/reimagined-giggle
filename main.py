648652b76cc888d2bec803a8fa97a12811b46ba7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:33:44 +0000

    bet custom opportunity knowledge cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
