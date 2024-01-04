e56214293e4ae586a01e1558579be147e0d53b13
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:11:25 +0000

    cap ilovedicks to quick profit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
