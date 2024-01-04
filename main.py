fe8830faebf79e3fd2aad4c747267ea8388ff23d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:11:32 +0000

    oil total move manner turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
