549d71e9b86a93532dd28328d79a81a3277df452
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:08:28 +0000

    wish used steer up effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
