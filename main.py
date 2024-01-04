20aa543eda730fea9c04ba1cbfd5f2c3f590ef43
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:20:43 +0000

    continue effort relative pipe thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
