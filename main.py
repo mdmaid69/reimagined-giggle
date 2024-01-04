69bc29cfc36d17b35985a2288840e7063cbec32f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:52:23 +0000

    Jj dead receive swing ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def clear_array(array):
+        array *= 0
