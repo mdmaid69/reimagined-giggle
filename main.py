91e8f81a874b532ccae05baadef1572bf555f285
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:56:11 +0000

    look regard whether pretty benefit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print("Hello, world!")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
