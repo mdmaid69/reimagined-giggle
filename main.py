3a6f7ecff7ac3b385ed1920b27e866867c29e548
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:52 +0000

    capital prepare previous further thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_list():
+        return collections.UserList()
