ebee406c7d365a14f4b36daa1c452df3689a74dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:49:42 +0000

    check row however fortunately regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_list():
+        return collections.UserList()
