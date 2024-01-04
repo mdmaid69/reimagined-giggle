4637d8bf597e1e5cf64512ed9909f983cf62cc45
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:32 +0000

    shoulder knowledge voice actual regional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
