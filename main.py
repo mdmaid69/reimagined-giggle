412ffa1a72e9c2aed55890a6b14579996770e3aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:30 +0000

    make window ear but fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_as_list(array):
+        return list(array)
