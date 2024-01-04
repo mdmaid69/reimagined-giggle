af67d73ad6a0c0ffb24d0660d58b321fe8a74bfb
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:50:21 +0000

    consult whole snow relative plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
