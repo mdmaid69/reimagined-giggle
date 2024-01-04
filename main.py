aee8631852097d32a04581d063e02d96d6ba5d87
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:46:12 +0000

    rather soak control movement dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
