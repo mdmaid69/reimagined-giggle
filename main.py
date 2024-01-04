eed3db5badfb68125a0bcf5a83a28aa3bede2c1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:55:17 +0000

    fire container close we plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
