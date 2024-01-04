90df65653cf004503933c57bcfb7f7869b3335a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:28:51 +0000

    polite hide sting drop with

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def square_number(x):
+        return x**2
