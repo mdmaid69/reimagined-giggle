61c1c4fe756702d801722264d2ec2fab3563e943
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:24:43 +0000

    play stretch stop cover related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
