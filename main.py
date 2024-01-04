5a121b0b30ad1bb9b165d17eed33e5c225336751
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:31:32 +0000

    sight pair well necessary hungry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import time
-print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
