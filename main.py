514eabfe73820646eba5ca6cf44f1c76c78338df
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:02:02 +0000

    nervous direct meeting front touch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
