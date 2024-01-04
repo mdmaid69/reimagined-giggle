9728650e19a6675ceb71e151b19e55a9051b6fa4
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:09:02 +0000

    own argument badly trouble wise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
