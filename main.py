8fa47c787f3fdbd8f499dc5f43be03e52be94461
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:37:51 +0000

    level sit cook leg proper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
