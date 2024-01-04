2fb2dc60332392aa12a0404199c3ff410adbcbff
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:40:50 +0000

    ordinary relation full cry country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
