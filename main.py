cd6d0a8cb089134cb5798cc3e3cc01b9ef8649ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:48:24 +0000

    separate speech hurry drop plain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+for i in range(10): print(i)
