59d5132570f34e36655a392171ed84a72232d91a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:47 +0000

    forward level in snow crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def square_number(x):
+        return x**2
+def count_words(sentence):
+        return len(sentence.split())
