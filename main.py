181038ca016c91c03b96c725a5a0b575a13fc766
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:54:51 +0000

    Ww blame also 572enter and

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
