399fd51e32787ef6c25fec4e1b9a982b3b0e7524
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:03 +0000

    more turndown especially definite answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
