76db27d7347687ebc601fa6262678aa50c23686a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:12 +0000

    swim scrap professional black principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
