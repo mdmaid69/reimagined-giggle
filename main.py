b799705122372b5ad7e06deb53b4f223ea837ab5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:55 +0000

    mistake typical film downstairs plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def exit_program():
-        sys.exit()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
