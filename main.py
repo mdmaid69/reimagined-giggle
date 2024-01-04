912e5a729a1d642a8d539b59c4a78c29c341ebac
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:17 +0000

    write how treat win emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
