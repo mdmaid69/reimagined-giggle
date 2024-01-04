0d6a6f81cc19c5bc77f140439b3f2572721662ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:04:07 +0000

    left choose bear lead completely

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
