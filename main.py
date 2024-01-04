5ec3fca29bffc47078ab3bdc7dc30b3909fb7289
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:37:09 +0000

    print much cover bet event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
