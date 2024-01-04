dc71d51f3d2f69accea659178e72c755707f5197
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:17:27 +0000

    family alone pour pride rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-print(math.pi)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
