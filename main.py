4f98b77964106738ae854397e94af3667de00a97
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:16:53 +0000

    square hurry underneath receive lesson

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
