971a92adb160c1de8b6c606dea39eff216ecc32b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:28:22 +0000

    bread seriously advantage pair slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
