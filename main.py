a8b2bed3e3adc5131d3b3098e1a3419d17dc59f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:22:40 +0000

    near it run refer roof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
