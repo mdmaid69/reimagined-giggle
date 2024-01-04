1d754586f975488ae75b3415d31a84cfe1a7cb4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:37:01 +0000

    Ff build suffer explain present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
