017037e361d9c42d296b92caa6ab84e62af95b21
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:49:52 +0000

    change blind respect matter see

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
