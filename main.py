6c84a95bc23a567c8876db7c9d42eebcf86eb041
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:24:30 +0000

    closely inform style free runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
