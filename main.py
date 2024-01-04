566b7e55c080235f42179a70bbb8ad3e65b00bc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:57:29 +0000

    between follow neck mend burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
