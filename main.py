c2f844c30ff83168c75159dc41e20125f051de74
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:41:24 +0000

    yourself right through flat bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
