41c68e38fa305cbff68c70c609eee512e8b50c3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:50:49 +0000

    exhibition father know sport prefer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def subtract_numbers(x, y):
-        return x - y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
