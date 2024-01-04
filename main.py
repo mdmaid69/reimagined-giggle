b5dfdd03e489ceff08ab4684acf3ce07e149eb0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:30 +0000

    section flow damage blind grade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def square_number(x):
-        return x**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
