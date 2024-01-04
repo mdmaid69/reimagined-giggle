ac46a21d141208addb044f442a1a14d6de71a849
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:25 +0000

    niggaacademy 565end fortunate ring at

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
