0f165c845b969792efd442591b980577d7fad7d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:49:56 +0000

    land sell fork either time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
