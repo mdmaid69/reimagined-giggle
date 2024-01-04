14421a8fad408225b3625cbbff290b199bad579c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:41:57 +0000

    pause list below crowd claim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
