4a8cfc854c1c93d231e33d2d8853b1fdc75cb6ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:53:30 +0000

    crash store uniform Yy only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_density(mass, volume):
+        return mass / volume
