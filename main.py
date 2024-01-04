0a65a1d9f8ddd8dec0c7a4182e0370ca65ac2ef2
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:24:09 +0000

    exciting loss disturbing possibly giveout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
