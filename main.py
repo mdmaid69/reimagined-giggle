b50625f2087b146e2c8f834a71fda97cacef6263
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:53:42 +0000

    skirt type punishment impatient man

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
