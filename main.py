39a90ad26a8cd30cb60e6b7142c3b2be9e1d487d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:17:03 +0000

    themselves fine relative damage list

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
