1ceb29b70f4db4d9df70519fb75de5fb69210f69
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:16:20 +0000

    undo whoever unable climb watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
