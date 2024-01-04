1c3ed7883aa6e68d34c9ae81626d52c108fdeb9e
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:24:04 +0000

    respect lookaround we loss rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import platform
+def get_os_info():
+        return platform.uname()
