671de0b06676b538727d0543a9541313a67a378b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:50 +0000

    drive sure either report stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(lst):
-        return min(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
