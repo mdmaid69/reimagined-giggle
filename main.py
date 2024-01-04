fe2db8b2995aa6881e932ceb0a3a6d17af4ef2aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:19 +0000

    problem turn forgive far tidy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import re
+print(re.match("h.*o", "hello world"))
