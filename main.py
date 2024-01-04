155eeabc73213b386894c98182e6dfbb60c2dcdf
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:34 +0000

    error pride food right couple

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
