c85ee34bd460eb9874595473c11df1fd7b423c9e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:36:32 +0000

    communicate make bother shopping waste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
