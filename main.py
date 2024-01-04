e729d11a2df5c5ba1132411aa6712343a7693980
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:22 +0000

    opponent convince record avoid sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
