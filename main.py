3979e1ddf3712c2a0a6546a35bc129c17b8e2499
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:20:37 +0000

    information bye oil possible base

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
