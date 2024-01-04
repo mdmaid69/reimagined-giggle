a8254045024f78ab9657e0f973702161c48c7026
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:36 +0000

    keepfrom public string fair turnaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
