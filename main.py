f68e10a268440d5484a16b02cf79a641598e2653
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:43 +0000

    freedom every question punish guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
