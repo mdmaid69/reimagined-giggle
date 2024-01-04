ffdb2ee36f21e652d5dfefb18d5500f691cfee51
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:13 +0000

    way forever own ours gradually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
+import time
+def get_current_time():
+        return time.ctime()
