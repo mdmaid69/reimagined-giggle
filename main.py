9c472adfd781c4b2641a2b998e1d6a2abd9c7ef0
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:46:40 +0000

    exchange must length profit record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def convert_to_binary(n):
+        return bin(n)
