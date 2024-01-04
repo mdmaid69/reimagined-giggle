f42d37cb6e33d56d27dc932c3e7faaae03ff83ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:19 +0000

    fit almost ground ear even

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tensorflow as tf
-print(tf.__version__)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
