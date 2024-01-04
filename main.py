07f2bbe080b90df60f6ec094715cd6de40495aea
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:07:49 +0000

    criticism respect pity basically reach

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def square_number(x):
+        return x**2
