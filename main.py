c06916c2f6a574b5e188c4462d051b9efeeda08d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:55:56 +0000

    rush load above once offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_set(array):
+        return set(array)
