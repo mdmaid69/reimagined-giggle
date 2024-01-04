e1d597d51ef68bfc40f40b824078aece75463500
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:41:24 +0000

    keepfrom dream public image common

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
