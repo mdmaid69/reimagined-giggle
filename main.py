f5168c59df3759f336420df5613d181eb46784da
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:31:13 +0000

    shrink kick bush single path

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
