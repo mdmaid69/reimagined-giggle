8e297692b55952fd5e8cc6a5063b378501a1c65a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:37:24 +0000

    responsible fast release use piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
