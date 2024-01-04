ae9392640700e36a84839d14cb2f726a5247fdbd
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:38 +0000

    dry fold protest large open

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+from collections import Counter
+print(Counter("hello world"))
