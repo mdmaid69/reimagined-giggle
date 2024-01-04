e49ef48b1111c557982459b707f4151a782b1ae8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:41:16 +0000

    range out worst whom emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
