02e6f2fccfb2997e2df353d6d425cce2a7d9bf5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:34:01 +0000

    Qq development reply stop chase

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
