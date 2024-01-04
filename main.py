626fdc09e86953a55ded4d06cfd7e450d1c9eefc
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:59:11 +0000

    design lay long work nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def multiply_numbers(x, y):
+        return x * y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
