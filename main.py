a88d64fe5e4e13295fbbcf46780d8dcf748eb7b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:23:31 +0000

    meet worst interview train shock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
