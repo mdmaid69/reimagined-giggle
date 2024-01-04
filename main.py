3b6c3d4f79f3cec02648e07f5f16b34898a9af0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:28:02 +0000

    later intelligent medium seat unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
