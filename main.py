1f551194954e6b04cc3aeb061810ccbc61e29e46
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:49 +0000

    mainly with shock suddenly emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
