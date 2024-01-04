e993d9343ad996c17784c3281470ff1d722e585e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:59 +0000

    giveup basically fact rain next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
