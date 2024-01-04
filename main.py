dd5656bb5efedc7b498e8bb9bf6c70e7111126d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:39:20 +0000

    interested dinner pump wet fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
