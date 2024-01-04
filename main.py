ced1b64ad421ce0aa0e8d673f5b2a605ab14a5a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:06:50 +0000

    offer card cross ahead below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_speed(distance, time):
+        return distance / time
