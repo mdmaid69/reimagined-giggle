a9d95a1cf412c18177eb07c47de529a1df3f51a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:15 +0000

    uncomfortable justice record thorough exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
