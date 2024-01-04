422e383e42481ce208b8286fdf17688825b43290
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:53:59 +0000

    visit criticism error impossible pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_array_to_string(array):
+        return array.tostring()
