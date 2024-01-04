59e9e9e0d517e132e2302283d952b51cec63c670
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:46:59 +0000

    convincing activity murder press press

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
