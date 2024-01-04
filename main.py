8d6ed58032fa41c1aaca6e08d120b43f92b46a6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:21:07 +0000

    accept beside beautiful definite book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import os
+print(os.getcwd())
