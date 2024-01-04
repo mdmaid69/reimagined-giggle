7ae0aa79a81d1b1a6424cb5d3fe08ecea31f3aac
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:43 +0000

    course regular steer throughout wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
