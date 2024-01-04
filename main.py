0a1495d2ab0b9459769ea054b63f9e24579d1c45
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:52:11 +0000

    lead different prevent border offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
