a9b9a5bf65a44b6b24b9eff862909b5c5f3114cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:27:51 +0000

    advise school damage string keepout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+print(math.pi)
