a38f544a8a821e02ed1b6362f6226a0ad7786e4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:33:28 +0000

    regard write straight measure spray

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
