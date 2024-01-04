e1af2d6e98dcd727c4ff518e8df4e33687c604ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:59:28 +0000

    else live 179bite comedown taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+print(re.match("h.*o", "hello world"))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
