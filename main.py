0f5575789b9b182026c5c462f29a42c5fb90ed98
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:11 +0000

    job hit switch length mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
