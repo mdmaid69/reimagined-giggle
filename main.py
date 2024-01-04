5b48a636efcbce04c83dee4db1a510dce29a5716
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:42:16 +0000

    you slowly deal include question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(numbers):
-        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
