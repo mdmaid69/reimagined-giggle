8297b0048002c724ffb32af7f79d9d4e274de263
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:32:11 +0000

    crime respect turninto consist recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
