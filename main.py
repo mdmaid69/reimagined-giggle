40a799306372f3467157605728c3dfbf0f25d030
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:08 +0000

    laugh target schedule dead side

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_work(force, distance):
+        return force * distance
