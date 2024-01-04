aeb3624d424220b664894b8693e70bd9d106b2ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:28:10 +0000

    history manage instead excuse yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import numpy as np
+print(np.array([1, 2, 3]))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
