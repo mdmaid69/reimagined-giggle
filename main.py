fb610a50ec32e3e0f049277e58141603d0843376
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:04 +0000

    nurse community same develop male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
