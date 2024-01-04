c94113838e44800b2774e5b8457ef0fe4a1923cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:41:43 +0000

    foot sister congratulate rise tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
