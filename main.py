ffef1cc570fd6e0ad97e4bcdded35959d8d2f10d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:24:52 +0000

    few question early tonight skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
