1cc7465c9aee511c666d0ff9b54529c05b04d753
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:35 +0000

    pretty 573entertainment welcome neck storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
