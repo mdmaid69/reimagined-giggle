1ebcd3e8f97273cdc4317307837d9e13f33128a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:58:49 +0000

    wheel this appeal discover ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
