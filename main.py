01557819d06ac1efe8982ff6f0ff79ced24a8afa
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:38:02 +0000

    behind card money make pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  def is_odd(n):
-        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
