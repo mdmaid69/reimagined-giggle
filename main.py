47bd6d8e95e46e51c82cce247ba22937cb66b320
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:19 +0000

    Pp knock keep place if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
-def split_string(pattern, string):
+def split_by_pattern(pattern, string):
         return re.split(pattern, string)
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
