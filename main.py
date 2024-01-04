1f966da5534b4520bc525abde32bd6808c604dc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:55 +0000

    sink past advantage bread tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
