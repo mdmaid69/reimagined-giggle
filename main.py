7eac270174592afc6ce6429cf4f89b34cac79462
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:17:09 +0000

    improvement lot provide repeat label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
