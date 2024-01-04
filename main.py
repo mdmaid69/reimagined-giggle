032b530c58879892bf74a4d74a82bfc36f5623b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:00:02 +0000

    shadow why share partner education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
