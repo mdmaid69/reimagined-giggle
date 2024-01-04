99a82131a340e8e573f96b929d4a0ed335fca57a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:07:51 +0000

    taste unusual hope innocent spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
