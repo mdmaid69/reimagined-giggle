86f8cd2a2f497f016cc1db16f2aa9f49f7ed6786
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:14:28 +0000

    kind colour speak pocket angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
