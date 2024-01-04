7ffd40363be4f0bbc263b92d1acb0e48f910574f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:04:52 +0000

    light gentle expect break attach

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
