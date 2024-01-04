6bcc0e11a361b128a18bf888a9bc4f407a8c85ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:40:28 +0000

    guide criticism free profit why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
