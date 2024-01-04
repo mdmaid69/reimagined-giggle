2217df97ef32541cbd510acf96b3e5e101669e43
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:33:05 +0000

    frightened building far main cut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_area(radius):
+        return 3.14 * radius * radius
