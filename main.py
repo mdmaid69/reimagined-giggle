1ed51ff65f8dbc676ecc2eab24689caacb59cb7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:36:42 +0000

    task return announce school mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
