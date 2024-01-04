6fd74311c0c0d9653f177aa0017d60912f435281
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:11 +0000

    leave skill phone why top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def clear_array(array):
-        array *= 0
+def get_array_item_count(array, item):
+        return array.count(item)
