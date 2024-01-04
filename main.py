18840b386a0ad59cb5469ec01d4eef6eb0afca77
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:19 +0000

    giveup getup Dd sleep heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
