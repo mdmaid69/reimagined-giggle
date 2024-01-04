ab07d511ad50d667a534d76a772c804db3eb71f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:42:02 +0000

    name payment round wise characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
