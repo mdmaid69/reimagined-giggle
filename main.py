ced3f801a99ee732f9d64a773be3bf0fe7c29869
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:46 +0000

    bang fierce pressure set lead

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def split_path(path):
+        return os.path.split(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
