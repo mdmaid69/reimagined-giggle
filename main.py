ad7fd5cbe96780873e03447845ba02a08badb85c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:09 +0000

    whole punish himself organize steady

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
