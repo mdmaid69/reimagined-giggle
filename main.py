365239b6268e12038db28c1c3a896ab33b9d1072
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:51:50 +0000

    holiday holiday complete especially breathe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sys
-  def get_python_version():
-        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
