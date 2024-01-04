b9bd0fda5353e48567bc4110b4fc0695d85867a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:08:52 +0000

    wander shop quick welcome serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import sys
+  def get_python_version():
+        return sys.version
