9fc8235d5c4a99b5d68ca344a8d886d825cc8c93
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:34:00 +0000

    doubt recover fear natural shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
