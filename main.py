652219d0ff159d8aa875643b1de1f6eb3695686f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:57:48 +0000

    deliberate geton shout anyone careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
