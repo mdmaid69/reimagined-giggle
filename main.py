056b6776b1a23cdf2dfd3068621061c6585b3255
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:38:46 +0000

    actually delicate sky relationship birth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
