1172fa7f89e9f921c1aa5f12ae8cb86689273fb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:54:41 +0000

    bar press send rush window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
