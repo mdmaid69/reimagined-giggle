09f07409d5a7b972b3c715669291a4777a2945de
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:55 +0000

    discussion wave border club far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def subtract_numbers(x, y):
-        return x - y
+import math
+print(math.pi)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
