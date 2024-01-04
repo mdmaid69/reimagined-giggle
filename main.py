f57ab3838da54944beced56f0626f638a6b52a07
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:55:55 +0000

    light 572enter high search impossible

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
