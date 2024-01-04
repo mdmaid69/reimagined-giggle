aec14dd3f03e8c78c350abc14659cfe81542a246
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:06:58 +0000

    before fortunate sensitive bottom opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_bool(array):
+        return bool(array)
