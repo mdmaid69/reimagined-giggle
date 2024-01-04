05b7b67abd47836498703acf0a5334fc2ae82074
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:25 +0000

    nose car both dream uncertain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
