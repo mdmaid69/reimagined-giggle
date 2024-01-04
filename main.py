e931673196d0a406b249d4df12312185faaf0aa2
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:54:29 +0000

    minute pour turnoff chat respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
