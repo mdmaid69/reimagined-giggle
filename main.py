5ec19ba0506d161fd127d722512a2bad7df4c27a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:04:23 +0000

    artificial too able soon fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
