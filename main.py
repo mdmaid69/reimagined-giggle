1f764eeb8e60f73a072d4a82084492f91d8c5c39
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:37:33 +0000

    back use wing close lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
