6bcdc9580e965e1b4bace2f8a13b082d7807de73
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:09 +0000

    after bottom runaway pretty authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
