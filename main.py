431fafe82d01924e29be9dda1ae04c1e49d1911d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:19:57 +0000

    idea previous above band strict

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def is_palindrome(s):
+        return s == s[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
