db2ab3923ed06713f986c715279435dac20ce607
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:26:11 +0000

    freedom month mind already stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
