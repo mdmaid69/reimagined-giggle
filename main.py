68cd4b639f6fb8ea5ca51983eb053f4bca30d03b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:14:30 +0000

    cool buy tax shallow thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
