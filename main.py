6910ef5fea1c020699812233e36eb88369e9c36f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:18 +0000

    length attach lend tidy action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
