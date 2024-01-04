82ae46536156e11d50d3cb3b38e62a791f195d4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:51:12 +0000

    formal giveaway government part expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
