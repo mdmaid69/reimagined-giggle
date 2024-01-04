5085656404f04f843e5ac25aa8882a040f46873d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:56 +0000

    partner theirs material exciting emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
