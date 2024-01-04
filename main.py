277c0b836653b52d473ab6261faea3973b1d9457
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:58:13 +0000

    argument refer cause convincing type

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
