214da98eb833a536889e43e9446dfd32f78d66df
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:53:41 +0000

    accommodation view embarrassed transparent both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
