e600a7f8ce817d763714cd81baf4a8ad198a4a9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:37:32 +0000

    period fasten neither 572enter cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
