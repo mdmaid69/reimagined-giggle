f51e78428c202d75d73c4a0cc04a4251870f8403
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:02 +0000

    decide pleasure along flow bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
