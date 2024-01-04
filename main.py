8a230fc957770fea898d46befeea51851a54cfd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:24:28 +0000

    welcome creature deep platform future

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
