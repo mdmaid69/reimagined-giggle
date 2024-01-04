350967b6f6e0b7a06c0a6e66f52965c51be24cc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:21:45 +0000

    fasten deliberately precise chin worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
