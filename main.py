ff22236affa98ac6439fdaa2297fe604f6fe3beb
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:22 +0000

    study turninto fault care pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def append_to_array(array, item):
+        array.append(item)
