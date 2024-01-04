8d86e8e43cfdf72fd3cc7f720799c59ab25ea08b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:22:23 +0000

    objection healthy gift book pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
