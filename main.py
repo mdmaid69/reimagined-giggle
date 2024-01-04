2e31a7cfd1e4fa765f8be8c13d5facbb6a2c8096
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:11 +0000

    waste win suit bend rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
