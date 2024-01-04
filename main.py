83a0460db7512bbe885e43ec40c5975496d3b63e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:21:25 +0000

    word shade because unlikely sudden

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_exponential(x):
+        return math.exp(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
