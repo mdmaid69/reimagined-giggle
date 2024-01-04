775fdf50153e6604387084e2da4aa3c78e6a0394
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:00:10 +0000

    needle breakfast error party reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
