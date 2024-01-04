6cb3ba6552806111dc7b9137ef041d0b19e88705
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:04:34 +0000

    prefer lean ground estimate turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
