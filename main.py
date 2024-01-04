685c901ad54682ba4eb001b843db7b1f8ac9e3b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:00:05 +0000

    may depend week become bet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-print("Hello, world!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
