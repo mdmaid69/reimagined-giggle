e86b1a69f214e2e0732626b7a588090176c8af1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:29:42 +0000

    demand takeoff crowded brain central

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import tensorflow as tf
-print(tf.__version__)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def sort_numbers(numbers):
+        return sorted(numbers)
