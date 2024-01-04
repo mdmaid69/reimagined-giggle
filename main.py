925a1663dba1097e7f30216a64f89e819bf88f56
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:22 +0000

    noisy science disgusting tread passage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
