1ff7dbe564e8a9029b9dd12b8bfcecb2bea3b7b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:39:16 +0000

    emergency partner future bring petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
