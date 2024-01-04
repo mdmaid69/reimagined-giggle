db16a10027165b1c234dd9a524f548b5e846d870
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:37 +0000

    physical now fun never poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
