23ade90a2bbdce5701d8393e61be07bd86d82db7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:05:44 +0000

    force goup defend turnon sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_stack():
+        return collections.deque()
