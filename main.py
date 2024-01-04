32a6bf4bc7ff75cd343addd1fd9035917c843309
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:42:44 +0000

    Bb Vv light confident spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
