ebc6bb3977191243dbe6dbc4948de8ee8a401c2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:19:10 +0000

    discuss callat character replace neck

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
