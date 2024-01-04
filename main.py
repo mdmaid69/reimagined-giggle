9729995a580eb835881afd2dd486e51efdb171de
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:15:14 +0000

    frame plan floor weigh birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
