356bbd5dbdbbd07daccf4c461c9ba1cc3d6d8402
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:00:15 +0000

    temperature walk pronunciation praise opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
