cb0c6faf53590d68ddcd0916a9ba9c4fc005e051
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:19:04 +0000

    pride badly somehow on order

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
