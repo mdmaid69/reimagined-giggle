23f6dc6067cb169d8941743f1bc603b2c2d5688b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:53:22 +0000

    official order whom wind mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
