ff7c1e27cc4dc44c3c82a2637976eeb432283d21
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:44:21 +0000

    sail limit wind fasten past

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
