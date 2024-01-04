cb19eff9526a2ee64861522fa258b2577d10656a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:27 +0000

    interview scare doubt pay pupil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
