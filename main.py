5b8ced3a140b784fb014396e94c069ab6072ddaf
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:46:03 +0000

    concerning later past awake uncomfortable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
