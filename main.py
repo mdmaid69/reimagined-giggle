cb4c50689ed31a8910e7150434543d354041a0ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:04:25 +0000

    march floor help destroy whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import sys
+print(sys.version)
