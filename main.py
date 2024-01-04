83729cfca8e3afc4c75be9c6a4fcb9ffd628a71c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:54:06 +0000

    comeover theirs direct breakinto original

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
