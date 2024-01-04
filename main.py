6a5449e4f73a9b1cb453bae2688558971e14ee42
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:20:48 +0000

    rain hate justice rubbish bank

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
