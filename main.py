53c4148a0c03f02e7a4bfed51f16e05b449d0112
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:01:16 +0000

    space hand conversation date cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
