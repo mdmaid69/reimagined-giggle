1acea3c75920196efb00237d3d0d167788322e1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:09:41 +0000

    different unhappy false equal could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
