c589af67726f085f5665d4fec84c1598ea712ff3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:37 +0000

    low pass they advanced forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
