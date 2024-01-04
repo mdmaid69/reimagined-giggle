2ed481f49eb317283d20211bcd344515f818ebd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:34 +0000

    this form injury single responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
