4ece52b93d1bcf06a20f13be26063167cd92530f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:26 +0000

    worst ahead judge Ee fortunately

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
