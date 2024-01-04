acc5c16e2eca75dcff3e65606829c05e2710aaa7
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:21:41 +0000

    walk soft turn change introduce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
