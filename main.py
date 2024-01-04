f3832155c6896d619abe5626b29294bae82c85b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:02:46 +0000

    quality may risk crisis obvious

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
