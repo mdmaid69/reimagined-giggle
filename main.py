d5c8ec3737833d65bfd7e0ae0baf048a9bfbef0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:44:37 +0000

    convinced minimum never prefer protest

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import re
+print(re.match("h.*o", "hello world"))
+import os
+print(os.getcwd())
