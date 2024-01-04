eccbc0b882b17ed475c3cf1f8a4de88d4060d86f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:54:23 +0000

    only black judge keepaway line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
