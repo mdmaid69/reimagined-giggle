15a562fdf5b92506bfaaf001f30c7f6f39f30816
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:28:05 +0000

    Ss little brain convenient effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
