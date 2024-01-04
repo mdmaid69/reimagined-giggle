d244c12eaaa14d669426cb897b92e829fb00d363
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:26:47 +0000

    argue contain after shopping country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import datetime
+print(datetime.datetime.now())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
