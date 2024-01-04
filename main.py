a8fc02e10535dbfd5b8bddbd1461fbb5de723997
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:29 +0000

    union fly bored copy fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
