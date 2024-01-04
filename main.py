39e258d784f52c22abb9a36014719b7d0ad57761
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:03:23 +0000

    shall produce content healthy cruelty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
