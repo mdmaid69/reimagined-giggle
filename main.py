8d4043f1ec89f0413f6917da6f00224d8440f75e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:52:47 +0000

    crowd copy regional song objection

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-for i in range(5):
-        print(i)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import sys
+def print_python_version():
+        return sys.version
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
