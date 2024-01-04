3088330a30efeef9d7cbc1564adb3134acd628f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:19 +0000

    rub pure house bet custom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def clear_array(array):
+        array *= 0
