fa4df2f5af8a7880b82fa55fb4552c479ddfca8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:57 +0000

    otherwise professional square check sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_min(lst):
-        return min(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
