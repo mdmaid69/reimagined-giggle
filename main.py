6b1373f7e51907a07bd3a1e88818ffa35022f6a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:08:59 +0000

    stir apart paper or opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_length(array):
+        return len(array)
