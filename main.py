e5b31fac0bfc53e52a698f3d33c2356f89dd109c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:13 +0000

    load expression give repeat report

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def add_numbers(x, y):
+        return x + y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
