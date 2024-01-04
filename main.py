1adf55ae0803299656d3bbd42d70291c7937e34f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:24:18 +0000

    unless sort used talkoutof similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
