dae274114856965b52b9888d070ef301c60f5a40
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:19:53 +0000

    warm float foreign follow certainly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_interest(principal, rate, time):
+import tensorflow as tf
+print(tf.__version__)
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
