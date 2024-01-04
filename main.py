b2fa815010e6e7bada85fd68d4ef64be95b15794
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:51:47 +0000

    attractive uniform gooff hardly crash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
