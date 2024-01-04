0a39296a80ac8725e17b45b39a92c3f29c245fee
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:27:21 +0000

    sting bring common yesterday benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
