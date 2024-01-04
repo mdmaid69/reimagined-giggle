aa4e199bcfcda034419feb64fa6e8197176240ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:11 +0000

    work backwards couple snow rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
