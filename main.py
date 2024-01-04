de1a0e82ef6f89edfae71153f416a59844a3e51d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:51:43 +0000

    anything invite chase connection reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
