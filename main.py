f4c1151569dbc6328ca98da9fa638d53bdf55b34
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:01:22 +0000

    close guard skin image anyone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
