2456d01584dfa721f7cd99a03f0a0d29ebe243d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:27:03 +0000

    broken lean prefer fail return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
