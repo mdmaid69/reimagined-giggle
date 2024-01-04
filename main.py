1b3991cc0c311c969e938937e11cc294e381fd4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:09:45 +0000

    confused relationship join delight care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
