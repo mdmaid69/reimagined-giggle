10e0347b3be3ea9a131af42a72ebe0714e9bd049
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:06:55 +0000

    possible arrangement many speed convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
