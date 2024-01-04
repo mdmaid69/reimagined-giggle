46dde7b7ea4ceb7786f735b24269e03e4f01c9f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:45:13 +0000

    rough over keen shade bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
