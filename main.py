a477c40d8de30122aac5d7c5d90604d69e39ff4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:45 +0000

    question die tall state group

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
