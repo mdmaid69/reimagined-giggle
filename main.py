101696c16cc16a8c9355d41d79a1e84e760636eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:32:07 +0000

    kill cross touch serious main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def convert_to_hex(n):
+        return hex(n)
