3f9bdd56608f0922fd738a5baa5c2f4f42ba9f9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:05:40 +0000

    satisfied continue upstairs swim otherwise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+print(sum(range(10)))
   import pandas as pd
   def write_to_excel_file(file_name, data):
         df = pd.DataFrame(data)
         df.to_excel(file_name, index=False)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
