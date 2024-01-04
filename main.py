7c15f5eb7fe8c766cfa49a8ffca5f9dade8c7c6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:15:47 +0000

    whistle build 179bite nerve example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
