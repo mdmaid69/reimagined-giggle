aa34bb3ea53585aae00e3508d4acdf25c80c2466
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:13:05 +0000

    wander design freeze petrol leave

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
