40bfd0aaac9616a756531f2fde7149de1f505701
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:29 +0000

    field error pile someone regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
