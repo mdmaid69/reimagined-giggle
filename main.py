52a1c4b1b2b31d20cf9426a5bdf998def1c607e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:13:01 +0000

    unlikely body purpose her back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
