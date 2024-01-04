4775ac2e7a9813a5fd0588d59cc81db1b1b7b6b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:58 +0000

    refuse perhaps demonstrate block mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
