755c52b069a7bf505a42d27b0f4c60140c367ea5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:11 +0000

    correction breakout man available support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
