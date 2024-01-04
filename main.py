806cd74c039b3e52437c572b608491a0b5e1b4a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:36:01 +0000

    hit warn while bridge arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_list(array):
+        return list(array)
