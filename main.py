010ec7f5a99a09f4e65be9a1150a273d83d3a21d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:12:20 +0000

    advice nonsense hand value light

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
 def calculate_speed(distance, time):
         return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
