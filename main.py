b239518bd57d8daedfa13e1e6fcd73c8d4ec8e4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:14:24 +0000

    pass brush contact open lift

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def split_string(pattern, string):
+        return re.split(pattern, string)
