75992be7186624cbf859c4e966bb0563200d6cf5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:04:46 +0000

    purpose Zz somewhere appear bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_str(array):
+        return str(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
