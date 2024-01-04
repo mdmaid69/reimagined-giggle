6af554d223964ea56232379cc18ca13391956842
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:07:24 +0000

    promise Ee before since image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
