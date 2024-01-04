9bb84380fecc99cc9c3912eeadf85f64d5e1e30d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:14 +0000

    connection ask introduce nowhere afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
