98872eb3e10b3769e3787f315b063bd1fc68543a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:11:10 +0000

    case relief neat range study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
