d771d44fd67dd269c8babc99e7c9839eda399819
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:55:59 +0000

    detail smile brush ring train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
