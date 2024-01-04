28fb2bd2a0989047b485ef11d5907a82993250ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:44:45 +0000

    year solve female workon classroom

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-import collections
-def create_user_string():
-        return collections.UserString()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_itemsize(array):
+        return array.itemsize
