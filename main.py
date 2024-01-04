839adf1ae702bd644d4801f83375689ff0b27760
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:38:13 +0000

    marry scream raise run leg

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
