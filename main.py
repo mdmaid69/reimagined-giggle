4d28b8a3ff3bd2562a4cb866eb90c51aab529133
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:12:43 +0000

    force bye late judge congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
