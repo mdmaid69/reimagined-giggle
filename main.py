df2f664c5898b61ac5999621dac81fb47827f8f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:45:40 +0000

    section bunch break treatment leave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import tensorflow as tf
+print(tf.__version__)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
