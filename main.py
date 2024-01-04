c136a17bece06d83ebb772db39bfcbb83ae5dea9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:16 +0000

    worry maintain limit mix mother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
+  def square_number(x):
+        return x**2
