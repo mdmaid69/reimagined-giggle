df2c92e1c6016b0e5c764e0074b5b6f94882eeca
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:02:35 +0000

    steep wander attractive yes partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
