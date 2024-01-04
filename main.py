3d527d8c71aafdbd1e406626c8fc40b1454aac6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:39:17 +0000

    contents side design country off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
