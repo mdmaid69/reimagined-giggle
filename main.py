bd14d12c94f4f786a82a7f352e0328f262f59191
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:01:38 +0000

    tone effort least delicate safe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
