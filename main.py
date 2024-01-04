307cbe16269d06c3ccdb1b455f91f806b2c05480
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:12:03 +0000

    will rise lock raw behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
