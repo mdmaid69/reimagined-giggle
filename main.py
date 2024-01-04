ca3b8420b463d6c4a60237a9ab1731d23100f0e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:25 +0000

    type across match admit clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
