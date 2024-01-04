28a1cb3862517a04796f11e933d498c9d2aaf4c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:58:51 +0000

    invite grow taste ticket cover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
