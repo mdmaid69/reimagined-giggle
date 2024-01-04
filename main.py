bad9086c5f86102d8e85be70b194c39e291282e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:44:38 +0000

    aim 569enjoy otherwise regret stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
