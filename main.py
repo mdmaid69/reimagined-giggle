d5e7201b0749a80ddc6dd356b5704dc68ffbf11e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:32:57 +0000

    leg behaviour over quality lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
