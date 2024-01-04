4f03c3f154f4ab35a92e695d63a33bc61c5a6f36
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:59:27 +0000

    late voice top bye authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def count_elements(lst):
-        return len(lst)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
