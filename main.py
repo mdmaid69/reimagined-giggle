a3410b58a499cf8f6577db9f2844d62a78128286
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:21:23 +0000

    healthy our cause my get

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
