4e3dc6f0c9878aec2e3aee1ecd2e0a0727148a93
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:47:53 +0000

    proper lend fetch stare why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
