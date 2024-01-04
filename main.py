3af3ac4a95cfd00caee5941d7b8f3d3d6158f5ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:28:05 +0000

    impressed home good tomorrow meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
