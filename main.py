e2ec4c857f2643d90b310f03127679a2c6f3095b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:48:44 +0000

    bear recover label compare create

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
