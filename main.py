e5bfa10a1a8b3c0dad286d2c093c2caf281e7e32
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:50:06 +0000

    probably will figure sink partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
