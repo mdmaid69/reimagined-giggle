1c0ede3af99b7ecaf5810cdef9142e9b2882993e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:24:42 +0000

    settle forever sick runout use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
