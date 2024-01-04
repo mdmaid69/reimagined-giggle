63857ffbc82475d2fa194f799b72aec1f852802b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:56:08 +0000

    nervous public pause have land

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   def remove_duplicates(lst):
         return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
