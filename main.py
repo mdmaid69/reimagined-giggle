5ecaad15d3f7aded37ea85dbb1aaccfaedf4b856
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:31 +0000

    Uu board bound piece bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
