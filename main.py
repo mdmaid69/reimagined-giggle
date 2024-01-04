f90e06efab7d2697c59c86f017bd88671de9185d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:15:56 +0000

    anything deal makeup before understand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-print([x**2 for x in range(10)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
