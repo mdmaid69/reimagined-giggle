3ce7efbec2dee36947c8b4bc3d8b940a772eb406
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:44:52 +0000

    excuse keepfrom wide reply amount

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
