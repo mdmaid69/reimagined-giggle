57eb1140080284d1681917f324ff7feb5d0d24b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:27:04 +0000

    pronounce official even sort tired

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_bool(array):
+        return bool(array)
