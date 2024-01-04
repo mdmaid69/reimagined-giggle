4f8ea8a0cce37392aad2b293f08352b347683f45
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:20 +0000

    long team nurse school year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def get_array_slice(array, i, j):
+        return array[i:j]
