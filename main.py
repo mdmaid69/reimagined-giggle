f84babed26e0630a7f372562e0f5f22683cbdd4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:35:37 +0000

    best blow rest cruelty nor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
