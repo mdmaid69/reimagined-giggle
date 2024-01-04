a0a571361570461aab21257a6194a31642c69b32
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:43:53 +0000

    Qq smile various not recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def find_max(numbers):
+        return max(numbers)
