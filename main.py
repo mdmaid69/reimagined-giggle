a7ac1d17eea3e74a79aa22b23ebfab55c852faaf
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:19:26 +0000

    holiday perform hospital mind start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
