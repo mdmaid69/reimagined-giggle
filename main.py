450a05a83a52620231ab1924dac10fe8b97758a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:14 +0000

    far possibility big strength play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
