eca959f1a96ca33b9044a2a4380933fe6de263cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:22:09 +0000

    unfair form exactly may pass

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+name = "Python"
+print("Hello,", name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
