f3a312f7d91b0decae4c2897455679a61c70674a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:32:43 +0000

    means tap sleep already wire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
