f4003aa574fed2664a14949a40cf9ce9d3afc69a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:58:04 +0000

    Ss task threaten sometimes interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
