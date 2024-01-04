924bdb90f6b09aaca8c9bd8f8386a27422c0ecc7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:58 +0000

    unkind test against lookback(on) order

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
