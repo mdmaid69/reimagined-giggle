12d7525139005a39bff22b569e437185618a38d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:06:02 +0000

    probably argument hot switch protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
