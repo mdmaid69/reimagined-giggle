f9c3ea0f01a804a3edcdde65760e81d6c48f3f18
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:42 +0000

    respect plan aim my cheek

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import sys
-def print_python_version():
-        print(sys.version)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
