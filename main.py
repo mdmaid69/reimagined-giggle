af23567cbd44ceb15ce6b0ae375265ec386462d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:28:36 +0000

    prepare hurt someone trust basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+for i in range(5):
+        print(i)
