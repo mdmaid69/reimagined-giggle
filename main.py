69a9e46200b3e22b01afab9127cf35c742978e83
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:19:16 +0000

    used custom land total man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
