6d6474032fc09b04217379b81e8b07e0b85894b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:39:30 +0000

    score trial health custom door

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
