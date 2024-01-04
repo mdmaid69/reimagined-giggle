0c27590b458f4d71ee3d19f8746ab46c132c0237
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:36:28 +0000

    importance central chat anxious fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
