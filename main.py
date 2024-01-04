b696327fd4ca1f194c833bd1f1681436973f2cfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:15 +0000

    that goout godown could careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
