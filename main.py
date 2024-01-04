e530328ad591d11fbf459377b21b46a6dba33926
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:06:28 +0000

    professional clear pick rough urgent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+print([x**2 for x in range(10)])
