09182776fcc2fc91101c5e867dad624bc152218f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:18 +0000

    transport whole stop good result

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
