8300b536e4eb588622586b835a20e97f3954bf35
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:18:25 +0000

    copy first spot scene warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import random
-print(random.randint(0, 100))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_list_from_array(array):
+        return array.tolist()
