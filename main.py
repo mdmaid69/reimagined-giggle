11463448f7f19fbde560aa5b657df04471baa069
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:07:20 +0000

    Mm fill thank doctor young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
