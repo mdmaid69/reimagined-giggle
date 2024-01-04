73db917220c94eb5131838b1e12fbe362e842ecb
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:03:58 +0000

    discussion lookafter must tread spare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_min(lst):
-        return min(lst)
+def get_array_buffer_info(array):
+        return array.buffer_info()
