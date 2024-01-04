bc7088d888d9d5bd0ce2317ab2dd42ba031015a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:13:47 +0000

    curious world thorough neither accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+print(os.getcwd())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
