d8e57797e700214827aedaf31e4a326c63178e72
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:33:12 +0000

    spill label bringforward normal suck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
