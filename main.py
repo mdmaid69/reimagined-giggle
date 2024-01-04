0bfc8ef848d7f2fb949c8c00c07c0505822b3ee5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:47:29 +0000

    mean exist scare information be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import platform
-def get_python_version():
-        return platform.python_version()
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
