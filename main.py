44fb8f264e89c06a2053bfe821daa931bca0198b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:18:49 +0000

    unfortunate stay necessary narrow substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
