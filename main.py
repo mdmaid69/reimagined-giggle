53617ea095f43305930047345ae6c22990630f0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:16:27 +0000

    action communicate chance genuine funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
