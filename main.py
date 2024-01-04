b371713a4cfac2f5e12df152e32254f229627f23
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:29 +0000

    education listenfor nose juice comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
