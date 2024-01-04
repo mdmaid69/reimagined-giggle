67b3ae30e1c83f1cf18c3410f3fd201e3386a194
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:27 +0000

    miss difficult book come cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
