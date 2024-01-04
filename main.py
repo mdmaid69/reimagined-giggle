d166bee0b35dd344b6181699b880d793dabbb341
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:35:33 +0000

    contact rest impressive balance trick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
