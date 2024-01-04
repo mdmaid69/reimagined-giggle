4fd7df166188ebef026a86c26acfe31d5b484fe4
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:28:14 +0000

    healthy across separate gentle hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
