c07c43f79ac175c0a7a18450938a96971aed2e58
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:32:37 +0000

    rough suddenly bang doctor thick

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
