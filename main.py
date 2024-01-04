c8df2f1f91aca29e833bf0cc14632bfb18430f55
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:18:19 +0000

    doubt hurt freedom several takeaway

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
