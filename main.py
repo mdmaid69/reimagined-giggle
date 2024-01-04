060b3aefd1a71952cf2769730e439c0f650c9f08
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:16:12 +0000

    burn past spread settle do

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
