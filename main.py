2b3c2b2fe4b6d6049cd13df7da4603a700d61e81
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:59 +0000

    light movement reject cum dull

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+print(time.time())
