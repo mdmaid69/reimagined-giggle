5c4916a6b6b81e38389da9a50bdc7f0f62f9d58a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:43:47 +0000

    guilty hold estimate company train

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
