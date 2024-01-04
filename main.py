8cd4184601b010f24ece2cc71dfc5df2dc9ff24f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:12:40 +0000

    I person state Jj stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def multiply_numbers(x, y):
+        return x * y
