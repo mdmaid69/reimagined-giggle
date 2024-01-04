8dd31c4bf527a6a131abb7b31879f37da636bc6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:35:21 +0000

    workout future bad adjective fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
