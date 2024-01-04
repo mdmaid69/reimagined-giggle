262f38797bc60a62953ea66c6982296b6b5e20a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:42:22 +0000

    rubbish disapprove turnup relationship nothing

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
