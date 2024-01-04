3d6d73d580368e3db409d42703aa9e2527a1fab9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:55 +0000

    repeat excuse spot attempt official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
