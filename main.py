c40a82e047bcee2f893c3c6c5dbad03146eb6ec3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:57:00 +0000

    anybody environment theirs clue circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_index(array, item):
+        return array.index(item)
