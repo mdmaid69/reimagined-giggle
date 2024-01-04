4cb7ed05a93ce049199e470a2c47658f81b9492f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:07:41 +0000

    key beginning lean lose rubbish

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def convert_to_binary(n):
+        return bin(n)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
