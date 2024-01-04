b8b38307db491527a17650052b49f73c851741cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:42:25 +0000

    lookafter estimate know quick needle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
