89ef0e817262f557df1e04e3dad835a1cc0bbc2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:06:29 +0000

    popular score ignore trade hurt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
