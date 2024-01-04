f98725dafdd11ea63c0c70f9d81d0994b0aa095c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:42:25 +0000

    again expect by straight everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import random
+print(random.randint(0, 100))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
