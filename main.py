681fbcc331db3af8974ba2d5be91aa20928c8d77
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:10 +0000

    seat woman fair entry taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+for i in range(10): print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
