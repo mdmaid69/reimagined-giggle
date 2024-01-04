c021108253ed51613bd5eaeb4e6a21d2ee512e32
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:19:20 +0000

    steal consist familiar smell wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
