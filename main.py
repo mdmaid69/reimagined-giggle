c3e7eea24d9757af44ef1487b04ac074034b2121
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:57:35 +0000

    key earth doubt suggest bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
