b9fa85da0b7511f1ca04aea4726ed320dbff5f64
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:28:13 +0000

    approve comfortable soft delight bear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
