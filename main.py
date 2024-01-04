2ec78bbaab642caa7bf0b2ddd549505a5556dd1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:58:56 +0000

    not out friend Ii each

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
