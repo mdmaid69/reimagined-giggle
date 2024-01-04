3ff35ce3940a47635e27d68b3becb091b46fb2df
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:07:18 +0000

    quickly base different state praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
