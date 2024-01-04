2a2d1353ba326031411127d80ab1389857a4eaaa
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:17:30 +0000

    mention similar completely regularly kind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
