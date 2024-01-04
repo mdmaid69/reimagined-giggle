126f2f99a31d03b2645bf1209cc29b937b8f7702
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:22 +0000

    hurry basically tend battery motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
