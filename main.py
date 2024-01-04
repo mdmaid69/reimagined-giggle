dd5dddeae5b92cf6aaacf39a8458190aa6a0cfd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:40:12 +0000

    runinto phone constantly centimetre favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
