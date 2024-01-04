c5a5868cdeb852e0f166026598463eaddfcbc4ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:28:00 +0000

    position exact ground thought contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+print(math.pi)
