9d48fb32d333a6249f93eb2a4a8a2201e8a30ab4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:09 +0000

    some late goaway lay suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_min(lst):
-        return min(lst)
+  def convert_to_octal(n):
+        return oct(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
