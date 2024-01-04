0f2b9301bd61df8a7b691ad1f4936955746c644b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:07 +0000

    surprising software compare over dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
