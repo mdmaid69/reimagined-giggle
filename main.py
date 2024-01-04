38b10497056347e5e96d8475ee00828c8fa67805
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:26:04 +0000

    prepare bite there stand season

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import json
-print(json.dumps({"name": "John", "age": 30}))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
