f7a43dd1a94f1770becf2c23b0e27857eacc5094
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:23:14 +0000

    among scrap fine relative business

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def get_list_from_array(array):
+        return array.tolist()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
