aab7bbd39a2da4ad062d33b5737721fb0c692364
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:45:24 +0000

    loan dress first getoff fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
