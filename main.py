d9eda72e534acde66aabe37c398cb63f06ab44b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:01:37 +0000

    party play mind body concentration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
