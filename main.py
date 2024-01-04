e6e8c183d9dfeb998cc2e5287c1e5eef31481548
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:59:39 +0000

    abroad shame Ww miss plenty

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
