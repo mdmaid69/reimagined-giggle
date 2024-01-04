4262e41586977b73d6171fb046e7430ca1676b1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:23:26 +0000

    convenient behind basically buy instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-print([x**2 for x in range(10)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
