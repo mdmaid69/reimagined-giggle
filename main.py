25170e06383b295a0fabd9c2716f04b3e431c3bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:52:39 +0000

    depend wait work politics goaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_pressure(force, area):
+        return force / area
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
