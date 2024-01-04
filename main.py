b25ab6334d71a8be9d5b65bbdcd71f1b4ded2f80
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:02:04 +0000

    private holdout rarely consult extra

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bool(array):
+        return bool(array)
