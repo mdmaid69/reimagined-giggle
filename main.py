9f91e7973da0367cdee4360fb7764d160943a9cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:18:03 +0000

    yesterday combine experiment down rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
 def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
