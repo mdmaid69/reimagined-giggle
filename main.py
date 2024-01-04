fe51172973f6058f8c8b72c85c1a6bb3585c6717
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:41:06 +0000

    hide congratulations think remark press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
