e57d76b9adda04b6b585f5ff76164f4b9cbf057a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:48:41 +0000

    beginning pronounce reach first performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+print(datetime.datetime.now())
