4bae50fcf8e8c32778eaec430f139fc793c3fb4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:08:00 +0000

    bye cut tired thirsty many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
