a0e50d8eeb004bae6175c4ea7772524e6c314fad
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:21:07 +0000

    frightened service clear comeup either

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print(sum(range(10)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
