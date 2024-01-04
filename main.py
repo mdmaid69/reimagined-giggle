00e002f014747dcb06f0265c56faeca4b9ca3365
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:47 +0000

    mainly tall major foreign fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
