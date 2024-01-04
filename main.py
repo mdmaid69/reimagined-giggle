24d389386e3aec14379c615d8fd8d14d97d2f3a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:27:30 +0000

    store shock react slightly contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def square_number(x):
-        return x**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
