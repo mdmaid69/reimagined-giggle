b3d799b758afbe892d468d410a9fbfb7281a884a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:13 +0000

    area drop terrible run broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
