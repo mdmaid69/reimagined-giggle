7e1b8d2e5ebf4502f0aa24541bac1cbc007450ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:28:42 +0000

    gate me real answer by

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
