f705a444919f299aad4f627da1c691b9c16f3cc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:38 +0000

    considering total traffic comeover slice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
