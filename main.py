8aa9b170a2049671b7b837c18c265bcd6ed1dccc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:00:46 +0000

    responsible lend ball result score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
