43e6742a399c2a3c4556e8f03d64d6166e6b9d45
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:17 +0000

    permission balance protest squash little

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_queue():
-        return collections.deque()
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_work(force, distance):
+        return force * distance
