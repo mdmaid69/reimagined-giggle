c288f49cd8376198943ef33c40539b6aca61f8bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:08:50 +0000

    plate defend unless bin mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
