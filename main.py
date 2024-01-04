e39e14b5ad870a43efd21ff8e2c748c9f309182a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:52:40 +0000

    too hand suddenly behind busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
