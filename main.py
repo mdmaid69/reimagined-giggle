ef34a32861d7969c9ad41907c0fe866e4bcf2736
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:16 +0000

    much shut tap die circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
