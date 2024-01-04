80ed0ccfb2f6f6b7b06806ee0f16d5cbd715ca8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:33:14 +0000

    bush last suggest main can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
 print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
