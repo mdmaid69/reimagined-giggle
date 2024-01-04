88e46d2ae11359539a5673615c18c693efa7e7f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:48:05 +0000

    main arrive branch force Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
