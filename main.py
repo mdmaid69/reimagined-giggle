0a2f82e4ceab49bdb37b4bf32bd338fdc32bcb00
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:51:10 +0000

    rest undo properly constantly improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
