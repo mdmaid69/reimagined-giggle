60eae7aae4aa7dfe23414d0a47758e822da767af
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:57:53 +0000

    frequent direct feed compete condition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
