92224a037e58f473265470d50fe1aa93a20cf361
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:21:09 +0000

    force sky pull responsible aim

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
