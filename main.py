ad99710df08f564c76ecc8a76cf3fdfee12100f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:31 +0000

    narrow home list movement meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
