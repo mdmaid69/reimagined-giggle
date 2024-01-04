48842d7dbfb0b12cda9568142e66b25ab65943f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:49 +0000

    protest small evening 572enter drop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def square_number(x):
+        return x**2
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
