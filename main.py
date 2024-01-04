9f7e39784c750d2c11196c913411cbff8200ca8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:18:02 +0000

    comefrom yet past actual yes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
