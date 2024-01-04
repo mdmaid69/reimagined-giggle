4ef02cccc09c8785a8db4d056ee4a6d3d09ee65b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:14 +0000

    war path separate shake hear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import time
+def get_current_time():
+        return time.ctime()
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
