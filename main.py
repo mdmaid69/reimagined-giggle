2eca040cd2539340017ccc8adfd88ef0928fd430
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:46:31 +0000

    mean failure too sudden backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
 import math
 def calculate_root(x, n):
         return math.pow(x, 1/n)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
