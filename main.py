aaef142879bbd2aed585c02c1bc866fd466eed91
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:06:44 +0000

    join seriously dish crazy feeling

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def square_number(x):
+        return x**2
