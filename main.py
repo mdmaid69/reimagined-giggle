1e947768e0c873fceea309e44d80ecc6c840d1f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:05 +0000

    equal equal star very giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
