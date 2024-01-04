a33fc833efb1c3f8a09020baf332cba6c33fd487
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:27 +0000

    taste declare our imagine comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
