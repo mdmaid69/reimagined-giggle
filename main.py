363b2ab17af15464e1f9fed9b40e084647796732
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:44 +0000

    produce bread desperate fix suit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
