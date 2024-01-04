267f19967bd0e8a03015e3e8998519e31ea2c9d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:12:18 +0000

    sometimes object pleased national negative

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_float(array):
+        return float(array[0])
