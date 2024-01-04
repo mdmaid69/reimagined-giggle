b07041300bed7373f9dc8992014ceb1f4d077259
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:40 +0000

    serious matter mark Ff relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
