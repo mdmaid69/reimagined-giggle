38b33c6a1421336b7a353b34a6f33f1f88d5d13b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:00:48 +0000

    571enough burst happy only introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_str(array):
+        return str(array)
