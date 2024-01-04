d02d2205db0a338442787c49c63d4f0b2977355b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:28:56 +0000

    yourself scene slip thick because

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_current_directory():
-        return os.getcwd()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
