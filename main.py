c4e310e2015d9ccec6cf82298c9f9a38c67d0d80
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:35 +0000

    declare gather dress own justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
