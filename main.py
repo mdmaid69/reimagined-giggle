e55e3877caf710a6707982802620f5890f1b7437
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:40 +0000

    scared bank round touch influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import time
-print(time.time())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
