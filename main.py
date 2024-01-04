56b9a028306665828401b333026e6d9663f1c440
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:14:05 +0000

    wing close improve rush shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
