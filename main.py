7398bc751f8711e8bb3550283ca146f8aca8bdab
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:36:53 +0000

    exchange hospital acceptable wide score

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
