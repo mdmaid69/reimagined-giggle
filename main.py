f02e063f24624cf6912bd828e2673e4044a8c56d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:06:41 +0000

    activity breakdown hungry centre calm

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
