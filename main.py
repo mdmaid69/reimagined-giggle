e2e0d28859f7baea98cacb54c9d6aac8bb05583e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:27:09 +0000

    behave breathe table unhappy measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
