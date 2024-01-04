4dfdc426a01e983a03eb688b3ec3fac438a569a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:41 +0000

    heavy young nurse office himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
