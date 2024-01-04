2d4c3fc4a7f387854e2841107d5c0da6c15f2b00
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:26:56 +0000

    bother threaten label exaggerate effect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
