09f886fb96b6c47e892db757f7c3a9d173191cb9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:57 +0000

    respect shut lie loose about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
