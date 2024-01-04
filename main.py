b9424d4f4b746736dbcd24737f22efa487ab466d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:58:45 +0000

    fine aware morning hardly neck

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
