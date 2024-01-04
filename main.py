338630be26777e4391beb4ae7b32d468a9c10288
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:11 +0000

    affect second godown eat concerned

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
