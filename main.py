c117a8599898b3a797b078a48b1f81b96350f8ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:40:18 +0000

    degree might excellent practise steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
