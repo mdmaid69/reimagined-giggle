cdfc3b4e65843daf047361fe4d0e73404d232294
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:32 +0000

    true join throw body speech

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
