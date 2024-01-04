8cb6c6f04b187a62ac9282765350fbf4180aca59
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:12:49 +0000

    Hh branch turn true takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
