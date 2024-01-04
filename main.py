33c68cf75cbdff012015ed7ee7b8e27205de9ec2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:56:35 +0000

    every sore bury smile plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
