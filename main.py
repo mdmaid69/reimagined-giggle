a3d0e68dad7a1510297e344d4192942d617f3c64
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:03 +0000

    want climb past across nor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_length(array):
+        return len(array)
