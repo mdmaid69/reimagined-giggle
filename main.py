27e870a84ed2b5adee756e445251136a5d51bf41
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:23:21 +0000

    correct further line translate front

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def roll_die():
+        return random.randint(1, 6)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
