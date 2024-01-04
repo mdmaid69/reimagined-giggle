91ecf413aa6423e4bc7582dfefc109649b68f553
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:37:05 +0000

    cheat near point voice if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
