bb671859cff8967d831b57253618a56c91f8d394
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:03:36 +0000

    failure undo unpleasant better walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
