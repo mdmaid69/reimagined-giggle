8bd162568e7a7d8525b0bdd4835f9fe134ec55d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:48:58 +0000

    standard similar speak Itself this

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
