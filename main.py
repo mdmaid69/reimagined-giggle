666bc2f432239f0ddd3af51f1badcafce21b4df8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:15:56 +0000

    calloff nearly protest him mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
