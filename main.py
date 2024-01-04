e9017072654c016fbb8df283cb9f5daaec45cf52
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:13:02 +0000

    keen someone thank turnover blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_list_from_array(array):
+        return array.tolist()
