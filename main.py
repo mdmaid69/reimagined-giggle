535d4e9f288f5b52273e4007eb6100465177ce16
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:49:36 +0000

    573entertainment over personal getoff swallow

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+print(re.match("h.*o", "hello world"))
