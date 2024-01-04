912e8aee758fb05e7828bc652f221b07b48c89c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:11:13 +0000

    maintain enthusiastic roll claim tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
