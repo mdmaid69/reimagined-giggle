933fffacdeeff3d06421a8d22d99b778087ef469
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:05 +0000

    hit mysterious friend activity attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
