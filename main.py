30b2660d91865f84b930fdbb7ce8428590a3d9bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:56:30 +0000

    sink say world heavy needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
