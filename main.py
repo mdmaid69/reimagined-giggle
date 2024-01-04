e9e4886e0beda8b965be05b02ed5b5d6cd2b1e2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:47 +0000

    without home circle pen look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
