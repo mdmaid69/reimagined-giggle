61ca2ace1cc23bb88db56819e46b9e737e1a3ab8
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:41:38 +0000

    takeout style like bottom crazy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
