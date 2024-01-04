e526c5a16f58491b9511d6ae7ea43a02b9ec7605
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:54:49 +0000

    bank feeling write secret Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
