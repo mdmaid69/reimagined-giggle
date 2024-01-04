f6570fd3560e58b35461b2478a80b0ef3b0d95c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:19:36 +0000

    crowded across mine roll convinced

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_odd(n):
+        return n % 2 != 0
