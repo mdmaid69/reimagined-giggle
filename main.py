6a3f1c950eac2f3466b0a930940da18cc3a4851f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:49:00 +0000

    now join train effect division

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_item_count(array, item):
+        return array.count(item)
