aaff19b0b5667f9901a31392a996c28e59da606b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:49:35 +0000

    cap though Dd exist float

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
