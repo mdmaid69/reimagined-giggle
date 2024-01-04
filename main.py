37312c221f564ef4861ead6ef93de3fe51084e6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:07:22 +0000

    trap peace murder sudden possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
