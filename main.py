84e98eb8821e2aa792c78016f3f313e2a7b1809f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:08 +0000

    noisy necessary far onto face

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+print(random.randint(0, 100))
