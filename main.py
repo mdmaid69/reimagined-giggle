c6f23e7de57d1419e6ae23b578bcec1e85a725ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:46:57 +0000

    match buy imagination even ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
