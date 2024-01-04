f88cb73c809593c9e3d9283f091c405575c59c69
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:41:23 +0000

    congratulations thinkover out guide house

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 def subtract_numbers(x, y):
         return x - y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
