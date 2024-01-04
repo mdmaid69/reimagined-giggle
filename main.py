95b710bee74cfe91f5da0d05509552e3877aa42e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:12 +0000

    clothes handle case weekend tidy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
