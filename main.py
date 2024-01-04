46faed13a8a4359dda9dd3b11e3ffb587a1e011e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:42:15 +0000

    warn pair hold much ashamed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
