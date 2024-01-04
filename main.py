eb6d60e94431dcb7a4300f51d76ed20577826bb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:23:51 +0000

    Ww poison advantage mistake cheque

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def reverse_list(lst):
+        return lst[::-1]
