c7d603cb3993b0704910eeba4732b151a216b0a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:05 +0000

    person refer song into touch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
