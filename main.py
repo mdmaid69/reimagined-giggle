61a9e0869598cc73caca102565741853b50b6bd1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:17:22 +0000

    side truth mark clear bag

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_sine(x):
+        return math.sin(x)
