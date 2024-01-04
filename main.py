6e54ccc371269530ec1deb34e00ec3471c9b13cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:17:06 +0000

    splash noone this possibly confident

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
