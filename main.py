c4e6bab8122c6e26e4d94df20bcc37e4959fa1cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:59:01 +0000

    positive callout copy into apply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
