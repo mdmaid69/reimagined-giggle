9ee65074459834fc3e0c0e5f1e8d6c378c284508
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:17 +0000

    prevent change activity winter close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_characters(sentence):
-        return len(sentence)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
