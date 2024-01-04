955222264ff45d57c4082d4cfe753ca1f15ec641
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:18:54 +0000

    satisfied comeout pause obtain total

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def print_python_version():
-        return sys.version
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
