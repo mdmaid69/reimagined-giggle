5c8eaccfc7accb40de729cd6ce33895aac7d20a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:26:20 +0000

    intelligent sticky only hard surprise

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
