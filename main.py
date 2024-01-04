a3f9f9e3733b7d85edb4c7786cb09c1ad7be5fd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:24 +0000

    light surprising strong pretty proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
