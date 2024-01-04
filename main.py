68c0eef477177b1adf9ba99753e9de7830a793d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:39:03 +0000

    show experience tall average height

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
