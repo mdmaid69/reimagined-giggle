d435c053689a0eab228fde86c40753399fbc84b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:16:36 +0000

    fasten stop attractive heavy previous

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
