5749838a15e7f245a8494f267e5690341189a632
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:52:13 +0000

    together sort accommodation wash movement

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def is_odd(n):
+        return n % 2 != 0
