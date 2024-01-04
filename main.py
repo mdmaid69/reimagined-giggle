f3629c7606cf9f7aa4e035ce3ac1abcdbb762965
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:53:56 +0000

    explanation goback(to) morning weigh create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
