947ee214fdf2c997a932d82f7b7b6b5eccad53db
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:54:59 +0000

    goodbye deliver shake reply introduction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
