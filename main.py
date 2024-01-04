9a503e34da1a18b99aea21ce35f1761741fe722c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:52:18 +0000

    contain will recognize conclusion safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
