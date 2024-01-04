facdd6796462cbfc624c272a8fee9951b4b44f90
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:36:00 +0000

    afford never little majority train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
