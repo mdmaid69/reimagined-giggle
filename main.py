1ba80ba8536ed0a7220a22c19adf3bc2ca13aff5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:12:50 +0000

    summer relation pin reject stare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
