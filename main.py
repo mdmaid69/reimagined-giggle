0bafd934f9449a90307b1820dcd579059e1a1664
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:24:03 +0000

    relax between warn later brush

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
