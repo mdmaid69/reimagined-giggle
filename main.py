e63baa681d896184be6354451a69c2ff83607871
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:36:37 +0000

    goout burst square fast promise

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
