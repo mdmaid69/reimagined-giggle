9099b9d715b15124f641b03dff299a34f15da702
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:21:58 +0000

    dick respect wing gentle disturb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
