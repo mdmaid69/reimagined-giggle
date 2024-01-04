4fed1045ae075304d4f0384cc3d1a735c1480da2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:18:37 +0000

    passage just day message although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
