f29c849c25943cd0e811fe00ba66dda5eb79544d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:55:39 +0000

    involve come shadow project married

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
