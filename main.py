93b71716e3b94b2c6ccb906431e700951659b421
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:35 +0000

    realize language level store question

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import time
-print(time.time())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+print(os.getcwd())
