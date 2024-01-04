2023975a52684ce628437530f70ea5832fcd9fc9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:42:44 +0000

    pump somehow waste miss actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
