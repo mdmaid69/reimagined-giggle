02bd7d2605065f4b80e5bc15528a67a890a39f14
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:59:04 +0000

    impression herself bound contact willing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
