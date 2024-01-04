a576dd2de18b63e542d13698926ba5a4a815e414
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:58 +0000

    talkback why shape explain when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_item(array, i):
+        return array[i]
