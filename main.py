ab794a80675ec52603002a841059f40c14c5c3b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:58 +0000

    sense excellent giveup forget approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
