3e04b074f867b105a5bc098e9fd42b24cc4db8e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:03:25 +0000

    really right note noone Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
