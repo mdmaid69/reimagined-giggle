f4ef1d7d31e3a6c668347beffbde4ef9f5abfe01
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:24:51 +0000

    feel rent improvement embarrassed this

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
