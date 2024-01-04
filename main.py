2f5c47ba278382e9872f85adadc37198e39d058f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:23:26 +0000

    gather thinkthrough light dead Zz

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
