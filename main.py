7194ab7b515fb71fb69565b06b6c0118e82b1abf
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:25:31 +0000

    little fold scream importance busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
