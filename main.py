d714a9737ce0967b5e5c6aa2fea4340df8c6d18b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:24:44 +0000

    score increase correct idea among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
