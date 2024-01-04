4e2b1cfb1268a8e74b99dab1735c92eff66f077e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:37:05 +0000

    lock die moment custom somehow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_binary(n):
+        return bin(n)
+import sys
+def exit_program():
+        sys.exit()
