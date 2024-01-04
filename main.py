20e90dfe38112f476133e7fd3c9041e9d47b11c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:22:45 +0000

    sale around kind sticky splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
