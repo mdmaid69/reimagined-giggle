3019f9824e144a5fa565c8eb3525f573aabfb5a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:22:13 +0000

    border live can bang closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
