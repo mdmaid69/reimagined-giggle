c275cb0d09db4aed4dc6a69329dc2809ac91d966
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:39:26 +0000

    lock even conduct argument finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_min(numbers):
+        return min(numbers)
