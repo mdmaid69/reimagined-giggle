4626cd97ba839df67e996a95c3b262fbbf36b99a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:34 +0000

    before choice care block lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
