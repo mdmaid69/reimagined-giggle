8a5026efad7353e67539b9a5464a55423c8cb21d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:02 +0000

    war afternoon whisper prize appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
