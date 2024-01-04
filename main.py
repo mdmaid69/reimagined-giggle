9b1ed764afb5370555e6d5ec3911c7032c782ae2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:07 +0000

    laugh better him safety patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def split_path(path):
+        return os.path.split(path)
