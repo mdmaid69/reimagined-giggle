887e67df3e0f4fbf1d244f8dcab363d705529fd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:15 +0000

    favourite quickly old callat flood

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
