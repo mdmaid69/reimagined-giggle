eeb3d602a307c0dfca0aa6b159be48bd726c1375
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:31:37 +0000

    defeat previous decision know influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import datetime
+def get_current_date():
+        return datetime.date.today()
