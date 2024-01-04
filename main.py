718fae9b0b7591f46a6aee6f4af993d86da03cd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:55:02 +0000

    main indeed especially either upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
