650912d0af0d0c59ef374ca40b3a4af28bf15b83
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:39 +0000

    journey land fortunate offence improve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
