e645e7707737bd3778eff21f1df3dac93c2f00cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:35:15 +0000

    display maybe breakout view noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
