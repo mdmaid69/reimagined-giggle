2fb960a120121dbd742e4a1cdf195408155394df
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:27:09 +0000

    nature limit thanks he shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
