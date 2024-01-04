44f73145651baae28a5621cae83529d228f34a6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:17 +0000

    picture light exit trade true

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
