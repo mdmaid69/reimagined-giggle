15b7f94c200a09420e14a3587abddc6044399ac4
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:28:40 +0000

    rule base range I keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+name = "Python"
+print("Hello,", name)
