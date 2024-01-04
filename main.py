8ff7f63134f9967a5b1ae139ef1302c4a85dfd92
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:45:12 +0000

    theirs brief downstairs afraid popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
