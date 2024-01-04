510872bc20f314a92b1c31f1f3c09808f0ccee54
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:45:40 +0000

    smell round before scream positive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
