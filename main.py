4b0514f274ac92065c4d535ce898d21ecd6ac882
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:04:41 +0000

    education different sign recommend except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
