c870dbca5629b9f52b0987405fecafc89697218b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:00 +0000

    prove collect introduce nerve spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
