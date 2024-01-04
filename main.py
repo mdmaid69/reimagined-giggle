4158235014ad076bc7d9b42cebe94581a2812ab2
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:56:17 +0000

    finger attitude smell neat strike

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
