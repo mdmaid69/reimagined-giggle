6ee234cbf3143408dc707bde948d5152d1d12898
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:53 +0000

    late pile smile care emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def reverse_list(lst):
+        return lst[::-1]
