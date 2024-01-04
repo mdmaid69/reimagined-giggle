81a477a242116b3359c3148e536309925f5e4341
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:31 +0000

    play bar introduce minor stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
