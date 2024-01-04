710e76a5268f7e5ca8f444860835b71cdfd82560
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:11:53 +0000

    entry loose full take point

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
