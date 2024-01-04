33c8099cdfc42af919b92a58eb5905f83e72f448
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:24 +0000

    he unkind whistle mass not

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        print(sys.version)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
