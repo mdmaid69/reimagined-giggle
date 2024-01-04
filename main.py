268e76d0bb95ca8229c7b9ab5c51c15f9468a16d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:07 +0000

    base place risk off anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
