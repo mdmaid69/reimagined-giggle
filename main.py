92f902dab72ca3fd957e4a5a101142926fb6e5f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:06 +0000

    true accidentally rise your shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
