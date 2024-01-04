113c6d72d36ab56dfadd0d7ffea753afbceab997
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:13:37 +0000

    tough free approval line hold

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
