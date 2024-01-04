ce1d5b84a1fc328bd51f9e883aa374ad8292a333
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:11:16 +0000

    escape apologizeapologise mail extra makeinto

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
