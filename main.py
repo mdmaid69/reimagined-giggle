4d1c2146ceb1d0f424b056938b6ca4a2811f00d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:23:13 +0000

    vanish interesting reject edge work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
