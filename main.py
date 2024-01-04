e85b1b24668f0a259d59ab7acfdc192a4643084c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:01:59 +0000

    bridge difficulty crazy treat ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import collections
-def create_counter():
-        return collections.Counter()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
