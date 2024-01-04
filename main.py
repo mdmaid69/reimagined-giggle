f1f6185456f66e8a50fe61141cb9e8b637cc0659
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:14 +0000

    view cheque slow speak sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
