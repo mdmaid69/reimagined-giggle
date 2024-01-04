3cbd8995acee4a7e3271a7684c02be8b0b77382f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:21 +0000

    flat control reserve quarter onto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
