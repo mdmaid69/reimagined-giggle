8117e8aa5ec85096f97d47b643eef6df3c997611
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:47 +0000

    trip expect offend confusing at

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_set(array):
+        return set(array)
