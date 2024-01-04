b75713b1f8fcc9cad7419b8b370ee57d91424a39
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:49:54 +0000

    object watch welcome card hand

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
