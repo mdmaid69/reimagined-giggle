03a6b4aaa2d1bfc76c6a0428ebd0ae9ee23318e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:20:24 +0000

    cut juice organization warning below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
