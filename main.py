05f3145454532758da45c7e68e3c9935ee001c67
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:23:27 +0000

    attitude key government trick careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_float(array):
+        return float(array[0])
