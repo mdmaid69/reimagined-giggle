afd91eafdf4b2547478781f5e656e0c973373624
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:43 +0000

    before busy could order immediate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
