f59f5ed10c856bbf3daf7b10746b4fd66901b49e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:35:45 +0000

    hospital favour thing than year

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
