9c27d4ae0b89c11206879c5d1fbbbfb31cf49547
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:28 +0000

    year convince variety damage blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(numbers):
+        return min(numbers)
