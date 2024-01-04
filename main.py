9b29bce2f4fa9108005d42fdcc6526d4216ad13f
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:47:26 +0000

    future price beyond shout hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
