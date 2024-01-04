038e137df8a50156fb611e1ff323e9f2d2cf1d50
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:08 +0000

    artificial bad attempt underneath upset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_density(mass, volume):
+        return mass / volume
