fed51cf156479fa554bfc76a4c4213c833a97e59
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:39:57 +0000

    previous machine platform rent problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
