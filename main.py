0a5c30a6fcafdd989ddfa213d6f2147d4538a433
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:04:26 +0000

    people post decision watch 571enough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def is_odd(n):
+        return n % 2 != 0
+import re
+print(re.match("h.*o", "hello world"))
