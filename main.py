de32ecbd83657e63a61426e97b019a6d36b1af21
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:19:19 +0000

    feed often turnon by steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
