596fe3e59c5a0ee3a73e7a6d4a894d5b9004dd6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:50:38 +0000

    who move reliable inquire complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
