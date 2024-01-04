4e202d25e4fde511561a14397780c8d96bb7ae77
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:19:33 +0000

    confident able hair free neither

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
