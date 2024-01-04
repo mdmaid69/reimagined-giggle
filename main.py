2708f96190d79c55a744fbd5c8acd22184e92fca
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:24:32 +0000

    embarrassed pick sometimes contact wish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
