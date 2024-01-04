bc68fed1f5b4dbcf9a3b8e80854ee2b2dad98f63
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:37:10 +0000

    pretend average bringback moon description

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import getpass
-def get_username():
-        return getpass.getuser()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
