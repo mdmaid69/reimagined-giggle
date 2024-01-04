7b047026a90ee187c800f9e31b600c4b535e25b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:25:02 +0000

    how achieve switch interview drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
