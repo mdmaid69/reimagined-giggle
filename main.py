94e56a22b5b717fd4036d96f17a22dd57ff006a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:23:01 +0000

    mail several already natural cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
