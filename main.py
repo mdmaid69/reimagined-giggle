60464a63bc10a4f41d45c544943fbefcc15cb9fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:07 +0000

    sometimes water appeal weather ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
