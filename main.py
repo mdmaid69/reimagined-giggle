ae1efaa8ac073d7077c63103835ea7483a598a6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:41:41 +0000

    welcome meet wish prevent yourself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
