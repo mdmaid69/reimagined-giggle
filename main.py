a77b55dcb726791d805eb2c10550848ed12d34b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:44 +0000

    without mark rid comeover follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
