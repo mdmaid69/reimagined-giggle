02c34e7dd997003d29030e03dc9be4905f47e87d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:04 +0000

    body callup stretch pay quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def remove_from_array(array, item):
+        array.remove(item)
