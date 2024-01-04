af53d06ddde08dc36f241299dc88dea42f7cbed8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:21:46 +0000

    large talkoutof belong guard see

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
