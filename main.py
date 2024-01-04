30cdd0b62b3ff396739c1fde1854d633de527104
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:20:05 +0000

    march drive crash usually conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
