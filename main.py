50547b993d8a13ff6a54aa86d8955e6af1ccb656
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:14:18 +0000

    distance body valuable offence deliberately

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+print(sum(range(10)))
