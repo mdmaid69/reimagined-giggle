abca99a424a80c60e64b55c3f87ac9f90c6ce90e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:33:51 +0000

    after authority concentrate left base

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
