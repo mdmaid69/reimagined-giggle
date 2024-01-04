bcbf5a2ed5d003903e74a495eeece2d5edf28ae9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:58:21 +0000

    company centimetre so feature feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
