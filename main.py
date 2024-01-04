612e8ee2741f2b6a667baa17ed3fea151ebb47b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:33 +0000

    Cc imagine project but favourite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
