0c37fe3809f8de86aebb4810f67f5798e96c3879
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:47 +0000

    complicated chin worse practice slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  def square_number(x):
-        return x**2
+def get_array_item_count(array, item):
+        return array.count(item)
+for i in range(5):
+        print(i)
