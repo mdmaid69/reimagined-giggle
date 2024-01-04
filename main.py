d6827e42e6ce127f728be9619365c580e6763070
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:00:55 +0000

    feeling level discuss alive forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
