0a2ee534538799437181297b7088bf65d5d2ec8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:34 +0000

    direction appeal Ee together mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
