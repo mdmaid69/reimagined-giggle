0fbce13c2331f8e2452aa2de4eb57e0db28c6416
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:42 +0000

    description unless excellent mention blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
