cee7898b63a3f69b60febdc475ad8da8ae8ae1a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:43 +0000

    proud slightly over aim tidy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
