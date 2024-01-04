8828e48618cfdce23f8a84c2537003a74a1974c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:21:26 +0000

    develop hit cause life reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
