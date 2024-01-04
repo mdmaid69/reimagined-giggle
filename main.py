987a7aff0228582f2f733a44c601774b911b2d9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:06:24 +0000

    water nice help check proper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
