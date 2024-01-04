d46bb98fb146a0a887003bae8c12ba2b89799728
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:52:06 +0000

    test demonstrate match bound Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
