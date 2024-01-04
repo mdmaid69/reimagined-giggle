58e783c760148ec4921a29dec7b77ce7e08e3f52
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:49 +0000

    base Cc remember abroad manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
