4296dc9723c5845a1129e836f42eebc234f23ea7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:27:43 +0000

    breakinto mystery post really doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def cube_number(x):
-        return x**3
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
