54a53602dd9789c4fcf02fd69ce4632fafa509c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:02:42 +0000

    tear this plan bill night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
