fc5f306ecd0192655cdc4562d1b6c67fa0a5abb9
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:46:29 +0000

    consist minority spot squash recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def get_array_as_set(array):
+        return set(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
