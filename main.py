94861a608b7563f4b31917ceb055ece341557aec
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:27:44 +0000

    tax situation boil while be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
