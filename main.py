708a50eb76f42b61df26c1968842e3a56cb96144
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:46:15 +0000

    favour sting back wish damage

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
