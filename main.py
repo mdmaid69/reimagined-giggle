b7cf800e4682c96d1babdcdb1290582bc8439a89
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:38 +0000

    forever fix chat anyone level

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
