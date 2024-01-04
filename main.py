a979f059627d15981951e564000a43301f4a1fc8
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:05:29 +0000

    scheme sensitive understand marry tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import tensorflow as tf
+print(tf.__version__)
