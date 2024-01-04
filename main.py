82feef2d12a944db5ac110013373f039953aca88
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:15 +0000

    really would area letter almost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-print(random.randint(0, 100))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
