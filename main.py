c24e08181bf366fe3c6da338951565d9f27f991e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:04:32 +0000

    keepout image hardly wire dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_max(numbers):
+        return max(numbers)
