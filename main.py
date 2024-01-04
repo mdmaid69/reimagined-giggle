4542191e4d2e7c749756f28babc6727b163464e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:22:52 +0000

    open extraordinary obtain plan call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
