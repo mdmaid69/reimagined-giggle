1399e3860aeba8642bb81abac4b73bd0b8e44692
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:29:08 +0000

    tonight necessary perfect nurse attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_power(work, time):
+        return work / time
