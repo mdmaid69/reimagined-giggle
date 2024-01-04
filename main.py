9d55ee66472f36e7ea3706f08d87596fe08fe440
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:47:35 +0000

    meal cost heart hurt wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def convert_to_octal(n):
-        return oct(n)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
