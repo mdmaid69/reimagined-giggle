3113aa73db587eaf62251f2e547413edc1a7e29e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:03:02 +0000

    that able justice later worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
