dd622269a31158aee314f9d9c577f937e1458d3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:15:00 +0000

    during Cc pocket lookback(on) hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def find_max(numbers):
+        return max(numbers)
