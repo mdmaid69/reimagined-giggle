a6ca25f616f06f34131b83984afe4004f354b45a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:45 +0000

    sink provide home shine find

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
