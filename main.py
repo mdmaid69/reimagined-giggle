88b1ffbe969acd4f844193dc3df6e97112a6e41a
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:47:57 +0000

    plant judge usual trust noisy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import time
+def get_current_time():
+        return time.time()
