7a50e6696cd25867ded698f618a4ab0e6ed75a15
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:15 +0000

    within culture bringup so innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import tensorflow as tf
+print(tf.__version__)
