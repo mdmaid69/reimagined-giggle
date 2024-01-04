dec4f6a8452a35e829ab2d2bc538617afb710c34
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:23:40 +0000

    accidentally whistle war collect safe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_min(lst):
+        return min(lst)
