6efa33fd596985d8ff9c8b0346ada1d152a94bd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:02:20 +0000

    declaration over pot label excited

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
