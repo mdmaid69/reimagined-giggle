1d26b03feda952df99853a34ef835df2cc1d05f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:50:49 +0000

    available little whoever put badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
