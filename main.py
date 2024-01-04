b8feba53dcfc385faa2bd3ca6d14ff339e15c7a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:40:43 +0000

    tall giveup tradition follow off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+for i in range(5):
+        print(i)
