54a3b336152e40a3722aaa7a17fda6164bdc72b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:45:40 +0000

    welcome march name fetch wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.time()
