ae564d9ce33bbab9f2b5bb34fc99b0ad77d6ac53
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:09:21 +0000

    strange miss pen wander describe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
