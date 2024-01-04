7f01a3e526b888f3a92229aeed8d1f0f90854185
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:31:19 +0000

    government wheel back other off

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
