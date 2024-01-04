811ec7022d1f7443928a13d9157d161899881e6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:30:40 +0000

    able perhaps yes laugh place

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
