a6eace4a6d02ba6a52327065e6f4c7d50e85a2ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:34:22 +0000

    payment settle behind but fire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+print([x**2 for x in range(10)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
