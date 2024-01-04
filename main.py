742c26874ca4a3ed34fb62c1ea66b860a55e6c6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:54:40 +0000

    right really own low sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
