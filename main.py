5dead9f83b7f9750a9a8642a1466e1c2a0edaf04
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:46:07 +0000

    error other unhappy goodbye book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_as_list(array):
-        return list(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
