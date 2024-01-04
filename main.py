548cfdfb0c41255eafbcfc8526d546decf8df4fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:58:56 +0000

    plastic treat consult discovery discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
