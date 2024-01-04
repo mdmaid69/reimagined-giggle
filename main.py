18c77aa50df662453775b8a456f8613e16a9214e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:46:25 +0000

    support back mean issue limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def set_array_item(array, i, item):
+        array[i] = item
