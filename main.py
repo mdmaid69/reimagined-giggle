97e828203242a2920e6b0aeeac65fcd3b134c86f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:49:06 +0000

    gooff ready listen mother come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
