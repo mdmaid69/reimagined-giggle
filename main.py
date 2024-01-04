e09b0c40c6e2e1d52362f4f67927feeddf3996c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:05:27 +0000

    cover many difficulty thankyou throughout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
