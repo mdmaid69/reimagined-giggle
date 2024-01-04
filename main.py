05b028939b4cd69e2cab26a9808b5ed7684afd25
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:33:19 +0000

    Nn instead form Mm something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
