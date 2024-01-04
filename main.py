c7317ac0154e5aaba021335c0cdd81df9612796d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:20:42 +0000

    rid his method action basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
