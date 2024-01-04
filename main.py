f2b918d4c0342ac3ff9d90217bf1123c0e77bccb
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:32:11 +0000

    holdup present illness repair imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
