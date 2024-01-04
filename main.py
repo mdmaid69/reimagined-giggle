28821b87423b43e2572476ad50eb18316140bfc3
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:22:53 +0000

    male proof level stiff communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
