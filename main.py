2ef3059e62d741ece551580f79db57097720f02f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:32 +0000

    instead only damage legal classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
