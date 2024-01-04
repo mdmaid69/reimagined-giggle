e910f750d92cc900280f9d2d69f0d48e918c89dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:36:28 +0000

    design breakfast blind direct interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
