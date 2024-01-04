96b9f0e5d3f0d6e944986ffb01684765a7e433e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:45:08 +0000

    he part talkinto pressure voice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
