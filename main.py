9583f26913d067f7795b87e1823b191c06dd09b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:16 +0000

    stamp talk funny child bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
