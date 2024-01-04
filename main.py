413486d7a29d05acf6f900b47cabc974e3b947a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:12 +0000

    square reach increase share brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_length(array):
+        return len(array)
