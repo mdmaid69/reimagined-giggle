b39bddc54f887d37370582214481c9953a33392b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:10:34 +0000

    question man sore probably well

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_volume(length, width, height):
+        return length * width * height
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
