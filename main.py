a3419a388445e3a4435e411b79a1bd32e9952e26
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:49:11 +0000

    true comefrom trip single phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def exit_program():
+        sys.exit()
