3d09bebaa96d55b2f1c3ef25607fede589329eab
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:13:32 +0000

    Pp star present male less

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
