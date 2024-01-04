8f54d4e86ad4d57a217272b70ffc7448416d9080
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:52:23 +0000

    pause block hit put know

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
