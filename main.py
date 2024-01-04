16261cfacd20809dadea5674bd60e6ec20285013
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:21:02 +0000

    talkoutof common more rid state

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
