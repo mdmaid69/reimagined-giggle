862fed2073952f3460ac3b405bbf46084588819c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:24:57 +0000

    temper scream poison introduce general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
