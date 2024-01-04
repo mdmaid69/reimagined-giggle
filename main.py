eb1c6d04f5d316566715a00311ac44fa2a14b2ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:38:30 +0000

    reserve usual about creature fetch

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_to_binary(n):
+        return bin(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
