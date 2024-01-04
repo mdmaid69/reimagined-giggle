b77a9f35235327332ac0809afae8ab9167f5d1d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:32:10 +0000

    interest round scream where scrape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_max(lst):
-        return max(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
