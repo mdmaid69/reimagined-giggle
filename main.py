e0f74737d081ed69863b5106b3e24ea1c01b89e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:36:30 +0000

    plan force lead luck pot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
