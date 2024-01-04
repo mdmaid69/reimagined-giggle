ef7486b3460de345e61b19c424e74688041885a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:05 +0000

    at rubbish personal bang habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
