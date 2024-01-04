c62ca12c140878387aadc4aed33cb46c2b5f16be
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:12:43 +0000

    add Yy against dick necessary

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
