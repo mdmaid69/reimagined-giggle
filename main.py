e9aa474b58b6fcba3b67f648e6e48755015ee669
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:03:04 +0000

    advanced yourself prove wind hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
