ea9d2f44520834668e9f2b4023109074e0b6a633
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:17:46 +0000

    if swing set blame kill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import sys
-def print_python_version():
-        print(sys.version)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
