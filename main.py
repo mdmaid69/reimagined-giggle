d6e490104e25917c360e6542cfc781f042005517
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:28 +0000

    reply save illness effort practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def roll_die():
+        return random.randint(1, 6)
+import os
+def change_working_directory(path):
+        os.chdir(path)
