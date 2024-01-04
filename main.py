efc25075cf3e5b72b46e7d55705de1454ebb8141
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:28:09 +0000

    trousers treatment of getback pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
