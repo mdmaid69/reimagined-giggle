c55ffbde14e36b46736887269ef6b35b75504e3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:04 +0000

    black record list rid full

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import time
+print(time.time())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
