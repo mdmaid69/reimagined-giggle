d5b886917f653d0dc3e163c59ef8b84bd8b33707
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:16:30 +0000

    admire often weak of battery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
