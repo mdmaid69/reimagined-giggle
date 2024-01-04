822cf6b444ca5333147ea81b61555b9fd3f2a2b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:21:04 +0000

    split disturb form number answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
