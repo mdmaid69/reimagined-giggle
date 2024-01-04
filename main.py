3d7034f02332387591a25e4a2609fcfa36e18625
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:18 +0000

    ask flat up entrance past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_typecode(array):
+        return array.typecode
