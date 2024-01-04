feb457a0d975d310e8aa5cf66f42c3734e7fac2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:44 +0000

    shop wrap advanced alive cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_bool(array):
+        return bool(array)
