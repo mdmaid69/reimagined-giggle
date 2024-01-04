8072914801f3b92348f10f525b90c5e3cf356848
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:38 +0000

    Itself note pick unfair worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_length(array):
-        return len(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
