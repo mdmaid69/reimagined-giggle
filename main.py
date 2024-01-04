f09fdd8fe364543766c27ce744cdade97ca08dc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:21 +0000

    extra bringdown translate vote watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+from collections import Counter
+print(Counter("hello world"))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
