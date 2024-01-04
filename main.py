105180905552bdc7463cb40540d24c696f1413ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:33:53 +0000

    desperate as interesting belong holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
