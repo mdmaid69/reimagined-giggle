2a88a4b775f9ea8bf5bc6cae04ba6084caaff6b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:14:35 +0000

    every inform distant permission figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
