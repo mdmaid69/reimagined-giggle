949fec18bdf4d960086c10980d5e68eace80a8f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:07:04 +0000

    takeover only gas play chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
