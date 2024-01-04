ab9ab003466e3f63923426786a25f5e28619f1e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:20:06 +0000

    substance male pump runover someone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import platform
+def get_python_version():
+        return platform.python_version()
