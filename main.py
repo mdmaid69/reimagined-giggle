18d8c4fbcfdfaf4ca5230a861e2a334c703d593d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:51:26 +0000

    lock preparation long convince normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
