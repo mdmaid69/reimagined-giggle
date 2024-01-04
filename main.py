5224bd2622be8186ecc28d5a9eccf2217a24f1ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:03 +0000

    feed trap legal lookback(on) meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
