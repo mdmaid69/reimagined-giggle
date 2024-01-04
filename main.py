6fa3c8b5db16324d86293996939103e4924a635f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:51:50 +0000

    part theirs maximum ball declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_error_function(x):
+        return math.erf(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
