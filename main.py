505f305c131f6b83787d0487b3e7e65b91cc427f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:16 +0000

    hungry season warm in record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def reverse_array(array):
-        array.reverse()
+  import sys
+  def get_python_version():
+        return sys.version
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
