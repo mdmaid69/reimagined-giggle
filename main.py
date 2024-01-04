1b23db67110f6719d0c986533900f13793ff6645
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:29:31 +0000

    remove easy strange persuade scheme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def find_min(lst):
+        return min(lst)
