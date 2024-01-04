70da7c911f2b8e1d7a03faf65d8e4f428a369ff1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:26 +0000

    566end nigga model Oo meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def multiply_numbers(x, y):
-        return x * y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
