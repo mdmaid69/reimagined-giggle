0ac02198b75bd0e79e42e777fb8aafa7ea3f9372
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:02 +0000

    subject yes second rain there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
