2827ce1fbaa3c986543fa7c35f01adc8e2638181
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:06:33 +0000

    land aware goback(to) degree long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
