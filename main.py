15d12895f1654f72d10a7c1b4e60c77a821f5eb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:37:51 +0000

    grade exercise personal determined argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_directory_name(path):
+        return os.path.dirname(path)
