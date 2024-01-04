d16fd63375d76e681f561747cefa8625bbbb0148
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:34:20 +0000

    process production cope possession wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
