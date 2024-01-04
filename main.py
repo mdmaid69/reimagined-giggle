2e6ac9e0204bda332a7f220d3dbecd68bfe3524e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:02:27 +0000

    so describe mind increase scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def cube_number(x):
-        return x**3
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
