25ab639edcef08051fb1d744cda0bdafa1bc7fe5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:02:00 +0000

    task petrol everybody sell dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
