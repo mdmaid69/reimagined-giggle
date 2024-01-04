0576c441bdd9cfd85d596ce28536e8ad125cb0c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:15 +0000

    somewhere warning different no gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_length(array):
+        return len(array)
