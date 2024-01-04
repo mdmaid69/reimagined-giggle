0733a9545d65e4b9038b52e4acae2d1d0833f08f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:59:44 +0000

    reply persuade stretch expression scream

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
