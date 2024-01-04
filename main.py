0703b8b99ea354623f06c959cf2a816b771726bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:15 +0000

    scale force none section base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
