f1a73c04c24a71009d437bffc86b3f5f32b8e248
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:03:45 +0000

    part Rr weather hand mad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-print(sum(range(10)))
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
