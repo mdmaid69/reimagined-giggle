7dedb295a19a60740f4b649d5ac11412777153b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:37:52 +0000

    polite about cry any narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+x = 10
+y = 20
+print("Sum:", x + y)
