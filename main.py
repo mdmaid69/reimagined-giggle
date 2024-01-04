553180a2d26bd978426488167d68ae99b40504d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:15:40 +0000

    search continue exchange study traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
