8c4d5a3e22b13e04b390d5985b792c25292d3224
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:49:48 +0000

    no of of scale your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+import os
+def remove_directory(path):
+        os.rmdir(path)
