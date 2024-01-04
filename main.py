fae1fbe6255c80783bd37f1d1716423b09161fe6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:05:45 +0000

    hurt have sell always popular

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
