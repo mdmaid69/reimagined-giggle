98c9a231245127e3e28330df3935be59cbd6a35a
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:59:09 +0000

    attempt brain pretty hurt together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
