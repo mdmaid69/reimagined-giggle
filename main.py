b60fe7928d3b47a22fed0a7886a3c4444fc8932f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:32:27 +0000

    worth share opinion freedom anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_length(array):
+        return len(array)
