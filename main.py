32bd71d89d5670a33885b5491f259fceb93d2e26
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:56:31 +0000

    commit note regional connect meet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def sort_list(lst):
+        return sorted(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
