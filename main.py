771dd044064a0e0011dd8e04d89fdbe09ba233c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:06 +0000

    draw bite now sometimes defend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
