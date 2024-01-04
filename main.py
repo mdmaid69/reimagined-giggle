3f416c7cadd8dbc45d2da7742443a50b9a712ea2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:41:47 +0000

    culture classroom wish worried authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
