a4d664d5951576448a63c4122d427907540b4a12
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:16:53 +0000

    practical plenty official concern duty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def convert_to_octal(n):
+        return oct(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
