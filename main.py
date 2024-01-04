585d542a7f9c1b7b02a11e7aa9830e8e3d15b341
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:04:33 +0000

    crash introduction life true education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_typecode(array):
+        return array.typecode
