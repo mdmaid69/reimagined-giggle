bfcb35511f0964f521f6699f5fa8fc5fd241ecb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:17 +0000

    process thorough shadow operate star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+import sys
+def print_python_version():
+        return sys.version
+import sys
+def exit_program():
+        sys.exit()
