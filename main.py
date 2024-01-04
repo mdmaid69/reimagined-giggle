2f02aa9348415945da7d2563452670bd86aac9ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:10 +0000

    trip positive away store comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
