2f6195f90281a7386975c490c7d1b18df2480d71
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:54:04 +0000

    cope pay inquire able past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
