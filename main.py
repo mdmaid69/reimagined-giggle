e24be5062f5b4703e6cdf8b36871d0794eb35e01
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:17:25 +0000

    fortunately interview shine comedown beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
