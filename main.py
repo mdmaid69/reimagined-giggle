e46d2a7d9d47c72b32390eac92e0e363d6e205e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:04 +0000

    turnup convinced left somewhere maintain

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
