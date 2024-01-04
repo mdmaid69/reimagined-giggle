c4696c9fb34578d1ad0b7f9480963e0b188f1887
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:28:02 +0000

    set narrow post approval turninto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
