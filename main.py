41ff8a4b602620f031bafe55f6005af1dda1b675
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:50:12 +0000

    fire chin further acceptable pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
