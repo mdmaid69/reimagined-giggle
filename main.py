10e09b5ff2dc59529d2ef71c15be152a93be483f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:37:58 +0000

    word pronounce introduce pour beneath

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def remove_duplicates(lst):
+        return list(set(lst))
