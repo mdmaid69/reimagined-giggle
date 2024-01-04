65ce15c3656cbcc780eb1dbd6979d3a17373dbf8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:26:16 +0000

    opponent provide keepaway pen course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
