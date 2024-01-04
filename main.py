24db2583c6392d4b19c28f2fb75dda3ca658a5fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:38 +0000

    firm yesterday know calloff game

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+print(math.pi)
+import time
+print(time.time())
