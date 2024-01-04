9db926324bcd8e66c6fb209a5b203e25d81aaec5
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:08:52 +0000

    neck equal petrol word opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
