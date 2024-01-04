954024e3d4c5bb559150e9a99ec24bc2a425123e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:12 +0000

    load official copy eager unkind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
