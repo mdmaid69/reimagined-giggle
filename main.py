876d839e23a339381b617bec845bd74b21e66dbb
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:42:33 +0000

    slow possibly exciting unfortunate talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
