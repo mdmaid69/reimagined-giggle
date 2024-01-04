917597b880a3e23c460007e6ff2e2d9f52d4914b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:54:53 +0000

    talk level effort path argue

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_string_from_array(array):
+        return array.tobytes()
