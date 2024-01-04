9cb77c0f8d751c2c282c5e14cbda4d3f0f5082ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:41 +0000

    joke meet communicate drive speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
