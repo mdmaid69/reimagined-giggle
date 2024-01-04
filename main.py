bef9262f1a64a9f04d2e131382450dae5c71db8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:27 +0000

    mistake slip company club petrol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def split_path(path):
+        return os.path.split(path)
