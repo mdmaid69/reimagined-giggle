ad3371c05e552684b03747baefcf854da009ebbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:57:15 +0000

    middle along emotion Zz no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
