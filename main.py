71cc36968aaa4dce0e514665d9da5dd26f55e302
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:27:32 +0000

    trap disappointed dinner once light

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
