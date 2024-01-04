46ae5c3a804f7e56a838b8afa24f71d3f7d87a42
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:39 +0000

    calm excuse cut popular stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
