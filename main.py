0117bac9d75362c39f43bc23c28f1c897f41329e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:26:48 +0000

    search half bad search woman

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
