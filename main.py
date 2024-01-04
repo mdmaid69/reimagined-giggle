26dd062754055ddb7ccb9643062bbddf8ccfe5f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:49:42 +0000

    off woman human tread suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
