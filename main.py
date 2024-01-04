1893cddc8bb4818e7370aab6eee0e02f864cb56b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:20:19 +0000

    coat complain correct how complicated

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
