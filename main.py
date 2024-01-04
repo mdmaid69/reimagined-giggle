54bf8947e289bfb4bf75ddd82717f85b09362df5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:09:14 +0000

    flood tense land surface guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_binary(n):
-        return bin(n)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
