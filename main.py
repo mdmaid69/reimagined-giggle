7fd5dfa1a53a5a27b4a1fd0a6043da8909785f37
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:22 +0000

    ground during pollution exist someone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_area_rectangle(l, w):
+        return l * w
