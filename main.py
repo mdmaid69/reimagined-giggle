34a71e96bdc9769107e77d7a5f06910d43ca3399
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:59:38 +0000

    objection wet television splash dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
