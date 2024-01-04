3b4fccad92c33488573a366d4377d09aa610b723
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:50 +0000

    shape doubt waste upstairs skin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
