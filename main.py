5effa4260cfd321202739eb3ea7867904dfcd523
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:18:07 +0000

    lock find lesson use touch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+def calculate_work(force, distance):
+        return force * distance
+name = "Python"
+print("Hello,", name)
