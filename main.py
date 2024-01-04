8fae7c942c916f16610b4066827e1e80fa6cb5a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:46:09 +0000

    comefrom direct practise rule level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
