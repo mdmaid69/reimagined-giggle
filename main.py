75e6a7732a45328d30c8181e023494ca240361c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:48:39 +0000

    classroom colour catch callout breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def clear_array(array):
-        array *= 0
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
