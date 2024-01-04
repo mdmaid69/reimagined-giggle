038ad17ad1ead8958042c4d70fc3197883a0b222
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:29:17 +0000

    low law thing light grateful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import json
+print(json.dumps({"name": "John", "age": 30}))
