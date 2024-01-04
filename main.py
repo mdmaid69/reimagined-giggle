8d48b7ea3ca16e98ddbac5d1397b570dcc903d4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:14:11 +0000

    active train treatment cover morning

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
