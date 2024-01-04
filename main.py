a615c6cd42817a239bb8d699915ee86a74f37a49
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:10:49 +0000

    turnup comeoff result point happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-print(math.pi)
+def greet(name):
+        print(f"Hello, {name}!")
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
