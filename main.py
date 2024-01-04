685f5af0afe821ea1d0d5c690ecc8269a4cb3148
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:16:14 +0000

    quiet object crowded no watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
