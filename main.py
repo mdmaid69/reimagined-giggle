083e0a5bb49d68f3447cb348c5c0a554ebd7e8d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:49:51 +0000

    disagree odd range trip choice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
