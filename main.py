9ef79d12d724c53fd9cfbca73e360982f1eea4a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:31:49 +0000

    sticky high inform bored name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
