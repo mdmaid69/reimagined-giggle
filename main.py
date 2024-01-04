7f0bb6f85664ca9f908a32827f7b3543d27f6639
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:08:13 +0000

    drive home freeze thankyou but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
