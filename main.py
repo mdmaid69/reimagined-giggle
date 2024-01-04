88491fe8e5864578045fa1055aab7895fd2e2b9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:16:40 +0000

    dig wide visit offer either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def greet(name):
+        print(f"Hello, {name}!")
