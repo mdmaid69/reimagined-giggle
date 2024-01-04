d1946e275a857dbff2ab227285737af9cded1b6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:54:30 +0000

    except into level mess when

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
