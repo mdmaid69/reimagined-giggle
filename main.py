5b9c942570e366e572fa9e54086d695d16c468cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:45:51 +0000

    patience lookback(on) art contact through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
