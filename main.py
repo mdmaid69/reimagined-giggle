deff70e18a808c98de6f3e79ec99773dbcfa2c5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:37 +0000

    substance child match scrap name

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        return sys.version
