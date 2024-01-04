9f03b7b81884decfc7c53947940383d259a3b136
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:17:33 +0000

    belief hard form pipe surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import sys
+def exit_program():
+        sys.exit()
