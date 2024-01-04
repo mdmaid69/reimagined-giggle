84d25bc00464853e580fc6781ccf74410de1d5fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:03:57 +0000

    but failure freedom smooth gooff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Uppercase:", text.upper())
