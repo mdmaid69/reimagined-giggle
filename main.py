309a489ed1daf4c8b06f3929678c50aaf24df704
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:01:42 +0000

    exaggerate their fault front otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def convert_to_hex(n):
-        return hex(n)
+print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
