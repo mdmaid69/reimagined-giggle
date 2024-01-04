b62fa0a9f28491e840b6136f3e6e26d938a45802
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:04:38 +0000

    have man cope strict decision

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
