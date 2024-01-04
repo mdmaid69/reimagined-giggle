b8ac7112f27691b1a934ab7412167862404bafb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:17:14 +0000

    type content instant she plenty

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
