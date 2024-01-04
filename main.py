edb9e1ed53a988be8072bdec0e070b44f1675af1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:26 +0000

    sign shop makeup petrol absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
