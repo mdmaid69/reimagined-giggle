8b2875b51078e8e6229bf7c49286cceebbb686a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:47:23 +0000

    shut exactly 567enemy raw them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
