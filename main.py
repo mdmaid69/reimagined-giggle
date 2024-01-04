622678fc0483dc37896b45c058f6fefa4edb3cf4
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:34:45 +0000

    true often rise desperate popular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
