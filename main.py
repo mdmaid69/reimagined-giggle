c6ac03a73cec749429264c8d85a1e57d7053fc3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:49 +0000

    fix fork lend file promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import datetime
+def get_current_date():
+        return datetime.date.today()
