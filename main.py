7d4459a3d88c1ebe2148cec5f725ee0b79203a5c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:13:22 +0000

    situation principle even surprised laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
