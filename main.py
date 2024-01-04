e954fef041dafc9edba0ae82303f62e0d10849b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:51 +0000

    rarely under thinkthrough slide patience

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
