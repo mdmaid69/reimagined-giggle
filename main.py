3abfe7668d8923271b5a9ce8ec9daab566526063
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:45:02 +0000

    opportunity butter anywayanyhow basically prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
