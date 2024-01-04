133c859abb47949dfd59ae646e7f1b338a60eba8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:21:23 +0000

    unkind protest confidence record skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
