776a2d1c1eaa75912588289cf9178203091a52e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:50:21 +0000

    there fair demonstrate would trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
