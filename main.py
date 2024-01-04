b3665be53b77cc58b4a5d875dda0ecf8cff6c642
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:40:43 +0000

    ability fierce gentle judge skin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
