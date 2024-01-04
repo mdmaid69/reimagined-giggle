3e8b3ee2aa361cb73aa5930801cfde3b5bfcbf0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:04 +0000

    peaceful wonder smooth road floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
