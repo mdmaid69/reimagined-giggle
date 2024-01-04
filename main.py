3ba126b39e2644766fdf3babf8b7a860257a918e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:55 +0000

    wall lesson after ability sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import sys
+  def get_python_version():
+        return sys.version
