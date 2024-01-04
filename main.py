df588eb24b6b1f6b9274bd04454ffad92ace79b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:27:12 +0000

    drop corner degree bother would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
