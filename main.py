42458ca1391778990cea608aff8268c5cb0eec6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:05:48 +0000

    except attract by across interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
