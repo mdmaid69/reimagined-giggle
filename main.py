96abc8d7e87f416a8c5203508756033add5a905b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:12:16 +0000

    keen suffer we slow spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
