6b965b4fce54885fa23a1d4c5716405dbef15cd4
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:04 +0000

    rough bringup wait himself march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
