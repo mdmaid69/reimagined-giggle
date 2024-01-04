6bf341b08d89d98ef2caff1302d81888097d7eed
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:15 +0000

    trousers deliver us save accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
