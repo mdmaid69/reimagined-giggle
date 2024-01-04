8ce61da0974caf91f500ddd4f631c17ce874e044
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:35:01 +0000

    present birthday 568energy neither tooth

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-print(time.time())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
