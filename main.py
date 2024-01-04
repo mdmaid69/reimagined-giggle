88099a5fbb5f6b08d3b85752c5150e74f925a6ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:56:25 +0000

    show hang friendly freeze out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
