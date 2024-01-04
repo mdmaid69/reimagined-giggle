f4ecce97ee39668b3320217373fc772e46f536ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:10:08 +0000

    frighten punishment achievement awake fail

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import sys
-print(sys.version)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
