7a96b9d14ada3f2a3a48895eeeec527a5dbfadfd
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:04:23 +0000

    charge confusing we equal fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Powers of 2:", [2**x for x in range(n)])
