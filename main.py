3dc1db5cdb14932390bed3009e01333fd50b7626
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:25 +0000

    abroad later deal too influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
