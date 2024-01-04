4d4c4b99770e52e6d5d567e48bf18ed064ca69b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:43:40 +0000

    doubt regard successful mean next

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
