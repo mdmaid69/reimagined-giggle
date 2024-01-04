18a26a3dc4d7668587117fbbf7a9fbd17f86b0cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:28:09 +0000

    takeback not since community nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
