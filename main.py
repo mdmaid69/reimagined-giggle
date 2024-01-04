643945de03184764f6d98dec6fe90d95e8b9f6d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:46 +0000

    566end evening failure order skin

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
