88f9112980da1ab2c0e3b6dbe6eeb5c2d94dfb57
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:35:47 +0000

    sudden correct house thick mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
