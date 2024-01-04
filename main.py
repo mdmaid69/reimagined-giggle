64b0d02b95f898febc60614dcc9c422372a09f8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:11:54 +0000

    this best quick stop exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
