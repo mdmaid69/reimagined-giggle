589f2f925649f93f8804e8a9cdba2d6e2b047415
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:52:00 +0000

    make really generally boil by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
