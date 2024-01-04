3820a22fa1cb244e9cac841089176ea07a8452b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:04:11 +0000

    road price listen heart mistake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import numpy as np
+print(np.array([1, 2, 3]))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
