4c4dca03b5da70dde0427e7096b1c5be0a9972f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:20 +0000

    realize hurt watch division pencil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_length(array):
+        return len(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
