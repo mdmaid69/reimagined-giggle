84aafd311454f56f65318659866b541f56302711
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:48:40 +0000

    set contrast bang state occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
