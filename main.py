cd7d4efd84541f2bb3ff95d0f247b87f541290e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:59:44 +0000

    crowded time impossible lot bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
