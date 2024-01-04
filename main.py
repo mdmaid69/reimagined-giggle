ea4440ade28d63e37462cc064c51260fda051611
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:16:20 +0000

    detail hot content weekend unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
