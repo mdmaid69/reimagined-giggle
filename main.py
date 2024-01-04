8ffc28edb05a2195338c854b1fe1238590d1c8fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:58:07 +0000

    lunch slowly special serve identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(a, b):
-        return a + b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
