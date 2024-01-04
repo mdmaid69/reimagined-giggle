0aca53180d21fa9109862d9216337c0b0776c8a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:33 +0000

    angry proof various if ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
