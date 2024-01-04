2651d0e8699fba4bce989cb9e026c69baf53cfac
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:21 +0000

    public second building kick skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
