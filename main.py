5b7cb0d2138f159724e05c95e71ec2450fe18513
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:54 +0000

    tight wide insist recommend somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
