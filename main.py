b8d521ffa39a5524874bf267a5cb1e190903f509
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:23 +0000

    burst turninto myself sleep remain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+print(sum(range(10)))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
