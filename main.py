b3d86f460b0c0c6013e53f0ad1cf56365af26fa6
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:24:59 +0000

    man recovery during face flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
