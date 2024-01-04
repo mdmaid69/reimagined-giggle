fdf9ecf8d210807e488f0a81ac2cefe076fa5014
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:00:14 +0000

    can power walk murder stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
