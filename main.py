85f96ac784be6d56ec9512d53532e457ad82f8b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:34:37 +0000

    respect another lookup explanation regard

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
