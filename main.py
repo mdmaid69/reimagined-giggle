4064c47f196ad538e1c02029333df950359b834a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:49 +0000

    keepup relief finally expensive Zz

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
