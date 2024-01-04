9472df07906aeb45562205f01a2d0eebf77076ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:16:52 +0000

    narrow square float progress decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+print(math.pi)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_complex(array):
+        return complex(array[0])
