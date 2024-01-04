7bb9f9b8fe3fcdcd9dd13b9eb784715ee6028965
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:55:43 +0000

    letter impression heat minority skirt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
