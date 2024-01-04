0c5918a7b35b899b771013dd3bf0a1a51e12cbb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:44:06 +0000

    part claim basis passage characteristic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
