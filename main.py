beaa9124788a1b401ab9815e89c3b75603498d37
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:30:26 +0000

    reply sharp think since journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
