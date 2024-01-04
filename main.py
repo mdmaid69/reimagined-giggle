4dab8e208f3c6f4ea5d78ccc4fa93377d3e43fd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:10:30 +0000

    telephone apparently throw confused suffer

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
