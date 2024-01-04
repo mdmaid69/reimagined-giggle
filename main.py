402a2be682539156af4ff639e5fc61d2a5e9707e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:57 +0000

    bang break rely relation wrong

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
