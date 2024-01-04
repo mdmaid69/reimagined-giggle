d1b3271ad33e04083af1ed2d93d9759506968aca
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:56 +0000

    study hit however safe transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-print(os.getcwd())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
