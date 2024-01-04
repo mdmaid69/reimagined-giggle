1517647f1b49db1b361f3afb4faa48d8d7cb0ae8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:28:29 +0000

    me run slowly flat ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
