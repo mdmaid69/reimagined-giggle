0e9be54defb0c9826f022db37b2cc7b2eb5aeb86
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:52:24 +0000

    type sky careless ours shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
