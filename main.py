7323c02f94ad70d8d769e8719cdcaffaf1e7ebcf
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:22:10 +0000

    message tragedy schedule Gg bit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
