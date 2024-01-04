e543a2040481f7847e685a55d78579ff9de6916d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:34:16 +0000

    family Zz no let prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
