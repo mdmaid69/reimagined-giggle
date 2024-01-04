03001784e7aac394fcf9593f02d36085e52000f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:54 +0000

    round release direction ought season

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import numpy as np
+print(np.array([1, 2, 3]))
