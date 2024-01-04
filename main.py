6ad68d65ac8dc005b6a212c6f96b95ea60873428
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:26 +0000

    both float exchange dull wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def multiply_numbers(x, y):
-        return x * y
+for i in range(5):
+        print(i)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
