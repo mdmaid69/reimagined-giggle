321588c07b87e8815039590ed3b6260df4588aad
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:13:12 +0000

    connection invite pupil circle also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
