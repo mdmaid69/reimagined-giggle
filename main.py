7e8b40a5833cbd141ce7c4e8f047efb6e4634df4
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:45:45 +0000

    runinto mystery move for judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
