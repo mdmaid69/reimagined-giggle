6df6c3064c37cc6747b79d140ce506f8c936250a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:09:52 +0000

    Hh scale tape recent piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
