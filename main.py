ec28cdacd267a23531bbbcb3b165f29c52686694
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:26:11 +0000

    in used bend present protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def reverse_list(lst):
+  def reverse_list(lst):
         return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
