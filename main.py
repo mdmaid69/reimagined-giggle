4be3e28f471942f89c62e3d10e81cf7bced88b65
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:03:24 +0000

    mass breakin worry trouble error

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
