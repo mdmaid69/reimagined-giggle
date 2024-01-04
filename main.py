424596d66b0186e38a0ad94fb161ff04496fb901
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:08:32 +0000

    perhaps dance broad them fall

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import platform
+def get_python_version():
+        return platform.python_version()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
