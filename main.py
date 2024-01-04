499b8d8eb790cdfe86961d62f29b38594b45a461
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:43:10 +0000

    journey powerful steer whoever move

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
