1f45290f9243af3f596eabef3a76843b5e3357ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:42:39 +0000

    crisis empty equal constantly matter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
