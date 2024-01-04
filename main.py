eb5a8e6bebd0bd598f4b52ebdf8edf87491a1203
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:39 +0000

    wonder lose low sting some

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def cube_number(x):
-        return x**3
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
