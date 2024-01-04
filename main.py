244e33b21f82bbada001e2ee097f0d0dc3bee679
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:40:39 +0000

    keepoff difficult post disgusting exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
