14bdd4ae25bd6bc107f5bb39b1c57a6c66197ebe
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:36:28 +0000

    my must funny closely confusing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-print(re.match("h.*o", "hello world"))
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
