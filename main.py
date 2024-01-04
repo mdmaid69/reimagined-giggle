1ac19a026a6ecd1753748aba31c9096145033f3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:00:43 +0000

    miss cry receive water capital

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-print(math.pi)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
