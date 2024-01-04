5d67f2614c123fe44915ff830459373ab7d8d2a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:50:06 +0000

    consult laugh battery often left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
