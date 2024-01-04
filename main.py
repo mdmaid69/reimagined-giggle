a21cb7463f3539737300bda90c500bcdad41cfbc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:49:45 +0000

    apologizeapologise flood repeat down direction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
