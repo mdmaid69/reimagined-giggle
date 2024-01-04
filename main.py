a24db84c2e9921985906c347c5a4d1753eafb759
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:52 +0000

    breakin oil cause print cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
