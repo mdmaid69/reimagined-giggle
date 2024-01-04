312ec8cf19084d2b118cbe764ecdf1d076e0e52a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:03:10 +0000

    ball 567enemy stand beside replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import json
   def convert_dict_to_json(d):
         return json.dumps(d)
