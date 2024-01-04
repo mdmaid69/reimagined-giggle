f3529c494d9bf6d4190135552eb0eef67abd5a38
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:02:28 +0000

    consult old run protest price

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def clear_array(array):
+        array *= 0
+  import os
+  def get_current_directory():
+        return os.getcwd()
