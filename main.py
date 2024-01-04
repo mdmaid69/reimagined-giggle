dc512e5642d119b44b243ade10f5b6833651a84d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:41:55 +0000

    pretty train loan trade accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
