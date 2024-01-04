4427936b383c8e79ff6b6fb05145ecbe4ccba4f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:30:21 +0000

    autumn contents connect now out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
