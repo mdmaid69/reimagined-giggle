77a910caf70dc672c9e01d6909638f33919b8469
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:37:43 +0000

    fast importance visit difficult chance

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
