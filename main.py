c31b74f97565c4308c518309f1d7d1cbba54c246
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:08:51 +0000

    become build proper quarter disturb

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print(sum(range(10)))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
