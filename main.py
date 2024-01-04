f7917d031c4ad0bbdc58d8068a5d6dda799b425e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:46:10 +0000

    fierce noise pretty argue butter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
