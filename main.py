80e6596c0be4eb9cdfae32a7734cbaa755c58b80
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:51:13 +0000

    broken sea fierce cost write

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
