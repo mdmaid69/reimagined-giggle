d237d4998feb647595ffa5b326b30875f2c4e203
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:09 +0000

    crazy taste only spoil mass

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
