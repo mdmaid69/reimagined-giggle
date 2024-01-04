19f573fe052153295278c0ae950ad2467b7228ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:13:43 +0000

    protest wide frame downstairs friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
