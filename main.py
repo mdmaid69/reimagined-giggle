4d5c059dcebfb51b43012009d44f26acb05e9601
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:51 +0000

    earn measure again all slowly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
