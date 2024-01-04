6508333d73878b3a02d5ad3fc4c813ff6b10d941
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:44:59 +0000

    pump top lend Gg priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
