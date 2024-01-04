ad88eb5ee5412f4a7936c4938afe4cabe874ca61
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:38:50 +0000

    certain Ff 565end comeback plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
