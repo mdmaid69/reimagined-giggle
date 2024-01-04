142d0c4e3d6214e8899f7f2fde3b8fca88b4a03b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:28:12 +0000

    health limit wait independent proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
