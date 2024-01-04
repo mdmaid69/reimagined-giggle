bd7c5bbab7e4094bf5e467adf173ae6a363f363b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:43:59 +0000

    ring or permission market history

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def is_odd(n):
-        return n % 2 != 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
