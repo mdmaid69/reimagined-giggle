ecd426c9cc3ab29e9e5f1362f4ac52e8257124ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:10:50 +0000

    mysterious gas keepoff direction government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
