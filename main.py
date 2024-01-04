a07cdbaee92f93d9c2ec7489938faad8e16b4ac4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:53:19 +0000

    certain someone match protection finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
