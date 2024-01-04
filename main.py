647e0573e79a548289c2b2aa7ace2f8ddb0148a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:22 +0000

    place upset child escape love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
