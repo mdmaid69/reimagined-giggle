09168ce5eaef5f24e16bc763905e5e7af78ed114
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:25:58 +0000

    hit goodbye gate fun stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
