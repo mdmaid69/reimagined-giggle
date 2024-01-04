1e376967f4b4e515a9bc6921363880b7ef595e7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:50 +0000

    skin payment settle laugh sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print(sum(range(10)))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
