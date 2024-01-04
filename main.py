a15b6881bcabdcb348d80892e79304a2c6b91f7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:11 +0000

    recovery mark gift relative world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
