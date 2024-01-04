41b306d46a4c3ac23eef6b5999e44c0fb6768b37
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:34:09 +0000

    precise report stamp train temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
