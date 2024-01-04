f4566de60524f76a82df5d42840fb5a0edf9e0fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:31:21 +0000

    exam workout buy brain much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
