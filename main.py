a7d6561a2ab5755e882c481d6ab41fe4bf225ee4
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:07:18 +0000

    medium thinkthrough plate community dinner

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
