4a0457ffed8f7bf9888a456c40b4412466246473
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:19 +0000

    lost travel determined common believe

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
