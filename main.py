176a6679967339e3a9ed6883817210c07add28d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:02:39 +0000

    offer surprised brother round flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
