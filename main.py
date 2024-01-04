768bb92d9e43d85ddf410a596069fb98499f0c38
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:31 +0000

    original reduce hide tend trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
