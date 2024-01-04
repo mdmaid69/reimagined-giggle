921b1ed0a2747c7896b0b614394a6f6b532233b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:51 +0000

    programme goout move coffee fun

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
