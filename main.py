d939bf3d7cfc73284740892eb0dd50a38672c058
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:11:33 +0000

    shame anything deliberately game lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
