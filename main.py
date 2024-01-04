9ea93f00cf4010c2a78d46c5b9772f1716cb8faf
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:49:45 +0000

    realize real tour style authority

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
