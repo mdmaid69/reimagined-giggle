3c91be2bc1dfb26647c337dea14885d65e6eb9bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:50:10 +0000

    hard ocean care temperature bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_priority_queue():
+        return collections.deque()
