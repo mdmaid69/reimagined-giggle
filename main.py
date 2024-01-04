d432f420d27b1bace1ef1f7bef64393af4ade464
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:23 +0000

    ride air catch bunch their

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
