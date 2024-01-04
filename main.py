70df869982bb9031371e3b4f5e5b229246d9549e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:23 +0000

    count attempt tongue become another

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
