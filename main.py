15904b68e9534624f2a6310c5e4797beb0274e6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:59:36 +0000

    language class turnover accident pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
