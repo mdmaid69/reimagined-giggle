f611e4c989eb65823d3ac3c9c9c0aec7c3816958
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:27 +0000

    failure 570enjoyable raw 564encourage belief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
