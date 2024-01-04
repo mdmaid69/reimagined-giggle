7a25b14e11b74965eff863e157c039d6c6f111a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:35:32 +0000

    make steam miss ill change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
