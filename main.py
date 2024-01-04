8d844cb9328b560015cce782994ea28e050af55a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:28 +0000

    tour Oo fast niggaacademy bath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
