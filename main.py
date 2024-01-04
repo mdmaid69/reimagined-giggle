74b50df660b9808f801a0a60036fdea3a23e6b67
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:11:14 +0000

    straight slice centimetre exciting petrol

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
