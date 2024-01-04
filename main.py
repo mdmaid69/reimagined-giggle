f59af4ffd2f8ce8eda17335d137fd32c53860eba
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:19 +0000

    charge ashamed balance whether makeup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
