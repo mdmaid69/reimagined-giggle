6e9efa103e09918b46fe21b4e6c2128c9e414640
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:05:49 +0000

    hear patient disturb hang commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_speed(distance, time):
+        return distance / time
