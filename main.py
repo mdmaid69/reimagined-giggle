32a6f324de58cfe15c89e47126d42d99dcc1787e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:52:55 +0000

    usual closely near direction each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
