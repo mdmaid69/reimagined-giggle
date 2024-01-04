7e0b3491a226e96f51232360b44bd51b0db8fd62
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:24:06 +0000

    advice light judge mess constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
