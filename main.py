0871e0e19a773064ac5de3b529267e49fc510a3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:11:35 +0000

    last dress hour comeround allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
