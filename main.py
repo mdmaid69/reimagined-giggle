6381c6acb1c542e3920f19934c980cbfd604325e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:26 +0000

    run influence act shoot rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
