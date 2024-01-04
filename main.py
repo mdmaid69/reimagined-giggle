26275604566ec2da3e63779c45a31cbd1d48ef94
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:47:44 +0000

    attitude giveback drink comeover taste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
