3f5829d1f40f5c899dd509cf3056a14fd25d7882
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:07:05 +0000

    question honest under characteristic odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
