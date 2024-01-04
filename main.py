a8072741d135e26f34361f6694844cf1a3b3a003
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:22:22 +0000

    usually least slice far total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
