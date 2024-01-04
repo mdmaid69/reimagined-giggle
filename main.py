ab4fc616042493384aaf71f4f6fb5942f3c7daa7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:48 +0000

    contrast holiday country vote ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
