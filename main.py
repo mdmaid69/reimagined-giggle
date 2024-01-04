fa75fa797385f4e6713b23c24fb888cbf9f90961
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:59:57 +0000

    failure takeout trouble tax concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
