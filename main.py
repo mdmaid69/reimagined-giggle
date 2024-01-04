20f5f4fadca4247bba97f6390ca1609c824b4802
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:55:43 +0000

    match centimetre morning face ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
