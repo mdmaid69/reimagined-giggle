fc786fca5bc2c264dbefc7026b1eb5afa9c73238
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:20:30 +0000

    stay life tomorrow instant spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def cube_number(x):
+        return x**3
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
