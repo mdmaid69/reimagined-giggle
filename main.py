e231071a614b5e2536e9b40e99e45deedad34e57
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:12 +0000

    floor knock trouble call vote

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
