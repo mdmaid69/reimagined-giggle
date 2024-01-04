30284dbaec5d62defe5700957b030d810c5472cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:42:48 +0000

    pleasure yes tomorrow none definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
