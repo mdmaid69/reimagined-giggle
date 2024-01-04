fc000ffe25214e1cf6d83c9b51d3feaac9f9bc34
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:47 +0000

    our estimate thinkthrough mend tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
