22ec09d6dcba60024f5317e3a3a0655e137ec6b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:20:31 +0000

    respect over improvement start cover

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_max(numbers):
+        return max(numbers)
