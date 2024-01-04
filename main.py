6339498af95c4b9f3a9af9c9afa001f45ca272a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:16:43 +0000

    coffee with wash ignore central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def subtract_numbers(x, y):
-        return x - y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
