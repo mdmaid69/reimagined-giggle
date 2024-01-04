1452302fea5200aa6e98ce80beb5b571d4a0f325
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:51:59 +0000

    plain country build 569enjoy between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_itemsize(array):
+        return array.itemsize
