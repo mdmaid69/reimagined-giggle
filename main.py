a26648a7fe2f208bc34ecc302122a5f334aa66e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:51 +0000

    shock cope fat opinion properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
