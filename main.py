fb6d7e68f3bd8d404f54256be424f66fe22a9a99
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:41:31 +0000

    belong miss form maybe shop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
