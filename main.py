71aec03192b78b283e0697e12f07231b40d34832
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:10:47 +0000

    pinch act Mm impression average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
