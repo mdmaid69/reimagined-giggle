b84a18365332207715f99f7031ee1f6af5f3df10
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:05:06 +0000

    block bank immediate stamp pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
