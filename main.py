0fd1f42895bcb8b546d6bb6f83cefc5d836b2c13
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:40 +0000

    pot over oil child success

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_index(array, item):
+        return array.index(item)
