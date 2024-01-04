19d5a06eb1ff2bfa58c744ba7b23e8875776ce88
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:47 +0000

    special he translate that large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def close_database_connection(connection):
+        connection.close()
