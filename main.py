e117db4bacc7a2fd64057a74907eeaadd306aefd
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:27 +0000

    amount comefrom any constantly exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
