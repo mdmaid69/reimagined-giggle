19f071db4ce5c8de34ea957cfa0d3fafbf670373
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:33:20 +0000

    block bend benefit direct stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import os
+print(os.getcwd())
