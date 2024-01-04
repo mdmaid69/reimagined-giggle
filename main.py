33c41a248e17b81a66ce9c48d8883107fd519c93
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:51:48 +0000

    part pretty convince raise dull

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-print("Hello, world!")
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
