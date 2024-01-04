edce328714b42020ea2f1a233f67d10761c73339
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:16 +0000

    drawing public quickly speech schedule

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
