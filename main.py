a0af349dd7081e8f54cfa34ad5b7751cd8e4e646
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:33:26 +0000

    workupto difficult hold top stare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_list():
+        return collections.UserList()
