49971e018a979ba9604f3b5b9825a23bfe937b09
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:00:51 +0000

    present careless sleep settle difference

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import getpass
+def get_username():
+        return getpass.getuser()
+name = "Python"
+print("Hello,", name)
