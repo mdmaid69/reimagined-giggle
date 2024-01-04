8631e10cdc926aa826c31533c2ee680345c0cbad
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:23 +0000

    sleep need live lead reaction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
