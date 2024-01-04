016d8ccf3c5c2bbf80cdcf9e45d69d7a2994bcc8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:15:33 +0000

    rich language delight raise style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_set(array):
+        return set(array)
