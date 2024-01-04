d1e6113e11563bf4306a3a3a8865ce2b27474f81
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:17:14 +0000

    giveaway list involve bend afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+print(os.getcwd())
