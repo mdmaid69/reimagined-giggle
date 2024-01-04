d0c4fc49f6d5723b605e262eb8e168b77689f8a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:39:50 +0000

    comein sometimes move lookaround disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
