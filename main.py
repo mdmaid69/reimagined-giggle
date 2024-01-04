5561255bdde009525a116bfb9e2825802d159c1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:56:58 +0000

    scale path bang kick fine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
