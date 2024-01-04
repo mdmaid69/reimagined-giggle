d8d04b4dd707d3fc28c71726b13654e3001135df
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:02:07 +0000

    shout birthday similar force letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
