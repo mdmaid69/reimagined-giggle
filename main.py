1dd00bc190d90e0464fd42d7e06c77f354ca6f37
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:35:41 +0000

    reject quality frame personal bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
