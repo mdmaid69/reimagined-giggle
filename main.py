ebca3fbdac95269c36c258e28181c8364a98a1b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:57:45 +0000

    unable climb there move occasion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def square_number(x):
-        return x**2
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
