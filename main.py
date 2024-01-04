bd7e40fbeada72e56656689d3a72e3eef63cd9d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:54 +0000

    comedown count name stop attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
