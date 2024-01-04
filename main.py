2ec37fad50746e7b79e5ea3154083c772cab9053
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:38:28 +0000

    near offend know possession serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+print(sum(range(10)))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
