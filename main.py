ca76605cb60adc2426a9ee70dbbe23de1f76141c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:25:13 +0000

    cause workout use mail wall

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
