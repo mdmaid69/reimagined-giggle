3ea431cc8f2ed0897afa576295c17caf5e4a0e62
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:14:18 +0000

    hope own responsible general frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
