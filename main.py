83f09eb0a43ba57d5acca1eaccc28e0649fe4f7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:19:22 +0000

    no valuable reaction free once

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
