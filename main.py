4946b2401a33eba00958c792560c2248135f4610
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:36:55 +0000

    accordingto wide smile permission foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
