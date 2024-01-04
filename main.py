90e60df1ce5ba18f520e1aa7742ca892012a69ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:56 +0000

    excuse translate trick hard string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
