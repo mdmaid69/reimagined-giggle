4ae5ebdb13764d617408280370b13093bb16b6c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:05:05 +0000

    combination cry life late spray

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
