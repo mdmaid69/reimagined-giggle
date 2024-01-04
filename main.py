37f4cecc3a5c1852e34d190dcbcdd532e11de1b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:26 +0000

    measure provide praise simple test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
