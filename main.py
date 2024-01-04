04f5e19fd4837516e16391c5500e8b0bb73d8222
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:18:30 +0000

    fault grow alone money emphasize

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
