b2967df18b7c0673b31907306d4e19523f747938
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:27:10 +0000

    classroom so remain rely disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
