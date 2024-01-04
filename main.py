8b51fd8cad4ac2991f80052618c19bfa71652592
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:38 +0000

    worst although careless also tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import datetime
+def get_today_date():
+        return datetime.date.today()
