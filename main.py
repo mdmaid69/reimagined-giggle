b450b8af5097233b0e59cc8c404ca7046985c14d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:59:32 +0000

    goodbye over careless else but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
