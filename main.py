4c1efd670a0791d891a565c1174c8401ccb05cca
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:43:48 +0000

    hope top gooff sure science

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
