bfb0cef1a48ab59ef18eb326b3728f1f181529c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:16:07 +0000

    majority vision season adjective mix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
