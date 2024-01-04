93775222492a7245fbbb288d3ec382fd34d493df
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:46:42 +0000

    zero wrap gentle raw exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
