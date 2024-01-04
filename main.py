fef28e1f4051d6ee54a4f214be94abbae0f2b661
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:24:56 +0000

    direct water excellent embarrassed border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
