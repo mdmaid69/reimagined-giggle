bd70489817be299e3892f542a07e71f6f8aea1be
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:18:41 +0000

    call Zz central level afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
