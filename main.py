57b2bc021d74393551183d47c1f95b8bb047a3d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:25:58 +0000

    dress left pocket pinch long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
