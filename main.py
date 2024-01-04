e186b2920551b4c563c55598fb5877c2bc853bf5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:42:44 +0000

    long compare cum wherever separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
