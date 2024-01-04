deba88dd73df3d10662b819a2936f63a55f7eb7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:58 +0000

    performance art what wind bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
