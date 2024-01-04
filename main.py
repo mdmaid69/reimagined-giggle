a0f4c4b636cddfa13921ffc5f690907d7d9631d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:05:17 +0000

    tie Vv set winter where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
