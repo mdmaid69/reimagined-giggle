28d3fdc3f115405c93fc15e958f2c25ecd2f6251
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:32 +0000

    happy want runaway dream solve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
