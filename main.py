94627665ca32b1abafa32c3c121ca5350817510c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:30 +0000

    careful warning fair own relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
