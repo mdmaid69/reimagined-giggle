4e10913ceaa4808b1f5b39d715f5964e6ca2e4d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:02:05 +0000

    worry indeed unkind uncomfortable natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
