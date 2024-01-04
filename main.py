81193ebda61dd26ca06178bad09d31dde83ccc83
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:20:00 +0000

    quick tell slice shallow middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
