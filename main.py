2c76ecef21fda5ea204dc731cfe15bbf2de79964
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:26:37 +0000

    pool or workout damage operation

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def find_max(lst):
+        return max(lst)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
