53ad381cc50435d602931d574e9026a4f5a4d1ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:13:14 +0000

    might journey respect report interrupt

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+for i in range(10): print(i)
