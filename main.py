d52a50640077d4309098da8864917ad35c915cc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:30 +0000

    typical communicate left wing reduce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
