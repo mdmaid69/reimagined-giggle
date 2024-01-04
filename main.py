47dd0b784a5d74bd479d0be019abd49be4957bfb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:48:02 +0000

    spare television field shut previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
