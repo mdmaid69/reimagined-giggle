aca60e10af35a5c2ded256200488112600d3a42a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:07:46 +0000

    early runaway through tape bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
