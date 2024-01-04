f6af0ee31a11b983d6728acdc6c9501a80ad84c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:50:54 +0000

    thirsty climb conduct Ss mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
