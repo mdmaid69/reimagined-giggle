6e2504126cee2d7246b4e874c3e7d2e59a91ec61
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:13:14 +0000

    sale us its thinkover software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
