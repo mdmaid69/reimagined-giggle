43db6bba369285b4936b8475284f90ad56e96ed6
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:45 +0000

    note heart comefrom really failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
