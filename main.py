f5260d3eabccd318b07c90a1dd23aa83e3e1a248
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:42:27 +0000

    deliberate fierce Ss to part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
