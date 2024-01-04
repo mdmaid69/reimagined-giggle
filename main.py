d914f870de5b172046ba84e5660fc32e4523812d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:09:40 +0000

    talkoutof once man occasion replace

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_volume(length, width, height):
+        return length * width * height
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
