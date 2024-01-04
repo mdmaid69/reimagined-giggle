b971e3d5f1f62697eef1de87acf065c6fd371a7e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:02:59 +0000

    leave hurry fairly closed bag

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print(os.getcwd())
