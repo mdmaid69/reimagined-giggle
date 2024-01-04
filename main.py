6177e7cbab64c740713ecf76f8477d57125a1272
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:21:13 +0000

    adjective measure better belong rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        return sys.version
