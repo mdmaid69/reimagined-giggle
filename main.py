0b12f7ebf2e98eabca88414ee3648e2c35d0f36d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:20:52 +0000

    shoot convenient formal of middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
