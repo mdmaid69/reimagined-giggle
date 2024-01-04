ae5a5e94db1d0085e1521126e2f358f81fc6badd
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:30:09 +0000

    themselves start bringforward keen frequent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
