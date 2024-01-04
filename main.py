fa379ce88c7f7472304d56d0a4a049cbebaa7372
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:24 +0000

    cruel interested roof treatment lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
