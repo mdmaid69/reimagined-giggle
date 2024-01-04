adb7e36ccf4deaefd019c2356bd44660e2ccb97b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:08:21 +0000

    callat world sweep male command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
