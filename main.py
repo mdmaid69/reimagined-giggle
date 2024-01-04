d81b37707f0ca428d39503efca524c2ffdd368b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:59:09 +0000

    unite decision gate set occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def append_to_array(array, item):
+        array.append(item)
