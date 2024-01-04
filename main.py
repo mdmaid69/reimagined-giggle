19def38ca4344383c9afc2d278101664028eb286
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:06:33 +0000

    when wear clearly side lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
