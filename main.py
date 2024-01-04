7b616b630790d93a87e48f282997a3845c9ac95d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:32:44 +0000

    frame sudden slowly jump work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
