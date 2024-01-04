6ca30351bfd33d8a85218bc78e92df04c0912910
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:41:18 +0000

    what suffer correction deliberate eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
