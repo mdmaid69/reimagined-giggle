90b9584a82c2472e7c7b388a4de4afaa7995eaef
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:38:55 +0000

    far pull in unable than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
