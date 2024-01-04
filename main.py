5431e875a82e517cdd4872e98be2515b604d21f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:35 +0000

    pin anything back view call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_acceleration(speed, time):
+        return speed / time
