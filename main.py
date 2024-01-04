a1cd6c38dfbcfdfdc49b6cf1a29d04adcc7d7189
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:34 +0000

    here ordinary through exaggerate set

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_power(work, time):
+        return work / time
