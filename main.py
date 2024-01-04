ae9e40e1b90b3cc60e9beafd846e63a8b3546974
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:27 +0000

    let quality improve typical safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
