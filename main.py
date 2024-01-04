bc5dc43b57c917766a13ae8bb4d578c88e9608a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:58:50 +0000

    than special let dare drop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
