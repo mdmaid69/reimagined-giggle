3a5ab741f5b3314d207a7edc0469272c71274ca7
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:39:24 +0000

    pleasure free feeling false promise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import time
+print(time.time())
