b6632e124397814035c70e80b3492d472b982deb
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:25:56 +0000

    score steal black phone day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
