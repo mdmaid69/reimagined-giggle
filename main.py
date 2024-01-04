0a085af44b50f462057e469f41bc30acd1bcc31d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:05 +0000

    chief depend violence waste flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
