2a1468ad6f5de7a2d82e9ff4d307a9ba2b1c9c85
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:57 +0000

    suckme badly black nigga life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
   def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
