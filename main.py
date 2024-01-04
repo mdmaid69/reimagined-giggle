9a52ad26b2a40af14fab34f0e5540b5e8b309e62
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:59:38 +0000

    chin afternoon rescue approve worry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
