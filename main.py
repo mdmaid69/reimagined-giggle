7667000b979981531976c1a79ca5024ef26e250c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:26:14 +0000

    whom shelter increase right issue

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
