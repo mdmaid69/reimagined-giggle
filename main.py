bac963814d2e7da11e079d3aba3ccf4ec0e29d05
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:49:56 +0000

    development watch collect mend call

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
