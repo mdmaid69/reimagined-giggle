4be69897a36b38bb98766080f3381feec48724c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:09:54 +0000

    basically situation thankyou no tape

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
