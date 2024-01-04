3a095eea91e8a8b1cfbc061100d605a460d8780e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:11:05 +0000

    finally strike bringback giveaway listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
