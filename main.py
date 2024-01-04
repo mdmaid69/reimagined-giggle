f41300a6df9b5d35d4f28fc81475f33be638af0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:04 +0000

    concentration stir shake thirsty second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
