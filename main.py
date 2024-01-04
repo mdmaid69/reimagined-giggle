445aa5ba2ebd3776980e7ab2355f8b7458f0ebc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:50:53 +0000

    gentle flight hang empty make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
