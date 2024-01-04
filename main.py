887b0c9094865878c4a8ef186138592c90049d77
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:14:13 +0000

    sweet towards patient unite cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
