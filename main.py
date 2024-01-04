43a709d9aeb0a84227eb5ca1718cde7d159107fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:06 +0000

    determined second chase flat bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
