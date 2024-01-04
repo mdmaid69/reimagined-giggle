403e4930df859bfa2871ef0266e3fd4608ce3c52
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:04:08 +0000

    spring may pity justice learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
