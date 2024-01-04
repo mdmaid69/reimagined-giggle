e0a434bb0390fd68881d45c18ead96501e6713ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:32:17 +0000

    float rule behind let fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import time
+print(time.time())
