0b601215b6fe52af79a7ae84b58e839a712eb8c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:26:58 +0000

    trust cry unless mouth born

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
