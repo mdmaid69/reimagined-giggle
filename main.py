1e4db4e162213593a89c3da3891a5a7950f7a9e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:01:04 +0000

    violent black getup lunch habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def exit_program():
+        sys.exit()
