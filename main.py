3fc65c5fcefc09ce6f553142df67f34198d5105c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:42:02 +0000

    borrow reasonable perform once snow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 def calculate_speed(distance, time):
         return distance / time
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
