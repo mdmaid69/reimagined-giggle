c754e6186ba58311b1c34d2d0e17618bf0c136fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:15:12 +0000

    pleasure cope cruelty market clever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_perpetuity(payment, rate):
+        return payment / rate
