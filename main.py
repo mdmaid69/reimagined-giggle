7103ba45766d700f576d474400e461968988c5d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:52:35 +0000

    want instead force Ww scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
