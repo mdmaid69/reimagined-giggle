e0f38b71be1cd924926e6d68e35fe35d39fb03f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:33 +0000

    reliable effort enthusiastic dead declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
