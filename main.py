9c8b70b9e6e2ea0944597dc782bbede45ab588b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:13 +0000

    thumb turnout rid emphasize pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
