8fc23b5b20b83106d915f5a02fc2bb50a7810192
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:11:52 +0000

    cut afternoon honest left confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def subtract_numbers(x, y):
+        return x - y
