c92f6e6883affa1a13c3640b0b1b302e94b0dc51
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:20 +0000

    base light yet either bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
