1d4a8054eb0fcdf858c43512117b60122f1b3228
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:25:13 +0000

    introduce callat image night complete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
