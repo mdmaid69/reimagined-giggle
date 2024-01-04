aec135a56259a31be92c9d1dc45f4933bb45c055
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:26:30 +0000

    summer underneath sink among stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def square_number(x):
+        return x**2
