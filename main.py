fb5ffccfccf21dab65abbf8fdd6db999b85e0efd
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:20:59 +0000

    wheel fast regional in base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
