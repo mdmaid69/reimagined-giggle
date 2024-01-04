d66083aa5bb00c69b9d142f65208a5beee7715b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:50:10 +0000

    actually I tense full best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
