ac37c88882a25af38642f1764041a7fe0c9b4acc
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:45:54 +0000

    Oo family stone few such

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  def subtract_numbers(x, y):
-        return x - y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
