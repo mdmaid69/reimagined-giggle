d661c1210e121c3af8746683eff9836ab6e154d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:59:04 +0000

    comment herself deal angry substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
