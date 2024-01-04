2a8d06675537f9c28fab1af7fc0fc07453870ca4
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:07:56 +0000

    risk fit bright pinch deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
