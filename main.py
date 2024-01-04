25d23787f150c3877d7e96e62616fb3cebad0f2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:58:08 +0000

    round regularly note goodbye everything

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_length(array):
-        return len(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
