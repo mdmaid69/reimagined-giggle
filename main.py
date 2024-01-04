9d35c3bbc352336990e6c3522f80f3cf9755d110
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:16:26 +0000

    accommodation divide geton cause whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
