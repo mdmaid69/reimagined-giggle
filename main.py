5ebdf2c41587cc54574b84cf48895c3ec6be900e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:14:40 +0000

    sight target gift close really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
