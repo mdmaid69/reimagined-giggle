f1f8c602fcff9a1dc96b76ada90fe760468ef582
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:33:10 +0000

    worry promise middle return type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
