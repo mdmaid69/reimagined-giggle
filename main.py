cefd4ef7dd6af32486d65851a81149f35598a906
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:42:49 +0000

    hide interfere stick comment bottom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
