6ee0537e06d5174d12dd59e34df82ae0be7d389a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:34:43 +0000

    spread spread mark use motion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
