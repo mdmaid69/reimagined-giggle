3c59847f509174f06ad333f13becbbe91f9561b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:28:14 +0000

    concentration boil doubt office tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
