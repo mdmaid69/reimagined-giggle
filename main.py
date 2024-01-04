b8dca054baf5b114f6b94c3c0edc9388b63ae18a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:04:04 +0000

    accordingto everything television whether breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import time
-def get_time_since_epoch():
-        return time.time()
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
