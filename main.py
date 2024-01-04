d2010812c6cdff199969d27ce39b048d72941761
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:40:08 +0000

    566end fault hide probably period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
