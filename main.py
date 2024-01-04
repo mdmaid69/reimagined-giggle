44c7719e8574f0fed1aef0679dd7075ef56cbaa1
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:51 +0000

    heavy excuse surface another several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_ceiling(x):
+        return math.ceil(x)
