93cbf2e078b13157954a152896341b1fd8419df6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:02:56 +0000

    spite police check race in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_typecode(array):
+        return array.typecode
