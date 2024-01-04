c414aaebe663188a6a34e9ad4a33e9f107a07342
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:08:57 +0000

    concerned save fold best goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
