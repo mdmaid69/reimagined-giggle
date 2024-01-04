a4c41f6f2f1c926656de3364af23f981fc9e2012
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:01:32 +0000

    conduct generally country moon shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
