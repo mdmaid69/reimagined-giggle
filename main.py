03e5ad27b2b733abc481ac025679a53027ff0ed5
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:34:07 +0000

    stay afterwards straight face freedom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
