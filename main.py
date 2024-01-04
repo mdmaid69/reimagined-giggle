b3e5c09839756e44dc379b55c0c07d574d570197
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:25:01 +0000

    fence answer share professional field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
