eac6eb66f7ef487abadd0a49d2cd331124f046f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:12:19 +0000

    close regard tie platform relation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
