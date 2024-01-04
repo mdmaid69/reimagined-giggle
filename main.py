417ab09ae4b9e9d8b173e0d4c2a4b0b86094e86c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:53 +0000

    circle turndown spray party Qq

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import random
+print(random.randint(0, 100))
+import array
+def append_to_array(array, item):
+        array.append(item)
