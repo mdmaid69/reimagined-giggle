6626a3cba215304c142ab2d60f78e2579264b5ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:07:35 +0000

    payment whole fine square direct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_complex(array):
+        return complex(array[0])
