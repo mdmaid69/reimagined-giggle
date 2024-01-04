0e99022f2d597b03fdc2d907ed0ae565c7a8e37e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:30 +0000

    bound themselves mood area region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
