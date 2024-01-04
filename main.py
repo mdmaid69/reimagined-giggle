05bd5f67446fefad760bb3fdd7d8b0ffe4835f02
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:10 +0000

    point over grand neither takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import sys
+def exit_program():
+        sys.exit()
+def find_max(numbers):
+        return max(numbers)
