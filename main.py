08ea8d9195c7f17900fced1dc55faa696c9e9b67
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:54:43 +0000

    ill full reaction confidence sure

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
