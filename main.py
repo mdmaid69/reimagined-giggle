16f9eb163c76ea2f1e3175522ec952024aaa20ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:40 +0000

    remove report meaning person keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
