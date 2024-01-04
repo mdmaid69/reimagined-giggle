7fa7a63e84dc82c416229d6bfe9a13f3c4ccabce
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:27 +0000

    interested present pull pile finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
