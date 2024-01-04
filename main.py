22b5a97ff57fca31ddd8cfeccbac019a28561778
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:57:20 +0000

    concern proud off control shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import collections
+def create_counter():
+        return collections.Counter()
