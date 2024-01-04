cbfaa20eaf4dc5b8d8109b2ee781cea2727d3a68
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:44 +0000

    available depend treatment return skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
