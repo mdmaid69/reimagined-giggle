5728e05b2b8cdb311ca5dc97481ef5020c22099c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:22 +0000

    moment punish score turnaway comein

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def cube_number(x):
-        return x**3
+def calculate_pressure(force, area):
+        return force / area
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
