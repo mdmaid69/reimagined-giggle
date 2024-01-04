17115bd1b55fc9f2e799262c5d62d6db0d678ee8
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:52:06 +0000

    comeback suck wrong be national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
