c8bb3b60153c5ab45e4e07926c11a8ccb0bf7538
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:46:42 +0000

    human capital deep tour count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
