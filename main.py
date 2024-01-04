a574b064ddddf2b6396518229d73780c6cb791eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:38 +0000

    thinkthrough pour glass pain into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_area(radius):
+        return 3.14 * radius * radius
