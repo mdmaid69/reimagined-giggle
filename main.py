0a507c39f4febaadc93230b99f7772e2fbb3d69b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:34:28 +0000

    its operate reject include sign

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+print(re.match("h.*o", "hello world"))
