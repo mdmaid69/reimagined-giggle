093c0b30b9ce0f5da129857a80a4277b6b161534
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:13:21 +0000

    line far mass common giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
