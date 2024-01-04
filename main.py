36b8df340e44f29217dee69c09d54b44c6f123dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:09:55 +0000

    cool scream Nn throughout holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
