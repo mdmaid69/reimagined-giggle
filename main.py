eb50804d6347010d15e2a12ca78db28e264ae7e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:07:02 +0000

    second talkoutof makeinto process slide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
