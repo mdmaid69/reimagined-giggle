3bec1ac8bbb63b506bef77fd2fef05ca33fa04a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:02:50 +0000

    repeat calloff run mood attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def remove_duplicates(lst):
+        return list(set(lst))
