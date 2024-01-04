1b3ea1f8f45bd304fefd05817b6f4d4c9025b442
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:27:56 +0000

    method odd stupid payment home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_power(work, time):
+        return work / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
