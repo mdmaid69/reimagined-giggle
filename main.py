7b58533556d71553110acdf3165e772d9ece50c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:16:57 +0000

    since involve takeover identify discover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
