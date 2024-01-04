2617a55149339a5fd4859a233f487f5faa49eade
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:36:30 +0000

    nonsense famous exercise his mend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import platform
+def get_os_info():
+        return platform.uname()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
