ac241b0ddb40f1804f232c1a03ad05e8329f2604
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:08:41 +0000

    connected pay copy holiday explanation

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import tensorflow as tf
+print(tf.__version__)
