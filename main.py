e0d9fe3d56356620c8f076aed002cc6fa47ac6c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:05:50 +0000

    put agreement minor would university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_floor(x):
+        return math.floor(x)
