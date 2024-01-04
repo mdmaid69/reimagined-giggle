873a88bc88b239dc2f90e013d0b80c39ec53228f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:06:28 +0000

    once emphasize television makeinto nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
