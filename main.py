52702782a638617823702c3d120bbd98fbefd39f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:01 +0000

    movement line lock opportunity method

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
