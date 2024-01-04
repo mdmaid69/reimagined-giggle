3cbf7c54572f092312e91087dd94f5965b0809a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:23:11 +0000

    possibly goon furniture along wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_density(mass, volume):
+        return mass / volume
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
