221f4bd16b9121278f7372b4c918927ae8d2ef37
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:49:06 +0000

    patience further dangerous relative talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_os_info():
+        return platform.uname()
