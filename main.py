5f97af1f06428a6c08cd77b5c05a0f738d5e2598
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:01:47 +0000

    top mean design computer perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
