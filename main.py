ab85940822b54d4aabe5f3bcecc0468aa0a5474e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:45:14 +0000

    influence defeat cinema complete nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
