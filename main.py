1946174ee504adbd7909133b96a9304f61bec7a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:15:29 +0000

    clear war sport future work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def find_max(lst):
+        return max(lst)
