c1fa8e1e4904333327eba22ef8f593bfad0ee732
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:58:20 +0000

    music below drive loan corner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
