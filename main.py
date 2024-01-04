73c3b2b08f5138024234a0f73b8fe7ad25555630
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:49:09 +0000

    out thorough beside believe order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def clear_array(array):
+        array *= 0
