3f816436c483dce4040ef92298a10b0d058c1d30
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:41:26 +0000

    standard middle noone roll getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
