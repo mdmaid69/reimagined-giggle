7b6a8ac22b6950931b7ab7c5cc3bcaf1404ec8f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:22 +0000

    charge level mood star grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
