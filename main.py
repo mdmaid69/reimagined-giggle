3cc9492b13068ce164432820ed227f6fe2a79b49
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:51:39 +0000

    gate top fail guard disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
