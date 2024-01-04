fc594bf4f4c755acca47f8d9a54a7dfe2aebdcd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:11 +0000

    cool typical protest there declare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def find_max(lst):
-        return max(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
