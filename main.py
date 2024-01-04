1c4154d61a1a09f6b50fca69559e896018b1b01a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:12:28 +0000

    star entrance company lead pride

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def reverse_list(lst):
-        return lst[::-1]
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_odd(n):
+        return n % 2 != 0
