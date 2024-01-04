8f3955673d2936c5d31ccaae4baa8c79ee352589
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:37:43 +0000

    something glass smooth figure hour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
