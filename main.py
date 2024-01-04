f369bc212d7e536cebd778bb3d0729efa064c63d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:47:18 +0000

    makeinto period callout way size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
