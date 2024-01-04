d3b667bead0e0891e25715737175ed7a5f502135
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:05 +0000

    impossible below especially trade crash

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
