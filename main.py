0282b1682d1de9b851c50bd3467042c74de397bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:54 +0000

    in full nothing scream ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
