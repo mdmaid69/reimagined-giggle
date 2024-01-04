f823323919db1f260911bfcab568867e8ee3478c
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:30:13 +0000

    rate 570enjoyable seem relief spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
