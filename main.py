bded5d227d51ee13061a5b257aab6063e89b64ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:25:58 +0000

    feed pretty plant language crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def sort_list(lst):
-        return sorted(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
