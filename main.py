29626a11ead280bbbc7059366ab66c647aa94308
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:46:21 +0000

    sight inform print theirs use

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
