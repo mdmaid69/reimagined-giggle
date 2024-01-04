67eb71b41a8cc492357c004af93000f4e680bb21
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:07:09 +0000

    out choice brush hour believe

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
