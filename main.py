c1a21c328b43f6cdd674d3888d52ff6aa9fab413
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:19:41 +0000

    formal programme meanwhile dream university

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
