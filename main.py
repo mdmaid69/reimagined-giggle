ae7d729ea47f2ba13477e92e69100f64cc6423b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:41:22 +0000

    fire excuse trust plenty neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
