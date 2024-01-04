6a9571c2166cd88941a3d5835318977f792f5fff
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:54:13 +0000

    crash lonely convincing mix bringforward

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
