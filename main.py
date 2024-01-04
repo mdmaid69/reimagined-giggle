c0a421abf7aaec805f9e0fec25a168df1c13bb8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:16:02 +0000

    modern trick concerning sound pile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+print(re.match("h.*o", "hello world"))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
