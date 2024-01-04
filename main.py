3268ed737c187db40da946aaa11a0c6b373798c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:05:32 +0000

    spin entry while communication confident

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
