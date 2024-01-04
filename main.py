ec931aec7c86289b79ad8f8d07a2ec5008425fcf
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:49:25 +0000

    moon lay anywayanyhow scrap smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def check_if_array_contains_item(array, item):
+        return item in array
