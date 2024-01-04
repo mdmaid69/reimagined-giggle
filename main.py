d92232e484b96078ee4163219124f68bd044161f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:53:51 +0000

    evening hollow mend fit store

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
