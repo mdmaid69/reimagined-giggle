72e886b4c3e4d0547822d8146808b0f2d240a4e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:16 +0000

    strong unless once Uu aim

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def square_number(x):
-        return x**2
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
