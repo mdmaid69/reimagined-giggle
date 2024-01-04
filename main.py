1eca494608cdeecc4204996fa265ea6d5e80c63f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:49:18 +0000

    quick big further human waste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
