d4172e57a29fffeebee1273b29aeed0029ae888a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:23 +0000

    language skirt dangerous climb even

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
+def find_unique_words(sentence):
+        return set(sentence.split())
