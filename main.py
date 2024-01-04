f09439fd7ee78447b74debca622f66f973c6a3d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:33:51 +0000

    second live bored smile 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import platform
-def get_python_version():
-        return platform.python_version()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def convert_to_octal(n):
+        return oct(n)
