de2ab712c2a4162dbbf821a3aa182230c798902f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:25:46 +0000

    keep open luck block attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
