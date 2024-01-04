f12babffbee5facf9efc144dcb812b7d77d8d830
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:20:21 +0000

    positive fight mainly stare remark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+def is_odd(n):
+        return n % 2 != 0
