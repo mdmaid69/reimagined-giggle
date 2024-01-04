9e1a96598f1ae17e0056a0046dabb084a942373b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:38 +0000

    transport appeal whole form company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
