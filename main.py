7b2597d441cb8af2b84cd8e37df890c655004a90
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:30:46 +0000

    interesting shoulder piece keen show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
