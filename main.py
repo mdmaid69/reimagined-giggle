1dbdcf4d16c636928bb7133923e05747de92b50f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:00:04 +0000

    past murder several form ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_power(work, time):
+        return work / time
