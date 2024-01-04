e50f72b5f5a15e176a524ff66ac933406930754d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:18 +0000

    probably satisfactory live generous way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
