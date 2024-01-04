ec72cc4b49c38b3da6b402b14c1b9db6012bd409
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:48:03 +0000

    gradually advantage mind country run

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Squared:", [n**2 for n in numbers])
+def reverse_list(lst):
+        return lst[::-1]
