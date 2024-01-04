0f1090adbfd2624dd676380be8bbca8a4db2290a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:43:05 +0000

    very seem fit advice wherever

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_force(mass, acceleration):
+        return mass * acceleration
