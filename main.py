f1d2dd1d292e2f68358841ee65ccdf90907d9d82
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:10:44 +0000

    straight embarrassed wash tool of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
