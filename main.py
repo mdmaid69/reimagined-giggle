7a64c4e4b677515580394592a4b257235f97e1e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:44:11 +0000

    gradually design teach justice curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
