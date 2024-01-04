ec85866d54aa136d510896bb43b81b6711718756
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:47:35 +0000

    rely present copy scared dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import platform
-def get_os_info():
-        return platform.uname()
+import os
+print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
