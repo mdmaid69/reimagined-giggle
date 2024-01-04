3fe61693627d374684ce91d88be40b9ba094c156
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:29:33 +0000

    climb influence snow apologizeapologise calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def convert_to_octal(n):
+        return oct(n)
