397aa3dc9dcb5bb316a35964c829b0b090a7bb35
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:58:39 +0000

    best sudden prepare blue protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
