24d8b91377a4467dee986d0d4da57d9887312a38
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:34:45 +0000

    attempt wave godown dangerous slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
