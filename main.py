21b86246f390a2289bc15fd2997bb42b5151ebfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:18:28 +0000

    bright burst across sting wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
