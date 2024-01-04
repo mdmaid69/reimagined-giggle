bfcb7b09d357046a6efdd4a3b68ffa94c1ac0603
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:51 +0000

    about wire success drink knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
