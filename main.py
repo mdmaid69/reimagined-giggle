c9471a86704e0cf1defd16ee33582249cecafe39
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:06:35 +0000

    callout tend remove space lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def square_number(x):
+        return x**2
