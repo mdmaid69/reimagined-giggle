5c643477dd6ea56721874ed5cd89c8208329aebc
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:25:14 +0000

    damage fit independent authority middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
