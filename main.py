9c5ad61858b8cff1d0b8cb02c24ea8c6a80d86de
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:09 +0000

    demand what range marry interview

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+for i in range(10): print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
