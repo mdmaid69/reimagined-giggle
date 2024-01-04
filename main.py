291c292d6a5de0d962aedc4e3936574be8d954f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:12 +0000

    pattern fair proof capital clear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
