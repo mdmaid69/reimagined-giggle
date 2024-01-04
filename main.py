4f8b509a05f84feb50500574eeb30686f38ff5e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:56:56 +0000

    crash responsible bury close typical

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
