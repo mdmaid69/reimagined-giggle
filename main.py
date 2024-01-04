65ad9103a9e933c50772c3707cb253865c3dfbf2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:25:02 +0000

    turnon will heart wide skill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
