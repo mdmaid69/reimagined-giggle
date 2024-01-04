a98ac3ac72e66394c2256fdacbaaecb164dd0376
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:58:17 +0000

    blame divide picture reserve identify

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
 def calculate_speed(distance, time):
         return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
