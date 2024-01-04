968b86a9a9c69c50219185579b1f20c8977d4da8
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:49:17 +0000

    number everywhere achieve quantity building

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
