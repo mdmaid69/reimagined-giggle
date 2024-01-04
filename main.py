514bd8bd03f5a4faba5c4d376747b9616dbd9373
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:07:49 +0000

    back convinced base bridge rise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
