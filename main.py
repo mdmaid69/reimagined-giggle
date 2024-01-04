88fd200e7efd4b5b8d4c2b4225c921becfb96bb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:16:57 +0000

    theirs confidence find produce forward

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
