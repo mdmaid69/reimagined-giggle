121604531a32aa09e8621d1c8f6fcc2c640ab1d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:06:49 +0000

    certainly near team cough hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
