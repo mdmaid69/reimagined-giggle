6f1d05fdb5e86263ac43c392585e69fe5725aa5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:31:32 +0000

    claim smooth peaceful flow sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print([x**2 for x in range(10)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
