b2edc0cd0f00101ebfaf676ea142db9ce1f88501
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:19 +0000

    suckme calmly hardly price usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
