33eaa46a3f93b81c7fff902c522639b3c39881d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:15 +0000

    poison trust warning date closed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def cube_number(x):
+        return x**3
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
