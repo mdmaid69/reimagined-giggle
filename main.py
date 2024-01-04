c2b7798330a339b849b54b679bdcfebbe3dc5ab0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:22:13 +0000

    bread kind bin warm talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
