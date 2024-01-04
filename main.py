036e249cefc292155c90a1b35a127bcf4f9a8bd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:05:48 +0000

    fault people persuade artificial during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
