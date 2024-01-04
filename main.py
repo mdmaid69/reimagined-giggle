f90755d470d8d1db2d0305127672164d48ebd80c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:37:06 +0000

    successful fight clean children appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import time
-def get_current_time():
-        return time.time()
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
