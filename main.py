20aa9da08c1f84de20d379090cb44179d523149c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:13:56 +0000

    turninto pocket brown dull floor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
