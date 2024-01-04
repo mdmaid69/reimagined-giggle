d6a65866711030d6a26b6be4bf42c144945ac7cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:41:43 +0000

    communication range pure seriously branch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
