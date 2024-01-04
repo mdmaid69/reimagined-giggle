74825d61f98962863837b7c4eca0374f7eeb4e04
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:11:30 +0000

    confused familiar dream appear run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
