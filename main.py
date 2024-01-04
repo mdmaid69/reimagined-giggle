d5d13f3f82550b949b7d9cf81c943bafe770d27e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:47:08 +0000

    children impression truth enthusiastic hurt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_pressure(force, area):
+        return force / area
