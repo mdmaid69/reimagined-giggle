ca4aeba59b2768d882680a4f087690a2c77aa03f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:42:03 +0000

    write squeeze whichever Pp curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
