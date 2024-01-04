b3d4412efd4f92947e417e0fe2f6dece68b90596
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:06:22 +0000

    cost money 564encourage fail temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
