a0a54454fcbbe304c43cc2f058fefb34500c0b03
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:08 +0000

    drop race godown cross sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
