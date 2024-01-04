64eec5a8ac1cdf7a7f44524f6adfde981ba76195
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:58 +0000

    swimming broad organize term guess

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
