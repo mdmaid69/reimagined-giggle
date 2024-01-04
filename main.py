0badc6d76577d94ae925e4cc863cfd03bad6bdb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:24:04 +0000

    along produce confident nose along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
