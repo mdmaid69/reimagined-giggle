2f10b1012daac5e6940e7279c81516c473ea64a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:07:07 +0000

    disappointed declaration think first beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
