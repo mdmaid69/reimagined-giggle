4609212d55c98260aa1a61e199c5d37adcc31adc
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:14:05 +0000

    someone gas cook lookback(on) later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
