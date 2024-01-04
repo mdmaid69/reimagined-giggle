ce346a782fcc8ed1e8af8a98fc7a65ca058ed893
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:07 +0000

    mine car convincing innocent shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
