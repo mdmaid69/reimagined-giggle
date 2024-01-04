94a3ad81ae8813375cfb5449dedbd1b22a9a5fd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:02 +0000

    steer bottom woman miss condition

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
