1d0a74aad35d670dd7ee0fabf9e905a6153ec21e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:33:55 +0000

    under many become concerned past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
