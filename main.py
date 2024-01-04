1db1ae4585947635ce4ec1197b3c18df71d9ffe7
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:14:47 +0000

    exist pronunciation strict fierce fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
