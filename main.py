caaa7df359094e3b82bf42d557b571d05a234b0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:09 +0000

    anxious neck getaway dear apologizeapologise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
