a62035ae43dedf59a02482aee0801c939df58944
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:33 +0000

    imagine clean priority owe fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
