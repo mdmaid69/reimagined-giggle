c5414f5ab0b823f3b93879207833e4fda35cff07
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:54:56 +0000

    later believe comeround spring runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
