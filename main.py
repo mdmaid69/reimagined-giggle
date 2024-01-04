5ca7f2855fff2ee387ad343af8057c24d658da5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:14:48 +0000

    turnon various Nn disagree leave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
