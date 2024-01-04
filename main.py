23e31cca8db4bb7cabe86bd4a53d287735758f42
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:46:29 +0000

    565end do inside takeout university

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
