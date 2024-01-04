430b15712310be0ea08a71df2bd44de9a19ae949
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:42 +0000

    some education steer sudden his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
