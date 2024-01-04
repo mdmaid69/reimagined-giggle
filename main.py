07bd08faffe7fe66a883aa5451bbd98704bb01ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:39:21 +0000

    gas comeup damage child within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_item(array, i):
+        return array[i]
+def is_palindrome(s):
+        return s == s[::-1]
