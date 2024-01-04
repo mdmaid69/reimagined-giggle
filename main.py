c3626724e26c56f34e01db073a6422c354e691b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:56:45 +0000

    leg rid bang run before

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import datetime
+def get_current_date():
+        return datetime.date.today()
