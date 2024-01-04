58a86176442dd84f877e58628d28543d13e2b1b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:31 +0000

    disgusting correction old radio gas

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
