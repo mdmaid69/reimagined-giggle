dab54fa29f8ef8e02410131febcee9962a898f83
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:04:48 +0000

    no miss active until swing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def is_odd(n):
-        return n % 2 != 0
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+for i in range(5):
+        print(i)
