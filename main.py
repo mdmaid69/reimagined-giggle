4137e1b2b9594bcb2f3b953a2b30d122d531a0bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:31:37 +0000

    accidentally attempt decision listenfor damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
