44ebe8ccf9b61894b423742d79f455897f06bc95
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:09:04 +0000

    difficulty also hard mistake ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
