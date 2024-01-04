86b8643088db1172fabc545c21aa486c608ca63e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:13:41 +0000

    violent wish public benefit opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_power(work, time):
+        return work / time
