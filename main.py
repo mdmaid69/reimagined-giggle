3c92415d39f0550adbaf876ab2238226650fdeb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:53 +0000

    damage everyone comein beat raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
