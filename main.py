ec6f08b89f9f76ba51a54ceb0da734a4e72aa9ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:11 +0000

    minimum minor throw communicate picture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
