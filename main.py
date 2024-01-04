88e90d6b4505bf699e09879e9504f660df8aaddf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:04 +0000

    bill over it Vv artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
