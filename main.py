87312aa9cbf7147517258aa71ab4297137893144
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:24:26 +0000

    still busy development fast culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
