48f76bf01d41edac81cd0cdcd35b9b4aab6e6f6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:25:15 +0000

    rich pure set their tragedy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+print([x**2 for x in range(10)])
