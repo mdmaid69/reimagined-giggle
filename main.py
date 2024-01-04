c97b586c2a8c281ef866221ea4dab325bcf5cb09
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:23 +0000

    city old bread appear fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
