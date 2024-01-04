05f1cca7cef47eff3f29ebe4f2f804ae26aca7a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:07:40 +0000

    yesterday drink cope steep description

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-print(math.pi)
+def is_odd(n):
+        return n % 2 != 0
+import re
+print(re.match("h.*o", "hello world"))
