b2c4003a0a6f240713945326e9b0c244ef1add5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:16 +0000

    impressive popular contrast want actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
