c8e141be4f6fad01d794a938bab5305f68cfaee4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:45:05 +0000

    key grateful hit couple mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-for i in range(10): print(i)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
