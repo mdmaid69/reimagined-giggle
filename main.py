a263709ea6a5fe821ad6ea5389b64ccaa355635d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:59 +0000

    Jj children smell poison innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
