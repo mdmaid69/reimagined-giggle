5633dfebac01d15b7e65dd4eafe002561faeaf7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:34:50 +0000

    underneath funny mistake bringforward herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
