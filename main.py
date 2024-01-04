ff8e6c32861d76b69c09a6f896122d82d6346bca
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:58:16 +0000

    measure desperate statement weekend goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
