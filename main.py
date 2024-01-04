b7d20d3d2318dd18979380904a06be2dfc484d6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:47:54 +0000

    learn intelligent age far rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
