5c0d8282012d100af28142678e851fafd57e7fe5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:13:08 +0000

    maybe unless season sink then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Characters:", len(text))
