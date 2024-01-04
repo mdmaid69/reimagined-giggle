6532543ddd157ad260d38aec843fe7eafb074637
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:50:46 +0000

    exploration music rain bother agreement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
 def calculate_floor(x):
         return math.floor(x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
