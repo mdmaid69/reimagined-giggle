de67d3f0a73e0170b091134da122ff76200886dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:39 +0000

    cut rare just gas crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
