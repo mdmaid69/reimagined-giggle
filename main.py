dfba0252934aa3d6685817cd299d323a15475133
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:06 +0000

    aim reduce back cold confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
