341dfbed1fb235974aaae30326db46dfd2ea39fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:12:33 +0000

    on welcome community breakdown show

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def multiply_numbers(x, y):
-        return x * y
+  def is_even(n):
+        return n % 2 == 0
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
