5f4b2cd808442c0e93eda0ac289ca401f4469faa
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:10:05 +0000

    relief arrival talk appearance upstairs

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
