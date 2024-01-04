68fb451e2552a151414d635330caafcf70531fab
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:17:18 +0000

    float comfort legal be float

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
