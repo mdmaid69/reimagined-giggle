d219a90e214628dfd8bc9e082c64b3fec3538299
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:43:19 +0000

    everyone increase gradually appearance neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import getpass
-def get_username():
-        return getpass.getuser()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
