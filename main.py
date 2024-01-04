776457e25aa6530bb4ad7cc382a76daaca70dca4
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:06:14 +0000

    provide practical dark bridge transparent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+from collections import Counter
+print(Counter("hello world"))
