04798be64b29b317e37a8269665dbdfcd351e408
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:21:34 +0000

    work dress black dig run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
