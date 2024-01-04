0f20b51344f7aa9b030b080a3382a2ebcefda191
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:11:14 +0000

    near child result clean match

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def reverse_list(lst):
-        return lst[::-1]
+print([x**2 for x in range(10)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
