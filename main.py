0a336cb1cecfeaaaa7bf2392c93e419645598fc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:51 +0000

    copy disappointed unlikely average practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
