865fe9b2352a72ba885e296cd8a605d0a13001fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:56 +0000

    fail if instead trade demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-print(os.getcwd())
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
