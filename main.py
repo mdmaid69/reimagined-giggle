3ca691bc8ec38bf58d3fe58f5193a4bc8431cc8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:46 +0000

    world easy burst right but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
