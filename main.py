416f23892d992e717be4108f6f1b04ec962584ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:13:40 +0000

    nowhere harm either age unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import sys
+def exit_program():
+        sys.exit()
