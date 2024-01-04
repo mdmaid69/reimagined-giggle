7d7dd3c89a9205b890afca14cf7658c6e7aed68b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:08:09 +0000

    lend sink feel almost drink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
