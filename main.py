f10d172329398fdec81d70191387e985c064a5f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:55 +0000

    wear able within basic emphasize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import array
 def get_array_as_bytearray(array):
         return bytearray(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
