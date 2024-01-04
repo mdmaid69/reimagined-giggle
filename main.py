a4c7d26294471b13b9e6196f5adbe24ecffd2ba7
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:03:41 +0000

    birth likely discuss one mean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
