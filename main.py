7730f82c16cb7684799c24e809f0a168e46e1557
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:47:18 +0000

    harm blow secret feeling square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
