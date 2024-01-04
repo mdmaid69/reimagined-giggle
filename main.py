2047cfed471bfacc6c6725087b830e13a8f512f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:25:17 +0000

    rubbish twist gentle pollution sing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
