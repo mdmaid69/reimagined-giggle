faa0827dd25ae94f55f0ad953de1d78f02e407cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:47:47 +0000

    knowledge concentration control feel miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
