0891016f72066118d528788f4bc84a156ac64f44
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:22:37 +0000

    generous public personal bringdown new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
