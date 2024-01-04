80a7115bff4d9909ba3b55d2873faf46c447be1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:10:02 +0000

    special whichever acceptable tread backwards

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
