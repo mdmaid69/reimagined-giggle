453bec6e2cb9664be4d09dc5fe71353c0adc6eba
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:41:26 +0000

    on sink attempt arrive variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
