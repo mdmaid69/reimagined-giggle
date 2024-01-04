e4d7de8147c2244697cb4a3d2c3977c565496d5c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:50:45 +0000

    available case hardly object but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_current_working_directory():
+        return os.getcwd()
