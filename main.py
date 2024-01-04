980eb1a20a1f79f77788c62df41f68f4402e9852
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:20:36 +0000

    proper male seem war dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
