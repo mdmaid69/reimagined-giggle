35ea664af1f46d5b217b418a8f10c763e9820e31
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:58:49 +0000

    radio scrape eager water add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
