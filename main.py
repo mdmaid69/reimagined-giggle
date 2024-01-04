61e2ddf3f6cdb0ed2ca527f1f6ecea0948827b8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:51:22 +0000

    instruction before breakup control late

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_density(mass, volume):
+        return mass / volume
