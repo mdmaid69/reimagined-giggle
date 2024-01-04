02912843863bc63ad384b50f5ddc61ee2976697e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:19:07 +0000

    arrive middle now far hungry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
