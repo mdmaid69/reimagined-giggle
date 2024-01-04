5761839583a278ed13d23776ae4aae1e45092266
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:39:15 +0000

    Ii tough remember please wing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
