b45704be54cf7458618a31c908045cd4f986aa92
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:39:31 +0000

    turnaway food try goback(to) brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
