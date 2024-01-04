876ebeece1f0afd93688ce89793b213b014b8a50
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:00:00 +0000

    practise through boat put accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
