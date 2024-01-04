b9394c79fb72f81cad3c69580ba5e559f7a1d8eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:17:41 +0000

    around usual fail try satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def reverse_string(s):
+        return s[::-1]
