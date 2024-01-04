df356cc6f5c34b966682d3de6728f391a0220227
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:27:55 +0000

    please watch display result practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
