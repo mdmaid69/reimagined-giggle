3defea9b58fb57ccf111ae0550aeab8e7190ec4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:57:51 +0000

    absolutely must behind pollution dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import collections
+def create_counter():
+        return collections.Counter()
