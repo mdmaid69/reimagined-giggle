25e05b1c1cc980a7f0f9c52779a54c85298a9971
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:24:10 +0000

    bang takeaway surprising label nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
