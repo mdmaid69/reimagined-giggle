828d39523fb5c75d241e5fc3e15f662727962beb
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:06:22 +0000

    agreement unite careful attention unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
