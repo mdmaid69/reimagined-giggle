bb39c5e50e92d53c10cbe4466addf78e8782ab3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:58:57 +0000

    urgent tear cough frame inquire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
