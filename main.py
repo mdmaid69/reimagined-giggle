07547cb4440f54376e5b15f4c98663538aba35b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:31 +0000

    figure into radio yes rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
