f794429c6fb63cafeb176a5ae6956b7f2352d3e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:39:51 +0000

    pot contact careless father crisis

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
