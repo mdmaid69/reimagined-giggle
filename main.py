521465eb3467218c88087f5be8ce449eb8564adf
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:29:21 +0000

    damage write factory various honest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
