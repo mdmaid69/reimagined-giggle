177de1e35de96edfcb0aad546028d416bd977ba9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:22:02 +0000

    half teach perform hide public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
