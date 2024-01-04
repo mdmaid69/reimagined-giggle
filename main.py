ab366798e1cb8a33492790f050959a15640d3e12
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:27:02 +0000

    in most yesterday available finish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
