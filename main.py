744fe4e8abd572176e799221a780133aa4259430
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:01:55 +0000

    violence this comfortable become grade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
