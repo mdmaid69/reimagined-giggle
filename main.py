4c3671fe444a92b2b5f0f9ae3d56849e2c80756d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:06:58 +0000

    part same workout ocean happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
