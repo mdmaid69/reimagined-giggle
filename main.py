5eab36a059074c26dcd9a4a7704b8f8ef13e4f24
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:42:54 +0000

    increase aim business careless just

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
