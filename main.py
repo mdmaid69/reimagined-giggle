ac81c29be05df5e0b72fe1974f3a85ef859da6ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:20 +0000

    sound test now concentration sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
