1a9042987411d4cbb164d30efb22fd1c313e8c45
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:10:51 +0000

    private rough finally confidence last

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
