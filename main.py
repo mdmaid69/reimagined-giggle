3b7019228bf90fd70648d690c6fcd4c0cf4a44b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:25 +0000

    week accidental tidy recognize achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
