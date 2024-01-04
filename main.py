311e4a3ec91d2000fbf35cf1fecaa0119c1f1b03
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:48 +0000

    breakdown amount force really position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
