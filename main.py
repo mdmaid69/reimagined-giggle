bcc07015ff4bca5646e9ead2b2c466fc1c66fbff
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:42:16 +0000

    keepfrom judge explain press identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
