400558b156c63dae89479127a2e6ad566c6e3fd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:25 +0000

    present copy lookaround safe work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
