9ee119f0d5734ff75371b52df90ed0dbb8f62f05
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:19:18 +0000

    open confusing before wire expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
