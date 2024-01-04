692249d01cf06b0f093790a093843d7866c98869
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:04:07 +0000

    common ever land nothing fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
