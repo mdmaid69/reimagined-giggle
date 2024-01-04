54ce49f53c2c84c753e01f05c125cb672a6e8541
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:47:50 +0000

    walk turn tip everyone flight

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def cube_number(x):
-        return x**3
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def square_number(x):
+        return x**2
