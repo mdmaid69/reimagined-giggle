98734b0126f1865501edb45ae407f6a4606b9bae
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:23:18 +0000

    invitation name across order big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
