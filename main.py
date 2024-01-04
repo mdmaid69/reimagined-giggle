fa9179b077056ec8e11d5a283854e7e034d40009
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:18:46 +0000

    visit eat list bottom possible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
