a41054403d97a25abf0db380aee7fdf071577450
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:45:31 +0000

    trust sensible centimetre spray stop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
