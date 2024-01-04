15876cf4eba93ff24c47af301097b3c464d246d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:34:41 +0000

    behave ourselves holdup pause peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
