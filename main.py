931e0489b335cc4c968f8f90a0c59f939541aa4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:43:14 +0000

    mind while switch please soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
