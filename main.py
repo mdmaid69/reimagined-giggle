0c52d13ac4a9bde53c03496ddd9a7c7ec862198e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:27:53 +0000

    noone important against concentration possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
