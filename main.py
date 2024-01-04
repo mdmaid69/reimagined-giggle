621cd84ca8fb4fc5b05d01ef9bf03033e235b438
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:19:43 +0000

    well help whisper inside development

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
