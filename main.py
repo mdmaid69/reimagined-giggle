2e5ddcbf2d0b3490d2588654a7c35dea3bf6a93e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:03:42 +0000

    any giveback plan far classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def convert_to_octal(n):
+        return oct(n)
