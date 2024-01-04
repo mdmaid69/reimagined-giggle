f451b7877d08611d288596c04695680498065398
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:18:35 +0000

    unfortunate effort content least follow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def add_numbers(x, y):
+        return x + y
