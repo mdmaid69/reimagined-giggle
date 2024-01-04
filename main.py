fb5fa7a5b00d9e9513df265d7d921e5603a45b7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:39 +0000

    talkinto claim length swimming cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
