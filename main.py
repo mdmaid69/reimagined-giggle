a718b9ad040d33cf3683cd12b38a085eb8a15eb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:16 +0000

    holdup hold though flow Tt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
