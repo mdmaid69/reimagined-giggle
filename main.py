12e464ff450e10252e087944d833d4fb3dd04f29
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:27:32 +0000

    that bringback interview stand post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
