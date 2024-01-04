97467bca3848a02c350a8a2249b1be0762a376c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:18:21 +0000

    rare confident cost behaviour 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   def divide_numbers(x, y):
         return x / y if y != 0 else "Cannot divide by zero"
