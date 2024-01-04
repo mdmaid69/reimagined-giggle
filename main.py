80ea6e2a162d9da23db785c46afed6359ba777fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:34:12 +0000

    set birthday plate discuss burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
