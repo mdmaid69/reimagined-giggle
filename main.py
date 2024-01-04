2a3b89d1f68e8c209472ad07dde8aae4e58e712b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:03:14 +0000

    delicate takeoff minimum course lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
