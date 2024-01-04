fd47d066c88324938ea86b800fb2e5154d76d7e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:34:49 +0000

    spread correct together pencil side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
