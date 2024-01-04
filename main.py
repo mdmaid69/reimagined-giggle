5cb56d00b2878f733cc0fb82fa9e376ddd86342a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:04:15 +0000

    confused repair frighten division holdup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
