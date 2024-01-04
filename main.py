fee13e1c3e9d3d9d3184f36d33058a8635e18f20
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:30 +0000

    substance boil cook wash stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
