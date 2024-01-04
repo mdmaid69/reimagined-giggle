9574d39b3fe4c74a0f8ee06b9e780149e664c0b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:28 +0000

    vote experience watch in in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
