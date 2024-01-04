b5c76cc6b82149f904619c7996c403f423bac624
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:23:51 +0000

    healthy married whistle suit close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
