fb364feab4de613f44b217be95dcb69a4b520ada
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:59 +0000

    lead tired unite approve whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_item(array, i):
+        return array[i]
