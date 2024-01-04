625275fc70a4494b2808a4db1ce6ca36e99a158c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:18:42 +0000

    as hole dry job house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
