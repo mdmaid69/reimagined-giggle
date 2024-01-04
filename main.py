aa49ac5f98f46cfcda4a89f29ffd041e3efe6a91
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:27:58 +0000

    very apply sore finally or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import tensorflow as tf
+print(tf.__version__)
