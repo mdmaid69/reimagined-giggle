838a8a39555e19161894fdabac931767fca4b15d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:07:48 +0000

    confuse burn which tight vanish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+print(random.randint(0, 100))
