9b6601ed4815683d8f2ac1818669be762992f8ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:57 +0000

    key survive ear please holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
