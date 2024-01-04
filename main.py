a5d4bd2e179dc6b654fc23ec5026c71aa649a8ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:30 +0000

    ignore movement sight lay little

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
