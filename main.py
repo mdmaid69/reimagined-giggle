c1d7bfad56e37311db340ce25126c23c9934ce56
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:35 +0000

    runinto since lend agree finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
