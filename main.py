3dd2668d39ac0c499bbdd19e89308dadd0cab953
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:02 +0000

    attach shoot phone punishment time

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
