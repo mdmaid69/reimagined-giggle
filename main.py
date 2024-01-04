cf67542af9d0e42542cd5abe5cec1cab2c276321
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:29:58 +0000

    awake record enthusiastic Bb accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
