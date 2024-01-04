c99c39ff0e706f0bfd3c88bd610dda7b2ff7947a
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:36:07 +0000

    mistake touch holdon tend object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
