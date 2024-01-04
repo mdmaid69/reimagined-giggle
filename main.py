89e20b966cb957e8b253a4e88cab7bdfe0d330b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:59:06 +0000

    turndown soon address business wander

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
