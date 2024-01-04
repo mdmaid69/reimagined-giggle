5c7766f9fe56c0c0a09965e98e6879bdf155bcfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:16:00 +0000

    score carry swim but hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
