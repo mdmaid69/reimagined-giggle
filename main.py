60eba37d89beba3ca2a46fe157b2ccc3fb4eb750
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:54:20 +0000

    arrangement car direction tear rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_queue():
+        return collections.deque()
