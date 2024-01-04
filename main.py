11fcb14543f417f9dede04a4acaaa676171e1e81
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:23:42 +0000

    about serve wire safe excited

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
