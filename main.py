ac8ff51938eeaa9f0c8afd7c4e924b7bcae6cdfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:39 +0000

    distance shock century against comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
