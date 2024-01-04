580ddbd39dca92445a0d026f516b4f2cdad79323
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:00:41 +0000

    quarter side mistake confused success

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_elements(lst):
+        return len(lst)
