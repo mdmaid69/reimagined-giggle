13086437025eadf8bf3bf5c569457e6de21666d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:41 +0000

    makeinto your section line burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
