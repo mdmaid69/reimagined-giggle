912ab39b2d20b905febdafa5bd92c94b295770e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:23:23 +0000

    trade safe she how careful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
