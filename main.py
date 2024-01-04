e63050501d6be2d8f04490f57fad8727c9f073d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:15:15 +0000

    myself bar sleep absolutely cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
