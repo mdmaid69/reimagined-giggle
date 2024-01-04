32345c6a207d4e2016abd5fc9afa1197235e9541
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:40:22 +0000

    drop pride related old bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
