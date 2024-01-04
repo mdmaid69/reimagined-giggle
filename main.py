ae6790552c052e14f1ebde11010478042a94df1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:28:33 +0000

    level will normal day bound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
+import random
+print(random.randint(0, 100))
