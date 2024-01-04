28a62eb535c8a82f8c4b201ad28d1694c9fdbe3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:11 +0000

    touch print guide funny with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
