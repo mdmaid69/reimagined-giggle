2fdeb83d97b17dc0dfe909024dceb3d68e8c15b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:22:39 +0000

    congratulations slight more talkback up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_string():
+        return collections.UserString()
