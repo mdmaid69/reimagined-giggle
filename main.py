17e9811e473e59cc9a4820d9e08cac028eac6a66
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:41 +0000

    mixture hospital guilty allow happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
