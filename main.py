1232107d8d9fab18bc08e026785237b6b0339085
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:56 +0000

    claim port get blind turndown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
