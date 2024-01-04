97236f02ef664b59f5b0c8d931f37a05778fce28
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:30 +0000

    tooth partner destroy keepup opportunity

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
