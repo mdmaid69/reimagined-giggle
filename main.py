c4fa12634bc012f663a0234012c671ba97bda292
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:04:28 +0000

    gas out steam detail aim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
