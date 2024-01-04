0c37f27f30860eae79259122ab4c4b7808a4d6e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:36:32 +0000

    go high variety listen union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_base_name(path):
+        return os.path.basename(path)
