3b290e9b3cbd55093e597b76ae3b4c14c30b2a76
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:00:21 +0000

    centimetre section temperature lend hour

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
