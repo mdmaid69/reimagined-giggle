3b5cd2f94a5c2a5a61e5839f3056dfa4c5762339
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:02:08 +0000

    thin unless fly repeat protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_length(array):
+        return len(array)
