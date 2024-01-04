206c225a5d3ff1228e95924042a92c9f8d432535
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:48:17 +0000

    service cheap course rate comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
