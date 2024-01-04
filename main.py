11eb2a7b1cadf22b49cee7a735997f817d1cc125
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:45 +0000

    meet round Tt uniform ilovedicks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
