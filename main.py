369fd756ee51625bbbfaaa9bda85716fdebac338
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:36 +0000

    everywhere concerned walk unfair purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_mean(arr):
+        return np.mean(arr)
