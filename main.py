adcc6fad004d3ce614a34aae17153f5864a3f14b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:52:29 +0000

    bringdown godown child classroom dry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
