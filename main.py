4994fcd6efa728abaca7fa05dea7484bc6f5dec8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:20 +0000

    practice circle principle flow glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
