32c8eabfb8b51db86b2edbbe5bb7a388fbaabbe5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:58 +0000

    niggaacademy makeup entrance reply indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
