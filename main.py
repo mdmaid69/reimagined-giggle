9f2197455dd8c5d07fd8da8d4f74064f27ebe6a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:09:22 +0000

    perfect means find disturb split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def convert_to_octal(n):
+        return oct(n)
