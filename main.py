283d96aa3457e25999ace03c0104b96f952549a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:38:07 +0000

    rest into smell bad profit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
