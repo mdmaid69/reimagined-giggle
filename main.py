4c90b6b5faba06c0079c07ad00a51dbfd7d56f00
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:39:20 +0000

    comeround term back nervous eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
