12dc05d361aa69ec8f8ca68c4242a207adaa6fea
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:12:28 +0000

    worried drawing sea speech night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_binary(n):
-        return bin(n)
+  def get_current_working_directory():
+        return os.getcwd()
