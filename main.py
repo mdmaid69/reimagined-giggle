c488e300c27e39fa501c1e6c118eb1e936d22584
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:45:00 +0000

    wide find region justice ourselves

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
