6387dfd3407d80a641e7f3adbe3a86cd62e5e5f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:39:01 +0000

    story eat destroy like city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
