f4ca59016341e38a258546d567226a959efe5ec4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:38:10 +0000

    anxious pain duty settle guard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
