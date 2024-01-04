5a20a07880748a3af32e74f52b1b24c27894dd9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:37:31 +0000

    naturally takeoff quarter miss term

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(lst):
+        return min(lst)
