964b96f74b3edbc43525f5641d278a1d40d595c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:25:04 +0000

    see him close nonsense stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
