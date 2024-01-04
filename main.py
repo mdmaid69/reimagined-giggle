0b6a3e7b41d5eccbdfbd38a9da3576c8b950ff12
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:38:10 +0000

    ago realize possession pull right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
