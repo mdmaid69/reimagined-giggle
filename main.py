db62f6477dcb6eb679124e36c105ee6371d4ffd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:05:24 +0000

    dinner afraid modern feature sail

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import array
-def get_array_as_set(array):
-        return set(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def convert_array_to_string(array):
+        return array.tostring()
