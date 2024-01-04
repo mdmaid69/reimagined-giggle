e0714a0f6fb65086d0cfec992cd7231099087de7
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:53 +0000

    sometimes any letter pleased fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import random
+print(random.randint(0, 100))
