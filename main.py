8d094366a51ea0d6e49eaa3cb294b7dc4d6faf01
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:01:12 +0000

    neck appeal usual company account

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
