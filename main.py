05d274b5b1db471cb84fbd45072c7c22109553b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:10 +0000

    sign I bringforward more storm

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
