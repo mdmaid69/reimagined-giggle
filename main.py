5ca5f0350ad79c1124261ca9fb16a4d8c524f945
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:35:52 +0000

    may black cook cause sell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
