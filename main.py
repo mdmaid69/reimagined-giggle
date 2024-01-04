d3dc58c9bef3b583675964e2615996801184ce5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:55:58 +0000

    tight hold home card extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
