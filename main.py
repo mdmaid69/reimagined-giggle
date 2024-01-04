caa4aebc7673764a396ff0e9ba46bceb687a4017
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:10:58 +0000

    delicate willing contrast attract own

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
