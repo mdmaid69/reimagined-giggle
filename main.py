4cea9ed035d7aa04f1699d0bd7a62e1ed2ad3f0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:18 +0000

    thinkover 179bite high eager whom

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+print([x**2 for x in range(10)])
