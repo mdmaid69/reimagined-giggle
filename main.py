f6bfde6f903987feea944a4a77164759a2f80653
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:23:34 +0000

    confused calloff begin turndown store

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def find_min(numbers):
-        return min(numbers)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
