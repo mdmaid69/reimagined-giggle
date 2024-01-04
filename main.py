bd7502c0dcaadaf1a10ebd24f222503824d2c945
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:28:16 +0000

    right fear consider method cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
