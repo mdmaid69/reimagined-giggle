b9ecfc03588dd494cf207bd1d1d51ea2659db27c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:54:03 +0000

    gate confusing wander front please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
