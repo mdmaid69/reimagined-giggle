4de9e0fa61fae138f8f336a6252574504341b1a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:52 +0000

    clear fortunately characteristic person fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
