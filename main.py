6642e2a824ba2ed330dea7417e94495720a1f548
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:45:52 +0000

    arrange pay keen expression different

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+print([x**2 for x in range(10)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
