3a174c34a9e1a66ccf55fbd6b00cabeb0147982d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:22:28 +0000

    fall switch point stop vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
