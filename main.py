cbc85c2d8b8aa47b3d9bcc068a5d81fbe3c4563e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:06 +0000

    thank boring agree beginning make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
