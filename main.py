f5ba9645913624b30681a5a95b71f2b481fac8d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:28 +0000

    apparently plastic certain close pause

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
