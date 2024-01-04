13cca034863dd8d7cca9beaa88ff057707d5cac8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:23:10 +0000

    forget flow mistake bush famous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+print(math.pi)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
