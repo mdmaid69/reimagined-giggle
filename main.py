f4f65436c6db2a700530347d53db3ed1d67cdeee
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:23:25 +0000

    happen ought embarrassed opportunity 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
