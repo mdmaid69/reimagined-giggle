0c542c104144338abdce462c968f890093dc5d36
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:34 +0000

    life birth shall proof talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
