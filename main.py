dc9f61f71595c40a299afa87f452df4870e1454e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:55:32 +0000

    delight successful talk change swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
