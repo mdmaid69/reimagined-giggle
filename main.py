9ec69e9d6394e08205e3733f5f14b45976ce4885
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:02:59 +0000

    control pressure either mix show

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
