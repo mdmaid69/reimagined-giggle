9808ea60b0840502d91c4e6b9ebc8f33542e54ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:58:31 +0000

    wire flat great shall birth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
