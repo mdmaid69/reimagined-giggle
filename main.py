2389c6b88b2b721f57b4f44dbcf9e86545632951
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:52:50 +0000

    roll any opponent brush challenge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_acceleration(speed, time):
+        return speed / time
