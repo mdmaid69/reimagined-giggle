b2d553008aaefad89312575f94af8fd10d6c0a21
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:10:18 +0000

    neat dangerous plant ahead curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
