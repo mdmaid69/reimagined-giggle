6cce8e287987713272e8868285b680dfe55fbbdb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:05:11 +0000

    friendly measure suit breakout turninto

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
