293b63fc2884799fcd404fc9b11ea9e4b0bab103
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:40:15 +0000

    early takeover ball smile congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
