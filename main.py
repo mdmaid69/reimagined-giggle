2fc9c4150ea454fa2f9e76b5ee1acf151bd7678e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:12 +0000

    suckme storm we pump knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
