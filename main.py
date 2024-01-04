d959658675e71125638e3602c56943c009e870ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:29:50 +0000

    relax weekend pattern proud fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
