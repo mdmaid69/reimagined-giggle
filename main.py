28c0341e9249e7e60ebcce3389bf24ef502400c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:20:58 +0000

    before radio heat plenty Kk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(numbers):
-        return max(numbers)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
