8f24d8179baeb2992cddbc1114a1b552ea6e800e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:41:09 +0000

    lock doctor middle wish arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
