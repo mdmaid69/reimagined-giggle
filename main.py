a1703e71e4560fe60d13b89a9bb05fa45fc10239
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:17:51 +0000

    whenever hit size change taste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def is_odd(n):
-        return n % 2 != 0
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
