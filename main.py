5334c37bd506b348506a85077ab5f1ee4b5f6e27
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:26:52 +0000

    Nn trade search decision gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
