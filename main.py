09bcdd30c1f04656bb167276bd1e5d68ee289d56
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:10:45 +0000

    hair involve next rarely comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_repr(array):
+        return repr(array)
