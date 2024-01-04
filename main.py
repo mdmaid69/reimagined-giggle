5a2a6cb038c952febfe014fce553b2c058752062
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:44:39 +0000

    study patience lunch square noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
