ea1bf790eed404ffae6bf44b2f89da63c61bc287
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:00:20 +0000

    gooff tour cross private no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
