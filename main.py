a5023743c0484f6544544916531e69d3d5c6ca80
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:34:39 +0000

    entry determined trouble fasten bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
