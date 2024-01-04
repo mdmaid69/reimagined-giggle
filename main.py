51b69574f20d325bef7188ce1a5a7d0d215313f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:07:48 +0000

    the anything word Mm stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
