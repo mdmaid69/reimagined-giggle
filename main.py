1ee341276b276a97dde51e146d2ec8b5a15582d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:56:03 +0000

    Gg case fond aware can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
