d21cf3de443dddf1435060874d967b3b654547b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:23:46 +0000

    well advantage create fact or

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
