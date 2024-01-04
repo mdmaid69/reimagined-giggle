80fa067b651a8a1d5a84d927b3337464bcc139f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:26 +0000

    skirt mood with narrow circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
