fb2cae963bfa9e9bc1c6ca2f1dc691c6ea1bc6be
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:46 +0000

    show demonstrate mysterious minute either

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
