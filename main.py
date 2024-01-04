c90a5aee36d679909c1585e8524f9efe8ff4f665
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:33:00 +0000

    aim hair lead ourselves available

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
