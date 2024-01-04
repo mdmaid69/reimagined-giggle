c9d518aa6c3c2b6579925c13ad3fedf746031037
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:31:30 +0000

    definite still declare thanks there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
