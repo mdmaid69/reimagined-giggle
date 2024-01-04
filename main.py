b447139b53942788b95870c6491cb6f2c01a8bd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:41:07 +0000

    nigga extremely hold win damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
