660d89be017e0d7636fb67d7de5cadd1cff08903
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:14:15 +0000

    match beside treatment performance ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
