fbde1fec8bc491e957127d6e1c7d3761e9fd9d1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:01:28 +0000

    environment whole Mm police improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def create_counter():
-        return collections.Counter()
+def get_array_as_bytearray(array):
+        return bytearray(array)
