72c70bb306334b173dad313c57c3d9e5ae8e271e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:11:16 +0000

    raise sail delicate let progress

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
