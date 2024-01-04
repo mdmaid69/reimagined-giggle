e4440846784e77349976997a58d1217fb30612bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:45 +0000

    complicated confidence abroad thankyou poison

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
