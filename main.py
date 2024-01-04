7b9421bacd080d7557e41d7371a21ee3f7d6a9b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:58 +0000

    book thing natural Rr play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import collections
 def group_by(iterable, key_func):
         return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
