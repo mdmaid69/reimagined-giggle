8f4c34cb153295a1eb50c11681d85d12ad12e923
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:59 +0000

    lunch as to minimum health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
