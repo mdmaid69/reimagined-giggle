95499de759bf1015c1458608975713247c257343
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:29:29 +0000

    container comeup pull prepare month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
