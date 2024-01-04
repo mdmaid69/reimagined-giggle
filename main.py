f7e5d72eaf79cdbd00572a44b0b665d2a41991c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:55 +0000

    thank cough person goout book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
