868db11e37e537a0253a226d8a4b229160b5f2f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:54:39 +0000

    only getout picture room trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
