f02e3e7145de86b67c964d23971ef888dee4b478
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:37 +0000

    lookafter shut whistle grateful point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
