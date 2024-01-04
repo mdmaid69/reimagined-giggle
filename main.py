01f897a154d13324ceabdbde2c885191aff1c2b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:05:02 +0000

    every war secret fence Kk

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
