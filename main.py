e19050774b1f97d9fa35a98e8d9388adacab0b61
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:20 +0000

    inform skirt each real recently

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
