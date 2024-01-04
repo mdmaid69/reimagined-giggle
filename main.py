06903a1360283274f4d5368adfe531c72ffe071e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:39:57 +0000

    impressive 573entertainment worried close past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def find_min(numbers):
+        return min(numbers)
