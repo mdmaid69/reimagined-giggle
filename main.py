c7d2de81715687e66f1bd802971d3e994bfed637
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:30:13 +0000

    left learn breakout track exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
