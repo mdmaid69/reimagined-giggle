f735e0360113dc169342196b54df864650387ea9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:57:53 +0000

    include early practice before funny

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
