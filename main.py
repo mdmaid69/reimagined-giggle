7ac0309aeeb9d6c6174b8fee43a73e89c3e66072
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:45:36 +0000

    discussion secret stand comeround today

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
