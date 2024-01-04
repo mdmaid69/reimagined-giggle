053093a40a4045efd34994098b527f36f2c48210
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:46:28 +0000

    like order plain sensible everybody

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def reverse_list(lst):
-        return lst[::-1]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
