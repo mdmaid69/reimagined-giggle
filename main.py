95fcf4ea635ebb4e26fffc498f16f5d674c181dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:25:36 +0000

    schedule basis only roof book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
