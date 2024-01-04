9810715365c3c9d0baaee9bb0ae68b5532eb0bf8
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:39:13 +0000

    whichever welcome feed top remove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
