3e901a67ff85817ea08c5542fdc89aa330fc6885
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:20:39 +0000

    like heat line throw plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
