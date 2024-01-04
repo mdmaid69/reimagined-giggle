f1cb793cf23a7b4f7656909807d24bfe95a97068
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:42:04 +0000

    brave equal account 565end major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
