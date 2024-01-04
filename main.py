044e0a87059eb5352d396a25a78604cd4936d649
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:24 +0000

    pinch disgusting time blue design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
