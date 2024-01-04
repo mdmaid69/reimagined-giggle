fd97afc275626724e17979695b6966c9801497e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:41 +0000

    complete near tragedy excellent completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import os
+def remove_directory(path):
+        os.rmdir(path)
