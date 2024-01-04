dffa641b85772696b7d2f62546200fb9de36133d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:11:32 +0000

    cross adjective anywhere separate cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
