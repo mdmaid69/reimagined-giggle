074117b53da183bfc89f6b342af79ac24e00fe05
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:06 +0000

    combine borrow could keen follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
