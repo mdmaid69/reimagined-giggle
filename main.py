faac45de3722f9a3961b3b68b1283271e4441674
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:10:56 +0000

    discuss sister lunch branch plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
