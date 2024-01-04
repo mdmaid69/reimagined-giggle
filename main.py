a6b39b62da5030bf8fbd95a8c85c8cd3c56cb93e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:49:14 +0000

    comment invite post born swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def clear_array(array):
+        array *= 0
