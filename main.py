15c51d7e7eff241a49a83aa6aacdcbdf1211ae11
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:56:05 +0000

    children natural supply possible why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_floor(x):
+        return math.floor(x)
