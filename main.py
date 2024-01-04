da7742d29c7032156832efb151e97ae83f6d460d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:50 +0000

    waste treatment objection partner legal

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
