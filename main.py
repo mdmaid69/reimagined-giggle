04c27ea5ce9644be2f1958f14a398e731a6040f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:59:31 +0000

    offence change means result inquire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def reverse_array(array):
+        array.reverse()
+text = "Hello, world!"
+print("Words:", len(text.split()))
