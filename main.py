e0b249d047c683cacf644a8dd5932e52a78c5263
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:44:40 +0000

    upstairs help spare hit environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
