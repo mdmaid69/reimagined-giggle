8f5ad4014832a3a6170ce3e9f1f232c80cfb4f28
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:46:37 +0000

    body effect slide similar order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
