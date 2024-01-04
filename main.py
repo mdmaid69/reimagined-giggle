c70c3a1cdcedf0bad72e9b4f5fbd2a1d8ee2623e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:10:48 +0000

    hard 565end mainly still throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_str(array):
-        return str(array)
+import getpass
+def get_username():
+        return getpass.getuser()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
