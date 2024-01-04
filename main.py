470ad7fa1c81e8bd14063da3686f52fe2292fdd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:37 +0000

    ashamed hospital neck run crowded

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def convert_to_octal(n):
+        return oct(n)
