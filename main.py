634735ab8f0050ab25c170d55dc4352c0687f416
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:47:15 +0000

    trust difficult action speech off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 def calculate_work(force, distance):
         return force * distance
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
