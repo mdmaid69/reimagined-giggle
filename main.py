ec7f8639d6f0b033efa8e2fc602a0659d88871a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:21:04 +0000

    talkinto clear cause mine thorough

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
