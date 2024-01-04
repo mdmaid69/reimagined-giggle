74eb0e99f53e1c4d272d2b0ed845f02b7e3196a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:41:37 +0000

    likely improvement region guess importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
