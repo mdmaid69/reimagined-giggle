53edbf5feca237b91ee2690d733ba190aa8d19ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:15:39 +0000

    film whistle fairly lookback(on) forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
