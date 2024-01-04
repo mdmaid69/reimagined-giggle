4042862e842e7801aaeebd42052fd837acdf521f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:18:24 +0000

    shallow plate even road keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def cube_number(x):
-        return x**3
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
