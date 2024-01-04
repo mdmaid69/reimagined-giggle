d35d2f4f7e8d59789f2f3ce9ca7ce586d583c503
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:29:05 +0000

    second value than wipe next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
