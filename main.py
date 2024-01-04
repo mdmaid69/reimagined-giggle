e87b80adf9f7e5e4adcd4cf9776b840849b9d0c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:40:30 +0000

    punishment shoulder Ss return position

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
