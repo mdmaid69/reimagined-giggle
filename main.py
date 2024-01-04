a50b767fe8a02d6ad20ef18132358b653b62c66a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:10 +0000

    wing time abroad bet pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_str(array):
+        return str(array)
