59527899ceddce7474b5694f33e904a947e846d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:30:18 +0000

    serve excited doubt listenfor spoil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
