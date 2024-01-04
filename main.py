89fa38d87bbe1fb18e7b5c2918a8dbf2b1024b34
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:38:21 +0000

    quit break treat serve record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
