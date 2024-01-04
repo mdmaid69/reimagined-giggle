248ea5c1f07b8256eda1a001183a6cfcfdfcaf7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:58:43 +0000

    dry board police check near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
