92948d05eaead97f70545bdfe481f3ff456a6338
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:41 +0000

    idea impressed repeat spoil report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+text = "Hello, world!"
+print("Characters:", len(text))
