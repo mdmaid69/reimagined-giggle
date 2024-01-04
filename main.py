04a2331efec85d0f4830f6b8af54ebdf19d41530
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:22:17 +0000

    other rarely many clothes table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
