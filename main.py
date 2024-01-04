61fa214a8566244ebd867f3c812555a74840171b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:52:06 +0000

    aim fall any opinion pour

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_max(numbers):
+        return max(numbers)
