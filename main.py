7b8de923de8495a3cda9cfd79a65397b0b755039
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:23:28 +0000

    example friendly until swallow soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_item(array, i):
-        return array[i]
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
