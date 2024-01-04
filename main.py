8f72d477886d8a766f86a88a8900274ecfa5ad96
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:59:55 +0000

    condition round always practical old

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
