33c54c3cc7b30089cf4d597136c0ec61fc802774
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:33 +0000

    company keep ourselves slightly broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_index(array, item):
+        return array.index(item)
