76f36b1cd135557f614e0dcf1f3341b1fb6008eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:13 +0000

    accidentally increase shadow takeaway action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
