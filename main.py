9d708947dae1411348c9e0e0f8ddc56db38e663d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:59 +0000

    right process bag major detail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-print(math.pi)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_hex(n):
+        return hex(n)
