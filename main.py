182fd0a6e86a5137cd479c681d0e6e6d3da5ae9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:15:37 +0000

    cry the quick under interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
