2959238c743d8ac46f41a6bccea0033128c730cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:29 +0000

    hit bag whisper callup cheat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
