b9bcdaa63b9bb90967068ea517a6f92384747653
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:40 +0000

    unless physical black force table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
