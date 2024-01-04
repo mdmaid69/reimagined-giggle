b32c2eb30adf7bdde3d48d95a9e147d395556eea
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:14:20 +0000

    straight mention harm press lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
