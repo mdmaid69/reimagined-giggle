410202f45b24fc50f0dead22eb2345f7c9501a38
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:28:29 +0000

    furniture appear switch regular report

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_acceleration(speed, time):
+        return speed / time
