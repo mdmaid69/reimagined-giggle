1bcd2cb94baa82908588b468e19e4711bc6c0bd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:53:09 +0000

    collect talkoutof label talkoutof passenger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
