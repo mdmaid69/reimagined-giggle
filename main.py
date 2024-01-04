522ebd6c0ea96814ea5beed179895c732450e81a
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:31:31 +0000

    line close limit large vanish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
