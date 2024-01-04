865d43c2b4e035951f8f520d5519e88a2b3ab5d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:53:33 +0000

    excuse belief unite label Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
