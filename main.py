5e9d511741ff64a44b2e31403bc3d3ce30864acf
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:41:49 +0000

    year tough demand computer tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
