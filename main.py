e392830fdf173cb05d2557a15e14628de5473d9b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:25:34 +0000

    extraordinary excited purpose choice myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_floor(x):
+        return math.floor(x)
