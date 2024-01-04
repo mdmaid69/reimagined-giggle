0e19a61f3ac63ca5ede3f3252c17a329ed6abbf9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:16:32 +0000

    spot this hollow from activity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+def reverse_string(s):
+        return s[::-1]
+def find_max(lst):
+        return max(lst)
