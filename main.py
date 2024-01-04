863e9c1982ce39c80c75c7a68698cfe7eb91d272
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:22:52 +0000

    wise live politics across manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def find_max(lst):
-        return max(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
