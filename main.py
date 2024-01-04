33399d35f4c5ba3c86034f354fc742022614dfd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:26:57 +0000

    Ii store someone under odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
