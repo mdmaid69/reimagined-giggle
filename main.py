84faa018f834269d2d64fb5c631096126b44ae90
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:48:07 +0000

    normal after attitude this sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_item(array, i):
+        return array[i]
+def subtract_numbers(x, y):
+        return x - y
