7aa9242e6ccf7fb8825157ed5298e58df0f5b8fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:50:18 +0000

    mad guard tongue suck phone

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
