ed9bea94827f2b22fe45153d5e2ca74db768df09
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:48:12 +0000

    forever honest this exaggerate complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
