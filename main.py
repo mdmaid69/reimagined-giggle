fefaa2a40d47183b871b9202a978c4bc439b10dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:13:58 +0000

    slice loud mean equal out

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
