2bacff99dcf4c2154e0bdb8eb541eb932d4076ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:35 +0000

    skill pick rest its regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
