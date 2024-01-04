2078be927e11acd25c3df4a6a27effb184f233fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:22:05 +0000

    bunch himself watch material final

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
