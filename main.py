1a6596d5d174100cadce54d5dcefd43928e26ba1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:07:07 +0000

    race office reason if only

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
