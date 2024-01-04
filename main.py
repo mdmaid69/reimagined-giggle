098d2829e7491b2e227c3d63a40ba4c694959815
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:44:55 +0000

    prove ashamed interfere help fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def reverse_string(s):
+        return s[::-1]
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
