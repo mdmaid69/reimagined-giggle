61b44e98a46724b0740664ef4f381baceb80668e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:15:37 +0000

    rough bury vanish effect look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def find_max(lst):
-        return max(lst)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
