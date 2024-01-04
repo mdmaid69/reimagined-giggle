96127691ad3859c490d089a21b0bf3f5479cd20b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:22:20 +0000

    hollow powerful act generally negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
