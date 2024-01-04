e5bf68f31b13e3786431fda8b01ca4784f2299f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:45:59 +0000

    state anybody Zz welcome brush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
