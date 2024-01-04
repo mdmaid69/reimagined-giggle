0bc0e17583a64d4a71adb0ab433366c993d69afa
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:55:56 +0000

    manner position account interesting command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
