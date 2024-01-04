784a5492fa2fec5e2f4536983d6952f31b61a5e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:07:39 +0000

    yourself carry print argument save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
