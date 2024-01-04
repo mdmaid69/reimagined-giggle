be69b4caa382b070930e3d893b95d1fa74d22e73
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:28:04 +0000

    full approve time risk butter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
