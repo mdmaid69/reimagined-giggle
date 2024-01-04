304d59b3f05eb36ae66b26aadf642798342f6017
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:36:59 +0000

    crowd justice because exploration smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def roll_die():
+        return random.randint(1, 6)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
