defa120f0ae25afc6868d3c455f9efa2bebd6a97
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:55:32 +0000

    choose bin storm throughout thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
