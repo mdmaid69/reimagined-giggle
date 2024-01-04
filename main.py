dfb4d479ae877107cbb2c739b82e3cdf543ceb5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:00:02 +0000

    share crazy stretch them detail

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def find_max(numbers):
-        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+print("Hello, world!")
