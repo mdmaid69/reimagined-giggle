509dd1fe4f81d1e9e23dee8b444b8cebabb1c93a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:34:16 +0000

    abroad comfortable spray name ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
