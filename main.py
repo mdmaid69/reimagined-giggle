34cdb5e354b6988b684e62f0949f72de00d0425b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:53:58 +0000

    still sense fly near face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
