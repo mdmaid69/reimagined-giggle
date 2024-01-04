0cfd8a89939d2db1fc43f71f83646ed897e51b55
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:51:22 +0000

    authority disturb ill wise benefit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
