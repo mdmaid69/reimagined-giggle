a5e628c3817f82747dce3efd447712482d23c066
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:45:57 +0000

    better laugh please something love

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
