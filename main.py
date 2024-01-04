23c247a02208d37c90ebb5387eff1e59e720bf2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:54 +0000

    callout display pen classroom war

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
 def calculate_ceiling(x):
         return math.ceil(x)
