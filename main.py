fcf16e952eac7ab10c3e17c2c20496f3ad2f2f03
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:52:16 +0000

    breakinto copy getout foreign reserve

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def reverse_list(lst):
+        return lst[::-1]
