5d41bb1d5a92aeb50faed5f6fa9041bfa6983d03
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:09:06 +0000

    oil account finally various answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
