aeeec5fbaa14ae56e476a0986a04871e57042e4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:13:18 +0000

    concerned give number substance duty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def reverse_array(array):
+        array.reverse()
+  def convert_to_hex(n):
+        return hex(n)
