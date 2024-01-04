21adce9da1c4f2848071ae1941f7e8c74b894cde
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:35:35 +0000

    wake feed again now geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def reverse_list(lst):
-        return lst[::-1]
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
