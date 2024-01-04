04131f1d3b38c542228fdc22d58eb1c53cb86ae7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:14:56 +0000

    completely wrap sad takeback tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
