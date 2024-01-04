351cc8f25e39ca16aa56a4f2f0694e95ea11b242
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:20 +0000

    connected characteristic news live unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
