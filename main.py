dc7d187d4066a2c971045a5621208886e96d5ee9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:06 +0000

    maintain amount turnaway best special

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
