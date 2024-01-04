d2eaea3813f8df5e1a557ed9b9c2fe627ee00be5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:41:30 +0000

    father receive terrible far custom

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import time
+def get_time_since_epoch():
+        return time.time()
