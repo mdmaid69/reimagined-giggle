b4fed4c724c369b1a8d55f7ae55cf1f44e047a47
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:38:34 +0000

    climb unpleasant consist plain see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
