bce99dba80c8e855f49d97528f2d95a3ec551e4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:24 +0000

    she from second material environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
