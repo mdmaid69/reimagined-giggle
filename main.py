6a70673c8e289c8595c986c24d22f56eedd74b1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:22 +0000

    cross burst grateful sick test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
