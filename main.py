e065926691b7dadad5e378e70eaf34c9ea6e89f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:23 +0000

    bill save deserve fortunate now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
