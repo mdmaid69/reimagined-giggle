5d3f1bf1593ce45ba88ff9bb61af16f1400f39de
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:15:26 +0000

    major bright book body back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
