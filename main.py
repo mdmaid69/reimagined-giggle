57e3e3a9dd996380420a5c6fc89890089bd5efb6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:48:19 +0000

    instead clean sudden damage unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def get_array_item(array, i):
+        return array[i]
+def find_min(lst):
+        return min(lst)
