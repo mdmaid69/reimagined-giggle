9ed2e7e34c7dacfdb3730b3623ab286a4495a658
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:56 +0000

    address lookafter deserve name final

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
