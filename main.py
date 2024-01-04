249bcf3bf843af284465dd5ce29c3d42621825b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:03 +0000

    clear science name progress exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
