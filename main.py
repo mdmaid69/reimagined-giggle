7c6d972fbcdb51b1a89edca8debbf72271559571
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:02 +0000

    last each somewhere past pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
