7ab23bdebfa4a63d92600cc11b156fe944b72437
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:43:29 +0000

    maximum fair limit affect practice

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
