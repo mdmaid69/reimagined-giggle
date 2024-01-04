6d5664d127fab4b25fd436e5ce76e57b62e4191f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:43:42 +0000

    account against clever guess any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_floor(x):
+        return math.floor(x)
