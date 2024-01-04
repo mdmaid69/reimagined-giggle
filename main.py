4ea0f1beff212742fe5085e6f496df6a20eb9597
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:24:14 +0000

    arrive only forward connection peaceful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
