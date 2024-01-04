271809b8754b5e6f88c7079b62b7708bf7e76216
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:19:09 +0000

    shadow control thanks suggest tone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
