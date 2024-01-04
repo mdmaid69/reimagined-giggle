c31f6f3768aafa1dfd1670d8351fbe993ac7ea8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:33:38 +0000

    throw material snow uncertain sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
