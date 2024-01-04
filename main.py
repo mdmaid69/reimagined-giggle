0a9332bbfb633c8c2218f2dadb9ed48e0ae02688
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:10 +0000

    below meanwhile try lend recommend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
