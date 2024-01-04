da94bcd1f99007193dabc084b425c0aa3fdfe009
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:16:54 +0000

    anxious duty early offend name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
