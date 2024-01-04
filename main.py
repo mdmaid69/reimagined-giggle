9c04f93ade789b1244d488fec0c9eb4b8c0c515a
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:52:09 +0000

    mass string slightly Cc remember

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
