735b41fd881cc895d5fe2ad672d49965e7489888
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:56:48 +0000

    substance its tongue please check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_list_from_array(array):
+        return array.tolist()
