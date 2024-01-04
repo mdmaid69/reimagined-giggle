6b2547fe4ffe8fe280df32773f7bd5e67d4b0aa4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:19:50 +0000

    behind carry embarrassing anywhere meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
