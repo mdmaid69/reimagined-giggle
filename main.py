37896e2e87b30c594a9a83b791a2f1a2950b0a60
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:37:47 +0000

    spin region equipment right worse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
