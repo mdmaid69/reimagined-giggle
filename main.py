b74a1b8f99cd364b34844c52cfecdda27f900ecd
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:53:00 +0000

    same bush possibly birthday interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
