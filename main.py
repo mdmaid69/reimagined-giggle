9db5d8b8d58613a018d6aac6ed4ec020f32d2623
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:09 +0000

    might takeback many pretty actually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print([x**2 for x in range(10)])
+import collections
+def create_queue():
+        return collections.deque()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
