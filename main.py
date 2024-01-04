3dc436ec235162207712815541408066b489d135
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:38 +0000

    meet pile choice dig aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
