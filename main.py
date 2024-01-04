f674585810d97a9b6ee8702d53f52db906c9b502
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:11:55 +0000

    mine thanks once cross air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
