0e9754b7d18b0a7f2ae6fec0e939cd1e8dd9ae8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:27:41 +0000

    whatever own suggestion scrap low

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
