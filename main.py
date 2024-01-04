716140fcd9d012dcdca7d2c0061f985750230c3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:54:25 +0000

    truth loud increase profit mass

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
