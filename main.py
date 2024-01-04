feb908a864f9bbac27956ec047218b5926cb98ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:25:31 +0000

    573entertainment travel fold hurt lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
