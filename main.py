deff771aaf1fe132c6e3293608d2413235923899
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:59:42 +0000

    table getup branch wave display

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
