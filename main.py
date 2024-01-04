bd7b4376e8d4434ef610188c68174ec411f89bc7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:09:56 +0000

    school disturbing over general noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_length(array):
-        return len(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
