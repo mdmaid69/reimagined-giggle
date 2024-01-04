4d4a4345a0b8b21a0e7d53678187dc255eaa1302
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:51 +0000

    turninto address car period less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
