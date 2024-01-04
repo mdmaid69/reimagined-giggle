8cc795377aa92b840c1dae5061af2094f4568235
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:06:04 +0000

    early challenge hot wish control

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-print(time.time())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
