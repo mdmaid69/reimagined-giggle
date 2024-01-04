1128c46b8c9338e9ea86980857086b2d1ea13c8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:35 +0000

    convinced number university injury maintain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+import sys
+print(sys.version)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
