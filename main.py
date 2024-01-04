ca7a1d8dd62fc3ef8e65f2db7a5c28f9502fdeb6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:49:37 +0000

    blame fear rich treat drink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
