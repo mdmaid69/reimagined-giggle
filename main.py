77af89d150f8d1c4a48ed62906a060850e841f4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:23:09 +0000

    bunch lift wonder takeover Itself

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_pressure(force, area):
+        return force / area
