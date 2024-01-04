1d4a71927bbbdd51c330ae8b556d3cad37b0fe95
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:11:35 +0000

    kill turn cheat real available

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
