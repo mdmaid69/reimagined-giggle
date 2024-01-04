6daf4135b45292ee53ba016e08d6c1a2e7e43198
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:40:28 +0000

    connect year shopping rent empty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
