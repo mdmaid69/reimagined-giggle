ac1f5fef03220377f7c30af4b52e89aeae2a12e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:56:23 +0000

    fear demonstrate Uu mark line

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
