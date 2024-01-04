f3843bca684b3fc3ca6bcce46f7725723b175252
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:10:07 +0000

    obtain so empty convince compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
