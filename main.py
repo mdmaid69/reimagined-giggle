1cb7ad8c013a265ad7479d61335a58fddde00fac
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:05:29 +0000

    city sensitive familiar dish dress

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
