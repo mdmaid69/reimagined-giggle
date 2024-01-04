165b3d17ef239fe0956df9385a3a7707ee4aa794
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:38 +0000

    fast holiday Ee relief blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
