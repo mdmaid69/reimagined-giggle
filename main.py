fa9977dc2b9d1b38d8e53ea89c2a8729d032f656
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:41:16 +0000

    as apologizeapologise crash successful consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import sys
-  def get_python_version():
-        return sys.version
