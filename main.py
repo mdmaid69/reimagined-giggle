1c7d980d20131aa320cf969acb6895c8440bb8bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:02:04 +0000

    grow suddenly excuse pronounce conduct

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
