ed925bae2e39d10a6cd7c39df1cab532f9c667ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:18:43 +0000

    stare certain prize appearance land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
