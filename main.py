db86d00f70dbafcb4593044d18d129cb5e088075
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:56:02 +0000

    tip content getout reaction row

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_str(array):
-        return str(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
