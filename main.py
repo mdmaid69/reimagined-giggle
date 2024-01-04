70cd9b7b41683a37874eab2fd7f018c11c7f4db5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:15 +0000

    fashion moon Tt undo interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
