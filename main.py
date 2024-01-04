809213fcfff235caa8ed9e9d3f5f39bcd03ce361
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:25 +0000

    clean comeover high state behave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_pressure(force, area):
+        return force / area
