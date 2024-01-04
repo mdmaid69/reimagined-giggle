5889c414f141ac74b0e0f6069ea4eeba856ad140
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:55:03 +0000

    union doubt beginning alone sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
