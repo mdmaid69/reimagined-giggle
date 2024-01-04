dc4e238f8ae1dfa774e3ad5d552b8b7721ae9d18
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:11:12 +0000

    imagine letter secret escape hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def convert_to_hex(n):
+        return hex(n)
