a65ef900bc75ae272b31c8d5130c8f13cfc36e23
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:18:05 +0000

    write match people Tt sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
