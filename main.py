6b2a46244e5d00fd9ee3c51a8ccd956667096c05
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:21:59 +0000

    lie normal guess pain butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
