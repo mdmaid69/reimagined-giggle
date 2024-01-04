6852829ee229e41467d3c2443d33ca86176680f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:33:24 +0000

    nigga tip job conversation extraordinary

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
