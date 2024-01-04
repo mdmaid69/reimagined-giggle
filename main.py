0b953c7bd5e34ef1676b22bf8062d207bbd4141e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:36 +0000

    my describe noisy meanwhile pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-print(os.getcwd())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
