6d6b88e74e1f433a188f72bba05002f307aef11d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:17:32 +0000

    personal someone very ordinary cover

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
