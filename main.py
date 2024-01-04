2f5045eb30f4ac20cbce1a7b9208f796c7884bb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:56:55 +0000

    argue take never male rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import time
+print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
