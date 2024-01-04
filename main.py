26245a026b6301706f6855dc0725b20bf962686c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:51:08 +0000

    support turn record standard float

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
