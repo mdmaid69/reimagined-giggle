5c8e58d75acba09b831634ffd39264e5ff63182f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:48 +0000

    time reason something yes telephone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import os
+print(os.getcwd())
