87e9ac4956d4da64040d597175c7751a177fcd21
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:11:16 +0000

    kill club slight cool but

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
