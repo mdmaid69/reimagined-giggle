c1cfb763fc30f7ea84b99239e5075b11e79be268
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:34 +0000

    slide generally ask shop table

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
