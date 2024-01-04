d091cae8f9fc6fbe1ce4dfa818072e2f864d7c77
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:47:00 +0000

    through study need something neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
