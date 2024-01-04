da3f3d961e6781960fafe9536ddc5921f9113a56
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:56:52 +0000

    quarter nerve community early consist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
