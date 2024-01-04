0d5de903c690eba139022d5d3fa6cc1dc0fa74f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:33:10 +0000

    measure return keepout several I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def reverse_array(array):
+        array.reverse()
