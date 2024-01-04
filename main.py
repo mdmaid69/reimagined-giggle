65b3c557c09b0367a5827bbfc6d09a63afcb70a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:50 +0000

    meanwhile afterwards flight meet strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
