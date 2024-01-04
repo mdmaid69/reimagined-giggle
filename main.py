43a84a0123955e0fe883adde0e30d1eb2a96509b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:03:58 +0000

    I scrape protest phone collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
