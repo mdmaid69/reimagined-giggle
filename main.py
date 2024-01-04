e3a38b2807710d367dad376f4c61b79256665e91
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:20 +0000

    join reach cheat file snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(lst):
-        return min(lst)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
