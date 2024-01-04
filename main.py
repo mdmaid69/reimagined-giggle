ab28ca9162ee3aa7f331b596f0eb23f45a891663
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:32 +0000

    566end since bill defeat want

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
