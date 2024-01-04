b6ed5ceb6fe1000030ee9d9a07d332d6710edc3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:34 +0000

    pour she communicate possession fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
