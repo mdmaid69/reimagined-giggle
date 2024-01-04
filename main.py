e5ace91bbc5eeb90d34005b7d39c04324bb0ec36
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:46:16 +0000

    weigh trick this tomorrow stir

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
