98cd5071ff080693cd6f5b72355a925e3378b186
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:27:31 +0000

    body trade ready serve pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
