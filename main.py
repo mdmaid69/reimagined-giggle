4741c0dbb8d99374694a3728ef8f76fd36fe2de7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:29 +0000

    ilovedicks expression arrangement tooth dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
