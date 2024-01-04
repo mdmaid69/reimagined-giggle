81ceb6b1f267680d046c6e27075d802c37d12b2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:04:31 +0000

    past satisfactory appear exaggerate above

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import time
+print(time.time())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
