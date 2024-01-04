7f6eaef7ae6240d4e2f456eab55b535bf41ad912
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:42:16 +0000

    distant where match man home

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
