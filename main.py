671395472abf6f92391fcad2ab1b5e197f7f2e2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:20:47 +0000

    trust attention fat squash goout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
