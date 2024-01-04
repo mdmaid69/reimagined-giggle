00ade00a543aafe87add11929bef4fb099f4dcf9
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:48 +0000

    feature who majority bend least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
