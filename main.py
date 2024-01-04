35a4ab9b8da67be366c547c3a365db2054e6ada6
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:33:19 +0000

    your Qq day like over

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
