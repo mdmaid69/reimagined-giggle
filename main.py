d521b5edf56ebed16e7477e33c20447a70a5dd4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:34 +0000

    hospital doubt science adjective wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
