d36aaea8e400fbe86c5fa2b937eb51e624bf50d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:14 +0000

    original how sit measure only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
