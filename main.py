2bd0cb6db03a44c3ad7dbc8be18ca1231c6fb6dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:16 +0000

    involve relax spite but pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
