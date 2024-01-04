01ce333eacadca793ac2da55dbd5721849f35a66
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:09:02 +0000

    snow rule needle about otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
