d050a4eca61d1d3c28e3e5170d188b5df49a14f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:45:50 +0000

    exaggerate throw cheap present game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
