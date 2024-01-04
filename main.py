def2df20f1c66a63e5b910ecb5ff9803ce082efd
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:55:30 +0000

    air cross by mix cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
