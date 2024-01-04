18066b6f743b5bd591e242e789cf0662ab0a3c6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:25:49 +0000

    deal characteristic direction how prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
