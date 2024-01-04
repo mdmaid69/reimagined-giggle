fd785cbbe0c8926dc66cbe20fa4b3c3bf5627153
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:03:58 +0000

    hurt rain job gooff some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
