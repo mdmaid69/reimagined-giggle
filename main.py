4fe88cd9ac7b91ec2b5c2a1050724c033dce5d14
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:43:01 +0000

    process ride tight instruction arrange

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
