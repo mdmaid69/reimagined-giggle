9c2fbdd6838ede51f24821cba911ef8082348715
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:12:34 +0000

    hope tight private professional classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_density(mass, volume):
+        return mass / volume
