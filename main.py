da8b4df3745101d6846dc354ed8536e28d816209
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:15 +0000

    keen shout skin instrument firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_bool(array):
+        return bool(array)
