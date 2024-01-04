f51434b0ce861077e3411c43826cfa766b6441ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:26 +0000

    contact block before bright succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
