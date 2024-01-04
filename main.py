65a36b6d17c2c158d2c864018ba5b9044928e677
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:01:17 +0000

    free least bang full waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
