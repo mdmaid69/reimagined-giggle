3dd7b1479d2d3533ecc1d3ebb23e1d9186c62874
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:20 +0000

    nor still container simple order

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
