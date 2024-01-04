1b3ea177f1a113cb46e8b849de7883251b8ec569
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:21:25 +0000

    track Vv price fork twist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
