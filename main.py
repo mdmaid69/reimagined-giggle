49ea79e288ef12817598f97981002d0f7b0e6742
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:49:10 +0000

    rob still appear tap path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
