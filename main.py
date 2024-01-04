237ddebd0317601c778e2bde5fdc2f7a10d994e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:05:53 +0000

    occasional value passage petrol run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def remove_from_array(array, item):
+        array.remove(item)
