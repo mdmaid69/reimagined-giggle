3a8c46d3f2e7b5dc092f97ebb0c11792b496c8ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:29 +0000

    food attention special write statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def is_odd(n):
-        return n % 2 != 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
