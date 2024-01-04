b3fe1dac0ba231d58c1281a6caa809255aebced3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:20:49 +0000

    whoever address age correct war

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
