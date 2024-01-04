2522f7da14afa8426b189322a3c6dca6232338f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:14:26 +0000

    use lost wealth story advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
