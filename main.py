0da09689117ec35eb018231b1aaed56890f7d62b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:16:22 +0000

    apply search whenever match arrival

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
