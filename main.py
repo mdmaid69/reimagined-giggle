dea895e075727b1fb16ef2b496d91ab9e97c9a5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:03:24 +0000

    bringdown steer connected innocent murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
