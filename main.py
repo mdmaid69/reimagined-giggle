75ef2fb0c9762e27630de5d8446ad37f59c2038b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:00:02 +0000

    Zz desperate wild drink priority

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import math
-print(math.pi)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import numpy as np
+print(np.array([1, 2, 3]))
