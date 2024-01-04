aa76cfacd09062cfedd05f536b9a6041d8a6ea5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:30:29 +0000

    turnaway rich makeinto this weekend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_work(force, distance):
+        return force * distance
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
