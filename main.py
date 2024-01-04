447aa8f4dae7df10e0b3854af3a64d112c86e757
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:37 +0000

    pen fold shoulder worry connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
