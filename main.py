3ce276ed58583936de336518089696daf53ceb68
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:38:56 +0000

    satisfactory Ee hear already opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
