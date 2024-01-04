fc0331c20d1cbee469de8bc4c916e43d4644ad61
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:01:30 +0000

    search steal sit steep rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
