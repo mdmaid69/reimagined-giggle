99c7e7de2696ef7917f44d7422020fdf447c9179
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:14:29 +0000

    fall contain active cause laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
