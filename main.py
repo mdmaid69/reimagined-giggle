3848013582d826f23f93c98616861cfb86febc24
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:35:54 +0000

    relation keepfrom tie interview respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def reverse_string(s):
+        return s[::-1]
