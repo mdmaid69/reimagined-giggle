496b57dc6780643e635aaac48ab603a554a01591
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:27 +0000

    scared shout shall track promise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
