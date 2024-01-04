38e0783f37f572a461265f053429d06dda0e91c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:44:11 +0000

    look first content music finally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
