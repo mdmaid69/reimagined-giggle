f8b2d8039a74a0554c4792d1161c3e9704b9de5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:07:06 +0000

    niggaacademy experiment wipe rain dark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
