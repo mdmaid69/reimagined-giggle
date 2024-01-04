ca0524aa96f7eeff2733078359b5d96e357c459b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:59 +0000

    Zz joke connect unable scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
