a1311c20008f41010ba3916e26f5ae83b394afc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:00 +0000

    communication runover guess laugh takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
