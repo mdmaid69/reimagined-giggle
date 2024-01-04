3d6d0bd76bfe92919c18d47c73046bd33a000c8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:10:08 +0000

    means instant relation active settle

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
