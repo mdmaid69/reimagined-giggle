291a05673b2b7306b1089bd758660b349eb87390
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:10:00 +0000

    attract shade move room judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
