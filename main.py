c2c4d33813901678bb9012705d15e6d9452f1ebb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:07:16 +0000

    serve platform produce scared suggestion

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+print([x**2 for x in range(10)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
