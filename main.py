853af5592ccd1752187b15e0223aacfe464e45b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:41:27 +0000

    hope lead mainly mystery call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
