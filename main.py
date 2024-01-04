a0b0d53fb6ebadaa34c64d7f6c526757c071f4b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:06 +0000

    consist serve coat maximum equipment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+from collections import Counter
+print(Counter("hello world"))
