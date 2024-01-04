115c759e374b372bae5408b7e601c29f2d338474
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:50:36 +0000

    block few none shopping patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
