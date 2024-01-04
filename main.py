c2918a1e341c3831d58dea61fad5139c999ea684
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:02:39 +0000

    silent scale desperate surprising some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
