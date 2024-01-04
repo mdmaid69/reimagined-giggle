480ed21f3224d70fd5a8a2805c34dd45d5cf5bd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:53:23 +0000

    practise turnon angry too 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
