e5cf29370b5e2d43f140191fe977c2bb4814c055
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:19:07 +0000

    finger hurry mouth reaction part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
