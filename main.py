c9cc881e4e1433322a2ed9fe2917637fe2d0a165
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:01 +0000

    cost whether slightly prefer nothing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
