42a6f89c465e598b8d5263a05f37145bf005dbf1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:42:55 +0000

    theirs far passenger worry kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+def print_python_version():
+        return sys.version
