2c13a99c3be3fc2eda4e96e14c34fb1eb4f56fdf
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:04:56 +0000

    allow impossible try communication guess

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def square_number(x):
+        return x**2
+  def multiply_numbers(x, y):
+        return x * y
