3b9990af9b0e075bc7d637e8d34399f25d87d101
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:39:15 +0000

    careful male study point getoff

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
