0f1765389ba8432fd776b83dcbd5e1c18530c594
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:57 +0000

    practise eager office great Oo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
