54fb4fb0ade45001f21c2f25ef6f7c66f2b0fff5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:01:47 +0000

    bush bite short ticket imagine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
