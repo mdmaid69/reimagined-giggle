fcbeb6d2c59efca16311066b246ee1e019c6a3d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:30:25 +0000

    goback(to) easy impressed extra responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
