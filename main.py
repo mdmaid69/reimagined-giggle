56926e6b866aefaa89b01df2f1e6aa16039c3ae9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:48:44 +0000

    afraid turnup someone gate fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
