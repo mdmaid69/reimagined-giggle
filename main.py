f1c42710bd62244f738d02843cfc940ebd8da882
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:58 +0000

    rent simple talkoutof fly breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
