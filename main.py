7546be9ea2d87bf96e0b2b54bba91eb3883f531a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:14:27 +0000

    post feeling forever inside backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
