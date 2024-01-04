85b103e75c66ae5ff3a152cd47ec173e86191a46
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:13:18 +0000

    with rent fence well machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
