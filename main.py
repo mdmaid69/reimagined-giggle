8919389f212b2bfd2f31a4a59a8a937253ae7630
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:22:07 +0000

    branch perform dare reach fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
