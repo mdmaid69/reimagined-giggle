7c6190c07a37e65e81be95801899682482d13c25
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:13:51 +0000

    phone stiff hole meal language

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def split_path(path):
+        return os.path.split(path)
