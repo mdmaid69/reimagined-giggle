af27cb9c681b449195a111968699a0a0bb7ab519
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:45:57 +0000

    shoot deep owe mainly talk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
