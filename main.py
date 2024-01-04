cf3147dbe049fe04e719d9c71910024654bf94fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:30 +0000

    possible book turnout ours patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def count_elements(lst):
+        return len(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
