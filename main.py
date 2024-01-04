1f451b6f18f5ee4312a6dd9b3441388747257f9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:32:33 +0000

    argument used rain cheat grateful

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
