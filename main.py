10e1f44e4b33882cb04fb73f386f72f425206df5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:11:53 +0000

    drink radio needle fashion Itself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
