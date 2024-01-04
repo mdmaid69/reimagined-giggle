e6c5b784fcf15f374c17c379754df2e10a37f4f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:45:59 +0000

    include degree feel shade trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
