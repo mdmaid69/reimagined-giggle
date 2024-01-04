4a74b076be7d3ece8cda29e18e17a396be391c98
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:19:39 +0000

    lay married project welcome oil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
