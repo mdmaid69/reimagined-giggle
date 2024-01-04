ccc3ef44a057c74903b089dc99b42c85346a1f3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:45 +0000

    introduction brother everyone callup here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
