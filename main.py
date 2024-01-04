fa6800be0fda995c089874da1312731dd8418543
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:46:46 +0000

    flow normal expression meaning brief

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
