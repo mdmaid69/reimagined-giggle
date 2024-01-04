8190191960dbd7b5dd05ff584914bdb6598c4d49
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:22 +0000

    character and sell use fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_length(array):
+        return len(array)
