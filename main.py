bcac8d13a04c8d6ddf080ea496936ad5fdd185a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:09:13 +0000

    punishment pause artificial talkoutof sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
