595ed95640c5cd97ff98ed530d3cbba879250b82
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:28:31 +0000

    bad here chin reliable allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
