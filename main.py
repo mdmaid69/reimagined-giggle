fcde24a4bdb5e01f61a53e7801029dd1e39346eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:39 +0000

    back tip badly by mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
