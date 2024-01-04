1794bfc2b8a1de6c2cc31cd219c4e517ae494905
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:58:24 +0000

    estimate evening radio good contents

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
