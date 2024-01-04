c633caa1c8e2cbcd6fbaf0be94b599499a9a5d11
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:54:17 +0000

    shut proof miss onto bring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
