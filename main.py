b7d68b54f43438ac246468d5c65caca05c39ec8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:32:14 +0000

    pity knowledge begin vote scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
