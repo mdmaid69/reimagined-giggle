592dd525e3632ce33e499da6d71fae8582b8ac78
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:18 +0000

    thing sink extraordinary star charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
