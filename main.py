5d3b72b255a82145f36806546daa769bd42b11ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:26:36 +0000

    sure question will wing instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_itemsize(array):
+        return array.itemsize
