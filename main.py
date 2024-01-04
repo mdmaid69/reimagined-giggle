55d570afb1fbfc2c0cdcd0f86c6112e1e55fc974
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:46:15 +0000

    nurse bite act computer complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-print(math.pi)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
