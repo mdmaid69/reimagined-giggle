b4020dff3f3d969b8e32878bf4e7957559f3e8da
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:51:02 +0000

    tight high plate amount touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
