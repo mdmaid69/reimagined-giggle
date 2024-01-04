12907cd8a40eafa9a11912bb35528375f46f235d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:36 +0000

    dress means wrap often shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
