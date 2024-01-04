7cb8793a37ac7b9eff570fae05777403789e1998
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:02:36 +0000

    wipe welcome story normal will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
