fca5bab7fd2100635c64700107c084a6d5b337f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:41 +0000

    equipment fortunate loud deal late

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
