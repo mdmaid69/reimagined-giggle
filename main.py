117ed3104a3888c03693a3f80badb34543d88ab1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:28 +0000

    doubt official against in accept

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
