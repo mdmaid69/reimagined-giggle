e2d3f629f43d15b55ec8b528d7e33ed380acee3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:07:29 +0000

    previous use necessary rain legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
