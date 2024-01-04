dbc0f0bfaa156f1245795d61ba9bbd7ae24d9638
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:47 +0000

    somehow bringback lower past divide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-print([x**2 for x in range(10)])
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
