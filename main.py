c195e0281c6219338451981b9c1ea623b43cd6d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:23:14 +0000

    sort debt block everything sight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def sort_list(lst):
-        return sorted(lst)
+def square_number(x):
+        return x**2
+  def convert_to_binary(n):
+        return bin(n)
