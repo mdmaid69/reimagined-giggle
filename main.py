5180a708b164d69c9d3c92cbfa84b2d2179bae91
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:24 +0000

    middle later aim branch detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
