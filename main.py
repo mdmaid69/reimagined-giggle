9a86ac6bcee356ff3e6e150604f8ef4da759439b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:16 +0000

    trick team bill like few

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_current_directory():
+        return os.getcwd()
