783be054b916fc6beb254b03a3ed8823d87e6a37
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:49 +0000

    spray be meet duty strange

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
