37f8f49212219423b92716eb57bdb7b5fdaa09ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:30 +0000

    surprised practice arrive although of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_length(array):
-        return len(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
