ec71a7121236a679d9c6d038be73e752175cf069
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:34 +0000

    loss common dare treat suggest

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
