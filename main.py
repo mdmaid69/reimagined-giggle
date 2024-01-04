d31aac71503c0ed88a578ea2faa78007d963de96
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:55:05 +0000

    favourite exciting boil back just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
