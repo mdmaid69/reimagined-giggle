1d14d09e1f9a9cb66dde91021429b5590d668881
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:59 +0000

    lookup loss ring break tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_int(array):
+        return int(array[0])
