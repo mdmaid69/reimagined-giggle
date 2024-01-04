939b798af7fb953540ed652ebee494810ae96c88
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:09 +0000

    leave return doubt feeling cry

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
