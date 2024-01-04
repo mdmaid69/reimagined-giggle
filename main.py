777c57c19e1975d72ef8ee989dee7b9554a88b4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:24:32 +0000

    during loud flavour she traffic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_list(array):
+        return list(array)
+def find_max(lst):
+        return max(lst)
