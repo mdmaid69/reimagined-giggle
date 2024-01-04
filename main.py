62b3bd5a23a2725655f1f047673c69bfd74102c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:39:00 +0000

    spring step yes eye curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
