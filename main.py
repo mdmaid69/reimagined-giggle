14c4adec1f972cf05cad31c5bb4b6a978a077863
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:28:29 +0000

    blame hollow emotion subject tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
