4d1ff9971645d4970a19e3f28738a5224750ba38
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:34 +0000

    passage stop identify split weigh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
