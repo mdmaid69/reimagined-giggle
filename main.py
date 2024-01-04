d198e838848b8178428f0a422db1a4230344a088
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:54:31 +0000

    need doubt upwards boat male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def convert_to_binary(n):
+        return bin(n)
