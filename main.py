9528d93af6a3e5e21c715ec1db398df061418c71
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:13:54 +0000

    far tomorrow plant point possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
