f4c88a379dfb1dca74097b504125f8f51aab03f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:00:49 +0000

    so bored second famous telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
