a33bd7d029f78fc33a200d8fdd89803ab95cee7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:06 +0000

    female recent calloff Ee heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
