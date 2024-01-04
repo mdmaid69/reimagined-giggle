129472bf64fc69d76746f987ae1e8e078b49c70b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:11:39 +0000

    basic decide thank your cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import platform
+def get_python_version():
+        return platform.python_version()
