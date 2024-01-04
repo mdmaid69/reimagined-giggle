3c6e00c8aaa1e5c70608c4559214968c7ebb1774
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:07:28 +0000

    display hungry centre help stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
