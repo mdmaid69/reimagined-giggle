1557fc42321f09697b788d6ad5766d52f1499bc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:36 +0000

    genuine show respect artificial return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
