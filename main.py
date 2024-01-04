c758d12c95088eef19b3d909394505c155f8e162
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:29:48 +0000

    street cause major mother seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
