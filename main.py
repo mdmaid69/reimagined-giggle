100ff27cdac16fe682edc903c55e934e1ffb9720
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:31:14 +0000

    convinced display underneath disturbing contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import platform
+def get_python_version():
+        return platform.python_version()
