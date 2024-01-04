957ac57bf9418c84f9ab13eb92e661c24f5910a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:47:30 +0000

    flavour outside way offence about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_volume(length, width, height):
+        return length * width * height
