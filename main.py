c339d29c2189b8eae8db8d491b8d6076e76f3736
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:34:32 +0000

    form address rough concentration challenge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import datetime
+print(datetime.datetime.now())
