8b84527a97b81eade5832996f549f2b7e590def8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:50 +0000

    warn Ee evening still attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
