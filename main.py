91c1b4b1e5fef2f859ed211f8b124babc3e046a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:40:46 +0000

    together hot generally stamp bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
