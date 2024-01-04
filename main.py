ecedaf8d99d55ff2c73e7ae4271f3a3026e3fcff
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:00 +0000

    wind health circle mess tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
