b6029ae07abdd4633063c11ff5f0cf458f4f5706
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:57:52 +0000

    conversation rank still train away

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
