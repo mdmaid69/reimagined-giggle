d4454d0c0f386f478ba3b5ca3060c1d014ae36fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:26 +0000

    keepup priority actual improvement sound

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
