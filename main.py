8e6deb0e973e808d294deab24630381f4cc53a97
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:55 +0000

    cover off trouble hole impossible

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+name = "Python"
+print("Hello,", name)
