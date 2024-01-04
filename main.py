9c8f599724a879443d63a94bdfa86985e237140e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:26:07 +0000

    excited appearance improvement during flat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
