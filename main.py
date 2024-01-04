4234ecdcebd5cfe8ec43580b7ef28bb0be47833b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:36:29 +0000

    walk cross copy ball really

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def add_numbers(x, y):
+        return x + y
