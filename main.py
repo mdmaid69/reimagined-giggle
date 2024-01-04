a28cd286d509dca9dbc417e246b7454ab3ef3e2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:34 +0000

    suck thorough your from relative

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
