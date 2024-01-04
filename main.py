3180edfb393853a921921316f9103412adf120e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:44 +0000

    smile rest stupid not loud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def is_odd(n):
+        return n % 2 != 0
