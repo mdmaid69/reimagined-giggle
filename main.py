c7468a18d5c9acd0be7885630b70279e398677a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:53 +0000

    minute warn stay comefrom shout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import time
+print(time.time())
