45e6710f77b76cf91b17a9bd2ed055f6ebe503a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:33:02 +0000

    design rubbish across long beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
