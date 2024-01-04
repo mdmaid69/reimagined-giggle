55c7f713eef6b5dd69910ae411862249c2557d56
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:09:04 +0000

    contact quiet feeling chat freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
