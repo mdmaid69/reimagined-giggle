318919e66db0d0194dfe4f1b814a57119c20bac9
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:34:47 +0000

    war ring skill woman fold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def clear_array(array):
+        array *= 0
