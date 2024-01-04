8410a08dbd023c78f121ad41ce3a5b4f71b66f34
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:13:17 +0000

    before task broad anxious bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
