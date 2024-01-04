0216b668da3336a96b4928bb60c258821ae03ff4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:23 +0000

    characteristic discuss scream song wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
