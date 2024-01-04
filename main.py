5c0e019958e152ab1eca92368c095b14d21ce4d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:00 +0000

    start joke say never tight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
