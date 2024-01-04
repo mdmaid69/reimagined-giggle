76323b217331664e71e828ff49f4a6698d6840c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:06:15 +0000

    pollution also cap keep persuade

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
