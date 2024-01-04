1bdaa278bdada7d69a2d05df0276d9600d20211c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:57:37 +0000

    arrive direction check unpleasant run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
