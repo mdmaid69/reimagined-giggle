06bdb9e832c6721c67bdc4a968cc42933d863e89
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:49 +0000

    above often fight high open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import collections
+def create_stack():
+        return collections.deque()
