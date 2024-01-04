e8b390a47255be175d4893a95c6e6c6ed1061b20
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:56:04 +0000

    air further alone comeround male

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_characters(sentence):
-        return len(sentence)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def calculate_area_circle(r):
+        return 3.14 * r**2
