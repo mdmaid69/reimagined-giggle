46bfb71bca4d03334f203186bfa2da41fc2f8e28
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:32:50 +0000

    anything car eat separate when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
