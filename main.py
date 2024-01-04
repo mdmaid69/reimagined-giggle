b35cf31622852be0b5a800ec0446827528152407
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:19 +0000

    usually wish intend record scare

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_list(array):
+        return list(array)
