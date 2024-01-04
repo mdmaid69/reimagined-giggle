52892eec25e9e26308cba9c5a17b8ab898451e8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:14 +0000

    national movement before rely regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
