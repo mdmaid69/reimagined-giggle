f6164bbb6ebab8fbd6a7b00f6315d7083590a16d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:05:06 +0000

    bath second bringup wonder only

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
