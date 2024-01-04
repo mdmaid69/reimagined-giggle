dd9197691dc6d33d1577622aaf1f2b176f6379d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:14:26 +0000

    dull hit shade funny right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
