e89f7552211f59c64ac51963364e0f15f5743d1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:51:25 +0000

    swing artificial runover Tt lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
