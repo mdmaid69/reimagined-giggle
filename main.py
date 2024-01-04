8a33bc04aa675534614f51aa0cf041d7618839be
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:13 +0000

    tap habit answer team dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
