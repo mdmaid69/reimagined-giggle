eb791509fa2615513e3b4fd86a7e82b6a44a1c11
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:02:28 +0000

    ocean keepfrom apart persuade no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
