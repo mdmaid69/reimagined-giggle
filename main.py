39de06bf316242475829a44aa1ffdf84145045bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:52:22 +0000

    burn judge typical bend traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def clear_array(array):
+        array *= 0
