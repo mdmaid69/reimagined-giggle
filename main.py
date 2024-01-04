e0999feeafb554729eb50387b22a27b5caa72f80
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:26 +0000

    dick land trick practical principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
