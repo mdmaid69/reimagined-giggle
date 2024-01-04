645632c0a4e693b6eb1f6e192c82366a4fa0de36
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:28 +0000

    value turninto matter belong add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
