1b3f01b00ea1c1236c3419b28a5f009ff9586de4
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:33 +0000

    turn miss swallow though ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
