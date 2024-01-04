660bf9bca387942cb3473a3c02a4762ac53309ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:47:38 +0000

    either makeinto doctor professional Pp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+print(sys.version)
