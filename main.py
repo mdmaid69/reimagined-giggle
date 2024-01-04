542d7e8f9f3ead970d188d8d6ce440090f34bc1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:29:01 +0000

    forget forget report afterwards contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
