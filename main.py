43c8a897fe80bb99678b7956a62bce9628fe503f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:06:10 +0000

    objection fear terrible fire refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
