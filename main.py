e25099a7811a4257639c8203d795deae0fa8481f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:02:41 +0000

    talk dare her shut record

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
