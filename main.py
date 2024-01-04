2fbfd5c281c901fcf1a9034d1f65781c88b20c72
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:22:12 +0000

    relative several whole art already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
