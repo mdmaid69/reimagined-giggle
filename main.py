8f2bde983638a4deedee917f6b0c896cb8ba061a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:58 +0000

    rather cost strike mysterious land

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
