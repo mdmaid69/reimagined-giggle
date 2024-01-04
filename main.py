263728c4519fbc9313132b07f5a59f565951d26d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:05 +0000

    certain lend popular tight drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
