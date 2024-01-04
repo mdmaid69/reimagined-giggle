b019e3c990e7e8a1fd90fa6d6916ae99f64b1902
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:04 +0000

    corner mark disturbing crash kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
