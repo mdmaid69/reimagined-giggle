75543d4882f979b6144247e2d25cc7b9189dc742
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:01:05 +0000

    damp sing fire reduce Zz

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.time()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
