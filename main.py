7e4d896b708153abdd47c2234c969d41b8c9d93e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:40:49 +0000

    load seem pause turnon design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def change_working_directory(path):
+        os.chdir(path)
