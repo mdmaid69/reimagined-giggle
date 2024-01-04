e6c23c7fce1a71108aec334c654fa19fa24b352d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:42:42 +0000

    bus free night lookback(on) connected

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def find_min(numbers):
-        return min(numbers)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
