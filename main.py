2d37a53ef9bb93d39cff4b023f94d7b68d5271d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:05:15 +0000

    food scream drop terrible complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
