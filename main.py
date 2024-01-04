dd73fe6d96258e18bc919d2a704d323dc9c22d30
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:31:34 +0000

    black might embarrassing image sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def print_python_version():
+        print(sys.version)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
