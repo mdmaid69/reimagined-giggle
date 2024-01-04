5150389037b4aa718ea9d23e03204e6419ca1838
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:15:29 +0000

    take admit danger crowd real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import collections
 def create_stack():
         return collections.deque()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
