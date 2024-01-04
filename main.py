48eae02b8ffc85ca38b09969498bc2fb39def4cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:36:42 +0000

    live near equal less speech

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
