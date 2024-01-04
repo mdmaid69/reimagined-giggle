0f410d16014ec82b2ac527822a1dba8cf6c0c76c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:53:17 +0000

    bang giveout joke pride about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
