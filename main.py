3cbc587152bb0dc7cb1e131d0587502fc65ef826
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:49 +0000

    level guilty otherwise performance but

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
