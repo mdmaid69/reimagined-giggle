3ce41a9711be39e195282ac47211489bd2402451
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:31:51 +0000

    sound draw knowledge fit keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
