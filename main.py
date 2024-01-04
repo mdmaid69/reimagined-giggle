35e4cf2694a6273129ce4df81969551cd26aca29
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:44:44 +0000

    pair air state complaint ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
