e3fda6ca97211ba2b8cf436c8c16c6814c86b3e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:36:24 +0000

    stranger square yourself perfect dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
