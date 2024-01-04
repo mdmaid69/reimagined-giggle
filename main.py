5824068d5dfc01697bdf9c90f6b506e7a48ac095
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:32:41 +0000

    ready swim long fortunate lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 numbers = [1, 2, 3, 4, 5]
 print("Max:", max(numbers))
