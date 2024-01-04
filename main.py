f948f9b7085a7ea45c2e45ca1d7538ea6b493951
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:24:11 +0000

    upset divide name occur cheque

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def count_characters(sentence):
+        return len(sentence)
+  def add_numbers(x, y):
+        return x + y
