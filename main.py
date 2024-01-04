d66fbb9e6406a12a361a18f1eff49172cea1d52a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:45:03 +0000

    rich please dress attempt godown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import random
+def roll_die():
+        return random.randint(1, 6)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
