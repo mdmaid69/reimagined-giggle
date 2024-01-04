64551eefb188fe6c0473c7d2f9cd0004cbd96987
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:35:58 +0000

    proof tend scream suddenly section

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
