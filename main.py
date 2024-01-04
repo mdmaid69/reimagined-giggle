f61340bd6af60f5a4657229ec6056c7533709679
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:22:48 +0000

    whether broad abroad film set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def set_array_item(array, i, item):
+        array[i] = item
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
