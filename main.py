64ace74c3e7cbf4ad728e0a9867cc750654e1674
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:44:15 +0000

    dream follow party awake pot

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
