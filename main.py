dc9029ec239ce46eec908be84d44d93099ece3bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:29:53 +0000

    laugh rather lesson idea pride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
