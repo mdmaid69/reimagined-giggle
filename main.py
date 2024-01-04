eb6e2863d5db2b7219b2161b78c30a9336b74b95
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:11 +0000

    spite mine thinkthrough note connect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
