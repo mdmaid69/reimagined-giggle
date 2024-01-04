4a342773ae2d78735486a55d8e24c79603cd4762
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:17 +0000

    speed story disturb comment so

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
