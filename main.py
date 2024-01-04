f610f508e53480212bceea7e6145c5a3b3c2a8c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:12:44 +0000

    meet target objection art pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
