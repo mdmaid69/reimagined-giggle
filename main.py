f438bf19af71a4aeff27c12eead83e1d5f75680f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:39:44 +0000

    chat share whichever raw right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_area_rectangle(l, w):
+        return l * w
