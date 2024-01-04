7d41cfd52c05572a1c58c46560482904c9057ed3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:42:54 +0000

    spring road by physical be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def count_elements(lst):
-        return len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
