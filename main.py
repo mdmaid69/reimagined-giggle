f3fcf6a371e8b20634aa989aec6e8f4e2df19b57
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:39:48 +0000

    father phone after drink may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_binary(n):
-        return bin(n)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
