6bccfcc8febbe3c2819e3998da1fd60a2d7d310b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:27 +0000

    basis effect information keep settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
