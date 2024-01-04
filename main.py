6ca3942a1e4c7eb96ba3c2e3fa5d6918f493eddf
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:13:20 +0000

    earth direct bank criticize along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
