160f6bd8188e153f6cc79b7bf9e52476f0d01d53
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:14 +0000

    ready anyone warm although machine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
