b6960186a2514c8c922ad7f8e73fe01fa0a75312
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:11 +0000

    finally consult list mouth face

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print(sum(range(10)))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
