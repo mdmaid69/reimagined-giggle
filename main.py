0a3f8888ff2f238ceeb1f02adcf17d3d03cf8123
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:20 +0000

    against wind ring goout anything

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
