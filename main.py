099a92aa4861e31faa6ff8cd0a94959560a5b47a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:26:20 +0000

    recent happy add what dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import random
+print(random.randint(0, 100))
