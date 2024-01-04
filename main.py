1305f6a778c3120b1fc4b43ccf82d34279bc4ad9
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:29 +0000

    split rent another tax proper

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
