f11e7d23cbc872ea7ca327b6df5048f14816ea96
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:57:50 +0000

    giveback chance remain another minority

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
