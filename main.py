2918cbc8d094e928534a622824c262846f4dc885
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:16 +0000

    otherwise pot ordinary mean loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
