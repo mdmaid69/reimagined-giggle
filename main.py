40f97ac0dcd90bd1e64261e755e47a29b4a97f2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:56:38 +0000

    exciting regret fire factory before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+print(math.pi)
