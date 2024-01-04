a6f01508dfd2a76bc3f40f77bd3aec9e38dd5064
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:29:23 +0000

    relax influence prove force Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_queue():
+        return collections.deque()
+def find_max(lst):
+        return max(lst)
