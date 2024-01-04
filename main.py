70d71274976aca1f9ef779c9ff14e76fd66fb97e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:56 +0000

    disagree base plan reserve heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
