c86964bc34698bbccd6f65dccc1516e6ab671414
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:39:03 +0000

    once relationship sink general centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
