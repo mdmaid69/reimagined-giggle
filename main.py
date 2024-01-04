a3bcbf3f85eace57acd70186ca5015e5732107aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:17:36 +0000

    ready union past memory trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
