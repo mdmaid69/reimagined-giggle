dca8d89349777d5cca3f04499362de93a3b57e90
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:24:19 +0000

    wash high furniture come suck

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
