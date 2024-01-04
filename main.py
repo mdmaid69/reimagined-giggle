6fe1ead9a6c8a848543da20225c9e5089d262e22
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:49:58 +0000

    drive very record admire exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
