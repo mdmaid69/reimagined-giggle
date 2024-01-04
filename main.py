d3945f74e35a437372e559b397aef02f251c7311
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:14 +0000

    murder close soon judge trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
