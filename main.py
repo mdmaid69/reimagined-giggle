28b61cbc24cd7b890ebaac50b6c69fa5dc04a85a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:29:36 +0000

    remove nigga spend disagree correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def cube_number(x):
-        return x**3
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
