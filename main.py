a59aed107a93a3e3decdcfc6fa5ce4220f862538
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:54 +0000

    count hurry minimum married emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
