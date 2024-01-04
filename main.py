e373e9e8be1a9f2cee5f4bef15442c90c499c173
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:55:39 +0000

    frighten breathe become extra remark

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_error_function(x):
+        return math.erf(x)
