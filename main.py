29a8fd29ceeb3d2ece11261ca3d52592435df92c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:53:28 +0000

    usually expect hungry centre date

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
