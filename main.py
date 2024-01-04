edf6b297866362b3d4b63b65cb0e17a8224f708a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:10:31 +0000

    nurse can load central test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
