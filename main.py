ba3939cfb65b682ebb7554de115a9a8bfe73dc0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:20:59 +0000

    tip definite proper communication chief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
