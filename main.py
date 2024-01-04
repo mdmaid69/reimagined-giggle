a9c289c804d3e619ea00ded7f5a1b97a0471abb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:03:23 +0000

    573entertainment poor money any skin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_floor(x):
+        return math.floor(x)
