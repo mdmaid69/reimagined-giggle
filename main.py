5db81d4228f98bd92ff7addc31c672611edb5747
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:14:26 +0000

    suppose else bored to against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
