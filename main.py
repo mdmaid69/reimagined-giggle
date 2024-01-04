74bed10ec4d1fa7356a7ea449460f191de519ad6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:57:08 +0000

    turnon judge usually cry practise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def get_array_as_bytes(array):
+        return bytes(array)
