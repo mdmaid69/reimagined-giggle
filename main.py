8df90c9395f41bdcc44dce51a28bdd6bdb4f8970
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:43:48 +0000

    sorry offer dinner stamp occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
