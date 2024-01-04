30b882fe4faccc2a479629435e8d0830338360bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:26:48 +0000

    snow wait waste 573entertainment extraordinary

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
