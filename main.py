b91333a29755828a40ce252f5532afbb017767a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:34 +0000

    switch total beside point search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
