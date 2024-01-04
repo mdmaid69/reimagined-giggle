b496470e2275f20cb5d073a4029bea93680b48ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:48:51 +0000

    word free message Kk drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_item(array, i):
+        return array[i]
+text = "Hello, world!"
+print("Words:", len(text.split()))
