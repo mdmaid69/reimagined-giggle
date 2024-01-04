de7603609de05e8ad29626f0fcb800240ee63bb0
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:34 +0000

    benefit treat treat hello actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_square_root(x):
+        return math.sqrt(x)
