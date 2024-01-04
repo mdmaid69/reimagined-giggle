c652db509a44a91d8309498c25eac690212f7966
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:23:44 +0000

    coat strong bar feature fetch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+for i in range(5):
+        print(i)
