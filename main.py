57057420fa3140846d0cd09fd7c774ffe744ca05
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:41 +0000

    spite soil pay possible stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
