e3235177eb10a86946b4f5361c9d9c68a1a57313
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:29:02 +0000

    dead extra precise speech bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
