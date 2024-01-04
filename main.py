8dfa940e506e18a5b672b57e853ad69a7e3e4ce5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:51 +0000

    forward community favourite wing minority

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
