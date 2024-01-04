1c8ff56b0aca4826a986727983300305159a205c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:02:41 +0000

    contact exist talk such sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
