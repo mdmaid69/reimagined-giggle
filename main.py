f27193e023a23325e8869428dcf8ac9852f20a0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:03:06 +0000

    catch risk top takeaway what

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
