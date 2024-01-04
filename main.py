c12399a2791e583cd7fa11acd5d186aad31669aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:58:55 +0000

    protection dry sound getback impossible

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_area(radius):
+        return 3.14 * radius * radius
