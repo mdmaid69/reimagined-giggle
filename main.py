9033476eeade2cc722fec308dd76f7252a539cf9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:48:36 +0000

    watch free crash keepaway bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
