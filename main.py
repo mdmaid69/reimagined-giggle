5b2ddd19d3814f2ffc4bbfbc568a55f26e49a253
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:13:36 +0000

    Nn silent mix usual before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
