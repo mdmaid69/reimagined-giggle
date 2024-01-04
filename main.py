5983f0b9dea0c5a91d12c11b8de5d6b89ada47be
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:01:00 +0000

    follow anybody shadow switch increase

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
