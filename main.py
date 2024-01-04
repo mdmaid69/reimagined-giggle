6e0c1a9a9d256f0650c6de11a80d91adb065c9c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:49:44 +0000

    pronunciation crowded giveup juice boring

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def is_odd(n):
+        return n % 2 != 0
