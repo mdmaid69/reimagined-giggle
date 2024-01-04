184ac774ae4cf859bb2ef661d29e521b5e3e2fe2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:39:39 +0000

    whenever explain sticky everyone bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
