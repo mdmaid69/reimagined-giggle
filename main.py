399e13453df86b0c32afd017e51584695b903266
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:23:58 +0000

    tap mark cold plain OKorokay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_array_as_float(array):
+        return float(array[0])
