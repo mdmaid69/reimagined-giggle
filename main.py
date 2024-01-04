ca7f87aa58824f3cc195617ebbd3252453494a4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:01:01 +0000

    turn break tired wonder deal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+print(math.pi)
