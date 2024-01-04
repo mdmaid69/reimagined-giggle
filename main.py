1dde1ec661df3feb63f5bcb998a36a4cf7968343
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:29:30 +0000

    used smoke transparent fall grow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
