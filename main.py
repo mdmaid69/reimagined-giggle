6297f3554fd54fbd617ee7f56b51a5ece5c4c7ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:27 +0000

    man fork Gg second swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
