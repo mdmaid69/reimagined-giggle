aa4b4fc3b1adbdb555c30f69c2eb76314a33e556
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:10:26 +0000

    tidy follow find challenge defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_density(mass, volume):
+        return mass / volume
