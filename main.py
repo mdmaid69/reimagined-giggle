2ff59173fef5f500e955cffc275d5b30bea535b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:36 +0000

    determined introduction fairly concentration hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
