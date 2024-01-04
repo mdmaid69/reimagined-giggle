d573eee36564ec9f599de34ce8fa3add537e1473
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:18:52 +0000

    shape Ll roll love face

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_pressure(force, area):
+        return force / area
+def convert_to_binary(n):
+        return bin(n)
