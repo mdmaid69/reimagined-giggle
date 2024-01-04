6745413fa45dda8a016027b2d57a101025c3f012
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:05:50 +0000

    choose attractive paper juice demonstrate

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
