fb6cdf17093bd16d27072eb7537bcc4120dc1552
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:28 +0000

    say crime forget head on

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-print([x**2 for x in range(10)])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
