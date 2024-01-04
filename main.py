3289e38e77244a148f319dee50668802decaef79
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:08:51 +0000

    recover light child the act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
