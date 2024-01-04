762a37e4745fb2af96ffe1ba8ce48751396fbf8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:57:48 +0000

    fall admire so almost reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def subtract_numbers(x, y):
+        return x - y
