32ce3693cfaae4aa006a457d850b44d6f28fb41e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:07:38 +0000

    free rain treat minute face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def convert_to_hex(n):
+        return hex(n)
