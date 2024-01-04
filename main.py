93567429654c2185a4c1487c1624e9af31091006
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:36:58 +0000

    copy 572enter either calm collect

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
