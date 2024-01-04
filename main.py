97de090059eb9d25d957c69f6e1496fb63df849b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:45 +0000

    motion expect deaf before reduction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
