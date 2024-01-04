9b9673d321a7ea65ccace82babda739773239e9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:03:47 +0000

    Itself important centre prefer definite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
