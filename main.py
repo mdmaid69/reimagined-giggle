0c62d48ab14648e6ff4fa2440440ef4e5b027bc6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:00 +0000

    peace wise ours talkback round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
