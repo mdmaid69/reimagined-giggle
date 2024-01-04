dce4333493d994c359d78299a6179369940804ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:02:16 +0000

    high include sense national hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
