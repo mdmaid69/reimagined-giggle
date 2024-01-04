7c73e82dc1255e43e266180814fb13f021b9d1a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:33:52 +0000

    bus band plain public beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
