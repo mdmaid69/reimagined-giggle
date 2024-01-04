cbb7186112cd136fbfb6339d481b1b78056fd15a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:23 +0000

    treatment only collect regional world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(numbers):
-        return min(numbers)
+import array
+def reverse_array(array):
+        array.reverse()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
