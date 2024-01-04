0aecf82100dc77c53d9ee61c7a796fdb766c1b4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:15 +0000

    confusing birthday necessary practise lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
