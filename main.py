11545f3e82c3e28d95d3d216e6ba98e2e41f1b30
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:04:38 +0000

    block callback whatever comeback obvious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+from collections import Counter
+print(Counter("hello world"))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
