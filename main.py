539bc51926472bfe618d382e9709854416b7a499
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:52 +0000

    pay lookafter smile thinkover relative

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
