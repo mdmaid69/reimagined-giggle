a485c4cb735026426cff412bd8a8ec92fa71d406
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:26:39 +0000

    survive skirt certainly pleasant children

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
