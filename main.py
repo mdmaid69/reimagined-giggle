1d0659ffb7bc459a88ed737c99cf782ae84228a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:02 +0000

    some trip excellent top middle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_even(n):
-        return n % 2 == 0
+  def delete_file(file_name):
+        os.remove(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
