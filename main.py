da4d27d663f132d0dfccceccd51dcaa8352d3726
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:02:24 +0000

    like scared while badly exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
