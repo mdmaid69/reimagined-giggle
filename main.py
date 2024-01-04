7768f4a44b28052f1537679b33d9a39c0f384f8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:26:39 +0000

    anywhere reduce purpose doubt Nn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
