28e536bd429c55d486dd22163c4edc5b62dc2008
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:54:52 +0000

    lock trick unit recently violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
