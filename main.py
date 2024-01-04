4383d688ab8096511d8f39062f28ae798cc3f57e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:25 +0000

    comeround successful damage performance thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def add_numbers(a, b):
+        return a + b
