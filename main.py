97bed9f85ffb81dd39af14ec26a0af87c922335a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:24:09 +0000

    little below drive switch behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
