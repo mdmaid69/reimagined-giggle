9b841977aa8a9fb8e43a04be30fc3915e10410b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:13 +0000

    law interfere 566end add face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
