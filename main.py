747b50815ee815410374da79a289ab62eb8619db
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:48:05 +0000

    keepout police leave wear emphasize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
