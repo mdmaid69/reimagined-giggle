be1fef85f2e4c74326eb1910d0b3e792af77fc97
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:49:49 +0000

    question modern coffee rubbish more

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_typecode(array):
+        return array.typecode
