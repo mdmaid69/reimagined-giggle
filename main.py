036869fa68e938d08c064a8422717fb52e105311
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:26:15 +0000

    short write tune breakinto connected

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
