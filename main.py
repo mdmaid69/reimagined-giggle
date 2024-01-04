f349239e3cde179e331b2af95c26f3e09f316482
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:08:43 +0000

    us hit demonstrate absolutely pass

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
