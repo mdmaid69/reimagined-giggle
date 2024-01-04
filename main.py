48d007189a8822e21cbe922a034c644c881f423d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:26 +0000

    apparently frequent still whatever organization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def reverse_string(s):
+        return s[::-1]
