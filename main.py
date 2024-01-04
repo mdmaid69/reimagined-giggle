28ad364fb62f534caeb38943daae2192c95c55ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:36:01 +0000

    buy yesterday crisis human equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
