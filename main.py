c4da1f8f833927280b626714316d09c286e9c9c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:09:05 +0000

    agree apologizeapologise remark discovery split

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
