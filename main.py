274c9e577b13d457fd50891ebd4eceee44b54e50
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:54:18 +0000

    certain person under heart charge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+for i in range(10): print(i)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
