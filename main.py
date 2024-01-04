023047f20e5f985ec9e8f7af0ca16bc66933c1fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:15:59 +0000

    mention demand turn story enthusiastic

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
