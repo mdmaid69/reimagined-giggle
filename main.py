4be5b1b4d58157044e4f2d0b33a4e03fb025c279
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:47:52 +0000

    street nature positive move wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
