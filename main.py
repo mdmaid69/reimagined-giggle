3aadbc029fca1b9f24b4d9267e8535e4086c2668
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:32:52 +0000

    person neck recover action study

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
