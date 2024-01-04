ed8952b455ad94e8a171f7145a91280502af1f4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:13:04 +0000

    worry long mistake influence offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
