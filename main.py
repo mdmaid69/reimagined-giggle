3d4ee2436602cea995cdf01637989cd7f1ef5568
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:24 +0000

    up hurry pupil chase belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+def get_current_time():
+        return time.time()
+import sys
+def print_python_version():
+        return sys.version
