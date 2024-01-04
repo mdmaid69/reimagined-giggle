21015923097f68a08c4e2f0707c3b40058bf09fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:52:44 +0000

    Ff shrink release wish past

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
