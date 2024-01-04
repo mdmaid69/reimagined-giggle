3009e7be0252f8234184e4138c120c3adc89be01
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:41:37 +0000

    left twist couple hungry quickly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
