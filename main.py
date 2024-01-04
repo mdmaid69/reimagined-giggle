c5ac8fb8fdaceed4f043329587cdc7059e44fdd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:09:10 +0000

    season run steady anything start

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-name = "Python"
-print("Hello,", name)
+def calculate_acceleration(speed, time):
+        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
