9477b732f84498663e5bbcd8dd89d6b49720ce5c
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:11:39 +0000

    pause division rough grow them

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
