f332bd397d832f6feef08bb99b946450b8a63d48
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:41 +0000

    worried through 571enough sense position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_octal(n):
+        return oct(n)
