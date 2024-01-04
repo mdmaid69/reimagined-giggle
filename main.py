34faef3d996fdd712c637b5191cac5469eb9e16e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:25:27 +0000

    by centre sense authority ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import collections
+def create_user_string():
+        return collections.UserString()
