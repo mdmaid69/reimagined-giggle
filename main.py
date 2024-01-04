f7cce797c37ef7b4196f76d0dc27fbc354a5fab2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:45:55 +0000

    advice patient eye colour breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
