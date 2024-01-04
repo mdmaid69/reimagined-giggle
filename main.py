15c3bca7be668240a15dfbc1bda94ae676ad0962
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:49:10 +0000

    exchange mysterious bury bill tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
