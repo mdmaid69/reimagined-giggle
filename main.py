618deacd5a2d3c842ed19b0a5b856a8942796e53
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:18 +0000

    survive regret mention dead bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
