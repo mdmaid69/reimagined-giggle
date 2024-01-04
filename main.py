1818064ff67a96745a36dd9a37f760f83f54dc89
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:08:49 +0000

    force police today left apply

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def subtract_numbers(x, y):
-        return x - y
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_current_directory():
+        return os.getcwd()
