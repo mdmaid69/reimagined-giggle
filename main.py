970d1abd889b26342859d800ebd74b98b017e05e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:54 +0000

    below friend use bridge final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
