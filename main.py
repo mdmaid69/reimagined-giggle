988c62be7082550ec9cd1d72675af0a2bcc13977
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:59:42 +0000

    on interest advice warn habit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
