d06d8fda508a0c6ddb6b006904e69b68a589fba6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:39:39 +0000

    offer actually carry someone contribute

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import collections
+def create_user_dict():
+        return collections.UserDict()
