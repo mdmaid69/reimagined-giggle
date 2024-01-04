dee771ba6f1e449fa2ef74aad788651d61db1953
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:05 +0000

    happy climb foreign control once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
