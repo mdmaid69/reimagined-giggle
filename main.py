275033d6da85453182aee8fb72e2a6e75dee1bce
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:22:17 +0000

    easy past tune zero drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
