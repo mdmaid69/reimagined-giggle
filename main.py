bf99fc50a3cc339035e4fdce5324e166ab86e847
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:25:49 +0000

    past sensible left hollow light

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import sys
 def print_python_version():
         print(sys.version)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
