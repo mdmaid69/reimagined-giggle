406728a7f43af113925812d1ed75419410d9b2c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:10:26 +0000

    somebody 573entertainment occur down judge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
