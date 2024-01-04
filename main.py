e9c5736813cf4712817caf803c0dc93a8c90d1b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:59:21 +0000

    float spring runover bring our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-print(random.randint(0, 100))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
