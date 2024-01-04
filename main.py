5c9496b2536921180c1d4181cda950217c3a55bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:13 +0000

    by smile roll land confuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def reverse_list(lst):
-        return lst[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
