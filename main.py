014b783794b87c1e62dd2d62034373fae4f5f7e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:23:44 +0000

    check Nn quickly flow effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
