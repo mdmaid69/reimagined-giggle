f104b75e56d55d00631cb7ca122ffb646835922d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:28:02 +0000

    generally Bb song interest wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
