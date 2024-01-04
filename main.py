066daed64427c51cc5c505205164e6b3955cac06
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:17:59 +0000

    cost drop doctor aim notice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
