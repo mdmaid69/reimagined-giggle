9a23c4e989322fe850a84d071e521dbc6d47f398
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:35 +0000

    turndown extremely cap 570enjoyable pride

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
