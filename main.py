271595cc89694a2ca87decac4d1d56033fe35f5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:32:01 +0000

    possible crazy compare represent dress

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
