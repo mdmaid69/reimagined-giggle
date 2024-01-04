14d2987a6c308ded7997aef6944c779b243e3e83
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:27:07 +0000

    yesterday breakinto spite home century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
