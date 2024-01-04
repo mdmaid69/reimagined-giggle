37edd85535a0a2cf395db02fdf6e5283dd126a4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:26:12 +0000

    yourself worth bringup holiday statement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+print(sys.version)
