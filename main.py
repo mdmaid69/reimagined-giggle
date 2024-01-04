a77717d3f1d937043378af0da046418a5768c696
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:31:12 +0000

    scream store uniform tool my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import time
+print(time.time())
 import array
-def append_to_array(array, item):
-        array.append(item)
+def clear_array(array):
+        array *= 0
