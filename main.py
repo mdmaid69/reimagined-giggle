f40283c7c4d2c99b539bdf064a62c01739355453
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:15:16 +0000

    surprised bend prove passage opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
