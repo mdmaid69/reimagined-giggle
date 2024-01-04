ccf171926415235c28d1687d7be561cc286f3524
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:30:19 +0000

    tear birth chief fairly lookafter

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_area_rectangle(l, w):
-        return l * w
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def count_elements(lst):
+        return len(lst)
