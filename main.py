910033c1a1f0b75e7ee2d9ea30ab88dcf53d1a03
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:01:52 +0000

    calmly extremely man comeover paper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
