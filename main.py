831828fa0b2bc6ad41980dfccdb487e147252c50
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:01:31 +0000

    actually 571enough hungry cross full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
