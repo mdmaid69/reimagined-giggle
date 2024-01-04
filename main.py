35c7b941d1b8fbf087b8a177c36b11451798c37c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:40 +0000

    finish thankyou can thing join

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
