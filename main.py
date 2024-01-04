8adb6ba7b74a4ca8a55a19eec2e47c57cee7cd9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:55:37 +0000

    expect right prize crisis clothes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
