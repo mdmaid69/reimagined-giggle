4a7d88a90c39b08778652f7860980eef277e2633
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:43:30 +0000

    damage approve fond quantity cap

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
