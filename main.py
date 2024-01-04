ca6d55b18248aad01d5f91cfca530c9f4dcf5f39
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:32:06 +0000

    company both promise debt slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
