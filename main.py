70394582b088ec8dd97e25d60e4513f830dc14d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:53:09 +0000

    equal list provide secret compare

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import matplotlib.pyplot as plt
   def plot_graph(x, y):
         plt.plot(x, y)
         plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
