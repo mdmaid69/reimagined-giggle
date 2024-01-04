1e37f4a6305580aa0e248e5b2f01f55fda2181af
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:54:00 +0000

    after shall male pause wild

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
