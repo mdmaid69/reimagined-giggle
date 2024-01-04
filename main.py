095cf00abf220dc4a472c20ceba34018343ecd98
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:07 +0000

    admire pin land language breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_user_string():
+        return collections.UserString()
