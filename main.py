97fa32f0a3a18385c96957cd1c3f09c901fbcedc
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:44:05 +0000

    certainly minute capital regular seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
