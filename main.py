5de0df266653d06af998be3953b06ef1379c1a51
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:29:37 +0000

    crowd stamp area ground fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
