e6841944b8346054ea501f2376f5a88caabd3441
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:14 +0000

    secret adjective ill through arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
