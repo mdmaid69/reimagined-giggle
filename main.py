416519fc56e81759a1772d65e826df2d42691bd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:44:49 +0000

    run recover slowly praise used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
