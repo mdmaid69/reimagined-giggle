746a52a44abf212a3848352345d38d8b0d31dfb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:55:52 +0000

    minority might major tense grateful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
