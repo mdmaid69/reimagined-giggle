ca5be73cd19b11c1a51583782975c3974355c44a
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:48:55 +0000

    spot pity cook by bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
