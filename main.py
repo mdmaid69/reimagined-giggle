874a6ae0c77451a4ea765fb22bd9ca828b6402d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:58:48 +0000

    once exit complicated odd rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
