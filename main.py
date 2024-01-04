bf06d31d11e62f0089ab0021ef31122c8e80e482
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:30 +0000

    disappear interview upstairs uniform describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
