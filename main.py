870c1171adb99f2fc08a4446987f8ae8f719d37d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:26 +0000

    separate work mainly sometimes kick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
