2261ff94c0044d00d390f7785b815c660d3d3cb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:53:10 +0000

    report number event advise accidental

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def get_time_since_epoch():
+        return time.time()
