80b3ccbdeec49e67e0a09d6b87fe6e2f035e04ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:36:41 +0000

    fun imagine refuse business advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
