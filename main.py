c14719f3bff523be0ebe77ed286a7d075a79d915
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:16:28 +0000

    lock strange live meal criticism

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_list(array):
+        return list(array)
+import platform
+def get_os_info():
+        return platform.uname()
