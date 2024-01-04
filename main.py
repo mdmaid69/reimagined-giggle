9bee3cfac3e68e2c7455c1d5d12e2884ffebbb96
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:59:20 +0000

    rate patient equal begin move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
 import math
 def calculate_polar_to_cartesian_coordinates(r, theta):
         return math.polar((r, theta))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
