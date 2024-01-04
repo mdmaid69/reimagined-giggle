b5f480d673d1e2044d436040d7ef86acc6016f59
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:33:21 +0000

    intend peace any proper sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_area(radius):
+        return 3.14 * radius * radius
