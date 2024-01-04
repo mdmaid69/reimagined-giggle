48f2e89d946993327a46dd2dd5433066e3160ecf
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:04:52 +0000

    develop total oil promise occasional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import sys
-def exit_program():
-        sys.exit()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
