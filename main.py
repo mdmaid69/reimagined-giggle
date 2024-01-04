ccf178d7ca58b6478a48a93eaa401ef8399a89bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:46:25 +0000

    exchange completely married go afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
