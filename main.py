798f9825eba4a0791307b576282072bc5a994871
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:03:27 +0000

    record still swimming sleep coffee

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
