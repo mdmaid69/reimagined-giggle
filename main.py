d9a2adb71ef9347b18e3a9c53d6323e4248057fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:45:47 +0000

    so friend star habit but

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
