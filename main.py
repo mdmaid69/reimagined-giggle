a69c224192863278586424bf9ef3c17f6bdff17f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:56:04 +0000

    shape connection see hour reject

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_density(mass, volume):
-        return mass / volume
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import getpass
+def get_username():
+        return getpass.getuser()
