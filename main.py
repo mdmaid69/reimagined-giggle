a4bd4498b58a5e2f27f3745491d8037de3757af9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:03 +0000

    contact pride careful correction hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def sort_list(lst):
-        return sorted(lst)
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
