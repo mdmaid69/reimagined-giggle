c4af1a46fbffc98afc37a551b44f0fd206b72848
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:32:58 +0000

    protect community roll pair tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
