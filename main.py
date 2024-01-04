bf69cd4beefedac37e7dce3a9bc1dff4ec8fb8f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:37:01 +0000

    swallow prevent fall 564encourage calloff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
