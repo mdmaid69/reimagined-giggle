4983267ba2eeccdd9ce0d3dfbc8bb4eabfec5023
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:49:22 +0000

    Ee walk mother offence work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
