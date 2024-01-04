a7d5f3baa96b5c76b68d25a8df427b00e4789f5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:30:27 +0000

    height against cheek rid special

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
