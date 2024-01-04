791212e316b325fd72d454e81a618168efb3e198
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:50 +0000

    well stick use special well

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
