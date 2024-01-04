48e7138c7ae4377c678ba243d0a376444ca8cea5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:49 +0000

    sometimes inside slight afraid fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def cube_number(x):
-        return x**3
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
