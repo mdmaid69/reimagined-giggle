65bcae053bd88a9791018f3d387b01c6a142a3de
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:58 +0000

    reaction punishment opposite crime hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_directory():
+        return os.getcwd()
