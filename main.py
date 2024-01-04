3f92fbfabea69c855958d9d4d5abce4b7535f961
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:24:43 +0000

    service expression event judge suddenly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
