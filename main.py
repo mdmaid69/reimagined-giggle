3f9557331ba8edadad071710425c03368f1071f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:10 +0000

    goup centre whichever interrupt confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
