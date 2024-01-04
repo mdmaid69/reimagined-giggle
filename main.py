375fecd945f7e470d1940d173bd041bae00a4668
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:23 +0000

    slowly forever tend turnoff either

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def add_numbers(x, y):
+        return x + y
