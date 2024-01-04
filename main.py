d8adeea7e62c1374814082b0752488f26b053fe9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:07:11 +0000

    how petrol get excited cover

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
