886d2f42c4c65ca25efc0a78b9b4c56256cc913e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:44:47 +0000

    into expect warning indeed public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
