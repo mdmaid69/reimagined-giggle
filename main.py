62833a7cd4d527eb5ef26939e449d9ecff25b3af
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:50:47 +0000

    before beat raise contrast little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
