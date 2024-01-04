64bb1db63ac2f8f3a70132039064356d23934b12
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:10 +0000

    house software even left walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
