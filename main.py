4886c48ca44ce322e616bce98a60e2f76d0bc57a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:47 +0000

    sing talkoutof bite ride welcome

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import collections
-def create_stack():
-        return collections.deque()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+def roll_die():
+        return random.randint(1, 6)
