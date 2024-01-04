aac416c8d63e064021630537a645f39ee6161be6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:46 +0000

    yourself colour ride avoid compare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+print([x**2 for x in range(10)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
