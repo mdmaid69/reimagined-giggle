6e7eed613c9cd1223a312372c9c90d0cd49fce72
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:46:37 +0000

    same laugh fat really most

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
