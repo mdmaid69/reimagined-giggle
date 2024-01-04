e265d5e3171729906bd734c3eaca02e7e29eed8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:26:08 +0000

    ever improvement usual work share

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def clear_array(array):
+        array *= 0
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
