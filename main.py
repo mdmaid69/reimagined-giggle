60c0d4ccf5b23836f417377bf96beb2136cf89ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:51 +0000

    swimming beneath would cook welcome

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
