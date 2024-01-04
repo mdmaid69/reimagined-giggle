0be50eafc2682af5a8a2ff79667a013c3d9b6f1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:36:29 +0000

    clearly offence wild instead precise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
