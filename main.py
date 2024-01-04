6ec83372f4fdf8d34110fd8eee13467be1d8446f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:00:58 +0000

    already whatever completely copy set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
