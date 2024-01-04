7c70d34c990ab703f84c75542223c472454a98f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:03:45 +0000

    worried forever fear rush organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
