3463d8fc23b2dd52f1fd1206ffdaca062b1f579d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:45 +0000

    mystery afterwards quick discover question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
