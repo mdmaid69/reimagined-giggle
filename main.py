5de41f44e001a2334f14365d34655920a2aed01f
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:32:44 +0000

    kick slow such arrival tax

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def get_current_time():
+        return time.time()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
