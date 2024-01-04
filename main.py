0fe359a6886685bc370ac27723d99783004afd3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:24:34 +0000

    bill space immediately pinch trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import time
+print(time.time())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
