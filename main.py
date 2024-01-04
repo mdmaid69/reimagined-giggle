b6494ae1732def5f31bd0b60d88334526d8ff50a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:07 +0000

    row space pinch development cinema

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
