6d1fbe553aad494fd3f6e3e3a1fd43de1c6843f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:54 +0000

    instrument idea congratulate wash almost

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
