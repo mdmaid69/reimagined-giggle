871732dd3ecb56bdef50beddb1e11b26d3b9c450
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:05 +0000

    quickly stamp without side translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
