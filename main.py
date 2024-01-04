5e940a2a7dcb063cbc225dcc5cbdb150b2c3f033
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:56 +0000

    event content preparation offence stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
