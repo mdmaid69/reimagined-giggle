e58bb02fba4cb311757b9ddf1a0df979920e4bb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:26:16 +0000

    embarrassed holiday hold serious material

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
