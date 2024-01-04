03e90729b9580bf5d08deb3b515613b43606b016
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:23:19 +0000

    rid project workon clear together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
