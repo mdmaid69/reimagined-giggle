1be58c688ee8aff201605ea5d0b79148362ae585
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:06:28 +0000

    Hh doubt odd relationship situation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def count_words(sentence):
-        return len(sentence.split())
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
