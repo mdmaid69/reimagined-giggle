d128015682382496c1b6bde9e265bc2ea961bf83
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:46:38 +0000

    consult exploration scrape put prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
