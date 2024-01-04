57051fa1d6bbd1fbbd5d6be5c0285b407b052bf5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:05 +0000

    sticky idea bottom goup tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_power(work, time):
+        return work / time
