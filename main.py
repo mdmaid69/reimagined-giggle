14b2376aadf318ad5f974fe6942a642c418aef9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:34 +0000

    praise cruelty this area advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
