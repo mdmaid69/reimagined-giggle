49289e2ce453eb4bc2f94db1ce4f9c1c4f38ef76
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:53:07 +0000

    punishment pleasure proud quit freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
+import time
+print(time.time())
