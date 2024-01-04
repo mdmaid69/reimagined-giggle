b2883fa6fd180a86afbde055fae7c2e76914395b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:39:53 +0000

    extra no she far level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def get_array_as_frozenset(array):
+        return frozenset(array)
