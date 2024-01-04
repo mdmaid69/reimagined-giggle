f09fb2c9071d4e4d059a518773d05b44c6367413
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:53:19 +0000

    office promise noisy move before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
