d7d3a100fadff61c83ba14103ed86ce7afee8042
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:29:22 +0000

    makeup slowly independent good practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
