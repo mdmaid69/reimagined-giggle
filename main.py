a4c1dc6a0847a7a7e993c4147660bb02f18ff852
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:26:36 +0000

    it test owe union within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_density(mass, volume):
-        return mass / volume
+def get_array_item(array, i):
+        return array[i]
