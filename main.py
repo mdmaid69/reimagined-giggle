a12e7adfb5a343df68be2664e575826850b76247
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:56 +0000

    fit boil pretend agree form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
