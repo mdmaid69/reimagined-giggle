da8ef412bc7c8a9e791a0642629835a5fecbb839
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:59:39 +0000

    measure market moral frequent bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
