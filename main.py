ee60980309ccb818578eb873e14931b1af56a7d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:57:10 +0000

    juice interest high debt basis

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def is_even(n):
+        return n % 2 == 0
