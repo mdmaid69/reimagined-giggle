22ae4a6f347c74536d819d7fe262ca1a65a04495
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:54:14 +0000

    pool joke violent do belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
