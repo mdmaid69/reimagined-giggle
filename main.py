f5b5c12c63adb279ffed5176ac09fb8dc34c03f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:05:18 +0000

    balance burn bad 568energy wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_rectangle(l, w):
+        return l * w
