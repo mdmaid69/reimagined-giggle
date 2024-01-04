30b0b5bcb5f307d671bb431d535dfc14f523a125
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:01:48 +0000

    lie concern interfere owe high

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
