92dcc623bc9543c1aca6ee08b15bac1255f67a90
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:11 +0000

    effect gap port sea ashamed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
