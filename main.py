b950c393b6744403831aebd5162c467829d73056
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:40 +0000

    instrument exit avoid whistle squeeze

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_str(array):
+        return str(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
