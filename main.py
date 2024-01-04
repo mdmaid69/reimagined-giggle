f4b76b77b99c873e62797cfe53f94464c7318d64
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:55:56 +0000

    bang smooth head us fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
