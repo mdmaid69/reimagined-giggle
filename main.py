b38d993a05d3ae8218a46b10dd4001c6165a4e0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:25:22 +0000

    whole sometimes sad basically ashamed

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
