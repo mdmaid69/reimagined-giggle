fe962896400ff6472e264d5ba71d430f19f18487
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:48:30 +0000

    proof treatment fairly little ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+x = 10
+y = 20
+print("Sum:", x + y)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
