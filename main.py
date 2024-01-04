6f82c3e1b12cd50d20e3a86c655ad22570cb6afa
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:37:39 +0000

    picture fail rely worse peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
