bc8ecde7aaab57f46baaa8c4235bb48f212a4111
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:27:07 +0000

    bridge separate guide around interest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
