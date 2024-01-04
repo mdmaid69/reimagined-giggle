0678bac171e86c23447689d0118119bfec275745
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:55 +0000

    very wrong below calloff land

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
