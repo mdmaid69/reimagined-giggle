baa64f55b4ff5d65664a21d27f241987838e6d76
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:13 +0000

    frequent cover artificial thinkof close

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
