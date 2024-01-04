ef77d21177fa657bf9aaa47c735f52611ecc5e9c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:51:08 +0000

    float comedown card negative accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
