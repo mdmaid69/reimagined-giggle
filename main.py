44ce4489cb679e1cd8d42e11dd303bda130cd64e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:08:48 +0000

    urgent kind land praise she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
