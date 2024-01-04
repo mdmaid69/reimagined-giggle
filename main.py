36cd26040d0bb520b3c32c6143fcb5a06e47e9f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:13:04 +0000

    offer trust owe answer especially

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
