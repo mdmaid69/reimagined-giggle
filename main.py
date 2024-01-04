ea6ad87f87fd51781570c0cacf4d518cfbf690ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:17:26 +0000

    quantity platform ordinary Ee manage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
