e66cf2aee25f23e9e2c541788bd2c4827f605593
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:16:14 +0000

    pleasure pour length education somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
