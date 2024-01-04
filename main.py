a51b41752f539e5f7bfbe876c860dbaac6051ec6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:16:56 +0000

    class rest scream goodbye tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
