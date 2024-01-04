a81df959b0bcdb4a2c0b13f6f837add35697a99b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:36:03 +0000

    cry mystery definite school strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_set(array):
+        return set(array)
