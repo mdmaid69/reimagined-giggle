f3db7e626300d59cc9222f4a572607b7cd38e123
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:49:53 +0000

    strength idea count worth reach

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def reverse_string(s):
+        return s[::-1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
