a715f2c266c280a738e31d86d20160afe236d388
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:12:07 +0000

    pair tip what forever collection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
