bdacfb9689a33728e1d06f2128cb1eac3600b129
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:09 +0000

    luck bad spray anyone mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
