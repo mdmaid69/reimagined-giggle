26c50678ecd9d26aaaae19399c08b0c9618c9f89
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:46 +0000

    him objection thanks shape confusing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
