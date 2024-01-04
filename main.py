6d230d5d5241e0a6aad0db60f018dc2c700ccaa0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:25:48 +0000

    surface schedule gift right fire

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def multiply_numbers(x, y):
+        return x * y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
