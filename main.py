abe8291181764af9100c6ff939d701cd57ae3f6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:02:42 +0000

    slide course train beat bottom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
