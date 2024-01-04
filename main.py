8dc8613d114d856711ee9e35bc65689ae87dbfd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:58:27 +0000

    disappear object agreement belong roll

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
+for i in range(5):
+        print(i)
