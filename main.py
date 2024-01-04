7e9afd582cc7e73f652829a4aae2d3b8a9ea6be3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:18:56 +0000

    favourite window during wind undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
