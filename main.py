6bc400d447f33b1b2219ef00502d55d00ff7b741
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:02 +0000

    cheat sentence Rr only spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_even(n):
+        return n % 2 == 0
