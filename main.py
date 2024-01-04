9bc60d6c902362875208028127dd880710ff3617
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:14:55 +0000

    wash public dead dance nearly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def get_array_typecode(array):
+        return array.typecode
+import os
+def remove_directory(path):
+        os.rmdir(path)
