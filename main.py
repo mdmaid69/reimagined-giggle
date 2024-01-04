2ba933ebb30415985c0667fc74176fc467d190cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:52:41 +0000

    key proud sink maintain influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
