0d0c8fc4ca9f0b23c7a0328aeb0575ee8af8c74a
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:01:13 +0000

    hand safety between noone term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
