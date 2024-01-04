0b87a533aa987de26472d1b09593de7641d61bea
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:20:19 +0000

    change sport actually whichever perfect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import time
+def get_time_since_epoch():
+        return time.time()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
