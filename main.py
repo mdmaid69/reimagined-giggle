ff5b7f70624fa71b2deacf295e46b37c247d9c3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:05:47 +0000

    pay strong Kk mend frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
