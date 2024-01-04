6e565605ed21020dfa4899589da4230e1b3b22ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:23:56 +0000

    sad profit emphasis oil land

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+for i in range(10): print(i)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
