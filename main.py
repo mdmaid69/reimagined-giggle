152a1c0febac03f0b2d8d469b65e959eeeeeabec
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:02:36 +0000

    paper neither feed cough exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
