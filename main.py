21494f0cc6733f7cb6a52b20c06ce265ffd5b6f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:10:46 +0000

    tread approval none smile wonder

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
