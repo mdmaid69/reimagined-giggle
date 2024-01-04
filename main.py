1f5dad7a37551919b4b318735b1778a97625e114
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:35:27 +0000

    belief turndown aim class corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
