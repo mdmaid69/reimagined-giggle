f80f76809867ced3089c6679837e76727dbababa
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:12:08 +0000

    already trick afternoon condition enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
 def run_command(cmd):
         return subprocess.check_output(cmd, shell=True)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
