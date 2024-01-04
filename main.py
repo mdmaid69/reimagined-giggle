88bed7f1dea800ebb350ef26dc75651435644067
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:11 +0000

    birth bound brush check confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def square_number(x):
+        return x**2
