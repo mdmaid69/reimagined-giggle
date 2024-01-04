09b570c480516abf1b52c82d24450ff4da760410
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:23:54 +0000

    tooth illness confuse suck no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
