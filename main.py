d53b67126401542cc60d081b32959500d576665c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:27:45 +0000

    near split 569enjoy cheek first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-print(time.time())
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
