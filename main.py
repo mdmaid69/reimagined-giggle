ec225046b34921da5f65f83d57420d1595986f62
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:14:11 +0000

    heart oil front calloff base

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
