f692868746e7242a0840b798c348979bac933450
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:46:04 +0000

    present dress from careless will

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
