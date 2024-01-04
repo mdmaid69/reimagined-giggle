b42db7eb09abdae96846cd1bbd6e8642a098b93e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:38:17 +0000

    somebody copy jump suggestion arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
