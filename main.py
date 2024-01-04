b7d59284b9034893401909df717e623013f66fd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:27:15 +0000

    sensible odd negative instruction figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
