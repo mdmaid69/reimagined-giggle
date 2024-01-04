363528d1c04f0d2b05ba72917581b0705920363d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:48:58 +0000

    real home point offer dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  def is_odd(n):
+        return n % 2 != 0
