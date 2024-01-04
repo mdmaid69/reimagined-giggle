5e70065a48f86fd5364fe4146e8c802c03c8f50e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:16 +0000

    emphasis press learn dark remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
