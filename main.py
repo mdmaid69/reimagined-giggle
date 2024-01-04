d50026b0c247b7e019e38ea801ba2dfd65eab854
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:05 +0000

    standard similar getback any eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
