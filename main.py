3909eec5708625ee678bffad7d685102aa9562f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:49:19 +0000

    organize drive glad view Ee

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
 import array
-def get_array_from_file(filename, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.fromstring(string)
         return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
