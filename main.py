9a24cb86c1d17951759f5e4cfb252e9be4ce507e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:50:32 +0000

    respect petrol drive birthday comfortable

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
