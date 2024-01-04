c811a5f4cc2842cc5cf99f64093ad261749a705d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:45:18 +0000

    spare child organize answer callout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-print("Hello, world!")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
