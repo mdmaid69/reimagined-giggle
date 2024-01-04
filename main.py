a7832bd0e594efdb16dd3506e0b89cf35ce14cc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:47 +0000

    connected look after black shall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_power(work, time):
-        return work / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
