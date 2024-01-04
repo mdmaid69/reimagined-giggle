bb8b9b3bc31be594df23bcb3d2429ac990638082
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:39:40 +0000

    original sink reserve piece information

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print(sum(range(10)))
