e5c259c02cdaa9f5c77b6f1436434df46799a18e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:47:23 +0000

    influence happen familiar birth bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
