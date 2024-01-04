4d7dd36605a856652ff0bfaa61649d12826b935a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:07:14 +0000

    spoil innocent impressive appeal attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
