278577ffc441b780b8e96a322517056c76e28041
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:49 +0000

    discuss repeat bridge lean spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
