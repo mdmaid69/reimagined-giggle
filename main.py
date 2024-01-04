9cda496a169e9a2a0ee0ebbf23c145b58fa6d4d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:41:08 +0000

    schedule television scrape now thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-print(datetime.datetime.now())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
