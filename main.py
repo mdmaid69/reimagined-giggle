acf17a4288731816589b816f5902b8092b98f567
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:32:18 +0000

    happy Itself bread pocket run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
