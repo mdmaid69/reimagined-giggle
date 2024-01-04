a5302bd28183f430260d471583ea291ee186125d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:27:16 +0000

    shape first still freedom move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
