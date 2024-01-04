3bac5475617bcb12de764d12184bde046c9bb95f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:54:32 +0000

    probably evidence fortunate city attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
