d1a00bb0a67ca6afd65d872bcfa887dac630a9e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:51:21 +0000

    shrink able food minimum wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
