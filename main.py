8d4f4c9acd6c2f41f17a798b5b6fbec9e44302db
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:44:40 +0000

    try beautiful willing reach fight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
