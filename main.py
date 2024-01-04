ee3f7567965217d161c23b099e07ef54fc05427d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:47:12 +0000

    thumb mouth shade community she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
