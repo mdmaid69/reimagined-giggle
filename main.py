ffa0a232f4554c6565e6b549095f555a5a970355
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:55:11 +0000

    justice later welcome allow tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
