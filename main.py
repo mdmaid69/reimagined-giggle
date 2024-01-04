8030b7269b760bfa17a4dcc9204cb27e6a986a6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:44:50 +0000

    report appeal increase slightly reply

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(numbers):
-        return min(numbers)
+  def sort_list(lst):
+        return sorted(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
