8da5db5faf2502efd1952c3a91cd50b4a4d1ba66
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:50:28 +0000

    more field demonstrate doubt dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
