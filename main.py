0b4cf75b7b59c0c63dce88a632c792a75129cf89
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:12 +0000

    cool arrange crazy kill degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def count_words(sentence):
+        return len(sentence.split())
 import math
 def calculate_floor(x):
         return math.floor(x)
