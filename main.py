77c915f1985a1625a773d2b0c8728db83b942eb6
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:31:15 +0000

    loan offer yet Oo cost

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
