8a7f377624c3d93518da1fb4f3abf44644223772
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:41:49 +0000

    excuse constantly responsibility sensitive almost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-name = "Python"
-print("Hello,", name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
