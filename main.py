86beed79fb2d52543c18391f9144984123a68462
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:09 +0000

    trust squeeze settle back study

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sys
-print(sys.version)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
