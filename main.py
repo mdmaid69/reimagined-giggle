61ce843516b0388ae6ba396db3a9e34e7f988ce1
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:44 +0000

    hurry authority emphasis design exhibition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
