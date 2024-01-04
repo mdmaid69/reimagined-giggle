7026acddc830a93fed9d2311f935c4a75f40c683
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:28:29 +0000

    shelter wall beat contrast pleasure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
