20ff80b1b8ab0e336946aaf6e0331c0b1675f4d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:01:36 +0000

    check have lookup worse Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
