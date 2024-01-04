29d78293d2c145e76ebbcc06e44ac88d70330664
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:54:48 +0000

    film address block increase border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
