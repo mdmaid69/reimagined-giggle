e8eb98c7e71ac339abc77ff4ea1b3193e8b59474
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:10:53 +0000

    say school tonight stand exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
