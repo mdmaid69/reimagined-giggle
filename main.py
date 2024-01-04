fff45090e1ac679f938e8154e2858a1cb5ca6b61
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:17 +0000

    main save murder train type

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_acceleration(speed, time):
+        return speed / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
