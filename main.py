e823e525b92b295c08e6dfca6b4d2dbc77da5621
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:16:57 +0000

    convincing demand date him abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
