551a997d16060b2decf0ad601223c1e827483381
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:28:16 +0000

    even peaceful never 568energy exam

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
