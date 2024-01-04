a611495ad3597983783aa6686700207d86508844
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:20:54 +0000

    whichever church tidy tall wave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
