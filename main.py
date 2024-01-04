e8952f6c7e03db283398dd6371d1d5013aa926f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:58 +0000

    her side under any flat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def get_array_itemsize(array):
+        return array.itemsize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
