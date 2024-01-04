98f484f5d701d77311981a71ec030c67363d4ea2
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:55 +0000

    total nature statement uncertain involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
