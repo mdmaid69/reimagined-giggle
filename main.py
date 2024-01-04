725b29a3e93e5e6a5fa5c324c98bf1640af8200b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:52:31 +0000

    prepare punishment else build succeed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
