fe44407a70323c035659b2bb805461be9fb55ab8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:06:25 +0000

    demand finish holdup should ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
