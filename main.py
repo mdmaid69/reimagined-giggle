d414689dfffa5746b3e7ea4f3efd19484570f8cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:00:20 +0000

    voice exploration conduct load walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def square_number(x):
+        return x**2
+import array
+def get_array_typecode(array):
+        return array.typecode
