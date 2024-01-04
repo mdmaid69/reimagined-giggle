323cf9d8c8c803cf87c3434b57b4b693bf9b37bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:50:07 +0000

    treat family visit comeover holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def is_odd(n):
+        return n % 2 != 0
