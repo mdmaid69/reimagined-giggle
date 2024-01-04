d8040a1517a12cb2efc1c3e8912b09120a57b512
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:42:54 +0000

    street likely through forget plenty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def sort_numbers(numbers):
+        return sorted(numbers)
