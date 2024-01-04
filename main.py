68f396b47d93c2f54b84d1824d5ccd4f8f0e61e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:57:28 +0000

    movement meet know whether bill

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
