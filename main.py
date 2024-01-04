f08810318b32a9796bfa7df80373fb9ce36db9d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:08 +0000

    concentrate yours congratulate uniform wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
