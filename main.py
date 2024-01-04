cb79a49e2d7909d12a3368505067f2472ce01df1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:31 +0000

    wall autumn lookup accident increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
