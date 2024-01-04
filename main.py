7289dde717858dbc2a3770d1db20ccfc0e8153b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:39:14 +0000

    flow borrow transparent unfortunate comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
