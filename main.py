af5742f397038a1fb4bc5ab80ddfa09233cd239c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:26 +0000

    human personal her unable rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
