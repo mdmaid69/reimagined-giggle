9eebb4fc1970f34fa2da77c50e11e3d3e9cea787
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:58:40 +0000

    declare light disappear left plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
