932bdec2d2ce00718e0ec80bb4c3979706c5566b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:38 +0000

    respect throughout heart famous hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
