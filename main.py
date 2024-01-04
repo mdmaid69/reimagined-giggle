2fb6a2503415a8cb135c7366610cc44e2586881a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:02:55 +0000

    bother mass free calmly description

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
