123822fd1eef7cfdcbdc20838a8382d6adae2a6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:59:36 +0000

    generally whoever yes view ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_length(array):
+        return len(array)
