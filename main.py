e24d0a5b1663b5f9c24589ac50b75bbc8db254af
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:34:44 +0000

    object do callback crash example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
