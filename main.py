f91116c33ba73bf96c4db1dc32824f8efebdd33b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:18 +0000

    bite about of serve interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
