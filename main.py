816d4928d9f54aedb9120cdb19baa1fc08a18fc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:37:58 +0000

    definite well after sick funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import sys
+def exit_program():
+        sys.exit()
