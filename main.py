11e739e6adbc833f16fe893031f56aec571d94e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:13:28 +0000

    broken goup design represent like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
