ac290e6487e3f073a16080dbf58e610b107865fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:01 +0000

    various whichever loss slow solve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
