046a838986bda42434cbff718a10a8321a477af3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:01:16 +0000

    lookback(on) eager rule to embarrassed

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import random
+print(random.randint(0, 100))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
