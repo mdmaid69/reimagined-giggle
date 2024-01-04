475431d49ba629a7b4cf2305975a278be18ae822
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:53:27 +0000

    close comein priority sell 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
