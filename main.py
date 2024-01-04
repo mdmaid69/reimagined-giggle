dff585a69c9eacd49b5b1df5c552833f843d0982
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:56:26 +0000

    immediate large convenient least no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def count_elements(lst):
+        return len(lst)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
