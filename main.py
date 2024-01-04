d46592165d8eaaccf793593ce0938aef8e6d8e74
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:18:53 +0000

    her future about Nn afterwards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
