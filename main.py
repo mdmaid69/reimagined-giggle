40256e1504b8658419efe20a79b7ed7f3ca382bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:27:21 +0000

    pool minimum quit uponon that

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
