a40153c0f1fd0e248dc78a2d8807a37f55c22d8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:00:15 +0000

    base sensitive last community class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
