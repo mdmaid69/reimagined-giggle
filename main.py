54b6dcefa2543911d79ec134df3140b604a432ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:27 +0000

    various after complicated row doubt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
