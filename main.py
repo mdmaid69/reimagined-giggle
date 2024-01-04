c1382ca4f5fe55ee342e4e50365e405a2ba79f22
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:03 +0000

    material road under crime ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+print(sys.version)
