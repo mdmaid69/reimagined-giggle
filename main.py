c58f5fb46c9d11c460da9311e32bfa27f19daca5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:37 +0000

    correct weekend low wise holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(x, y):
+        return x + y
