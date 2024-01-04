65ce6407d6c08b99a98d004d5ba8e9192a637539
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:08 +0000

    clear getoff uponon shock hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
