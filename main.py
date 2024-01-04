69897d9390e547930569b77cb0b48942d309397a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:06:39 +0000

    separate organization holdout mix sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
         return a + b + c
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
