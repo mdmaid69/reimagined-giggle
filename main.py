6581865c06f3991249a18f643a33a28d22070c26
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:39:24 +0000

    out and course person band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
