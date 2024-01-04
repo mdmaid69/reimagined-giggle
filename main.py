7f5de3d325421f42ad6288a0314de7f03b7bcd8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:00:18 +0000

    task service terrible arrival decide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
