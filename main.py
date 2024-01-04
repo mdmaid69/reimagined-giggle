58e7b684aa2265215acdd142bd1ee65990bb8510
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:05:11 +0000

    available escape within later war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
