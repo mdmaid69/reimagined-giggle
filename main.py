c7db1fc264bb784551dd586a7c6070be1540dc16
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:56:25 +0000

    surprised unlucky fix believe home

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def subtract_numbers(x, y):
+        return x - y
