b870eed670ea8111e2ed1d991f6fff7393174e2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:01 +0000

    only fresh transport 567enemy light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
