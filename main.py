9fbb10bd8715776fd78280d5a34177fb4372411f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:16 +0000

    near shrink quit common keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
