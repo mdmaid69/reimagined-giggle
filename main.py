a3ca6d35deca2a7b98e1653d2f4e7e82d6167a63
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:48:18 +0000

    consist bus but help lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
