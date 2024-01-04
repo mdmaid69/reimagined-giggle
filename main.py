d525abc7508b81615908b65f14f8d2976ac1f92f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:25:23 +0000

    complete greeting objection address government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
