6bd38f04df15be66294f07e142780d1380b2d9e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:29:12 +0000

    vanish grand drawing exciting murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
