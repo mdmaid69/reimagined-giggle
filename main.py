c50bfdc523cd39d74fe7cdafa3457ad17efab229
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:29 +0000

    mean uniform several father pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
