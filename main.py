0b0b85d5e161da29fc6784068d3e3fda6b07e9c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:27 +0000

    crowd drive drive object instrument

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def remove_duplicates(lst):
-        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
