32b9111ea6d60af34432795b003f5b30bfb25314
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:05:08 +0000

    language serve switch sail calm

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
