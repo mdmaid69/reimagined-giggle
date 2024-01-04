b2098bfcb903083cc18d586049e7249e39b694d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:59 +0000

    calmly valuable mail plenty shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
