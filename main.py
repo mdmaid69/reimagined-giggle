a5b9b731cb204189ada0d774787d383c0e9afc93
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:10 +0000

    anxious load bite uponon first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
