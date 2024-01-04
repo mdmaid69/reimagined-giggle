470ca40ec1c3c4932020785803e3482cc3536c42
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:56:03 +0000

    them hers refuse bath bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
