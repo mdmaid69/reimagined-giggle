c5b7aeefe817075ec8ffacd90780d54a1c6f162e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:55:19 +0000

    pocket change work failure schedule

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
