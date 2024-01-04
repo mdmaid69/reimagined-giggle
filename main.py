00d54b8eaede7519353c52b5302cc77175dd4bfd
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:20:46 +0000

    objection crash last first meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
