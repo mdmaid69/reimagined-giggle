416e0ead0e16437dd6bd31804971aeb4176a6d90
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:31:21 +0000

    far less clearly possibly anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
