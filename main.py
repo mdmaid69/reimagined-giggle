3fb51ada2343e76ac23520e3a927d9e208e59159
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:34:30 +0000

    measure check claim frequent smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
