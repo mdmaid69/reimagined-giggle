87bdab47fc80f661da8908b709ce7caa16cb4e44
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:26:23 +0000

    lucky slow ours case backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
