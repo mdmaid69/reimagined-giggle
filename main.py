8108051b99bc27c6078bdedf72cb83e42bd539b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:15:14 +0000

    tie but transport collection disappointed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+from collections import Counter
+print(Counter("hello world"))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
