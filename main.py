e7af6908bd677d41f024fdf399dc76c106f718b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:15:21 +0000

    regret anywayanyhow recognize sure eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
