aefd9131b5713994f128746070faa312ed0b4dd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:45:31 +0000

    reasonable swing empty protection comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
