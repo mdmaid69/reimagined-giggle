a66e6f4e5fc29defa273eb85df6f2eb3d6fb2711
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:16:44 +0000

    run firm call violence not

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
