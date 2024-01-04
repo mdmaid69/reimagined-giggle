f7ce320453551201975bfe88d14ebda08c7bd7f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:34:32 +0000

    cook free be sensitive Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
