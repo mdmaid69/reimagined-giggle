41e8a369d15caf71389dde0c52c9376a06deb0d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:02:41 +0000

    comfort listen bad Uu agreement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
