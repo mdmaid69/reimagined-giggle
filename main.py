497a04fd01fc2c4dfe9ad0cfbf46c37c73d814d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:09:46 +0000

    mind top slide amount cough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
