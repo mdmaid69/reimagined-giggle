9e654d72e41e9ba5b74008c9b83030c5ff36aca9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:28:39 +0000

    flavour union past sort practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
