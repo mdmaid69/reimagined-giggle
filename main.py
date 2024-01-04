b0ca358ff2ed376634726e3f4bd3d223dac2c235
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:28:16 +0000

    actual later pressure politics community

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
