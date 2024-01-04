2a2422cacb8b680fd91725a6c4297d33a12c8090
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:13:56 +0000

    crime still remark language message

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
