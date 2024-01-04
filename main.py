ca91ec68733f3886abddf33f33dc48a4bafbc820
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:00:07 +0000

    roll represent about spring match

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def remove_duplicates(lst):
+        return list(set(lst))
