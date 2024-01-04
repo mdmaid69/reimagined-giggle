3ca9cc19591d68b79b72390656de21d2dcf043c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:56:27 +0000

    involve for blue anything start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
