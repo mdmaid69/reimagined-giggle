d09642dbe09209e6a6e2ee592d3decae679c8dce
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:06 +0000

    cry guess discuss beat wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
