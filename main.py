a782868e6a970326d048716af20d9c8ff732105b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:05:39 +0000

    for clear great awake loose

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def square_number(x):
-        return x**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
