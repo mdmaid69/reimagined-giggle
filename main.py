759bbf6ecad93269278e75a0ce058fb705643830
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:25 +0000

    introduction giveup charge image range

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def divide_numbers(x, y):
-        return x / y
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
