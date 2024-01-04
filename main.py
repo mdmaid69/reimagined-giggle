5ceb64b7ace2b44a018216a6bf9bcbb0194c23ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:39 +0000

    supply bringforward beyond balance target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
