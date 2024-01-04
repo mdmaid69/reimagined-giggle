fbccfd502deb498af49051ea8146f6772e32797e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:24 +0000

    official Nn high bin below

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_density(mass, volume):
-        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
