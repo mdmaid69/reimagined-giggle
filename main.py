510dc3d056fa0be0018d0f1174bce11abb86e53e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:03 +0000

    ordinary squeeze expression drink make

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
