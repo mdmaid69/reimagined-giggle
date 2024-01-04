b4b1053caec4a579c9b873790f938784062e5634
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:50:02 +0000

    minimum speak our actual pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_odd(n):
-        return n % 2 != 0
+import array
+def clear_array(array):
+        array *= 0
+import platform
+def get_os_info():
+        return platform.uname()
