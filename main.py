71d1c9e80d7d0804571b859de60b24aafe67d14b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:11 +0000

    tape Gg close murder combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
