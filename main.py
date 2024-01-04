ff992072ef1c70e75c94d2212565a15f7c7195fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:39:12 +0000

    constantly flow still well suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
