348f17512dfbdcf334da6fa89052142a8f207d07
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:16 +0000

    trust choose receive disgusting disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
