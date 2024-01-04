c805bc853fd283c21377965367ddf3d4895bff14
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:37:16 +0000

    worse improve laugh niggaacademy branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
