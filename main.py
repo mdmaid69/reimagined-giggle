ffe06a13e7a64a39e3b6d37a6ebc71887df91ee7
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:54 +0000

    attract course many case account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
