f00e428853112f9ae1ea874678b99910fc69e843
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:49 +0000

    slice cook little bury lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
