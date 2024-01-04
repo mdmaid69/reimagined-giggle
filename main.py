0c1231fde62470fc96dcb8a63376517141797c81
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:52:43 +0000

    involve connect capital butter now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def clear_array(array):
+        array *= 0
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
