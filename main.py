172d81836d41152c6e6186691ba2ca8486a908a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:07:22 +0000

    about side pollution rise maybe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
