55b242258368799bc963941c1cb53fea8bcac020
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:51:41 +0000

    above operate send clear will

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
