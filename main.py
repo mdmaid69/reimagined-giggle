28428f0773e87e60ac4334d32ae430fc91f1559d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:46:11 +0000

    pass pass score touch to

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
