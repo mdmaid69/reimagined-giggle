cc01dcd9ddb224cd68d4a14e7403ea10c4273748
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:22:06 +0000

    claim wash thing scene here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
