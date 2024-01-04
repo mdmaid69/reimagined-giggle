08e38cebd7a0f847693c309fcdc0c1c8a487e554
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:44:49 +0000

    beat afterwards fortunately crime worried

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def reverse_string(s):
-        return s[::-1]
+for i in range(10): print(i)
+import array
+def get_array_length(array):
+        return len(array)
