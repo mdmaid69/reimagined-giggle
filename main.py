04279d330999b7e00d946ccb939412cb25aeebfd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:12 +0000

    illness bit argument bet clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
