b532490c028b74e9b7cec2f4d346067ee3ef5fc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:32:16 +0000

    fill cruelty forget Zz Uu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_pressure(force, area):
+        return force / area
