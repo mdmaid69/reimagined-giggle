b56b26040091fcc7925071b6b0cc361b96fb2e9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:15:50 +0000

    572enter dare decide look insist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+print(sum(range(10)))
