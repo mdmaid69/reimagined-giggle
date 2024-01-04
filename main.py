5bb0f20cce5bb9aa125dfab2318d40a5f3082bc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:15:47 +0000

    leg callback hope direct our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
