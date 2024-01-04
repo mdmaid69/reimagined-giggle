4f50d2b6bbbfa4697e7c98c1b11484bf8c84ba3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:57:51 +0000

    total firm move come another

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+print(sum(range(10)))
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
