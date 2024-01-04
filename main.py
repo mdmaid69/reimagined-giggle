f7922d8c59e6eeb98832f622efb5ce8be6689465
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:05:48 +0000

    Gg argue perhaps blame risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
