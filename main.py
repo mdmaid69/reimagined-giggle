01d5853505b0aeb01601e8ffd9ccdb8cc70cdd73
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:15:32 +0000

    force other class until film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
