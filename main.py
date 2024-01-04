c01d39ebdae6651035ec8c1b51b4f3d82c90c179
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:58:58 +0000

    bath home poor check dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
