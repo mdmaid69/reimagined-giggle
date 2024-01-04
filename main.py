87b8f55ea9aba2253d0f02a800a0e499e7cfb869
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:38 +0000

    suit gather sink mouth next

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
