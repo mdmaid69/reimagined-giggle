c5b16a43e0dd01c224ebcad2c185e422f9845f08
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:29:14 +0000

    pollution permission serve exhibition comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
