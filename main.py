12d141825f8eea79c0ebb9de3166ba259e9c425d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:55:01 +0000

    once guide getup middle area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
