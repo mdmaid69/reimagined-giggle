cc870496d684385f42404ee07797d98cb72946f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:52:24 +0000

    door conversation afternoon wave ignore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+print(time.time())
