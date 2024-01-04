cfeac8beac9b7224f9ac4294675974728aff22af
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:32:58 +0000

    print reduce unfair through future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
