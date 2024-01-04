fc90f80cb3872ea837f32f68963a7ae284007aa6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:53:09 +0000

    bear minimum breakthrough save direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
