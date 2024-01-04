32bf71f4083540663959a49c44145e4e347cdeb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:57:10 +0000

    distant skirt estimate creature divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
