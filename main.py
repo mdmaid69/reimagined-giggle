5c4f81cf7a71304f0974093ff14c87c172aa4563
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:39 +0000

    principle check risk lost exit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import math
-print(math.pi)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
