197a62b9dc6ae819cb1f97858263f71dbe2e8d02
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:08:13 +0000

    invitation relation town environment sort

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(numbers):
-        return max(numbers)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
