cd689db713b46cd9eec11447d782999ebb226f2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:08:36 +0000

    some understand make place goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
