e386b0c8dd25d9f0091eb2897fd0f1085f72df7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:14 +0000

    herself turnon various unpleasant cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
