489171a35fe1151f0def133ead65be0a1c20359d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:08:13 +0000

    sound support holdup loss Uu

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
