c86dac93206108c398e23eaf87527fae0cac12f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:29:35 +0000

    favourite sting rise afraid pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_pressure(force, area):
-        return force / area
+def get_array_itemsize(array):
+        return array.itemsize
