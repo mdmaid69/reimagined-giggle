44ac647c7d40b9726b6ff6d6f91cfe6ef164e709
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:37 +0000

    flow beyond fire achievement flat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
