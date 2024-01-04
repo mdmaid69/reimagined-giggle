e7034ecebb8fc528215bdd4f9aa7c2d767fe8e1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:36:43 +0000

    fast badly mother brown bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
