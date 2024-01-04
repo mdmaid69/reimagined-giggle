ebaa8b310820890511a9b18708ff75e5c71a6d5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:43:11 +0000

    runaway all consult mistake complain

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
