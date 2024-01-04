e53b88f159b1d927f8ce0c99d272aa6a08c6a277
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:49:05 +0000

    damp reject sound consider relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
