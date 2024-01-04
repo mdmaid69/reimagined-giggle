33090ab76e6ec28f8a994ff65e41dc51e9a263dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:14:00 +0000

    mistake responsible worst group purpose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
