366d0e89bed51c4605911265ad88db724535a25e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:14:31 +0000

    drive bound conclusion care swim

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def convert_to_octal(n):
-        return oct(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
