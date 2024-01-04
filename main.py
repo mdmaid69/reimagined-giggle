aea1638639d8c47a2341fa5facaba577a75efb72
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:21:28 +0000

    moon ear discussion future factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
