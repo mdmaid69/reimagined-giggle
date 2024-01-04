0c3244b6bb96b2bb1d649f9c87c85ae4880fdbac
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:10:07 +0000

    somehow squeeze on safe communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
