10fe083959e5eefe4676c7fe1377d201d8bbc129
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:40:04 +0000

    statement mix remove strange whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
