0a507f10f9f16cb2cf059f19a4f3f3c127b94565
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:32 +0000

    steep charge turnon damage noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_sine(x):
+        return math.sin(x)
