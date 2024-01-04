b2f2a4f626012881edf9838c6a5288691e9d6717
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:16:41 +0000

    minority mysterious workupto radio shame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
