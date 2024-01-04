7cc4d5e3467cee4ced1d9b7fa84a4f1efaaa9f90
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:15:01 +0000

    catch argument her edge give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_even(n):
+        return n % 2 == 0
