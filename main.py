0b106406de4cd8e3c88c4631cf4a29d5e4faf5d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:26:41 +0000

    neither sweep buy perform trick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
