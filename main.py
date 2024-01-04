1eee0c7554792ebf3e430d55045bfa41dc88debf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:45 +0000

    impossible crowded school terrible lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
