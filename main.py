c2d738009b416343a1090fb124161f61a9c4d59b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:14:11 +0000

    born travel everyone order wonder

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
