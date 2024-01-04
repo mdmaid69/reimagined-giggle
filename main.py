822f532bd6185666dec8faee50f7438f42ca5dee
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:47:24 +0000

    takeout preparation after extra loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
