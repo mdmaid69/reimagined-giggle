cf0cca8bff2eec42e586e60c3e9c55e8f4dd0900
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:48 +0000

    obvious shock unusual either agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
