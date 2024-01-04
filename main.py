3c6cae89c5976c556b4f88d92ae35c69a5119807
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:44 +0000

    risk dress deal tidy final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
