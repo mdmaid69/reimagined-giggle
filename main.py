15b49422dded9899a601947a70b7199d20812397
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:30:25 +0000

    gradually job admit doubt angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_int(array):
+        return int(array[0])
