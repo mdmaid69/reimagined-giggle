6cd7d4dc32e4a5bb6c9d1e57cadbe3bc0bb407b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:56:43 +0000

    tool community reply walk preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
