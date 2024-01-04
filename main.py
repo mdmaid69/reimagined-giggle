179e33ed48c81ec13af25e3b170668c75a0962b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:21 +0000

    reduce than along snow priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import datetime
+print(datetime.datetime.now())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
