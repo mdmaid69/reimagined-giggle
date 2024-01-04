d6e11b3b8e925c6159cbe709ab2d41b1dfb2cae6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:06:58 +0000

    567enemy somewhere development waste teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
