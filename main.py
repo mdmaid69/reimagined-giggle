0be1f6f85c29d1388d971f51d427aa45dfc61ca1
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:26:40 +0000

    complicated glass less complicated duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
