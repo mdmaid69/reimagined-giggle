fe020ee2c94b74302788ea2a7d69d7e9608db33c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:19:47 +0000

    effort suppose quickly before study

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
