347421a9edd5da791b9d41f78e755c19bd156252
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:49:40 +0000

    interested care cross advice become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
