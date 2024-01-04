d58648b0e263762979316fb252a25532f5440b59
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:08:14 +0000

    hold Ii listen able job

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_python_version():
-        return platform.python_version()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
