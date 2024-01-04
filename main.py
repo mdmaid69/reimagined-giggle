da1f062b29629417fad14300fe2262ba7c69f9e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:32:28 +0000

    his funny mass unpleasant nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
