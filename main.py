8c99aaf4a3cb1f21132b8f74181170b199b254dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:51:24 +0000

    duty joke bang attract church

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def create_counter():
+        return collections.Counter()
