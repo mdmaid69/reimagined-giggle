033dc5d5f96d9155f8cca885f15afa6ff6917ab6
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:34:59 +0000

    again friend force practise straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def reverse_string(s):
+        return s[::-1]
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_string_from_array(array):
+        return array.tobytes()
