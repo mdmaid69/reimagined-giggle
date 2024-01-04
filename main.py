86d9872528e8b9a61edb2e24eed3790d088181fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:48:28 +0000

    inform tax basically float quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
