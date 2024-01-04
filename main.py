4d3b7b2ccbc6495f8b1c77fe2219ef280e5dba96
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:29:12 +0000

    innocent intend trouble however slice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def square_number(x):
+        return x**2
+def calculate_work(force, distance):
+        return force * distance
