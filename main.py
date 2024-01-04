fc52c718685c99ada1a8cfdc2dae32433ff9d0f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:12:30 +0000

    with clothes all think kick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
