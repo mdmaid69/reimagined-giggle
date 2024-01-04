d47cf9f87610a6fddc81599cc2f4f7aed6cc4c6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:09:42 +0000

    lock cut be artificial contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
