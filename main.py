854ab3fc81eab086cd576dc3504e03b5c3e92d9e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:26:10 +0000

    appear argument performance turnback apart

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
