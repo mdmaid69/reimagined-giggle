501a6c75d6350c32b949fd63337a0e53ba700e17
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:08 +0000

    powerful final serious available plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_density(mass, volume):
-        return mass / volume
+def set_array_item(array, i, item):
+        array[i] = item
