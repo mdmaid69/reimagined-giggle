42319944986f75ddacf414294d2f753f96decdc8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:59:55 +0000

    warn squash glad bill figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
