9ad43d72659d35afeee79ecad392d6550e87492b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:17:15 +0000

    instead involve crowded innocent practice

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
