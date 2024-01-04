b8be901825d986a1b05e94865361f74fc1e0cb58
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:38 +0000

    tall holiday pleasant mine right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
