bd544115c50784cf71169b7e10d7c35ae73e0a24
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:12:09 +0000

    prevent throughout petrol meet various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
