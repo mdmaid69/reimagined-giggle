38129f1aa105657f89c3e48417c493078bfcc803
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:47:01 +0000

    ill reduction special recommend result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
 def calculate_factorial(n):
         return math.factorial(n)
+def calculate_pressure(force, area):
+        return force / area
