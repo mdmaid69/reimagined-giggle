87cfcb4bc113e61fcba52fbf7a1c85cf350719eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:27:57 +0000

    row makeinto sudden call godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
