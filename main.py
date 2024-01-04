fe91a550ed6dafc74fd1a868340be8ba80294639
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:12 +0000

    correction consult bridge wipe comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
