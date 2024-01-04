48b6dd9137a4f400e8741e72c789d6f12a30838d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:26:17 +0000

    black fast lay neat ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
