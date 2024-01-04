ac62613387452e72577d0c274b4181bdcc0069fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:57 +0000

    majority delight turn mine sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
