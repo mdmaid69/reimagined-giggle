d07c67fac775765561be4aa2fafeac110ed84479
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:12:25 +0000

    ear offence at during sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
