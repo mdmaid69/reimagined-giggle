511289c4be815b5af300e4f668c115392415c4b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:50 +0000

    damp 571enough correction conversation cost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def reverse_string(s):
+        return s[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
