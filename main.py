2778d6434868ad472831f13a6e093a6ae871bc67
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:31 +0000

    bring suppose careless develop concerning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def find_min(lst):
+        return min(lst)
