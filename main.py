9e1a61edfad06885ff55b98b90cbf80d61039a0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:11 +0000

    general old dance seriously original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
