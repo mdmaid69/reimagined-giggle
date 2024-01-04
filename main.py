51c3feba9c56f05d44f4d746d1f2dd2f2c6828bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:21:32 +0000

    journey nigga limit responsibility possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
