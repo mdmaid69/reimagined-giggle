2f03cb910461f41dbd7336ce733ae86ab82e9f1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:52:33 +0000

    traffic power 571enough offer deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
