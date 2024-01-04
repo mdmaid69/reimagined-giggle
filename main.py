34d4e8109ec1ae6b49c836872cd85bc08da5ff2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:53:12 +0000

    minute make ours advice building

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print(sum(range(10)))
