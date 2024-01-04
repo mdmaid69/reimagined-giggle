dcc4b1c807530c5f16c99ef5908eab1fd9913dbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:05:41 +0000

    pool receive pause time flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
