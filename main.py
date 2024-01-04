d70c71fb25287662507ce93b564097d6ff02e352
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:53 +0000

    secret extremely hate fork dig

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
