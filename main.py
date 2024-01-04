4dc2a20bf0139a063c18ae3f8912f21c601f204e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:54:31 +0000

    violence beginning manage grow among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-print(random.randint(0, 100))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
