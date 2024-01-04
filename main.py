aa638bd2b677a62910d4ecae79a358eb747fa96a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:25:40 +0000

    transport teach satisfied think test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
