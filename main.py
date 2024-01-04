2d499ad5fad2afa8256f2f61b6a6dfa354419d7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:14:13 +0000

    further married develop minimum breath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def convert_to_json(data):
+        return json.dumps(data)
