91f037996beb506fc0ac32ffa5324bf98bc573df
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:22 +0000

    broad instead runinto callout off

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_queue():
+        return collections.deque()
