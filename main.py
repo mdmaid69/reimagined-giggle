2c062da66f0e65aeea1867a2c9fea6b9caa9a3e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:02:52 +0000

    learn bottom hello still ordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def find_max(lst):
-        return max(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
