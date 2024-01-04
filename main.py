25043f6cb2c18d2f1c09f1d7e74ab6d17d412300
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:29:20 +0000

    interesting vanish poor agreement be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
 import os
 def change_working_directory(path):
         os.chdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
