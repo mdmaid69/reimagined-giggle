f70e9c736c3ba8826ecb7ea70d041c8e1470ae8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:03:19 +0000

    begin fierce instant patience hard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
