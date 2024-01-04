c9b284573f4816cea9609e2d25473ce778da06d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:09:36 +0000

    plan getoff size weak stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def count_characters(sentence):
+        return len(sentence)
