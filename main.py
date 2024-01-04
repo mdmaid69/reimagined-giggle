03ade23d424f4adba9d82d7efe09180b2f4a0eb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:57:23 +0000

    wherever from influence exciting in

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
