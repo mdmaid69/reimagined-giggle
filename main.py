4ebfbc2745d16599e8f8bc0e958e8eb0cf6dc1bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:16:44 +0000

    notice shock evening Ff high

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
