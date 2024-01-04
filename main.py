3f1eb85a2a9d1f36502e689fbeacce6b4ffc91c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:05 +0000

    estimate politics hope discovery spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
