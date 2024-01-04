9b8f932259fe5112fd093706cd72fdae0faedf06
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:28:00 +0000

    pause pleasant permission hold trousers

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
