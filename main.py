26e250b4295a3650debef343f8e619744944632d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:08 +0000

    build rush objection laugh ought

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
