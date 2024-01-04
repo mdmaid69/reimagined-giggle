11de26fa2d9c968647f0292ce8f93da6efeb3621
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:34:18 +0000

    accommodation foot home between secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
