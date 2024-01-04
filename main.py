57b13d8e776e0bc91732c2384449cef1d48d6e6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:45:38 +0000

    squeeze nurse dear ask delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
