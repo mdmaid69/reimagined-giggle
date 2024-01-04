26f96a14af182d672e645e9ec4cd1c94368e1206
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:56:46 +0000

    breakin teach no 567enemy turnback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
