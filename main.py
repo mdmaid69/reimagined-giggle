18117e9761ef00c5929cd61e32ed723bbe3b7bfd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:56:50 +0000

    pinch speed seem flight typical

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
