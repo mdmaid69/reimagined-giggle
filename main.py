99a86668ea2b855419c316c5f0b5ac8cd99e3262
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:01:41 +0000

    go calmly demand well estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_pressure(force, area):
-        return force / area
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
