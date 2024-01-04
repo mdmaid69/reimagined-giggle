aabb12a45f42772c335169a26ab023a68f193842
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:43 +0000

    tonight comeback comeback example strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
