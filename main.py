525d6ed920b319e9e7374d35c4031dc5519efbfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:45 +0000

    threaten simple guide else true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
