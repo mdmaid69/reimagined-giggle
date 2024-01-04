38d92fad71985bc81f98b188793b31e66dc20129
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:35:02 +0000

    unlikely comein break open return

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
