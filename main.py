1c798eff827de9e8998213e9e1693e1efe0b68be
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:49:07 +0000

    connection appear talkback react snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def find_unique_words(sentence):
-        return set(sentence.split())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
