ac8e6d35acaba94afbad96e6f32d87217ed483f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:40 +0000

    occasional runinto correct pin tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
