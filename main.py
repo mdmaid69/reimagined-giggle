77255d8fed6eb09ac5baad247d9441cf9bb1a7ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:52:18 +0000

    give attach wander unite measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
