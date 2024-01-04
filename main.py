634002766c381c6e56af38aec4df2c520aba5c21
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:00 +0000

    apologizeapologise slight collection gentle union

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
