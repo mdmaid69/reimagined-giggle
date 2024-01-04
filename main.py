7da59d5575514d66c20246dccfa1890baf6bd245
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:46:14 +0000

    upset tool rob drop house

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
