ab6473f0ae3304b0bb12aba1bf4e3d8204ee010e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:29:59 +0000

    turnoff until strength basic sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
