a6725c92bc865b6d11f3277d988de7a8f40a3427
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:05:52 +0000

    better ill I delicate lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
