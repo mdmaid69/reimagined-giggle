23fb8030bb1c4de37525ff946eda9c54afd15611
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:59:49 +0000

    force square birthday top Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
