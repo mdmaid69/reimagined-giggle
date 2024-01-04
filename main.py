2dde765bd4da294b7b1e029d2a7f800fc28608b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:18:24 +0000

    mine film spoil male goodbye

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
