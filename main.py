b2c6f0d6c04435475081328015b896db09327feb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:00 +0000

    fire risk guard keepup shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def subtract_numbers(x, y):
+        return x - y
