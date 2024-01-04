cb111e5116bc2aa6256616df535b6a73756b237a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:04 +0000

    getout getup may any remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
