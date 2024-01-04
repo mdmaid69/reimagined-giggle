e6b3a517f3f636fc63ef1f194d769cddb677e0cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:42:54 +0000

    transparent knowledge report walk job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def convert_to_binary(n):
+        return bin(n)
