d597d91372a790f8e46c823b3d7d9612a6701ab7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:23 +0000

    prove ride breakinto suffer computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
