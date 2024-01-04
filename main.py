116fc8536b5369d352c0b85ee4f42e3b2bc1a798
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:34:16 +0000

    arrange stiff attempt pour lost

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
