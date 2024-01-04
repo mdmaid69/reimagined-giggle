7ea770fcfccac9d8fe7cf73a4ac7c2c3a7cce1de
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:12:19 +0000

    team opposite always operate remain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
