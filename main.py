158e9ab488d0c2e09f0fae50fc558d11bad90bd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:39 +0000

    later be new blow cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
