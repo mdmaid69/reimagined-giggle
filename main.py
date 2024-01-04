c775ab3e4153bbbf198f39ac14b487b920cef0c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:49:03 +0000

    exploration regard unusual mail example

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+for i in range(5):
+        print(i)
+import array
+def get_list_from_array(array):
+        return array.tolist()
