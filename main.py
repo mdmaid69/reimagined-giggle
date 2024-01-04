f1086c74a8d8e8b2bafd25cd1a251d08fa5e0ca0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:49 +0000

    search neither complete land hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
