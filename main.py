2a6a7f18a1b9ce02cdb5db823b27e7f4aa0bcfea
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:16:49 +0000

    near teach discuss foot survive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def reverse_array(array):
+        array.reverse()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
