0fee4597b1cf967d128f7176899b7d3f1f5ad669
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:33:41 +0000

    still tonight difference stretch preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
