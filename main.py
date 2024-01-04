54573b3096a38aefbedad312d7afe6019a01d001
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:21:09 +0000

    whichever owe stone Ii lead

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_speed(distance, time):
+        return distance / time
+  def remove_duplicates(lst):
+        return list(set(lst))
