6a49c3b1b35d162911efb607162e857e7bbeadfb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:51 +0000

    smell quickly problem pleasant beginning

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
