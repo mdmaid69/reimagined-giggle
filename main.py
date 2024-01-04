d676935cf485e07b7c4efb0527476c4811b9bb3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:07:18 +0000

    innocent sit glad town rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
