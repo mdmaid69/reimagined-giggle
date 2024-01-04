4c5cf27fec6293067fb754d7bacfeb18de589138
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:11:01 +0000

    related glass from able touch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
