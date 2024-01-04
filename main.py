80cace9552cc3ef5a4f03527e9715c4be9325d9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:10:15 +0000

    loose invite process expensive object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
