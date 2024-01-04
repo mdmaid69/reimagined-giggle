3113570a84dd819a9976e2168ed80d57867b40fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:34 +0000

    plenty dangerous tense thankyou excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
