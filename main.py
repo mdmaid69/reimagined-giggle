d2c286f1a132ef9a5ff6778d89c79c983d5f817f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:55 +0000

    whenever form bend hate spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
