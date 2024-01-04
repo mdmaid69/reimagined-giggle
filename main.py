b22587360c61eb4d7698e8367ceaba31de882d25
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:59:08 +0000

    save event free perfect play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
