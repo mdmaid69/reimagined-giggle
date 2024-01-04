80d59cc4d3cba9e627ec1d5d890c184930e74a3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:00:17 +0000

    similar develop 564encourage ought shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
