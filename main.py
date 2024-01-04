d715c5720ba0a2f49f9da62abf9043e4dcb6606d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:45:42 +0000

    confused congratulations still modern dare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
