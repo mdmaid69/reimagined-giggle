646f056a4e237ddc84e67302e8d01457ac4ebed1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:45 +0000

    swallow occur news walk before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
