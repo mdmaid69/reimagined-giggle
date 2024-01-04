d91bcf589cd97bd1a893e51cef21e51cb5a65cde
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:44:14 +0000

    private stretch replace raise juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
