a2268458d30f48b5dec901b84cd9b9f937611e36
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:45 +0000

    past mean bush recognize temperature

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
