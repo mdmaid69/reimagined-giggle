92417d5b430314ede419b01da5a0d284deaea98f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:46 +0000

    keen measure store foot unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
