f55cefabf7b31847102aa5c11fa03275bc297fea
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:27:33 +0000

    thought eager maintain modern bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
