ab10e5a4fceb5be49d1705c9c9461dfa4ccaacd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:23:39 +0000

    try pupil with up payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
