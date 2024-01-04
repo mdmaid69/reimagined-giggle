20cfb2b8add94381822d07e92001d143bdf38622
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:19:24 +0000

    probably tone control sick relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
