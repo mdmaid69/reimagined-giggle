5ba070a8d9578c4ad71040a28ca8fcf86b03c98e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:47 +0000

    spoil Gg success congratulations modern

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_repr(array):
+        return repr(array)
