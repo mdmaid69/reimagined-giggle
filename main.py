bcca0e2a66f6588fc1ab80458d7e397e5e9d5f02
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:09 +0000

    lookback(on) boil through closed pollution

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
