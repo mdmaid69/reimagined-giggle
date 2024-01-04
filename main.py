ee774629b8ad442d68935ca31b3867974d4eda3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:30:18 +0000

    fat ready talk finger laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_queue():
+        return collections.deque()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
