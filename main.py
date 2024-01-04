112e0f755eced19390abd6a6b035925c76e7b65c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:16:16 +0000

    note keepout foot sure task

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
