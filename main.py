9ebf4b085e170d68afca36410e5f6feee932a61b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:11:34 +0000

    turnover stick concerned beneath welcome

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
