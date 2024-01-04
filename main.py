38dcfb8f3e1f09b63da2c0c8d3e9f42f7c486bac
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:19:16 +0000

    office trade phone 566end backwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+print(math.pi)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
