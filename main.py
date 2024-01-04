ac38379f6ee3015be1f28bb40d2903e8bd0a4bec
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:21:24 +0000

    turninto seat either everywhere else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
