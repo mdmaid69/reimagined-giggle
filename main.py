cb038a48e93ef22166436c0d1d1e93cbb0a90b82
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:31:06 +0000

    turndown positive light along dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
