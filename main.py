6267e243583cf0df9859f01d8ae5795d71eab188
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:07:58 +0000

    breakinto straight receive immediate couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
