5f7193e76f298eb0fade21b85d13cc96770d84a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:11:03 +0000

    turnon tooth next some disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
