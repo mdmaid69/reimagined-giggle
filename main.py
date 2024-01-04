e74a94e786c31432085e9f6981e6da0c87853112
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:07:27 +0000

    freedom trip exhibition fight last

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
