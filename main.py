d1667fcc21655806c49990c4dfe35a8a06f3ebea
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:06 +0000

    breakout usual case nor fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
