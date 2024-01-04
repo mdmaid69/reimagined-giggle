714372c155bdc66322efa820decb8ff1c2293d78
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:33 +0000

    spare lead handle female interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
