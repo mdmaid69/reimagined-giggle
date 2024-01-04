48150ef6f7efdec59c9e8951209af97f573f1ac5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:19:06 +0000

    as before reserve inquire shoulder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
