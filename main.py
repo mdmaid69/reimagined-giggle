c04783efe79af80b871e4f253e7273b0206aa22d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:09:25 +0000

    lead it view backwards sick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
