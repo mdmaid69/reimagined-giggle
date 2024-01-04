d31276ec104c82a45223af485fb834dc6579867f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:17:08 +0000

    afternoon towards space Vv letter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
