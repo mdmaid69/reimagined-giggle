6ab0cfa9c4ed102d84b88dd97b8c4f5785155c77
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:44:18 +0000

    save foot lookaround beginning pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
