da2c8756748b3d73ea81acdc6ca78def8883bb65
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:15 +0000

    play sting company display to

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
