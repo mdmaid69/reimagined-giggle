58eafe2ccacbad2139471bc823126490cc00fb38
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:31:17 +0000

    and centimetre indeed breakdown Uu

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
