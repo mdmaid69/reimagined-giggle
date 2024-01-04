abf98b5d77558fcec5502194d27bf0a0fd113c93
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:37:44 +0000

    shock worried house proper content

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
