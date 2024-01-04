a1b5b252da297cf8fd4c161ee762aeece598e987
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:00:33 +0000

    public runaway become race petrol

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import tensorflow as tf
-print(tf.__version__)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
