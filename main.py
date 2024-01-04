8c330132630f4a34d9f4436ee1e1f0945ad4bc28
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:17 +0000

    sort plain information ahead empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
