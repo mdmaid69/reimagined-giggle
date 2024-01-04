2a19d8c53db9df8adc19bde7dce00ab8a53a7169
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:24:41 +0000

    violent split shut left cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
