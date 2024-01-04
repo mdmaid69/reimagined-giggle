6bc426809332b4de0a330dc9a5943cb35eeaa06f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:09:50 +0000

    power steady future extra repeat

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_power(work, time):
-        return work / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
