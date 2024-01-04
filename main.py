58a3b48ff14f755b4866da52a7c929c04f83cdc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:13:05 +0000

    switch whichever narrow deal callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
