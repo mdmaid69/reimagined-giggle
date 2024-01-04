0d4fa26f0adf8e80efb1339353e904ce89183064
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:41 +0000

    ability embarrassing sure godown space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
