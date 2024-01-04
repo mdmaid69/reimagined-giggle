2a397059b686a93eb36b0118b838f7fcb4549454
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:40:41 +0000

    drawing recover get dry arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
