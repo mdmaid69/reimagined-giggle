2e3532f16309543a8678433b212a523f20f9c7ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:47:21 +0000

    from practice extraordinary sky escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
