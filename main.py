4f814817d1177d926cf82ff100736fc6a3e91da6
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:06:14 +0000

    react low market talkoutof head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_float(array):
+        return float(array[0])
