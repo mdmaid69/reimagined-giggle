a3647fda3daab6b5fc426d033caccd4e15709c7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:29:09 +0000

    positive Gg change thinkof continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
 def calculate_tangent(x):
         return math.tan(x)
