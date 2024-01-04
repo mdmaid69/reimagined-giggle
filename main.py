96fbe3341b8970eb36d5f19073d1c5b098511422
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:37 +0000

    sleep put 573entertainment doubt only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
