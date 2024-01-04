6fbe054ec297f1fa32ac6e6f4902b98322939ba2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:56:10 +0000

    body wrap shopping rain late

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def convert_to_binary(n):
+        return bin(n)
