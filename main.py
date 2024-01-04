358555c8771296c1c49054c734ee28847fff6923
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:13 +0000

    depend environment city under away

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
