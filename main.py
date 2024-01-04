d5a79e580a3179e323679473fbec8d5033d1901a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:29:09 +0000

    walk relax claim Jj miss

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
