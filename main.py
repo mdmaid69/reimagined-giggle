34cdda2ea842c3559e4bf780d94d5135566a627e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:38:05 +0000

    load runaway about godown deep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import random
+def roll_die():
+        return random.randint(1, 6)
