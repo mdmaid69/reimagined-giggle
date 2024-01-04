28312cf620100e4facbc04d21be2bb1f1018614b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:52 +0000

    typical getup oil pleasure surprise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
