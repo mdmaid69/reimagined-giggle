4ac728a3096764ee16874a875d269f6a70f8fdd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:20:10 +0000

    majority reduction whistle position shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
