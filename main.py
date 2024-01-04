b6808fbf3de079b3687a0ef26163584f6a17d36e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:41:33 +0000

    sink nowhere sick Rr explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
