679a626bc124caffe80e09225b96816c066dc9e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:48:23 +0000

    study connection lookaround embarrassed face

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
