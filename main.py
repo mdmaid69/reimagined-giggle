4101ae2f4a4c6d1b314b186014591d45d9bf22aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:55 +0000

    doctor threaten spill hollow family

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_pressure(force, area):
+        return force / area
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
