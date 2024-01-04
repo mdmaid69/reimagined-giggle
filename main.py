3b054ef9448e79eb116b072af392218855d5f748
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:35 +0000

    prove wake well save relax

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
