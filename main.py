826ab8cccf6444186a69aad5b42d05a986321c47
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:49:55 +0000

    wash bill divide post comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def greet(name):
+        print(f"Hello, {name}!")
