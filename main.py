94221f6f8002efe9ac169a47c6f0981a0e5f4731
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:24:27 +0000

    approve grateful debt home fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def clear_array(array):
-        array *= 0
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
