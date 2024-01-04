5f230d342f9c0cea33adb1d2bf7b44b74b6dcee3
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:46 +0000

    everyone search general language run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
