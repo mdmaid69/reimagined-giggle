8cecb64fdb0980b423f0fcc5f4bbc5328d350619
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:52:36 +0000

    Zz form several provide clean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
