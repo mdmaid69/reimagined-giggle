7c02ac7e2be4acfd5d597c417b970547ab9b0d65
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:11:36 +0000

    level getup front yes false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
