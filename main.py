770d7b581f061f961834478251019cb34e14407f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:42:08 +0000

    involve hold receive shock convenient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
