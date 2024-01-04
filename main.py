7ed79a5a0cf3805522648fdfb0bc78bea1444306
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:06 +0000

    declaration born 572enter pretty off

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def sort_list(lst):
-        return sorted(lst)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
