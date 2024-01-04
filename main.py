108ccdc20f3d7955c3443cd32eb4ae6eec6dce4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:18:40 +0000

    handle importance send should ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_set(array):
-        return set(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
