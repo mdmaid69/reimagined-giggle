a39699a1afb007b2fa07deb326af60b4a62c62fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:03:44 +0000

    sign confuse think over satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
