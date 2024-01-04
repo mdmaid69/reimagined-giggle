cc352c03c7f935c2dd43ce13b5320fdc88ffcc9b
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:30 +0000

    passenger prevent Ii impatient thinkthrough

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
