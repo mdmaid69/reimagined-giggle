6a0a673d4d8efd1b665b7cfadec69d4cf367d1f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:40:51 +0000

    trick disappointed unlikely nature this

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
