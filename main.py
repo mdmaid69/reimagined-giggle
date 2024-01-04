c0dda8dc3aa0b277b6c78af0c192a7bc91ee870c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:36:47 +0000

    lead bit hungry rule warning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
