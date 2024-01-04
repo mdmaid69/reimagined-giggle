ca0bf9520a74145a80a84190213bc75a7708765c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:42:38 +0000

    it why score lost rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_current_directory():
+        return os.getcwd()
