d60ef5ee8e8d23232277c2dcdfaa6b7c3e1ab8d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:29:06 +0000

    theirs taste none interesting off

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
