3555aa7a88ffe76933a2476c6412c4fe31e764cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:13:24 +0000

    brain die relative although street

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def reverse_list(lst):
+        return lst[::-1]
