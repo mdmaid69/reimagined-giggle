25a0be3f87799526b85274ac80cb9c69a8e9575f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:20:11 +0000

    town breakup line declaration age

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
