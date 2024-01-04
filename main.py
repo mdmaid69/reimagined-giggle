0fe0cc998cc5c600814b24088eaef2f73d116da6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:28:43 +0000

    ball tap rush wait flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
