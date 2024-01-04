dd3b0887ae17b4e15cb528e9fcdbc7b511a31e97
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:23 +0000

    stranger 568energy since only national

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+print(time.time())
+def count_characters(sentence):
+        return len(sentence)
