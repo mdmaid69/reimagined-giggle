523414bb82f0ae0714e785bcceba3e8e6f315ead
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:18 +0000

    rescue Mm how blind float

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
