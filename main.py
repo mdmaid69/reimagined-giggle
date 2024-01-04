4e8d0e7c9352cfe87d64356336d09d667b5ffb33
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:43 +0000

    bet platform total receive fair

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
