d5ac54b8ce9184c0f195175635c3fe09659187b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:33 +0000

    neck hit other shut comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def sort_list(lst):
+        return sorted(lst)
