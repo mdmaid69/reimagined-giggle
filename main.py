eebbd6334db0f3574e935b504055dbfe374a34f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:46:38 +0000

    last peace fly duty matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
