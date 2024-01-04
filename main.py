9f8c56835c0a8200a0cd6a245fcc2c49f66a4720
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:26:10 +0000

    join satisfied difference drink surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
