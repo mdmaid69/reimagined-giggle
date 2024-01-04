13da6c1f9b6816c0aa93d5c5ae3769f5f600453f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:17 +0000

    bill artificial rule trade track

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(lst):
-        return max(lst)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
