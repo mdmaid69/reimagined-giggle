ef0dd05da314648d7e027872062215b1c35e6c96
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:32:59 +0000

    union ticket possible thank Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_even(n):
+        return n % 2 == 0
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
