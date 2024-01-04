8858120388781d8acda7e6845f82536c7349316f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:55:31 +0000

    method once goaway offer freedom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def count_elements(lst):
+        return len(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
