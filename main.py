4e20acf8d18d1ba3b62d36e34cc435bafa58b9b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:50:22 +0000

    confidence eat chin inquire success

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
