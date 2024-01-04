b3118a4e48ec2563e4a49d8dc2fc72c2374eadf7
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:19:44 +0000

    dinner talk suit matter snow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def multiply_numbers(x, y):
-        return x * y
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_speed(distance, time):
+        return distance / time
