82dd37685c018a7409c91f1e613f7435fd5e765a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:26:53 +0000

    about stamp satisfied frightened 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
