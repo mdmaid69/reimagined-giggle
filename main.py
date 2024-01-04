e11ecec915acf1b3b93d38dbd2961d5ae5bafaae
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:58:46 +0000

    part estimate since forget copy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import sys
-def print_python_version():
-        print(sys.version)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
