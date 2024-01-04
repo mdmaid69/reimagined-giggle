e7ae2511a4fcc50e766a3e5aa0a4cdbedc03f935
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:57:12 +0000

    boat argue nose hurry spare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
