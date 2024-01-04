94c634c98180005bd684ab0bccf2af988f647ae2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:33:37 +0000

    welcome criticism train sound life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
