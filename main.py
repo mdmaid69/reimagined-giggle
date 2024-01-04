18f2ea7b5ecf0574ad459c7d252653e11769d18a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:54:53 +0000

    feeling print shine path forget

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def convert_to_octal(n):
-        return oct(n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
