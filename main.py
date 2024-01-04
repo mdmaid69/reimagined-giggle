2985d30ce7639b25c4481bf172fcd4a5b7769d8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:42:15 +0000

    trick surface call base soft

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_odd(n):
+        return n % 2 != 0
