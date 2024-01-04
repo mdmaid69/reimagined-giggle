9849f1024f5554e9b43273ed8174b737ea942648
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:10 +0000

    prefer perfect offence together hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def convert_to_binary(n):
+        return bin(n)
