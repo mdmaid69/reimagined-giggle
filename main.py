9632047c668426a1aadb52e32ab75693fbade599
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:09 +0000

    healthy market hair fill distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
