fa24c0908b45ded4812172464aacdd4a3e85b745
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:49:35 +0000

    attempt family separate punishment appeal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
