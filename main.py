614a661fdce8c1e679a5226017618771b95e2c6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:09 +0000

    die ring mine wash imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
