185884a05cb72a04afdbfbed7334d1cbaea2afd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:48:50 +0000

    burst power find precise Bb

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
