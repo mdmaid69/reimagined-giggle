0cf1b545e335f6e426fc09ace86652efd6b44890
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:39:00 +0000

    politics middle habit announce backwards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
