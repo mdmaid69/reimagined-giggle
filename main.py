91eff3ee5d199adb6c873eac0d2e1a9b3847d324
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:51:43 +0000

    taste shelter rough natural surprise

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+print(sys.version)
+def subtract_numbers(x, y):
+        return x - y
