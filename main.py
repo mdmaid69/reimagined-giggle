f7aae611824ef2477ffe61eb2fb388513fe54ae8
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:38:40 +0000

    error stare reach theirs physical

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
