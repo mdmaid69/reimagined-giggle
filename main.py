e0175bdd677195ee5076622d6a22fb593389d157
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:32:28 +0000

    opponent moment quickly sky expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
