8fe61ebb8902b135837fd16eb649cc2043fa939c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:57:56 +0000

    pleasure maybe star kind music

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_power(work, time):
+        return work / time
