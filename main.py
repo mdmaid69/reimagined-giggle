feb7f3c795abd307e79b1e6aa521d49a9163eb7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:30:05 +0000

    phone opinion speed more wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
