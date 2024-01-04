26eb2fabdbf68306f5c07ee976962b72422f242c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:32 +0000

    concentrate each noise 573entertainment touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
