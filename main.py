705de071ffbdebf453ef898eff8ccf6768bef22a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:22 +0000

    freeze pencil suckme apparently godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
