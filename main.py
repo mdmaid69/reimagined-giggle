8579e7e33761b5702e7daaa557e557b8aee99857
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:24 +0000

    firm foreign alone spot 567enemy

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-for i in range(10): print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
