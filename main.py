3b5f8746fbd35edb3bd18b6947266a6f469ec5b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:21:17 +0000

    able finger holiday extremely without

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
