9dfe8109956ca2e64948fefd84fa0c34c4ffc686
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:13:30 +0000

    exchange welcome rid expression floor

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
