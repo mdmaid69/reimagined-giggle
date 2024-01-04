8b80bb57b5a99846e55fbef860e756f29fd21c1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:00:03 +0000

    let after regional firm born

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
