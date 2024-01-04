b1c8df54217ebc8d8e035d2405fbf688ea5f2bdd
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:38:23 +0000

    guess basis down without position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import datetime
 print(datetime.datetime.now())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
