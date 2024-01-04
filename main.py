52828dc7240dce357ebd76b9d55ba213b99a7542
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:14:17 +0000

    pick cry make spend excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+x = 10
+y = 20
+print("Sum:", x + y)
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
