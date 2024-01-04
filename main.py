0d1ed1f14fe4b24e1ffaa35f64684a68739e26d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:06:01 +0000

    twist protection 573entertainment along swimming

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
