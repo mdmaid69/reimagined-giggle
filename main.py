624eafa56f0db85cc662107f1b749b3d9f1095dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:06:05 +0000

    put apart class arrival reliable

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+print(sum(range(10)))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
