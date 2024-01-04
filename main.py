e2c61c95e1312b1921e535fed3fc82a7d97956a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:03:16 +0000

    ball whistle flight needle keepoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
