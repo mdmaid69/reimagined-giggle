c108b4bb83e9070e1cc91b43bd3ccc60a7dcc675
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:05 +0000

    big respect thorough confident 573entertainment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import sys
+def exit_program():
+        sys.exit()
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def change_working_directory(path):
+        os.chdir(path)
