16491e88fddc28f61c21c231efda4f60aabbf3bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:05:56 +0000

    visit fresh twist start fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
