827c74c41e889ba9df8b5dbc1c6346b32e2df4e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:20:02 +0000

    trick whom contact boil Tt

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
