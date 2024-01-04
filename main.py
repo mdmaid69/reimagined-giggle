2bb3b5348e6e1c4ec62de328a52058b77e728cd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:00:26 +0000

    loose lift beside quiet system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
