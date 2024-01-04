fb9f8be9d9873d00704f026b56061c70222c6d7e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:07 +0000

    brave swing stranger variety bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
