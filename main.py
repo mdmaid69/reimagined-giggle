b41abb3b19cc227f10e44430eab8c2df7d7f1be0
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:16:45 +0000

    float sure wonder phone treatment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
