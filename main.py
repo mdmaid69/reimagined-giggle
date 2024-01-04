d78317d48f5e855b3f14cfd8377f9c4e08399020
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:53 +0000

    soil urgent trust service we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
