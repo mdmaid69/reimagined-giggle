95bb97310b6c349b542ed3f877e808de40843eb0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:56:58 +0000

    talk door fortunately part recent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
