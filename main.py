ab06b4d25fa32edb4a8512ac7c52ecf71e120425
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:10 +0000

    pair company escape explanation guide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
