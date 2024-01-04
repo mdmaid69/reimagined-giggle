cc182b8da054df2e9091ea103a734e3dfadeefa2
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:28 +0000

    approval positive anywhere slightly carry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
