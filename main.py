1486392366a66b32c9d0d5032d0acdc04e36f0eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:39:42 +0000

    train original juice bound noise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-print(sum(range(10)))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
