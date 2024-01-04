516ae23d63cccd1a42cc1400afe4d8421e88ec4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:52 +0000

    arrange round guess classroom rest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
