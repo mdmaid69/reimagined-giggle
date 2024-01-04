07d39993d8791021f50c15e2873ef8d89c5bda0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:13:56 +0000

    depend slice needle confuse history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_error_function(x):
+        return math.erf(x)
