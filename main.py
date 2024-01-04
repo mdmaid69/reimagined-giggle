115e9cb5ee457a87b217179724de7ac52ecc9c69
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:40 +0000

    rate circle in separate mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
