ff7f4997cbfa463bed775640daf21080263b22ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:06 +0000

    neither rule holdout murder communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
