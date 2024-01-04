e56be81af4b3b1c60ec0933b601359d95ae3f7eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:04:47 +0000

    possession chin young heat Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
