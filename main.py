168333fa00de6ee84eacf66c7b6cb721693ef711
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:09 +0000

    watch safety town like hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
