73dc5c26003df46c339c373fbe8edc7309c1981b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:09 +0000

    worst temperature education dry extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
