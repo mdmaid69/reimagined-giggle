1e496da82d6e63f414fc50a2592478fbeb49cf55
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:41:56 +0000

    tragedy yes both put steam

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
