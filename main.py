b4511be561aedb640aabec08f186e882b51ac6ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:02 +0000

    best wealth relationship me professional

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
