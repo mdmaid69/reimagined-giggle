ad5d03fc9d951142737484b5248f4950e81bea65
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:31 +0000

    meeting turn set cause punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
