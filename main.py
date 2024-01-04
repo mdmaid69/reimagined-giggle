b6dd2ecf0e5ba22870830fad9c2a3fb970e2260d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:36:53 +0000

    ashamed little declaration used detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
