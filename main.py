0985f24df992cc798e181543b6ca2e7d78e06ed1
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:45 +0000

    nowhere otherwise stop well combination

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+print(datetime.datetime.now())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
