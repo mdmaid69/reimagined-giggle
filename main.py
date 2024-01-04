fcd138274a7002b78561f8373e25ae2006042ae1
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:19 +0000

    art expression police possibility last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
