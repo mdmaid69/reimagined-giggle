1fde9943791962a34497ca0b0426f1beac56b329
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:35:25 +0000

    style improvement brief however miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
