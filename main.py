4fa2d70c3a41c1be5d33e1a167011f7bd5e7fa03
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:11:21 +0000

    takeover hole pull intend blame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def count_characters(sentence):
+        return len(sentence)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
