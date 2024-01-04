9d3fadd3e64ea7f8377b4671e3ad91752561f293
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:43:50 +0000

    cool bringup variety characteristic limit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import numpy as np
+print(np.array([1, 2, 3]))
