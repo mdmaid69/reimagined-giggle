86c8b206ffa462bcc0b86facf7724cbe10993db9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:33:57 +0000

    damage his spread yourself choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
