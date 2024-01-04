4a3fe32f64327ec348d9253a28e492c533554ec9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:39:40 +0000

    blame opinion strength past beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
