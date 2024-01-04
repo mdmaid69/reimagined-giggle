7b89d38178b39ea9866322286956706ad8b2b49b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:43 +0000

    modern charge boring protect stare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
