3b12f99d325f0c54de1b9c0b4e0bbf35bd99dffb
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:58:13 +0000

    determined difference empty sail complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
