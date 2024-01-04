d90ee8858625bfb23a1b03fca15403423d8e4786
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:36:09 +0000

    address pile confusing bill Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
