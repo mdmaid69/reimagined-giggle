4c1ad885a3599fa71191b7674363884b38d9f506
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:37 +0000

    damage under deep Tt bath

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-print(os.getcwd())
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
