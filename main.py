54b7a88f04f082a7a8a8616832c55c611c709ba5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:40:57 +0000

    mine plate open please proud

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
