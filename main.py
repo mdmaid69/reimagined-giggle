cb7d1c465edb650fdec2024ca1f3cfdfac703566
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:05:09 +0000

    explain risk ground from talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
