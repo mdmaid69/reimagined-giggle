e3a92d403f2a6c52ba35a287ab9c670120fda5bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:53:44 +0000

    history news number lie ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
