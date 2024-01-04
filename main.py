8795aaa1a4b4014d5a498431be7f3d08fe483284
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:56 +0000

    rid strange die rate total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
