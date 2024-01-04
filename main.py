f42c558439b44fd364f61bcd63f69cb5c6d4b101
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:42:18 +0000

    main stranger carry report cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
