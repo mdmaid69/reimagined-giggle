ba453d7b68845ccde2ef353530c2b38cf27168d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:07:04 +0000

    but stir detail label regard

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  def square_number(x):
-        return x**2
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
