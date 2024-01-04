b4d9a65bf1a7157a0ee8a1b19a8e4c8fdefb4977
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:44 +0000

    positive relief nigga 572enter delicate

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
