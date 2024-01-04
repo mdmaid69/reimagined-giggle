4b0d55d1b2a33bfa0bff179ce381e91caec42169
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:08:06 +0000

    soil kick spoil pressure crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
