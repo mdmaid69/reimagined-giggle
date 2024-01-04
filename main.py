19bd714de43d16c78b5f5daf2b8a3d5e12f654da
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:49 +0000

    achievement once lose niggaacademy notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
