2958cec0dfad18b738f3f888b8e22824788cde8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:26:13 +0000

    similar switch wave criticize invite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
