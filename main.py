89f2e7a1b5d66cf4a7c5145700dba0896b2dce42
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:54:30 +0000

    shake turnover hers success paper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
