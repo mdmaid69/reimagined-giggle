71b892ff9af63fc5954564098689ac9702d43ea0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:48 +0000

    their dress sound slice summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_array_to_string(array):
+        return array.tostring()
