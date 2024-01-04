ad9dc7a5c73c416276d2370eb174b642359295d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:08:36 +0000

    attention recommend race trick noone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
