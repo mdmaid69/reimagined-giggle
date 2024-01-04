ebcf4ce27a919dea71a44399a42f39dc8b9437c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:25 +0000

    wide true team year air

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
