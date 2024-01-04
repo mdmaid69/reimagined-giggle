7836eb130b30843c09a3b2eed272c4725fba259b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:20 +0000

    disagree choice heart this weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import platform
+def get_os_info():
+        return platform.uname()
