e27a09f0df0b686255af5ede02f6afbe518621e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:20:25 +0000

    wave considering disappointed giveaway better

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-for i in range(5):
-        print(i)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
