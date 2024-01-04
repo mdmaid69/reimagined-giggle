42cfea294259d487bc76dc8024f83c294278a9bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:18:13 +0000

    owe luck reason probably 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
