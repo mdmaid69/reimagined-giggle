8e1e2d951954ef4664d3dd6944d5be986f81ce23
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:30 +0000

    567enemy never by remove tap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+  def reverse_list(lst):
+        return lst[::-1]
