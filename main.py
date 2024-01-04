aaa0ce38d2a8ddd5136a94202b95431c874f40a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:55 +0000

    hang mysterious sharp music without

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
