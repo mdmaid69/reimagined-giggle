340fd14f2b1fee80c1735114cc6c11341b3927ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:20:32 +0000

    border table unit lean plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
