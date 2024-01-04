faf427531d6de1b278465fb26b9a20dab45297ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:35:54 +0000

    phone relax question feature breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
