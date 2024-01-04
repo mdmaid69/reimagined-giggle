73837602913d428a567902ddb6032c4bea9f720b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:43:51 +0000

    load maybe feeling shoot change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
