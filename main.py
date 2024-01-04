cfd054f731a2429d352dfd6266c702a4fef47803
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:22:02 +0000

    petrol discuss see uniform pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_work(force, distance):
+        return force * distance
