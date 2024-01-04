f8faedb092aa7bcfc61fc8b8e2b94f47ff88470f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:08:06 +0000

    179bite situation mean leave plan

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
