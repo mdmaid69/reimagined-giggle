723fab7ea267a1a3ac99bb01fd4d8b03ff35dd6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:40:00 +0000

    turnover friendly firm allow convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
