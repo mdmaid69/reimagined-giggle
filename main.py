45ad9c5f134d9f4b46914528f510f2c88ae1aeeb
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:57:18 +0000

    hollow achievement because injury clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def log_message(message):
+        logging.info(message)
