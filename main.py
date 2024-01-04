d07636d4740958a3344aab5291692a00b6b015e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:59:38 +0000

    original crime great wave brave

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_length(array):
-        return len(array)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
