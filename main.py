9c37c9220c4f996b8e310f39046b9ad8edfec778
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:00 +0000

    dead sensible music joke pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
