f2c2cd5794dadcfa99d030afa8f1073803d6fa3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:54:03 +0000

    until means agree minor funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
