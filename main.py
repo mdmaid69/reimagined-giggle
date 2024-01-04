926776fcda7c0ab9d4854d717221db371c949f3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:33:18 +0000

    post punish shut spread work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
