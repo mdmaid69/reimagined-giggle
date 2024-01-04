1997202538cdcf991793c3bedc6a165cc7026d7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:20:47 +0000

    refuse godown climb mystery connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def add_numbers(x, y):
-        return x + y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
