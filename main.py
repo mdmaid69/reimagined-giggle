0c524b78d1934bce926a576aa77cc71ab830c13a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:42 +0000

    reduce minute train surprise back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
