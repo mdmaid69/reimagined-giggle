707e23448fea57416df189b8f76be56914f90d4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:37:34 +0000

    union through second occur seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import numpy as np
-print(np.array([1, 2, 3]))
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
