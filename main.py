888970cf2cfc46243607ee3ffc43c845472a83f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:32:49 +0000

    public back uponon sink involve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def reverse_array(array):
+        array.reverse()
+def divide_numbers(x, y):
+        return x / y
