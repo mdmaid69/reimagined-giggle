5dd25c617a3cd16a799c22506182563f56d0342f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:54 +0000

    move power where contact present

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
