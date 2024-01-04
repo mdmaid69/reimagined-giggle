dd76c8898a3525940af8db7e6e04f96d86b2cbaf
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:43:15 +0000

    visit ring switch allow kick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
