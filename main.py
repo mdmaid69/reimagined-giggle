86f267c812533774e00aebb69b9ce143bbfabb04
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:39 +0000

    anywayanyhow effect holdup runover beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
