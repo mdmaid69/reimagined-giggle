0898a5c88744d4d87ba96dbd1f5d8af256df75f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:11:55 +0000

    whistle pretend roof live mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
