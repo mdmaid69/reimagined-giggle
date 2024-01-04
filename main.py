7c4b3f1ca1b3124b37eaa5e5fcb0889234e170ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:07:07 +0000

    rest blame declare belong left

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def convert_to_octal(n):
-        return oct(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
