1f00a1073ccd059a97ea0a54ed6bab73365fdbba
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:07 +0000

    film whoever allow slip declare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
