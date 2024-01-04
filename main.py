062bd779d7eb81dd0758fb980927d8ffdd01ee3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:32:20 +0000

    fact apologizeapologise noise forgive stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
