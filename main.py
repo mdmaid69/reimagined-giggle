6a0dcd5906ab0c230ceff49d4bc59adca485cd38
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:38:50 +0000

    swallow callout contrast genuine aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
