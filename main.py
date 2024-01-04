506f5ae9e7207cebf7658e86c5674702602e45f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:05 +0000

    stamp fear mix rough miss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_even(n):
-        return n % 2 == 0
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
