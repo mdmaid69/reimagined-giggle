a1d072db30f0525aeedc0d4eff9860906b53a4c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:01 +0000

    temper object their detail light

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
