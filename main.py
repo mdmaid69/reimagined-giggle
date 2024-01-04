32474f2e5fd837e370dd2f8d0444803cb7f11fd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:11 +0000

    completely father regular prize generous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
