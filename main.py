ec90b3d3ecea166a4c6f5c23766f5003481431c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:09:58 +0000

    many pay conversation likely cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
