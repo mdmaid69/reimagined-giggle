3d999930a695949c2ed715214dc0b210670aa3a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:18:16 +0000

    onto bend last present passage

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def remove_duplicates(lst):
+        return list(set(lst))
