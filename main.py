462ff64d7e7c23c82fb6372a369dd72838380386
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:36:02 +0000

    public lesson appear freeze gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import random
-print(random.randint(0, 100))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
