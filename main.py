ad216c552553c56fd8c3a8b3eede8a4a83ef5da1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:56 +0000

    excuse joke become design broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
