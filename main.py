c59acd87faf0a7ccbe01fb6f6508679315b806c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:40:42 +0000

    home peace politics start nowhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
