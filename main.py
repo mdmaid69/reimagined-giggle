28a1b60e1640c737202ea944f701cc920b812caf
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:48 +0000

    decide custom land or regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+def count_elements(lst):
+        return len(lst)
