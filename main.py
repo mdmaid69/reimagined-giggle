844462d74b547cc6087e7e5997b4a12fdf6a13cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:36 +0000

    acceptable importance giveout file when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
