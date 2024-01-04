da25726abd4c8dbc181f0ac7c8eb88ea249cab9b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:11 +0000

    possible brain close heat cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
