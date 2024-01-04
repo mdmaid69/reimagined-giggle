773a4982b00a66470644348b7b0df4ebf6675844
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:33:34 +0000

    offer dream hang swim convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
