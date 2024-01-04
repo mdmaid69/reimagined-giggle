82a3d889b40b0a19c673ece96a98fa94b8f251ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:13:23 +0000

    anyone nowhere since sad downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
