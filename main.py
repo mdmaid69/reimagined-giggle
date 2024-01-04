a2990b5093963211d623e9372fa00c19e6b9e193
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:15:07 +0000

    afternoon instrument flight mess theirs

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
