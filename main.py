2328b9a0ef66c332051dc1bdd2dc921496a5abbb
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:42:22 +0000

    choose century choice work outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_work(force, distance):
+        return force * distance
