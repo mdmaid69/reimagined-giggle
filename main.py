f54b0dece460c2f5b73bd878b4bc32150bf69bb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:11:21 +0000

    fill I dinner hungry importance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
