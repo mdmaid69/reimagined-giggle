2f473ed7d38a7f85efa0d156ecf533d37ad7b2cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:37:41 +0000

    the politics provide brother winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
