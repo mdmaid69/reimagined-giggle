11367c3175c7efbe89a9a8a8ca130766bab4a67a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:15 +0000

    risk regular before deal score

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
