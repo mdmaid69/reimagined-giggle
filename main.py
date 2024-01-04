90678961db862375c1dc1f44f80b46dac4953882
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:41:46 +0000

    prepare terrible light consider phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
