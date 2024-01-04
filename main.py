5a0dc648ec3f062f41cb82690ea6005cc970aabc
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:14:54 +0000

    important building suggest responsibility seem

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import numpy as np
+print(np.array([1, 2, 3]))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
