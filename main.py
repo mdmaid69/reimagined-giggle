a9770fca956673d87d44d08dcab7a6878ff1f2ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:30 +0000

    cool bill disappointed standard street

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_to_binary(n):
+        return bin(n)
+import re
+print(re.match("h.*o", "hello world"))
