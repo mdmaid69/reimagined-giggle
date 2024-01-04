d5c396e15e88629e8ba821c12b7945d8218ad83d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:49:12 +0000

    check sign zero carry blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
