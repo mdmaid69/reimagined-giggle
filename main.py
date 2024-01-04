b54881cdc96d83c4aa7193efca46621c0b48073a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:49:54 +0000

    world old open example gooff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
