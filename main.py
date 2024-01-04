d180c2ae3e602fdf746ecaae0067a893e4417de3
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:19:58 +0000

    together down blame boat exactly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
