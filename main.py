81fb2e739f09b6e5caa0c0bff78cd86f70f14a64
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:05:58 +0000

    ask question breakinto show characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
