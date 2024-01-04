7d5103f3bf99c6b6450b2d021ba529462728d1d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:38:26 +0000

    present tip sick square or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
