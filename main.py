72eaaf1c4a8e47839ce630c85f92625cb2349089
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:43:41 +0000

    explanation your through level fail

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
