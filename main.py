4a670e3b45eaf6c1a75f98347e51cd3aa5de2cb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:29:47 +0000

    healthy woman concern tour Ii

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import sys
-print(sys.version)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_item(array, i):
+        return array[i]
