846d71aa8325add8352f86b78fc3a72abac04210
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:50 +0000

    either will far Qq different

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def convert_to_hex(n):
+        return hex(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
