3c56902d8b4b774a1a55c9015131887fa474f241
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:39:43 +0000

    summer slice worse life uniform

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_acceleration(speed, time):
+        return speed / time
