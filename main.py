1a3d70207e7b8d0cece0ff6b15f888399134553e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:19:27 +0000

    apart Qq order label attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import random
+print(random.randint(0, 100))
