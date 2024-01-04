247e6d7b75fdbac2dbac5da8e6dc2c4211067b20
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:06:52 +0000

    nearly sleep sticky against roof

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import sys
-print(sys.version)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def multiply_numbers(x, y):
+        return x * y
