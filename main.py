ef1b4b0ce676ef5b650ada372b07395f0f259adb
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:05:39 +0000

    friendly stupid down smart waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
