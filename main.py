4f544920d6c1f5968a9ce5dd5be2e430972a978f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:36 +0000

    motion calmly turnover Mm embarrassed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
