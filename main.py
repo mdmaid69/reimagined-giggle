5e803be520a72f8c35f1da057b69f0d0de5e7c0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:20 +0000

    keepfrom when on Uu regional

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_acceleration(speed, time):
+        return speed / time
