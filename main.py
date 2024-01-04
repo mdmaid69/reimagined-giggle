8db8473fc6d37a1e7db91cc7b712e6f565fa938e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:32 +0000

    equal interview black Uu care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
