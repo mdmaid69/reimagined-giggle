8e41cbd383e53dc890fffc9f982f99a4e9a20dde
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:30:03 +0000

    gradually argue official male interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
