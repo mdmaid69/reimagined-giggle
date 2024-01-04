48594b55f7e217510d2ffa42a1315b3cb326420c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:51:37 +0000

    within close union skill habit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
