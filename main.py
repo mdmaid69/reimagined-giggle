fdf652a69b0e3b509413239672fa6d4360eefb0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:51 +0000

    house regular close attach evidence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
