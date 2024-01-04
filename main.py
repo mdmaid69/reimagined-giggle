061daf4f38b1420e38ab4366925b6037e714192d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:23:26 +0000

    promise turn creature scare bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_item(array, i):
+        return array[i]
