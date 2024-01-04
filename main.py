a9f970af5252ee798d5f971c7f8682354a515ce2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:22:29 +0000

    offer consist why progress pool

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
