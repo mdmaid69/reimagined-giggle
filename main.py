e0c908e0e143a2cbb063d39d0c7d8b48704d263b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:52:38 +0000

    advanced condition estimate second past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import collections
-def create_user_list():
-        return collections.UserList()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def create_ordered_dict():
+        return collections.OrderedDict()
