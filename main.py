b7512719d4d11799f41cff7a2f4a4406786c79b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:29:06 +0000

    cough by correct wild release

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
