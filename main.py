378a7fa9288501a888271f0b2027e0ecda9a72f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:48 +0000

    pattern smile minority plan reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
