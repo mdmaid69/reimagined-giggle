d0c75485c212daf31199e35eb4d47ecbfaed5d5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:37:22 +0000

    quantity fairly row attempt advantage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
