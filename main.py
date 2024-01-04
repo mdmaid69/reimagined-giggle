17364e3463dbe38adbe7e91fa46c738662406a6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:53 +0000

    doubt promise comment body level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
