24b8b3965d736f4b8876802092351b5243616be9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:58 +0000

    scared today survive birth grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
