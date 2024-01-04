d49fe70f720d4b959659566e2e4e8cbbcddea69f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:51:29 +0000

    physical danger owe offence spoil

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_user_string():
+        return collections.UserString()
