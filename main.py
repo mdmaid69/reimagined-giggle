491863efc3f0a006464cae58c757fbfa4145de80
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:10:05 +0000

    behave minimum stir trick name

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def convert_to_octal(n):
+        return oct(n)
