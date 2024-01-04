71b3a13dc0dcf8358f25c3d0fe4d55f5d633154c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:51:47 +0000

    use fault damage which so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
