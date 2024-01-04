6525c6a5eb193a15225b1b9bd4a81eb5ca29511d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:27:08 +0000

    suckme to judge radio original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+print(math.pi)
