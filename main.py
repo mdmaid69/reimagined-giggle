f76784d33edcf7a5887af2129bcc390d8068a52d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:34:53 +0000

    building instruction tragedy flat late

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
