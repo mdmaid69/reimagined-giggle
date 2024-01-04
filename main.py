f3e54de6c0ff1b217d01f96b078d64a488b5d23b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:24:03 +0000

    learn narrow downstairs church instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
