e87e36da280d67e29872a122be9d566ad20e98a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:56:29 +0000

    deaf standard whistle shame instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
