43221f6b06772e3f24a46a791cf3a423a1a2b184
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:12 +0000

    hurt something steam achievement seriously

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def sort_list(lst):
+        return sorted(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
