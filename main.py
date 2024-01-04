ad1c539da2d3036fd1f48715cd0b3da52aa914fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:01:50 +0000

    transport pass visit mean produce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
