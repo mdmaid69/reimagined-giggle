16d26814cea3e6eae48521d7068b587aec918a10
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:17:45 +0000

    tell whenever match smell damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def square_number(x):
+        return x**2
+def sort_numbers(numbers):
+        return sorted(numbers)
