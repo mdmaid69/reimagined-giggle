8c3a72e1597b381f7ced57d8058c65f9db9ebe11
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:05:46 +0000

    chance holdup protest birth blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
