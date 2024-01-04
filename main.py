70020bf0d5dfaab42d15c116de7926e1eee8c21b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:55 +0000

    contain slow attempt unlucky spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
