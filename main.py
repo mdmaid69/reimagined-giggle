3814ef149b7124f7427b7a36f247199e9fe2a2f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:58 +0000

    picture plan appeal repeat harm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
