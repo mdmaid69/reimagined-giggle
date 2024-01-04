bcd5df2a20103c4006bcde1f7d7e5cf97c4acc1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:38:41 +0000

    keepaway complaint collect couple now

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def convert_to_hex(n):
+        return hex(n)
