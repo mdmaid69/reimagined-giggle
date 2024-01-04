c8654b7719fc5ad8ad84ac9ec14e64c6008d2593
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:23:38 +0000

    none believe above stick clothes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
