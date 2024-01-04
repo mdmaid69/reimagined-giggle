dc04ca6275ef96ddcb1d98c48f4dfb539dce23df
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:50:29 +0000

    base hold never ill become

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import os
-print(os.getcwd())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
