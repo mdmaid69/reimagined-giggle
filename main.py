432a65689e843cd7d0ccbef7972591588da98a04
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:32:13 +0000

    bother stupid clear offend pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
