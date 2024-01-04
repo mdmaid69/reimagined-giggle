bb2a9e3435d9f499d979b7b80bb3ab9b9d0de91d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:00 +0000

    judge standard excuse constantly bar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
