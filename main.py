d653cb7c9dbe9bc9d7f4a7556af94d8be8ab59b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:26:29 +0000

    sink march niggaacademy slice getaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+print(math.pi)
