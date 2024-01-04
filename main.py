5a4a5b832ef88acd89726b4eb19070e6c8ee1efd
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:04:39 +0000

    first cruelty same involve basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
