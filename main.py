545e69cf13581ab4953fdc0b16e95fefcb63a078
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:18 +0000

    wave still interrupt responsible beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
