3c7f487cfa5ff7662ba5adf00752fd413a715308
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:39:37 +0000

    start morning remember pupil bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
