5d949666536066d8d36d49b7a02c43c62597efbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:20:31 +0000

    child pressure warn creature second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
