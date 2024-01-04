cc2c32b7e1d033de0c6d567d4895b3039fff7fdc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:16:34 +0000

    brave father floor genuine touch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
