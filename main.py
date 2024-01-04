015992da3913a1905fffdf07709b8aff20c36f44
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:18 +0000

    easy mark main move 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
