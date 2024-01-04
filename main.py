ea7f8e0dbaf1e9c838d1540ec17117e4bbd58ccd
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:25 +0000

    stir beautiful broken protect bring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def convert_to_octal(n):
+        return oct(n)
+import os
+print(os.getcwd())
