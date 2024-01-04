4d5641a42bf227de62c7d777f38c52ef57c7c8dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:16:20 +0000

    space able principle complain moral

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def is_palindrome(s):
+        return s == s[::-1]
