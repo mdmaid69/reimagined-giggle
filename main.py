119951a60c6276ee2b6cee6538cdcd3d90da348d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:39:18 +0000

    suckme on sure tap pollution

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
