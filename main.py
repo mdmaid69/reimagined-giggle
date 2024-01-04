d5acd3bf22b399ec5950d5a795d0d219c2053f57
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:51:23 +0000

    principle attractive repeat connected by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
