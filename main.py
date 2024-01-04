2b2f4d30985f6eff10487152072b5dea1bb723ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:09:59 +0000

    final separate joke thumb please

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_counter():
+        return collections.Counter()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
