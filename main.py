e1eb424b451799c8b393c45af3fb658ea387da3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:44:11 +0000

    stretch swimming Qq extremely clothes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def change_working_directory(path):
+        os.chdir(path)
