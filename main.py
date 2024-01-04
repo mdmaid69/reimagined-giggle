edcf81ce8477a5be6c32ebf121995fafdc12ef11
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:40:00 +0000

    hang contrast may up often

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_work(force, distance):
+        return force * distance
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
