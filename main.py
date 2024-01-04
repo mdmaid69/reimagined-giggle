f16c5b29642f0718e95bc4ac32957f18c172b86d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:14:19 +0000

    path object noise agree lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
