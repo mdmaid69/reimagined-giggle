e6c51ca97ce044152538280972abf60b69d0a187
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:56:50 +0000

    height carry back search physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
