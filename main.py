9feb82950e92a32c784e70c02ae3d63fed5f6f6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:32:38 +0000

    letter safety sight church similar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_item(array, i):
+        return array[i]
+for i in range(10): print(i)
