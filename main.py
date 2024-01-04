55fff4841c23c02b4c2fa42538bb1b6bf39b77e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:25:40 +0000

    regular slowly sister band teach

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+import time
+print(time.time())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
