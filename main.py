1a73ab21ab8c3ba80e47ef1015d0e06fb54f7046
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:50:41 +0000

    pot petrol uncomfortable extraordinary male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_list(array):
+        return list(array)
