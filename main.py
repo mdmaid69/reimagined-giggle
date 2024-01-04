6da9bc124cb8929c31f2cf4e252d171279fa5f90
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:59:20 +0000

    direction hard opposite time rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
