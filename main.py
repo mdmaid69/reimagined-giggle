74487d9c42c659edf60573686451a02291d2b282
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:18:44 +0000

    theirs scrap near lay our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
