c39acc49eceabe35da04913028c48d3f18d8dedf
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:25:50 +0000

    success extraordinary organization boring jump

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
