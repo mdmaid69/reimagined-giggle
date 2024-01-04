075d3f8614a2eb1b4348d243f5b04ba8e3944b21
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:52:33 +0000

    even delight concern 570enjoyable cost

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def subtract_numbers(x, y):
+        return x - y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
