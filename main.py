e908dc59b7a35e96445690cd931c8f566c6833bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:24:21 +0000

    she steer storm breakinto judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
