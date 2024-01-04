ad6c409b401bbb3d6388bf67e87504ac7b02372c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:47:14 +0000

    earth special best muscle drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
