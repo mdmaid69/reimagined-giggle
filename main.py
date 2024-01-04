dd19e1b4cb2c2319717be0a9ac52a9c7a29b2a47
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:01:53 +0000

    hurt fast crash aim underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
