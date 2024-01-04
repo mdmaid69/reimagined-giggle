326ee06d9d7e115569039a923325ef475d453ae6
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:31:50 +0000

    ahead debt proud experience trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
