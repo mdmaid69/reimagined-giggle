2468adf27ab76fa060de1d241518c90bf2ec668d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:00:32 +0000

    speak temperature once physical tear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import time
-def get_current_time():
-        return time.time()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
