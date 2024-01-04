3f7cfdb633284c1c6c2d781eae0b394fe8c45851
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:45 +0000

    mess urgent change hardly react

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
