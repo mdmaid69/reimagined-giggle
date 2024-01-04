8379daf0af0ecd369a104d3a00c69947335d4375
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:00:37 +0000

    ordinary exaggerate phone shout punish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def is_even(n):
-        return n % 2 == 0
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
