fc255b223e58fe5164a3ee19afc518f0417ae843
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:53:36 +0000

    distant sort now Dd early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
