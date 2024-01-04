d4f3526b4292d1de4c15ed3c96101fa9b01389b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:38:25 +0000

    stupid total decision completely unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sine(x):
+        return math.sin(x)
