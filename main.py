ee676c47290a79d220fea573677dcd7e609b17cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:44:34 +0000

    closed consist now precise increase

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
