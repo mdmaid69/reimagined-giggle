4a02eb055dc510e3e749fff15648cc6343d5d259
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:29:14 +0000

    through turnout bringup scene hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
