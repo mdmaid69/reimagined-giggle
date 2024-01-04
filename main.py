e51e77a16c0bc482cd56b57eda8d41f4f9884ea2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:20:36 +0000

    keepup principle simple wherever disgusting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
