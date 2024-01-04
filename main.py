26c065dcfeaadc049e0840c4c9cb34389838b107
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:35:01 +0000

    573entertainment pay tragedy tap comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
