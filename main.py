551b19ebe6b15b8eb58b52478a448ba24a1c7cbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:47:04 +0000

    loan just care pure stay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
