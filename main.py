2c10a7d26858639e1ecddeba4f51daf548797fff
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:54 +0000

    pencil school learn unable thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
