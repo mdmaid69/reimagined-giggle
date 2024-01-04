7618aa67accd9c18f045851af09afa591c9c72d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:06:43 +0000

    radio especially fence rely Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
