152ddb25d62b6b9e2f22c4471ee3cb86151abbcf
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:33 +0000

    switch shoot enthusiastic settle over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
