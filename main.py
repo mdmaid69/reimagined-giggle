81effbe840c8f0deff84c25f8581b300e0b38e41
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:18:56 +0000

    several go custom display admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
