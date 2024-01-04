af2a33bda1f070071e9424a044d7eef27a8f8c84
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:10:10 +0000

    card high process bound degree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_speed(distance, time):
+        return distance / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
