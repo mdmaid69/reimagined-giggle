8011b48576e4040635d2edd5988d556bdfbb9582
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:56 +0000

    more stop keepfrom escape woman

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
