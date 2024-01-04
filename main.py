9b4e06c81de514ce5fb62243034950e3e6a8f5f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:06:14 +0000

    measure whenever tape border talkoutof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
