d7385c8ea25f45e2edeb43e3ac373f8f9f4732f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:39:50 +0000

    bring fight guide fault swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
