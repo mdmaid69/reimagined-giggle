7ba865057ab0511b06e7462cf42c54f6468b7e2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:48:47 +0000

    fetch interesting learn positive original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(a, b):
-        return a + b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
