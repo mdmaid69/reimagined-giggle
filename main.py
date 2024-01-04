dbcb458cd6667c37b476c47621be8a72c802b000
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:49 +0000

    concentrate border dare pain join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
