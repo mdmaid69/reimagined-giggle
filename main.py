1b3b85eec4d7b6574455401d55f52f41166dd47c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:45:29 +0000

    normal load active cruel hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_typecode(array):
+        return array.typecode
