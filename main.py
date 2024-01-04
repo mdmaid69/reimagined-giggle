91631a82ff94db9f0f496ae644bac57296145a3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:07:56 +0000

    laugh central excuse software takeback

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import getpass
+def get_username():
+        return getpass.getuser()
