4756c5f19240806ec257e05281c0cafb1db3213d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:54 +0000

    project slide shake nearly careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
