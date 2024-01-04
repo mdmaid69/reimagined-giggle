792726e302fbe1e6d2ba045b1abb196766d71891
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:55 +0000

    similar else touch tonight generally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+print(json.dumps({"name": "John", "age": 30}))
