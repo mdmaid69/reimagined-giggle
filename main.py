6cad15d0f543c8b837ec4d4957dfc993efbe9556
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:00:30 +0000

    ride coat with care there

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
