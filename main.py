c892c606a4afababc880b82f773119ca684c0fee
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:10:46 +0000

    shut again fortunate pause sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
