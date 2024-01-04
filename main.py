68e59bcf29ffbafbec26bc7deb17101ba7e0d224
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:47:40 +0000

    project boil include dish switch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_as_int(array):
+        return int(array[0])
