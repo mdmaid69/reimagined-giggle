b06c5f4fe9a40461ceb5761e117bb8c7cbda391f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:09:25 +0000

    dare draw address style succeed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_work(force, distance):
+        return force * distance
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
