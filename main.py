7a0000390ca84f98c07c0a278533c93db122ab1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:15:22 +0000

    wind plan deliver inquire bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_directory():
+        return os.getcwd()
