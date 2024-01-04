ed0604b96b542b5c5d8a651e57465062b926ab46
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:10:11 +0000

    travel regular head start out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
