7528690665fe802833a99ae2cc4ed92291d94b2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:09:23 +0000

    to crash know slice interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
