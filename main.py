b87ca72640fab858c5d6094836654f657e135acb
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:17 +0000

    basic concern over blame drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
