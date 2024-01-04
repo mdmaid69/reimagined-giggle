2a1be35ece83e14c043d24d9f40012c4359de8a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:22:11 +0000

    square exploration substance relative crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
