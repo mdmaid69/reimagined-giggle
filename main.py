0965f9cb4f84d3284a38180d22610185f9f0e6cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:38:21 +0000

    think refuse performance judge film

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def get_current_time():
+        return time.time()
