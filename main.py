3d57d16e2fdec24ead08ee56504f7e97847ffaf8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:06 +0000

    reaction exciting whistle machine flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
