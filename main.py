2f8fe704dbbca74cd286c4c4d9322c60fbbe1f16
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:28:06 +0000

    experience read that name benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
