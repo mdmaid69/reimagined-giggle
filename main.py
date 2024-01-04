f22f148918212201efae71b95facaf024147e173
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:08:20 +0000

    chief estimate last how emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
