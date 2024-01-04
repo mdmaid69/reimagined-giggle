07c288f75e19bf5ebbb01c0ec5fc425c29bb64ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:44 +0000

    stop how crowd criticize cum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
