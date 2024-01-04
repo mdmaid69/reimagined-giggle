9a1d08e9aabd1ea985fbbec953ea6d772aba2356
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:24:00 +0000

    use cap when trouble swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
