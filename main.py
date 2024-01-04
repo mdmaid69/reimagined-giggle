356287f7d84d1b7e816306f83729d92df2367ed0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:02 +0000

    reduction thick block confuse record

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
