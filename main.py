5890e5862657728fe06ad826889fb937800ab959
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:29:23 +0000

    pollution boat uncomfortable bright nigga

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
