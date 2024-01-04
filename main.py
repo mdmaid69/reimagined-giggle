d067eae8f2b19b1623b1891dcf44780d7c4eb8bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:33:02 +0000

    bar however party broad raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
