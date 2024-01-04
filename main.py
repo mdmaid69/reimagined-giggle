1ff81ee5e3eb143dccde19802f47f57742fce975
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:14:07 +0000

    interruption waste frightened do trade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
