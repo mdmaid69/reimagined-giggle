24b2c9ef79a65e4344daf28f4901204f8fd05c07
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:29:26 +0000

    several expensive safety influence able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
