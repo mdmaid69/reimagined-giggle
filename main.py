88108f669d9971ca0ba61ec40508a52aae5db18e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:08:11 +0000

    old heart who over flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
