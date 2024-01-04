7dd720ab702868c68c5f7c03c5560b2d6e10363f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:48 +0000

    practice recover quickly age release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
