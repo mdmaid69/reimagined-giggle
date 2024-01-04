51d209c43890cc7a9c20901ee288a7683df7e651
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:10 +0000

    think spend wave takeout afford

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
