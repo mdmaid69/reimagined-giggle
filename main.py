016ad3d25462356c6b55cb7920a33f989148013b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:31:26 +0000

    tight mind wide able attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_stack():
-        return collections.deque()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
