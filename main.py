ea58e4f2440f74a8300de37fed8fba388257ecae
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:45:05 +0000

    walk eye charge money rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
