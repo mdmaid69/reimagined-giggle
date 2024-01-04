9f5f10f1736c490b6b07a4b26f4089d382ab202f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:29 +0000

    familiar illness freeze steer drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
