59cbcdff3a375c739ef59621f707ffca06b7e023
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:39:45 +0000

    sell arrival more probably in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
