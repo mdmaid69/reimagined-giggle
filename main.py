75e2a85d269d86f5009ca7fe5d42466a4d71a8d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:43 +0000

    terrible frightened ticket flow climb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
