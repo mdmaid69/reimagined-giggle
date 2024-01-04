daf3695e6e9ce4de43fe02099bc38acbec58e2d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:28:36 +0000

    talkoutof as wait laugh admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
