1d636683c0b7e5eec6a7fafe2d472e5b97fb44d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:04:15 +0000

    roll century previous convenient move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
