60e495e94c36d13a11eaae5eda1cb0503dc0d8a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:03:32 +0000

    careless pleasure state stick whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
