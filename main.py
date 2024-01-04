51438d560386af66cc0a46811a5d7719eb2ea5a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:37:46 +0000

    guilty sweep slight little case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
