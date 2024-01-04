3909f9d8ca80246d2df91e3072aeba3078c6b9c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:31:11 +0000

    top relief hold say centre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
