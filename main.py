737383964d7d134b3394f24c1696c098868ab4b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:36:56 +0000

    relation runout soil theirs marry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import datetime
+def get_today_date():
+        return datetime.date.today()
