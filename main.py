260a2d72c507e1e5c0f7fe95f4ba1fa859a27acf
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:01:55 +0000

    sport previous watch save cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
