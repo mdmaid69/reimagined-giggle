ff36c2fed99b3f9386ebdd2512eceb82cb4b0839
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:40:20 +0000

    reject trade yours off least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
