9623183904d27e891b43a9ecb17d6a86ed176172
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:32:56 +0000

    exam minority bad help private

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
