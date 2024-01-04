9b19c29a8775b145c8752ea61402f406ac047541
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:27:18 +0000

    skin completely order avoid dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
