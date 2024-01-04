3c7418a46cbcacee6fe88b346ea4e5022f044a5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:53:00 +0000

    concerned children commit nervous flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
