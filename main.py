fd3c57dbeb598dee10d80a2164b570da1d3e29bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:09:21 +0000

    share over real health join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
