8cea6c35dca37c02b98c8254f76871efd2174e14
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:09:54 +0000

    temperature unable lookaround account rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
