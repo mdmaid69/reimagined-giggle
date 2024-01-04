7b159f2759f6fbe53cfbb9328d268354a118b896
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:25:13 +0000

    best escape neat buy point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
