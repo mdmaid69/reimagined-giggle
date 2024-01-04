8245f84c0c43d7c7d832c518e33c08ef524a719a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:59:49 +0000

    comfortable use without command rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_logarithm(base, x):
+        return math.log(x, base)
