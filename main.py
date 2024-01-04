062cfd4902fc77a57d974c06faa35622d83872fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:56:34 +0000

    chin weather guilty lean tax

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
