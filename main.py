1769c03920bb860ca0240162c9a072218f6310ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:26:40 +0000

    mood sail brief word match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
