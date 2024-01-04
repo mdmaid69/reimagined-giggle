112927713e8397aaef8528a5cb4f4eadc4044238
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:44 +0000

    quick song past translate size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
