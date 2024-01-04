297cbee2e70b45cc0b0faff6ccfd7edc8a8adcab
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:53:05 +0000

    once exist shut help through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
