69c5884bcfa584a4de6aeb07b652bc08b2dcc586
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:51:58 +0000

    chat mess juice dangerous form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
