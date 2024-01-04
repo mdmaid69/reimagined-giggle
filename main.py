2e7c80eca5e26c09ed24a0f6bc3add518bbe9e21
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:40:09 +0000

    here everybody deaf bite downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
