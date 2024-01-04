9887ccdd3db11aa7acd30fae9afb9dcd95cf7ace
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:17 +0000

    crime fall moral freeze fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import time
-def get_current_time():
-        return time.time()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
