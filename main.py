3d999dbd2886617a4630ff223a7c4a9766c849d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:21 +0000

    partner past violent uncertain shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
