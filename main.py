4741d7cfbf1eab85a2c8f2af662d10109f4515b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:12:40 +0000

    middle cheque unite welcome organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
