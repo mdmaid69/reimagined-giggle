6f467c8d2aa3b4bfc4b1687f03c2b0514ae64841
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:50:24 +0000

    getoff handle across ourselves education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
