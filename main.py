f970f35097968ae38250a4edb48948c9f5c3e847
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:02:12 +0000

    trap thinkover hang image earth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
