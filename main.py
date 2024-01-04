b900a2f71dd7dbe9212e69a681d914a67ba66500
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:01:11 +0000

    blame secret course activity pocket

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
