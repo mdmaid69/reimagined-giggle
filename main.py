afd8330e59c1f14530510a3ce145e5b51dcb9d74
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:32:55 +0000

    hate permission suggestion extraordinary accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
