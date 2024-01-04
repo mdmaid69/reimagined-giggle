d64d48ad74d77a1059c5b6645a71472868c4cfde
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:10:52 +0000

    practical medium come kind sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-print(time.time())
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
