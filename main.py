b2c2582c6bf14f94f1567a1ad2efe551d084fd8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:27 +0000

    steady near laugh shut understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
