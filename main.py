416a6f1f8b5ca080c6c74ee4f92e827a4d325f1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:06 +0000

    cinema part tour allow hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
