5f14510c9fa68c2e1c28511602a8681141123e4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:33:44 +0000

    calloff contact spend live sound

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
