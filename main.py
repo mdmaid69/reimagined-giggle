6c151bf168184945f7ce09ea88712ef9c02d1013
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:07:21 +0000

    or comeup scream quickly silent

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
