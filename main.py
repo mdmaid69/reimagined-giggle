16692336e7fcbca3e6950b57fb8adbc9f3d73b2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:07:46 +0000

    fairly scare fairly opportunity serve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
