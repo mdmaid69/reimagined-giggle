76348307ed9b32ccc4619d9da0ec61abb7491181
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:12 +0000

    extremely important collect image follow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
