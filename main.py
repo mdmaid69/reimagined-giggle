9b2ca59bd63411d003b8215b9cb40501c5b7c79b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:22:47 +0000

    brown my mistake apparently play

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
