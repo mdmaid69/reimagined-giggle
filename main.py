dcf330f769ec8219d63b3533809be6423a32ec01
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:42:51 +0000

    expect joke mark Kk level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
