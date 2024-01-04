bd5583a7f10177ef537fcd658b29185cf6c8ca28
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:48:49 +0000

    trap spring receive whistle fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
