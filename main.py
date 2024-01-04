b2f4faf8ab675ce254ba2227456db608eb37e653
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:48:37 +0000

    tongue complain certainly form under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
