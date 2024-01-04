bd46930b4e333b39cef03eadf85910125778b488
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:08:20 +0000

    persuade lookup here cruelty born

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
