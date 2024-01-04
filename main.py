fd1326685454c7acd603ceb2c2095bf066861345
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:34 +0000

    knock again confused all except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
