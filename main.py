8d04f8d6fde78fb78553b14c6d8fd8c147139bfa
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:40 +0000

    snow rarely control needle stick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
