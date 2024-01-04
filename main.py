799dc44c13c1f64373ec26088cdafcd7767d796e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:48:10 +0000

    few fail breakout rush finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
