0e8c12195f35509c7394f26211fe2c831c83dc14
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:58:25 +0000

    think answer certainly steal naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
