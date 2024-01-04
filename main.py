6d974cb5c5006b82cfb309bde4b251dfa943c382
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:09:20 +0000

    birth slide share obtain thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
