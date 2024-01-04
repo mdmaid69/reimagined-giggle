07f385fa0f22c2d4a4568f8b57f6a54c37b40ede
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:00 +0000

    still sweet chin foreign desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
