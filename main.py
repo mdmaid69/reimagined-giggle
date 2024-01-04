a3248036f8f031327e8060a54d1293137923cae2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:00 +0000

    job for wind second other

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
