b04026a96ee80b278194ebbc8f5c7b06c7cb0052
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:59:00 +0000

    absolutely interview share grade friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_set(array):
+        return set(array)
