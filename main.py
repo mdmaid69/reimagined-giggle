1a7c7e79ababefc03cb83765015a14b494663a0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:14:25 +0000

    own pride consist backwards cope

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
