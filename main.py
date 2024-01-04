9d015d3a7a01876ead4603e60d19a445165e454d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:23:53 +0000

    himself not many special accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
