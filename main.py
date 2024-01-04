2b06404e5e99d2e5e9ff89df1236dbb1a4fae29c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:37 +0000

    escape soil show especially stare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+from collections import Counter
+print(Counter("hello world"))
+import sys
+def exit_program():
+        sys.exit()
