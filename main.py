3e665dbd50e68ce9aa4b30aa2d8b68943fc9c0be
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:38 +0000

    excuse because trust die during

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
