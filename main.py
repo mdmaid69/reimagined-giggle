3bfeecac5c1617e9a4ed0cec95690b91004838df
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:09:06 +0000

    believe sometimes dream fashion sting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import re
+print(re.match("h.*o", "hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
