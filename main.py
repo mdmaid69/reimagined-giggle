197eee3e49db305448b90fc9bb6ac8f438c16f47
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:27 +0000

    suit trust space sentence gradually

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_words(sentence):
+        return len(sentence.split())
