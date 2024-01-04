3139ca7c70c3f069fe72761d36bf2b3c5801a467
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:14:52 +0000

    second cap work talkoutof opportunity

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
