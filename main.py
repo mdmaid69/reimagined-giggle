5aea57a62a1698bab69800e987a172da0cbb979e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:43:43 +0000

    radio bottom frighten get lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
