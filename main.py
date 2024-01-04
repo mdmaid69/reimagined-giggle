dee4a9edad36b6fd9df6b86c64df11004c353398
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:36:43 +0000

    afterwards factory rich size foot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
-print(sys.version)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def reverse_list(lst):
+        return lst[::-1]
