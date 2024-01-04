4d1f4d0d62c4c1e5d35fd2d23ec2e8ee0671290f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:15:36 +0000

    effect breathe callout reason commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
