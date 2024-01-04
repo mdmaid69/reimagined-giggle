d0255cb96894236f46185b0007625722475c7e92
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:45:09 +0000

    home rough sticky shout long

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
