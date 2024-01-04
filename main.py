3a12f444ed76220499972dc045348818a605b721
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:23 +0000

    instant vision voice border pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
