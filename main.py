39e9fb80bb4ad94e18f1374136663ed3242a5aec
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:32 +0000

    government exam copy charge involve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
