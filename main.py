dba157438a86454ba65145c7670b920afcb43dec
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:28:55 +0000

    guilty raise low discovery tap

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
