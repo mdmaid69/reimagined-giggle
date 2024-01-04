18eea70d480bc051007b0cecd6264b33001546b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:45 +0000

    juice problem reasonable test conclusion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
