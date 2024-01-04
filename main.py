2efb992aa053c32c3f72e7fd04025b36a8ea891e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:46:56 +0000

    damage react pen tip godown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
