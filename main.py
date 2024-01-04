38f8326d5daa7e7a23e241991dbf1eb6d0a37945
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:24:35 +0000

    use wise meeting back search

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
