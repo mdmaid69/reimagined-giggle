8c0ca22ef139fc0dd233b6d5367cda8c54507056
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:34:59 +0000

    sign afternoon cross party hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
