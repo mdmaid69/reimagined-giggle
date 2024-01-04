7a132351123c4be4096b2053583f37098e83199b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:54 +0000

    suddenly grow advice much backwards

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import array
-def get_array_length(array):
-        return len(array)
+for i in range(10): print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
