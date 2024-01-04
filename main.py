9c0dde80f6252d214ab77a7a14af5e8467874b5c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:24 +0000

    bottom scare after guess backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
