768299e56e98778c134e024bdc3e96df6e1a5a20
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:24:36 +0000

    ball kick out various catch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
