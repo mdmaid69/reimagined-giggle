b5675132b64206e1308c02e4651401cf2183a599
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:50:55 +0000

    birth autumn bar so reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
