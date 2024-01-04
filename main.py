593afa73d64d710923c98fbb37defe6462e7b1cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:36:13 +0000

    sore better visit major still

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-print("Hello, world!")
+  def square_number(x):
+        return x**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
