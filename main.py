30189eec3dd531229206ef60d9be640484fbed4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:41 +0000

    over listenfor reply path step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
 def calculate_logarithm_base_10(x):
         return math.log10(x)
-def cube_number(x):
-        return x**3
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
