5ac14bb7b480cd57901f5a08f6d8a0cfedc63852
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:31:19 +0000

    balance century loan share special

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_exponential(x):
+        return math.exp(x)
