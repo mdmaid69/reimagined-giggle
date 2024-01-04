02f8b5873187868be69dfeac40d2a0cfd5d632f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:11:27 +0000

    ill base gas judge wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
