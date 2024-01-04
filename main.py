3d3527aef51d7472461a28b4406e6b4efd203b0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:20:59 +0000

    equal afterwards beautiful patience rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
