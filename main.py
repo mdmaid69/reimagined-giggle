2f4b675d3793e17acae5ae8d6e02b9bb4597e7e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:00 +0000

    our home present quarter up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
