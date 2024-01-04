2b98db9b5d864830961f3cac882d73ec2da31173
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:04 +0000

    face force out decide copy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
