2e65b3721d130f387683d182773600b172f025a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:11 +0000

    often mind connection exchange raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
