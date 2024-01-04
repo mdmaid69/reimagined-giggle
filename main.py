2cefbf690a8a4d3804a9e0de493549ff107dd2bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:54:36 +0000

    borrow punish which feature age

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_min(numbers):
-        return min(numbers)
+import array
+def clear_array(array):
+        array *= 0
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
