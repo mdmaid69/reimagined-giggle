9a772841d94ddf02fbe012446c4699967bd62488
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:06:48 +0000

    talk fresh see treat good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
