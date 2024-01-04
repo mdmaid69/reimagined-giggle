1e32789b90b4dba58fc64a4aed90bd95deec02d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:57:21 +0000

    myself science flat proud cut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def split_path(path):
+        return os.path.split(path)
