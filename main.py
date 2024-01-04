ea1de355134d7d382cd96c8531a485906b5075e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:33:42 +0000

    breakdown exact by sure justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
