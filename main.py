c164e8b2f6037a1c5bed0783c0f24cacba3ca32d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:35:42 +0000

    equipment similar every underneath common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
 import array
-def get_array_length(array):
-        return len(array)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def check_if_array_contains_item(array, item):
+        return item in array
