9726b367f5331b6e4d3575a02096c62c395a3d5c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:50 +0000

    hold success justice tune noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
