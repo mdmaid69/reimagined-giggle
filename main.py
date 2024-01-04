19bc3c682dbd12f5f917e2d33754dad5f4dff3a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:46:05 +0000

    sign scared runinto will chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(lst):
-        return max(lst)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
