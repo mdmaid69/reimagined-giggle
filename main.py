dbbdddba35f6857b2063bc6b36d8c684aeb2ebf0
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:14:49 +0000

    public guide bend tour land

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_prime(n):
-        if n < 2: return False
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
