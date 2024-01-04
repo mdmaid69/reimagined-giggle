56f84a9eebc84026f8dbd2130a27b07cce1497af
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:02 +0000

    rank getaway this relief with

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def get_array_slice(array, i, j):
+        return array[i:j]
+def count_characters(sentence):
+        return len(sentence)
