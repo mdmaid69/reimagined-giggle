4d68f1ef5a3b56f8549b970f5997651e795de2f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:59:34 +0000

    rather meaning combine play spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
