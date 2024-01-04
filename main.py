8506bebc780267894c6a2a1c0fe66dde80a99535
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:50:31 +0000

    limit maintain miss group duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_numbers(numbers):
+        return sorted(numbers)
