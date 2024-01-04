bad80cd226ac1fc228e7d0a6369953119041c460
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:08:28 +0000

    rest that kind comeover meaning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def subtract_numbers(x, y):
+        return x - y
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
