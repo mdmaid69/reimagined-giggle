05952dfc67787211dea96e07ccdf22159f1aca83
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:04:58 +0000

    address actually pull equal cry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import sys
+print(sys.version)
