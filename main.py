584774b08342f55cd222ed72c7d958dd9101d8c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:09:48 +0000

    noisy down tap jump design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
