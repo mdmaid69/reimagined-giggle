eb51085ed328684fb21a6b45ba2c9e69750f8b23
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:19 +0000

    keepfrom threaten bringdown bringdown simple

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
