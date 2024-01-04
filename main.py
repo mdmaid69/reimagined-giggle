da4fbed3398aa81dcce5651f4b55173966e91699
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:17:08 +0000

    hang information bother Bb confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
