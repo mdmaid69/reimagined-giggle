bab54e2f0236b0b80152c278ee7fa04fd3c1c99f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:45:18 +0000

    month effect friend mess through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
