e3656861b17bfab243e0df9a4d1da093955dfb89
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:33:38 +0000

    pause injury work or sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
