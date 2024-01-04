b3b175a27046f9d15a01003c5ad3f518c42cc47d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:33:18 +0000

    tip twist instrument before afraid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
