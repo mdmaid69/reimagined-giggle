11389975708188e8d81567d447d4adc554400539
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:14:57 +0000

    sea building practice slight damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
