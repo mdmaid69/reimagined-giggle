73ddf0349f1d86f15f234ee3d2f35b6907d41b1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:16:04 +0000

    confusing male sink flow plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def square_number(x):
-        return x**2
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
