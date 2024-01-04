10b087a46aa2535e5584eeb39b04dc9714220ebb
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:54 +0000

    sting though prove cross behaviour

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def reverse_string(s):
+        return s[::-1]
