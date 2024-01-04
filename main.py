720822f48e59f98ae14c16d0b9daa99cb0a1313b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:16 +0000

    direct group clear unlikely centre

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
