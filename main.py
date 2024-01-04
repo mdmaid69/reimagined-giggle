bf0d56c670c1afd09b27755482631c081d72b5a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:01:06 +0000

    565end gift fail soak obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import datetime
+print(datetime.datetime.now())
