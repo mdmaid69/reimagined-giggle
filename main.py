dd6d0edd8620d8fb11f943da3a8e3a4f06bed596
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:53:06 +0000

    whom over tap suppose limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
