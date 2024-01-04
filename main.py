36073f6c1e09114780dff61082a14bfc6e94f59d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:27:01 +0000

    store tough characteristic undo free

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
