5945a924c93883056a0219015a7508520c608663
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:50 +0000

    under try gate emotion punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import os
+print(os.getcwd())
