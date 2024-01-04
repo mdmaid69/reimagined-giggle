b02744e67d7d8689a78e4e86b5ca612677c749b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:53 +0000

    apparently safety choice bye variety

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
