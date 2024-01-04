47ed3b0a26d75e26fd9a0634a87e01416f6c69d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:19:03 +0000

    float last pretty anywayanyhow each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
