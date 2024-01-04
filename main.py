39d75e16fc8fc9eae1a389aad262316e2030b707
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:43:36 +0000

    accident record herself relationship wrong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
