6dda1bc58ec58dbc9c45ce4a92d929e0c3a407f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:47 +0000

    frightened squash organize like address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
