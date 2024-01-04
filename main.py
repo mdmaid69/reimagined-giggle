54b85adc785e932051ed18c7a126b740b29e6c6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:31 +0000

    wrap lookup scheme woman wheel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import sys
-def print_python_version():
-        return sys.version
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+name = "Python"
+print("Hello,", name)
