678ae993251724e07719417bfc9bc5b1502e155f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:53 +0000

    too area dish view compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+from collections import Counter
+print(Counter("hello world"))
+import os
+def change_working_directory(path):
+        os.chdir(path)
