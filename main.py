2cad4e0015d4f1b953a3e9947cfb61ef39ba6432
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:48:34 +0000

    waste likely pride fear record

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
