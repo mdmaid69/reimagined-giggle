f6f09a65bd3a83436e28a5765718dad76e72a440
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:23:58 +0000

    rest area person drink teach

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-print([x**2 for x in range(10)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
