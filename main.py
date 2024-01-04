1516f7ce3d9dd8f4669b8bc871308607ad6c561f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:02:52 +0000

    fear thinkover dream strength charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
