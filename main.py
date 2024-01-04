4f5e113734fab3e522d78b499dc6a2660f40b608
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:25:54 +0000

    stiff street meeting bury hope

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
