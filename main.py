928df669c83ada1c98baadbc2c504096c4befc30
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:16:38 +0000

    control if debt left genuine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def subtract_numbers(x, y):
-        return x - y
+import tensorflow as tf
+print(tf.__version__)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
