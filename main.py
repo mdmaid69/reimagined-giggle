0b54354cfe5d03eb3a5c9aa55878161543ba13ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:41 +0000

    side which last private type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-print(math.pi)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
