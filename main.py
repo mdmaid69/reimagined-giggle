84f99dff6bdd62105a6795751436db256131716f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:32:57 +0000

    nurse your prepare child Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
