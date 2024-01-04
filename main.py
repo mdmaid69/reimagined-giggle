7f9356c850005662ff975f6973d3453b4ef6d21c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:43:58 +0000

    now trial confident reasonable without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
