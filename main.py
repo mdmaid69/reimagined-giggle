5d31f1ee57050088535ec9c211406f811f6de335
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:20:56 +0000

    cut mixture giveback runinto female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def subtract_numbers(x, y):
+        return x - y
