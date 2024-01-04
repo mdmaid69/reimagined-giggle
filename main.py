e50b18997e88c3d29bc9d9a3918afdb419db8bc1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:26:41 +0000

    doubt thinkof squash confident walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
