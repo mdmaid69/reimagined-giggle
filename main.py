0f893e84dbc5a2b13aa5d30ba16f0078e159f3bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:41:38 +0000

    excuse patience crash trap quit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
