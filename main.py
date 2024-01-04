9fd11ab685a90dc173b28cb16c80b4cf707ecdd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:07 +0000

    argue post Bb week juice

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def convert_to_hex(n):
-        return hex(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
