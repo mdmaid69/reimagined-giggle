f7cef4a5b762537b769fe21d575975d985982076
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:00:11 +0000

    forgive blame chief see sort

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def get_array_as_bytearray(array):
+        return bytearray(array)
