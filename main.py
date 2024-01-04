dbecd60788b7d90d7a774aff6790ba822811c9cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:40 +0000

    sail use holdon sing chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
