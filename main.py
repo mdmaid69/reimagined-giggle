17533ab9196bff24ead69cc07b92c7877e12b86e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:14:14 +0000

    embarrassed cover unpleasant see correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
