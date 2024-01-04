bbc3190f7c3bc16f9d7feb08ff7d110bec8d23c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:38:36 +0000

    free winter know uniform Ll

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
