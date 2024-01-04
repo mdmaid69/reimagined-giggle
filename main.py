fe1c8db92505ba873f998d4cc61e8ce5cb813d19
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:40 +0000

    noise Nn ordinary scare question

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_numbers(numbers):
-        return sorted(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
