ca0c45cf843d27adf1b18d01eb57cc4586286154
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:21:30 +0000

    tidy taste excuse relationship geton

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def convert_to_hex(n):
-        return hex(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
