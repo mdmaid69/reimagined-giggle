81dd9fff4ad006ec63bcf21f3b0e699081399f88
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:23:22 +0000

    degree anybody waste sail Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
