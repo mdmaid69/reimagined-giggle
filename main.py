af0d924ba841efd6e949a2c681373580ce672f2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:32 +0000

    intend opposite forward entry thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
