70f3c364fb076f7d156537afd83865a4d2ec7228
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:28 +0000

    pay card soft together play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
