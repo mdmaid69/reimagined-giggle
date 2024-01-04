093355241c0ecfd81c253f189fa59074c0d3b9f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:36:46 +0000

    occasional steady cruel simple later

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
