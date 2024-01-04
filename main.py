0e87797f65790fde7e3324f42be3d3e80121ba30
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:55:09 +0000

    little squeeze reach drink practice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
