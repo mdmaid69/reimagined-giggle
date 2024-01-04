c376b3da40f82aed48539045307d5ea9dd9f02e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:50 +0000

    nor cruelty firm maybe drop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def square_number(x):
+        return x**2
