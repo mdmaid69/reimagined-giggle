1bce1bac117729384d948218e2c4c92d4772cbda
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:56:59 +0000

    star pollution everywhere deserve splash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_item(array, i):
+        return array[i]
