b7134693f78fac5ba9a8f7eebeab25ecc28a8d15
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:35 +0000

    play Qq want comeround definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
