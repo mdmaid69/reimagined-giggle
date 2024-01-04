e332b3bc754bb7453c9c4df4cf74d8a45e1bc6ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:03 +0000

    holdon crisis yesterday fault commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def convert_to_binary(n):
+        return bin(n)
