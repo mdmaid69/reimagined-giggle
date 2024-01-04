acb16cae6b51e896d33c7f80eb4b7c82276b0b1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:51:43 +0000

    fire opposite recognize hard wait

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
