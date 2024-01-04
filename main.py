0de6f092397fafbdf24bcc5f3e6371d7c5062a60
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:31 +0000

    callback thin fair clear getout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
