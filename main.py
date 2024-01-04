3438f8890f138198afc5d65109f50b46ef52fc35
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:28 +0000

    respect build fight Uu especially

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
