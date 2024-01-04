5f0078847ab869019ffea94b00ca1ed5be445da9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:08:08 +0000

    hurt nor speak character stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
