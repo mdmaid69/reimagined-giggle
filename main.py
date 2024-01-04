abf43bdb753b55829930120ad53de9141eb8af51
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:24:24 +0000

    answer reduce unless deal exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
