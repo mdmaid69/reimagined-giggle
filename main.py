4c391024771f68d7239ed596d2e00f4f497b660d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:32:08 +0000

    event disturb during involve quality

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
