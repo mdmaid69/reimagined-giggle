cab251e6cf6421b012d5f7b5eab7dbf0e7b2caeb
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:07:31 +0000

    value of official advice thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
