f95c032ed43120900a899fb4588e40ea7aeec84e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:32 +0000

    thick business means least think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def cube_number(x):
+        return x**3
