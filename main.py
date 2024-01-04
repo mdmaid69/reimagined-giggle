b4ea76560ad1724d00d89d18074a5e538671b63a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:11:51 +0000

    laugh takeover blow exploration corner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+print(math.pi)
