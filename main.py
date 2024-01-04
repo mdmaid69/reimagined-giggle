e012b3727abf5130e84855b07b8a479be71188ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:40:26 +0000

    turninto push mistake excited discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
