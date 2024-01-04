ed5b85e6ac18557b52eae760dd13d408c5fc20d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:27:05 +0000

    stop factory face any word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
