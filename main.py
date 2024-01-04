954b2c4532fbf1a1976b4102455af96f5fc50f6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:04:06 +0000

    want somewhere sometimes operate well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
