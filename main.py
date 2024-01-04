31a7cc046a4aed114f40d60e73a01a12dd1e6e8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:26:25 +0000

    talkinto certainly inform then nose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
