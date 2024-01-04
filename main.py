e7c41fee00bfd74b52ffe8cb2e52bc85e661eee1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:16:25 +0000

    apologizeapologise move ill argument work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
