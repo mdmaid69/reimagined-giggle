52f0cb545e4414efdf5fa9f6eb9b82956c824435
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:20:07 +0000

    task lock order also improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
