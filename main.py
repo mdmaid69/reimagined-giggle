9914e06e6dc3d398f582924a818021384c8d9a12
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:27 +0000

    anxious border real speed far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
