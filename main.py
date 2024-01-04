9afe24191fe0dd274aa6ffc46a615ee99d45d2e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:07:53 +0000

    fairly nor enthusiastic official phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
