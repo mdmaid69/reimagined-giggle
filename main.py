ed32dea396a5670f6dce34902d3a2e0d08312f45
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:29 +0000

    paper kick force programme sure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
