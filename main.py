5a483ab61bbbc8b6c2197ed983d76dc417670a12
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:03:09 +0000

    unpleasant keepout ride on weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
