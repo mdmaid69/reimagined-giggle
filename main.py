772aab7bc2701c432a7e7649bef23e2a9f1f4f65
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:11:26 +0000

    bet maintain instead set ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
