d3713a5c0ea1403dc6246219ea4f659359392d7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:39:37 +0000

    call imagine herself comeround intelligent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
