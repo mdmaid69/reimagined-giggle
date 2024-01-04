cbc56dd38e0adf663d20db0bd257f6b281e69354
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:58:04 +0000

    offer geton word rest young

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
