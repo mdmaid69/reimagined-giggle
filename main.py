b264fb5e6ec97fb14b996fa1b0131efbdacfeb1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:22:38 +0000

    stupid OKorokay beat kind takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
