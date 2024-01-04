4d3f8478dcab9d6400af80c3ffc7739dd1a52f5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:34:57 +0000

    gift mouth apply patient sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
