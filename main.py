ab005ca968680b29233480b61c1eb6bbb880fecf
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:26:35 +0000

    doubt left still use motion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def convert_to_binary(n):
-        return bin(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+print([x**2 for x in range(10)])
