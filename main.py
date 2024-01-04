0944f0c95bfd0828701d6ed086bef4ec5c012329
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:09:58 +0000

    fight fat willing wind should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
