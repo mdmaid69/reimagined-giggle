dfa6fc933e08eb84bd504c53d17c4e1c757e6f87
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:10:13 +0000

    any better black behind shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
 def create_directory(path):
         os.makedirs(path, exist_ok=True)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
