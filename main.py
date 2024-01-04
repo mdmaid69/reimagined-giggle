93cbe085ed3b564ba86981c8cbb5bceeeb220fde
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:25 +0000

    thumb past sentence speak odd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
