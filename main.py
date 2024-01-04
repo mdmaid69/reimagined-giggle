1515b261f2319b308354f37694f467b0558dc34e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:50:06 +0000

    564encourage scream hospital finally leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
