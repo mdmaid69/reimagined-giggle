a570de05b3fca0c746f4f1e01bf2b4245a505f96
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:46 +0000

    organization conclusion rain possibility float

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+print(sum(range(10)))
