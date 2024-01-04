5a3115197ce5ca11f4733559de55ccd1a0047a6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:53:13 +0000

    demand handle sting wonder so

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+for i in range(5):
+        print(i)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
