e35b494b807ce35212ebeb1b6e5cfd47ea73228f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:47 +0000

    judge amount confused wrong dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
