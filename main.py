300246611a4ba41811aea575020dc108182a0e62
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:08:13 +0000

    pride stay rain divide afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
