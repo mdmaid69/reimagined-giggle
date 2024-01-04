6077e2db98b443b8e14a257c83dccc23f6c34f23
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:59:44 +0000

    rule even story chief affect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
