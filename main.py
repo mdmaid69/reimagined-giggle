c9a0b1a685f20729d882f3ddab8d46b3cd040714
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:16:52 +0000

    father sky whichever pleasant front

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
