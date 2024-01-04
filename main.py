77156923c2e2b73e66233ae34b3722a1828711f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:16:35 +0000

    appearance getback determined row pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+print(datetime.datetime.now())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
