947761c07d19565a0b9f308e925ef4bdd855bece
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:04:21 +0000

    correction punish post convenient night

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
