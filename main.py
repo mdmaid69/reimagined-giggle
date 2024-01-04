ebcef29ca59681dee86ef372baec27c7ac04d872
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:17:42 +0000

    generally partner pull underneath settle

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def create_queue():
+        return collections.deque()
