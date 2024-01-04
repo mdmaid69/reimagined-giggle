0511b1d789cb1f748ddb5ca4eb66fb0b6441d20b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:46:00 +0000

    open guess charge willing especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
