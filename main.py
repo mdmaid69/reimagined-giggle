9569b417a37c73981c2f0cda17764478051ac2d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:37:43 +0000

    body ill steep pay mistake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
