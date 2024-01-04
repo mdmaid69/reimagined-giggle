41e51dd686dc738b64be1f55176b1fc832126a17
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:16:30 +0000

    shade plate impatient disgusting reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
