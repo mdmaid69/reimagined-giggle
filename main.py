13461387db7ba600e8ce9dc39b6f86e340ec1c4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:43 +0000

    gradually keepout rain transport power

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def reverse_list(lst):
+        return lst[::-1]
