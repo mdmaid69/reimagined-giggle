9f497f7a051eb13b7e50ee2e37995950f436f09d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:14 +0000

    really sign thumb giveaway lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_speed(distance, time):
+        return distance / time
