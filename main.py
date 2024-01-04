18d349eaf6156dfcedd4689a4247e61737ad3b2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:05:54 +0000

    bite careful fast music ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
