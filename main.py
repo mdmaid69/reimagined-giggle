383239e74e83c0037697e5423d0483ddff61628f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:00 +0000

    entry pile tap hers complaint

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_palindrome(s):
+        return s == s[::-1]
