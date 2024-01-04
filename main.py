a55a17e7f7e2a1b01d4e8e35cc34c1ff80447da2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:37:33 +0000

    strange difficult treat sale cost

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
