1ba686959e2ccdf410f24e3cf2277051fe9a0cd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:17:20 +0000

    serious towards group minimum everybody

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import collections
-def create_stack():
-        return collections.deque()
-import unittest
+import queue
 
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
