1daf9500448f3dba749bf625fd35a1a90594a710
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:05:47 +0000

    rid cook second genuine worse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def insert_into_array(array, i, item):
+        array.insert(i, item)
