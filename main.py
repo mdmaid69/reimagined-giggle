762029731538ab4387fad7a5178b8df3fd2071dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:51:57 +0000

    contribute roll board forget runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
