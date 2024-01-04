3a71e6a506309e7576c856a2db70c904cd8b22f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:05 +0000

    tradition tread like tear 565end

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
