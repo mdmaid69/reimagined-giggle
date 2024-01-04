052f870fdb973f0ccb75e996a22374d1367d96df
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:12 +0000

    secret mass other badly necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
