ec5f953f08d7e4f09a2ac48605cfa9aedecb7d42
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:58:20 +0000

    embarrassing injury pot 570enjoyable record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
