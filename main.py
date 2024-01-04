59c22d294815bb274aaf7f991ba12fc20c82146d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:28 +0000

    when star tense hope private

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
