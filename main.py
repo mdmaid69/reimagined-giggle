6ca3f6e2eb1e03b47a3d33ea007b2cd9f438872b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:14:54 +0000

    abroad region Ww call shake

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def count_elements(lst):
+        return len(lst)
