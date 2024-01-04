5da3d94796247bf4124fa2f884830d87bd09c27c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:32:47 +0000

    care instant goodbye bang while

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
