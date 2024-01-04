6e920a46f9a78bab93cdb8dfdfb315c6a12e86ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:54:08 +0000

    yourself wait combine defend estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
