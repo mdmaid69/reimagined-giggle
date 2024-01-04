75420be0dbdf322afdc4f3241ae23c98b889f283
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:01:41 +0000

    under agree good minute club

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def get_current_working_directory():
+        return os.getcwd()
