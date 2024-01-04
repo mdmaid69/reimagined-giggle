1d6b33fee921920887a1a22483c4d1810971b153
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:45:12 +0000

    nice deliver save unit plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
