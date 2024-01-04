5586d98dab81f8e26947630f23facce872d2bcc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:17:00 +0000

    persuade let above clear upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import collections
+def create_queue():
+        return collections.deque()
+import collections
+def create_queue():
+        return collections.deque()
