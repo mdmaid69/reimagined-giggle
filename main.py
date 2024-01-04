d8b8f4895bf4223c4d418d54e24be4417660f928
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:12:38 +0000

    better blame trouble fit point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def append_to_array(array, item):
+        array.append(item)
