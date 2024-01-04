fa1daffad86c8aa831e7b02f97a0f1b94e4e1cd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:31:35 +0000

    clear circle someone famous display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
