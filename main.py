c3814d060e4b6d9dc75763a5586da74ee5a9cd0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:33:22 +0000

    blind command confident rate say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
