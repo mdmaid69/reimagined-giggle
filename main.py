c58f6b215db50a947ad43c5437d190fe33f7ac9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:40:30 +0000

    sure aim tidy condition everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
