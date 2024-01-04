5f4b3f6ac3c1a9b0547d35319365a909913b8382
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:59:45 +0000

    bread arrange gradually interfere gap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
