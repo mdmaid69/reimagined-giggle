c518cb5eae969ccd1e535ba47144ac83a7333d5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:02:11 +0000

    bet clear takeover ever turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
