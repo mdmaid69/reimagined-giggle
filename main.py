33d8b52067e44219fc09736155adf5ead365131f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:40:50 +0000

    niggaacademy entrance to or spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
