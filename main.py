4a2f55206e5a4cc61be00a50d62fd0ed254c89ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:10:33 +0000

    callat tooth cry ocean loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
