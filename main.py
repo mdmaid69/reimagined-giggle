43543f8b6255a45d5d91b4831487db042767a572
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:41 +0000

    talk community acceptable progress dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
