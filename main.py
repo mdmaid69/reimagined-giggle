c75b69a604d1cced617e49778e7365fbcbc13da6
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:38 +0000

    welcome belong shallow accident nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
