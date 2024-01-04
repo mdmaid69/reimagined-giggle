4a380045bca71bd3fa2222e64986fed57b458056
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:13:34 +0000

    hurry interesting wealth share become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
