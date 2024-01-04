e29dd01514808f045bb8c9a995bbe1ba41e28e88
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:11:19 +0000

    total luck turnup result either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
