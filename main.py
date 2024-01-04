6e7978a2a2cdf2d9023e2f195c39133af2196e29
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:40 +0000

    better lead takeaway ignore another

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import sys
-def exit_program():
-        sys.exit()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
