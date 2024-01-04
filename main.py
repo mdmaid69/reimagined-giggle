ad1e3c89e710d298cfe17663e5aed98e2feda422
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:52:55 +0000

    getup clue petrol round true

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  def calculate_area_circle(r):
+        return 3.14 * r**2
