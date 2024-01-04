5bcea3f4273a4f9855f7caa8ff82de35c328f03a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:59 +0000

    shock soft talkinto raw neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
