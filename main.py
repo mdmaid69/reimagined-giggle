c103fa2052c5d63c98e6e0017324cb30772b6e2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:05:53 +0000

    treat post reduction Hh private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_area_rectangle(l, w):
+        return l * w
