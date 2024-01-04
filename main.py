d7f46c2c5c1fa1efec7a803a3a54ea828834e580
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:05:56 +0000

    government warn turn shoot exact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+print(math.pi)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
