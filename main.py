c0e40f7d4dd98ee7455e0867bbbbc26a3778986e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:35:26 +0000

    mind depend rise contrast satisfied

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def reverse_list(lst):
+        return lst[::-1]
