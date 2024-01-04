665df76d3332288ef9ef0ba3c3e0f71ae8b5e9a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:42:05 +0000

    tip across anywayanyhow commit position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
