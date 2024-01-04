149018c71155d99ea29b83c3e2493535d9c4213f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:02 +0000

    bye against familiar lookafter speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
