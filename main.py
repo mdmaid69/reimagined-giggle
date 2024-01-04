6eef24d4f8fdfe7ccac15f5fef296a90c280feca
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:56:23 +0000

    complaint please spring door impressed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
