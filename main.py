47c03e4c46c847fbcc26a31c9a248504dc331ee4
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:11:26 +0000

    there fat quality protest dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_item(array, i):
+        return array[i]
