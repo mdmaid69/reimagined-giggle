310ec97f08b702eff21631f49cdf89b492933265
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:42 +0000

    influence most correction carry occasion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
