b3e02b91ecddb04cb4b561d6d1142f016d4a8c71
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:05:25 +0000

    coffee possible afraid keepaway shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
