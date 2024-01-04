aa105d403bb2a089f4c5a441e38639064a912b17
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:55:12 +0000

    disturb ever fasten possession eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
