84e40fa2a813b8ebdd800cea327eabe84ebcae17
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:54:42 +0000

    lookafter think holdout continue climb

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-import os
-print(os.getcwd())
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
