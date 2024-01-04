6ed571daf49460a61a8ba40b543cb98eede7afef
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:03:51 +0000

    holdup keepup conduct dare software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
