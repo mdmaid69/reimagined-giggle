04dd97aba12c59e50d4d0bf84cc5c7eaa8ab5c0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:24:16 +0000

    winter forever smooth upstairs common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
