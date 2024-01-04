4e4e7f24b1c8850640cec3be95de5c0377c23519
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:47:03 +0000

    goback(to) judge heart please beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
