7e971c8f5c4e2bc8b0ae832230820cc740cb524a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:04 +0000

    transport with stay criticism back

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def find_min(lst):
-        return min(lst)
+print("Hello, world!")
+text = "Hello, world!"
+print("Uppercase:", text.upper())
