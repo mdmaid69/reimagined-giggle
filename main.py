e5dfa50a4c15d395196d502f65191a840b5e2e49
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:01:14 +0000

    type unlikely wide cause flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
