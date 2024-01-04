321b97f97e350f354fa6114b6cf6413e1bff58b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:10:27 +0000

    share basis disgusting Tt lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
