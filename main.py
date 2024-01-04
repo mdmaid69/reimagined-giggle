7554fc5bf4b8f242ca1c66e213db0e88ac837b07
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:53:21 +0000

    coffee guard represent possibly high

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
