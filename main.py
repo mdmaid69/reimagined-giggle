e1ab52a63569d8574b3c445f4eae269481df6b03
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:58:42 +0000

    mouth poor artificial ill sharp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
