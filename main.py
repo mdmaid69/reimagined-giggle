189773e0f3a824615dbca62654f0b623623b7157
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:20:53 +0000

    off mark cold tidy concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
