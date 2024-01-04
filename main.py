058ae29b595440e42c126204a4e44692c9ac7481
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:58:50 +0000

    dangerous worry lay fairly fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
