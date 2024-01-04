7f988a4be9bb39f74d2878fed7283b47e4124a66
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:02:31 +0000

    grade prove successful where disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_string():
+        return collections.UserString()
