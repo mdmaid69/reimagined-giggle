098bb086c5f5d8fbcd5d4e4697cba8598dbc74ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:34:39 +0000

    past port weekend permission strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_tangent(x):
+        return math.tan(x)
