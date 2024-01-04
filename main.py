a8c0d6194abb8584c218331d45c3b7ebf40ca38d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:41:51 +0000

    divide hit squeeze well time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Words:", len(text.split()))
