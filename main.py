09e5c9116cff69a79e2784f4a455b5ba09cd8263
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:42:42 +0000

    grade repair nose human leave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def find_min(lst):
+        return min(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
