6b6376107a185f8edd82ec4ec3fa427d27f49e1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:53:43 +0000

    slide rarely grade expensive whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_array_to_list(array):
+        return array.tolist()
