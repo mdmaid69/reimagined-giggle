0c900115d780a7176df5a48b781ed530dec1e860
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:58:01 +0000

    disapprove makeup scare perform picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
