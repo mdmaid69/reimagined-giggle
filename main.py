ba8f9cefd57278e7f5f1bf5c910f2e74e2c29a54
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:32:36 +0000

    ring hate tradition band big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
