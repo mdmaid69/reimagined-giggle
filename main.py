400f70c9a6ead25e667e6b25925fb03382dbf36e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:33 +0000

    satisfactory perfect little give tax

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
