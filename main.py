6853a7a860ea2fc3b70a4f8bb4fcaf444c7dff87
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:41 +0000

    fight scheme where centimetre preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
