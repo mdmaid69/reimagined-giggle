8ca8273fc599ff0a3e4cb14b1c7ca46809e32514
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:00:09 +0000

    cause follow water you nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
