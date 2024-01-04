d9a5f82b01eda232ab51fc7e754c1aeeaba29897
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:18:27 +0000

    music single complete makeup separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+print(sys.version)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
