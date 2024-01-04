fd56988f78fe15ac5c88441b81d4014c3ba014e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:23:52 +0000

    quit passage miss surface write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
