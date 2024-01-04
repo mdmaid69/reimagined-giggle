d07cb68e35642ffd5bedcc97e846111c074bd4a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:57:20 +0000

    Cc progress all finally glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
