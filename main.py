72a88ce634aaf96fd1760273ee52b29f38858d2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:57 +0000

    make obtain free destroy unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
