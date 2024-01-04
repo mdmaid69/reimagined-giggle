0c8120537451047af2d22fcd1dbecf948967d8bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:16:12 +0000

    crime wind Cc bury accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
