092b13ef05322d0cf97677aa171357f64e388476
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:24:13 +0000

    bend open custom worth far

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def cube_number(x):
-        return x**3
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
