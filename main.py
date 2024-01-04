0bbd941c8bbd6aa49392912c684fdf792317e068
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:47:18 +0000

    aim calloff along downstairs false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
