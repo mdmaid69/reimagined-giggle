a04c609b36723f7bdbbb9fdfca513a1ea5b2f6f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:09:02 +0000

    exact successful sudden one sport

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
