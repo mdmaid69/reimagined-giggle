4319ad1b9af009c077c2d0123f7de6398b6607fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:52 +0000

    aim alive print oil what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
