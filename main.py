e59c78aa3e3783f9888fc38bd4542c70b63d288c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:30:06 +0000

    patience and law you act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
