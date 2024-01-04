3250e92b77381763d865e0dedca00058584bc99c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:40 +0000

    mad want match Gg label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
