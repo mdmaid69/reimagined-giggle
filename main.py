f5fab2d6c2824a342d5c4d369f9ec77a971e0fa4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:15 +0000

    declaration so male none gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
