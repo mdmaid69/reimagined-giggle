c8deba707acd2fcdc4a3df447ae3b834973b72c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:09:40 +0000

    imagination place vote interview before

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import logging
+def log_message(message):
+        logging.info(message)
