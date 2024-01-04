5a32ecce0a046d759039a3851ed7f8a9ba931ffc
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:43:54 +0000

    ocean wish plant tool trial

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def sort_list(lst):
+        return sorted(lst)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
