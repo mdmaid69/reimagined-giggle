a405276eef7cc5ff84d0e254423a8452c5021560
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:43 +0000

    cruel steer talkback plan method

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
