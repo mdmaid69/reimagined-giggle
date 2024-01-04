82f768bf65b28e0c73b28e32aaf47d5d7e02c42b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:33 +0000

    turnup out sweet include feature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import sys
-def exit_program():
-        sys.exit()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def sort_list(lst):
+        return sorted(lst)
