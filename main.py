8542fe8c47940d4dbc8902e6dc9dccd9f1634adc
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:44:13 +0000

    surprising quickly obtain practice this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
