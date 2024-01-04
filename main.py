f36a81ade3dad2a7c666136fa5dab2652314b171
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:24:39 +0000

    remark once left immediately guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
