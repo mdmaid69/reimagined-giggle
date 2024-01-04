6ef90ec787ae1cae777efa24e7b2d86cda51325e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:33 +0000

    thank pin passage history cost

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_odd(n):
-        return n % 2 != 0
+def find_max(lst):
+        return max(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
