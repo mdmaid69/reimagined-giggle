4fd431aa79eeffa85b2f10b136fe10b2e5f57706
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:20:24 +0000

    suck scream disapprove sound much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_max(numbers):
+        return max(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
