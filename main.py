33253e1d221288918a2d9a56542e7ff0cdf81188
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:27:20 +0000

    yet quantity string while find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
