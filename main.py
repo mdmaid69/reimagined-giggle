5f64b7edf83f351b5a3c74fae4487aa9b37b7a7e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:55:23 +0000

    project check late judge period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
