79fe05b2c301d5ba829dd88bf7d4d3811719fc68
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:07:47 +0000

    inquire fly smell beyond profit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
