f9248ce00c9806952ee115b3da4c498a7bf01edc
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:51 +0000

    book different most bread steal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
