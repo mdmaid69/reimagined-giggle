352de6552b9073c5305c4474fa2156d8912292c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:28:50 +0000

    object mistake finger dare star

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
