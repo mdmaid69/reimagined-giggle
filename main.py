a2fa1135b3341eeb8cfecc789046bb353ef4cd54
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:48 +0000

    have brother whenever choice would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
+import os
+def get_current_working_directory():
+        return os.getcwd()
