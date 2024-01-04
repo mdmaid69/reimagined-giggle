fd8f59be15a315acf845089637034f79506e691d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:10:02 +0000

    forgive street patience accident circle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
