da4cbc1b54de1881d594319cb344af8b9ea71f14
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:43:26 +0000

    discuss race false sink keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
