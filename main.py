2c1df8a614f9eaa8fc84995bab597f6298bf4468
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:39:39 +0000

    nose when close than nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
