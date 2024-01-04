b951e37bd4117b23255fe14dc099d50be76d4fb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:46:59 +0000

    doubt pipe action cut advantage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
