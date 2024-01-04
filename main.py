f7b55c5b2f5072db9eb9a4690e4ef28506da3e78
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:49:35 +0000

    nice communication post save market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def sort_list(lst):
-        return sorted(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
