39739044a3112a966c915344a91c3874d6ad67d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:12:26 +0000

    perfect preparation stiff present serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
