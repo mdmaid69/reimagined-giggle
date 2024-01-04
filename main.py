f44f27e5ef901241e02b42c2c9684e49b7a78dc9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:50:50 +0000

    subject reject commit swing exchange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+for i in range(10): print(i)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
