ac9d5194f6b92ac3c9f5f912c38549b71848fb58
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:56:34 +0000

    present shock rich ourselves threaten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def convert_to_hex(n):
+        return hex(n)
+def subtract_numbers(x, y):
+        return x - y
