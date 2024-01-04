0a9a6b65655e7aa815c26bcbb4fe2967d64b24d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:56:47 +0000

    tall load play friendly recover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
