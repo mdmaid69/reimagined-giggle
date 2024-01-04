6d1e043c93e425638aadcd023bcefd25ffad69a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:02:52 +0000

    team favour worse keepout tongue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def convert_to_binary(n):
-        return bin(n)
+def get_array_as_bytearray(array):
+        return bytearray(array)
