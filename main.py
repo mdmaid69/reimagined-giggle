2c2bd974df330e85716d19be522d261832d65aa7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:07:50 +0000

    good turninto high original test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
