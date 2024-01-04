168cb97cfc647eed2feea10e3efbd335d5a5eace
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:26 +0000

    instead foot mean yes weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_typecode(array):
-        return array.typecode
+import time
+print(time.time())
+import sys
+def exit_program():
+        sys.exit()
