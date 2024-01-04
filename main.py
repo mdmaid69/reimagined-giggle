0e8324a5504ce76201823d55516517d015b3c141
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:10:23 +0000

    mixture almost best broken rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
