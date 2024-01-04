0b47108095333099fd2f7062725506fda3186047
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:39:05 +0000

    miss dress remain progress path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
