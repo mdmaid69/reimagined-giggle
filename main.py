4dd289a9f39a1b1273d8a731ea51c88899960401
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:44 +0000

    illness town spend collect variety

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
