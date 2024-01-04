31fc8eda9f4780dc1b3f5b7fbf57d508f859c484
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:56:31 +0000

    or holiday division cinema giveaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
