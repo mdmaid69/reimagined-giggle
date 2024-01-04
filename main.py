29c5b65ec8da9f2f2afc312c646a24f43050559c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:36:49 +0000

    minority fairly use by exchange

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
