fde719d68dea95ce5042cd60cd9eaae77d6ff7d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:12:13 +0000

    bush by copy note peaceful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
