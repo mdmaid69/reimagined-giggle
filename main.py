f6561f3436dd5373afef546376f36aa531e0aa7e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:13:23 +0000

    shame high takeout common responsibility

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def get_current_working_directory():
+        return os.getcwd()
