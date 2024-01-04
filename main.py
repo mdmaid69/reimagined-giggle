e733b711e4e8198da960a8921254809e1f4fd95c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:33:40 +0000

    even quiet want yourself telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+print(math.pi)
