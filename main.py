884b16896aea6b9b7441fbcd2e4674fc92fb918b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:54:05 +0000

    body post backwards Dd mess

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import datetime
-print(datetime.datetime.now())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
