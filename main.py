4b6dd5b063a19bd24c6c37b343896fe02f235112
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:46:05 +0000

    any wind lucky freeze argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
