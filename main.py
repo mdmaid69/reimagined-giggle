1c0aaa1884810fbc8a23fcc7f380bc7324ee5291
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:28 +0000

    earth squeeze come wrap argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
