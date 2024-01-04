b1b8c45805c8184a3c5f4c212c31f31a37227a66
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:15:11 +0000

    greeting advanced solve state maintain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sys
-def exit_program():
-        sys.exit()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def add_numbers(x, y):
+        return x + y
