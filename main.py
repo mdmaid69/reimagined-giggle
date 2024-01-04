5546330138dd452e9387ec1a12c641918090c0c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:12 +0000

    burn Uu sit represent nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
 def get_list_from_array(array):
         return array.tolist()
