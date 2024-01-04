0e532f35b7724182d9d379bb0ea5d77d730b09b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:19 +0000

    comfort seem explain aim persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
