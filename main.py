3c34145cbd601331434dd35b58fd1d2e6593867f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:29:14 +0000

    to estimate unite proper talkinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_work(force, distance):
-        return force * distance
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
