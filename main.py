647366276a027a22f4021732763c13514116e4cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:30:07 +0000

    reliable force look unit godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_union(list1, list2):
+        return set(list1) | set(list2)
