04e1858fa616ee8b680b41646951175ec0ffb90d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:37:35 +0000

    each sort rent arrive letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
