67d133f2b4eeac02f65550ccaac7660b305874ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:21:35 +0000

    female replace taste hollow clear

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def is_palindrome(s):
+        return s == s[::-1]
+name = "Python"
+print("Hello,", name)
