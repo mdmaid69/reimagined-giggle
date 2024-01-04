aa3eac90e620406a2ce451054dbba7678d873a72
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:40:13 +0000

    or contact sound connection stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
