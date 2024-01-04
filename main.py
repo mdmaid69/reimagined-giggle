edb4f1e982e2dea39bf9dfe2d1825fd39611e411
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:26:08 +0000

    danger thorough all in wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
