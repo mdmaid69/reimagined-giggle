2e6081f264bc20ffda31ca56c9ad0308ce542278
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:11:25 +0000

    prize myself offer steal comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
