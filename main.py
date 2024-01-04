57e974bb7391766b74dddb11caeefca1bbdeea05
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:07:50 +0000

    sleep suffer drop burst perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
