b3eb4d798c4d0b655fbae7df70739ee8b78a93b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:08:01 +0000

    compare meet 570enjoyable interrupt better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
