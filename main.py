6369431a1bf5f19592470c45ee7328dfe64202f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:00:19 +0000

    goout by hurt type respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import collections
+def create_counter():
+        return collections.Counter()
