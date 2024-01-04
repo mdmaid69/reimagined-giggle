c8d8181bcbf509c0b444925aad5abd983fbb1bc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:21:22 +0000

    pressure start bite towards copy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(5):
+        print(i)
