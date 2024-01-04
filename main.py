e8d8038888c28e25ee0744a1fc79ec0a228bb79f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:07 +0000

    spot children after fortunately arrange

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def is_even(n):
-        return n % 2 == 0
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
