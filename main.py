9df268dc9cae413626af1ea51da4c35aa53c6ad1
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:58:57 +0000

    of violent power joke use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
