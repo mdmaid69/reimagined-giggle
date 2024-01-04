fab2d6ed3cacf3b215432df7db3d8fc34dd4b90d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:49:50 +0000

    instant chief bottom 572enter something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
