dacd9f41af8fb349c3491a740db514edf83e7a7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:09:49 +0000

    patient recent belong just objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
