f43beb70d7578f43fae9a365e5d58edb607bffa8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:28:37 +0000

    569enjoy born chat clean ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def count_characters(sentence):
+        return len(sentence)
