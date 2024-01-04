fc324531f121d907656722d77afb77ce194a11f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:07:20 +0000

    downstairs price ill second create

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
