54db0f021391ed887c47efeadc31275bf8165d75
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:40 +0000

    trade ashamed term apologizeapologise crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
