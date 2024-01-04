52e81c693ec5cd678276e8bb60cbcfb3aaed9a5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:35:13 +0000

    regular tend true judge survive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
