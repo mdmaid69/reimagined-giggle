78ad62e0cb87a2212473a09a56999a3737078517
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:12:16 +0000

    mean thinkthrough share rich equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
