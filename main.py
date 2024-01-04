86fef09a7ac625ef3559682cad8077fb2fa2c770
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:36:24 +0000

    forward before artificial recently skill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
