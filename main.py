a3c18f6aee9f5cda890a8cb15501f5a2a67e406f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:09:38 +0000

    recognize empty scream teach accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
