06a5e46c08852b67e9f3c41a6eb9451f8f1d8fa1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:32:06 +0000

    relative nonsense dick moment getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import time
+print(time.time())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
