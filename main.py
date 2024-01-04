f802e3872c525ff4f8c561fb019097b52ceac109
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:03:51 +0000

    quarter unpleasant creature previous strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
