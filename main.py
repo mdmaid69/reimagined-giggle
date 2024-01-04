4c4a5ea39bf7217cf0826cac1dd376d127f55b40
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:24:21 +0000

    he week tap sting hand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
