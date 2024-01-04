2b2ac0af557bac5a4c9cdb02c26ff41224688030
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:08:04 +0000

    splash pretty low estimate bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
