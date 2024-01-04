76365d6d40dd638417495a0c4d36b21130ca8c92
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:26 +0000

    machine scream welcome interest rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+x = 10
+y = 20
+print("Sum:", x + y)
