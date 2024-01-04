a59469a1aa31f61a838637f12663ced62c153e82
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:58:45 +0000

    surprising influence birthday broken ourselves

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
