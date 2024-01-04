0c5816a7d50d34f1bfee22c30d74e4c8075adbc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:02 +0000

    heavy speech interview hospital operation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def is_palindrome(s):
-        return s == s[::-1]
+  def sort_list(lst):
+        return sorted(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
