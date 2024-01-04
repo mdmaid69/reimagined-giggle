4a04c17ba12c640255afaafc0d829e53f960f1ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:01 +0000

    government right young settle comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
