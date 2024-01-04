291d92a31ed28b2aef29a2524a874c0bad05d1eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:07:00 +0000

    acceptable track absolutely with central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
