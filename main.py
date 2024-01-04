156bf1a9aca4571246f1efa94da7794dfb97f0c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:16 +0000

    holdout clear them row neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
