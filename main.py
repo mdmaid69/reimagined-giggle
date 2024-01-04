ecc7228da413d9c1bd514226783d6d8f23077755
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:56:43 +0000

    advice desperate pronounce new view

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
