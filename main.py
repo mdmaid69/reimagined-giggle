0ecffd2ff8c2ae88b2051f5f9b6caa2aa9bf294f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:50:53 +0000

    scream attempt treat zero early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
