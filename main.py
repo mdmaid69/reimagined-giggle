694f418ccab32aad4706c828247e26e24aee5d16
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:29 +0000

    uponon nature uniform grade belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
