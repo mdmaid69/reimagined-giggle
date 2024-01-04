057b3c2fe2534545473b3df7ac4ae752eb27a259
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:52:47 +0000

    partner quickly left sad rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
