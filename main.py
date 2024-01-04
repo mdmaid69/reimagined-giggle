13864b46ecddbd5ff525abd87196e33281691fe6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:14:32 +0000

    Ss large tend cost someone

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
