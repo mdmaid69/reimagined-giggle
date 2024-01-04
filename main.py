61431e4f040684709ee90b2cf4d458765c0dca57
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:52:44 +0000

    pocket neither clever tape church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
