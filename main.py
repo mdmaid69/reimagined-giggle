40d59fcb068332e49da4ca7813e56b021e559562
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:46:25 +0000

    569enjoy shock if accordingto trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
