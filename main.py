51cd5b028d02d731458b98e591d0b508ff10e676
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:21:33 +0000

    give rush shape neck cut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
