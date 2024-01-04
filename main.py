6241e3830bf301b52e34c84098f53eebb3bd93a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:04 +0000

    find progress every office lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
