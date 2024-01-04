1f7f70e95b725f1d9908030e267b987e09dac5cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:02:41 +0000

    foreign model giveout partner learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
