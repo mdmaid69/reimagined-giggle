eeb4d2264637977bfc733b9480dfe0d279672e54
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:40:09 +0000

    hospital second private mine book

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def square_number(x):
-        return x**2
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
