d7bd80f344763e653fc513deb0484c1c6fc19385
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:15:44 +0000

    contrast cover need closely face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
