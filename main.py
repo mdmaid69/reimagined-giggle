04fdc263c0f5cf54885242aa7e3bfb03e25bfb35
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:20:30 +0000

    before whole hello easy disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
