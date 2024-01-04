5d8d94bb7481c5efd4ff5813944ac899786232a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:50:51 +0000

    lend boring lesson downstairs afternoon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
