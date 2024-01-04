32bdc7b20546c133cc604116296005317f004f7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:27:19 +0000

    eye trap talk slowly accidentally

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
