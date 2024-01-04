63ddecff512c559f2602b8ecdf33f0bd3fbb584b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:03 +0000

    heat angry cover summer past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def append_to_array(array, item):
-        array.append(item)
-def convert_to_binary(n):
-        return bin(n)
+def get_array_as_complex(array):
+        return complex(array[0])
