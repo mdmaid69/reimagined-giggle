958a28fca04abb268e06d8c5cfaecda192faf6ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:17:42 +0000

    develop Tt invitation voice undo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
