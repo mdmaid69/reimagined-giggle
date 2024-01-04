4057a3062535d820896253823d121a7fc374bfba
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:16:31 +0000

    might choose pupil pinch dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
