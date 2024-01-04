e28405c91976750bea9e9ceccc866287b5e7eb99
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:12 +0000

    post second design feel slight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
