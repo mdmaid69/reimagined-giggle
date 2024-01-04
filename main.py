5fa5055eb4e30df7471f12c95b8714af19c497df
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:43 +0000

    march holdout everything whichever lock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def subtract_numbers(x, y):
+        return x - y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
