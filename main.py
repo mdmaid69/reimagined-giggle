97111118a18572979eb8a19306a195a10d932963
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:20:33 +0000

    hit mass consider stamp after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
