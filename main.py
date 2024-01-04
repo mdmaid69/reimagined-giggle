7cb13edba9b36d30a22b89180568947073dd6819
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:06:17 +0000

    record suckme run material laugh

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
