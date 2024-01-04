6cf7cc40fd8001bee7204ca73e2c9a0257f87ac5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:22:18 +0000

    sale number exactly keen plan

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def create_stack():
+        return collections.deque()
+def divide_numbers(x, y):
+        return x / y
