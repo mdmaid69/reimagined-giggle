f5d0244cd39fe052a90d27902be9b65a56968d04
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:12 +0000

    know comeup last today capital

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
