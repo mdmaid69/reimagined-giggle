23ac768ee5fcae3aecc2d0f2730e8cbfbe7b3ef5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:42:45 +0000

    gift wealth you shine giveback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import re
-def split_string(pattern, string):
+def split_by_pattern(pattern, string):
         return re.split(pattern, string)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
