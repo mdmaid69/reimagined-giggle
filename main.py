4a26e7d58ae6c9c82703c1fb9e22347ba4fedfa7
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:51:14 +0000

    again contribute smell out repair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def count_elements(lst):
+        return len(lst)
+def calculate_speed(distance, time):
+        return distance / time
