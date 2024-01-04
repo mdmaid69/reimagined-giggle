5b5a03ceefe4d30def9637bf68bd6e49046a4b11
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:39:52 +0000

    few holdon love best pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def get_current_time():
+        return time.ctime()
