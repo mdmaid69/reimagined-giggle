ca3e3d89ade8988215e7c606dd5eb21129038809
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:36:48 +0000

    hurt receive paper fasten heart

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
