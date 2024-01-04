7cad1a5196f917955d8eaa533d225a92ddc7d250
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:32:57 +0000

    else disturbing piece realize rob

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def convert_to_hex(n):
+        return hex(n)
