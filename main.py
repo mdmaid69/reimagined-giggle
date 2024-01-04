04f496e105f8704f2f2d092fc70f070b4dd94f1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:03:00 +0000

    weather forever fly worried report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
