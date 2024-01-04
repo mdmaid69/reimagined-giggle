6468b21762d3e5b85f4065de0319cad3c862449b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:19:21 +0000

    else slightly threaten soil rubbish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def subtract_numbers(x, y):
+        return x - y
