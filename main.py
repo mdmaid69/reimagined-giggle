5fec4afaec9cb2909737cd425cafeed90d2358ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:53 +0000

    connect success trade disturbing tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
