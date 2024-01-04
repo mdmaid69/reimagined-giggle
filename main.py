2781c00fd14cb446d8c975174da1124fa1a2d8a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:43:52 +0000

    address whole total season much

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
