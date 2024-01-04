8654a6adab391fd628b42a30a619f91052f45ba1
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:04:23 +0000

    hard break tour this scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
