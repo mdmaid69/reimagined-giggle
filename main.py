1cd06793e6b27696043ac73f7d8d30b279f63a27
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:49:47 +0000

    inform activity close also wise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+for i in range(10): print(i)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
