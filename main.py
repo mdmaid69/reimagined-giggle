9e7d2307f1e286bef427df7669240016d6e4e013
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:15:21 +0000

    reduction half major than Zz

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+print(re.match("h.*o", "hello world"))
