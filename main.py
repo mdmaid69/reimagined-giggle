995b4008185a95a1570bd435ca3905d120280432
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:30:37 +0000

    evidence whether taste male takeoff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
