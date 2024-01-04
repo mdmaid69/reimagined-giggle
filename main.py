034e3c86f31070730a3175dadd7a086c87e71705
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:34 +0000

    thought he tomorrow view school

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
