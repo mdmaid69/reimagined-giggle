d50a228c3fd8a41e6d82dc658e359e8995807361
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:42:01 +0000

    finger explanation deep rule event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
