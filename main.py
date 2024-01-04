dc147b5adbf1ef19ea170982e535c2f8cb793702
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:09:47 +0000

    risk formal play marry pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
