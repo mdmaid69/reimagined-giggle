fc38277ba5bdd41f9af9d74e1f0fb54726e275e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:54:09 +0000

    let rule neither group pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
