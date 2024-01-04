f68b14d6a2e6ee323ce3c5b35ea3200f84bd0c1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:29 +0000

    accident job beneath down display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
