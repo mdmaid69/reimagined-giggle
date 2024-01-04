644cbebac61ba71c9977bdf922cea56da4946de8
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:38 +0000

    anywhere nature shadow broad give

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import time
+def get_current_time():
+        return time.ctime()
