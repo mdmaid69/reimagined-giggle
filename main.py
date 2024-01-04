0453f25d3241c622cdf46f7d089ea3abda85499f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:37:51 +0000

    private score successful woman official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
