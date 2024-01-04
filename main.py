3910a73d026d966a708a0290a48a02367c5563c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:40:54 +0000

    object breath middle reach persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
