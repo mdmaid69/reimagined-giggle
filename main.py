a7a51b3bf6163fcb98134cdcac1e9c0b2d039fee
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:01:44 +0000

    happy coffee wave drop arm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+import sys
+def print_python_version():
+        print(sys.version)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
