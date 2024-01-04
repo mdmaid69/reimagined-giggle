4affac9f48c5cedd6a8ed32ee46f89afb0a7970c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:09 +0000

    compete neither bother boil familiar

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
