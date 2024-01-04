c102852545362e3a1ae15f858dddb35ac82282b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:23:05 +0000

    wheel hot climb want godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import time
-print(time.time())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
