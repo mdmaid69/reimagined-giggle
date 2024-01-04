971c535d93758ad9dd3035b1dc100ce100fdb690
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:51:36 +0000

    goout much stupid which date

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
