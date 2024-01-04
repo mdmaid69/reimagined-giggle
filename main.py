395eea86ca94a40f0c942c33663dd3b98f0396d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:14 +0000

    concentration about spill respect medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
