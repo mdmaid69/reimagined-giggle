94c694b0cea5d53501b5587ad8f02deb18944256
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:45:27 +0000

    cause still debt commit payment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
