302293baa8db420dc27d6b4a9faf01a5d574786f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:00:53 +0000

    help runover correction turninto party

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
