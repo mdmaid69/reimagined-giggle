1561397703b2570f702cd00f79c332e9287027f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:43:09 +0000

    suppose soil form operation accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import time
-def get_current_time():
-        return time.ctime()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
