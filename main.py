489f269eaf8a36c4855a616400d41afb4b5470a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:23:24 +0000

    innocent go head lookup post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
