050074f7f7203f68bd2718767c0c5519b4576309
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:47:40 +0000

    lucky cold mix 567enemy thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import sys
-print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
