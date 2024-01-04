55addf165c12e0ddd83e3011a6a8c0f10f7c3169
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:17 +0000

    range write label air front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_speed(distance, time):
-        return distance / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_sine(x):
+        return math.sin(x)
