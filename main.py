790e9de1a45ae76c3f7e2d1c17849493dc59f0c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:19:50 +0000

    busy near young autumn year

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
