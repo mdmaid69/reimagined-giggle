915730ea8a064eb38c863122836950143a5993c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:02:49 +0000

    wild type classroom thinkof human

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_max(lst):
+        return max(lst)
