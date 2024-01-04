de3fc4699d217ef523643f71650ca1a01a4f7f84
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:42:18 +0000

    flood regularly hurry surface beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import sys
+def exit_program():
+        sys.exit()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
