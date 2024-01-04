0cb247b7448959246c5d105a8f866d89789f3786
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:43:56 +0000

    affect branch keepaway interesting someone

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
