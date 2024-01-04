a1a1bc261b04820daf07bd7f1ae71dac475b73f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:38:19 +0000

    priority operation interested country 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
