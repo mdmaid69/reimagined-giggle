91547a3508880507d20d52b8894b94533382a55c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:55:29 +0000

    lose otherwise shut light partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
