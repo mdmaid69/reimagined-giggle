9b4e2474b5580dab56a40387062f01d17202a263
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:51 +0000

    record confidence tend listen bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+from collections import Counter
+print(Counter("hello world"))
