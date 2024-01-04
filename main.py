cf73d8713ef438f8b143ae4390bc24a5a5088630
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:25:03 +0000

    keepout finally now not until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
