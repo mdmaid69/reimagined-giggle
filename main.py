657ddeaa64d8464f21d551e117b56f39735da951
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:45:35 +0000

    back strict disapprove crash bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
