b8c143a993500f439f5143a4779a555a0d1def53
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:20:52 +0000

    tough try protect impatient suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
