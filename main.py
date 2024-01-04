9e8288eb6d1ec1a2160d70d645ca8b41b5988364
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:21 +0000

    small waste excellent visit list

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
