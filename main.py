3b5a25dbd34d91ad544e04376e4af4c500b728dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:38 +0000

    confusing mention contain this may

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_index(array, item):
+        return array.index(item)
