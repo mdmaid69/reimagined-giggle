2d0b56a338872440cd9581e8016502a1d91d52f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:09:50 +0000

    rather tape objection underneath apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
