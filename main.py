a7b6110e2f6b9c28ce15e82e51b09cdc4ac425e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:44:14 +0000

    comeout long admire carry sick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def count_elements(lst):
+        return len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
