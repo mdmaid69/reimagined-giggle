8ba854c4a57238716593a14e365d8dddaedf952a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:27:30 +0000

    total through neither education travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
