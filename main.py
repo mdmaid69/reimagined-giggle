e1a57a38f223857a07050c2c4488e62e380bcbf7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:28:31 +0000

    major sale calloff action influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
