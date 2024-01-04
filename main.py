b0de09ae26d62a9207fe3840fa4fcced278ddc76
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:51:49 +0000

    actually many expression cost go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
