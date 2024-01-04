cc4e0eb1d7c3c328d3904535c2d8a6aa051d12ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:59:57 +0000

    short reduction clear search patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
