1d537c47f110802108bd6ff92757fab378b8365d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:58 +0000

    pile exciting explanation finally work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
