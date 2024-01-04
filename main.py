592f327da83462128dc9c922ba726981693d155e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:10:53 +0000

    extraordinary stir false trick exciting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+print(os.getcwd())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
