7fbc2def785c4a485b158516464dc661cb5df0b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:45 +0000

    blow round system interview know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_tangent(x):
+        return math.tan(x)
