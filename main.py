2036a4d2c26ca970e0bef428eb6649f7515fa77a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:49:47 +0000

    distant cum punish among 565end

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_list(lst):
-        return lst[::-1]
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
