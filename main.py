f05558b9cdd1ea9de268043347326ae1b7b190f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:47 +0000

    side wind unhappy subject often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
