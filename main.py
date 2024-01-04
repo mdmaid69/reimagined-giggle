63237fd51dfc7a792f2737d08e7a5f9b9ff1551f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:04:10 +0000

    foot force trust manage fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
