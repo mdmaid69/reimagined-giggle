f60f96bb2f770cb71aeb24c6fb41505e04b48678
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:46:07 +0000

    anxious interview eager weather chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
