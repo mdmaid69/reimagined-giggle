5ade2fa9fb9c3ccfdb48fdeaa2f5b41aea1b23b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:06:50 +0000

    answer beautiful lesson through actual

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
