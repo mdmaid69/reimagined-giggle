4a1e75483406653c562227cf36e9ff0404a88866
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:18:29 +0000

    eager now emotion quickly frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_item_count(array, item):
+        return array.count(item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
