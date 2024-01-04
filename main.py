5c5724bb7a26bb7dd8b1d69b03847f2f55f12cf4
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:10 +0000

    month meanwhile organize occasion exhibition

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
