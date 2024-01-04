949988f5f5b46e052a3a71a02ce5a9ca96f78da4
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:44:27 +0000

    idea for stare empty delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import time
+print(time.time())
