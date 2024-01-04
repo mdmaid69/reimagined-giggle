cc2de05d122f1441167420fe91886edce589241c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:10 +0000

    workon drive benefit gate thumb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
