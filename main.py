3cfa176971079619a0f716de80e0a87c98ad7ee4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:29:54 +0000

    eager each think most evening

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
