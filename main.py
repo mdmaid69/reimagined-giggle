d1acc294dd88e1b2978b68c544185b6809dd46b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:34:12 +0000

    mad there all thanks suit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
