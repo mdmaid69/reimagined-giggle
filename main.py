f0e3290112e82fb382e018c461dd3929ca4a595f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:01:42 +0000

    science Gg turn child battery

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
