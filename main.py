b5667d343281d9d7c4132a770dd98ce7e2990819
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:05:26 +0000

    cruel case gap deliberate principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
