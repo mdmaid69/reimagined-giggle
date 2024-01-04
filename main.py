147cf934bff638be9a7fa4a24e9cacdba8030f58
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:06:47 +0000

    brain apparently offer region wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
