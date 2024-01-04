eba09f246f256b3631be20a7f4df9f01fa9923b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:16:16 +0000

    display undo push considering share

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def convert_to_octal(n):
+        return oct(n)
+print("Hello, world!")
