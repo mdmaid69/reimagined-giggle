fecca90343234e8e66f15a5a6d341f99be16d444
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:50:20 +0000

    throughout patient balance flat turnout

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
