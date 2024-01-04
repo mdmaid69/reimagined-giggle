64711939fbd9ddb13ac8fbcdf7aa5ec8861163f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:02:16 +0000

    bottom talk intelligent necessary handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
