1abe75b622ef4269a0909e2ec6d4ae0f6c49a370
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:29 +0000

    unkind natural wing professional acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
