c2541859484a8da393ddbfe76928eaa2f22381d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:22 +0000

    fall across apply trial turndown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+print(time.time())
