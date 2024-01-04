fb2f33394a9ae0a3c1200611305584e4d93be456
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:22 +0000

    appeal true absolutely building getoff

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+for i in range(10): print(i)
