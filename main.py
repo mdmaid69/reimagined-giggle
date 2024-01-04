ab297246fd26bef98f5b1956f265628cdd55fa5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:47 +0000

    lookback(on) between university boring concerned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
