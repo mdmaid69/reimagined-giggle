5f244ebad0382a92a067e04dde5740d97d94b8c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:02 +0000

    anyone stick duty apparently exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
