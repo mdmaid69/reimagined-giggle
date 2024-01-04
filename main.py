29ce6cb6ff38dea17aa499581ad5a34177b3d7db
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:15:53 +0000

    house slight expensive design round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
