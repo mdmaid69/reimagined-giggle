f3e58f2ca5b2ddb582f5430fd4b6f2bc3035f58f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:35 +0000

    future very recent cost sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
