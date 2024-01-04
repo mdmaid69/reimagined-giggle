e48a5df86d5d4a4e5d55063b76405a8f87197ef1
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:11:56 +0000

    convince kill could scrap by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
