60cdf58767eb129a33c89be656866aa6f9b3988e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:13 +0000

    successful turninto pupil joke leave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
