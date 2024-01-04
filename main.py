cecc5a7477f44e8d388ef20d75f1b9893be9347a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:51:37 +0000

    beautiful majority recognize whichever aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
