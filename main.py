f6b975b7d92ca1b0cd91699ed6ca09b3b01aa962
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:56:38 +0000

    congratulate related create test square

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
