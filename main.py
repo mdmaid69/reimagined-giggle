763ce1f00d816c7f85dc442444eb2e160994b9ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:03 +0000

    coffee suggest tooth getback embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
