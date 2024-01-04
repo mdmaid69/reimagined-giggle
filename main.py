200f94e4a6ffe6e51a993544dd806860a79124e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:30 +0000

    fast callout emergency wish seem

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
