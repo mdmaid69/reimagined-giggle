fcaf015d2c27a4c2660dd9b62a5c631b163a7df8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:31:58 +0000

    deaf wake key stamp child

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
