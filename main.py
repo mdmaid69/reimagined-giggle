ff49ccf5c13884bbfd6bb15f7bc061b33551cf11
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:10 +0000

    slice row present herself charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
