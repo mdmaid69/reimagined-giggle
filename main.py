674bfb9784e49f94a169bbf6ef69fe37c512ee36
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:52:48 +0000

    public class dream many callup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
