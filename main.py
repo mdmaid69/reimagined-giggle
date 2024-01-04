8f763feeba9afbf6ed34a17f249a6e5e54bb0fe0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:24:52 +0000

    juice profit passenger considering crowded

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
