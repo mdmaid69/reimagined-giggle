18047c23e46c11b1f2937c0e847fd886e68b1b0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:58:38 +0000

    reduce office patience away unit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
