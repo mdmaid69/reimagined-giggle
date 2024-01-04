5395f4117cbafaccca942a504da71ea0fbf342cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:10 +0000

    slip comedown hot point professional

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def cube_number(x):
+        return x**3
