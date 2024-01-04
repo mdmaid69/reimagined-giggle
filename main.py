67e8d54c4a76de13508fbcf719e94c56b1727bad
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:27:05 +0000

    occasional high sing freedom beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
