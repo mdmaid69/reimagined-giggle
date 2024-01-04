dac25f30ea19f5bd8cd7e39b6c6a7966161ac3ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:42:36 +0000

    possibly less crime strength but

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
