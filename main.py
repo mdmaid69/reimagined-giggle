328f180f7d3f8b71afa89e8a13b8a1a78adef1f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:33:02 +0000

    deep responsible home him cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def is_odd(n):
-        return n % 2 != 0
+def pop_from_array(array, i=-1):
+        return array.pop(i)
