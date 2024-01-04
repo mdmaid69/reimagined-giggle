13f51816688ab5ad9b8aaf40a5b7cf7f94fafbfa
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:20:58 +0000

    estimate confusing rest onto 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
