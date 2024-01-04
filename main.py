1f4368486e69c235bb9af97b88786368c5132933
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:41 +0000

    read apparently day mood meaning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
