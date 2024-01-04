804c19b73ba5cbd020ce0f3447b9d2f18058fd45
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:44 +0000

    effect meet fair war flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
