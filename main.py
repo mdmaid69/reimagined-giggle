2d3dba301369dedf3d5044653127f8f0331e2743
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:35:03 +0000

    brush takeover among kind flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def is_odd(n):
-        return n % 2 != 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
