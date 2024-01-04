ed4c6ddbf34ce568b4b6c4d169cc429a5641fd64
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:56:54 +0000

    similar distant wheel shoot exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
