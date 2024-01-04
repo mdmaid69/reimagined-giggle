8c80344fc760e7a8b1af7d4aa0d5f6e13dc10a0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:05:34 +0000

    measure die arrive wire connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
