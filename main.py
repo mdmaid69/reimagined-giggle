f736b7a014c351221c9bef0c68f9a03c6ff8808c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:26:15 +0000

    extremely evidence excuse church common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import random
   def generate_random_number(start, end):
         return random.randint(start, end)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
