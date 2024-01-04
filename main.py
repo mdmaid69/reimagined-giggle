b8f86da23f7f281db871eb722d4310e6c48a7389
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:39:42 +0000

    introduce criticism bag general seem

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
