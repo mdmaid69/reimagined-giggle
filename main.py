37c2e2a029d330cb96abab429ba4fe3b23690e51
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:20:38 +0000

    once power unusual official crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
