12fbf78af57fa6a8690cbe64a34443b20bf75d5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:28:13 +0000

    congratulate edge area mainly pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
