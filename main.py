ba371cabf07a607a284cf840a07b396c4c1f3d08
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:55:50 +0000

    official few judge region plant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
