4dc4e00f6a5b6ff85556bb0d3790c9c5e2a8e976
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:06:45 +0000

    building change terrible possibly accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
