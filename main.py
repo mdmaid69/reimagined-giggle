1e7330b98d7a62c32302de84a7cfd4b735c31a1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:02:02 +0000

    water wrong 568energy declare perform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
