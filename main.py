a3fe43361e4911596dd642d0864cb4b584e1265c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:02:57 +0000

    Uu used read relief roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
