979ad3a0574b5835059662dd13f8e36572cb7db0
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:28:33 +0000

    mean complain often tie fresh

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
