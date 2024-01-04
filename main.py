8d3d5a5eb789fb7eebd5bab7c5e5743cae215bef
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:52:29 +0000

    order between record trust borrow

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
