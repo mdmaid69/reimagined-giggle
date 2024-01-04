8602e71c43bcbfedfc151a41b5ef5e26c48516c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:48 +0000

    region responsible sensitive each wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
