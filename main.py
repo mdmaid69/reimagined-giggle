16498e68c1158ce190e27422b593d1545cb5d1d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:16 +0000

    short 567enemy long rarely buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
