4df644e8c74c3c5a63c7e7e0c5d32034df7e973f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:04:03 +0000

    dead mention sick shelter tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
