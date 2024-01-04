5aa4b1ffe46c882c9115baac22d54a1c85f9f180
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:58 +0000

    shout light rescue soon warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_power(work, time):
+        return work / time
