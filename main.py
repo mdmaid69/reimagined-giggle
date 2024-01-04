9ebce8ab7e662692f025caeec2c02d35e073ecc9
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:17 +0000

    stop equal recently change comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
