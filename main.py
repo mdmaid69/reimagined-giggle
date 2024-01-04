08bdf70b4e573ce765b29ee726a2d431cf3060e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:26:34 +0000

    window blind use comedown shade

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
