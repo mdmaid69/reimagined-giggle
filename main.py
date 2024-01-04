2507083b8f6b8178b0b04bb7f7a070d5a9cb36ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:42:51 +0000

    earth majority problem door remember

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
