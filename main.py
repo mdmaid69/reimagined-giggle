10992f6400cf85cb1c60db164d4a1512035057ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:47:48 +0000

    stiff control machine distant pronunciation

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
