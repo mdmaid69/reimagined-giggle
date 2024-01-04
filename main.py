f36f14453a693f7dc62d6d1e8bb1fc99dc843993
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:32:18 +0000

    fat string answer attract agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
