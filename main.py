7f89045a0afda78cacdb9403b852544c3171b4a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:50:32 +0000

    argument chance rain better rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
