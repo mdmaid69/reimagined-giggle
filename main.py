7958d90031b841c4e4811b197edcee1431cdd753
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:31:07 +0000

    light summer question attempt either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def add_numbers(a, b):
-        return a + b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
