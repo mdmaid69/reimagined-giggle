54afb213dc62e35b548005fda92e93bb4d5da370
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:00 +0000

    owe whole weigh tax dear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
