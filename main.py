d41f0ca81bd0b1a27581fea550a5f5a1a7cba8e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:00:44 +0000

    occasional always laugh give man

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
