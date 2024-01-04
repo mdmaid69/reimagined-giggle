8bc1a8092c6560df927c81de665f8a855171bffc
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:11:17 +0000

    dead tidy case narrow post

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
