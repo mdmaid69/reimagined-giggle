afeb00642b874b54e53891d3def8ff1d92e1bd9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:57 +0000

    lookback(on) bye silent her together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
