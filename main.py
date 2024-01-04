65af5d9d08bc2d77fccd352e028cdc08c56260eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:23:20 +0000

    taste this their regret keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
