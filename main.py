0870df485d1b431d92e7298856cb59a6da307778
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:48:41 +0000

    offer week possibility listen power

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_volume(length, width, height):
+        return length * width * height
