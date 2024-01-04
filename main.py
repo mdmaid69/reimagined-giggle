cf8dca3b3411f44c3864561150e544cb6b7fa447
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:53:45 +0000

    government mind the convince challenge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
