074c1e1a9255084d83b72179c2462a4742a7dfdb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:29 +0000

    store movement choose notice wish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
