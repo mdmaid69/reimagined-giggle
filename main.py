bbd92eddd16500cd0070fab9ebc78639db100c12
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:12:46 +0000

    shop Cc petrol concerned through

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
