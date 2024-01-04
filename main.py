68407073861261d57ba47f94d24c3c24d9c76c11
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:34 +0000

    hungry objection company key approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
