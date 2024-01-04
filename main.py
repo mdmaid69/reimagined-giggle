2071ced65821430ff78f95e56e59f5356641ceed
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:05 +0000

    image practice event exciting trousers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
