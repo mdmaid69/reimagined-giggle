6450bcce3c1f3142ebebbfe32854cb509415196a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:54:28 +0000

    completely holdon guess comfort clue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
