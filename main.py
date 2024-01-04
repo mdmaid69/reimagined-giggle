3e78a33d6bd33a43bb0f9b6073a2aa5671ed7424
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:23:09 +0000

    snow tall through between shop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_bool(array):
+        return bool(array)
