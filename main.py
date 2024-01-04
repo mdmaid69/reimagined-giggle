041f8df1b3ec3b13f79fa89cecba8963a56ca5b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:07:27 +0000

    breath reserve speak holdup only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
