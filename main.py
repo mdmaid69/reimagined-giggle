25ea575724e990f868d5ee67107c2e161f77c755
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:30 +0000

    right ordinary his fence intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_words(sentence):
-        return len(sentence.split())
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
