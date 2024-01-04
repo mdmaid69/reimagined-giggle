9e6e86ce98a9cb2a71b789b067949c0340b7beb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:50:37 +0000

    snow unlucky whoever tread sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
