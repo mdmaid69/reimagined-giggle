39a491190ba930333c336a6408ac9a93d69295e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:22:46 +0000

    backwards patient apart modern recently

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
