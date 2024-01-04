9903a1c8d8148ec02171e83c2bfdf0ac697d90ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:17:54 +0000

    impressed some lie proud slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_item(array, i):
+        return array[i]
