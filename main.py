6d4126bfb283499ccaac1f707070f8909fb009a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:57:50 +0000

    rest kill home community slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
