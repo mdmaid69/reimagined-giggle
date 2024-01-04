25ec9af8ced8dcd1d1465b0f289767c726bbe298
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:49:57 +0000

    tour deep fact win stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
