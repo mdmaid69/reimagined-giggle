0e8b11c2f39a5d50b066b78b59fc1d5bbb3e0095
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:30:42 +0000

    bin poison note schedule tell

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_binary(n):
+        return bin(n)
