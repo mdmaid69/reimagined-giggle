49f6cc262415b53526794a272aab00a9147eb8c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:48:40 +0000

    great regularly whistle getout consult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
