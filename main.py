2a37913e567769a8e3f244d7d1155043764c76c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:51:06 +0000

    interested natural vision date takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
