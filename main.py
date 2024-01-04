67ff5513221e5d3542aa179bd82f3c5bb890691c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:53:25 +0000

    soil criticism murder tomorrow walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
