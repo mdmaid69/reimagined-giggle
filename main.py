a2dc706ef97df4b82444a35b237b6b36b05521ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:00:51 +0000

    present regular trick gather form

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
