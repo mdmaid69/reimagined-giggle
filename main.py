56c0276b250bb7bdd964dc1c0702d9332d29ff6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:14:17 +0000

    lookback(on) preparation left word average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
