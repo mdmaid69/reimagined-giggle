16ffcee2e0902ca93d3b920d91163523dfc52f60
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:59 +0000

    often entrance giveup mean lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
