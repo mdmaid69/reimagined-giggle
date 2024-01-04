22f95bc1b8b7974a0c2815e88a15dd421805a477
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:03:42 +0000

    trust treatment false flood callat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
