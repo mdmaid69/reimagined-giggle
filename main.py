7a65f8f0c917165e125e983f2e94535eb836ba08
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:17:16 +0000

    name brush job deserve they

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
