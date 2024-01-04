6132c481a756dd963fd891d87b94b685da92c6cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:32 +0000

    Ee curious minute radio brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
