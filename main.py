56139f66e12a2fbb2a7b6c6262c8cadbf024f1a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:53:46 +0000

    family temper sea round play

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_work(force, distance):
+        return force * distance
+def find_unique_words(sentence):
+        return set(sentence.split())
