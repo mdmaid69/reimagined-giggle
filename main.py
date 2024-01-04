0fdfcd09ecb7b84368124e613355c9f70c4a73f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:57 +0000

    fork nearly runaway change congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
