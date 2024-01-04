ba37eef98168f2be3e09428ed2e375e5a0387871
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:10:00 +0000

    responsible design equal exit clear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
