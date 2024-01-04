fda1297e6eafce987ae15044fd9f61c3640b152a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:36:06 +0000

    opposite battery kind nose herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def delete_file(file_name):
+        os.remove(file_name)
