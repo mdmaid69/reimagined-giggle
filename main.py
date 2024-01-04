add3d24f85866fab0dfa98c44ff9470b1d2b47d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:13:42 +0000

    everywhere guess hope workon key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
