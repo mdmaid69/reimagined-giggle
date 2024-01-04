0724897a66d8a0569caa83aaf356fbf8dbc9f72a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:28:03 +0000

    whoever balance butter connection back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
