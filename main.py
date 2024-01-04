cf6b1afa6823079d5e51a1a08c1b92e0adcf9c3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:29:24 +0000

    pride Itself pick keep disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def cube_number(x):
+        return x**3
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
