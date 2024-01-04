1cf1c9f27b093f42d5af50c48cc45fc35b46fd32
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:09:25 +0000

    Cc congratulations culture business artificial

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
