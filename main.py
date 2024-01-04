94685e14d822e9efd117a6d42aaa0385ed6b42a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:25:57 +0000

    bright ready gift comeup thin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
