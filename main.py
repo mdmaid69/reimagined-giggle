80afc5d7ade4e9d76021fec8fc2a4fdf2777e53b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:52:39 +0000

    turnover determined try challenge visit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def append_to_array(array, item):
+        array.append(item)
