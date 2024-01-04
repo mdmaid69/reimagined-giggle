ba1db9bd870f9bb6b75d2bdfa5c610d122beadcb
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:37:08 +0000

    life general look present call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
