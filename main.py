fc6e6f444d13abe1635cf0dfff3590f521bb2f23
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:51:52 +0000

    sky chase bear there number

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
