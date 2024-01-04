e698da72431583efd6fe702a6a69b6f49944c553
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:44 +0000

    watch nose advise during long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 def find_unique_words(sentence):
         return set(sentence.split())
