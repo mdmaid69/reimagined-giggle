b5e3293e9a5ce94be3f5d04a036175c1f1589059
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:46 +0000

    die pressure callup dream under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
