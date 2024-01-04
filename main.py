49d29f7555db209b0f8ec506355e6258e1a3c3bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:24:53 +0000

    runaway mark eat ignore leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
