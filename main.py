1a5c84d28f855c9c23d950df256feea3d703f23a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:28:18 +0000

    house apply hit succeed hang

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def cube_number(x):
+        return x**3
