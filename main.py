15daeba4a1d9873c963b8580c2c0806a44267b7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:03:21 +0000

    think Qq back brief entrance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def get_array_as_float(array):
+        return float(array[0])
