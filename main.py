e7d40f625931aa8d1fa5e3cab498847f3505f811
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:41:32 +0000

    moon object concentrate breathe area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
