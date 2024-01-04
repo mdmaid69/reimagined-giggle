8319ec962b80ebca64206e5409f6dfc2b195dbca
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:00:34 +0000

    manner just street muscle lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
