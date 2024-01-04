44f19d5ff0a3bb706c84b8d019e5cba9e25200d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:41 +0000

    exciting pull children start taste

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
