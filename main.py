828e3e098a1c8f51663e221d1b2fe3095b62eca7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:36:00 +0000

    exciting loss friendly while operation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-text = "Hello, world!"
-print("Characters:", len(text))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
