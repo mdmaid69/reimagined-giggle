b1307aeffd8b9e187c8ebb676ab32df19aad1013
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:31:03 +0000

    both trick type coat connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
