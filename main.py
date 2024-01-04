ca3fe7bbb022d5b3253bdc33c91be1b1d10c0f94
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:23:24 +0000

    save motion needle except force

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
