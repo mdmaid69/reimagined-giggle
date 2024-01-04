2fe5bb5a252cceda25153c67e126c05e82d1d049
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:52 +0000

    almost market either present angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
