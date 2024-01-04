2d1944c507d7f6ed990f47a4eb9f2db0e03b75fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:40:45 +0000

    comfortable breakthrough our key talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
