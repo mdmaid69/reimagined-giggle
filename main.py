70d2aebdbe08308574f90a0fb712ba69d48db0c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:22:01 +0000

    power general together machine worst

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-print(sum(range(10)))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
