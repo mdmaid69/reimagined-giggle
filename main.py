fbc0918866139cb22edafff1aac9d785bda4cb55
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:54 +0000

    ever Qq emphasize contain yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
