e04a76972be85fb2634fd7aaf6c8af686b3bdd07
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:25 +0000

    custom against bet inquire area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
