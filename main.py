d84a3b44da588cdab76c6ebf526168a32dc654dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:56 +0000

    exit related share unfortunate house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import sys
+def exit_program():
+        sys.exit()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
