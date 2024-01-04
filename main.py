cd05f85e7ccfb6840528e882f262a732c61c43da
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:51:31 +0000

    pass crowd early print high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_set(array):
+        return set(array)
