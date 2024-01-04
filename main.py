a7d20b8c6b20a5f94993241d9b5bf8e8a31e0201
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:23:42 +0000

    band flight excited intelligent bread

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_list(array):
-        return list(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
