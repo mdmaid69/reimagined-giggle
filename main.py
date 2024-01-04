c7d1b0fb31cc18afa7013322847ea843591da27a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:46:24 +0000

    set both tap traffic black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
