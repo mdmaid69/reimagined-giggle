ef5a740c1eebf8655fe8f658fd68220bc258b42c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:49:28 +0000

    respect control Uu food uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
