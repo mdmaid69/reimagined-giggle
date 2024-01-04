b825fe89340882478a6bb419e64f4429f697a1aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:24:18 +0000

    future quality study while recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
