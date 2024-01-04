e47052e58f84b941bfc3c8b405ba943d8d370a51
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:26:36 +0000

    black general innocent realize pride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
