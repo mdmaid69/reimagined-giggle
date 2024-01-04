591fabe94ebfbbfaf05e24f92fbf50a1c425cafa
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:36:07 +0000

    must study bread secret plenty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import collections
-def create_user_dict():
-        return collections.UserDict()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def square_number(x):
+        return x**2
