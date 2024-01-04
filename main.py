fb3d40cda2dbe3bc9d0c9f9bb9df9318a564d91b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:03:25 +0000

    thought muscle another before acceptable

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
