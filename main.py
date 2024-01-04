726303750d8fa3b214656db9482d9836c63f2666
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:46:27 +0000

    water holdout visit safety interruption

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
