47529d9e3024b94c5a27fb22d9a1b6835918567e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:54:09 +0000

    quick welcome health something keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
