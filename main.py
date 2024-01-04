1b25ba352b769a0425f1da6e34b4e37827c422a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:06:05 +0000

    content persuade cook on talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_list(array):
+        return list(array)
