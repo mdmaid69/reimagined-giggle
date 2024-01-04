5c437c59d6d6f6fbaf4395bb5e8e78fa49fdfb4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:46:28 +0000

    transparent even software mine broad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
