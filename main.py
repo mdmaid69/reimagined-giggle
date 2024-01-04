7d1b3d5656e901a4163db3f0eb2b6ba11e5f35f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:42:43 +0000

    I success week complain wing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-print(os.getcwd())
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
