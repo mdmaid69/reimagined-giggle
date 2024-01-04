94de5f5ec2ec4ffa19978642f1966214232af8c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:19 +0000

    lock tired will over rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
