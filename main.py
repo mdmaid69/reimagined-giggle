2648809f2be5730aab82b01e4ed9e21a24a7ea57
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:47:54 +0000

    smile happy mad satisfactory burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
