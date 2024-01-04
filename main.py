340551e1f1ab3873ec75b134d16f9f24c719ad36
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:26:06 +0000

    soft term look birthday why

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def create_queue():
+        return collections.deque()
