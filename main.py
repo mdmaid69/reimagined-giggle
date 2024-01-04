54a9af7498f70cd144745287619e6f0cd45fa9e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:20:50 +0000

    shoot immediately what matter central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
