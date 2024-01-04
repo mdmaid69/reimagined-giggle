da1105d9974a56cd83b56215a6795e30886f2fbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:48 +0000

    promise answer than action towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def reverse_array(array):
+        array.reverse()
