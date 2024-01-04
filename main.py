8b8a7d12d1efe81a6af04bbbc2e815be09d0c71a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:39:50 +0000

    breathe shake increase phone crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
