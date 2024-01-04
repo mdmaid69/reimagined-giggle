f5fa26d4460b2cbb5d78eea6db05843b05c02b76
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:37:14 +0000

    spring even common speak cough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def create_stack():
-        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
