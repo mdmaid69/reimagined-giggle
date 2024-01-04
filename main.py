a78ccf2479988cbf30d91fc078ddbf30c064a444
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:09:37 +0000

    wait apologizeapologise deal happy invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
