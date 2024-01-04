b4e14c042124c48832be233766aef95ba20120fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:37:14 +0000

    Nn refer in block objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def find_min(lst):
-        return min(lst)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
