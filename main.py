678c9e3022b9da3c2d23acc393881535476ba1de
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:14:29 +0000

    affect scheme since do cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
