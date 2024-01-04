b10e29030af2dca1e3b75bf754534ebf118e0fe3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:27:18 +0000

    plenty demand hungry let underneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
