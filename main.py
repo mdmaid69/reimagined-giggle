bb1c14b3ceb8cd151462398adea08b35fb019bc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:43 +0000

    depend journey knowledge upwards instruction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
