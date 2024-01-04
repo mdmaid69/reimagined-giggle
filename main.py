b6a4fb650ac2a14efa7ab45898ef90a0e5e1e2e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:58:30 +0000

    spite unfortunate pain noisy what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import collections
 def create_stack():
         return collections.deque()
