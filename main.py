1fcf9ed2b4cd9e136ac2b87f223463cf1a3e7843
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:18:33 +0000

    dark before slice age nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
