57df2f96ac8dd782a6aa4d252b7b48db85b3c335
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:21:07 +0000

    size before beginning smell badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
