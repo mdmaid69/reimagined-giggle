bd33d212abc395c52cd2701b62a42edd2ff24c5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:44:42 +0000

    unless empty many in journey

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def square_number(x):
-        return x**2
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
