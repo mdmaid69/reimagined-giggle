696b071778d18dd2b13657ef8d46746e943a70f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:04:50 +0000

    progress tear carry Qq amount

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import sys
+def exit_program():
+        sys.exit()
