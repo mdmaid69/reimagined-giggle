db3ee204bcdfc189f767c3b598a9bb5970d04f8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:29:44 +0000

    same gentle turninto cruelty order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
