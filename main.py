ec9904cd8530f709f33197fad7b6c967a167bf95
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:42:12 +0000

    actual present evidence mean on

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
