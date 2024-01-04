b995b94870fa6013e9a46fabd553a3ccfcb242be
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:57:30 +0000

    back treat little spin top

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_float(array):
+        return float(array[0])
+print("Hello, world!")
