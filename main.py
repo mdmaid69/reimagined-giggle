0c27e0a95ae3c8229367d7eb772f3b7b7180c5ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:19:50 +0000

    soon hole organization light minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def sort_list(lst):
+        return sorted(lst)
