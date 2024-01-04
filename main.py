c76a774efa9b00fe2b4ce169c42ae0dc35b8d35d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:26:30 +0000

    rush splash usual rise shout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import logging
-def log_message(message):
-        logging.info(message)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
