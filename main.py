503fb68258631f3aa22a8e7a22c2c19586ae999a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:46:31 +0000

    central tour Kk seem reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
