1d0cad77b011469896adad9614a971aac406275f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:12:58 +0000

    must use turnoff trade special

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
