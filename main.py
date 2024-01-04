d973ea2f23255ed90650e267d802b42b578a6c8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:23:22 +0000

    emergency confused no marry rob

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def reverse_list(lst):
+        return lst[::-1]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
