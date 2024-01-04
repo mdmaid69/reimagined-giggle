6ccfa0723ba42c19c0f7a2ecf5dd6d7307221f3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:37:10 +0000

    godown world flat record sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import numpy as np
+print(np.array([1, 2, 3]))
