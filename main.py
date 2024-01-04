330609b4e233bfa74a820ce4b767c388f14758dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:58:06 +0000

    neither fence breakdown test touch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
