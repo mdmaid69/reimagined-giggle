204cabd8040e4c285adc80fe2cb7628e58351061
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:29 +0000

    alive plant organize challenge giveout

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
