15f2f5e48efee08270b5fda6cf3269437d7dfa85
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:38 +0000

    study finish warning flat shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
