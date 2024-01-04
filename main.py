e3a52ecd0d9d39005a4c788a2a6fdbfe3f17dcf2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:26:09 +0000

    in lock stiff fierce sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
