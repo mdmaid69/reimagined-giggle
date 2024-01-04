5bd9a91519702e8fd51b5dfcfcc2d8f3a29d7fe0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:56:58 +0000

    shade upstairs plate unkind over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
