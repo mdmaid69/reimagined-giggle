b2eae67a400d09d059da57df0f45cf007cac58f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:53 +0000

    decide father tall any lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
