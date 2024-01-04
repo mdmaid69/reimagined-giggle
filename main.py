b98c75d154672b2942b2c025309b3bad7bfd8b53
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:01 +0000

    steer bring congratulations fine describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
