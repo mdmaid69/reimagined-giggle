f3b0b17a8e97087a465900bab10ed405a3d4a5da
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:00:52 +0000

    pleasure age criticize whisper circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
