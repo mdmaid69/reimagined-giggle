60d7c365b7ccf8d20a98f45f17626d99626470fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:14 +0000

    male success kind tradition tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
