0b6272efad583fcc6a69da1286cc433b7080700e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:31 +0000

    solve principle same deep pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
