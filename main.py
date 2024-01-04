dc6889c5ccbecf9cc0007726856c102777dfffd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:55:54 +0000

    wipe arrange acceptable why cook

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
