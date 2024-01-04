b36e9a69bcb1161f2cea7c6ed053408e61844f42
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:02:21 +0000

    telephone standard store compete window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
