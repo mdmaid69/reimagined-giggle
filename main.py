1831cf1f61770bef24f0dc8bcab4f6d862143df1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:00:46 +0000

    spill can aim favourite special

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
