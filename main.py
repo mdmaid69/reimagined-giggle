e6c9a91ab388ecbe1729d0543d788afaf2860911
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:28:53 +0000

    train follow beginning normal artificial

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def square_number(x):
-        return x**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
