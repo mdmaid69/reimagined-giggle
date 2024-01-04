4ef5b0a9c5d359018b43d976202b76f742dc924a
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:00:05 +0000

    point sea accommodation recommend hold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
