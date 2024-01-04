eef3a0d0fb367bfe3fcc4b2d9177c4576ff73d02
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:35 +0000

    secret rescue spread on situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
