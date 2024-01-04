6c1d2d44789b31735719050aedbce33418da9cd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:56:18 +0000

    aim importance ticket bin advantage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
