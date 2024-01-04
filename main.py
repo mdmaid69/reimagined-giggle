dba56a213352d41af53a7683db417b0d8eeb4a30
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:32:57 +0000

    laugh comedown star pronounce soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
