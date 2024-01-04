c4b5fceece83e94f1c2a36b79ecddac53d3b2adc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:10:50 +0000

    breakup noone getup hit lock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
