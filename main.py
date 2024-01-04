fe4b624c0d97e61a20d7f26099ca0bc2ecb2ce18
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:02:05 +0000

    scream value path square shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
