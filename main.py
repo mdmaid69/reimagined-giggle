871c2dc0e1ecf3207d7cef32a71752d215d4bb5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:49:27 +0000

    tune trousers myself play pleasant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def clear_array(array):
+        array *= 0
