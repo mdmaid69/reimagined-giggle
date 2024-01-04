8860cf8367e55afff80d639c8f88235056233ccd
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:41:08 +0000

    flow family day search announce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
