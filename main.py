06eb2084d503cd3f0e5a1b105275c5e99ebe50d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:43:18 +0000

    adjective breath wall other rate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
