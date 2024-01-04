94d28c17157bb90e428798c682223df38d87a51a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:12:45 +0000

    gas moment payment doubt beginning

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  def add_numbers(x, y):
-        return x + y
+  def is_even(n):
+        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
