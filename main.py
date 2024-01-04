a2057b89e6d9d62ec9c1d7e1788c2b60719bfba1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:08:26 +0000

    ball payment healthy feel pleasant

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(sum(range(10)))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
