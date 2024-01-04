492b721416f3659fc4a75ad598755ef0d5c5e48a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:28:40 +0000

    aware science debt cheap system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
