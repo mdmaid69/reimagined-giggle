2e57928b515177b4271b7c8dd7d35e062f19d4cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:21 +0000

    centimetre judge foot workout danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import datetime
 def get_today_date():
         return datetime.date.today()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
