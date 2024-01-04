4bddc73860ad4c0c59f1dfcef3195cddc640e1cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:43:05 +0000

    respect both watch accident tie

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
