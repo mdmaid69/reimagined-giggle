f10ac6970446aa76c1226abbfd7fc57af601567b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:23 +0000

    knock unusual information appearance road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
