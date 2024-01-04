71066a9b0c72d26e8227bb2408b7278e2bbe1d55
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:27 +0000

    spring scheme jump deliberately abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
