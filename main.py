e510f0150f5e82a0a20d352215eca7fc33790ef6
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:54:02 +0000

    Hh around opposite out guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
