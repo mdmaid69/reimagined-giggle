a88bc66974b7a88b924298effdb719e41b2c5860
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:06:22 +0000

    scrap value future his although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
