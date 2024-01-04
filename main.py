8e28c84a25d67bb417cea92934af4b7e4a4cc8ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:41:51 +0000

    welcome thought gate worried load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_min(numbers):
+        return min(numbers)
+  def convert_to_binary(n):
+        return bin(n)
