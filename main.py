450e00daa93442e27214c3eb3119926821dad828
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:55 +0000

    patient float instruction present workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
