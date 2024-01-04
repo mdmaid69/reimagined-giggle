48f2e5ba77e46b80782610511b89491168285d76
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:02:22 +0000

    desperate easy damage laugh passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
