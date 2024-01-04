01f97a4214aff5a628129c85e174b872cec78000
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:18:28 +0000

    fear wind world sing young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
