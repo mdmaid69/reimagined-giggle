a0d2bbe054c1b956717125038c6d947d73a043b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:13:14 +0000

    great branch advanced whom change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def cube_number(x):
+        return x**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
