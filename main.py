03c763b01bcb9707c0bd4c408b92bee0b5943dd1
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:53 +0000

    treatment 564encourage roll sort experiment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
