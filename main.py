6cf3b33d9dac5bf0f123845b922b54c2d0aecaa6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:16 +0000

    vote least here fortunate congratulate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_min(lst):
+        return min(lst)
