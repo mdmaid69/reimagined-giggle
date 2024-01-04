1bad7e48794e562681c362304b466bebadc340da
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:37 +0000

    transport interfere relief arm safe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def reverse_list(lst):
-        return lst[::-1]
+import time
+print(time.time())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
