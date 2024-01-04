84ccf1362a8ea281102e6408dc3fc347ceeec75b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:24:15 +0000

    experiment study exit dark clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def remove_duplicates(lst):
-        return list(set(lst))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
