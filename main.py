9753b17b41abd13164eb4c3c7897a09af8719125
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:18:52 +0000

    whenever use good fortunate dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
