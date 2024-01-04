b520dde6d312dfaf215d629bd66d887bf7f4e31d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:28:54 +0000

    who each since job punishment

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
