737352b4e6a5be95c3292da519ea6d6c24b80f31
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:07 +0000

    use mention press appear stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_odd(n):
-        return n % 2 != 0
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def reverse_array(array):
+        array.reverse()
