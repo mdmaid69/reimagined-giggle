859c190f9decd064781ccd6b6be191770290045e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:34 +0000

    straight family kick child strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
