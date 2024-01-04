38c5142a863c1c56c6df6b123469fd4d9b613023
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:31:54 +0000

    air runaway platform goout market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import collections
-def create_stack():
-        return collections.deque()
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
