2ac534eaba379b6e9a17d5b0696800c09c9b5d5a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:42:59 +0000

    post drop close comedown price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
