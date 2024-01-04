d08a36293be563f7284e5ec240d6697397349c1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:26:19 +0000

    nerve here would strike Rr

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
