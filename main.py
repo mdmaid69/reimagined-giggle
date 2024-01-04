c7398b6a4721bda6aa5255126e806a5478a96d8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:03:10 +0000

    emotion afterwards grateful shut busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
