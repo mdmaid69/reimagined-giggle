3127fd2ffe1b0d80509ccbe1b5f9708a110b28de
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:02:35 +0000

    drawing unable quantity in about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
