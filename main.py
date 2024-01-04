424c40528d96c6984b4592d6afdac0cd4d9188be
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:19:03 +0000

    set swallow custom unite exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
