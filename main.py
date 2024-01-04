88416eccb71a28799cf84f75f0682d1b852c1395
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:00:39 +0000

    measure refer cum drive rich

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_file_size(filename):
+        return os.path.getsize(filename)
