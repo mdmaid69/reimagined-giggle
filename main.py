2038e3ae0b162bf7f05260ef630cfc07fa34958d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:08:40 +0000

    someone out before alive contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
