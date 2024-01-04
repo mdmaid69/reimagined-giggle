056a5842f521d2772be5862b324ce596e2cc54ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:15:51 +0000

    criticism statement stick instrument the

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
