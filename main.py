cbcf41925b55988d10b53a3e79f29949449d68c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:02:03 +0000

    fly room force design colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
