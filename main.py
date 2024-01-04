ecab8e6905c62e3483b5841600964e1034a3a54b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:16:45 +0000

    inquire land test side round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
