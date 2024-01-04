47752f9c6c18b7fda854e076c836d2858caea7e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:36:30 +0000

    sleep any trust shout sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
