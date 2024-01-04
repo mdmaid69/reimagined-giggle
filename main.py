bad2bd7294fce2954c9b048cd746c4824e06504b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:26:53 +0000

    reaction flight grateful beside spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
