9b1df3ce966a58ece4c98c74c128660597e0ca7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:15:41 +0000

    fall this defeat recommend pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
