baecf651784b8bbbf42d08bd32a1906f4633d4b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:16 +0000

    getout pick sure hers offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
