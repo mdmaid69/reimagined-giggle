ae7f474af867f8cee3e42831801c7ee0f5f8e626
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:46:14 +0000

    bye 565end hour down immediately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_unique_words(sentence):
+        return set(sentence.split())
