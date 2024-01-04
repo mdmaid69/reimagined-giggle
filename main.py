733a5afa0c2fbb70df3e4a55014e109544ae37fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:05:54 +0000

    tune grow copy worth spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
