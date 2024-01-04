d72f86fb2dda954f8afff4a43fe2f896ac73adcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:25 +0000

    thinkthrough react underneath over shock

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
