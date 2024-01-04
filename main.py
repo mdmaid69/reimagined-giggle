12640dfffffa4ff83d31c222b84572e29e0b707c
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:38:43 +0000

    surprising either spring wait ignore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
+print(sum(range(10)))
