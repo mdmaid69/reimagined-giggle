159e6ac4be3c7ffbc747fdfca39ddcb1fec03518
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:56:44 +0000

    match shock violence means callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
