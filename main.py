f3e34a04938d4c9066c0b7a18926a4dc26c1f16a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:09:51 +0000

    should sensible film fire concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
