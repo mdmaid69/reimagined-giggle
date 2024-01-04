7bfd61f5f152c567c5ba3a04f0f2b7faaf458ab7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:28 +0000

    treat later century image field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sys
+def exit_program():
+        sys.exit()
