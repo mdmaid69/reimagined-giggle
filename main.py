8f87fd7668b730d4144e44998c20dc36d010337f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:17:30 +0000

    experience both tough environment unable

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
