df96055679782de2394ef20d5e2f63632e85f902
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:12:43 +0000

    rule unusual refer right Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
