bf79f0d40a109331f51748644bc93522d1660664
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:22 +0000

    patient announce know television our

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
