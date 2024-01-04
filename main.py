1b19a2c1f40d85690930e53369cdd0086b6242e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:00 +0000

    goout connect press noisy right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
