c1d55ffc75838a829fa967a3f197d77e9e535bbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:04 +0000

    injury dry again situation crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def square_number(x):
-        return x**2
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
