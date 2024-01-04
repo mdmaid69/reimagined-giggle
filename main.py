ec8fd404e73e12c9918db5bd1e0f7c8d5a503d4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:57 +0000

    example accommodation concerning explanation whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
