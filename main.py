5a0ca7f6014df8f967969f4f54f0653acb828898
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:16:40 +0000

    near somehow drop lower proper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
