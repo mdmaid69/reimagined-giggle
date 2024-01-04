55b89b9b32a357dddf6bc069787d00f2c9b38cda
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:13:41 +0000

    interview protection rest 566end film

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
