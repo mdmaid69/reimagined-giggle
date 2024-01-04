40ac6379a650ebf5972132ebfc1bf63f37648f31
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:16:02 +0000

    honest stare nor land happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
