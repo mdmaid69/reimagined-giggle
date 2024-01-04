4a4a403d9916b8cca7a523c57655c5bf495f71a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:47 +0000

    hair tend lower everybody raise

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-print("Hello, world!")
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
