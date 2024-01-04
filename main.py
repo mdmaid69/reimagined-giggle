df4e23cb72a9ccae36784f9b3c80474c23fde4b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:34 +0000

    impatient sticky before degree combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
