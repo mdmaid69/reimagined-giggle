a237ca72c51d9e872cc1c173a6bbbee3d85e9842
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:29 +0000

    happen brown stand love join

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_str(array):
+        return str(array)
