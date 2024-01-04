c1ec4df745a327ad87cfeb146d54bdde8ccfe038
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:02 +0000

    common small low on past

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
