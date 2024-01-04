9cf59523fc2e260796bbb440a07a78c4a83db41f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:06:31 +0000

    need bringback feel pretty pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
