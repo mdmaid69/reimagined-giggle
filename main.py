da86f79c0843e32b5b67f48955c08c4a8df787fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:09:36 +0000

    warm sorry stay suit dangerous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
