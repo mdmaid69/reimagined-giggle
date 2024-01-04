bd1014a22e4b5c122378d457fd8b6c092fe57d06
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:14:57 +0000

    see takeout lookback(on) address correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
