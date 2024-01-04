e40c7c20b4e545b6510d7332542d0a922cce24c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:49:46 +0000

    everybody issue nigga uncertain year

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
