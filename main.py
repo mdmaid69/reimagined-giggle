4122d396638ec92f9a73109bb7b9aa42b4cf2dfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:41:26 +0000

    your expression maintain deaf estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
