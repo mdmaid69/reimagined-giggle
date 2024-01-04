60b785842f033afc3fe7a9a895024a31ded003da
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:14 +0000

    gift vanish blame police wall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
