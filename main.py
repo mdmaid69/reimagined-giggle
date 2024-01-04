e2179713791299cdfa7e35a15c6ba4ab11a4120e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:25 +0000

    day food mix division human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
