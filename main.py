b7b0729690088735f5db9486fee5813655cce182
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:51 +0000

    skin squeeze arrangement discussion criticize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
