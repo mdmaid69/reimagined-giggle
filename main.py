50f1196faf1d365d2bdf7a16f7a67b95836d4443
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:28:53 +0000

    clean turnout separate turn influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
