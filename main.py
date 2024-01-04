65ffa77f04e2db1faec0f8a6c5651c28e85cbd83
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:56 +0000

    trap poison load illness cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
