95ad2e9f750784dacf0fc2b985594d7366a4aa97
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:07:34 +0000

    frighten ear needle principle ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
