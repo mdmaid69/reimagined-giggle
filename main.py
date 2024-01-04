bbb91b372ddc7f01bf81951ea2d23c99ce98423f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:37:34 +0000

    round niggaacademy warning bring damage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def find_min(lst):
-        return min(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
