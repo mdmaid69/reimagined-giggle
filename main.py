b5fb776e14536a29e2381c5dc388838086fecd3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:54:42 +0000

    favourite tall sure head whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+x = 10
+y = 20
+print("Sum:", x + y)
