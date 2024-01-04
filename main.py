29893ac9fb1cc8e476517fe79a509866dc72568c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:14:54 +0000

    appeal holdon dark turnup report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
