a176bfbcbf40b0fde76801519be0465558ae8f26
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:30:17 +0000

    female grade split mother make

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
