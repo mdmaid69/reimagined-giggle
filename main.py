ebe72c8bf07f0865c12f6882d6773afd9c5d1009
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:10:43 +0000

    knowledge anywayanyhow die space course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
