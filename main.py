975b4921a902cade324d8834e3e9875c2e730264
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:20:35 +0000

    principle command keepout attention keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
