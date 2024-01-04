451274bcf13eb7183f5b649a674654c464793918
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:06 +0000

    live film maximum know judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
