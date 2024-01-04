1511c6d93d10f07f98205c97013b64f6c83e364c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:28 +0000

    walk light detail soak drop

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
