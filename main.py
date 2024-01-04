3c7c131ec1b14311f8c5714cbef88c32ef3ba261
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:47 +0000

    quality team push breakdown search

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
