c4fc831e6c653f45c4c04389845ead8ae090388e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:42 +0000

    collection wide go probably lift

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def setup_logging(level):
+        logging.basicConfig(level=level)
