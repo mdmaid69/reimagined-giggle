28bce4169256645773dcd4bbaef1ba3363a0b798
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:50 +0000

    pass direct hurry fast warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
