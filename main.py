3ce044f2483066893f6c5424975605ada3081a88
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:51:26 +0000

    into to exist several standard

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
