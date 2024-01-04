f9a33c4ea8b282de62f4bd0ff65d91bd6ec2f5d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:54:42 +0000

    police appearance bath beginning me

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def subtract_numbers(x, y):
+        return x - y
