5b7e0dd0fa3613731e2bd8b14b75298e311ceac1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:45:34 +0000

    how petrol date lay limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
