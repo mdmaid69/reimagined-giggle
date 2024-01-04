706eb0a09522b720ef92859944f23212dd146bb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:51:49 +0000

    cry split ourselves respect tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def find_max(numbers):
+        return max(numbers)
