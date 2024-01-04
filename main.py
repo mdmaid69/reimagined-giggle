ae31180479b56aa02e0ff63fce7eed6c2f58241e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:02:04 +0000

    forever man solve gas near

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-for i in range(10): print(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
