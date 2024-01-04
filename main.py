d6fe8d934b72362bb27d44e2860dd3e5f6a26288
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:52 +0000

    agree proud fortunate moral float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
