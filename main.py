2df0cbd514be6568818415e33fc6dc8071a618db
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:27 +0000

    extraordinary performance tour off unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
