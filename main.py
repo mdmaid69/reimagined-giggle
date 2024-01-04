daf3a60b424deda5b84b72a18beaa811addcf48c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:45 +0000

    comfortable adjective neck nature ability

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
