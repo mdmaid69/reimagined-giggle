9b549435bf9e25c80f526984d3d200ddb1316c5c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:38 +0000

    old clue extra leave anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_float(array):
+        return float(array[0])
