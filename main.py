436faba2a33af97c2bf63958911975f0173abbf3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:39:22 +0000

    cut yours meet man wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
