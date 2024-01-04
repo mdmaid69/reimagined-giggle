90b9adb17a0ed5a3d0a0ffeb5539f01bf85d2547
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:35:40 +0000

    part hungry good think sound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
