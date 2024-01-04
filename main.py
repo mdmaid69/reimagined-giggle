8afce7a09d5751c6e9a39c1999f94942522e9295
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:27 +0000

    straight mark handle only save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
