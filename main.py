576cf4bf9c39dfd8f0f3f3bd3387353f3b6d877f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:38 +0000

    nowhere excuse praise impressive autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def reverse_string(s):
+        return s[::-1]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
