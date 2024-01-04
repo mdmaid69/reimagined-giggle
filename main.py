9166b22dc4f7562985b2e4b2b712d6911be8efa0
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:03:39 +0000

    black difficulty trial down goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
