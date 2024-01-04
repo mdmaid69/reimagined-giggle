bf9d1aac2a8c069837e807cc20e7ec3d0793a5eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:47:29 +0000

    runout exhibition thinkof he thank

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
