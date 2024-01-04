e594aa8c33ef9f98f3a5912e51d10dbbc52288e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:14:28 +0000

    underneath meaning cost lift increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
