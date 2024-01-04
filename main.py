c3f873c6184cd2746d573fc948c4f2eb47b235b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:56:08 +0000

    warning agree account limit prize

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
