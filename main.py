df203f7fbac28c2ba92227aac7b601b71a575f82
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:46:56 +0000

    apply laugh since blame ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
