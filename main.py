cf1909b57adcf59104e1bbab0507ae46b0ccc1e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:59:54 +0000

    shout keen too first clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
