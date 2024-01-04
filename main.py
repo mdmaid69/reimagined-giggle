5c40b46acaa31031d8774e6ef53e6e01cec7d60a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:45 +0000

    instrument damage both anything sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_max(lst):
+        return max(lst)
