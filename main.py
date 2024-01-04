779f3475b6dd09800c40a0ea3f4dab0604a84022
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:11 +0000

    account look attach charge satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def convert_to_binary(n):
+        return bin(n)
