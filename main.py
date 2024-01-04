a4788886077fbb4e8506e52537ab52c78c0bf020
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:38:53 +0000

    swallow behaviour judge prefer pupil

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def reverse_list(lst):
+        return lst[::-1]
