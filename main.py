a4676924ee119587076884f9a34d2df7b1be7127
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:30 +0000

    mess debt confused separate Dd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
