622efbf6555fa080061065022b2bd2d362ac6252
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:42:10 +0000

    cry then average never call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_even(n):
+        return n % 2 == 0
