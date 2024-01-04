8d31ad10448b0bc0b634c346107059a43ecfe11e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:48 +0000

    accordingto weak possibly fasten square

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
