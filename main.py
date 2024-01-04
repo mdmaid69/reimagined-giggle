66d188b913aefc7c4636650caa245cac0dbb0d29
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:20:41 +0000

    main prepare show full reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
