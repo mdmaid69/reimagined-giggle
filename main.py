dfe55118cb14c6512a281200b1436acfa4d46b20
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:38:21 +0000

    official hurry pattern actually steady

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
