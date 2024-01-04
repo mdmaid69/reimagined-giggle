2d4c821a777abe87759033fc1a40fe5d356c1850
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:07:17 +0000

    unusual comfortable change funny stretch

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
