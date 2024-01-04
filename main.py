d20b47d9437e52ab6295f520b9a93eb9c6bd9fa1
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:48:56 +0000

    plan zero man away describe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
