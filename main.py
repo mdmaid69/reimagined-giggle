e7583927dfb59072d10da8b7f1beacfaa1a6ada1
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:23:15 +0000

    paper 564encourage building accidentally suggestion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
