5a678e9abc2656d55b40c446e3026700ad90c846
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:35:22 +0000

    deserve suckme evidence proper splash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
