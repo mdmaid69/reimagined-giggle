6f362de6a476d6e56e6277efa287d3569accc40e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:17:01 +0000

    escape explain worth experiment emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
