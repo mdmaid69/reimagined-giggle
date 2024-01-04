85bce493f38acdea23d975c3770d5a6542fc55e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:26:35 +0000

    complaint size mess declare path

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
