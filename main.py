ead56992d202da1ebb63a15d5e6805d7e038eccb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:41 +0000

    bottom offend bottom occasion practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
