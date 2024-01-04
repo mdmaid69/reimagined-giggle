81be79d8c71a201b8fd889aebb05ad1b43504075
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:18:37 +0000

    city trap scared curious platform

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
