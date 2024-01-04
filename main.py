b2f8e56336f9729c1b52d175c1c95e264af4f362
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:28:02 +0000

    support regard base exact regularly

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
