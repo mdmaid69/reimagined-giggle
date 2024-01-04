66720da98d6238c660bba0edf2a1b0c290d41b52
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:49 +0000

    rent feeling remain size Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
