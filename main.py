3cd94e19a60e34d7330fbf9a7ab23eee4891092c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:29:19 +0000

    fond make carry term actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
