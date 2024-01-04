9f9c83c91f7f35ea1d676ec3d1a504fd876cb318
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:19 +0000

    invitation give lay stand battery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
