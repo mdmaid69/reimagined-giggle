02f56fe19114aef8a1fa40fcd7f04e2ab6d17810
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:14 +0000

    rarely platform wish giveback progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def find_max(lst):
+        return max(lst)
