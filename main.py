d2e4ad0442e348acb2cfddf247f6bb2f189cab73
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:22:44 +0000

    lookaround plant prize mess interesting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
