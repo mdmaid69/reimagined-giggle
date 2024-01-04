0a296a5e0568bbe2cbeed3a66fe514e5411a87a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:33:11 +0000

    lead frequent substance cook today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
