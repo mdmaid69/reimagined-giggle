ff27f42d724ff8d761c01454884cc23c97b7e817
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:00:34 +0000

    community holdup watch mean authority

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
+def greet(name):
+        print(f"Hello, {name}!")
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
