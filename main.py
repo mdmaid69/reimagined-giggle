9b634d55b219ed27f7a258acce548a21259f2cdb
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:25:05 +0000

    takeover violent suckme distance lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
