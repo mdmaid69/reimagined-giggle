51f14343a85e9284042872632c0b6980da139cf3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:06 +0000

    keepup hit us drink about

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_power(work, time):
-        return work / time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
