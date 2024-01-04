30f48499f3c0f76c294a77f90eacbd298bec86ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:37:49 +0000

    contact uponon when plan order

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_bool(array):
+        return bool(array)
