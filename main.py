4324abc929c8c2e86b6b6bfedf4d5bb433139db9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:48:56 +0000

    damage unkind towards cause welcome

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import numpy as np
-print(np.array([1, 2, 3]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
