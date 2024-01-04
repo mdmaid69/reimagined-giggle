32c353a346ce252088d4180057909022db41cb82
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:49:35 +0000

    situation behind concerning show runover

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+name = "Python"
+print("Hello,", name)
