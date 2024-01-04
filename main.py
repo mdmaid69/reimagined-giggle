aeb331f2441cf24cd8e76f450522a6718b60c5b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:42:11 +0000

    stick however form air 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
