c2259d6f994715c2ee03b4dc89ecd7e84e9290d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:47:26 +0000

    commit duty thankyou spot properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
