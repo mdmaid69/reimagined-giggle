99b131a4cc9978ca08e31cdbb7e5f948aa7aab71
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:51 +0000

    dream opportunity night accept switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
