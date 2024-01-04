34291ecdc4f56f2f5d0c854829081328e3a25a4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:07:07 +0000

    yet need flood suppose he

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
