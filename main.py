6f5ef7e0260ffe1b07e9c6de595dc7104e098e5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:14:15 +0000

    strict improvement raise steer rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
