ce14fca5a9dd558eaada7723dbf7c5893dc68ac7
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:01 +0000

    car whether turnaway type home

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def count_elements(lst):
+        return len(lst)
+import collections
+def create_counter():
+        return collections.Counter()
