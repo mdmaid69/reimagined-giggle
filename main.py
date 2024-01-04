7485f99abc1ddd0cad30947c130be08c5bd2a1df
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:39:06 +0000

    order woman convince unkind commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
