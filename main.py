1ffec63ccfa408d9caddd53bf1c87b691016be12
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:36:13 +0000

    badly upstairs create negative even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
+def remove_duplicates(lst):
+        return list(set(lst))
