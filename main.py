be6a81dcf9533b79b09c4a8f8e262803b2779469
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:37 +0000

    shock open lead refer mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
