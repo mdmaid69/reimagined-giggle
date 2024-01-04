43e97f9b09025b90056119e81c0c474957deb22d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:17:16 +0000

    maybe commit around light beside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import time
+print(time.time())
+import array
+def reverse_array(array):
+        array.reverse()
