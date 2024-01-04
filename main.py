212e3aaa8007a78b6f42ff82132f910d8cce85bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:00:45 +0000

    track complete catch scream understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def append_to_array(array, item):
+        array.append(item)
+import re
+print(re.match("h.*o", "hello world"))
