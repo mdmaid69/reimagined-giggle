857530c31daf9f8f23cabb5c42149c301a70451b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:54:19 +0000

    secret pen beginning never admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
