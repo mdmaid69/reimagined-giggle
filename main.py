26b4a2c226aaea9fec6df74439cd795f2eabc540
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:50:17 +0000

    weather murder type listen charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
