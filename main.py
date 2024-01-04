4b41672acb14ea07574cb90add50fb6516965681
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:06:39 +0000

    rate step recently unite reach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
