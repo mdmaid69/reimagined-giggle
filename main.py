3df673e62668d86ddc4918cd10eb7255773415c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:17:24 +0000

    advise interview grand watch recommend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def count_elements(lst):
+        return len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
