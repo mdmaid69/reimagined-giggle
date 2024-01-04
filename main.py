fa82969ad3541ca8009a51514ee607fab2f9e584
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:15:19 +0000

    finish ball cry effect later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
