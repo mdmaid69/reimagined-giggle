970c8cc677f014b362106b94436961bc28863f69
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:27 +0000

    should gather steer lower region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
