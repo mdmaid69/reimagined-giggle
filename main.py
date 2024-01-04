ded2f9383c7372e101e2b9fcc7371212a2f48759
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:17:45 +0000

    in part fast average scheme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def divide_numbers(x, y):
-        return x / y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
