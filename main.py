a67400289fc7e419e6b898b0ecf3fc4aa694c544
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:36:13 +0000

    anyone may rise contain brain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_work(force, distance):
+        return force * distance
