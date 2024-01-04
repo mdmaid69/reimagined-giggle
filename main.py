fd1c27780c54a86e3f16cf55823d7f35f588cd1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:46:53 +0000

    blind modern goout order height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
