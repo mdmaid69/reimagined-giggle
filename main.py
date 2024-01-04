a3e82f358c3d1ae5c73d93b8cda1ecea8270330b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:39:38 +0000

    cheat indeed label turnon Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
