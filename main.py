e9e4f99e9551791fe9be8fea37310b99e0a5422a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:21:41 +0000

    please considering belief then shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def convert_to_hex(n):
-        return hex(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
