5a69e4aba38025a077c3c8cc451de905d0e25588
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:46 +0000

    apparently cheat unpleasant join drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
