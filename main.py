56f81fe164bcc48998b296e35a06b48432a97d8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:18:09 +0000

    front suggestion drive close call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
