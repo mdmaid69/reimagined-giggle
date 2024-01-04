4dda2a86bd10f4ea6e1c1bd619ed0f8cb498b1d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:42:56 +0000

    character follow equal ago interest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
