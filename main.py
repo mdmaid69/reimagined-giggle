ac7aa50bf77675b1045ef080de87c4f1bb2ec3f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:55:55 +0000

    fight consist war upwards clean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
