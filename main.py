5e4e4e06e020a67bb3443d509428fc0de3e65d90
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:05:52 +0000

    approve fill bin laugh terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
