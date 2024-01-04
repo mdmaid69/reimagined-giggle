482f4a37a39213150348aea89ef4ff6a8f3a15c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:29:21 +0000

    clearly factory person front getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
