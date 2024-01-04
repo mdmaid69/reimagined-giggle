7b32423e926febf8f2cd934ff7c52abf7e2b6b01
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:49 +0000

    tragedy lot advise success below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
