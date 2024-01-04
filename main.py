cba9797dffb49a6d75be5009d66a9f709c7685ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:41:19 +0000

    mark who responsibility classroom left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def square_number(x):
+        return x**2
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
