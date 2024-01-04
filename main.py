faaeac39db2adb84f5340b063f040420399abeec
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:11 +0000

    objection lookback(on) compare comment region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
