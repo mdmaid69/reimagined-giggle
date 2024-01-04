ce074ccbc0e24f332e53eae0069abb5578079f2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:34:49 +0000

    emphasis interruption past arrive back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
