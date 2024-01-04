9ab598ed31e04fca3332dff1903fec7a6bf468ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:42 +0000

    gooff obvious gas opposite progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_array_to_string(array):
+        return array.tostring()
