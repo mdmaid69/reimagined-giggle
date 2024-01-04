bab567925a17aa368fd5d5f0c2404828a31e8e2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:50:26 +0000

    head damage standard corner form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
