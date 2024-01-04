f592680a75f9488b6dec9ad56e06abce55e4e132
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:48:51 +0000

    thank lost bye wrong approval

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_bytes_from_array(array):
+        return array.tobytes()
