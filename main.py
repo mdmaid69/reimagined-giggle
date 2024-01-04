ea17ec60efe9c51dd04ba89780f903fa1ad07937
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:31 +0000

    ask show discussion compare this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
