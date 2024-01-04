8e5bfb5ba6f3face8ea926a5c0541d53925774b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:30 +0000

    break shopping section getaway doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 def add_numbers(x, y):
         return x + y
