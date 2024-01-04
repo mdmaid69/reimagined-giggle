4b5d06971b441d2c56955d446fdd5e919767c65e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:07 +0000

    square chin approve summer soft

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
