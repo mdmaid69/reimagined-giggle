8417085feafae4e1ba9499b31782eff2079e5cca
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:54:27 +0000

    space formal welcome ahead remove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
