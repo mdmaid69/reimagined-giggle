a21bccc5e1c506381b41204a88d6f6258b9a6e76
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:48:42 +0000

    fall holdout else fasten block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
