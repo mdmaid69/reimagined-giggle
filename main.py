07dd7d3cd6aeff6d2cfd43f41ce81b9075059df4
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:11:07 +0000

    conversation capital share several range

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
