2138a869d89df2a0c367cd31e144d106bde9ced9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:40:18 +0000

    meanwhile rest impossible independent apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def count_characters(sentence):
+        return len(sentence)
