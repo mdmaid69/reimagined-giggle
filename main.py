792c26b0f96744f76fd2f42c5cea787f29f0925f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:05 +0000

    pretend target band second remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
