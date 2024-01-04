36efa8cb8efdc1315f5cb70fdf0ff1f102642a76
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:47:13 +0000

    truth blind under road abroad

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def square_number(x):
+        return x**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
