50715355d35bc877fbe0a8dc53d95f3d755bab75
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:53:45 +0000

    allow record edge cold talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
