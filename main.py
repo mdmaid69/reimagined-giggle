2b00511210388986210e21bcebd6ba8ad4777ec7
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:42 +0000

    mood figure less pattern combine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
