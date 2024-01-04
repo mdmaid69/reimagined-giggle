b0fc2aada4f44df70e0d8a1662f956b2e6a0a009
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:33 +0000

    empty speak centre operation generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
