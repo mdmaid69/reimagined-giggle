d0da0edd7c26446fcd9c526a86c55c58d0f51446
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:31 +0000

    slow shop interesting most hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
