6c4759738868e953ccfd2e7600de7309e8736afa
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:57:16 +0000

    broad ago crazy fasten wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_int(array):
+        return int(array[0])
+import math
+print(math.pi)
