03cc5ad79559def1533c1424aeadd2ffd133f696
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:09:15 +0000

    hello sport great choice part

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
