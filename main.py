5aca23bc663fcca4297d2308884fe6eab24570f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:39:47 +0000

    school apologizeapologise impressive burst unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
