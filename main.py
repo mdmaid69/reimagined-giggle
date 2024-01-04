ad0f9e9d6eba4a1828106c8ab4d5abae9bc83d8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:24:40 +0000

    scale slowly stranger think born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
