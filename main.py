864917ef8cc46520d53ee8f4e382a796ebf71a56
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:59 +0000

    recent recovery pride community treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
