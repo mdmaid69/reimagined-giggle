9ea1c07a40ae787e40b3a126d93abc1b4508ce6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:28:13 +0000

    evidence watch wise to hospital

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
