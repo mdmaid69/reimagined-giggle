8acacde8914a5912a44014db74b1b270196d41a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:21:58 +0000

    smile wing concentrate male closely

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
