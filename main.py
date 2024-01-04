ce2fd57f158154752dd5989f8dd2f23a458a5e70
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:47 +0000

    by special pocket everybody settle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
