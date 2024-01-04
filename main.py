b789964224ff261111695c8092b795b9e58930e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:56:29 +0000

    makeup ashamed either send convenient

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
