7d83219a1a17aef7f1ad436fe6fc00fda4dc8cd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:33:58 +0000

    design extraordinary related account us

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import os
+print(os.getcwd())
