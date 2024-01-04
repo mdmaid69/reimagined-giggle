d162ac039be685a9ef2d95b3c5f48ec14b7aa5ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:50:40 +0000

    mess border difficulty border no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
