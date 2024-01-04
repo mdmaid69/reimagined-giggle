f28c4d438b002cd982315d1ebb34006af6883a90
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:44:00 +0000

    fence snow oil interrupt guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
