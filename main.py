ca17bf86c5bde72e45f9ba77005449e3cd9b3bdc
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:08:04 +0000

    lock ago container suddenly cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
