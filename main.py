85f9db03b52803decc2ecd6d0496d9ecb8a2f5c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:28:42 +0000

    shine there hold pair past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
