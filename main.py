b561b23c4732ef8ba893046eeb3bb22cdb3a149e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:03:54 +0000

    dress loose ocean discover attract

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
