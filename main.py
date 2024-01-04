b2d337a1a92347dd0f053ae98c653a393cce5065
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:41:36 +0000

    heavy street Gg woman sink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
