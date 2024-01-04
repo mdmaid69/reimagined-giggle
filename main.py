ea01967f912643d229bf1256ad39fa2df96cb35d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:49:12 +0000

    tidy wrong thanks Ii too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
