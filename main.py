1ddc07ff09b41113afe68ab48c39a56011b29f7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:15:40 +0000

    near flavour touch card call

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
