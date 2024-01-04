fc86d7fb6471b661833fd47b9377ab35517ea43a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:51:40 +0000

    regional glass backwards special increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
