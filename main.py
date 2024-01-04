b3c0650b57a5e5f475ac854440d7abbe790c770d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:47:29 +0000

    sensitive together kind organization climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
