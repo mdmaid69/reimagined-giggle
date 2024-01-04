ff151a02feee44c3295afb380f4e0f7f18d56a2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:53 +0000

    sign explain load Cc teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
