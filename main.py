c90a69256046e7fa4b974ee1a5c303bbdd4f1366
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:18:34 +0000

    turnout juice pump concerning die

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
