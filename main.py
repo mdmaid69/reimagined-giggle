3853304faa8142a6a1b9eebc6e8458ef4e7c0367
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:09:40 +0000

    new quality throughout ashamed bringback

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
