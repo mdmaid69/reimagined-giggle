ecc7a9c0464b46bc9a4605feba7a43cc625e06c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:06:30 +0000

    juice impatient person home job

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
