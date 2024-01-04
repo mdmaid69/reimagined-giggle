6643a076c4d87c5fdc7bee866dde1879f1094702
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:48:11 +0000

    picture continue wealth smile estimate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def divide_numbers(x, y):
-        return x / y
+import getpass
+def get_username():
+        return getpass.getuser()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
