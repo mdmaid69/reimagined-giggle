5d6deb0105723041c912474b05a91cbf3f50ea38
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:09:34 +0000

    try sign tape strike sudden

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
