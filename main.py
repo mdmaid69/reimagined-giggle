c2c5e1aa442e2369214299d5f76b72b0213ab4e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:12:28 +0000

    feature whichever play scale decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
