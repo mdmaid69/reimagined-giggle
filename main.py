33fc98263163218b3253b47612a3aab8a31ca96d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:18:57 +0000

    wrong fortunately innocent minimum fear

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
