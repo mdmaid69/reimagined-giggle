cb52d93fc1f998da2a2e32fa776fd8e1afdb65b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:12:03 +0000

    step objection ear dull mine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
