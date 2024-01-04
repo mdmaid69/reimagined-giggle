afcd5a25210ae8085688674dd4e071f8a990089e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:30:53 +0000

    several tradition careless star suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def roll_die():
-        return random.randint(1, 6)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def find_min(numbers):
+        return min(numbers)
