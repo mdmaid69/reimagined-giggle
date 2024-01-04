a4f24ac700bc25d2f30e41c57da5a807834d3713
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:37:18 +0000

    gradually month influence tend that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
