4ebd8c336c63dbdf270b22a25262dcafa19a10b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:54 +0000

    would copy style also with

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
