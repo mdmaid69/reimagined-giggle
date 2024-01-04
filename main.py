d2a40efde4312031c9bd974c2bd38bca6842867f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:03 +0000

    off slice getout big recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
