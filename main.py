0cfccbcd1128b7fa9af90128b1f3c61bb6d889b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:31:30 +0000

    plant mainly tape pour Oo

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import sys
-def print_python_version():
-        print(sys.version)
+print([x**2 for x in range(10)])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
