7d0eb41f57d1b3e6991fb63c0b17ac9e70b81540
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:37:27 +0000

    drink offer reply during rule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
