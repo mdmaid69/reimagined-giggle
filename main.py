bb4a1be9a03eb5ef4bb3bbf1018918e59d3ef852
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:28:09 +0000

    how deliberately base morning principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
