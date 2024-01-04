6ea2e648823fb68cbc59529ec1c5a7b20b5c4edf
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:15:24 +0000

    hit minor doubt skin tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import random
+def roll_die():
+        return random.randint(1, 6)
