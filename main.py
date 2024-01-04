81f96b0a49c010a2c2a6d89d5612be14768a929f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:34 +0000

    tomorrow let piece last section

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
