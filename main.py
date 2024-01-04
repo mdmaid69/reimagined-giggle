42fa642996c9354c6a720f9974006945c26c6d03
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:52:50 +0000

    fall split stupid hardly both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def divide_numbers(x, y):
+        return x / y
