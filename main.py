25829d41bc5c9ec18c60f5fd67aad941c3c7681d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:02:59 +0000

    goback(to) true turnoff hit female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
