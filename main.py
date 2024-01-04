b2ba10840498e3c3b1b980b9c94c79f3e41629b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:56 +0000

    family contribute likely friend touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
