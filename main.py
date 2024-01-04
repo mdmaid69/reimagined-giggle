f05787009513c3f5b869b791e6f8a9518eb2124d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:20:42 +0000

    smile frequent address deaf let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
