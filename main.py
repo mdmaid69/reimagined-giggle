9a71d76ea05d404e82562df89425277973e04742
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:28:42 +0000

    mixture climb slight giveaway right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
