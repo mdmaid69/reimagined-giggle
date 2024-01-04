eff7be592b1fbd4f5975baccb871d249cbf8d8d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:04:24 +0000

    dream pull picture break friend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
