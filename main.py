a834979a6c27f6577f82d7b8e48659d2b687c42c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:30 +0000

    official alone different refuse apparently

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_set(array):
+        return set(array)
