e1abd9e3253a89a9d7eaf54863274301ad0f6dcc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:00:26 +0000

    he turnup phone answer would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import time
-print(time.time())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
