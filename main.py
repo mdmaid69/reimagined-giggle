ea7aa46b5737d490043f9395f087b802df9c80a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:43:53 +0000

    reject comeout off protest just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
