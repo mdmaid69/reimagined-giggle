dfbbe3d25648c281ba7be6c9d2d15d07526d2af2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:28:20 +0000

    lock weekend amount container father

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
