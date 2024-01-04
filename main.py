07d77e59fda4338d5f3048f99757a6344b08da71
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:47:24 +0000

    post dare chance since their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
