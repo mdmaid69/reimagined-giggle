124a11a56cf8b1e2e6eb08eefc06b705c266ccfb
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:35:03 +0000

    bet ourselves busy exhibition error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
