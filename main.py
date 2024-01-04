f2fe47568fe5145aced4d040cb3f00c90a92a829
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:56:45 +0000

    pipe achieve free failure near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
