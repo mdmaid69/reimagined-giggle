87a0cd364bca7b3858e31e740802683c5b1cdf22
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:28:08 +0000

    string guilty term getout mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
