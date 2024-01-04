0e1dc8a108018ad8772a259067a5c2ed9ae67980
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:40:43 +0000

    well dish greeting time company

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
