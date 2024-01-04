effabe8470ce3c3ea1d595e9af85c30699595f39
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:36 +0000

    generally dinner visit copy similar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
+import datetime
+print(datetime.datetime.now())
