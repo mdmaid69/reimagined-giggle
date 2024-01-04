21aa32f0e608bfa2044f71b6de68cf307581e8cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:04:17 +0000

    exercise last comment through Zz

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import datetime
+print(datetime.datetime.now())
