4e6530d12f9c83a80523c136bfd134ba3f45bfe0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:19:36 +0000

    fine her holdout one shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
