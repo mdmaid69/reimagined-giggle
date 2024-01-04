b9a6aa54939b9c895e20ca15075bcf481bafb8c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:36 +0000

    eager attach authority dry street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
