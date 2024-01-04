6bd09d60b01feea7478662416902028027cbd2d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:06:50 +0000

    argument plan blow contrast spring

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
