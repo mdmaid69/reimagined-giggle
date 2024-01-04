4f067a4700e6bb37d23d87317267d3715eef2123
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:20:39 +0000

    pinch right subject match roll

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
