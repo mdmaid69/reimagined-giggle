0d1b975ac09ca58b17e99b5f0d90597cb8dc3ecc
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:13:20 +0000

    disagree past criticism several cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def remove_directory(path):
+        os.rmdir(path)
