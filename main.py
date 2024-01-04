a37ab3c14f144481c3728ba81435555f02d3e371
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:40:42 +0000

    police meeting spite curious talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
