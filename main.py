dbb0e47a5f683023b70282104b89885be636b82e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:18 +0000

    children emphasize ignore content year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_arc_sine(x):
+        return math.asin(x)
