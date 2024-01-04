1ffb02c58ada2e30585db32e3c6fec7adacc12e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:08:36 +0000

    arrangement choose convinced petrol hang

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_min(lst):
+        return min(lst)
+import time
+print(time.time())
