a34667ef1091fe621cb773b5d4dd4c7fa50cb8d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:44:14 +0000

    truth winter possibility lend expensive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
