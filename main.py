ae45d500028bad9d4710617e8c7b910744663205
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:53:41 +0000

    wire one tape rest brush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
