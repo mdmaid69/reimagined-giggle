70eecae49cee7ab6d82e9c84e9cc581eccf54133
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:19:03 +0000

    normal similar comefrom example software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
