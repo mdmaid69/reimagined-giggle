9110afa8569ab8bf1edcc524ec375f8bc20f6efa
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:56:33 +0000

    present reach house normal climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_typecode(array):
+        return array.typecode
