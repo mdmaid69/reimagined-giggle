3889cdb5217296d3ef56ac37703dec2820c35522
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:45:44 +0000

    abroad chin fair generally damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def get_array_as_set(array):
+        return set(array)
