735dc1a115aefced55ac2826cd168b9903aa7f06
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:55 +0000

    voice detail probably callout strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
