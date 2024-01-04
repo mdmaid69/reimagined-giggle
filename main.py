4b3e8931f49e0d0f9e8a5f60de263ca504f7c97f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:56:32 +0000

    improvement clean dry determined suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
