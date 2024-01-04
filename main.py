35e3494a9bb93fa8dbfa9af19f767df67d5d0e79
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:29:49 +0000

    human sale car community score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_floor(x):
+        return math.floor(x)
