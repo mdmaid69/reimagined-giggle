64b40e693758d01f9902b8a6609f816e8d9fb673
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:25:36 +0000

    decision comment 568energy thinkof explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import datetime
+def get_today_date():
+        return datetime.date.today()
