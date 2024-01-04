c1535838e3cb255b7fd9cdf3f5300fb81749583a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:19 +0000

    takeoff imagine bunch formal instead

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def is_odd(n):
+        return n % 2 != 0
