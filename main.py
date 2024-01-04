cd739c2fb56356c98d2015f83738c5cd5ba576e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:32:20 +0000

    treat between stretch compare blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
