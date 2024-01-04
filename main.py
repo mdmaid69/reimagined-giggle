deea21e55f7752b9e37f0b25086313023102b426
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:24:27 +0000

    frightened sit centre bother workupto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-print(os.getcwd())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
