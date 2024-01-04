efa0d74e58a7172757b3236372e75c6d02894822
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:21:55 +0000

    evening keepout possible area practical

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import sys
+def exit_program():
+        sys.exit()
