cb04b8811cd06e60d3882f4ff3caf5bb7d80f61e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:11 +0000

    opinion Mm look straight town

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area(radius):
+        return 3.14 * radius * radius
