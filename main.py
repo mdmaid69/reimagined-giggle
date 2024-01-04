8d1dd3f46bbf11644356a4cb21c284786c053475
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:35 +0000

    chat doctor over whom snow

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
