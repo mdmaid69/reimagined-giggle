176693718174df757d3af9bd5c27a27f0cbba7ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:12:34 +0000

    police or child runout dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
