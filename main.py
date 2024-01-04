fd11822618460bf94fd44dbdac31d7920eaf6c65
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:07:31 +0000

    trade sometimes stage small achieve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
