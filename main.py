4ccc4d9501284ad21c39f4105cdc7ed6cb9e3309
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:10:18 +0000

    Dd aim warm further century

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
