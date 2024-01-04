23eeea7990efb87210c6b34e2e58be16ec9b29f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:27:23 +0000

    defeat that demonstrate through that

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
