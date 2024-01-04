38ae722a41ea7450d65a41dfbb67cebc9ebe4007
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:42 +0000

    Jj previous after scheme black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
