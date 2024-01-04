32ddc68fc255cba38865b00c7c17a34bfff717ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:36:50 +0000

    decision future Yy send partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
