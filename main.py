043d6909c0c29e8352f21c5732fa5da84e8e0082
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:19:52 +0000

    constantly partner open exercise trip

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
