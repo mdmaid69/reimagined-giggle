e1c0fc59c21bfc48b241524c839333f64ddebcbc
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:52:47 +0000

    must people laugh turnaway compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
