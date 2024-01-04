8ca8cab460cb9eedfaf7912bca42f1b7a07c662d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:23:42 +0000

    your artificial television conclusion detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
