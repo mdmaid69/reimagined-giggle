e5983e404809c421676f750b49972a1f47247a2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:41:30 +0000

    run split necessary block character

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
