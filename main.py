f8f2c157ebda1bab6b8603d2dd49b26b4630221f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:23:46 +0000

    chase injury tooth occasional machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-for i in range(5):
-        print(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
