d31cd62078741230725e179fc14bed7ce99b29e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:41:41 +0000

    tear complete flood could remark

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
