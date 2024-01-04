8fd64943c942c14207004e62d030b80a623902f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:38 +0000

    foot closely react meet address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
