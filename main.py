5edb2dc4aee2f83df5481a3f99560075846aae3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:01:44 +0000

    business mixture welcome except wing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
