de5673a87decf24deb139e7993cdf780b9341b8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:59:19 +0000

    bringforward callup thought image before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_volume(length, width, height):
+        return length * width * height
