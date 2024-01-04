a070aa80c51eab96ffa733911851d615eb0892c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:47:46 +0000

    shop contain object turnon 571enough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+print(sum(range(10)))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
