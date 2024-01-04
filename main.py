bb684b54ecf743ec7708ed611ca3ab73ad11348f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:08 +0000

    team lookaround dream open question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
