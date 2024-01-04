d4cebd1b5a704546a07f02b468ee7b0185b77aea
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:08:02 +0000

    repeat turn confuse float mysterious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def convert_to_hex(n):
+        return hex(n)
