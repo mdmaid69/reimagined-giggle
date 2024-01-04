3884ded9959671d60ec4479aca2df17e857910b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:08:19 +0000

    store interview succeed squash platform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
