7338628342a8d206a43455cf48632416f20f8ef4
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:38:15 +0000

    teach little calm drink burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-for i in range(5):
-        print(i)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
