31a5a67e5ab12cab43aad68b98cca98894227be3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:05 +0000

    part gate clothes city plan

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
