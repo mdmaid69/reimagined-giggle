8b6d86e2b475d139aefcec5becf8948ee5eb9227
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:27 +0000

    trust approve joke skirt tidy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_str(array):
+        return str(array)
