d1ec473b249b073b723ba5faf3f1f2c5e7e4f635
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:28 +0000

    pipe introduction extremely broken mistake

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def count_words(sentence):
+        return len(sentence.split())
