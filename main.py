ec538cab08c82ef591a3d81b8fb39f35efd4b56f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:06 +0000

    fire sink note weekend best

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_typecode(array):
+        return array.typecode
