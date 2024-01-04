c2eda1cde72bcb772c6ea2cefe21543d326947ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:50 +0000

    command careless standard sometimes circumstance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
