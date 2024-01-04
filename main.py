836819ce27a5035047ce0bbc4f61009d338bf70f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:24:27 +0000

    both mad smooth off born

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def subtract_numbers(x, y):
+        return x - y
