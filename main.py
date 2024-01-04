b7f70923c954e0fca5eb4d1b6ffc72865601693a
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:54:38 +0000

    various board pure generous towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
