c477f489ad937afbbf7cb45d59dea5568b0583bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:47:25 +0000

    wish theirs strict concentration weather

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+print("Hello, world!")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
