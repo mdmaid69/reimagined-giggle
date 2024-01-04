92bf574401ef5252456cec76d471ec8b2abf5387
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:13:45 +0000

    reserve damage successful back once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_density(mass, volume):
+        return mass / volume
