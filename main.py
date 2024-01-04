83051d539b0907a983e3a1bfd4b9e05ffff76ea2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:10:34 +0000

    advise computer lookaround centre gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
