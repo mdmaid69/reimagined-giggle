5b7ae490cd3a157be8f5bb6bbf314aa11b151d0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:02 +0000

    official available copy game demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
