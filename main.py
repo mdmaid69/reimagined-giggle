a9af8b12707e96c6df4830b63537ca99360f8127
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:17 +0000

    model suddenly that do base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import time
+def get_current_time():
+        return time.ctime()
