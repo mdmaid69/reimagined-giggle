f74de61c9cc9049a7eebb9da32f891df01cec9d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:54:20 +0000

    arrange lookup reasonable education run

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
