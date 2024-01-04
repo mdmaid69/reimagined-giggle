d5fed30e8de428cf39b6bf44400b860edf537952
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:23:10 +0000

    warning worth acceptable platform invite

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
