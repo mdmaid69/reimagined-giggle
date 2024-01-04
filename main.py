4ac8cd7dc02aaec5c5bd51772e861080dff8ed53
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:08:47 +0000

    petrol month effort dance convincing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
