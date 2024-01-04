29aa41edf2d80fc5991ca7ebe255d412ffaf5d1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:11 +0000

    ground flat some lonely dull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
