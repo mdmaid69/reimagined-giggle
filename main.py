37b1f5a1442efa5250f3321febe3a05ec79c1947
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:09:31 +0000

    this start other freeze crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+import platform
+def get_python_version():
+        return platform.python_version()
+def find_max(lst):
+        return max(lst)
