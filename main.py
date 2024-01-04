3aeac20b9733028638d5f9945f279b35ddf02c51
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:43 +0000

    breakout mess cool lend joke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
