b689e61a0e3bf765966c1b16ba45c517548c42d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:45 +0000

    port row about blame make

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
