c8a6e4d4a65fafb17e4c12ccc8b8d63cbbc3acc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:23:23 +0000

    last battery national unfortunate be

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def count_words(sentence):
+        return len(sentence.split())
