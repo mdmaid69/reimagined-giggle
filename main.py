49008dc740f7b3f640e0d13adac28bee3ab9ecac
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:23 +0000

    behind yes edge giveup store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
