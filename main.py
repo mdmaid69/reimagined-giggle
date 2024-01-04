84deb916c715677f107bd10482bf8d5009634949
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:15:40 +0000

    past tend leg certain single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+x = 10
+y = 20
+print("Sum:", x + y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
