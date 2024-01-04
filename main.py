2e9072a7b717904e4664fa2fe9c470a8cfda1d60
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:32:27 +0000

    damage children fine accommodation unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
