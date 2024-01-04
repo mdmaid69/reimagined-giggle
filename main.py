8f600aa18b6c194f839b26366ab31b853be7699e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:13:53 +0000

    mess male proof off rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
