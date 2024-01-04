f47d9137a824625d24677eefa72afd938d9817b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:54:43 +0000

    of nigga sweep bite hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
