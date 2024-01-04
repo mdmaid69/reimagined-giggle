d5e117b3f193e79d809c270b8dde45fe507176a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:36:19 +0000

    crisis get communication thanks fortunate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def clear_array(array):
+        array *= 0
