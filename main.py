08541cb5f95dc4214b97f9bbdec5401c88848d5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:37:17 +0000

    collect estimate unlucky pair stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
