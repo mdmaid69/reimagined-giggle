e3c462892f97cb3d17f4f4980bf17d8cdf710736
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:52:08 +0000

    combine rent way sometimes desperate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
