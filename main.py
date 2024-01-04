f31278c522eab7149334d3f3c40625e6048cb68e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:02:50 +0000

    world government impressed independent spare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import re
+print(re.match("h.*o", "hello world"))
+  def multiply_numbers(x, y):
+        return x * y
