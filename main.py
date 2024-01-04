a2dbd9f8bbca972c4a9821631973fc53611181fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:04 +0000

    key exist one land shelter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def square_number(x):
+        return x**2
+def find_max(lst):
+        return max(lst)
