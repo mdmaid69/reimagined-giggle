2caa30431141b269340622bc655842a7f6de3ab0
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:54:41 +0000

    honest blame wind me reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
