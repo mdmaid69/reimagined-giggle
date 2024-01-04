d8bf8ac431baf6c710af2dea93a93cf8a9a09d6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:04 +0000

    match splash probably through meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_even(n):
-        return n % 2 == 0
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
