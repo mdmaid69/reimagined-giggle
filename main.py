dbb1ca80691e97e277271059b3e2e0a15d0ee3f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:34 +0000

    number oil key turninto our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_time_since_epoch():
+        return time.time()
