d7985c0c7456536135a177c6177a27c1ce2fe5cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:12 +0000

    dangerous record blind feature interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
