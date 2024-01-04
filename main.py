cd85cb7b826d6ce56a7713852f1f56d5b62664b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:29 +0000

    expression turnoff form in quantity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def is_palindrome(s):
+        return s == s[::-1]
+  def convert_to_hex(n):
+        return hex(n)
