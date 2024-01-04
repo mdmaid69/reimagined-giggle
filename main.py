a4c4f20b4241b7f09730a296d52c79ad2051473e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:19:17 +0000

    rob sharp spend well friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
