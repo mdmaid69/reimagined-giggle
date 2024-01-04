636e801b1e1e35e01740d847545c1836fb9bb7eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:00:48 +0000

    coffee possible proud fork shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
