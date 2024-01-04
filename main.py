4b74bd564eec3d9fd180d48f53103739c8d57343
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:51:03 +0000

    admit prepare forgive stay national

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
