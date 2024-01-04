1b784da0fcf6131e67e46e3fb9bad96e757057b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:11:13 +0000

    mainly true piece valuable demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import datetime
+def get_today_date():
+        return datetime.date.today()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
