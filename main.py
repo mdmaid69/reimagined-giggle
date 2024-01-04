29ede2759a40da36f1691a54a6ae699d4aca7c39
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:42:24 +0000

    steer bottom exact bath week

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
