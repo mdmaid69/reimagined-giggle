908eb4a6dbed7c5c3cb9c529e1a25750cfbcec37
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:29:42 +0000

    sort frequent experiment first them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
