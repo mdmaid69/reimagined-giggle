d799638a5a27c690164828d6c1dfa6e6a0e835aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:11 +0000

    run tidy help talkoutof workon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def sort_list(lst):
+        return sorted(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
