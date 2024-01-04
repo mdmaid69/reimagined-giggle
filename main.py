1b9075a906cb115d1b4a8f5db7ef7cefd15fcd73
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:40:22 +0000

    bank conclusion thought receive another

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
