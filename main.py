36a0b81474df294272877e125b724b5c820ba6d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:02:01 +0000

    achievement available the disapprove spring

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
