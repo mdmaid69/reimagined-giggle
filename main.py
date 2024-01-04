0cfbb50b4ca2d418f3b426aabb718a45b2c405d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:26:43 +0000

    have crazy takeover miss go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
