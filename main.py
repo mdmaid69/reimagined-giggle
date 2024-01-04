225a445c49a43aeae60e33bfd029bd10d3ba6504
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:11 +0000

    travel spite settle marry achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def is_even(n):
+        return n % 2 == 0
