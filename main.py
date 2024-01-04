2e64a68ddce8150af10b3a8abce6e4669c8ea7e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:34:41 +0000

    vanish type round handle physical

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def append_to_array(array, item):
+        array.append(item)
