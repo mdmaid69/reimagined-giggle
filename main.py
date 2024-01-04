f90b7c13941cf94471f8eaf2417f18b28ffa8f93
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:07:03 +0000

    travel settle hurt callat might

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
 import array
 def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
+def calculate_speed(distance, time):
+        return distance / time
