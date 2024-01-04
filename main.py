f110b6ab7607695ea2eb2ee4cfb5ff4b9de1d93a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:26:39 +0000

    cough him debt discover everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
