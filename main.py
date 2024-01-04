e57fa7e50ed16d4e1b08c50d281f674c7353bc48
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:49 +0000

    his illness catch tidy only

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_counter():
+        return collections.Counter()
