e057cfa8e0e425960b2e4f76df35ac3cbe2e66f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:24:37 +0000

    different exist will comefrom rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
