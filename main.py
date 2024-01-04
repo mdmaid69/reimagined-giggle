21f6cfd3cdf55ebc3e0689c90d577bfe256dd7d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:24:45 +0000

    file trap smell across worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_pressure(force, area):
+        return force / area
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
