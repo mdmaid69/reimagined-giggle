2ad7c2f3a3e4069e7b643c53d0f3af6bd0650d6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:40 +0000

    typical strong heart evidence benefit

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+print([x**2 for x in range(10)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
