4c4356fc3a257ab74e63c1dba0904bdfb013eefc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:30 +0000

    profit upwards question see trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
