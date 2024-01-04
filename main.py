b42eca40d0196a608ae6d35de929b19d2591e718
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:20:04 +0000

    impatient bottom bit deliberate century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
