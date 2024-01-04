ce471282ab85647cdbcef630be545f75940d438c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:15 +0000

    freedom fat peace clear left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def reverse_array(array):
+        array.reverse()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
