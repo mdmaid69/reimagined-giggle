376910d912de3a6b6c6d9fd5324d665531f7e152
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:22 +0000

    right callout promise draw declare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
