2b213a3c1de626d23edcfcd055e7586ae5861c44
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:40 +0000

    stand deliberate temper base by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
