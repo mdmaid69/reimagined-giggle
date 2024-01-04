842d0ca0d8a6818ea881cc73702aa81f0ea7eff8
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:29:40 +0000

    couple joke disappointed copy mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
