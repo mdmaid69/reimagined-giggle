fd7d05250157a7d5549f059f8c104a3c70803111
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:23:05 +0000

    hair remember favour workupto too

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
