e306ec8b408827f006d2d734f3bf1db7b00f0709
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:12:02 +0000

    depend embarrassing check without environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
