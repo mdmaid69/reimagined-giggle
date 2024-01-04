637193a713187f9a39278698c1a01cedbc1274fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:41:03 +0000

    break boil shame should wall

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
