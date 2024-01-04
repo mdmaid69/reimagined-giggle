059cd60410f12a7fdfa474ce242244487b3e2d53
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:10:57 +0000

    second Cc ticket success ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
