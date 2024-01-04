154c9b77b8aa48b3ee9a067e550936e959756c2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:20:48 +0000

    name invite stranger defend makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def sort_list(lst):
+        return sorted(lst)
+import time
+def get_time_since_epoch():
+        return time.time()
