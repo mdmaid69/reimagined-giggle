e2a6be6b9d5c48695cc93dd43dbb3dd80c7a837f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:26:00 +0000

    recovery port cope regional ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
