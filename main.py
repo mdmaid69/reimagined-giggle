07cb9d1ff836cdeab1409ef2510bf73a6bbd9d07
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:47:31 +0000

    around callout noise behaviour oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
