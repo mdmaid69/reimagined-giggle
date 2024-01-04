1d1b739a99d5e0d52d01c71ecb7905c2b10a3eec
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:03 +0000

    finish no guess practical react

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
