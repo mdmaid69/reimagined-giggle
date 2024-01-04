d9e2ed839408dc9d6e975e07f5b64b33581ed026
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:45:48 +0000

    laugh wind treat gather ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
