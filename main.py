8d38d2a708c6873aaec585ac264fbb77bacd18e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:37:06 +0000

    responsibility active support loose side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_area(radius):
+        return 3.14 * radius * radius
