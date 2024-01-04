0f76ebb3eac0a5deb3031f3f05565b0698a7800b
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:40:19 +0000

    accommodation used certainly swimming game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
