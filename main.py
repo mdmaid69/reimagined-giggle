adbfae2c8225195ee00ccd6f235c62469f0b30b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:51 +0000

    anybody young minute crisis tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
