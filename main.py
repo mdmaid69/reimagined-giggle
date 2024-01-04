be6cd582547b9cc0f34f30d1b1dabea980e5a1d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:15:32 +0000

    slip typical shine fall disagree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def count_elements(lst):
-        return len(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
