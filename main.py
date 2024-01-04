19c6a12d603e38e11b940c0897710beced3bb8be
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:39 +0000

    runout tape field nonsense month

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+print(time.time())
