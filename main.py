1ab13cfc8d6ea1f2374e21a12fcc1e128cf878d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:16:02 +0000

    interrupt term instrument goaway turnaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
