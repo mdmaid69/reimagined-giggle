66db7187e0afae26c60a69b380a9be68fdfb8fb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:59:21 +0000

    comeout Hh block forward we

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-print(time.time())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
