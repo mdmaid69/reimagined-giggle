6144aa38a55f125a55ba5da1c03545a2a4ec2f3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:13:14 +0000

    past good estimate makeup birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
