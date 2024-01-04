4bde5e1537b8aa9609d4ebfe04226a503151a51c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:50:30 +0000

    wave contribute performance slight backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
