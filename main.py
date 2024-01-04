b7a20c2e2a9f7fd7e1ffd2183ea3c194dac2c092
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:34:40 +0000

    pot edge unfortunate relax influence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
