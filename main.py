ec1793086e01e3a6e20f2db2397eec51826f63e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:44 +0000

    send past clean somehow strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
