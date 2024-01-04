aaff13b8e5f452001c313a45ac8b32c9577cbbee
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:09:03 +0000

    unlikely hang late any free

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_as_int(array):
+        return int(array[0])
