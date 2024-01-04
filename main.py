6622074a20e4e1a32214525ac9359b1ac6ab4b6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:10 +0000

    plenty hardly play pleasure wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
