6f1f6447bb3c7466c594967754659f158dc45169
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:21:32 +0000

    certain square lay snow one

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
