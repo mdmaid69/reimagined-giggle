c5b47d30affe62d4fa67ad17a7828d608041cb70
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:38:54 +0000

    wire lookaround remain against march

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
