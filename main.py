002e4b637dab81649cea3d64ece7c394f8413746
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:39:18 +0000

    favour let come hardly frequent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def count_elements(lst):
-        return len(lst)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
