cd2042f0b427e83501b15dca0085bb162b66531b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:16 +0000

    store hit talkback show busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_power(work, time):
-        return work / time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
