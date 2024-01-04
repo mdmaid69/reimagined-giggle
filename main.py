b67c3aec5050fb4f447b3925095f4b059e5a8561
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:13:35 +0000

    theirs season distant note Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
