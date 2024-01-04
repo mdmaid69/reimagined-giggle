2548cf1c5a860eead1efee3d71d672f8f7ccb9f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:52:46 +0000

    like style scrape soak shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
