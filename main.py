ec92379b8e193494be7a5226a4aeb32c86d2c028
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:16:45 +0000

    and opportunity dry flat peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def get_current_time():
+        return time.time()
