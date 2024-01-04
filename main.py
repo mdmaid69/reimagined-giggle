00d29dab057fbb28e09a27a46265d499731e193f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:32 +0000

    occasion basis rain treatment Bb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+for i in range(5):
+        print(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
