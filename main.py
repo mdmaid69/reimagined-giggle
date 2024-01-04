c024c3476b0a6b739a984d7ea6ed13f455739b79
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:22 +0000

    over gate treatment gooff hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+name = "Python"
+print("Hello,", name)
