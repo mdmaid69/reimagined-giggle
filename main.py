8915d05cae9eade32a84f6d226a12ecf2c868d0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:24:27 +0000

    surprising see load open beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_int(array):
+        return int(array[0])
