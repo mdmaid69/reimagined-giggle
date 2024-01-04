0f9ec8cbfc4a3dd2a8f9a4c2abf480554270017d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:53:11 +0000

    size accidental control cost relation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
