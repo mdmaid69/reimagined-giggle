d6d0dab5a34863607053f74a0cc75a330a2e7114
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:17 +0000

    discuss scheme train contain memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
