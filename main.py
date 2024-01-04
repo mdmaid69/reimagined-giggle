f2b769dd35209c017b1fe8ee20e40c9ce183b819
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:57 +0000

    rubbish fight Oo place jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
