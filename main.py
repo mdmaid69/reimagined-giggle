61e060b066ba6e5b089d3efd4fc3e22fdecd8eb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:40:17 +0000

    feed argument union live sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
+import datetime
+def get_today_date():
+        return datetime.date.today()
