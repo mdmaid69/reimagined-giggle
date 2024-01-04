cd3da55e72095823f828f9d73cd864ecf53b4807
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:27:27 +0000

    clear risk watch hello grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def multiply_numbers(x, y):
+        return x * y
