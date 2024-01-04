6ff0fe35678ce9021cc2335cd47a97459c454875
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:14:31 +0000

    limit father weigh trip obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def subtract_numbers(x, y):
+        return x - y
