636a9e2fccb6e86bd31bcf2d550d12c08775cd32
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:26:32 +0000

    hold introduction adjective remember stare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
