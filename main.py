a015e2101d8adaf415eee2cabe0fa93e05dc8fca
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:33:27 +0000

    breakdown minute represent Tt correct

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
