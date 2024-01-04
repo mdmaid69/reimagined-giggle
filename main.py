136656df6afb1219120b3b85e93bd8147650429e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:35:14 +0000

    father naturally reliable note burst

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
