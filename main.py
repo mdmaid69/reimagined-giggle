37d63eb8edfbea4dfa962788481e6a64ca0e9f0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:49:23 +0000

    sweet variety comfort gate brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
