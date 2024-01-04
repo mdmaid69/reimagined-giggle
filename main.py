2af850373e6beed31e6cf461818a3b4a8ee17b66
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:43 +0000

    record pump believe old considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
