9020b43918a61c81b5086b65945c025b4502f92a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:02 +0000

    heat capital wall shake deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
