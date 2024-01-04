fec4db92524b5218239f424d969293542796a47c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:46:08 +0000

    cap during agree exam walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
