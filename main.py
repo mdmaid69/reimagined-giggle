f99b3833e6931963004272ff23867cca38fdb89c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:44 +0000

    theirs after book emergency score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_power(work, time):
+        return work / time
