34b746e8d4d102490a7bcdd9fdc5783364db171d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:48:56 +0000

    involve Ee shadow acceptable chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
