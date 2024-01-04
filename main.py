6ac833c0701c284510818fd65652de47d17ff7d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:09:12 +0000

    minimum Bb about ground good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
