83bb7bb32cec95b05bcc1a3c8fe5c34ef412a74a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:04:56 +0000

    once tone care case church

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
