56fa29f3ab17ce082b9f3e72c7b22c880a9fd482
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:53:53 +0000

    achieve present reduce doctor principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
