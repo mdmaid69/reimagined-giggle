4a28010ff231c4da83166160819670f08273fbcb
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:22:30 +0000

    ticket bus black begin accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
