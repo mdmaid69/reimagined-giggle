1e7f9395f2678c1550028d2a1a177d74a19ef7a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:02:33 +0000

    fly funny represent worry ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
