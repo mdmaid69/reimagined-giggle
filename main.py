3c33c105aebb148be598f04fa00f287f19673839
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:26:50 +0000

    my attempt watch wash go

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_rectangle(l, w):
+        return l * w
