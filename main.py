e430c896baafa4b6cbbdd69de75b142bef7c4911
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:37 +0000

    pity admire fortunate murder activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sys
-def exit_program():
-        sys.exit()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
