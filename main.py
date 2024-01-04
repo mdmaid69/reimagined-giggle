01d0c8bd1e6362c4165c81dd5864960e1b950af8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:34 +0000

    debt test body comfort straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_octal(n):
-        return oct(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_queue():
+        return collections.deque()
