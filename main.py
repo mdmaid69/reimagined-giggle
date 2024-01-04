ea7186a7b1ab664e1a2d31d681c74335c3034b01
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:43 +0000

    defeat opinion glad cheque simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
