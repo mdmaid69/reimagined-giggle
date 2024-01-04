2af9f64e11329cec1c9fec27c83ef58716471836
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:44:21 +0000

    fact Qq estimate convince join

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
