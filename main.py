d5cc303e13cfed21f97d5473cd63775bf3942146
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:40:26 +0000

    section cause present immediately whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
