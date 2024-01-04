4af8e81b141cbeb5cae01451669dcabca9461dc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:50 +0000

    noone tomorrow about school key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
