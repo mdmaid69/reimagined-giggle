18a23dba58eddd830c719c7c407f74345b9fe41b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:21:05 +0000

    mother drive stretch complicated refuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
