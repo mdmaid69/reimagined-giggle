4a347e8a627add9fb619fe8c8f906041dd317b02
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:31 +0000

    breakout glass woman Mm imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import re
+print(re.match("h.*o", "hello world"))
