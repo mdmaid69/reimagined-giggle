a3e8cba44ec1f061fc5e1e2162e09e4b87450272
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:00:29 +0000

    holdup powerful able brave quick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def sort_list(lst):
+        return sorted(lst)
+def find_min(numbers):
+        return min(numbers)
