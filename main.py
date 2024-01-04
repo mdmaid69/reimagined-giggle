adab4ec11e251573947ac271d4ee6db2cac830af
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:55:07 +0000

    sharp nurse project interfere creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
