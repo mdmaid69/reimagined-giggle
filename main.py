0bf1fa45facb6b4ff9ad13ea55f1081fdc65728e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:50 +0000

    butter thankyou tape picture especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
