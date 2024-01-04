1fc4fbc0d143b819963da21a1702b094adf43e77
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:21:13 +0000

    country both possible transport minor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_to_octal(n):
+        return oct(n)
+def find_max(lst):
+        return max(lst)
