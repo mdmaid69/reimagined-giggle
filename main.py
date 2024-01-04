0d7e2dc36a048a1135426974bd56b5ff9d54ab14
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:40 +0000

    form comeout clearly flat after

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
