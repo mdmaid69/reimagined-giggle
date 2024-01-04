724d98997e7fe372612a30c94716b0aa4b273393
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:06 +0000

    hello intelligent information whether outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
