73dffd49f2024960211357bef0e72a4519018bbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:58 +0000

    wave influence storm movement out

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def append_to_array(array, item):
+        array.append(item)
+import numpy as np
+print(np.array([1, 2, 3]))
