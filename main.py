325d5af639f0a74e0d86c1f9ae38600282850d69
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:35:18 +0000

    water rest effort file vision

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
