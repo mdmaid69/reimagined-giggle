44beb02a04da85c6a6cc08420f828cd27cb11bd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:20:43 +0000

    reach Mm may fast trust

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
