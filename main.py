47ab32f43158afac76947461de5cb9243b057a6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:02:51 +0000

    work university minimum information scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import numpy as np
+print(np.array([1, 2, 3]))
