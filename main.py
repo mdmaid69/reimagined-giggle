a983e48d29f832dca24146bc42f9caf19f6d7725
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:17:49 +0000

    block surface improvement behave whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
