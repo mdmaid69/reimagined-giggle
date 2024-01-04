2a2e3a01f08f91c79517bb2d855dcaca4b8a27f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:33:28 +0000

    neat future shout obvious show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
