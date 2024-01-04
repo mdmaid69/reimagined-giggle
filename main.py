e104f182bd9bd4dc5738954229ba742934e70397
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:40:43 +0000

    awake estimate 564encourage information cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
