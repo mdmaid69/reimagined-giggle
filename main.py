116920f39ad72917abdf743ce07d9471071c0eea
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:14:07 +0000

    fire pour illness tear tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
