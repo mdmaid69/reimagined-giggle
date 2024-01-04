201817ad9df586b494006ed271f1e2b65521e904
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:46:23 +0000

    advantage bring bet fire afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
