9511d53ab8ae91c12a8e6bd3853f345b10083a2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:17 +0000

    story bet light succeed breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
