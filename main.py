940144fe98dcb6bf16e9b1d12b27bf064d95f60e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:13:45 +0000

    same call destroy listenfor alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
