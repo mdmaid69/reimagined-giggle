e649a9a8d4dffba7dadb4f1f2a9b7660b1d169d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:52:58 +0000

    normal rub along board understand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def is_odd(n):
+        return n % 2 != 0
+import sys
+def exit_program():
+        sys.exit()
