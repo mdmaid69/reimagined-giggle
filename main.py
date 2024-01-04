797dd9211384ddc36758536f646c46f12090b415
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:31 +0000

    moment suit Ee sign wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
