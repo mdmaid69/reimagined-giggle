0c671e43520874ec2a2c6dff83fc6f0f335d78e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:25:16 +0000

    risk grade perhaps content show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
