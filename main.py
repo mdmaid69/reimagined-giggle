eecb3bbe895afa14932928c2c903aca222d46972
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:46:31 +0000

    runinto mistake smoke confused who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
