feeced48c4603e319e994fa01b8f684e27f03b5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:06:37 +0000

    brown joke owe count complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_queue():
+        return collections.deque()
