f7d5844d22a7f3c4197ff36ffd6365c10464a5ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:17:34 +0000

    appearance buy fit except geton

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
