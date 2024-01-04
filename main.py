7b963b68e0c401bad40e38eeb8e1a84cbe0e39df
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:19:43 +0000

    door excuse give raw mixture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_string_from_array(array):
+        return array.tobytes()
