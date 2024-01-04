8c8a4bcda44ec8361f9916edcc3c618a089032a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:55:51 +0000

    challenge lesson thank true speak

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_area_circle(r):
+        return 3.14 * r**2
