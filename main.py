bd8ace3913549fd2d6f78f09c2501e16f3ff8ac6
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:20:49 +0000

    date party office means dangerous

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_sine(x):
+        return math.sin(x)
