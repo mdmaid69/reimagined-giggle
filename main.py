15f864ae7e793267e921f3c4ddf30fc6f31fd1ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:09 +0000

    beside usual move there freeze

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
