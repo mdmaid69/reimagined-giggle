b45d2f1ff7597a951df7cacdb49c827f992fe947
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:59 +0000

    base from boil neck success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
