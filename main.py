264c57bfdd30df850ba33599562cd107740e4aab
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:58:13 +0000

    introduction formal interruption immediately complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
