d9f75313a8f352446c73983d4ed44a026502aa35
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:39 +0000

    better pressure basic health reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
