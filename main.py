0b0407b2e86a8b6d1d24d454bc77e2ecb938f863
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:49:30 +0000

    unhappy mean also relax claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
