23e903fd0c5c6df5f0054ccb86326ba9fb4ac3ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:15:03 +0000

    unkind boat permission dance myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
