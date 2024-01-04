41d8c81f10e0e2e4e08f2a88719b1c4f83588933
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:25 +0000

    but low crash need stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
