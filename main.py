d22868132252790c2358c79f92ba3ad5eb579e81
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:01:10 +0000

    duty measure miss through thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
