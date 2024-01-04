33c153f208574288b222094f15fc7353dcfb8cca
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:54:54 +0000

    whom available awake broad bend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_even(n):
+        return n % 2 == 0
