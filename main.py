5be5f95e11311859e131a7cbee3bcd0604b488f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:00 +0000

    past month admit doubt welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
