0c13ee5c03c1ec979eab285c8e0e51fbb4eb80a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:27 +0000

    blind fasten personal suit holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
