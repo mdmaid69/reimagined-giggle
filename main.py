983bacf32ac9fef60c83523ee3fee0cab1d63987
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:31:23 +0000

    giveaway patience grade holdout debt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
