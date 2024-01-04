d892e2776486a907eccb56bba5f42f3dcb6b5f39
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:41:51 +0000

    him invite unpleasant pressure callat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
