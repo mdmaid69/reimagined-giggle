e54f12506f5b9eb60b26cb640431ac042f143eec
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:03 +0000

    reserve of correction team debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_memoryview(array):
+        return memoryview(array)
