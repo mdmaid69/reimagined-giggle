8e3f1384cc6bb475788fb7b331da44c8ea76f09d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:15 +0000

    wonder board calloff figure flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-print(math.pi)
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_area_rectangle(l, w):
+        return l * w
