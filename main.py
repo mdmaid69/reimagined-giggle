42af98ba7a38a6ea9469445506d0140c188d44a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:27 +0000

    damage around free behaviour wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
