b8db1b10c42c5197ec2488b86ba5e41a1be762db
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:39 +0000

    correct contrast forgive since rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
