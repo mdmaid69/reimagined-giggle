9fbef1c0641cf4a953fc44c7b517c5b99d3fab0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:28:56 +0000

    some borrow support plate tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
