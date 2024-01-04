1e208bb2c4dff0aeeb9ff61a8c56e866c12247ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:37 +0000

    tone acceptable person prize speech

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
