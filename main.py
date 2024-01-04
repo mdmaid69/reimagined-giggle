069d7ec20ebf41dd8823fff533c6b68a6502dfc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:28 +0000

    book address accept pronounce achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_set(array):
+        return set(array)
