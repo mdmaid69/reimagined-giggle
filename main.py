c2e6554ac6c0b6a1277040311ceb573a978146cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:59:22 +0000

    cost border office correct unlikely

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
