a879300897ecfc27d53961883ae7041054fbb736
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:54:52 +0000

    minority shape sudden fight fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
