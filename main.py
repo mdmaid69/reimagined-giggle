c8b9212d69bb341a0afa349df08c8283a058e3fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:24 +0000

    opposite second trap bet smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def reverse_string(s):
+        return s[::-1]
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
