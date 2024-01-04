8eb540421ab6d089a8fec2dceb8854bfa41ed7c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:51:58 +0000

    rid everywhere sound tense one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
