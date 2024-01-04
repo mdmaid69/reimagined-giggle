4b6a752c194dc9d5dd88d5f4f640727a94c6f5da
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:51 +0000

    conduct decide worth save centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
