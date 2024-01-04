32fc6abc2c09db4db5e2ed1d3e4164b06135e875
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:01:46 +0000

    workupto confident behaviour connection freeze

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def change_working_directory(path):
+        os.chdir(path)
