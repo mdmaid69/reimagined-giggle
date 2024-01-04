0c0ba9b02832b003f6fbf14d1c53ccbf65d46ffb
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:53 +0000

    565end scared band blame pupil

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_index(array, item):
-        return array.index(item)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
