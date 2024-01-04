96bd30411c066738fc036c0473b33e6a0ef4c8ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:59 +0000

    wheel slice important name standard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
