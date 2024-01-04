f8294cf2cb2adabc94337f5a6914a230c59619a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:09:53 +0000

    occur must face wander invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def subtract_numbers(x, y):
+        return x - y
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
