a6065a045de9ff74d4f1a8b9b877a224971e8c3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:48:49 +0000

    modern talkback Cc trip risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
