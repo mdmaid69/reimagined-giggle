10378d98c41934e2a9782f598d6d215da3774886
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:17:45 +0000

    twist sharp Bb hope delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
