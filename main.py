c23b66292be105c6426f0459ed0a67dcda2f0ab9
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:10:27 +0000

    thanks apparently right unlucky cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_str(array):
+        return str(array)
