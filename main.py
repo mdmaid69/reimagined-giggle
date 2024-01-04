48089b10a2a5b8724e1705c18c2d2c6383b36355
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:48:14 +0000

    disappear track that tape somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
