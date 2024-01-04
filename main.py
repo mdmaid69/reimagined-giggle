2a075fd3ea827b5baa786d94078395049dcb5b45
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:06 +0000

    unlikely create furniture takeout deep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
