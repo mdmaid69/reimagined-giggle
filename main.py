0a35fe155632fe99f91edcc7a9c288394a8b62ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:03:07 +0000

    share repair gather again introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def append_to_array(array, item):
+        array.append(item)
