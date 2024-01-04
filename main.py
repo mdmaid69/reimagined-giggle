fc912a71818a5ea66c3ebe7e8131773e4fecea43
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:53:25 +0000

    see admire break below characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
