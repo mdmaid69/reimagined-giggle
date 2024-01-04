69bd3135077ce9a85c474beb1be6459a5fa9a74b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:58:27 +0000

    weekend final well search convenient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import os
-print(os.getcwd())
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
