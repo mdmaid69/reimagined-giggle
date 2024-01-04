b0cea5e9947d756e0610bf86aff8d0e8b8afaef9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:51 +0000

    discussion judge command tool approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_time_since_epoch():
+        return time.time()
