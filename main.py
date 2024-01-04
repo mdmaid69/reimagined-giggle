c9774d7efe9d4ec3f1971bcc62c4ad2a7e06f161
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:24:32 +0000

    myself tall declaration whatever spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
