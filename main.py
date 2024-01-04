2d94cd76d2d3e5da88ccc96281af4e2174792e56
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:41:59 +0000

    insist basis woman off holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
