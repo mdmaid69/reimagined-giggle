3899b605424cbf77d1333f3623b541a7d2ef726f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:07:56 +0000

    cruelty convenient breakthrough spring pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
