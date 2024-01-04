cebfc2c70714fb5d653ed42a12d251c312c83e41
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:15:56 +0000

    breakfast single sticky protest obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
