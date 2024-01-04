7793c3ca9c6d0ea28bd7897f6e3db240b699ff73
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:26:39 +0000

    branch protect party mail taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_bool(array):
+        return bool(array)
