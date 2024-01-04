5972dc6fb95ecbc222eb2cb0ee93093b65b457bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:36 +0000

    relax woman television forget afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
