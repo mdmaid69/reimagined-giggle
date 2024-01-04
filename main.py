fbeaf58b3500324815efbc13b465b91bc55123e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:08 +0000

    after flat move gather shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_float(array):
+        return float(array[0])
