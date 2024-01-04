48467dff86617ac19348323c48606f5db5234c74
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:19:29 +0000

    back tooth everyone tense continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import random
-print(random.randint(0, 100))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
