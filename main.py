ee58476d49edbd139e187a75f5d004f955984ec0
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:29:24 +0000

    creature minor Dd range rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
