d460d9abd9ae31d32219c8217601ba644f729889
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:27:57 +0000

    beside imagine force suffer busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
