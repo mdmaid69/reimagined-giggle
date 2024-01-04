0516ea62120bbd15a0a7254c5e93794c0de5ec0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:20:11 +0000

    entrance immediate use grand round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
