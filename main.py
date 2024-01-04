ac3233ccc670bd28115ea183f3e5245fdbe8a790
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:21 +0000

    quick separate danger persuade break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
