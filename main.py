250aac63a23300fb78a01de07c3d94eb4845ba24
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:19:42 +0000

    ring breakinto proof rent callup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def multiply_numbers(x, y):
+        return x * y
