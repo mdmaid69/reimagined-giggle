022d41e3fb0f534d3d35c02536c21fd2548f1438
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:38 +0000

    other hit mean mysterious uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
