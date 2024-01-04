7dadc541dfb228170a8c04d65a15808a2599a19b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:15 +0000

    discover row rare shop rarely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_counter():
-        return collections.Counter()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
