a48691bf28a23387f9090a6a1cf353ee421bec65
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:27 +0000

    reasonable behind deal bet wind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def add_numbers(a, b):
+        return a + b
