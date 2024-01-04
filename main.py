a5e42d5ee1624d279c4c8232be6d7e80317a5604
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:46:20 +0000

    complete keepout gather callat satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
