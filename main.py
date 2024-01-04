558ff14a00a0da541593bb8e57a4a98e9f2adfd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:52:18 +0000

    score smooth against uncertain smile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
