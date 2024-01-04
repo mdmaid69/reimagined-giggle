7d26cf1d1b146dd5cc49106f9fc1b26266081861
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:43:07 +0000

    convinced offer book urgent sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-print(datetime.datetime.now())
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
