5e96c964ba7b0e7b42266cf72b51787e58c27ecf
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:04 +0000

    clothes future storm may wash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
