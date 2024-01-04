9fd28a24dfc3cda27e6ccabf5a46d508da2bc05c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:01 +0000

    weather nerve getback cry opinion

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
