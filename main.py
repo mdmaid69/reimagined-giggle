2c28b6d0742bb60c6dcf0831110605d9c63308dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:02:02 +0000

    independent lock meanwhile back shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-name = "Python"
-print("Hello,", name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
