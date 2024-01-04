8b3f61d9d7bde77e58ce6cd3f3038ed3be2d0d4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:10:31 +0000

    promise legal mail warm impression

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
