68ed6e42b1b3eb9ee56f35cfad381a9d043c8922
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:06 +0000

    keep cook either call responsibility

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
