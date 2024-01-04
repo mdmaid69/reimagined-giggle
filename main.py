2faf28dc62afda9ced8b86d4959bdcf9322be227
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:49:32 +0000

    freeze past father luck carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
