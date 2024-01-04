a0517b23b373d6227ff8307941f38845cd73b1d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:29:18 +0000

    married comment shop company of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
