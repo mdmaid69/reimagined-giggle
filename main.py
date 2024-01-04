e1dc09affe3f3f6ca8c19352dc4c901d776f915b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:46:02 +0000

    true Gg artificial meeting listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_int(array):
+        return int(array[0])
