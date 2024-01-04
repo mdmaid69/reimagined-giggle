a412b6212c0c472d5253b71e2c57d034b9c111d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:10:13 +0000

    attitude cheap machine travel catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
