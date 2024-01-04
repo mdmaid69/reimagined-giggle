c97eb99d8c8e5002f42a2828b1ca647231a0a513
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:45:46 +0000

    occasional organization alive reply criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
