97edd8c2976097a7bcb37fbe175ea8e0d31677e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:55:53 +0000

    slight myself like quality project

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
