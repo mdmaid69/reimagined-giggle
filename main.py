1860b517a3caed1a2754eee6d4ca131c02f0c3b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:42:22 +0000

    test bend home suit blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def add_numbers(a, b):
+        return a + b
