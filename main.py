8e847ad257acf9dec84ac4339d500caef1447e0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:10 +0000

    water taste nerve Pp certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import os
+print(os.getcwd())
