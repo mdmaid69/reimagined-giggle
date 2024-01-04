2a7addd1888b17737645048c4934b6415104402e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:05 +0000

    substance eat rub relative pride

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def is_palindrome(s):
+        return s == s[::-1]
+def reverse_list(lst):
+        return lst[::-1]
