790f61c4c0f9f73a5906741156bfb1f47bf57fa8
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:55:13 +0000

    level can aim prefer interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+for i in range(10): print(i)
