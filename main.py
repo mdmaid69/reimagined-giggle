cf96b73b4512aee41ee1c5d6a7fad358d36f171b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:59:53 +0000

    bury then pay middle accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
