4afe4d927c047df42f43da01c51ca25062dc2e1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:29 +0000

    habit open zero science myself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
