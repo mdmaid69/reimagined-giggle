e4fe92b3a93d7e778752139a00abd7f536d57fb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:05 +0000

    cause begin attitude friend worried

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
