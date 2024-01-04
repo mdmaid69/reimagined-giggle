98807db6c538d716dd2c0c84ce1e5711bff98381
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:44:42 +0000

    relax defeat change interfere interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
