efbf24dd93d31e8d6c286534fa4e1205ffb2b3cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:32:06 +0000

    holiday interesting warning create deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def subtract_numbers(x, y):
-        return x - y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def append_to_array(array, item):
+        array.append(item)
