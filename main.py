da15280fbb47f27f2c0e7c0dca6b3c4572d26539
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:34 +0000

    rid deliberately centimetre danger effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
