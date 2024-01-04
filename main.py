af2846bee22276ae946aae20dda7a109515b6a86
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:44:58 +0000

    disturb taste wonder swallow reason

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
