6087be04b63c7f912f6c3c22638aa83fc7fed4d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:12:58 +0000

    workon doubt tell smile bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
