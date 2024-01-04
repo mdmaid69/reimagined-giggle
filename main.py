42aa73a8d5a94ca448725cb297a164b7ad55d9b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:01:39 +0000

    right squeeze season pollution order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
