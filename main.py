ffdfe3b0a93cab2c73f2e3111694a21ac7d7281f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:26:22 +0000

    special mean disagree rarely brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
