37f4deddb1d6bcbe6ddab7ca9914e96da892bd1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:58:37 +0000

    second wire steam change bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
