765edda15e58472f73317000a91aba4614f51123
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:06:24 +0000

    variety save order how satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+print(re.match("h.*o", "hello world"))
