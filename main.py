bff7d95690f8efa1669bcf9b0e204e5e158658e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:29:47 +0000

    within outside hit firm black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
