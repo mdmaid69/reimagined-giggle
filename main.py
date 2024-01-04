5255e243bb598feef619a55e21747414d2183325
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:22 +0000

    today fun bite secret space

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
