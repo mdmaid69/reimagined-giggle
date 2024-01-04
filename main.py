03fca86a0aca3921b2cb27a91ac87dde7c07b049
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:44 +0000

    charge later bound offer bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
