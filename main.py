4c6f2de41bd4bdc37319b7462eae8743145c0a31
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:45:04 +0000

    most owe why bound arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
