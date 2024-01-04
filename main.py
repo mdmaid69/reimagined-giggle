ee8179333e59d286b5628bc1ee0f6552cdfd678f
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:11:52 +0000

    mother connected beginning sticky soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
