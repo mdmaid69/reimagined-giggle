f73798a010baf4462a8622908426edc24ebc1287
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:44:25 +0000

    confidence one night unlucky punishment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_area_rectangle(l, w):
+        return l * w
