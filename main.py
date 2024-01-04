866dd0bdc04ca99bbc5af62309a298e6a2c2e13b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:16 +0000

    guard hope production practical or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def is_odd(n):
+        return n % 2 != 0
