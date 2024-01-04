33a04628c742711d47364f1fc796dd4a07f76129
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:39:53 +0000

    570enjoyable something stretch payment much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def square_number(x):
+        return x**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
