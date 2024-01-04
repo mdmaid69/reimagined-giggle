be3b2ef5f48baa1c315b686d4da75c76df123300
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:33:32 +0000

    national trouble can recover reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
