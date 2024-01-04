dc2fc911a53359e27550ddaad2a309235b469b30
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:15:39 +0000

    dark borrow rather introduce anything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
