89a31737df25a27bf1f6e5e3918499b4154e3ba0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:31 +0000

    office suit blame calm allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
