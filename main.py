0e0706c0a31d780ba7b88c176e10738dae139141
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:45 +0000

    successful possible many typical know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
