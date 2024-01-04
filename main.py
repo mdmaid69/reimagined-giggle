d22ca7f2b2967cdee832d0acda6d97ea4f2b8615
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:23:52 +0000

    problem dry punishment until copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_float(array):
+        return float(array[0])
