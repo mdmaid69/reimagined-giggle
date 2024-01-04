376d65a169223f0859a48f0493e503a7f6025399
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:18:14 +0000

    body confusing arrival direction help

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
