fcf03ef503d7fe54b9a2d484eb56b4e6c19861f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:11:07 +0000

    hurt result room fold important

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
