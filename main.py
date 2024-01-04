fd4a31e888efbc22f707aebeabdcc6cd00648f7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:21:16 +0000

    cover feeling performance rare achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
