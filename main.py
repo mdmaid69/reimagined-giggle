848ee1076804bf45538297a30a59f0035c4cf34a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:32:13 +0000

    prove inside command care run

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def convert_to_octal(n):
+        return oct(n)
