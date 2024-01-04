471ba58bf55cdcacd07096f2e902675947c64516
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:45:05 +0000

    pollution touch else mess conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
