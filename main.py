b63f881fe6b9125d7e30dd43779583a07098149b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:39:09 +0000

    right middle defend fierce home

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
