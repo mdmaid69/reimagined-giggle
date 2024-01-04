5390267d04997eefb26df8ae28668370d93fb676
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:53 +0000

    getback file difference cross person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
