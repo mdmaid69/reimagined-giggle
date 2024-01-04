a6c935661fff52dc316916bfcff3c1e333470713
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:37:34 +0000

    beneath innocent age satisfactory separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
