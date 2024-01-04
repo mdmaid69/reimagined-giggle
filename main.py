29abcd8f265d2be2d1e91f15ec75e37efbd63979
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:50:19 +0000

    poor Cc remain fast excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
