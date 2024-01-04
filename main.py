ea1a4c182b3b481cefac491d5d07db86816c191e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:26:48 +0000

    no where hurt public earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
