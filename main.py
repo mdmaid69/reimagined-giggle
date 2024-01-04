00f67a249fefdeb70dc9f1dc325556e6349c2956
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:26 +0000

    danger correct everything sleep snow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
