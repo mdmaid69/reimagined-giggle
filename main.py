31eb884e00858d927d0e01ab690a09b373592ad1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:19:37 +0000

    safety ago design turnoff behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
