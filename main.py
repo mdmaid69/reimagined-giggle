d3bf908d1313b26bf870566329f493cee52d592d
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:42:48 +0000

    pull disagree almost number noisy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
