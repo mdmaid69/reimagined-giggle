d2949681506c572d16c5172f82e140d0e620f5e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:48 +0000

    compare usual thankyou way class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bool(array):
+        return bool(array)
