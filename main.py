fa0c58b3ea45d14d358c3083306b50dfe23e8759
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:11:45 +0000

    demonstrate manner frequent relation 568energy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-for i in range(10): print(i)
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
