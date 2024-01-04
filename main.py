fc7cd5a7c17c3f2cf2f6fadd21abea221e0a45a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:32:34 +0000

    best air respect tense sad

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_speed(distance, time):
+        return distance / time
