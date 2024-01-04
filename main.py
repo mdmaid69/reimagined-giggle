103f25d3ee93d37e83c52a8e9dc422c726eff349
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:26:41 +0000

    pleased sometimes lookup reason avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
