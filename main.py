5adde6326795f4db00b69569865590ebbd403bf4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:45 +0000

    afford shopping target regular juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
