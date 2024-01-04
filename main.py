50509103f9338fb5386a618c8d26454487170d9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:04:05 +0000

    mess abroad negative there bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+def print_python_version():
+        print(sys.version)
