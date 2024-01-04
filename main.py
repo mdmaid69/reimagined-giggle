441429d2a1cac30e5eddca20811e44fa5d52f898
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:27 +0000

    opinion fall lock yours unkind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sys
-def exit_program():
-        sys.exit()
+def is_odd(n):
+        return n % 2 != 0
+def is_even(n):
+        return n % 2 == 0
