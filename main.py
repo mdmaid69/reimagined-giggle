09cbc122e3f4d0023b007f995b7aa35bf254055e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:10 +0000

    meaning trick whatever bringdown dress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
