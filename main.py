efdc23a711e87fb1e6f7b032e1b77d76d79091a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:36 +0000

    we chat Vv ground ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_length(array):
+        return len(array)
