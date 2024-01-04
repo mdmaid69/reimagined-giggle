568671cee12dcd8cc4f35c60b36ac4bb01fbd2ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:34 +0000

    bye children fix theirs giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
