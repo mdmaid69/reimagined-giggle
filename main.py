8ba1bbed2c9be161250090580963e80c6e1eb04e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:26:00 +0000

    in joke disgusting depend sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
