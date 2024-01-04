5399809c0e40140c0da2ab98cefb50be0d9f18cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:45 +0000

    check remember treat sweep everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
