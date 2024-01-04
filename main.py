b6dc969ec4505919f99c7adcd09f6cedfe62b165
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:21:57 +0000

    duty fit powerful set sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def multiply_numbers(x, y):
-        return x * y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
