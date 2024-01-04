1eacd9147c9817a5b20dc276da3cfe35e63942f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:18 +0000

    receive down would among usually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def convert_to_binary(n):
+        return bin(n)
