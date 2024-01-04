3d67b3ae05d0207bcf8aa1505d3bce46f58bc75b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:20 +0000

    guide juice risk many handle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
