7eb58e3227c7a73358936a3b1895298a1f722d86
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:23:58 +0000

    increase impressive drawing connection letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_length(array):
-        return len(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
