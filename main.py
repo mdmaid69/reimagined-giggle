8ff6f4ca913fd2d52901023ac3fcb49c27ff26f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:36 +0000

    treat dance party his her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
