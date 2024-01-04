0d87aa4047be9aa51da5661e7e891576cb4fc0b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:09 +0000

    grand careful reduce top somebody

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
