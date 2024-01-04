5022ba6629dc6a54d57dd1c065991146b9960505
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:17:19 +0000

    in so avoid opinion danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
