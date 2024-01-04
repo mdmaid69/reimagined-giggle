3e6c4f50ae44764c5f37e147ce139a0e481e5f48
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:37:10 +0000

    lucky soon name except perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
