fb8a3f9cec8ea50922d0078eacdb09de4f7a6a8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:29 +0000

    dangerous neither comedown Oo challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
