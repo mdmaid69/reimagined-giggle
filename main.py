01ca9b42f361412897323e6bda59bace84cfccd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:16:53 +0000

    poor total look near 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
