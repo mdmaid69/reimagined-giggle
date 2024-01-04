fecfd6274d2426143d69c58de73a2255b9d0618b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:14 +0000

    judge flow drive organize error

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-x = 10
-y = 20
-print("Sum:", x + y)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
