2b3caf4be6c43633baadb0147300f65456d2c117
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:57:33 +0000

    shoulder hurry different Kk patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
