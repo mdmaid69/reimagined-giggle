3e6ec221fb08e2abe653c66787b7a877fc7572e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:35 +0000

    sell Uu everything record shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_power(work, time):
+        return work / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
