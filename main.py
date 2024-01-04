c5cfce1210b7f185bf80181e7b59591f16ab7bf7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:00:57 +0000

    fail no own polite grow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
