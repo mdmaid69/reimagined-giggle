99a67d8a351b8a7fcc17c120621d0bc28c846aee
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:03:44 +0000

    married practise moon say justice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
