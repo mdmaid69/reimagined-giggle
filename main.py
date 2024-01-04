b5db5c52ebae4853aac04e2532c71f08b70ea765
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:53 +0000

    answer press mess ring storm

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def square_number(x):
+        return x**2
