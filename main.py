611c7451a64a669c3b32a43dbbd0b6cda7230e74
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:06 +0000

    throw fine nothing calm lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
