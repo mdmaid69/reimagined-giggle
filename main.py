80255e2051f615d9c274aadee2c25a5d6bbad383
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:26:58 +0000

    direct little thick once on

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import sys
-print(sys.version)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
