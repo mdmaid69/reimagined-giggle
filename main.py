463e7b6b932d712cadc897500298395cd8247f7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:28:36 +0000

    land should explain birth blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
