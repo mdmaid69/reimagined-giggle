11adfbfd41c89b0c43f9f5266e33427931d18e0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:35:56 +0000

    still themselves opinion frame post

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def reverse_string(s):
-        return s[::-1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
