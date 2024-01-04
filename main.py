300b285aa4f592209cbb2c904ce13b7c0a9957ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:16:11 +0000

    distant interesting everything eat result

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import time
+print(time.time())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
