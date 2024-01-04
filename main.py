0e366946f4fdf9e2f658c8c3c0e58be595217757
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:16:51 +0000

    slight not fault turnout friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+from collections import Counter
+print(Counter("hello world"))
+import os
+def change_working_directory(path):
+        os.chdir(path)
