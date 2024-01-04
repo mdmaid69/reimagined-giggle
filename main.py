f90ccc71ee33f33fc35f05e9aed6dd75ad2e3081
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:44:41 +0000

    shame deliberately difficult record medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
