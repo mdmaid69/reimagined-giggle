193040cd67029a38758aab19d2dc8c046c7cf681
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:51:37 +0000

    afternoon present keepfrom disgusting guide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
