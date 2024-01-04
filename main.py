6b556fe52fe29004806597b7dbf278a7ae352106
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:23:14 +0000

    hang through turnon sad often

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_max(numbers):
+        return max(numbers)
