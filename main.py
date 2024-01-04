c6fbfe79d0591ae9d2a8268dbdc5eb2ec58579fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:54:42 +0000

    well convenient feel goon sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
