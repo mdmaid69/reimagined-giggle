a4a8632df253ef9937d1ad014d16b2c86184274e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:02 +0000

    guess brave art harm all

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
