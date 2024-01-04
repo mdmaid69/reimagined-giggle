36d02d440d2468319b05a65b1f2e69ede9b4a35f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:40:11 +0000

    would somebody depend separate will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
