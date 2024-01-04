45223f63726dabb0e5cd3e81f042a0b48113721a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:34:11 +0000

    especially false everything convince common

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
