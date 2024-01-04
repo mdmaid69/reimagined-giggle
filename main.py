16ead9f955d5669d603bd9585791ff74c07b2bfb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:43 +0000

    transport event downstairs sore wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
