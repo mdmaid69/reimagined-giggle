b7aaa8cd009b9bea18baa6b3ef507a24610aeb1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:11:44 +0000

    noisy ignore black round language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def add_numbers(a, b):
-        return a + b
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
