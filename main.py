54b8fffab409ed22ef6216ef120eafab15f21cdd
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:58:18 +0000

    hold uniform wrong there estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
