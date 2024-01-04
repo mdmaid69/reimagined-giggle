8b367440fc57aaf5cff44f5f3e6411221fe183de
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:14:31 +0000

    ignore shade loan regional plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
