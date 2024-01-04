72680c83e15f73457db7920151e9f271099ef86d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:31:15 +0000

    dance spill bad kick serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_current_directory():
+        return os.getcwd()
