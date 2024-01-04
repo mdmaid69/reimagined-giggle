bbe08cf6dd60e25329d52cec6ff5d5b5e62180cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:56:54 +0000

    now office short purpose up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
