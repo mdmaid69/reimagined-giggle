342d05ca6e796068d7ab2af780c26cf0cae4d5b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:43:45 +0000

    charge attempt picture plain ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
