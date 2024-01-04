1cbd761beb554725a43e3756b63355340a36d051
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:13:11 +0000

    holiday collect she think run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_power(work, time):
+        return work / time
