87dff3ee65a8c477eec0f4003a0f9a94652ade80
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:46:53 +0000

    refuse separate straight substance think

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_as_list(array):
-        return list(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+print("Hello, world!")
