bf0b3fd67ef8ec52599dd6f362be8c7a89bbf7f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:12:07 +0000

    picture regional translate piece silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
