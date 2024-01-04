6bf370ba995756a16e9abd5c7c3a3e1c1154c175
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:11:39 +0000

    grand naturally shut height breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
