ef00b95e7032dcf63702f5c16a856e65365d3959
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:36:52 +0000

    account judge lot chin manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def find_max(numbers):
-        return max(numbers)
+import datetime
+print(datetime.datetime.now())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
