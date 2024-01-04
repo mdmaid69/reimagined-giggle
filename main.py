1b5cb469407d5e6a1639c5e1edf14f11c6a39e38
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:38:07 +0000

    contents rich related wheel pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
