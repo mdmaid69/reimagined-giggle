254fa70bd8b71259df7bb93b59b1ee800c11469e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:03:32 +0000

    quantity both getout rank copy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
