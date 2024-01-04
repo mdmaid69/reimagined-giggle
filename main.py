a65aa423a23cc205d0db2bd4fdcb0008533a7936
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:14 +0000

    getup free discuss interest strict

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
