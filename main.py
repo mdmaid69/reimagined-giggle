4dffd00388fee50c40a5db3d4950c70848f5514b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:42:46 +0000

    praise settle bang treat protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
