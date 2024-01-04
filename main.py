49265ac82b156475686517b176527d01e1f9ef4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:44:01 +0000

    interview fill improvement personal afford

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_base_name(path):
+        return os.path.basename(path)
