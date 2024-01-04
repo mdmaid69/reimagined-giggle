d7a68b44e757be5c414151f63efb57b50d428809
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:56:47 +0000

    float wind allow suppose fetch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
