5f7acd8b9cad2adf2b6c4560f09e7d53a3b81557
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:41:42 +0000

    old from recent practical journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_user_string():
+        return collections.UserString()
