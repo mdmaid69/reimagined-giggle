9bbcc74c2099b2e3e6a01927f43dcdbd3dae2bbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:53 +0000

    turnaway experiment object 572enter thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
