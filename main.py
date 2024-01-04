cba8e4b88ea558aa9392ca58a443f90e7aa3a96d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:27:24 +0000

    risk makeinto goout tragedy bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
