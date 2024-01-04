298c4de6f7598c10978b1a709e8df7b9648dca06
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:51 +0000

    fly post Yy shadow welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def append_to_array(array, item):
+        array.append(item)
