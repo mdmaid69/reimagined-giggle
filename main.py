4ad33b1d0ef8c888ca6b0cab26b6ee028d61f34b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:22 +0000

    rise excuse national already crime

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
