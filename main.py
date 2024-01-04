8f93d9fa6d4fb122f2e0865cf44ee9a26d14fccc
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:26 +0000

    have offence play important mix

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
