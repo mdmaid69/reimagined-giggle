5b2ebb300234c20b25213a0baa2332bcf6a29d83
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:33:54 +0000

    neat passenger land excited illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
