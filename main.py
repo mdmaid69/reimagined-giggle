f478b643190458ec2f52f6214a97a84d0a8367c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:11:36 +0000

    cinema indeed claim rent taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
