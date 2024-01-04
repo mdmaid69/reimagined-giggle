51ea32347ccd491ba942783b2a7b44904c57996a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:03:18 +0000

    Zz promise thin far forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Words:", len(text.split()))
