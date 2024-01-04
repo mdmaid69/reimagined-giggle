900c5a4936840852891d625333a503e2d28a9b9e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:48 +0000

    go pronounce smell tip cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
