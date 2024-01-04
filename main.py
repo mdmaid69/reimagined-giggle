b092ce4fa37a62612e7e3df1db1d46851c027ff2
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:46:08 +0000

    mind voice stay arrive complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
