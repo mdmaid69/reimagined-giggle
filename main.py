7620f75a8742aa4c76b3dad3c655d8715b14da70
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:34 +0000

    apologizeapologise favourite name obtain warm

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
