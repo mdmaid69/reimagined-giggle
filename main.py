5b5071f0dfb97390f0ff77e0e6dea0e1815837b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:41:02 +0000

    interruption claim everything sense bag

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
