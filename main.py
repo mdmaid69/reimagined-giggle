77433c2351c8c3e4a040eb51cf25d69059d71254
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:03 +0000

    suggestion breakin comment opportunity worry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
