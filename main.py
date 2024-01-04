43345f11ef818a94afb22d5856904a5c6a54a15f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:13:22 +0000

    deliberately comefrom just violent light

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+print(sum(range(10)))
+def remove_duplicates(lst):
+        return list(set(lst))
