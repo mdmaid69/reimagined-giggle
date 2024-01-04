8e0f7e61baae708aaff67d8158e68ba8a2da9f79
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:45:08 +0000

    loss gas ball high repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_unique_words(sentence):
+        return set(sentence.split())
