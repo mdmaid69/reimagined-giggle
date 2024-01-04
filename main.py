5e91f7b06ae20f16e0b931c420f2d9e7aff6f732
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:43:35 +0000

    claim fact notice advanced strange

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
