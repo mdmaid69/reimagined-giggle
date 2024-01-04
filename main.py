7b0c772e846fd25be66d85300720aa71a525e3fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:33 +0000

    flow light Ff border black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
