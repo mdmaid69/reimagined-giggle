7f244566a20b7370b2b04d6d2fe33dc4df1180c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:13 +0000

    table pronunciation hers treat Oo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
