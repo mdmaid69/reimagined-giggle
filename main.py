5e4eee0956e085a2ff3c6ce0a35fc9609127950d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:45 +0000

    comeup everywhere no important average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
