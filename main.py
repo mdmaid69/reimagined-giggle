1892bf7268eb098c4b06ac4556620417041fa5e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:21 +0000

    transport sudden sensible somehow fire

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
