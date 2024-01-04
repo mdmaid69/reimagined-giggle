790f0305c1f626acf9063baab1c65819799b7554
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:06 +0000

    illness tie possibly sleep drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
