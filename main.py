207569e0911b1fa200aa66aaaf60422ec0ba7da8
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:07:30 +0000

    cause survive shock furniture well

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
