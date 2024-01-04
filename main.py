34ff887fe29855ff4f3e0147f091c730267b0d10
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:44:04 +0000

    advice basically after police smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def multiply_numbers(x, y):
+        return x * y
