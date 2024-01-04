77273922469414b4dbcfe808317b31efb528086c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:25 +0000

    theirs stranger pure confusing in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_max(lst):
+        return max(lst)
