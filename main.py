2d37cb226355e877806d99e544b17213fb353ae3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:57:01 +0000

    strike harm attach present bet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
