adc6497e63257749698e416f952e1f7bccbb6c79
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:39 +0000

    town welcome increase average wave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
