a0c0f7d29c71119c253b5c87ce66206eeed24957
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:43:39 +0000

    personal sight bite holdout precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
