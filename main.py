b31a09fdce5735d191f1784bdbab5faee16abd45
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:20 +0000

    tap dry go nowhere hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
