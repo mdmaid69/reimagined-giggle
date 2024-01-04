9f4bc60cf9ad3dc02ed530b502d73784ec362faf
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:17:26 +0000

    string battery bright secret opposite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def find_min(lst):
+        return min(lst)
