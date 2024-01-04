3a7bdd8410567752d632757faed0fe49d539bd93
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:49:02 +0000

    basically agreement any possible correct

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
