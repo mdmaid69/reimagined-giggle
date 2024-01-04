d731f9d85b0eede74c13df5acbc3540dca2674b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:22:02 +0000

    happen feature call divide previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
