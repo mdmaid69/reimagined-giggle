8511e3168721a93757953a4d28f6627c5ab7b09e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:06:07 +0000

    common trip check congratulate test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
