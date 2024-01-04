3bd225bb357f6fc98e760590b2a871ac341eddec
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:49 +0000

    sort violent aware occur powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
