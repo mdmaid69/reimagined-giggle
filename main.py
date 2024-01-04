91b6d5e2784733ccf9e8b820374f0289de22d8f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:32:47 +0000

    remember match fork 571enough hurry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
