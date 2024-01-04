629106239f0105a14f98e167eeeceeed7dfa5dd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:22 +0000

    justice large excellent wire fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import shutil
 def move_file(src, dst):
         shutil.move(src, dst)
-import time
-def get_time_since_epoch():
-        return time.time()
