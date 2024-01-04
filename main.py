910087dff420a32982af8966a424c20b1d4359a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:12:58 +0000

    basic difficulty spring nearly pinch

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
