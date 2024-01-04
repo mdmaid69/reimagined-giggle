40a3ae8a288f71620ba8b6974ae12aa169b33ac8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:11:33 +0000

    whichever under apologizeapologise shoulder base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_index(array, item):
+        return array.index(item)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
