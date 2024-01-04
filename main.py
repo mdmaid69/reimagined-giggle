c26c1127dd0cc4a9a3f4263c87e1f9d7f6c3bf13
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:56:41 +0000

    unpleasant mouth blow part pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import os
-print(os.getcwd())
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_float(array):
+        return float(array[0])
