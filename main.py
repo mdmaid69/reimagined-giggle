73f71ef64fd1189350667a98dbb44bd7e02bfb4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:40:02 +0000

    warn unable giveaway zero table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+def convert_to_octal(n):
+        return oct(n)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
