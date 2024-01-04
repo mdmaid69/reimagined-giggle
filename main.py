a0aab97feb8ebd3022030a95d2ab6df74a56f553
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:48 +0000

    sure past destroy accident complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
