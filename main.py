31e3a94ccd1f912f302dc75d282850352d79ce3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:59 +0000

    science carry practical image match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
