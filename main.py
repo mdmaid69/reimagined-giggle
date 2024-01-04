fd91f4cb292c654dfb83b09f1e5af964602523d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:48 +0000

    deep responsibility meanwhile Ll statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
