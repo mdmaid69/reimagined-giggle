53d38d2c13a2f66712c3bea38285cd6fd2633dd1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:16:59 +0000

    congratulations ticket lookafter prize once

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
