9edf2b24ab9a069c6262ba50eb1a9eedd0e3dfd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:38 +0000

    city front child recognize card

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
