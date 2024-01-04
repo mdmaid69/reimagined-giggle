4986114b73698084973e7ca0a107ac770477c87f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:56 +0000

    official actual children alive actually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def convert_to_hex(n):
+        return hex(n)
