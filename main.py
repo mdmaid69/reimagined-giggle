e00f656da23c69a553c85a66b545d691daf6d72d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:48:07 +0000

    eye measure disappear ring tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import re
+print(re.match("h.*o", "hello world"))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
