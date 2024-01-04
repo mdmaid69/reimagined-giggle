b6775aa73108a0c5db889d71e2913d8f364c1e64
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:46:39 +0000

    play sink teach prevent on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(lst):
-        return min(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
