fa0a7ac285374ea8f5dfdb8355d4c67d1413bc9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:23 +0000

    spray couple close sort union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
