4e2b883a694ddd169b71a8683c5acd042129e92d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:22:21 +0000

    promise underneath climb risk this

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
