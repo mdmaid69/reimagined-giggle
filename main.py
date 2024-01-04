7833b12acc9a9b3cbe882bd6c5de79fd12b7e01e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:32 +0000

    miss experiment conclusion cheap attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
