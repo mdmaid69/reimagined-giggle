f284ee261b554e89743ea22bc06103a833f0e605
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:18:31 +0000

    path condition form able decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
