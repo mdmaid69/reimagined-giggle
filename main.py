ac03dcb76ad8c4b6df31a2a22928eb183ae94061
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:01:51 +0000

    passenger object comment unfair work

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_tuple(array):
+        return tuple(array)
+def find_min(numbers):
+        return min(numbers)
