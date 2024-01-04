68b285c7979ee7fc73007c871af5cbc734490bff
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:06:00 +0000

    please dry sudden average factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_list(array):
+        return list(array)
