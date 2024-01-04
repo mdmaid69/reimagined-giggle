4d971dc191c7488b5bf96a5c40c5785c91e398c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:44 +0000

    fire personal quality point early

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
