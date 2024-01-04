90423a39544394aa71f4154b25c4738312e02b20
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:03 +0000

    bring funny image same scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
