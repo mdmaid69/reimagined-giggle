c6665581858e3f3455dbd5c75891c101e8d946ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:21:50 +0000

    along swim have best comeout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import sys
+print(sys.version)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
