328b65668ef0ca29c696d9c34a476b19d145f9d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:56:45 +0000

    should terrible dick please warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
