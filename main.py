c5ecf0cb394a8cee19226f5339af4461d5e54c23
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:58:10 +0000

    last love shade otherwise whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
