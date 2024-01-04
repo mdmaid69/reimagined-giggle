298e38ef8eacdd463c49f3153766f5cb5792a724
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:41:43 +0000

    professional unless memory undo tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
