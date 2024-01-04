5308bcb20fcdc1f0b57d85153c8900ab867005e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:17:46 +0000

    post direct hollow that honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
