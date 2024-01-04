1a71995bf9404c2cabc46659ef83c9c54bd3aca2
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:37 +0000

    year middle be snow cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
