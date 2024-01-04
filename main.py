0f986b15e9f3b7c2f04a47b333ec6e2185c4f152
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:41:22 +0000

    free wall fold furniture sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   def calculate_perimeter_triangle(a, b, c):
         return a + b + c
+import array
+def set_array_item(array, i, item):
+        array[i] = item
