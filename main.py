be7dc24a8b15fe679bda2b78e087527fc32a90f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:16:09 +0000

    cover fashion within later at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import sys
+print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
