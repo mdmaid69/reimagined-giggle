c24726e1f7a9f0aabf7469d13233004f171ea58f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:55 +0000

    sink tidy go getup file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
