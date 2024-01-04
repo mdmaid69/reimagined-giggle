00846531546b25c991293badf4b3a51f78bc5dfd
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:49 +0000

    race Ww influence fast steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
