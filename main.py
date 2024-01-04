46bbc027de90d1bcfa559e64b569dbcbaae68c72
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:52:29 +0000

    purpose collection anyone row key

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
