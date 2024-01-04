f8302ae88a166e430fb90ade59c3621a039ea80a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:33:26 +0000

    cry bottom aware crime welcome

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
