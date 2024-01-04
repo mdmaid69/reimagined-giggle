85bcf23a69f1f02c5a6df8220ac028091f87963a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:52 +0000

    television pause split shock steep

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+for i in range(5):
+        print(i)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
