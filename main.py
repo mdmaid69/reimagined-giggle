d414c58479a7937444233ad6e28ae0c730d29fd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:48:07 +0000

    comeround concerned waste not quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
