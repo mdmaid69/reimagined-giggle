e103400910a29ffc7f2351929b2aa40f6a0f803f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:32:12 +0000

    every be least behind only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
