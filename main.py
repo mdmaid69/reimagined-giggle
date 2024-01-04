d785011e207f15cff75e8ca452aec9c29e365a89
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:05:24 +0000

    upwards game war Yy niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
