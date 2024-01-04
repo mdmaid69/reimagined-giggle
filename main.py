13404c90dc7daf97401412202b783c9eb9ad8983
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:45:46 +0000

    common division pretend line mark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def convert_to_octal(n):
-        return oct(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
