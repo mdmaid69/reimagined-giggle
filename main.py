a1256f81396abbf08108715b2ee962579c0293a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:25 +0000

    slightly sick invite love male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
