d2b50377172dce826a239cea68e5fd9dd18b7a20
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:22:54 +0000

    price cook sort original shout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
