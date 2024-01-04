ec822633e8e8bdc7079ec9a17a72a3b6f47b30dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:28:44 +0000

    everything bite gooff regional worry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import time
+print(time.time())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
