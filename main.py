94e9021f7c8a112d4502180bbab2e19d3a16846e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:54:35 +0000

    or place bank surprise wire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
