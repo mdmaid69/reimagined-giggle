1ac66335d12e55ad0de4005b65d8b00b3a62d7f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:55:40 +0000

    rest list go limit offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
