53edbcc8fbb6714308c8b989ee0232a75879131e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:45 +0000

    call crowd settle scrap declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
