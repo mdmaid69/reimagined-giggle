14113c76bd1ccf1c5aa7d7e511674c95ccaf7ee2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:22:31 +0000

    whistle beginning basis occasion receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
