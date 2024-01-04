e161f557db765eee782b21fc42ca3c3253cdc061
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:14 +0000

    fair professional best company hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_density(mass, volume):
+        return mass / volume
