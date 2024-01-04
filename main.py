3c618ffbc2d68bb797fd9960c095992c9d227d7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:30:52 +0000

    kill strict teach trust extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
