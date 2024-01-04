48bccc80ae2880080e1becb792e8a6766381ff3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:22:37 +0000

    shall comeout appeal fairly soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_max(lst):
-        return max(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
