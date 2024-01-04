8f8dc235f3f76e7c8ab9fce8564cc10d6376ae2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:53:44 +0000

    team while help squash regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
