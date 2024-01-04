d95c71e69e95a3b6d9c3f37c7fee1c0aadbed6e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:32:54 +0000

    ignore coffee must deep empty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+from collections import Counter
+print(Counter("hello world"))
