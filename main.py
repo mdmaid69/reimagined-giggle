847462c00e4f802cc1f9b077af7b559456b0d9ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:22:29 +0000

    autumn top intend check public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
