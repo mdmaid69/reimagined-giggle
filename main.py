ed048b5f1fff0f98cb4c4d69a33ed3c41ebf8503
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:40:16 +0000

    between will centimetre accommodation teach

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
