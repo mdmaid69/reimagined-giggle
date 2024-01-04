dd5bcf24c584a4bc00a6f19a60ffbd1a8150e218
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:12 +0000

    join towards leg tape finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
