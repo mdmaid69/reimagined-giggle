08aa069f8cbf8a9324422933dee3795c091ee4c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:50:56 +0000

    swallow centre spring listenfor however

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
