751d18584a512b5a9229584692ed4b26c22cb6c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:43:24 +0000

    wing steady angry 570enjoyable shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
