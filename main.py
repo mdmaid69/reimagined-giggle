83ab5461f367853b2d1a548307c095c2c76a2253
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:27:19 +0000

    regular care free danger clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
