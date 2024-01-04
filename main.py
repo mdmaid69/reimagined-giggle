b92ddf88cdfff1770956ade152c2f3e279f038b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:02 +0000

    dish prefer us fashion plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
