5122cc4672240078381d0d4d1af95c8c6173e01f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:44:46 +0000

    mixture frequent stranger young minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
