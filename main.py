f2a97528dc78ef581a1a194973ef321381d1dd3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:22:37 +0000

    so limit scream poor drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def remove_duplicates(lst):
+        return list(set(lst))
