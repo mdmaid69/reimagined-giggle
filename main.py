5318d5dacdab1fdad37045910c6068933d0c6e28
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:07:06 +0000

    zero condition distant getup punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_priority_queue():
+        return collections.deque()
