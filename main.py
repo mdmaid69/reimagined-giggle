1f619fd7a60f11d12713dd02e40579e25647f97c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:03 +0000

    rough Hh pencil range birth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+text = "Hello, world!"
+print("Words:", len(text.split()))
