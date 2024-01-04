7124db0b003e338f998fbba6d9f766b1e4e61b49
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:27:26 +0000

    might start great squeeze plastic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
+def calculate_speed(distance, time):
+        return distance / time
