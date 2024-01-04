5c7315a78e74eb11f3339c488ccd5d39d1cc874c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:16:11 +0000

    problem issue team high skin

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
