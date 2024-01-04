223ddc38edbf7dd808a7466c779033552d05901e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:26:32 +0000

    against strict brief foot offer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_density(mass, volume):
+        return mass / volume
