48e99a58a1bd44f7da644ba38b4ce843b4c5a789
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:50 +0000

    scared worth seem introduction getoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
