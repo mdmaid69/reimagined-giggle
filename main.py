cc407f7ebbc7b8c18b95f7892ddddbf77e0e04e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:18:28 +0000

    please try Gg Zz draw

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
