038d147a2fabd3b698f7b156984497f8339f4738
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:11 +0000

    job contain thinkthrough equal activity

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
