95d2e573197a596e58aab83f423c3103ea4ed3f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:51:42 +0000

    meanwhile corner sport lookaround comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
