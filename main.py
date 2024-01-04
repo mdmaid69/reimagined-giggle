ec554fbf634a202a0fd607eef1feea7273eefd62
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:13:12 +0000

    getaway be back unusual however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
