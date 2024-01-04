0b54d257ef01a1b8f20b0a78ca68ea8e7cbf41df
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:36:01 +0000

    value sticky pity bring tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
