57e88680fe10e3a3cb3fe74990d6030dcecf414c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:06 +0000

    upwards impression mother ashamed rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
 def calculate_density(mass, volume):
         return mass / volume
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
