c26df3e8afee8ad23f42a00a7d4bb4f117252bcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:15:11 +0000

    wrong Dd plant example holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
