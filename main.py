de118033649284d9d3ce6a7f6af2f9df34b5bd39
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:24:00 +0000

    most cruelty offer statement improve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_rectangle(l, w):
+        return l * w
