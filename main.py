a1e8bb6229a5afffe31480d420bc8ae59a8a6c78
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:24 +0000

    sure turnback ignore foreign too

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
