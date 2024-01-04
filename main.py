1d07b3941b181c93527a4794022280887963e9a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:22 +0000

    send ill behave trade radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
