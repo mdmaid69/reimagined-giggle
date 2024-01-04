51b8dad4bb318d814fe786b53bcf6992c1eb53c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:04:08 +0000

    however 567enemy march arrive write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
