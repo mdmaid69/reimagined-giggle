63f2b0d703ef712a13ffe70229e3298a81f61a1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:56:49 +0000

    nose organization noone slice callout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+print(os.getcwd())
