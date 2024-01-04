75bc775d1422689799a78c2c3dcb25fb091780c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:22 +0000

    exchange blind case holdup majority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def remove_duplicates(lst):
+        return list(set(lst))
