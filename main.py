24d6e399f10f019a0c4d93547ce58742c5ada21f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:35:28 +0000

    pretend best dare grateful way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
