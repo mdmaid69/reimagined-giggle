b10477cb463c8b1c7f518c29e972cbc9460c2f40
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:09 +0000

    favourite wide visit never walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
