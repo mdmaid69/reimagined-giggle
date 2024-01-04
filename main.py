e9a560b71c3db0b94b8f341b81e8ae9ca835f959
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:47:48 +0000

    ahead beyond public dare fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 def is_palindrome(s):
         return s == s[::-1]
