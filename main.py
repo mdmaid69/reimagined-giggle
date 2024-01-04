b3a6758b55e8f824ebcab0c65301de5827621110
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:51 +0000

    edge behaviour father tell large

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
