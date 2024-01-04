4c8f2946a966ecfc41c26dc6a14f7f1d337e12e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:06:48 +0000

    surprise smell cross cut dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
