8a31a103ffc53f4513d94f058fa8e4154f17fd88
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:19:56 +0000

    settle settle satisfactory bar admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
