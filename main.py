8e92bfa60d02820f476af9294150446ac701d9f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:31:17 +0000

    accidental battery furniture beginning inquire

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
