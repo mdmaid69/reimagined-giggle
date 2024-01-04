0d6187aa6a01a30640f586bd8fd50b8fbf64f218
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:45:47 +0000

    leave rain criticism whom may

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def reverse_array(array):
+        array.reverse()
+def calculate_work(force, distance):
+        return force * distance
