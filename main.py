e599bf0479e55b2c960419cd1578efae2eddf0f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:59:12 +0000

    store normal treat spoil trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
