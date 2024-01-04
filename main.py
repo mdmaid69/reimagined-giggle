4d81c352efad6097dcd4ab7e3523f085e3f6065f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:06:50 +0000

    service further lesson tour active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
