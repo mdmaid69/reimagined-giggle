91404163101887fef82d993e7adaf8dcc62f4670
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:52:51 +0000

    Cc experience weather under maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
