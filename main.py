6712fdeea152ad24210967cbdeeb6490820b488a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:49 +0000

    travel history instant destroy comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
