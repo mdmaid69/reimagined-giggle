3e7472b0511e289639683cd44e7562f5a6f3f755
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:28:19 +0000

    valuable cheek either excellent leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
