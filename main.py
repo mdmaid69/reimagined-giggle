62b1a195767e18cd47008aef0555480eb26b36bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:15 +0000

    afterwards set immediate perform star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
