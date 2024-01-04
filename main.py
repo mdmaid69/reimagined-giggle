a97a0e27954849047daea0f6ae200f4113f802ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:57:50 +0000

    turnup way born occur prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
