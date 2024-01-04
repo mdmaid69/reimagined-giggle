e4a65665e779715cdcf9a0d5d168b353dc93b2e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:16:28 +0000

    bread relative value wrong load

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
