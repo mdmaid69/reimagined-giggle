4d9e5119b6de263bcb9c2dae154b28afc817cb67
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:58:14 +0000

    down keepaway adjective best embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
