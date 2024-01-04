548ce69b89729a687489a2c46ced2fff49976bfd
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:35 +0000

    combine swim most immediately over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
