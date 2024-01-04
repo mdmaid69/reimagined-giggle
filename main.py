ecf8bd1000ea5f4675cbc3848a748c075040003b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:50:03 +0000

    on long explain ill taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
