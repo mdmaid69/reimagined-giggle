6e830e6831b600694d6429987ffe116c2e33a389
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:47:13 +0000

    blame shallow copy train shadow

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def is_even(n):
+        return n % 2 == 0
+print(sum(range(10)))
