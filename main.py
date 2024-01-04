b09405bba6e3f10a1dcadece9af52a43481cc785
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:21 +0000

    measure shine clear challenge wet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
