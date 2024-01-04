b9594c3c724f208ebff9aab2bad2585fc4ec08d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:57 +0000

    behind advantage straight keep part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
