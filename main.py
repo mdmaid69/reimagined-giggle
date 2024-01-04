7f0e36484551f8907a842196dc5a85382925fa36
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:40 +0000

    less arrive swallow divide swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
