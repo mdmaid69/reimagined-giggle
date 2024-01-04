6401e8d3ca17af08ce4bec5ca1026634c33d5014
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:30:07 +0000

    strength off physical present through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
