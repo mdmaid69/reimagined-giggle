3eeb538ddb994d315b4708094be65cfc3b212ca3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:52:27 +0000

    bread warm fall fire rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def reverse_list(lst):
+        return lst[::-1]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
