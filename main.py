55c68474f49ca34f24da66a03a903b1f4a3fa3b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:18:42 +0000

    clever admit ignore apart strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
