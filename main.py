7d7dab06f882086d941098b1850da4e94a13a2d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:12:04 +0000

    wild immediately school danger Hh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
