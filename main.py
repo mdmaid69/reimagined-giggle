9836a614c84e5c11fb55b58e164ec1cdf655b777
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:54 +0000

    development act steal yesterday pump

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
