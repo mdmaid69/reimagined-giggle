ad43e722f0342213b50cd96b06a567fa7c7226d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:29 +0000

    need protection sure slowly pen

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
