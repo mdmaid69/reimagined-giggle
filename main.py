72c3f4b9418631631ef4ea59629e015d46c1e2d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:08:40 +0000

    concern the measure town have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
