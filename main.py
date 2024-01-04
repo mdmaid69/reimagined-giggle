9b07d9afb64f3dfd2108a7958ea14f9fbd4e412e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:13:09 +0000

    prepare ring speak conclusion bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
