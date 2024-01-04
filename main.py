6c358cd70e304aaf990ad6b2c01c0da4fc74aac5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:39:22 +0000

    season mine always circle idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
