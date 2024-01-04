7b5f4b5f8176c1a0de7a4f10a2fc4f8380d2a8a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:57 +0000

    down round rescue fight add

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
