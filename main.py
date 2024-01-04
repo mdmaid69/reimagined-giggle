e4b7934607a8994e25ac05e7f48ae2c64bb80b8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:12:24 +0000

    pleasure mark add inside wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
