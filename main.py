ce10cfb7fa6014afa2adbeb85b39b083c7248f45
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:00:41 +0000

    forgive fat dream repair method

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
