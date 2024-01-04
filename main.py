dac43b849de78e4d7d4b39d98e571f9c83579b95
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:04:39 +0000

    private pure steer talkoutof difficulty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
