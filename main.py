21280530e8870e5c64e5163f97d16991f45a0927
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:55:46 +0000

    kind steer suit correct today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
