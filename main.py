49d4332c81dac419655939debda32157dd3c071b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:19 +0000

    breakout wire promise watch better

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
