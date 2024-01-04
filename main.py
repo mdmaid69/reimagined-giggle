edd816d2501d77708dee587787661b5e45b719d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:35:49 +0000

    street onto admit little squeeze

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
