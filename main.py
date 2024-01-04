b19295dc913eb5685fd289d1f9981e20d9cfea5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:52:08 +0000

    shop use OKorokay pay wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_as_str(array):
+        return str(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
