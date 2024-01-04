b095fc689887af9d9b9c803ba6ae0a29e69ab6c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:39 +0000

    floor OKorokay ours neat yesterday

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
