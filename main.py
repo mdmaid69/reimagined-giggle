7df1647ee4e657188133753145e145162145e239
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:37:24 +0000

    correct remain manage middle mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
