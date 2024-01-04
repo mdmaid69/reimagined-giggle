a64ed19899ac80f9fa5cd0924e92d27e25bdad1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:34:20 +0000

    worth cool excited do address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
