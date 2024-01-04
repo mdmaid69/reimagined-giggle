c91f7bdea3fac94582a2341ef3774f3741cfe88b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:45:17 +0000

    Bb mistake middle hello point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
