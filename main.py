7a78bbcb4b8736ef5dcb06e3ed53754d42432011
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:44:43 +0000

    drive involve fortunately supply human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
