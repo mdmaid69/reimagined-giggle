2bace842b00e8b6c0e1b6c2967dc1ca1c06c62b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:12:42 +0000

    can ourselves come straight contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
