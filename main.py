57931ddfabbed12f90a01cec9b5f93fea2b0c3bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:18:04 +0000

    unite interview love repeat process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def get_array_as_str(array):
+        return str(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
