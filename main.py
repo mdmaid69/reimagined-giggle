d55285faa2d343d80c46d9c0a2b0be801caaf703
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:35:01 +0000

    give the brown piece walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
