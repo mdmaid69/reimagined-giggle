f52c847122a852a2ed7412cb3f31a23125c7be5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:01 +0000

    chat become shop other high

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_average(lst):
+        return sum(lst) / len(lst)
