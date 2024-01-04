69eb6973c742466a88403df2276e9a33ebe6b532
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:08:23 +0000

    hurry transport turnup instant disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
