9704053062ab0e20529966b8f7c8429ed4176d83
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:59:19 +0000

    sore cook direct easy birthday

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
