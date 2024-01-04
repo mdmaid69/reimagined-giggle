8eff8995fb130b6dbc61ee95b687d117638b9f96
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:24:28 +0000

    phone climb until police off

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
