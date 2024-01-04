c8efd93b938764ed477c7b89f0662d856215c8ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:28:29 +0000

    freeze cheat hollow storm Ii

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-print(os.getcwd())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
