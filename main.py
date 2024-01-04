ef5d410febcdcd812d31a487fa47ebea0206ce45
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:44:47 +0000

    very steady pen survive doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
