afe70ae8d07021c86e420ea42053cdd859732044
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:45:44 +0000

    block study benefit hurry breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_current_directory():
+        return os.getcwd()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
