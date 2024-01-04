205b98e758fe2118fbfef574c833221143663f06
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:43:11 +0000

    holdup turninto dance expect quick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
