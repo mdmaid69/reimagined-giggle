b6f8ea89de7b21d4184d039ef5e3587ec42dd306
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:52:24 +0000

    their probably song problem understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
