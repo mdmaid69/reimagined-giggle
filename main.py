e3c9b4dde0f2ba3292c44ecadd49c16c45ecc530
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:38 +0000

    frightened middle opposite who drive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
