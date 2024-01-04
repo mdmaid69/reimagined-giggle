48f2f4475cd4b3a5631821563a7c85c6aca7347f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:45:54 +0000

    peace just beside joke fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
