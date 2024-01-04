c7730d826cb9dd3f61ae7454c0bc325533e26b05
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:08:42 +0000

    smoke progress soak possibly correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+def change_working_directory(path):
+        os.chdir(path)
