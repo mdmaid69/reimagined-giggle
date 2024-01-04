ad3c064b3eb122819012e793c522cf32cef90f42
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:30:02 +0000

    common march harm pass band

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
