158b6a078d5f579a775fe17f81574f62aaf8c0db
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:18:17 +0000

    congratulations takeback by relationship keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
