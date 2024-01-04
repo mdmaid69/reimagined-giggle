4f1618e34052943e7b06a923a0ef79699b49c29e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:11 +0000

    bury start upset confident treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
