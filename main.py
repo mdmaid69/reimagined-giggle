08b72f1d231c19b3e0758ac50b4d9997d401f8d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:13:43 +0000

    finally likely interruption limit century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import os
+def remove_directory(path):
+        os.rmdir(path)
