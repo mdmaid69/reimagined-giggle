e69648c0b0ebc28b63d99f4fc88c610896f1f047
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:56:41 +0000

    Itself score suddenly convince side

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+name = "Python"
+print("Hello,", name)
