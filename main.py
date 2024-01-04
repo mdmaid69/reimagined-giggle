79b9736a0fcc159fdba7b5e6827965a51dac1fe0
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:07:45 +0000

    patient sort leg level add

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-print(sum(range(10)))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def is_even(n):
+        return n % 2 == 0
