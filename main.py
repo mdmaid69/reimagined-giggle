0a68bd88c3557c568165cc7011fba984475e3180
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:33:01 +0000

    deep constantly some later especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
