5bb5e5446b65ca6e4b02fc1e39e8129a72eda68e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:13:51 +0000

    crash willing stretch want can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
