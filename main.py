d096b2ef8576c0ff563b9e085b69888371217e25
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:18:31 +0000

    street like action grateful mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
