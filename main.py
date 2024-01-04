8706d4352189d3aaa931a9ceef66100673696871
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:59:38 +0000

    married size circle be swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
