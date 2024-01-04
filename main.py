289d6c681e858301f7ea9a711f5e9d2ba7c61548
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:42 +0000

    tone straight feed notice not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import re
+print(re.match("h.*o", "hello world"))
