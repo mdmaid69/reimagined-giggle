190629eba40960bc4ce7805519bf9550d3cb637b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:15:07 +0000

    yourself colour organization unable false

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def add_numbers(x, y):
+        return x + y
+def calculate_force(mass, acceleration):
+        return mass * acceleration
