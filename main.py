3220caf0a75a68748a08f88d62d081991e31baf7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:50:53 +0000

    scared get wing different fall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
