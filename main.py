264abcd97243cb308beb01f9f5fc0d96c0d70bf1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:39:23 +0000

    trouble add heavy scare walk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
