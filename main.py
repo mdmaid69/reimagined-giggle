6dc65b4f78ef5541a03526c64b8188ca45afa0a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:43:39 +0000

    blame task repeat especially cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
