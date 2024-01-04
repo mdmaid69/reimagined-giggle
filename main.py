176dfed8eaac4c79650abf2eb3d2255ac70702ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:17 +0000

    report crowd club ought modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
