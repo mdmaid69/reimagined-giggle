e2463f64ec6ef32e5c1bddbbc282129d43510bbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:08 +0000

    attractive consist statement equal entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
