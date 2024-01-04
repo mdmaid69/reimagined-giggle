4fe26519b46867a9ca11d8f786f7b1053cd65f27
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:21:52 +0000

    charge stand thirsty conclusion row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
