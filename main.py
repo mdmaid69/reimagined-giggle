4d7a8937ae0aed468253391f01c9937c0e3057f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:24 +0000

    deaf cough tall sight genuine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
