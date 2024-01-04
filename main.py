622ebee543f1d79bf5dc258680eb75cf8251ab52
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:55:40 +0000

    shock blind arm every come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
