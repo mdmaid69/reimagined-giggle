c57a59d0677bfd76869aea5ed3e670e129760114
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:56 +0000

    makeup brush interest cruel meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def add_numbers(x, y):
+        return x + y
