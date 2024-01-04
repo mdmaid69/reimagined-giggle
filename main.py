7111bc877f64828afb6b9002eb94895638943469
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:56:35 +0000

    condition where visit payment wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
