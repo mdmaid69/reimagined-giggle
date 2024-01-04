cd493fe93abade8d291ccf7726aabefbd87b7685
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:04 +0000

    creature power interview disagree list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
