ef759a9106a886353aa92c7efe0d7d4c3fe1faf8
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:16:06 +0000

    affect weekend future tip lose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
