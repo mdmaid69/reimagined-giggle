16aecabb2ad1b793d0eb3d2cfc85e1f539d93df5
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:38:11 +0000

    persuade scream actually fresh stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
