6f5f7390cb4c85b94d848ccf9e8d4b0b8c4a1a74
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:55:27 +0000

    church imagine heart government finger

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
