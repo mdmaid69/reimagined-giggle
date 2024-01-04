b982dc9a67740f0942d2c6344e25dc42df89dcd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:39:58 +0000

    table familiar pull skin sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
