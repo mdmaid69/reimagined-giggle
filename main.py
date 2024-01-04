d7b1f3cf248b98263e607e38a2d36f940ab0ac35
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:59:41 +0000

    kick strict upset tongue Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
