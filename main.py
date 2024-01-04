a45dc22637ca579293a3f4edeec29cc5f66bc0a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:29:37 +0000

    weekend underneath black near yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
