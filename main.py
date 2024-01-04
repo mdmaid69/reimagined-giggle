3f54c983d0d6f7841392b656a8af261c15288b38
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:31:34 +0000

    exaggerate object definite depend figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def is_palindrome(s):
+        return s == s[::-1]
