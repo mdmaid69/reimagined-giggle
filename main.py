211c2b71afe039c5a9292d2ab870237c86c0dfc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:30 +0000

    plate callback roof compete careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
