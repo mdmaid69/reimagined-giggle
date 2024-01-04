bd6d3d644c7a7a905031dfda03e10738412a63b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:27:05 +0000

    frightened forget skin wonder friend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
