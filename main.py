375ed9c27ef29cd5d49cf67da50aeea5ba6aad4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:28 +0000

    hear quiet forgive development heavy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
