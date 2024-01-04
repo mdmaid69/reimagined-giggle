6b021363e23836d4cedaebef4f92235f9da18243
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:15:03 +0000

    store show field instant occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
