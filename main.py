2224f62d9988ef5c8c0f3674bb802be4533d4e97
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:58:16 +0000

    speak inside poison mail bit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
