e696d39d4d1480019d1336d9c847a1cc9c177d26
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:04:07 +0000

    intelligent glad transparent of charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
