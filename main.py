72d91ef375314fe046e596c6039d844eec4d2adb
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:02 +0000

    fortunate fortunately happen comeup wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_tangent(x):
+        return math.tan(x)
