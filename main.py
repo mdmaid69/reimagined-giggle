949da37fb62f7721b8912797ade42c84aaceb957
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:41:16 +0000

    but unfair divide married prefer

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
