57e1dffd432b5c8b88c3473b56e670ac6ac33f88
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:29 +0000

    noisy pencil purpose love raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
