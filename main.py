e70fb128ab945cbaa5d8e32490ad950949d858c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:51:28 +0000

    although proud blame destroy each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
