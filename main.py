80cb005ca2a3da6c87ad561d8b0b03ab057cf99b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:25 +0000

    fear friendly reduction freeze offence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
