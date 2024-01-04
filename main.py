065583c2fc059ac92651e70abf240e175e2526a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:06:39 +0000

    accidental tap peaceful help imagine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def convert_to_binary(n):
-        return bin(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
