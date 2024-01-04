89e8e712b9260a4d16d296de79c96129279683d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:47 +0000

    conduct better type machine union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_pressure(force, area):
+        return force / area
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
