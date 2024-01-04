93b90491f4812adb6a93ed575d3a3db6c2f5e434
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:35:30 +0000

    abroad party part nothing storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
