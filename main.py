fe3ca495b6323d095e325e66262167cc8043eb29
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:52:07 +0000

    track reasonable occur dark own

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import datetime
+print(datetime.datetime.now())
+import numpy as np
+print(np.array([1, 2, 3]))
