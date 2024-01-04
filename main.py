489b0c938adf824432197f5d0b311017560d8636
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:07:30 +0000

    passage ashamed convenient go unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import array
-def get_array_as_str(array):
-        return str(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def get_array_as_int(array):
+        return int(array[0])
