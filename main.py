219e563fd608a831dc76d86bb69d176e1c98ab5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:46 +0000

    slow journey delicate satisfied several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
