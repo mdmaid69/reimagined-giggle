ee9f20d2c92da899163b22ce228bdff0dfb907f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:56:22 +0000

    hit knowledge juice tie muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
