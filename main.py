0cfd6cb2b7e301dc61e1dffd1cb17ab921db429e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:33 +0000

    rest yourself connect entry follow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
