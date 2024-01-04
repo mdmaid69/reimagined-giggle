17144ac9a8ec82208703e5e134530d2bd1452943
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:06:50 +0000

    bit figure decide noone transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import sys
+print(sys.version)
