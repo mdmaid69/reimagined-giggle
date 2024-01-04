f3f5a4946792168a4402fe9d0810f913cb47a79c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:05:03 +0000

    legal test mysterious 568energy birthday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
+def is_even(n):
+        return n % 2 == 0
