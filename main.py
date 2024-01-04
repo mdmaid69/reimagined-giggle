9941c282420872186b7052c12bf3c4bcf18bd9fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:03:49 +0000

    state so pour unite wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
