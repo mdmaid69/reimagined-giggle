b076baf62b5d83343ab4ebab6001cebeff715b6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:08:21 +0000

    confused so appear debt everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
