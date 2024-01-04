a9793b848653b58d948e59f17ea237f549b44c57
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:03:10 +0000

    callat shape also Oo now

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-print(sum(range(10)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
