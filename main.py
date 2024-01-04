81bbedaac97770b89c0c91121838e39b840e6c4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:38:42 +0000

    blow art warn surprise whoever

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
