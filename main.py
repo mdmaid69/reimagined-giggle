8b4d8cc8fccc8c9b1496b7981b0cbe3602a2a8cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:32 +0000

    dress sky false far share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
