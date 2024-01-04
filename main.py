bb9dab45636825cb96cbab9693f69f31038071a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:02 +0000

    taste this please brain notice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_string():
+        return collections.UserString()
+import getpass
+def get_username():
+        return getpass.getuser()
