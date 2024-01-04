9d694d5f7a8ce3f04145fdac1186c7dd2c306613
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:05:41 +0000

    turninto conduct pressure 572enter benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
