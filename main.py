dacfed6a4988464b6623c908f66b20939075aa64
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:42 +0000

    sweep first rough compare clothes

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
