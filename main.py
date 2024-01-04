9b3716b699bfc2d1c3b26dd8a9691e865dda0767
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:57:25 +0000

    extremely ring depend getoff thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
