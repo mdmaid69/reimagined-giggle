543fc79d18b9b1e2ddd136e278dcc463093dd8e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:09 +0000

    or suit successful shape practice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
