b4bb416e78962fb8a9b401dc5c88aea83230834c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:24:24 +0000

    level female divide special sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
