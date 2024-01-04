276ef3157d30955c619ea5e019ee73813f359eb0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:32:31 +0000

    purpose sing plastic neither performance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
