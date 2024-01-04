455d05a146c3e727ba89fd4f42839dbf67e0aac2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:47:27 +0000

    next praise ordinary uniform dry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
