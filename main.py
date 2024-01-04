f627c6efef5dc22c92967c65e7487cd450965eb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:00:49 +0000

    share harm shame will rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
