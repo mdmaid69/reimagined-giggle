e0b6a628a47b07e71f5b566afba44abc7af955f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:40:20 +0000

    zero board Itself appeal education

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import time
+print(time.time())
