062dc91e1a62a3ae4903472638acdc0608934457
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:06:20 +0000

    become rubbish limit sticky pain

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from collections import Counter
+print(Counter("hello world"))
