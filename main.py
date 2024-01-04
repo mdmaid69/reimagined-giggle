d8532798f2ac642394db645cc65e87dd0156ce10
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:42 +0000

    have contents manner nor along

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
