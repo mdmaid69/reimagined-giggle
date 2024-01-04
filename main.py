12f4f2ad50c48274647527b389d8d8ccb28f8552
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:37:31 +0000

    roof in hold creature afford

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
