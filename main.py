be8fd0356417d56e8bc2874b027568112b88ff4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:27:44 +0000

    niggaacademy rate keepfrom use edge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
