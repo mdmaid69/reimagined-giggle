5e05d2d9fbbadaa588ae99e267e2c7edbfa1d94b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:54:00 +0000

    dance already telephone state wild

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
