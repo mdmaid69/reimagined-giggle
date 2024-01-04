1de8eb571191e1546bf8d4b12f02534d6f558e35
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:16:09 +0000

    trap mouth permission slice owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
