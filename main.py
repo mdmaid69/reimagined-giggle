3e1148544045102c48d39e739bd98b3a01082766
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:19:24 +0000

    chase treat to introduction chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_density(mass, volume):
+        return mass / volume
