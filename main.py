9f5aea46c2ff8ffe4c6d1fc1526dde1746a86fcc
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:17 +0000

    sign stretch concentrate belong in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
