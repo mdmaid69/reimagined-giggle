552456bf1119c8bfa0131cff677a3d623a39fd16
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:46 +0000

    blue splash sale shine scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
