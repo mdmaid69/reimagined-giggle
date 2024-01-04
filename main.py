dcfae18a19a47cad17127aa093f8d44770647dc9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:54:34 +0000

    discover sail few emergency clothes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
