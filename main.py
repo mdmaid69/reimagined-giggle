a54086a7d4fcbaf33a72069fb82ac97a5b1f51ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:17 +0000

    cry calloff lean comfort top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
