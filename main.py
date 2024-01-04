7d39a735601acf3b13584724fe45db288e67e004
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:47:31 +0000

    impressive confused example language doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
