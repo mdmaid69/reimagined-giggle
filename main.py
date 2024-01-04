d79fede0f121722ca90ca1b304fb03d2d9225626
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:10:54 +0000

    interruption wise various interview worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
