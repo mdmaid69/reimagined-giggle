7edbd9a15d503b1cf5cd6eb9f5a72648fd0aad52
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:29 +0000

    interest border means neat apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
