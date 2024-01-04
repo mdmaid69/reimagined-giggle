3ae7def48d6ae8afc892f63707daab74883e867e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:58:09 +0000

    laugh attempt level me child

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import time
+print(time.time())
