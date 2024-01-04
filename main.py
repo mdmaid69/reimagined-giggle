63fececf201efea4d0c3bfdc49c6049a46a924f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:17 +0000

    long poison future cause dream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
