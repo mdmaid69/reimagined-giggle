75aa5ccd866dae4dbde248f0aa9594e17c9af75a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:24 +0000

    charge child read seriously generous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def count_words(sentence):
+        return len(sentence.split())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
