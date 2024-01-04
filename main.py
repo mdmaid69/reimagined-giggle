e231bbe526748e47073f8a9f1d64423882b4cbe2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:02:02 +0000

    now special relative pronunciation niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
