3e3eed97df94ff6fd4d3056d0a940195c8ae3de2
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:56 +0000

    mark relation bush support rare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
