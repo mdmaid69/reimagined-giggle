b0564d4b4e16d35f07b921e8834bc2d75f8a26db
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:01:32 +0000

    correct mistake remark boring dick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
