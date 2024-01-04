09cb2252ffa795ee4f35b5d7d01603553ef50f44
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:37:12 +0000

    shut rather awake angry anxious

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
