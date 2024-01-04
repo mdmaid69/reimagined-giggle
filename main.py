ea60c00fe9ded8e6acc790549ff7f7e4e68bc8b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:43:56 +0000

    emotion limit keep tradition connected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
