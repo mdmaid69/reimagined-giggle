db4e1c2e9759b06dd306d9c93613755f25a4552e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:01:58 +0000

    bright opposite centimetre sale step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
