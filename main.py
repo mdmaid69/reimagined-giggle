0c3f98279506e61ece989f26fd605344b9463724
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:54:11 +0000

    support maintain size sensible discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
