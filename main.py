5911f93bc9ef0e6c7ef9e9aa73b58ba999aa9261
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:27:18 +0000

    which drop storm perfect temperature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
