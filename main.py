98da22f01b468c7c7c937a45b5743c46dd41a421
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:01:26 +0000

    defeat recommend how art flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
