efbb36e278c6b4cc0f41d1bf730a512017b81756
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:54:48 +0000

    importance dish praise any laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
