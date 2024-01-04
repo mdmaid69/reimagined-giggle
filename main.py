7640b3dcdaa236728127e7b7055f93d15457b8b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:33 +0000

    guide act miss expression independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
