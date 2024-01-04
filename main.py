244d80360eb149b461fb22e81c0fd916bd4ae605
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:51 +0000

    keepout offence high length once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
