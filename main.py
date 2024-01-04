3851a6ba004dc804ba5a42f075a91406cda504cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:43:46 +0000

    shock partner rush build frequent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
