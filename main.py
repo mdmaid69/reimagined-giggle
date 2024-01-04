9c6ccac4a6a04014ac1a147265fea3f0c4127e6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:10 +0000

    closely moon wire rather consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
