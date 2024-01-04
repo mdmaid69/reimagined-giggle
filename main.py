7cbf1af1745f44464be55c25fb1d2073877372a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:13 +0000

    recently react hot plenty that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+print(random.randint(0, 100))
