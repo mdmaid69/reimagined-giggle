4113d7d9173628b3a185ac1e066ce6d6b880bb00
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:12:16 +0000

    trial future guide beneath acceptable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
