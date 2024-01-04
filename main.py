e69862a4d75947748fa63fd1a6cd3bc9fb099e7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:27 +0000

    branch precise 568energy everybody back

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
