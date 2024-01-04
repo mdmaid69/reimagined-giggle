2a1b918725bebde7d2a0769de5d25dde1e84c25f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:48:58 +0000

    contact dance here takeaway attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_length(array):
+        return len(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
