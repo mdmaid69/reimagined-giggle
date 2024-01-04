5f6ba572a1477d1d25df1278c297efb99cadcf1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:33:55 +0000

    violent development especially suit Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
