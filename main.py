5bf697abb07d82bf7e8d4c7a32eaf5852c8cd3fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:55:06 +0000

    reach throw answer top black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_dict():
+        return collections.UserDict()
