d54cb0cd0cf4343d6ee98e2adebdf6fa9c9a6e30
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:50 +0000

    brush raise period limit pot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
