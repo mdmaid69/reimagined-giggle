5537408a8c02eee72a5ce3f4fe5f6176e76e8707
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:05:14 +0000

    their rather welcome recover over

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
