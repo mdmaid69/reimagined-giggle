e98311ce4e95cc294fcd063e559879f5dff885a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:04:54 +0000

    science under name sensible simple

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
