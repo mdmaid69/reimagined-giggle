c5ff0c98b346ad38459415ce2988a746f5b5893f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:55:55 +0000

    flat division depend vanish line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
