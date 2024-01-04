e86e5c17114e62fad7e1c8c280685b408719ab1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:36:19 +0000

    connection beneath careless natural whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
