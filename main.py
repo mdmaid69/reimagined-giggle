f73c5ad356a1924eca75817a45cc70fb629e46b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:44:45 +0000

    naturally comeover date steam present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
