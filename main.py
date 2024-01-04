13233f2bbdfb4b35cd6c419f104d6decf77ed34c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:02 +0000

    talkback bear explain another fair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
+def add_numbers(a, b):
+        return a + b
