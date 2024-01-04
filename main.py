284da775b2cb119ba9118e7ef3ec889fe318c2ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:04:26 +0000

    issue stop pin vanish equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
