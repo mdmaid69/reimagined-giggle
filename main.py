7ce65584014da55090f63b9cb37727eb06a33c23
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:23:09 +0000

    572enter bring into picture frame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_odd(n):
-        return n % 2 != 0
+print(sum(range(10)))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
