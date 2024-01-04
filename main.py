04eb5eac6fa439798d0aba232ac2ca854e5053c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:07:53 +0000

    so hurry flat name actual

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def split_path(path):
+        return os.path.split(path)
