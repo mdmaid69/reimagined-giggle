b038956617e23701630decd962b04284a9529a28
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:38:58 +0000

    really call move intelligent downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
