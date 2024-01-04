b93b0c6f7870f4365572b14165dc6802b0993673
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:27:18 +0000

    tool clean grateful far call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def add_numbers(x, y):
+        return x + y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
