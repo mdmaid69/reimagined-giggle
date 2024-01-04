f6ec8b7540cdbfce2a5c1309c35f8e7615666edf
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:09 +0000

    impossible raise corner prize possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
