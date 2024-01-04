54db7cd8e46cf55edb7ec049fbb0be2d7760dbd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:14:37 +0000

    family crash computer instant constantly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
