aae219ca419adb6e73148036174a200af53aa3c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:58:09 +0000

    computer burst nurse safety praise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
