01803355847cbc70865e2c595e8041f042029d36
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:31 +0000

    close crash admire workupto file

--- main.py
+++ main.py
@@ -5,6 +5,4 @@ def calculate_payback_period(cash_flows):
         if cumulative_cash_flow >= 0:
                 return i
         return None
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
