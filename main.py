f9e86354fcfcbf829f01235d8e1f114ad07f5270
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:17:12 +0000

    cheque dream sell down university

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
