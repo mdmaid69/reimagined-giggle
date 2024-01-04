e31756f45336fd9e0245fa95544b1cb71fb1a041
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:00:22 +0000

    convince run television maximum skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
