ffa4c7962d0507484af69eb873bbd692e8de643b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:19 +0000

    spread big well attempt over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
