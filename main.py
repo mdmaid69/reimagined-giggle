3d3e2e1168725aaf50cd0fb06dafbf7ec3975a68
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:12:03 +0000

    disgusting left period suckme keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
