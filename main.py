62d11fa5fd693834fd5c0a0875374960b3e7340f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:31:50 +0000

    clever the quantity practical save

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
 print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_work(force, distance):
-        return force * distance
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
