5730ea0d948e0edd1ef54083afb9da25d95778de
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:25:53 +0000

    to the warning light certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
