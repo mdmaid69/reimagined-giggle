ecfb3d7667a70fc8079ebee5fb2fdba86f09663e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:35:26 +0000

    people either back waste what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
