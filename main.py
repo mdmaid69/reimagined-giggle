44c6fd39dd6e754dff39a650981e47897ff1fb7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:16:24 +0000

    anxious situation tap wind worried

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_as_set(array):
+        return set(array)
