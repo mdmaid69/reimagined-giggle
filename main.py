99174d8ed8750bb95c837f6d615808e92b501251
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:33:10 +0000

    bang hear usually embarrassed getback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
