52db7f48fa53a1b510824a9276ed2ed20be286ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:30:15 +0000

    bill tragedy one trial everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
