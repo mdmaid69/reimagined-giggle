605fa7bfab1b8b4616243af1fc73e34bd0576fd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:09:01 +0000

    meaning hit about mad yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
