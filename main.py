340f65ef74f603d3dac96643364974fdc275fb53
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:04:22 +0000

    downstairs pencil issue confident begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
+import getpass
+def get_username():
+        return getpass.getuser()
