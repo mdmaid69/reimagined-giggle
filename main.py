fb1764e232277cef3229a22ca1bdc9d43c686b15
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:22:57 +0000

    replace upstairs insist shall cruel

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+print(sum(range(10)))
