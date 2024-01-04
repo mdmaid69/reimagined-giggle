63c4801debd6559e964d3dbe07b2db3a67fc4b4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:05 +0000

    practise flavour properly represent factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 def calculate_force(mass, acceleration):
         return mass * acceleration
