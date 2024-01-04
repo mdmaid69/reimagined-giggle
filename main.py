6bdd391f2204de1bf3eabb29cc64d76665058fc7
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:28:43 +0000

    wonder crime drop country that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
