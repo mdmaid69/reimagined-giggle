44d0abfbf6f5da3ac869681eff36bb2913f5cb62
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:10:17 +0000

    smell condition type she but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_item(array, i):
+        return array[i]
