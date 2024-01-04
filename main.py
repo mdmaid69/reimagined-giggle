e4b29f44d846debba1758f6eeaa583b733b7d6b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:11:38 +0000

    expensive progress win some draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
