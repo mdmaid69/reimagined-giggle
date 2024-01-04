53bdbba9f72295c01a19b05ac60d3534d0b898cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:07:40 +0000

    description oil pencil debt that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
