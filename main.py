42b13ef0feff7531d9a3b2ca9487b0584f701ca2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:36 +0000

    persuade drink burn step space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
