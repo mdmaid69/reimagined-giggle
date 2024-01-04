39be7701849c1919a9136835d01f74fa4cfaaa4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:09:03 +0000

    this exchange godown mother please

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_density(mass, volume):
+        return mass / volume
