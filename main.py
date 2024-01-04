cf2ad854697d6740fb68abd3cf1d5a6a96921e08
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:21:52 +0000

    wind everywhere port game until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
