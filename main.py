37a11ec5af8660edebc4ca8ab2e00543d76e1dea
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:19:52 +0000

    think division holdout operate tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
