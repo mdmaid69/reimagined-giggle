f71f440ffb7d68491341b64891a6f313fc4eb438
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:00:20 +0000

    tonight section confident general relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_work(force, distance):
+        return force * distance
