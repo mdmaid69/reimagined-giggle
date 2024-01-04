84c62b29f08075e0bcaf3338df705eac938b5131
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:00:37 +0000

    turnover neat dark impatient proud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
