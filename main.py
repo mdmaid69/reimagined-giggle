35a3e32682279a7d82d8a41f482e8825d8e3a63d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:45:10 +0000

    accept cruel comeback genuine feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import platform
+def get_python_version():
+        return platform.python_version()
