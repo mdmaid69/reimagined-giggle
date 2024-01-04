bccafe4767ce602f356ed9d3f2fe76aa0f04874f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:12:50 +0000

    noone joke plan mix push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
