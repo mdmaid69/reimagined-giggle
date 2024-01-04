dbb5acff85c2b8fc8c6fb362bac18c43cabf70e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:57:41 +0000

    clever cause face tongue possibility

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
+def subtract_numbers(x, y):
+        return x - y
