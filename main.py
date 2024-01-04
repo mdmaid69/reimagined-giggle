aae01f94e49fcd92fc9f251b62adb91afc8c8ba2
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:24 +0000

    Ll something further runaway past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def clear_array(array):
-        array *= 0
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def get_string_from_array(array):
+        return array.tobytes()
