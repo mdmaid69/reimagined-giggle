5414cf0208fa59f7d57bdcbd1f3e59dfe1826a34
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:16:39 +0000

    unlucky silent listenfor finish attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
