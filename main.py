0b818bde7aa852c930c54681e5e30633c87a4e5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:17:56 +0000

    accordingto Ff switch tool fast

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
