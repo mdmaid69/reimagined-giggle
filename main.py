44c84c93fbe9cfe63c6ebafd42aaeeae8e26fb53
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:45:16 +0000

    keepup direct share to most

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import logging
+def log_message(message):
+        logging.info(message)
