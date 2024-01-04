a867a9a95dc70399671301db84db560d39d00d8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:33:57 +0000

    lead benefit float post comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
