26edae9442513ab5d6b6a3ea8382e117a3c11fd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:32:03 +0000

    war recognize excuse consult bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def append_to_array(array, item):
+        array.append(item)
