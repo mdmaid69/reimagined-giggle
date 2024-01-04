2caa68bce6cc4aed2815ef9a19780571f3237a13
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:59:30 +0000

    cut consist government he boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
