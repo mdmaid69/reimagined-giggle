c7c5e5f4b27555c3055f3d69597d76a74ee153dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:20:59 +0000

    suggest judge failure regret treatment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
