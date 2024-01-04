05c14183dda5aecd5cf399c12f8a15ef029c8af6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:31 +0000

    suck bringback suppose warning increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
