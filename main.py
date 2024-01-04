3205e0e5cc5fff214cb1dc71897cd529edd792dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:46 +0000

    point weather tap track sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_area_rectangle(l, w):
-        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import sys
+def exit_program():
+        sys.exit()
