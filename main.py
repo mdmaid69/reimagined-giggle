3354a1b991303c7e7fbe31637aed3675fc2e14b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:15:08 +0000

    welcome instant success noise hold

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
