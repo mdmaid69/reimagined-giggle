fc50a7a507fb37bcec4a08ed580a7905ab45397c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:07:36 +0000

    form drive plenty from hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
