7a23abc1216892302855f276f2207b6cc65712ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:17:53 +0000

    occasion tense Ee number go

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
