924364f6876a8a04d84f5e3d9ac5e3c79af6c453
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:40:24 +0000

    vote illness correct fix wait

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def convert_array_to_string(array):
+        return array.tostring()
