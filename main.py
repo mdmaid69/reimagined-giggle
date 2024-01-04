e49492d75beba7bd618b1ee997e7e22bb8c8403b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:55:46 +0000

    must succeed hot dry see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
