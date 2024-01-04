97100bf37864b15df84a9fa1560a23c529031b11
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:26 +0000

    duty cope half runaway length

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
