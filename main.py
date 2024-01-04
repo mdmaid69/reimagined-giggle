09c93a340811119aad9d709e3a789fc32737a26e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:17 +0000

    will cold join boat escape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import platform
+def get_os_info():
+        return platform.uname()
