b2d1dae8eb81cd116e1cc588de3d6cbe99257540
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:40:52 +0000

    prize black broad tough disappear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-print(math.pi)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
