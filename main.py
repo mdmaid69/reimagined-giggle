b80891d61469e20d82daca3c4f849a6ac9589f2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:56 +0000

    sit fall but pass 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
