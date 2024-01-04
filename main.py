56b6d2d3d56458a3f77560e20bdb5e1b89c5a607
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:48 +0000

    pull off fierce tired Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
