eabcafb9c5f5222fb8cea73c2bbd4563b4420d89
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:00:41 +0000

    delicate tragedy main experiment even

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
