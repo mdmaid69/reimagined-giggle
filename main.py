25b5c14ac7315d07aace5c219e94b5c983cd3963
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:53 +0000

    shop confuse study lean Ii

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+text = "Hello, world!"
+print("Characters:", len(text))
