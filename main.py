5794f9fe3ecdffb4c0f172a442249bea26683e1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:20:38 +0000

    nature drop weak giveback form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
