fe32d58320df748a16a4f457bd9c0e0af2f2ad52
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:24 +0000

    rate contact rarely valuable term

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_power(work, time):
-        return work / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
