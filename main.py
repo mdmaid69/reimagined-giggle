fa1da48fbbfcd00bb1f54ef8fdb86ca7909d5cc1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:44:43 +0000

    fall yours fit criticism down

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_dict():
+        return collections.UserDict()
