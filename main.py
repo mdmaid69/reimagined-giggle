f6e10154c6cf46f12c46242c9a2d41babc1fd9dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:27 +0000

    apparently interview software should various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
