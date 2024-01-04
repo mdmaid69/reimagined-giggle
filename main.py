412801e68f6374a86d97405dbac5d03f4a246347
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:35 +0000

    tip limit relative doubt sight

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
