dbeb49c0236288a791283c43f636271c75113871
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:35:05 +0000

    job active perhaps successful keepout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
