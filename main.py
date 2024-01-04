959f245babe72096b3eb566aefb68c264113fa55
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:29:39 +0000

    character hospital instant mystery turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
