89e39d0eec30837807760af82912969fdf52176f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:02 +0000

    advice delicate truth anybody check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
