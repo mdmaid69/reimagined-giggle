a6038401d5af3f377cbbc55baeb49160dc952bc7
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:03:56 +0000

    distant politics interested excuse bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
