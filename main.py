f1e44ea57a14586dbfa11a600444cd844dff6439
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:03 +0000

    legal reliable start touch big

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def multiply_numbers(x, y):
+        return x * y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
