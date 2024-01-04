ca0fa6bc6ef5d44789fb8b24dacde48a0fef1e42
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:50:03 +0000

    indeed treat prefer everywhere consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
