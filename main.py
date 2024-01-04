6126b20ad3d3676ab78e2fe53c23d32aece1989a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:33:47 +0000

    contents smell pick independent they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import collections
-def create_counter():
-        return collections.Counter()
+def get_array_as_tuple(array):
+        return tuple(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
