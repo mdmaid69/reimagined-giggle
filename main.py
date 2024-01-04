e962b0f4e9c5c32493c15ad8bbee22fd04077889
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:35:38 +0000

    comefrom need that arm content

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
+def find_max(lst):
+        return max(lst)
