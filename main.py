2b19c90c58ec89a8a1d998567cae54117bdcc06c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:30:57 +0000

    mine either exist snow disapprove

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
 def calculate_logarithm_of_gamma_function(x):
         return math.lgamma(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
