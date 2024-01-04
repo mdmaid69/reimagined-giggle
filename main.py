a7192155c644e081f29ab6f0aa5e4e55a7f299c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:43:17 +0000

    apologizeapologise justice worst worth communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def sort_list(lst):
+        return sorted(lst)
