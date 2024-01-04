bc3e5e868c17c973a2d61e9a7f8cf16a6f8dc252
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:30:00 +0000

    tour continue takeover adjective part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
