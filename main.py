4eda45aa2c880c0c160083c8c422eba22f4b925f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:46 +0000

    emphasize interfere like possible preparation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
