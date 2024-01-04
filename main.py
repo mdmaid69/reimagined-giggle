ef0dd38d60b0e6bf564e2e151d4044b20baa6d34
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:50:10 +0000

    switch plastic plate example birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
