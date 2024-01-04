fd1b75145ed01f9fcd274b5d24dabbfd3e2cce91
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:57:38 +0000

    unpleasant defend march success pause

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_max(numbers):
-        return max(numbers)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
