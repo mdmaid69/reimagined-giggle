93559b558ba346969f293e9d2aaca17858606763
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:50 +0000

    whistle total motion hear swallow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_priority_queue():
+        return collections.deque()
