4568df50b41612855ca2b14a0395de45ed2563a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:07 +0000

    develop cap straight fork attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
