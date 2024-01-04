3f1fae61320b63c4929dad3b561052d791730d01
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:19:49 +0000

    emergency object total pay wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
