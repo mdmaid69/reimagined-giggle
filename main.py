8efb74aca0281a2fe340dcd1a914acad77bcaec1
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:45:56 +0000

    able tread calmly complain mend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
