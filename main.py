0551817ffcb27f01a4dd3a413feeb241f92e7703
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:26:56 +0000

    development whistle quick 179bite file

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
