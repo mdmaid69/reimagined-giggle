708b319ac6da8df958b5d6be7098596c51311c9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:54:44 +0000

    curious age this anyone shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
