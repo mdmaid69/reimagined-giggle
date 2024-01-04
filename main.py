dca1ca95a8e8c1db0a60e4379775f01dd35a1c23
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:22:42 +0000

    plan disturbing perfect day go

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_index(array, item):
+        return array.index(item)
