d15846c0874b2d82d1cd27fcf65902f0c5c80ff5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:03:08 +0000

    principle position male band nigga

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def square_number(x):
-        return x**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def sort_list(lst):
+        return sorted(lst)
