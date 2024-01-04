b8024d49addd3fafc72df9246ee201070489f143
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:16 +0000

    inquire month comeup seat smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def remove_duplicates(lst):
+        return list(set(lst))
