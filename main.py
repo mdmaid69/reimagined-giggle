c1b2f244b2f2d0a294c91885186181084cc46857
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:02:27 +0000

    brain especially sure pass production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def convert_to_binary(n):
+        return bin(n)
