d02b4a152c7bff64488ccd576955d213941c6b86
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:52:07 +0000

    Qq inform remove point instant

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
