cffa028f194e230bb29e9b9cb2af0c5d277afa91
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:51:35 +0000

    sensible tour truth cope thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
