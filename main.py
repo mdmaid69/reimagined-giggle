e73b95d8b4065ac062d319eb7e9657849a6a0b5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:35:05 +0000

    above often practice complicated ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
