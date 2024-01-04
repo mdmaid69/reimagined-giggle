9668a202b25db0cf225f52baea32f500ca5acdc3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:56 +0000

    secret body involve father unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
