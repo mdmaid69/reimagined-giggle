8ad9dc69bb83c3e3cb4237d53a3a9af388603758
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:11:13 +0000

    poor suckme project Hh impressed

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
