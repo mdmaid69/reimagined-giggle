38b537ca7c87ec188aa24ac3e53b20bbbfe34ca4
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:32:11 +0000

    sea person eye comeout unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
