41f903589705019d3a936033377e8cdb9d37f0a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:59:00 +0000

    bag terrible live able nurse

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
