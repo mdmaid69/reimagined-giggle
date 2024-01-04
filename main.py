4b68dfbcae40d7f5368fd8f57177f7eedeaa440d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:26:22 +0000

    strike crash wrap herself consult

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
