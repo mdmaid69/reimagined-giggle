cdb3c6a22d18bb0f8a5c2e9d357b168645173241
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:38 +0000

    fear grow sick accept yet

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
