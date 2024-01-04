0f12c7fe87c5c412a7d8ae8e53cd3e4ac1b15e9b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:33:15 +0000

    talkinto manage brush cheque slice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_power(work, time):
-        return work / time
+def convert_to_hex(n):
+        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
