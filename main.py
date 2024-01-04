e159065f85c47f2b1524366201f2cdf3b8ecb5b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:15:15 +0000

    name length ground exchange disagree

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
