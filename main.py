d480d8f650b8e04f9989fba345319412a26ab8c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:44:54 +0000

    fear freedom exchange 567enemy exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
