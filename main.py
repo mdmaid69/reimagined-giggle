1a65f2a929b07afd05aca5dc6aa5c3e5e3deb3b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:21 +0000

    lost talk mystery example handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
