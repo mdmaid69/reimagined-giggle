90774b7688ddeb6b0baa4cf5c328ea0c3fa81dca
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:45 +0000

    well silent 564encourage move unit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
