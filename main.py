65a0d3e1589bab71e986d74cc7a10ae9a973a93d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:00:40 +0000

    priority flood drink almost child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
