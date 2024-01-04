9b8708df60147c6f8e427ac7b278b169e95ad67f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:12:57 +0000

    majority record bear travel your

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
