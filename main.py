7b59a2f8556aedaa1810dcd562538bcd27815acc
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:39:14 +0000

    job slight own familiar picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
