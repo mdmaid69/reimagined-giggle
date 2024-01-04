e30f44f3da365cc52883ee50475c999b17b29e4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:18:36 +0000

    callback now hollow replace tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
