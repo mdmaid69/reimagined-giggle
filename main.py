6d07e4227f678de2d3abb04d4cd0f4a7053b6104
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:21:30 +0000

    area polite past street circle

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_length(array):
-        return len(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
