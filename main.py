a76db27f15ca5f33d8e2ed1d5dae8cf4ce90b963
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:10 +0000

    lift cause fix full bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
