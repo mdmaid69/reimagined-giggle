6d8165c6d3cddd28b0a6fae9f1b87f2a216c423e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:18:07 +0000

    stick flavour store rid bit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
