dfd5f2c24f8de1b2a7c5a7b533836fb768776d96
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:48:17 +0000

    uncomfortable refuse into guess physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
