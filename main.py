a506a24a5e415dadf794a603b9ecd5cc7e673824
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:07:35 +0000

    runaway extremely steady last comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
