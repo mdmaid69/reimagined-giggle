eff36789e183c1075e1fe0c569c49613c7f1df15
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:32:45 +0000

    runinto conversation event hurt shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_list_from_array(array):
+        return array.tolist()
