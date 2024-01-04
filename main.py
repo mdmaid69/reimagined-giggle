d62dd1c1d50825f33105d7f39337a5eddfce397e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:15:29 +0000

    patience computer rate suckme free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
