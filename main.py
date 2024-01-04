2179a1f7f16ccc319d61ba862c2b5be35bc3ba20
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:50:47 +0000

    estimate fast knock until always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
