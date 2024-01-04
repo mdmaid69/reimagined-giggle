06f5cd60dc3874b534d7db27edbea50826831fec
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:02:53 +0000

    field meet sure inquire often

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
