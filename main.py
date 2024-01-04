6c2cc67c1810a86e48a046c9ab38ec6b46bb6465
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:47:04 +0000

    nowhere scream forgive remove violent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
