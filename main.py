c0fefb7a8cc8c8e4daecfbfbc9031e5a71585492
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:43:16 +0000

    cry creature hour mean below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
