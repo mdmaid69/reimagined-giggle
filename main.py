cb1c044ce190024f49c83f1e4545ca1827ba8a7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:16:34 +0000

    water occasional cheap awake sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
