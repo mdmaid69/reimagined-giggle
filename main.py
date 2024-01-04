86ad8501faf06e2b0813b1cab7ac9a83db112473
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:08 +0000

    eat general intelligent stare radio

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def is_even(n):
+        return n % 2 == 0
