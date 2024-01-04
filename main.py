99aa13370fca87bf51d7612180d91d26d8c73d4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:05:17 +0000

    turnup tie shape nerve chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
