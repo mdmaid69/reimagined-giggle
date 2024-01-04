40c983b60bc03f12ba95ca50c92bbfaeda6b6d69
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:21 +0000

    share pour insist to interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
