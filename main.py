7376e8a3b8768c87ba95ac3dfa143fa618d8ee80
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:27 +0000

    exist check pollution education year

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
