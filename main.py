9efd0621f0de1733a4683a715e1a89bd5f9d6c1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:49 +0000

    first temperature closed square absolutely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
