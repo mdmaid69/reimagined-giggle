670e74344d47d0b59332f19fcdf14de63142cf20
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:03 +0000

    raise or piece answer stick

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
