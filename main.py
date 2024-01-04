e35413c18958c60665c47a3fc792a4e3664cbe88
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:07 +0000

    visit shake middle easy too

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
