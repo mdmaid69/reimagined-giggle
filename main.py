9adbf816c694f7ce0093e0b33b789e40bdf5d614
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:51:20 +0000

    wish sport cheat join that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
