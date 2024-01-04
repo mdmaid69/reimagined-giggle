424ca33c6011e6e7444053d76b4b603a18991ea6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:07:31 +0000

    clean date activity explain be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
