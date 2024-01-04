2f96b5b388bbcb15eca9952314ac7f7eeb40d436
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:21:29 +0000

    rank expect combine light afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
