ee769ec9b1ec76f6381f78c7db8c5fc772d316be
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:21:46 +0000

    state increase Dd show some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
   def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
