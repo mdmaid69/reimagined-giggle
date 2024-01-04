ffeff8e4cbc29a2ffab99207e6f0bf15d0ff3534
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:34 +0000

    level herself blame broad temper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import time
-print(time.time())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
