e9a834c2896fb8ccff6a6846f86c6a61d4f43a86
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:34:18 +0000

    she important into book either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
