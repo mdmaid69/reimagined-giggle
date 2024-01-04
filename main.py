2c5d7f2c29986ecb792e28efab459909220ccaa7
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:35 +0000

    either sore unpleasant custom frighten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
