06d7c7dc42ed2ef8c9ab0dd27900abc65e22d948
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:18:11 +0000

    luck basic search skill ear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
