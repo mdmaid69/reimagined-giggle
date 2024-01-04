6fe9bcb150cb34f97b26e9d0f4f491bfc65a71e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:55 +0000

    invitation bread wheel pleasure tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
