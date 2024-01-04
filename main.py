a7c3ff139f3b42f7354824d745b22c60b43adc62
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:18:23 +0000

    remember keepout Ff organization exactly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
