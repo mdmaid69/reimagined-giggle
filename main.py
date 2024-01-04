0d254a61fe6b5a50941b9dd516fb37bd36230071
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:53:27 +0000

    attach perfect after correct deaf

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def is_odd(n):
+        return n % 2 != 0
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
