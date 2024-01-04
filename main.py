79e6e81e39c418cab69e84daa91928e6d10ad33f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:49 +0000

    run will reply speech minute

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
