f51c432b033f770f5578db3ebd7efa5dc5223065
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:54:39 +0000

    down recommend song we blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
