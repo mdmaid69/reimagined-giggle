28b35456d0a05a47086f4e4667b59cdb47350895
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:35 +0000

    I band unlucky increase everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
