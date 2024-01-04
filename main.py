1b5a13d21427f6218b84f5030c1c6a25e7fa955e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:30 +0000

    weather cost same stage far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
