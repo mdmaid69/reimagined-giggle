ec56c7cfe4484e2dfd5d63bd5adedd3dfb7679db
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:20 +0000

    pipe mind service run he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
