f004a23b1d5cb2ec4290714bb2d51bc29e05e8df
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:27 +0000

    snow miss paper develop face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
