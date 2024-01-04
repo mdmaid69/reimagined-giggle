ee0f69f1a54596b0f3c455c62163d07dc408af58
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:07:41 +0000

    wide level previous we Ee

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
