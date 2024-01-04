a022ebefba0791b115cd18ec1ebdf7b095cd881e
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:08:47 +0000

    knock goup cruelty distance soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def reverse_string(s):
+        return s[::-1]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
