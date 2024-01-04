529558b47492f867ab840a3452822346ed0da545
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:41:26 +0000

    tax might impatient scrape lookafter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
