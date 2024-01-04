8ebf813ef613942ced95d82d29c42c94c3b71a24
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:54:00 +0000

    hard exist further level whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def convert_to_binary(n):
+        return bin(n)
