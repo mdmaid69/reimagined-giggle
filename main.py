43004537d187f4fe53ccb9c76ed73d9d0613c12e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:50:38 +0000

    round pause indeed people doctor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
