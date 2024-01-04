a364f2a552c2738355cad03d575f1d9847b749dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:04 +0000

    check advanced operate skin separate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def convert_to_octal(n):
-        return oct(n)
+print("Powers of 2:", [2**x for x in range(n)])
