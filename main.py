31b3aa526315f70ce69bc75195f21f4a3f52879d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:53:05 +0000

    disturb earn wave dream plant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
