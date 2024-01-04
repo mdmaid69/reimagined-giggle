d5af1a1e769fe40ffca4db295743de5dad8b7665
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:32:32 +0000

    possible such patient level trick

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
