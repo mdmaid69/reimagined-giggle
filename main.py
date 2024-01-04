90d1b3ac57ba0e8d307dfa71b5cdf36a18255d32
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:37 +0000

    hair hope ear attention turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
