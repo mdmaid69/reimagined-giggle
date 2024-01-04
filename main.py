e778d056cbe551b3702557624616ed6b687ee4c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:34 +0000

    main pattern search experiment top

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
