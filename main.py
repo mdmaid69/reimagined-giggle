3aa862be909b1c53311e0004719b8800090ef4af
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:32:53 +0000

    important become lead whole test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
