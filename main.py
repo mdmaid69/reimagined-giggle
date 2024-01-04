0538106132c1b06548e91e7f1abc4e1009b7f41e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:52 +0000

    similar expensive 567enemy among sell

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
