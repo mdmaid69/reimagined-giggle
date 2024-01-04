298b98e63c8e5ec45b2ff7534651be43fa16ce0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:49:50 +0000

    proof spray part present mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
