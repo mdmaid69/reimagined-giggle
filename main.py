b770965e99ef4ba485b949aa97f69b442dc57f5a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:37:48 +0000

    total permission anywayanyhow payment judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
