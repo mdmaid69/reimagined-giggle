2fd764d3875e8092a76eff407526ac476f8132d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:54:40 +0000

    exit warn collect evidence drive

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+for i in range(10): print(i)
+import os
+print(os.getcwd())
