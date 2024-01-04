e6317d27a730b43b1a5a606708915a8e595599a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:08 +0000

    close especially considering again takeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_current_directory():
+        return os.getcwd()
+def convert_to_hex(n):
+        return hex(n)
