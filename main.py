0e3f3d4ff37741e20c67d5462574b27902448779
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:25:52 +0000

    escape fix foot wide destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
