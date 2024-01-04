0c47fb665bc2f8bff44622541957f7df7d1f6104
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:56:52 +0000

    progress level bottom Qq can

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
