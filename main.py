e84dcd801f33dad01615956ba6044caca73ff7bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:08:42 +0000

    comeback soak cheat deserve before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
