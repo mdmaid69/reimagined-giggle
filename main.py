24ecd65db43b8ddb501a11672765280b77b2ad67
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:00 +0000

    unhappy piece pen about risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
