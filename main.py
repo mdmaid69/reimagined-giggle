bdc419d9df49f5d21e3c7754a02ed1e07fa3e8be
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:19:31 +0000

    lucky serve uncertain bound again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_characters(sentence):
+        return len(sentence)
