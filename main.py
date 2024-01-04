d10f1ad0f607faab638487a5b1d9a979028d15c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:20 +0000

    priority bye fair consider no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
