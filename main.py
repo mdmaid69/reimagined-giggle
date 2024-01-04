776fe13d717b9b71f2afae8f0671cc01bb6703f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:51:52 +0000

    tend also only top silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def reverse_list(lst):
+        return lst[::-1]
