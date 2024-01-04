4156f6eee3a579618d709469982edd4915035e89
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:01:15 +0000

    move invite inquire pronounce back

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
