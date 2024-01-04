0bfb56339afe6279a4c6dcac278110871ca8fb5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:51:29 +0000

    close wall fortunate over live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def convert_to_octal(n):
+        return oct(n)
