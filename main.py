483deddb482987b4aea9b92a75dffa9c35b55ee0
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:56 +0000

    terrible right next rob pressure

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
