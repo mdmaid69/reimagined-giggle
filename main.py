b37fc155e72e4ab5a71d68010606baac855420ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:08:07 +0000

    curious intend pronounce number note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
