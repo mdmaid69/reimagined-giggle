89a952610e9c2508fcb2b8db39fed9fa82b67099
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:00:59 +0000

    fix than young whisper lock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
