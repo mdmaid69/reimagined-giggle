fae71ee0744fcf78ed51710018e6a4fd6705f945
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:57:15 +0000

    mix fight speak nor advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
