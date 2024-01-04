fd241ddce2b4824d6439ed891137bc2dc91609fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:11:24 +0000

    hospital moon split belief soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
