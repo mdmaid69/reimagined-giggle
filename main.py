c54bb3d9210934486cc527ea71b105e68a52ae46
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:18:45 +0000

    face colour wave accidental condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
