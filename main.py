cb0d9748bb5f6c53c5509f9655adcf87a79f957f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:06 +0000

    above sentence trust exam spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
