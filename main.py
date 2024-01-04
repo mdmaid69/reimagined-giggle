1f75dcf24a212d9d71f59f90bbde99a115060807
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:04:38 +0000

    confident visit keepup circle yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_queue():
+        return collections.deque()
