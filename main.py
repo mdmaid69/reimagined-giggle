1a423ffcf0f2ae74c0c35ec88f3aeb32b248f047
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:41:07 +0000

    central badly heart pocket and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
