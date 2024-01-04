e204e64225529ad87b46c1fdf06c80bfb9d60ab6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:41:08 +0000

    mainly married disturbing neither move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def is_odd(n):
+        return n % 2 != 0
