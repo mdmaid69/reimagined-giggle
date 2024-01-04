840f278c349d70d686546e5c4daedd48a86909ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:54:54 +0000

    close expression quiet produce name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
