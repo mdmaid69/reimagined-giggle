09f653eb598c859efd5909da42ea00fd18ed3b68
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:37:18 +0000

    how voice section pleasant education

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def square_number(x):
+        return x**2
+  def convert_to_hex(n):
+        return hex(n)
