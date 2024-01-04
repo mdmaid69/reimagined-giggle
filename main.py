8d2c29bc9e585dac43d08111043d959bd30a6941
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:47 +0000

    discussion such them pull spring

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
