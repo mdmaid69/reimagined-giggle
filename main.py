2ae3a71be96b261cd2e1f15a9f070bcad0aa90e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:56:57 +0000

    upset rule on run within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_list_from_array(array):
+        return array.tolist()
