f4470fd40f2b9219f176094b66a20cfeb83cf416
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:55:59 +0000

    complete disapprove know date through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_item_count(array, item):
+        return array.count(item)
