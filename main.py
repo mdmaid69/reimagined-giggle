dbef145074c728d04162b76d52a0eb8136b132ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:23:04 +0000

    please fail left face write

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
