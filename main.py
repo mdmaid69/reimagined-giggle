b99236c756fa5ee8db8d0b18d7646486cffcacf9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:51:37 +0000

    far announce weekend march schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
