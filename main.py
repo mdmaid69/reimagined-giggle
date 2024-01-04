23b181c309b740e27250c3167194c1d3921ad4b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:02:35 +0000

    send switch play accidentally stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
