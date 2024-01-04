e02375b4b34fe5cbf15c8c7f61fb4800a487a16d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:27:37 +0000

    none soft press shock imagine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_binary(n):
+        return bin(n)
