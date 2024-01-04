41e8cb1fec5d0423b00e3f31e644088a4a5baeda
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:29:12 +0000

    church boat contain and centimetre

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def multiply_numbers(x, y):
-        return x * y
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
