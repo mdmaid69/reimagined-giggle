bac7d91acc616739e54651bc02564779e0def15d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:09 +0000

    represent snow better narrow most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
