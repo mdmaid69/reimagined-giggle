81b2a07e7d46f1a9b6521560e92f4ad256486c71
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:07:59 +0000

    loss kind hear breakup emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
