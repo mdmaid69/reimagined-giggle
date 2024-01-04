fc5c2380d83f8a7e431ffa4d0dc7c961874f4f33
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:17:44 +0000

    tap summer fashion neck stranger

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
