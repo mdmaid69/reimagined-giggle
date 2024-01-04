4c56b2bee77c4a9dd966d2784f4e839ca9184e89
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:24:04 +0000

    strict talkoutof notice boat practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
