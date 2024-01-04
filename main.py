ff7c052902908e4b1ac8b053e571345c120b2679
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:23 +0000

    worst shock present confusing straight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def sort_list(lst):
+        return sorted(lst)
