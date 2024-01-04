22b686bfbea350096722fad77269bb6483a6564b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:40 +0000

    material true use mine yesterday

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
