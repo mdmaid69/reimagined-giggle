ddbd46b7fcb7938696d44455c5ca6b0e78b89712
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:17:33 +0000

    smell forget breakdown pin circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
