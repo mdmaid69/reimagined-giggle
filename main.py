e3e10b538e8467c976613eadbb039dda4b08b088
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:44 +0000

    natural cap tired satisfactory cost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
