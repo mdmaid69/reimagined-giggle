9c9acdffe275cb89a2f80c0563e07f88ca06d015
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:57:01 +0000

    past pure exchange whom ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_user_dict():
+        return collections.UserDict()
