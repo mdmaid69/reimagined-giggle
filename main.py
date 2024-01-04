f0f4b0c98443acdb789623a7e80632da5508ae41
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:38:04 +0000

    level Ee expensive priority feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
