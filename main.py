5561d217c28fc0fbbdf15bc3cc1a860257e83f1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:36 +0000

    life variety attach matter join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_speed(distance, time):
+        return distance / time
