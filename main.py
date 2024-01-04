ecdf0591a816f070e0934669818628c217b35026
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:14:19 +0000

    street back pupil advanced shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def clear_array(array):
-        array *= 0
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
