191153f209017f40b0d9ed9f71984ec67729de05
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:57 +0000

    whistle history will makeinto roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
