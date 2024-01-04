28e3550174c0be973eedc011a0445ccfc3617d47
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:35:41 +0000

    perhaps listenfor underneath land advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
