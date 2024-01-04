a2f2749c238ea9de9da548418422ca759a278c4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:33 +0000

    dance exhibition number centre anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
