d68182c35f85a20f3d39cda5939d1f443e4c463a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:47 +0000

    operation change platform female different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
