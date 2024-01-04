bb9753d4d5b2a6e09f177fe7536f5bcdc68c4217
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:55 +0000

    conversation politics fail fierce possession

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_repr(array):
+        return repr(array)
