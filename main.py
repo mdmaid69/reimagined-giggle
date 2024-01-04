1a6183dddabe823ef8b15fdca1297b37661df19a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:58:52 +0000

    somehow house opportunity thing cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
