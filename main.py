d2050aea422588daa0085268ba4c810c772e55ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:45 +0000

    spoil recommend professional age conduct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
