0a1e762e3b6dd61ec435e35af857b41182186950
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:59:55 +0000

    related tax find moon message

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
