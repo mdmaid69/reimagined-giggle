12e7fab43437b19aca224824b327581937542732
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:45:53 +0000

    length welcome smart experiment pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def append_to_array(array, item):
-        array.append(item)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
