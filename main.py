d2e6e7e5ce8f246350f4ae4e8d875ad04d25fc34
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:00:40 +0000

    I day shame thankyou murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
