0d9e7ce0136161a8621d2bb2468b7fa82ebce431
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:13:33 +0000

    frightened notice himself watch crowded

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
