7060f933693a9897e991b505112c5ba9b1b51c9c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:36:37 +0000

    slice murder result bury realize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def cube_number(x):
-        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
