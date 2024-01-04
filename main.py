4738a34e109461d0034b0d082c54c95914059223
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:47:20 +0000

    colour much for pile listen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_directory():
+        return os.getcwd()
