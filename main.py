6c1ed729ceb06096c4a0940d7141c22466629f1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:40 +0000

    low subject disturb show you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def get_array_as_list(array):
+        return list(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
