9f8aca68ef1746489e8eeaf9919699884567e95a
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:43 +0000

    guess roof neither pupil post

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
