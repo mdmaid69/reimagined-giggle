d90b59913f7c6944b54fcc19272f1ba9542c082b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:54:22 +0000

    back basis snow seriously their

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
