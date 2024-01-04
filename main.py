7cbbd4302265faf84e0a30b5b1b51536f632d628
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:49:16 +0000

    cause disturb thought range question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
