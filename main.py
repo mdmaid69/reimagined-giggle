e4f6420cf153dbcff89d2d674267f213163abc3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:29 +0000

    among skill offer minority influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
