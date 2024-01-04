8459d56f2b9095bdb4755f11f9df857f0709d865
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:53 +0000

    foreign measure journey year risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
