a9b3fd7a07b50dc6775b783a379604b5e97d0083
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:32 +0000

    passenger between furniture physical gather

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
