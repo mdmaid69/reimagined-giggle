a2e7c6295bfdf2b6aada745342e7f821f570117e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:05:04 +0000

    check winter stop excellent seem

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-for i in range(10): print(i)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
