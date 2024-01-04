061b4c9acce3238e2784a9a6db239fa042d54e59
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:56:32 +0000

    workupto attention letter innocent meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def clear_array(array):
-        array *= 0
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def add_numbers(x, y):
+        return x + y
