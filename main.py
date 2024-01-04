a26d4d9be8346624bd4feeb081538feee72c5ec8
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:28:17 +0000

    spin apparently mend old conclusion

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
