ad1eaed5b6b540e4c67641b83e9c059a1f257868
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:59:02 +0000

    new waste man dangerous authority

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-  def reverse_list(lst):
-        return lst[::-1]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
