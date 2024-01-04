9576c3e7b0ba21e99c75e8d81081e88fbd07daf3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:50:20 +0000

    backwards write sport spoil character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
