e36aece8ddde8d79ebb7e1f828447e057e045041
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:55 +0000

    other lookafter have scare stone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
