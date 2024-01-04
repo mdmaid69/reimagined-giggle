c4e8400efa2da0abac31d85320d723ea1492893c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:23 +0000

    appear leave hard matter arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
