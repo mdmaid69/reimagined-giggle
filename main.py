d4703464e84ce8b9a6c9b299085206cdfb9963f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:48:55 +0000

    join name petrol war kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
