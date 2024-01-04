2ab2b68b489cf88cef91f4c4be9560341192b8a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:32:07 +0000

    home attractive strange store tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
