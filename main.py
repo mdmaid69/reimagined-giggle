5e8a5a544a43b54fc9c64940c29ff4d191dc6482
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:47:10 +0000

    occur loss offer crazy lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
