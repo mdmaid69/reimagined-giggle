ab216d75c6edc7e16573258421d713f18eb7e342
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:56 +0000

    sell estimate mess study rid

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
