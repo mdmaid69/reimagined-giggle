2bbdc1a4a56a5691c4f34d41a8bf6b45ae0bdf98
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:31:25 +0000

    very bad well other reach

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
