a131c789ac49cc04ef6fc8a6bd325b915eb28f9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:11 +0000

    rich difficulty somewhere ring trip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def reverse_list(lst):
+        return lst[::-1]
