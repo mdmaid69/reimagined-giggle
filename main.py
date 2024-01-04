480cbd7dac8debd3c9f1939989a94d8e6a975178
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:56 +0000

    charge card centre behind duty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
