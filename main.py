76037aba57d46e369e33305ab130b52ff6eee3ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:24:35 +0000

    lunch ignore interfere Ee wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_hex(n):
+        return hex(n)
