c3fd3be026d7a6c0e6a15619c040562adc713615
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:41:08 +0000

    mark use spray neat stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
