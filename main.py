46b2c201a16a069651d5eb2cc3a0c3fef7783fb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:46:49 +0000

    opinion can pronunciation platform Mm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
