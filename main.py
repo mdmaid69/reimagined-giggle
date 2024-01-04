e9da6bc73ebd2185835194f75aa628e954cd5d8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:43 +0000

    past pay surprise concerning sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
