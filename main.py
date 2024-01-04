c431dd739723f75770c0c0e4ca5a39e5c4314ea6
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:01:02 +0000

    occur measure person instead game

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
