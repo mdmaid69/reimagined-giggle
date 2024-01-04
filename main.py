46b1bc826aa5637c6caec98559b48739b5c9de83
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:41:24 +0000

    road like success remove fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
