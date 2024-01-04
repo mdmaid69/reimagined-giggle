8cfab75c2c7a1625dd452bb3fb1d5716ae74f797
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:16 +0000

    argue block Pp preparation experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
