3482fd943daf8f6991cc17c3b5aa6f12010aac3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:08:21 +0000

    shoulder measure year fire height

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
