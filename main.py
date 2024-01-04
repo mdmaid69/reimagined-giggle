dd6b69fa54d598918e3380b19132d3c589f825ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:01:51 +0000

    anything long characteristic roll future

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_index(array, item):
-        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def is_odd(n):
+        return n % 2 != 0
