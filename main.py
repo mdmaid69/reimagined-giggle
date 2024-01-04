ad5010d43944a46c27ad51ef887e68db21bb085a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:32 +0000

    to book turnout collection write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
