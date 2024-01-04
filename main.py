f994a9f1af659e06c2eed8a343e0302d250e8e1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:50:56 +0000

    classroom become too properly tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_even(n):
+        return n % 2 == 0
