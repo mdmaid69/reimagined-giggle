6203ceb9c8595057e6c140130ab884a678e3b4c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:20:56 +0000

    blame basis surprised wealth train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
