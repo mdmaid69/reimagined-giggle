1fc7574108b7ced78a503403a4261e9e04a29b9e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:43:28 +0000

    place break breakdown admit Ww

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_area_rectangle(l, w):
+        return l * w
