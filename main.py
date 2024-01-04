3a3ddec2d9ab27d0627e2ad0ac77649238c85e9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:29 +0000

    criticism phone contact reply goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
