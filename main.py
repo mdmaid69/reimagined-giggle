0b2e4f7a7691e7e808794a7bb91d009e81e3436d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:12 +0000

    project exam available square their

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print("Hello, world!")
