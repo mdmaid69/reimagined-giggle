fabae39b77923d6956bc5a2a99204359779c1bf8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:53 +0000

    shock colour wish gooff entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
