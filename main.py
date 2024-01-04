e441a6652926da18f0641fd6248f23fb995cc014
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:36 +0000

    broken demonstrate mixture disturb takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+i = 0
+while i < 5:
+        print(i)
+        i += 1
