40cd1c8a40715e6f9bc556791b8034e124cc544a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:30:55 +0000

    yours crash activity sleep yourself

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
