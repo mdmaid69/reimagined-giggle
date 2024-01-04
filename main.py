04006b5f9fbbb44d5edab09b36e63556e473434e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:35 +0000

    vote bath plenty however after

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_pressure(force, area):
-        return force / area
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
