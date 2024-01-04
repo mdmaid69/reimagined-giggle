759795b848f4516d2b6dd372c91b0e199adf2771
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:44:44 +0000

    drop kind broken project terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def count_elements(lst):
+        return len(lst)
