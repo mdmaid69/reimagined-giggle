13b38e1efb53dd68551352862b88865d4206e0d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:34:10 +0000

    majority main father justice never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
