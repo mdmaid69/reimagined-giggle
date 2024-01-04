e5b8361ad18f091ca0476fdb892fdaeb17508ef2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:23:33 +0000

    thankyou guide opposite journey study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def divide_numbers(x, y):
+        return x / y
+def find_min(lst):
+        return min(lst)
