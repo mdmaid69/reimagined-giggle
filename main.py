c7ce9353a2a879239125a02eb4bcc0b15df5907a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:18:46 +0000

    sale custom write benefit waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def reverse_array(array):
+        array.reverse()
