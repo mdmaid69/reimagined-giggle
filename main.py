81220eda46a0b1bf148b3ef2287db546a10bf6b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:16:54 +0000

    breath development purpose towards within

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import time
+def get_current_time():
+        return time.ctime()
