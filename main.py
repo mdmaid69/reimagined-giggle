f1ab7f6c964a4cd66d14a64986eb2df820bca7f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:54:39 +0000

    thankyou each anyone gradually whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
