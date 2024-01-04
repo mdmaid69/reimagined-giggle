1debabb30fdf476784916098d0d47684473182e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:03:30 +0000

    smell arrange attempt still voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
