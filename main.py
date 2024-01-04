dd370e0360363c8f83409f4f0e91fb80568c4e16
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:46:38 +0000

    noisy I soft manner like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
