2e2e042234e33c50cbdb6413c28f8ee1853e76ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:03:19 +0000

    almost check comment important complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
