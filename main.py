e2feaae0b343cb38d89978e702d03fdd4fe12da8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:31 +0000

    few mistake relation some expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
