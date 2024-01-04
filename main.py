12ce1b04f14cfcc079344692e27ce8f6c37d9a5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:39:58 +0000

    achievement appear lunch list realize

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
