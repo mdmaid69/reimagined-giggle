4a11e9c3820cce66b61cadcba0160bd8ab69005c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:57:27 +0000

    reply near through jump mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
