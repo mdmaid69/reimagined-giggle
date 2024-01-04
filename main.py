9194dd07bcf7061ef8e22c9d34f1373d430757cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:30 +0000

    bringup fork trousers waste arrangement

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def convert_to_binary(n):
+        return bin(n)
