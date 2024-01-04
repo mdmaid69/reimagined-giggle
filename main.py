ac06947cc2db446801af63b04a1eccfdfb09e994
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:56 +0000

    level compare crisis tonight operate

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
