19d8833376f3f2f45a1beddb4d84097c088808b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:25:09 +0000

    underneath bar become familiar play

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_logarithm(base, x):
+        return math.log(x, base)
