088690e8065e754babcc19b3ce0a6e1b5ea1a804
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:43:23 +0000

    definite wherever count comeover trick

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
