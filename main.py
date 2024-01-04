18cba3575d5eab95d4bbec19188ad73d7386e5a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:44 +0000

    over relax workon radio low

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
