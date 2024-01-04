b34489a9bb46c630a3a9a2c2c1008289fb0cbb2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:07 +0000

    taste able fierce no miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
