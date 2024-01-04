2d4fdaf05c4ad060e51a6da147bfdc6309981b5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:21 +0000

    movement judge cover gas open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
