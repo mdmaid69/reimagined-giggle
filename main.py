0941ea930db0e14683c5c8d6829877749dd8b90b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:04:53 +0000

    refer himself determined grand urgent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
