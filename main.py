dcc4592ab19dac6415c6c01cc506e80ef25e1a7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:45:58 +0000

    base familiar use should frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
