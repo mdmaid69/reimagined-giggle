49ae4cc75769ff4d8775e157cd2bb9f55e3d336a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:50:41 +0000

    gooff except achieve culture contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
