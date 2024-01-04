c0a5af2e31abe620d1e9a33ea32253742fc60bc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:08:34 +0000

    ahead bottom share shout exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
