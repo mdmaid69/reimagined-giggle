177ef62f8cdc81df4e879eb006e526cb84fe9484
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:55:05 +0000

    occasional whenever comfort noisy suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
