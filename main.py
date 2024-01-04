51b3d2e1fb50ffba4913824b777cf0dd7449cc9c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:05 +0000

    group under weak otherwise mail

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_odd(n):
+        return n % 2 != 0
+text = "Hello, world!"
+print("Uppercase:", text.upper())
