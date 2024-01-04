5e969e85c4a95e368990962d34585740868b702d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:37:25 +0000

    weak pay rest display something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
 def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
                 if n % i == 0:
