561e446571f2c5f1cc1819f4256ec34dfafeaed6
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:55:31 +0000

    over unable basically anybody empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
