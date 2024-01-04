3090895a4b4c9d9493d5dd88fa9619ca28bbacea
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:35:06 +0000

    needle whoever order dangerous strong

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
