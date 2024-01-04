6211e8fd68dfa8fd4baaef710906e072c43ace79
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:22 +0000

    knowledge impressive collection active face

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
