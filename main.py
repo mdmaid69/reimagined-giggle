6306f4067eefc8e5b1e2c3bf755c23b028411dae
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:20:04 +0000

    emphasis repair talkoutof definite physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
