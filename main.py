d115ab557dd18330e64ccfc2a8a0e9fb71a777c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:00 +0000

    hollow high careful deliberate convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+print(re.match("h.*o", "hello world"))
