451fdc6f03657ba0804a5ca91d4c3464e855f5c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:15:56 +0000

    early loud impatient outside agree

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
