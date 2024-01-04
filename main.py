f4cca274effa8f21d7a583a86639b4fdfe31a0bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:42:38 +0000

    body odd call such appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
