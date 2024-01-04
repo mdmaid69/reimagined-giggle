ab221b505578cc8e9b3236d1162cc45d2982e030
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:51:25 +0000

    stage attach force level dear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
