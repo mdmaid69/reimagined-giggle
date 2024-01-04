a561e733bb824cd876f80103ec7656c75d823334
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:56:15 +0000

    accordingto watch art spite oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
