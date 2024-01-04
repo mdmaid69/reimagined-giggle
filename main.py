d985258e9674afa01a770d25718dddbeef716a12
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:15:04 +0000

    write muscle appeal drive table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
