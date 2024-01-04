f586156a98320afcb233fed100eb1958f2a53195
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:34:25 +0000

    morning doubt alive calm smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
