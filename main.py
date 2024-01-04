398e23572d4ea7728f65ff1d1840838e29147f27
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:25:23 +0000

    dance trip doctor cut rarely

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+for i in range(10): print(i)
