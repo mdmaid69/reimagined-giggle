7a4aca6b5f1be423a6c4e104ae10dc9ce5b55da6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:08 +0000

    though knowledge edge refer opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
