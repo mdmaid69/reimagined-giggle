c28c17a5c78bb7dbebe94b5f6a6b1a71f0f5f33a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:47 +0000

    area bite opportunity unlikely cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
