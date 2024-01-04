a0f19023f69d690e436c1aa321548176a3248db0
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:55:50 +0000

    wave stir fork announce yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
