e6dbecb62d591922b9c141208ed603946f4777f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:33 +0000

    spin winter dream point payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
