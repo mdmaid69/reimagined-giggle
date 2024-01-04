0a3ec86e0827e6f34a5d59ad8dac9196964c1680
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:35:21 +0000

    level improvement whistle flood shadow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
