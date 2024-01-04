5473dab10531fcf21ecb5d7579c6dac299211c27
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:54:33 +0000

    pain stiff interrupt 565end machine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
