ec5cb3ac5d9f4276b709ba29db2c4cd5e71e7c89
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:07:01 +0000

    certain relax swing or Yy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
