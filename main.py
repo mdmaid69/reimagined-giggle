8a0bbb69f891920988cee207c68bfb49ff2ae9a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:26:52 +0000

    steady until without pleasant freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
