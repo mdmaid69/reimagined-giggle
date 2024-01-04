266e9e70ff0e00fd1f2f4a760cebd836e3d64b57
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:02 +0000

    address depend reply behaviour funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
