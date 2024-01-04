854c913dfae9819e8d8fbc90165c054b98a7b9a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:07:57 +0000

    meaning hardly weekend steady path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
