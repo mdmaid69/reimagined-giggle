0aaea678f1d689f1ad0333f080f8b5e60dd3e987
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:28:56 +0000

    glass centre question never base

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
+def calculate_density(mass, volume):
+        return mass / volume
