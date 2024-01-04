aa83b11f3c45110faa2110fec23fb3352551bf38
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:49:39 +0000

    season up account run put

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def subtract_numbers(x, y):
-        return x - y
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
