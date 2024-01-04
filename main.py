cdb6d533fc641734161d0f1aaa8089c8ef6f9535
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:04 +0000

    charge tie store range happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_priority_queue():
+        return collections.deque()
