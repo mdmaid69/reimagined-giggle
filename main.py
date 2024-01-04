36312537e0d8c75a10afbd0471fbb6d1a155e4b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:10:04 +0000

    direct target programme cap into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
