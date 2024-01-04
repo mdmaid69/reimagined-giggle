1fad1ecbb39ed181234c6f0791a2681edba6a240
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:51 +0000

    in muscle comfortable definite concerned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
