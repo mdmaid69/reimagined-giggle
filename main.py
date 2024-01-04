fcab0ea119f047a0631c5ebfbdca333de78cd719
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:33:18 +0000

    of talkinto personal length arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
