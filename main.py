47db5f7f94aa1290c264d23fdb971ee4ed230502
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:08 +0000

    schedule dangerous patient plan gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
