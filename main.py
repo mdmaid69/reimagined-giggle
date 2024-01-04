4fb5dfe51a19e20552b4cbe1433417e9efa53265
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:08:22 +0000

    tape there maximum system hand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def is_odd(n):
+        return n % 2 != 0
