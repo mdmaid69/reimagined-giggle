dc71245a8cd004ae140a431d69e1aaa811284456
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:06:30 +0000

    modern fail bus float outside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
