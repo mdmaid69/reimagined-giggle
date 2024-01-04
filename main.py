8da67194e3440d411e30e74a5242ac8c9653a5e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:14:33 +0000

    minority stamp question authority between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
