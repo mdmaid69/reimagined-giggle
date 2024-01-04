36eeb1824e720da861e4fb58c3aff98c4af1b677
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:00 +0000

    usually spray figure face repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
