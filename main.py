1a3b0ce1cc2ee6baebc2e15fcf2b9b8f7102e6e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:56 +0000

    write below relation successful figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
