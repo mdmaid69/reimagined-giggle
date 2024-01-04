ba2cefe17f30dc9f0562c2ca70bd5a41878f79ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:12:45 +0000

    string early gas mood approval

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
