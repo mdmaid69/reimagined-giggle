fa5e4386cd8e0e69976a04c8fdd6cb02514a106d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:03 +0000

    better conclusion standard report male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_int(array):
+        return int(array[0])
