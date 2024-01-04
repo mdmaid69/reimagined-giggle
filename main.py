8f23ca274893c224d2ca2ce93b3b723dadbf6545
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:50:05 +0000

    rank experiment this generous pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
