feb22f9b7f58d3986e7a1439557fa6a871be2009
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:42:13 +0000

    refuse cap draw write makeinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
