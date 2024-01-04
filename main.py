d4e4ab47ee6b196a8788fc4a74c435b9c3357944
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:58:06 +0000

    cold nose brush plastic practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
