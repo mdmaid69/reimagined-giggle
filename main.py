570d17165f7484b2acf88da5db705ada1cff0691
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:23:45 +0000

    mention intend invite young ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def subtract_numbers(x, y):
-        return x - y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
