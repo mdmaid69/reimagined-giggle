5aaa550f8c55664028d08d7e32491d50cf2c65c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:29:17 +0000

    vision somebody handle shape trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
