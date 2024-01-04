401384c92f17cb0ffbb45153fd5985ec4808cfa6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:00:15 +0000

    godown he path ourselves badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
