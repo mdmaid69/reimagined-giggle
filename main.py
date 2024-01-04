565352b3ef0f4a918c0ab15a0de61ab4f7ed57ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:18:15 +0000

    school ought only oil extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
