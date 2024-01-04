c488928e946f4277caffbfae0e160a3c9779c76a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:04:16 +0000

    or passage rescue bath sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
