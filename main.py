45189b4758b8d73c791495b1a7c8102af772b230
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:07:12 +0000

    top too band crowded Ee

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
