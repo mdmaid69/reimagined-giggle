8a474ca5fdc4d39b2edde82c769fb7740a243ef3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:00:54 +0000

    right ring colour into instant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_hex(n):
-        return hex(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def remove_duplicates(lst):
+        return list(set(lst))
