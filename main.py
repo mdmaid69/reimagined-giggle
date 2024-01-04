ca8a4cb2e8b42a7be3e8a4c3652d8abc157f84c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:41:18 +0000

    listen throw bringup sharp choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_floor(x):
+        return math.floor(x)
