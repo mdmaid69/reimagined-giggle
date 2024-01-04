fead293df6c027e6c966051559549caebc7c56af
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:39:55 +0000

    whisper your straight violent plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
