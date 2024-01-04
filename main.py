14ba8f10924cc1a1c7fd7b97f1832841f4ce8a90
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:47:57 +0000

    sorry company respect thinkthrough instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
