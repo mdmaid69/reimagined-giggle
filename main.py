3afabfb14e8dcfd0e263e6fa0ecc12775137e95d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:55:15 +0000

    story wing distance preparation sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_max(numbers):
+        return max(numbers)
