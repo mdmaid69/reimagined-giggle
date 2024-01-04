05b1910ff8537e4b622626e7b6485c507c1a7a7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:34 +0000

    touch choose pick city this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def square_number(x):
+        return x**2
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
