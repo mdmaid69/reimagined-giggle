3e3512b869b7992c1d5ae3834a4ad5466d1e1c44
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:07:42 +0000

    damage further track base sense

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import sys
-print(sys.version)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
