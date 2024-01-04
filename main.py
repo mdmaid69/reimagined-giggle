f72e464979f174cd7bf8e110ed999c8a40f0ac49
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:08:48 +0000

    floor nearly talkinto divide sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
