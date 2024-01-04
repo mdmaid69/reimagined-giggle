6071ffa8858676053a573afefbfa8d3a11583502
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:30:03 +0000

    soon thanks clear uncomfortable fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_density(mass, volume):
+        return mass / volume
