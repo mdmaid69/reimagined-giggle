c66e7609f5850ca08b5bf60e27fe5fbacf7ebe17
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:35:25 +0000

    pure willing greeting how neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
