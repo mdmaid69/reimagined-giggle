88cb88857684e66a6e54311200914e005fffe62e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:55:19 +0000

    safety moral rise genuine convinced

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
