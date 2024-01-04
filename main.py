67d77e8735779a9431b43d419b7bb3b541dd1e42
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:33:22 +0000

    wrong repeat suggestion turnoff speech

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
