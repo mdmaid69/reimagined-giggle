f501e6ae5b9e53e4ba7f4985ab797582afcb41b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:16:24 +0000

    stamp getout duty calmly party

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import tensorflow as tf
-print(tf.__version__)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
