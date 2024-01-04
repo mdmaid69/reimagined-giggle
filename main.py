b2b3469b96d6102fe9c2d63c47fbc64a28ee3ff2
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:13:12 +0000

    never unless knock not takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
