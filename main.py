32e521fa456e84913a07fef3473952aa3e15674c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:52:24 +0000

    mother theirs turn Zz several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
