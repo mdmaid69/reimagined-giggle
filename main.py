334f376771303918b4b59164d7de3127903bb394
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:33:19 +0000

    off gather share safe confused

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
