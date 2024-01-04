58292fcc6487743d762a843f16399a9e82051e07
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:55:29 +0000

    bring improvement curious valuable Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+print(datetime.datetime.now())
