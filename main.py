42c1ba356e66a63826bfe314f6ce77975344de91
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:35:12 +0000

    female worse shape Cc promise

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
