b5fa57f480720b91cf6a5b84ad4bdd249bb21f8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:37:18 +0000

    mine flat tooth giveaway visit

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
