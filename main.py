db508c712154960cd920c8db59f4d6ed5b841bd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:54:15 +0000

    old shake flight hard chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import collections
+def create_priority_queue():
+        return collections.deque()
