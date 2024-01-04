97b25a28f13f7ffbe4730969ee6bb449d6396bf3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:01:21 +0000

    children minute tense interfere plan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
+def is_even(n):
+        return n % 2 == 0
