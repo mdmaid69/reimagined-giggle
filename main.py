bbed00ae044c52b4cdfbe903096366fc118ae027
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:38 +0000

    comment protect miss birthday indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
