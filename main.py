d8408f38b1e2f77b2f6fcdbc27f206df7d632190
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:01 +0000

    hair pride cruelty win off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def reverse_string(s):
-        return s[::-1]
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
