cb8f0fb8844644f9c7c874c3a86cae478054dc5a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:47:45 +0000

    error steady character prepare satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
