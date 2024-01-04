a9d325a4b2a0d690b47caa59ba165e64716f9dce
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:55:26 +0000

    ask family thumb split skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
