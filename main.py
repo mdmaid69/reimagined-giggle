f6b6842ab967599188b652f29185e73884a69956
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:42 +0000

    each cope escape next shut

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
