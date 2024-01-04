36ba6660011ccdcc6c1a2abc73e87403f805f546
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:21:25 +0000

    afraid understand plastic best wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def print_python_version():
+        print(sys.version)
