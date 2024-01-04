02aace801d433da4f63075ef16dbd0f6f7729c32
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:16:13 +0000

    suffer borrow over Nn either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
