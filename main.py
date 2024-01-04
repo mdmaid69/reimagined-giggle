4ad6f4790103efe8875092fe9b8d7c639e1253a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:16:35 +0000

    near bury better afford luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
