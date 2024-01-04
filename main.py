7971d3c3b1e98e5e15791226b28514570f53d337
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:55:43 +0000

    train needle grateful up hollow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def get_array_as_memoryview(array):
+        return memoryview(array)
