79f4051d9b8a66803060ff827de8160862fded92
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:29:08 +0000

    yet medium hold minimum doubt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def add_numbers(a, b):
+        return a + b
