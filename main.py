fe9bab2b7e3181272da111df11935263ab21a850
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:40 +0000

    giveout fine ocean pity tape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
