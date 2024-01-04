904a288ec875e7ddea867ffbadac73e192819120
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:35:18 +0000

    meet mind survive tap accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_queue():
-        return collections.deque()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
