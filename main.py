391f594b2987e7697eab62f83c2b75dc93de9af6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:36 +0000

    put confused trick travel ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
