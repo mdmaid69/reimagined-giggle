5efe9e946cb4c39bbdcbba4d1939510607b0ac60
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:24:25 +0000

    command praise hit solve none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
