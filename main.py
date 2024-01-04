dc1578e800d63de6a4184fda86abd1d0c1126f7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:31:32 +0000

    sting wave prevent tap imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
