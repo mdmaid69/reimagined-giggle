3c937a52dcb781fe9059c7c914f8761a715e5d0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:34 +0000

    complain mark sometimes direct underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
