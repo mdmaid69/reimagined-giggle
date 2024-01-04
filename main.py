ba5b16dcb1212a213c84c22bc2376ea1c81de5ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:09:30 +0000

    close firm turnover accident bit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_octal(n):
-        return oct(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+text = "Hello, world!"
+print("Reversed:", text[::-1])
