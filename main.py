b061672f85166c7d2fab72876defba4be968dfb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:09:23 +0000

    name begin frightened repair violence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
