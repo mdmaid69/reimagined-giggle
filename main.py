0be8873d9ed1b0e9c5ae4f69316829ef80febb99
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:12:09 +0000

    straight wide workon plant welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
