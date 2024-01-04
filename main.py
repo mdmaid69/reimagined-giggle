8bb7160edcef34c6c1df0e20e95633691961f1ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:52 +0000

    bright harm trousers cheap zero

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
