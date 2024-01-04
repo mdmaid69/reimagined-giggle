018a9f35ba38969e883febccef1316f38b51785a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:55 +0000

    wish walk wet partner offer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
