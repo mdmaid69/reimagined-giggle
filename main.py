3956f0c52569d8972d271c7d5836b061ba10b3c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:53:03 +0000

    table Ff bin temper show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
