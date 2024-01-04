02dd4697530541bf94d84725b8fcf0129ab1e81f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:53:26 +0000

    repair punish town meanwhile off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import datetime
+print(datetime.datetime.now())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
