3bc17030b6f89698a0c486edbce08fcda028093c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:39:28 +0000

    shake practise hard time offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import sys
+  def get_python_version():
+        return sys.version
