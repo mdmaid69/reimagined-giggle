eaac99ede6c18e569b0292a53ec02f41ac84c4fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:57:55 +0000

    wrong copy party suit union

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
