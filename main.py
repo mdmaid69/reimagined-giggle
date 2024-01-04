e1331ffeaa932a189bc517de1b312b0b282066f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:29:59 +0000

    point sleep lookaround who dance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
