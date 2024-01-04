5a22e6db3970853e9fe1fed85582f9bd0477e823
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:01:56 +0000

    theirs bored year point performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
