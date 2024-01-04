011a41c2b839cc38c5d55fc68046dc213612f089
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:57:47 +0000

    achieve mention advanced chief free

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
