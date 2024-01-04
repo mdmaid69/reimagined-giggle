cbb794c323ee659a735fb6bad647362851ac853e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:32:59 +0000

    shelter most off excuse direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def remove_duplicates(lst):
+        return list(set(lst))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
