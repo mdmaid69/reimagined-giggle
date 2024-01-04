7e49e05c5d810ebfed849b11d5e3ec504a55e55e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:51:15 +0000

    inside slightly since occasional begin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
