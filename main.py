a74102041e993d88b54fa5d789700eb9ce006ee1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:26 +0000

    advanced mad worth wrong favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
