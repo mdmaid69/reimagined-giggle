1c902115253ee334ed02e40c051b5d538fb5208b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:02:22 +0000

    sound surprise church avoid allow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
