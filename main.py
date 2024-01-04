9c3001a34653c168f4246dc6f26f2542e8f4ef36
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:25:45 +0000

    notice fork capital lot round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
