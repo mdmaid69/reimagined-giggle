6ace9852baad45a597a69ade4e43016ae5682538
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:46:23 +0000

    offence hit calmly drawing below

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
