7e72e0437fb050959b81a76ac74c387de57fcf6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:16:59 +0000

    talkinto except by noise medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def reverse_array(array):
+        array.reverse()
