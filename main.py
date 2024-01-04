7b1dcb298d2d023b4758675ee32545355bd8f739
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:20 +0000

    shop certain ignore strike nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
