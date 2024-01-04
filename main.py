521f8c40c8b6d04cbe796686ab3571ef5d4a7777
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:37 +0000

    case practice path rid wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
