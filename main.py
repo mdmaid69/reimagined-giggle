35a3faba1f6e038c53bc6e34571344652897714b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:45 +0000

    refuse come dangerous dick shut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
