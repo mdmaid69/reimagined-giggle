f639dd13c0908d353136a07984b3dcfff423add4
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:26:28 +0000

    point quality getoff kill radio

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
