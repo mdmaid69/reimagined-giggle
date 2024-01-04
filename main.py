29fed5ff6d4fe4cc5bfbb38a4183d2222667cbf5
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:20:45 +0000

    name port game smooth skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_base_name(path):
+        return os.path.basename(path)
+def add_numbers(x, y):
+        return x + y
