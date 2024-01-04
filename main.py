2e8bdd89e560228ba59612104ab7c3e47fde3145
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:52 +0000

    swing steer get check name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
