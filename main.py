deec75a0e79c88a1c08d027b49594c81b982028b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:55:59 +0000

    measure difference within swing claim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
