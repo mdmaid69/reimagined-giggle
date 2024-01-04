e7ce268cb428897182abfbb7b046786aae903707
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:37 +0000

    innocent deep hurt gift year

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
