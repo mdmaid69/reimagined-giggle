9b98d12789d607de45f56298d8f232df2fa69ee7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:02:13 +0000

    work impression early uniform fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
