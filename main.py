2fedb65e3608b2dc6ea0dae1fd1e72334ffd0cfa
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:10:30 +0000

    exchange usually regard right interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_array_typecode(array):
-        return array.typecode
+def reverse_array(array):
+        array.reverse()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
