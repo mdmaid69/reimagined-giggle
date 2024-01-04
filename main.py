971b71e2245a7960819a7552942068de52e6fefd
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:55:25 +0000

    inquire city fly shadow neck

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
