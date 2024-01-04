50a05f5aa71655dd58a845b58f02aafddf7eb42b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:44:15 +0000

    unkind proof care lead term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
