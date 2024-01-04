3837adde1224f6fe0ac5644332cbf71dc5a96de2
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:10:38 +0000

    test take drop protect plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
