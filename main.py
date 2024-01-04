9fcd89b7afd972bfba438abb9e51b61328294cfd
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:47:10 +0000

    afford transparent process value slowly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
