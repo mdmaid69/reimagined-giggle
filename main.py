307d97e8b8eee40e691ae6e32cd36632aabdeed8
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:57:53 +0000

    plate steam underneath at dry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
