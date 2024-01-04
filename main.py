6cdefc2dd238214e1f931d455367348e7ac48bba
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:58:19 +0000

    stare roof break fun sometimes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
