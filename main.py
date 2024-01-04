c8fe6ada9649ac16b7aa7a8eb9558c8e8af926c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:18 +0000

    average he band wish cheek

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def subtract_numbers(x, y):
+        return x - y
