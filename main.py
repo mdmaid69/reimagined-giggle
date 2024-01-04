c754d335599122af1dd0efb5da17a279798e4a62
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:43:43 +0000

    goup comfort mine term forever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def count_elements(lst):
+        return len(lst)
