f099a12dea8f2a17313ee2e4cb863d3a6242f30a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:47:18 +0000

    nonsense 572enter towards manner smell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
