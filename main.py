dbefab507d444103bf6c6e96f99e3f2352cb0624
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:24:20 +0000

    collection scale choose school offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import collections
+def create_user_dict():
+        return collections.UserDict()
