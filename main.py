3b3cedb94d52a7189cde8395576045b45fbdb5e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:26 +0000

    walk union day beginning bill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
