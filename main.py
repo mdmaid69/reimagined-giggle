e6ec19aedd5f63eb9306c3c2ab5ca5fb2935961e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:06:28 +0000

    perfect when goback(to) concern turndown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
