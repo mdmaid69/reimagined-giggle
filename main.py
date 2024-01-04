3667b2fc328a83067e07548a4c505563038aa0c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:21:40 +0000

    purpose book bag walk pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def divide_numbers(x, y):
+        return x / y
