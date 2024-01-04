55ddc069cc3043ce535eedf0c64579014600e0d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:17:31 +0000

    bin firm weak dry upstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
