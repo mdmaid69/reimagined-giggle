b0ca2afb601af8bff67397a2a44f0b07bf02f5e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:23 +0000

    change instant neat range properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
