03906c4d51acf1d9d897fb858e0f942594f061b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:41:39 +0000

    add holdup odd easy provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
