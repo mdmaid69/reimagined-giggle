50e1424e0a9911819aee8ad484c767ebd9bd1cc8
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:12:32 +0000

    shut Zz sweet left recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
