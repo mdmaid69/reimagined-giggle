4beb031349e6706e8fca5a5174e649ded209897c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:25 +0000

    proper what save valuable central

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
