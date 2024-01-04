8a03215ea98a95a5dde8c486ecf2960cc0c01b37
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:25:29 +0000

    shock put exhibition thinkthrough less

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def sort_list(lst):
+        return sorted(lst)
