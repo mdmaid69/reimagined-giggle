0c1d65a8d90cbeefbcb6afe54944860573a221ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:56:54 +0000

    patience invitation pleased dream crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
