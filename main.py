93b111a512d0d399e2ce549598d8345ce876a9fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:08 +0000

    scrape arm him interview shopping

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+for i in range(5):
+        print(i)
