9352b5dd614582deb0c751f3ba44f9ea10e5e086
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:46:43 +0000

    art watch term interview issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def greet(name):
-        print(f"Hello, {name}!")
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
