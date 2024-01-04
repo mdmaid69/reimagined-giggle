63ba282830df04e9439af7f825bfcdac3c9fb056
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:56 +0000

    nearly insist relief line performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_item(array, i):
+        return array[i]
