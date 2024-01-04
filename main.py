8449046791a135150aca6d887a56c9b5c4fe2624
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:17:04 +0000

    scared complain let rest design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
