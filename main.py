96b81aa09486cab6b23ccc19d8a1d8e56a801e67
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:23 +0000

    opposite seat organize rush concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
