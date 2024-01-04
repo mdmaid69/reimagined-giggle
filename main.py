cf757ccae1e0a5edbf4b6c10585ae9cb18fe930b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:38 +0000

    yours odd personal estimate translate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
