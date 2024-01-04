8b458a515a911f62818c87470dc4d054c9fa7738
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:30 +0000

    unit duty desperate question reason

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
