6d9eadfa06f3bfbc0b3e78af90b388078a9c3719
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:24 +0000

    burst getout control go dull

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def clear_array(array):
+        array *= 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
