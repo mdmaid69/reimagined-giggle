812a38198829e756bb90d70f36fafcaae5942e71
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:56:30 +0000

    bottom create keepout excuse near

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
