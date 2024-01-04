ae92c4d34226f10f4e1efc185498bce577b0374d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:42:53 +0000

    die lean combination dead minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
