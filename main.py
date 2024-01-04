41a1b854d77c3487ffa35b2e3f7da55b5086f87f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:20 +0000

    film whistle limit need hit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
