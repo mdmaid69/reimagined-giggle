2d71f879df785406c9ee491ca3e278c9e7d2cede
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:54:57 +0000

    unfortunate Bb example heat comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
