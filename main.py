fbf7395da77c6a075de37f3b7742e3848b678283
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:44:13 +0000

    centre remove worth do tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_item(array, i):
+        return array[i]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
