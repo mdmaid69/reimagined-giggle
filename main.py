e1de9fea1cf1fac35f5e1a5ef3084152c11f7eea
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:50 +0000

    flat emphasize high love eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.time()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
