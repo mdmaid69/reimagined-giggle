28ee5f860b1f2e132073da4ae702082c022ded59
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:15:47 +0000

    close question name their deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
