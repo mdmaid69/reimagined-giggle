549bb05942e02b388c2afea532d4e6403a531626
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:01 +0000

    platform breath than circle beneath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
