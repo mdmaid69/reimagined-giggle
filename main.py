7423bd27aeeaa1b1e171e181f02c772e32620afb
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:32:06 +0000

    sick record in bottom pump

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
