4d581eb5d067178eee942d48b95de5bfd3d2b69e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:38:32 +0000

    instrument sink onto Gg confidence

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
