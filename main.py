faca28470828791d2e14209840e24726d756cab3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:23:21 +0000

    noone offer concentration safety people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def split_path(path):
+        return os.path.split(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
