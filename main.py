72a2a057695a9679b76d2dd35ddb5e8cce361520
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:52:58 +0000

    worth swing hit spread listen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
