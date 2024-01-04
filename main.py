8045ff39f6fb619c3002a74ae9ddb55f8c8608fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:10:08 +0000

    conversation exciting modern responsibility experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
