efdffc9974365330d13d4cdd0207cf3e6c840b50
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:29:14 +0000

    curious get university argument couple

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
