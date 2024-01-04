282447518c18d9898e73cc63c580620ca3d63254
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:05:38 +0000

    hit complete regularly conclusion short

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
