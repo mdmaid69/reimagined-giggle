3f0d43a9f121e1e614720b6f1c99289a82a25dda
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:47:17 +0000

    bringdown post bread come flood

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
