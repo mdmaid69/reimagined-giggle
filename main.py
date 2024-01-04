e6ef26797f7470c0227b44964dd7f0fdf521aa7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:47 +0000

    very lower miss frame break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
