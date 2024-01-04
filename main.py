ec3613b5afbdbb46bac698b47f8190e65886eaa5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:43:38 +0000

    flight every drawing job stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
