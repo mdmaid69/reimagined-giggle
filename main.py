411437e8f9ec717873c3b390d6af7b651216bfcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:22:45 +0000

    platform yours way lesson whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
