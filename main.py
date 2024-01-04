206d954581df518d12c47649d839c9420ac9aafa
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:49:01 +0000

    fall exchange bill move tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
