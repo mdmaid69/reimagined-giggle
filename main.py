672e489c4a2e3d9d9ae939c6dce749f1e4f857a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:25:58 +0000

    private whoever fair hungry dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
