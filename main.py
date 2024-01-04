f3a9b546308b978f5e959527cede62b70384c033
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:27 +0000

    conclusion mine manner fact land

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def find_max(lst):
-        return max(lst)
+print(sum(range(10)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
