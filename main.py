7ecb7a6b326b1bb1e08f29975a42d6fe84ead272
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:56:37 +0000

    many laugh make satisfied keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
