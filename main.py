7d89899fa3f95aef7780a1aedde1a9b9dc6f9e44
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:58:51 +0000

    straight among natural available anyone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
