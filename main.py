9193d4be51a2aa57486a1c0f2d3a26a128cf88f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:24:54 +0000

    gradually clearly trick anyone inquire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
