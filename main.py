e8863c3758271a69be4b8a0140240fce28c96665
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:06 +0000

    hollow round interruption spot shop

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+print("Hello, world!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
