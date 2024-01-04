4c2541aa46b6e4a09a10ae7c36c1b181aba41c8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:44:06 +0000

    the remove angry solve complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def find_max(lst):
+        return max(lst)
