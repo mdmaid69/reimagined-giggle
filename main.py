49ebf46b6953c0c7cd4488f2c31b13b22adb81b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:41:33 +0000

    obtain cover oil study rough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
