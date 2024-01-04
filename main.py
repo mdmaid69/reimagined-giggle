37e4441e0bac3df49a70148300aee35e47b68155
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:40:03 +0000

    ground undo door beautiful sign

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-for i in range(5):
-        print(i)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_error_function(x):
+        return math.erf(x)
