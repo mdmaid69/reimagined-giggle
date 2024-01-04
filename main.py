c7586e57aae68860875266a0b049316ff830b121
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:58:06 +0000

    neither at round compete near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def clear_array(array):
+        array *= 0
