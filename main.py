cdd1b275dc92ba4a0715b039dabfc2a2b7f7cc4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:41 +0000

    birth remember ring science his

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
