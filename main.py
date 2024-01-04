d463102d2fc87a1edb0cc6e0c63c5619f4e330e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:57:53 +0000

    cost history in police quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
