eb2ff6394c3f269214987959e61c9190720e2f0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:37:53 +0000

    morning mouth weak cover attempt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
