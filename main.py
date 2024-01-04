d94233221668927e716a108f80827096631e806a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:30 +0000

    travel negative bury lesson grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_power(work, time):
+        return work / time
