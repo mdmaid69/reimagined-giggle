5707b2102daeeb7a9340176d25188f2a8b47ebdb
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:26:51 +0000

    worst listen possession warm other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
