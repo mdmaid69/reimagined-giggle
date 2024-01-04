9f56099e9e97899c924bc19ebb7f0c441ef8c860
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:53 +0000

    near difficulty us gift bang

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
