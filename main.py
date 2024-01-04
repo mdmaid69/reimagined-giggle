2656666592be703a7bab6609ee203a6c7689c2d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:31:11 +0000

    common surprise about out try

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
