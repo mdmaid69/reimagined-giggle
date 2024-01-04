476df988243571e4e632c4f4c004241c94db18c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:13:12 +0000

    into account necessary future occasion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   def divide_numbers(x, y):
         return x / y if y != 0 else "Cannot divide by zero"
+  def convert_to_octal(n):
+        return oct(n)
