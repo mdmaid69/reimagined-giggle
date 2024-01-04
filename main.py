7341bf003a53b86d4cafa43c559f46ad7a1f4912
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:44:34 +0000

    firm freeze tooth cool build

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
