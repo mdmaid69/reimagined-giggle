2e81cba658180e2d42783052cabd099ae2b3069f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:54 +0000

    damage put spoil aim vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sys
+def print_python_version():
+        return sys.version
