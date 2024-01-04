8f64a76fe8c1aeeef38b95dc3e50214525dd972e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:30:56 +0000

    mind 572enter learn respect hide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
