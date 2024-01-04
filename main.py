5b955568a65037b7b3bf99a3a485d5d2f5fd946c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:23:28 +0000

    practical still mess music air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def is_even(n):
+        return n % 2 == 0
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
