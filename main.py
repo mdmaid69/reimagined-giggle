8d3aef0655259fcf0f8dde8045d09ab067986e1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:40 +0000

    getaway study disappear attempt disturb

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def divide_numbers(x, y):
-        return x / y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def reverse_string(s):
+        return s[::-1]
