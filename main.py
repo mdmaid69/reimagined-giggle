5ee6d1f7007e5568c81850175af2115987f54724
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:45 +0000

    cause by entrance hard success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
