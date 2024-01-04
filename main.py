7244056c22ce34c96269c3986cee56de497f7a50
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:10:31 +0000

    fix pupil turnaway have tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def reverse_list(lst):
+        return lst[::-1]
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
