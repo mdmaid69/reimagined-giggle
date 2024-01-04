d466d2ff69fe8dff777e664979167b104bcd8d4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:05 +0000

    average violence ilovedicks suppose tongue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
