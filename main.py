74037a2e81307dea86faf12705b16e3e794be78c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:52:35 +0000

    pupil pollution step shall trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
