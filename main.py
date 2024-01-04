29ab3e0f249af4079fda2f36f03adf6cd0749000
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:20 +0000

    through tape shut terrible run

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
