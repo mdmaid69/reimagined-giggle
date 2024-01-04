00574f7635cb131e50fcb8d478a36dcecabc1791
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:42 +0000

    dead buy appeal computer load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def find_max(numbers):
+        return max(numbers)
+import os
+print(os.getcwd())
