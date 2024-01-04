2d464af8de60dc7ced507771f87bb08a5a5f02c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:35:13 +0000

    moon Mm male towards after

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
 import random
-print(random.randint(0, 100))
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
