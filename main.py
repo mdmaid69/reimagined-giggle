d644ebfaa98a13a92aaf255e31d1634262b81d51
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:56:09 +0000

    so sentence as depend kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
