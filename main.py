52ed71f931a57b8273d6d350f5b36c89dad0148e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:59:38 +0000

    chance still tape shame popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
