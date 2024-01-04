c8c2b47e8d1caf1ee44a7f9be7d28e83781f3777
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:04 +0000

    comfortable go fashion brief difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
