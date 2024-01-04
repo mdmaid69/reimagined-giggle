4f370c4e44d9c74e7b1c50761d3712f91ea02dd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:34 +0000

    crazy pretend than pocket expensive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
