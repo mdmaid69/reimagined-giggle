3ca405da40031bab92b22a8cb4d185bd537213ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:51:00 +0000

    base settle pretty spin side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
