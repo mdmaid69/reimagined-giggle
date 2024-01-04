9808a8d1a845fc3799624952fbe113cfc1ef038b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:36:02 +0000

    responsibility another gap picture label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
