d9c53b4d58ffb3a6d9aa015773c2571030a8ea3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:17:03 +0000

    if bread variety present reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
