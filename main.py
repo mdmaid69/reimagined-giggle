33eb1107b27937e6de7e51e2a06d985c28028445
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:15:08 +0000

    well excuse colour where remain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_palindrome(s):
+        return s == s[::-1]
