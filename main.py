e1d4c5f623e74bf4af697bfbe2956a1f02f7c3e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:16:47 +0000

    it lay Rr pocket call

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
