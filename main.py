b39a2f11bc5717fd0f32bc8f2407984732ab25b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:20 +0000

    normal off left unable but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_octal(n):
+        return oct(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
