3d0622baceb18ede5fba95a6095ef4dbbf733c7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:07:14 +0000

    possibly getaway concentrate hold moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
