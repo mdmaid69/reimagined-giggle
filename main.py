c4e917a553f5d9a7287b28ea62da38fa7f2cbb68
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:53:21 +0000

    wipe wear at record closed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
