dd461c13226bbd666c75b3790d466b9d2ea1b4d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:46:11 +0000

    petrol far send joke blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
