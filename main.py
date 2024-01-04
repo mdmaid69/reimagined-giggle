f30b6823209e27265bc4608aac3984209c4db1fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:02:37 +0000

    become class wish breakinto delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
