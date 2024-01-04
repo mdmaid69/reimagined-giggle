875077a3f612b6e9b0380d5b9e587335752c3340
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:49:03 +0000

    autumn shallow success bringforward match

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
