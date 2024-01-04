4367ebace885b5b92f02dcd9d86ae6db37442f32
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:04:15 +0000

    display shout pay argue snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
