966358eaabf3a2854034892866dd0d9b777aaa03
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:46:20 +0000

    tape approve own myself experiment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
