e4f5b5d09981a55574191ed142527c54e6505301
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:51 +0000

    willing clever smart obtain pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
