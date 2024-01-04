533b63bec8b35c147ed7492be86d98bd881d77ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:36:51 +0000

    attach bread attention ilovedicks on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
