52b5eb7681bb358f9effdc909e965697907846a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:02 +0000

    stone cheek view imagine father

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
