50b43d28cd708a6283c1b266ccc7d6ce245f4885
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:21:01 +0000

    mention wrap wheel foreign foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+print(datetime.datetime.now())
