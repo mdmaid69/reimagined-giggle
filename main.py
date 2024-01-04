ac347ea1962a0dd881a137203acf1c92f4264287
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:36 +0000

    spread flat make explain failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def count_characters(sentence):
+        return len(sentence)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
