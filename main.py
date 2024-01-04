00a7f84ae8118d1ec82854c988ef5d3cd81a10b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:38:01 +0000

    study telephone relation minimum replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
