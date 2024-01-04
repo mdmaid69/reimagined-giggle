2578cb33aa5187b6686ff5d397e281c7908ea7e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:51:41 +0000

    surprise whoever road bring however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
