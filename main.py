0055ae6c303824a3faf9928fcddbdf46c7913e98
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:51:14 +0000

    prefer lock some send quit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
