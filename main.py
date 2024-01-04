1783c3adf6436d644d3ab6cc31c85f595398085b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:39:18 +0000

    image job consult unhappy introduction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
