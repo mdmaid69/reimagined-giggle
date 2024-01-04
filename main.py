409d27c15633cd9ad95cb36e3f063b5fee2cdfb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:24:46 +0000

    brain godown fail disappointed smile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def convert_to_binary(n):
+        return bin(n)
