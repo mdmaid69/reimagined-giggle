f0e1adc7381ed06cd4bea016fee223d3361cde8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:23:33 +0000

    rid lesson tongue price finish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
