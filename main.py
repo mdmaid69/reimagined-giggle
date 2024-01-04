4c6e8f37ca2d72db5b0f12d7a31e7304b6a5f16c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:23:31 +0000

    ourselves name chase deal image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
