666b39f1c5a68880b167a64e79ed8ba14af5d2e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:34:41 +0000

    supply realize sure dig serve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
