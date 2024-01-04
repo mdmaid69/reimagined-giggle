bc18979539d453f0c7047eeec8a9a1a5a43b8df0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:27 +0000

    wheel press subject we rob

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import numpy as np
-print(np.array([1, 2, 3]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
