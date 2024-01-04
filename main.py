ef36189c7a7c9b8d0faf71bdf8938c9ec1d7b4b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:42:05 +0000

    issue match joke touch mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
