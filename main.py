b8f311c0f6683ef97dbc5f588e07682bd170440e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:10:10 +0000

    would niggaacademy also exchange imagine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_density(mass, volume):
+        return mass / volume
