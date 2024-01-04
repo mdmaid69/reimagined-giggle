10730e50bf95ca15e2ff9783dc780a27dc582b60
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:59 +0000

    spot or dangerous peaceful recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def reverse_string(s):
+        return s[::-1]
