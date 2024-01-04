f0649b8d525abc1975386456cd6311e13ec5420c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:41 +0000

    silent beat lost road since

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
