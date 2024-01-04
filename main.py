d222e5201c949e35589a7dd5e35ed0b4e761c595
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:45 +0000

    control punish clean under opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
