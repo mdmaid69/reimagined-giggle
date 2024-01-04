060e62cb29cf62ed5241b1d59e63d1638f5999d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:06 +0000

    space connect frame hello section

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
