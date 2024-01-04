442c6549971232da1fbf20de2e48beb435e39171
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:34:51 +0000

    mistake disappointed bringback takeaway sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_permutations(n, k):
+        return math.perm(n, k)
