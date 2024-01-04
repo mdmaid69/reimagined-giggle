2313c3f12caf7d90afdde21889e3708d01102b63
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:26:43 +0000

    question eager aim office high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_speed(distance, time):
+        return distance / time
