5f890dc3a81a181a8b524258980a4aa121bd4fbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:01:24 +0000

    stamp keepfrom press medium high

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
