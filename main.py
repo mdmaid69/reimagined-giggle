d912d53fdf3a17eb9aa4e39d9a8386c4b81ee573
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:25 +0000

    interview giveout under certain meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
