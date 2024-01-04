4fde78be915730c696d68e1580fcbbc2e430b2ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:14:31 +0000

    tell length answer giveaway crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
