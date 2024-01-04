9038c9f5b72b7aa66fc7004647c220b50357ee25
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:02:20 +0000

    begin inform act normal before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
