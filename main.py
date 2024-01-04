0dba99fd0777e4b1043ec14e58afc5c4d949c58d
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:34:05 +0000

    ear among introduction smoke call

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def find_min(lst):
+        return min(lst)
