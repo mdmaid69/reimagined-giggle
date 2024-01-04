097b7552b9c7d84cc5d8718e1589348cd76322e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:40:29 +0000

    thin excellent confidence company shop

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
