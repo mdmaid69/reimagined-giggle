5c638b116910cd2b94cb94fbc8ae55fab91d2a24
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:35 +0000

    underneath single drawing enthusiastic taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
