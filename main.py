c5145164f489d057e4af66df9174c52e15b8d339
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:40:55 +0000

    quantity several he private direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
