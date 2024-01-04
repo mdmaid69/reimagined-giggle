a7666c7131dcbba55e29aa8f819437e4817e2136
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:54:39 +0000

    stupid will turnout occasional welcome

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
