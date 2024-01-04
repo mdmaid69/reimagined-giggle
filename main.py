7e4173db2db901b68377bddb840d62dd26cf4388
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:25:43 +0000

    taste effort board region reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
