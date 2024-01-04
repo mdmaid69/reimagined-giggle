26f3f093b236216ec61b92d9a8b0faacaea1acc1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:30 +0000

    something brave deliberately Itself second

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
