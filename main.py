ac739fc0436ed3ec7bacb1b523e0bfeaa25a8c88
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:14 +0000

    boil touch shut seem on

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def convert_array_to_string(array):
+        return array.tostring()
