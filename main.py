c40d549d8601f980e68a39fc5b32c44d976d1dc9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:07:45 +0000

    turn behind cool mistake vision

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
