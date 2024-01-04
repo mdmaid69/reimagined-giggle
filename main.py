647e820c3a7ee824601126b57c0ebe61cb95de64
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:53 +0000

    sad space room sense sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
