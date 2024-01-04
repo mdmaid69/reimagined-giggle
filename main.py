61ef10b25a4701f279002521e8c92a4abc3b08cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:31 +0000

    test successful your point computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import os
-print(os.getcwd())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+print(sys.version)
