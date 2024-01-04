0ef9d9e0dc620ad6c1c6a5e98a542824f0001faf
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:54:32 +0000

    completely side bright suck takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
