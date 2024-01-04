1fdaaf806ed5ef0d99e1f2465a8dd2935d97140b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:38:23 +0000

    roof bridge law operate find

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
