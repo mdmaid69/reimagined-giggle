edb66e870eab45998c404ceedfae0f5e810e4838
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:51:06 +0000

    conduct play unlucky trouble attention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
