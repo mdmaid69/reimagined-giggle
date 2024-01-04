b8bb35a0b86601d8cd38f00d3ff6aebeee8bcbff
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:50:49 +0000

    contribute conclusion place time art

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
