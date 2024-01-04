69ade962b88057ceb310b0ecc83c7244e00f5f42
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:29 +0000

    570enjoyable accommodation floor wander minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
