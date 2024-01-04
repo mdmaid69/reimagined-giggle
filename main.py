5b9f2ac661dbe02e16b6118b9dffcdb9931fa8af
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:39:46 +0000

    repeat manner relative week terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def convert_to_binary(n):
+        return bin(n)
