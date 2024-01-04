f0c85a5aa295eb7f63f3d4b126fa1f4473d23b90
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:11:16 +0000

    breakout crash roll brave which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
