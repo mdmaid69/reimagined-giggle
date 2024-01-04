9ff5bceb562fb709603a2a855011aaaaf127f106
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:23:26 +0000

    move over right question pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_item(array, i):
+        return array[i]
+import datetime
+def get_current_date():
+        return datetime.date.today()
