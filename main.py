4e828f6a552e67d27003ee598dc89b1cfd4919e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:13:01 +0000

    Dd list confidence bringback mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
