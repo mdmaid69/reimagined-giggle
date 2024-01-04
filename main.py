6301d9fef8ca69074de5b7f68e9f3ca03e2600f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:43:29 +0000

    fall Bb phone shoot than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
