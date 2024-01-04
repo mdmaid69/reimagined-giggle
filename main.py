c38e82bee9ab97e4d121207c28398f2fbc2cd062
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:04:59 +0000

    will argument recent might strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import logging
+def log_message(message):
+        logging.info(message)
+for i in range(5):
+        print(i)
