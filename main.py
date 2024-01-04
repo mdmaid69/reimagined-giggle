d3973fac54b80a50e8fec44b382a4269323814e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:57:32 +0000

    bother agreement conclusion news public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
