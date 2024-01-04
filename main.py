f7b4155a13ccf50090a9296bea7b44b676fdcf51
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:09 +0000

    difference one ahead different complaint

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-name = "Python"
-print("Hello,", name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
