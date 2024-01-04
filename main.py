dca39e9079c777ea7c4313a73fbd4af879923b79
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:48 +0000

    address return turnaway ago smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_base_name(path):
+        return os.path.basename(path)
