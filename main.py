50dde50187f44fe1000cf53217bff281fca9730c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:40 +0000

    ring start talkinto connected soak

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
