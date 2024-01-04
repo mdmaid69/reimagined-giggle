aab59b71c231a1220150839d667fd778342c608f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:57:15 +0000

    holdup far state save trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
