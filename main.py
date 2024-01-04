c7f472e6aff242b898adf5ab45d222616fa04416
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:26:42 +0000

    cap Zz obvious difference wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
