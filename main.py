90a734444d9dac4acf53d58f6d52c941e6c7a9bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:35:11 +0000

    neck pity ill contact prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
