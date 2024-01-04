3f3630634e24961e4afba530e4215db00800bad6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:13:23 +0000

    language whistle except commit wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
