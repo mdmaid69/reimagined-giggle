8f5a0179f73f46e5288277f0fc0c54e2577e15ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:30:57 +0000

    minority ignore stone spoil road

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def count_elements(lst):
-        return len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
