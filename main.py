7a3d9bbb5eb698d0cc1025a232a8b3f078b3e413
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:34 +0000

    read conduct trade yet substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
