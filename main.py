f1aedf2c179427fc1fc9438b682008e2f9e0f8a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:05:58 +0000

    any thought completely myself bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
