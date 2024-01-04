eec982aca2f039ff17ffa3ab8cf85c3f54ed4dec
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:30 +0000

    concerned inform drawing quiet dead

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
