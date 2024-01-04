e831f7f91b218c10d40b87aed450d1455c60dd87
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:58:04 +0000

    179bite but today air else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
