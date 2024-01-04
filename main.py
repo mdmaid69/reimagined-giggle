23ce2be3e965a84ac1a2bc1767af908be878fa16
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:06 +0000

    day part another where few

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
