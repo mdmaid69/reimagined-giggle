05795a16f81c8753934069a907c1ed3a122c96c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:49:27 +0000

    area pin much frequent so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def square_number(x):
+        return x**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
