cb56f0482c53ffe64078a4aa2f9a86f661836d64
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:40:26 +0000

    cheque square pass affect proud

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
