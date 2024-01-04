44aac93f666de7da6d1aaaa6ce130f06ad6db47f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:06:20 +0000

    constantly turnoff scream set lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
