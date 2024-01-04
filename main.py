98f9065aaed27e64cbb710a43f04160e0679608a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:08:12 +0000

    treat collection seat danger emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
