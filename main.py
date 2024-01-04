69f65c648c35b388f7870a830f37dfe5343abce6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:36:07 +0000

    house as copy practise doubt

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
