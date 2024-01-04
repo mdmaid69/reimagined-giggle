4c369c716337454e365d269627bbe36c695d74fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:31:23 +0000

    degree attractive narrow broad mad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
