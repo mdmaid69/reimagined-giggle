212cdd0cbb0dbec33bb1a8e8363bcc4dc58e859c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:26:33 +0000

    display properly promise copy project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def convert_to_binary(n):
+        return bin(n)
