dff22ac9b6aac8f223e780d97f5b922b1e7ecbd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:56:02 +0000

    do computer material university behind

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def append_to_array(array, item):
+        array.append(item)
