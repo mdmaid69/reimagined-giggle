6f8052522c830c0281b6a331f803c50f12fa1f66
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:36:09 +0000

    chase hot contrast boil cruelty

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
