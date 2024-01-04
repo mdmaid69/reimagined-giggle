e614d067d80aaa9b951d07b3aee1d1b0a89c45f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:35:30 +0000

    interested spare situation underneath answer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def reverse_list(lst):
+        return lst[::-1]
