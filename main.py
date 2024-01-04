b910f21825c5b9bbd84855cd1b737d479ec55035
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:15:40 +0000

    needle promise clean live total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def greet(name):
+        print(f"Hello, {name}!")
