582569548bfdb1cbf1e74a207a8f6a0a17dcf0ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:25:04 +0000

    worst answer exploration air tall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
