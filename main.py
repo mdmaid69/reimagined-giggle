f3ad89fbd108060ad4739d403e7bc5b87be7e32b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:27:41 +0000

    warning miss definite goon ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
