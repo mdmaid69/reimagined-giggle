e6f5d3ace5e975068f5df4d080ad8d2406c55ee3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:13 +0000

    error everybody delicate steal brown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
+def calculate_acceleration(speed, time):
+        return speed / time
