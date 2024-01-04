1fb417896fc8e28084d82e942a3eb608026bd7a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:01:27 +0000

    doubt trade impressed bringdown improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
