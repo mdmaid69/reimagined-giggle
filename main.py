69375e320c5bd16b5e91d861a67baf6770403d63
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:07:26 +0000

    splash unit steady offer godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_min(lst):
+        return min(lst)
