f538a3b92cdb47d1b1042626a60885f05f4ab10f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:16:31 +0000

    honest discover jump complete pinch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
