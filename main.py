21e830d1efc262158c8aeef6b12555d90bd81fba
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:04:13 +0000

    dry face wheel child period

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
