2cbb102753d1095e5dbd267031650a9c67e1d009
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:09:56 +0000

    work tape guess fit ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def sort_numbers(numbers):
-        return sorted(numbers)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
