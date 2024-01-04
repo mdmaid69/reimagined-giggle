38b621958b5e48a617933012071a156e4e457f1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:20:24 +0000

    lean left run wise power

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def remove_directory(path):
+        os.rmdir(path)
