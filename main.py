cb5857f1955c59786b9e1a4b30da38c6fbb22ea2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:53:24 +0000

    face content approve area glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
