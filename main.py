9a93f5ff2a169bdb2c0dcb1e4dc4e56abf7a2d68
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:09:52 +0000

    threaten its sort Zz slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
