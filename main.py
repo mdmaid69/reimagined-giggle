278d1216dc2e94bf23e614c37372b5be58f200d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:13:43 +0000

    slowly thinkover tone visit sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_power(work, time):
-        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
