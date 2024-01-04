94c2972f2fd36abeaea4f9bbd3886d783de4e5fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:22:10 +0000

    bend gas convincing operation introduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
