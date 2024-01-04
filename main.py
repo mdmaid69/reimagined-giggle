c7eb2b9208f85759c9cef99c540cf6adc4e47963
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:44:25 +0000

    church tooth snow fact development

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
