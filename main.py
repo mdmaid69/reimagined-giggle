7e09e85081c15e4e4e5b7b13fb3272434141e46c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:31 +0000

    chance unfortunate autumn me tomorrow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
