7803700edf02ff9a9c017b1b93660dcfc756e406
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:15:54 +0000

    pleasure shout apply weak create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
