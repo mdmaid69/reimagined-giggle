a07d1597d86d7743942a3d5d13740f2ca16414df
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:50:56 +0000

    bar trip bang tool apparently

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+print([x**2 for x in range(10)])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
