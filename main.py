2fb0c86e95d2bb5425fbb8a3815f55e6fe8b14f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:06:12 +0000

    previous smooth tomorrow gooff change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_tangent(x):
+        return math.tan(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
