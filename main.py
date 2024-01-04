700aa9ab6f558c4c7128d36cd6b8c487bd69f746
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:58:12 +0000

    true turnover knowledge smart search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
