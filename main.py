ce9453f1ccc359cf57a2ef1353f6f7c4bae0974b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:51:52 +0000

    curious turnup unkind Ww nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
