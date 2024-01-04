75caf427ab9f1a8803a90987a8311e28a2173516
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:53:46 +0000

    commit happy nor determined noone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
