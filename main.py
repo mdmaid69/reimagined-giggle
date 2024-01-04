aa789aa881b7dab9b5ccb07c211484a424315974
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:23 +0000

    past lay decision among long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def reverse_list(lst):
+        return lst[::-1]
