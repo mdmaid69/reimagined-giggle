49aa75e332bda19244fde24c9fd55bc841c4d98b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:39:26 +0000

    bored close influence ordinary promise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
