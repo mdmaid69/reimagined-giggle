5e78aca06934d50422a4581bc145aeae684e3314
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:49 +0000

    vanish sound imagination unit child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
