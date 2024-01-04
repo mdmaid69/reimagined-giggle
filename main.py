d94c4a6b27df27be78da9aa0c7e0f53e83744815
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:39:18 +0000

    reserve certainly guess admire key

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def subtract_numbers(x, y):
+        return x - y
