5dc80dbf10e69e51c40ead1d1047d93e041b3824
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:24:28 +0000

    later criticize sure excuse accommodation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
