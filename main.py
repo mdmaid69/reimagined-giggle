ee4c87e025503e11530d7d70ca98112665a64795
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:56:11 +0000

    bad keepoff profit ordinary direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
