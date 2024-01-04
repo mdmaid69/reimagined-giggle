4eb961c038c5fd6abe440c92f27c4038bdc88bf7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:14:14 +0000

    whistle luck calm kind male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
