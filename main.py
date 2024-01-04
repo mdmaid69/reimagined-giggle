aee5148ca8f95b976e5b5b0ce2c4a6fd88d0137a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:56:43 +0000

    vote brain flow bright unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
