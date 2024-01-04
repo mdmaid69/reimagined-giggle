9b90605a9bd7dcd06d9842c5d1294a552909608a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:24:46 +0000

    choice land treat shoot set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import platform
+def get_os_info():
+        return platform.uname()
