ea4cda1482752a766b3b40958b24537f797e9db5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:03:55 +0000

    step will straight remove terrible

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_hex(n):
-        return hex(n)
-def square_number(x):
-        return x**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
