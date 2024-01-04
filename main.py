676f05eca864f599b7cbfd51a653d26b7e468805
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:05 +0000

    crime dig building regional scrap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
