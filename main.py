53523c85747b0599f3c66dc8e5896dfcbc3a0e49
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:02:56 +0000

    mistake call legal butter provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
