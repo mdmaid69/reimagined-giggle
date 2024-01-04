e96e9e4742ef49a9f904c3d44436c79becc42418
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:42 +0000

    chance finish uncertain ability confused

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
