c93e1c956ec7bcca04694574a76647a41c7bc9bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:24:18 +0000

    dull born dream entrance maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
