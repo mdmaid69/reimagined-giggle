d18200349fe6ef00471b82a829ba5789e49ffc20
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:18:46 +0000

    trap exciting ill off comeoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
