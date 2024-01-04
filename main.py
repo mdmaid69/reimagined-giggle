8c0d5b1d04f6711b7b3a4213423e50b7d2b5319d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:40:33 +0000

    hang use damage disgusting lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
