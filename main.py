cbbe88249f8746e7c2760e778068d1ef0c7b8e89
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:53:36 +0000

    fear emotion myself artificial field

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def remove_directory(path):
+        os.rmdir(path)
