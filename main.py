dcdc054619ec0df9534a817662f173dcc3f7f5aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:41:54 +0000

    be gooff history total settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
