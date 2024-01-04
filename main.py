79a7caecdc7ace33584a56b9ca56d86e9d24c109
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:34:45 +0000

    early plenty issue share wait

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
