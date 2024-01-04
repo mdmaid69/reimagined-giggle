d13ced131fd5c3dd2e18e045948e7754fe867708
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:40:17 +0000

    squash give ignore tax welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
