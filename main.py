6fb91f67f20f709fc7168172825c19973272f96f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:12:53 +0000

    by fun nose film turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
