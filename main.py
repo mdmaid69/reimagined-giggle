77adcce68ee3e4a25c5c4926847b52c1f8a680e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:30 +0000

    that light any contain used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
