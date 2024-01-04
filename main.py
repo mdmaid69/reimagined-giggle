7315d06beb27a097ce1bd52a55eb1b335f72e679
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:30:34 +0000

    rather cap yesterday load die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
