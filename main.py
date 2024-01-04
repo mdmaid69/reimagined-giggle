4be72d8a31b56229ee774e4c2b56c72dcfb90a24
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:46:33 +0000

    dance flight hello represent floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
