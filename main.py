72745130c74cdbcda46927a1679542abaff6a086
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:48:20 +0000

    part wrong godown funny hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+print(sys.version)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
