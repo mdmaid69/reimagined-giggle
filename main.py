fbf31d010f3c8f1b02d3b982e37bf6f76e05ecdc
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:21:17 +0000

    protect act centre sound only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def sort_list(lst):
+        return sorted(lst)
