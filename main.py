c025906ecfd1f71052febc8ceae75471c634d36c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:18:02 +0000

    opportunity way little die lock

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
