ae54ec9fedaadc7104bea202ac9896b455f56dc8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:53 +0000

    before breakout our blame Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
