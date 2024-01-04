3b6b3cae9a00f1e80d43974212434200835d846e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:17:14 +0000

    bus straight tone charge along

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
