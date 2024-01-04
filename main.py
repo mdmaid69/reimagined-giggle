0458c0c53b43f6cdd09dcaad55c9eec3fd511e81
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:44:21 +0000

    thinkthrough example cause but can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
