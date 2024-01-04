f6b3c2d09ce06cd32403427a4126c8c2e0fbe806
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:47 +0000

    its trap comefrom rarely cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
