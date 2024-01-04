4f0be282c235bdb21e5ee7711bd6e282766e482b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:25:54 +0000

    listenfor event backwards report it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-name = "Python"
-print("Hello,", name)
+import array
+def reverse_array(array):
+        array.reverse()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
