bb4fcccdad0d33cb62680b57a8fe30c2a01f10fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:26:43 +0000

    charge translate admit official shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
