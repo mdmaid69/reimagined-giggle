12ca918576398c513877e7af17a7bc1c8b759ff9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:13 +0000

    turnover before speak office cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
