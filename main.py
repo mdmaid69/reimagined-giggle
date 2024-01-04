19c967d239279e27de262cfabcacdcbbbfbda36d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:03:49 +0000

    will low obvious tired authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
