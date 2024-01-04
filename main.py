a5ba06e2dae345828817bc17a2661a58bf92e78f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:24:15 +0000

    branch fond test turndown heavy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
