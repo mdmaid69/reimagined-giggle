651a081e181dd54b5b4765162deede6db6b5b676
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:57:44 +0000

    wrong date interview special active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
