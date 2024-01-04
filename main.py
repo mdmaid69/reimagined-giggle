1a84af89f2cf4d5330c0e591d1e2bca50ff1e23a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:05:08 +0000

    post why shrink once drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
