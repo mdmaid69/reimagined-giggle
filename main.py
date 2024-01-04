1b5c463d0777c0c2dbabbaff51caf11ebc51d5ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:04 +0000

    talk exit combine party declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
