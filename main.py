6cf2814dd15cfe5ea52386bea776a36e48843267
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:42:52 +0000

    exaggerate sorry drive against software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
