e6111bd7c5058a6a4fc6ddb5fdc47f7d623f77d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:50:27 +0000

    unite any treatment print load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
