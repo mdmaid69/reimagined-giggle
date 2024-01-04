0fdd55aae2ad5bfea56d82217bf1bbe548cc97a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:58:09 +0000

    shrink free nearly old prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
