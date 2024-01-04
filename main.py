b808d28d0d502d471eb966729b22a5daa6e16abd
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:07:42 +0000

    debt account hers trade free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
