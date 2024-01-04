2d7ad989df65249fca6cc113de21b71f9c6ccfdd
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:36:12 +0000

    passage though spot leg achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_current_working_directory():
+        return os.getcwd()
