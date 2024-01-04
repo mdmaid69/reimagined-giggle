31dd7134b4cb43c1248c6fb87770d4031b9ab719
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:28:37 +0000

    back temper illness address degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
