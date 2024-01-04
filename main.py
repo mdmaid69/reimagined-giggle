1881f27d1d00dc08e1872a6ee9b1249dc1dc7eeb
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:22:23 +0000

    suckme print hand wrong combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
