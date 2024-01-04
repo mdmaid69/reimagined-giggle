b35f0fe8872888002187355c429e02ccd941215d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:00:49 +0000

    meeting skin market roof design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
