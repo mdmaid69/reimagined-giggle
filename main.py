6d64bc88eed97f7b9408335105fd6507bf8f3873
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:43 +0000

    criticism run flat holdout taste

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
