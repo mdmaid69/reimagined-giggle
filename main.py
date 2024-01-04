53c43056a08bce3364ce27e818595df702412755
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:46:39 +0000

    shop backwards trap frighten pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
