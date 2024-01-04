5c568b710e3d5206f782031c2c292b0fb2dc0eb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:47:46 +0000

    getoff slowly before whisper family

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def is_palindrome(s):
+        return s == s[::-1]
