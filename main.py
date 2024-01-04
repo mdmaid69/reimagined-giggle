8bf589437158ac339d14dbbc43d7d237918b0032
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:31:48 +0000

    why achieve produce turnout smart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
