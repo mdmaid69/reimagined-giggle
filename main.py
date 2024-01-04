b9850e4984581a89699a9ace33acb4a6af661b13
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:00:47 +0000

    performance software practise comeback level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
