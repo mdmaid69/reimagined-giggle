15fc071b60e65a1a5b466b9addf4218ab1f5ca24
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:04:51 +0000

    high awake mark wind hold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+print([x**2 for x in range(10)])
