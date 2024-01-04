426d2f7dbec81ae33cf23d69efaf0fcc92577fc9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:38 +0000

    married crowded takeback small bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
