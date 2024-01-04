fd63bf57c905c2f57266288c588ca21f4c79f206
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:45:16 +0000

    responsibility bend belief light weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def exit_program():
-        sys.exit()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
