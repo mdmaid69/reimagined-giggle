d1f10d9b83a2b8d2f45cb6fcffe25f91c974ad2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:23 +0000

    notice count notice brush image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
