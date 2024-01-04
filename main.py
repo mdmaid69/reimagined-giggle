4da52150a8a8447311e370c4e68b51b431a23a35
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:07 +0000

    little check into talkback typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
