0b286c8b6bf098583ba062141f9271294a5a0c91
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:06:50 +0000

    occasion calmly basic only themselves

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def sort_list(lst):
-        return sorted(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
