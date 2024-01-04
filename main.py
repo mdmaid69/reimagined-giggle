f3d1d41b6a626ad82dea96df58621390ca2a1582
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:37:40 +0000

    design dream least than onto

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+print(sys.version)
