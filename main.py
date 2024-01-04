218c350da662bcd699d45d8bde723dfeba14ba6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:08:15 +0000

    although typical ear accidental tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import time
+def get_current_time():
+        return time.time()
