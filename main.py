22b274608cc0978d15982b04b612d4c6799c9c74
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:26:39 +0000

    prepare circle underneath stage with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
