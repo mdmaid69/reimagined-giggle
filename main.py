52a91f1bfda04e526023c7805b1074e5e973eefb
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:09:48 +0000

    very mysterious whether show slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
