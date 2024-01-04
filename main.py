fc422815fc8f0a5e41c04a00b6eb75acbe3aa0cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:10:32 +0000

    doctor force there ocean work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
