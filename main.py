dc7195981f6f799d2b14335fae1504cc81694b74
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:48:31 +0000

    notice reach result model label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
