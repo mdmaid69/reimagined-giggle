dce21a9e902a7eee87d8c405b29b7bc9d49058e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:37:55 +0000

    turnon definite cough watch spin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
