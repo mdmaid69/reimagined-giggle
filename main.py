301529a1183572e6f6f47c3902ed7b8ecbdff41f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:49:18 +0000

    cost trick organize mixture fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
