745e200e4105fd60b5e35bff677cc58244c15e27
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:52:21 +0000

    set passage tour rich past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
