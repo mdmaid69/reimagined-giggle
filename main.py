c9505a44151b8a95c67df8ee754d7d9221e04ff5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:45:38 +0000

    quarter grade exactly upwards rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
