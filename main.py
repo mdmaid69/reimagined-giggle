52cbc868098f0406625600a638c08bdc0bedb0fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:31:23 +0000

    cost congratulations each refuse condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
