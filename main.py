80d34561e98a9bfbbd2b314a921ebd9ced4eaa90
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:09:51 +0000

    himself taste punish herself nor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
