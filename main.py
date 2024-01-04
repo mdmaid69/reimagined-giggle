fb7c941d81c8b4e9fc03721bc0bf50c9d1ece094
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:58:27 +0000

    favourite OKorokay steal pay skirt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
