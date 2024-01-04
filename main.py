8b1d36e9277147192c7cbd9beb2119e328fa0e5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:57:37 +0000

    565end appeal record last stiff

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_as_set(array):
+        return set(array)
 import csv
-def read_csv_file(filename):
+def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
