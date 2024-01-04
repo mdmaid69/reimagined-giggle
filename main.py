e44d35ca750519c6a792d215606dfab995691464
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:55 +0000

    bad indeed talk pattern explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
