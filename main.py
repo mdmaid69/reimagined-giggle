19a1aeadc1401482fff8d997c5a2d9ef59431220
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:09 +0000

    confusing so every exist frightened

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
