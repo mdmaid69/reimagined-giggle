c87ded2c689ecf3a6b8b8cb0ed50130b769e8616
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:27:52 +0000

    lose glad careful contribute high

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import time
-def get_current_time():
-        return time.time()
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
