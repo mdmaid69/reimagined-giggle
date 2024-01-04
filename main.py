2a259f73156a0d0ac0815df5423dc2748dcdf951
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:45:12 +0000

    abroad tune respect sure help

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def count_elements(lst):
+        return len(lst)
