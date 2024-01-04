cb1203520f3b5f5c6c41500ae06c7114aeacbf3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:36:17 +0000

    wherever Kk bet exchange keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def count_elements(lst):
+        return len(lst)
