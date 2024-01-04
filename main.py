3972667aa564e23e6ecf5dfeaa60ebed92fc7ec6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:03:51 +0000

    before turnup fast accept decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
