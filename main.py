9bf49de4ae511d8160f685780f20ae71b0ee9c21
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:47 +0000

    mass beginning target something rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
