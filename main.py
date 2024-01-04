542f1286e561c0c6b5440e0bab500f415dcb3da2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:00:53 +0000

    design note soft form gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
