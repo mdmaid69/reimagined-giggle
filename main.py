a106f0ccbc9c2caa3baa4f6c9eef9d33c344bd71
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:31:56 +0000

    principle false event rob off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def find_max(lst):
+        return max(lst)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
