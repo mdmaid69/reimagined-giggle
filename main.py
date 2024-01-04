ad71eec6cdff1100b6708262009ebb551cbd25b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:25 +0000

    kind force fashion standard spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def convert_to_hex(n):
+        return hex(n)
