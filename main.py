a9b78954deb22df1ed692cb4bb633f5abb34dfcb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:13 +0000

    first tread getaway strong large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
