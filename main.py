9b74ee9e988db6d61ca90477c6807b818b7105c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:31:40 +0000

    place challenge unpleasant giveup room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
