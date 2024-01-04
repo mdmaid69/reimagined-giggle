9eecd06a2546f405a3f55e46150abed02dcb7c12
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:39:59 +0000

    ahead alone hurry teach close

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
