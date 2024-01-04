b3619e2d4d2f76c4dbcb2cabb375f3a02ab2c601
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:36 +0000

    similar common idea market mean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
