c2fb5cf893075bb5fe29eb425d62912b3ffe9c9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:36 +0000

    sort slice tell stupid 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
