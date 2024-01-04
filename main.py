555f393246928ce050a943aeccabe4b12d7d9028
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:34:53 +0000

    gather room bank public intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
