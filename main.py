12b1b0c71b65501c8c1825081e2b1f627131106f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:59:14 +0000

    load about related thumb professional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import os
+def remove_directory(path):
+        os.rmdir(path)
