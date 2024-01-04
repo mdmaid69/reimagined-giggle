e56d646af0b8da97b8a600f9cc39ccab1b55949f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:38:00 +0000

    act ever concentration improve callback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def cube_number(x):
-        return x**3
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
