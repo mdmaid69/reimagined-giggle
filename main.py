7efbb96bbeffba700bba0906265d4e4278f9eb1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:15:25 +0000

    food special guilty violence refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
