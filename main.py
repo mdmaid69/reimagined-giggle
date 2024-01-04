58f3ef37e4012ceea28b05f6b5ea8c95583cb080
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:57:02 +0000

    will offend neither there underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import numpy as np
+print(np.array([1, 2, 3]))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
