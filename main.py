e015d1c3f811c22138e7d78dc8df3367df3d3d07
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:24 +0000

    already content post tired arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
