9bd408093e42793d940e0d40983940618b8b1334
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:58:39 +0000

    new centre cruel right treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
