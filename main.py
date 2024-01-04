ec9c884276b91e339dc8a02876fcc8a274716676
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:26:02 +0000

    gap satisfied game steal quality

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import json
+print(json.dumps({"name": "John", "age": 30}))
