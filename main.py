0d15e083f0eae71d3675e32f60866d2e119b3d6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:33 +0000

    family join wear extremely minimum

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-print([x**2 for x in range(10)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
