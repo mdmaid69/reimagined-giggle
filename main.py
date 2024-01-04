41b274b1e2b236fc9aac816ac2b2934e143f63a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:08:04 +0000

    back mine occasion score snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_complex(array):
+        return complex(array[0])
