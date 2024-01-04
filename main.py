78c4ce4331e53ab6179824500ca713450ab9229b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:05 +0000

    somehow great door firm fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
