781f9c7076427fd96a7efdad15042cb6472a498c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:00:57 +0000

    education rule copy cook strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def divide_numbers(x, y):
+        return x / y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
