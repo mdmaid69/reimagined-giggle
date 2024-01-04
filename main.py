fef7f13403462d82881c8605719b9a2bb0b31239
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:08:10 +0000

    long conclusion breakthrough suck straight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
