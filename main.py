d85c792cb7a8aff49f747c1e6dbefc426063c90a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:22:55 +0000

    from challenge imagination hurry hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
