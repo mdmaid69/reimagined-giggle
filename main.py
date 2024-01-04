b1bcc67cb63206137739875880861de7ef5d0af7
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:18 +0000

    obvious ourselves everyone sign confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
