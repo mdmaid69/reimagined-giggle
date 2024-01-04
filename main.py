6645364a09df31ea567bf922854d6da8827a4e2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:01:42 +0000

    across bite cheque label speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import numpy as np
+print(np.array([1, 2, 3]))
