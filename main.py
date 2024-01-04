598eea6862b9df5c0cdf1107d589fb5194bb5b78
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:42:15 +0000

    Cc feed mend deep set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
