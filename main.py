44c87f16b32b167a12ab58a8c54b363758140e6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:12:12 +0000

    relief something family fond extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
