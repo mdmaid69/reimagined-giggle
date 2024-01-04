a191e048bbfc6b883005da353abf5d62fabdc5ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:32:31 +0000

    OKorokay arm radio married pen

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def divide_numbers(x, y):
-        return x / y
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
