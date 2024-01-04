41e355f078c5fd2700fab4588a08c80abd64b733
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:56:51 +0000

    impressive tear meaning possible square

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
