b840d0ae6e8bfba493dba357a58d6765ee239180
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:43:35 +0000

    over market various behaviour well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
