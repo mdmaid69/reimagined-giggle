f9af1e2c07585f5119743e037f5ab395837b64d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:42:24 +0000

    persuade charge natural tap Ii

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
