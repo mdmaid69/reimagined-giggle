0ea6bcf9b43cb2cc455499ca872a86da46d79539
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:21:30 +0000

    tell just goodbye shop person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
