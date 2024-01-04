3775cfccba286a3b851174b77ca35dab7bcbc1be
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:53:55 +0000

    pinch fault light typical near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
