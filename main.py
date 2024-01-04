9e93d834daaf116e315a56449a53311f0eb128cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:19:19 +0000

    touch aim professional both relax

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def is_odd(n):
+        return n % 2 != 0
