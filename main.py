23b1ef66a78817fa6f38b87f777da56afa7a52bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:01:58 +0000

    them consider care bend have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
