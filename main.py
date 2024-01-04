118148c6fdc40781a9a6a57fd9ac3fd7e3a3385c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:21 +0000

    cost colour sensible fast child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
