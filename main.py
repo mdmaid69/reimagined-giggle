0117668918fd3582ba8edc7863f44f80e55f6050
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:43 +0000

    minimum become thought work stick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_density(mass, volume):
+        return mass / volume
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
