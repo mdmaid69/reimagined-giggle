a583b37613815b1c367a2d8b792c8398f4e96ec0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:22:55 +0000

    worry exactly female count smile

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
