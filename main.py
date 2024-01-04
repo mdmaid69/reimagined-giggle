f3cec28eac133a4ca052800086d44e2f6689a17d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:58:08 +0000

    567enemy difficulty news flow majority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_perimeter_triangle(a, b, c):
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def calculate_perimeter_triangle(a, b, c):
         return a + b + c
