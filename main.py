0a63d6a9d4d87217ffcda7b3f14b5d3fd755e404
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:22:01 +0000

    control money fortunately snow sky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
