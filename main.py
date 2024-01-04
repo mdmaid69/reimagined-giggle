c6e59ec5f3e195d1d9193e23177d5b2b042672ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:08:56 +0000

    best term mine pressure Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
