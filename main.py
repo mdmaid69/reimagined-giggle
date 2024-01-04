6e5ded87043acef66a77124d31f70ed5cf3e8ea5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:17 +0000

    sentence Hh afraid myself attitude

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def square_number(x):
-        return x**2
-def find_unique_words(sentence):
-        return set(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
