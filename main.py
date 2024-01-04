cb3e4db8f9da42580d60498cf1064531ac224dd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:23:24 +0000

    modern Gg me spin instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
