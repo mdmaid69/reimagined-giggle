66e25eb76668a366de8531d0dce305c1f3576c94
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:10 +0000

    bag sweet arrangement question area

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
