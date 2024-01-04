cf875713efd09fd36af2a88e4e960ed5903a5a67
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:12:45 +0000

    sound Ff unlikely crazy worse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def multiply_numbers(x, y):
+        return x * y
