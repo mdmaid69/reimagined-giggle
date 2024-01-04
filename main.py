107391647141482fde518a8b0afd1a7e92a76b69
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:17:34 +0000

    rather consist tap later gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
