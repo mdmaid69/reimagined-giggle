eff3b3e5d765d8ea9e99edd4ad5658a4bccbc509
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:39:32 +0000

    careful dick great comedown blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
