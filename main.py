c27e58bf801192d2b4b9aed692af01e4745dc7cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:28:03 +0000

    boring usually country owe comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
