65797902f5e66ae876cbf3291e365e196d0ea7fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:27 +0000

    workon supply underneath 570enjoyable fortunate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
