f8002c9c81bd5d0d0a8840f7f2740db85729fd71
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:04:08 +0000

    decide plan small task confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
