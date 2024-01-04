acab8e85cc8886b7f152fd17e652c42375c8d5bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:49:36 +0000

    lead towards mean circle practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
