d6a1e27dfc263108fb5282a3ded5a9d0ecdc5689
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:10 +0000

    university Ff easy say boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
