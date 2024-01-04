e730669e5194eccba567ba921874e5c4d7659d22
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:44 +0000

    drink absolutely thirsty within something

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import collections
+def create_stack():
+        return collections.deque()
