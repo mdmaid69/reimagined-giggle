4d551da34778b391d630a2aa671aa4787ea6b7b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:08:38 +0000

    turnaway address live care slowly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
