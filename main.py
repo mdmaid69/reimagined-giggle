47d492bb28afa036ed6b7bcf546ea97c82b1de95
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:26:05 +0000

    either peaceful claim charge miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
