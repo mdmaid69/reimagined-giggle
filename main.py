e4a8a2eee18feb41249b620d1a83f664cc8e5f63
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:40:08 +0000

    present extremely exciting attempt handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def divide_numbers(x, y):
-        return x / y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
