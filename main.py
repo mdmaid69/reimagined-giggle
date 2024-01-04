8fe8e8fd1b4205ba0805cd1144e3ad54c41f0f5c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:27:52 +0000

    tap movement plenty fair performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
