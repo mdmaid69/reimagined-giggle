5a526f633cfe6c1f7ed5246c5954e47ec4a175ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:02:30 +0000

    winter judge impatient task hurry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
