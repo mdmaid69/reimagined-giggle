03d4e62ae8396b4167fd6c036b2dc7bd5b8f432c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:09:52 +0000

    partner comeround persuade none force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_power(work, time):
-        return work / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+def exit_program():
+        sys.exit()
