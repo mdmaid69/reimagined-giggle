4226fc39e32b1d3424fc7e1b1d964a528d544bb9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:43:39 +0000

    right niggaacademy thought exchange invite

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-print(sum(range(10)))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
