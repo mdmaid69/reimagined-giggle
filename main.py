53adcd61533c76d8a5b1ad575c7e708a77991a93
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:10:38 +0000

    bury personal big illness react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
