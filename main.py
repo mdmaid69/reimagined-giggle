54ceee7f4fc9b3c043e721f3d0492b6f841c505c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:09 +0000

    quick song drawing speak bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
