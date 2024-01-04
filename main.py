97b38629cd4b7cd1bd7542f3e1232c3860d0cff0
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:59:51 +0000

    smile loss spring none skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
