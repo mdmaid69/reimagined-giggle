96e4076a60289de547577b2caf2c7bfc12125f50
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:35:10 +0000

    offend arrival bang wish prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
