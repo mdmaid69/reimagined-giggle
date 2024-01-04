c9751b34fdb5e24bafb6f7b1cb1fdd3f0d007f51
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:16:11 +0000

    offer dry slight pronunciation cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
