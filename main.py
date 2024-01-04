bf995d4f2c2ceed1bb8df06599083ecf32ae9f86
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:26:45 +0000

    ring manage seriously shop forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
