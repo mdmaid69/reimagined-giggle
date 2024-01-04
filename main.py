efb1fbb612ff52c6c611488a72a6d1708c2fa78e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:53:33 +0000

    word able please message also

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
