1e10a413d35968711ef6ad30dd9d23d9540a462b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:11:17 +0000

    list image in remove stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
