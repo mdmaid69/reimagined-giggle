8da5d1050a141c8cea3a79bba27594e17e9932d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:36 +0000

    throw exam miss keepoff Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
