b702e731651c5b3c8276b612079e6c52ed257c84
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:36:13 +0000

    earth appearance brush invitation past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
