02fe1bcd82d3c1300a4ce97485a308a03033d0d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:16 +0000

    fall shop new empty something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
