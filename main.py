5e90e71f8de4b6aac4c0e2e45a3842840a3f6a63
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:17 +0000

    worry low start push offence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
