ee40d3103bd076a2eaef4c1c3d795db12234ee88
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:38:57 +0000

    increase climb protest Rr whoever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
