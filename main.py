a7c9df209f5ca674041d4bc57f29f9b940f9c149
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:48:59 +0000

    she lead form comedown travel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
