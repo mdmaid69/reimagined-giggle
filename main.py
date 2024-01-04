69f7182e15c0d11ab17c1d93569805e45772cc71
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:32:42 +0000

    weekend rule beneath slice sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-text = "Hello, world!"
-print("Characters:", len(text))
+def get_array_as_repr(array):
+        return repr(array)
