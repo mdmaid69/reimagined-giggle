36900696d704b50b15ff8f11d204f75ec6969280
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:44:01 +0000

    slight worth cough keepup shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
