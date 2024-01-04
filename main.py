1337795c27824c3739fc2c3b05c3b1397d87080d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:35:14 +0000

    child proud have lookaround food

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
