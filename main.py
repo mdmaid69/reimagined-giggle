310ae2a6af09d62a60df9faf2781131404d733b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:11:55 +0000

    manner necessary climb catch foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_unique_words(sentence):
+        return set(sentence.split())
