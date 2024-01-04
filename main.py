34a2f12cf6e52d6ef0b4ebb9db5a93e9da3e702c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:13:49 +0000

    sit accident pupil content punishment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def append_to_array(array, item):
-        array.append(item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
