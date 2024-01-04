2b1b3f2b7237867b18f42f96564f135afbff6244
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:28 +0000

    world generally yes instrument goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
