3bda7bfd77088a8c485b35ade16b168660bcebf8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:20:59 +0000

    freedom sometimes sister feeling minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
