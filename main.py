e148131b38d995c90c6b6a10e632cef2581c16bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:20:09 +0000

    continue claim every turnoff once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import sys
+  def get_python_version():
+        return sys.version
