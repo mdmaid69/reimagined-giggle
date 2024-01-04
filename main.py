467c355708344957e8c2c6b6b35c0ad223cf2673
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:54:57 +0000

    cough spite dress breakfast settle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
