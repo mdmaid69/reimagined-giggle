b3c6a52a4d3ccf2764cc7a872d70bf20540ecf5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:15 +0000

    carry doctor against brave stiff

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_power(work, time):
+        return work / time
