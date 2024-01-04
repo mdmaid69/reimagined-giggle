bcc2fcaab9fad1b9e8bebc1e5d1e94d8041d5c23
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:10:15 +0000

    bringup zero beneath mistake press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_float(array):
+        return float(array[0])
