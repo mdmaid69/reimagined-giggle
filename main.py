06536f172e7791d5e9181fd39e7c087451ea57db
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:29:04 +0000

    correct trick gate trial become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
