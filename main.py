3a065bb34bce61c5fd95df8db6b182f823741c81
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:33 +0000

    dance opponent plain worried pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
