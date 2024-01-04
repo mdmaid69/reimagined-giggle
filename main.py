8b15d06127f0c826f85c6ec080e979908886a7a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:25 +0000

    broken level use along attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def calculate_work(force, distance):
+        return force * distance
