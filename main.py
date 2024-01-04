c058265750db31875a74a2712098b1f3e82f5765
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:36:57 +0000

    travel tradition message thirsty book

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
