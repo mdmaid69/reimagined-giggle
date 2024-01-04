e57b7e5f69d8a5a82eadd209f161b3eb11f67e33
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:42 +0000

    copy variety stay sensitive slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
