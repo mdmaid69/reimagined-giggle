9658297ddcb58f9e1693fb8771c110b90c5f5b3d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:43:39 +0000

    flat callat power deliberate cross

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
