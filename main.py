35af92c035c09bf273e7d132d3bb7aab5bf4ba57
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:33:26 +0000

    pinch reduce definite best combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
