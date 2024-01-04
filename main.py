d6618fb36de6c129db31e92164e91d49314ba9ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:36:39 +0000

    sad heavy runover with minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
