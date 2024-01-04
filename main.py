1ce56e2416e4256ac376919f4e17b83a7d4bdfe6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:26:32 +0000

    play telephone must 565end lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
