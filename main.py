bd94b6f0cd714b45f214edc13fa5f07445ab84a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:38 +0000

    object change measure scene around

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
