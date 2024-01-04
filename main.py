745e356c265433da225719aa7f86975a367328fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:45:07 +0000

    organize include hers maintain boat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
