94c0909508dfd99fc883dde81f574ffd745a6831
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:11 +0000

    further estimate either should for

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-name = "Python"
-print("Hello,", name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
