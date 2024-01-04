2163815c975612825dbc3f27c1849df46a69523c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:20:29 +0000

    shallow wherever manage sure dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
+import platform
+def get_python_version():
+        return platform.python_version()
