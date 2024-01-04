2be7079df3877fadfcae071ae4b489181a131b5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:35 +0000

    nurse whatever file smell swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
