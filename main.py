1a68611f300b0a824e1f22a70432f7fc18e0baba
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:27 +0000

    bin law further the pretty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(numbers):
+        return max(numbers)
+def calculate_power(work, time):
+        return work / time
