8554b877adeedd1294d4fcd4f99d4411286978eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:20:46 +0000

    influence middle laugh part indeed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def reverse_list(lst):
+        return lst[::-1]
