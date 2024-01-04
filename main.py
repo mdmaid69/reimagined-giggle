49e047afc5642d73fee0836a2bdf0d834160cdb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:17:06 +0000

    fence human call find trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
