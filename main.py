a9e2cd2847533282736bd60492c319920b90d6e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:08:08 +0000

    share violence separate storm winter

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
