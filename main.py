59c52827104b15b93a6814bb8caf59306b635f0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:57 +0000

    contents area clear pressure goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import time
+def get_current_time():
+        return time.time()
