772cb177e8bf144ebaa75b4179a7330763b7f0ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:49:40 +0000

    flavour open relax luck runaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print(sum(range(10)))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
