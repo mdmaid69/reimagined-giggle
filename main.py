3196e6b35740e06d5fbb40196d096eb4fbee97c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:57:54 +0000

    very 564encourage thinkthrough arrive excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
