382f50f2b805aa9c4850dadfc366363665bfd02a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:59 +0000

    achieve flat star certainly smart

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-def square_number(x):
-        return x**2
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
