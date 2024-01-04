19a0ffc09a6aacb83b9b456b62151810ae149712
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:31 +0000

    mark develop waste suffer programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_floor(x):
+        return math.floor(x)
