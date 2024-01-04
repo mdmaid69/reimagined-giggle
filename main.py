799cf07cc69e96cfd3e7eba558fed19607e170fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:26:57 +0000

    wipe geton transport takeoff blow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_itemsize(array):
+        return array.itemsize
