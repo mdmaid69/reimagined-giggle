2185062ff878b465f15a0553b94ff3861f4b593f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:03 +0000

    bringdown responsible average letter challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_item(array, i):
+        return array[i]
