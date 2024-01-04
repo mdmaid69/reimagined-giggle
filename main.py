89b341cd577fd0a6142b5db1c2af67a68892c8e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:39:01 +0000

    advantage method circle makeup too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def convert_to_binary(n):
+        return bin(n)
