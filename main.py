d34e0b576a3b5d9921a2ef996e837d215bc7e417
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:19:41 +0000

    minority lean effort threaten generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
