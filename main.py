200fab5ae16ec2f8b777f85c7033a5325188d40e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:47 +0000

    giveback permission gate report yours

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
