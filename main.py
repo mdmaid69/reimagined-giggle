6a3448a852bddea71a8ee091c98fe4397c09720a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:49 +0000

    region think pay matter yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
