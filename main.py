7fcdb1ea2dacad75fb98e4da77c6f35ecf7ed6b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:20:23 +0000

    away always tape climb main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
