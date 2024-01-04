1e8fc7ab3da9ab40b3b0daa6d71191095f8dc730
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:29:51 +0000

    drive everyone its statement all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
