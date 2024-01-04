23ada41177f41cb016ad9f292a160b558d52d9a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:27:50 +0000

    eye entry 571enough perhaps official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_base_name(path):
+        return os.path.basename(path)
+import time
+def get_current_time():
+        return time.time()
