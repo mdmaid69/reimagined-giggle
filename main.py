3a302c57bbc35470c13ae913c201262bd93dee92
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:28:50 +0000

    injury computer definite certainly already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_work(force, distance):
-        return force * distance
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
