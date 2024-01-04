3f86e3c5ae6842a5707db34ea4ed6b1695d8d768
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:01:43 +0000

    shape moral experiment get language

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
