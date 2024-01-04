b1c0a8cfd3e3c65842f3e5d551d20a169a29a3fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:34 +0000

    prove announce live 179bite vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
