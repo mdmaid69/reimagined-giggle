37e558ce5085abd005be54d1e98ca8f5bea210af
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:30:53 +0000

    air before bother improve wealth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
