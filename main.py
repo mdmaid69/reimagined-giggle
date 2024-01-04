760259acfcae72df464e473d84e95c8a4b6417b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:28:26 +0000

    contain lean pull bury job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
