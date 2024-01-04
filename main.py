70bd48047368e43984b7b5ebd4e01c9a1f91bf3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:58:06 +0000

    show before tip form behind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def multiply_numbers(x, y):
-        return x * y
+def square_number(x):
+        return x**2
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
