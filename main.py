bf8f2a0a828f2abe138261f4df223a8f6aff261b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:03:03 +0000

    public wind demand peaceful anyone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
