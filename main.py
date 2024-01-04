3888a4934b6abb08168a38647c16aa1b197b14de
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:22 +0000

    success warning never rush persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
