5a91acfa6b361584714a83e823b10f9b36899cc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:38 +0000

    cry mine threaten miss wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def square_number(x):
+        return x**2
