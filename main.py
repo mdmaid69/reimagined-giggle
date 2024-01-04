233f54deb3b0973253a4f0a72876ce1af56ffdca
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:00:47 +0000

    combination ear block threaten battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
