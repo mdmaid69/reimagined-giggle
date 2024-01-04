9b0e34c03dfbb15a4cea6c4eaa32487ff2438ef7
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:27:23 +0000

    need across pile runaway fetch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
