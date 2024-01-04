a19e1fe011a137319a4fa90ac59660d83b010a35
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:19:39 +0000

    belief her memory pin side

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
