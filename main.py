e11a7661372ae67ff84fd80ebaadd86d7215d579
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:40 +0000

    hardly vision zero crowd moment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
