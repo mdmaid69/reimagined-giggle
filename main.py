3abcf991272921d331dbb3eb77627a4185c722fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:52:12 +0000

    separate start no knowledge tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import re
 def split_string(pattern, string):
         return re.split(pattern, string)
