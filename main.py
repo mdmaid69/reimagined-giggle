57d4eaef2a1dff2bf8d7ab17af741e77535b51be
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:28:02 +0000

    present yourself suit talkinto taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
