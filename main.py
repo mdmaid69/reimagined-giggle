ef91c2597bbf9c6035d938cb55c88e4d0a3731ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:52:33 +0000

    OKorokay boat temperature comeup off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
