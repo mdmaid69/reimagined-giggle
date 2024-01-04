8e863cb5b64d01ddcb0fd8a0c4e969ca7b2e0ba7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:33:40 +0000

    comefrom phone evening occasional listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
