bc990fadf03dde35208466739b0644178dc11291
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:38 +0000

    rubbish happen support software half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
