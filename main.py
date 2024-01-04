c56d1db0c4a8d9f492c327226aa25b0e228d2e33
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:13:58 +0000

    bang hand lose willing crazy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
