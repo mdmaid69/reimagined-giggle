1b9dfea4810d416f2226ba31c0e2a5887b8c6481
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:56 +0000

    movement such once original thorough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
         return sys.version
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
