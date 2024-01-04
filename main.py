e2eba42f13460932fa3321c7a76e07200cd555bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:12 +0000

    crowded lay exchange goon know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
