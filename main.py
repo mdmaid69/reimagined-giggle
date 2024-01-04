ec1d2faaad03b98f5562a75609055472100ea344
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:53:37 +0000

    divide date vision doubt young

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def count_elements(lst):
-        return len(lst)
+from collections import Counter
+print(Counter("hello world"))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
