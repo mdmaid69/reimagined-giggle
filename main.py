823355e51abf4ad706c2cff5443077f45f043bf2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:11:29 +0000

    block round muscle stop pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def cube_number(x):
-        return x**3
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import random
+print(random.randint(0, 100))
