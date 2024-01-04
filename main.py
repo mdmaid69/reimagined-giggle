f6dbd5a31936939ccbfb7f03d1a323f9cbb18b5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:01 +0000

    future back true supply earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
