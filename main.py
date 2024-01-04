982c176061c9c535af1fe15e8380bbce3828d4f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:45:03 +0000

    unfortunate fire lonely comeup violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+for i in range(10): print(i)
