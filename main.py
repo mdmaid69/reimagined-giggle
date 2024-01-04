d75adf1a91ea9ec2508547249c606019d8513447
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:17:51 +0000

    welcome pronunciation most limit risk

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_list(array):
-        return list(array)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
