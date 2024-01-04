83ca516cc8783762e6e0a6a8b3bcf3e90d749d80
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:54:55 +0000

    skirt rush hope entry tidy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
