58c937bed563c2113b83c9dafe8920cca0a2a32a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:49:10 +0000

    view Ll skirt handle almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
