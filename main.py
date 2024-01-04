d33827f6feb2c428fae6d617e0e56d98495aff12
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:26:32 +0000

    immediate past foot Kk compete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+print(sys.version)
