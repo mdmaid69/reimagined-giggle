2d844090fb505444f58c6c129ba10a236ab1378b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:34:13 +0000

    post dry mad welcome hospital

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_min(numbers):
+        return min(numbers)
