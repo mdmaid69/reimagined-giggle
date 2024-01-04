54782e800a5f08610c99def36fdf5ed74629f44f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:58 +0000

    true piece comefrom trade if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
