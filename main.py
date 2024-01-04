2de51c938cb03472c3a47c4cb1a0e6b51dcc3fa7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:03 +0000

    process onto on entry excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def clear_array(array):
+        array *= 0
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
