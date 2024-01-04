f9fb8cef7a8217c45da113936ecd07f8c8a60f23
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:13 +0000

    scared whenever strength rubbish trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
