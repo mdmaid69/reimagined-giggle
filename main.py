1d098b5b50900826c1632b2040b39c86375db993
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:55:17 +0000

    shock hide strength introduction mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
