f1514d4d7e92b34ce76493cb521f7af2f81de165
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:08 +0000

    matter face breakthrough refuse guess

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
