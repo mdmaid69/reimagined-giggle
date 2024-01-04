cad1f2fd18d3c64a88f45496413bb1c993be0fb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:17:38 +0000

    table thinkthrough hit guilty on

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
