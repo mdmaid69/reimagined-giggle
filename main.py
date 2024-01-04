8af5165f6aae207e2874a2f59db7d48c649afc70
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:23:22 +0000

    minimum effort poison lucky hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
