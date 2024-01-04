81b7051288ce031de366b34fab2ac6295946efe9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:53 +0000

    odd regard sick hospital tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
