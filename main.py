cb911e7c4d698d606bc4cba70d3a58bd4fa35d43
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:00:17 +0000

    holiday Uu fight forward store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_stack():
+        return collections.deque()
