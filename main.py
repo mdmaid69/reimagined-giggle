e4719ce1173d2aaa225354488f753c2dcd668ea5
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:51:26 +0000

    danger more thanks situation take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def delete_file(file_name):
+        os.remove(file_name)
