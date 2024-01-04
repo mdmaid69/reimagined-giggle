e44e79e08ff23e63d45b17fa13bd1b8c2a23fe46
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:00:36 +0000

    properly break bottom stay smoke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
