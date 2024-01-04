c88530ba94bae22788e117e87eef61f39394f722
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:41:41 +0000

    work give fit winter offend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
