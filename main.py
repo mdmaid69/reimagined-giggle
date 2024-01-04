1e7afd6622ffe35461bb4c08fd9634f8d67972f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:16:46 +0000

    store town tax teach forget

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def sort_list(lst):
+        return sorted(lst)
