d61faeab5bafafb3d87f156ba97a33b832f8cbcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:41:26 +0000

    strike anywhere trousers create follow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
