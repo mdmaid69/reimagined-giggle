3364b603a283f3ad6026cf3f24800acbd8a90880
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:25:25 +0000

    human street birth form form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
