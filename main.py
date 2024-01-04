d7ceb5f4136cf991677119fd41e4afa474e4dfdc
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:06:32 +0000

    find drop load pure must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
