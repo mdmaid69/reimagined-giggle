fb644fa0df62cdc93016bcbdfa0d052d57ada358
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:40 +0000

    special before before coffee surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
