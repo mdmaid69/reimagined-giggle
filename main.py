4598bb6f98d101d6fa91b5f09fb061571b27f3a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:04 +0000

    man similar fold since television

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
