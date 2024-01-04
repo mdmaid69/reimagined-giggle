4cf3fd22248d5c1608df8cf21c2f37a15ff45a2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:09 +0000

    beyond chief whichever spring object

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+print([x**2 for x in range(10)])
