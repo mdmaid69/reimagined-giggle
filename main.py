2eb4d6565a697da379f8b70015abb42cf33eaf57
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:08:43 +0000

    once goon fire cap soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
