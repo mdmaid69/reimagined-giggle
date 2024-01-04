cd12c27794d9a84671f44bb9cdbffb11737da958
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:55:23 +0000

    clever communication shout this point

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
