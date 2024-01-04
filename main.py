82f00483eba4b870d2a785bda50803da16c77631
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:11:45 +0000

    attention straight key congratulate admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
