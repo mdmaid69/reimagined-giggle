122dbd171129f5c8359c4e27e7b210a7dab1afe8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:18 +0000

    colour bye sink good realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import sys
+def exit_program():
+        sys.exit()
