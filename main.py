5eb1494e0c97271b3d748fc588d17bd54f4b5086
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:26:44 +0000

    exhibition promise responsibility light mind

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
