10a92c0e3a43b006e58ab2fe701cb97b2e966cf0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:18 +0000

    promise factory home artificial pleased

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
