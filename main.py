3db1f2197a192d599f9681e8d9623b9536fcd559
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:24:34 +0000

    permission callback glass treat big

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def cube_number(x):
-        return x**3
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_power(work, time):
+        return work / time
