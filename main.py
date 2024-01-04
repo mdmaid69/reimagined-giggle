09fade84432566bf4c9c180d2752857d2105de4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:46:50 +0000

    mass 570enjoyable himself runout easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import time
-def get_time_since_epoch():
-        return time.time()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
