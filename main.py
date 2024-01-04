9e82b83d6ab8ab4b5576f041bcbafa14385a7191
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:13:29 +0000

    Gg tax work across pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
