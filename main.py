c4a00ec0c2caa6d5fe867df0fc7e863c73ac1401
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:15:18 +0000

    undo survive promise attract present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
