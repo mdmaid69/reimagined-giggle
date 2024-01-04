8e9abf917762148c2522db02c27666c48c98e2d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:04 +0000

    comeover language active trust furniture

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-print([x**2 for x in range(10)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
