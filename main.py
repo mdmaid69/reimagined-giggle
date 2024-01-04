73114b253d9009232e3f5e9ffb067521446ae3ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:55:45 +0000

    sense bringdown too far mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
