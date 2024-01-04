d637fcfdb6c77a2bbf612cb7c6d063182a53c020
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:11:12 +0000

    study condition touch smile she

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def remove_duplicates(lst):
+        return list(set(lst))
