7144edaa97886e91df1cea55ccb0ec589dc6015e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:38 +0000

    fold care street violent plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
