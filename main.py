72641e49bb844fe61ef39b8af667744720966599
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:27:55 +0000

    degree recently delicate move form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
