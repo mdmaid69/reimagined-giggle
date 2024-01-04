d0652577019d20999b07a59a65e333dfe6360332
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:07:11 +0000

    forward guilty distant cover term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
