fc4dd4f06b08a776ed1eacc43e2d4057e7120c11
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:58:39 +0000

    pretty prevent phone road goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import logging
-def log_message(message):
-        logging.info(message)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
