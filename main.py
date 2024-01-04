d85a1535a3167eb8231f4ef860af41bf20e4116b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:26:00 +0000

    thinkover inside Jj personal carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
