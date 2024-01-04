518aab6d58588c45f177c0f95dbcb77ffdd89086
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:53:26 +0000

    aim ill true thanks cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
