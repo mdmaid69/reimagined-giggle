02dee63192003869478f01c0f6037c29dc0053a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:53:10 +0000

    takeback criticism plastic manage stone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
