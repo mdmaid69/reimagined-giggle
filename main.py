e009cfdf455596f835d11e2819c0c967ebb915cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:01:18 +0000

    pollution eat pin brain trust

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
