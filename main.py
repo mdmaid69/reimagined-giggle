ecdc5390eda13ff1fca236d7b8a2456fb09cde92
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:48 +0000

    high view off admit embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
