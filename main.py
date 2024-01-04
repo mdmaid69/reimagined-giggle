4081bbfaf810896295c9f4b59203bf601f2986c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:39:52 +0000

    simple occasion provide think generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
