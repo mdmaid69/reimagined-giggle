9262d159d46ad64b402a802792be5211b2203076
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:22 +0000

    pride in wish luck food

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def convert_to_octal(n):
+        return oct(n)
