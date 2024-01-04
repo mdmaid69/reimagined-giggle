69c058774046a6a51fe7c5fecdfea91d5dc3a40d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:28:14 +0000

    whom include idea piece comeround

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
