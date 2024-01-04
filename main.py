56d98e5b8293dcfa7426748bdfb0b610a7e78ccc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:26 +0000

    copy low trade trick ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def convert_to_octal(n):
+        return oct(n)
