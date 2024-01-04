d4c1a2cc93f355e1eaedc784a39b8ebbec0fc084
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:45:57 +0000

    threaten admit personal fence number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
