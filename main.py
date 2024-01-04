ec3277e0ba8f5523b8b37eafc8e631b2482465b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:41:35 +0000

    goback(to) sing club wrong custom

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
