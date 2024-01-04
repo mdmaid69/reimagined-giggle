63423decb8adab153a55fe87d092b8171f3cbcd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:42:40 +0000

    mine broad pain whatever should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
