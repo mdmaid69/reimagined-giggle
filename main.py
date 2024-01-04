9549829ac3c75546da6cbc21454181e6606a6698
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:48:07 +0000

    top nonsense or make tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import time
+print(time.time())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
