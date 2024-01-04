24de9d114e1487cdb0e27174b891415291a3a201
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:40:24 +0000

    interrupt little factory poor guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+import time
+def get_time_since_epoch():
+        return time.time()
