6d7f2aabfe529a2d43e4337bd6d62b64755cf3b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:42 +0000

    anywhere regret problem fix shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
