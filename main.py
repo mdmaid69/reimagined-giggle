6effd679c9372dc913950b5496eb24c01686e973
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:33:57 +0000

    left computer crash definite whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import datetime
+print(datetime.datetime.now())
