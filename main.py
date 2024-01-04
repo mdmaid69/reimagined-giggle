38c36513ec2115216fda22ad8ff32c5269990204
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:25:05 +0000

    related shelter consider allow long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def roll_die():
+        return random.randint(1, 6)
+  def square_number(x):
+        return x**2
