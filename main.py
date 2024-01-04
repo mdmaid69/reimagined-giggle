538870d4f0fe74c9650671d73ec7e25360a2eb27
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:46:52 +0000

    while relax illness beginning unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
