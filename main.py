3bf25f164cd49f320942afd2980259f43cdc5a58
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:14:44 +0000

    bury organization please collection listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
