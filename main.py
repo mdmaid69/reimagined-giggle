718cb3c884d34a390f764e13abe3af6c2e65a508
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:50:12 +0000

    powerful control spray rise display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
