1800765b1a504089aeec20cfb98fdc501e22374c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:46:38 +0000

    look school urgent flavour slow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
