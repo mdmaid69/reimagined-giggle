28007ed841e937d8034303957195cd049eb87e0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:38:11 +0000

    frightened few what free care

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-print("Hello, world!")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
