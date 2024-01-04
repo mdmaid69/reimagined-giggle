b05de17963fdb909a878a17e8ea24c6b1ea46ced
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:51 +0000

    out tap little fierce face

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
