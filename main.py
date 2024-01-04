b8e1d4336621949399aa8711f459a8dd20192687
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:47:46 +0000

    under poor taste guard like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
