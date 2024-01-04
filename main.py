7169bea84917d33c51395968b362eab8f0737ce8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:25:34 +0000

    step communication connection film impression

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
