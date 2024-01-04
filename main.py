85ea7f020a391b67d16a595a37d646870a5ed886
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:01:51 +0000

    community with road draw progress

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
