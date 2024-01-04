fc88f8553d1688fc6737d74e428fb12cc7fe3baa
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:43:45 +0000

    single danger weak poor floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_exponential(x):
+        return math.exp(x)
