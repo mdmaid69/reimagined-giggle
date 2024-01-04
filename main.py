6623da80fc5db8e8c17646353502e66812dae678
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:20 +0000

    finally take point comeup turnoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
