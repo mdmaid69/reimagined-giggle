58dbbc9c7028a1e1288d795701ed1ec2f0f77df5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:22:55 +0000

    risk several stick can mistake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
