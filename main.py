e85fb1b39237bb037495d1aa36df40a6dcec41cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:28:50 +0000

    tax themselves late design behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
