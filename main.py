a4c140320b46801a1b8f2371f9492d4f9928ea53
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:27:43 +0000

    size brown angry as honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
