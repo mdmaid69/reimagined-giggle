6671d301f8909535564ac3404f5ee57af6cf00d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:23:14 +0000

    572enter out determined admit practise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
