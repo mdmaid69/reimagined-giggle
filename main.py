906ff2a90d7b69d5b6daed45229f8551b43cad33
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:39 +0000

    admit bottom convince abroad press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
