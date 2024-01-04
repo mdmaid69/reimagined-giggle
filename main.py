532e93ee81f97bca2851a8920f5161682cd72073
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:26:23 +0000

    standard raise his forward tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+x = 10
+y = 20
+print("Sum:", x + y)
