7994004c10992d1acdb35c7aba48fe3ad94205a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:43:05 +0000

    seriously die swing day but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+print(datetime.datetime.now())
