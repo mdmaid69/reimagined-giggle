c2919b5401c8da9daae1ea6d5d2aab9be2aefe83
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:19 +0000

    born anywayanyhow stay exchange opposite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
