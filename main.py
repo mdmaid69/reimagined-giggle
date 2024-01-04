807dd987c599902dd168c6b8569900eb3fb9ed95
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:49:50 +0000

    total for play write drink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
