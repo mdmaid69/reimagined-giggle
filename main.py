152c29bbe4bae9bb98c4a5dde0c535b068ac49b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:39:58 +0000

    deserve demand fit weekend wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
