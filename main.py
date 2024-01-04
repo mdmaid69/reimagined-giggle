c598c4b50542ee34db83f78bc1b6fdb6cf209784
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:11:16 +0000

    high hello decide may stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
