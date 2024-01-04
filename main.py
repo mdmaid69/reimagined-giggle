5ad1bd562eba8c7fd03ae96ca93df9b1d8022ee5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:02:32 +0000

    tired send fail rain collect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
