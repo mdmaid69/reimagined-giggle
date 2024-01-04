9c5335b934d1e7c9f708fe1a90123326ba26f166
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:41 +0000

    film supply until supply introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
