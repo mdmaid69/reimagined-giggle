000fed0f80358fe6011150299713ab9ea34b39bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:03:29 +0000

    responsibility face more lookafter term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
