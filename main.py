bb79bef311791eaff05c83ca8989e9d805bda7b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:38:54 +0000

    reasonable really this nothing agree

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_acceleration(speed, time):
+        return speed / time
