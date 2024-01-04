d4c03d7dfa3e1093894362ff1edaad98b7be594b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:22:15 +0000

    sleep have afford raw compete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
