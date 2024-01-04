a85f05a925044e92a2f43f15e210b9478e857c89
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:33:49 +0000

    mine where popular hang damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
