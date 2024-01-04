cea8cc0593598af586e9aaf69394efb581fcb10f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:31:06 +0000

    do meanwhile benefit once clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
