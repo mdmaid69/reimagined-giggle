58cd02672b1bf15119a4b50166e8e1788e0270bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:18:38 +0000

    abroad thick communicate beautiful refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_binary(n):
+        return bin(n)
