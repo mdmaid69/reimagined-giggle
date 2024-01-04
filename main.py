a85427b11a1e44cf6947cb57b55fd014c62a9556
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:30:40 +0000

    ordinary back laugh slowly score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
