5e9f3df25fb7ae19f3de1db59d4a311911c8ef65
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:53:40 +0000

    active mend copy matter estimate

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def multiply_numbers(x, y):
-        return x * y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
