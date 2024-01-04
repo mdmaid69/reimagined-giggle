e9a1cfc5554a3112301897794f02f795cf0461a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:07 +0000

    murder wet recently pull trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
