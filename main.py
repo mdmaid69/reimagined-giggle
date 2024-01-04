bf774f5ca328290be30a169bc8ff86d646250593
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:23:22 +0000

    future little ever legal that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
