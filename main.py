4c942b199717f8ed899f594f379b1e71be265efd
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:03:06 +0000

    far breakinto only create 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+import array
+def get_array_as_list(array):
+        return list(array)
