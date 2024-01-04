696712111b83b6563613f960cabad8ad32d7ae32
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:27:01 +0000

    simple because right deserve no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-name = "Python"
-print("Hello,", name)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def sort_list(lst):
+        return sorted(lst)
