ad80a285ea3c4cd4cdd5d600aa51e88351378bf5
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:16 +0000

    spot name profit neither niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
