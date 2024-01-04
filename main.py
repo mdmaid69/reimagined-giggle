cc8d906af3720b2d639298d2a3ce30dfeed893b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:01:13 +0000

    tread move branch difference meal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
