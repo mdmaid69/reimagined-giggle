6ef126a8597ae1937dc640c4f0894ff1c19b74a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:49:23 +0000

    everybody attempt kind convenient shut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
