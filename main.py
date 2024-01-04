95564739e678518b70affff21f161675425fd0e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:26:02 +0000

    finger comefrom meanwhile cover value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
