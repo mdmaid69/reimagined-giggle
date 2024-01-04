a5c32492b00db1624d94dc77be83e2d94c9919b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:35:57 +0000

    funny rob instead section body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
