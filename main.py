7b9753c3273866e12f87f42203abe79c2d956962
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:38 +0000

    phone high escape fire advice

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
