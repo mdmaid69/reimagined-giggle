30c75d94bd4e1478c7e92015f3f9621cd4e94586
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:39:46 +0000

    shame admit personal mail tragedy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def sort_numbers(numbers):
+        return sorted(numbers)
