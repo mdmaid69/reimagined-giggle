d9b2a2e76d6589b0c10c2d11de685dc8480df531
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:13:14 +0000

    character declaration base free both

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import random
-def roll_die():
-        return random.randint(1, 6)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
