ff8c5cfc49331d04c83699abff8cc178f7ecaee9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:41:55 +0000

    if how important valuable sticky

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
