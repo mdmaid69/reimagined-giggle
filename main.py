ce0b0e4a01ee3f7ed4c2a8b45b11fe9a7fa9a77c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:24 +0000

    general typical centre professional mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
