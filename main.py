db30a604c09c02a02ce0b5bf3e1708dd449c86af
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:31:38 +0000

    learn dick convenient almost collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
