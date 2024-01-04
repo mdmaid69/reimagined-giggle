9a9ee7cd1afdad34198288de368379c17e87869e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:09 +0000

    attitude warm figure reject rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
