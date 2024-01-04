58ff2a1ced7b5b6dedfb44acf0c16a11f9210f91
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:58:01 +0000

    smile round central dry slight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
