190683e6c0eaddcb15ef747d6db43bf69fd1d770
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:34 +0000

    Zz dear organize uncomfortable description

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_work(force, distance):
-        return force * distance
+for i in range(5):
+        print(i)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
