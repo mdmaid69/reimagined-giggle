6eae6a4846f91f5593d60658a37a97ff9fe75e11
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:00:45 +0000

    commit the lookaround move accident

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
