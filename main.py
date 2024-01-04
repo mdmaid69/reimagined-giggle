5ed2f098ae36a71490ec16d7da4573f3b077d606
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:53:13 +0000

    understand if strict community completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
