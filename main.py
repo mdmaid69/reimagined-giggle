b14abccf8d6ac4a9734f6fa2ba2cfa7d8de1ee0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:51:36 +0000

    lucky style owe sail anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
