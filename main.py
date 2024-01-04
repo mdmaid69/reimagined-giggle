5c566e826dc638c47d6c7339edec65f10d45267e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:43:40 +0000

    possession last difficulty office sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
