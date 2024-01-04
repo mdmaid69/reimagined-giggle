eb110179b0667211123de23ad8dcfa8df35fc892
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:22:06 +0000

    giveaway share mixture turnoff count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
