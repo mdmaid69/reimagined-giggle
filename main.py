8887054ee9213e18e1736452921269f41f5aeb7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:29:34 +0000

    nerve new pretty task rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
