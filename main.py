e533492790b3e2cfcb8e2357f3d6a848305fc7a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:57 +0000

    relative note thank split weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
