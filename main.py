27d3bc3f922397c337fe0022ef66d1ab69b69687
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:27:23 +0000

    bad floor thin hate suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import tensorflow as tf
+print(tf.__version__)
