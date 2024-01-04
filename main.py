ca526583f2237dc7509c5377bd9a90a5e451a517
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:16 +0000

    ours breakinto recovery beginning workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def find_max(lst):
-        return max(lst)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
