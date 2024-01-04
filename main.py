63a6cb489347325c84803af52ca284392b3fc52c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:46 +0000

    illness night sad makeinto delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def subtract_numbers(x, y):
+        return x - y
