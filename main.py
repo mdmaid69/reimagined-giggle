d39119becce42e85340c5d2b65f5bd5e98350ec7
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:00:50 +0000

    spring on new store still

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
