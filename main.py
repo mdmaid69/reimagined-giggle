1184731bf0886fcca3ba3df5d1f2e9f0ada380d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:37:07 +0000

    cross activity pretend centre rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
