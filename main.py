1ca5e45642f88f393d5d358c7f0be9ae5f9f252d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:12:16 +0000

    clear temperature constantly complain sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def convert_to_octal(n):
+        return oct(n)
+  def square_number(x):
+        return x**2
