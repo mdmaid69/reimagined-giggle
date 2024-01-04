5645d84dc81538a4ca771bf91b6ff7f44db76bc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:52 +0000

    shut far close detail comedown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def reverse_list(lst):
+        return lst[::-1]
