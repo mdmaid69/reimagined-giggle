0055d3b19d67fcbdd2f364d7067cafb6769b3736
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:32:46 +0000

    post skin later show throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
