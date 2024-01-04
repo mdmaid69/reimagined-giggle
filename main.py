65001669a1051b8ea67493e29fefd403bbae7231
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:51:06 +0000

    type disgusting work section protection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
