ca1d6bafd85da645e1a3a74f23a56468f27a8552
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:24:59 +0000

    spread raise guide telephone niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
