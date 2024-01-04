c4845459a7ef4dcb69109da669502497855b020f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:32:13 +0000

    benefit impatient empty correct Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
