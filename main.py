9595afc47c4acb85a827a779515e2fb83b2aab27
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:44:41 +0000

    press further piece stretch rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+def calculate_power(work, time):
+        return work / time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
