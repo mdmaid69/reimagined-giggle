e02d043169438bdc5d8bfbe59b14d2178fbc2351
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:23:45 +0000

    opposite ilovedicks makeinto enthusiastic spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def convert_to_binary(n):
+        return bin(n)
