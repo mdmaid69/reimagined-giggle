9d90ed022bb514a191f761ffbf296eaa2f520f20
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:55:11 +0000

    understand lay special people dress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
