f346f885a1ffed9ef625516308b8d2a87665b2ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:40:20 +0000

    year action squeeze search office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
