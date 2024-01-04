f6e6dc43e548b287d4b04cb88c4d4feff734f430
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:44:29 +0000

    boat over shock rescue minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
