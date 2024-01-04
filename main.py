53685bc67dda87567ffb3dcfee2cd22feb6b1476
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:25 +0000

    shoulder forever play sore best

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
