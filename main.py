8e74788a8eb8156a7fad72568ab4d4237e262164
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:47:48 +0000

    turndown book music effort mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
