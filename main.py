1b72bef1acd9f37be5ae4cfff408d2a0cd82b87d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:19:17 +0000

    method design good steady judge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
