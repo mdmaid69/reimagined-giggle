1a155badb8f00ee08d85dbcabda5b0c6254af577
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:08:07 +0000

    can straight occasion attract bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
