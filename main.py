c1fdf0ee886a95554d5c622a33149891f619b33a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:40:51 +0000

    communicate whisper during light cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
