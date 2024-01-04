ca53e22c490b0beb164f5e6b63eca7c93d39341e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:48:14 +0000

    club form worried protest steal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def reverse_list(lst):
-        return lst[::-1]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
