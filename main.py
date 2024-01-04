b36cd090e9a02cef9e0981b17ff00997426087ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:09:03 +0000

    injury push expect bringup secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
