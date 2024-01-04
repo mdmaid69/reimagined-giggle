7a736197e3d13fb5a30492fb0c4a68cc93421c6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:21:03 +0000

    cut fall Jj wash principle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import sys
+print(sys.version)
