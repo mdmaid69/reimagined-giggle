c960f739f8ae14f6dfe04b233b7bbdd7fdf68d4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:20:44 +0000

    use description prize unless shout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
