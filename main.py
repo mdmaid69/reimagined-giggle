fcc184e2c91aca1f6fedc44c7bf0beb7d7178c6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:50 +0000

    doubt help boil occur number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
