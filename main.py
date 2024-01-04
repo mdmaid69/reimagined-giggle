b33dc29f829a12c81bf265f6dea47f4616eda745
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:53 +0000

    catch battery pass command visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_density(mass, volume):
+        return mass / volume
