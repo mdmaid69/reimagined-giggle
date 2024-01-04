d1065353a4fc6255ca82943e8456c5cdabadcd96
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:48:27 +0000

    bite recently proud on further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
