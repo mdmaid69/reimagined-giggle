9908a2eb94f95a883a7a0caf421477fbd4f66c7e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:22 +0000

    medium related description branch dress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def is_even(n):
-        return n % 2 == 0
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
