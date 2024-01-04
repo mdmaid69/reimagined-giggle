9bdb01f2d5b5e24e1502ec81d22e7273d3234c62
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:08 +0000

    sister lost warn runout hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
