f07e478290ebd479617ec14bd456c7d687ea1dc3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:34:37 +0000

    collection against make style breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
