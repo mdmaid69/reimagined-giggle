05c071611d1f515039dcf77430586915283d0917
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:22 +0000

    part set Pp giveout interview

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
