3b17329a488a0d6f82ef58c38ec0de6eab1aeb20
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:23:56 +0000

    shadow nothing question appearance build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
