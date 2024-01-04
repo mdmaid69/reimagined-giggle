7a31af12311f9981a866cc1972516bf607dbcd4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:36:33 +0000

    delight dress concern loose angry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
