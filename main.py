2e90f440bd9bbc85c27bcfde36958674c8c81b0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:33:31 +0000

    giveout attract variety bit occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
