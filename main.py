98bf0a05ccc44c27cbe9006855287ca29d95b290
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:52:14 +0000

    gradually shall rest next way

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
