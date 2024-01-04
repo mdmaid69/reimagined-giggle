4096f4c9a9bd21e3f9ef271d0339869ee88de918
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:36 +0000

    anywayanyhow appearance ball frame lose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
