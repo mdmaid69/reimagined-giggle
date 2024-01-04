abdad36727cff35204070a2318ee4c0ad0169a93
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:49:38 +0000

    tall amount match safe along

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
