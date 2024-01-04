ea5984035b16b58a2df1eaf010240dd8baa4454b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:11:24 +0000

    start please face cool makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
