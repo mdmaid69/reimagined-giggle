1ca830f7d2eb9e620a974362f7c6b913a07f717a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:36 +0000

    age complain floor regret independent

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
