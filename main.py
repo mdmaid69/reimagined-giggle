a2c41bd1a8706d630f5be483822df672a9404873
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:48:22 +0000

    doubt unlikely conduct country opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
