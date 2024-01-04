2eb5767dd4a8e3b4bf51f7eb599fdcb8e45c1e97
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:44 +0000

    bend her contrast afterwards everything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def is_palindrome(s):
+        return s == s[::-1]
