a5b85855f42ac336077c58e12c2bd067491e5159
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:05 +0000

    sure attractive this scrape hold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
