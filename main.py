6a604f94b5b3336890fc15f8ab1159971d10fb91
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:25:46 +0000

    telephone powerful swallow mine mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
