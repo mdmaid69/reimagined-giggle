cb44da1283fd7fd435fd8aa5a6a58b8300fe3fb0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:19 +0000

    gradually set behaviour worry expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def add_numbers(x, y):
+        return x + y
