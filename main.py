30db57f360e76f6f7800c99cc2296f8429a4369c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:36:47 +0000

    well discuss attractive reasonable back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import time
-def get_current_time():
-        return time.ctime()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
