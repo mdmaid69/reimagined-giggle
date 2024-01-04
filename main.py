763f8d99750fcb57c00cce50f1611821b9b33a5c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:13 +0000

    week price slide trade nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
