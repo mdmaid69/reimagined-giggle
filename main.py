7339b6405df34420af19ac2fc0bc887484680db9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:43:04 +0000

    heavy show sure rush organize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
