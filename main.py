dd117a2a278a2c7f285a8b7cee64c1277e73915a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:00 +0000

    cruelty public law stop subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
