7e165887190b6fb0a209a4fcab85a4031d4279d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:54 +0000

    shop tax matter shut control

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import sys
+def print_python_version():
+        return sys.version
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
