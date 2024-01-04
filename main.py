3e8bc9b689a1bd94f45bb09e1c6765e9f03046f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:38:13 +0000

    insist or seat worry wash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
