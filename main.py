cae8b3a7db026ac21072a6a59ccdb30c98cbee69
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:08:25 +0000

    tune put act burst intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
