144d3646f73524dcc84664dd020ff4f864e10762
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:37:54 +0000

    voice office inside listen precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
