480e253cd5c666aa2c7775966a32100501cdef5a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:50 +0000

    second recovery free ignore hold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
