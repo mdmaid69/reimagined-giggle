bba08be615105879c04a9d5b8632bead6ff37cb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:55:30 +0000

    acceptable better round choose onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
