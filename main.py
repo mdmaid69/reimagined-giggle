464669ebbe78a850b06c3479b48eead7f78642f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:05:56 +0000

    view 568energy favour holdout prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def count_elements(lst):
-        return len(lst)
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
