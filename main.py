0a22ac873e98cd2578debede02cff47949fcaa63
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:10:46 +0000

    react makeinto experiment murder transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+print(math.pi)
