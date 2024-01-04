6078ab8f98d8e70949c71785d537dc69f253660d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:24:20 +0000

    steady contact fit abroad train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
