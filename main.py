889981c746439ec6bb7e4cd4b57f6c15dcfba090
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:55:44 +0000

    off hard chat boil rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_tangent(x):
+        return math.tan(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
