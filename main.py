e43d62cf275cd377017734d2779483465f473d44
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:46:48 +0000

    zero tough back patient aim

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
