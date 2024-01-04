61fa1cdd00ca09566a9a2be0d055aa87b9b2e66f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:55:35 +0000

    steady nigga trouble motion land

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_density(mass, volume):
+        return mass / volume
