c88913026e53691ac634559d5f166cb1917ee9fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:07:13 +0000

    anything business seem record book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
