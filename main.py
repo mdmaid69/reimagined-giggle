c32bd81d8b7887bab1214af98071f76aa4c7eb00
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:10:56 +0000

    less concentration man limit steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
