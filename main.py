d46a107557d273ac4befc61e4b739852661d595e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:59:21 +0000

    keen original somebody unite greeting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
