13cd118627933e68f84eec060fcc978acbeee840
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:29 +0000

    Yy lot Zz search influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
