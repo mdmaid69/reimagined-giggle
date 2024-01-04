87476cd87ce2a3bc63d8c4f4e8aaa482a08bccd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:02:55 +0000

    fast contrast wind journey paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
