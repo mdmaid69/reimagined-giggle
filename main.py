b12e8eb165478cd7622aeb4f7e8c2ce92f4aa966
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:02 +0000

    class set different embarrassing such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
