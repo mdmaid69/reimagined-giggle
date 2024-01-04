f00107b2dcb1963b6814417099232928bf886fe9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:50 +0000

    habit Oo goup exciting solve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
