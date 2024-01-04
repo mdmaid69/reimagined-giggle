b59775456678f73274eed778648cba10c1d4f096
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:58:57 +0000

    female greeting read arrangement adjective

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
