a13ae20e4c5da3c4dbcd524fa307337aa34b81cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:28 +0000

    bored disagree protest better odd

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  def add_numbers(x, y):
-        return x + y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
