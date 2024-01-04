d45f0a89fb73c369f09e25fe49f7b6d0b3e0997e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:29 +0000

    trap lead perfect sure well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
