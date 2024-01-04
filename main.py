6340a2ec4c99f7081c80a233a93083989e0cd75d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:07:03 +0000

    create before agree comeback factory

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+print(sys.version)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
