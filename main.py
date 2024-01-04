3b0158e82f8fcccbf91cff23ffa9f8cdd642246c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:16:51 +0000

    nothing beside burst pure anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
