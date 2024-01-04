db377f3f390198c2f40668c5450a64807cf0ac13
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:55 +0000

    spot bright return deep love

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
