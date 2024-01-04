59455465be480905f7292cae4a388c5e6e3f02ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:24:59 +0000

    effort defeat indeed ring long

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+for i in range(5):
+        print(i)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
