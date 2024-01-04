ab23675b151d6551198dcc5f45ce0dca6e7f0944
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:14 +0000

    minority whistle whole rob shoot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
+for i in range(5):
+        print(i)
