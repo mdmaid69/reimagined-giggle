fd5b198c867aafbf0330a9c9b2cc075e5a205d5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:00:19 +0000

    comeout confuse commit could post

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
