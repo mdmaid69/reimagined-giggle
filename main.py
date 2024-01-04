64e8861039022dfa7d8d7a4ebe6b8ba99a484212
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:28:40 +0000

    normal trial whichever dress memory

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
