7b96d891f311d19df040d1bbaa2f8875532cc1f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:39 +0000

    recovery least within 569enjoy protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def find_min(lst):
+        return min(lst)
