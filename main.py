3fa6df0234207ef30b9b0d1fcb235e2e25dc5a44
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:39 +0000

    pinch her hot perfect minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
