3bdc3c5c3619b097748af36a83b5adac66783bef
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:31:03 +0000

    stamp upstairs undo harm nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
