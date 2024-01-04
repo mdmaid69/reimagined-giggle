e910d74460d13940f96c8d08fcc1e6b56e731dbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:47 +0000

    cruel book unless key ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
