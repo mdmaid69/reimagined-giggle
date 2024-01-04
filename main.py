89392d1f3dc1ead18076a45141bfe5d8097077ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:25 +0000

    Mm taste skill seat certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
