fb3544010efcc6c6bd0b6c00e319e49386ebab05
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:52:32 +0000

    follow bored branch best chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
