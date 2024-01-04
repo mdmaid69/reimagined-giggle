111f5ed2ac7b68c2fd52e5e3d21abd872b350892
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:40:01 +0000

    time meeting base light together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_length(array):
-        return len(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+for i in range(5):
+        print(i)
