5ad67d62a3e280b858d35c45e00cfb5d96e78e6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:35:18 +0000

    bringup section fun false money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import tensorflow as tf
+print(tf.__version__)
