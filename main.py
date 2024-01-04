bdbd3c5cd4fce0b1961b74f2aaea5886b8861f52
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:14:38 +0000

    debt bye change pipe rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
