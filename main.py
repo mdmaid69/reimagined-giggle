5fc19a0e7859d2b93870190b807f27d2db2415a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:06 +0000

    basic keepaway blue fashion shop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
