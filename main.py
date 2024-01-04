78d214df1a2dcf8de1a9340e99a17447d513fac9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:09:31 +0000

    doctor combination programme either goaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def add_numbers(x, y):
-        return x + y
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
