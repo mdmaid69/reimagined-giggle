bbbe350a30b08dc56b712e2ad641c72a6ca4c088
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:52:27 +0000

    oil leg hollow callback recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
