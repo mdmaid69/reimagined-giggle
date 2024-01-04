16d4800593698732ca6dd4dda2548d4603440b2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:54:53 +0000

    shopping gate sick down moon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
