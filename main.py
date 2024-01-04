4a99f63fa57d5718676caf45497d6a77ccdf329d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:28:34 +0000

    satisfied thank earth better recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
