74bdbf2c4b88cc8c301575dd9f6f73aa6fbe1adc
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:31 +0000

    their already careless hurt thick

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(lst):
-        return max(lst)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
