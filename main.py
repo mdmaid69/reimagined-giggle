765e1078947e85d97a6ca604affa1538c0defb17
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:35:28 +0000

    defeat murder between trade trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
