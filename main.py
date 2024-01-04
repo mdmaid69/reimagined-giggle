8c07570732049a2195a370aa223038ab8eff41e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:11:53 +0000

    mixture breakfast idea destroy cheat

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def get_array_length(array):
-        return len(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def get_string_from_array(array):
+        return array.tobytes()
