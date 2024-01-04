412d93220167be30a6efc97a57fbc09bbca12d8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:59:35 +0000

    seriously stick leg now exploration

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def cube_number(x):
+        return x**3
