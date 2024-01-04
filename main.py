98db76f1fbc4152b59734985cd500792d8bf9b91
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:19:50 +0000

    order weather goodbye slowly nurse

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
