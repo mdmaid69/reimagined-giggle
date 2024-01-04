ee61daf7b8d958bfcc3631fba8945255da1a534d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:40:52 +0000

    message interruption fear feel share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_base_name(path):
+        return os.path.basename(path)
