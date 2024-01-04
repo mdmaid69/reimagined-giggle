a7ecb1fad947987427873394c45590e4e598f053
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:41:13 +0000

    value wherever gap wing laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        print(sys.version)
+def is_odd(n):
+        return n % 2 != 0
