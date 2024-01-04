088b5e2d21cb392080fcca9d593055f245e7055e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:12:45 +0000

    style tip happy learn take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def is_even(n):
+        return n % 2 == 0
