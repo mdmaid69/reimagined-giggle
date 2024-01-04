678a4ac1a1d7cb394f70291b70975df5f4b33150
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:17:54 +0000

    police university strike tough platform

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def is_even(n):
-        return n % 2 == 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_binary(n):
+        return bin(n)
