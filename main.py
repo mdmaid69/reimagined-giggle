87eb15eab6ce2208869f2b8e758b4aef4c1da81c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:10:38 +0000

    afterwards male thank half imagine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
