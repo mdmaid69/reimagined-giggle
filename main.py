62e22117d3974f4db1c416b89986a810a89ad8d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:25 +0000

    not drawing angry muscle brush

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import collections
+def create_user_list():
+        return collections.UserList()
