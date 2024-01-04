8f3eed8cea7b10b8c3378372bd03e2284bca1807
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:46:34 +0000

    tape actually mind mouth mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
