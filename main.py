62bf56b3a8c3b2919e3ff905ada67e612b7addab
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:38:02 +0000

    smell there design active unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
