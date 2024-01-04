38e1eb4e36acc0e2597128dda2e5b7ab08718561
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:01:18 +0000

    service around scene together possibility

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
