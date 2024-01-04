a6e8ef6e155743d529a8ca8623e093fb636b1563
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:13:58 +0000

    area lead subject neither appeal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
