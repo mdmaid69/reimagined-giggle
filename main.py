fbf62688df073f5db6cebc516e1fcf0e33222367
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:49:44 +0000

    shop become come easy number

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-print(math.pi)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
