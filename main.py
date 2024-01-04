fa3a917489bdbc42bc3bf3847db30193633199d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:37:14 +0000

    floor brief drop actually appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
