06c6a75759bb9fc41b42eea99fc923a370dc66cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:13:44 +0000

    quality feature worse scare represent

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+for i in range(10): print(i)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
