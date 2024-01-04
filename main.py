f8827607ea04c8b9188062938efe66a1bbd428ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:07:56 +0000

    appeal base tape quantity underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
