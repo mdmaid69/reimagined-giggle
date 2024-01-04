67eda2b75588f282976c61b44e3d187199a01579
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:52:36 +0000

    exist seat impressed contrast tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
