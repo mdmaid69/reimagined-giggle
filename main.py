7eee9cbfd473e85cb41c7fa78c34238c759ae32a
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:08:49 +0000

    tap grade deliver learn immediately

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+print(json.dumps({"name": "John", "age": 30}))
