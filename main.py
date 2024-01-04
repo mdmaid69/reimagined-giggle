eb886047e4ae8ebedb2b07292f72510317a401ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:57 +0000

    once this drawing 566end paper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
