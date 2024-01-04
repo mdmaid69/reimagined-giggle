c782f4ca35e0f70c88f214e835840eced0cbb12e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:30:38 +0000

    silent left cheek praise confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
