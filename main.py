49e0822b5ca8cd972fa515e06f08fcb1281fec49
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:15:52 +0000

    equipment worth at arrive Gg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-print(sum(range(10)))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
