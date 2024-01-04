6e4f09c112766ea8e056fba74dc98ee07b9d0981
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:12:28 +0000

    match book possibility market smell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
