591d29f95d37e10b571b0f03d080f962f10a02c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:49:12 +0000

    sister speech straight reaction view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
