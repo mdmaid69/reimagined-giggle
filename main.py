97983a698f747c5b32f9a3c741f96f2784d2f842
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:36:23 +0000

    satisfactory life refer print consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
