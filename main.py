5b2bddddbdbdf1f5536cea9ed4d72991ec4dfb30
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:41:50 +0000

    discuss destroy forward quit murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
