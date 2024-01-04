1569eb6baf7a2c5c59b325227d38db28017130c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:17 +0000

    consult taste trip my experience

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
