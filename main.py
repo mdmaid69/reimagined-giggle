6c7c2dfc219c8e8287cb51fcf53390479903be82
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:37 +0000

    suppose against die turnback embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
