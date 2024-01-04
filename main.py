acc8f1a080185a8c89b5b92099a59fbd53c6bddf
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:07 +0000

    busy holdup popular now actually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def find_max(numbers):
+        return max(numbers)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
