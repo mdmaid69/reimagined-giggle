7190f351505ddf063d82e92a633e191c55659218
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:04 +0000

    solve unable rush Ii target

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+def convert_to_binary(n):
+        return bin(n)
