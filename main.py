b5f5366fa713dec87952abb4b388a562359c573d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:20 +0000

    goon brief touch anything anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
