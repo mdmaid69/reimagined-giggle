359c03cf2637edf9f8e06bcbad76bc704fb36966
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:23:44 +0000

    plenty glass threaten subject play

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
