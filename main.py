57507abbd06b8e0c6984386e446fd83ed6c29213
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:11 +0000

    most bag grow familiar love

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
