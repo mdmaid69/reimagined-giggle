458fc5e4d9c10142362d2680d330d3e39f084a5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:10 +0000

    hospital left fortunate plenty stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
