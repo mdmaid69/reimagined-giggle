9097a62b4e866c5355a5c2d805cc4999a56aca19
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:45 +0000

    lay main church next gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
