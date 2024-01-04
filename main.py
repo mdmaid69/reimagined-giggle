7173650aadddad352af98d48ba1cbf6bf300c8ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:13:53 +0000

    undo please bank kind second

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
