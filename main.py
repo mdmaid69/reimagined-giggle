cbafc2278b5793bb53e653782905ef5d8d173ab9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:50:07 +0000

    answer aim edge exciting study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
