cc329c6d095258f9cfc6dc146875fd0a6e0a9d27
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:28:52 +0000

    similar into public keepout trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
