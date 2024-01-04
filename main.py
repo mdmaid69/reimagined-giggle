bdcf6e78afb0858a8e91a4f37f107cee531b33ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:48:48 +0000

    realize nearly room uponon comment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
