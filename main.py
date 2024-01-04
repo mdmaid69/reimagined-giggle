f068377ecd8d314d3c8ca3186e3fd2feeb9be5f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:05:23 +0000

    wave behind active yet top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
