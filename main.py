3a39d8272b21bc601c708ed0d01462b008cda800
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:33:38 +0000

    moment rarely funny print allow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_username():
-        return getpass.getuser()
+def add_numbers(a, b):
+        return a + b
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
