f20d1733eb114f29479439d85e57a9bebbf271fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:09 +0000

    clothes none complain used broken

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
