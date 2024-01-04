855c5a8dd11f11f14c4cbf48288fb7c8c04b8901
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:51:31 +0000

    complaint meanwhile fear cause speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
