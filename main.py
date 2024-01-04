86a12d9bc73b4b10d59f523e7d3478575b0094ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:54:32 +0000

    usual change fit achievement please

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_area_rectangle(l, w):
+        return l * w
