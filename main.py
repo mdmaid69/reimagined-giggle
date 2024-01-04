bf0a4e178cf5d96f6d32e79f481b361b7954d4d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:47 +0000

    pretty steep again convenient practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def sort_list(lst):
+        return sorted(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
