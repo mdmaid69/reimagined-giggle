be0d3e86b219cc68ad9a600ba769d921f447fba2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:24:00 +0000

    drop discuss remember write begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
