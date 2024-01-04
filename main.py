7b4137f08dc0a6baed0079c5e09bc02cfee8186d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:00 +0000

    leave account advice especially major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
