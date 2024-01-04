6c5360895dc33e474d1e15a1f99e58569b5218b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:53:58 +0000

    practical instruction development regularly nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import random
+print(random.randint(0, 100))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
