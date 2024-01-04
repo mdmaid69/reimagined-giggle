4973209f21544c95f077a580998b54ce91a7c5cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:29 +0000

    neither film sister lookback(on) tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
