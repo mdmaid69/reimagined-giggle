80bdfce2c92bf78174f8764ebcef5a3972dfc2ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:12:02 +0000

    whisper nurse might union he

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
