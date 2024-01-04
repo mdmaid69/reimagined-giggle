3e81be4652a780885c30da0470d198142dbb8cfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:01 +0000

    union discovery importance under hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
