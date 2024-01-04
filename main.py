d61804c45b1af6bee43533b617437cc625c3b71d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:02:06 +0000

    distance compete if crash division

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
