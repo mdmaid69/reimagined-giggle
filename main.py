a1091c407ccb3eb923296c3f804ce1310d82a2c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:19 +0000

    may might collection owe increase

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_density(mass, volume):
+        return mass / volume
+def square_number(x):
+        return x**2
