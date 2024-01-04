90870ac6b835660c90d142004ae42808449dc4e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:49 +0000

    border face no usual react

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_area(radius):
+        return 3.14 * radius * radius
