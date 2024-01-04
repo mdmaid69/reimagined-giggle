92460b774cbc50df1375888abb600c795588f233
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:27:13 +0000

    warn hand change introduction needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
