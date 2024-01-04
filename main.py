cf085be880f4b1c0a11f19e27490f5e75c89fd4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:37:03 +0000

    settle physical clear Zz settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
