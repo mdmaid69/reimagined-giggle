466c120a80891576bef4adc4ae0b93e9f25e0c79
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:24 +0000

    available hold connected summer division

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
