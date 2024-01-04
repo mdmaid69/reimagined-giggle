a135bc3fd62fb78753cda07d2728a1c86d9aeaad
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:52:36 +0000

    length closed sure remove prepare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
