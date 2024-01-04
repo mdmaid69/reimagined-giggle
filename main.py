a6c08c0e56fe6b0f2f6044f7a75a0e8cf67f47f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:13:17 +0000

    tax plenty difference poison extremely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
