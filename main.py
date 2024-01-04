cd1948930d81cb4f2470559b64a6441db8f6d2fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:04 +0000

    frame right keepup cool along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import time
-print(time.time())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
