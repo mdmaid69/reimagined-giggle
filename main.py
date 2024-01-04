507e7cd1eb145812af8fdb76e86b35ef8252c1f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:10:02 +0000

    aim explanation pump must convinced

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
