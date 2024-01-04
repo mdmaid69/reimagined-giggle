653258bea0db43bc83352ab2a41570515f5c88a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:17:56 +0000

    doctor turnout allow shoulder surprise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def convert_to_binary(n):
-        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
