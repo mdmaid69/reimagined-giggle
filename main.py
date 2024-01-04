696bce2ef9bf1333f3316153de753988dcf164d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:03 +0000

    group exciting dinner war danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
