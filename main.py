7eec212d5bd4245f0cbf0c9403446e10c02bd312
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:31:18 +0000

    sort point image crazy winter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-print(os.getcwd())
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
