aab952e67b09d0dd8c6ceb711aebd2b831cc47ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:59:07 +0000

    suit sensitive sometimes beside earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
