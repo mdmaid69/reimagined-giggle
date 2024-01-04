9f739f1324653aa1a8f057a4ff53ad75b4120bc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:29 +0000

    winter border cost identify tough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_volume(length, width, height):
+        return length * width * height
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
