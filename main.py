f7a84912a7a67f299255bee71e8bf0ff42dca779
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:55:33 +0000

    science suddenly inside sea platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
