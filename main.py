913aa25ae481c6c353f90575a873f979c63b5bf9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:20:37 +0000

    disturbing common 568energy usually narrow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
