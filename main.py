bb0750efdf219313640449a7613e1b1d83b5bd96
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:39:16 +0000

    nothing difficult pin probably height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
