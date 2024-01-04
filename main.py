6605ab90000e6c833c9a340582940f46942b07c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:17:13 +0000

    file turnaway turnover past receive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_bool(array):
+        return bool(array)
