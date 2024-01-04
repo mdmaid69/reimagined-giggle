ece415c1e880d4125aa585a4733a9e53e30c19a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:24:07 +0000

    inquire low off speed surprising

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
