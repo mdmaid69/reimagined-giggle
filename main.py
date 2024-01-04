fd900523cb6358ff8ff1cb46fd98cd3f77e00178
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:11 +0000

    straight somewhere drop some talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
