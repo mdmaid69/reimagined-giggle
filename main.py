8f82ee91507975f32531bbba0b356eab999f145f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:33:47 +0000

    willing system pretty afterwards last

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def reverse_array(array):
-        array.reverse()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def create_counter():
+        return collections.Counter()
