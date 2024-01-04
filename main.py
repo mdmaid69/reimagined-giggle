bb56e86bee2a1204d6685639221e23ffbdb6b287
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:58:07 +0000

    trap criticize about reply related

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def reverse_string(s):
+        return s[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
