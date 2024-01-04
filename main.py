546128ec267251706176be57b1369b8dae02eae2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:12 +0000

    perfect pretend shop lose off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import platform
+def get_os_info():
+        return platform.uname()
