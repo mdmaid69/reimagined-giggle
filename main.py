6bad4702bf1cf12ac8825837e6aa29393a9e307e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:57:59 +0000

    powerful shopping declaration slight taste

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
