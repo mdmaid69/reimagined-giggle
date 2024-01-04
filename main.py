5566be67b16846d3e576e6ff8762ac30b07185b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:29:08 +0000

    character light steep sticky strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
