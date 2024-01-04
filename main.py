47e98674d5a499bc4db2461781310d153ebfe811
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:30:35 +0000

    hole mood violence effect part

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def count_elements(lst):
+        return len(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
