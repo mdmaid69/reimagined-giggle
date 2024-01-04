a9af31ab1a91ff0368e99e9b0faa64df9fc0f8ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:32:51 +0000

    emergency store use contact back

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
