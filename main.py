0f1f232dc19f74bb4c4210975d5bf165e38ea4bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:51:54 +0000

    fault one land comment valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
