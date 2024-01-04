dec5657dcbc40dd7083a79cdb1636310b40066e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:28:28 +0000

    unkind interfere goback(to) shout dear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
