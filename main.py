eb3d35ca72deef916f98435367ca43c107a230de
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:45:06 +0000

    compare brother comeoff 179bite single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
