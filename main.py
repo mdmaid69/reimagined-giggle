5d883b216f86aaf4ed9ca57484e5c3c8d56eddad
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:00 +0000

    system getoff separate split compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import time
+print(time.time())
+import array
+def get_list_from_array(array):
+        return array.tolist()
