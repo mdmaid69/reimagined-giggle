4dc6d3aaa9fbef01708bab929713cb4ed42afa7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:26 +0000

    thirsty figure phone apply memory

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
