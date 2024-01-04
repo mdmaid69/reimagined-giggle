de814b9ce117576c00e0596fee90c44d16bbcdfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:21:43 +0000

    Zz autumn speak loose comeup

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+print(sum(range(10)))
