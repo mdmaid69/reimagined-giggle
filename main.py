3404316bbd4a7f6e6d02ed0630190c1c092c8f10
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:38:32 +0000

    whether unpleasant solve clean pipe

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
