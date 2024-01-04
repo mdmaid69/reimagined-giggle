d775d53f81b22ae91515a268c515f4b5d53412c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:36 +0000

    most runout message money disappointed

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
