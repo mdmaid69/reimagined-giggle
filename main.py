ae29598199dfa5371d0b7c0f6b4886e47f5f432b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:37 +0000

    apart by but argue for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
