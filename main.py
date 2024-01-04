d6f6b478dbcec2d5a0a41d6aaceac6c5f1134fcf
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:51 +0000

    uncomfortable rain charge goodbye watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
