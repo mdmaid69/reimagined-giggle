8e039727a7158a6875c29792a9907022af3d3a9c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:59:05 +0000

    warn later warning big section

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
