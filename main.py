f9f7ace912f854b0fab76fd40bbf8c0ed18a1e0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:21 +0000

    ability immediate quickly thin maximum

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
