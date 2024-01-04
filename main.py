ec028fbed410dc44cfbaf6994be110f3fc6d956a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:00 +0000

    create fault lean 566end finish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
