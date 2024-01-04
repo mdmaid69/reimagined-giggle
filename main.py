01d892de89d9b7a08b9b7874bd7b38fa913281a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:06 +0000

    care pretty they praise thing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def square_number(x):
+        return x**2
