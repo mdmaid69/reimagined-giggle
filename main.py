eb6caa1cbb0537c80e9cbcda584e1e4faaf7f255
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:11 +0000

    problem roof rest possibility polite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
