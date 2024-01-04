af0d3f8fe861e137779641c9c44d420e1f2548c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:00 +0000

    bother respect peaceful it patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
