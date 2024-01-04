3be978935c2862cdcdefb16db92f78d642e7df96
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:17:20 +0000

    ought city order wake talkinto

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def convert_to_octal(n):
+        return oct(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
