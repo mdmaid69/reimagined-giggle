52ecb72bff9e6e2c79ce079a9d5a391473d95901
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:44:05 +0000

    example convenient crash minor consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+text = "Hello, world!"
+print("Characters:", len(text))
