9c244e8f5adad67e4b4005eb833369d9e965a457
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:40:26 +0000

    fast remember set leave comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
