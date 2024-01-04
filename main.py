75d2e69e79c99646f0d5e67422a2247aafbc0368
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:19:07 +0000

    copy clearly lookaround complete when

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def divide_numbers(x, y):
+        return x / y
