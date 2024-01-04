1c0eb663045a60e3c37a20910e4abe0c50131c6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:49:14 +0000

    nothing holdup better object word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
