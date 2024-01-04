0f080794087050f694d535b529a5d8eea6c8b370
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:10 +0000

    face sting project basic suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+print(datetime.datetime.now())
