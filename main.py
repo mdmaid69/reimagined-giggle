2b48ad3d92d70481d0a0ac0badc20f821862d326
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:32:24 +0000

    insist help pump zero callup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+print(json.dumps({"name": "John", "age": 30}))
