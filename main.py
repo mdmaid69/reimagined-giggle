1cd585cf187ac62e0fa44cf5b714fbc78151e3a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:25:38 +0000

    person touch rob point be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
