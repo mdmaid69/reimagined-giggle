f21d58c4581672f2bdc5dec31f83a373ea8da1ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:07:00 +0000

    our crowd already front pour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
