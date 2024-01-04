1678a3a3b3e696b3631d21e148b0f4e090fb8fd4
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:34 +0000

    quickly remark key giveback yet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
