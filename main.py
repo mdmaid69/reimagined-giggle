287028c23fe0efd2c855b60ae5f65b442a3f8cb6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:07 +0000

    describe proof shout attention cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
