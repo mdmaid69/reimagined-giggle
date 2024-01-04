8c0cf5859eb97ffcfcc332f2577c030ab1551654
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:23:24 +0000

    characteristic persuade slightly suit under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_str(array):
+        return str(array)
