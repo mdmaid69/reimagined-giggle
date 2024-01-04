b1fa842ca7bf099786dd36777270ad32f4776304
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:51 +0000

    head remain tone method copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
