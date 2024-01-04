794456e13e1b2359d47bbf2ecd75494af9d282fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:39:47 +0000

    bar themselves again add wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
