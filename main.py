ef9167604418c2ce6b6dd985aa1b2c8229f8025e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:05 +0000

    object rescue please party reserve

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def subtract_numbers(x, y):
-        return x - y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
