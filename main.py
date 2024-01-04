78763f6d0d067246ced00fc7a690691b08cf99e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:10:11 +0000

    instruction steep pleased bar mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
