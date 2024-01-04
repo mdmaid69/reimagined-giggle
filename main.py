a29a3aa56204c5c1e38d703fd8946a47cade9741
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:40:35 +0000

    actually morning duty figure receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_bool(array):
+        return bool(array)
