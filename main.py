787e0bbf2eff6a7472d065817ef282dc3eb222b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:10:05 +0000

    wander male interesting closely national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
