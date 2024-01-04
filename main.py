8fa6ab7169a375dbded1a9cf4650b3efb835561f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:49:08 +0000

    deep bound Kk disappointed petrol

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_current_working_directory():
+        return os.getcwd()
