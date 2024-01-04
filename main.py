2024bbb2113e669efc0609f969b3da56f453df2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:10 +0000

    throughout shadow run tape best

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import random
-print(random.randint(0, 100))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
