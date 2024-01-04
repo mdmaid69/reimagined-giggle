8498dc04d276a9aca08cf663dca39cf854485cbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:54:16 +0000

    close should public owe though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
