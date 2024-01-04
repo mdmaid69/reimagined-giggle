601db1ca8e99604cdd1aa9ba7a43d97cbffcaf29
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:36:16 +0000

    drop earth see book black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import tensorflow as tf
+print(tf.__version__)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
