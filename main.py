a3d8d2e1e26c6d92becaf68fae768c566d48ce38
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:52:11 +0000

    cost grateful middle search accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
