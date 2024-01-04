2c247ed099a678db1c984ae4f896be2a5babf166
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:56:35 +0000

    afraid under already few hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
