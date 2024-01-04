0ff9650d02914602820aa5c8133cf01c8ef2bacb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:51 +0000

    week kind instead total pollution

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+print(sys.version)
