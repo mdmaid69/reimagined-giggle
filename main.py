5ab570cb01b84eeea662ef01ce5f5a91f4fa3615
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:29:33 +0000

    soft Ww used crisis club

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_pressure(force, area):
+        return force / area
