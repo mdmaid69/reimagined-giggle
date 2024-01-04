0a3eef5ae9af75337b07a1e61e769cd12729d40a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:51 +0000

    be deal before shape crazy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print([x**2 for x in range(10)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
