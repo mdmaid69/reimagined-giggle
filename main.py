2cdd7cb1bacda2c08e512ce777d9b0df976a6893
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:16:28 +0000

    absolutely cross section importance male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
