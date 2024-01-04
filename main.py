fc53cdd05f91207dcd6e3a31ca1c493f01404531
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:50:52 +0000

    again steam travel workon float

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def find_max(numbers):
+        return max(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
