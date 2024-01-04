4b632d478d74fc4469e54c8a0c9df79d6d485d06
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:17:21 +0000

    swing confused love lost after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
