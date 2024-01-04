593d45b51b3f325f2038f8c3efa085a9e01376f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:10:56 +0000

    drawing comeoff mainly build all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
