d60f1960b9bdf941a0667faeefd158607fd94582
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:45:37 +0000

    serve choice tax tap charge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def is_even(n):
+        return n % 2 == 0
