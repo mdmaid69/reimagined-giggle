a80483f2ae91ba435d27d19ba8e578b978103f4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:05 +0000

    team face recommend boring every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
