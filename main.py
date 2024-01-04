f7d8959f8198b7ff0a74bd423f1f26edf96fa882
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:39:26 +0000

    trap run any odd roll

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def get_array_item(array, i):
+        return array[i]
