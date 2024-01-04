25e17d21dc4339317116da4a79a40bf0b46fb518
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:28:21 +0000

    stone study short chat difficult

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
