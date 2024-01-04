9f50cf52243300cc918324bf4b1b7f41b4f751b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:19:10 +0000

    road telephone chase comeround floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def square_number(x):
+        return x**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
