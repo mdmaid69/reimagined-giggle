dcdbbda378c30d10113050b04ccb31d96ea7db83
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:22 +0000

    Ff this tradition friendly I

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def square_number(x):
+        return x**2
