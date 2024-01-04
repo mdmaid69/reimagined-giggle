db70de316d3b2fefb2b3944483671dbb257d1d2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:14:25 +0000

    sorry convinced below please important

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
