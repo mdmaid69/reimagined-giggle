7b8c40329cdd45c0b6ae951bd2793adb30aff204
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:53:23 +0000

    national win Tt matter whenever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
