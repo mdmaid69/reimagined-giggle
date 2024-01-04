fd209ac804deb3c9b5ad0ecc9b1ddb7cdde071fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:28:07 +0000

    Nn runinto comeoff willing rain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def sort_list(lst):
-        return sorted(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
