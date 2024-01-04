6910edca0fb96d80fe255f13bc30db27379271ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:16 +0000

    feature flood practice our live

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_acceleration(speed, time):
+        return speed / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
