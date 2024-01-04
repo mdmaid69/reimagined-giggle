849a422b56a66f6285dba283acc55daba43a9f3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:39 +0000

    more delight survive badly getup

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from collections import Counter
-print(Counter("hello world"))
-import sys
-def exit_program():
-        sys.exit()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
