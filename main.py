6af44de514d1713f75fdeb84b76aca92173a56f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:38 +0000

    broken could name branch custom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
