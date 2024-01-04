d23f3b3def05bd6346c72abe2aeb5cbc493b0ddc
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:23:31 +0000

    fold add pay second match

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
