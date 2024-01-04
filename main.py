59bf1332619a4fe8873794027ab6100d7f700257
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:59:57 +0000

    curious relief production formal lock

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
