e5f42535e41fc6ff0d2835092d7a1e70bee82d3d
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:28:33 +0000

    disturb giveup able Ww modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
