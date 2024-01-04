e0bfae8877499840abdd92428a137102aad83b2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:40:51 +0000

    set mess share finger through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
