a12fab7ca5fa2cd4cd385218ceba4f867dab452c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:37:22 +0000

    clearly however official several really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
