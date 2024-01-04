8aec86fa969c8b5577e69595fcc70bf631133ed7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:42:58 +0000

    mass last bringback system everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
