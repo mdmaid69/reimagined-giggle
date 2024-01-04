f9356f625bf65b1baa7fe46ed095f97cf67d386a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:21:20 +0000

    muscle attitude famous trust interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def list_files_in_directory(path):
+        return os.listdir(path)
