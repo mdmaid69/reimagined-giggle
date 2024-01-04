8284332ce456a4f324fc248e58845cef60a43990
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:46:17 +0000

    job interfere plastic strong lot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
