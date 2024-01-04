00fbf660f4e48c64852448636561d8ef85f2e331
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:52:27 +0000

    total escape display failure 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
