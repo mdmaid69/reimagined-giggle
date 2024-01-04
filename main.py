df1c754f4589209780d1cdea589367cf607e0ec0
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:29:10 +0000

    instant both success final file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
