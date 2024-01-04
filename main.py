db060acbfb9c79c4d4f4214c7b082e2ff8821b1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:59 +0000

    Zz content event bringup man

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_as_str(array):
+        return str(array)
