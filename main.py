cd256e94d6940849e09186c0eb8185bc82aa6ef4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:47:31 +0000

    classroom into first let surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
