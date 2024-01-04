fe4ac1f7afc45ec140c4374775c721001c674170
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:10:37 +0000

    doubt ill discussion swallow congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+for i in range(10): print(i)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
