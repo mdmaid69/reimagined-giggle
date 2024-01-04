dcc4e3fbe1bb7933241cf99da87fe7008d941485
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:55 +0000

    apparently sad snow swim stone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
