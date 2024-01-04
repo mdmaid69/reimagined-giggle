2b70086d8512a288fde15ed74c584812cfb93ccb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:19 +0000

    spot large reduce strike artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
