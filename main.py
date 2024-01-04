ec837ae90d24ba3e05976fcb631295ed1235dd51
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:14:18 +0000

    hang little goout piece design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
