52290678591f481481d79c9949f25c00b245c469
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:38 +0000

    brother copy middle achieve unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+name = "Python"
+print("Hello,", name)
