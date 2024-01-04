21fdccc3729720eab26be0a727fb06099ebddf39
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:02 +0000

    flavour calloff rise generous small

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
