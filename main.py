c22669bec70cd5ebe4f48f53a7b8ca1aa63ef5c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:31:17 +0000

    command drive enthusiastic easy birth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
