f5e8c3a8c3df20fd84f16717f24bbe95dd2e1987
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:22 +0000

    somewhere afterwards set upstairs achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
