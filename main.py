d7d5962e194522277c87f676e6cc1653d3ae3ecc
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:09 +0000

    tone design related represent fairly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-print([x**2 for x in range(10)])
+def set_array_item(array, i, item):
+        array[i] = item
