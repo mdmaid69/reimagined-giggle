ca72c2186c363dd5a1984a92db5b7aada44b00f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:27:22 +0000

    surprise thought left our under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
