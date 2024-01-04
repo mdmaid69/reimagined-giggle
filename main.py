5493e224c139e68ab876ab0516be0b8490d31fb0
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:19 +0000

    chase breakthrough period somehow continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
