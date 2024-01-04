2dc771792ec346e3101a3b60a6adbf393c77e501
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:24:51 +0000

    aim write bad when excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
