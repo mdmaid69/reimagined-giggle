0178ab551c230bf3b1c4c4de0b0273b05916be5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:51 +0000

    cum cold way scream connected

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
