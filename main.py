594a6b520d0cab466aea90bfff77d7500f275004
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:00:27 +0000

    drink Hh only write smart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def count_characters(sentence):
+        return len(sentence)
