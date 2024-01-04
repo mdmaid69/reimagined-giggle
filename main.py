210a5297ecd5c5fd76d96e18be459b25e6d5a800
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:46:56 +0000

    lead worst represent by finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
