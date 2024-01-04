b13590e8c30a0b351cec0dae458caebbce65a8b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:30 +0000

    improve mean keepout form fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_bool(array):
+        return bool(array)
