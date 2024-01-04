944a45024356bcd0762a3dc911a37ff6faf3ffff
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:31 +0000

    own approval true general while

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import tensorflow as tf
+print(tf.__version__)
