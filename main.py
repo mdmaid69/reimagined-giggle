c3f01e415697d79b18813e51ac6dffbc45105183
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:01:55 +0000

    impatient right control quickly Tt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
