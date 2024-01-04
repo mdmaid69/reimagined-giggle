49c6ca778558d596133f944fba2bc17122565faa
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:31:02 +0000

    minimum freeze serve personal join

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print(sum(range(10)))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
