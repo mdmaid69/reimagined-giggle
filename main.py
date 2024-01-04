6516e62f63ae49de20bb3d5baec8310817b0b9ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:18 +0000

    trade approve expensive connected sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
