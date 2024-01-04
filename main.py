d219e3ce4a2326128909e624aba672d6853b9f7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:22 +0000

    exact popular touch advice comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def reverse_array(array):
+        array.reverse()
