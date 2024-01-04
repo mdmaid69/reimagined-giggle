19c742ca6ab4613bfdb6669bec6091e542d9357f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:38:40 +0000

    Ii university every live pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import math
-print(math.pi)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
