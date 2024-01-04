a17ba1f31d5e6ed9671a42d3126f1f2b3b94d8d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:40:21 +0000

    culture partner off bear after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
