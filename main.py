4e3d9a4c42867e73d38e869c2d398fe8f6a40646
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:45:42 +0000

    mad fit slice extraordinary speech

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def square_number(x):
+        return x**2
