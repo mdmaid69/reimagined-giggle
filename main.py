bfc4caed7ba8601943ea0ed0d7e3eb626db2ad8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:38:45 +0000

    round square force answer trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
