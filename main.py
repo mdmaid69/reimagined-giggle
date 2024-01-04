741d990d2ba4b582e14eafe1d5ae99a83c69aa05
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:56:24 +0000

    somewhere appear confident opponent determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
