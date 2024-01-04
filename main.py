e9c503f837a891f3f7a0e94ded7522db033efe7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:44:34 +0000

    yet top store callup comeover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
