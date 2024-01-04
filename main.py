ad2f4f38d3308b5b1984fa50be853d02134bea8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:33:55 +0000

    almost fact increase book hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def add_numbers(a, b):
-        return a + b
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
