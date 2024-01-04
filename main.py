4f5be63c3c5715a3f8e1357702a32f0b05a417cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:22:37 +0000

    early callup different goback(to) I

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
