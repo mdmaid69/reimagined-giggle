55cb62be2b40a3def63007256b238ab33dd07f39
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:29:19 +0000

    subject string mine curious tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
