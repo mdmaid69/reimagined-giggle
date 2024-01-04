7317914aa517015a1414658fe6ae229136e841a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:56:45 +0000

    shoulder neither attention top middle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
