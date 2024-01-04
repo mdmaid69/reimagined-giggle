6c819e5b9826785358894484e8f92082c551ed31
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:13:13 +0000

    concerning project luck cover ask

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
