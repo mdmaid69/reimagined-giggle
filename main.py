f7f332f4a52947881b6bbcbe96fd09952706a104
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:28 +0000

    Ll zero all degree must

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
