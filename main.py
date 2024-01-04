4258da14e70f25069f7e6862e519aa945707742c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:54 +0000

    relationship cut wet about suggestion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
