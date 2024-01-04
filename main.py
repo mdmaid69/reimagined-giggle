bd13805da7c0f3cdbad5d510ddd547765da2e9dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:20:09 +0000

    in progress chief keen equal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
