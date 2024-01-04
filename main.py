be1deb4b3cec58ab317797819e92003526051ca3
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:01:00 +0000

    also average skill worth calmly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
