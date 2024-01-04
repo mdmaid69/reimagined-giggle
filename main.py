42fa5932f76de3583e48890ac4b821a53c82242f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:30:37 +0000

    level reliable brain bus would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
