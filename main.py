0f6c9a7127ec326bf4644546ff048606960da887
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:52 +0000

    contribute Zz try mine woman

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print(sum(range(10)))
+import array
+def get_array_length(array):
+        return len(array)
