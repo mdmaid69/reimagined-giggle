2761b93b2bf8d01207340227dad1417afa4d4fd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:47:07 +0000

    every measure equal put song

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
