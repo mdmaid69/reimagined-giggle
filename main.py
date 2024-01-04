031c1b913e5b050de14ab0e3945dfc7798c5f290
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:10:25 +0000

    open war side suck this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
