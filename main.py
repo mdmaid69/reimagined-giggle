5764fd87e6ecd3e0f1b472cb6d9f9342211c202a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:36 +0000

    thinkover never concern split pity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
