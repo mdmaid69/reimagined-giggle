e867168be5ec6c159dac4241f2116e85e39996dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:47 +0000

    past maybe otherwise cost mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+print(os.getcwd())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
