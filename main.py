0a5945cd4a7bfb24e8d75b4bb742a7d5cc24c33d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:54 +0000

    worry detail chance round deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+def get_current_working_directory():
+        return os.getcwd()
