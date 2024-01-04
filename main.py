bcda4ebe8facb7803b2c8e84b07cea2bf808b6b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:28 +0000

    repair female unkind escape ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
