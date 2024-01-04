4f5cc3a9390cd8dfc300d638fba167d5e6be0e87
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:39:26 +0000

    quantity hot wrong murder spite

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def is_even(n):
+        return n % 2 == 0
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
