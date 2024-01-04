530c73c0cca1950fceb500276181bb424104d696
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:26:44 +0000

    back 571enough put legal over

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
