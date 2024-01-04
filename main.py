c9f1b89d4f480a5b04574ab6f8fd8db43b34d451
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:38 +0000

    comeup class situation near same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
