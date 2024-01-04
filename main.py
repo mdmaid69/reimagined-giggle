462905f5670a2fc599d300f08c8e501b19592c07
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:42:00 +0000

    pair keepoff reply weekend and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
