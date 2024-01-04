4bbfc3964b9576fa1f3170f25334cd2bb90ae13c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:52:29 +0000

    shock peaceful broken strict bag

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
+def square_number(x):
+        return x**2
