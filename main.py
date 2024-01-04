13804084f46ad5bdd0dacd1f4382b940bbcd0959
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:12:06 +0000

    calm unfair ticket flood fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+  def is_odd(n):
+        return n % 2 != 0
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
