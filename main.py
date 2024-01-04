2835be0d5404113a7c90837129c104d505023fb9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:59:35 +0000

    relation kill save substance song

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
