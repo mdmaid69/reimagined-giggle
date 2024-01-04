f55f03a31731f2b0bcb0d42ac3b01a3600a857cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:19:08 +0000

    black official release word once

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
