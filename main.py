54d19a8316874e9fda94232dde621f78e3e92d6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:44:47 +0000

    drive snow sometimes wealth cry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
