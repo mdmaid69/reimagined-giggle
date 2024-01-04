c92d258cd507d2b08654343762125f6c6c94dd6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:29:38 +0000

    slice disgusting finally skill survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
