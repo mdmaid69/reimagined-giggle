cae3d73732e6b02b890948ebf1201643c5126a42
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:39:37 +0000

    survive side change come bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
