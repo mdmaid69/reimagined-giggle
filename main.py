10a9eac0fca5065ecf6131e9f7ade31eb4189ebe
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:06:57 +0000

    570enjoyable put pool limit aim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
