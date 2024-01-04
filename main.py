e8d1328e3e3e0e313f8841a685c3629370410bd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:05:23 +0000

    forever conclusion rough plan talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
