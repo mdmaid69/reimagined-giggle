a98661bb39004a442b29aec536620f4e54ba583f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:04 +0000

    bring round tear outside legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_floor(x):
+        return math.floor(x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
