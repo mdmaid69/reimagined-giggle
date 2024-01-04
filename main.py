76899b406184f3849527464766c6cdb644d15020
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:11:21 +0000

    wealth disapprove bread claim style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
