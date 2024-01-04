b85ea3c4dd18f6356f4d073e36c940d00d8f0815
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:51:17 +0000

    guilty apply similar pay emphasize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-print([x**2 for x in range(10)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
