4d30eac041e4d92ee6b60f6b1fa942f5ef51e77d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:09:31 +0000

    exciting height instant strike press

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+for i in range(10): print(i)
