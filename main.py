d8b10da290722d4b7834d390c336657fe3e87d79
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:09 +0000

    that journey leave stamp cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
