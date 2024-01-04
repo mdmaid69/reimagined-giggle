ec80408b2b481ca0e65c57fcf4fb5c560ebe82b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:55:49 +0000

    satisfactory front speed clean crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
