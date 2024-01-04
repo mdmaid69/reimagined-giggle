8faa337833c292503ff0bc2872c5d3a369ac96b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:56 +0000

    pure power cruelty offer accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def check_if_array_contains_item(array, item):
+        return item in array
