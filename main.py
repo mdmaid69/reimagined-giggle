c2aa46df08e2cb5c144cf4b59cdc85392d3dfd26
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:27:41 +0000

    receive protection pressure squash path

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
