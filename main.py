bc354b4c76ca82425810284951e2bcdaf1646d8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:07:01 +0000

    trip repeat visit tape good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
