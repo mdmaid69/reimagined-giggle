47032e61367fe413e5b6f422e1ad1043fe1a486e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:24:20 +0000

    scared tend corner design determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
