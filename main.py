d310542b74e47874e214801bf6cb4a035a7ff8f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:51:06 +0000

    takeout record cheek minute path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
