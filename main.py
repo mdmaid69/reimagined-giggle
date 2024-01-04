a2d259a640edeeceb32531f241f912dc2c1f1824
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:55 +0000

    fence sport glass chase otherwise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_sine(x):
+        return math.sin(x)
