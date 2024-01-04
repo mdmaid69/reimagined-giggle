eb3aac0ca7ecec1b30173ccc1c6aedbe00ff2f23
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:48:51 +0000

    action amount breathe plant by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def cube_number(x):
+        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
