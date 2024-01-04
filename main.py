465c13a3aed0eb0d60da21721f97d26a269caafa
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:14:36 +0000

    record properly scrape blame them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
