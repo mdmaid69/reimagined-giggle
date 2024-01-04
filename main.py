bf6761273422d44fe4ddb97c4a0cca335b8d49d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:05:19 +0000

    bother fun this apply sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_acceleration(speed, time):
+        return speed / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
