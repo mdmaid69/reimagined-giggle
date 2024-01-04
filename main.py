5da80626819b49962e8557127ab55b71123731c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:02:51 +0000

    sound cover swimming play answer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def reverse_list(lst):
+        return lst[::-1]
