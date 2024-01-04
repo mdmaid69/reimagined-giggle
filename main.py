f283a8308220bef8c09cf253c7976684a26550e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:42:22 +0000

    lie relief work understand tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
