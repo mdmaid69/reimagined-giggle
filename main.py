b7236c1106542a90c09494fb556c3171c15efb75
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:01 +0000

    limit stone temper left add

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
