5f9a6703682c4d092dacfe637b972b9ccd913d59
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:59:30 +0000

    government material connect certainly general

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def count_elements(lst):
-        return len(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
