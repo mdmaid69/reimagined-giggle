c6f48409e5da817f0bf0add14b49bc9cde9be7e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:56:21 +0000

    count criticize admit rest question

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
