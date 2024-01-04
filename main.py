9db2b271e7a34b6340ea2d85e80069c2b28516af
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:16:19 +0000

    might culture stretch post natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
