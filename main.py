341bdd1ac8fe4cbf03002e081bda23d6ba1064da
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:29 +0000

    country empty vanish unless shout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
