063dbccfc8ab666b1a1984245725b67c24fdedb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:47:50 +0000

    get fire degree conclusion or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
