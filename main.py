8d1d7a6c587109084df36d9b4c2a4489bbe6869f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:56:24 +0000

    snow less communication cook week

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
