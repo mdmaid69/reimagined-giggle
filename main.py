3daec2b161f8e43dbb78ece0fb1a60d296868f09
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:05:51 +0000

    task present attitude Oo wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
