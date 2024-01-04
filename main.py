bbb788884dfbcc8fc9ccb1c148d912067ae1b189
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:28:27 +0000

    danger bye ashamed whistle slice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
+  def multiply_numbers(x, y):
+        return x * y
