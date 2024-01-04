0ad2be97be0f670aee58e0651332577f293eadc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:55:46 +0000

    back make bunch tomorrow strong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
