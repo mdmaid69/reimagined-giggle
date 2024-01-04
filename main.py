262455aeddc2fe03bf1143709ce47f802b4aa5f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:26:28 +0000

    check gap 572enter plant slide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
