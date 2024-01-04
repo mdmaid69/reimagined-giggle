444dd050bfeb9d8f92a2009a1ea6130b900903c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:04 +0000

    turnoff stick forgive shade better

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
