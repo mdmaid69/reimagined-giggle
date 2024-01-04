b9360402bcb42bc93011fd69fcab44aba5de1253
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:41:20 +0000

    inside on month she to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
