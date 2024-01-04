2e38c59bccfd5e8a276cd0777496b23243be5d26
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:45:47 +0000

    bury deserve mark long tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
