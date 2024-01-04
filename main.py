5a548cc57b3c5b9eed0e1cf353a8e45b5bff1bd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:03:46 +0000

    university goon mention afford possibly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_bool(array):
+        return bool(array)
