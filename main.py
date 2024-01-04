ac1faab39b4b9305b9fdc20bb8a610836277f142
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:38 +0000

    crisis scheme emphasis print keen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def count_elements(lst):
+        return len(lst)
