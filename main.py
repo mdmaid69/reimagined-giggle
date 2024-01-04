035564555df039dc965f967050b45817b3165ece
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:35:41 +0000

    tax thing niggaacademy yes punishment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
