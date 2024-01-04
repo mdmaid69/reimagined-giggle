76a417f1f3b26a2cdc3767c8197e40882e59dc79
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:31:48 +0000

    relax laugh expect warm blame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_set(array):
+        return set(array)
