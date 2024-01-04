7a2ca103fe5e6e96a5b1b302ef39b01e8b7b4dec
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:09:25 +0000

    damage shrink pretty concerning house

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def split_path(path):
+        return os.path.split(path)
