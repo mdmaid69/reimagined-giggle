481b4d439019c2ecc658f66287c5838166a63d60
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:32:01 +0000

    turnback OKorokay prefer next criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
