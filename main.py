595b74d0dc05457b2361ba307340fbb1a4e7d855
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:22 +0000

    lend rely knock record wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
