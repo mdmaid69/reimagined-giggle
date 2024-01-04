7176c6b86dc6f2c8c9cca5c61fbb90299cf1f56d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:39:58 +0000

    survive include stretch many music

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
