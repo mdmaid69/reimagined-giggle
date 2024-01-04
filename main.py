e0b6fda189ea896833ea864526ca69a0bb7cd4b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:03 +0000

    slow cost rid soak meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_area(radius):
+        return 3.14 * radius * radius
