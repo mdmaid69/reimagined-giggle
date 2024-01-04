d33c9795451536d3079f92a64852090cb68308ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:52:38 +0000

    burn list dance respect commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
