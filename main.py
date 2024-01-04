3b277fc3a9b7fc1eee550d67a57e221f77205dc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:07:01 +0000

    main week opportunity takeover in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
