af2c628de45f185d80e7c0b3d61474b6bb51e364
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:14:37 +0000

    label carry slight nerve pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
