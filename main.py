39061e7ed8892276e7b98add88793d871612faf4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:12 +0000

    history female agreement secret absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(lst):
-        return max(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
