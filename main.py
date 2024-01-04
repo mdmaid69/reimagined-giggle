fa4aa0cab4e572734df115870a964ff8f6908884
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:47:55 +0000

    early calloff with entrance increase

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
