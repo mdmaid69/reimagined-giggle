8cdb16c73c82b1e2c867b5d0ad39dae4588cd7d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:44:46 +0000

    flood somebody while advantage day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
