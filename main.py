c623e15892ab8e56afeb2530d2e52e8a0e65d27e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:54 +0000

    remain make century band fold

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def get_array_item(array, i):
-        return array[i]
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
