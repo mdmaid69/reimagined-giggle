f6ce020a930ac5f87a7f6b9c0be1f7f423400dc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:07:05 +0000

    art wash centimetre fat key

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
