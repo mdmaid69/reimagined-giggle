830b1f6a47295dc0eef2aa3936f51a6a134bcd15
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:13:00 +0000

    spot suckme message combination undo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
