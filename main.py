c8ee5399a0a381412cbf7876d0899e82c01d0381
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:49:00 +0000

    contents breakout demonstrate material return

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+print(sum(range(10)))
