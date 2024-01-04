79792d8ff9eb99e56a7783f2c067aa65c2123f8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:21:13 +0000

    along go number block rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
