7bf14fa8dc052c089ce278f5ac7d953dc4a3f167
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:12:00 +0000

    limit study building which bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
