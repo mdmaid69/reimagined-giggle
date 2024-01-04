8347d5b69585a7ac1591c450234dcd13ef1b2a05
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:38:24 +0000

    might aware thirsty he type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
