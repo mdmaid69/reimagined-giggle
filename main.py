a30b82b05d4fc2652348215709883dbbc4ace536
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:21 +0000

    downstairs concern second estimate uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
