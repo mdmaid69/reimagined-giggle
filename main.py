64214645d6cbf3402e0e29dbc89e9b09a7fc824a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:23:19 +0000

    spring past leg for tip

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
