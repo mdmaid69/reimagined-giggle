a237b1177e2e7604f9da4ab022997c90cf0f72c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:52:12 +0000

    sing uniform escape fork yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
