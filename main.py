929dd9cad9214b99504989cea7ebfc6a853f9072
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:12:21 +0000

    strike somewhere again winter familiar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def subtract_numbers(x, y):
+        return x - y
