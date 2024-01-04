2663cf61feeaf08cfb2a2c5fc3e66c5147fb1ed3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:05 +0000

    top church through father quarter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
