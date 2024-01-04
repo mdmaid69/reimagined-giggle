0f0f239ac822aed9f8ac0a432f7282a0c3675d2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:24:13 +0000

    wish worried burn proof themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
