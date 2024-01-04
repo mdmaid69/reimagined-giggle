389d259932eb318e09dfc3b7bbf305981516d644
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:17:23 +0000

    blame relationship turnup about thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
