3edec761011198794392d0c5a3fe357d739b0e62
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:54:51 +0000

    throughout train worth battery print

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def is_odd(n):
+        return n % 2 != 0
