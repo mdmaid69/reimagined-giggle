0246f53447b7d6817f46f819f505e7a8a3e80a89
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:24:25 +0000

    afterwards little Mm plenty tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
