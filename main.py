41efd6dd38b4aafafcc0e20344b50f424d3a56f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:28:57 +0000

    morning sensible tidy down organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
