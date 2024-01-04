1a7b4c5e521e3918bae68ec77e914c707eeddd24
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:35:39 +0000

    listen effort height container emphasis

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
