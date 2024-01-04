b3be26eff19be4454a01af30eb5f92fa1246197b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:46:13 +0000

    crowd disturbing talk neck check

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
