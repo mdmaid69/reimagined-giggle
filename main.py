6f0a9c1543535828499e3e315f74c6b0d142782c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:14 +0000

    appear pride abroad hurt work

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-from collections import Counter
-print(Counter("hello world"))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
