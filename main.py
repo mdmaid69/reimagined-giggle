726e57a88fccb6e8c74d481346b3a96062f43371
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:40:08 +0000

    phone boil whichever finger male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
