f3b0e8112f7224112514f450fee079966f0b13d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:08:13 +0000

    shape properly hurt blue today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_priority_queue():
+        return collections.deque()
