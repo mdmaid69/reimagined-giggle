7600a5028d1b2c9374484dbe30328648a457ee78
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:52:37 +0000

    peace meanwhile waste fall takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_as_float(array):
+        return float(array[0])
