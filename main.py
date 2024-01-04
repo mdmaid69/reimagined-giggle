fc34aed933b34ed88a3dd586047a19c2dcb18733
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:46:40 +0000

    trip broad Jj somehow mail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
