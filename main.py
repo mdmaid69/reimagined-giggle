cd134bdb6e8ea2e0d8ff03ec63c6e39cf07bf74c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:39:03 +0000

    several permission push effect stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
