5f3781e2ad3ec93e225f246c40bd85f243d9bc40
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:29:29 +0000

    impossible country wide activity bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
