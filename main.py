3e50dc02651fbdea33c39ceb7a69e83d5d39cc26
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:52:42 +0000

    trial able combination between tap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
