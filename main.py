0634a6cb48df13d8712e8d833231c4b66ac6b646
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:32 +0000

    warm feed nerve book advantage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
