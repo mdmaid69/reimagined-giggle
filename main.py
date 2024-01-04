90fc1cf085f5ec1cc7aafac585347e9dc490c06e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:59:44 +0000

    their beat criticism exaggerate pain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
