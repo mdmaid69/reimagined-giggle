bb0426da87ca1bcdafcc9350da9015335b324f57
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:40:32 +0000

    imagine declaration along suddenly race

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
