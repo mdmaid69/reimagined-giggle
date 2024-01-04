4924288f059ef7e66628c2fe9ca4f2011188effd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:32 +0000

    main always equal organization right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
