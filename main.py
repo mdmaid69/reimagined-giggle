1d9d638b78e7564cb4e1c423512c85323532201f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:02:38 +0000

    case Ss culture pause why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import sys
+print(sys.version)
