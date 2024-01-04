8f1b700b653feb0f7485218ad4f36f5d8dbc7b2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:01:03 +0000

    great loss accident next church

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
