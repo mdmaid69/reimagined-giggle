475343292b7bb2d2007b516e10d4c10c2bc5741c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:03 +0000

    tomorrow shop steam know face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
