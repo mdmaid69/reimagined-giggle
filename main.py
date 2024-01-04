174a4f32aa66f3e2dcaf6ad6f2e281e8936daa0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:25:05 +0000

    follow thinkover continue murder Ss

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
