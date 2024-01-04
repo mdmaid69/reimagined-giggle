dbe01f4af0476cb04b37685d73e602b6fb47f8f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:57:06 +0000

    way combination tense recent material

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_octal(n):
-        return oct(n)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
