d2298873bde715bd2ae42549894ce0049bdfad8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:08:14 +0000

    bag instruction middle truth come

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
