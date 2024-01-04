dc3f821c838938f53df8ceaced41bd9bb11f92f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:38:32 +0000

    maybe comment welcome church giveup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+for i in range(10): print(i)
