922282109249e1726200f7ae572609ad310520ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:38 +0000

    naturally especially present weak follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_typecode(array):
+        return array.typecode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
