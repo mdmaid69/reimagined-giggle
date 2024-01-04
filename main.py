a2f3c665203b060db06e32afecaa6626ff709354
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:38:00 +0000

    plan most rent error blow

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
