4cafc0507700496d1340d6abf737d5f483b6c117
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:26:41 +0000

    bang pleased nonsense give demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
