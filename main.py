449eb639e743241e778fcb5240d79531d47848d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:01 +0000

    pencil book spill again wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
