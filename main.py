7f10c10bb60812f09a880d844129ce4680d79930
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:38 +0000

    stop suppose often below single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+import os
+print(os.getcwd())
