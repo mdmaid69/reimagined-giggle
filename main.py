e884928c89f2993dcd609c721ee85c577a76ef06
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:02 +0000

    dig university bread mad love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
 def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+        return math.factorial(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
