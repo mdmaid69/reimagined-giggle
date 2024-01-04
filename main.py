197a5c8d5dd2dd87dd3b1ac5c7d6bfa4333af462
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:44:27 +0000

    better thinkover light store push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_density(mass, volume):
+        return mass / volume
