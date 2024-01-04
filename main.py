1e107a556c1e7952a3623429fed3d45746064161
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:40 +0000

    rid wave grade our before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
