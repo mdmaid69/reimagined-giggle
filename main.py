2def4d0f218d37f3e8f7ea3519ea16858ebe3dd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:54:50 +0000

    variety instant shrink lesson flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
