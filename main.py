d2e2967eb2fcc53fd17a586ef12f743185da8dd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:51:33 +0000

    cross complete value move best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_string():
-        return collections.UserString()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_max(lst):
+        return max(lst)
