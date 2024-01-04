fb5522e153faebce564b9001a15d704ab33fcdf1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:56 +0000

    exercise Vv wrong seem catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def count_characters(sentence):
+        return len(sentence)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
