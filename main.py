e2404d5deaad187c8f16cb0310dbefc98e072557
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:16:24 +0000

    hurt slowly money downstairs tense

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
