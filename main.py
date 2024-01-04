48bd4ba2274bfa8d7cc0a79b171ad45b6d66a4b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:05:28 +0000

    turnoff rescue occur normal noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
