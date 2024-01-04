135cd5c9dece34d83481e14f316e0a0379cc1fbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:54:06 +0000

    conversation soak land Rr goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_bytearray(array):
+        return bytearray(array)
