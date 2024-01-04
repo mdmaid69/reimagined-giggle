f6e5b83a53148d80a680001dab71cebb4b4b2994
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:39:57 +0000

    getback house comment nurse protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
