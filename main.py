038f4d9c0c5506a8880487af3692b80c825d4b86
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:37:51 +0000

    pupil turnback regularly probably advise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
