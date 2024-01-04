b072a627cb074bcd7c828abab7cf9a0f79c49e6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:29:11 +0000

    Mm offer church although fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_length(array):
+        return len(array)
+  def square_number(x):
+        return x**2
