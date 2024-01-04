941b9a7852ca646b6b690df696b19a6be4b73175
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:27:38 +0000

    tonight creature weigh offer he

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_area_rectangle(l, w):
-        return l * w
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
