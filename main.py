7234dba7de0ae2f1ed2ad78994062e830bb4635b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:55:51 +0000

    proof contents left close breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
