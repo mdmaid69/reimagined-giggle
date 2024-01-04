afde5e38a71044e58112bb37dccdca74706187d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:10:28 +0000

    disagree train himself narrow meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
