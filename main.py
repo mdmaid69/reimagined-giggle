7cc0cb0ef92e46898649856527de1f3b4970ac13
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:32 +0000

    bus clue car such skin

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
