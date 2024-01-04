525ee5a8a08950f0148700fd4ecb42d7f08571c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:36:11 +0000

    water man their callup relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
