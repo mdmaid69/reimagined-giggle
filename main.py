58c228785aefab83480aebcaee28ae7178b381b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:54 +0000

    that fierce otherwise wave error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
