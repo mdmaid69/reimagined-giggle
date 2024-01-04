1031689115c7b3bea5710a184ce490566d340183
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:56 +0000

    total art trade open convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import time
+def get_current_time():
+        return time.time()
