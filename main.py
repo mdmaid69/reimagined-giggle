37d4e35e18bde59e77216a84042f560a63b296ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:32:18 +0000

    swim smile boil determined factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
