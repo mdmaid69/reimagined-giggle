1487978126aeb45370baa9029a0c1e5d199d7a35
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:08 +0000

    long clever you get area

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import logging
-def log_message(message):
-        logging.info(message)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_max(numbers):
+        return max(numbers)
