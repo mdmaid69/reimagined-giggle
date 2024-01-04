c5b9a77849cdbe517c8cc0414af57e5428b8ab95
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:39:20 +0000

    surprised transparent block Ff tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
