259ca23ff49745fc75cad927ff6b7c3a96ca060c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:01:28 +0000

    argument band mention badly level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
