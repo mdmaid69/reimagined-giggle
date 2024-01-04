83e1f26e94479ef3882b87c1375a91b8038f791e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:45:09 +0000

    who sell knowledge tongue window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_characters(sentence):
-        return len(sentence)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
