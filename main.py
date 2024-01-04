d667ca6e11236e7995b16f47e34928f179dd1305
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:00:43 +0000

    dream thanks breakinto hardly beat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
