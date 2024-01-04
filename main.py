3845683637de4480436e6389749074c6753d2592
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:26 +0000

    either convinced thanks damage move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
