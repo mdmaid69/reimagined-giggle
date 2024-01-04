5263523c33c15f5ac07b4f1a0d33743ab936c27e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:08:47 +0000

    bet course yesterday belief let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  import sys
+  def get_python_version():
+        return sys.version
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
