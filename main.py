563eab21b21504e41cc418947c619460b03c6586
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:39 +0000

    dinner age set actually lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import sys
+  def get_python_version():
+        return sys.version
