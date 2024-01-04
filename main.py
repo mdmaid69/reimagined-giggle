a65a7643eb2d2038e77bdccf660d0e57ed1754de
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:12:17 +0000

    avoid suffer real target protest

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
