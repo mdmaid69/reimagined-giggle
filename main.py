b6ba31437c6618527a758d22c164f75ef6bf29de
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:16:37 +0000

    actually model fence bank please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.time()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
