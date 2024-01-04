36f79e4b1461b7cb5d77f56a0cfb9688caf6b620
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:26:38 +0000

    thinkof city new seriously murder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
