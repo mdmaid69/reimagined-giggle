a739988ef63ce7101901200657c852db35179a16
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:41 +0000

    wide hair share division likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
