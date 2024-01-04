10bb78427754a71c5eb45ac7fef3e3299d4a41b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:14:32 +0000

    possibility disturbing unfair touch welcome

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
