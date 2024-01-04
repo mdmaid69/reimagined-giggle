393ab8b8b3d60f4c75c7899474ff3acac283d198
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:41:04 +0000

    damage mind transparent dress push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
