6fe36eaa04e1dbb349f474634140f582ff22440a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:15:49 +0000

    fork several stone drawing sure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def remove_duplicates(lst):
+        return list(set(lst))
