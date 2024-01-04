9c090eeec4d50f6efb1f61ab8f277234fd61c33e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:44 +0000

    dress shine easy bear expensive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
