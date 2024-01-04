2781f7a55ed3689ea30a362b80a3e8d8ab58fe63
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:02:50 +0000

    holdup genuine comeround land bill

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
