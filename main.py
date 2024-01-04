cd8074311fc5720c42d46691c7dde6b50b66203b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:19:58 +0000

    hair sign narrow care fall

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
