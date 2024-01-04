9b8a9665267c3a3cbd23a3724f7a4257c58a02b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:30 +0000

    to square lay trousers keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
