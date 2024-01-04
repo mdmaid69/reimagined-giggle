8d4defd20601f7f3140c271667eb3f6638a1145d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:20 +0000

    turnout wish influence transparent light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
