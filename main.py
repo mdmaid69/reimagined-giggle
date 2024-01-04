d6536eb81da14f10d808ca4fc6c095f5c01b3ac3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:55 +0000

    speech few earn only form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
