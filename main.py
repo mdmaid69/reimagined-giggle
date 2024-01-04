3a6dbd777a7130cd0a67c2ce73995fe8042c3859
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:32 +0000

    mystery on easy generous makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
