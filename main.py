196f1dd158718d0954ac0757a323cb90a0423b9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:13 +0000

    many stamp equal anxious objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+print(time.time())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
