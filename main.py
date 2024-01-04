3915860c6f29be08a7504371ab21fe0da5a683b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:50 +0000

    supply innocent Rr part perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
