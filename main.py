794ec55596a90ed56fa64f8fa4583e3a7cfe8553
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:24 +0000

    polite lonely wet stare hollow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
