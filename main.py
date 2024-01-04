ba9dca465aa9a0b6bc74dd5bd06fd8b1a79271f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:04 +0000

    sort advise doubt generally level

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
