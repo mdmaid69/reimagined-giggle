ac0ca74501d693f6bece634e6942c95058f7eb61
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:21:32 +0000

    close necessary build form Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def square_number(x):
+        return x**2
+import sys
+def exit_program():
+        sys.exit()
