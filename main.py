f13dfaa6b108bbf5b00a1897f39ce05457225798
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:58:29 +0000

    waste programme second payment wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
