65e90a071c63f9e821b108938044a8f73ecf932f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:24:40 +0000

    part wise earth beginning progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
