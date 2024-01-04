019d360bdcb413cfbfcf10d1cbc8be8dec54718e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:10:42 +0000

    turnon well song score unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_list(lst):
-        return lst[::-1]
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
