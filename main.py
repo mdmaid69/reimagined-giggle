1f2f4125ac6f37465499a31a93c0447645b4b710
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:00 +0000

    as long get later bottom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
