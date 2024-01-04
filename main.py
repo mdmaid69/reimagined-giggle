2d073f731917759e8ceff99fbfa75276fdffa23c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:42 +0000

    power afterwards keen agreement trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
