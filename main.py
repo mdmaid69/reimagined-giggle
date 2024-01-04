fffccb6080023a986917ea6d21741610c71de8a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:58:09 +0000

    bush leave increase hurt tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
