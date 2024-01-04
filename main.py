c58db4420d53fdb339a46408d168761dedd7ed4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:47:55 +0000

    perfect condition correction professional trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
