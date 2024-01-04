1ec97aca2792b5ce11d34771eead2b954940a204
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:51:54 +0000

    hospital equal back feature tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
