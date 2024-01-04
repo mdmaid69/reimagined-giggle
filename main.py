4f75bd3287caf45766aa93f4e857737cbb60e829
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:03 +0000

    rubbish wealth afford yet whichever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+def cube_number(x):
+        return x**3
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
