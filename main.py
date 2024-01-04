8a17f8a098c9f22772fe2b9e1cbf56267d5965fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:13 +0000

    welcome even care see mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import collections
-def create_priority_queue():
-        return collections.deque()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
