f86459efd978a727190935541c05e94d89108248
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:48:44 +0000

    call release trouble release doubt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def get_array_item_count(array, item):
+        return array.count(item)
