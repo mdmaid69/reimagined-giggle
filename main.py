8f8d436b80d524dc805cbac27ecfe98180746ff9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:29:40 +0000

    cut uponon public still joke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
