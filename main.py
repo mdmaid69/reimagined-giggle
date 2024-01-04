cbff2bfd5482f1778e0fb7ea3feb6882c61a9c21
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:51 +0000

    system report dangerous Ll majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
