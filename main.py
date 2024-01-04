dc9a5eb77d434b575179e3e49c9b1d38fbee0270
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:25:19 +0000

    awake cost crash marry cum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_list(array):
+        return list(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
