7b9b15abef1637826ac6f8976befad49151438b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:43:44 +0000

    study stare tie bus anything

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
