31ac744ab56ef3503fd54b1701bb9d3087149225
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:58:38 +0000

    mysterious wild us tax great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_area_circle(r):
+        return 3.14 * r**2
