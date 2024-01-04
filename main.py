6cb289267ccc77d80de6cf4dee5cbe211d385e43
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:15:42 +0000

    fence excuse nearly make purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_as_int(array):
+        return int(array[0])
