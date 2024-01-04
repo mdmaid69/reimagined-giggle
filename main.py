8746c18ada542e3b557fe3f5fd83ae31f729c038
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:31:55 +0000

    getup turnup roll capital weather

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
