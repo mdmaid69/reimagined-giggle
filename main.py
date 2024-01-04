8ab3d4e3783368dc3dde3538454dc86caaee0a1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:15:34 +0000

    familiar error in repeat nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
