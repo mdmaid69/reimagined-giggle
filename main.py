ad57d27f810187e6b02f628479bf97127134d3ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:17:49 +0000

    below already place friend fit

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def calculate_irr(cash_flows):
                 return rate
         rate += 0.01
         return None
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
