bf6c01293ca757e6edf15111ae37a9e382e93c1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:55:24 +0000

    already so determined let slow

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def find_min(lst):
-        return min(lst)
-print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
