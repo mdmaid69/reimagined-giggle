861506e6beb2d3f1aba9717b448a25b44a972774
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:07:33 +0000

    sink finish ago break fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
