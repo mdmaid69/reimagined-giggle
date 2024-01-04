9cc9e5b1473a3e100986fbdecff2a523a34eae2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:33:29 +0000

    comeover list instead understand square

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_power(work, time):
+        return work / time
