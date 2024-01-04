4948682719177e3a1cdfea76d6e18d6776eda804
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:16 +0000

    suit culture pretty rest round

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
