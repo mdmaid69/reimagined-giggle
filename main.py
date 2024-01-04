0cd12ca06a16f05fb5050ef02953422449974af2
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:58:48 +0000

    old write purpose rough subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_as_float(array):
+        return float(array[0])
