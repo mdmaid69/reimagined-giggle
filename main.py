ec4a7939713e9352bf51e2b79dd91ba472fd5083
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:47 +0000

    afford surprising together carry rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_length(array):
+        return len(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
