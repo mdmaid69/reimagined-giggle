f8a3f7b5e1c2b6ebc325e7b4f65038fecf1bf12c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:23:39 +0000

    greeting Ff major relation aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
