4a2b3fe18bbfb2fff792f991de1e0939b97aa853
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:35:15 +0000

    minority public temper trust loan

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
