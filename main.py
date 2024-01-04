ee9d36630ab36e597f044c1819de0f3099adb5ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:19:13 +0000

    confusing rough much amount recently

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
