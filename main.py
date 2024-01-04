5d0237b84911a4300f3645720eb43db8558cd052
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:30 +0000

    young obvious book stretch difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
