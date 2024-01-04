f777b3f667738083e2278dfcb9b3975873eb5740
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:55 +0000

    should properly shake shrink whenever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
