6773085134de7c5ee895c10fe05e5692265b8169
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:08 +0000

    blame platform underneath show achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
