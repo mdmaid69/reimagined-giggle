ec53495b24b5b57ff910fa1b370f67cc6dbcfb28
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:59:46 +0000

    hungry respect duty cry mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
