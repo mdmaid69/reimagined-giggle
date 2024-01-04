4262a6b9125942f8e2bf45f7bd0db265b7017569
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:16:53 +0000

    cover address approve sad interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
