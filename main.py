fe9a77fa51235ab02ea06f5f5d6abf8bdccde9e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:52:52 +0000

    Vv appearance sensitive memory top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
