79655e9f94a761aaa75c5ec4a02d9eb771d51f97
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:24:51 +0000

    disappointed view flat list chin

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
