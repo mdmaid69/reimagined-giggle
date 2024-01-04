687a1f55c80393ae756f9039938ba85a7dfa17d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:08 +0000

    lay exciting train compete separate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
