0bf80b27c62fec380cd719b505eebd80d181a4c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:12:43 +0000

    one risk future visit be

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def sort_list(lst):
-        return sorted(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
