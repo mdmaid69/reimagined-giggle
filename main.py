d4400a99d2eca729f9b7a59a7f6c099b57b5ed13
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:46:36 +0000

    window number soon stupid pile

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
