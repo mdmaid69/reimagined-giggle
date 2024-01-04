0049e7c463b51055a341aba4af6f53b6451c0c66
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:05:40 +0000

    right lie question hers cap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
