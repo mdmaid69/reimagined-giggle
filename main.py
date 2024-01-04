e6331152e3cc88794e460598257449c4fefa368b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:31:35 +0000

    drink say terrible yours lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+from collections import Counter
+print(Counter("hello world"))
 import math
 def calculate_bessel_function_of_first_kind(n, x):
         return math.jn(n, x)
