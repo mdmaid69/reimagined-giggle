17c0e9ddae5fd56da909d320135016eb9189ec73
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:49 +0000

    machine estimate pay my snow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
