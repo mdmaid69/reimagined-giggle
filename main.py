c7d69e7186c734916f26daedef53afd6f44e8d36
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:03 +0000

    either Bb indeed safety breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_palindrome(s):
+        return s == s[::-1]
