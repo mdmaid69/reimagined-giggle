8eed8548e07d3efe281948f94a64ecb02a9ba8e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:09:27 +0000

    exam sink bringback embarrassing breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
 import array
 def get_array_as_dict(array):
         return {i: item for i, item in enumerate(array)}
+  def subtract_numbers(x, y):
+        return x - y
