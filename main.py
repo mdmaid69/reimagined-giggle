532aa613a26f05310958ca1d93212d5db264b701
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:16 +0000

    breakin mine outside fortunate introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
