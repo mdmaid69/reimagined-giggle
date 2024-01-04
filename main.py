917c592a046c1adf91fa7603a86d99c8689950f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:24:37 +0000

    sting order sea moon single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_hex(n):
-        return hex(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def find_max(lst):
+        return max(lst)
