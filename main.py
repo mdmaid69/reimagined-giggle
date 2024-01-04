f294334e32a8bb152e7072347b6bebe2707ab877
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:23:42 +0000

    poor guilty fat extremely alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
