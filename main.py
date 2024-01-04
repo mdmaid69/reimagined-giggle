9e5a490b83724ae13cef8c9133d00a73e791f9b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:48 +0000

    communication hang fierce type private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
