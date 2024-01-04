e58ad120ecdbf79c9d85077975791a942ebb2783
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:54:03 +0000

    roll sensitive old obtain giveaway

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
