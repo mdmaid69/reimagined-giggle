b42295e5e0e946e53276146f9c6732126d5dcdcc
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:17:47 +0000

    light proper turnon opportunity error

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+print([x**2 for x in range(10)])
