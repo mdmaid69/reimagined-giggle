2effab76970397ef829110d8f670825e3d356e56
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:33:36 +0000

    already simple thinkof report first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
