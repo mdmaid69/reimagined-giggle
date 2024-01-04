5e4cf709e7bfe996f9189b2eef7a97f39f926bd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:37:26 +0000

    from back dangerous react crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
