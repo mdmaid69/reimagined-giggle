b95a843ed5b4be32efcbde2792fd0317a26b754f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:48:52 +0000

    able national artificial every since

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
