10562ba1c19bac628fe383ae316733a9e9eaabe6
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:21:06 +0000

    bet powerful comeround rent job

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import sys
-print(sys.version)
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
