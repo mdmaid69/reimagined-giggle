0fd3c50a5573248d292840da351af22c7695d6b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:32 +0000

    all Mm Hh should clever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
