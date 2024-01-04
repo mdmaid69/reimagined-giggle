e4c23dbcf3b6ac51a3fbe5782da8af3f42a4af85
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:39:24 +0000

    mixture climb attempt common raw

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
