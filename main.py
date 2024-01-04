2c36ca1976ebcb296d8d76bd726e72391e0240e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:06:53 +0000

    comfortable gas statement present bang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
