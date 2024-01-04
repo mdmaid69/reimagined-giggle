72286c572988939af2ad8243dd5f39c7623efa93
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:42:35 +0000

    ear ring noise opponent 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
