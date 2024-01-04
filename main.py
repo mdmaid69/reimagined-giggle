a1bf2a2d941ffe704d7e6ee6e56ec8486f7a22fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:57:27 +0000

    anywhere smooth wing always gradually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
