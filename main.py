4299e8e96e4b633f5f92cebe1efb00d0ab169b8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:55:14 +0000

    I I mad spot machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
