62794debaffda036ff4c77042594fa0b199f9436
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:25:06 +0000

    hate hand car mine show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
