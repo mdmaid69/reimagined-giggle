2332b349f7d06d16a6dcdb8f439b9210260e26e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:46 +0000

    kill happen border have leg

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
