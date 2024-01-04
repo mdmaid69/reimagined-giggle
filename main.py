ed903270ea0e572a08883ed5735d1df24325b4d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:16 +0000

    fresh forget disagree hurry hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
