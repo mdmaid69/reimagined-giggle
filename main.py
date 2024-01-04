8fcf1be233920d72ad8b8f437f29507e45985e39
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:15 +0000

    purpose government exaggerate pure bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
