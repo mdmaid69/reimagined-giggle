cf88e576eef2e162a8908c19e8afe1eda7380780
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:52 +0000

    cry shadow imagination reaction male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
