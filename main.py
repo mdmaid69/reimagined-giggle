07de6cfad4a9439a878c453561c5e016b461872f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:31:19 +0000

    bury ordinary buy read type

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
