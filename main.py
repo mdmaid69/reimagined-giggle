231da657f2943b01342520de0af7a303d5c96bde
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:38:29 +0000

    description recognize guilty wrap shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
