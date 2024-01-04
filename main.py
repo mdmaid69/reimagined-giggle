b28a60fed640d46881005689dea922475aa34f37
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:58 +0000

    confusing keepoff knowledge fast either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
