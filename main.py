25e968608607ba730c2b585d2842985ec02a2985
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:23 +0000

    produce purpose meet deep effort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
