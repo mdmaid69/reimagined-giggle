0b91f259cb0c3599476d5d49f4cd9130457ee9e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:07 +0000

    disapprove car rush confuse fair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
