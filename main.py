6fb79c6145f41d2c9301fb3cdce34d26ab5a2f6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:11 +0000

    nigga attempt emotion use rather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_max(numbers):
+        return max(numbers)
