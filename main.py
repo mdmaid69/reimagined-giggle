72e42cb39591964297b9cecd3380f1336a7ab9af
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:19:43 +0000

    clever correct new bringback while

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
