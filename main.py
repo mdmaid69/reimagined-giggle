9759cd60acc4d7376ad90d58caecf01acc72232f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:24 +0000

    cut love attitude passage exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-name = "Python"
-print("Hello,", name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
