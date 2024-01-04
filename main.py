2c25edede803873af0e0411481cfd37e5cc018d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:08:28 +0000

    community ride example confuse cry

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
