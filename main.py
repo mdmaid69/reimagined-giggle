496d57170563e8f69add0823fc1b2f41705438ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:16 +0000

    squeeze century pour dear address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def count_characters(sentence):
+        return len(sentence)
