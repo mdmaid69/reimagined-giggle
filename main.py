e8084e1450927a325b33a2617cf81a080ec45b8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:57:27 +0000

    among claim sky factory them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
