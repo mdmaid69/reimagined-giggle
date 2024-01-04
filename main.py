eb67b9d28ddab3fd5f0580353e331d6eaeb3fbd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:13 +0000

    pretty unhappy as expect complete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
