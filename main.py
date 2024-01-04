661b5a3ed7c31303fdcdb5850b6bd9f7d46b47d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:34:39 +0000

    Vv suddenly bar common manner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
