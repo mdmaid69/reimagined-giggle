0328bdeb49100f1a8453028331c36346a3cb5b14
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:43:17 +0000

    surprised Nn regret doubt forever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
