0531ce6cd7253e59f44122e14f74322238344985
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:43:09 +0000

    fit pretend embarrassed across allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
