cb5fe46b8999588c6bf9c9ca75b9bc5ec775fdbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:11:50 +0000

    married force last gap message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
