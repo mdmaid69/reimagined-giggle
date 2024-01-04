172f33052a584a46cb109a0305fa19862c4bad1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:08 +0000

    blind remark physical foreign safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
