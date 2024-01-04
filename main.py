ef045068d85d8845d6c49d1b2694dad90615b345
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:36:50 +0000

    suppose dare weekend concern progress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
