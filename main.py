754b50fa6a352bbe8c7cb7f99e120935730705b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:32:38 +0000

    programme goon bread introduce cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
