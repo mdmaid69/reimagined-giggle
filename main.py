27fa6e5a643cf9af43d61e6fd5af7daa22cdd10d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:43 +0000

    none callback walk cap surprised

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_acceleration(speed, time):
+        return speed / time
