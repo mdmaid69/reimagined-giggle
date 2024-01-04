49d18185149227d5f8269b0c36afb110fdb4d2a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:02:00 +0000

    pretend sail neither curious borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
