e4ff7ac19c9321171ba508ca38ac8198ae0c3084
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:42:14 +0000

    machine Qq tax involve bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_set(array):
+        return set(array)
