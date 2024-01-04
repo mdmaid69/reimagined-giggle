1026f70cc5b3d6dd872d9f323ed0bea54725a514
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:05 +0000

    tonight style soak appearance bound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
