66ee3d951db6880a04f72da86b2f4a433f4772f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:29:35 +0000

    murder jump dull occur visit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_area_rectangle(l, w):
-        return l * w
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
