70608340f20769ab7ca689996b33994d57424a8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:15:34 +0000

    over crowded organization Uu help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def append_to_array(array, item):
+        array.append(item)
