d5c110302c1ffc33b95b890949b7554ab1f42f20
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:22 +0000

    bad rank point tonight couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
