aec5f11d5aa5f42a9e225fefa207cb22a657a792
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:28:39 +0000

    suppose view measure complicated suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
