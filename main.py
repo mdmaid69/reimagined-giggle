dd5a2ce163a6dad2e1f19f49cf64025145e1da58
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:03:24 +0000

    lay complete mail estimate line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_power(work, time):
+        return work / time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
