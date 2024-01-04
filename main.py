29e606456bdc2712581c8d2362826ab078abb74d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:01:38 +0000

    worth respect public punish consider

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def cube_number(x):
+        return x**3
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
