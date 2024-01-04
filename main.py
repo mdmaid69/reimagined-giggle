01f8c96135954769308f31d122d6b6ce341558df
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:19:26 +0000

    comeoff wrap before nor first

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def cube_number(x):
+        return x**3
