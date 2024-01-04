c7731080ff7629734496366fbe7f1b0bdc600ffe
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:55:55 +0000

    measure warning size thin guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
