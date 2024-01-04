44f7102dcc5cd61b7d2ccf3a80c9a016bffc02e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:46:31 +0000

    relationship mess join giveout hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        print(sys.version)
