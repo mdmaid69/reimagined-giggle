d120de292394bec82baa9a3028e90442cde7cd6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:21:47 +0000

    somewhere near row thick loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+print(math.pi)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
