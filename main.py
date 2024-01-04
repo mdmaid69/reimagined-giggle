462b1736adff7520d96fe20b922f97b451e5dd14
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:20 +0000

    juice its congratulations destroy until

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
