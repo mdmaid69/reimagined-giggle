e30faa81b5ff192e1987b6a5e3431a65ae1370f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:34 +0000

    pretty group sad instrument sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
