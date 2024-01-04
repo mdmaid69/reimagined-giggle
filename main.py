642559826ff0b260dfe6f61a2c5fab9a5db8ff10
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:41 +0000

    object concentration recent pump hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
