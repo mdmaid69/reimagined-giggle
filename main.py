c4105a8c6e2e1fa75efca5b8d6d58f496b818ac5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:31 +0000

    wind trade promise admire giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
