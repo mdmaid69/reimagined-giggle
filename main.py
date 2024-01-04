d2a8b6a5d924e92e608fc09ab0076512e888a3e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:23:22 +0000

    fear estimate afterwards cause tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
