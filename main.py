1755a6db449addb5ebe81c1d13eeb8e0a908db87
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:35 +0000

    factory part point proud fall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-print(sum(range(10)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
