52322df5bd6e5e51ec38c57579906d6cbc8ff464
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:47:37 +0000

    pause certainly hungry thinkover machine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(lst):
+        return max(lst)
