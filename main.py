b85d0fcb014cb32ac5b846445ee0f3948ee9af46
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:42:57 +0000

    telephone criticism care surprised underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
