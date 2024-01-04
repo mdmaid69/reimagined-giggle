3ef918704f9ab1a8fb128fa52124f9a6b7570f2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:54:55 +0000

    race truth anxious operation thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(a, b):
-        return a + b
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
