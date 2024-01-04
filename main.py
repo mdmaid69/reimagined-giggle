ae87178ebad6228eae054fb18d7ff006e84c23ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:22:59 +0000

    objection guess chance duty fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+text = "Hello, world!"
+print("Characters:", len(text))
