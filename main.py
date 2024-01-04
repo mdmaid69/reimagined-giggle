79b94472bacfbbd7e5471f26547637a33b77cc9e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:33:42 +0000

    public grow start floor attention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
