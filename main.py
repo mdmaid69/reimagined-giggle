9984f27e6ea21553d86ba4aa236402b1538fe597
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:44:25 +0000

    stick light interfere cheek better

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
