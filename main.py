3559ef9ca42f0f45a268d02ea757ce621196b5d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:44:15 +0000

    recovery pair takeout dry top

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
