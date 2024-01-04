76c95322cee80d1ca2c9081903934b3abaa61655
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:36 +0000

    arrangement different software earth expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
