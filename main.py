5cff68a59c45616df8f2eed4a474636d1a72e0be
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:51 +0000

    upstairs hers able join average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_power(work, time):
+        return work / time
