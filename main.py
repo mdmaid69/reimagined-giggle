31af3d0a9d67ce06a13d7d1d28c38b5e2f9cf9e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:50:36 +0000

    form keepup official error vision

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+print(sum(range(10)))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
