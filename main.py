5d7170edbf0e2024d6f0adb9a5311d5727af1f4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:29 +0000

    content handle true himself through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
