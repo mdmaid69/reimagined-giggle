19e1d834cc7d13774d545398964f74666dbdcc72
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:31:04 +0000

    boring party hang cry upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
