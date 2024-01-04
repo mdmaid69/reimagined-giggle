a50b3428031e6f1a69777e2516681527de66c4ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:16:15 +0000

    pull entry them compete land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
