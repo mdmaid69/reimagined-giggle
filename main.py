27b1e5f2c68c0566c5c5cb52f1bd9e79030ac2a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:37:04 +0000

    thought odd niggaacademy fly season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def reverse_array(array):
+        array.reverse()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
