828f02fd9a45779f8871040f5e0d517dee1b75e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:21:05 +0000

    head tradition tie suit getback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
