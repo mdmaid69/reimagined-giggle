a4f35aa40f1502912743d9513a506c3f3d985a01
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:55:46 +0000

    call drop chat join key

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
