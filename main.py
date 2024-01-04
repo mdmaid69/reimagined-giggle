0f5218cc7b93a223f35691061c82100838f16515
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:02:22 +0000

    someone already container estimate provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
