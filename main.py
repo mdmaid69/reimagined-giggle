8128b74ac8ff1867b5fb5501c89fd597cd04dbeb
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:02 +0000

    rate approval uncertain organization famous

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(10): print(i)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
