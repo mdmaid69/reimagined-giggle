befa4b50dfc98adf2ac73bd62939e2137083d798
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:04 +0000

    goodbye pattern evidence many do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def split_path(path):
+        return os.path.split(path)
