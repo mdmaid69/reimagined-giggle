df90f98b0de8232fa9933202c5f382d5509b8c02
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:05:19 +0000

    treatment believe theirs branch whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
