b709e7e8405c548085db9e6488226c5143792b33
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:53:35 +0000

    holdout distance with meanwhile formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
