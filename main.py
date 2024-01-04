ff5f5b758f7814d26e4d960b7fa5c0a6bd9c99dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:02:44 +0000

    brush mistake answer bite its

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
