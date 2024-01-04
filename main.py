11fa438abbc0b70bfc5bbf2a149b11f0998b2c82
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:23:09 +0000

    guard clear themselves market modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
