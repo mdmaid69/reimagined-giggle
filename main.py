9047e3c628b9e142e9b3bcee2f267e58c9a3e6d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:05 +0000

    vote swim active entrance sight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_list_from_array(array):
+        return array.tolist()
