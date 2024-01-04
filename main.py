31af5e054c27e3a01ad169a3982a60711effc4bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:11:20 +0000

    hurry than lean 569enjoy exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
