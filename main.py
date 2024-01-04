0d017e85beb453c79e4efe7fff8c28716c094036
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:12:40 +0000

    turnaway dick decision pattern spring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
