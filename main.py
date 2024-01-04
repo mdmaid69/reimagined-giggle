43a87b27559d8a5a63fb5298be2ff320075a3013
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:37:20 +0000

    play pause replace unite couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
