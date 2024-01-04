8f121d1f0430b0bc0a4d55c6612747f26ac30aee
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:41:14 +0000

    connected would key treatment colour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
