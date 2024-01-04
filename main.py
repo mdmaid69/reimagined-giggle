20682b95e24ada595fb8704959e699cc62326a9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:04:40 +0000

    kind bank deep expect Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area(radius):
+        return 3.14 * radius * radius
