84ae132c4a35e6c099430a86ebe4b89519d3b439
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:25:47 +0000

    problem press lookback(on) already worry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
