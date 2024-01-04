cce99ea561163c06f89de2fd82d1c68d8ce5c0a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:45:34 +0000

    hello shadow left radio joke

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
