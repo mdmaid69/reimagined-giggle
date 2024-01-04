f1d34a35d95ddadd85223719647850aa965f5d35
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:19:27 +0000

    comfort measure soil wide so

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
