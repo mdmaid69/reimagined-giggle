20827baaa27d3d5a2467eceb5fb51c562f69cc64
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:27:12 +0000

    taste rich naturally deep love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
