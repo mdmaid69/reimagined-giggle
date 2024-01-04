def0e69956ecb04f273eed6b4862432804d0f84f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:11:22 +0000

    frightened stamp able base comment

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
