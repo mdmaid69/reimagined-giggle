1857ed2a566065d1315da355c0078982b5dc897b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:04:41 +0000

    that stranger information school union

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+name = "Python"
+print("Hello,", name)
