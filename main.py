71d5c02b7eae943e86577d6d50c3e9f53d5084ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:25:21 +0000

    weather common hope everybody complete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
