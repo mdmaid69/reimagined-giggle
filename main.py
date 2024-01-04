57e9a96ef75dc187ac627e95931657e13f5a4435
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:33:00 +0000

    emphasis influence human interview name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
