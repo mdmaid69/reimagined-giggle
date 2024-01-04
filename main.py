3d90c8ea4d002df9dff17f1991fe66ed6f71d978
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:15:15 +0000

    expression quick manage without difficulty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
