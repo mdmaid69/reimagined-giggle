25ef769895d546b610cf65067406fee5214431af
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:28:18 +0000

    show trip stupid pretend opposite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
