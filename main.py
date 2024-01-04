b3972144978bc61a09bb32e641f876f7c04238e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:59:33 +0000

    miss boil improve contrast beneath

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_volume(length, width, height):
-        return length * width * height
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
