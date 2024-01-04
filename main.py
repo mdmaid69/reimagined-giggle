e6c87ca74e3281bdfd98620fc0f93aa53b01a911
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:09:08 +0000

    various stage clean public emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
