a4298b9f84821036d7f91acb87e9e0f8ab284a05
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:28:48 +0000

    service crash cinema estimate operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
