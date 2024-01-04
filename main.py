f0aa89b2975575675774f537cbd3e4e45e2a82c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:20:57 +0000

    tip however level back notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def convert_to_binary(n):
+        return bin(n)
