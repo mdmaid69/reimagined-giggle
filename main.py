73de412865123ff96e783378d541f6fb8a738a6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:23 +0000

    light congratulate rarely takeoff plant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
