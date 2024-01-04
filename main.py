dae0e5c34df51272a2c15ba32ea8d62f4c13f703
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:22:16 +0000

    black criticism list confuse Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
