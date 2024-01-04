1e9d4330c5a7aafadca68aa690ced3033a43ac34
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:33:49 +0000

    issue poison near carry name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
