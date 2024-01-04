10d950411a6799122e5b7aca93c1e4a4a1c0370b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:49:25 +0000

    straight convince nature permission fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_list_from_array(array):
+        return array.tolist()
