c95dde7e8ed96981482b658f106934148f2003e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:12:57 +0000

    everywhere passage insist maybe appear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
