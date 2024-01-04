304299e76ed8c1a97a7146e518fe8638c9e9b538
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:39:13 +0000

    valuable grade strike unkind smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def square_number(x):
-        return x**2
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
