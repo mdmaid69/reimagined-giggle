44d63b873fe29f518c64100b3705f5d6ca7992ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:25 +0000

    improvement crisis try roll fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
