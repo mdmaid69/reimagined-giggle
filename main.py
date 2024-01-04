60fb388cee82c5481aad9bf9d5d40e571a27bf18
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:52 +0000

    fierce row ahead work number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
