d3b6bf00f0635f1324b3f080e5adffd375829b45
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:04:37 +0000

    trousers fond support new unfair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
