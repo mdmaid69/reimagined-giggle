9e5cfc470ae34103bc11826721a56d2d599b553c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:56 +0000

    thinkthrough expect near same owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def reverse_array(array):
+        array.reverse()
   import numpy as np
   def calculate_correlation(arr1, arr2):
         return np.corrcoef(arr1, arr2)
