27bc9cea0d9bfa9ad62550c75ff048c48ca8c8b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:26:06 +0000

    address considering form dick thick

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-def square_number(x):
-        return x**2
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
