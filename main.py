52c6a909bad80628e6e2c92457880e54d8857a27
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:52:39 +0000

    they happy order introduction nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_base_name(path):
+        return os.path.basename(path)
