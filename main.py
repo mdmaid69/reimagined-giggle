f360c69115b527a4c7964016f2757d82f3bdd44d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:53:31 +0000

    dangerous forever surprising show unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
