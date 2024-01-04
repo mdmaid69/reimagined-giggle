61d6cfa82376baf8733a4d48a04a8b27ac6044b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:05:03 +0000

    party favour opposite exercise talk

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_pressure(force, area):
-        return force / area
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_speed(distance, time):
+        return distance / time
