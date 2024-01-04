9415c97e1280f7d2eb6f6cd58d5f4be42ddc8258
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:17:57 +0000

    flat summer difficult discovery sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def multiply_numbers(x, y):
-        return x * y
+def convert_to_json(data):
+        return json.dumps(data)
