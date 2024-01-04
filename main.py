ce20ef9ff1b7fd655e4910c1b5daebd89b227900
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:11:19 +0000

    fresh accommodation estimate border ear

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
