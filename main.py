40ab67c878a97a5cfbf79a5f0715013f9214cbc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:19:54 +0000

    wing otherwise second flow instead

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
