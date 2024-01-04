60d0228c8703f090587a4fccc9d57208fe1ffdd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:55:39 +0000

    pot face ourselves takeoff drawing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
