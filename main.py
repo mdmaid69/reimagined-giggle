dd08162220ae672d975a82bba9b5fc1f9f0dc85d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:28:15 +0000

    Dd now risk defeat realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
