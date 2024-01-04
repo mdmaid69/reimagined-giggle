04de88e7ab5465761ba31714c203ba5e099e953a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:47:38 +0000

    real shade can plant stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
