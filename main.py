e990fb763c0b44056009a1a4dddbdb05bc92c8e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:11:51 +0000

    sail yes contain know guess

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_length(array):
+        return len(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
