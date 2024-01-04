366c751cafd36478ab17f0becd310ef8753353b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:18:53 +0000

    have smell dear weigh branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
