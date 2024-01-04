c371dc6dd770ac885d800b445ab06f686ebf7752
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:56 +0000

    hello touch accidentally never likely

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def square_number(x):
+        return x**2
