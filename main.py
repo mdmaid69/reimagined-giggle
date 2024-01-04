c14501d1903c71614572e898185a7575c6d44cab
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:07:55 +0000

    chance indeed cover once sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
