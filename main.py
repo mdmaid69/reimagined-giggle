79c376d213310c5ca0869e22450c35267b44ffdd
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:09:57 +0000

    drive discover special square Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_priority_queue():
+        return collections.deque()
