5fb6be632e011f54abbbbed756732c2df9f8555a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:54 +0000

    confidence precise calm several not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(5):
-        print(i)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
