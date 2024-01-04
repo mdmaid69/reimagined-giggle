7100d45779b86f96420c61bb34c1de4da362ce24
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:57 +0000

    underneath declaration hurt congratulate favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
