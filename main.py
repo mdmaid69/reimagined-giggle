de6a77522412d4056948eb22924dbb1ce4ea1f6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:46:28 +0000

    friend pour opportunity spite worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
