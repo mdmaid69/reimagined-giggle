619a1e59ab987287355b311f83d2f3870277e705
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:02:47 +0000

    bring underneath heavy necessary tool

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_volume(length, width, height):
-        return length * width * height
+def clear_array(array):
+        array *= 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
