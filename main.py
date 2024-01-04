92b6085810dafd3b1c8b64af2af660e5f03eeaeb
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:17:38 +0000

    question blow protect walk search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
