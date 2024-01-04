eb44035033c42be1dca5d8a1bbcad6dd1bc56b7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:43:40 +0000

    pin perhaps wave objection entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
