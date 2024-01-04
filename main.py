2b8e9d7d9c585c0b0496b14ddb24ee10af8054e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:00:49 +0000

    afternoon how react loss keepup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_list(array):
+        return list(array)
