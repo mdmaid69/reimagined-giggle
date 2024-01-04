cf91fc07bde2dde03322c03678f35e31f1804a0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:22:48 +0000

    tie declare scale possible begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+print(datetime.datetime.now())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
