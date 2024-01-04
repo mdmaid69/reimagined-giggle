e7929e98ca84af394051bace04886db87bcb2cbb
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:59:49 +0000

    upwards pleasure characteristic typical hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
