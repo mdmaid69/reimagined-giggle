fa6a6ca563023387d255b2bfdebebe5a78f5d250
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:11:15 +0000

    giveaway luck fall height sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
