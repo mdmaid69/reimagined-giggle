750c5fbfd9f4b278ed8f0cc099149d63b445fe59
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:52:34 +0000

    test something eat early clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
