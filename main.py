d20f18654f33ac378eb80e48d23977c0d5f3bf4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:03 +0000

    unless sink possibly turnon opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
