df78ba8d9d7af9c1ca11b248799ff246f6ccfddf
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:24:54 +0000

    reserve breakdown cinema common pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
