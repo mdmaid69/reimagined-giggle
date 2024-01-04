0c502ed8fa9f707d2aa460466847df49ef46fcca
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:38:23 +0000

    unit opposite emotion short afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
