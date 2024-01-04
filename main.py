0062da78dbc7f9107732463217874b1ede09864d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:21:06 +0000

    medium makeinto unfair slide unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def convert_to_binary(n):
+        return bin(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
