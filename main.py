f714cbeb3148ddbc92310e853d8dadf6bb5fd522
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:52:37 +0000

    neck rise professional period season

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
