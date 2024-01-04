4fb2e0dd922df646e15606dfc83bc7beea205b80
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:23:06 +0000

    time reasonable own mad poison

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
