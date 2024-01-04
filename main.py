e4326abd4df186296fc3ccab74e89ef361f4ddad
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:39:42 +0000

    safety considering meal comeback and

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
