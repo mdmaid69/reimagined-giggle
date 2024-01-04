3cbe681783f5e201a5464e3b084edd1888321770
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:53:23 +0000

    score us experience hard new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
