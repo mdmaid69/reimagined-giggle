c9bf2d4a36ac86d2487ce7622d1f7e1d80c3f124
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:08 +0000

    advantage organization really improve stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
