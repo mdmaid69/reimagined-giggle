4ebfcfd04d6f775e1749fa47eeaecbd4f4aeba0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:52:44 +0000

    defend though job child stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
