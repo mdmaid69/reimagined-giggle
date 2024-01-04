f90e3d931045600a7534321b15cb84570b503b14
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:46:07 +0000

    whisper community still schedule congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+print(os.getcwd())
