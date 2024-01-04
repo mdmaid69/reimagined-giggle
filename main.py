8faed22795020059653cc3b4e63ae9eefecc83b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:01 +0000

    properly finish debt sick rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import time
-print(time.time())
+def get_time_since_epoch():
+        return time.time()
