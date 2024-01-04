9b3b701f0f65cbdce9f83364d3bfb9600274fd9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:27 +0000

    Ll wash move reply head

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
