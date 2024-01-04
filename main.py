ebfa1ed05662c292ad8c254a4815383c3779c9e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:42:36 +0000

    reasonable rate wipe uniform care

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def add_numbers(a, b):
+        return a + b
