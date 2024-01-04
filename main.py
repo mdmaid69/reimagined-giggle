6e363f2f064eb750cf72578c11abda2f740078d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:16 +0000

    cool rain threaten judge practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
