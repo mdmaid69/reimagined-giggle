a697b91b7fcd9537fe0f19ad490c5a7ebbc5761b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:40 +0000

    pleasure calloff cruelty moment after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+text = "Hello, world!"
+print("Words:", len(text.split()))
