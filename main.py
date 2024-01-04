1f44b98039b05db21069a2488736ff6a97aa059c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:01:34 +0000

    rest serve eat drink pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
