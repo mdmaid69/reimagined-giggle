21bb9fadde470e2980527b60c8728f315adacfcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:09:54 +0000

    luck emphasis sleep careful identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_max(lst):
+        return max(lst)
