bd4668bb123b02b12d6bd120dd9ac4978046836c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:58:50 +0000

    shine exactly taste Ll bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
