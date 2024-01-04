6f8a9941e4158ed9942c9140e85d5ee5f2edda93
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:10:17 +0000

    burn nigga various film run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
