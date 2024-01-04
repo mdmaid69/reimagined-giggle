cb9910d87a65543fa9366d41ce07728c3dad9eb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:26:23 +0000

    when coffee his deaf machine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
