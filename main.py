828351eae9fe5e61f5fd6352b2e2bbc9d3fd1a6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:09:12 +0000

    defend doctor dress splash profit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
