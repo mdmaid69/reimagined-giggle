e9359bfec01ae538a36dcd9b066ea34eb57b64f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:55 +0000

    across nice stop chat cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_power(work, time):
-        return work / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
