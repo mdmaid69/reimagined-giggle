bd4644f7fdd4706bdb38f781bc1c7b8c6f9ddde0
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:02 +0000

    fire see murder rubbish exist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
