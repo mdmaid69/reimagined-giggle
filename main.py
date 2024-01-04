e71b535502e2d6f65e47cd11c7831d69b046ff8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:43 +0000

    getout birth reduce pupil exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
