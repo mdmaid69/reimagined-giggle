492452a9382ff8cba7a7e007ecec383e1daffafc
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:24:05 +0000

    arm stare left friendly unlikely

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
