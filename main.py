67c231e42e21faf759cb38f8ede4831b176cdbc8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:31 +0000

    callback breath aim effect leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_speed(distance, time):
+        return distance / time
