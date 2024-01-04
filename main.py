8d0afecce0c42a5267694a696750ad46912bdfdf
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:18 +0000

    condition maintain second mark think

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
