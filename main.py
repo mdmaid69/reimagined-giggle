a1c4915cb0a962bf42c774ef50552d02e56ac330
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:44:48 +0000

    fear custom gap even because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
