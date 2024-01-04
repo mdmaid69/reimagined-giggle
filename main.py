1a55a64941c18673db7631713da6d403fb5f52a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:24:17 +0000

    entrance concern cheek opposite wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
