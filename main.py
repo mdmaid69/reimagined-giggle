f709eaa8a511b5d735a5cd2ed351ae57066974b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:13:11 +0000

    neither shine Hh sing pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
