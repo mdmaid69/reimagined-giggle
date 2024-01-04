db92e35bec1360eb5bdd0ddb3a591316e22af4f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:40:40 +0000

    yet thinkthrough sure nothing separate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
