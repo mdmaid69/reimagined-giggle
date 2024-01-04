ac5ae797e7f5dea6ee790d76b83a88631cf1e7ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:19:08 +0000

    afterwards exhibition hurry street suggest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
