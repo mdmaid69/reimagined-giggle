dd39a8cc287f35d138e68162dc8506bf504a6adc
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:49:19 +0000

    famous wet below cheque wash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
