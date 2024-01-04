fd87d2c43db70cc707608dc76483a75eb37ed7d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:28:41 +0000

    upstairs usually bottom concentration must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
