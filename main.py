7418199574f12510b5661c0615a15e635f987be8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:24:27 +0000

    process pattern keep ring previous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_list(array):
+        return list(array)
