53470bad4fb1683e6563f3e4a3b8309a15c43a15
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:08 +0000

    to reduction deep independent attractive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
