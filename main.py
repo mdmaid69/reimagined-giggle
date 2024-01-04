8401f0413d10694d8b4f58171c2879b70f1f6245
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:17:36 +0000

    task than fear use swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
