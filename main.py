6fcbbec3e3abb5436d9b321623ead0761ef54adf
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:10:10 +0000

    insist keepup balance backwards sure

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def count_characters(sentence):
+        return len(sentence)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
