4240ca672052c5283203cc7aec6fc07953834d70
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:03:22 +0000

    prize birth among rush weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
