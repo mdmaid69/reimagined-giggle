23f724f851bf2f445177a907680be491ef76ece9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:19:24 +0000

    risk warning scale ask birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def square_number(x):
-        return x**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
