cf8dc5dbb754080bfdaa6ac59e14bbbb3f4e2667
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:48:21 +0000

    suckme town flat attractive surprised

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
