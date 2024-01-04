d236ce5c4b0bffbb223fac8a95fd4dd58ab63d4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:35:06 +0000

    us arm duty guilty form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
