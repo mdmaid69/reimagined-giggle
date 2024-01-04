e49eddac48b18264f1c524197f648bf9254faccf
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:26:03 +0000

    present when bear say match

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def reverse_list(lst):
+        return lst[::-1]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
