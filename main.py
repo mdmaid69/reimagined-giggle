abc8ae8ce7514e43225396a121b39a0809575115
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:53:06 +0000

    fat summer earn lend warning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
