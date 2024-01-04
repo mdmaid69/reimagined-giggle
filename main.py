2938e72273259ab9734619cd738a0c60abb4c8b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:13 +0000

    all ourselves duty roof Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
