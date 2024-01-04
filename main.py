89cdab7602d4162e1b40f2fa5aa850c6a632d7e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:01 +0000

    post breakfast congratulate expression married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
