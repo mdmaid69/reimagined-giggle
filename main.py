c1d95aa5a09ef5540d20c18210d49fe0b99b9ba9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:00:44 +0000

    spot past mad schedule square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
