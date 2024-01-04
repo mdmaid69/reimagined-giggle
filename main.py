0dc25b7e9261e25303ae6c5cdb032c0531f72b55
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:44:13 +0000

    guess careless fall nice beautiful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
