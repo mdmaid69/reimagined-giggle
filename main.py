26aec323fecfae207e096a699f3186cedccfc40b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:43 +0000

    smooth trust book praise step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def reverse_array(array):
-        array.reverse()
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
