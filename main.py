425312f6dc9927c7b0181ec8bdf0f4f936fbdffc
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:11:43 +0000

    emphasis urgent other impressed lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
