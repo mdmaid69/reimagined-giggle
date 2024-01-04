8d2c03f82e78bfbb7d390bacac017ed1ac0ab955
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:55:59 +0000

    reduction still forward watch fault

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def find_min(numbers):
+        return min(numbers)
