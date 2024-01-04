09fcf3276b62aac0b9e5c76a3e255b5c27bc5e0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:23 +0000

    substance opposite catch pain ago

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
