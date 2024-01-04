69a77bb6f802e9b09fb806077b0541451b84f742
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:54:49 +0000

    pupil miss police this 573entertainment

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
