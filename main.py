105bb3ec8275a74052bb8776fe63a62b5755fdaa
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:09 +0000

    means tight compare godown shoot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
