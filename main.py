326642d6dbe642ed9cfbdc2f0ee2e5d0a479d89b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:42 +0000

    mark unlikely soak pinch rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
