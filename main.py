734368aaca6ca71624f40bd14b9a2d9d85c089ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:42 +0000

    hour bother powerful declare late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def reverse_list(lst):
+        return lst[::-1]
