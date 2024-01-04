c6c45c3d688d655a5eb3ef645e557cc739517feb
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:23:20 +0000

    exact guess touch wall cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
