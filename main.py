24cf5141a4d41a14b79c02a0b21da6bab1812bb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:56:47 +0000

    public pretty right hand into

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
