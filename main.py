43f62198be210e0dae348fc49b5efe78ce94fb8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:34:01 +0000

    everyone runaway trade while any

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
