1828b1c556a931f78157d48cccde507dc0009883
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:22:49 +0000

    second precise breath century value

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def count_characters(sentence):
+        return len(sentence)
