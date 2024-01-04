70c36c6803d4769b6c6c2cab555015fb76966e61
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:40:40 +0000

    attach raw broken tense closely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
