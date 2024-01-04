cd43aa473cbb1596d59207f6ac5e6d1bfc768888
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:03:49 +0000

    cut blame woman trade general

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
