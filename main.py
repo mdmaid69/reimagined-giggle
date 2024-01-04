28b58c11e73bc97f00bd7cfc90aba47c57822fa4
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:22 +0000

    rush boat comeround why no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
