5567f127882ba3855a720f78b4ab71a798581390
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:17:26 +0000

    choose image maybe needle love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
