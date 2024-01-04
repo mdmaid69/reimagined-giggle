c1f731ba3a38fe1cecb460bd6a4a7408f5d088f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:00:34 +0000

    announce slow breakdown feed become

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
