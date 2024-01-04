ba45d8d71be89ed1426b95fb1bc756f1f752e207
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:27:55 +0000

    early slight relax pretty minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
