26e7e9ed991cf3493a4db0e382758c9a5a26df1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:25:33 +0000

    sad fashion because public hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def cube_number(x):
-        return x**3
+import os
+print(os.getcwd())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
