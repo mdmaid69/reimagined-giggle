ed99a6066ac0eb4bbe918f6f6cfcecff5c23cd10
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:43:01 +0000

    hang spot centimetre admit although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
