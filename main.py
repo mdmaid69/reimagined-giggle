f2628f9c87e3ea275e926639da3efa1e026ba823
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:44 +0000

    official determined scrap criticism learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import re
-print(re.match("h.*o", "hello world"))
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
