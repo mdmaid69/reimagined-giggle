db29901f54b93261407e92f78d8198f5f59c6c8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:25:09 +0000

    read condition argue separate star

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-print(sum(range(10)))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
