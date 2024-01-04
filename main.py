0ad1cb659ba0752ba7512056dae6b9d328a1e1a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:09:16 +0000

    deaf disturb determined artificial trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
