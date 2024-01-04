2a8a2e446316cfb53f98f2fd5aa0aaf1f6db20ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:15:43 +0000

    exaggerate content true track you

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
