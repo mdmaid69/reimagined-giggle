cf929708d4d82369c6137c2a4377a175d8d290ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:10 +0000

    play why law grade burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
