61f7bc3367536b144df555155046e5776f7032fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:25:02 +0000

    safe spring no closed tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
