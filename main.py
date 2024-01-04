312f1887dc54ef22758a026caa52c761d10a9858
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:39 +0000

    lesson similar workupto turnaway hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
