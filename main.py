062f3b70deffc04bc1b7fba8ba0b4a0e7857664f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:53:21 +0000

    your considering correct no comfortable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
