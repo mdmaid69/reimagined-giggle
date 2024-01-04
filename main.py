7feaf4acbc80658e8f4477f291ba8415dc6bca3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:33 +0000

    add injury reply knock experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
