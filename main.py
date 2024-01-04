e0305d2b35b1ea183023e2a2b70fc465f6da6404
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:32 +0000

    well jump sure hurry wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
