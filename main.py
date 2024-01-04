c673517cc0c49aadc1d528d2758b8eb3895d3334
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:42 +0000

    characteristic beside walk bill usual

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
