430210b5ca7b9b01a65fb76074903b074f3cabb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:44 +0000

    Rr form sticky reduce pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
