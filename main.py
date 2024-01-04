824042c7ed3a3621b0ecbd6173b3c8c2c7cfa4eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:59:48 +0000

    top control fun physical number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
