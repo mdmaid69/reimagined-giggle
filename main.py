c59e2a85fbed5d4bf20f14144f261bf40ab5d2b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:14:16 +0000

    born which breakdown least contribute

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+print(time.time())
