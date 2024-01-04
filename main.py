680b67b206864ac031313514208d1a805334d386
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:25:15 +0000

    curious my tap once sale

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
