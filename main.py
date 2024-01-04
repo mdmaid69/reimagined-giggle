26b28df9fa34a77787d649b76923b8d0b791e7df
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:47:37 +0000

    shop once expect satisfactory space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
