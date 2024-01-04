cfee3ab46a666a6bcb49217fd4571663eff6a7a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:30 +0000

    twist Kk each practice piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
