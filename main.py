97ff1b4bb6342c81af5e10957d2ec7122b2ad364
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:53:56 +0000

    crowded minute dry near properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
