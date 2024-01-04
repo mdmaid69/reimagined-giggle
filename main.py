0b8173701bd9629ab6b220e94504b927b1d085b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:29 +0000

    commit play run transport expensive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import numpy as np
+print(np.array([1, 2, 3]))
