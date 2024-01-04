62e058d2c156ed8370119e2b2d88b4bb6c335a37
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:55:25 +0000

    trousers school friend side dry

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
