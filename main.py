73243ce82c1d00482daa689c81db793ca7f32bd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:19:11 +0000

    defeat second fault appear comeoff

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 def fibonacci(n):
         a, b = 0, 1
         while a < n:
         print(a, end=" ")
         a, b = b, a+b
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
