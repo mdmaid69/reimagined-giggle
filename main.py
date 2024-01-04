cfce6db0bbc4fbb8530662d0a1e731b800c43a10
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:34 +0000

    corner attitude again standard upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_floor(x):
+        return math.floor(x)
