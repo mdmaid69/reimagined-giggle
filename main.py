5692f4a3863a83df5a128ef5bca80c73f106ed55
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:51:21 +0000

    shop swallow after tonight sail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
