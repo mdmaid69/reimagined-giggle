ac58fe1fb9fa172915f8ce04e190f347eace422d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:10 +0000

    herself year size freedom reply

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
