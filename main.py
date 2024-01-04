0845e59c7951c5ba4285907f9cc923db7ebc8038
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:52:56 +0000

    bang silent small pure pattern

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
