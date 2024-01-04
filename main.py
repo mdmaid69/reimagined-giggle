e81c0fb0ff757fe93a93ab893becb63576a476b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:31 +0000

    confuse prove shop 566end picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
