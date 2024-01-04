5e18b747b96dcec20ba60405a9abb65cf4158a64
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:47:14 +0000

    perform to rain run open

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
