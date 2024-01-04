933b9c2bed56c6eab2d3096c452f3c22d7a1dfcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:32:01 +0000

    picture cheek quiet record record

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
