4ae39f3bd08a93a182719e1e950d892c0831d8b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:30:14 +0000

    badly very thinkthrough present increase

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
