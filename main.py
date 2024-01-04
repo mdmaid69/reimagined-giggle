ab32b4f99cdbaab47030268f2f81271c04bbd3db
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:27:58 +0000

    draw plenty rob pool hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
