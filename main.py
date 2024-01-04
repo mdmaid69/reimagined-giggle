3617d66b87a74af242d52d36ad7bf22b93d8acd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:43:08 +0000

    post pain most proper want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sine(x):
+        return math.sin(x)
