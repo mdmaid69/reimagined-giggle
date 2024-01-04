95a2200d37fc832fb8b012f13337c34227684efc
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:44 +0000

    gap always important let gate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
 import os
-print(os.getcwd())
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
