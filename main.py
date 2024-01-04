7629c16752f80757659bea2ae5895a2976199471
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:48 +0000

    job exit order press shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
