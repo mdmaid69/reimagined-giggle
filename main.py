457cbafc22e2daa214fda8272609c46d0e9d1693
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:39 +0000

    press pen check owe club

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
