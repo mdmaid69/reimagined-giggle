0f2ab27346e6eefa3555d0f73403561e122284b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:10:22 +0000

    impressive equipment can full Qq

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import random
-print(random.randint(0, 100))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
