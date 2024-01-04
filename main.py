33092f030857c1b9eaa4836bcd2f655e6297d520
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:40:13 +0000

    hang could comment somehow sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def is_palindrome(s):
+        return s == s[::-1]
