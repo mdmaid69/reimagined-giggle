1fe99ba5f5138a52dded0910fbdfa0234247e000
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:27:53 +0000

    only lift sure change main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
