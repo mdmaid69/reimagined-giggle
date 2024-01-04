65866b9febeea0ce9e9f26bbba98bcfb791e6393
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:18:53 +0000

    behave while software cum short

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
