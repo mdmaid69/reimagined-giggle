dc98d2eb30c3647c4cbc30eba548660f42e69f9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:19:13 +0000

    agreement ask trousers lucky strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
