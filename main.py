d42e2caebc5690c4302cb6b18b17d0a966c9c98a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:11:13 +0000

    kind ready house somewhere against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
