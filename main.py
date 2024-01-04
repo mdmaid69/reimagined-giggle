cff33ef24945d2c8e1f4745d1c78ac01322d4c2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:43:26 +0000

    family turnup safe opportunity admit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-print(sum(range(10)))
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
