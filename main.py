880ad72a8a2cf876679a128b054ab0359783488a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:45:52 +0000

    away boat support broad her

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
