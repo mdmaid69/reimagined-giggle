4bb5f37c57ee877bc5fee99b905db56ac1c0a313
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:19:06 +0000

    takeback relax art turn will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
