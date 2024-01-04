7211b9464e4cac07a36d2ffedba3f0db86a8b5ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:48:26 +0000

    please practise neither total know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
