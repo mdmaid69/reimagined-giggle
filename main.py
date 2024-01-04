53de3ca4aed9a576581e7d43971a0c88dde0dce1
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:28:23 +0000

    accidentally benefit acceptable introduction rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
