e365fbe303645b0a9e218572fd50ec76221fc909
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:43:07 +0000

    beginning keepfrom change bottom coffee

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
