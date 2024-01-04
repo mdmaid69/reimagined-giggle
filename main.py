4a4d0cd785345c7fa994bfb37afc8bb55ec99720
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:44:27 +0000

    tomorrow thing public whistle plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
