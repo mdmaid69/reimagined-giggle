29c0a468cc184d22ea8db22997bd2e13361acbf8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:25:52 +0000

    pool succeed error it brush

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def remove_duplicates(lst):
+        return list(set(lst))
