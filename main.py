040ca84e673b9b3f7009cd4235e07e6539bd5dd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:37 +0000

    wherever operate talk trade answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
