f5fdef59b4e3c4b6f096e248e7c69ddc80b3156f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:00 +0000

    manner intend free gate hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
