db69b0760b8fa7e27d4fdaa159a406f00570d7c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:53 +0000

    news way hollow draw giveout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_power(work, time):
+        return work / time
