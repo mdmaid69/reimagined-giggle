c1dce2b634f9aaebf52e0175af4c1d8cca9adfb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:44:30 +0000

    grand snow shall tend pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
