accfc5d14e3f895221d70b298622f95fa4c70433
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:28:25 +0000

    approve second regret post shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
