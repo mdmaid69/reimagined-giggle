a8972ca2f620ad8356a10146b55a23052fc6fb98
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:50:17 +0000

    most half tradition sort bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_typecode(array):
+        return array.typecode
