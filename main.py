4c6596416d4876e38ae4394e72c3abdab7c71fa8
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:00:51 +0000

    once thinkover wheel before height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
