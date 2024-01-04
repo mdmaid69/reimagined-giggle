639581b0d9e0354c97d5b87d5ec5a22e8b216ce3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:58:56 +0000

    bring callat situation either to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
