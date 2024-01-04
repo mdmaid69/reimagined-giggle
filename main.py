458b3b1873455b145154a86f177c08b52c1a053e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:52 +0000

    rush wear near short cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
