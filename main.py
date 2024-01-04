2c9e02bc79ba0a0a2fea08adb1a1057b0717bfa6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:21:37 +0000

    occasion suckme sound field comefrom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def convert_to_binary(n):
+        return bin(n)
+import sys
+print(sys.version)
