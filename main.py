0ece24675ad5157bb49f6280ba62fbf33a619a9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:49 +0000

    sale wise great involve interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
