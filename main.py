f8753632f5a763995e1c767b69e32225e61f3397
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:48 +0000

    dare unfortunate describe punish match

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
