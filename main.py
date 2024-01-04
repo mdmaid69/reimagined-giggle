6cf85993bde1a066c7788842eec120d7f3f0f47d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:24:59 +0000

    voice obvious me survive will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
