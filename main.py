d2b1fb12147a32a0a734a3ad686dbe905f3ce977
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:16:03 +0000

    national hungry after slice reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
