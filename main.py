c079eea770c761af93afc6b67c0dc3849f863cb6
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:52:08 +0000

    sorry before match doctor band

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
