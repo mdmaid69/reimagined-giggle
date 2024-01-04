ed6e355e6c5b1abce170c65d5262c61f279109a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:28:17 +0000

    yourself phone sail crash praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
