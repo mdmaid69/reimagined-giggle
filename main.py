75dbe8efe5a03a5c0009c99cbb9d20cb71cd6597
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:53:49 +0000

    mystery worry disappear advanced off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
