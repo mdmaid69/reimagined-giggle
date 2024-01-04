f77498284371dcd29965326026882b56c9f18626
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:49:52 +0000

    complete event tongue upstairs work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
