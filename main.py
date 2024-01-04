9a3d40b8b0eba10c2df9d627b455a09aca57cf37
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:41:59 +0000

    live between store wave bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
