77f96e95fa4e986286232a8f8834629d1e535627
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:49:17 +0000

    slice zero offer squash object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
