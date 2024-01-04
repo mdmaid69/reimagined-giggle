f89ac76349b076d34701226e6dac6836274187db
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:29:30 +0000

    pain takeoff old brush test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
