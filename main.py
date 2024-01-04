d6148f08a57a9f15ebc47e72da0c52c79caae508
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:29:55 +0000

    type pot gap discover wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
