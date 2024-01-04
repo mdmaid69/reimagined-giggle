df6a1175690b5d246ec7c96c6053011ea2c75db1
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:40:07 +0000

    destroy thanks report violence keepoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
