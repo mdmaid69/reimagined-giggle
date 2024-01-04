aaf3e8b6bd9164aefe22024733c9adfbc9617820
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:24 +0000

    round awake takeout borrow pause

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
