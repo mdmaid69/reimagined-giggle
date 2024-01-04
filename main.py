567a315d88560a93f5b6884c66328c709b7e3241
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:44 +0000

    formal narrow market pleased love

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
