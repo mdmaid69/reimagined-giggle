eb6f310c776b4ec9ddcb5767a84171783928167c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:01:12 +0000

    yes schedule save cause rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
