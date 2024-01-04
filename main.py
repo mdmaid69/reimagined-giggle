29d0f63dc0337d729d47ef1ff514e3c1cb8208a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:33:36 +0000

    pressure middle bury either attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_length(array):
-        return len(array)
+import os
+print(os.getcwd())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
