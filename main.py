e2ff8ca8c65c037df8ab809fb1c207a2c5c19bf2
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:19:54 +0000

    Ss fold effort cruel child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
