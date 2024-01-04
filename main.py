489e3cac23c4dfe1fed4422e5ff0982067f23efb
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:11:37 +0000

    usual accommodation appearance mix task

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
