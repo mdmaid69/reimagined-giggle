3dd92c2cf3b1dd85211236ec065f73f01f2f782a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:59:25 +0000

    long comeover tear write sure

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tensorflow as tf
+print(tf.__version__)
