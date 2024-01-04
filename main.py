1e5c78364350e91130f1690d54c747a51ba9db6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:51:15 +0000

    uncomfortable study pride noisy takeover

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import time
+print(time.time())
