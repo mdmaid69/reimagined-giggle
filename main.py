8b50450f1eef2fdd69d007d02c66e6419a30d1e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:42 +0000

    estimate callout shopping pump everybody

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
