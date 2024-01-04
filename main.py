f3640d01ec5eb1b1379a6daf90d9c5c5ab60f739
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:07:14 +0000

    giveback wealth half wheel service

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
