2663e4767a5c96142aa9170886b17fa7d054e1fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:44 +0000

    course Ee cross Ee instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
