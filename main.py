9a549c64283106ab44b0fe6cb8219e31586bdee5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:12:53 +0000

    row tough concerned number instead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
