e548c495e0d6c8c272d18473395f8a963d27afef
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:53:43 +0000

    commit switch goodbye ordinary boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
