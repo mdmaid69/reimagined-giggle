95461f8f39643b5d3add1344d93955d56c9be281
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:43 +0000

    close cap comefrom dig Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
