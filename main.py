a645a8163ac1b69d38f42af6771af633587a5456
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:23:03 +0000

    instrument earth inside strength let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import time
+print(time.time())
