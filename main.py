a04c327a09cd033c29c3044850f64fbb9746225d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:00:14 +0000

    soft single flight Ee company

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
