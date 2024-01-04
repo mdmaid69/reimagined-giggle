911322f19596be2b2fc3fdfc379f28363778ff3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:35:25 +0000

    quiet capital content appeal more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
