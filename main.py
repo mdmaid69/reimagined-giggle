2132e13bfb0a609dabfa536cf69cba43c6faa38c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:56:02 +0000

    rank common suggest proof correction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-print(math.pi)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
