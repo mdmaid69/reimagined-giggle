876ffb4c5767cbe810a78f688d1216eb5f56a25c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:49:25 +0000

    giveback send takeout innocent keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
