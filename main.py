7428bf9b11c4be6f2949ed536a6ec024c6bbf658
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:57 +0000

    without store pen proof whether

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
