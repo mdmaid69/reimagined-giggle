e7b088ff6a3dc4b814bc6a87300247a484aeda47
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:20 +0000

    quick shake understand change smart

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
