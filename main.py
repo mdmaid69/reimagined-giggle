306eae02242e5ee8c386a7a0cb0dee7545e73aa3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:48 +0000

    hot zero story kick still

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
