337fe537c7be608a6419cf54ae13c528ece003b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:08 +0000

    wave better reduce manner definite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
