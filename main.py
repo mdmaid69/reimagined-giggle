56353218a287889f7348f08a1319d95777832f8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:05:08 +0000

    study discuss better fat blow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_dict():
+        return collections.UserDict()
