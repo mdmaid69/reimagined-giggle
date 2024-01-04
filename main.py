01b89a63d5407e6d987fcfce597345522624cfa7
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:46:34 +0000

    clear control means lend accordingto

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+print([x**2 for x in range(10)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
