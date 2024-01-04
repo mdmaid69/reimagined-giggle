cedd42e0e92a883bf49c4f480e683cbce2fe80ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:38:37 +0000

    post petrol care while which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_item(array, i):
+        return array[i]
