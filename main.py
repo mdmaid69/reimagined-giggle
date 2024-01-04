92572fe2377ec8179c456e116888ee429550dc78
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:06 +0000

    interesting warning praise keen swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import sys
+print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
