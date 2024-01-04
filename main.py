f22f9b3ee589bd2985fbc77f83ad70d4b8e9ca25
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:33:49 +0000

    point answer shock patient kill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import collections
-def create_counter():
-        return collections.Counter()
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
