d0972778b292fb1b6ff897bc3db491c0d73c9f29
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:45:33 +0000

    quickly fetch future agreement gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def convert_array_to_bytes(array):
         return array.tobytes()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
