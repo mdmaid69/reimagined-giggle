7c8e625ae4404c14e4ae5df8151679a0d29beea5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:55:30 +0000

    project ground gentle realize convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import sys
+def print_python_version():
+        return sys.version
