c89e7eab33b95e3d7644ac1d786461ee260f58d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:14 +0000

    plate success thanks let accept

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
