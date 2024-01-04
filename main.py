2980448bd43efc98715c3b62e5ed46ef3455f2bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:33:51 +0000

    mother dance accommodation hand feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_floor(x):
+        return math.floor(x)
