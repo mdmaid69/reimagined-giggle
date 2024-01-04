b749ee78a822f8103f3a12ded71517387c6d7160
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:50 +0000

    pin block answer fire length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import collections
+def create_user_dict():
+        return collections.UserDict()
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_float(array):
+        return float(array[0])
