ecba4bca85feeed1ca4e762a8b8d23f88668e42a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:34:36 +0000

    cope enthusiastic collection object quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import sys
-def exit_program():
-        sys.exit()
+def get_array_as_complex(array):
+        return complex(array[0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
