a4988884f0b387c712e6256e4b7d73c00e273639
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:09:16 +0000

    generally town spite bill onto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_max(lst):
+        return max(lst)
+def convert_to_hex(n):
+        return hex(n)
