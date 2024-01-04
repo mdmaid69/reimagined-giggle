ca886ab612bd2b5bf538666bf8334f97f03ca5af
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:53:37 +0000

    connection spoil everywhere mad boring

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
