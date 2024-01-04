d2329b0c19c13f7a92d76d7ef6fceb63ba7fa39d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:26:23 +0000

    business practise naturally Nn pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
