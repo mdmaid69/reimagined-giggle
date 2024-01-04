2e9ff9eade1940d1c67b61e837b19f8fa9ac17c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:51 +0000

    attract neither pass summer steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_current_directory():
+        return os.getcwd()
