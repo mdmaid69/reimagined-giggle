5e1c73323c28026a8a93f61dd633b4c417abdb5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:58:41 +0000

    permission exercise clothes fair tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
