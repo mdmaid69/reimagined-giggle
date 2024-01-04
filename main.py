209315e77dc2362d31e190248ff076b7818d3def
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:17:33 +0000

    new 568energy wind afternoon sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
