95957469bdcae30cd53dcca337f48c6a89319a19
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:23 +0000

    bringup somehow treat beside immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
