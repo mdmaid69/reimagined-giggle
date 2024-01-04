ded095d1408557898c727b3daa4723aba79d2fd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:44:16 +0000

    only promise hungry crash keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import time
+def get_time_since_epoch():
+        return time.time()
