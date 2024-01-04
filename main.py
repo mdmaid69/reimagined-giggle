68484e4136c6fc9de7492455b120035dc8cc0557
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:34:03 +0000

    family familiar bit plenty error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def divide_numbers(x, y):
-        return x / y
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
