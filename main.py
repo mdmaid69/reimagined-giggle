247d51fa5daebdaba3c2e057300d1be73865ef7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:51:40 +0000

    weigh hurry lookup race already

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
