8d009819da657c87b77492316bc1034aeb3fd59f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:10 +0000

    unlikely mixture city pull regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
