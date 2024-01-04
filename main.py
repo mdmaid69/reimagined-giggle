0f4af29074fda6d8000abec916f9a01a1cb77ca4
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:17:44 +0000

    criticize original just glad company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_max(numbers):
+        return max(numbers)
