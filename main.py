b9f9d592a2df9ce4f9e1dd85f090b682fd83d136
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:57:13 +0000

    godown stare married production down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+print(time.time())
+import array
+def get_array_itemsize(array):
+        return array.itemsize
