9fd07b3899a861f85eab7721ddbcaa496bdd3d6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:38:37 +0000

    connected shallow unlucky also throw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def subtract_numbers(x, y):
-        return x - y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+x = 10
+y = 20
+print("Sum:", x + y)
