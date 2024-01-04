359ff9ad667f5dfcf78171e1f4f8f6614db474c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:48:00 +0000

    list crowd wipe contrast road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
