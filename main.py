8f530caba3121f6ae352304de8b9ba30ad93bc45
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:24:42 +0000

    ought cover arrival means trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
