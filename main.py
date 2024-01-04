7432a2604452c9f689627bddc85f262206dc6665
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:58 +0000

    affect brief answer read flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
