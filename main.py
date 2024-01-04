7690f26d4d5a272897f02459cca49c8650caf383
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:11:03 +0000

    trick defend left cough computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
