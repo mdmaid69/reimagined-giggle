898df66551bbf74782615bbe551e1b672beb74c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:27:47 +0000

    speak separate height law really

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def square_number(x):
-        return x**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
