3aca5944c391f58d3143ee7dba81e51b11b2a20a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:05:58 +0000

    Rr loan regret delicate figure

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-print(math.pi)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+print(os.getcwd())
