c48cb51dd8d9844646aa3018199b3575995a7892
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:01:06 +0000

    best reject town character appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
