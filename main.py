205a9998a2f55ed7a0d9b2d17f4e4e8ce433943e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:57:43 +0000

    confidence person music reply possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
