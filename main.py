614660786a3d45600a4f44d64f451232ab13ccae
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:59:20 +0000

    dress reduce his difficult ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+name = "Python"
+print("Hello,", name)
