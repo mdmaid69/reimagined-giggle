3488f5ec6dbe4f2db357aaa67f540eea99f121b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:39:02 +0000

    shade careful appear greeting connect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
