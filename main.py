55791129d9d322e8f8ae1a46a077f03a63bc1a18
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:32:16 +0000

    object cap target exam basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_power(work, time):
+        return work / time
+import array
+def clear_array(array):
+        array *= 0
