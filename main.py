777f44581ad32e558c1596c8553eb3455fa7d097
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:52:38 +0000

    top live workon lead violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
