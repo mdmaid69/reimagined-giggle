cfd341f9650e1c4154194441826c6c59a1f70c7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:50:22 +0000

    receive burn by my day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
