dc0ef9d28a95e858e525f13fd11f885c708edf2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:35:09 +0000

    shop yours try compete never

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def cube_number(x):
+        return x**3
