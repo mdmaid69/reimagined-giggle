e2b764f9eef7aa1fa477c3fb8003f2c5a29b9475
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:47:51 +0000

    dick tie communicate common tread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
