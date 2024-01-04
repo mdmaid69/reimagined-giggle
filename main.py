842d7c5cdc19f4b8236f956855548f39c289a93d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:16 +0000

    hurt on right lead wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
