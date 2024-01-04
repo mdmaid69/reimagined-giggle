4cbcae9e6bd83334fba13f27d24523128caf3554
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:45:32 +0000

    determined project condition keepaway reduction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
