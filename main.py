6617d36c3b2bfa79918cf02e08edfffc8a7202f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:19:27 +0000

    sea accidentally possibility into town

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
