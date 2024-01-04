c149269194f71cd4bde59130f3e70a7b0b385341
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:29:01 +0000

    acceptable rest wave brave church

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import getpass
-def get_username():
-        return getpass.getuser()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
