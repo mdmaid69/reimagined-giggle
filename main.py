5e643b8f1613478080697b8cce32bf3e559a6304
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:12:37 +0000

    drop now prefer level distant

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
