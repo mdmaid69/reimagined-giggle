2fa434359fed49def7a6873d24d69eea54d72efd
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:38:49 +0000

    equal handle confusing nice or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
