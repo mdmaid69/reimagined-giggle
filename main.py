fb2978f2d0a9803678d4187cd52ee516aa6bb661
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:42:59 +0000

    reaction tip education union feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
