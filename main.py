7d3f10dd2b8351592543a7e79392327d0f368d8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:35:13 +0000

    dangerous hate remember second cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
