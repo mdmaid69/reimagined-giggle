a535c7a2705e13b30f59574d075501cc21897309
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:02 +0000

    adjective quit direct apart birth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
