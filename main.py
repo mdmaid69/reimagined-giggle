0db99ec84ca4449315c6f9196b785cae9887c9e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:09 +0000

    obvious plant ready certain add

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
