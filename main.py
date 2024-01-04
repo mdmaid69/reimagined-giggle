bb579ae637e643af9c1a3ade635f8b2f90d39ce2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:01:55 +0000

    enthusiastic patience prove prove hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
