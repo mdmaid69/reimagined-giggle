030826611c84f06352388cdf33f1375a2bfbf56e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:31:19 +0000

    belief workon target turnaway platform

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
