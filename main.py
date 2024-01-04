d93d8ea82701eab2aa1acf2b85a5fd174d71706e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:16 +0000

    embarrassed any boring bringback runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
