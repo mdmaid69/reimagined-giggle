ac99cd3f601dd56ba8354c10f34d4774419edd3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:01 +0000

    time off coat classroom news

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
