6ea648e0efa8f54fd9b610faa6da43bf9457d23a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:54 +0000

    imagine sensible Yy vanish exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
