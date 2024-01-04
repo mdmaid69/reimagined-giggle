8675a00c55807f9562eabb0b549ed203de517fc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:39:53 +0000

    hour perform laugh wipe still

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
