5c6f53723845c172065f76860a093f3300df1e18
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:42:47 +0000

    goout season realize start dinner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def create_priority_queue():
+        return collections.deque()
