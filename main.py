115a3713ef997a4cd5e06d2ed11917c2a6697f8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:21 +0000

    persuade trap play pour position

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-for i in range(10): print(i)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
