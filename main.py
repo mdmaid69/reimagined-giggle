79a570a6e42313d0bf90c6747b98b035c1e146e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:58:42 +0000

    arrangement grade second takeout oil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
