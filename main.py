ff07594247026515bbda3444750c9ebb3b705469
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:20:19 +0000

    bury deliberate recover development live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
