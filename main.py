1ee13157c3e8e1efdda3eef9f9a9080edbdee75e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:30:35 +0000

    familiar mail themselves whistle comfort

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
