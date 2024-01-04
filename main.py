c27794241cbeea80d942203a344caaac340b1662
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:36 +0000

    display road opinion less each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import collections
-def create_queue():
-        return collections.deque()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def square_number(x):
+        return x**2
