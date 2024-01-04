9e84452b9212124d124223f4bb24093da54daf16
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:36 +0000

    measure emphasis dress trade wheel

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
