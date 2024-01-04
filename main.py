31bd18f4baf7e6700ce01ebfc0ce56417468a6e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:09 +0000

    reaction surface past valuable next

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_directory():
+        return os.getcwd()
