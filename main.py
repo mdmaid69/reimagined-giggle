9bad48194047195c8f1fcb93e45ddd9791cad1d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:45:48 +0000

    before declaration know threaten stiff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
