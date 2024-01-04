80057e9bb3fcfcd200235bd9e417e1f377dd5725
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:12 +0000

    slice skill behave practice stupid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
