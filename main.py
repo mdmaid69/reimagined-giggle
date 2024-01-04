4813c138d969b6cb258d7e32938c287753d1e483
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:07 +0000

    prevent memory taste turn bush

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print([x**2 for x in range(10)])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
