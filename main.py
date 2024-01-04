e584860c38236ad5ef77f0b8882fa4193832a8e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:32:34 +0000

    short stay autumn from course

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
