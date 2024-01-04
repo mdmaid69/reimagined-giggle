57f9272c55c6c1122f112c049275017bad15a344
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:57:35 +0000

    on official mainly safety journey

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def cube_number(x):
+        return x**3
