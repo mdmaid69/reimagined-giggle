cdfe6b131665e5cef90f19244284b3c389f39da9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:53:03 +0000

    finish better fail left brother

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
