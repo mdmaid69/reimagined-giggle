a9d17439c81eecfee11f0067a4a561af31fcc26a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:12:53 +0000

    Vv workon but friend question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
