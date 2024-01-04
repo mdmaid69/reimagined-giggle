2c48784f0ad620843aa73353590f57a0c9b80028
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:11:27 +0000

    tear frighten record belief traffic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def subtract_numbers(x, y):
+        return x - y
