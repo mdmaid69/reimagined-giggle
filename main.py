db7758b40485333545e518f4fb510d7733604808
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:02:26 +0000

    favourite contrast rid spare brief

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
