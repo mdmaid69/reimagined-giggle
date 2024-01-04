2bf382ae5a600d32150b35f2c1323a6a4b5e68dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:37:57 +0000

    swallow present record explanation tidy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
