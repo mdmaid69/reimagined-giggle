e863d6fe3216908791590746ac612afdc282f59d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:21:14 +0000

    soft pretty scream open freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
