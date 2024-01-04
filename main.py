82ce39e7539f2847ac0441eb31481a9049886bd4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:19 +0000

    herself spill forever front nerve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def subtract_numbers(x, y):
+        return x - y
