9793ec5ee6922b218fdd87d8488fef3544163049
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:34:11 +0000

    main mad trust disappear arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
