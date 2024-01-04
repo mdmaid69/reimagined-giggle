20447740410ed9d93baa2b2245dcf51e99b06829
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:26 +0000

    uniform prevent kill late character

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
