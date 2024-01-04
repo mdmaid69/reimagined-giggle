fea9dd6dd93c5235b2543bd76eba873f8b450305
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:26:31 +0000

    take actual creature contact make

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
