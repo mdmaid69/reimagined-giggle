bfad13c2f095d4b77a75c5dcba09e9de91f31018
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:07 +0000

    excuse though summer sweep bright

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
+print([x**2 for x in range(10)])
