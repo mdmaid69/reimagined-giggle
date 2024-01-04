f63663d7ec2ce1f0c1b17f75f47c0e0fe6ad00ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:45 +0000

    shock comein no experience business

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
