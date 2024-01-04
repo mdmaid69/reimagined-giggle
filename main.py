ced70df0b89a73f4561c7f52437c67b4860133a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:11:29 +0000

    peace wall reply recent circumstance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def subtract_numbers(x, y):
-        return x - y
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+i = 0
+while i < 5:
+        print(i)
+        i += 1
