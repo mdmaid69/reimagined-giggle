faf40859d44c56c71ad19fecde2db503412e3838
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:38 +0000

    sale proud discovery persuade lend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
