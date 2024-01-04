60fe5bf3c0e3f38ca5be06b209ad57b0d190b0be
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:05:47 +0000

    outside treat himself he holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def sort_list(lst):
+        return sorted(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
