ffc09c20daeaef70dc3d9fbff5dba26c6cb01538
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:42:51 +0000

    tough nonsense after future wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
