0730635c642edf06e11e82b39fef4ce2645a9c9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:14:58 +0000

    busy charge powerful year tend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+name = "Python"
+print("Hello,", name)
