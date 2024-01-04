24dcf3504fafe58c06c98dd25c815c475debde14
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:58:36 +0000

    rarely cost free skin activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-print(sys.version)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
