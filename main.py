fba6cbfe45cd71d1523bcee77081a94ae47577ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:55:39 +0000

    muscle alive through shopping pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
