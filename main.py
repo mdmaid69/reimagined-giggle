63226eec06c5a7ea271778ca8244f5614419f33f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:14:40 +0000

    principle fall set treat seriously

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
