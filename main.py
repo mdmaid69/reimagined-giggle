28d2bd6fa7d5cd0249c963c6a3e9ea524895538f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:06:46 +0000

    trouble rid comein operate muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
