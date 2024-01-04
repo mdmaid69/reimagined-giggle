5118302c88c256d6a56e4ed773683052a09cdb39
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:45 +0000

    holiday frighten rely issue seem

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
