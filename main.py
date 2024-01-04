fdd2492f87cc1b5483979340142d13609a04309b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:51:04 +0000

    clever church getout food undo

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import random
-print(random.randint(0, 100))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
