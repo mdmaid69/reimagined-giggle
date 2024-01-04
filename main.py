4d9c6e8385e7cbbbcb71e978bfbda7fc49bcaf35
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:38:58 +0000

    sure left tone negative paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
