cfd8f43154079debab9e234c7ab3f759fcac9224
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:18 +0000

    later definite stay hurry life

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
