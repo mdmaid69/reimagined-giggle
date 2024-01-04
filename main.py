189635c8d2285c4875d831415c514edc819aafb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:45:40 +0000

    satisfactory accommodation vision sweep never

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
