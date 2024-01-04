0f98c8bd406f4b5abb4fcd0e9784bb3f57d722c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:35:58 +0000

    priority catch case concern obtain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
