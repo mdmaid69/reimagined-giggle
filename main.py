7f37bdbf0f72efddb55e9bb77828bb25bd2db5b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:04 +0000

    regard guilty scene dear above

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
