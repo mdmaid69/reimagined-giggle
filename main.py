d2842e040ebc4d0dcb90bc9c42c4f7359bd9e6b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:27:37 +0000

    flat inquire impressive note innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
