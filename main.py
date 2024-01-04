b5f4fd93ff71c024b4b62b37e0b0e65ef58bf978
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:32 +0000

    tape comment lot meanwhile bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
