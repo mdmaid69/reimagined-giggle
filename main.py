163816dddda42b93a4630bf4ea73790b3b4dd801
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:52 +0000

    gooff natural turndown day more

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
