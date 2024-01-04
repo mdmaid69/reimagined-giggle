30d7a197fd69f4fd176d9a0bfd5edb6af605386a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:37 +0000

    discussion pair light somewhere practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
