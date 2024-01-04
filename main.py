487e5a2e83b5218aa9c6d8bbd2e9f3f40d387ccd
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:28 +0000

    steal limit fashion half tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
