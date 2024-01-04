ac6db1c54166d3a95e8597773d91da06750a2fb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:17:22 +0000

    petrol ability minority method bet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
