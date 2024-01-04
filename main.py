0fdda32976f3b1faeee101e341d12c7fdd761789
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:29:01 +0000

    tend Kk unpleasant respect couple

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
