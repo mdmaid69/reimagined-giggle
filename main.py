f147715773607cd2ca5ca8d464dda185113ac23d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:01:14 +0000

    expect foreign his bringforward foreign

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_counter():
-        return collections.Counter()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
