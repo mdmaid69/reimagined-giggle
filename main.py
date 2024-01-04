296b931b3083273a559fa68d749fddc82e923419
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:09:45 +0000

    sleep insist takeaway various slight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_str(array):
+        return str(array)
+for i in range(10): print(i)
