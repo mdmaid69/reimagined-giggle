b1bd82ef06be3ceb2227700b3542da5a04f7e50c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:17:31 +0000

    suppose meet set smoke wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_list():
+        return collections.UserList()
