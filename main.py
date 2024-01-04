4a532f2091ec7bf05e34f52ac63c3ce0fdd5b7bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:35:58 +0000

    oil central noise movement swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
