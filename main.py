19e42ea06c2e15ab3b8a5bac09a9adfe7f7caca3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:07:28 +0000

    key deaf plenty name some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
