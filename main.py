f7bd435a851f4bf74617407d80949eaa43d2d43e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:39:28 +0000

    wet whistle matter keepaway either

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import re
+print(re.match("h.*o", "hello world"))
