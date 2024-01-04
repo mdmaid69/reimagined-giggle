fc0fbc422c4a7142d121ea1d85af3d679d2f0088
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:23:14 +0000

    performance main scrap hers for

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_set(array):
+        return set(array)
