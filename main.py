ebea87e208bc89c5c9f21df8c3c784fc8a0394ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:20:50 +0000

    fear about 572enter scene check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.ctime()
