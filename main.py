0f93210c4f91ca46bbdb41938cad96d2556a3ff7
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:09:03 +0000

    judge original shout warn realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import json
+print(json.dumps({"name": "John", "age": 30}))
