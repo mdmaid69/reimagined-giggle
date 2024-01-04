ad60c397aa67b177161d892e05ebeacc3cbb44da
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:18:33 +0000

    knowledge middle formal of moment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
