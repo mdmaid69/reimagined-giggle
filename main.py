675bec906367ee4a90f63cb90ecb86201d7d518d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:32 +0000

    failure cut Tt create interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
