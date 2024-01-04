5b49d4a46480ec3fbf434971149d027cbcdc64cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:45:54 +0000

    picture brown reason fortunately cross

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
