5aeed1578682034a9370250324fcbe9fd61dd0fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:43 +0000

    getback familiar belong attempt think

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
