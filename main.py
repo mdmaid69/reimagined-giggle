4c94e5cc18f7f563a66921586a1ab6f2e5b23b01
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:22 +0000

    congratulations safe train border impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import platform
-def get_os_info():
-        return platform.uname()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
