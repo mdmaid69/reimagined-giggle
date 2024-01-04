ba261914e5e466db399ea0910fdfb4324fb012f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:14 +0000

    only father watch possible odd

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
