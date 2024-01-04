cf36c502d0fc530288a8f2aa1084a37ecf5ca5b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:31 +0000

    shrink family dish game near

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def convert_to_binary(n):
+        return bin(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
