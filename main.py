27bc3cd975f77c00525ee80f61eb4506f548e702
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:31:05 +0000

    skill gift pass thin depend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
+import sys
+print(sys.version)
