a4183cadfa21f48d94000169cbd21148b990533e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:07:15 +0000

    kind unlucky air almost match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
