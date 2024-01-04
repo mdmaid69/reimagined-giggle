651f74343548023a540a6ebae39ecbe01dac0316
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:02 +0000

    glad callout tidy charge away

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
