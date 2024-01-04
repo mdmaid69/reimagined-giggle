191fa34aedb7b0f14774fe2f5008a73f4258369b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:45:43 +0000

    unfair rush study turnaway getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
