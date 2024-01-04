b935c455858b2addccfd1f8de9ca2cb509557123
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:34 +0000

    knowledge steep reply test unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
