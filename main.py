356c3fbd807aa953ccdd92df7582721b4ee58585
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:32:15 +0000

    exaggerate difficult father hot warm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+name = "Python"
+print("Hello,", name)
+def find_max(lst):
+        return max(lst)
