86a0c37f45c446da29f41d0d3a117b3f0c73fe59
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:48 +0000

    copy target rent calm stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
