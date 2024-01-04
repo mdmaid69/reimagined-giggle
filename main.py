c26ccfbf0de6106dca4a8b06b0b28b48133f7808
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:20:10 +0000

    world through equal flow brief

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
