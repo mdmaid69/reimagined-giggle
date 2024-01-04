4972622e16b79053f116ebdb3c53f4c8b85d1377
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:49 +0000

    content secret family every treat

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ def create_thread(target):
         thread = threading.Thread(target=target)
         thread.start()
         return thread
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
