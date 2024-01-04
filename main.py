d413a87fcac0df4829682048c38b4987434a1d69
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:39:17 +0000

    choice smoke buy everywhere similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
