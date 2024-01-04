2d8ea758a8406c1136930de47eb130ae0594a358
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:49:54 +0000

    cheat appeal unkind ground hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
