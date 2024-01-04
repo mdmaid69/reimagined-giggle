8cb670974bed63d90b47594c49dcc4f51c01cd4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:23:37 +0000

    makeup lesson appeal comeoff shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
