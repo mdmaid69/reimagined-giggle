afa7c62214c5bba8e34e23de2a3bbd7e84c3d0ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:57:47 +0000

    dare scream taste rent strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
