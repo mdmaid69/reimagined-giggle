6e270420e721f4f64aebf1328a5b02c08be0b26a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:38:14 +0000

    there heavy getoff respect unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
