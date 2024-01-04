a6fe6e8beb851515035c135420f72608250fb59c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:11 +0000

    clearly protect defend giveback undo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
