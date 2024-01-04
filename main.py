7c61404e91d67b0aa9359565d74b156486630de2
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:48:22 +0000

    ill minor character bag long

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-name = "Python"
-print("Hello,", name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
