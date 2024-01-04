b9a281c9345e66fb3513d22d8841844005eb8987
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:30:22 +0000

    hardly seem obvious unlucky breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
