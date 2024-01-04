85c840d55b92c50e9dbd6b30c11a6ba176f17429
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:03:06 +0000

    plate kind anywhere neat cruelty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
