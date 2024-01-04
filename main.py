ddabb4fbe50ca451c86c1eaa10b59da5908a1b4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:18:18 +0000

    grateful nurse thought child shallow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
