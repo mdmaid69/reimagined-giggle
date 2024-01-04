aea46cb91f36ed55c465178107c65950434edf08
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:52:32 +0000

    determined decide stop broad cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
