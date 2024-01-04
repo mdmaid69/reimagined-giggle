e5690bb9248ba9003c1b031386ea0f8060b2f310
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:24:04 +0000

    advice straight breathe shut rise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
