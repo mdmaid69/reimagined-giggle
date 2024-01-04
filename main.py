b1f149b6b80b9ca287a644a7a16167b50462a033
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:11:57 +0000

    city their twist subject profit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_set(array):
+        return set(array)
