66e676754a606147d8d08c0c2c4541f373c337b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:23:00 +0000

    term sister emotion closed across

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+for i in range(10): print(i)
