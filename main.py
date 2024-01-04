be352e280fb5903143b470fb00265346e4d6e528
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:00:23 +0000

    pencil vote arrange dear bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
