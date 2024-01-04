4b71dea73d1aac6fcb9406f8cb01b1d67deaef28
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:30:03 +0000

    niggaacademy if raise fault herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
