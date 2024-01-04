eef5da0f931fa1300795f0423b36f31d4d05065a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:46:27 +0000

    steer instrument ought wide herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
