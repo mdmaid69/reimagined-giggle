2312951744855816f007840c42aaab314ab8eeee
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:46:17 +0000

    decide cheap of variety except

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
