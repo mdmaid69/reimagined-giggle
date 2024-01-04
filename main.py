67c9c4eee17f4ca763524d5f8ca6ded99e670990
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:38:19 +0000

    deliver wall sister divide slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
