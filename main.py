afa172c0333c3ffdf31e6dbd8e91aa9c80b2c6da
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:45:57 +0000

    considering manage family goon suddenly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
