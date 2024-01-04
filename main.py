e3df44026f13cedec74aba8ccb3c10da80d49706
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:25 +0000

    bin information determined pity drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
