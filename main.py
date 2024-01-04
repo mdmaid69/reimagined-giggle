be5c9e1a2b39313ef00b427ed10367b12cfcbda2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:37 +0000

    neither far equal destroy weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
