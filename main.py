a0028818596526598497a3617790c86f1fc7e5f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:47 +0000

    base between apart respect contents

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def cube_number(x):
+        return x**3
+import array
+def get_array_as_bool(array):
+        return bool(array)
