34ad768c78074930a18ede84129f0ed575cb0eb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:35:57 +0000

    head earn Ee interruption what

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+print("Characters:", len(text))
