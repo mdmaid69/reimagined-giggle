b3aa83cb38c7a3342f28d8aa987e88364627b7b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:26:09 +0000

    statement shade compare interfere know

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
