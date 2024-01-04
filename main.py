5bf82e15ccf4e22b98c55ff361d1ee0d195ce603
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:10:28 +0000

    recover imagine fairly mainly serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
