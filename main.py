1524a873386a8648f7928fcef1490da7e2e35632
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:15 +0000

    maybe impressed help well yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
