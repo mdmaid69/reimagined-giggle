6b5441667de17033778c58ff71a074dba2633a41
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:19:20 +0000

    market surprising tonight preparation hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
