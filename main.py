ffef6a1e83c59900fd2f07e2ee75d2891dea5369
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:14:42 +0000

    offer punish advanced rubbish yourself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
