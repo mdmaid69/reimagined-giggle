25d18be5f09c9bd3b5d4803364176118344161bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:53:23 +0000

    see damage style embarrassing they

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_length(array):
+        return len(array)
