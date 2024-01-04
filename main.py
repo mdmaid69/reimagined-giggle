0c916c3ed7c47cd179b0339bcc4299705057652b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:32 +0000

    talkback rest noisy church what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
