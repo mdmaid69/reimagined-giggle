5b43ec993f2fc703ec170c87912e696326e3d981
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:09:16 +0000

    weather tragedy in keepout bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def convert_to_octal(n):
-        return oct(n)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
