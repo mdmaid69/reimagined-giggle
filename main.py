b1046a38a8106e7e9280287085a901477c8f1017
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:36:53 +0000

    hospital goon neither possession deaf

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
