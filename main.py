c30e46ee31d28bf2adac284b4ec061bb0d1d5846
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:01:54 +0000

    still by during immediately available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
