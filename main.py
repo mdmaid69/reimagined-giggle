1a0ab5cf6e4ce5cb98236968f7382e1c014d6c2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:58:18 +0000

    few uponon run reach vision

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import sys
+def exit_program():
+        sys.exit()
