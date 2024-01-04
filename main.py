2f50b8ddf5df6e6786c3b161b65f6894106001f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:20:25 +0000

    shine market unit medium holdout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_pressure(force, area):
+        return force / area
