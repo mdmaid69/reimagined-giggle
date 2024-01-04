5bc53e7fb8315dcc70c21c352d95d121d1d0c15f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:19:04 +0000

    comeup recover joke pass frighten

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
