19591ca73811620a5b1dc3b138c270911c968f74
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:49:44 +0000

    reserve around Qq sometimes hurry

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
