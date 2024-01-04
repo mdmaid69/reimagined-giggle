a7e987f8f62f7ed3217d94a8d776195c1823525a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:18:32 +0000

    me skill greeting tap storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
