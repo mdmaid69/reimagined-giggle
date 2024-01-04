186414f516935d36051a9aa4c4cc82b062cf326f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:27 +0000

    positive pocket most giveup free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
