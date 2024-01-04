2ace708c181a67da03e7bf0a5b34b25112d23704
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:24 +0000

    recent badly remove own stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
