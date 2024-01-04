dd68cbc0d8f3560dd92736910211ea31ccb3f124
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:50 +0000

    exactly limit off pronunciation after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
