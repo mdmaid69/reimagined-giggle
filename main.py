555e5c8c45bee6556f749fb8e6ef4fcececedf97
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:12:38 +0000

    wait pour attitude switch address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def get_array_as_bytes(array):
+        return bytes(array)
