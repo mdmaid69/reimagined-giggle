a06a564926ca67b5a162cd00ec3e8a979cb17610
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:08 +0000

    hope design strong keepoff underneath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
