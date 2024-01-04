3597ebbc1b8dba7e881d57df14afef5872c99770
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:05 +0000

    watch stamp border shape equal

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+def calculate_power(work, time):
+        return work / time
   import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
