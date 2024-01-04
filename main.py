09bd671139b78c770b5f9f7c8cb200d6ff4b2dd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:29:18 +0000

    silent accommodation ours damage part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
