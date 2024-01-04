5e27fdf769db2d1559d621d70725257a28efdec3
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:35:57 +0000

    giveup regard them always become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_cosine(x):
+        return math.cos(x)
