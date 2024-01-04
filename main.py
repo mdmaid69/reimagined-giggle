7b5ea99675372d7743ff1beda492c1d69824ae46
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:48 +0000

    when cover compare as deliver

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
