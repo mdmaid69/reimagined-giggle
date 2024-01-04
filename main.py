4b23337906ecd0fde24c3c0cf09e5499971ad0ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:00 +0000

    find Uu hope artificial entrance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
