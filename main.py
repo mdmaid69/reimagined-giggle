ffd988c0f58fa1aed3322ff8d2e588a7d8bd1c6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:45:24 +0000

    communication earn tough 569enjoy fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
