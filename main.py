33481eaa2c81560d7c98b7d934eca9f6f147c777
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:19 +0000

    slowly belief smell onto rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
