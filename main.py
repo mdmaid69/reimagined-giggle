7fc7ab1d300cae005b8ed999f5005b06d3d23ef1
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:46 +0000

    exploration trip stiff breakout responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
