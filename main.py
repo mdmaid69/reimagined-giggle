335eb317bc802ff303d5fab1f6f10acad8dddc00
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:54:51 +0000

    break strike meeting manage better

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+for i in range(5):
+        print(i)
