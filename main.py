7ca2831a3efdd6464895fd637e3bb53a1b688234
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:35:03 +0000

    wind 569enjoy behave handle bringup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
