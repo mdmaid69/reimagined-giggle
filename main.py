0e4dfc319e3ac6822e2fa54f549f7a42c017a24a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:33:17 +0000

    city lose influence track whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
