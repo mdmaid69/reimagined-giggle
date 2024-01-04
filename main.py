278e47b987b2efdd873ba07387cc243b39b84fff
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:29:02 +0000

    across responsible smell minimum news

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
