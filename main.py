362aceb36ecea2014b534312a7098e8c51666242
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:52:53 +0000

    peaceful emphasize himself once snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
