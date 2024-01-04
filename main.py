8a0d80f71205e6983442af515d277b14a5ebe367
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:03:07 +0000

    complicated scheme exercise hand conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
