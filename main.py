8ba0b301a2c5bdf9e4f6d39ce94896604176834d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:53 +0000

    normal getaway unfortunate attract mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
