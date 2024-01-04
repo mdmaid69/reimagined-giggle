72483588e31a9c80938cc9fb774dadca591a949a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:36:10 +0000

    sea plan instant drop workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
