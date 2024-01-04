73a15da5a70e71d0c232f4518b6e1ae448f3754d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:54:21 +0000

    combination front slice spot breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
