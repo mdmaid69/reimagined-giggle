a26342d99f2477f1fe8f59b2c2e8fbe125499350
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:04:00 +0000

    offend someone beneath problem cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
