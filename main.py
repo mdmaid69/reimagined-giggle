9ac5fbc9272fd0c1be0366bf801c241627dd9681
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:23:09 +0000

    fit operation coffee glad pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
