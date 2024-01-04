e03c0c5470c7ed8a2dbbef49df7bd4ceedabd7b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:27 +0000

    lost different without minimum injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
