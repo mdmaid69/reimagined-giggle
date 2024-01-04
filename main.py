fe3715234f85781c8cdd678d118a5960322cc1f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:01:52 +0000

    cool burst party breakinto actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
