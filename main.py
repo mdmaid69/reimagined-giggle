1a1d352b447619f24b8cfe0c52f111758b8a8cc3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:30 +0000

    pleasure deep alone 566end our

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
