73b66df592ee9b08003b66bfe2d09795f3d17c84
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:00:27 +0000

    light giveaway bet apart realize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
