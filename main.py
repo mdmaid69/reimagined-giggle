238c7d9a16fca2dabf9c26d673a7c285c54f8034
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:23 +0000

    within Kk value scare spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
