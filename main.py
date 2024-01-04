eab2fdc7c9e4f35c8583949dfea2f2801bec9105
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:53:55 +0000

    neck might plan fun dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
