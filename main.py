2791f2402edfdd8607aa986dc4bf08621e6e5013
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:50:22 +0000

    disapprove once tread shadow whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+print("Hello, world!")
