24f51413607850d846be132823a322fe5108dd75
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:42:20 +0000

    visit kick nowhere frame getaway

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
