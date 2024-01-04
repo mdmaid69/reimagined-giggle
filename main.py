66d5f3a4bd05f001a6ff5ecb4e92616764ca0982
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:23:21 +0000

    statement example around spread come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
