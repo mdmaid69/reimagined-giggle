382c8466e62b4dfdc96a96618015b66f7d1e3b7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:46:15 +0000

    fix lot club tight worry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
