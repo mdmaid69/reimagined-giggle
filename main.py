41f8b7537c387a2eba82c12b1cc8b909bf28fa99
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:53:38 +0000

    authority close opposite ever hang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_array_to_bytes(array):
+        return array.tobytes()
