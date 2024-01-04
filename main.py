4b4ab35632729b1aa2360cf4c1ba5f71a9bbbf61
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:10:38 +0000

    brave judge odd burst tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
