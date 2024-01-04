deeebd98ededc95e261a06f2800f1ea09cc15a76
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:45:21 +0000

    brown live until Zz purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
