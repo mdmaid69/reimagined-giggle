54543402107dad38122bbd53c98cb38df5b3d8c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:40:07 +0000

    snow keepoff connected nerve preparation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def convert_to_binary(n):
-        return bin(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_acceleration(speed, time):
+        return speed / time
