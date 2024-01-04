be7b2709a0c5ae9f696aec00834043547cd56945
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:37:00 +0000

    scream prove me promise load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
