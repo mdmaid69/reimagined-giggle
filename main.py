9fa434f3c70a158ae6bbc254c45584410f4ba653
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:18 +0000

    opinion half up situation murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
