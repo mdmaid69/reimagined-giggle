63f202fabb5029229b94d117625f98908e6b278d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:35:24 +0000

    vanish shade ours breakdown suddenly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+def sort_list(lst):
+        return sorted(lst)
