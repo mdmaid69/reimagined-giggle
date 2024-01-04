767eda2d58dfe5a498a53247b3499f4caf96ff2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:41:56 +0000

    game calloff distant none pencil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_power(work, time):
+        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
