b7b7c50ae05c26fa0f471a5f04756bb4be809784
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:09 +0000

    live use public go worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_item(array, i):
-        return array[i]
+def check_if_array_contains_item(array, item):
+        return item in array
