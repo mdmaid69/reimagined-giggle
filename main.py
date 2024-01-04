d278c43712bdf1a3dbe57fb732a76741bdf997f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:04 +0000

    what past colour blind backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
