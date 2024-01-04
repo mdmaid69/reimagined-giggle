7a7ea42b82199152786f628fbab6711bd8832fe2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:57 +0000

    back already turnup worry duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
