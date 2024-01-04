e719d086ddb115d988f2047ac8704197bbc2e02d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:44:33 +0000

    her discover fear shout ocean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
