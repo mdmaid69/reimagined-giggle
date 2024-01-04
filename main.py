f126cb707e1b90ec703d4c744e03ba5f1cad876d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:59:12 +0000

    gooff answer development independent kick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
