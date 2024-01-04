4a73928c84ed6f33305bac5997d094c6f2d662ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:27 +0000

    service promise by formal supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
