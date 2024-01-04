8c62e64a57f4a538deeb74a20ca8cfdb67c3f467
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:43:57 +0000

    duty worse broken keep goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_rectangle(l, w):
+        return l * w
