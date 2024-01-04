d8e579ade53b3317f4be4554ac1de2662373a06c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:28:16 +0000

    free recognize hurry gap soak

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
