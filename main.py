0d12a5f55241fa61285795f7e31c0f2f39c20b76
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:21 +0000

    tend thankyou put workupto period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
