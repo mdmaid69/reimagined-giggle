6224c6a459f01f9f9d53d0e81e89068e84b0be7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:41:39 +0000

    address clean interesting invite birthday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
