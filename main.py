79ed235b32878d28f8b005c49b6076066f5210e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:33:56 +0000

    firm dead load minority familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
