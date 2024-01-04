11d90aa1a472fc01f04778d7a264cf665825173a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:27:00 +0000

    impression destroy create turnaway fault

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def get_array_from_string(string, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
