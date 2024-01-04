1e8661018e5147fc307ec2936e2eea773a841984
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:44:43 +0000

    open major middle cheek whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
