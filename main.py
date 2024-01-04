dedd76f00fb2aab8e69a38f173a7e6b3792e6285
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:14 +0000

    neat talk experiment gather bound

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
