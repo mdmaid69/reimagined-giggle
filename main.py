3e0582fb3b8dc16f73e34d7c085e0da14deefdb4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:04:08 +0000

    risk ability risk soil create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
