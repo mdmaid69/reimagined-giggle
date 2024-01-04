0337153a6dd794794e1d4bf343ec8e8d51079385
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:07:41 +0000

    limit makeinto whenever keepaway patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
