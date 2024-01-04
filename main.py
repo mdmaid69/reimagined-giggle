25d012832a01b3086510a7713aff661799ba7e4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:00:02 +0000

    cruelty here suck wing copy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-print(sum(range(10)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def is_odd(n):
+        return n % 2 != 0
