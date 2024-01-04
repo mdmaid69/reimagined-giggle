d451c7683c32b760cbf63791dc8b81736394483a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:52:32 +0000

    certainly class lock head recommend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
