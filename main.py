0320d44cfd57b479fb6d4ae2c9d15aa1711afd0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:47:01 +0000

    arm very squash invitation fond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import sys
-def print_python_version():
-        print(sys.version)
+  def convert_to_octal(n):
+        return oct(n)
+def find_max(numbers):
+        return max(numbers)
