7adc0bb36b9ca6c29e4cec9d6d294c806d84fab2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:29:37 +0000

    father write fall charge supply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
