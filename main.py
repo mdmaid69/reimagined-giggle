35e48a6972cde25b19fe55cbcbf81d0a2893b51d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:28:48 +0000

    comfortable land judge all we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_work(force, distance):
+        return force * distance
