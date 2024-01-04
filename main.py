4ff8e811bb683ebb4f286750813cf2a86ed1dc0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:19:14 +0000

    behave environment flight phone considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+print(sum(range(10)))
