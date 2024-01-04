bbe4d349827d210af37bc23a36c693c740bd44d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:32:39 +0000

    large close borrow Ll occasion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
