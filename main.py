7b1251981039f1dae2f25b6af196ade53d5d561f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:59:42 +0000

    smell callup operate rough formal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
