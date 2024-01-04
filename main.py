be88ce516e4b6cca86fac649b9b4c62e53712736
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:47 +0000

    hate among whom follow second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def get_array_item_count(array, item):
         return array.count(item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
