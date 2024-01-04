3fa1460f0ab634c34fa4324390d08730edb083a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:17 +0000

    visit mind either now blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_sine(x):
+        return math.sin(x)
