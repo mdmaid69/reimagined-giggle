4ebffdee0a4bec63fa5d11846523661dd371d496
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:00 +0000

    mine list tread many who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
