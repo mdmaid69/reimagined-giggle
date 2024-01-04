a2462ff93bebc65236d9d5064614a9c96773a84b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:14:42 +0000

    quarter yesterday crash fast imagination

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
