7288d0e7c0fc73ba2574b82dad479c69ae31d1bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:25:21 +0000

    566end treat stamp funny unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
