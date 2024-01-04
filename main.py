8bf01448dee402d6a47c479b94ca366377cdd492
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:11:12 +0000

    spill wander soil persuade along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
