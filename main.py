108008961f357b4925dad09ae0aab96c2305adcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:45:17 +0000

    demonstrate her question dance hospital

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def create_user_dict():
+        return collections.UserDict()
