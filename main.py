dae821b856a5f34b550882d491891efb85237003
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:52:08 +0000

    result gather bored operate foot

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def find_max(numbers):
-        return max(numbers)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
