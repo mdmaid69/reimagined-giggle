b8b74bc6977762c076edd934de6c88399b2142c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:56:50 +0000

    decision earn situation figure occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
