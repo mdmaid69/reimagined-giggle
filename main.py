d4f6d7cb30d0a07509a8162b522dbf6d6d226cd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:19 +0000

    write arrival difficult general result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
