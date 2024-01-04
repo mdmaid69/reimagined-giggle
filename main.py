322a2a8fc838ce0d254f2a1b4094f0be5e270056
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:51 +0000

    Jj speed art unpleasant transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
