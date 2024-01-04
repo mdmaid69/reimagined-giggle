714199f928cc17816ec62d8b24cf3d537d61b2c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:30:16 +0000

    expression father perform be regularly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
