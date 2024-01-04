bc904ebd2e69ec71fde614a23f523b96595323b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:50 +0000

    shake listen instrument idea forward

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
