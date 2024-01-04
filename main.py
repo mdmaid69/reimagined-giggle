1eed8194ef77ccad6b217b7f7452a38999651d20
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:29:46 +0000

    busy tie dress partner father

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_min(numbers):
+        return min(numbers)
