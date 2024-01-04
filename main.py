454e46766dcbfe74c9d7eacd81d9238ff58b1731
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:57:30 +0000

    song hurt better sister thin

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-print(math.pi)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
