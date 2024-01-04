6a00059aaedcbc32484c911572c4dec2a68b0baa
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:18:57 +0000

    can correction tonight lose afraid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
