6e8d6c24cd996378462b3973086c97d32a77886b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:27:28 +0000

    success calm mail passage around

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_speed(distance, time):
+        return distance / time
