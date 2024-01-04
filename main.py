33eab839d5ce6adb98b4a8898dc2663f1ea30415
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:03:18 +0000

    during free continue trade dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
