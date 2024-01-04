d35e4a15690dd051b2a1c40389e7677d3df08420
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:19:44 +0000

    quiet keep light freeze live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
