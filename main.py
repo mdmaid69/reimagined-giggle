7054084aa0d7b1f4398d2a7491912d03aed82e4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:43:33 +0000

    reserve character left study smile

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_speed(distance, time):
-        return distance / time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
