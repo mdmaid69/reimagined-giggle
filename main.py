68d4ebc485102e1d4ffe0566813f8b2686077048
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:14 +0000

    reliable partner listenfor bag drink

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
