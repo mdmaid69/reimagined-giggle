ce81898e2fb3e0dcad9df099ad2a9856801e6336
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:31:24 +0000

    loud pity well fair constantly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
