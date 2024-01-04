b0a5c24016c2a8937c37cbe9f3739f2668299805
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:00 +0000

    climb quick platform boat comeback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def square_number(x):
-        return x**2
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
