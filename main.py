ad0e552f67aeecc5b0eae753912dad827d76cc6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:51 +0000

    form strict heat ever shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_characters(sentence):
+        return len(sentence)
