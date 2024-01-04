eefb9223481c7dc84842a9ff8026d73902ee20de
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:28:58 +0000

    crime control computer wake tense

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def set_array_item(array, i, item):
+        array[i] = item
+  def remove_duplicates(lst):
+        return list(set(lst))
