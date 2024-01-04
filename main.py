8de9cb7cda21a95225b6d3bddc17fae894317ffb
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:24 +0000

    straight television well terrible case

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_power(work, time):
+        return work / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
