2ad07df2f099749194ce868a228c13724c37c5dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:23:31 +0000

    occasion sweep separate fall favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def reverse_list(lst):
+        return lst[::-1]
