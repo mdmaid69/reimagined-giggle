070c2dad07e381c97cb1a50881263f7fc480b9e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:16:20 +0000

    divide fashion sore find model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
