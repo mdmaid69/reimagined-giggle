fc358f20fb40dd939e120c1a7bbc9a2625c3f3d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:14:32 +0000

    group admit design bury thought

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
