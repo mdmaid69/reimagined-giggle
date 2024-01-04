6e1f90439bdb311ac2131b287abe03a794346cbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:43 +0000

    giveback month vote view quick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
