64a85d0055c7002a9f3b5bd023a12a0a9eb80826
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:47:52 +0000

    reasonable dream demonstrate second period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
