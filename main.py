4f7a52d4e05606213f165d0598d1309f6a48d3eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:14:23 +0000

    first actually sight spoil achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import os
+def get_current_working_directory():
+        return os.getcwd()
