2846ffd6bb5048df1ce78ac9189b8fab3baa490b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:25:14 +0000

    welcome wonder possibility takeover lot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_binary(n):
-        return bin(n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
