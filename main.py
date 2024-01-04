738191aca41c2376dbe975c9d113f5a1b921f7b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:00:17 +0000

    runout meeting right giveout suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
