530c1d3f4627a0bb69a1867d9ffb5566be49e216
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:25:32 +0000

    brief broad tonight estimate operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
