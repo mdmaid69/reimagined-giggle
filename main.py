8541936d70a47d4615ae13755e23f43d4f1df45d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:03:15 +0000

    tour lesson 565end suffer load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
