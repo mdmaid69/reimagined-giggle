f1cf349995343395d869eb22f7bc601f1851305d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:54 +0000

    conversation serious free along comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
