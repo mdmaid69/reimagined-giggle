0105501ae4b1cea7baf74c279419c8becf758a50
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:07:43 +0000

    suck lead Ww shut breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
