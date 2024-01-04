747011a4fb149fb483ef0bf321090e65f7708e03
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:01:21 +0000

    owe keep Tt some escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
