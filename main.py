405d39568bff76f62c2033a9f4ddd394d570a2de
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:30 +0000

    name command outside question rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
