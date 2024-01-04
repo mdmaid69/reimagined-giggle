5667eb380fe5893fa043e76622299f0d840b5b2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:36 +0000

    original honest holdup opinion comeround

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
