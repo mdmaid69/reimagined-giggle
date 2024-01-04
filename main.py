12c38bf7107232c967dc13aff55ad71f34a3f272
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:04:26 +0000

    label can limit else describe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
