d6b2eab6a64c08fb7c1c6f666e59e4b9beb23833
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:05 +0000

    catch the influence pain however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_current_directory():
+        return os.getcwd()
