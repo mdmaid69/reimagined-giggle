ce080eeede26cdeaf16842c233bf6b4b3019beae
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:36:36 +0000

    change bank knowledge answer gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
