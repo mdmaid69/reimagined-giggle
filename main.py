317a49d319e1fb4d243cafbede4cbd13832cd67b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:39:28 +0000

    trade side space believe emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
