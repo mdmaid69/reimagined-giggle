3a4fa774865c3a288662217a1c56d98958802cfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:17:06 +0000

    bound bottom fail description paper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
