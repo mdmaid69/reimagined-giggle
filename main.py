9a974ae4758e83d72df3767e38b8b2ca4095b4eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:28:30 +0000

    anything power if fun motion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
