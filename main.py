fa6e66856e2483c5d444539b8bb50761cc875c4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:41 +0000

    change outside from set within

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
