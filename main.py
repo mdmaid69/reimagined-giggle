db6ade1d285a9c901e411555eee0a9ad0c2699e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:39:14 +0000

    carry away attempt copy off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
