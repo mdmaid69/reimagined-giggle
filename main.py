6bed188ed1ebc6fc721d402f4cea7c00abfa50d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:06:09 +0000

    already invitation mistake make attract

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_current_working_directory():
+        return os.getcwd()
+  def convert_to_binary(n):
+        return bin(n)
