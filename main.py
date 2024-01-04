ff3448eba147240dad403e6abe3950484668e7b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:24:41 +0000

    price really reliable vote business

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
