ef89a7eace414937df5db3381dc6cc79b404faec
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:02:04 +0000

    excuse rescue criticism correct common

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def multiply_numbers(x, y):
+        return x * y
