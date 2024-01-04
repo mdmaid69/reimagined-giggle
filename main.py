4af56de9a3b2b13dff0d4ba6e726c5d20e141c6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:59 +0000

    favourite platform stop passenger knowledge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
