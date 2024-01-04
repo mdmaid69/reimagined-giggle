03e628b06fca31a2a7b44c531df631b4f378ed3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:24:59 +0000

    priority sink brown fork open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def reverse_list(lst):
+        return lst[::-1]
