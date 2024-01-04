0b4225cd02bb7412b1223d49c26231014ec70073
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:37:08 +0000

    hurry pleasant declaration today test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_min(lst):
+        return min(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
