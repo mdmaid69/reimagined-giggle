bbb11ad3fec0ae326891b25abd50506610fcde33
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:58:01 +0000

    pause dare division fight emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
