af3f61e68e505456ad7dc461b6427cdad6ec514c
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:01:35 +0000

    suggest explain conclusion thin complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
