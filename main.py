8c92e3a110da006f44b46bc08543d6e242d8c169
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:04:45 +0000

    positive in such bend preparation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
