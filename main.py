6917241736166e05b0716a6336489098715202b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:33:10 +0000

    oil Hh priority level inform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
