f48c0f17b11f151ae5d57e381a0db194109e9bd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:14:15 +0000

    share risk load trade embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
