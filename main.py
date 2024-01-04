4d1f851e6698e86ca4e4af7eb61a48cbd9bb3da9
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:51 +0000

    arrange rank ahead wire entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def reverse_string(s):
+        return s[::-1]
