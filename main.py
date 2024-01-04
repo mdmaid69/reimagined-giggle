e8b5cf5e04e53b02da47f4b98850fbeea1a614e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:39 +0000

    notice first couple Gg lift

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
