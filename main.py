c2f79782d67c7c466832fed5dc403ee28dca8964
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:11:38 +0000

    average still tragedy real rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
