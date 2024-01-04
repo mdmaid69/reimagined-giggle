48b003000c6654bc5319f1b9a59797d93161ab71
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:53:57 +0000

    little swimming strict makeinto breakinto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(5):
-        print(i)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
