1d56813a1e453b63c7fc092a856bc853be897f40
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:48:26 +0000

    way backwards eat actually impatient

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
