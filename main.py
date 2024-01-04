39fb1f40924bf3e05f53dd71d8a2adfbcda1f0b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:26:21 +0000

    add badly murder teach group

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_max(numbers):
+        return max(numbers)
