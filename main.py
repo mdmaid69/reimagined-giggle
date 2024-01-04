6ea36a3f9a7e9839f996e0c577ebcb747b1ad56b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:32 +0000

    every breathe argument convinced waste

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def convert_to_binary(n):
-        return bin(n)
+  def convert_to_octal(n):
+        return oct(n)
+def convert_to_hex(n):
+        return hex(n)
