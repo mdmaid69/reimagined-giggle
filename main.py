3ddab8fcf9e7b07201d688c07e72147d2197ca2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:49 +0000

    contrast breakdown tight middle clearly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
