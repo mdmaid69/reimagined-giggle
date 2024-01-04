a7345cab07483bf50a7dd4c1446ca8d55174c526
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:03:23 +0000

    sea somewhere rubbish tread mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
