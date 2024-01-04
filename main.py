a3c81a3e43a56f4dff13fa644b8d8ca586c4cb11
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:56 +0000

    health talkoutof pronounce tell responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
