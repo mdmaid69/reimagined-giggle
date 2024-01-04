ee0046fcf522580a11123689e2ec18a6e56f68a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:09 +0000

    turnaway dish contact keepout disappointed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_density(mass, volume):
+        return mass / volume
 def calculate_work(force, distance):
         return force * distance
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
