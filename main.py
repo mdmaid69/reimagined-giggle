2fef695249a3a0441883cd88b8ae5cd0325b1422
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:40 +0000

    coat lift love good takeaway

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
