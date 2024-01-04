d66d77c42ba02fbdec5aee0d87fbf72c352576a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:31:42 +0000

    shade time matter fire authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def square_number(x):
-        return x**2
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
