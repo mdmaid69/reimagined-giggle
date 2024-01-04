bbd6f0052c9edfdb0d0cfcba999e1a67e262c24d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:46 +0000

    spray production write hospital Ff

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
