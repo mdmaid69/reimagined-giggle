10f6a3f4035b576e1131a8b9103872fa3abd9f64
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:39 +0000

    laugh takeout arrangement ago train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_power(work, time):
+        return work / time
