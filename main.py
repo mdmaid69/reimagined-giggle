5f4747604d6cdea87411f17831244b0b408e3aa2
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:29:06 +0000

    wild far precise touch future

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
