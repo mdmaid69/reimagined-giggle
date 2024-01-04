e42a3fd0183b879a8b9bfe4a7c3b6e2ca4404581
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:03:46 +0000

    skill total wide priority want

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
