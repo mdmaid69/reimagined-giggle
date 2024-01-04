fde567d8cd58beedbda140b780cc9396fcff3692
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:51 +0000

    shopping commit scream top comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import os
+def change_working_directory(path):
+        os.chdir(path)
