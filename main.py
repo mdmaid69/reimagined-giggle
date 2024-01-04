f2072b15902135615b182d19c22f1b8c18f6bcfb
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:09:52 +0000

    room sore model anything will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
