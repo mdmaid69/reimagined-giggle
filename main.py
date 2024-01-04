d7f120711f8bd29c503611f51448751c45331d89
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:03 +0000

    guess blow mail cum water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
