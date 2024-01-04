a6548af4ee0decae40d1e4dae6d6fb70a33aad16
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:10:34 +0000

    line lookafter only tired concentration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def convert_to_octal(n):
+        return oct(n)
