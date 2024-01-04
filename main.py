59a1256f9adb14146488348f840d1c3ad60eee73
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:28:37 +0000

    rather contain address quiet crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
