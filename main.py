7c497f2a18a644a8caa5c05a5c621e6d22222c62
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:05:42 +0000

    cruel male tip before male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print(sum(range(10)))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
