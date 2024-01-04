1408584cf35c9eb23961c8c3947407f06110c635
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:22:34 +0000

    keen change over partner wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
