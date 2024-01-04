955ea9e9925f29965e2144a14a54f6dbb8158e17
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:27:25 +0000

    convince getup noone regular finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def multiply_numbers(x, y):
-        return x * y
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
