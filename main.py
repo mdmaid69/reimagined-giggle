bb31509cd828438a0de6e2f9f35fca1de6dfb33e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:49:44 +0000

    warm mother powerful comment deep

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-def is_even(n):
-        return n % 2 == 0
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
