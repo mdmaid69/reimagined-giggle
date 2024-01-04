33d135868013cfd6ad1c01b9758ba79628d48156
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:29:55 +0000

    row drink desperate exactly film

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
