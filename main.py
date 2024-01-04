81f4752805be1167ff8f826884f88fbab67a12a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:24:09 +0000

    satisfactory side sting motion thirsty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_speed(distance, time):
+        return distance / time
