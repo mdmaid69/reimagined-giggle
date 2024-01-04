74b7eb93ed5d6f03c795d25762177b64106ab866
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:51 +0000

    run turn estimate clean communicate

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
