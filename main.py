8ee0ff90274c722f9ad12d40b212f2a16b77c30b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:41 +0000

    whichever seem Uu relax arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
