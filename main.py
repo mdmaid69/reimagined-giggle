bd54dc11a90c9f9fb004feedd170f5e156bfd11a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:25 +0000

    custom ordinary relative appearance duty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
