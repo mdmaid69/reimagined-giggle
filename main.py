17165804747b948bc81f99b8a399425cc71d5b7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:42:04 +0000

    somehow introduction zero even ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
