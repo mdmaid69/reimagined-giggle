b7403eb4ac68c5edec143b9a2725e9655b4a32b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:42:56 +0000

    nonsense commit themselves pair Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
