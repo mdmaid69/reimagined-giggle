2e649732dc0536a4bdab8ad3d90954633cb1644b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:03:30 +0000

    near sport radio walk lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
