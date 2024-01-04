b37c962f22383438c3da1272154d352e7df7d528
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:44 +0000

    believe train tidy cost hungry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
