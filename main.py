7705708cc3cc01f4a2c4b0f4542a99df2bd85f0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:13 +0000

    test beyond entry public lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
