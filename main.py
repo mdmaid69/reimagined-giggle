c8016ec1c328ad80348773abb6ab4b5fc51a30e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:36:43 +0000

    react OKorokay visit deserve over

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
