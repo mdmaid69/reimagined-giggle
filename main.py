80becca872279702a8d2565e19a075d5aed7eb71
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:49:55 +0000

    recommend right funny exaggerate unite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
