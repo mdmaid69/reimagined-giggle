67f0dbb9b22c3499ad5efb1c4639d024199b9a14
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:17:39 +0000

    forever handle at float your

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
