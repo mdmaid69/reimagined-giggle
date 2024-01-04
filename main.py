3c5e3bdd268235db3ccad354ba76bcf229113862
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:07:46 +0000

    work interruption although scream confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
