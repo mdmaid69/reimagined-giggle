93475c35a767cf0d24e1576a6f247235f414ba48
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:36 +0000

    century offer the decision admire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
