d164331aff1f219e63b13fd32da3f7d3e46abc98
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:21:33 +0000

    with nor branch turnup carry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
