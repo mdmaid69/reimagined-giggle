7ff01a5aa409edc0765a87e210e10c6de4951f23
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:22 +0000

    road if hope shock tidy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def reverse_list(lst):
+        return lst[::-1]
