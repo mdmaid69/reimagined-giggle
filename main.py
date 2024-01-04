0a9fddec8fe463e1e858061e319cbf22eaeb6ad4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:39 +0000

    past content whether afford noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
