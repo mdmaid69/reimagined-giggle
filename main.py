541b5bd73a9a46d4fe686f1bc82cacbb2f5c67ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:46:13 +0000

    before city between quantity wet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+print(math.pi)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
