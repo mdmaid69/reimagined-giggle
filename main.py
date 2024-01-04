c430548ccef899c5c5b1acd8c9319a4aa9356269
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:00 +0000

    stranger determined greeting frame image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
