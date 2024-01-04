d2b589941936c81400ece2b1e1b1019a413b87bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:16:34 +0000

    passenger children second show coffee

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+x = 10
+y = 20
+print("Sum:", x + y)
