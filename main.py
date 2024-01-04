d9b7d201ce274fa1e7aef00bd5c0952439abf8ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:05:32 +0000

    justice plenty promise spill afternoon

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
