2919de49c62aeca200ed9d5d32d289376bdbe143
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:06:26 +0000

    spend cheap shallow mend travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
