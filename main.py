3db66d5e3c4f15a803612f446e0585b6a8d21a4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:53 +0000

    winter creature except whistle shopping

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
