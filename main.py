7c974e61cb347ab913f58d16ff62b1ed8dba197f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:52:46 +0000

    year occasional scrape cruel round

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def subtract_numbers(x, y):
-        return x - y
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
