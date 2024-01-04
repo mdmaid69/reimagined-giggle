8a1790733765c04528c81bec0ba21dc95c5b801f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:46:45 +0000

    strict exist system Kk scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
