4aa0745236a370a16f1690dae43eb95ad13cc8a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:07 +0000

    keepoff address pride class broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
