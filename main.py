a36884eec4c4359d304d2a6cca18bd0693268158
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:34:55 +0000

    innocent our sound 568energy tap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
