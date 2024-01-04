6a2a3bebcd03c586e8ae8ecd1420cb89983fab02
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:20 +0000

    blame complain shut confident satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def convert_to_octal(n):
+        return oct(n)
