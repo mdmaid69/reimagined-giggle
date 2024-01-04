b54b759c38134b6c6200bc4163df1a0dee35a299
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:08:02 +0000

    afterwards serve mistake season influence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
