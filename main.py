76a55c88e5be28f7e0748666a090c49db054d148
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:11 +0000

    enthusiastic occasional regional exciting show

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
