0a4b8274fc93e3f4e92cb7fc19502d8ba4bcc95d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:33 +0000

    guess murder good wide frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
