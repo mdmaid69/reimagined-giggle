2090bb6916c79ed45c67b8d82ffc71b2639358e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:30:50 +0000

    regularly mistake possibility sit niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import numpy as np
+print(np.array([1, 2, 3]))
