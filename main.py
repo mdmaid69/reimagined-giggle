ab1ccf26e0d0e59a88fd66151cad9b4c147b030d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:04:29 +0000

    takeover rather talk interview arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
