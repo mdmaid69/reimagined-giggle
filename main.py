bb29571ec14d506a5999a1d92fe4196ac3cebb1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:11:36 +0000

    live exam aim deaf politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
