8241c42f8974de7d7f0fc68be6438127bf2307b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:39:09 +0000

    narrow break only turnout comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
