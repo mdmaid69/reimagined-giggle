b367ee47a032d61ae75983f9187cdfa47bb4c05c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:27:34 +0000

    public soak previous visit power

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
