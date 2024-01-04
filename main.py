c2810d0394fcdc128ad85903c744843b0f3bcd95
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:49:10 +0000

    lonely come steer guard previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_hyperbolic_arc_sine(x):
         return math.asinh(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
