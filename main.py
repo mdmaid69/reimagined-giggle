669c296eb56bc6a2fcbc7f33632a8f48de40db4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:37:33 +0000

    pipe generous building closely loose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
