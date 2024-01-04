93b0cf0fcc80b4dc74c4112ab1a14546886e492b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:44:20 +0000

    instruction our appearance poison nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
