999a38109e452b2872e6bf7fe7f86cc7e6672514
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:51:17 +0000

    wash picture everywhere close carry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import collections
-def create_stack():
-        return collections.deque()
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
