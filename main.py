b331701c96fb8e31e84c7c3e0a87187b9e72ab50
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:27 +0000

    discuss listenfor reach minute funny

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import os
-print(os.getcwd())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
