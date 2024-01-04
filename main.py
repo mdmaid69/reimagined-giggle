54572365a4043a466d03276454589a5445b055de
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:28 +0000

    favour divide answer time inform

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_area_rectangle(l, w):
+        return l * w
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
