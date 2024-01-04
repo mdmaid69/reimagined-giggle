63a51323fd5471f4a49888ba9cb88a3e84c619bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:35:08 +0000

    phone greeting machine neither minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
