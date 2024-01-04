9145f84afc1296f0c7eda7bcd351861fa2c95625
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:58:50 +0000

    surprise fat problem us better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
