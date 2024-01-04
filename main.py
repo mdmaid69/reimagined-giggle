1e25a8292e72efe476041712d2df02657a2736f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:27 +0000

    precise shelter result translate whole

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
