a4b83991ffaf32793df2dcaa46d39446dc509f36
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:52:26 +0000

    571enough follow morning comeup label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
