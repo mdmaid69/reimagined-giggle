e109405c41f4989293b5dc773bb40724e308433b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:52:31 +0000

    pay confusing view truth culture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
