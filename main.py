047223ddf776cc617e769ceb032d2244c8f190c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:01 +0000

    before place flow praise problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
