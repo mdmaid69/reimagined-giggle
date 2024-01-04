5d76106fa8f0d87f0d483b57eba01b8ca0348988
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:35:51 +0000

    prove compete concern damage form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
