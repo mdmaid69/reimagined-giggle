54a5ea99f88a96cca4115ac8115199a72bbd44ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:42:07 +0000

    prize furniture mix do sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
