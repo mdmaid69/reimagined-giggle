2105cdc45cf590c01711bdef6fc4cb73ebd97ce0
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:53:43 +0000

    act introduction hold trade godown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Uppercase:", text.upper())
