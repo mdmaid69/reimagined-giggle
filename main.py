f05e0e2efa1b9391d61b97c03d7ad25b45abf2ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:48:45 +0000

    whom spot play tend mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import collections
-def create_priority_queue():
+def create_stack():
         return collections.deque()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
