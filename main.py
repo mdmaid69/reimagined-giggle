3e29e6027aa01a78bef1a9f63396357f34d89826
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:42:10 +0000

    violent now mistake head too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  def count_elements(lst):
+        return len(lst)
