77fca3d8c058e1a371636873eb95cfc9d102fff5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:12:32 +0000

    country usually fear bag payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
