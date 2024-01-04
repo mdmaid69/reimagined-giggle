3c90d6e0aeb63e781e31d5b0b63a666a4bad393a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:38:02 +0000

    nervous takeoff suit influence 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
