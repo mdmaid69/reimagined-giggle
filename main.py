ea8944bca0aec0402d840171c24c1b6ede5b8ce4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:42:42 +0000

    attractive confused window warm grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
