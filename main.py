811bc7a38964a8af6ebde4a2ce47657607c5e69f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:14 +0000

    mark raw mine record really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
