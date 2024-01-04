3ff8d118642703dccad851fab66b191c51bcdd18
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:32 +0000

    fortunately man even natural ring

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
