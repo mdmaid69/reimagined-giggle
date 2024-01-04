e0db61d03176cbd963df187c8ffcd6fec4e864b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:36:42 +0000

    Gg invitation tap second cry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+print([x**2 for x in range(10)])
