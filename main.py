24062bd74628eb36fb0b13a559aa63a14a6d98b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:40:26 +0000

    season force most see calm

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_repr(array):
+        return repr(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
