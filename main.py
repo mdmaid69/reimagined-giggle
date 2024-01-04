34e8b30cdb08ca9950d111a2744a40379ef55bf1
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:38:52 +0000

    problem holdout entrance favour proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
