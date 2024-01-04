c181b0c96f3026986274015713f06227f0f59620
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:31:44 +0000

    curious quit poison fun except

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def sort_list(lst):
+        return sorted(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
