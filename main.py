4879d0d1d71216b13733c53345af319a404df505
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:00:06 +0000

    introduce handle godown the environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
