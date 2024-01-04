4c0735aa7294f52d8a208731fa8eff85d5ce3f25
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:18:15 +0000

    sale own fold hurt government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
