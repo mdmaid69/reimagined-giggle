4b4991c6b93f30e1d6da670a7e2d57234e4d2914
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:04 +0000

    rank path provide bear course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
