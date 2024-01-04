b097fa36b0ce630178033b2d0705da3d3c0f95ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:22:31 +0000

    must accidental number bank pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def subtract_numbers(x, y):
-        return x - y
+  def convert_to_octal(n):
+        return oct(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
