db334bdc5cc32d16255a5b6ef49daf9c1693b31d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:39:13 +0000

    boat onto calloff mixture doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pressure(force, area):
+        return force / area
