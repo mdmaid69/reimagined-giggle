9947aa7735a43dc01a4012750f927a88e3e292dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:17:03 +0000

    through sight surprised organize count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
