d13ec1d503cea0a2753a8a020897824f4b475785
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:04 +0000

    ourselves pair effort pretend tool

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_power(work, time):
+        return work / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
