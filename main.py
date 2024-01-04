c849a4d8c0a71e7797d05b4d8226d1c1ea08459e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:52:41 +0000

    organization bite somehow wrong unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
