04d868fde81747f6202e6e8634c850fb3b201952
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:59 +0000

    without band become on telephone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
