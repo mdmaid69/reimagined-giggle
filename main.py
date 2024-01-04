c911aa2462e2c4b632fda773e769c52decd0df92
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:27:54 +0000

    along continue such there switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-print(math.pi)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def remove_directory(path):
+        os.rmdir(path)
