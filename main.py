bb494b9a8be9dd0b026dc4e5e4f422a44a2ce2b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:46 +0000

    bound level close sudden minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_set(array):
+        return set(array)
