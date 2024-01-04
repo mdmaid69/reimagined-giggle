7be2383f97768a45614398b9bfd3e970d02fc0b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:51:05 +0000

    usually bear angry than smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
