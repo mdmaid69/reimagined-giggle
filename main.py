52d7f6c687defba2fac82aa5103928b2cf350160
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:59:34 +0000

    obtain little slight pretty no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+print(sys.version)
