0f80dfbaea8c19639cb1b7815263d39d3eff89c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:06:18 +0000

    bang seem favourite blame permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
