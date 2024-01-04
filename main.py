edf41b802007a90f90f835cd49f82a848dac57b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:06:47 +0000

    shock food dare ahead window

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import time
-print(time.time())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
