9122a24bdfc83587acc707637390902d64526bab
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:49 +0000

    practice yesterday bank nice necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
