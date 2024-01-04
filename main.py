27fec59218d9a0a19d9aa4aa1bd36718a4fb8391
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:24:30 +0000

    interview shadow operate attach need

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def remove_duplicates(lst):
-        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
