8c8379e5f514e182ae93363200ad4a914e087598
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:21:05 +0000

    scale comein fat my gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
