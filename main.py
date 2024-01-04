f58f706f2d7bfc8faa1c2edd6f2fd67eba50a7b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:39:08 +0000

    mean consult telephone should 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def count_words(sentence):
+        return len(sentence.split())
