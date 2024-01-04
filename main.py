018594ffcab8a260989eec31950bb151b3e6550c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:47:52 +0000

    habit consist agreement regular their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
