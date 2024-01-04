a0168fc5742252ddf2ece42ea6d83304eb2396fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:22:47 +0000

    everything in task safe cheap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
