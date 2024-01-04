f3237cec337329bc7790445b6223434e7af31131
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:05 +0000

    tooth advanced height develop look

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
