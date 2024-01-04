767a3b24edd1a2c1d243bfd597a7ddd2f7670084
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:44:37 +0000

    taste bank straight keepfrom course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
