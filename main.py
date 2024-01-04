06ea502ea94784c22225c59812184da7e887bce3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:41 +0000

    open talkinto them later neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
