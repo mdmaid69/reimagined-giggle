a2cf8babac1ef72a30ca153604efca438a08c6c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:15:11 +0000

    store run get Qq trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
