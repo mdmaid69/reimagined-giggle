62ee37d1108b2525da806806f8f457a44d69879e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:22:14 +0000

    please decide school similar cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
