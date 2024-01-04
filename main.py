a46ba8bbfc1023922b82160514c3edb22595facc
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:31:05 +0000

    fall factory alone giveaway interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
