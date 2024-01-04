0707f5c7e3306f44f759b4146fdaf0d257eea977
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:59:53 +0000

    guide mend hollow improvement perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
