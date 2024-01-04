8633ae3ae17b1908b0dfff72bd62349622c9f2c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:42:52 +0000

    satisfied least plastic unite impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import time
+def get_current_time():
+        return time.ctime()
