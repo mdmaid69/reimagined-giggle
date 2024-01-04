596c47b77fe65dd7023effee3b16cc06b2ac8ad3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:10 +0000

    apply field crime welcome university

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
