6198a6152727ff6946eca7490addc973d8cfe2f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:02:05 +0000

    fairly contrast hard false lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
