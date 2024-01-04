206c0221854668977dd79c1b342a12f294ebaf62
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:20:24 +0000

    arrange runout Gg curious house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
