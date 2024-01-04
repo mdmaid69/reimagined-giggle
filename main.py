93f1c477bc58cf05d468b6d5e2490780731e2493
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:05 +0000

    shelter never extra argument trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_counter():
+        return collections.Counter()
