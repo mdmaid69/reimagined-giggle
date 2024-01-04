1c023d6e5220122967027d8c1918e40ab4e5252b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:26:18 +0000

    improve promise damp water border

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
