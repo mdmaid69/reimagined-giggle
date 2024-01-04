734c6414c4ac186e03dc8dce3fbe600650efdbcb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:54:30 +0000

    handle 179bite hot getaway transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
