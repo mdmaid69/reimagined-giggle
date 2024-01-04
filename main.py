85ba53462b1b67102b44755ef0ee847569141125
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:14 +0000

    willing cover soft tight of

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def divide_numbers(x, y):
+        return x / y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
