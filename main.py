71a5969e0db71417fb0a6430c9768ce4f9d5dbe9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:48:31 +0000

    search suddenly typical goon generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_index(array, item):
+        return array.index(item)
