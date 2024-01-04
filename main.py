2e6e2a9439623839599d5dc84380a6ef8faf64b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:28:39 +0000

    hungry only nonsense row stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def count_characters(sentence):
-        return len(sentence)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
