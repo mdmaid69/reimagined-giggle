eeb304f070353fa1b21906006762a44cba51d3aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:03:41 +0000

    favour club funny cost possible

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+for i in range(5):
+        print(i)
