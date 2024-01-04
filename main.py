f0d52b2fb342d0beb170cd09817906527846c690
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:22:39 +0000

    exercise meanwhile cope enthusiastic quick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
