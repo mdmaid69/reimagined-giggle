6a44979e46ba45fe8d45c5bb7ae3beafaadaf382
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:41 +0000

    comeoff make theirs kind wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
