4ce23e36a75adb83d6597577be3b6897d9668588
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:34 +0000

    left least convince hear absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
