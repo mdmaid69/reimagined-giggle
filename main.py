166c49356acb730bdf9c42d045bcb1caefe6b240
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:09 +0000

    kind by extraordinary shoulder damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
