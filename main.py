471510183cf243a2c22716cecca645948b05b93f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:39:31 +0000

    he stone amount dark confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
