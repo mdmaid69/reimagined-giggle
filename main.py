e02b3fd2714f609e0167d06c101fb23d72c4f2a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:50:04 +0000

    argue somebody Nn wrap hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
