6bd59a553e2b6e3201dfe463c1ad274368c1206c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:26:25 +0000

    system zero wherever vision lock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
