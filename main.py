ee4e7eb21660a9d6b0f951d12004e9cabe00039a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:13:56 +0000

    Mm account still spring which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
