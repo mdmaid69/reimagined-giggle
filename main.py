6211960334d86003bf05497c0bb4a918d0233936
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:38 +0000

    official better exist properly middle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
