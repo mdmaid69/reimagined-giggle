4de80525b7eff3398d2592b4dbf2b946952c7da1
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:08:11 +0000

    tonight smile heavy exhibition error

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
