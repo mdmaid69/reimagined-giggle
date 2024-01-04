b4997ce23514d48b43146f559b2fa71ada4813e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:13:48 +0000

    disappear secret path quiet row

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
