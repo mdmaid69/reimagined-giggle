28f7595b37aaea36bae281afb0c8f8f62202aaf3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:14:09 +0000

    afraid there love disturbing last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
