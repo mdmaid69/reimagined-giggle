f591b54cddda6bad64a964db751647ff1d1f4379
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:04:11 +0000

    damage rest identify stay sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_file(file_name):
+        os.remove(file_name)
