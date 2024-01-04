e7e5937e41d0bca512b8eaaad8a0f6c768d04aa3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:53:43 +0000

    positive pretend phone square evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
