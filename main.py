8144073905e280eb434c562264cb45d0759404fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:15:41 +0000

    country use run expensive pleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
