ac39cbc4a5a30de15f8ba573fdaa676b2fc59815
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:53:20 +0000

    smile relation favourite fetch firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
