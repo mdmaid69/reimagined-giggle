dab416efbf05893471de9f5ca25e9f12b00e7a9c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:07:46 +0000

    example cover wave behaviour safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
