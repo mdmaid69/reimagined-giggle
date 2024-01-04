819be69d8b5656ee862bfa39858af8a5ca29c37e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:16:26 +0000

    defeat male people common let

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
