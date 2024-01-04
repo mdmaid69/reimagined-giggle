d3e9a9907bdaa3680e51a31d485f8066596e3595
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:03:17 +0000

    slightly few show object steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
