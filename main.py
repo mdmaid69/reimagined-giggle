579aefc5f1331f859a12a5e9fbdb1f42bd759bf1
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:03:52 +0000

    answer yes yet meal answer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def square_number(x):
-        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def cube_number(x):
+        return x**3
