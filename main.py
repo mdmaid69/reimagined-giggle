181186d35b8cb7408fb6ed1c98e9374c6e103056
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:47 +0000

    announce public morning brief listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def multiply_numbers(x, y):
-        return x * y
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
