54e02e0c4ba8559e2e1ac9dea8172c9125e4bb63
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:08:09 +0000

    authority bear OKorokay muscle again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
