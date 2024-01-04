894b3a51d6919b77274de716c176260a19410826
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:47:00 +0000

    common approval fix crazy range

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
