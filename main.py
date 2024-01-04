23a7bc07e45d7584414bd7fadc0b86f1b1f04f64
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:34 +0000

    maximum break trap dinner still

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
