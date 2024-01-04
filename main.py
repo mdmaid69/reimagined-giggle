3c780cb6b8e29296289f817dd2917beaba9d42d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:25:34 +0000

    on round fit Hh low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
