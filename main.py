936b72e4d435220750c9b9864cb6fba39c7eb9f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:03:08 +0000

    children believe difficult win 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
