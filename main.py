5fa766e9de21abb742e6fcafb12c1c24f52cd5c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:28 +0000

    area base develop contribute proof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def reverse_string(s):
-        return s[::-1]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
