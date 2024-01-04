2d38ad18930184efa3ccc2edf04710c3cb25bdd1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:23:10 +0000

    draw television Ww present knock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
