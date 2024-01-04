e0f99a72b8da335cbfdea0f1410fb4e3315e1ec6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:21 +0000

    happy ticket valuable onto exciting

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
