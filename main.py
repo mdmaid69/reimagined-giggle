963c3776d3a079cde2931a7f39c8ac583af7d728
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:44 +0000

    backwards surprise pump something position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
