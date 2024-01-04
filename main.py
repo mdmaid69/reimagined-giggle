ffeab76521c2e485e292ea346c1df200054eae1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:14:44 +0000

    soak herself company terrible swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
