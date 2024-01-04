689d71bf8cae4c0340cb226933f56374c838bafe
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:29:04 +0000

    rain success curious programme around

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
