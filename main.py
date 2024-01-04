0474f0e4b5d13515a4a201df049c846e67470bd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:36:25 +0000

    guide welcome set time long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import json
+print(json.dumps({"name": "John", "age": 30}))
