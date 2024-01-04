f322c898b78ea7c46af8ae106862d5029a36b4d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:08:27 +0000

    who harm comfort guide naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
