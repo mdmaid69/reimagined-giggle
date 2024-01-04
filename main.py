202ac50b1ebb4ce9ef3aad8627a1e520f220025e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:58:40 +0000

    shall fly emphasize move immediately

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
