b7e1bf20d983063651dee58d93c87d6dd0503e3d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:31:16 +0000

    familiar coat relationship fear forever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
