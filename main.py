f7f31b06ea467eba7523396f211733bb0c78ab0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:09:18 +0000

    without right wheel dream thin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
