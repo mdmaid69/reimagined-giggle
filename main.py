bcd5b66f03dc7691c262da107fe9b21432bb48ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:22:37 +0000

    Ww sure its normal entrance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
 text = "Hello, world!"
 print("Words:", len(text.split()))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
