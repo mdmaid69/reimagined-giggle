beb07c2bf6cf0c2c8d9c2fd235f3b69279407deb
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:38:03 +0000

    grade listenfor clue unlikely either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
