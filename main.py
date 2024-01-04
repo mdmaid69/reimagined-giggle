aebed8699d1cf02e6a95f3e933b06e33743aeec1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:25:15 +0000

    splash opportunity thirsty centimetre goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
