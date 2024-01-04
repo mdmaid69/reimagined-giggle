a7842f35c58724db3ce5c60db0614b964180d88b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:51:38 +0000

    stay 566end sensitive taste talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
