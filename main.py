8ce73f29b5aa9cfb11fcd8a080996c41f18c88b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:40:27 +0000

    male firm marry throughout until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
 import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
