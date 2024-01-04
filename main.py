0b1bcbbfbabb50479b8e086aacdc24c83b24fd13
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:53:34 +0000

    wet shoulder lead accept bath

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_item(array, i):
-        return array[i]
+import time
+def get_time_since_epoch():
+        return time.time()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
