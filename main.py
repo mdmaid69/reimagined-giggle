6de494d523a37f5b74e23b3179e97cd9f90d94cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:54:23 +0000

    comment stop mysterious dish sudden

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
