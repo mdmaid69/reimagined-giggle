7069013c153cd50107e585e0874a176bae78f767
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:47 +0000

    straight appear bus switch book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
