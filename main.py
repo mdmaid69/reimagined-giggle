861afc7b167eb95130bfd3d8f221c97631c774e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:04:27 +0000

    there love before face respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+for i in range(10): print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
