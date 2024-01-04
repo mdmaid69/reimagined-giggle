cdb7cd20cdfb74f19246a53d8280a8f127e05c31
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:51:04 +0000

    where other importance famous shout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
