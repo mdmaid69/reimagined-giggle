5f7ae6f600fd657c89b34635faead35f4ed565f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:21:46 +0000

    cut rush practice drawing takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
