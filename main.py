edcac262fcafca068e2264f5746dfc78b59ec84e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:11:52 +0000

    recommend recently convenient telephone succeed

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
