77364dfdf7432ac00fd9b1ef686798985e0f8908
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:29:17 +0000

    impossible keen talkinto female battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
