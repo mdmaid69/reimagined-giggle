2f2580834c0b1fec7bd336eda0266ce4d8cbe0a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:52:19 +0000

    exploration communicate design foot left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
