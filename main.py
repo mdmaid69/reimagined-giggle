618318e40601c0ea72e524123f6150e877e02dc9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:30:12 +0000

    disturb bringdown work off moment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_max(numbers):
+        return max(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
