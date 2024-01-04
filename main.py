bc38bc454d9db080196b634679439bedec54ccda
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:15:18 +0000

    raw defend one nerve worse

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
