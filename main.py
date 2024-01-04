4644c6a57f29a77fe8311f293d709b93b51a9278
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:01:43 +0000

    respect none pot ring dance

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_priority_queue():
+        return collections.deque()
