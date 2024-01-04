44c8ef3cafc71bd406aceee9118bf483fb91a046
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:30:43 +0000

    Bb stare community everybody for

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
