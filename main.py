1a74eaf346b496598f1a050b0090fc1183b86f30
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:21 +0000

    challenge anywayanyhow violence moral sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import collections
+def create_user_list():
+        return collections.UserList()
