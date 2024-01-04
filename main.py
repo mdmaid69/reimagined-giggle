6a7e94309ed27a5b4d0702c92ac3434291e48c7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:11:28 +0000

    false stir 564encourage use often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_typecode(array):
+        return array.typecode
