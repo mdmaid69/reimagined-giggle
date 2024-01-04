497f9257b0867e70520ebf866f3d396c74de63ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:52:09 +0000

    cover find original more partner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
