6261da19bb3d0ec06abfa14050828f57025b2b72
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:30 +0000

    steam attention beginning exploration cool

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_area(radius):
+        return 3.14 * radius * radius
