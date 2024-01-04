ba8663aa2c3407aba7b3787ff28d01479f2af365
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:09:13 +0000

    hope test burst some schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
+  def sort_list(lst):
+        return sorted(lst)
