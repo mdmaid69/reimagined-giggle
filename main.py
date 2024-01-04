cca52d8b2988dad08a54f8bd0ec6da8b7a930d08
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:28:58 +0000

    balance unpleasant holiday beneath lock

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
