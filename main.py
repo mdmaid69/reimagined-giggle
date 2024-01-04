c3f27eb75bee6a49eee7f7fa284359ce0638ea89
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:17:14 +0000

    smell develop factory model Gg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print(sum(range(10)))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
