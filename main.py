ec7eb36ee4a0f844713a8e391473e71e258c1610
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:17 +0000

    flood guilty goaway break recent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def reverse_array(array):
+        array.reverse()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
