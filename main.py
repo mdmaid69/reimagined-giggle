841152fa355683c01c645cc318fca84e3eaf5bed
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:28:55 +0000

    position many order taste hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import json
 def convert_to_json(data):
         return json.dumps(data)
