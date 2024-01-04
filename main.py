83284aceb8a5a916ea8de1940a2cff0eb7dfb297
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:11:32 +0000

    comeover borrow shade check Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
