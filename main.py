3456702d635dd7757888388cc4aca4a2ee25f3f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:21:02 +0000

    Rr hurt excellent drive water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
