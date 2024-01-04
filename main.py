117a1d25c46f81a05f3718a0eb79b031b9af39a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:06 +0000

    task cross various private afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
