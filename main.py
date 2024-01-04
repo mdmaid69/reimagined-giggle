cf490301905d4c624526332f3b30f9a08ec7b74f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:34 +0000

    goaway weak shut they scare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
