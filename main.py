4c98916343aaf9a4d4234f432c83b848afa68806
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:40 +0000

    dry single area dull close

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
