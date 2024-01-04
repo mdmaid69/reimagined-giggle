28ee895ae114e07fed3bd75f5d422e1fad0b1f68
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:40:18 +0000

    improvement continue intend control level

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
