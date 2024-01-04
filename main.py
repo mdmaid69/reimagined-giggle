3ab7531d02cf2d56376965a4f869c6ef68b89c4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:13:43 +0000

    eat scene anywhere anyone rather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def convert_to_binary(n):
-        return bin(n)
+import sys
+def exit_program():
+        sys.exit()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
