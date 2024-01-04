32efaf727ecc342099f522f2d2fe9e573e960931
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:10:48 +0000

    disagree steady like Uu buy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
