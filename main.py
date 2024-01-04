bc88d342db9982ecc354573349ba34a677138f9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:35 +0000

    list tomorrow come duty connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_current_directory():
+        return os.getcwd()
