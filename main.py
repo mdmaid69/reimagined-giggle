7d0651179e32877d2f72fb6531e4edc330090b8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:42:26 +0000

    right afford art long stare

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
