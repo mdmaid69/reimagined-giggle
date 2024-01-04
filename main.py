94a8809861a45a34a3cdb2325ab82860e80378f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:21 +0000

    hardly attitude spray should nurse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
