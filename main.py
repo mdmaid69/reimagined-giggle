de5bc19cd25f8fbf1378e23639a63dceea7f0cee
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:21:39 +0000

    but suck mix mysterious total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def add_numbers(a, b):
-        return a + b
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
