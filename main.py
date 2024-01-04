3e7f2b4f9e2fd8502affa0a3b37d06cacbd9ffc6
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:55:41 +0000

    little measure class government mainly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def reverse_array(array):
+        array.reverse()
