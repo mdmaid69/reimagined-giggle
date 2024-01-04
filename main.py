0189e7ef97ac8933299a1bdf92253f71b4444220
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:26:02 +0000

    bill turnover familiar reply false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_floor(x):
+        return math.floor(x)
