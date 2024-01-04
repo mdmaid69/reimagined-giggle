cc9c0ac6dd0a4beb129850c5bad2b9a9a4282b78
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:47:40 +0000

    smell stiff unfair weekend wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
