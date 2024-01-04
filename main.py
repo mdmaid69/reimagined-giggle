69341f2b5f135821a2b2aa780f60867507b5da3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:52:59 +0000

    shelter reliable home wear sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
