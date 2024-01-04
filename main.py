24fba8e503ab58e20ba7a5520297f1b354187758
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:55 +0000

    that comein shake properly can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
