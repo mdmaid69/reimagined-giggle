5987a3f9a707fe2a25a4a32c1b8e9d1453a4ae82
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:11 +0000

    smell Ee colour wing comfort

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
