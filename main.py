8ad471e643ab35ef1230b99e07fd4180cb47aba8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:04:42 +0000

    block relief goaway where slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
