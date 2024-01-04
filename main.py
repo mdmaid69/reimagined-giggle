11a0a196c8f46b7a38ca8bd672ccae62b06a9ab6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:02:34 +0000

    angry confuse hate strong organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
