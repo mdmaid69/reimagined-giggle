a44f6d7f1ed0d3893b1d55945841057c4d3ca2fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:30 +0000

    straight music general our live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
