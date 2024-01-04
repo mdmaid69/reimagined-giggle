d0e0fcd786b7cc380c2c431e4965a90feacd7955
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:55 +0000

    pay exaggerate objection average visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
