2b6c86dc1419854c31735a6657e35f32c7d3bb02
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:21:14 +0000

    fence vote actually the kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def convert_to_binary(n):
-        return bin(n)
+def get_array_as_tuple(array):
+        return tuple(array)
