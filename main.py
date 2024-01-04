c22b8514343488b5c87382cc411814c9693cc61b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:28 +0000

    mend low system anybody fence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def convert_to_octal(n):
+        return oct(n)
