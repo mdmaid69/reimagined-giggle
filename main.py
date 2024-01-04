79e2f8034e4799d6c43c972f271ffa865ffd5474
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:17:15 +0000

    independent form tend explanation once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+print(math.pi)
