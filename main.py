acf5aee9f1d40a8375c43c084d7df27fe9f6c8fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:48:29 +0000

    university shake excellent anybody splash

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
