29f483780f6bd88bb5a0358cd72e38fe41d16147
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:42:23 +0000

    force store sleep aware free

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-print(math.pi)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
