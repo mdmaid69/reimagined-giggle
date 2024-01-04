de5c0e6bc3cb9e59c4aa6e0d34dd444b6cae0c39
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:44:58 +0000

    radio embarrassed grateful tour weekend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
+import tensorflow as tf
+print(tf.__version__)
+import socket
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
