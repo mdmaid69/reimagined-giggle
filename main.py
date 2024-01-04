ee1012f7e7d66bc30e4f18ae6dadd16caa66eea0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:32:25 +0000

    spray protect comment makeinto key

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
