ea88eaa1e4a1033d59ef130603899ac896cb6ef7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:33:51 +0000

    dream actual remark draw cook

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
