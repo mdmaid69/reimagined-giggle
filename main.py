9410148848169f0d793771e01ea4e7e6bfe8ae68
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:48:45 +0000

    Dd smooth everybody confidence about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
