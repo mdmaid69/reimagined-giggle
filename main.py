7f89fcf7eef40ce49de3161064cf510f8fda88c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:20:28 +0000

    blame away survive fast family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
