b5a060d8682b5f31194b9b59e14a3966a79af40e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:54 +0000

    smart especially comfortable present form

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def convert_to_binary(n):
+        return bin(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
