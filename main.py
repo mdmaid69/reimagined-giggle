dedfb06e59c4b4147e99cbb1ba2a720a77010ee9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:08 +0000

    on anywayanyhow present follow fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
