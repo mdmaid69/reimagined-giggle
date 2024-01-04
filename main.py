50cd77969c00ec4762a5183626cb310ee3f83bc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:01 +0000

    sort murder strange recently report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
