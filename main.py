5f98901864e489360c18159b5c4bb2b6ba259b7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:32:49 +0000

    cost bag weak perfect warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_directory_name(path):
         return os.path.dirname(path)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
