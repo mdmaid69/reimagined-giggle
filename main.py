7dd2939d22e200a72714780c5b77361ae5e49aeb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:51 +0000

    order hope paper scene takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
