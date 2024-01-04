a6d81ffd0ea432c71350ec9bcddce863dbb3f643
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:01:54 +0000

    alone themselves direct profit blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
