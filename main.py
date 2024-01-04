509f4a649d256c12e73d8293777fd42788a11e8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:50:38 +0000

    discover explanation apply possession operation

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
