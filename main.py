053f7159564398d68295537ca37a145b7b7e48fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:14:37 +0000

    occasional tomorrow without rush common

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-def remove_duplicates(lst):
-        return list(set(lst))
+def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
