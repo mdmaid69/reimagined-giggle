c592e8d4549bd34b91120252c98e68f6b21d9de3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:04:20 +0000

    list software fix damage slowly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
