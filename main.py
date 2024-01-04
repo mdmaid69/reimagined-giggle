22ad47b6299fd2c513b3d8575a6e8ebffe246fed
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:14:00 +0000

    value disagree tough means law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
