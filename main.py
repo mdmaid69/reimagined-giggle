1b2f07cbee25607bcbf8d7eaca6669096a267f68
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:15:17 +0000

    practice stretch human of follow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
