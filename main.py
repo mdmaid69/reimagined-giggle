760b147fe21e4631c1ec9c4b996753eb77d86d8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:31:11 +0000

    clear mend stone wrap least

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
