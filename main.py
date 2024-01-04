9c1ba5e15ff711a9694145a94fc42774d8ba509c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:00:07 +0000

    appeal hang breakdown pronounce word

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
