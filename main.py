546e0bbf55fecf538907fc0de581abc2f028591d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:45:14 +0000

    comeover catch old general geton

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def count_elements(lst):
-        return len(lst)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
