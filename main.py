4b91be2f5848ec6ae1e9ac9356766c50e91f8b37
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:36 +0000

    spin everybody split if unless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import random
-print(random.randint(0, 100))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
