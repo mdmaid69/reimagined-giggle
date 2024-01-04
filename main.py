461824dd6f85a4d8cb1dc8e8fd646f80ba5588bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:19:11 +0000

    happy tough cut thanks ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from collections import Counter
+print(Counter("hello world"))
