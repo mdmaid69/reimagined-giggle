0c8729423b721f5900524e7c8df334c3f6ed4de6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:18:38 +0000

    pure vote use godown building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
