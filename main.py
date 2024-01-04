d89aa02de54c923f32e3760cb0bac23cd43f38ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:33:56 +0000

    alone crash sort print morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
