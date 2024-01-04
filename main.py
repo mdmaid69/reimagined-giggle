b6be95dd9ba3c67a9992db5548b993ed58be4caf
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:37:21 +0000

    theirs trick share bread not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
