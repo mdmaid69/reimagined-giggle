6fd21f245c42b8085ae1cb95c2530bbc94588a47
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:54:26 +0000

    heat this up turnback Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
 def calculate_annuity(payment, rate, time):
         return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
