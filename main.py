bcd6e1af3f6c24d26ad7b3894fa47ded41f83fb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:29:49 +0000

    emotion sad bye subject wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
