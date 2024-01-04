133c8a681171e0ba7bb12c24015662967646722a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:32:30 +0000

    bank goaway he embarrassing hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
