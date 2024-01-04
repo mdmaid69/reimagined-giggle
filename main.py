2b0a12338ae0b72c7659fd72ed6a6f0e0f856ec0
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:25:15 +0000

    fair wing long ago load

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
