60052fd68d1a5cd596dc1c7f2f4cc39036be7312
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:23 +0000

    beside grow ever everyone himself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
