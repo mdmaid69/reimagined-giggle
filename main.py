3abca0038350084789f47a29abb8f70bb501b1eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:09:13 +0000

    we activity long interruption story

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
