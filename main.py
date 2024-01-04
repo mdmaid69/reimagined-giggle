4a7286706517fbcd50294206f9adcbdf947a16bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:19 +0000

    spill obvious store lookafter married

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
