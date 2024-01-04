852eed4d5cbd26277dc1bb4e059921f0c72ebb47
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:49 +0000

    interesting though joke smell behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
