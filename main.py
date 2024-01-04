c5818305cd5d47ed233ea30c016ed477d1e92cbd
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:03:51 +0000

    quickly lookafter sing too advice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+print(sum(range(10)))
