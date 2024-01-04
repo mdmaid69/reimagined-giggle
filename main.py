7e0d73d158061a37287f0c642ad984097e626dc7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:44:55 +0000

    drink side wonder result quality

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
