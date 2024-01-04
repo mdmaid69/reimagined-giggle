0a0fca916f7841068a782ef59e63d55d73b43e15
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:46:52 +0000

    information free set know nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
