8360d0285f8526c8ab73ecac89d4dfc641835fa7
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:35:29 +0000

    law identify open dry performance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
