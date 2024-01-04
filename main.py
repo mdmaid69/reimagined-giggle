1a4effced7aabadc34e425542f79b65b6e81ee28
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:58 +0000

    cheque whistle bringforward instrument turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
