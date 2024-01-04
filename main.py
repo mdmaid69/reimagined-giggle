24c6a8d22c3549bd7f7a10a52591a8e8ce680306
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:03:51 +0000

    getback somewhere sensible clever takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
