78ffa71e720169ad0c4f9b98928a1215fbb11fc6
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:51:27 +0000

    warn story however bridge dead

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
