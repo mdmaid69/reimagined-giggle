e259bd543715992072b66a682c1f479871447bce
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:13:12 +0000

    positive pour dream fortunate search

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
