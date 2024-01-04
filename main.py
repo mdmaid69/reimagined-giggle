5e74dfc2e542f60ccb6ee780a08507b141ea4324
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:30:17 +0000

    only better rent runinto coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
