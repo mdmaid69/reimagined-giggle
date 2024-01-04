978ddbf0f8d890179d12adb0e702a5a422f6a26e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:32:43 +0000

    sound easy account matter pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
