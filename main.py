f28f1f84ecd54f61023fe4c50cd70720b2e37a5a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:28:04 +0000

    extraordinary afternoon dream design or

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
