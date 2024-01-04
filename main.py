44d021e42fe747d9a7b875f19ef421e171e6d8d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:55 +0000

    might complicated cry nonsense release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
