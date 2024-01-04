3bdd57aa127e47958cbc32ec7e8cebe1849cd65d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:44 +0000

    fit truth share here pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
