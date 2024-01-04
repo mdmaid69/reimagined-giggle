9f24c894c3af714b0a95cc650e30dbf984dbc323
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:03 +0000

    model appeal guilty fair step

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
