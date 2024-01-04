c6f9f6a721ed86e024b4d0d0551f682c1fb97c01
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:35 +0000

    possibility consider weather my solve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
+def reverse_list(lst):
+        return lst[::-1]
