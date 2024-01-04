e389f27a7985c6d717921b8ecdb2e634cb9200fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:17:35 +0000

    neat breakinto various frame sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def count_words(sentence):
+        return len(sentence.split())
