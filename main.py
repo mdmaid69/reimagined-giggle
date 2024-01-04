0b3a6c45830c8aebc4f7d7a47b0d4fe53daa23f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:01:05 +0000

    food lie usually basically real

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def get_array_as_str(array):
+        return str(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
