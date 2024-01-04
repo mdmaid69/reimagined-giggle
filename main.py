6ad11e1ea749946430221271ebaf42c358eaae89
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:23 +0000

    basically to behave unless switch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+for i in range(10): print(i)
