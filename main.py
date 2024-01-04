743ec583d10a774bf61267ca1081afdbc76310a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:18 +0000

    century thinkthrough during up fire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def square_number(x):
-        return x**2
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
