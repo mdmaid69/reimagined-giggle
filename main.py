5adfd8c6b6a111fe3476a1da95b2eead3a14d919
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:18:50 +0000

    container rent pressure watch female

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
