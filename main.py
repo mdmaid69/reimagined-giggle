b414b1fd48969db984b96dd6c0349a0688f46110
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:31:43 +0000

    alive impatient know public laugh

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
