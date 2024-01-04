95fa7cfe0e303c2d5a1ee6c54a73069418866921
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:19 +0000

    container average better boring club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
