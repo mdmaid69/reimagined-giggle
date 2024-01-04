bb83b5539927e12c1d86f68df9577cb4aafe2feb
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:30:44 +0000

    crisis separate police reaction base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
