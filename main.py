1c8d6fdfebf749e2fc4a4e9683e1e06b55b33da0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:02 +0000

    else drink press probably plate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
+def divide_numbers(x, y):
+        return x / y
