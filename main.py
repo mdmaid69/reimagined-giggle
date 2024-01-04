44d93c1deb40d8ac78e46d3948df43055df52a00
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:05:31 +0000

    sound smoke disagree idea wrong

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_max(numbers):
+        return max(numbers)
