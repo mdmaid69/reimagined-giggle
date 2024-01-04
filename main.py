79a2438dbcfabb3acfd96f1e10a255c3edde0e8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:11:40 +0000

    minority stamp translate male motion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
