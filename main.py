4b6ce1b763508c3995deb54d7c500d99eff671b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:12:17 +0000

    still kick break family workout

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+def print_python_version():
+        print(sys.version)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
