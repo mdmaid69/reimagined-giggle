cc8631dc74b0bb1ac51e2a8d3e6d0966c013f244
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:50 +0000

    sting guilty above wild obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
