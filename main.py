f7b216fd06bc6053f58bbf414377d5647f95aa45
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:38 +0000

    charge peace cool fall better

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import time
+print(time.time())
