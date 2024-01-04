295466c5f16426f52d18be13ae58b3469ef0f5d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:34 +0000

    little permission one uniform clothes

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def reverse_array(array):
+        array.reverse()
