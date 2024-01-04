80becf2cb5dfcfa1d98d5f44a3bababeac0fd2c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:23:01 +0000

    turndown doubt base odd definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
