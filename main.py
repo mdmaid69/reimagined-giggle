758be5d8cf83083e6aea4f37bbe8bd96de5cdfa8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:11:23 +0000

    justice report bread bin bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
