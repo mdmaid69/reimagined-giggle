9eff67b841f8420f06cbcb4846e36f6afdb1ca11
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:15 +0000

    hospital always reduction address so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import datetime
+print(datetime.datetime.now())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
