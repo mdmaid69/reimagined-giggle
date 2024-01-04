3757af61b9b4d7cc145446b2ddd7faca10009948
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:44:02 +0000

    weigh smoke unlikely eye accommodation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
