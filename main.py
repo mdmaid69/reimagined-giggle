1dc1d7e9b82ae0b978e75c7d73dbde3e84422ad4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:26:24 +0000

    sorry takeback anybody large swing

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_max(numbers):
-        return max(numbers)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
