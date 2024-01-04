007eeac398b42a83662242a65e36bda13e10118c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:00 +0000

    touch build town busy path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
