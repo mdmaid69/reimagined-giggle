0be6135459489080b878d088f73d31369a842ace
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:13:07 +0000

    principle breakin fond mood rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
