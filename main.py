8c550894172636c676fdac093970539ec0351e86
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:46 +0000

    amount subject support insist urgent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
