e71184ad4eb6c04bb81ed14ea64466221998c645
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:49:19 +0000

    wire invite supply immediately cover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
