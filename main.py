00a1d28f951fe0995a47dc349d3d494aaa15114c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:47:31 +0000

    tear ours birth turnback affect

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
