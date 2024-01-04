a15b2ab82e9b239650aa61f882b995a6157f2db4
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:49:08 +0000

    juice profit tragedy month spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
