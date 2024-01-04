994d8285fc28a745d10188322b35398d35548ff1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:37 +0000

    freeze collection war beat pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
