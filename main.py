4b7471c514c3ea2b89d7cfbd6d1d70bc3489ad2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:10:17 +0000

    throughout embarrassed negative and storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
