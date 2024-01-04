5626be17d730e94b7bc1e998e3cd549e81cf5363
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:22 +0000

    full interruption Ll correction factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
