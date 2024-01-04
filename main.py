1081da6a2021937346f6afa088ed2e5102e5fede
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:47 +0000

    anywhere feature post nearly fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
