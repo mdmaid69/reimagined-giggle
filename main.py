89b245c15a921afca1d6e726fdcf6ee6d4e2f302
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:39:07 +0000

    small scrap plenty opposite whole

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
