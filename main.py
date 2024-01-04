0eb11126a8cb7bdf5c5fbf7327336eaefdae226b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:44:54 +0000

    paper busy use command this

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
