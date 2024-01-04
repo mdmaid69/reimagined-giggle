6eedb27bc74e72c3d856202658e560b2f463ab66
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:46 +0000

    swimming doubt keep design independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
