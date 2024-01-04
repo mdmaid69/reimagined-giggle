e84451a59d741f51f93e2db73a8dc1ec75f1b450
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:30:29 +0000

    total report kind record standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def multiply_numbers(x, y):
+        return x * y
