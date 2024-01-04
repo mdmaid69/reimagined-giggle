7210d751bc6dc865cce68bcd5d18bb3ec99b2378
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:42:11 +0000

    mistake track bet unless pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
