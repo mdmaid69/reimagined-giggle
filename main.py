59572da3c935b038b7effd1a1e46ea9e49b001e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:25 +0000

    include there division ready pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
