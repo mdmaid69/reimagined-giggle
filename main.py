c9ae521db181981d109b0908e2fd4e10a0c424f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:15:03 +0000

    brother tense shut long official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
