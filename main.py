4956959da201f6020dd778837a5e08515249acdf
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:01:52 +0000

    service lead dick environment container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
