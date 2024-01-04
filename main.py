b0b74eb696021347b065b1defffb967aaf32157a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:59 +0000

    phone picture round book cover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_typecode(array):
+        return array.typecode
