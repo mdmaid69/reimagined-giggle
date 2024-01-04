941672dcfd46b5ee3e60ef2fdaa8d0c861fb4e54
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:57:47 +0000

    run keen blow couple welcome

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-print("Hello, world!")
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
