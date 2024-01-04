5a15b36c593ecebf1ef1521a02280864b4f27e3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:40 +0000

    kill match lower undo winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
