671025ed1f8f5cf06bd54ed0b56519992d429c16
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:50:19 +0000

    minimum whom wall fall about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import getpass
+def get_username():
+        return getpass.getuser()
