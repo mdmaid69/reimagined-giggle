6a13d02d1640fa80ad6a62375742e26e59a460fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:59 +0000

    free clean loss poison attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
