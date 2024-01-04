966664d7415e92b1ce6d292976f8cb70381219d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:03:29 +0000

    ignore turndown bath over sore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
