738a49a9b186d0ee7fae6510962b56ce80d98ba1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:14:07 +0000

    travel reasonable swallow unfair cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
