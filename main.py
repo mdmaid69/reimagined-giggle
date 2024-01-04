c76342ac2d793a1a6beae5f3b94a48b4297f3cba
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:51:27 +0000

    polite odd usual forget worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
