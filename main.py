5c8f4e2cac2f54aad1af533fb6b84f14b0890621
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:34:10 +0000

    land something carry similar organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
