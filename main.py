45678725f72740cb4e59775ea2d02088dd06c263
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:26:35 +0000

    bringforward hers wonder limit suckme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+x = 10
+y = 20
+print("Sum:", x + y)
