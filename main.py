c25540d6205401197e65e28a7c75643f5411266c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:50:19 +0000

    apologizeapologise belief run rob succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
 def calculate_floor(x):
         return math.floor(x)
+import os
+print(os.getcwd())
