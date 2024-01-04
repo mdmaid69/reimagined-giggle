4a714703f8895fa83194fb38cfd21e6fbfa1f07c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:14:39 +0000

    content not keep sister underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
