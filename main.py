575198e5d7d1a25abf38c8cc09e9409863684c30
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:20:06 +0000

    patience run put blame ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def add_numbers(x, y):
-        return x + y
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
