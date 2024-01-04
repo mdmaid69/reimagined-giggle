dc2934978d82696f62b4e70200f9c0ec46bf715f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:17:20 +0000

    type Itself copy this lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import time
+def get_time_since_epoch():
+        return time.time()
