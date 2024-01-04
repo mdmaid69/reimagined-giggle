da26477358c9a9aa3d4c8da2ee6d3e42b283e674
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:04 +0000

    find bother sail fortunate since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
