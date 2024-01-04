02daf07f62d8f63bc542bbf6e23b246ea4e81cb0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:16:53 +0000

    among willing message loose rough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
