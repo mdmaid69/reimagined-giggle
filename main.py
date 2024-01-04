4690e7aaa7dcced119824d9a5542e10ace155962
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:11 +0000

    cheap then average survive past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
