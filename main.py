4b36a8339aa9fe7ff83279a85fe1b6fe74b6c952
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:43:46 +0000

    wide opportunity takeover organization opportunity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_list(array):
-        return list(array)
+for i in range(5):
+        print(i)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
