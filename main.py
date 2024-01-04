33d2558b0d011a437229f56ded5855051d27fbbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:16:01 +0000

    there skin arrangement wild depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
