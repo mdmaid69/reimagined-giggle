db983fd9c6fc071d3f695c99cc1a1ebfa615690b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:09:32 +0000

    type visit unfair nigga description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
