2fc6a14047f082d83dfa5611260afb942081ffa6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:28 +0000

    gentle free surprised wall project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
