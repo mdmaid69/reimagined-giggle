f5f4c9fcbfd477879c35cde4f6808353d4374e4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:31:08 +0000

    noisy fashion child everybody aware

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
