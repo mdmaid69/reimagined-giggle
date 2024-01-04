9de5698712fb794be83809d9b1816049369fab3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:47:20 +0000

    pay pretend best object remain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+print(sum(range(10)))
