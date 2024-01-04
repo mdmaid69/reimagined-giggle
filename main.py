1e56ec7d8a4839031eef0831af7148ff509561bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:18 +0000

    why spend exaggerate shelter friend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
