6f38e9bce43913f0118cb322872263f9a2b46629
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:54:24 +0000

    shop true test pain produce

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import socket
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import tensorflow as tf
-print(tf.__version__)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
