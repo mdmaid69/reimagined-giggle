e6ecf624aff7bfc1ab4458e9cc9e626540fc9474
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:37:27 +0000

    away difficult change surface punish

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
