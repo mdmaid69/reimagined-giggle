e50fa965285852913b2aa7126dfc589db1e57a24
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:13:26 +0000

    pull behaviour to scream class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
