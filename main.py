71f79c00598a7ab0707613a216090ace9517952a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:16:43 +0000

    exercise phone contain dry nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
