4b9da868c0f3258790f0509286166f928e8caf59
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:50:12 +0000

    private around popular surface comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.ctime()
