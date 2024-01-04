e9b8ba4ae253e60be287700bd009f3a4aadc1d5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:46:54 +0000

    narrow cheque recommend and colour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def add_numbers(x, y):
-        return x + y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
