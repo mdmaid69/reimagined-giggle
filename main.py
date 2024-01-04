9655c09e9583614196acf5978ffd35cecbf3db6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:36:57 +0000

    pump thumb mention clothes male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
