8e8c1a41b59ca47d8dd932ae31829089b3e35477
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:07:54 +0000

    table anything taste to keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import time
+print(time.time())
