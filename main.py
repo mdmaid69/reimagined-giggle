9c0743f52e958103962556d4e67ad1b1f6ded3ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:15:06 +0000

    size she knowledge command pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_area_rectangle(l, w):
+        return l * w
