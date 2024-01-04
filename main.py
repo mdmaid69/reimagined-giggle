44271f78bc3639d582834dc2b3d3f63713ba16cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:10:56 +0000

    ought us shoot depend step

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
