2ec584101cd131b42ee605e8139031449b6da490
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:20:39 +0000

    phone lunch basic person manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import platform
+def get_python_version():
+        return platform.python_version()
