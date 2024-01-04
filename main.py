72bad582c5713147cf7e1637abe15387a5f69dda
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:02:21 +0000

    case runout sleep tonight where

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-from collections import Counter
-print(Counter("hello world"))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_as_set(array):
+        return set(array)
