76eccdd494827a0113d68bafbc9d9aa22239a642
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:04:45 +0000

    admit fasten hair black possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
