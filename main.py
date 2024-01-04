9713246f9ee13628f37283e8497e76e931fc9394
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:52 +0000

    loose slow challenge out concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
