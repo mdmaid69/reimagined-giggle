8232e58b2aac69ed1dc5e5ffa94a65a73e935116
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:21:02 +0000

    no choose release apply common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
