3bc5f61c9a4c048d6781af0c3cde211ea6df85cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:41:46 +0000

    ordinary hot before negative attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import random
-print(random.randint(0, 100))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
