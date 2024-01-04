0742ce8fdb9e7b5b66ade7bcd6d3b877062d5881
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:55:04 +0000

    communicate consider professional direct tone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
