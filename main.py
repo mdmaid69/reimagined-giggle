915ced815281d84806151df1c8f0ea37cd7970f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:29:47 +0000

    at sorry rarely row band

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
