4f254d4442e2d4f2801533cdcff17eabd2ca77c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:56:27 +0000

    suckme point complain earn brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_acceleration(speed, time):
+        return speed / time
