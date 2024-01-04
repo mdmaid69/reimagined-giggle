290130fc23383f0b5b81ae8e2f2d9b91b83054c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:10 +0000

    method apologizeapologise used book term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
