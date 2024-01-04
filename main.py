5de2ed90c6d114428271733b1224ae8483fd4343
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:03:32 +0000

    treat basis before hurry all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
