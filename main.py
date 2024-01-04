b9ae278e2b1468898853d2b8837a39a11ebf9b7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:57:00 +0000

    so call increase divide thought

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
