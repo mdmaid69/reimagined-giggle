23cdb7ccd761bdf2a4bf1b554af8f10c9f19411a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:29:50 +0000

    agreement scream honest rush pride

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
