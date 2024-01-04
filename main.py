60e040802103bc32aea59e1908df41c3fbd24414
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:32:51 +0000

    connect respect space or smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
