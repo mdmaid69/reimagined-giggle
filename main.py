7fb6ce23c0f5b71cef14913ffcc6d2740f8718c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:31:45 +0000

    ahead first form help sleep

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
