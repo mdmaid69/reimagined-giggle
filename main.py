d8fb38841e793146753a98d1b30834b3886e7eae
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:43:54 +0000

    disgusting lose possible Ww surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
