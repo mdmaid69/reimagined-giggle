d127b52d270fdc881f6ba13e99517cb8609617a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:40:37 +0000

    determined same breakin excellent bring

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
