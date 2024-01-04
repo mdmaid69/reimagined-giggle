969b4805629319ee01af394878cf792ddc317c86
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:35 +0000

    close vanish pause gentle comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
