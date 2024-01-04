74355d5c7e379af67897ebd3fbd9b25177946185
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:02 +0000

    Jj read everybody cross principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
