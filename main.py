90cf4459e05013712d9d8af110e7160a76940939
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:30:55 +0000

    steep like everybody test must

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
