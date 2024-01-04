307b4bf81c5cf90c0d72dcb1104505c187ed0611
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:27:46 +0000

    moral payment set standard inquire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
