2177ca0e44d2bb9887c2deb5f746a73007cc57b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:22:49 +0000

    pretty lead use calm giveout

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+def roll_die():
+        return random.randint(1, 6)
