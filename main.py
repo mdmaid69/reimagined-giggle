bb55bf3399ccd44a013bf88bb74ca969ba8402f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:17:11 +0000

    stay talkback mark healthy put

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+print(math.pi)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
