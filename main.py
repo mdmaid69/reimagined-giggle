00a8199134c6dc197b7104da5133084ea2e98209
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:01 +0000

    dream lock load swallow getup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def find_max(lst):
-        return max(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_min(lst):
+        return min(lst)
