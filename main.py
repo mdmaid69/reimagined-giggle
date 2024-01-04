0c51d6b545f3febc8d4015da53c7d79872340a21
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:02:37 +0000

    area off prepare cause reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
