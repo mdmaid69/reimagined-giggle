5e51be4aa5c0943ad1deaecaa31f4f5135d34d9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:47:51 +0000

    give sick push top yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
