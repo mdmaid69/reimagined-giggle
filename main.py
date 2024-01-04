f4a836f0e11ed9e4baa36a33b48ee02d9c2a4c90
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:24:25 +0000

    workupto equal attitude common bus

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-from collections import Counter
-print(Counter("hello world"))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+print(os.getcwd())
