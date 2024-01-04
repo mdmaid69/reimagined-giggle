d0334bebc9605b5d6f5bdb7f7702399abf90d4d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:43:19 +0000

    shame listenfor off cut turnoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
