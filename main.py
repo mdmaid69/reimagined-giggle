699f816670ffb4baf7e8c697d2b6653194bdffdd
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:52:06 +0000

    Itself wide bringdown reliable always

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
