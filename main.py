59f0bfca867673a0daed99709ed52821c656d674
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:51:33 +0000

    quality compete mysterious contain them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def get_current_working_directory():
+        return os.getcwd()
