bbdded5d93bd5fc6fd03447de7409b352df88a1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:18:59 +0000

    purpose mark Oo strong brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def split_path(path):
+        return os.path.split(path)
