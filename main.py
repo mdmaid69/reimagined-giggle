6faca4460531a37e3fdd66cdd26d53ea8173ab0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:23:54 +0000

    emphasis expect wide bang especially

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def remove_duplicates(lst):
+        return list(set(lst))
