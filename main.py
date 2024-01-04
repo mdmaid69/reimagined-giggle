c7a9a6bd4ea7b66d33d0e40c6f476cccdabebf91
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:25:02 +0000

    catch shame teach dinner possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
