2c4ca1f0658032720d49704584790717fa29f62c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:31:53 +0000

    where rescue principle tooth achievement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_speed(distance, time):
+        return distance / time
