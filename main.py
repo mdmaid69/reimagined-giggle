14a2c9841432f2ea255515768eef77c0395764b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:49:12 +0000

    something slide port bottom wall

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
