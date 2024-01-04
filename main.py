f4c18238c62560d8177382b709f762c50f1de206
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:25:11 +0000

    object seem mend thirsty scale

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
