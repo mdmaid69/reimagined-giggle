59326c3590889231ca036918e0a068e709354e4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:11 +0000

    empty block tomorrow steal nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
