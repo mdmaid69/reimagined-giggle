ec32a05ccba118268a2a87e540c4252f5ea7f92c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:13:50 +0000

    turnaway ordinary tidy whenever next

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_item(array, i):
-        return array[i]
+def count_elements(lst):
+        return len(lst)
+  def convert_to_octal(n):
+        return oct(n)
