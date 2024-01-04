d02402d9d54425405a2593fecc96eaf26804fe3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:00:48 +0000

    quit matter uponon word definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+import sys
+print(sys.version)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
