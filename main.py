03c1083562fe0e7e8e8d9631ca8c3f3cc54468da
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:48:42 +0000

    bringforward recover custom previous react

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
