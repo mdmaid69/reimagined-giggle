f617a2dc63fe2251bd6d9b9894ca4a2c2fbdd290
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:06 +0000

    part rich touch peaceful set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
