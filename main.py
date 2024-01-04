cf07115498e02cbc2b83a8f6558e24ce311a7bee
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:13:19 +0000

    567enemy afford thinkof rather only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def reverse_array(array):
+        array.reverse()
