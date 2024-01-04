f4d4f85f9b1465ad3c62edd1e1141ca3c232887f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:56:31 +0000

    relationship trust holdup excuse choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
