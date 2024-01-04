ae816ebda40ed04eb8bdf12514622b6a05500269
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:40 +0000

    sea prize stupid naturally lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_words(sentence):
+        return len(sentence.split())
