5307259ffa4839642eae640bf591f2016e776c71
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:41 +0000

    moon collection destroy fun nigga

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+import sys
+def exit_program():
+        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
