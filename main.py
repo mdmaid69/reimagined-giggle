9688e23214a6449e356ef10f8f73a4926ecf5f33
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:05:29 +0000

    post Ii television rid rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import sys
+print(sys.version)
