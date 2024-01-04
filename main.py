0df959934d700696e122710a6f3c94c8e0153e86
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:33 +0000

    force arm prize pot moral

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import platform
+def get_python_version():
+        return platform.python_version()
