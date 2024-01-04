f982afe95f6be3eeaf5a35b076bb4dc68480e09f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:17 +0000

    understand geton nowhere star Cc

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def is_even(n):
-        return n % 2 == 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
