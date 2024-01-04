bc1558fef672b39887e73abb0396bc4b428340e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:10:58 +0000

    achieve back through turnup collection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
