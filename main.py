e982b84c12ea5fc5b7a967c4ee9f591e62eafbe4
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:25:56 +0000

    call visit makeup memory unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def sort_list(lst):
+        return sorted(lst)
