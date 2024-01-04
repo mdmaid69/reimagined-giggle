ba59969f496c1f0885dcf3de2c225b4cc9307974
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:22:29 +0000

    contents guess avoid correction fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
