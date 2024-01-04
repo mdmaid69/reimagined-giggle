9ed396d9b2984e85dbd0c9d55c47c615a49e4049
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:26 +0000

    disapprove help influence fun none

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
