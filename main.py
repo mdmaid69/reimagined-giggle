28e5d73b96e77d021e2247d2603808d2b07dcc2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:10 +0000

    forgive life welcome respect thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def count_elements(lst):
-        return len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
