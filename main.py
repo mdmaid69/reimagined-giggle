419641b56808717c25716ca219011983bc40ed03
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:58 +0000

    conduct oil reliable pass car

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print(sum(range(10)))
