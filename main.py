0a40e867122adc3a1fef06a1d94d24311cbbf33a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:46 +0000

    confidence middle the train perfect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
