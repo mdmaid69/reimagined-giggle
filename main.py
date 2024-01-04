7967af8b4609e907ebf59926918418dfe54b9193
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:23:56 +0000

    impatient doubt sharp mistake hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
