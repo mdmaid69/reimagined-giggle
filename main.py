f361b610963622e9dbddde538a6a380a449f03d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:07:09 +0000

    then correct reply Mm near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
