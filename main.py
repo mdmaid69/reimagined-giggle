e9fe4b6c9f919b24079677ff311d2cee6ff18985
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:28:42 +0000

    face condition steam comeout here

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
