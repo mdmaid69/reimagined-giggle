99700c585a8faaa5bd27e88f4e83b64547bae7dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:21 +0000

    tragedy forward lend situation suffer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def square_number(x):
-        return x**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_power(work, time):
+        return work / time
