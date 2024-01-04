3bea857ecdbfcf6eb26e6ee5b46fde5a399f7791
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:49:56 +0000

    anywayanyhow cinema separate news tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
