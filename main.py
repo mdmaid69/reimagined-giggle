c465b37d9967e84ab39813c7f42268353e6ebc1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:17:16 +0000

    success runinto beautiful compete comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-print(os.getcwd())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
