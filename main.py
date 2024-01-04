48577fed4075bfc290a56a7488db5e4ce4c9a209
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:47:45 +0000

    prize mood score deliberately pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
