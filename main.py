d9fea92e9815ad534a8a21bafa4678c14f959ee2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:45:45 +0000

    pressure regularly fasten in half

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
