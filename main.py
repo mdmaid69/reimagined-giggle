98efb53e1ce616a1a43b95dca915458530a635fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:55:25 +0000

    uncertain Uu little indeed today

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
