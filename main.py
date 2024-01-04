39b428104122b06aab2576bb7ac177923eb897b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:02:32 +0000

    difference cold much impressive certain

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def get_list_from_array(array):
+        return array.tolist()
+import random
+def roll_die():
+        return random.randint(1, 6)
