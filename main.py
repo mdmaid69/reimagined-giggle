62a175c66cd3ed1f40471d7df845de43c4d224e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:30:08 +0000

    equal split cost mad bored

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
