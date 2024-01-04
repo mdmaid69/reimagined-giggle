4580461c30f9fc7fffb28ca629c82e2ac10c8db7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:11:15 +0000

    upwards brief order would actually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print("Hello, world!")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
