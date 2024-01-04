c8a12a9ed382bd218a0c00d3e1764447ae703326
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:33:25 +0000

    exact tidy pleased popular future

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
