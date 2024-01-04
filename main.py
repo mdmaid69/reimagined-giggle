7fb08421c3ce831519b43761434b0e990b9f763f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:46:58 +0000

    formal can vote chief complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Hello, world!")
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
