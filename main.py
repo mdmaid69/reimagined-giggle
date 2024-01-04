be194d9a16a46ca375f37ff2d55634d178eadd07
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:12:07 +0000

    calloff apologizeapologise opinion upset Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import os
-print(os.getcwd())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
