2820efc6588d6df2e1cc67d2384c06ddb5148131
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:00:27 +0000

    possession Oo read previous town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
