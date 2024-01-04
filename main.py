1154719dafedb447c490c000924fe217260937a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:49 +0000

    world sign visit greeting similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
