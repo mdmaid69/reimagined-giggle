70a117312e74c5179f8ed96c8a006f37d6fb410c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:25:40 +0000

    gate likely exam little change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def divide_numbers(x, y):
+        return x / y
