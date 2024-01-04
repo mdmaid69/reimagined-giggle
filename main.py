349c31e62ee79bc21a321fdbfeff869c876e8d4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:27:19 +0000

    human label travel bet me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
