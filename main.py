fea2e27adf9bfa35e4076d904ba8d05f0ea10af2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:30:29 +0000

    wash similar conversation rent whole

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+from collections import Counter
+print(Counter("hello world"))
