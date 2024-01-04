097a55dc2453b09c169881dfdd98ec135fac0fb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:20:24 +0000

    spray try whether owe protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
