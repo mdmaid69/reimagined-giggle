294f3ff25f528b6121c6996a8ebfc4b4ec40f683
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:09 +0000

    comefrom practical well health plastic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print(sum(range(10)))
