9d9335f8bbd858e6dba8b13f1d325a17fbe42b17
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:25 +0000

    comment relief careless neither cold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
