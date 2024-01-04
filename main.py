452633629b1659fa7339ddd23127844efbbc863b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:42:55 +0000

    performance fire hide share equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
