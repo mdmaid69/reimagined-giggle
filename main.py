16ae6c6b4aaff4ba5c2339baf07dd3a5a1224168
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:26:43 +0000

    bother somewhere weather door mean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
