20990dca1de46dfaf77ea2b2ebfbd509ec2dd9d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:47 +0000

    warning dig chin scream relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
