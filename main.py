78c165becc39b822a8f9015ebb0fee569e91b6f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:09:06 +0000

    firm Tt suckme thirsty perform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
+def subtract_numbers(x, y):
+        return x - y
