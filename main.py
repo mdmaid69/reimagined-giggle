051e8beca68b145f484e3bd4bde010db65373435
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:44 +0000

    people how emotion safety getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def count_words(sentence):
+        return len(sentence.split())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
