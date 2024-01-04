fb85fd9049219a48400bd3e801a2d9ff3bd61022
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:29 +0000

    empty insist advanced gap yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
