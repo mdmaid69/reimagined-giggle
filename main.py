ec21f2910e31abeb8cdec92331a3d07025054161
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:29:11 +0000

    strong hour shape fit doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def clear_array(array):
-        array *= 0
+def check_if_array_contains_item(array, item):
+        return item in array
