9895d5e593387276b7ee1c7d6512af308672f794
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:56:23 +0000

    impossible depend serve keepfrom quit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
