41f262888c115174c8a1f0584db3426209aedac7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:05 +0000

    autumn other land move autumn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def convert_to_hex(n):
+        return hex(n)
