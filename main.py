ef09f65f64dc7df4f24df6c28c46df2b0d388ad0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:43 +0000

    send common narrow pleased them

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def square_number(x):
+        return x**2
