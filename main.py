ac86d53c288c8f57625cc9fe3bf75c097afd9ecc
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:10:35 +0000

    wide number grow swim act

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def append_to_array(array, item):
+        array.append(item)
