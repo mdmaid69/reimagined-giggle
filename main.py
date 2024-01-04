3beb5e5de8d3ed885d50c5de0397f9a1b44b8257
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:35:09 +0000

    successful age wish proper mark

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
