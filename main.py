7f4924ad69126f3bf143ed757d44217989064c03
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:32:27 +0000

    strange exaggerate vanish word warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def append_to_array(array, item):
+        array.append(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
