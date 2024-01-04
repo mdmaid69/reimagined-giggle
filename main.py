66873aa537c18e64af2333d41719f79daecf0760
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:00:22 +0000

    shadow finish classroom case sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
