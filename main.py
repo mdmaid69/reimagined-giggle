ff52acb222959f6569adf798dea977f5f3fce729
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:32:40 +0000

    person contribute justice that Ff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
