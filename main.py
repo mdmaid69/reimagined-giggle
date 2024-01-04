21da3c472018a7fb6356582ffd7246bdb5ee835f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:44 +0000

    identify noone satisfied boring exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
