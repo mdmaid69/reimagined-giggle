d5c5a5d25bdca7edc86f32d75fbdba85070b5f1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:06:02 +0000

    level over Ee boil release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
