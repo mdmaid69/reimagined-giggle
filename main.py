1782f4caca8ec0f73d779473460ea02e35b79385
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:44 +0000

    discussion meal lookafter fork tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
