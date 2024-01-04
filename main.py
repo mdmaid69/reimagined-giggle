b324ea484d75ab643b06543bcca1518075549235
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:39:09 +0000

    negative visit purpose independent meaning

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
