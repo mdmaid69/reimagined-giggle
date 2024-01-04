be87c015105441893d1cf774d71681bbcbda52a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:29:03 +0000

    run left sting popular snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
