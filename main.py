a136f9bbeb95d478666791f58b2b0adab183e206
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:46:27 +0000

    state special there circumstance loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_item(array, i):
+        return array[i]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
