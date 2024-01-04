03bb558b2b397c695bab48d8d17e424f2016b0b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:37 +0000

    sign breakdown soak suit path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
