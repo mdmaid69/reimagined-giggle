e1b8877e021b3abae4106b9354a2b70f26edaf7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:24:52 +0000

    able plant there somehow clean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
