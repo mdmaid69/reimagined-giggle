0446d9edffa55763e063bb492bd300119416bc7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:17 +0000

    emphasize funny interview pair start

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
