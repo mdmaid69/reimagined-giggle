2f25b749a0fc0f450c372c34204caff73a9e7136
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:03 +0000

    profit kind turnout they why

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
