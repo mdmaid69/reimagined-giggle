86a7e07e17ebde269aa05d67cd7713cbda1bf3f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:05:25 +0000

    uniform on note leave frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
