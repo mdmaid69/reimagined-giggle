ad64c0e2f81bc71f44f25a84870defd7b8168fd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:34 +0000

    ill holdon soil recent long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
