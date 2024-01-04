9b778692b944e39fe2a456fd8e7c932d605384eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:11:49 +0000

    right general especially save usually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
