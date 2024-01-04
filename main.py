c6995cacfe89d166e744db139818c7bf45b30ae1
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:35 +0000

    stare weigh by fresh powerful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def count_elements(lst):
+        return len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
