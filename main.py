462944ef2e205e4c74444330fd2f52799191d880
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:12:51 +0000

    into lucky edge star gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
