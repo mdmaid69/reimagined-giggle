3aa77216637fd1b4938ee2ddd3a50fb702de053b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:19:34 +0000

    height tall shall shopping reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
