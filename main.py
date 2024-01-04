41b4d841a40b515a5258451d19b7c2a6822bedd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:02 +0000

    hit explain arm pull connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
