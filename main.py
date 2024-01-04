3ddff9bbd9a4c7fdcb859c07c989ea9e68713be9
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:54:39 +0000

    stop space over though lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
