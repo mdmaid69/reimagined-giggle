7b3067e119147ebd15e9ac47107892c82df745cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:46 +0000

    steam beneath file key correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_octal(n):
+        return oct(n)
