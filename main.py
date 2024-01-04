4b326c7c22c0deacf6cf03ddc5b403d8f45fa5fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:26:45 +0000

    communication way sticky equal fall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import array
-def get_array_as_set(array):
-        return set(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
