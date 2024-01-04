3a473cb810f87568d1f8fc9009c325f3219c9be2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:58:50 +0000

    classroom connect waste slow workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
