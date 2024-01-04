63c8458401c1310b5e9e15fd1788b63cb7ed8dd1
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:11:29 +0000

    necessary letter proper subject usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
