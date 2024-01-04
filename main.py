9f5f0bcb9fca4b762838acf1aa147afc43d65497
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:59:56 +0000

    meet union eat fresh duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
