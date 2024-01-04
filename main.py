0a944ef04b2e1f2cf6a147104d1fb190e72ba920
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:45 +0000

    correct pressure row keep explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
