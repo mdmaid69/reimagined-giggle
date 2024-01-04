830afee7d4b0dc5db08c58efcbf2286aaeb71e88
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:25:40 +0000

    discovery do challenge file ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_max(lst):
-        return max(lst)
+def append_to_array(array, item):
+        array.append(item)
