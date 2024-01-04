8d80628dd84ce79fd77a1c82fd0ed526d2ac7b12
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:21:01 +0000

    mixture issue rich event strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
