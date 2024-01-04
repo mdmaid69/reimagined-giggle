46a88c7c91d8b1370f7679fccf93115c13dc4ed6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:59 +0000

    content sweep help free nerve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
