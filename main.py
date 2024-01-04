f172e41e3b0e34d93943cbbbab2f0ff11eb645ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:43:43 +0000

    calloff lookafter confidence any proud

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
