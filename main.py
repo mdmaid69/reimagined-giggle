dd46cea43b2c953af8f1a60939616a29e3a87bcc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:39 +0000

    holiday clean platform realize any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def reverse_array(array):
+        array.reverse()
