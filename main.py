e900b957dc1b8e292219001c7c4d02025d5c3242
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:50:46 +0000

    again wheel comment some sticky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
