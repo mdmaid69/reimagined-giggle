e26dcce1dcbbda60244c2019a99db50bf10b41b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:39 +0000

    quality suggest moon reach geton

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
