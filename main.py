4db38a8ce839bfbe5cd7db5c72790600eb049fe3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:47:12 +0000

    believe him any long maybe

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
