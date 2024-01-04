c44ec2b872857c0f304e8164f2dc1c6f6dbd95fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:05:52 +0000

    fun let car on makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
