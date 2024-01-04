47b087c589c0ef29642cdde85771f38ecb64f194
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:07:27 +0000

    move respect sudden glass firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
