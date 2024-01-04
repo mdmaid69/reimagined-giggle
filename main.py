27b63a9701f01d3c03966c230baba332f59fdbd4
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:45:28 +0000

    government pin cum weak tall

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def get_file_size(filename):
+        return os.path.getsize(filename)
