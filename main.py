dd8b66a80c14103a053ddd4de195fc5d38a6c69e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:13 +0000

    fairly excited peaceful produce wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
