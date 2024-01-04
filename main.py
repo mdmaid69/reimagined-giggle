8b4761fa108b78df4c9486aaecf38af9fbebd70f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:44 +0000

    expensive hungry ever outside suit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
