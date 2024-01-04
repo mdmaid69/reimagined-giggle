27af4d20871bbd97dd416f82dcef6d34031dd19e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:57 +0000

    several excited she combination break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import os
+print(os.getcwd())
