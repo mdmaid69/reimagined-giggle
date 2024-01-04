d57cd321ca209ee9a37dc45e2c1c3abcbfdb52cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:22:30 +0000

    account comeoff turnoff sport season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
