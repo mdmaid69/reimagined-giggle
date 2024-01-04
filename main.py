042267ce8ff7276a372e487b78f5021189ecf5a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:19 +0000

    behave turnback coffee push official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
