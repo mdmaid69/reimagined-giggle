3e9d2c59eeb63dd3992b536fea985a9380f5a8cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:42:48 +0000

    warn dark contain beside awake

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
