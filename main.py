d756d69adc7143fc52a6c85b8bfa7af5a297a6fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:57 +0000

    hold weekend easy create sense

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
