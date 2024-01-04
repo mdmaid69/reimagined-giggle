63fd55767622e36f108078eac4f7131e827aebcf
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:21:58 +0000

    advantage wonder grand correct sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import sys
-def print_python_version():
-        return sys.version
+def calculate_power(work, time):
+        return work / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
