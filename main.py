f3fa7f69b4e9838f984fb9d41bf8c2349c5d4a68
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:08:10 +0000

    bottom listen total expect knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
