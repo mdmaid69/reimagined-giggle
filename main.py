c2f267ea2660af3d95f604d1e2a54c9448a5cfc6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:27:33 +0000

    announce decide suggestion morning picture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
