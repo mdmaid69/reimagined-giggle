2e5741bfbf3f16220c71cf2fe1af9b426072d736
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:36:48 +0000

    upwards commit organize to thumb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
