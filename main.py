b2de19912bbfc429471727065599a9922957caca
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:50 +0000

    bite butter Rr than ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def is_even(n):
+        return n % 2 == 0
