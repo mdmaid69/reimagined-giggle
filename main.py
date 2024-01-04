47b7abffa7c90d55fbd855f55865a837be21b869
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:21 +0000

    original distant joke birth down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
