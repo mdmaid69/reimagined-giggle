dc9bd0de300edb9c5948631477da999b8777957d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:42:19 +0000

    shut myself effort store chief

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
