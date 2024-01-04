7dc60dfe77656f99eae62e307ce66b7554a83f34
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:08:20 +0000

    still bite spot open read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
