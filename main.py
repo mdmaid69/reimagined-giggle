0c3ca4698a1143623db59bf944e5488e54f3a6b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:24:29 +0000

    empty offence shade label similar

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_acceleration(speed, time):
+        return speed / time
