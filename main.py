803056172d61776e281f95a53e0b0c1a1b4a8049
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:05 +0000

    cut show along public against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
