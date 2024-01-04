0f694e920b195d25c31bbf865ad8955c9c2a4821
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:45:56 +0000

    modern big combination ability getoff

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
