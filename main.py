46ecdc1afd3e00db9164934c11ae873a012fa43f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:31:43 +0000

    Ss smoke body takeback quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
