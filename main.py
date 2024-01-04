509af9cc32fcebc2610e26cc4181b3566871b818
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:53:02 +0000

    border punish shallow public vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
