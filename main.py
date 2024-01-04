016680d487b423d564cea83bc32e6f6a50ef2733
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:01 +0000

    hot introduction best change goon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
