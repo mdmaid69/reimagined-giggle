e1d9b069a89fd36321bb2f48026aac74a47c83f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:53 +0000

    smoke pocket issue bright dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
