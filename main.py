e5d2feb231bfaa88cfd02053648ad76acf2f582f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:57 +0000

    promise surprise constantly awake blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
