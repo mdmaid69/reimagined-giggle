6b5322aa06beded64638821bd034b4cc2b052942
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:36:25 +0000

    care this form poor use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
