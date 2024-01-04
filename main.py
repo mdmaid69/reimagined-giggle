1e4731e5919321e3ebc8b6b2c7550273b65cdc54
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:58 +0000

    breakup century ball ask hear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
