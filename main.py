d27dfb8692423be563e911b8084aae33978747ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:19:39 +0000

    criticism truth scrap persuade comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
