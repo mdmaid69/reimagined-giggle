c68bbca072f1d19660179ee71243ffd104f1e5ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:38 +0000

    birthday display Ss method artificial

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_index(array, item):
+        return array.index(item)
