66ab7d52a474e65f7c3b0d18277b9ff69183a73e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:43:32 +0000

    not copy maximum least space

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import collections
+def create_priority_queue():
+        return collections.deque()
