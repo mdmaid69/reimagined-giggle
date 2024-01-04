2e7f1eedf4c6802b91134c707b94619f72ee4a68
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:16:36 +0000

    share hers fault bringback proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import time
+print(time.time())
