493fc2a2407b60653e754786492d5a396e8e600b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:04:01 +0000

    discuss enthusiastic influence class better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def subtract_numbers(x, y):
+        return x - y
