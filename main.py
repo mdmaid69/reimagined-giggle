407f0ced898c84e7856976bcde1d1a703f5b8d14
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:36 +0000

    check freeze jump search blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+from collections import Counter
+print(Counter("hello world"))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
