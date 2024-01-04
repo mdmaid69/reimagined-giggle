11dda20d7da9df69869293428091dd10ce3f6c52
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:51 +0000

    offence pure warm exhibition shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
