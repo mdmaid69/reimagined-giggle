8b87a706276ac8851f796af44cf4e6b81ba09641
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:02:00 +0000

    block picture bank second keep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
