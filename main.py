3444d93da758fe95b69c0383de6b7eb6869aced0
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:55:18 +0000

    Zz direct create carry type

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_typecode(array):
-        return array.typecode
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import re
+print(re.match("h.*o", "hello world"))
