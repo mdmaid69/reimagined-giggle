13b7eeddf770589c43d7d1435d666dd4cbe38f42
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:07 +0000

    positive combine general over getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
