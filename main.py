8ff79986ffea5b8712f6a2d008ac5da243427636
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:21 +0000

    godown walk declaration he hit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
