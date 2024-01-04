0a9bbeb18367d7a6fc177350e62016076de815a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:23:20 +0000

    its deep single corner goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
