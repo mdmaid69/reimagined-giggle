d00602caebd19600e5492566ba95462741e5526c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:32 +0000

    communicate rely himself black band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
