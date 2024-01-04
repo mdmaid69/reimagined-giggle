4b40f7f9dfde13266c6c27c7bc337bd09817c712
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:02 +0000

    between birthday thin cold deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
