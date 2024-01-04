580a245ee2cbb410a258a457df34eaae9d292bdc
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:37:51 +0000

    behind callback all occur press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
