31001f664fc5ebed844abedcccefce6083dfb994
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:38:17 +0000

    space before work favourite comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
