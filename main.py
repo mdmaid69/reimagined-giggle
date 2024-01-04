914853c3f6b4e0de61fa5b7b4ab02390ec253260
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:12:39 +0000

    strange trouble physical away admire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def multiply_numbers(x, y):
+        return x * y
