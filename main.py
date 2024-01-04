82daad51f0c4cb81cf298a7f1bd40c257d8392d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:05:07 +0000

    react honest light your take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
