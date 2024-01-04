c862b50d2fa9783ad20d6d927224abb93aad3098
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:42 +0000

    change generous demand direct along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
