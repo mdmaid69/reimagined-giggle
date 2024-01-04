ca441f42981c51d216c9bfe92d203550dc6b2c87
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:37:19 +0000

    damage string chase close aware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
