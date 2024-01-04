4ee47c204946c75bdefca4337da32c273c2af429
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:55 +0000

    past image hot loose plastic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def sort_list(lst):
+        return sorted(lst)
