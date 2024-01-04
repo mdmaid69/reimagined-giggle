ff811e8f9eba97da8b6f6deefbeccabb438f50e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:17:36 +0000

    phone Uu turnback process burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_sine(x):
+        return math.sin(x)
