6d868d57c3418bd64d57a3f199815da6526ebe3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:32:06 +0000

    rich occasion odd Jj pressure

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def cube_number(x):
+        return x**3
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
