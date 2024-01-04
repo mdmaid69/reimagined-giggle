05ba28e970275ea3ec82294a65345598cfd559e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:30:25 +0000

    thanks demand private short ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
