f7253622b612efcefaa18f063459fd2e0ba90ac6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:30 +0000

    fall present language naturally view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
