fde2cbd18cba0ad2e4aab44919a3d82874b4b39b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:49 +0000

    although dry emergency ring port

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+def exit_program():
+        sys.exit()
