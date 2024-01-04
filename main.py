75b946e97a3d8089c1779e195f935a5a41d573d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:39:55 +0000

    unhappy funny holiday sound design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
