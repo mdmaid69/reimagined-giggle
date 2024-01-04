1fc053937ba3fee33ca8ab3c4b7acea9fbecc8c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:38:41 +0000

    sink search deliberate set geton

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def add_numbers(a, b):
-        return a + b
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
