7fe9073cda1e62aa9661e7ba38ba6b3fe3419fb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:45:43 +0000

    whether rid pass loan drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def square_number(x):
-        return x**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
