5fdb60710c328a8bc8a1a509f34389a2557dba28
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:48:07 +0000

    send mad moral exchange furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
