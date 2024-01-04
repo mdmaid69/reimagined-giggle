9152b03c4c0202a8c78c929c1c0bab1d527ec492
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:10 +0000

    solve turn slide statement schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
