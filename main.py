cfdc727017b1a6b31955826b85e644ab079229c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:40 +0000

    ring talkoutof holdup hospital drop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_binary(n):
+        return bin(n)
