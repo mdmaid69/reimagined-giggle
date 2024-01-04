76b0abaf58a44c0809a9d00e2b3ce3b6aa087d8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:43 +0000

    spill lift real Zz hole

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
