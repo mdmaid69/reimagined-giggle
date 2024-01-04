d3b7b2a5c4f7a6f81a4f443c4edc7a549b5a47c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:36:21 +0000

    evening convinced matter doubt flow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
