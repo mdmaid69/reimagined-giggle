f939e79ecc555ed083e2aa6f2763e02655c5e93a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:37:41 +0000

    it reach may post during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_current_date():
+        return datetime.date.today()
