1e156dc15745875e960401bc6e9a73c0d28087b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:41:36 +0000

    swallow surprised cook mouth thinkthrough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
