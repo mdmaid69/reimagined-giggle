e1c0abdf49a53f5e5f410c2976f44bdcb945ddcc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:33:44 +0000

    accidental sharp tradition different turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
