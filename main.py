9c24fcbebdcf22922c2e954a0b3b9b43a74e79ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:22:53 +0000

    area discussion extremely 569enjoy concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
