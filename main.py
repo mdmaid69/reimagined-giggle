0cf9f4f025235e5e5f8040d7b6298208d8c36b81
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:23 +0000

    schedule yesterday everyone straight pin

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
