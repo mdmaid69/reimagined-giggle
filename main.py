b0cd3d28463b2042e3f4d170e7ac13a26f7f1b95
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:34:42 +0000

    wind week ear work silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
