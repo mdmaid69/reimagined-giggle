2380f9d916673395f777786712f4d2f4d6ef97ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:43 +0000

    nurse period practice light hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
