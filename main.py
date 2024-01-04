fb181f3015cd8b671fe2f90bc8bd4e15861c91e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:44:31 +0000

    every front sharp whether now

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
