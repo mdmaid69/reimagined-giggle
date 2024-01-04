72db2dc3813aa6328738788aae16a6018b863d3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:15:46 +0000

    lead enthusiastic field but central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
