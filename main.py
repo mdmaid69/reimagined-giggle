7e5aec8fc07e04d3f2ab4697fcb42212544d71b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:31:47 +0000

    reject journey slowly usual whichever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_hex(n):
-        return hex(n)
+def find_max(lst):
+        return max(lst)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
