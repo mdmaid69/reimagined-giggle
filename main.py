a835c1e9638961c8db8f703cd53832f652ec5121
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:47:48 +0000

    hurt protest unlikely furniture pity

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def cube_number(x):
+        return x**3
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
