d2725d064c6604fd547310adbc9401c039707029
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:27 +0000

    use bet emphasis join ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_to_octal(n):
+        return oct(n)
