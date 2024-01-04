592c89b0cd2779bf06bf0f7e1f0229ed5ec154db
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:54:45 +0000

    fashion strange equal middle insist

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
