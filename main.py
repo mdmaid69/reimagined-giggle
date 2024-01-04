2e6667bc4516edf90833ef8e6c79a8923a8a9fc1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:47 +0000

    fortunately paper round hair height

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+print(sum(range(10)))
