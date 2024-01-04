28b2b37faf6ba27b6a2676654e159263f2845309
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:14 +0000

    179bite rough train change pride

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
