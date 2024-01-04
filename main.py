2cbbf0ee73e622dbd08c1f40a4d32efccefd4826
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:04:11 +0000

    wire off awake goback(to) rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
