594b844c4e6f0849d72944a784031b620523e709
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:47:54 +0000

    attract cum remember question already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def reverse_string(s):
+        return s[::-1]
