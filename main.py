57710da13f812832b869781a351e106c2bb12577
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:58 +0000

    activity account draw pause contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
