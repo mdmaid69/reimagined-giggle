a5cfca7de30c8ecc47504a712352cb8af604ff3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:36:46 +0000

    talkoutof cover university live hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_itemsize(array):
+        return array.itemsize
