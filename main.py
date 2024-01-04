58a4654b1a1e9277a6a3b3a0c7bc998ac03ebb05
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:27 +0000

    direct time comein telephone violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import os
+def change_working_directory(path):
+        os.chdir(path)
