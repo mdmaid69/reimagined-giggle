e5cef1e20ecd7a12ff1305368e5e9ebf5ab32c3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:44:29 +0000

    warning continue tear fashion kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
