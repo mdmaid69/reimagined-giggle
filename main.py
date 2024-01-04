f65174a88e4c476bc8559e40f67f9eb5a320c9bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:25:17 +0000

    conversation cut tap couple takeaway

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-print([x**2 for x in range(10)])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def convert_to_hex(n):
+        return hex(n)
