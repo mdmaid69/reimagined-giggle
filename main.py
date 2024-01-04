f2acfc8e71bccc39c5eacea205f926ef4607665b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:58:16 +0000

    colour possibly unable news goon

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
