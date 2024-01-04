b040655bc1e93578226cfee420a19bef58ab8e29
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:17:18 +0000

    everyone back decision once keepoff

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
