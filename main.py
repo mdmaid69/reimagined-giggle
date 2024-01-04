ff7f62aab28ff851d19f976f9a9d3cb74bbdec31
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:40 +0000

    snow price choose range accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import numpy as np
+print(np.array([1, 2, 3]))
