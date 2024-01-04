958281c5108ad467ffcd5d9b2f06cc260c6ef06d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:17:52 +0000

    thanks upstairs underneath move limit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
