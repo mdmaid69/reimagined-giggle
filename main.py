49ae01ff6843b31aa8a4bde39625a33273df2d6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:33:06 +0000

    wall reliable that phone spoil

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
