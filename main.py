52f3be953f439cfbfe12674c3a85bcb744c5df7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:14:27 +0000

    maybe owe each much protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
