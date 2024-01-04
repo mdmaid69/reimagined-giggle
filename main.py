7edf5a52ab98f2dd1ef1cd168404c44e93128a65
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:35 +0000

    broken beginning moon wash trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
