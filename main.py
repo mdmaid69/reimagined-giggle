92022b117189540008b12360a753b96cf98d2488
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:59:02 +0000

    there comeover game each runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
