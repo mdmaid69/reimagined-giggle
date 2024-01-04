05f3f375391420ec2af245752d5e2bdc35949ce5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:28:38 +0000

    mix generally might my ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
