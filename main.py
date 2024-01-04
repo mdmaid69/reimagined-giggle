651fa448a87bbc5d12ff4b50718fc17c906c9cb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:47:04 +0000

    cope total hurry swimming strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+print(math.pi)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
