af442b3e74b9de3de20f8e646502af327f293957
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:47:02 +0000

    complaint tell plastic grateful appearance

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
