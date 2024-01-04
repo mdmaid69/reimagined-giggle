0beb980bc5f3dd9e54f9b209e5f5a94228c2e4a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:54:34 +0000

    flood minute its battery back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
