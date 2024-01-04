d58375e6edd45be2f88e5d5996ce20f2cbeb89ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:35:45 +0000

    extraordinary good herself pretend talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-print(math.pi)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
