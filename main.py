4786ca36b53cfa8079d3b53a84a508fdde6c7070
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:57:10 +0000

    interview wander whichever bother brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
