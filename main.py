378ef1775e3e5f59e3e318b23c46b2105d1d1219
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:39 +0000

    meaning count type reason rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
