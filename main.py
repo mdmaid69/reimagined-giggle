84a2b675bfa61b98486dd6abcb93ef9848e54806
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:48:29 +0000

    live care who cook pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
