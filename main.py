89def2a76fdd6d3c3768f03e8e7a37c1eae868ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:48:16 +0000

    hurt yet their since independent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
