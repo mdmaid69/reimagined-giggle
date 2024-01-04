3486b1afd64b853d57e7b52e89f5a2029f9eb4f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:47:56 +0000

    cough used broad later high

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 text = "Hello, world!"
-print("Words:", len(text.split()))
-import getpass
-def get_username():
-        return getpass.getuser()
+print("Reversed:", text[::-1])
