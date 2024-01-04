d0f2a4cc0cec5c50eea814761d2cbebbdb0553c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:48 +0000

    start worry hers there even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
