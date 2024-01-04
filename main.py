58de7ae70fbbceeb40e8c557167af9f3dd533514
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:39:06 +0000

    rank Oo fire Yy past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
