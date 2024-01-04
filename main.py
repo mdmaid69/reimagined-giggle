b488ba0610873cc400932b23da0bd9d0a9987da6
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:50 +0000

    check nervous fold about pronounce

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-print(sum(range(10)))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
