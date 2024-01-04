06916701cf627a9b03f96f9702993dce4454aaaf
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:57 +0000

    understand attach tight separate dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def convert_to_octal(n):
+        return oct(n)
