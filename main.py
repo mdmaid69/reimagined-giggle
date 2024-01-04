479413ea439022f517036a42e25aea91cae3db36
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:51 +0000

    address turnback breakin above pupil

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
