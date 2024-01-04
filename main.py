c599e4a7c89c580bb28385972ca77da84e6411df
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:55:37 +0000

    pull ocean Tt disagree myself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sys
+print(sys.version)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
