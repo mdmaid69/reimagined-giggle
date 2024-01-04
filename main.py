cc373aead0106fbcf3b53c7535c7cd2d9417561a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:45:57 +0000

    soon wish look favour exploration

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
