e4f5aebbac2b4983e2a73567543e478c335487ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:45:13 +0000

    bad rely takeback frequent reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
