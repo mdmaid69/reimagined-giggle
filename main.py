72cebeadcca77c750a6ad6d13effe145a8136bda
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:24 +0000

    squeeze thing prevent new hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
