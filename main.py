5cbeb1dfaedc0116b33da1b89264fa3bfcebb4e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:26:32 +0000

    giveaway talk spend again walk

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
