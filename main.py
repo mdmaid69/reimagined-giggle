d3c03906b22bcdbe226b0ebdc9e8e50b9a6a4c33
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:07 +0000

    fond wonder shut stretch collect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+name = "Python"
+print("Hello,", name)
