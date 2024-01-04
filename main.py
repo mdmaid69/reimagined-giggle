dbdb60a2c1a63ce58844796ddcdde91a7edcbd64
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:19:45 +0000

    impression splash mess weak sticky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import logging
+def log_message(message):
+        logging.info(message)
+for i in range(5):
+        print(i)
