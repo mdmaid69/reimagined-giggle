daa7647356d7025c1d583619636257ef1bdff256
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:07:09 +0000

    want night Ww for beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def set_array_item(array, i, item):
+        array[i] = item
