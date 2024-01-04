c320505ca8209ace0485ef621e977ab44579df0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:09:29 +0000

    criticism preparation lift pause world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
