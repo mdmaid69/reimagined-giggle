805757bfd461c8f1e6955a76cedbee3b4d4c1003
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:05:08 +0000

    run common what mistake contain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
