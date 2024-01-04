4502937d91c444035e8b00c17de44f5d5511112c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:40:59 +0000

    less spring will business somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+x = 10
+y = 20
+print("Sum:", x + y)
