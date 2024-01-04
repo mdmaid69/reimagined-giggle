573467b745bd9e8318cb481727c39328aeaf7fa1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:54 +0000

    sorry usually mess expression deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def clear_array(array):
+        array *= 0
