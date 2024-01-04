64da8180ef58796e69540877283d402e03ea1103
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:16 +0000

    tax practise different level near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
