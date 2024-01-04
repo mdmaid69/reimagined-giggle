5569862bffbed585c39f262d5ca45981517f8637
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:19 +0000

    test mass suck sad or

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def print_python_version():
-        return sys.version
-import sys
-def exit_program():
-        sys.exit()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
