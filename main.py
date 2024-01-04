4c62e7a601e6b34dd8a32863e5135a65e828322a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:21:03 +0000

    dance owe card weigh regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
