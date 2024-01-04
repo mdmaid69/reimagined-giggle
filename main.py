a552ea41d45cc3841766308dad65fc35aa2dbdcc
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:16:51 +0000

    dead tip praise stand whichever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def add_numbers(x, y):
-        return x + y
+for i in range(5):
+        print(i)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
