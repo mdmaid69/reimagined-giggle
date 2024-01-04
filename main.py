2430d77e64e5fd3d820eec1d2869f13e57930f32
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:22 +0000

    busy smoke influence plan bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
