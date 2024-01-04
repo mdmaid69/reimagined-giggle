ba3d91c9511822267018f069060fe167adda9194
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:33 +0000

    full earth string floor swing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
