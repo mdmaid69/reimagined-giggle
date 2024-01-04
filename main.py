fa135488d7728830e9e9ed87cfa8f66e58b49a15
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:39:28 +0000

    mystery accept preparation doubt standard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
