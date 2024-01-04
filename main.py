552e19ea5775d60b2675fce0261433c44e573a14
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:18:15 +0000

    pleasure reasonable politics wish along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
