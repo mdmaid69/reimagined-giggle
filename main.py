242d1da2e91d5688b2fdbcf44d6f951b1148696b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:20 +0000

    see off fire sight town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
