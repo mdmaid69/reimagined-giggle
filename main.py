f51c451391daa15f5bae21ba60c6d68ebccd8290
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:47:38 +0000

    contain comment add top ours

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
