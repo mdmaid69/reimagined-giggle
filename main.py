5edae06aabcaa2f6ead629a0637ec77e827806d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:58:17 +0000

    grade report offer silent rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
