b316bba6203e56af7b4f406cebeccce5cc9545f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:13:25 +0000

    grand crash with equipment position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
   def get_file_device(file_name):
         return os.stat(file_name).st_dev
