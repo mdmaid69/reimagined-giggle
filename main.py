7226e615f304345da72dd24bf03866bd7ce26920
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:08:49 +0000

    burn illness secret church clothes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
