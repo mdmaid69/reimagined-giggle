6abe6e0c807325a58c18f5294ce92a3b0fb6e040
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:25:24 +0000

    left measure plenty town other

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_numbers(numbers):
+        return sorted(numbers)
