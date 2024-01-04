c9d2343f04ede184fad954f96714c4aa7e381e23
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:58 +0000

    repeat want total couple play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
