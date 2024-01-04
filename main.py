dedc8ab9d9bee5e6035f1e6301b8e068da990324
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:43:52 +0000

    exchange loss switch direction by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
