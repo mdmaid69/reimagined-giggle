6aa2c54bde4a4ba9862edf6b109731be4ec1e705
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:27:48 +0000

    in chief office buy wealth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
