0bfe9cc051b29d4abdebb46e25d53b6b7f9e3eb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:50:50 +0000

    mean level name safety everything

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
