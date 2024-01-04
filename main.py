d1bca4b0b0bb635a2afaaa22e9db25e8eb7fbcff
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:15 +0000

    bill accidentally though getoff over

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+def cube_number(x):
+        return x**3
