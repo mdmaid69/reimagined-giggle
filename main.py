d837c9d376b96d10775a8cd3129dbb92eb6a159b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:41:51 +0000

    alone watch any throughout single

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
