ef1309a9ce8d04aab3c82a001de71ccaf96de85d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:22:33 +0000

    set organize recently nigga slice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def count_words(sentence):
+        return len(sentence.split())
