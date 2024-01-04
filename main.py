8c0489c7f608b8facd0aefb47b3507914df10597
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:50:28 +0000

    content from pause snow part

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_typecode(array):
+        return array.typecode
