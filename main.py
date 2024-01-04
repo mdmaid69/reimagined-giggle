41ecec3f6cac9026231595898679f863863d8fa0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:02:31 +0000

    turnback giveback comeback healthy offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
