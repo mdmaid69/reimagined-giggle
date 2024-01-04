f248e9b07dd915be5728995391567ba14e689515
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:45:25 +0000

    upstairs joke movement narrow reasonable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
