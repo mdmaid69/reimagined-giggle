e6a24e84d6783c5fed7b5694971a031926086497
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:48:32 +0000

    rate film simple shut through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
+  def convert_to_binary(n):
+        return bin(n)
