e33cc57050a581e05cde1412dfc6a8707803f405
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:48:40 +0000

    chin afternoon outside excuse path

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
