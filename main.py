3292e2b05fdf3dfec298791ba65c0487d9639f1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:29:58 +0000

    finally movement though well punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
