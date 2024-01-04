909d9e1cc4935fa4b08625ec91873b4914b88e17
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:42:45 +0000

    just company dig keen floor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
