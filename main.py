473eb0d455baadcb36175f8e31355b5edeadc6be
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:51:29 +0000

    issue regard city round hope

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+import numpy as np
+print(np.array([1, 2, 3]))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
