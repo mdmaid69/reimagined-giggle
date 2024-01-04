afa9b41f8880b82919c8be9419bdcd94b7601a6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:48 +0000

    future fall far attitude bad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
