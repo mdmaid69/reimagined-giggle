0460420e19b98a1cfa059f5d3562351f87626055
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:21:22 +0000

    murder leg over crisis tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
