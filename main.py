0ebb567a9c521a1984f154f888a3b4d827ba4d4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:07:43 +0000

    day born small right turnover

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+print(sum(range(10)))
