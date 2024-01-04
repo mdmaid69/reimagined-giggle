1b4c18833a811921a9c3f89be7191bc909ba3080
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:06 +0000

    steam priority comfortable characteristic nature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
