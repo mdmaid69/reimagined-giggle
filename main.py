6231775b74ab24f554c2dd2443f077d65ee72d06
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:07:16 +0000

    loud interfere charge ought burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_set(array):
+        return set(array)
