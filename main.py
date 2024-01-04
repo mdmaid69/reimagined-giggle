a69be3c138ed42d7e5e0ba288a61aaec25863e70
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:00:48 +0000

    difficulty miss increase prepare treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
