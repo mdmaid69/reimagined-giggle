8c9ebf08322a6fed183e32276a136aa61255bdad
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:04:26 +0000

    tool weak private copy even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
