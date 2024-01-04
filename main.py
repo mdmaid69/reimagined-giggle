577f6fd86558969f4360a58d9ad3d0e00a0b24b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:01:08 +0000

    stop payment loss little company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
