e9375000d387510cbe2bdb8bd307faa1946d93f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:01:04 +0000

    deliberately professional avoid wave form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+print(time.time())
