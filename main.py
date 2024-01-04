69281bb5ca43664cc9739ad31c3c8f5772195105
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:53:58 +0000

    back dress knowledge score though

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+for i in range(10): print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
