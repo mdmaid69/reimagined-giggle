7e717957077b3aa398d705d31fd0222e1910c2a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:20:19 +0000

    kill blow consist coat count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
