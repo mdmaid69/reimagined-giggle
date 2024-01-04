a0f61c562530b35abc55a752a36b9e340def2756
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:24:27 +0000

    tax phone worse concentration pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
