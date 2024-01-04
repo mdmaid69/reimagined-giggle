845eae70df937ccd91a4ee137462a3b1e3d2a856
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:45:08 +0000

    away minor necessary print question

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def cube_number(x):
+        return x**3
+def calculate_power(work, time):
+        return work / time
