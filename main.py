b0f22c9d21f1509645b9a9bcfb48074e10473351
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:02:07 +0000

    recognize peace label fix contain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def is_odd(n):
-        return n % 2 != 0
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
