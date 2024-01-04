0b8fe7ac725bd4acb3d07097a95198dac3b0dc8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:21:54 +0000

    tomorrow near break introduction keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
