5c92048f75b9e1a0cf3c4a25fa2c14d9761d791a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:33:32 +0000

    tax rule ashamed nature excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
