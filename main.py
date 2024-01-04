0f3b7b20d53db7f871321f3ce00a6b33f98f6aaf
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:09:26 +0000

    window beneath division sure deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
