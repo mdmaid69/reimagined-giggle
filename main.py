db01ba6f0e43dc2e70700b531ac50626212d7778
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:11 +0000

    frighten life Uu private fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_floor(x):
+        return math.floor(x)
