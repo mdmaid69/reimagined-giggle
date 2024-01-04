f922389ec1a913d78c4ac0429d1fd6e4c855dcfb
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:57:39 +0000

    Ll card dangerous come politics

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
