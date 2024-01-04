475fc06bbe6dabd9d57628d567b62ffdf9f38774
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:21:25 +0000

    interesting off often willing 571enough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
