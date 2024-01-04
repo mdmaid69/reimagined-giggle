150d1c7b0729d08c8d1290be111466ab95dbac83
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:43:34 +0000

    interview chase regret tidy live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
