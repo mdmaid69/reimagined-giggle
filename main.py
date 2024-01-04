e215af2b43cdeccbb5d6de38533f0cbfc119b983
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:48:51 +0000

    inquire definite cook phone aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
