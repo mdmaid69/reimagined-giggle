4d187c61c98e60bebad85c4875808d882e612283
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:12:44 +0000

    problem ability wish instrument raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def square_number(x):
+        return x**2
