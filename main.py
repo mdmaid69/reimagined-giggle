c7f58806cc0c554069a6425d393fe4f931bad139
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:19 +0000

    shock slightly operate sweep evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
