ae3c53ebd2550d52be409d3fdc8bb556611abe6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:26:26 +0000

    best zero release date knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def append_to_array(array, item):
-        array.append(item)
-  def add_numbers(x, y):
-        return x + y
+def get_array_as_bool(array):
+        return bool(array)
