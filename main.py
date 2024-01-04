8b5faa4a268082c7e60b22f64edde16287494567
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:59:38 +0000

    fold peace she Ff art

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def greet(name):
+        print(f"Hello, {name}!")
