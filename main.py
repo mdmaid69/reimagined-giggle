0034431c136e644a979500589140f51aa88161d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:25:56 +0000

    community search already make side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
