e6082b14f64653b6be6bb344f22b6a8bc5fc94f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:22:20 +0000

    mark breakfast criticize central certainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
