0d9df00381f0b22aa1c1eb56378afd9ea7cda232
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:33:49 +0000

    car grade run sleep several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import platform
+def get_python_version():
+        return platform.python_version()
