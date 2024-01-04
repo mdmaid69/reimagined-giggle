1b7fc188f5335b2a807a2c38ed9316996466c360
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:53:58 +0000

    reaction frame join leave load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
