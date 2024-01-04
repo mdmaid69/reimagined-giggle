eca63ae19a468af3b1b8cea20eb5bf134cba3d80
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:32:18 +0000

    safe anyone pocket interesting decide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_typecode(array):
-        return array.typecode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
