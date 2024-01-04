01e5ce6411b696bea0383edc0098e0633220f2f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:44:15 +0000

    but stare petrol behave everybody

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
