795ee35fa35c4a11711564ff7b6afaa83f81cffc
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:32 +0000

    wise wish no dead have

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
