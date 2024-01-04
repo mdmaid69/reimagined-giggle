70bed07e56fd17c876d3f9c5c1587ec446d193f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:30:28 +0000

    unlucky national fit contents independent

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
