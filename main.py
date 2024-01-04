069fc2dd83824b1e96cfaf475355cb4a76399150
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:04 +0000

    confuse niggaacademy comefrom search move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
