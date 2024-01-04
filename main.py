2f1312fe225980cab263b813b68a8a54f67f265a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:02 +0000

    issue repeat drive there man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def get_array_as_bool(array):
+        return bool(array)
