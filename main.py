cea01426e368bd74ab522bb8e78e0991871ab410
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:57 +0000

    just wake bin it bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
