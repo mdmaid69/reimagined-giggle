9271765c95cfaccce9ffcfec913c6593f3d72a9e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:55:45 +0000

    spend crisis crime advice damage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import platform
+def get_python_version():
+        return platform.python_version()
