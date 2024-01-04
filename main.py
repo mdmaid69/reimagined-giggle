c261d0342658311404f8d9c3c3dac3aa51fe59ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:21:01 +0000

    back present frighten continue star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
