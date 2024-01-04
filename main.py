324ae439be87622b0f4b600cb7c5fa29fb4ae74c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:39:45 +0000

    pronunciation matter chief forward brave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import datetime
-print(datetime.datetime.now())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
