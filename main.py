b368278e19e4e6f08a2119f9d1881998e24f30ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:55:26 +0000

    seem flow watch drink taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def find_max(numbers):
+        return max(numbers)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
