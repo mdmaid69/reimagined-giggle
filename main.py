0d5fe1b390feb7e6ff6ebb5b3c20fb7e7e4ea5a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:41:58 +0000

    consult past translate stretch criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
