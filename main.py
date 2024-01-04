0df5344ed799d7319ebc0cd9be1717d219124d28
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:47 +0000

    tradition cross unable understand game

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_density(mass, volume):
+        return mass / volume
