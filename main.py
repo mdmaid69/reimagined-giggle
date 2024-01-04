1a5247dea53b93327494793297fdfa2b6a5ba7cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:24:31 +0000

    duty soft last judge sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def reverse_string(s):
-        return s[::-1]
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
