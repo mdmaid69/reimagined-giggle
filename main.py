1aa921a12bc4832ce61ee1756ce9e59764396083
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:06 +0000

    no powerful clean whatever 572enter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
