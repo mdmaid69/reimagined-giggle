e50ae9f707f8c195f5811cf80136bdcdd4397f81
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:33 +0000

    last sing century couple wander

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import sys
+  def get_python_version():
+        return sys.version
