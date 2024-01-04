1f0f5fee7c3edb9dba27db16ddd0e21869afbf64
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:48:54 +0000

    us fit hit discussion frequent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
