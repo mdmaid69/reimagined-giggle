71722cf516712d9740a764bbe3e5ac50512f176d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:53:34 +0000

    once solve land Cc frequent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
