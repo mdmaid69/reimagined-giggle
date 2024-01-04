4c2525c2da248d2f89d6f2bcf9139ed15a8946c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:17:47 +0000

    harm walk picture agreement great

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
