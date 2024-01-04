be4884ec9f88a9d36aa8632989561df12d75d4c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:00:03 +0000

    exploration private moon be beneath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
