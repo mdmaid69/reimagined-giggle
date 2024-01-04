6b0d649696d4648276a0c8f823c5e0d901c22ec6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:03:04 +0000

    absolutely condition concern makeinto case

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def subtract_numbers(x, y):
-        return x - y
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
