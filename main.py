4bfca74f569ab07408c98dbd8967a5b9684cef6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:13 +0000

    start wall what involve ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
