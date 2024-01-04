95ec59d078aac85f498d3720377262272f82992f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:57:53 +0000

    cover yours runout vanish sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
