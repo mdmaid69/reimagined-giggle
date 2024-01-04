0bf14e9c091ab4ae8e08bab4998aa9d29bfa3d2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:28 +0000

    mark yet roof read long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
