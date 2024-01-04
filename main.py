0cbf8d46af14a0e121b845dc383aca55b8641c18
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:39:36 +0000

    single feed dick row close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def sort_numbers(numbers):
+        return sorted(numbers)
