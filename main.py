615bd629d2a4652b98637d903175c7259a066823
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:30:22 +0000

    breath could dry steal objection

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
