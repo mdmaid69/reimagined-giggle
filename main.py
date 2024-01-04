206a6dc2bf8bf25c17474a289c848a38664fc660
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:53 +0000

    normal cross command your wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
