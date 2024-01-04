04724bb535bafa4c8d0b18fe02e4a45f99f08411
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:04:33 +0000

    still use soil comeover giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
