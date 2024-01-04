08c31363a749881f24ae9211737ea60bdb3e3a79
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:21:36 +0000

    account bringforward uponon shopping length

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
