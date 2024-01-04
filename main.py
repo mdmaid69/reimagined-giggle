962860786d31a3a8e0748778b6e4cedf41405473
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:06:02 +0000

    spring eat any result use

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
