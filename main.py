09c627ad7fc624663f36da3a7450a1e4865cff08
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:23:27 +0000

    brain brush either furniture miss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
