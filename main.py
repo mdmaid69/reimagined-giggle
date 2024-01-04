6df054d84600d877f1ae289386c7ab3436fc3715
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:24 +0000

    float tip tough appeal swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_acceleration(speed, time):
+        return speed / time
