56fbe63d293381508655edbd9c7d5a9af3881843
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:05 +0000

    morning rank reason me height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
