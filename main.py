4abcba45ccfc071d9014c18afe4ae519d9640779
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:48 +0000

    telephone train keepaway exist swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
