a97faf344cf45c908c23391aa0ccb0b3b63a1a64
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:41:02 +0000

    say journey fit voice congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
