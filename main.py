1b661e89fc624e0bd43debd5ac42f292405834ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:20:08 +0000

    bath failure holiday stare except

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
