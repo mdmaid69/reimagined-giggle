fd3465dcea37f996364e84ecf2325ee27bb14c12
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:25:41 +0000

    vote let petrol shrink equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
