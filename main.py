dc4b761760cdb082f94dfd6575bff3575eca8667
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:18 +0000

    view afternoon sure influence week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
