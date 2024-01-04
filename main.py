7aa57d7a2b5fc10230df407b8f93a9e1f321ab79
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:15:54 +0000

    different neck introduce mine right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
