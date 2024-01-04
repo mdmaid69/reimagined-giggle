70e1fa130dbbf1cf622d5de090d21f927f44f1e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:13 +0000

    nowhere success discover generous theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
