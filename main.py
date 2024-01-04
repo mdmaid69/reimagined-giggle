e1dd45b51d99d257a6bfe2ec40a14785301fa726
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:56 +0000

    success raw happen love hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
