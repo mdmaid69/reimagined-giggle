e51a84353a06f514acbd233ba4371a8b40279b09
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:00:33 +0000

    pity turn birth disagree become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
