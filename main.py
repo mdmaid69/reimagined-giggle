dbc9658ae9df23003f298f45833b69d5ec17b7c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:28 +0000

    exercise glad note because speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
