a54efaf3d8d3ccfcb9f7b7ec2712c9d7868c76ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:35:07 +0000

    equal smoke yourself wave bath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
