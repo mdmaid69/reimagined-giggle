be612833510b38ca05e872abcef4e83eeb4f9da9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:34 +0000

    visit kind bang together proof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def remove_duplicates(lst):
+        return list(set(lst))
+import platform
+def get_python_version():
+        return platform.python_version()
