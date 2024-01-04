83c425c8646fbd78dc572cc1e9a2f6f40eba0b60
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:27:38 +0000

    whoever basis 565end slice blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
