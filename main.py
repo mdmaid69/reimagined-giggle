3879f559ca8f10d6de0ba5820a1e04d7df87e614
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:43:37 +0000

    arrival man address word before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
