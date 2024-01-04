ac1a2d40c312cd89568ef70a39e2e7c2d4121873
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:31:12 +0000

    lose search unlikely Oo book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
