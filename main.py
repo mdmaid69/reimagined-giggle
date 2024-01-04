883b6c344a84458da386732332039a1312fb1b4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:36:44 +0000

    Ss brave agreement authority miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
