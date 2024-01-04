4c75bd039180fc03c555238655d822d973a5a63c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:58 +0000

    temper personal keepup side Jj

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
