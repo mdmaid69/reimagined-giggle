828d9d23b00f981febeed64593084c2cf694e811
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:11:08 +0000

    takeout degree take consist until

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
