95e4deb8f94186e344bc21c0af7bb1c1b56e9518
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:12 +0000

    process night large exit breath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def split_path(path):
-        return os.path.split(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
