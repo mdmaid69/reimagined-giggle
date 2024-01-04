13ebb9e5c36bc013615e959b7c007641bbf065cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:32 +0000

    last mistake attach bang tape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def reverse_list(lst):
-        return lst[::-1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
