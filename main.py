90c0c5df9faf8f3993a04ffccc750100932d752f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:00 +0000

    over open car will history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def sort_list(lst):
+        return sorted(lst)
