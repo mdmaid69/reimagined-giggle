67752890a1ec559cb7fee702444dd08b26ff423a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:08:54 +0000

    plenty careful Ww suffer achievement

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_work(force, distance):
+        return force * distance
