53a7a3485b49bd9d77fe231a6178e263ad24f99b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:21:10 +0000

    pollution attempt scheme put rank

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
