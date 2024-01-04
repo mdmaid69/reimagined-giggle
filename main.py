a205a274d0134a4b9aebae8507a7d03e9e88c04b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:39 +0000

    year than shake centre actual

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
