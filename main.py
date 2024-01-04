07afcbe32ec8f42f5442d4a19b57d83e2b26b222
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:13:24 +0000

    stay several getaway hate crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
