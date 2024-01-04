0f34401f84b52463a259f87b7f1b6ebfd1693d81
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:49:39 +0000

    acceptable stay birth by holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
