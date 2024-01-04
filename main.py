d449779db01329c4db33acb282f09aaa133c9be4
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:59:23 +0000

    upwards squeeze change stamp none

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
