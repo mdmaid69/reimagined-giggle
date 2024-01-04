bebf3b81ee7097c17c16f8814c15a6d4d347d4d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:06:59 +0000

    lot class turnup day reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
