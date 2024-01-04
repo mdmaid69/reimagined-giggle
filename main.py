d0dc60c6a52634350c5fba3c360553862dff1f41
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:18 +0000

    building believe contrast deserve where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
