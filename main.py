5928a8542e685ff2489f9b3ebb53f49b4e839122
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:58:12 +0000

    appear comeup lead stamp arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_octal(n):
+        return oct(n)
