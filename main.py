b941155227a7da8f9606d1277ad00a412b3aa522
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:01:55 +0000

    normal relationship absolutely personal fast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
