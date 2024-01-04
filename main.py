995e19437da2b22697717ca2841fbb3a55cfb691
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:50 +0000

    ever lose mistake convincing left

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_density(mass, volume):
-        return mass / volume
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
