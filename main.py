e3374d2896344931b838a6b6c2b5a2a5a5002284
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:49 +0000

    conduct commit spite juice guess

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
