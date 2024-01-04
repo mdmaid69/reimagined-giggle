838c004d17b8d7fee4d1a4f58ea3dcf13bda4f95
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:53 +0000

    keep pin bridge hand cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def convert_to_hex(n):
+        return hex(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
