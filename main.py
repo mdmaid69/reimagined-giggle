d1012d4ff5dc380274e98a1b9493075f05384be5
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:50:44 +0000

    correct none answer pocket measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
