7900c869476528fdd3c1950a8ce6d9a1e4c05877
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:02:37 +0000

    poor education various while apply

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
