574aaeff44970f01c276ea0b89fbde8d48a9c88d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:13:28 +0000

    message unusual persuade test regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def get_array_as_list(array):
+        return list(array)
