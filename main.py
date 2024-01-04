b2550a9fb7b280fff41ee0ef629a3edb625eb076
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:28:31 +0000

    correction temperature afraid forgive round

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
