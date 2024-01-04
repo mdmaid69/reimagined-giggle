6706e25543dd6be1c40a12f0c50bdcfed1dec75f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:00 +0000

    common comeout aim recommend Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
