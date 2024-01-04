81d0e0a74def0e0e8fd34643aefb129065e6edde
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:48 +0000

    him breakfast draw smile flow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
