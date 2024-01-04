c6825827bf7f3a34418e4cd75c9bd77b6e4108dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:44:28 +0000

    pure judge play public funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import collections
+def create_user_string():
+        return collections.UserString()
