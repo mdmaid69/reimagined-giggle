11ad46810615fea4b56a982c1d437322b51924bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:40 +0000

    calmly achievement switch equal wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
