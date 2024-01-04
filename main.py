5b0f88a9f268e3c4ab7614d98cfc88b6b2e2d110
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:55 +0000

    cut sleep trip bite matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
