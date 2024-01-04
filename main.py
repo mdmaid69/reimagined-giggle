afe8f431d0bb0e34bcf32d87e4a2c3d84c58c039
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:24:11 +0000

    pour sharp add cry help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
