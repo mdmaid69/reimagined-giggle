8935888cb07f728d31774ac93b926e9a7d99dbb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:03:53 +0000

    workout natural burn snow shall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_pressure(force, area):
-        return force / area
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+print(os.getcwd())
