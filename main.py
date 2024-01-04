ccc7d9d435255443fec920c52c5b2f87ef3e35f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:15:37 +0000

    combination official she glass giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_circle(r):
+        return 3.14 * r**2
