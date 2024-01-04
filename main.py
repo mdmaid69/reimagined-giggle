a9376f9e6d40b664ba6b748d5d3b6dfa9479392e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:42:27 +0000

    failure obtain score happy belong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
