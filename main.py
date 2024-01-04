fcd07c2866f0c5a4fd56e2e80ac4acf0ae3104f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:08:02 +0000

    normal deal release angry at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
