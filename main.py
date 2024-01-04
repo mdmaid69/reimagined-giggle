2ac18331881d8282fde00059813451d7a72a8c5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:24:56 +0000

    convinced cool cum fairly return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+text = "Hello, world!"
+print("Characters:", len(text))
