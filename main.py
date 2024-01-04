3ffaf0a79ce85f1d0b3ef031acfddeff6c833c4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:03:33 +0000

    wise nowhere reduction operate time

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
