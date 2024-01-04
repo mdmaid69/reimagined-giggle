02a06531e2da92f1cb459d14eafa736a1a53fe50
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:40 +0000

    escape base tense left could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_current_working_directory():
+        return os.getcwd()
