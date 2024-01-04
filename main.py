8908ceff0ab8d049d0ebfe37a02052ca7ba0554f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:26:21 +0000

    heart human lie free joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
