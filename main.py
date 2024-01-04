565679ecf966a5db6c032720bf5d5c657a590867
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:55:15 +0000

    runaway dance hole healthy stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
