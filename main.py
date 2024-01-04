a9ba8f06a8bfe0d15e73160d6e3e9e1b978446c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:20 +0000

    birth make minute risk deal

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
