3518c26417155610c7d6d3da3ae63ab30c3c1b4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:37:50 +0000

    talkinto sign average black else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
