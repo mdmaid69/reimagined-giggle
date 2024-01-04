faa240e93b907c5722eb31cd3561291a379396fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:23 +0000

    combine try beat yourself safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import sys
+def exit_program():
+        sys.exit()
