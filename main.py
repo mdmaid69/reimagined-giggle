1f6c97ee4c0bd569b3eba753712141e281755ea2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:33 +0000

    distant than score decide achievement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
