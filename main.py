b824def8bc3d0f7a34b13dff6ec6e70ab4e07012
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:02 +0000

    wall pump slide yourself considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
