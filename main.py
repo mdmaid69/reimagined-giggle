f6aa7a18b311f309745c1c0eb0957a3155e0b07f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:43 +0000

    slide progress getup finger cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
