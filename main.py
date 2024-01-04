b4318b45d77fac89717a10fb54da40cb7e873a18
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:37:30 +0000

    care regular preparation level experience

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
