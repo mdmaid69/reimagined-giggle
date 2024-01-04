5409ea2a7a9c2e77a29e6b82c8301c7df13c8eeb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:08:11 +0000

    extremely event may rush course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
