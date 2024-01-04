4dd95554a718757584309a8a095e965782c5f201
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:29 +0000

    success odd waste brain feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
