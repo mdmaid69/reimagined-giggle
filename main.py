00f171627550dba7ea3aa897eb035c250b3b3cf1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:11:28 +0000

    question scream building neat keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
