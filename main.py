bf86cf1682abfea253aeb9e3c5c8c21533ab0533
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:42:05 +0000

    tap quality off first 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
