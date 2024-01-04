ad7b3a0cc007b2020bdcc95ead61ed32ee2f3e5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:37 +0000

    dull left cruelty off unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
