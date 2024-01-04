2c7daf1a118dda3359584e586dd8b3bb35802761
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:20:31 +0000

    point attract sink still your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
