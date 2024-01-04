6288b81dcdc4c731efb658717afca2c62748d684
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:18:29 +0000

    sea close excuse job set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import time
+def get_current_time():
+        return time.time()
