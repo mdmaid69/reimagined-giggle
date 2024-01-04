ab2babfbcf2cbb13ea9d965dd26947dbb875f025
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:37:44 +0000

    store temperature whichever bridge read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
