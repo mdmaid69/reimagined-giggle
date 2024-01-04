a15765e0023e30108c5c0844bb936423f68279ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:16 +0000

    let carry friendly must snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
