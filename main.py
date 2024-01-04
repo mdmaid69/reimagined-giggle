b1355a99bf0826759681fb176896efce2ce245ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:54 +0000

    know plastic country official fine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
