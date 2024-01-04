edfb7ab5614128f8d2d91bdc87c241a59108676c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:49:59 +0000

    closed standard lookback(on) cold finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import getpass
-def get_username():
-        return getpass.getuser()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
