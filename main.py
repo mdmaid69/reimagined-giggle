384d694e791e973022e716c88ad598f5e1836821
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:39 +0000

    cost summer worry tap play

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
