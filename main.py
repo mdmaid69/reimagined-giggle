e11dc2f1c646ea5b742e4fca8200f57012fd4b92
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:09:12 +0000

    Cc feature different want child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
