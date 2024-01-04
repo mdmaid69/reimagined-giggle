254c1a33a2650e0fe5a792a68ec8b96362c37168
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:06:52 +0000

    thankyou land godown which crowd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def multiply_numbers(x, y):
+        return x * y
