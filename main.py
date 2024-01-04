6c10d66a4d0538cf4710fe2211ca4b67efed86af
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:27:15 +0000

    train murder defeat damp share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
