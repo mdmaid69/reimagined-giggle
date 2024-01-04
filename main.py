45a2294766070c618242ccfc8a81b401a01a9798
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:34:22 +0000

    normal borrow fork rest her

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
