5ce5b9fb4991c0fdc4ef1c97000f63862897a7ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:45:13 +0000

    various early question agree stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(x, y):
-        return x + y
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
