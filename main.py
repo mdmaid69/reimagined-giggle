965b25dc60a49a447ce5f9d813a767f3af255dba
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:41:16 +0000

    sing themselves uncertain loan attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
