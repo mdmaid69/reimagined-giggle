f49d0a5e2499ca3ec7d2195d947c4d52b7b30150
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:25 +0000

    569enjoy Vv return there any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
