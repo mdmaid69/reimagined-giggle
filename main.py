bffd7e99e502f34a698d44408ea705f34db8b66b
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:42:03 +0000

    especially breakout quick issue carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
