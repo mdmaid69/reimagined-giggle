7976c96fb08f44415761eb7c4b3ad902b3bb94d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:01:54 +0000

    intend profit occasion cost reply

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def find_min(numbers):
-        return min(numbers)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
