119e0ec06a0bf1b45c70d7e8bcfac47109d562f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:11:45 +0000

    flow comment weekend negative chance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
