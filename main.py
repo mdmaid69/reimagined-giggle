69856f357c7efd91f4e9fcf3d9428614b21defb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:32:16 +0000

    day actually our full passenger

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def find_min(numbers):
+        return min(numbers)
