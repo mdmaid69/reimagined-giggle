3c7b4ac58f30a33286f43116dce9c54445072612
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:36 +0000

    difference wet be raw accordingto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import datetime
+print(datetime.datetime.now())
