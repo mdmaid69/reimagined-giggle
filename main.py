52a407ee7c8d1c6de5f7e813a6fa344439e92c63
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:03:55 +0000

    scared market rank nigga appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
