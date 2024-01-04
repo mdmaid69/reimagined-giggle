2632820c498b7faab46002959c52f21b51b307f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:48:35 +0000

    high belief vote however majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
