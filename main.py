c06e67a836b8ed40b1e81de731b4ff42deb567eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:17:12 +0000

    breakinto error settle less sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def square_number(x):
+        return x**2
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
