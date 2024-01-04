948c806d696c7feea04beb53860075ce7151f824
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:26 +0000

    wonder circle distance care dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def create_user_list():
+        return collections.UserList()
