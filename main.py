79b7eb3d2eeb7e6e97bb7fbbb07d40cfa260555f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:24:48 +0000

    experience famous central suit bridge

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def find_min(lst):
+        return min(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
