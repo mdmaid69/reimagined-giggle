d9c1b08167c3a5c08e54f99134b945cca4a6878c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:05 +0000

    attach pupil recover emphasis best

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_even(n):
+        return n % 2 == 0
