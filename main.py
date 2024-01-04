608f45799839ba058fddced0742ee6f62942418e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:22 +0000

    tell passage fear unite risk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
