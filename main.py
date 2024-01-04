02b53bc186b7ea6827523147bf82cdce7b05d3ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:04 +0000

    type chief description breath effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
