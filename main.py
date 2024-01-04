4ab21ed92524917da5892c1f7436c2727b4f2272
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:16 +0000

    violent advantage although influence dinner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
