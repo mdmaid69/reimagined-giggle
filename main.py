100a8c007195fd01db4225ac8b3f62011a508c3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:21 +0000

    country compare city high corner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
