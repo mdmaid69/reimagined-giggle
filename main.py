14e2bb30eea7d67080469f9105025506adddf5ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:49:46 +0000

    gift put contact my storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
