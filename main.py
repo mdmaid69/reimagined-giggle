6eba86aa349c0264e2f46dd3882296f8b1846138
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:56:16 +0000

    criticism only practise young follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
