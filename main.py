728117829fbacc13974f20eed46cb59c01e726ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:24:30 +0000

    beat file correction Itself rain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
