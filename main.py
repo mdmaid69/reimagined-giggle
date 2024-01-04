ca0b57516659c05dac824e946125aca4b51fc49e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:02 +0000

    little acceptable check machine could

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def count_elements(lst):
+        return len(lst)
