1b1c675e61b0430d4a83021bc82befac31936ffa
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:12:20 +0000

    before himself perfect aware please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def change_working_directory(path):
+        os.chdir(path)
