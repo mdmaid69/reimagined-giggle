84e27b448c8b0dc2215540e6c1bad8febfe3c0d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:14:35 +0000

    deliberately sad abroad country clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+print(os.getcwd())
