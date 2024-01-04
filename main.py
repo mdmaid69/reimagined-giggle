9e31142996d677264ec7e59933f4274b7e581706
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:00:32 +0000

    crazy uponon creature apply easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
