6d3de70e2b2f79d587cba54cd84f4ea177626329
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:34:05 +0000

    wear load history divide interruption

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
