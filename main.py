ffc42962a6382c3b89a37906022c685b21e9b555
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:22:38 +0000

    little file moment cover discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
