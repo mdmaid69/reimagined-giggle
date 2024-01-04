6e4bd6da15bc5c286a6731f721b67f67edf5bb4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:37:47 +0000

    meeting emphasize smart punish right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def count_characters(sentence):
+        return len(sentence)
