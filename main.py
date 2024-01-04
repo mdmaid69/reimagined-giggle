b2fd89b8e267006241fb3adadf2a903db415f5d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:50 +0000

    all blow discovery somebody shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
