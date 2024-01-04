6af56adc8fb310c44871dd6fdf409e9be7d28d47
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:08:23 +0000

    join rough room lost principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_even(n):
+        return n % 2 == 0
