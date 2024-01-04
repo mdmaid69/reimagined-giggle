636156fbded5f6d62e9e19b86a56b0a8496e50fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:47:46 +0000

    kind turnoff accept lunch opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
