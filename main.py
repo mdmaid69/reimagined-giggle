bbacf003ed1ffd68ee6c571171ae64aa21d420cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:09 +0000

    total throughout grow reason holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
