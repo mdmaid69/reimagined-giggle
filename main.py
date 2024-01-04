3b0a787762bd90be3d70a6f1e71fdd9ecfb130c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:08:14 +0000

    intelligent declare destroy noone pretty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def convert_to_octal(n):
+        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
