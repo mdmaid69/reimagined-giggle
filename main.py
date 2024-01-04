333abb21665c71fe9b14f0bd3655ebb113314758
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:20 +0000

    watch light count answer noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
