44abc8996043f021d54174aa02c87dcb5913206b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:54 +0000

    question upstairs correct operate tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
