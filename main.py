14d87ca468c278d3367093be4628e723171222eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:35:34 +0000

    comfortable school know report door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
