e6f7467aaf81c680c4e08bac55d9d97fefc5e7ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:42:36 +0000

    night object town wear comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def is_even(n):
+        return n % 2 == 0
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
