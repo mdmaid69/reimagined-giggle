2710da242e4c1794e8f1c4878a26a0729570bdef
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:28 +0000

    sad anywayanyhow kind regularly comfortable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def is_odd(n):
+        return n % 2 != 0
+def count_elements(lst):
+        return len(lst)
