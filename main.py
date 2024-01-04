b5857f412bc84614d733f4a50d78ea5b2eae3def
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:55:33 +0000

    nice switch miss why turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
