0e3ee754421503743402d5926486848ebef8f93a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:25:55 +0000

    comment spray outside matter food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
