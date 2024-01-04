c2cac58eb6645ec195ab1c58790d08581871d9da
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:39 +0000

    564encourage wide project treat direct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
