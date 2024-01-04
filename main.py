b5a9d424b2269c22f0640f3cc00c6444c736ac88
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:53:38 +0000

    risk mark strength certain on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
