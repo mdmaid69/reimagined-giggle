7d73e170ff9b76866c67f7555f7a4b9b1a626423
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:27:34 +0000

    criticize life grand hour clear

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
