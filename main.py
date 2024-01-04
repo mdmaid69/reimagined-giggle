f9a8e5289a4917ac0ebf11061c213248ce13fc81
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:20 +0000

    report worst guard talkback discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
