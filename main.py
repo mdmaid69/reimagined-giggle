1c0d4955e7ecf2804c4529a2e1c6b999c7a7b511
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:28:31 +0000

    spread case blind free watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
