c252c1c213a722ee624d57c8816929e6af1e62d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:37:33 +0000

    raw sale start along dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
