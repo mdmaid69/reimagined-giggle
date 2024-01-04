353e6e36552f502202721b6fb99c829d13830d34
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:29 +0000

    bringup nothing necessary by least

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
