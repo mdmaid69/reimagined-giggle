617a3e9c056d1e1ae40dd12e86f2079a676279e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:21 +0000

    bank nice escape genuine aware

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_length(array):
+        return len(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
