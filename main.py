b7b8c2cb6ba068443b4f99f5da21541eb001522c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:56 +0000

    computer trip grand hard which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+print(time.time())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
