b14e679129990d0845615b6bedd2fe1ff6688126
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:52:44 +0000

    store relation anything giveup poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+print(math.pi)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
