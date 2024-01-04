97023d4a98b2447e4ac8f30b2d425013f9460c86
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:57 +0000

    Oo history familiar heart sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+print(math.pi)
