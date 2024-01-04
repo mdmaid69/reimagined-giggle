bd4e0c95df8a3f8fcd7f46ec35e124ff32e273f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:16:53 +0000

    swing guilty pay fat threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
