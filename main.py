66c0cc3c8063299e42dfd6bf155e57bb4b771be7
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:50 +0000

    worth steam callback release hour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
