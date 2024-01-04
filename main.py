24102b2a4b55ab0a030d89427fba8fec05948914
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:27:49 +0000

    present dish completely goodbye comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
