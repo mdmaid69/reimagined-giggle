b3f392d388c8cba8d2f18301916cf19f3f74ebb6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:46 +0000

    address demand voice after lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
