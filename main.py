9c26972148dee0c01ecc2dfd46a7bbcc556bcf07
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:48:57 +0000

    wash deal level building bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
