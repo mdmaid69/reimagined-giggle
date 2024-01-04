e238b7bfbaf18da5a172c45c27ae20a87fae5497
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:16:05 +0000

    somebody ought connected bang each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
