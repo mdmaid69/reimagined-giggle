38f5ad3797348ca95f103886566a59fa7c33543d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:25 +0000

    breakinto wake will possible grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
