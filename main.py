c78c42be946454d17aad8b1420a207f2827897da
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:43:24 +0000

    lesson earth cinema middle lock

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def reverse_list(lst):
+        return lst[::-1]
