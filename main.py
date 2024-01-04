6bf9f9108d8ddde8596b7955ac0082bac7804461
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:27 +0000

    fine week several morning motion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
