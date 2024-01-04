c1782ee390dc7a580ce01eddc64e20697e1c7f49
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:49:50 +0000

    taste tape minimum charge bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def reverse_string(s):
-        return s[::-1]
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
