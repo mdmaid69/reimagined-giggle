0d3cf242eed49bcbc7f9833fe6bb13409f26ad08
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:56:42 +0000

    realize contact divide win spend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+text = "Hello, world!"
+print("Words:", len(text.split()))
