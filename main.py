6559a93ebe9ba3e519c59e4edf34378b1888eccc
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:05:23 +0000

    moral nonsense passenger us recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def cube_number(x):
+        return x**3
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
