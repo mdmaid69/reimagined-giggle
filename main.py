9cd905f55647d66c436ed3bbb25d12539f09575b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:05:09 +0000

    accordingto right spread operate or

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
