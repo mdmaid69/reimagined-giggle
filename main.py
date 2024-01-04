b0ba070519251416eb9dc51dbcbe5aee47a95534
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:46:39 +0000

    central suggest extra meal cross

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
