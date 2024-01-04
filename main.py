31289d321b865d0cbb6fa7a27a0b43bb513696b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:25:27 +0000

    concerned body become service extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def cube_number(x):
+        return x**3
