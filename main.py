237d8f30af4fe7bcd9e3c3b32750abb3da520fb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:41:28 +0000

    production original frame medium pattern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_volume(length, width, height):
+        return length * width * height
