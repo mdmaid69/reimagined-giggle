2a86f7aa08c38ad222620dc0eba4ebd5cebd092f
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:28:34 +0000

    feature own back send throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
