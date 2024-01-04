cf41fc7afe811d4d386f5949d613cb6ca5ebc505
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:59:50 +0000

    only whatever lend confusing declare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
