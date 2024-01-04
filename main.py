a71aa0e3a2e7f0e389f58524707312e434e7d289
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:53 +0000

    bin basic roof tooth order

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
