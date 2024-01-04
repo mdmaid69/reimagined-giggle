0c71e3322f1b6277b59096cd3236a7adad901cff
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:24:42 +0000

    try cruelty also temperature content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+def remove_directory(path):
+        os.rmdir(path)
