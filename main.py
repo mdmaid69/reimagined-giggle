72a67966ef0f7fe0f19563b40a18d4ef20f4f270
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:38 +0000

    either upwards alone table same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-print(time.time())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
