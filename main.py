388ba9e5c50f3c958c98bbbeefd082a4561bebe2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:45:27 +0000

    class hold complain understand shape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
