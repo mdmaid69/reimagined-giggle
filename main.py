7bdd17a04f2c8613483ea7976601eb0d659dcf3d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:31:57 +0000

    571enough wide flat rather measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
