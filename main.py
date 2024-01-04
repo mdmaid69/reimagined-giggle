0d6cc3aa808f71e736c653f9debd4ba2fe072aae
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:26 +0000

    upset watch bank careless either

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
+def calculate_volume(length, width, height):
+        return length * width * height
