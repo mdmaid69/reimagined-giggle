871034d15899e5e1fceeb736fd3684441c5061f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:49:45 +0000

    crisis affect reply film last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
