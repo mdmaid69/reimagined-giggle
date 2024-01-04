71201bffc04916e513ad94697148083d96f596ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:55 +0000

    backwards blow female opportunity turndown

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
