d158b13beef51be140e7a3e6f4e2fa5310f8fc45
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:22:23 +0000

    operation silent frighten century better

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def reverse_list(lst):
+        return lst[::-1]
+def is_even(n):
+        return n % 2 == 0
