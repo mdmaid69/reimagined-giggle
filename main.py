b9754b08685b7273ae1f7e1eab99658c948edc8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:31:02 +0000

    moral trouble like very translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def count_words(sentence):
+        return len(sentence.split())
