4d68612e03d87ef8baa526a3aa882370453a4058
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:50 +0000

    careless drawing principle arrival mood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
+import datetime
+def get_today_date():
+        return datetime.date.today()
