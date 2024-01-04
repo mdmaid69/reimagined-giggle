5ed2fde0943a8fa12576bacbeb5b45bc332754f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:13:33 +0000

    strong concerned fire interview safety

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
