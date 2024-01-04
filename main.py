21ba21c9a108b87f534d6439cb9faaebe4a4d04c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:20 +0000

    phone stamp full escape tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
