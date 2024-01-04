59a300cdef9dd096289d0c0cc4fbe7ef970f558f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:15 +0000

    Pp confident turnup time with

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_elements(lst):
-        return len(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
