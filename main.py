6a5fdecf971c1fbfa41d630fde6ccdf6974f8968
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:46:10 +0000

    lie manner figure shelter getaway

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-for i in range(5):
-        print(i)
+import random
+print(random.randint(0, 100))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
