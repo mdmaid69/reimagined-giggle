b6ab525567bfe265b06879129bf227dfbc35ad08
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:28 +0000

    project spin injury keep all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def check_if_array_contains_item(array, item):
+        return item in array
