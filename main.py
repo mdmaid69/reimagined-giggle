08142c1fd0a42841c6c3b7cb697b2e476f91a60c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:23 +0000

    clothes depend ashamed left mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
