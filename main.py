c8a790cdd9fc442a58111f6fe85c1f84728e496a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:15 +0000

    shame compete ready each Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_current_directory():
+        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
