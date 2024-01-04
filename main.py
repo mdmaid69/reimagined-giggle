c14deaf5a5b08d95572d340493dff98fe0548fd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:17 +0000

    fine fold clue the legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
