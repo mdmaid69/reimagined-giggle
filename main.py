b48b3b50882d833ed669f8d093c3cfd42fb38328
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:57:14 +0000

    wonder behind ride breakin for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sys
+def print_python_version():
+        print(sys.version)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
