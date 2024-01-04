98a22fb26a032c2a3640f34b5ed27bec27bbe9b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:55 +0000

    drop certain pride wind succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_set(array):
+        return set(array)
