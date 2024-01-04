e4afd966b9b99fc77485f1d8853de232aa312f33
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:38:06 +0000

    dance arrival plastic foot hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
