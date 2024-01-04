37c9e71598d8c31f4e3116217f61628bbe4d7385
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:09 +0000

    bite boring lay mean hand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
