8d980c82a3e39b5a7904fe0d2efae1c376b17c3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:58:57 +0000

    bet taste worst nor wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
