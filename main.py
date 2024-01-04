7be350b889066679875b9c4e3fed8f6d24186030
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:39:59 +0000

    doubt sort aim fire plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import tensorflow as tf
-print(tf.__version__)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
