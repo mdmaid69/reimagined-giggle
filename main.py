2e93fd1db28374ffb58a8396eb522c7e486b13d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:59:32 +0000

    worth fetch guess rent window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
