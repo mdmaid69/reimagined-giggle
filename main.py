93e42d96f4e14746f1ee0f52e4df9f94928282c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:41:45 +0000

    congratulations reject 565end responsibility blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
