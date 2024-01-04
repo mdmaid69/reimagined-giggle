893645a270948727a6965892aab36ac69acaf53c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:18:28 +0000

    will describe cold calmly consult

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
