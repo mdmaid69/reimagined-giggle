8633aceb3a47e90da30391336058d2508d1499f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:49:35 +0000

    ready cry embarrassing limit 569enjoy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def sort_list(lst):
+        return sorted(lst)
