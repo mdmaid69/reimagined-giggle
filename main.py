12d76aa1580397420fee3b444f7cccc79fa4a4d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:13:15 +0000

    generally blue emotion over influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
