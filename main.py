ba2a3338ae2c536c972aaafbcff8f7634945e5d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:53:20 +0000

    available dare keen belong worth

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
