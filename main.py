5f1226ebe08c0d9366408daa17b50b3529480a63
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:00:30 +0000

    can gift call immediate blame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
