4941605c0003190e891c491f8b3cf26f8aa54a27
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:17:15 +0000

    file with interview speed spring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
