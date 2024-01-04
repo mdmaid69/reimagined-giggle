7eab80b0c34b59064d517a72d045d8b66162f8a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:32:03 +0000

    myself maintain finish passenger neck

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
