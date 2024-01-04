6b70dd821d96e4d1dd9d8441af02fa0f99a57401
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:24:32 +0000

    contribute pour future flat win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
