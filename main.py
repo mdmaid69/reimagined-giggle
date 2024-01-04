9fa50337229edccdc00854517fbabdea6418a15a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:29 +0000

    home money ilovedicks fast dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
