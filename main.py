17516a2617460a8be4eb4095c8bdde8a463c0a8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:29:27 +0000

    smart old great deserve stop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
