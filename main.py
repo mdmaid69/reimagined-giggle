686377ec6d9e69bf76cd6fee847b66d8ed3b65d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:31:23 +0000

    left need fresh afraid total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
