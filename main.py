2fded74fbee5d828dae3ba95a0572c38f8db4e6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:49:52 +0000

    join expect knowledge would trust

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def remove_from_array(array, item):
+        array.remove(item)
