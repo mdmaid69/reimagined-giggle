bb84c145d1a91a1c645f50ad53b08aee4ab87df0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:23:01 +0000

    way settle set content worst

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
