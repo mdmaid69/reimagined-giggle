38b06a227b9c0ab85c5fe086edfae99531a306e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:23:04 +0000

    cruelty stick gooff smile recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def convert_to_binary(n):
+        return bin(n)
