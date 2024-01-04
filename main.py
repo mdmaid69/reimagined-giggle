26cbc316205be7bb1bd50d4ae2851fcaad60e1cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:23:54 +0000

    climb result hang send gradually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
