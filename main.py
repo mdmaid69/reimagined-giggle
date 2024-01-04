3f478684d44e8068e62d76bf8a21589341923e2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:23:41 +0000

    film test accidental remark discussion

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
