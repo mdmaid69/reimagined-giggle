28c80dee50d2aa9e59dfc481c98b85c88f980d2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:51:25 +0000

    detail sentence middle benefit else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import time
-def get_current_time():
-        return time.time()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
