a8396c696b146f9cc5292594a0796dfb710d5858
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:33:24 +0000

    friend cover cry bear pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
