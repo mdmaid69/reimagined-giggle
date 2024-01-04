18338ef096d93f8152edfe2aac30de40b266ad3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:17:19 +0000

    weigh write check container luck

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
