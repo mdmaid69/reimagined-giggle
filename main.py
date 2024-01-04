4b3f533ba8c361f0c3782abcfe3f90297b2098ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:03:37 +0000

    face though control process blame

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_as_list(array):
+        return list(array)
