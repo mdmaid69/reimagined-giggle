e54f777410bdf1db7b95c2d7535227450c6df5c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:02 +0000

    friendly themselves walk turndown exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
