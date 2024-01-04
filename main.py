feec201c7b2130ef824d5ddab538c6b966aa2b0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:42 +0000

    burst check squash become one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
