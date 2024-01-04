6feeaa59a116eaf15212eac603681b1bf6c7e4e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:36 +0000

    its understand sense shout and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
