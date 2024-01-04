38f34700c2a6f7819d3438587620c7ceb8220e7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:12 +0000

    decide find steep goaway depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
