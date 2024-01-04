a9c691e9835602f74ee6445bfee6c28f175db1be
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:36:15 +0000

    tidy tape warn mixture male

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
