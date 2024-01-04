29d697a5ef5f5abe90ec15c5645e7aef3e38612b
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:14:42 +0000

    win catch agree who be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def sort_list(lst):
+        return sorted(lst)
 import collections
-def create_priority_queue():
+def create_queue():
         return collections.deque()
