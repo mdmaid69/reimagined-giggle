f70e214c5c9c8605f4b61e19960c7666b0428063
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:22:02 +0000

    visit operate drawing damage bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
