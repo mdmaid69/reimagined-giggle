b6117a276db04ea374e7bf7c3b64d0b82d2bd352
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:07:22 +0000

    drink high comeover education blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
