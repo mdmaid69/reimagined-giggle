1e72f10991372c92371abecb0889f6ed602f2b60
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:37:13 +0000

    similar lay this so damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+def get_current_working_directory():
+        return os.getcwd()
