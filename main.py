21661be57b52c2a8b06b4f6124340971ac534bd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:25:18 +0000

    should bear admire space mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_pressure(force, area):
+        return force / area
