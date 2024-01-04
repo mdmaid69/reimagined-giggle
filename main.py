94a94a25fd1a7a9cc10a0790ca236777250b9ad1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:29:01 +0000

    film fetch drink love throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(numbers):
-        return max(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
