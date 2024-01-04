5654072a4fe39d6d2ceb06029b3825ee01a91222
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:02:22 +0000

    mistake for top pen deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
