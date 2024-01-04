bea5a01a90a54d1a506d85fb0e797f480f261e12
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:23 +0000

    guilty keepout borrow search teach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
