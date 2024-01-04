aead5e6ec53c76bfc4413857399bd60c921f1401
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:13:27 +0000

    fix invite raw shrink today

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+name = "Python"
+print("Hello,", name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
