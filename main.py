43d1eea78eb008cb24744769526a2a3cee0e23cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:50:23 +0000

    pump prepare exact kick stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
