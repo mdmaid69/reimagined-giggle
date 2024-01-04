7c881a7391650845033588b58cf1f09779391fbd
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:36:51 +0000

    strength dish object trust best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import tensorflow as tf
-print(tf.__version__)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
