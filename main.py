676b4c438230cdbaa3c5d1ccbbf4d46694369e25
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:51:49 +0000

    trap substance deaf Kk sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
