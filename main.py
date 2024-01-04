93b5d0f8651a039eba3ffff1c3b5725f8a87c9f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:41:44 +0000

    chance disapprove occasional guide takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
