fdbbc36c894a9c3fcbeebd6da50fb0ac7b7c80b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:30:22 +0000

    pull thankyou dish formal ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
