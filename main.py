777f72797efe335fe933343049096064c4c5e048
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:05:20 +0000

    switch opposite independent report pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
