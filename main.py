65ffcb2a17a8f55fa725e70934a70b7aec50c697
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:16 +0000

    various mouth spray section solve

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def square_number(x):
-        return x**2
-def find_unique_words(sentence):
-        return set(sentence.split())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
