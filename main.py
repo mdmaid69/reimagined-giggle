766b0c9ce26762dcffdf191856f19d83e15211a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:34:37 +0000

    frightened ride building each culture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def is_odd(n):
+        return n % 2 != 0
