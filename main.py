a2b34347ce0ef063f986f73a26e17cfa2747d089
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:12:54 +0000

    pretend comedown rise solve expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
