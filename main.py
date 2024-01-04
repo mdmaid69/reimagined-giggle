e63445825d54aad5fcf7b36e62462e1f8a497534
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:40 +0000

    along guess error film worst

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
