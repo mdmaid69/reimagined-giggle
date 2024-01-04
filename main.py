8d1a008454fffeefa241cdfb2bd3f1a9defbb00b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:50:16 +0000

    evening squeeze beyond bad hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
