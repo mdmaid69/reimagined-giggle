db99112c2eeb3e51447fea6482d244504ca906f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:58:15 +0000

    past sign lonely responsibility keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import re
-def find_all_occurrences(pattern, string):
+def find_pattern(pattern, string):
         return re.findall(pattern, string)
