47c9831913e2e48fe458eb2ca1e3a3df8e48aed8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:21 +0000

    occasional practical there journey region

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
