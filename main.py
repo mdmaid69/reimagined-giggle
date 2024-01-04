21ca28974f8e99e0661dc7c9fa39dff9d8bb840e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:14:46 +0000

    closed wrong distance table your

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import sys
+def print_python_version():
+        return sys.version
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
