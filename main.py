f6de722815bc68b3223b2338a9fc6c3618a34d94
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:01:00 +0000

    cause declare justice star tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
