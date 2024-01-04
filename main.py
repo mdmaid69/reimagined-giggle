936fd115a0d8b5e840750f35774bb28d36802daa
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:58 +0000

    deserve introduction equipment plenty callback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
