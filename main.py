77517658f962d41d793a772a75c197810a270719
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:20:52 +0000

    pretty mother feel community turnover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
+import time
+print(time.time())
