c6d0d013e3ce7747de10291f23f31ec75667bb52
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:41 +0000

    what if strange emphasize tired

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
