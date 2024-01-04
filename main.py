ea2e24f6bf7261b4c14b755fdc8a607507d223a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:25 +0000

    them receive unkind forgive normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
