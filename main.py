a31a74e4f2c46d1d8139ae11b7683773c236f006
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:42 +0000

    bridge country butter pressure relationship

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
