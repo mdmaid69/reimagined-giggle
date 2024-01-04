62871fce010a2bef821dccb1b403e8349012c97c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:54:56 +0000

    little book education beyond form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
