afef5c20557a96f87704128f4bcc53259383fa78
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:01 +0000

    wall port central change pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
