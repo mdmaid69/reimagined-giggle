facdeea7ceeccdbc963fd03c195ea069f3196e31
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:14:58 +0000

    opponent bored 570enjoyable class exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
