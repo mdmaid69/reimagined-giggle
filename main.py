480d1a95133bfc58d67483765b9e6f8bd92d4093
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:13:15 +0000

    breakout adjective Zz slip ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import os
+def remove_directory(path):
+        os.rmdir(path)
