9d128b28cbfaf8680dd46acee83206b38cfbf6c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:17:06 +0000

    hold hour bury exam lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sys
+  def get_python_version():
+        return sys.version
