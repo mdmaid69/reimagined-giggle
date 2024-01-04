e3fe1e380a45cb33c2aa9448bb4384e9b32f2ad0
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:13 +0000

    promise before office chance careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
