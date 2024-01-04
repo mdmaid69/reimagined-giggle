9ab09dc1b24bb6e4d6d9fa272bff80a90fc153c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:05:56 +0000

    distance memory certainly agree contact

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
