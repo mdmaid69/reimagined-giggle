31709d3ceca6033a9cbef42e9679d2c6cd772506
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:56:39 +0000

    understand change pity understand pretty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def greet(name):
+        print(f"Hello, {name}!")
