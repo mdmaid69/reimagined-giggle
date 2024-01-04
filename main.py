6dabc3b82e767528a52a6de1b042b34c51468a98
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:14 +0000

    Kk honest already comein crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
