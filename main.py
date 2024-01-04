e7f45d556bd5e13d62d8f17bf408082e77aa8fd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:45:47 +0000

    influence appear go argue teach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
