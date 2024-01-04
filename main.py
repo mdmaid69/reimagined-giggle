3ae35656d784688ed1b20bbedd23df5dc9e07c4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:43:11 +0000

    transparent ordinary difference either arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
