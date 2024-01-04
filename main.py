0231c43b513a522cce7caa434614215cd53185e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:03:18 +0000

    lay pleased news relax occur

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
