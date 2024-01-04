028c5665b93fb4a29b338c7595e51c5a46646502
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:05:00 +0000

    interesting help before rub boil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
