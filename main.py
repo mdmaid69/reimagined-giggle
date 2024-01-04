4c56d1d506a391ef48f85905cc33c8b305fa823e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:46:36 +0000

    fit hard dream nigga cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_array_to_list(array):
+        return array.tolist()
