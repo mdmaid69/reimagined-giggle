9f83b95e3b3cca7c60dd9a069d6c7fb6e36280e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:20 +0000

    downstairs light demand sink so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
