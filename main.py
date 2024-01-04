55d5b8610a2044b6e943d5f31537c6026c3ba9ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:25 +0000

    young cough song trade emphasize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_area_circle(r):
+        return 3.14 * r**2
