2c879ba2a31a37ecc21d6873b0cfa095f6a04a29
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:42:37 +0000

    interruption impression laugh belief shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
