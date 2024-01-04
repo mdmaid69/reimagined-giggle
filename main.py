60a9a70eda5693d7e2588b2c226fc12674aa4a8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:54:24 +0000

    its after area talkback upset

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
