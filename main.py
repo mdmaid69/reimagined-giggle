67e48c4e98cce77594a908268ef94c078d4270f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:59:50 +0000

    maintain scrap phone turnoff street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_repr(array):
+        return repr(array)
