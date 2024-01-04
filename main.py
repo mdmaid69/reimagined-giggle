358e49ac1e29865fae9a600f55f0620d560f66ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:18:04 +0000

    name just pour hope plan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def is_odd(n):
+        return n % 2 != 0
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
