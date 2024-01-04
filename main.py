5a2386e79c0db276e30f7487948bcd756cd504e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:24 +0000

    man open finish tip lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
