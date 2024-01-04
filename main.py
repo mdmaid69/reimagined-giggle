e5c0dafecc1d84a13020f99c141fa216b17204df
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:21:58 +0000

    false close direct head than

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
