e30d199918ab21b6b434be7e509ad99251c5595f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:46:14 +0000

    please clear port onto sport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import re
+print(re.match("h.*o", "hello world"))
