e3a4f3ac015c604a1512d43035f95c046640fc6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:57:38 +0000

    able instant Kk throughout somehow

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def convert_to_binary(n):
+        return bin(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
