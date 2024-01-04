6202aaec3b7c1b39ab055a25c7f971cef4c1a4fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:24 +0000

    man answer willing alive stop

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print(sum(range(10)))
-def square_number(x):
-        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
