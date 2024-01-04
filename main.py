9e4a5d40c2f45415cbe16da812f38eb293218feb
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:03:42 +0000

    transport may principle partner spill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_floor(x):
+        return math.floor(x)
