9a40530935796e3bfa4bc8bd5c26e3bc29ffd257
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:39:48 +0000

    pile hope tonight less breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
