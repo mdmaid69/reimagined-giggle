b0e3f6886b9ea0d69b6ab002fdb9d298b5c86140
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:40:10 +0000

    whisper position uniform themselves recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
