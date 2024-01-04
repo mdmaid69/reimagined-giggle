3e125df19fc6268220f222bde88d4212960333ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:00 +0000

    chance slice convinced capital drink

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
