98288376d98c1b32f5d4c145fed52a8eb9a8d16f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:26:46 +0000

    sport pride shop leave comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
