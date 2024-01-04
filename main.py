948cc5d2e00df97ad5b7cf19edcb2a2c54f16dd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:45 +0000

    operation part because badly church

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
