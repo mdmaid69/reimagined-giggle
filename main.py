a3f3467d52142c3dce4a8769df895b2f4bfb3344
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:53 +0000

    quiet snow solve deep shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
