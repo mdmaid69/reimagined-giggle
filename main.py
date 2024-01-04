3b29682649de5b424e2d09b7586b4ea500220097
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:34:05 +0000

    work fault dish bringup purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-x = 10
-y = 20
-print("Sum:", x + y)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
