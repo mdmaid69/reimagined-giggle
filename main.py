0b54c4a6b49f7585e6e8f9a792a4c32998fe8542
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:56:59 +0000

    independent during building keepup tragedy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_max(lst):
+        return max(lst)
