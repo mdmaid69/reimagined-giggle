831fe1985c3ba84e617a957e5e926d868ec4863d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:15 +0000

    man possession environment Yy female

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def multiply_numbers(x, y):
+        return x * y
