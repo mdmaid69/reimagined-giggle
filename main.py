812ad9cce577bb79ee8334dfdfb027c276196e86
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:44:00 +0000

    himself holiday car around promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
