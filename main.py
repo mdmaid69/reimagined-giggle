47f68df2f222e5953168873003ae164b077c468d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:41 +0000

    criticize limit unit makeinto reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
+import os
+print(os.getcwd())
