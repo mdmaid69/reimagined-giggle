ce4d254afd3775c9389aa97d0266347acae62629
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:48:13 +0000

    true him rid shout invitation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
