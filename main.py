3644f5ad0fe729633b4b6fd56c9c48e15f6b63da
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:13:25 +0000

    grow degree whoever treatment radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
