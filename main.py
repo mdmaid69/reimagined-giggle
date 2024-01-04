a3473b21f7901de3630c845e7a14e52f2db7496d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:37:25 +0000

    backwards right whatever notice steer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
+  def is_odd(n):
+        return n % 2 != 0
