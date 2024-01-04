36ed0805ebe06f4ca1d39d9c81db27fdc7bd8bde
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:20 +0000

    regional temper type already disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
