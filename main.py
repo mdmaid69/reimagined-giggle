937b3cd82f9a483ee941b29c9130eb9b7deca525
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:21 +0000

    punishment should effect church meal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
