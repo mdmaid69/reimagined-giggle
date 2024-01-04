0f2b57d073cee5cb82c75e38d3bb3caa78efac91
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:10 +0000

    invitation pronunciation brush be point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
