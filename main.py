06c09a5f12bbd3127f186132ca6bdde34ee52cd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:08 +0000

    prefer comeup report shame bad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_current_directory():
+        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
