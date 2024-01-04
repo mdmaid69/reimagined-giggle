c0a0547ac913676fed58e1405af74e2df7d8b6c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:06:56 +0000

    flood share sink bend picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
