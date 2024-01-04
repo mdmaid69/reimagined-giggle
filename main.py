3b3c3dadee05d22e07e223cac9f8f01a282a659b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:34 +0000

    miss raw murder takeout occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
