7e18bcba5db62c5558ffb747eb42c0a684a51857
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:51:25 +0000

    Vv wealth cope pause true

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_typecode(array):
+        return array.typecode
