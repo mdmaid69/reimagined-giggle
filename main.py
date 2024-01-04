53245ecf1a3e531a617693a4ee625b75b9ceca7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:36:00 +0000

    tear ignore hello herself hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
