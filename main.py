0438853790ac4a02c204ed542c7e23fb9b540dfe
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:00:32 +0000

    job store shock steep load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
