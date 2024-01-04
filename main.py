29507f1a91f4e72c28ddefa2c1de000333cf89c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:23 +0000

    several defeat only unable occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
