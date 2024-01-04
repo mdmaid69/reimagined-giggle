ef38546f7c4fbbbf2f0d5567ac8979a347055971
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:07:04 +0000

    skin slight change rate lookafter

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
