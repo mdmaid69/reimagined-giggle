dfa8cca8e33630d32aa19c087f241334ea51f055
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:37:37 +0000

    store minority pinch comeoff soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
