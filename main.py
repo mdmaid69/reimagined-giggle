1c3dae7ec2bf1be9de557ce40edac5034cc0291e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:11:38 +0000

    injury natural why scene positive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Characters:", len(text))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
