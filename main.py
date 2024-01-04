30739fe29a2f128c2a4a586a15d273e0d46560d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:37:50 +0000

    flat peace lead tonight state

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
