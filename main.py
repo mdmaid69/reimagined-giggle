fa2ee65f19167df53089d9ba92d1e735e4d911c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:26 +0000

    interesting instead most afford takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sys
-def print_python_version():
-        print(sys.version)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
