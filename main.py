199aec1eee3b0972971d2d93463b25ca93af0be4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:42 +0000

    study rescue connect Uu march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
