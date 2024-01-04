8228edb8bad5e35bf93b1438c11e885acc9197cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:48 +0000

    arrangement taste evening pure anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
