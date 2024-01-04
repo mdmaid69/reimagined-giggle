baff0d80deb549ec6b562e87300050fc06575834
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:25:09 +0000

    twist substance unite action content

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
