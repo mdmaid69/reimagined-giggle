1d2fcd0d4835c00bfdd281ca89ef6f99e122f95f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:08 +0000

    test whichever old unpleasant acceptable

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def clear_array(array):
-        array *= 0
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
