1707334cbf820621643f74531daf2166e7f02056
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:44:17 +0000

    harm figure confused guard sad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_length(array):
+        return len(array)
