00e159eed376087158c4e9eeeb76c1d886776991
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:16 +0000

    fire week lift hot charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
