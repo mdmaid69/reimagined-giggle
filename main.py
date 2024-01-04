70dc9b8bedab738d4fc15f4fa29adce658abe93f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:14:40 +0000

    seem yours neck roll she

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def find_min(lst):
+        return min(lst)
