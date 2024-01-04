ec07cebb92128414872679077162a29b7243b3f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:47 +0000

    waste block front concern prize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
