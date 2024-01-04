fc56160d04f8d1323c5e8b68a495f0aa1f5d6447
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:41:53 +0000

    month strike bad search afford

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-  def convert_to_binary(n):
-        return bin(n)
+def is_odd(n):
+        return n % 2 != 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
