69f008e5a5aed9f86a7e005d402d861b876a3f68
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:46 +0000

    fashion mean with impressed ill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
