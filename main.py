5d29c9692e79bfbff47df5ccc999dd0db963118f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:05:32 +0000

    Jj mark dry popular whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
