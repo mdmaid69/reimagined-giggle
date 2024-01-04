d4e25f9985b5dedbd2e1a5cd5e6e8d2fca3e8f00
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:56:47 +0000

    bet cum need mark slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+print(sys.version)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
