ba0e949f0a1a0ae762f49ba2c1e94a0b14f9f7c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:03:56 +0000

    Ss last sign excuse relationship

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
