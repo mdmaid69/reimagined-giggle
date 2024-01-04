abfbc11f4466470a02c5e17ba8a180c9ed70c208
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:58:54 +0000

    ticket relax tie description typical

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+name = "Python"
+print("Hello,", name)
