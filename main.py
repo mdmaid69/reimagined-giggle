26af85905d20be5c0a1263b06f11777194d9ceb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:03:32 +0000

    dress rate unkind business getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
