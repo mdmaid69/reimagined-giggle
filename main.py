5c6bdc1d1b15964286da7f74cc964ce47f558136
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:08:02 +0000

    depend lesson OKorokay active choose

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
