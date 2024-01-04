42b0e35a0451733aa2f6902ebc694308272f866c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:08:17 +0000

    basically duty excuse act born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_list_from_array(array):
+        return array.tolist()
