e007a472e29a6c225d7beea893db88cb950e3a5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:47:29 +0000

    regret situation will spin care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
