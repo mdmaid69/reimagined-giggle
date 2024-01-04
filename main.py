cd2c94d69785d4d8fcabdc8867250f3b6fdfac0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:40:53 +0000

    create hope limit dish next

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
