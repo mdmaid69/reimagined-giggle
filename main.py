03fd4547736875a043a511dbc607ce10477bb18f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:34 +0000

    bit hurt none sometimes silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
