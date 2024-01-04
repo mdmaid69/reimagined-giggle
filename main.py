d5ac6ae05ffe2ef0e1a3d67c326d34545e93df33
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:50:48 +0000

    tidy tear influence mix alive

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import time
-def get_time_since_epoch():
-        return time.time()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
