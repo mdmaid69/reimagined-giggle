bf73ffec54ecde90334275095e22ddc8e1009dca
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:07:57 +0000

    extra well dress apparently Mm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import time
+print(time.time())
