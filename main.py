0b8b32884fe85a034ee7fc8bb298fc8cd5eaeb06
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:46:11 +0000

    attention post schedule eat beside

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
