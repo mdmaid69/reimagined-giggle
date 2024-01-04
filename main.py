5f8ca83cce3c703f9d0581c5764624765d9c5a8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:00:59 +0000

    mark thank breakfast wonder clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
