fb0a030b34a85af4e4d4b2500e04229245754f84
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:05:33 +0000

    pour ability getaway label length

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
