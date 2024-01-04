043c5417089e71e9fca2e82fe63fe151e76d843d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:37 +0000

    apparently around shout one consist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
