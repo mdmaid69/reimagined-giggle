b3935a8a597a04430a49c8bf1e648698ec07dca1
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:45:37 +0000

    strength wall bringdown push strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
