46efbdc00918726b86d4ba4f41640a1bc0de6165
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:10:06 +0000

    active education naturally wrong hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
