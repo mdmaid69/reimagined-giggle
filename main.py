a07eddcb693fac90265c158d729289624c0b6246
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:54 +0000

    class traffic tax first cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
