faf0bf74397c77eb4998993c6d5a06605fd52f99
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:10:59 +0000

    sick block produce neck study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
