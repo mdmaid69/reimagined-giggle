da7a5ae06e4dc30334f45720e68a97530fd85ae3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:17 +0000

    minute detail however see pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
