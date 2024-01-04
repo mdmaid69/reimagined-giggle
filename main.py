792aee4c134f5308427b978169409413be3b92c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:09:42 +0000

    recover roll search dream spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
