67902d15abcbf31d79ccebc716540ad750ac88b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:33 +0000

    laugh calloff except improve stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
