4cf1350c0d0e047a383057c59aa40f8192bdb09e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:55 +0000

    fear back everywhere shut side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
