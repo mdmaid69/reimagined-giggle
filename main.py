45f6aa9021fea5a03b8663a16ea4c04a93f37680
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:32:47 +0000

    birthday miss odd perform freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
