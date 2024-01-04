ec4acb644e209cbc7ed7396bddb123301c6a02b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:22:25 +0000

    reduction practise congratulate accidentally consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
