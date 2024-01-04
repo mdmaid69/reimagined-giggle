d64cc6f10316ee0fc3273fb8e7ed98ebcbc07b2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:20:21 +0000

    window community purpose minimum benefit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
