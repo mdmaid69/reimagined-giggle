399189f35e049e2ca8692c6f1ae701a199c8d6cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:18:29 +0000

    enthusiastic success join mark produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def reverse_array(array):
+        array.reverse()
