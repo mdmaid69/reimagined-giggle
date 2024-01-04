49ddba74f42d9966efaa00b0faf4be1637557d44
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:02:01 +0000

    warm handle seriously back degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def create_priority_queue():
+        return collections.deque()
