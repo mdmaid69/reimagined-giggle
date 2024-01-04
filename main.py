cb222e08dd1385bdc6b7816c73f922ae7f14a0da
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:59:29 +0000

    change across out love information

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
