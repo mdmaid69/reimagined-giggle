717f146fa0c0f0fb1654323ab3a8f66a2c8b5346
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:27:21 +0000

    doctor desperate harm load imagination

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-  def sort_list(lst):
-        return sorted(lst)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
