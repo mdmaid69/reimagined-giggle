022fafb3781e77f80236e3f902ccdfd61d279624
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:37:14 +0000

    eye wander visit arrange error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def add_numbers(a, b):
+        return a + b
