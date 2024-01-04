beeb6fbf30a44c82127ab670290e9fa41482d6ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:13:11 +0000

    target loud leave exercise argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
