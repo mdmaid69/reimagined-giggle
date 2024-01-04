7b6c43d9c2ae354aea2c5255a46717d9ab5d4c07
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:57:46 +0000

    price run target who stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
