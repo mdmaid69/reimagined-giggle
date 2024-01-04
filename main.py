00581b66107b7ff326343aed2212775c1f899070
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:30 +0000

    bath full progress everything drawing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import tensorflow as tf
-print(tf.__version__)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
