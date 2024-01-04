11a950d02dacd24163193da8a183cdb2a4ccd7b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:21 +0000

    become from press do odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
