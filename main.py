5f6fca534c5013b765a6a7e075bc746467524796
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:35:23 +0000

    today class laugh generous suck

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def remove_duplicates(lst):
+        return list(set(lst))
