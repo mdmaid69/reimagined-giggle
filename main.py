817e3459da8c06def539831be5242a026713c162
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:46:54 +0000

    effort thing tidy ability smell

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
