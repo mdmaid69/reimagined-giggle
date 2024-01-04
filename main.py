bf922922049adfdc5d09dabfc1e8e86b75de9e32
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:52:12 +0000

    very rescue above name wear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def cube_number(x):
+        return x**3
+def cube_number(x):
+        return x**3
