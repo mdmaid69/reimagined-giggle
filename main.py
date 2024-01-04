caf7f3cb1dd95b70ff130edf3098421d763d6206
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:25:25 +0000

    shrink sit within rain hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
