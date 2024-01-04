5a99445d9ef07d1907d423a605c2426d86d30b6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:15:17 +0000

    attempt cough keepfrom crowd nowhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_volume(length, width, height):
+        return length * width * height
