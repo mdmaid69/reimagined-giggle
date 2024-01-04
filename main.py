90d43d79e2ee8a94eb9d70802a263bf713df5995
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:57:38 +0000

    callback list science exhibition minimum

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
