5b0cb2e84226dcedc33d988134de404ab8d615e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:17:27 +0000

    slightly blame bury challenge boring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def count_elements(lst):
-        return len(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
