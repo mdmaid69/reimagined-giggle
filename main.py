33b071a20b5bd5ae714d90c92bec7cbfc0a065f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:39 +0000

    ability be perform ours punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-print(random.randint(0, 100))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
