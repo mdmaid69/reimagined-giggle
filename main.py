52e4fc80e88cb100c40accf76e563ac0358c8a0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:38 +0000

    joke criticism expensive tired upstairs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
