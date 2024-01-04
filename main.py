7384aa89ab424021c238ce63a3b0cff451f8be45
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:52:55 +0000

    Tt female meeting blame smell

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_set(array):
-        return set(array)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
