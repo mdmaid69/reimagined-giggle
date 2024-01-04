c30ed43339062a54be44e8320a05363b5b26e61a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:44 +0000

    play up aim cover marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
