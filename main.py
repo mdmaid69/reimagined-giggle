9257d3cfc4a2e5e1d634d411b94bde5d768707c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:30 +0000

    target ticket as their every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
