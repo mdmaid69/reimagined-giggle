2ba2ca3f7c0cd63cf153fccbe3863552408cbcb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:45 +0000

    film fashion shall love half

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
