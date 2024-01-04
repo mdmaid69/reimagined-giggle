9cb926d8f7ae60a9da326a3825c8be98673ecd3d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:22:45 +0000

    correct single by reliable miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
