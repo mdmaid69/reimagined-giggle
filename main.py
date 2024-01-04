34845edd864ae794bc3b207179991d0d0bf1de6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:36 +0000

    so it sweep different otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
