def7cf1327f8a62294c09fd99a238a587546c38c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:26 +0000

    Zz least breakdown attitude area

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
