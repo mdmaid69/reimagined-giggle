f81d088b2cdd82618034ff3af576bb3a6701b4ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:53:23 +0000

    hers Dd whole far battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
