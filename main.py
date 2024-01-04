e38b10d2907df5484d147e31790f0458aa3b7386
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:02:43 +0000

    score do high disapprove idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def clear_array(array):
+        array *= 0
