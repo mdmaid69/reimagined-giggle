bde927294c2eaa285b8529f360e8f83bad218a4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:59:37 +0000

    name waste tap could operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
