34d89f84ec160d8260f46dc9c162faee8c0ca6df
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:09:01 +0000

    slightly bank face range upwards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
