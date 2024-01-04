202e3107bbd1b5c5ba5091678937579ab45b7b27
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:12:49 +0000

    wide worth serious watch subject

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_power(work, time):
-        return work / time
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
