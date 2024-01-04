7e23a37de0250b465ce1dea4f3cd34993e3b1200
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:57:54 +0000

    should tidy finally they comeout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_item(array, i):
+        return array[i]
