bb0fe1490025cb50e52e252af5fd547b47729381
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:40 +0000

    blame immediately sleep correct legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
