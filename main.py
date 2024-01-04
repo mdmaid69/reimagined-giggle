bb4babe96fcd1f88b50d20f0c3117f810f8f5813
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:59 +0000

    family buy behind frequent factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
