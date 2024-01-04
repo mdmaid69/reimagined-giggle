c10f9bd99f6c54c83b8d06274fb3312dbdb0a6c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:46:42 +0000

    sentence generous basis point table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
