12e36774c696d8cfb27b101ed13ff6f82f7fa8df
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:55:03 +0000

    decision pin convenient rarely seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
