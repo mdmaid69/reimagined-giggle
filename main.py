5e3334dfa3cc538102c99d1be66b51378c0be72d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:03:10 +0000

    turn butter practise hurry design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def sort_list(lst):
-        return sorted(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
