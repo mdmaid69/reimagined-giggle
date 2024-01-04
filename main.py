9798c545ec653d443af79e06b65f976c18341cbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:49:16 +0000

    talkoutof get spring judge wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
