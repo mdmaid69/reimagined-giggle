d404d8175109fcc0853acd203a1e09a4cf043fa8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:01 +0000

    create cost vanish wipe complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
