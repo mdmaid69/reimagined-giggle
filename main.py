faa50729d9d0c06b9d7fbccc25af5f9832e4e24b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:42 +0000

    frighten bad instead avoid description

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
