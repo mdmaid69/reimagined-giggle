f2d1f972584f781a24ca30e531bf67d5ce9e4cd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:24:56 +0000

    rarely bend under pretty train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_float(array):
+        return float(array[0])
