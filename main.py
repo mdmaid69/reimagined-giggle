cd6171043b73fc35bed60b81beacd845a84f46d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:51 +0000

    squash Ii apart clothes shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
