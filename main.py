30011972d7c5f17e1b0d271a177dfc245a38f6c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:35 +0000

    class estimate loose check imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_directory_name(path):
+        return os.path.dirname(path)
