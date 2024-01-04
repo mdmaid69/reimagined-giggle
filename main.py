3af1701764eca02e0826a55fb7d1427c487faa1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:39:26 +0000

    mind label loud scale price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
