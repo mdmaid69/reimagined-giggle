8584e9145ed9a819750af453c1aeb7412fb7f32f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:01:40 +0000

    figure suggestion interruption artificial thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
