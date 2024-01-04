ef4fe7348bee514008e3e0e0402454de9ddd9d77
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:52:06 +0000

    shout person kind direct leg

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
