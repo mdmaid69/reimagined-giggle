3be94ada5a381a1ef689a260a73ff42f12cc640b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:19:06 +0000

    her price passenger several trip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
