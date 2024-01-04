a29bb3cef05c0f89cc8149beb80f7b3f6a2fcb33
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:08:10 +0000

    pay time put cut lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
