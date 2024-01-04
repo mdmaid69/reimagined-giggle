543202fdbba86f3edfe22603a18d76aec1ebcb1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:36 +0000

    way empty burn boil coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
