35f20c378c4d263c12cff0a167f3e404d47442d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:23:17 +0000

    cost lock kick opposite report

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_force(mass, acceleration):
+        return mass * acceleration
