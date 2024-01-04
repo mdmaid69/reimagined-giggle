0cb17443939d0125bc298cfcd05b06e7e7e127b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:35 +0000

    start lower collect far careful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import getpass
+def get_username():
+        return getpass.getuser()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
