e86be5ba8fc55aef5c1f10a4573bb8ce609bd65a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:34 +0000

    damage contact urgent promise all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
