6330e016c9a854849d3b580b941879094289bd28
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:25 +0000

    noone information talkinto talk Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
