c385fd28432507cf737b431da882499a1252401a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:47:12 +0000

    about free home decide lock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
