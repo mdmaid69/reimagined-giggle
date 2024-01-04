a274c635658f1afdadfb0f326e8bce6e668bc718
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:38:19 +0000

    blind opposite comedown guard food

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
