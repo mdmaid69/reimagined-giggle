7377f06915c9ba2aa24fa031ed14378b7984d942
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:39:58 +0000

    recommend movement just getup appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
