06ea9e0daa509ec796b277f6df2c8148da815416
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:36 +0000

    backwards treat evening early charge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def clear_array(array):
-        array *= 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
