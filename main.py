71ca04b8addd56bf94dea1bc9c68218b927fa812
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:08:55 +0000

    danger profit record picture left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
