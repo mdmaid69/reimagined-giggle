941acca81bff1366b894c4c44bfacac6f8610fbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:59:46 +0000

    through alone form brave ill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-print("Hello, world!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
