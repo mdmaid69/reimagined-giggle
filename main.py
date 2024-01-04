7b2d808285e593a26db65b9024479de31a97a43b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:37:21 +0000

    whether string theirs official scare

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_acceleration(speed, time):
+        return speed / time
