49ef15b474b38e32f61d8df6f2d95215fcf08a3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:10:43 +0000

    protection my certainly mind flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
