60560b7198777c4e7594f971bb73de750b7465c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:38:04 +0000

    wrap turn post keepout performance

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
