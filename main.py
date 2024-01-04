96942bcb3a13f323a881c936045e6102cf8fc1be
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:37 +0000

    callup exist sentence but somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+  def delete_file(file_name):
+        os.remove(file_name)
+  def is_odd(n):
+        return n % 2 != 0
