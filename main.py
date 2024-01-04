315b13cfd135b29591e27f21aa435c9d548c9ab0
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:05:26 +0000

    push capital meaning family getoff

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def convert_to_binary(n):
+        return bin(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
