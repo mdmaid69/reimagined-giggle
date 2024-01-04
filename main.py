2d8f54cf7723974acc76d2da0522ce8a79ea40cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:47 +0000

    record off bridge future slight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def add_numbers(a, b):
+        return a + b
