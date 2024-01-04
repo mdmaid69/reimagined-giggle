d877cb60f976912a1bb7d28bdd5f387809eab102
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:42 +0000

    most mail visit number bus

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
