e97acbe7680683106df95f2232fdaf724dcea650
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:18 +0000

    emergency shoot similar mend tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
