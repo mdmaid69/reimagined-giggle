c7d8f2400407e256d1c6b206b558687d653c7638
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:25:30 +0000

    soft getout winter instruction cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
