7290035a6b6639461b77837dc928ffc767907e1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:55:41 +0000

    match touch pair turnup trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
