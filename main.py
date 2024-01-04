b65dd3dcd4b31fd07df7167507bc062b59832253
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:45:22 +0000

    neat genuine tomorrow each length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def reverse_array(array):
+        array.reverse()
