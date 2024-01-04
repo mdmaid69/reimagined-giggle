cad53c6ec778101024d0a66f1ac53dd04696864c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:29:36 +0000

    could mistake buy tune full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
