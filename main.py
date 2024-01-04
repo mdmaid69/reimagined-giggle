1a3b118341d10c01f54b8a53c60186cb78e78103
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:35:36 +0000

    me middle during lookup able

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
