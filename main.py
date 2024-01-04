e4b19bb67ead272771f04c18f64e8c7d19ce3aa3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:54:24 +0000

    bury stage reach hard march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_characters(sentence):
-        return len(sentence)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
