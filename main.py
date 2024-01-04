0b7661234638154db9bbc5681529991fabde0d92
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:24 +0000

    long old use preparation blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def exit_program():
-        sys.exit()
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import os
+def remove_directory(path):
+        os.rmdir(path)
