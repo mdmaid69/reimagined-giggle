a7addf14d0587000e6ad4ff35c3e402ab93e623f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:33:58 +0000

    life building model us carry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
