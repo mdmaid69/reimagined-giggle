7d00d6e1c18baa14e93db9b818bd57b36a75815e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:28 +0000

    emotion pair style weigh any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
