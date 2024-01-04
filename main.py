340917eda0d0fb3639fb8b145b92edeb4dcd7a0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:55:06 +0000

    activity hello Kk tight want

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
