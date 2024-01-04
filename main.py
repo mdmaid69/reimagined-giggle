bb68650c5982a8ecbb0f6fb8d568e4348c3f9b87
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:34:00 +0000

    impressed phone thinkthrough edge emphasize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
