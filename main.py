ce068e74df18f31365888dfd628d43d7f67e8517
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:26 +0000

    question claim remove question violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+print(json.dumps({"name": "John", "age": 30}))
