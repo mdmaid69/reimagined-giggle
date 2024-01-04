d58424ecbc0ee5fd30e966f7ed9d2f04f43fb62b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:33:13 +0000

    dress tune treat mark push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
