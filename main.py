d47b6c624a69ece9bb425ee35b4959210c37e41a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:01:41 +0000

    pen train best risk note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
