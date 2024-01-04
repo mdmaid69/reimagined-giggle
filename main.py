6bb2cb9760bdbd14413e528068f0e9d0c45c597e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:06:01 +0000

    the risk range dream Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def reverse_list(lst):
+        return lst[::-1]
