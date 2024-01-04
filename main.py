40f12deed8fe5c0d84ef7c6701878f39f08d38e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:52:46 +0000

    kick straight set creature fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
