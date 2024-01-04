8b4e8a95918d5dc7b99c7e8b11c351fd4c77b834
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:53 +0000

    physical silent Dd health keepout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
