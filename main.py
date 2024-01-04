442db11e5d44cf37a52b4058672860062b438ead
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:40:39 +0000

    waste stamp right sorry trade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
