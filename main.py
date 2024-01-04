24d05794339bfa4203d879bb56ec0a7651884b8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:24:17 +0000

    discussion deliver yesterday hot miss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
