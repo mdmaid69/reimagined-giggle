fb2727765b0dbb843b7ebe4a21ee00e9c17dac3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:51 +0000

    party awake single equal smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
