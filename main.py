2b1c1459b53c89ebc21cf03fe88ce89e9c093c35
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:39:57 +0000

    hot man male use enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
