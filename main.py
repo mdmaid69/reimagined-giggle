033528cd6c76bb6b2ec76f9c600aee18b5623e7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:25 +0000

    quick takeback exact callup carry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
