848b9329b57ca49d61034b9340db7b0c02f3fcba
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:44 +0000

    note develop benefit who breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
