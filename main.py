47add673d35b28a6df42713b69fec96130e0d40e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:08:16 +0000

    certainly whistle getup behaviour 568energy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
