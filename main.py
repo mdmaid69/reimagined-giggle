ef33864492bd50c01d35e5fda01a97918e4cb794
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:01:31 +0000

    look blow top 568energy dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
