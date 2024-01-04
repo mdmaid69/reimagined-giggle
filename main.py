33ead56144029f09c5ae56de06d7746e1851adc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:16:53 +0000

    frighten smoke mixture plan even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
