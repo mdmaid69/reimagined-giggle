81d834db5a1b7cdc9383df955f32f05f74493c13
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:51 +0000

    nurse reason convenient wire spite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_power(work, time):
-        return work / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
