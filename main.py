4a73e9e6f60b6c0ce06e6f75157d6f307da21ddf
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:53:31 +0000

    crowd promise kick machine office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
