e91f873bf780c263a88b44fafda46ae3edbdbb40
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:35 +0000

    opportunity passenger impressed face noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
