14c210ec54cabac5229e9d323c330c74bc42c678
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:27:42 +0000

    shine land story fast open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_time_since_epoch():
-        return time.time()
+def sort_numbers(numbers):
+        return sorted(numbers)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
