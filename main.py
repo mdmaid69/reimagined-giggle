ab6e9ac0eefa7f00a4202a98365fd09f9cc52e1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:08:15 +0000

    sort priority rest their morning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
