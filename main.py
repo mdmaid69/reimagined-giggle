89f90c25544b500756830a7e171ca4621bb74558
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:39:43 +0000

    long centre brave ticket brief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
