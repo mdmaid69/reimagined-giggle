f6f64d159b0ce2515697a134af7459f0ec56b7c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:57:21 +0000

    estimate cool accidentally principle change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import collections
+def create_queue():
+        return collections.deque()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
