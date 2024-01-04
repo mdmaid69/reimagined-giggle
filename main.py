65ac5bf67fad81235efffd896a3be5bd5053f55c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:10:09 +0000

    advise straight national lookafter someone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
