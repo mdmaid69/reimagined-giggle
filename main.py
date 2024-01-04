f2e8fe1e4561e3e5116e695da61a4994971a6833
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:58 +0000

    principle part turnback home practice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import sys
+def exit_program():
+        sys.exit()
