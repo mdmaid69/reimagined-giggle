c611824fafbabfc0319edc9ccc8a17949717f0a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:48:02 +0000

    post official place suckme fit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def get_current_working_directory():
+        return os.getcwd()
