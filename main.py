f992f20397a0c21ddceff80e29d3f53cb467ef41
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:11:47 +0000

    rob noone progress switch turnaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print(sum(range(10)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
