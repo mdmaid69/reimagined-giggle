db7a8edaf5532492b50b1a2e23827a67ced168f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:58:16 +0000

    improvement consist profit begin lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
