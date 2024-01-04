2e319a58c43494fcccdb89feb6d977ac568e73b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:52:04 +0000

    spot fold tip gate front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def multiply_numbers(x, y):
-        return x * y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
