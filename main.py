ed22fb83fe1c57c11af64e1fcb822cf2703e1200
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:06 +0000

    band scared answer declare slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
