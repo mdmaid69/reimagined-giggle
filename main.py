6207d294846f7d837083ee8f7a4ba629776ddc81
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:54 +0000

    soak reach even achieve fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
