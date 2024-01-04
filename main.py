5f9d600a7de8fe8161891cd5315f9d2128e2ff0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:36:19 +0000

    pause flow evidence perform swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
