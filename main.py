339ffa5d0624e623fc3ec7efb3aab64b138f6f85
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:08:36 +0000

    true stupid twist shrink always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
   def convert_to_octal(n):
         return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
