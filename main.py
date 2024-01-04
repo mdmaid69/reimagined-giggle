4a37fe89d650a54a2fe0235ede621f2fbc664ad0
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:32:55 +0000

    contents brush easy lock street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_float(array):
+        return float(array[0])
