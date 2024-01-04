a1bfc1e8f9a4a676637837851ae2150683711659
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:15 +0000

    cruel mine ilovedicks stand hair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def square_number(x):
-        return x**2
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def is_even(n):
+        return n % 2 == 0
