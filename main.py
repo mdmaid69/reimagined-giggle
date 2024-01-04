d4a3215253fa3b85158082ae5dc8fa361acdee24
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:18 +0000

    official content dance contrast hole

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-import math
-print(math.pi)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def square_number(x):
+        return x**2
