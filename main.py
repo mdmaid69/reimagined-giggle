92cdf87abda0cee2a1fc9ebd6f1bc0b9f36d5e4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:46:00 +0000

    Ww about talkinto turnoff answer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
