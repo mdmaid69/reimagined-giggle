d8f670e291b72b318ddb63e95aab69882f9973f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:57:36 +0000

    damage education whistle mainly clean

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+print([x**2 for x in range(10)])
+def calculate_volume(length, width, height):
+        return length * width * height
