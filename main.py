c42ae4666337559aeeaf38b3343d3cf0910fde9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:21:17 +0000

    love nurse port of bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
