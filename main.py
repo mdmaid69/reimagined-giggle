4c631e624488c6eb9b284ebc7f4d9db70f46793d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:50:25 +0000

    branch club kill Bb safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
