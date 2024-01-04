6fd6cb310b91cabdebe2c2e1c6c2d5dc17a2a08f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:49 +0000

    come deaf on nose so

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
