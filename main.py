3df673f0d6f7e22b171b10ed360e3ac27429131d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:21 +0000

    immediate let declare only wrong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
