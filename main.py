ba2dce6528ab05ef6d614401b84673161a0a8e44
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:54 +0000

    sore doubt who century squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_list(array):
+        return list(array)
