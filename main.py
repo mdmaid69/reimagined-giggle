9331a17ab2a2c2afcb761d587632409e3a1255b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:14:47 +0000

    plan tidy protest opposite after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_exponential(x):
+        return math.exp(x)
