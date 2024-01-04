530c61ab61c79dfb7cf591ae3b5b12ecb4328110
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:34:03 +0000

    base content treatment considering pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def multiply_numbers(x, y):
+        return x * y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
