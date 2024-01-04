7d2ddcaee2fa49564f11a0b9a4a12ec8699cc363
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:57 +0000

    height interested matter repair welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
