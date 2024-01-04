48117583ad7c38fe3e6786c0ddd6863c6dda0e04
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:53:12 +0000

    scream of steam black reaction

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
