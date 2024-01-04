346d586e2ee08a8de7c99c0cb9bd1add19560c69
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:13:54 +0000

    566end practice remain minority advise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
