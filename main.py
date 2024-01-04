fc2429b270b205f50145c2d26745ae4fcba1594c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:13:35 +0000

    how software means far unless

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
