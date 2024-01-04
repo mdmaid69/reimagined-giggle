9bbe2e08a99ae8868891a13a5416fde5119becb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:45 +0000

    minimum ability image sell influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import os
+def change_working_directory(path):
+        os.chdir(path)
