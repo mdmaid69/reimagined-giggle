0f21aa880ce3ad0963ab81328a11f925a04eccaa
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:21 +0000

    on effect reject set dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
