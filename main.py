bfaea185506e2d2c4e23201966770ab261dea802
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:23 +0000

    train and shallow twist discuss

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
