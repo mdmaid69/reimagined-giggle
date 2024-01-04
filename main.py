d79393f99be8ddc48fe51f649a24347444110284
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:34:53 +0000

    film flight nor where last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def find_max(numbers):
+        return max(numbers)
+def greet(name):
+        print(f"Hello, {name}!")
