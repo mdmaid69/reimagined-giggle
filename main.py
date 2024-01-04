a3c1a2a939c03349aa01c878aabfc6d480fef3c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:24:37 +0000

    except nature lift brief kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
