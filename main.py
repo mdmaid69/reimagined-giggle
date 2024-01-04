2dcb72b7316a9c2bbaaed09cdccd5878753e489f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:18:12 +0000

    shopping slice attention train wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
