c7029b2250b86a3ac8a08d2d253a6b0b3f46896f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:05:21 +0000

    lesson agree use 571enough right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_sine(x):
+        return math.sin(x)
