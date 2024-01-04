7244e3bf366f5653e75b8a4a619a8f0bd178b0b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:36:59 +0000

    worth dark beneath invitation bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_speed(distance, time):
+        return distance / time
