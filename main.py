f289922b53c9d31e9bf64b07e60da0a75e27915c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:46:22 +0000

    perform fetch squash effect each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
