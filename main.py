4bfb1db6ecd368b3afd5fdd0caf01de3b9dd804b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:07:36 +0000

    good discuss pretty reach woman

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
