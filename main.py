cca895fde20114fb8e8fd4b4baf625463dc86cc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:02:16 +0000

    combine comment smart command half

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-print([x**2 for x in range(10)])
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
