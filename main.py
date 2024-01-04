d578cae4b6132c8f75c3aa036136ab9a41b59c37
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:33:36 +0000

    swing appeal become defend work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
