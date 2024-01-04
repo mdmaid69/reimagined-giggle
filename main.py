411828d0a994572ece2e553e5e4e6f08f1765ee5
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:01:02 +0000

    cool regional honest state programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
