ed2b4d8e6af19b32ba7d1b4c66ec281aa659521b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:06:41 +0000

    sting wait border themselves ought

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def find_min(lst):
-        return min(lst)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
