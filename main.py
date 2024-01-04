3f4c75830fef38add17f05285b5c15401a96e16f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:32:03 +0000

    spend scare he experience argue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_min(lst):
+        return min(lst)
