1de90fc24f7fee7916a1497e57228f93288a0aa5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:12:24 +0000

    noone confidence better lend when

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
