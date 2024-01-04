4b50b3032905042d3fee868f75240c08553cf6ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:19:02 +0000

    gate fold build acceptable satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
