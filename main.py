e75515f6f82cac3124f58cc434d37509abf6fdf5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:46:04 +0000

    war quantity care change now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
