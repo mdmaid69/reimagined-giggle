e77ee9e4ed72f142910a3da98f6e0455831b8da5
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:57:41 +0000

    rate any contact about show

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
