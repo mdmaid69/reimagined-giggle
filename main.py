a63dda191dd68dc8bb2d5e403de407fbd99ebfdd
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:58:41 +0000

    trust concentration track back work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
