e6ced3135184dfe5a2e04fad89b1df0480fc25ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:32 +0000

    speech song figure sudden anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
