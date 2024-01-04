728a0fbc97b37799ae67fe3758b244f48cfaa26a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:46 +0000

    shadow underneath male pride anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import re
+print(re.match("h.*o", "hello world"))
