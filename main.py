fd093afeb30b47573299f830ef5513108cb96247
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:57 +0000

    expensive now regard tip case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
