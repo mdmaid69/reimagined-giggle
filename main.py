72f77e91321ebf089854e81777ab4fa21f83564c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:22 +0000

    glass bridge honest spill receive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
