e5a5918bac26bd53edb06a7230224f62ae0f2eb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:21:18 +0000

    bush patience luck leg approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
