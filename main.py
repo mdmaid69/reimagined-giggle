02674545b15c5bdffe98bd71ea71e6ca10b7c814
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:53 +0000

    nigga sort people giveout no

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def close_database_connection(connection):
+        connection.close()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
