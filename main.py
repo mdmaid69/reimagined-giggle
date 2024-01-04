75d1d36b7fe5fa93ffbd1e64f33743b9b2ffeaab
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:38 +0000

    interest bad performance middle report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
