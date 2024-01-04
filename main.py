330a8c1de91710aba20fd22399a33f19201a1159
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:40:15 +0000

    bar shadow cum estimate wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
