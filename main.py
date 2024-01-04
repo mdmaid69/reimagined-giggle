762f49e08a75298b87209aac6a558a910c3debd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:32 +0000

    live piece born at soft

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
