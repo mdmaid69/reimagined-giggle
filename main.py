0b96006b2437ca1aead0d136a341932154f1d8c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:19:01 +0000

    wire stage strike joke attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
