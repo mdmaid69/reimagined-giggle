26919697e01c1274db107caf20cb4cfbd6c6fee0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:43:24 +0000

    impossible wash children disturbing perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
