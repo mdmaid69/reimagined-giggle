714f370ad9f0a608ab7a59981e6bca5995142184
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:01:02 +0000

    regional shut murder wish occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
