1706fa5bb9059e3d45e14bdec6d39172fa0629cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:41 +0000

    on makeinto pump start safety

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
