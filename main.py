fb5f63105bec872ff38eac410cbbdb7bc7fe0415
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:37:32 +0000

    wire blame serious declare opinion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
