1ab6191751212fc8ee32d8583c11048c037b102a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:51:28 +0000

    many temperature happy bit that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
