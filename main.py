b5d918aac2300edc755d34fd087186752817f1ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:00:03 +0000

    weather right show recover paper

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
