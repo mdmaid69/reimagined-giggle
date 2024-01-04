4b9668354a568ffcd6b8f28a21383a28a6b74dbb
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:23:07 +0000

    cry when our achievement goaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
