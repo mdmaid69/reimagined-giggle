e10cdd68168e8cc00f55252ac934d5d10282f7bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:29:18 +0000

    ours guess confident through quarter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
