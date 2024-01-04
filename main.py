1a177d5072b70c5bf2fe7619e2fd09458ddec18b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:45:24 +0000

    duty declaration contact very length

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
