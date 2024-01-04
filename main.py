4c3bc4dce45e2c3ffe59686af714b3b36a649392
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:31:16 +0000

    disturb loose design trade lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
