8a1372b59a1fc4aeb17d9ddad8ca4ba01df1fb2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:40 +0000

    card I determined ball form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
