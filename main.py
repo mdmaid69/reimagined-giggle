2e47079eaca3f7aa8dc466fff0200ac50a4723f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:56 +0000

    need relationship sticky better worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
