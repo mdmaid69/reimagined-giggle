b026af1e3f311c2ebf9220ce21747200ba1143e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:06:32 +0000

    fear church pause wrong transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
