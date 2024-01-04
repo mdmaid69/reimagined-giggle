bbcbcf51b157ae9df497ba412b1baf327fac56d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:39:56 +0000

    danger introduce fault medium dance

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def split_path(path):
+        return os.path.split(path)
