0e4e9a1ebc29f7b9cf1ffbc883bc7ecb2a23252d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:52 +0000

    arrange stranger continue practise key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_str(array):
+        return str(array)
