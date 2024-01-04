a250913065010bc056c5aeaaccefe3dc1deb031b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:01:29 +0000

    move arrangement collect room correct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def is_palindrome(s):
-        return s == s[::-1]
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_sine(x):
+        return math.sin(x)
