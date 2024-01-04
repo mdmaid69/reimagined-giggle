cb2a069721bd16d065ad5b4491b49b7319780e54
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:16 +0000

    target centre scale interrupt object

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print(sum(range(10)))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
