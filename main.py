9d0a5cb943ab6eb9d178b505bc54abc872c8713c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:04:38 +0000

    many proper front nice accommodation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def square_number(x):
-        return x**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
