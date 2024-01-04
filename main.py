744b15b98f631d52eb2821d99cc9666dc63e9e92
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:45:34 +0000

    heart yet straight wheel spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
