0df715875148ace9c86a6a1d16975b8b6818733c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:24:14 +0000

    pronunciation wise emotion afford correction

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_pressure(force, area):
+        return force / area
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
