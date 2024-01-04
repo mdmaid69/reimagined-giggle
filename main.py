ced02233cb90b7151b6830bb8cca06cef550ed18
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:53:47 +0000

    beyond high niggaacademy appearance like

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
