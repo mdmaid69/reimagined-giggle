f2834dcf4251c5457680415852059e02362f70ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:06 +0000

    even opportunity for spring balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-print(sys.version)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
