dddb4d64207180b751b40c81ac6b7af77d697735
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:06:38 +0000

    passage instead clean born suppose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
