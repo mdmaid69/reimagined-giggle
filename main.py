4586e37fab2e1cebfe6de204ab5c2769eb0896a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:00:54 +0000

    delicate cheek Jj development beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import collections
+def create_user_string():
+        return collections.UserString()
