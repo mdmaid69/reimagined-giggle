5f8733ea2cfdc9fa167e681dda1dcee578a345fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:13:38 +0000

    godown slightly such motion band

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
