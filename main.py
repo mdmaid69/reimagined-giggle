a798c9a516237818c33446ebfe01dd3bb70cf5da
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:40:27 +0000

    save platform bringup justice properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
