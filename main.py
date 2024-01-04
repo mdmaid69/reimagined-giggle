7c3686e5e95d0791d2e52de98a76ffd675864e6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:13:22 +0000

    sentence list bus majority shout

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_min(lst):
-        return min(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
