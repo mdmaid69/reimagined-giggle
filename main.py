3776e432f2ee12260c81cce9a26543df875a7a1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:29 +0000

    judge 569enjoy stupid your spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
