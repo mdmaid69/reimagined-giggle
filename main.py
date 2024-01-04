e67767d00dec0048316b6ae624aeb3a12d0cef1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:02:54 +0000

    may eager smile save bit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import time
+print(time.time())
