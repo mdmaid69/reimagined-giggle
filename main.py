1b7a699af9ec8d4645edab246ae1620aac018a0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:43:09 +0000

    nonsense home punishment around reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def cube_number(x):
-        return x**3
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
