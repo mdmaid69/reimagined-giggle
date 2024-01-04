e9f229cb529438b935c62f90a9d163314e71c450
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:26:17 +0000

    prove spoil firm which pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def count_elements(lst):
+        return len(lst)
