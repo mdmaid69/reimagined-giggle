af3a84afe61d91b8ac4dc2850e970b1bc3085a85
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:12:00 +0000

    store straight goout warn bringup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(a, b):
+        return a + b
