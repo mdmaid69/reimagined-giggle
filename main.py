1f6c44467c1f551988a92aeac04343679eb8c58f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:30 +0000

    closed comeoff thin medium remark

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def is_odd(n):
+        return n % 2 != 0
