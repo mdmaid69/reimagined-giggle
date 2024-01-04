c933086f6fe8830e9d384d72239dc12a41c2a2d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:17:40 +0000

    temper unpleasant score 568energy pull

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+print([x**2 for x in range(10)])
