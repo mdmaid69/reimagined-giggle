dbe9d1aaf182bac6e9cfda530b8b71ade00f2e81
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:17 +0000

    himself vanish about unless unkind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import numpy as np
   def calculate_median(arr):
         return np.median(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
