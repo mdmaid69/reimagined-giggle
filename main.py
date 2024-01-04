fe7c1fbd33e914398b672901b940272de0302883
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:49 +0000

    preparation strong wave treatment run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
