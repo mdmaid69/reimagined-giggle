f61db2d1bf4b1593a8b6464cbb2206ab5e2fed14
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:22 +0000

    uncomfortable degree into write delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
