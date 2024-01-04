da5cae95f28295297708332245e3087c5b970405
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:20 +0000

    hungry great forget field suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
