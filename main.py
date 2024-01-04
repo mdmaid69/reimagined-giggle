5d96aea5904410866467817bbca34eaa738edbfd
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:04:04 +0000

    government exhibition boil you once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def count_words(sentence):
+        return len(sentence.split())
