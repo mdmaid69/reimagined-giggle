86652008ad57dcbe4cdc1eae4fa89951017ff570
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:04:29 +0000

    Tt fall tongue motion sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-for i in range(5):
-        print(i)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
