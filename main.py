fcd43f61cda9804535df5d67e249e805378b6a82
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:08 +0000

    judge agreement jump water actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import platform
+def get_python_version():
+        return platform.python_version()
