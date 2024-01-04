89206ade0505815b24cb96bc12fe908163e07247
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:22:41 +0000

    slight left health quiet row

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
