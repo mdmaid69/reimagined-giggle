4915534ec35bfce5bd77bfd309c99317de06a08a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:11 +0000

    raise it violence decide concerned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
