1fd4acea0697a98ffd79df9f7704436fb16f057f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:22 +0000

    address apply previous married nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
