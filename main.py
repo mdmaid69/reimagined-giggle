ecc62085534eb89a68e5facf65b57fe7a87668fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:58 +0000

    whether anywhere his demonstrate hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
