b62ee71f43e455fd314bb4f972ff45aab85195f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:03:05 +0000

    story fasten closely commit big

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
