1cc83f51ada4c4341784eb2260aa0bb911d0bfaa
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:45:42 +0000

    sensitive phone treat divide meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_current_time():
-        return time.time()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
