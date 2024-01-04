37099ce319ae7bd4d201951ade1f051e4cd8c12c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:28 +0000

    train wish waste luck will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
