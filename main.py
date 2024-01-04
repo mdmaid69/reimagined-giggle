897f6e265ef7c0007bf48367e0dbc9ae87ad9171
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:33:20 +0000

    professional run off benefit land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
