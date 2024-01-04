295d7639abb70e6836ccffc1dd63fee2f1d4a998
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:03 +0000

    word communicate where about figure

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
