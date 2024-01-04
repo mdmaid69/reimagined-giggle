2302961ddb322323e2b3a44407309a5448174cc3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:38:16 +0000

    yesterday study dance joke building

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
