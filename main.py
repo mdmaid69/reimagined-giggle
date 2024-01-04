d7fe6a260394be67db4ea1d259cf3adcd51200da
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:29 +0000

    than solve path or pleased

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_from_file(filename, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.frombytes(bytes)
         return a
+  def is_even(n):
+        return n % 2 == 0
