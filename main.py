112e8a4a358c475e6b1891f6022a63fe8859c03b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:38:59 +0000

    future shall battery lock street

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
