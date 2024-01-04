d77031aef8e7a81b1248f41aa6e03fac9456bf36
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:44:04 +0000

    fly arm accident coffee special

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
