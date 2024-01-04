0dd92a232dd322fd395ebdcface1fa3b26e16990
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:19 +0000

    opportunity card bag human container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import tensorflow as tf
+print(tf.__version__)
