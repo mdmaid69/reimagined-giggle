e40899c7325026d7f8705dc076fc77ce3620f196
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:07 +0000

    anything breath hope badly ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
