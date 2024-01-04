55fa3ef4f577a2621b1944ccbcadd0d76c0c3e00
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:01:08 +0000

    upstairs determined entrance mass forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
