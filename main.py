ea0c4ccd9631bd422ee36c33b71165f75decfe89
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:15 +0000

    die squash hold cook because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
