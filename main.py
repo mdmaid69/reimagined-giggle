7dcbb0539d0074d58aa1cd61463e2d7fe4054f91
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:55:07 +0000

    refuse payment show interested foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
