071ac91816f1e8f9ca35688ea28f46f2308efe10
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:11 +0000

    convinced walk bang collect communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
