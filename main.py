989174f1d73fb7b69db5363d67d6bb311d177667
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:53:02 +0000

    term top dress theirs towards

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+for i in range(5):
+        print(i)
+import time
+print(time.time())
