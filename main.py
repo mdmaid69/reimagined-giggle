1ff7b765b0a635997c1fba0e1b2640dc7e494ab2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:59 +0000

    empty normal turnaway excited after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+print(math.pi)
