d36891ae3a03f6a4fde713eb011569acb41b70b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:28 +0000

    us act thinkof 567enemy can

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import tensorflow as tf
+print(tf.__version__)
