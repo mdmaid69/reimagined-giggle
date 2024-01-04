b45d3e8c436aa4a33c96bb57a4b9f416bf2325ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:54:56 +0000

    excellent because cum storm criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
