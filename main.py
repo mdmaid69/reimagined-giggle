c50d88a55ae368248c598277785ca230056b3726
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:49 +0000

    crash area partner clever profit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import sys
+def print_python_version():
+        print(sys.version)
