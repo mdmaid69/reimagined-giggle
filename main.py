4a3947f1589d56a2beed70839e10e89073f26751
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:20 +0000

    direction wake separate plant relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
