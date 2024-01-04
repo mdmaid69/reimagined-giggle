2a6c0008c026d91ad8c64c33049ad1b7dd273112
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:59:21 +0000

    charge pleased band steam remain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_complex(array):
+        return complex(array[0])
