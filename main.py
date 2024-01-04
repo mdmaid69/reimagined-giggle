705d1b9e0bc64dbaa493eb078e50dd38cbc6d021
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:59 +0000

    true fence clearly offend little

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def is_odd(n):
-        return n % 2 != 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
