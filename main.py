86f559a9db8f1905399dea25b7b7ceb6aa25d657
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:25 +0000

    neither boring mind exciting pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
