780c6473526e0363dbaf3a5720921353744c065f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:40 +0000

    dear hear protest peaceful callback

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
