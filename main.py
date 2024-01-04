1570438c7cdc99943fba2ed71c08de7d3d365fdf
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:24 +0000

    performance disappointed pass smile book

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
