9148fd85dd84d99ceeb14706de13acbcbaf6223b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:43:28 +0000

    connect lend through any bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+from collections import Counter
+print(Counter("hello world"))
