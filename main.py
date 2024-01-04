fbcde0ee2a74f4352b6144b2026edbf858c7eb19
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:03:01 +0000

    holdup moon interfere quiet condition

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_length(array):
+        return len(array)
