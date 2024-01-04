0cd20d815e49224895860005dce0473ac31102b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:20:43 +0000

    comeround machine chat bet belief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_max(numbers):
-        return max(numbers)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
