a9b198af8c60f37aa6caedd74de9e316e46e66b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:40:32 +0000

    plan sing favour frame set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
