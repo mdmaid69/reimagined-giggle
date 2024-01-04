a19e57a86ba482d931437ec6977588478e6139c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:47:26 +0000

    wave label willing track swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
