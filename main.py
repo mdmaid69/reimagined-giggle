8115b8883b4d00690d5b404c146a03d9bd09ef5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:18:09 +0000

    curious conclusion proud concentration stare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
