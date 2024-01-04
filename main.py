9d5d41d3bc52ac1c59a673d71893d14e767dd0a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:20:04 +0000

    miss post lot release land

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
