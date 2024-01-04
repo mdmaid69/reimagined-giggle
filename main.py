1adbf4d8dd8e95beef3bc2088445bfe84e1f662c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:54:55 +0000

    sensitive sensible knowledge sometimes broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
