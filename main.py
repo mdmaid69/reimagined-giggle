961e3d62667ead3b457fb767c459311fc21d3a11
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:55:15 +0000

    awake plenty scene sink twist

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
