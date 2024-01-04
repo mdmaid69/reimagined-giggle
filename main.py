f311ecf4cac582e6a658066494e911cbec40b651
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:37:25 +0000

    sad object mainly callback contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
