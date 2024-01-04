d9a41033eee52f8a5512faa4fb1080353368bdae
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:28 +0000

    exploration excellent without load consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
