35a50ed60e3eb17db46457795fdc17012d64d5d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:03:05 +0000

    fit border Ww style deep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
