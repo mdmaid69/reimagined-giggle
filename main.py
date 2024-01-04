dbfc02e41919ff8a2454b5757a075795122937c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:48:28 +0000

    satisfactory supply hit comefrom confidence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def find_max(numbers):
+        return max(numbers)
