2cd0009b9b99c0802029dea39572b69aceb560d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:46:35 +0000

    direct or plan wide snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import collections
+def create_queue():
+        return collections.deque()
