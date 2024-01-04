7d84c05f59f6f53e6c02bba07c44519cc353190f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:59:23 +0000

    prove up sale pipe though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
