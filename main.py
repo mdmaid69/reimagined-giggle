e2ffc9b9dd4ba9cd20465444f820756335d2dce8
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:55:29 +0000

    eager admire so protest material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
