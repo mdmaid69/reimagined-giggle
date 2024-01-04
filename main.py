39a4f2fd57f3605cd6c912ac3ea0ab485237877d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:30:54 +0000

    since watch service hot ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
