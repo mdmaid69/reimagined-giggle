07b1f5ef70b1189514689a570580a8cf8e8698c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:27:16 +0000

    turnback official spin goback(to) confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
