690f74383ab38436782771c3a71c8ea855d6a8a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:45:54 +0000

    floor opinion powerful convenient slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
