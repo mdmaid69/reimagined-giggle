532afb061d7b54be7af6bc7d332081e8d1cc96b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:25:16 +0000

    rich turninto whole many sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
