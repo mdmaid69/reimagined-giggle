9bec37c4a8c68268bed1dbb56b8da71274e95808
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:56 +0000

    breakup myself belief 179bite complicated

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_priority_queue():
+        return collections.deque()
