4d093fac14a50d991b7def6df7b5cf26d8dbb632
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:26:51 +0000

    contact communication call spot round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
