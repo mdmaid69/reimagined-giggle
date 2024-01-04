49ab264679a2b8a1dddafe5ceb2f65976cfc0582
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:40:49 +0000

    today mention believe spin raise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
