2a2b5cf4415ae3f18585d2dfd506d47744758730
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:42:18 +0000

    delight treatment fly base appeal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+def get_time_since_epoch():
+        return time.time()
