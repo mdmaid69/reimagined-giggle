98016cd2b490084b311dd15c5436a929bb90cfde
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:02:52 +0000

    good create freedom gather successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
