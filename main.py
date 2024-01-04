6598aafcc66e72d64cf91697b458ede2e74ee40f
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:19:41 +0000

    turnback flood unit both failure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
