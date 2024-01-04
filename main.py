ba10e250fe205c090556a5440a274dbb07cbbf8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:13:30 +0000

    correct increase tour expensive question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_sine(x):
+        return math.sin(x)
