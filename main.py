b23c9b25bb9918c512d8d80e4a89b773d4d002ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:30:06 +0000

    mess fetch comeround thin father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
