ce65197ff330d40ac9ec4cb8507be748b3dcfee2
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:36:24 +0000

    rely general tight plant rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def reverse_array(array):
+        array.reverse()
