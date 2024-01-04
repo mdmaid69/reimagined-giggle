c39683e4204eb0a4f7646dbc1c5c62f0b54c6a13
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:35 +0000

    lay month harm feel contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
