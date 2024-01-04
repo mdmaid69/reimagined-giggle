9542629380e01f03232b145b2a7f04e8525b098b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:09 +0000

    thick pass praise instrument responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
