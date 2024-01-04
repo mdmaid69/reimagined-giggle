60a0b47b2075e444cd8371f1a98043e0626a4139
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:31:59 +0000

    bath suit ground conduct crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
