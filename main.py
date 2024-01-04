bb2c0c3d048e0c7f0c081e5a7548062a369b2f46
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:55:41 +0000

    declaration small tune actual company

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
