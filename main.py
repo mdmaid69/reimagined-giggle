9fd0d6d96b3ef1c858f543ea379127f0fe2b7509
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:18:01 +0000

    situation talk down radio difficult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
