f708525f8f434dfc4df19b12daf286b6b8cb8089
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:07 +0000

    except office danger mess congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_error_function(x):
+        return math.erf(x)
