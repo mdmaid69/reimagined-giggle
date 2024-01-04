577ec3222af1294d9b810d2bd398d035276830fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:51:42 +0000

    word wind guide clothes deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
