764303013e1d5300fa6769948d2aa74a3a3984ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:25:49 +0000

    majority anybody when plain mean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
