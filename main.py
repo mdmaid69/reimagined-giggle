fb59b29575a6cc854c4c1f53f5a78d39118b5a07
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:45:00 +0000

    now appearance bound Bb intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
