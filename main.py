086a8985fb5b301594a3d2252ce9126ed29dfed1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:24:48 +0000

    work claim motion mine mixture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
