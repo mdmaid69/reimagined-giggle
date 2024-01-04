38e7a6926d69243a573b6d7bee17fd3365ab9680
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:44:09 +0000

    sure so awake lost reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
