0f49b3f07c39a0b97fdd46368134eca26ad5bc44
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:36:19 +0000

    bottom hungry train possession behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
