320e245c1f3fa44a032dd2d4dfd820ed7c3984e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:53:00 +0000

    divide name between level pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
