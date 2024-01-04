3fab2997e505971511a41050caab8f9419200bb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:04:22 +0000

    wall crash valuable over tight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
