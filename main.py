7389d8027ab46bac52d39349faad51b19a4ba324
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:52:09 +0000

    far tomorrow service star immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
