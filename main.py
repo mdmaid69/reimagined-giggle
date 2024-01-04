1098dfd0af88bb9bac520aa49fd30ac3196651b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:01:27 +0000

    brush laugh science everyone post

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
