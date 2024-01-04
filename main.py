43ecadb5bf2c14e6429e14f0732c2adc1139ee6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:01 +0000

    teach within remove news tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
