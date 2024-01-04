5f6b30b1303595877ab0ac3095be8b8ead3448d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:50:49 +0000

    pin concern on face company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
