7f777c8356be99eace227001061067f2ac2a41a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:37:37 +0000

    pure realize die second possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
