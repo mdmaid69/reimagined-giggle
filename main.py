06e5c4bdef1e5ee253806bde40d8fdaae71195de
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:52:29 +0000

    reserve calmly interest total mean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_float(array):
+        return float(array[0])
