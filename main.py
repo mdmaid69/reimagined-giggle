fd81a594cd11b9afffd9312dcd9a287927fbebc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:50:11 +0000

    bet manage birthday exaggerate hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_int(array):
+        return int(array[0])
