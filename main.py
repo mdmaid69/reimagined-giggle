0b86ba80e6e1a86d16d41cea8e8ee0702c8b3d2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:06:09 +0000

    safe design combine previous worry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
