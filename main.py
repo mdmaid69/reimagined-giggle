c151265eea902f981baa840132c4f1b621d3baf7
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:29:15 +0000

    tax introduction benefit ground unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
