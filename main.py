b338ca593b0d29547f0febd11924c6ea02b56a27
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:07:52 +0000

    gift suit less survive exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
