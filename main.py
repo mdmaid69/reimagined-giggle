118818056e5e478851f06845229fc20ca7fd38c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:55:04 +0000

    style impressive welcome man advanced

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
