7f2b6f8f4c2eadc9ce98a3098524d8af9f2ede65
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:44:00 +0000

    agreement hold inquire rough private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
