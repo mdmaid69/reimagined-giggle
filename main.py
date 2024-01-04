15210c3d94f8f2c3e5d6de2ae3629ea614e4d9e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:33 +0000

    opinion police punishment beat priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def calculate_area_rectangle(l, w):
+        return l * w
