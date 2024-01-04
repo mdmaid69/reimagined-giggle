d01db3b78b32605aee87239bfa08be66b42e7e6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:03 +0000

    suffer sky discover along thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
