002bf1273ffa7e4d118bbd1f6088c1fa5468d563
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:36:00 +0000

    564encourage stretch union miss trust

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def is_even(n):
-        return n % 2 == 0
+def convert_to_binary(n):
+        return bin(n)
+  def cube_number(x):
+        return x**3
