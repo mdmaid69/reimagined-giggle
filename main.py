af13942c3d817131e66e268a192df75ca2cc2434
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:01:24 +0000

    rare difficulty minor raise fear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
