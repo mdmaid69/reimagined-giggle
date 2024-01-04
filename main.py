27d2d8c9b9bfa1803b3fcf636e7d837b19aa1368
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:02:21 +0000

    accommodation absolutely way rain mad

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
