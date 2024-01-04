a308df42eecfa3f69101b5e093f4269c719980d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:02:29 +0000

    forget pull gooff experience mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
