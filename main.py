d3a665b42c7889b9d395544de1315c6075e6921d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:02 +0000

    start comeover within mouth bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
