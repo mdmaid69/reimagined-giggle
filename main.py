71261a04a51f357fb23c0df60d225e9132c0df53
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:36 +0000

    moral age secret load hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
