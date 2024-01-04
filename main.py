ad3abafcb92ab5f1bdad474359b655a7ed80abe1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:03:12 +0000

    form transparent 573entertainment again snow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+print(os.getcwd())
