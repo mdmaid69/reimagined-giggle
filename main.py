1a85d4b08bea7015912fed32f291214ddd96829a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:09:00 +0000

    car somehow form long concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import time
+def get_current_time():
+        return time.ctime()
