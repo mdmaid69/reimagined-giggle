f1946730196359e2d98b5a88f12805fb269d4a49
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:35:08 +0000

    board right takeout ever case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
