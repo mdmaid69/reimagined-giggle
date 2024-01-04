cb7a19f5446a4714b18f7994226a0d6f272eb71b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:09:06 +0000

    everything heat window supply tooth

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def find_min(lst):
+        return min(lst)
+def greet(name):
+        print(f"Hello, {name}!")
