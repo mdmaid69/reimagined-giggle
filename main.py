752f6834fca7680c4c53705db9b3d6701de3d69d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:44:51 +0000

    imagination regular fasten pocket will

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
