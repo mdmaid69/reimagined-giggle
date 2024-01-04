d98b40c1ffdb5f0cf106ce9ef9770d3c010c89f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:16 +0000

    whoever clever fence stretch idea

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
