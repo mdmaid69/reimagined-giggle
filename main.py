9218d8c7b0962d1d5af78ba949188263befd9d38
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:48:14 +0000

    clothes run neither food without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
