315611720a13619dcac7341b5cc972eb99b11715
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:11:30 +0000

    frame hope spot behaviour favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
