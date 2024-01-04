0dbdd429553c2987d4b18329a0312939145f0789
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:18:27 +0000

    computer foreign minor this improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
