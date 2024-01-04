833fb3f09f15ca9f3cdd3dda593a63ffc22d0ef6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:44:19 +0000

    pay mistake heat recently show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def is_odd(n):
+        return n % 2 != 0
+def find_min(lst):
+        return min(lst)
