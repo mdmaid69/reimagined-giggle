a7b53f35db5d0c206769764157d001919688451d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:17 +0000

    building bury temper post speech

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def count_elements(lst):
+        return len(lst)
