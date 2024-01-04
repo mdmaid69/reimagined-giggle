e91b24250abeaecc268aab786689dfcd07edb82d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:51 +0000

    tragedy and inquire professional thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
