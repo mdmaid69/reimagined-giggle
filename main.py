3a48289909c88dabfadb395352729b47d05d7d01
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:28:21 +0000

    mine care impossible arrange break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
