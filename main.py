76ade6820768dea76b8f9ad0186afd794a8715fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:16:05 +0000

    mistake miss also laugh after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
