10efb0e3ea2c8dd74d418bad9564f5721b5c3ac4
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:41 +0000

    left criticism show breath tread

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
