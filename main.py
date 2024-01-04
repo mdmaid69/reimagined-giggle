ee89b51605168e2652c46909b0e7ddd88aecc8e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:47:56 +0000

    how scream pair hope drive

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+for i in range(10): print(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
