49a97cc71dcf869e407ce6a4f4219cf69f135aed
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:15:39 +0000

    passage book unit reaction whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
