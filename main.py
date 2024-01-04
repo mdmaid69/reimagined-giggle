f4e52c33425ab71a8e95bb66504fa6b91960ad81
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:32:20 +0000

    show provide tradition control towards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from collections import Counter
-print(Counter("hello world"))
+import array
+def reverse_array(array):
+        array.reverse()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
