d2f42f1a4c299fa15b639e624add965f1e9ac773
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:01 +0000

    left possibility swimming tour comment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def create_queue():
+        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
