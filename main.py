a19e5787e76f6a65502a1eea316e73c492e1d8c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:09 +0000

    record thankyou account method kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_set(array):
+        return set(array)
