4c8e61db96511123192d129b4279294389fbfeeb
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:22:53 +0000

    reserve matter stamp air innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
