1661807b0724b3bf7b776e651915c32d4057e7c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:03:38 +0000

    hit advice satisfied before promise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-print(os.getcwd())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
