e332fb03eaf54583a6bf121fb27e2d1964976c1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:35:09 +0000

    spread innocent along idea same

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
