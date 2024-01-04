f9fad2f1be5627e7a2ddc656625e5c68dad31e70
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:30:58 +0000

    immediate live lunch discuss tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
