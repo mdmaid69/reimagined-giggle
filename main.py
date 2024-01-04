088fdccab899b782f16edc9fe3c20af59adba844
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:24:43 +0000

    character free schedule of direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def create_user_string():
+        return collections.UserString()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
