5272dec5a93ac5416b7ec4b6c5edd32bdea3d453
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:16:31 +0000

    pocket rest available weak somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
