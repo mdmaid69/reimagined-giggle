275f2216b3f4799818c30037e264616e9c085142
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:52 +0000

    unpleasant wander breakin worried start

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
