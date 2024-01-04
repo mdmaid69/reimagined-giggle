49b4813ac86cb6e24b7d245874f62c51a75ab7fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:03:37 +0000

    empty snow brief float favourite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
