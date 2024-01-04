6e5bc9b0ae21c3cfd2ef5a4df00e1106018b7e81
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:50:05 +0000

    meet explanation average there intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def remove_duplicates(lst):
+        return list(set(lst))
