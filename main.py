3b6a07dc941c95d9f563aaf5039c83dc0260914a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:07:20 +0000

    accept same brief mother custom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import collections
+def create_priority_queue():
+        return collections.deque()
