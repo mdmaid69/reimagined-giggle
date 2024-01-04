31fc3d04f9ede7339e1f839e9d6803dfb7a4328f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:52 +0000

    street generous weak real journey

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
