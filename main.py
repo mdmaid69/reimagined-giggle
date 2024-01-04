a483f3503ffd1b26191dee518da1eeace23b9b72
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:50 +0000

    goback(to) mail boat wind subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sys
+print(sys.version)
