9c5ef6f9a06a4c1cb8cb49446fc33736d5088fc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:31 +0000

    takeback rescue need sense increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
