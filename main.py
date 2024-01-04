5bfbaff5ab36ace31d3d3db3fa428d5a53a8419f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:01:11 +0000

    board charge admit getup rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_memoryview(array):
+        return memoryview(array)
