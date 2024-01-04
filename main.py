d605894f1ff39ad33f916769d866bd1cde632e98
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:43:08 +0000

    Oo theirs turn do aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
