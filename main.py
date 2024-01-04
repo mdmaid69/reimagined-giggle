fc5971f5f69d3e83501fcfb4235ffc65bfe399be
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:58 +0000

    perfect on far club pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_exponential(x):
+        return math.exp(x)
