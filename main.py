18e33511fc7652b3413458e244d511f7c1a93c74
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:20:48 +0000

    house better lose finish floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_date():
+        return datetime.date.today()
