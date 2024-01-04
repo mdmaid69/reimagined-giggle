69b4494a9ab8de06d9513ef60f27f8ea984e6a48
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:10:57 +0000

    hurt today along argument equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-print(math.pi)
+  import sys
+  def get_python_version():
+        return sys.version
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
