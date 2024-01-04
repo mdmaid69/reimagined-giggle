deb4c90777de962b855dca6477871f7d86239623
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:27:35 +0000

    plant child admire shoot substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
