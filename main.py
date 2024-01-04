2f1c6462178fccf1e65f0fe09de3a3df9bebef9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:27 +0000

    possibility run accept match punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def add_numbers(a, b):
-        return a + b
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
