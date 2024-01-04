6ee285fdff9f93e3aba3c4f040eaf39d12ad946d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:08:19 +0000

    569enjoy unfortunate himself walk negative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import sys
-print(sys.version)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
