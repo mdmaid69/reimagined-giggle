fd628690281109584ae17e41f0f9f8b756b87987
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:58:54 +0000

    classroom better quick pause dark

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
