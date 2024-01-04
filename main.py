66c96f4af242b80ec1cefb3fa69fe3c394f646a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:59 +0000

    increase authority sound touch knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
