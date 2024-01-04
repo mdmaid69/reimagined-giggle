a1809446bb358165fe9732031e9b55bbb7a67ac2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:47:56 +0000

    chin concern achieve decision oil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
