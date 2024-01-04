4599e7276d11268508819c31858de41230f00559
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:25:59 +0000

    friend draw operate challenge best

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
