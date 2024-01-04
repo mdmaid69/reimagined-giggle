20b18c5b536d76cd1b07e5508ca937b2896376b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:58:33 +0000

    later valuable turn post illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def square_number(x):
+        return x**2
