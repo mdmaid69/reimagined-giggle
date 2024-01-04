c60f0e4579aa4423d0704c452107457b60c0a8b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:04:29 +0000

    material song do art harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+print(os.getcwd())
