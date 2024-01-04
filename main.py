7d983aa3eee87819a81a869bf8ab57f55d3a4f72
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:20 +0000

    detail 571enough settle tip Cc

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
