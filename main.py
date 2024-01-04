3a1e423f0b7a62b510dc9f7a01479ec9933b9a13
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:26:51 +0000

    large important used himself stare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+print(math.pi)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
