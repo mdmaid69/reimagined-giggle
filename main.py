c07b3520a24569aabba3ad444d22034aced83d7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:10 +0000

    yet rather discuss begin believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
