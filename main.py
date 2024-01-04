c486fd967ca5fcd4ff79b4a325c6e56daf869801
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:37:05 +0000

    hurry brain wall tough row

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def reverse_string(s):
-        return s[::-1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
