6cd2ca4330da90262405a00ac35e3e4c21883f68
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:46:58 +0000

    badly deliberate neck cry fortunately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
+def reverse_string(s):
+        return s[::-1]
