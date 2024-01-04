259b90b2957fa843bde8e27cb58b83485b9db577
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:23 +0000

    such dress exchange himself explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
+import sys
+def print_python_version():
+        print(sys.version)
