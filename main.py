78f183b433f70faca03f3dd2dc48e472265c2ce9
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:48:21 +0000

    live evidence fail turnover they

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
