3a367b02daba5af2bd5f1105bdd4ab7608b9185c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:40:47 +0000

    undo judge shoot hour outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_even(n):
-        return n % 2 == 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
