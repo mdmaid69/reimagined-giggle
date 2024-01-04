8a5fe773acd225b7654b25a16011ae227f491646
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:26:12 +0000

    fierce bye tense tone congratulate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
