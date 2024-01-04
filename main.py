c162d19dae6073392470692bab88e5f77a206042
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:54:23 +0000

    shout present risk furniture nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(x, y):
+        return x + y
