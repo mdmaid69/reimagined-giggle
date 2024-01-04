381338b6085f4937ea9a32419725fcfee04f6a23
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:19:49 +0000

    early cool can extra wind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
