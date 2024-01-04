ad9a45597cdfa158fd0e817afe2a0774ee51eb14
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:11:38 +0000

    brother slip themselves variety confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
