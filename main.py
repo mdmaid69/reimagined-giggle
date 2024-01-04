2ea5fbe33579ee7f2e16dcbc294d133316202f0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:59:31 +0000

    detail 571enough pot decision whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
