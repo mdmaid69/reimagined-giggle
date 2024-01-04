c4a48927d71abfdfaab616dcad196c84e3f981cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:22:50 +0000

    though air used flavour violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
