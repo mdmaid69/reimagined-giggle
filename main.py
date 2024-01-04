1cf34a4219578ed61df3cefae02ac6ad95edabdc
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:02:15 +0000

    basically genuine goout straight estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def count_characters(sentence):
+        return len(sentence)
