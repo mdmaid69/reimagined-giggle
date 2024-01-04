a3de7ed4c6c15461fa49afb01b257ed14c353871
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:02:42 +0000

    wait night back trousers middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
