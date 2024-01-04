63bcf50b340f432b8a6bdef9edad9de54a2d7f74
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:32:28 +0000

    sea float bread press have

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
