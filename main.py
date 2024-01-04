a4a9ce116bb300c76d7b7193b16dd2abaa1374fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:06:54 +0000

    far concentration even world walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def clear_array(array):
+        array *= 0
