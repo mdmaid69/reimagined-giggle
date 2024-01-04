57ee0be2ea510c2b575daa34dd3fcb4559e12bb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:51:50 +0000

    at loose block juice yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
