47aecc2254911cb1f1a4fc43878a7b257ced3394
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:34 +0000

    dare important by mark direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
