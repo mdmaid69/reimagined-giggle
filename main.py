0d2de8ff2447ac4b59c12d0f48eca7522f918e8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:04:47 +0000

    wander push child cap damage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
