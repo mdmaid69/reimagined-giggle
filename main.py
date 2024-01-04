ac01463ab150309ebd8d7dd2d18c3271383a7c26
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:12:01 +0000

    accommodation profit still fair generous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
