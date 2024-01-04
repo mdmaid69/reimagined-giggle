0b5b941f021f7e2e391afcd24f1ed395f1c428f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:19 +0000

    owe substance stop regret shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
