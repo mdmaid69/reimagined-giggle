063d0cdb470008e3b95785c26c52732a14d14b7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:00:32 +0000

    floor type city weather goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_current_date():
+        return datetime.date.today()
