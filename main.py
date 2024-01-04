403935916646325ec7ec17ca5f1c3620c4c8b869
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:04 +0000

    road always confident pause holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
