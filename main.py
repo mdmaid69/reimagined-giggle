05329147eecb1542054a2346744ab85155e3e177
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:18:39 +0000

    way road off trade accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area(radius):
+        return 3.14 * radius * radius
