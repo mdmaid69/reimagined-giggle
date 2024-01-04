72afd5eb05e4a5a19056507683d8df449190bd5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:41 +0000

    persuade uncomfortable everywhere thumb achieve

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_typecode(array):
+        return array.typecode
