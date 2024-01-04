934e2ab4772d91bcabe5432836a3c07f227c833f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:17:09 +0000

    whistle release bang pure fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
