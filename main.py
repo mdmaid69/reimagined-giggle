3673f61d5b93f96065a212cdd3a7efce4ce3e27e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:45:37 +0000

    basically clearly Ss convenient crazy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
