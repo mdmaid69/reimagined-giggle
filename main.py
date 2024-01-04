03f3bef807a9f4c6751d6328dfb666610d87de38
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:28:54 +0000

    hair over bright behave obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
