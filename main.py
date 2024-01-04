877905579b8ffb2c065de652ec8b3a5d88ef3703
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:36 +0000

    strength concentrate angry about great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
