5aad906512f001fa123295f167e095a2115f72e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:54:15 +0000

    supply except size attempt hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
