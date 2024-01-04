4660e60df2f454d058bf79e463e1fc62f429f8bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:52 +0000

    inside love accident attach draw

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
