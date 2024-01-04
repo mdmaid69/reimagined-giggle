9fba5e99b1d58c13be91b9f64840b9cbe12ec3d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:26:09 +0000

    meanwhile middle explanation watch rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def count_characters(sentence):
+        return len(sentence)
