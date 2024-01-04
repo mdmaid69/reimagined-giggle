44ce5123a2d64e5a9e9fd0c264cffd669522dfcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:27:22 +0000

    impressed guide bag sail office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
