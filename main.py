2c31669e0ec7431e5c6741914210b7159099af8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:06:06 +0000

    person dinner before side we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
