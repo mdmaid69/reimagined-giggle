3a44cf66343441b4e2ceef5deda3497e2177bc02
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:28:04 +0000

    fast lie without cost motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_item(array, i):
+        return array[i]
