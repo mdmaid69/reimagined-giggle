0d235573a03d16d18ea8e323bdc656a00759a1e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:18 +0000

    deliberate match contact claim without

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-print(time.time())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
