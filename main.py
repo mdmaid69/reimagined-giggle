c79716cdc2f92b4512773b41abce05dc4f08625b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:46 +0000

    cut doctor marry blue correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
