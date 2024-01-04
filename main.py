1c6b0ef4beab920d0abc9bd8803bc5c608ba1ca3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:18:09 +0000

    regularly write success create takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
