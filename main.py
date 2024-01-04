4555fcb21bb1cca331a73a7227bccc8b4cd478f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:17 +0000

    sore getback point beyond there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def subtract_numbers(x, y):
+        return x - y
