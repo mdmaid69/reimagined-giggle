8cdb57fcbc06c9fb7ef60aa8a1391c90dd3056aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:41:31 +0000

    separate walk almost piece suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import sys
-  def get_python_version():
-        return sys.version
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
