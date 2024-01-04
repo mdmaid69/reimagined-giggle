f1be2f09dc2d62fa16469ba85a3da189677ae303
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:59:17 +0000

    and explanation way heart damage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
