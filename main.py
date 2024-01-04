13b6cbd866943f68d3e228e356e4ee6d5ceef6b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:41 +0000

    shock service bang row talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_odd(n):
+        return n % 2 != 0
