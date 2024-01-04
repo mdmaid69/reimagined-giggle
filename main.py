a29ae678a8cb20576554eb101e2f4deeeab67a6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:47:10 +0000

    operate sign improvement road seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
