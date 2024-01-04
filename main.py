c5d4c79a31d14fb993fd0e89d663b3417ea35069
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:09:15 +0000

    characteristic afternoon godown meeting exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
