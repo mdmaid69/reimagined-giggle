6c76ddba6889632379ca52c7f1b4aab60ca4db73
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:12:49 +0000

    talk condition Oo know famous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+print(re.match("h.*o", "hello world"))
+name = "Python"
+print("Hello,", name)
