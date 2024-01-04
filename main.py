787ec4192facb60132e54f2f837e06b12aa8d288
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:10 +0000

    deliver means fit world recognize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
