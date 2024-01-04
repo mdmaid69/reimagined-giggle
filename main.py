c78b896f8763a750b607fe27282e27dd91b21162
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:53:15 +0000

    use recommend impatient morning yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
