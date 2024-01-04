365de81e5b220c3613e0fb19c6c74db8edf0d5c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:42:57 +0000

    long rid season wander us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
