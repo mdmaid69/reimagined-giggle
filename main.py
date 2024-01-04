56e919a701a3aa0c699edac28eae487a67439f92
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:42:28 +0000

    considering nature boring indeed slice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+print(math.pi)
+def find_max(lst):
+        return max(lst)
