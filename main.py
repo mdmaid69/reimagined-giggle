2cfc4cd9462808af62c18634a19b97f2a9ce3554
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:20:08 +0000

    disturb band bath disgusting fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def multiply_numbers(x, y):
-        return x * y
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
