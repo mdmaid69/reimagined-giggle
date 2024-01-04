78601bf31bc3e4f25d377b62d5d0878f86971cba
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:03:40 +0000

    quality telephone stage clear recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
