21ffbe70f5de55c0156441a1157d8f3a914d7b10
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:09 +0000

    mean half difficulty lead ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_user_string():
-        return collections.UserString()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
