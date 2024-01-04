dcac233862fd57a23266a123dac89dad14723bfe
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:16:40 +0000

    dark doubt over blame test

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def divide_numbers(x, y):
+        return x / y
+import os
+def remove_directory(path):
+        os.rmdir(path)
