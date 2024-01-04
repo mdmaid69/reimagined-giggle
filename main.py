4d5cdf45f8cee0ffaccc1907a2e78fe37ea473a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:32:45 +0000

    giveaway trial suck scale cheap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
