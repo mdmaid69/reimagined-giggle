ecc0fb03d94aa65a829a5ae4e3e4431ff46288b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:33:16 +0000

    week outside thumb flavour describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
