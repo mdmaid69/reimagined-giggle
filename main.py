3483896a329ea84101bdfc817e09dc4cd43118bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:56:18 +0000

    contrast nice tongue my reaction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
