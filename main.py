b63f33c07fd5a368f721bb5aea1ad76f293742f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:06 +0000

    purpose comment odd impossible shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_dict():
+        return collections.UserDict()
