723ce7511b1b451f7f33f19d61920ca6871837d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:23:57 +0000

    dull traffic hers skin immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
