dcd78114fb544148e6726a6a6791842e045b720c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:37:23 +0000

    before service burn book tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
