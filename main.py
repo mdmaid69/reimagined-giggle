67c271bb6fd9292ee2767e1d2d732d534dadd054
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:52 +0000

    loss sister thankyou situation everywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
