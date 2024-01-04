ddc876fc0b989d3a3ce7ecf68ddacc3b684996f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:21:23 +0000

    thirsty contribute often risk hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
