49c2d130b147130140157727704361c226915676
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:23:26 +0000

    ride impression crash firm breath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_str(array):
+        return str(array)
+def convert_to_hex(n):
+        return hex(n)
