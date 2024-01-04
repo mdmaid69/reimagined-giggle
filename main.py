0d90391012e197e831424d2eadc781f86edb6eda
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:53:10 +0000

    sticky limit tonight belief drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def cube_number(x):
-        return x**3
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
