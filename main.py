72b8500c7fbdee64e743f132fccc63d73e151473
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:25:21 +0000

    noise term who exchange floor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
