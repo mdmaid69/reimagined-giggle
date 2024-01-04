0a6c79fd5863742407becd8d789019a97f4ebd65
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:23 +0000

    gas not club neat bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def reverse_array(array):
+        array.reverse()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
