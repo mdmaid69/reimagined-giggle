7613a87301326c6a0d93da5a037477bc3c464b3d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:47:08 +0000

    drink pity punishment then anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sys
+print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
