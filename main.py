d6028395a873c86875598adc020ba831e5a96400
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:55:11 +0000

    disagree vanish forward move word

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
