9f90eceee2cdc451def5148ac08d68443553069d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:54 +0000

    recognize heavy crash grateful forward

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def subtract_numbers(x, y):
-        return x - y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
