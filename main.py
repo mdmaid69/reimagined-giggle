eee39112bca12883c479441e0d2cab3130bd955e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:47:52 +0000

    extremely quantity contrast substance own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
