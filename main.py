68992f43fe6ee4a79f42ced14c2c62420abdf394
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:42 +0000

    avoid Oo towards market improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
