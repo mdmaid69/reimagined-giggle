71ea6f8738c5b249231301780d324b0ba6d9de2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:55 +0000

    start dull pleasure effort attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
