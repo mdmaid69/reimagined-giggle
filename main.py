a390b3beb4c671874a4ee6aaa9b7e9e2a0eca239
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:14:04 +0000

    left fast impatient couple lead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
