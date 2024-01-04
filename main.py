ef7b4344c00729fe42ebb558089122beb2d71028
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:55:46 +0000

    aware breakthrough takeoff actual table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
