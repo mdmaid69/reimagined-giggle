083524154687924adc4bd54dec65fea939bad5f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:53:02 +0000

    back behaviour base interview mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
