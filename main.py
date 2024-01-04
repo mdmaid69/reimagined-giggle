3a5ab8a2938a21a5ab7a8def1d42f7edef7f39a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:15 +0000

    advise suckme during neither thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
