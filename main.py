6145efc7799a656dc0451c2a2f76c198ba2a6352
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:32:43 +0000

    quiet discover spare holdup ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
