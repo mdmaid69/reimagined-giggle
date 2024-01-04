389d3ccbb2dea0628e9b259e7910a6502925d2e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:42:08 +0000

    frighten mixture shape turnout turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import numpy as np
-print(np.array([1, 2, 3]))
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
