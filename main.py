ee4dcb086e42870ebf029267e029a3de312943a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:09:46 +0000

    family tune success divide bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def convert_to_binary(n):
+        return bin(n)
