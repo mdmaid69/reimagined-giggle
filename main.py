783865c254610d60c76487c9625a4f2ce08da2a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:15:35 +0000

    type approve sort glad detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_str(array):
+        return str(array)
+  def cube_number(x):
+        return x**3
