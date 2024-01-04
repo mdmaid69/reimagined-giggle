738d90365e257b226266abd942d737cbdd3a9557
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:29 +0000

    team season lock juice influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
