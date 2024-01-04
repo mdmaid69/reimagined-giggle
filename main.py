4c2d80a576a300c7cc2c4485eb3b51d8c34a47a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:57:06 +0000

    plenty confidence slightly feed glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
