1b7238ae5d756f0e270399d069e08eeb03372661
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:58:11 +0000

    neat score boat size reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
