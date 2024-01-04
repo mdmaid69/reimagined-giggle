6a1ba2a4ed621d7eaed44b299957909f29508e58
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:52 +0000

    apart smart false silent tidy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
