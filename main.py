7b41c1e9b55ae86b00aea2a7705632e010125ca1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:42 +0000

    raw shoulder operation nature practical

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+print(sum(range(10)))
