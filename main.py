4e83f11ffc5919497c20ff2d7d4a9517afdd9eab
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:07:06 +0000

    blow telephone successful spray mind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
