551e6295f23113a07162e267d465aa91ad5a061b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:01:41 +0000

    in explanation dangerous invite hate

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
