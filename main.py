2bdd9e7b60a98cc80dd689579681e4f3d6e8a184
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:02:32 +0000

    leave thorough support mail separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
