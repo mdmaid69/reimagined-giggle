f581386ba9324f0d6d840ac2817dd8d8f20641d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:15:09 +0000

    tired pile absolutely scrape sing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
