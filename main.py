3bc54f78be42fcc70bd202d4b0f44801764703a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:35 +0000

    character let sleep second through

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_work(force, distance):
-        return force * distance
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
