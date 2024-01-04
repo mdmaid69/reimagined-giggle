76979c1e7f8b0e94d38132cb149f272631f78517
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:08 +0000

    blow unhappy office replace disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
