2c9d1e74aa23387f28da103a2cc6550d6d0310df
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:46:12 +0000

    move march live region chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
