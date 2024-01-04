4bf82c17afeb0e2d55305410c409601665a9465c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:22:52 +0000

    into touch shock wet when

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
