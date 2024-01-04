5dae25a8af6a49af2fcfd35ea1358c9158efada8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:56:56 +0000

    exhibition lookup sport back even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
