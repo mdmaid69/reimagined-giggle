c35360573a1b0eba23db481b3271d7e9247a6717
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:27:00 +0000

    apart down term soil all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
