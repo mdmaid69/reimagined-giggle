39f053c74190cf5257da1ce526a239918069942c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:27 +0000

    tape duty will song while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
