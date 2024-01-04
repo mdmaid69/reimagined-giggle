315bb61cfb1b6e350743704d2213b7227fd53943
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:55:15 +0000

    comeback boat may declare deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
