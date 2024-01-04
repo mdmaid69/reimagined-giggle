c8d7cbb8a4769507be29592db92f5d6a1aaf41fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:31 +0000

    contrast welcome book people left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
