5b565be0c86f973b3a49169b3908b5cdf043b13f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:51 +0000

    class hungry sting so it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import sys
+  def get_python_version():
+        return sys.version
