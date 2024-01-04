7eecdda24e8c080f2925bb4d1731a8df599206a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:30 +0000

    journey influence protest far likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_repr(array):
+        return repr(array)
