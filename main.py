cd2dfd0db1b624a45f656303039a12a41ac620e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:43:31 +0000

    birthday cry 179bite head continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import os
 def get_file_creation_time(filename):
         return os.path.getctime(filename)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
