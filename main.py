af4d92512bfdf522b342b200ffb3c2e1ba2ccd6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:24:33 +0000

    steal steep dream dear equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
