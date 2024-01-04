7ffb83e1857642400aac311a6e6f493b94811bb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:19:48 +0000

    deal patient almost popular police

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
