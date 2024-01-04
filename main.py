3cc817e199171bc72f477fcdb89899b0e86a76f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:20 +0000

    life slight watch comfortable usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_current_directory():
+        return os.getcwd()
