1b0e91262669bbc1507c6df05f81b6b4230415dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:27:02 +0000

    hurry effect prefer let OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
   def get_base_name(path):
         return os.path.basename(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
