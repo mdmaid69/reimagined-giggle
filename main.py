9d5def6c40b4cb5ea510655e56a35c29a8d08758
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:17 +0000

    lend relative run willing while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
