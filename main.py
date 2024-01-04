a596b1b503964d1915030f165e2767551d244c79
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:52 +0000

    tight pleasant long drop tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
