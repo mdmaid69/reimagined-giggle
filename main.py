a4f255f88facf17dd95a1d5e945651b0a4f96241
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:47:30 +0000

    keepaway catch brush Tt stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
