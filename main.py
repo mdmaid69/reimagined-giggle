7f0cf7b1f72b849956a0fb2be1a74fe0d07bda68
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:44 +0000

    borrow treatment goup murder system

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
