aef834ce7b38f4f3335c12f664b12e2566992875
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:11:22 +0000

    duty weather sad nurse depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
