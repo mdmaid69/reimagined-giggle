97611bbde9f46d58d3155603147b0067ccd5d4d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:38 +0000

    noise schedule pronounce workout production

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
