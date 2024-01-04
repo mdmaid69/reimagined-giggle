72992be2ee79e4a49b23b85b6b1b080055c357fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:38 +0000

    leave ask keen takeout nice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
