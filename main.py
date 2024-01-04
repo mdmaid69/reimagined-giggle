0b1f305765bf2ea9fef8c504a6efe9df2852dd78
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:22:02 +0000

    quantity stamp dick depend positive

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
