7aa968e13bb31072fe24e58d20ca03588e2488ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:26:27 +0000

    up power cost quiet last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
