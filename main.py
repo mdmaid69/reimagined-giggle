feabc148b1676d3975b089f2906c3396a0320ae1
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:36:28 +0000

    shine view table sick replace

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
