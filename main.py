75773a951f3e5350081c731055169ecdbcf2ee25
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:35:45 +0000

    as trip gentle smell advanced

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
