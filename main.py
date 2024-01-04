c65c61d8be74d9587ee82d60bb583056e9f619fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:12:52 +0000

    seem stretch Nn practise first

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+x = 10
+y = 20
+print("Sum:", x + y)
