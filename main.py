19b6b1f7501b75f35aa833527734f4533923207d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:42 +0000

    learn among drive fasten cool

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+for i in range(10): print(i)
