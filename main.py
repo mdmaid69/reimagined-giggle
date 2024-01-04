02ba76fc79e9f6227cf003a5da38bd7a8a4cefd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:01:15 +0000

    doctor floor understand again feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
