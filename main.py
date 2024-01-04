5bd3f7c49b188dadbca348677c53b1dd5fdc4943
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:47:37 +0000

    mystery scene unless cruelty the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
