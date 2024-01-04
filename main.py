12344ce47efcf672d1acd21fea4bc5036cbebf8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:44 +0000

    stand while use somehow development

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
