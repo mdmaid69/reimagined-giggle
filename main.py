7600aa689596f8c980e05370a51eb2fe0beb434a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:35:02 +0000

    slightly Qq later bad fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def add_numbers(a, b):
-        return a + b
+name = "Python"
+print("Hello,", name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
