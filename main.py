110c2b12779445194a654c301a8eb5d6f3f66fa9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:46:08 +0000

    conclusion machine eager niggaacademy communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
