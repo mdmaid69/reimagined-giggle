192e3f7a099ea3762a3d0ecdefaa80156b3d6979
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:32 +0000

    kind spring state period ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
