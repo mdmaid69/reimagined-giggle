65cd575c9783b09e718b77205304ff2508f4883b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:17:41 +0000

    winter variety exactly year estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
