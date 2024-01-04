9729c921c1adf6a54d3f018efba267134ce8ad2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:27:27 +0000

    style heat slight loan copy

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
