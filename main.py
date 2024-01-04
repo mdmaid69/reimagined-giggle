28555f71581143a8c4ea17544362ffe9d3f1c430
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:20:08 +0000

    mess passenger any beyond condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def convert_to_octal(n):
+        return oct(n)
