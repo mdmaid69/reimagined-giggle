26cdf69daceed87163cffbe6702b226e361de9a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:22:58 +0000

    comeround find back deep leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
