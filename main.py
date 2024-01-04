d86a50e609961501403f9dbd91c70e468c58da12
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:34:24 +0000

    legal under yes fasten relief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(numbers):
+        return max(numbers)
