b121d6c43f49f5b4a490296ceae1c8c1bee85f71
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:28:27 +0000

    yours control convince borrow number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
