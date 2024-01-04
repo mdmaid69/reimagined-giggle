2c2318d76322c9f08d23afbe9b627b20a43ad3c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:57 +0000

    middle admit meaning maximum everything

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import numpy as np
+print(np.array([1, 2, 3]))
