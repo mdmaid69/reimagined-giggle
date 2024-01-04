4166cc4ee7275a91d2f4a66914a8f19b0c333e91
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:18:56 +0000

    Yy dig thinkover her today

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_length(array):
-        return len(array)
+def reverse_string(s):
+        return s[::-1]
+def calculate_work(force, distance):
+        return force * distance
