5e242cfd59df1c1ef1ef796984b8e2c6dccefa20
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:24:19 +0000

    sleep will suckme Hh battery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
