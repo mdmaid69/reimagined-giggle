40ca5f3e3dec1511666419aab5bf6294297d831e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:52 +0000

    disappointed vision advanced problem 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
