10ee7d9d0211765f55e487067b4c26f7c949647f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:29:39 +0000

    street spot he float crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def count_elements(lst):
+        return len(lst)
