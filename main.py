415a626ea1c948f4556f849f7ccf2bd096d1e1cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:51 +0000

    number close want profit confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
