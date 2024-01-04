25fbe8aef0fadfcc4a0bebdbfe126f04311d9203
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:42 +0000

    colour right holdout holiday regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_priority_queue():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
