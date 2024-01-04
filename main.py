c490d2bcd546085f48f9338ae3c1638fe7b95724
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:29:55 +0000

    major start comeup cap variety

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
