9392b2398feaddabea71a4cd08774528288622d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:02:10 +0000

    invite discuss ignore tour finger

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
