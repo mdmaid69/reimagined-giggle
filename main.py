c8e99d04e4767fcd8a4e451106dd357d38da08a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:07:36 +0000

    takeout score swim load wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
