3412106a42ce3060e978a448116c521021688b1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:46:57 +0000

    such fix out floor contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
