e12fc8d47f8e53207ebbbee037e08e4d13e1a1b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:32:06 +0000

    want business turn little television

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
