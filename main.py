631f8ff842c0487643d00a5809f42b8806e872ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:03 +0000

    excuse advantage do space before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
