eeedbe8eaccc2c109d9ef35ca8b783d489866545
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:02:28 +0000

    such thinkof effort command work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
 n = 10
 print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
