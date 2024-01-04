f3f9b0e574ebda00be0f70c88b0993601630bacb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:24:02 +0000

    pump zero tongue ago world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
