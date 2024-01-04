c46335e236db06ed9a3e15c8c5d1282d90068ab5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:52:21 +0000

    neither war regard comfortable defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def convert_to_octal(n):
+        return oct(n)
