c5f13493133578b328ce35ab736247fce87abcc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:04:31 +0000

    previous somewhere last weigh light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
