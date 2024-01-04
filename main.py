95cf506ea43ff522962fbd2e41786c7dc87e4c22
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:09:19 +0000

    bend foot guide promise understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
