646e8b50830b3fe0d205553dd1dd0e13d504b91a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:48:26 +0000

    relief warning prevent service show

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
