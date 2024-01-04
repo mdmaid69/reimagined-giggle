7d33e1a8ce8f6682788ea3d9e4fd74dc6f002fb0
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:17 +0000

    busy complaint available this calm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
