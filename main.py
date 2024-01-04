1a524b90af720e1f76b85474792b96c1d1d6f0f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:57 +0000

    argument 566end emotion set scale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
