758d22cb9aab6aec7099e13c7d4370b861418c8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:30:41 +0000

    school production steer female shoulder

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
