1fff57af4b28a714f72484c896148627bc736d79
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:01:50 +0000

    draw responsibility properly point path

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+  def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
