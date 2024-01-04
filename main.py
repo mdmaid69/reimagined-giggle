4af8c946f18acbcc0d47a307405adede1e71409b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:01 +0000

    odd life past wild happy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
