0e8ea58873169b07092c276809ba0a98d27eeb9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:33:17 +0000

    out grow question rely achieve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
