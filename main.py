13ecbb9507656d70c04d30868bfe6e09c16c32f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:23:08 +0000

    holdon worried car some minor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
