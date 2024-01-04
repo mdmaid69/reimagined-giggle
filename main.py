0ddf093580b3d4ecfe48718d59efc9cd19b5c628
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:09:52 +0000

    its put influence experiment genuine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
