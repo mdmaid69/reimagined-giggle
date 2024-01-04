cac4b8b727626fe97410f310c2667079c64df4fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:09:46 +0000

    set then friendly section complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
