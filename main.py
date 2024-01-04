70fcacfcf27f3dd8ceea141aa164da3fb9d7cb48
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:14:45 +0000

    loose light admit attach figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
