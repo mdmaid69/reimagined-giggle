e1b9ca2a0cc34ed3f5cf6fdfb1fe3a79940f9b88
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:47:31 +0000

    last home nigga invitation slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
