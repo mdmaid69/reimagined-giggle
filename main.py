534b54fe24855bf11f79afd25be776d9d0877b2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:08 +0000

    turnback congratulate sort as giveout

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sys
-print(sys.version)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
