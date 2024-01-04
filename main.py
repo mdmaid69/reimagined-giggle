46e6cf202e84168abca99d40779ab71ead5230f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:15 +0000

    compare better inquire easy store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
