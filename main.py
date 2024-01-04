a8e59443bb1a2f8792688a5b5e3526ba7ecd73ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:24:51 +0000

    roof Cc circumstance comment after

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import random
-print(random.randint(0, 100))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
