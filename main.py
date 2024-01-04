3e5cda9b95acb3a0d6e8ec5e8c01827b331a2e35
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:10:44 +0000

    no voice now lesson steady

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_error_function(x):
+        return math.erf(x)
