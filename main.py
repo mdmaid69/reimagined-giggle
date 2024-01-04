43ff62b0d7b3cccf4c391ae677e5a895679f6a36
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:27:14 +0000

    temper frame rise make temper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
