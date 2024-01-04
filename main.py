eca35c580d22dc1c2f25dc0fd90f138880217176
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:18:07 +0000

    advise provide action impressed long

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
