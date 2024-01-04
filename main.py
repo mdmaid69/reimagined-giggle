a5dfa576394bc46abe920f4756f0b4a8963e32b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:44:18 +0000

    cry press pause judge moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def add_numbers(x, y):
-        return x + y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
