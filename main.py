6381aa1cadbff6d342b8d346ec0c86e98b3df841
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:15:14 +0000

    law depend equal bury share

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
