a86919f50a866458d1457ba7fc7605bf0535574d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:44:33 +0000

    comeround provide flavour challenge left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
