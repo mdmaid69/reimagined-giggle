71955e5e61a3fdd282ac7791fd295c86d99f6b2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:23:34 +0000

    human part scrap attractive peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def count_elements(lst):
+        return len(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
