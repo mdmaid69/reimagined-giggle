1ddbec58eb9e116e9468a6e362cbebd66a98de15
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:33:14 +0000

    both heavy contribute sometimes violence

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_bytearray(array):
+        return bytearray(array)
