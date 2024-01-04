1bb0f83e4e8add0fd4837cac2b7e1d37c9bc9bf3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:29:04 +0000

    cope prize react earn apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import os
-print(os.getcwd())
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
