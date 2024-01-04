86a294931b77813b59eb9bd0132add6c2f8f618f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:58 +0000

    defend along uponon married target

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
