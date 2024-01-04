5cd64d0815b57171411fdba608b51c47218a723e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:31:44 +0000

    room tomorrow learn uponon fierce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
