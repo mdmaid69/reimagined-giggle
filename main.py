02662a41e38c0a464033dc74384a124d46af70e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:44 +0000

    knowledge afford switch odd dress

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-for i in range(10): print(i)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
