75efd30906748f869a52553d4cc151826119a6fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:13 +0000

    section quality keepoff OKorokay pride

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
