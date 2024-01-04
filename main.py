063d3c4571df3cd5cd1c752c4f157af87d86b014
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:31:37 +0000

    choice nerve comfortable successful inquire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_interest(principal, rate, time):
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
