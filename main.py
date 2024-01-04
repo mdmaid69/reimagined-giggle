c96b40dfc36fb8405264593f1f16562f21441336
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:48:45 +0000

    cause become public situation manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def is_palindrome(s):
-        return s == s[::-1]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
