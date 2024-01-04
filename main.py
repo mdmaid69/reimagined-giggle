0ecd6086e76ba9e8bd7255c4cf9447fcc74a181a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:59 +0000

    keep fall invitation support capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
