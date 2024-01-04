cf67058b3dac72e2da02d05eebad37bb91e353a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:20:41 +0000

    favourite frighten battery head question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
