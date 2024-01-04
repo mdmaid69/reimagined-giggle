dc078e6a03e5f3ed4e5eda4d8ffbccbf68ed827b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:42:47 +0000

    completely front tradition Itself noisy

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-print([x**2 for x in range(10)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
