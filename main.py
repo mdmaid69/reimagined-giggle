ed8ab9619aa3dfc74d9c703adea85386322eb5a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:18:31 +0000

    drink alone lend transport below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
