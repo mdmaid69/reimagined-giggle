18db3b6a94cfb57cefe6672889ab931a0bf08ac8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:41 +0000

    if computer purpose downstairs suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
   def get_file_atime_ns(file_name):
         return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
