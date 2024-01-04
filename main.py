09bdd473437708c4dbf2ae2b156af9bb482837f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:04:32 +0000

    breath 567enemy effort official thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
