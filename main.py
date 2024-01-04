32dfcdf9c3cc0f6bccac40bf30710f498ee97467
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:39:22 +0000

    poison goup lookafter butter rarely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def sort_numbers(numbers):
-        return sorted(numbers)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
