ed5c9c320786eda0de2bd8e7077c3b64ea4d3190
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:58:59 +0000

    Gg successful pencil exam main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
