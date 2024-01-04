4a46f6306b6df0c326ec2a14ab1e8aa1baf348d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:10:43 +0000

    whenever process bet married politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
