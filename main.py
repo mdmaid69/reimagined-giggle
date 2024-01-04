3c595bc4aaee4a8da4468f9f35b1e84879e58923
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:55:40 +0000

    first you difficult advantage by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def get_array_as_bytearray(array):
+        return bytearray(array)
