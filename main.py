5d65b95390c8a99666571e8d7512dc01b80466fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:36:53 +0000

    keepaway wise embarrassing sport television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_max(numbers):
+        return max(numbers)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
