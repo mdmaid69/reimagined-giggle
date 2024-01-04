fe7fac0a29d2e75b2956677e4dc0510f43e838b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:37:01 +0000

    part mark brush getup mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_string(array):
+        return array.tostring()
