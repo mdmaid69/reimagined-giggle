038d51449900aee2686c782d1e3c53a43ec7bc33
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:56 +0000

    nerve cover fork short from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
