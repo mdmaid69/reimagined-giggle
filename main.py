bcfadf87b14ab2d0c7d39ec390be84a934dca0e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:36:38 +0000

    dull married near rank statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
