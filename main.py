5cd23f2e00e12ea8b06730b74a47017cd0db27ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:28:52 +0000

    show lesson moral sad risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
