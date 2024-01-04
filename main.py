d0e1a387200e9967c3c2ff9f3b383fef88504152
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:51 +0000

    dick talkback continue no owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
