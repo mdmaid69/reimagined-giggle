485543a9171a8f1aec20bcfe03bf4ffa1efbaf09
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:14:03 +0000

    rare unless dream any ought

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
