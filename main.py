ebc97042e2c7f52cc6165802765140a5d6308339
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:23 +0000

    pleased hers deliberately hang underneath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
