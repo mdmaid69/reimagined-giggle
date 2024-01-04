7a805f03589877bdf116367aed9734b34c69be2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:12:19 +0000

    keep discussion autumn grow bringdown

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def count_characters(sentence):
+        return len(sentence)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
