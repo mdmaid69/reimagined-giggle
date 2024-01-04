492550274c77f651d560299b28a46dee43a4dbca
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:53:47 +0000

    lead argument burn tape out

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def reverse_string(s):
+        return s[::-1]
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
