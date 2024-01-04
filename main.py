432728c6c5f1dd2fabd46cd6563e51a1c843e21d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:36:28 +0000

    post him few news plenty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
