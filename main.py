c1bafe9e9e3dc9e4ca6610c0e9dd6e63f0614666
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:41 +0000

    rest subject comment comefrom tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
