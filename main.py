6635384c146d768a11127d583206ae556b194e60
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:58:09 +0000

    unless sentence off claim lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
