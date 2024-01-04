6d49dc58e27708f836ab487b060d03a2b69d9688
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:20 +0000

    failure they independent shine mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
