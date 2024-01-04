807285b47673fdd052ffcc40fbcdded32f99d1f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:35 +0000

    live tune runaway card brief

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
