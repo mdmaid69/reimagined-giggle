62abd61f0ef696f2b8c9cda1561521dd9174cdce
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:03:57 +0000

    combination arrangement press bite better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
