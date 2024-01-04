974c08dc5f1d4bbe1d6b95202892bd5896c18f0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:37:00 +0000

    Vv year today gentle offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
