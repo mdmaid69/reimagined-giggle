12485d044f27ea6221666281d1eee465c2e5e2a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:16:27 +0000

    right tour after war trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+def exit_program():
+        sys.exit()
