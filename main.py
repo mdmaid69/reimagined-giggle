56099e519b20fd1e3a82f2ce675eb110ee49aaec
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:08 +0000

    Vv more decision hard introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
