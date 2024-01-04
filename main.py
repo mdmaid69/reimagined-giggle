24219437370ca2d029d71d16a908221887ffd256
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:04 +0000

    surface except secret measure wet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
