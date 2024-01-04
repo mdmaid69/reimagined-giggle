2883f4fe05fb4cbeee14733feacffb0a9b51c04a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:42:50 +0000

    demand land present crime various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def append_to_array(array, item):
+        array.append(item)
