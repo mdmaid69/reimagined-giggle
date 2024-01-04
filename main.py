6d93ed5993a7314fa6fb27bc49a3e3527ca460bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:41:54 +0000

    lookback(on) safe cheat once sensitive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
