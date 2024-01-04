7d9d78563f1587bf60d5080bc75e607e8a1445b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:33:49 +0000

    tear answer offer trade clever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-for i in range(10): print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
