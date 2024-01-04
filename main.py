848224f2251b3159351cf39a2428a6a11c8ee245
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:26 +0000

    willing finish like good quality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_int(array):
+        return int(array[0])
