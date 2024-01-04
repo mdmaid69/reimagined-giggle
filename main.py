d93305ed63b1674fdd8baa57b6af009fb0ab90ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:25:34 +0000

    freeze myself trick earth own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
