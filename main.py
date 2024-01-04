d7aa80249fd40123f5a06658909680c69f5beaf6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:51 +0000

    possible crash dig draw report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
