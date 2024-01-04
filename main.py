5db38fa4388f2af2e3991363e1358630b9484d3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:18:28 +0000

    recover powerful reply put depend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def divide_numbers(x, y):
+        return x / y
