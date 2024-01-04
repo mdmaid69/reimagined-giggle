f0632ac50fa686313d3c69e93d139b923ab57f9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:13:16 +0000

    promise solve sport violence round

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import time
+def get_current_time():
+        return time.ctime()
