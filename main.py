9d7bea37388ebcb43a686898293a8ef09c3c0562
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:36:11 +0000

    separate Rr whom travel extra

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_array_as_tuple(array):
+        return tuple(array)
