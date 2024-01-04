bb2b16519a522f1d0db39072cf596af243e6f866
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:13:57 +0000

    impossible whichever while fault window

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print("Hello, world!")
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
