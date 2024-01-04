4a2d326d2416e8f3b3f1467af43584b097a35082
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:31:39 +0000

    correction hurry throughout what over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
