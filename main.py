806959f1c626d5384f28e881ac09a125eff17793
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:54:58 +0000

    hardly damage square belief month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
