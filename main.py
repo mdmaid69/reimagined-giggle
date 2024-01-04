da5fa518e629df713a9c049e91aafb14ebdcd668
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:04:28 +0000

    offer negative past choose break

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-import math
-print(math.pi)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_density(mass, volume):
+        return mass / volume
