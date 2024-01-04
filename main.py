beb2db1a3873c86fc9c766b2b820b31c93ad6042
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:50:57 +0000

    this agreement side connection blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
