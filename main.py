e0b245237c7927d0361061010858aa127dc61065
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:11 +0000

    old loud everybody report scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
