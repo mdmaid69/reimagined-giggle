3ac236e371d5273097991140c1df1edab2ec57de
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:09:24 +0000

    positive naturally hour expensive bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
+def reverse_list(lst):
+        return lst[::-1]
