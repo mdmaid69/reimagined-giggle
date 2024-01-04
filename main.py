7c949277a1c042f2a6a91f5161ee7d87e9f289f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:25:30 +0000

    ilovedicks communicate bunch write child

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def cube_number(x):
-        return x**3
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
