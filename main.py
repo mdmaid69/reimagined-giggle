77a841860f101848cbaa1b4fe81c4927d226ee52
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:49:35 +0000

    deep sort choose bar backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-print(math.pi)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
