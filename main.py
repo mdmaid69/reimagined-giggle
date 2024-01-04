12df67a92b164b923cd8b260427d113720720ded
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:44:02 +0000

    buy mine able roof protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
