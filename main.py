a892ccf3d48e26a380406d0b67ed0c407a0bc475
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:11:31 +0000

    load accept honest bet finger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
