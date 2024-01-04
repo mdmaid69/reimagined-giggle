5f208608b6ce46635f5b1a4935c7470ffe377161
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:49:29 +0000

    stage big record confusing soak

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def square_number(x):
-        return x**2
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+print(math.pi)
