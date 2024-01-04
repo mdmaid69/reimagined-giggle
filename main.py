4b17fcd6613035a12ce4513867838d0ee736b7cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:00 +0000

    brain indeed large relative deserve

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import sys
-print(sys.version)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
