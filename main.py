c4ecc90f9c9a018cc85e189a8a21d49c02c34cf3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:58:08 +0000

    and stamp tie sound forgive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
