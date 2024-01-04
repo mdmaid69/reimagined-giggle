21b3cfe003c11542311126105ffb6784457ec730
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:39 +0000

    run bright play either joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
