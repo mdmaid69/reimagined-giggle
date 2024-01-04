86fdd07bf3ea6643af7d1c52b0e5e683aa676a17
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:37:20 +0000

    separate support float none crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+print(sys.version)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
