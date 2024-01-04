4ae956c892b4d50506cea0b88a31420883f5f2ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:32:28 +0000

    home future hit petrol level

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
