7e8c57598a55a8221c27efca33d02ba7a0ffc21a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:45:39 +0000

    spoil understand store healthy fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
