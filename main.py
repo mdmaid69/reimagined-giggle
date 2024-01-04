8a488c232193c6fb1199c5e543ea1caf877e7069
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:39:02 +0000

    pleased square platform surprising clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
