51a5292eb9dc3a86e4309236be2c8e18c18256b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:45:15 +0000

    wash else lay murder smile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
