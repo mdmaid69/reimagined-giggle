ce7e4c91a7d494d1d26942e4caa7862aa020a038
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:54:00 +0000

    age regular scene joke by

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
