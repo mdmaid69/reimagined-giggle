d551bcf0e46e74e3a3606e5d9becef9cc2550624
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:58:06 +0000

    anywhere design hot club cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
