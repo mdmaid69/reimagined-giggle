8e857843525c1695825959d0ee473c4d5347e5ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:51:31 +0000

    tonight plenty mistake soon hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
