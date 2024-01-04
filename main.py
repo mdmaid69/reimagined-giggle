60d375350a0450121376cfda9cb923cca85ac4a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:10:14 +0000

    underneath block table design open

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
