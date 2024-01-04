d186ecf574a7f411e6f3cc2143e8718ad360ea89
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:00:12 +0000

    heart dance hide doctor force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
