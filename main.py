4ea58ebe94b8a9e60b09449682271a0aec155143
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:47:19 +0000

    true unable book rich mood

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-print("Hello, world!")
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+print(math.pi)
