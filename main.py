66b3acf2169be77e603b4183ce11b7f885718ad5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:03:01 +0000

    plan most become noone talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
