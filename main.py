75ca722bf7d330771686b9831c0f41e248a955a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:04:24 +0000

    again introduce opportunity mine bill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
