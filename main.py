9a20b318dbe5f686eddc602803b826c4f09b9a68
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:27 +0000

    closed lead vanish search sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def count_characters(sentence):
+        return len(sentence)
+import sys
+def exit_program():
+        sys.exit()
