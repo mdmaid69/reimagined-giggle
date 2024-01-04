bf25d6009e0200dc17e5603eab2bc6bfd216eba6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:13:29 +0000

    fire same crowded prize up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
