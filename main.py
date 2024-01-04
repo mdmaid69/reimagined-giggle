bfadc061ae5947a84c493ae334cf1003286e28f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:31:18 +0000

    Qq public comfort shop holdout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
