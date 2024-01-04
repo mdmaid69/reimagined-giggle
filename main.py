7bef002668549bdf37971e174aa04205cd6af7b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:12:53 +0000

    hurry movement day somebody emphasize

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-print(math.pi)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
