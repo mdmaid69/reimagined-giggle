3cfaf7e1d6735f8b5160e58b7a0a0a447a98f93c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:03:13 +0000

    world geton straight convenient mean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_work(force, distance):
+        return force * distance
+def subtract_numbers(x, y):
+        return x - y
