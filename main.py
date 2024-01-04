af8128396f4c17662155600caa6b0125dbeed355
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:59:47 +0000

    reduce rate giveup last manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_sine(x):
+        return math.sin(x)
