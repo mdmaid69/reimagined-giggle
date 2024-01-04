ae075a1f4f67ca9fd8e69e4a1c05064ba17b99f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:24:09 +0000

    getoff pick appeal extraordinary tip

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
