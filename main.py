8a4aa08205cd8d349f2d29c6493f184a22e677f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:33:07 +0000

    birth process pleasant pour strength

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_counter():
+        return collections.Counter()
