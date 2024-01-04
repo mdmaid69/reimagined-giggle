108b4a810f404d4642f3a449901af6877be8cc0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:11 +0000

    name row trade tap task

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
