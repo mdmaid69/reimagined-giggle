7552cc4ef41e361f3b3e556aa04d2138ef188e77
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:35:29 +0000

    bank anybody kind side back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
