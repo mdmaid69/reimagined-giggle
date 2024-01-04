7ba3713a7251e086427ec31b65bd5f633c46c4b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:19:16 +0000

    curious or but illness measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(a, b):
-        return a + b
+  def get_current_directory():
+        return os.getcwd()
