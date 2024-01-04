fb40bdba6f878c9587bbb15b7fa406675405c9d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:51:32 +0000

    flat embarrassed blame kind reliable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def get_array_itemsize(array):
+        return array.itemsize
