7c1cb06a40ee3f5ce3926e1610e949a0fdf93a77
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:00:21 +0000

    probably although ocean please worst

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
