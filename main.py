d385016b95175c00cb27795d0b6bd8c517eae4d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:33 +0000

    season shoot idea again among

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
