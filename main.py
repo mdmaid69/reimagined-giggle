c3d1c3db979397e37ffab1cf49c399b51b3d5a96
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:08:58 +0000

    present find only meaning production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_power(work, time):
-        return work / time
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
