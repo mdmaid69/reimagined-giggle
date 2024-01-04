41ebf0f98f879282627de2328b1abeccd9f973bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:46 +0000

    near previous bus fetch inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_bool(array):
+        return bool(array)
