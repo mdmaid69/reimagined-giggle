0ca513b5101239745758bf161b942cf26b46b4cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:13:49 +0000

    basically watch responsible report chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
