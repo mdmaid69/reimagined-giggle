aa0f5ef57190c5cd2a347e07d6f44f20d183eff9
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:44:49 +0000

    telephone we circle run somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import sys
+def print_python_version():
+        print(sys.version)
+import os
+def change_working_directory(path):
+        os.chdir(path)
