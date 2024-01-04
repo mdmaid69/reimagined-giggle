1207a91372076b49e8599f44bb167576646d8633
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:47 +0000

    act hurry increase free heavy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
