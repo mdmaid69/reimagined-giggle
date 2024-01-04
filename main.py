c880d7f173123c4351a922f74382dd85a4de3148
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:02 +0000

    heart genuine silent storm dish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
