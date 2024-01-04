b0e34b36c5a90a2d2f7711e27b610c27285d16c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:14:03 +0000

    refer for callat along hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
