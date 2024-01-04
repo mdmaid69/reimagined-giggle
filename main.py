9765fb7a628b42f99e03d7aae3227b2c7a03373a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:55:47 +0000

    although improvement reject pile limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def is_odd(n):
+        return n % 2 != 0
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
