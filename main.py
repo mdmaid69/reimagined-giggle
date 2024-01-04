3afd7b463644fa87a3f07c33c147bf9b95b02bf4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:10:39 +0000

    hold film talkback skirt wake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 for i in range(10): print(i)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
