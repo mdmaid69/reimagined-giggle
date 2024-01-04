6cc9414b2f7981c03167cb55afe0e30bac10e9e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:31:17 +0000

    shake payment check dry miss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
