03630d34b2b7c3862e737d6cc0bcbdb3c40dd8f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:01:14 +0000

    child long nature Jj steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
