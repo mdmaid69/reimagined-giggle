ba8ff54af57e5649e14e3e69e160cabc870abe76
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:07:10 +0000

    unable funny with follow pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
