e506f2e4e8b16edda2d9781bf6e9e7d7bea3cbda
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:58:57 +0000

    nose live exchange favour join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
