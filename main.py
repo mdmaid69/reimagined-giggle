e271497f9c23e0158eb5c4baff2db67747b56819
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:16:37 +0000

    classroom good task during dream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
