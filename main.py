cc0616095ea93fbf4eb96a3c7f6f76648ba72438
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:00:34 +0000

    example start term perform damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
