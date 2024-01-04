cc9c2f479090d4ee9ec2f09f65a4171a63d8f75b
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:23 +0000

    friendly Hh carry quarter spot

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
