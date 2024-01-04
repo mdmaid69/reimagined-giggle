fac7313d35a59fb8d913f29351410046fb4b9858
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:30:28 +0000

    unite excuse to exercise relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_list():
+        return collections.UserList()
