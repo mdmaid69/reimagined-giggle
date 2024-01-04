630be6732af2eb49c877e1c3f712f56059c91423
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:27 +0000

    school car our innocent connected

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
