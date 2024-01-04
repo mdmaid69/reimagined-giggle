02d3563d9a99b3bdba06a6acac23df6844fa7b6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:47:37 +0000

    city bound inside crash pen

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
