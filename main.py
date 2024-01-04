5029a5105ec449d6f6e52f52b6603eb9abeb2f84
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:25 +0000

    trip lose become else test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
