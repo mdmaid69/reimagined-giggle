7e44093b4f2482a418ca98d14d014600a3d9f3a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:01:28 +0000

    send content side clear range

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
