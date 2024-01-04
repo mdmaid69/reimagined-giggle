e17f80bdd921612286d41bc70412d90c473bca0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:50:25 +0000

    fear age past more about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
