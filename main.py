1bb41e64421f7ebbb9166b67f13251dae0f59c96
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:14 +0000

    sign shut priority thinkthrough mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
