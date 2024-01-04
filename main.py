8f8fb527b3c5e0a377e75c9d7f4ab69c69e22fed
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:38:13 +0000

    justice see centre consider invite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
