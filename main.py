fd6e90b6189a775f1f40713e1aa106f8561aecf6
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:19:20 +0000

    error shut excuse remember cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
