63417d9ab174201dfcec5f28a8ad69d3215d305b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:02:51 +0000

    skirt roof sound call Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
