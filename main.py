5033b8b6344442b3c89c378e3b1466645a6f54ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:59:09 +0000

    war waste suckme stupid serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
