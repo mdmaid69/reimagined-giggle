25d4e1b50f3e7a9d10e2259b5b6912ba13198d37
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:33:32 +0000

    cum priority better grateful takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
