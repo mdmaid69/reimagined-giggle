8533f62ab47130b641e96c155295cb42c1372e92
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:06 +0000

    dark heavy payment book dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
