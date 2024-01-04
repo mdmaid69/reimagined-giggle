b98c510b7d0f5b83cf4e6c16607b36eea5dd486f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:19:36 +0000

    secret to room breath tonight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
