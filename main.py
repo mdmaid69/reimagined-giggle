4dc49f64dfb147da0e06fb6ec71dc1e9f386641e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:44:49 +0000

    tour force plan class unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
