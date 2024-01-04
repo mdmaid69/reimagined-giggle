98a64de15f223b990953997db2bbc710849dd5f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:18:13 +0000

    communication mysterious offend backwards combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
