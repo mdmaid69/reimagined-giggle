a70aa0bad8a4ef606c688ad18866fa6dc4b8b411
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:24 +0000

    better scale bottom want secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
