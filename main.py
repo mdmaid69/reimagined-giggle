49a1e7208460624a7ccf175bb50dec51bfadd65d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:53:22 +0000

    world emotion cover late sit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
