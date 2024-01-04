c90f5c09347ba6f9751add87dfb28337dcd1fbef
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:31:26 +0000

    next soft art breakin far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
