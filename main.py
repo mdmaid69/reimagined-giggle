b2f7694da841f4d6fdbab14cd62b2bae1c1103a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:48:12 +0000

    pen argue simple consult argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
