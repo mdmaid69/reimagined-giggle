49c5c8f2127355d76402d9c48ed8a38fd8198e70
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:56:50 +0000

    make everybody score comeoff condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
