a9e1ce3253511b61b862305e84f9721e66bc46b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:38:56 +0000

    estimate pencil classroom discover she

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_rectangle(l, w):
-        return l * w
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
