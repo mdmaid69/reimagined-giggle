7fc84aeccccbe57028fdf00c899d689c64d12000
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:16:27 +0000

    reduction surprised temperature any border

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_binary(n):
+        return bin(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
