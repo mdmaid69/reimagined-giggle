e041698d0ed2349cd66063b948d8266402835518
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:49:50 +0000

    freeze giveaway strength shut tidy

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-text = "Hello, world!"
-print("Characters:", len(text))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
