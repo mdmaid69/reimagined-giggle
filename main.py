102564a729945416cc78c50bddc05bea23ce1101
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:02:12 +0000

    freeze total likely platform national

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def add_numbers(x, y):
+        return x + y
