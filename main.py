1726059934eb36d96ee94971cefd58678576f433
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:01 +0000

    cause flavour branch neither smooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def convert_to_hex(n):
+        return hex(n)
+def find_max(numbers):
+        return max(numbers)
