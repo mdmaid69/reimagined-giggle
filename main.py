773f0e60310da238a0aea8cd12cb6768c7c5fba5
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:00:13 +0000

    waste choose call moment 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print(sum(range(10)))
+import random
+print(random.randint(0, 100))
