ff28f7a1aa8297b6116e52812cd542e7884b88eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:59 +0000

    concern thanks exact at your

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
