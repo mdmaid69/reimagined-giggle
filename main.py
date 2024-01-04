b0f7b28409844d71f002de1c5909af6ce7396aee
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:41:40 +0000

    probably number workon cause date

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
