830f4db5447eca7036161586f0f625f653596566
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:44:46 +0000

    strike buy destroy successful type

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
