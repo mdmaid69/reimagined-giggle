4ebf7c0ff8d18bec1224bc4bb7b557db867c2de8
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:59:28 +0000

    form weekend computer trust judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
