d59a64e13fb19038c6fcf461bf58bf3915709952
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:25:11 +0000

    public less prove achieve tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_str(array):
+        return str(array)
