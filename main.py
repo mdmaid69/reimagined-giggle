169db565f7e9d9c3a336a93fee25884354d9d834
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:12:56 +0000

    objection try trip kick between

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
