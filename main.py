11d05c1c0e5d9b2ee25d7261f523c1ae3f816f7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:19:42 +0000

    rush his pain comeround name

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
