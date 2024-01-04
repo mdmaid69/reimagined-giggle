11f565593e45b6bd5abfe649e8c39105786894f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:40:31 +0000

    clean reasonable sharp eat rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
