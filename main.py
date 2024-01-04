6da35a9036167cab3e109ad1d0b8cb3a31457db5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:36:28 +0000

    accordingto cum regular education comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
