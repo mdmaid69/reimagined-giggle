50ea2ea8b382c717917b8917d0a24da0a67f9ae5
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:43:27 +0000

    direct afternoon class ever probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+x = 10
+y = 20
+print("Sum:", x + y)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
