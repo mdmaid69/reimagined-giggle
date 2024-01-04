3a2c95337deddc5211cc77703d7babdcee8622b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:53:01 +0000

    either makeinto lookaround communicate date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def convert_to_octal(n):
+        return oct(n)
