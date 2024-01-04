ee2c69a048f96ec631d7d6e3c8372b5bd29f0df5
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:15:50 +0000

    light noone treatment quickly away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
