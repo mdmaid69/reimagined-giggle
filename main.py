b6021029ac7de92f77da8e2c310fc95e150b5b3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:34:10 +0000

    impatient business stranger appear while

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
