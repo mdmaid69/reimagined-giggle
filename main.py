7af885d971e2885f80f36e61e7b9ff2db14a346b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:42 +0000

    trick geton achievement mood plastic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
