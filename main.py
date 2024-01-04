c3158c77441308fb796fcc59942c118cab944dde
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:37:24 +0000

    among evidence thanks song stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
