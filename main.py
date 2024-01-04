ea349ebfaa4f1f020a1882254723a981a3ee792b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:05:28 +0000

    ourselves sign straight must before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
