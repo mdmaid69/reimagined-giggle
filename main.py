e6a2154352651eaa281aebe296cabb4bbd0c7f65
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:18:05 +0000

    travel giveaway light formal connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
