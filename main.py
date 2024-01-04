86f1daf4286a54af1c35f5f0585a40fee71cbdec
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:30 +0000

    black himself stiff turn pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
