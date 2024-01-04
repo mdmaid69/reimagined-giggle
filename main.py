d107e89c0fb56ed7c1f6cbacfc5ad1bf77edc19d
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:51:13 +0000

    dance now model difficulty dare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def sort_list(lst):
+        return sorted(lst)
