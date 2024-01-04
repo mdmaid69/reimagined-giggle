499a8a537d63a6f43af5a3b14bab453355137eb0
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:31 +0000

    negative gooff such count skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
