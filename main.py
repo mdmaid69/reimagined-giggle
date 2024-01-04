433f55a7c2aadd02e5b60a8330f8b974c167b14a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:38:54 +0000

    set safety responsibility physical noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
