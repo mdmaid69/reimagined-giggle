b8b397382ead6274dda4d442e2eb9671c9f77021
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:44 +0000

    pump press scream coffee actual

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
