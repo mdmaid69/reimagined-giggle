2c637546b3f9f2d827473ff045f28a0f8f7a94ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:32:51 +0000

    thank bringdown score community support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
