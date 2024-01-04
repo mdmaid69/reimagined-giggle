559d0650452c370a5ec5a0c7d3c7f99a6e3bb8c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:13:03 +0000

    hit splash back divide its

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
