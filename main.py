9aa5242a9be9ea2cf6af9b46ac6306e208685c03
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:23:10 +0000

    pretend thumb towards comeover clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
