3d626ccc950812ebe2f7355c9552bc6a5772a758
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:35:58 +0000

    basic common probably save care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
