af02be3c078b21e5b0a0151da8c87f63807271fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:27:18 +0000

    situation split tooth hungry bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import time
-def get_current_time():
-        return time.ctime()
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
