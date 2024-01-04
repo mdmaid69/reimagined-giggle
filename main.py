e996fb75b1facf837711693694a0b7f1afd213e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:32:34 +0000

    duty motion responsible marry pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
