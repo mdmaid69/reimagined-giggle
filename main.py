dd015ecc0ac76849807767cb08265f3f4c6d257c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:25:00 +0000

    out substance measure thank gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_set(array):
+        return set(array)
