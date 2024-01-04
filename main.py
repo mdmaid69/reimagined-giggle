b39a729903a90a6ffc8d716ec16078dfbcc8e9f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:22 +0000

    plastic suddenly present prove true

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
