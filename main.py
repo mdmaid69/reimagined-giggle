c6fab5f1b7dd016b368e640dc32086dfff4a3976
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:34 +0000

    slice fork clue great left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.time()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
