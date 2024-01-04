d677f7d9035aa0a5b79ad10ac05d9a66aca7fc02
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:06:14 +0000

    cross she hello by go

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def divide_numbers(x, y):
+        return x / y
