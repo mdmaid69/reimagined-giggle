d1b18382736194a49011851c5c81608e45b0ab96
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:37:23 +0000

    supply combination blind opinion dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
