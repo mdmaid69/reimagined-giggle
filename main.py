5cb26c3f988add125e280036b96ec800efd6b04f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:42:14 +0000

    breakfast report extremely trial limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_current_directory():
+        return os.getcwd()
