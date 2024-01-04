5a1707bb164c5341d1eea96989bab417d0b25fe9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:55:30 +0000

    favourite uncertain display achieve forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import numpy as np
+print(np.array([1, 2, 3]))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
