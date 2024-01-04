597275352d4a1780a72b67dcd303708bbbe4273a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:01:36 +0000

    swimming believe around impressive average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
