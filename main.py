4444f00f881d593355bd3a040aeacc68ad3aae7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:33:24 +0000

    trick fairly brown process temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.ctime()
