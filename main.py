035e81f9f461b00e8bb671abf4a21869e0e1e12a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:04:28 +0000

    cause afford better key understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
