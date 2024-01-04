f0d4194e611a105430ddf799afe531a561d2a3c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:23:04 +0000

    best imagine catch arrive noone

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
