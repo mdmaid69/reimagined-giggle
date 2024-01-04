38cc8cf577c0bb3eae7e473ca758085802ea8e01
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:35:27 +0000

    emergency classroom treat bye refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
