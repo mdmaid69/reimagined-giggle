6fded2065348f73aeed07061477e3d914174823b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:20 +0000

    sensitive strength main but similar

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
