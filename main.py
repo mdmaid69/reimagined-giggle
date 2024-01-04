54dcccc4d9d99561d30b21816f6cb8b034993ef0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:50 +0000

    exhibition awake there getback wise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
