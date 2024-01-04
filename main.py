ca38bcb90fefa3db34f046f377639153680d7541
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:51 +0000

    wire 569enjoy joke sweet set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
