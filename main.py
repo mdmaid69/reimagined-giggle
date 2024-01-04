828996062d8984dd4e18b5e1a342d87fc3903539
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:52:03 +0000

    cheat middle cross environment 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
