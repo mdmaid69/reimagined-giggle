714e3ced73ea33fb8a88d6f22cd76afd9a72d369
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:14 +0000

    573entertainment see abroad tax violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
