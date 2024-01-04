9413e342f07b4a604b4b2d73977cd5df1a75a8cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:33:42 +0000

    character service will cry likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
