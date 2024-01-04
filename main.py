4f4d965c556c13c854def423ce1d386e4d10bcac
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:15:57 +0000

    mind control creature leg guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
