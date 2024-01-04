2638e5fa6b3815c19df19d641775d16fe91cffab
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:09:29 +0000

    scale spite suckme course responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
