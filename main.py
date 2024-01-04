b865621f7e2a1f4492bd4d07b1f8872323c22f76
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:05:12 +0000

    common you rely better market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
