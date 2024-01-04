2a62eb62ee454ac51020129e76936582094f4f21
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:03:35 +0000

    offend somebody head drop spite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def add_numbers(x, y):
+        return x + y
+def add_numbers(a, b):
+        return a + b
