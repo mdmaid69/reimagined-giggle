2c7ca4eda0fff87048154841ab5a4b5feeffd68d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:50:14 +0000

    promise fix relative explanation mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
