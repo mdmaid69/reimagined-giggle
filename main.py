2146dfa1787227bb26d30343afdb06a3dc5cedda
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:30 +0000

    Zz joke gradually disturb bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
