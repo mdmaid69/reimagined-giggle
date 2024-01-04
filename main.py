b87b39b54cc6a92c03b2c2a88e36402210a335d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:53:19 +0000

    him fear point him benefit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
