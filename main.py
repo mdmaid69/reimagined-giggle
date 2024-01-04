e06581bd350406cf7cdb8197b220bd034f2dfd54
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:53:00 +0000

    cruel simple concern serious test

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def cube_number(x):
-        return x**3
-for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
