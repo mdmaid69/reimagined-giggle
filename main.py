fad5676fe6b06ea33442dcf3aabd141599ce24e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:35:22 +0000

    draw read skirt round side

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import time
+print(time.time())
