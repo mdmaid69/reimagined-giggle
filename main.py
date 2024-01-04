bcc2890a4d0e2e38d8891788a8a27eac06faccce
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:58 +0000

    shall drink exist mark occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
