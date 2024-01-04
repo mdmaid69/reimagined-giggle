205e80275f4175ec0ae998781f7011a885c99e49
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:58:11 +0000

    uniform centimetre little 573entertainment season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
