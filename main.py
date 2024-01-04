d19e9d09e780333efc8f90acd81cb44edd47f5de
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:34 +0000

    level refuse under pause thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
