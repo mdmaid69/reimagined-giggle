b1edc76c26470bdd5bc2760edd87277d566a9478
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:43:49 +0000

    wave whichever cheat total indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def find_max(lst):
+        return max(lst)
