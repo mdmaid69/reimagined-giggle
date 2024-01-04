ecd79b62d3cfa71cb7a282aefb650df5e59a5fb0
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:04:57 +0000

    mad classroom act excited wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import datetime
+def get_current_date():
+        return datetime.date.today()
