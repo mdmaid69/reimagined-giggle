90530fa0a66258447aacd610c13a2f0322298581
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:49 +0000

    answer Zz speak far pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def square_number(x):
+        return x**2
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
