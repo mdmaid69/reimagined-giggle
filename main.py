79ee4c16216274d3da1f89c4a56e5fe94d7e344e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:04 +0000

    take pour getback dull ought

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
