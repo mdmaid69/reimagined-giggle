df630d91bdcae0fb00bcf00e436fc8aaadd60016
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:57 +0000

    relative bar bunch confidence talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
