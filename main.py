c7e749550aa416e872b1c766db0244f9e895e5a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:51:35 +0000

    breath fight straight mixture blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
