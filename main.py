aa6c6bce1b8aa16f67420479c55c1685c4437500
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:29:04 +0000

    guide forever deal performance practise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
