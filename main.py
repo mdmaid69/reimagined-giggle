812703cd2d32c6b03dcb36262a899eb981e94ac3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:26:24 +0000

    achievement switch official decision heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
