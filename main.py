2cd6809e0a5c3f324159c4059ca3a26a465d7c8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:46:44 +0000

    heart breath loud trap turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
