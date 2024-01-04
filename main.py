28cd73a4cfcd8e0dd2306ceb6c372757de76d93b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:54:17 +0000

    post brave exam every cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
