cdac0a19ffd965694080af4ff4938fd64da083f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:54:49 +0000

    limit dance OKorokay duty she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
