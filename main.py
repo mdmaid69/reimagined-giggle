aaaf3bd07c8093bcbc3fad31aa11f7de7b766494
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:56:34 +0000

    improve easy frightened adjective duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
