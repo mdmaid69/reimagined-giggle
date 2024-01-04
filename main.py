4b0c258fe7aab137f0696003bd0ea05dd0259ea1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:54 +0000

    herself anywhere talk after exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
