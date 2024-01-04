f6fd0ed0f69763088dda9a1856cbba4faa061443
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:08 +0000

    school team related alone silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def remove_directory(path):
+        os.rmdir(path)
