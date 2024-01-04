b5ec8b636a18063e00bc31741f97520f5916e4e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:00:41 +0000

    quantity trap count own letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
