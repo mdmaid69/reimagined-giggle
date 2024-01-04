0b70f878d58b502a6d09cf5c4ff45cab76819ad8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:39 +0000

    any blame bang clean sticky

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
