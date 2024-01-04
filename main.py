39e41d2387c0c85e263f1b6173b70fd55d8eed19
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:49:35 +0000

    many when development neither recognize

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+print("Hello, world!")
