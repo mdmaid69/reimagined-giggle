16fc5f5b385fc3c6c9703d1565d7f07c33a8ad9e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:17:05 +0000

    collection small negative provide child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
