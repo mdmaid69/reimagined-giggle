4764c50fb95120e4ec1fe3f8a36b214d58270967
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:18:01 +0000

    pity mystery tongue save disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
