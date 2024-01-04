d5808b2bdfd37284c58ecf12ffaaf4e47cea86f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:53:13 +0000

    cheat climb embarrassed lower twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
