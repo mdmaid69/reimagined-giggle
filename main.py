ca74e039bdb075d6f8a36c9eb09b9e6d0388a864
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:42:20 +0000

    drop scream praise since turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
