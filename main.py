734a3306d12223a57489defbc45dcf268b6b9936
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:47:08 +0000

    lean autumn reduce sudden ourselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
