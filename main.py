9007291c07968ae2595a7c47c835ed0185202eea
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:35 +0000

    effort difference of welcome raw

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
