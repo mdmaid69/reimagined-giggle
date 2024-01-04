1c9aa7e2da820167240867ac129b2601a5e18c2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:25:50 +0000

    ought weekend imagine ticket everyone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
