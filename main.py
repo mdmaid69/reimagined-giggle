0a9f15c945fb988371ca3a857fac73b8396417ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:38:49 +0000

    objection meal disgusting of show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_current_working_directory():
+        return os.getcwd()
