53d12a336009a520c56e0d72fe3470aa0eace280
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:20:33 +0000

    anxious cum refuse confused building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
