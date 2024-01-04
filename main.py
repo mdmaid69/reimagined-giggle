d46d6a1163e0a4e6a68a2e5478cd8a5dc7555e20
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:11:32 +0000

    giveup square wide above towards

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
