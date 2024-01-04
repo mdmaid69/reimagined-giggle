0b87d05c44767637e51fd52ac78135dcee4810c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:45:57 +0000

    include any under calm wake

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
