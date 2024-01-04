65ae220aca24dcd3903ca41731851bf2931a189a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:00:29 +0000

    confuse heat life nervous complete

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
