f9a400bb258f1c1851ca57ea621cf5cabd4a01fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:17:32 +0000

    past arrival mark strike height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
