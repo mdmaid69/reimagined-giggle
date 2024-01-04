94ea6adfbb5e63a85a3aaad64e13ab8d68224ce5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:52:55 +0000

    lead right determined beat preparation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
