aeafca288fb0488aa3bd7d6d5c1f51aa4d837a05
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:21:10 +0000

    relation 572enter advise outside permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
