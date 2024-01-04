47f771fcd53ad09ad22ca349c394e009e46d63c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:40:51 +0000

    activity large name minute light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
