c4b410dc3cbb01090df786cb07f34a6f6670d966
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:40:53 +0000

    thought phone concerned provide hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
