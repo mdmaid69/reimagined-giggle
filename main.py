6735d0b1f58fd32eb826ca3b248d84990fff64f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:57:07 +0000

    sound attractive beat better below

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
