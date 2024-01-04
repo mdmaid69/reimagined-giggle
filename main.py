4fb69b5144e62357a241e711bfc71d41792ab3a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:25:38 +0000

    object skill study own pile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
