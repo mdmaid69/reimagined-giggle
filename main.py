6f6473cb11624fec311ce9374e1884c840f7aa37
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:58 +0000

    contrast stick some group bridge

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_float(array):
+        return float(array[0])
