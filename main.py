9ed18407b4b2fc7a1bcb34e8d830795d28a7b063
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:18:14 +0000

    save same wind hit thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   def divide_numbers(x, y):
         return x / y if y != 0 else "Cannot divide by zero"
