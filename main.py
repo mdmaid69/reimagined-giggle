47d9efcb48fa6e2eb284bb7379d5b9b313e86603
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:28:07 +0000

    stay difficult smart fault excellent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-print(sum(range(10)))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
