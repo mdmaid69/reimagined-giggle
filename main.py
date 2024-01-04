d085550490b7eae7826b8ba5fd23bfc962c37104
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:20 +0000

    pretty turnup clear join sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
