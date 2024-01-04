530c266808109aa672705dbf6be3c7ee93ccd778
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:23:20 +0000

    view form spot bend measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
