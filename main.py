2790524cb67080db9f4c689fab812af4acb8cb1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:08 +0000

    upset if quarter sail determined

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
