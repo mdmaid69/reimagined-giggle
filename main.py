7229d7c68ad2cb22b3475420aedde998a6ba682a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:20:41 +0000

    manage satisfactory report task kill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
