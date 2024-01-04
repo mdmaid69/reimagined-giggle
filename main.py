99d4e796d03f448870be1d0b79eddda6e16bba22
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:07:11 +0000

    bottom collection future disturb sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
