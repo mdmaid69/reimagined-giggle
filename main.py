fa76fa5ce9928073a0ce45ced95d1e348a9eef5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:45:26 +0000

    keepup before proper noisy shock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sys
-print(sys.version)
+  def convert_to_hex(n):
+        return hex(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
