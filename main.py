14764b6b63d12ca3dfe9d729e9d79e1513337bd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:05:41 +0000

    provide permission education improve detail

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
