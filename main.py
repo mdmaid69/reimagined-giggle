e0f737a9f84f2a3c9127290f86fed69e31a3dc35
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:03:38 +0000

    concentrate bring delicate block judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
