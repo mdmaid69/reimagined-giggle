dd15382323fb50670da301a184d4bef9f38b2915
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:37:53 +0000

    die behind swing nowhere she

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
