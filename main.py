5f36047f3bd5d773062aa6ed6ce1a4837791c075
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:42:15 +0000

    decision Hh transport case next

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+for i in range(10): print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
