ecb8d251c92d7eea53ea38dff9942babf6d9229a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:56:57 +0000

    maybe risk mark fold first

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_min(numbers):
+        return min(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
