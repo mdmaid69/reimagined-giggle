a6654b9185c189e889c4682ebad3540d49a7fa69
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:38:19 +0000

    Nn model concerned just attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_list(array):
+        return list(array)
