17b9279b382aaacfb59cdad05417b7541e2f2b73
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:44:30 +0000

    limit left act keepup identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import sys
-  def get_python_version():
-        return sys.version
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
