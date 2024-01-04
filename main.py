8b81ee837caae7c75237110504397c9b0009cccc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:43 +0000

    line whatever lean he deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
