df57883b596940d9486f6924a39c36d3c52e9fc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:13:20 +0000

    backwards beneath I practise powerful

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-name = "Python"
-print("Hello,", name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def convert_array_to_string(array):
+        return array.tostring()
