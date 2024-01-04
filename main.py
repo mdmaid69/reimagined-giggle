7337715aaaf472a916ae89546825a09ed49d2767
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:44:38 +0000

    shout afraid official rain man

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_str(array):
-        return str(array)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
