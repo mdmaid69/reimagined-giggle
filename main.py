cc5f7004e02ba9819906fb581cbea4a845b52d2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:09:14 +0000

    develop important choose pick news

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
