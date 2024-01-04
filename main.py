6dec75749f0c6a5126be001ee161677549ec1c7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:16 +0000

    argument as thing twist method

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
