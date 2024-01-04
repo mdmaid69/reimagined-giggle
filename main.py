0e4cf82a8f72762f4bf115760477376e9f2afa75
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:06:07 +0000

    pick absolutely to bye satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
