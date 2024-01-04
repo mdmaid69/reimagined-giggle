e75a8987b77a7747ddb30f6909ac349ab225c5d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:40:51 +0000

    simple bad me top she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def square_number(x):
+        return x**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
