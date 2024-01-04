8a31e50c1395c030ecd9adb448c01939418f7b07
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:30:59 +0000

    repeat concern inside treat chance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
