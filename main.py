4b09c4299bf847dec6f67ffd85f8cae85d3b6d1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:21 +0000

    lookup mess border minor direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
