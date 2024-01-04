a79cb9634eba4a4798dd446b2ec9f33736ee20b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:34 +0000

    interruption shadow stop reason complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
