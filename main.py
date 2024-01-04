bf1459690aaaaed91cc26d49496cecb8039dbdc7
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:25:58 +0000

    polite awake get everywhere used

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def square_number(x):
+        return x**2
