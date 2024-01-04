c50d89142884de75924812f2f5238ecbea57e057
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:03:16 +0000

    image tap trousers refuse pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_set(array):
+        return set(array)
