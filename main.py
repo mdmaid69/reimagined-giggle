68d24643008ad0d4cc16d8f19e17fb1d563dc641
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:45:33 +0000

    kick start exciting they attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
