ba9a47459c4c4146d92991a62dfc158ee9474620
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:05:40 +0000

    similar terrible holiday appeal lost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
