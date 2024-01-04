7a129bd9b33bd36f67d271c7c6ba304b38d6f276
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:05 +0000

    attention turnaway process top ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_unique_words(sentence):
+        return set(sentence.split())
