2af9439aa27aafd9ee2d608330aa07a42ac0387c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:33:20 +0000

    platform advantage second pattern proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
