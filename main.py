7f1bf94b16196842b75c20869ec69c01a49e7a2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:51:17 +0000

    cost above force getup list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
