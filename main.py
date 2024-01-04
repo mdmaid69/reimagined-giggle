b0d52af5972fbb055490c5d68adb0a442b3a4ec0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:05:01 +0000

    Bb our various violent tend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
