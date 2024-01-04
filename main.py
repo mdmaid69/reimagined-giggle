9c4c5ddc37d0214b9d838cee60c407ddac422b84
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:05 +0000

    independent sign instant fence accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def square_number(x):
-        return x**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
