1de7297cfa9c98c7ab11320bbc3b467afbb43982
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:44 +0000

    disappear previous great material odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def reverse_list(lst):
+        return lst[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
