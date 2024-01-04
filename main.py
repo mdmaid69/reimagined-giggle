a11d652b20bcf80645dae58755d4584f83b4f8d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:08:56 +0000

    laugh bite pleasant mess themselves

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
