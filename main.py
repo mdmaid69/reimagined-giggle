36772fcb31310120d32a88617bfc096b05b89980
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:49:55 +0000

    grand shock sudden tap poor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
+  def square_number(x):
+        return x**2
