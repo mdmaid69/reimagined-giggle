61bd9fcafa046b7af6dd86cfe219c3e4fda75347
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:17:57 +0000

    equal born warm freedom blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
