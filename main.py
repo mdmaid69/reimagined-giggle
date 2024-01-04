09776a2d8bbd9ad9034e8fd8a2d25c5bc1845265
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:51:29 +0000

    introduction slip tie first lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
