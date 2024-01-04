ba61580b0dadec3ccfd840e113b5ed85f7d7d976
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:30:40 +0000

    pen already matter holdout him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
