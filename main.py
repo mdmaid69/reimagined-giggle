b5a8795d6bd7d95b70a1795259c516030e1db9a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:57 +0000

    start remark fetch measure issue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
