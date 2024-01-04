b02994652778281a44dcbedeb197dbdbdee47b65
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:55:32 +0000

    degree lookafter block excited talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
