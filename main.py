1ba1556fe5b9fb340898ce8ef4de69e8e3f5d692
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:51 +0000

    pile mention miss belong manner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
