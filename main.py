88c46963243f79391b06448f59ca56cb23d6ea23
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:41:58 +0000

    excuse free equal under crowd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+from collections import Counter
+print(Counter("hello world"))
