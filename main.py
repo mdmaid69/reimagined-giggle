53345473a13cf16581374d460f4bcf8f6f3e052a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:31:04 +0000

    somehow by confused hit motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
