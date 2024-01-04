f03fc7fbc5904aef43c9c66583a38da5a2fbd810
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:05:57 +0000

    better comeup sea control left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def count_elements(lst):
+        return len(lst)
