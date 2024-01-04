00886f7b2b82a4d4190704724ee29b6b85aaaed8
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:09:06 +0000

    even come better usual bin

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
