f78995aa4c510dd3b7fe7ecc00909de5c7c63ad8
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:34:44 +0000

    force search reply rent history

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
