53c77a2e9738dc6eafc6921d33362a75912732c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:59:33 +0000

    regularly ready wish put cost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
