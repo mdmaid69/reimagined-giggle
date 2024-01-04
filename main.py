8660814739b2d74ded1377a3ec94adb253e40215
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:40:01 +0000

    meanwhile sudden right slow count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def convert_to_binary(n):
-        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
