2890f9b3db71c83666c4784e900fc30a24a36092
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:14 +0000

    breakdown old rain entrance tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
