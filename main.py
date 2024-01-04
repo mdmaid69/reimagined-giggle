8271ceb3417cea80da18bd0d56e4fe1607f44ba5
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:43:01 +0000

    justice convincing unpleasant match solve

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import array
 def iterate_over_array(array):
         for item in array:
         print(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
