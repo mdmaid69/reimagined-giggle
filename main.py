c5adf91c31a149db58ac8b3da62f8bcdbebe8c51
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:45:51 +0000

    left cut remark cause build

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
