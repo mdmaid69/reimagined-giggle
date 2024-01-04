35728cd797086043babd1b52427e50d516898ce8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:03 +0000

    minor result final themselves unite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
