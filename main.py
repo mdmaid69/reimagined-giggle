32654ba47360ea924b8b7df40e7e1bdef59c7887
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:12 +0000

    ring brush Itself waste keepout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
