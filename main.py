dc1dd4cdcf7117d43536575b16f5757ad431164d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:59 +0000

    recently quit name bunch drink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
