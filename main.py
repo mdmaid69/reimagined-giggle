4e2bcfaa3fecaa29789eb83656a9a2f5316c7cfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:32 +0000

    odd conclusion hurt brown old

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
