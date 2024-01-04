51c66aa3fa11f5447dbdb5699a9b9aa9d2dcb58c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:25:13 +0000

    clue on definite error bend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import platform
+def get_os_info():
+        return platform.uname()
