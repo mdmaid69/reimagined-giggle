574f0e8a8c2601fdc204871b7b32f378166f2a29
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:32:44 +0000

    curious have left holiday classroom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
