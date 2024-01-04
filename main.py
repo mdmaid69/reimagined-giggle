082200f73a23c1c02b0eebc332c029f5dc3b3240
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:51:09 +0000

    opportunity force temperature accidental prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
