3151bc4f67c065f21c858f5042b773a44de8dd5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:59:29 +0000

    cut interrupt plastic recognize broken

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
