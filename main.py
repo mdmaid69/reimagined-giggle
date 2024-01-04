3a46522a9b437449233edecc10c0052d886ba5ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:28:28 +0000

    feed around careful convinced aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
