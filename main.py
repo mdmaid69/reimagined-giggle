31875a53b6c6ed6adddb1009149ee10ba1c41519
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:05:22 +0000

    hospital sometimes equal off once

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_length(array):
-        return len(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
