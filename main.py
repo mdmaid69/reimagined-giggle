e759fb46a698624ea7e1bb2c7f81fce899d17c1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:23:26 +0000

    square guard Ss bridge determined

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-def find_min(numbers):
-        return min(numbers)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
