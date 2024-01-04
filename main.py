80c8b06a20f310abf3adf0ebfb60e80d67a16c57
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:46:27 +0000

    develop hot bush like rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-print(math.pi)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
