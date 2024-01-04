ed7a9e2d3d51b8526a616b31d17cd46ff90f9d2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:48:57 +0000

    little blow exact glad risk

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import numpy as np
+print(np.array([1, 2, 3]))
