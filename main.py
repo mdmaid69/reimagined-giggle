76b3e0bc3923e23cde0aa517db292c1b37b03e35
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:36 +0000

    amount refer dear song dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
