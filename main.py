0fc816a027f0086a21b16c61697b40484f81a476
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:59:44 +0000

    only material unlucky base into

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
