49fa5f68fa49f48ab977ab2cbb2a2813541ff314
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:10:35 +0000

    thank who nearly callup spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
