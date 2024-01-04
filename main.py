13b694b5f19a8db29d2ce6275f766c484ef5edd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:25 +0000

    for extra certainly contrast pass

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
