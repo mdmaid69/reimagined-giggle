0f4e954d3960ac5b49dea10a822335de7a0e6d5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:03:41 +0000

    empty nothing close class group

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print(sum(range(10)))
+def add_numbers(a, b):
+        return a + b
+def is_odd(n):
+        return n % 2 != 0
