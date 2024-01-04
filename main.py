5f809bef7827b99ae6c5de3401649742005b6a22
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:06 +0000

    exit attractive lunch suggestion transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
