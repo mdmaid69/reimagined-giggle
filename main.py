6a75808e00c74ec6d6a57c39c7db352dbc254e0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:39 +0000

    sit learn system base nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
