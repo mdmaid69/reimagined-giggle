ade407fd7eae8fd267f9d00a995b4ae534d28d2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:31:40 +0000

    share production so failure catch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+print(math.pi)
