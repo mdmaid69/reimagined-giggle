3865ad77f363df2edfc4b87e8c3b6a721876792b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:53:11 +0000

    disturb crazy legal immediate accidentally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
