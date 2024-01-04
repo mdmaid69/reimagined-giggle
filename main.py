4b32e8d2395078cce3db0c57dce944ace439b335
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:24:02 +0000

    middle powerful brush drop leave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_unique_words(sentence):
+        return set(sentence.split())
