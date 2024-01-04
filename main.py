90005f711bfc8e4cc9533f2d12bccc8d21e308a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:19 +0000

    upwards plain away her combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
