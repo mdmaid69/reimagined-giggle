8b96fe0c3bc54f27d8f497b9e99d2e0f12494a3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:04:24 +0000

    poor important match clothes combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
