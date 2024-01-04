2a599133754023047ba15adc3e1bc4a837a5caac
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:30:38 +0000

    machine likely aim activity content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_area_rectangle(l, w):
+        return l * w
