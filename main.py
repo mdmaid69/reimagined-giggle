d5b7ca77b341334ab069ed3c8e94527f146e39d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:06:03 +0000

    yet coffee position lunch mother

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
