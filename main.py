2b35a584f29953f36f0eb4cddb0716aed4d96442
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:01 +0000

    push container experience music save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
