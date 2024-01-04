d0e8a4d760c2cdaf02676bb0c163e7bd5e8644d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:05:02 +0000

    language system mysterious laugh pleasure

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(10): print(i)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
