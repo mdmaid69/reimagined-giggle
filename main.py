91c934c6e42055025791a9dafe77becde0e2d4e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:53:41 +0000

    light shout past complicated danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
