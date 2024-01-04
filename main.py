bb1ab7e1fd6cf6a6949c4afb63d6cc9aa6f6c05d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:25 +0000

    shadow concentrate shoot mean computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
