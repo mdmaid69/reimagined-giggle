c0f5dfd1c5813db2a825217e07df644d24d035eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:57:11 +0000

    over damage furniture show Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
