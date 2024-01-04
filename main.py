7e6f4897f36fbc1a7ced914d619fd801277ffce3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:08:32 +0000

    hot strict I bath in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
