4ce750f35959286863329dae85be91e826137bcf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:55:34 +0000

    see doubt mixture sense really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_user_string():
+        return collections.UserString()
