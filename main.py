3bf6f02ebaac7fc4116a54653c1ec6af845a3b14
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:58:12 +0000

    attractive communication exhibition length broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
