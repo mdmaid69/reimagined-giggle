fa7de302e39522850f65b5aba79f39e931ffc6c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:38 +0000

    up burn special match true

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def square_number(x):
+def square_number(x):
         return x**2
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
