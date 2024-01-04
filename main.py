30a767283331ec677378161f14a3eadef265a74f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:33:36 +0000

    anybody thin outside suffer condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
