ca6920d8aeba79ed110d1705977d4111c26c8cda
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:25:00 +0000

    crazy learn wide dare bar

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
