41c344151cded80a4f4d20007e54fd929fbf9067
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:08:13 +0000

    miss wise open remain pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
