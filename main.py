65e5ace416e77d4712064b824a457e159caf9cd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:51:00 +0000

    trip sale term calm swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+for i in range(10): print(i)
