0efa37a0261067b8e92ece21af424586ef6d5081
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:55:19 +0000

    control past begin love tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
