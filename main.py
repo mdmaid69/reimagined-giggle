6741090c78615e829dd5789761c119c1e55d86d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:03 +0000

    race change tune bear inform

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_elements(lst):
-        return len(lst)
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
