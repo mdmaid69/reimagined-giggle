8cd39cb877e1e56bef1eee1b895a585b63709614
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:08:41 +0000

    reduce active doubt OKorokay remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
