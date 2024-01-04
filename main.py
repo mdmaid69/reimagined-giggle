06c980a6b9ebc3f9fea95f7ee1ff95fbf7b47bd1
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:33:08 +0000

    important breakfast runout sentence backwards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
