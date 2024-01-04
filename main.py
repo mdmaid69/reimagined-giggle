2c10650db8658ee8d368629fbc7b61e8220e59cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:37 +0000

    lie high everywhere talkinto belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import os
-print(os.getcwd())
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def remove_directory(path):
+        os.rmdir(path)
