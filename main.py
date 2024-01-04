b870695d626521154fb17ba03d13cb99e9936f26
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:28:05 +0000

    travel child firm workupto trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_min(numbers):
-        return min(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_counter():
+        return collections.Counter()
