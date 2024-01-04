795082a00c3628fcfa1815a9eb8f9dcbf18eff56
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:47 +0000

    amount clear possession post start

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+x = 10
+y = 20
+print("Sum:", x + y)
+import time
+def get_current_time():
+        return time.ctime()
