777bcd842412c4b9e89d3586c5c234c142e54f38
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:51 +0000

    system entrance violent friend important

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
