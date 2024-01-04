75838ac4f1e1a5940171d4180361755e70d5b12c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:26:51 +0000

    university explain appeal dig score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
