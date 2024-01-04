04348c6145a872b0b2cee1e885b465c01e1b4767
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:37:12 +0000

    suck exam common world so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def find_min(lst):
+        return min(lst)
