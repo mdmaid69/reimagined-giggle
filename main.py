60f008246a7c921208670247cacb402d84c2db0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:49:47 +0000

    patience dead each takeout basic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
