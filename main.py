d6f17da57f8360b45a0558a490e030494526db68
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:31:40 +0000

    interfere whistle song lay fear

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
