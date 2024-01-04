eeac7d15a8de4e1df5e8fe379cf6732ee9a2f474
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:59:50 +0000

    her rid little national difference

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
