e2a65b2a99835a87e9839e6b2e3ecc9ba61094ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:33:25 +0000

    polite communicate plant organize replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
