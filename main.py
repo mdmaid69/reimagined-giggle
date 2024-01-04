06d433ba53dd4a9ce184c7c7f1db666209579c18
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:55:52 +0000

    store recover ago better bring

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_str(array):
+        return str(array)
