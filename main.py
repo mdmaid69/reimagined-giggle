1babef220f445f7c61887218d70fb33f0300b501
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:21 +0000

    godown comefrom wheel see deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
