4d52b75d4576e9913d501f8324a333e63e286ed8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:39 +0000

    watch use scream pot middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def cube_number(x):
+        return x**3
