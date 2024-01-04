e808d3b59feb4837334e5443a4967ab4e480fd47
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:01:57 +0000

    stop thinkthrough first protest approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
