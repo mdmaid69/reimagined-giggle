84c1a63becfc65ee354a370b76a465df897c6ca4
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:03:14 +0000

    message worry battery number mess

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
