9b0b714d9a493b164dfa9d7a5fc0242b086ea1c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:23:38 +0000

    lunch report public live talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
