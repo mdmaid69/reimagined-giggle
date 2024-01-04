d4abb22e484839acd30202eb448a50761e1aab80
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:08:15 +0000

    closed minimum ought job space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
