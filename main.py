e5e497b6de4d2e3d1d76c1d433326e57a065320a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:07:46 +0000

    store control public kind own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import subprocess
 def run_command(cmd):
         return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
