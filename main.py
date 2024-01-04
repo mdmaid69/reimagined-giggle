406e2196b41fbab12775d3a8df113fb3b3ade5f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:52:41 +0000

    birth cry month fire drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
