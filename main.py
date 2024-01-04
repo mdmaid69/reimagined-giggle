91a9628a09468c4c87abfd089b5df5ecd819c31a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:27:25 +0000

    shrink track minority consult vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
