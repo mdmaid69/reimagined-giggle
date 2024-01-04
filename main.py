f00989f7cae16674431640d49aee5b5bc919a143
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:29:52 +0000

    choice getout judge compare joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
