967e4b2bc56c52f26d1b161d0fccbd06d6bf137e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:06 +0000

    between throw reserve spray used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
