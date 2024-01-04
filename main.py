08a40cd906b0402856aebb6746e943d3a53e3fda
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:44:31 +0000

    hold odd record chin wash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
