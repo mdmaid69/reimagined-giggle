e4d59bb69aada82e1074f8e94ac12aeabc4b5a7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:16:38 +0000

    turnout attempt Tt tongue perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
