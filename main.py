7a6c5b61988ec4204cb82639e321fdc31f0221c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:01:25 +0000

    Ii scared unhappy spill blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
