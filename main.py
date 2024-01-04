e8c2b18cb2c096c2bac6085cc532574e7c8b0b39
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:32:27 +0000

    please list between goon centre

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def is_odd(n):
+        return n % 2 != 0
+def reverse_list(lst):
+        return lst[::-1]
