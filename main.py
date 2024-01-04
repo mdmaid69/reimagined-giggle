b82c7c969037618ac26a4cbd08dfdda36e8ef1a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:52:23 +0000

    increase maintain no offence slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
