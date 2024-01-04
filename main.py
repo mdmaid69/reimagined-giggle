65bda3d836faff656b5520c88690c2004b9972a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:54:26 +0000

    stamp wish opposite reason completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
