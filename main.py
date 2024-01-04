21e4b0f2b1477d08350a3fd015d52e1cc14693f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:43:19 +0000

    proud past storm cruelty effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
