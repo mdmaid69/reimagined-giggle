13172820982343ec6495a58ee0df6d6a91dabd21
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:52:37 +0000

    begin on hard way mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
