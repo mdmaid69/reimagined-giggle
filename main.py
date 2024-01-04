2c5371a9d5509e0a56283f76dc302b09c58a5970
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:06:12 +0000

    information post person weather 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
