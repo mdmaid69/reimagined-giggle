1d43da0c850ef8229adeaacbb715877b2db9f23b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:52:19 +0000

    today old through 573entertainment pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def get_array_as_tuple(array):
+        return tuple(array)
