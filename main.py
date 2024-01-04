6f3d830ed50500f68820639c68851f3430f92cca
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:08:07 +0000

    share something deliberate knock always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
