d53ab2b5e87d35a0650b5ee90a3c2e8347419023
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:50:29 +0000

    buy cry Tt in cheque

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_work(force, distance):
+        return force * distance
