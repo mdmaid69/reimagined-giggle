6a4b94f91a7d77d7354f5582d40230d5a46bb098
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:55:00 +0000

    gentle fast switch stay popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
