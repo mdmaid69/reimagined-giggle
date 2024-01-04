38c858f895c55939d5dd24f8028ba9c473cc31e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:33:32 +0000

    waste calmly tragedy hard account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
