a29dd2bf7cfd3f292d94d561e284a6d05fd024e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:45:15 +0000

    bang know regret blue exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def is_even(n):
+        return n % 2 == 0
