b2e2a920a0c90e3097aceb63cf50a06a2d5f98c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:35:49 +0000

    clean regional equipment dance on

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
