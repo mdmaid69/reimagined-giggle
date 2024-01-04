878453f4cd733a94edec5af83ab5607fe86fc1d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:56:49 +0000

    write comedown rent conduct drink

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_power(work, time):
+        return work / time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
