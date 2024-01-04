cee4d7d7209a175cbbdbda7915f19b71fad941f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:46:26 +0000

    instead tape here grade front

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+for i in range(10): print(i)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
