e928d5209543b62e369a1eff9cdf84870ca597c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:33 +0000

    hope anybody day valuable crash

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
