84ba567ea65869e9c5ac7e5e24c28b1ed04f6b48
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:51 +0000

    task instrument closely attract notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-print(time.time())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
