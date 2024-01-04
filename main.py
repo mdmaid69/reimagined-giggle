e4ffee0b2bd490a0c1ffd824c02d5ca6d15ee790
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:25 +0000

    stare price offer shoulder stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
