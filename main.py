751c2e5e03ed9bce79a64c6bab2cbb6bc3711de9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:19:50 +0000

    us little some admit foreign

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
