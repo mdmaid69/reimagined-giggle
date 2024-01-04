667915f706c136883bdda1c3496fe7017aa5b554
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:25:58 +0000

    unusual chief press official formal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
