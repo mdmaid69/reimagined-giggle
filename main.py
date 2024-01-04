6a2043386ba3e7baa95ea01cd6183214731f7140
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:01:42 +0000

    very appear name pinch speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
