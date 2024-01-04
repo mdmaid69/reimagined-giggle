b5d25509130afcf66c18f5f7afc6bd764f4adb65
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:53 +0000

    message thanks thirsty runout truth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
