1d4a686fb48e31d96f5a1eec417fa34ff18d44d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:49:20 +0000

    breakout control do list side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
