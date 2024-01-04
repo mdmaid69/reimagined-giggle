fe0da5cc906b4f7a8f885ae821ccf0243dde9667
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:15 +0000

    of temperature movement play piece

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_max(lst):
+        return max(lst)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
