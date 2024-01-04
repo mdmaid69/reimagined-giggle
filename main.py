bd97f502cdb64dcb00c97c7a8104fb8463a5a9ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:16:58 +0000

    slice right turnback crowded judge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
