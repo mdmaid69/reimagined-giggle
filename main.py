e2dd9189b92015a185748e21ed8091349582609f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:12:03 +0000

    ticket apologizeapologise hope dress tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
