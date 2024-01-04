7390604e5d2d3a4e2db92eb79fe9fdc13b3f4c07
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:50:42 +0000

    repair print rely show impressive

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
