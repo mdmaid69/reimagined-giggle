79f96b7f195069a3125146eb78b73db1ad31c871
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:32:35 +0000

    refer declaration telephone thick height

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+for i in range(5):
+        print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
