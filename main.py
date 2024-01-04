c2eed59168827e36ec25c3d9f98ffaa20188848a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:10:51 +0000

    keepout front contents activity continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def convert_to_octal(n):
-        return oct(n)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
