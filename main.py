a71c8387305522649ab568a85b69d06af8699760
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:41:49 +0000

    declaration congratulate realize calmly earn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_user_list():
-        return collections.UserList()
+def find_max(lst):
+        return max(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
