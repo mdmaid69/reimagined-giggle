3f15d00cea89d2f309cb847b33f79f093829f256
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:36:40 +0000

    comefrom unlikely plate lunch remain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
