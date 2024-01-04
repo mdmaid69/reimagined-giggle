aee778e9e475d2f639a4be89d733e2c8311a8546
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:47 +0000

    friend letter single plan goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
