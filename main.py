9a7f7b3fe501ca1f863daba9cd374ca6cd5e2616
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:28:18 +0000

    imagine callout among correction increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
