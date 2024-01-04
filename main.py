aa77e23fb876a7bb84956171f628633141fe180c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:11:25 +0000

    benefit business uniform hand interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
