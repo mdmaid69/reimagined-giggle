170020c48fb8ca95d6f13e58d4da1de83026fec2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:43 +0000

    organization disturb wild clean sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
