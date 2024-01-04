0c882867bb0eec66d9b587c044dc485f9f7d179a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:33:55 +0000

    fairly pot operate fix stop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
