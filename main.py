0068a50ce5823e48ec772b6d88eb7dcc4747cb04
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:22:55 +0000

    adjective treat path shock excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
