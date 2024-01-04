9559a3ab1c61ba6505591d0f1c483f0c51bc2b2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:13:32 +0000

    any wish aware clue content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
