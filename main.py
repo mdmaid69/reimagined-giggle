c1ce8fe677d131a417d1cf964dba6468fd38e3e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:48 +0000

    influence wash study suggestion fail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def add_numbers(x, y):
-        return x + y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+for i in range(5):
+        print(i)
