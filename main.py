4aabfbb7a36c91fe952492096703a7e4b6259efd
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:01 +0000

    Ee gate belong forgive 179bite

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-x = 10
-y = 20
-print("Sum:", x + y)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
