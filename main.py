cb67e73b1e0c6d741fab0dbcab229fb078a7ca18
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:35:40 +0000

    phone offer system interrupt disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
