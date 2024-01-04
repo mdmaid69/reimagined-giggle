7b4bb4837a955d2478c8a7912b6f984602694034
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:24:57 +0000

    further refer burn takeout quality

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
