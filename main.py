a61e18095002c1852156183de4175b9e2806f6bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:09 +0000

    warning niggaacademy play chief steady

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def split_path(path):
-        return os.path.split(path)
+print([x**2 for x in range(10)])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
