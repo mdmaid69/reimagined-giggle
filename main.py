a2f0f7b916419fe5fbb0e863ec4bf8e9aaa36667
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:54:40 +0000

    pocket ill count trade today

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
