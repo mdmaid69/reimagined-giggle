0c6848e42c6b9663eed261439a0ecd2a9078d189
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:09:21 +0000

    height wide generous positive undo

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
