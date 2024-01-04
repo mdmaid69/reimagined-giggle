2d0e6dfed3698a31e75b24c6afe8a8545d2724c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:00:20 +0000

    law in shout rubbish land

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import time
+print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
