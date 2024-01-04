3e901386f103212b51e9a077ba6299b0b29a7730
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:21:11 +0000

    present objection look judge closed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
