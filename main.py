2f16f17b0f39ae7b8770eda5d91c815ddd3d9d93
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:21:24 +0000

    above protect justice bus welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
