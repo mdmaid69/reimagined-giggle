abebdbfc18fbf7d361bb92550b55d53a811f6d3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:12 +0000

    comment long makeup over keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
