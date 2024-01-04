7dab87641768c2c1c4b7d8f188cf0a4fca011a60
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:11 +0000

    introduce culture want beginning length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
