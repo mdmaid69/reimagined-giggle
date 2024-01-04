ccc600c0a8d8f895b2a03285b82eb9837df2b92c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:11:24 +0000

    his wake community cause frighten

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
