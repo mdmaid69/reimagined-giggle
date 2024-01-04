eefcc49b1e772a464ab336702f5736c05975defa
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:50 +0000

    woman section may set beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
