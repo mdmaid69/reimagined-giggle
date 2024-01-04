a79e3e6d6362456b7af490482c118a3553f2105c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:49:00 +0000

    people fence joke concerned demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def square_number(x):
+        return x**2
