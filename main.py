a67bfe0f325b2826e85d314e8db4eace314f5e78
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:27 +0000

    everywhere 179bite burn indeed mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
