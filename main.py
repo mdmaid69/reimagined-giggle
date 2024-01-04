f1a729c7db5fe35a2d73a51ff9c9735f68dc58fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:15:35 +0000

    bill tap innocent coffee mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
