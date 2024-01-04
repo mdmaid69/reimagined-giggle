72aa3a4aa7be16111572e9d6106b03d93ed7e433
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:57 +0000

    knowledge 564encourage bringback not breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-print(datetime.datetime.now())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
