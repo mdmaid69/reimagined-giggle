eabe98059cf4ed02cd8503f619c0d2bddd593be3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:57 +0000

    seriously whistle brave much divide

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
