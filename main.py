634755f9c89517db17c87ff5f8fde5824d6b2b09
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:25:56 +0000

    know turnoff often guess breath

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def convert_to_octal(n):
-        return oct(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
