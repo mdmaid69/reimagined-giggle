eb9862b3a79dc3fd494ef2231d942a3335ee6254
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:54:32 +0000

    earth against scared goback(to) moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
