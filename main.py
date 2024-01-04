951b81bc14f89f8ae4adfcac4e1434bcc81aa9df
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:19:27 +0000

    develop themselves size slow threaten

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_work(force, distance):
+        return force * distance
