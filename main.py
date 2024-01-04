3241cb174382cb7843d2d2774ad317bb38168c9b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:07:07 +0000

    he holiday away edge union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
