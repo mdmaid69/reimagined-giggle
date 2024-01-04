07e8c65bef31eac737b29bb1f065a1e2c2656726
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:28 +0000

    excuse list left agreement keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
