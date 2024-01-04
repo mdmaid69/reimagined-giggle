49f3350afda295b96f9077b20f758c4d58b5013f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:04:31 +0000

    interesting gas hit pin crazy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def sort_list(lst):
+        return sorted(lst)
