62adb7dc41ec61a776ef05c2a6a74ae7a9b75b44
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:20 +0000

    office police nothing watch city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
