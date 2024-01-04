dac2b4a5a6a215bd56bb3e19ce958338f92f03c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:23:47 +0000

    number ours plan callout flat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_palindrome(s):
+        return s == s[::-1]
