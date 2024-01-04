926a4c2185e38690e41b989555c28a78adcbef01
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:15 +0000

    strong play special fly recommend

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
