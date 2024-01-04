2b64a68dd130194616238c9cab74130bab7c4d38
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:07:23 +0000

    sell wander weigh clean polite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_binary(n):
+        return bin(n)
