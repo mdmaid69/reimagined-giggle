2fa8d5e1d6fd56a775ac4a9df885fce0eaeb253a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:08 +0000

    scene backwards weigh actually anywhere

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
