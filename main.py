9c40c94fb0d97cc688d2e7ff19a24357439e6ec0
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:31:13 +0000

    address case wall female rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
