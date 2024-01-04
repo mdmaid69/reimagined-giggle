f89b36fdfc2e20351bead68a205c25f10cd9c0ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:00:24 +0000

    once no careful block keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_index(array, item):
+        return array.index(item)
