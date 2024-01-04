fbc4930b053be0219b9fcce54356d9fb4e624362
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:34 +0000

    relief set free compete keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_file_size(filename):
+        return os.path.getsize(filename)
