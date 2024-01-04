98cf84bff9e664c99a9fd87fe8420685b604e84e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:24:14 +0000

    thick drink another 565end quiet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
