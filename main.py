c5d86c862f0c7e902edba025071992d0f4ee8619
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:31:19 +0000

    evidence splash set plenty production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_current_directory():
+        return os.getcwd()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
