b4c107557340a14f7f9105d1eabbb13f902a4a0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:10 +0000

    stiff slow dig rain lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
