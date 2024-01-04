5ad4a6ac36f5ae05fbbaa6b475cf33085780ec26
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:10:03 +0000

    slightly exercise sound pressure so

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
