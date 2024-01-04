0712e03e21dc3845a330fa3070ef097220fc95f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:03:40 +0000

    impression play vision hit schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
