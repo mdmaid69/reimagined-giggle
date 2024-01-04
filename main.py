4de06e4c84549139b5ab91c6fa69dac2f954176b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:59:00 +0000

    friendly round understand importance fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
