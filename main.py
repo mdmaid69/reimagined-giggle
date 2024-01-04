679edc4f228af6b0cb4e034964a1e09cc5736c85
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:06:50 +0000

    sudden tired balance tired community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
