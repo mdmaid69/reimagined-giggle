ef05541416df9ff8c3e426270164cf25d952c1ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:37:00 +0000

    comeup imagination back line obvious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import re
+print(re.match("h.*o", "hello world"))
