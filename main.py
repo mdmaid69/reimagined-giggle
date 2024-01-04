33613b189df9daff625f736e9484461f9a730de7
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:13:07 +0000

    study thin hurt feature order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
