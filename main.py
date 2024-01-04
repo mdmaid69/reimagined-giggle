4dfc609004dd0481d15cf3361d53f34b79745731
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:58:41 +0000

    public quickly so pollution principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
