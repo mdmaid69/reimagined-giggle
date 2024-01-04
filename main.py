c8aeee2214d83998dfbc0b4bd4e7bd2aae8a6300
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:39:58 +0000

    thumb phone correct down chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
