8d5ddf0a68ad2759fc3b5411493bbc2ec1b4db6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:45:08 +0000

    every tradition price major complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+print(random.randint(0, 100))
