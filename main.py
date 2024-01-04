65bf483ea2b717831daf689f52149bb08aa231bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:42:38 +0000

    book belief life another office

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
+def reverse_list(lst):
+        return lst[::-1]
