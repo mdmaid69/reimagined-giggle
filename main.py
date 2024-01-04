af5f71bc575f45363a868190790c2fecf887f8b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:24 +0000

    contact pause boat sail mainly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
