a3e37b8784c36ce822ad550dfe197f537a231f5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:38:25 +0000

    back when note tongue decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
