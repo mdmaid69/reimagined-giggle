ead70482221a84b1d549fae36bd7b7b72fecf9c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:29:45 +0000

    569enjoy extremely way comedown deal

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import platform
-def get_os_info():
-        return platform.uname()
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
