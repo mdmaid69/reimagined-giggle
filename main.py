ce6d68fd2b7f8099afa2f55a1923391fd83c6756
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:05:30 +0000

    reduction crowd feature Vv deal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_max(lst):
+        return max(lst)
