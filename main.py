0c805f26b444fd899d332d8a909bc4fa81a3a074
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:04:27 +0000

    proud deal lost pipe stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+for i in range(5):
+        print(i)
