c0a7c1d85db98a950819887d72ad6305fa2428b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:46 +0000

    hurry day the sometimes suggestion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
