dbdeb5b0818c886556ea9c214d139d847aaf8688
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:53:25 +0000

    little part normal office history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
