719306296dfbec091964178c005a581c45e7ddb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:50:54 +0000

    equal basic help forget couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
