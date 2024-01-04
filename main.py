4a16ed58444bddd4a66b57b6ca96c5ad1cf8e417
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:15:51 +0000

    light study protest transparent hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
