e5393caf413c74aa4b2bacf590c87ff1777372cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:18:45 +0000

    Nn represent central noise tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
