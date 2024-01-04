85773d253f00088bb940c9a470f0d7dadb6db16c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:50:34 +0000

    easy bye Gg dress lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
