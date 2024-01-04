7a55e24569b6925847ec5213aeec2d49ce41cbca
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:07:50 +0000

    voice occasion tap pencil pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
