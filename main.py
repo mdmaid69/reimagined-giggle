8657a7339b20f381e6d97c55e539f37636f4ef94
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:39:07 +0000

    estimate practise neck workout subject

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
