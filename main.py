2abaec73ffdcf24745aa15586c346e74fad902cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:24:30 +0000

    control press slight notice notice

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
