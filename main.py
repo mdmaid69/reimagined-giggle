c8c24df2d72171fe7e5d364f46d11f5aac1b62b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:59 +0000

    company exercise cover address slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
