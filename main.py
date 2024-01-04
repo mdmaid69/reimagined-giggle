8845f43022c07f27b75302804dddc858ec8a309f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:39 +0000

    dance song contents fast black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
