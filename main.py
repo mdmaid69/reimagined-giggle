8bf875811df2075ba0263006aaa32461d012c580
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:06:00 +0000

    continue powerful recommend cope estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
