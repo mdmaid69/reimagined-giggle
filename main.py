a4a2d63615e9185a71127f3cbdbd7a680d8fc14f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:46:41 +0000

    from runout thing ago powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
