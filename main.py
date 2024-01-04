5fa23400112b335369c4ab2cab5cd71d95c92475
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:12 +0000

    anything considering furniture city breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
