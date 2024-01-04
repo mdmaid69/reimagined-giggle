900e91efaf9667861f3c9719be4288f2943f3db2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:55:26 +0000

    turninto this either complain broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
