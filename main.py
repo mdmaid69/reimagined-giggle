506a73510d9d4a28f8cc587ab8636b11aec88e40
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:28 +0000

    refer shape murder cold release

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def is_even(n):
+        return n % 2 == 0
