0cb32a3818f75186fe01643df8ecf45afe7092b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:52:16 +0000

    drop safety foot process train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
