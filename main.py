f9b538e837a3cdfcd762d1f2b0681da28a1fa4bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:43:12 +0000

    conclusion contribute catch song smart

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_as_str(array):
-        return str(array)
+def convert_to_octal(n):
+        return oct(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
