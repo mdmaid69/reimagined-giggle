b7013daf748acc1b0e738afac54bb60808c1fb08
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:00:32 +0000

    skin since else hit normal

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
