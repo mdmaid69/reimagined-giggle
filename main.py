61bad8c255a276353085d77c2e9bbb36aa3d14d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:11 +0000

    path sting instruction motion church

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
