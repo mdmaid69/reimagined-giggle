149bb05a4fcf4824de17fd65decebefa7443f5c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:47:53 +0000

    service close related confuse return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(lst):
-        return min(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
