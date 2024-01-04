f0de5ee5ddf436e5de5b4b830753442540b0f0c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:51 +0000

    level ordinary less aware thankyou

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+for i in range(10): print(i)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
