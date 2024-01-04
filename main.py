cf65e0eabe1be303a7d29050c59702b595bb4eec
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:55 +0000

    government giveout equal runaway unlikely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import time
-print(time.time())
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
