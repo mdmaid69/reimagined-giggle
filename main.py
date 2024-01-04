17297453a9f66d46446a7a159f9707f7359225b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:30:00 +0000

    keepout fortunately far imagine long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
