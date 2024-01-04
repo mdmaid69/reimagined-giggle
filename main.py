89d1278b6ee6651f0016d88de5c5ba914970ca90
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:33:50 +0000

    rough forever same fill family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
