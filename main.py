2fa98c463404624e207600392445c2a316b3c832
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:50 +0000

    scene she next once breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
