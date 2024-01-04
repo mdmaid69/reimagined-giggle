a7cfe7537c8f73f6dbec3cdfbe651d379d1f36bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:53:04 +0000

    treat workon swing boil book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
