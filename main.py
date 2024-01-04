57e73fb4a99672e26e1876d329de7d65228de460
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:25:46 +0000

    sort permission definite worried opposite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
