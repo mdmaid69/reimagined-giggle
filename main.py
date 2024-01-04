460d0f2f3e727f1f00e817647b022316df5a01b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:57:15 +0000

    give length snow instrument walk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
