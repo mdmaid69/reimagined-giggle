2dbe334d202dfe3361775b22c82558afb2c75951
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:23:38 +0000

    common relative 565end great by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
