49aeb5cec91d2f7cd935802bacb79ec25c017cf4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:53:59 +0000

    everybody radio operate ask involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
