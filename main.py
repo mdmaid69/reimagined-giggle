496b9e56a6d3d1c841dbe4e06615bdf942762af1
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:06:46 +0000

    science better sit tap equal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-for i in range(5):
-        print(i)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
