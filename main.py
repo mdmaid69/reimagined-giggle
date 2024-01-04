476ef67f6f72f6b61011bdb5b0e38e592f303e00
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:38:26 +0000

    majority win work like strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import collections
-def create_user_string():
-        return collections.UserString()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
