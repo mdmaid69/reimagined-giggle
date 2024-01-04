aa90152cb71a84cc4e396fe53930dfd0dce867ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:54 +0000

    fit address interest wash cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
