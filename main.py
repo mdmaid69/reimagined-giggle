29acf5607de50357b3be2b58f7010f1e39c3038f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:28:15 +0000

    vote square school schedule achievement

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_area_circle(r):
+        return 3.14 * r**2
