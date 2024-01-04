adf5324ed5b7224c75e30b083ec2042464ff1938
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:16:50 +0000

    subject correction square Zz card

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
