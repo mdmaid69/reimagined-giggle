4241b1ccb38e5c8886a8bfb1be44c24e861db7ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:07:02 +0000

    black 565end whisper shout brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
