1db5523d6f441e59b8a816a121c7fbe01975b91e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:45 +0000

    shrink mention still sad inside

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import time
+print(time.time())
 import array
 def append_to_array(array, item):
         array.append(item)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
