fef6087c9e569ec9965bfeac4a0a1bcc4ae00cbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:34 +0000

    operate cause recently tongue bus

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
