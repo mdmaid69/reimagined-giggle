ff0537f2e5cb91150aef5e87bb53b4be3feaca84
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:01:06 +0000

    accordingto anyone priority clear offer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
