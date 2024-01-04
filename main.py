6988ce4b8d2dda1ebe7b6a3ad13f36b0fc426876
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:41 +0000

    through square remain near loss

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
