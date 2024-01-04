312d9b175baaede2f0fc07304e333bab2804e65d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:32:03 +0000

    speed raise woman talkinto use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def sort_list(lst):
-        return sorted(lst)
+print(math.pi)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
