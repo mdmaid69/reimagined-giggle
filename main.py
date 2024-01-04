b37012d4e7523d34dce2f9ee778b5337ab6fe754
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:23:52 +0000

    between dear bang whoever stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+print(math.pi)
