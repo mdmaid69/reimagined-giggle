fefebce4b5eccee61a9e0c58e80834e942b77d00
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:32 +0000

    accidentally loan suit stare however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_set(array):
-        return set(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def count_words(sentence):
+        return len(sentence.split())
