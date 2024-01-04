f89790910ece37bde9c6c98a3b84cc01f3825380
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:18:57 +0000

    loose same correct cry measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sys
+def print_python_version():
+        print(sys.version)
