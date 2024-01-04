a4cfdecf41fe58976109d69c1d97dc09d06281d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:07 +0000

    temper buy comefrom complicated announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
