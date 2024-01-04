ecc2eb95ec581e4aa48b37a6a5771b185a9c1104
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:49 +0000

    accept post use mixture correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+print(math.pi)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
