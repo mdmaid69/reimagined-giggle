c56bbed9a569efb4b7787ee1f2a41b00ce63baa6
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:04 +0000

    method region regret late positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
