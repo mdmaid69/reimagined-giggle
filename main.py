a921bde6239190199999a4b9db0bbff313f707a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:01:56 +0000

    invite moon hard hole myself

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
