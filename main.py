2ba45f9461ece66de2af5de95b1a31c21ce26acf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:47 +0000

    attempt family Qq increase genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
