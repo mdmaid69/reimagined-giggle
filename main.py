fe916caa38e24827cb9beb9d6f1617ea96b6fcdb
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:06:12 +0000

    confident either direct brain yet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def find_union(list1, list2):
+        return set(list1) | set(list2)
