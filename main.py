36216b45b2c43c287c719725067676bc4e9df766
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:08:51 +0000

    throughout left period poison key

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def convert_to_octal(n):
+        return oct(n)
