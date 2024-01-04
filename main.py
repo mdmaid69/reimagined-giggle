4dedf693ed8b70c47234305ec26afc562cb479a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:23 +0000

    because dull friend completely clear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
