8a1e442a80f168ca76f8133e276a697efb3fb4b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:56:29 +0000

    cheap catch recently complicated consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def square_number(x):
-        return x**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
