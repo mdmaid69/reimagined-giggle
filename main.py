aebf284b2d42562a23f1f5b3f239763f6e4d83ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:05 +0000

    later first name small seem

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
