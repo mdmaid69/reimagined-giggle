33fe47eceeaf48f08986e41c12b0f0b20c4f8ce7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:40 +0000

    platform agree before completely lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import platform
-def get_python_version():
-        return platform.python_version()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
