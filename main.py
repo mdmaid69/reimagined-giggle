748866c468e41bcf77de724407d31f1aacf261c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:56:11 +0000

    appearance space rain life keepoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def divide_numbers(x, y):
-        return x / y
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
