093d9cbd4336ba595346200886731a30ddaad304
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:35 +0000

    comeout slightly type refer acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_current_time():
-        return time.time()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
