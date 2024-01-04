3339eb6bcc6ca02b892382a6ed4862495090ae84
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:08 +0000

    break funny related shock if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import collections
+def create_stack():
+        return collections.deque()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
