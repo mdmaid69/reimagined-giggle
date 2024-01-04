0bf9ca53cfb76ff19b0205f98cbcffc648f69732
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:27 +0000

    start oil rest class argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
