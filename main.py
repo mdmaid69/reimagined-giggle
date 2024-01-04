6366935f5ba5a70e4f8d5b38b0bf11362b12dafe
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:28:05 +0000

    treat file track arrival exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
