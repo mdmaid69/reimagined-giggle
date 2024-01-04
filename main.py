065422ca942427b21c6b13f03b1ad36a964b4a80
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:11:05 +0000

    surface shame wander cry fasten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
