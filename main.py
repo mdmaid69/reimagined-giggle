ce0185fe07e68d519b19d8ba37b3384b857d8fb6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:03:11 +0000

    remark water half dress face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
