5bfa8c783fe0f9d956d67dbd068fec01d162c563
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:44:09 +0000

    welcome rid town tour lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
 def get_array_as_complex(array):
         return complex(array[0])
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
