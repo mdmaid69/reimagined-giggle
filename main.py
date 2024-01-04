a0497a970402867eaa156274952769ab2866280e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:21:29 +0000

    voice kind about whether big

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-for i in range(10): print(i)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
