1ae055d339de53f526d276d5980987f070935cda
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:15:29 +0000

    practise move happen comment temperature

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_speed(distance, time):
+        return distance / time
+  def subtract_numbers(x, y):
+        return x - y
