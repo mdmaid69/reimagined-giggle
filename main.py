da10eb1db271a2f5b93c30bc4b5b228f9846ad4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:08:04 +0000

    behind information cruelty always cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
