fbe47ed33f2a12edaebf7d2c07cd8f1139e1a183
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:32:37 +0000

    compare minor rank between water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
