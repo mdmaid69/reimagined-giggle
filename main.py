de07804b2f16fecc38ad687080d42480afb00f77
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:11:47 +0000

    foot tired important national wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
