e8d6a9f75c9a6463a898a28a82396ce2708afdcf
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:31 +0000

    polite frighten very last behind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def calculate_area_triangle(b, h):
+def calculate_area_triangle(b, h):
         return 0.5 * b * h
+def reverse_string(s):
+        return s[::-1]
