222daf353fedb8e9cd11b96f499cb2ea7b4b1b2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:42:12 +0000

    shock shall themselves Rr replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
