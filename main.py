4e1ffcf525c2c7ea9586d856cac86e49d175a196
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:12:38 +0000

    able disappointed front fear excuse

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
