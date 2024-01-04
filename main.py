b14f4d948a7d4ed341c082f14483efc9290a98a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:55:01 +0000

    foreign decision level strict platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
