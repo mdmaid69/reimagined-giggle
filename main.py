31a41254ea5b710669ba16d5f86c4f2d4b872997
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:27 +0000

    confuse mind ride pool however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
