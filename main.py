ad2716d05fdb75d31aa5ccb6a96ce289610eacf4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:24:01 +0000

    fresh deal begin convinced bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_list(lst):
-        return sorted(lst)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  def reverse_list(lst):
+        return lst[::-1]
