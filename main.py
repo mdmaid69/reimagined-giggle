bd0d6418beeb04d4f102211dbbe6eb984d79a4c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:30 +0000

    influence move comeback back crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_list():
+        return collections.UserList()
