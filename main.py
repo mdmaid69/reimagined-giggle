79de2466579fc01b64fa905679431d5a710e4b49
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:42:20 +0000

    expression punish put already freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
