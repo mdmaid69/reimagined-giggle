5379ee0432fc57bb371bd5f30c172b64c0a6a11b
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:32 +0000

    agree trip claim uncomfortable conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_tangent(x):
+        return math.tan(x)
