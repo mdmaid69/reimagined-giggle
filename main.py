ce5f0baaf772becc6d0e5b284c66b89fe55ef124
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:14:49 +0000

    opinion chat turnout crowd commit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
