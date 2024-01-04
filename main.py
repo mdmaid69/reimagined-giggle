37ad41d71ccc3824e556d3d7a77abee1fb1b735b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:27 +0000

    hard Pp business suggest care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
