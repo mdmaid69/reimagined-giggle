c91b82d4f1181965fe0af7b56ae4eaed8e1c971d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:54:39 +0000

    cold offer pump noise would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def set_array_item(array, i, item):
+        array[i] = item
