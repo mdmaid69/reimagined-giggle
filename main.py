c3e7d2125e263e584b41d971b35172f08b138e18
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:28 +0000

    almost conversation almost tomorrow combine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
