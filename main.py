7a00b1db95ce4d92978728162925375ce4400198
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:34:11 +0000

    side bend refuse harm needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
