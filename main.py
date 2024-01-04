cfa6032d0f5679c53879ce66ecdda08811c81c14
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:32:07 +0000

    circle nowhere full market wide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+for i in range(10): print(i)
