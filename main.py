6a68bd4258b28971350f4bb42c63cacbd66fb5cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:01 +0000

    instruction check breakout hour rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
