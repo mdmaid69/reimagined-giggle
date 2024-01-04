79713775de5e76b8373d8a5cea39ed3a414bc273
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:55:59 +0000

    wonder firm top entry kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
