b1058fa1c6d913f23597cc6f44c3cd2f5a09a64c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:06:29 +0000

    either to onto apparently holiday

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
