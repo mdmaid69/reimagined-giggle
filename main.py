c3be66ec4a669cae667588c6a543fa7a6d1e7b5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:18:47 +0000

    pain breakinto confidence brush fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
