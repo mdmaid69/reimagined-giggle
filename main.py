edff6aabadb6780c334c7fe393f2b17ecdecfb9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:30 +0000

    before bringforward bad circumstance smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
