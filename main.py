8ee4a8859a37ec4786dd665fcd5ade832c3d6e65
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:28:40 +0000

    priority smile bag mistake break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
