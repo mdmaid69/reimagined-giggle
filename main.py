8214f81682ee2ac3c5b2bb7a8a35146a65ee8852
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:14:56 +0000

    difference equipment cry skill couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
