c391d3ab25721f41b65c4ef76ed62310067541d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:28:44 +0000

    dance plenty business combine completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
