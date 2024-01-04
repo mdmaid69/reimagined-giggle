1f4e626311c6ee265b2cecd6338ba52303a81e6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:39:35 +0000

    Ww education previous matter wing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
