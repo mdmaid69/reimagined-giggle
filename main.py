40e7a67aee578ac237005db27a1c1e4080f9a305
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:53:41 +0000

    news address ticket sting deep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+print(os.getcwd())
+def calculate_area(radius):
+        return 3.14 * radius * radius
