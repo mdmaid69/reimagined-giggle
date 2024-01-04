0dd5eb95c4a83036c987b9538a7816b514ca122d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:54 +0000

    show happy him cope convenient

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
