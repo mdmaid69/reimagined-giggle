909c973f2214f58bb1635ced5387df1468522418
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:24:55 +0000

    everyone often protest sense stick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def count_elements(lst):
+        return len(lst)
+def convert_to_octal(n):
+        return oct(n)
