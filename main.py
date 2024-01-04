395411c28e2ecb9eb0e00e3f9045f7eea47c5f01
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:21:40 +0000

    amount sure punishment permission buy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
