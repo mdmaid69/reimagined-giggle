54547ed862b8e3bcada7a69409240ef3bb4d4453
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:21:54 +0000

    keepfrom claim full bye distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
