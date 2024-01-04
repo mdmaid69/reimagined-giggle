1c3857a7579b01f1fe5e2343cf0fb149eebf9da2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:13:55 +0000

    pretend be order appear satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
