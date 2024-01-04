ec79c6052d59900232d4882d66c6d63f81990c13
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:09 +0000

    noisy worried underneath treat patience

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import os
 def get_file_creation_time(filename):
         return os.path.getctime(filename)
