0c6b18fa2981f992d64a80fc519b8e77e1b19487
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:10:04 +0000

    obtain discover little practice surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
