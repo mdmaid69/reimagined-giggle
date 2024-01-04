6e8a4679e062bccbbcce0ca3eaf5455a9d5bdab7
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:10 +0000

    Oo plastic ready turn will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
