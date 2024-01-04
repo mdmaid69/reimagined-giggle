5e4cea4eddd0d27f3371639ff2fd42e8e76a215a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:18:15 +0000

    dare medium borrow takeaway space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_str(array):
+        return str(array)
