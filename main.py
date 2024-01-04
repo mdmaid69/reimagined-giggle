faa1af5195bf183a647053a254c8977544008912
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:50:00 +0000

    even characteristic peace real touch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
