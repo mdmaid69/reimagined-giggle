251a467dab6e1465555eddf2a41d6f960dff359d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:40:07 +0000

    final food refuse pour themselves

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
