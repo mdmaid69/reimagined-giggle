2e58437e52d0fef01baca5e2f1afa7de3b97e793
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:44:01 +0000

    path single search take beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
 import array
-def clear_array(array):
-        array *= 0
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_cosine(x):
+        return math.cos(x)
