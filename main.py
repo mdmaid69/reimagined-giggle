6ca03fc409cfb821675b467adda331361c7e3b04
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:03:42 +0000

    hate follow refer fast neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import sys
+def exit_program():
+        sys.exit()
