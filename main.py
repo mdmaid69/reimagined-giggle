7a84996bf65c56c71135ee41c338bbd74f2b0e4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:11:21 +0000

    respect Kk only go visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_item(array, i):
+        return array[i]
