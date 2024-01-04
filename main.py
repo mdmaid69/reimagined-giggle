38dcc305b0440b778abfe049e5fa3d18cebae65a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:48:49 +0000

    involve juice country subject family

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def get_array_slice(array, i, j):
+        return array[i:j]
