dcb34892a735bcb4c0187e3b3d35092ab6fd42e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:55:12 +0000

    door immediately angry hurt thinkof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
