b11330b2497dcf0ca90e725b79e73017a7c57b7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:26:30 +0000

    only quickly reduce steady side

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
