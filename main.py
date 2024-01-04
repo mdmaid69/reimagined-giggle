32d89166f76d14cfd7a73007833e9a356911be9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:58:41 +0000

    aim responsibility record accept confuse

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
