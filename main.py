76fc8f87903f378e10cc52cadbdb46f44f6ed387
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:08:01 +0000

    face walk enthusiastic late official

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
