5938f15fe876a318631af7bb2874d527ffe20aaf
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:38 +0000

    cry fly software spare unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
