e515b734ca5e47641ccb4e556e59d45aaa9e2c79
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:20:43 +0000

    beyond repeat mail lucky copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
