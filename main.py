02e3193a1cd1c3d1c19374940400eac48a92108e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:08:53 +0000

    570enjoyable accidental mysterious exact can

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
