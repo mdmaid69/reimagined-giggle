c4bafd16ba27d30d7fb5b125ffdfc9e07a673407
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:00:18 +0000

    very mail so urgent generous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-print(math.pi)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+for i in range(10): print(i)
