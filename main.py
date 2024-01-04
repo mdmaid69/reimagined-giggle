1e037e6e52c24ad021fb0ba33767e6aa8869b9c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:39:27 +0000

    meet taste sign bank anybody

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
