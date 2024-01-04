73f899fd12cdb4c79c3b823697c794fdf2feea36
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:54:36 +0000

    now stare like punish breakinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
