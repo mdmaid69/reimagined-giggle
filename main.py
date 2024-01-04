ba729880beb34663d55885700a46647befa1af9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:24 +0000

    find treat sting borrow reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
