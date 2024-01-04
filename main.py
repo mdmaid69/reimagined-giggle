f3af1d84522b5d9c8d61739e61e5deeb5aa5acc9
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:09:10 +0000

    their valuable success unlikely listenfor

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print([x**2 for x in range(10)])
-def square_number(x):
-        return x**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
