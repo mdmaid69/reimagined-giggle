6f8f6f86d7b367c59ddc709bbc5801e35645bf93
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:59:26 +0000

    occasional brief beneath takeback thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
