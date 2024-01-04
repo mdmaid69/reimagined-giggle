58a3a0aa54178f14b73a3ea1cfe1e596e1ddf228
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:47 +0000

    quiet worst post dead minority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
