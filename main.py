a35c855d04423db537bc6d57dcb0a23034304ec6
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:56 +0000

    final money approval pencil dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
