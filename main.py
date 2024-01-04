14b464872cf99fdee73385f9a8a094f48be3a5a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:03 +0000

    offer thin other effect mind

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
