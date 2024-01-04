6bcf485f1603181d6e21fb584b6c55cc97df4898
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:04:03 +0000

    indeed flow remark soon drink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
