5c1aab2d0a7f868ca56ddc8395e0e77e40081436
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:17:22 +0000

    history sleep trousers call push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
