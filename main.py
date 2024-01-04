2ed26dc24b5f110fc39289a257f9b931693d4820
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:25:40 +0000

    direct serious compete rate naturally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
