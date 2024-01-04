2f27b8084d260acdd6b6c0ffd6537c0cdc170688
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:33:33 +0000

    makeinto serve simple all memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
