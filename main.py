882480ccedacc2b6059a9efa16802313e0492d83
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:20 +0000

    how surface chance sound you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
