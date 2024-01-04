5d15145de701653d8ce2f12ba27d4f5096f7669e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:26:22 +0000

    remark interest full comfortable better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
