dab826e30216e2e8c529f7d0520bc4c5d764f441
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:48:39 +0000

    together learn thing probably excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
   def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
