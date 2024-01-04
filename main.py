bb491ed4de955ae75a60938c7abf5f84251b9319
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:16:00 +0000

    demand turnon suckme scared powerful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
