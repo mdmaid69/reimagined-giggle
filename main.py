ac56c6534adb9cd9b2785c7b96684d78d0cb5f6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:42 +0000

    slow pattern react scale storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
