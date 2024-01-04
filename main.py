a320b40dec0c311631310d1669819878c7dd5d48
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:47 +0000

    just whichever extra glass pleasant

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
