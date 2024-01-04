f33789125ba4e259651ba431ee4c6f5670978a6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:51:18 +0000

    carry ahead unkind finger vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
