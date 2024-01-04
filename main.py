26889b2b2a619a5369e46f4fa0ea71d4a2cf355f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:44:49 +0000

    clearly hurt fetch tradition swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
