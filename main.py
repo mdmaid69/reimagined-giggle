32e96d08c9e94027d69491c2867b65427b45acf6
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:32:53 +0000

    uncomfortable whole upset fact outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_list(array):
+        return list(array)
