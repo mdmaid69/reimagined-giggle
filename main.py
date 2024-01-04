3876d6fe99d3297ac0d21af492835ff3294422c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:29:54 +0000

    year book door disapprove also

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_counter():
+        return collections.Counter()
