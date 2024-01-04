7f66f37e6bfd70f8f99cbe5f59d0f7f84916bf24
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:07 +0000

    cheap blind fasten ball already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
