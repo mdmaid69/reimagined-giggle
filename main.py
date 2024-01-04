3c0afc18727cffee2195d40816c681bc29a4b89b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:55:45 +0000

    Tt somewhere scare final opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
