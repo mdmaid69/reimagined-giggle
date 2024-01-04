a4e2c76e0d3dcfc6698e932e16ca29c10be6021a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:09:03 +0000

    simple curious ours cry official

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def find_max(lst):
-        return max(lst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
