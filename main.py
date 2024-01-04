f77d41e88c9d596cf329f4b4f542936099f0ac84
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:57 +0000

    rate song rare complain Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
