1ab9d017472b92d5d18ef680cc029a83efa24dd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:31 +0000

    whole admit target class move

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
