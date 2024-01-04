6194e0c3ccec993ffd8ebc31f7e334f8e87a530c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:32:15 +0000

    offer repair wind indeed rest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
