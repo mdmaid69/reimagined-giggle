b6d9c499b3043f28f62e836f1f6367b2404a38eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:07 +0000

    near voice rob influence lower

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
