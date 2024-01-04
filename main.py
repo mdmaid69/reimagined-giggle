88fd0658c09d5e337bd9538016d1cd58bf0a7f0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:38:50 +0000

    curious excited great able giveout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def remove_duplicates(lst):
-        return list(set(lst))
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_item(array, i):
+        return array[i]
