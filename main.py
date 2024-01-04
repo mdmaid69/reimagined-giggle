e9faab20bd24aacd1eea62a9729d22b133417484
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:38:04 +0000

    moon wheel range maximum sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
