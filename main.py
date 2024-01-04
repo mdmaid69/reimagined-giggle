384bfdaee83e1d3ed16b27fa2e3ff7e07c34abb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:02 +0000

    tell continue joke ago serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item(array, i):
-        return array[i]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
