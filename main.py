e14c2c516150bba7c1994531e3d2a61d60f09118
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:21:34 +0000

    rather direction apologizeapologise show polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
