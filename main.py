11e929199be63196328c636a3f17bb5b78900e12
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:54:52 +0000

    female memory public take save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
