b05dc07412fd944c36116895edcfb1a79f5abfa2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:56 +0000

    emotion control knowledge safety fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
