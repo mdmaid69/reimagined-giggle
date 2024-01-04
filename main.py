0061de0c11fc6489c85af8205ab020ad792dd151
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:06:09 +0000

    case once somebody party concentration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
