1d47d435279f8e16e798a538bd1b6a3e40057705
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:45:10 +0000

    politics unusual hang poor clue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
