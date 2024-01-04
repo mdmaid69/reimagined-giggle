8c5aa023f74122120f55d3085c474c2af823f220
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:48 +0000

    responsibility rise manner comment hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
