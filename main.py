aa52374ece5aeadc27a769b0bcd270be30b2e9e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:28 +0000

    rather fear during leg progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
