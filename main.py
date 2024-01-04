b166d15fe736806b8258cf444ae9c1ec03181ac9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:48:45 +0000

    impatient wet claim injury spend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
