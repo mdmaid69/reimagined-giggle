48c454d7b7a6aba01a9bbd65be583030ef023f6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:06:51 +0000

    visit fold travel getout row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
