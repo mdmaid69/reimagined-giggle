97b6feaa63c425fe7780ea30f73510e2714dd947
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:47 +0000

    many bread replace if exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
