97ed82ef1b944eb98b709e2b832f5acccee9c16a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:45:18 +0000

    forgive argue reply welcome doubt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def multiply_numbers(x, y):
-        return x * y
+def convert_to_octal(n):
+        return oct(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
