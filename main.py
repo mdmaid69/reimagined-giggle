abbb42964add8640b0321f58124cc7b4ace70ae9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:19 +0000

    discovery score post Hh smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
