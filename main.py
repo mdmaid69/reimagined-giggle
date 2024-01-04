61c4915036d84c5bc13a903afd6964620a8e8b9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:22:35 +0000

    confusing pencil mood priority disappear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_max(lst):
+        return max(lst)
