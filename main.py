e4eb1e9824d2bda92e99c46266890b43728d69b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:18 +0000

    exist put Bb material could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
