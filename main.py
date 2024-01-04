a6fccb088944188ce5d4d89d0388e310932f9e98
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:47:33 +0000

    petrol laugh contrast pretty patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
