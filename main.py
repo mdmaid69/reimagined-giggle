09bd4083aaed4c723ed37054fa430a69fde4cad0
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:00:13 +0000

    used study peaceful fill level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_python_version():
+        return platform.python_version()
