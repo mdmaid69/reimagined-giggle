04c104a5804b88b0a8478f11cd809948ef85e0ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:13:18 +0000

    collection build pile simple pipe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-print(random.randint(0, 100))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
