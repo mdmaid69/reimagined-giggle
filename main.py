af9ba0046c816a8811ed545d4bddda97fccac237
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:45:22 +0000

    raise themselves medium pronounce callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
