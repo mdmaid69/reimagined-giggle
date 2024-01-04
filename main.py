4bd6854074e74eea0ff4789d20b0eb7c55edcea9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:58 +0000

    period subject runout matter travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
