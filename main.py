7f13e0c216125c0bc0d805a2d3ba88b792d662d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:13 +0000

    report height rely decision quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def append_to_array(array, item):
+        array.append(item)
