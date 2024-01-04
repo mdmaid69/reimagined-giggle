51c27c774c22266965126bfe067ecf7c11140769
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:14 +0000

    hit needle character bridge wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
