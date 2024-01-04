e7f0031387093a37b2daed974cd7b98d896d0741
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:10:48 +0000

    keen platform what claim deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
