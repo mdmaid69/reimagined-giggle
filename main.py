d8cd147078a70f82f7a7cfdb17e6e3a019758d40
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:45 +0000

    hers exactly we trade cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
