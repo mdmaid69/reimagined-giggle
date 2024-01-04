30f13538403c826721001ff17e8d51a1dce4fa8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:16:29 +0000

    creature pronounce breakin trade careful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
