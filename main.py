39c8c86fc0f46b028a7ca162a2083f95b7c4e248
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:31:36 +0000

    flood as major form smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
