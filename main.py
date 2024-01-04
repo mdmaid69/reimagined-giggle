2a80c3b05dbff19d59f49eb68dbf851dab80b56b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:52:33 +0000

    married refuse discovery difficulty begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def convert_to_hex(n):
+        return hex(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
