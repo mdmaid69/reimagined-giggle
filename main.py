1c1711875a4838ec4b92cf260922ba74d5e492a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:35 +0000

    uniform pinch entry while might

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
