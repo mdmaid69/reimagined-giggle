51945ec8cb6da7b4ec79f279afb99232f9779a76
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:51:33 +0000

    slow influence room under stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
