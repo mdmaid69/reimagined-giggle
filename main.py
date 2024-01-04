2fdc789aa6c51bd787016302d5846667544989f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:58 +0000

    common sensitive out serve important

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-print([x**2 for x in range(10)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
