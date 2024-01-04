0942adc85d96406c595328dde369e59d969c8ccb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:50:23 +0000

    range bang workout comment fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
