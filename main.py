f3c7673f5ac1c0fe58dc0212a64a7bc2e5f3aa1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:57:17 +0000

    keepfrom agree callback too sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
