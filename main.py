bb73530c113a4720bee4c5d95b2b6e5de3b542b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:32 +0000

    choice difficulty Itself congratulate so

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
