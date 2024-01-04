d588e3dc4ce3dd4299231b601aa2aa72c7fd491d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:03 +0000

    tongue tend spread material gradually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
