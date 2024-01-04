590d77fe9fa0164b870da085e9c9ca56735ff1cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:48:00 +0000

    runover cut waste enthusiastic form

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def find_max(lst):
-        return max(lst)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import collections
+def create_user_string():
+        return collections.UserString()
