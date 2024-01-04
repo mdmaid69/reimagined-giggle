cf0de0b54fbfcf4c30ef30e9cf4e9abedf6ba56e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:07:17 +0000

    bringback after tragedy powerful definite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+for i in range(10): print(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
