90eb14b7e82cf7c06c8e80b4962e2f858ad6c901
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:39:47 +0000

    interfere none ring unlucky tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
