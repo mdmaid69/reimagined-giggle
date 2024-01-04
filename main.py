1f7394f522b6bbd60734b9edb2c59cd909670aa5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:00:47 +0000

    himself goodbye pinch likely television

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_area_rectangle(l, w):
+        return l * w
