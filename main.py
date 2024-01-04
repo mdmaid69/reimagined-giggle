ab111e147b49cb9e58ffc74944f9c64045142e0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:18 +0000

    ignore top needle call congratulations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
