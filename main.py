c1ba8407ca2601def2aa1e95e631555637ad4743
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:12 +0000

    bin plan wind invite copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def is_palindrome(s):
-        return s == s[::-1]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
