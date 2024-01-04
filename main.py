e9d4e243bfdc7cef035dac7f17850aa91dc8f387
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:40:46 +0000

    join soft general wet support

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
