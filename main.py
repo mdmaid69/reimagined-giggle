537cee2d2abd2a04c81267f607dee59be9e241e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:18 +0000

    travel bang harm suit support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
