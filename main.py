607dfc809711dbd6f4234b07e7030ef015eebd17
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:00:05 +0000

    trip favour most himself shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_power(work, time):
-        return work / time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
