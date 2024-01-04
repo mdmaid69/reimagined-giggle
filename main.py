04e671834c0a98e0f60f392769ee6a3303fed62b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:06:48 +0000

    wash straight of national computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_list(array):
+        return list(array)
+def sort_list(lst):
+        return sorted(lst)
