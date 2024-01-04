523e5d2a233b96866e04958a66f1d57bae241903
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:51 +0000

    soak or birth object substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import re
-print(re.match("h.*o", "hello world"))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_power(work, time):
+        return work / time
