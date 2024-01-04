3a89a60c5ff7763f02fcf1d6989e0932b5baa0e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:59:38 +0000

    form her average disagree godown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_acceleration(speed, time):
+        return speed / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
