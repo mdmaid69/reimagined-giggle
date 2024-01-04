0d303811b81dddbbd805d99cc8066a550ae0d92a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:27:13 +0000

    influence system cheek already famous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
