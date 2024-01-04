526d993cdd654e88070aaefe04f9b5cacac8a79e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:05 +0000

    pipe sometimes used attitude transport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
