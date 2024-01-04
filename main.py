ddefb06f079312eaf8bdcfd623a7069c45b426a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:10:29 +0000

    profit flow divide turnout crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
