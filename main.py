610deeb04f96e038ae8dea94e7ab9ae3b8076b1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:26:34 +0000

    cope talkoutof on invite joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_as_set(array):
+        return set(array)
 def calculate_density(mass, volume):
         return mass / volume
