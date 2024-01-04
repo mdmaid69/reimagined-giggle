882f98cc727722d810e262583a550217f703f05a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:34:57 +0000

    history contribute pleasure birthday schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
