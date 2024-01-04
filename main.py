f8f836af378810a1d6d5e880c2378c94f17cd451
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:51:11 +0000

    winter minute profit reply fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
