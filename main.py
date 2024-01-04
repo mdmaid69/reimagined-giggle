e141ff94c8764eeceb7be030ef4590c8178334c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:12:21 +0000

    good big thing convincing tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_power(work, time):
-        return work / time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
