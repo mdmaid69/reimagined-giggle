2a0fba747bf334c0114442d721f1aa68fcd72424
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:01:35 +0000

    manage fault correct limit interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
