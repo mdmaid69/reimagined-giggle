33071b28ff47492f7802bbab913c77e770b8f014
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:47 +0000

    tax completely callout tape fortunate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
