f2516992b5606bdb6038181a0e57a7c122a5f970
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:19:13 +0000

    more beneath reaction Ee present

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def add_numbers(x, y):
-        return x + y
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
