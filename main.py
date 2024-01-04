e3b04cceaf727262b1982332e75956cd20db84f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:17:45 +0000

    mother circle confusing alive rely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
