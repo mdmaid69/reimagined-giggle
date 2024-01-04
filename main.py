4914e288a86d4a96512923ea23c063688c2f5640
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:51 +0000

    set history behave lead quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
