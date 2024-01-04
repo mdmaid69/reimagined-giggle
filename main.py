ce00094ae4c5a328b41d7a250e56fb89c88f4231
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:00:34 +0000

    lookback(on) brush satisfied boat share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
