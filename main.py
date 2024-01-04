b400a5fc3905d013a427220ad8b3246a9cd097ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:26:12 +0000

    careless thanks Gg force family

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def reverse_array(array):
+        array.reverse()
