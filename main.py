6473c42000e1b09250a540d35bfec4ed6f2e5d82
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:58:10 +0000

    sink improve less tough surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
