ef59ab4bc14b2adeef63983a0ec701621bfcfc06
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:03 +0000

    sink water meal that relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
