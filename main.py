78ee1a45f9adf07fd79d6137677dd951b3f1d200
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:58:52 +0000

    advantage trouble apply stretch strike

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
