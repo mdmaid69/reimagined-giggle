68b524e7d992e0ea8ce5cc57addc891f2e16e6ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:05:24 +0000

    mine form clearly way promise

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+from collections import Counter
+print(Counter("hello world"))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
