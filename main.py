523207a5ce4f450ab47130a9b6815ead796f58d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:42 +0000

    bath goback(to) question full mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
