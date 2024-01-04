2615b71e1b1eae620dea93ee64f7bfb828ac8fd8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:28:21 +0000

    space habit search wind set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
