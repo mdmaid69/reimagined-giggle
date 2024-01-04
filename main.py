2b193096dd0573aedef3ca76588ae03d413b89b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:27:39 +0000

    stranger win plenty swim flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
