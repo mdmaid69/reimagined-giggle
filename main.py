324edbff5500b537f51ecc0ab42fe9c9a679863f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:15:23 +0000

    centimetre mine immediately maintain excellent

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
