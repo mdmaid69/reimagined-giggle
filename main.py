c5ca5664164ec56ec43c5b7fa8da4a17c06ed233
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:00:03 +0000

    dead copy unpleasant possibly warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
 def get_array_from_list(list, typecode):
         return array.array(typecode, list)
-import datetime
-print(datetime.datetime.now())
