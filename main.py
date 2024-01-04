a564b382e11cb14c3b8777385ce9ce15f4ef246e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:29:17 +0000

    high surprising final funny above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
