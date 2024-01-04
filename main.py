6ad4f2bd4b8e40ba54e96fd8a266baf1d3377d29
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:28:00 +0000

    sudden exchange ring dare quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
