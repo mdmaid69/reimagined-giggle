bfbce16e727f83a1e53ff9588a751957b0f69d5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:34:20 +0000

    answer answer almost drawing either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
