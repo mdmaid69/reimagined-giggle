c271ccff375ce4f0c32477a4e8f29cfd1b0d7c41
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:55:11 +0000

    expression clean judge fat swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
