6b2aa7d2dc34f78aeaa644744097d7a5db6e9288
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:52:31 +0000

    Ee care zero behaviour refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+name = "Python"
+print("Hello,", name)
