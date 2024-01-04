8a381e52856d1e74b8fd9b7d84676415164bb1a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:50:50 +0000

    down love save Gg file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
