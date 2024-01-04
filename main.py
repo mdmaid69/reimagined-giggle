a8a27c9282bfa2d5795412f7e9fd4d825cee7910
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:11:56 +0000

    community order tired serious whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
