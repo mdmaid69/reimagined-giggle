9cd7589de83c5d405258667650b893b57579b16f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:57:18 +0000

    insist transparent can manage reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
