04ef23eb43f5fbf7453e09e86974678c624b0c58
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:43:45 +0000

    habit port description matter anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
