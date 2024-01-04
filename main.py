ce9c6f90557a3fb8438fcff79a14d06c8e678a9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:31:34 +0000

    sense platform turnoff news breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
