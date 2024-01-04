f9fb3c32326fe50bd6174ac522b5428d74bfba91
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:53:54 +0000

    anxious new sad run measure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
