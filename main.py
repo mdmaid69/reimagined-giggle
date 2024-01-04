7b31edbcc33504ac8b582d3ad003e29111bff6ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:29:30 +0000

    far consist hour talk detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
