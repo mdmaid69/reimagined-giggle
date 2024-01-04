357b91e2d3365512617aac4ea313e0bce54b747e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:26 +0000

    problem instruction bet represent reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
