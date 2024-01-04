4c992f0726cd78f68806f2cb3530f4c890c13fac
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:52:12 +0000

    emphasis snow eye invite tip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+text = "Hello, world!"
+print("Uppercase:", text.upper())
