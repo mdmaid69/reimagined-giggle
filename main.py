d7a0b1c3f584ca112fe3c1657017537dc5725ea8
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:15 +0000

    takeover party store special early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-print(math.pi)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
