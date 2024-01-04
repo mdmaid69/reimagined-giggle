ed72c53b3a40e2be2634547544ce5a46303849a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:45:12 +0000

    unfair difficulty make succeed science

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
