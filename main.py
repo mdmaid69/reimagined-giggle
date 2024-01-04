ecb73740cd582dfc3f2ec41c08650b64b7d5f355
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:10:37 +0000

    area star smell cheap provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
