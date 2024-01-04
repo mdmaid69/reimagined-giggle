4b04d0d1b6ae19bdca901a7fdf852cb667fd1428
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:59:37 +0000

    figure hardly offer begin question

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_set(array):
+        return set(array)
