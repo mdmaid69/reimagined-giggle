76a2c040523044a054aaa10b0318fcd471596e3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:53:17 +0000

    attach small year development win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
