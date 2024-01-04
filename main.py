4e8ef681a6fc4d60249ea1d6d80310fc344fe786
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:55:48 +0000

    scrap then thinkover official insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
