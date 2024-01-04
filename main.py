59680add4846faf20628f75c779cd79e9468d84c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:01 +0000

    organize eat tax holdup hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
