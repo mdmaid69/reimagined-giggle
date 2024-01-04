98d67c6bfd50942ef82b54b3d4605abcd98a5467
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:44:08 +0000

    bridge fortunate bank study party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def find_max(lst):
-        return max(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
