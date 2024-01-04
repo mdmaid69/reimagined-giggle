e07ca9d721d1d44e8a25c28e03a5e665f3d38555
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:22 +0000

    in with sense match miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
