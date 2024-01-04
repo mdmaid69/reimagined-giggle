c115f8eb336395686ac66d935b393e21220c12ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:09:50 +0000

    begin cruelty use cinema Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
