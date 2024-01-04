5fd558e487a9afe7fdec3f366250524ff1025d41
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:30:48 +0000

    forgive across admit worth either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
