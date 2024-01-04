9fe8d9e397016adc8ba38cb4724654850cb293a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:01:10 +0000

    Tt scare open home formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
