5066f335bab1d1a59156850db538a9f25b200578
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:43:47 +0000

    return ignore direct violence callat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
