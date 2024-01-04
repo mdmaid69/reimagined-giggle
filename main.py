8da74de23cf2f66eefb6c567a306960d8e7acc46
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:07 +0000

    because flood although immediately path

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
