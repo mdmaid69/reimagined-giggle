0ec140a2c564c6c9c09f37e9c40cba75144203c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:07:43 +0000

    responsible error punish deal number

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
