b7741302465586536b1f588b59ac08bf1d0e37f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:07:01 +0000

    holdout sudden fast clean unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
