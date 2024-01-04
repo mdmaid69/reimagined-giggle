99e714e0ce37b7947a43db90d41d6d3dacf1fe79
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:28:54 +0000

    underneath glad especially general should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
