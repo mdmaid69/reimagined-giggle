1131179b3f82b54789f4ae325d6b0f5adad3bb78
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:40:01 +0000

    slow even rest walk pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
