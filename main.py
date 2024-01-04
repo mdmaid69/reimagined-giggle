d5645391b485884cfb265189c9744bea2b7e339a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:01 +0000

    music near die solve second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
