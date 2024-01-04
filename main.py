120e51acc2ce66925fed0946037f721a6c39726c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:26:27 +0000

    travel Itself congratulate post forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
