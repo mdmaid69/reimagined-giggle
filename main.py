5d7b228932dd0d32a6dc54d979bb5fe85a24e31c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:53:28 +0000

    but turnup closely warning sort

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
