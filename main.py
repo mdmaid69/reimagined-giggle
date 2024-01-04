a6166b447b940c3739cc70831f626cd96b223863
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:55:23 +0000

    neither anywhere process image correct

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import random
-def roll_die():
-        return random.randint(1, 6)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
