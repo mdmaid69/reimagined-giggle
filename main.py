1421416381439d80aa7048eff13c63537e34ac8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:41:01 +0000

    used divide rely control dress

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-print([x**2 for x in range(10)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
