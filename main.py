bbd8703e80bc3e0b93ae024fbf5a7e51bb7bb0e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:23 +0000

    goback(to) really tomorrow during wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
