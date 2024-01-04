713f48028850c7691aefe6da48e73571df93a815
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:14:39 +0000

    increase cool believe bringforward wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
