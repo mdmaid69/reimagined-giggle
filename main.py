cf59874028615267bc77c49fec8675c9a6add345
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:17:53 +0000

    store bringdown also achievement somebody

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
