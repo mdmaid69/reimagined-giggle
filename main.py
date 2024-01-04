8fc967c1511f6ce1509eb23db72134af5b8ba461
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:34:09 +0000

    conduct light sweet rush open

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
