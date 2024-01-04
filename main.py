5fb5392bfbd5b148dcc4b755822843b0ef376d6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:38:49 +0000

    trial stretch burn emergency beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
