61ac3d887880d3fdef27b15f0343044f2460d941
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:12:59 +0000

    find perform themselves or people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def find_min(numbers):
-        return min(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
