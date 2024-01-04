fad46b563b0122c1dc1a9a3ddffc568cb1f6776c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:23:03 +0000

    calm right wet inquire emphasis

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
