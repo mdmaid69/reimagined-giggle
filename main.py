74b405714ac0af6ec33afa0a4f1a37c0943e5814
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:47 +0000

    wild sleep centimetre half anybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
