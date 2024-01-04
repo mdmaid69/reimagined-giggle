4e1326e4e443c07314000ce757ecb61701a78bba
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:12 +0000

    branch physical instrument information path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+print(math.pi)
