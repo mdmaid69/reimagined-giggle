09e42378ffb5cae1c00468482bf79b0276d30b67
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:13:24 +0000

    apart proper vanish Qq apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
