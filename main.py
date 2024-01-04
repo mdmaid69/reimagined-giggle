b0f1cf816d68fe08adb3fed88a76dcb07fb1cf3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:18 +0000

    spend track ball watch myself

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
