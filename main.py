7a0c4be533a39070f8f6d20c855ee7472ccdd5a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:19:15 +0000

    know wander fat about ordinary

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
