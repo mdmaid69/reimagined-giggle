b16e4ca83a6b1f5d8c9eb346a3d975df9058f1cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:10:54 +0000

    discovery unhappy little neck flood

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
