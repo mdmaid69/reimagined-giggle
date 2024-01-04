0ad0f08fcd7b2501531dd1c4c1489206e6bd4c62
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:53 +0000

    sell straight estimate own stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
