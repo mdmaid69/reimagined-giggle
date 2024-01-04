e20de670f01da8ec7d40c982d80582927141944b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:58:33 +0000

    goout trick cost also wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_set(array):
+        return set(array)
