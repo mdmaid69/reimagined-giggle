2535e76de4039ddf4f87721df07725aadfd923a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:57 +0000

    defend interview justice level down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
