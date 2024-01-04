37b550ef61d76ef2f4b6439fc35e86379512d8db
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:48 +0000

    activity building satisfactory that give

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        return sys.version
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
