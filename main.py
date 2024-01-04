bcf2f56c4ffc124947d550e844f92c8672348d64
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:04:34 +0000

    rarely nurse appearance edge meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
