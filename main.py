a5f8f851ea3f794445c109c0ccb51e3cb3d83659
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:00 +0000

    only argument concentration shout note

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
