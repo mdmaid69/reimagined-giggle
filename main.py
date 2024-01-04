fdc57c8985e4c8273869fee9de31265dc8ba900a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:52:06 +0000

    history see all ability juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_length(array):
+        return len(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
