3a073ed67aa9e05f9e42b4afc07c6508c1bdb8e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:44:56 +0000

    on alive tip sudden store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
