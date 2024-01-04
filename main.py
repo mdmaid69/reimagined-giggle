6a3efa2d16f9508e2ce3fb6966662d9fa180ea79
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:52:16 +0000

    wander clue float whom develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
