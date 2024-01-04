fe21afc22507f17de3752ef28ddb76600503aed3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:34:19 +0000

    bottom maybe broad notice whoever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
