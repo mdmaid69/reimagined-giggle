a5774655f3503f4fbf7dbb97b15233e842d44dcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:33 +0000

    our anywayanyhow nonsense furniture proper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
