1114bff65fb71f04f3b0c7b309ffee61024d5acd
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:23 +0000

    worry glass naturally behaviour house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
