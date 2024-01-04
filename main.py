a8591ade107b8df310d8cf00bf47aace1abd8a8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:57 +0000

    hang estimate song peace estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
