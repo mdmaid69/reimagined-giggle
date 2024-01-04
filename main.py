062f164abd9bdcac175d3eb411852536e5f40eef
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:54 +0000

    moon on hardly market scare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
