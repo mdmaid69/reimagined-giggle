48f5bf440737def08bfd6725c0c60677e334f7d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:40:20 +0000

    defeat other succeed traffic programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
