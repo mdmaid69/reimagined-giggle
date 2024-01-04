d9b11127e820e12a2f7f2960b68c24a284c579ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:14:26 +0000

    yesterday fear nice guilty guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
