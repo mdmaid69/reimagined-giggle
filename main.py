c02f57f6d5d340443bbd6324c972f1aecebdf496
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:49 +0000

    hour deep medium run load

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
