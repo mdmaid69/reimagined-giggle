46f34e15895eda0785cc8f5b94be873cf3d292fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:28:41 +0000

    ordinary if piece measure interest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
