31e1775ab57d9b949421e196ed15e4c80c80fbd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:57:57 +0000

    roof better persuade if alive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def is_odd(n):
+        return n % 2 != 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
