a9b1667167eade56f77862c51edf65e28903d695
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:20 +0000

    wrap healthy freedom better jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
