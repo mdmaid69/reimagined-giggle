28af0b5dfe4e1ff2d146c406167bca0fed780dde
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:04:06 +0000

    lookaround join hungry image theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def count_words(sentence):
-        return len(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
