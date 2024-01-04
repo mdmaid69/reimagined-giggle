740fa0fe13c71b2e8e40f28ebfc521de1b985721
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:09:16 +0000

    sell dangerous judge better heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
