04871d94d35737f87c34e0f49894fd28645cf2a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:45:16 +0000

    the copy tight place strength

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
