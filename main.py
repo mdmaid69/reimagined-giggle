7bc63f7e0540cedca57779dc6f02199af9dfd0f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:39 +0000

    law impressed degree right rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+print(math.pi)
