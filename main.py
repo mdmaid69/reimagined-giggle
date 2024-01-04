0aa02d462d566a8bf0490622e1f054b2c23db05f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:40:55 +0000

    divide call book damage pool

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
