65665192564fa0c782d47256cd66310779147d1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:02:41 +0000

    pair workout sight past please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def get_array_as_bool(array):
+        return bool(array)
