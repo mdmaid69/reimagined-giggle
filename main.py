8a001d296d163161dfdaa44e597c2d7862e501ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:28:53 +0000

    watch change success target match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
