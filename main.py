3a039085c08527d574f66cabb251bc51d770a166
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:55 +0000

    put bite turnoff conduct label

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def count_words(sentence):
+        return len(sentence.split())
