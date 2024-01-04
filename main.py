2889033bf112eaa51381a76ceb7ee0ac0765e021
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:26:52 +0000

    month present knowledge worst snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
