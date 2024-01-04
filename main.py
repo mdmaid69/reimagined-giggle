464ea552a50a1a4e3758bf3bbe0ecdb83cac74cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:25:35 +0000

    nice limit information coat deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
