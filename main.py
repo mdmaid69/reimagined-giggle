60c0d1cdb28b7fdebe1caaa14b12a9fdbe2f1b8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:41 +0000

    protect down but moral earth

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
