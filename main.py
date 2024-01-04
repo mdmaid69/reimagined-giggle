13e83b7bb97469f581b33b9f56355c1b184f4e57
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:07 +0000

    spend stone cause year prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Uppercase:", text.upper())
