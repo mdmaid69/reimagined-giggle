d39d80801311dcd09664ed54ab119053eb2663bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:28:06 +0000

    underneath open makeinto disturbing violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
