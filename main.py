59c914327f9fe67d2c2fb5b2ba9571798b1a7175
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:35:47 +0000

    common moral comeover public consult

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_arc_sine(x):
+        return math.asin(x)
