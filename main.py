8211adfaa32b319b4e39519061da5240c810f3ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:17 +0000

    best second worth react loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
