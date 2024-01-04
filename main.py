177c81272b97245a962281e9425ff95f6f4de0c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:04:13 +0000

    care lie first Pp far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
