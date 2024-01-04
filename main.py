1f6888badf462f5701fdd86de60116348878f2af
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:48 +0000

    short class friend home moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
