80268cb3bab2ef58f721347a83b0095489b1c899
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:03:02 +0000

    tap naturally build false closely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def subtract_numbers(x, y):
+        return x - y
