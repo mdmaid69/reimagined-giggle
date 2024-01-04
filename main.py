425ec1acd813318fd4d8c7c3cec0acaada396477
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:37:05 +0000

    after Kk move whole provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
