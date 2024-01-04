401a3b0cd5b82e749358a6364f5fc87a2450e63d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:47:26 +0000

    question exact effect unable us

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
