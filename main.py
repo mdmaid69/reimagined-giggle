19e56f8f7aee790e5a406100d165d6b051486a1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:24 +0000

    feed shout complicated piece out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
