52039ab38e78839ce1eb203d32731239035e2101
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:06:31 +0000

    picture actually reply into treat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+for i in range(10): print(i)
