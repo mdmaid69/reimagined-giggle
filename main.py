bc3e1468a8a61b040d1d346b49ad9d49510e75c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:45:27 +0000

    divide market limit crash he

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
