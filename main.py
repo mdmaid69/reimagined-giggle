64cdda2c94cc362fe6ea38908d1d03350a652dc6
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:47:11 +0000

    character suddenly next land today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
