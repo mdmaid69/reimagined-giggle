5c1c37e07430d176c2fcd780afc76364110af3c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:27 +0000

    enthusiastic state pleased push on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
   def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
