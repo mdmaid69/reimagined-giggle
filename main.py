e28a8884e21b0cebd3249714c68f923b69998c30
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:21:31 +0000

    block approval announce 567enemy produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
 import array
 def get_array_index(array, item):
         return array.index(item)
+import sys
+def exit_program():
+        sys.exit()
