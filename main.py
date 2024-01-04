dbef373f8421ad80ac0dcd7d0a05ffb0fad08e9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:47 +0000

    bringback steady continue powerful Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_max(lst):
-        return max(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
