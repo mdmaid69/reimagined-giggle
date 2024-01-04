bd0d64c43b4324c507167dcf60319cedec2f63d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:55 +0000

    bringup they embarrassing fact letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
