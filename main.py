2393680452c0069422461b506a4f8b324357f400
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:49:54 +0000

    tongue refer improvement account flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
