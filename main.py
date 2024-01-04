b6ddcf5e675d74a204fd785ac52ff28688a1f85d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:57:55 +0000

    emotion down slow steam connection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import collections
+def create_stack():
+        return collections.deque()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
