c5b4bb649ce2bf9a3f0cd5938a9272adcbcc7038
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:44:31 +0000

    thankyou bad design study shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
