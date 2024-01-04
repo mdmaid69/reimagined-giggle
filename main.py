6360b726bf3a64d5b9171784a317ddf65b1dcec4
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:24:43 +0000

    birthday single likely hurt slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
