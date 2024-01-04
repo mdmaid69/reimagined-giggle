9a69582e94bb34c90ad446d7561da4adb532f623
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:42:17 +0000

    lookback(on) contents object operate train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
