85436af23f873c9f87c67e36c3321b9f11ab2672
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:25:06 +0000

    better and deliver operation even

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
