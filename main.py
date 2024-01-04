ff3af23b3c92fa01d2c047e2c0278353962b95fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:46:49 +0000

    on OKorokay along pause fortunate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
