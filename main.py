768831166ac1fb60a4d0a9016be4a24b8befab2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:45:57 +0000

    disturbing steer official lock church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
