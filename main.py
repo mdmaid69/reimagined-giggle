72836066e33d4910040e420b6907c5bb7c6061e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:34:49 +0000

    big beginning deaf still search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import time
 def wait_for_seconds(seconds):
         time.sleep(seconds)
-def count_elements(lst):
-        return len(lst)
