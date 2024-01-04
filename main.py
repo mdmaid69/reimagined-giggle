a7ee962ce9fed04ff0dcd19ae7974e85d831dc63
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:42 +0000

    right minimum learn schedule only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+name = "Python"
+print("Hello,", name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
