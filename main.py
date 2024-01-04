0d900b8148869ac0cb7647cfe6eaa3353003e8cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:17 +0000

    whether outside fly push pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
