d34a4c1429cfae6c269d30667125d9794ea2f8dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:46:37 +0000

    wish same create fork get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
