294e07e386e4e9d121777a5f08e89cd543c4da21
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:13:12 +0000

    she condition this illness support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
