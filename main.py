58f3aa5c932d8ee70b613b86ddb806428a52b6e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:52:49 +0000

    cheek remain tough turnup choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def get_array_as_bool(array):
+        return bool(array)
