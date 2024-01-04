db16e0d0b078c8d0859f07aa9a10218592878236
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:08 +0000

    collection recover year declaration pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
