5cc522a13ed19833a967fb51549a056c895e9f3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:29:41 +0000

    price cook base order live

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_odd(n):
+        return n % 2 != 0
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
