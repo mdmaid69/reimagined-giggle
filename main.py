148c76d728e1a66497d20622bfacaad11b21752c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:32:22 +0000

    opportunity outside case our area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
