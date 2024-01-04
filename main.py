9a86ff62587f1778a7f27eebb2f4bce27c705129
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:11 +0000

    doubt closed temperature war cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
