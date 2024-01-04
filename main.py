4aa50530856d04a89fa2b2709b46d157f8e8626f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:19 +0000

    uncertain slice positive mistake century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
