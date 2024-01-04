c4dfa2421df98353a6a1363667f9a77a9180fcbc
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:21:29 +0000

    plain sometimes pull about discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
