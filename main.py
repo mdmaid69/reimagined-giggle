920a7e03908d6bab52d8cd34f27d2c5ab173145e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:17 +0000

    occur proof ours hope thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
