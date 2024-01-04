ebcae17748893e9b07242feac084ffa1668d23f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:08:22 +0000

    complete pocket walk clean capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
