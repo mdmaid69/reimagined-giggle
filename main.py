941ea499d60301bd6dd9dd484c3fa1a6e09f4b78
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:06:18 +0000

    calm workout use challenge violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+print(math.pi)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
