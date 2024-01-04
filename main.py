8e97fc695e0b70862d31ff1da2b42be32b76f186
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:14:24 +0000

    mainly generally complain silent trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
