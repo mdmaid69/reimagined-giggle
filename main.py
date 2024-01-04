adb872e97c4406f012ae82b92d737512f8e68672
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:04:24 +0000

    corner ready raw own cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
