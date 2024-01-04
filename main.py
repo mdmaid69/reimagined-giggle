21f5e4054546bf6edd836b4a79927e113fbfbbe3
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:26:35 +0000

    consider offer stiff post tap

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
 import smtplib
 def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
         with smtplib.SMTP(smtp_server, port) as server:
@@ -5,5 +8,3 @@ def send_email(smtp_server, port, username, password, from_addr, to_addr, subjec
         server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
 {body}")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
