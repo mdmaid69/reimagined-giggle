63f49351e951ea25c0c5c110a69b01f213b8b975
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:29 +0000

    test room round cover embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
