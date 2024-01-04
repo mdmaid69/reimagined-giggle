d789fdeb95ef4c7c2b4abe7aa411d81640b8557e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:20:24 +0000

    second bread breakdown practical swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_sign(x):
+        return math.copysign(1, x)
