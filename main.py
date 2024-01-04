c35d1726f86e1c4b536a5c4ca815201be59db360
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:09:59 +0000

    even tooth oil in anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
