60dbb621d2413baafa5525e8682f2e1c3e52d7ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:03 +0000

    holdup hard thirsty basis congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
