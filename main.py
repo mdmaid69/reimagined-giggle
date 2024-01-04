cb15ca1eba98e54980b5b80f13b248e27bee7bdd
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:04:53 +0000

    aim piece uncertain release amount

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
