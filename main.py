af43e9009760d141d36fa4693733573daa68c7b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:56:31 +0000

    seriously sometimes plain contrast lead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
