4948310d1547cc85641fae6fb62b6dcfd6e3bb7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:11 +0000

    afternoon happy introduce sticky challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
