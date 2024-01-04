24af07c24089f2ab6e3edbfa6a79e5a4862112c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:39 +0000

    clue murder accommodation model perform

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
