1ecf29122a57dc0dc010d020db84ce0e1970adb9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:34:40 +0000

    comeup food aim still warm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
