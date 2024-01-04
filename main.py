e3b8705a7be7f2c90a4231ad65958acf3ad95a52
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:48 +0000

    fortunately coffee Ee injury mass

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print(sum(range(10)))
