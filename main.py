207cc0ce64b9f380c5b898eab6f8f37ddd0436e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:44 +0000

    persuade letter tell never 569enjoy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
