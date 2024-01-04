72ced820540086096d521a62a069752160b9b4cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:09:58 +0000

    appeal us deal interview appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
