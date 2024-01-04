336526df094b52eda126372de4ab5ade52b60184
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:00 +0000

    scream wall degree cruelty let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
