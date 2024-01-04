4740bd881e56a96329f82b7ad033a2f41ddf87e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:31:14 +0000

    upwards injury could blue file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
