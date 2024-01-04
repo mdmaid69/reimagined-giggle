70f6a63da5f11181e8950057d916be1f73a471da
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:33:12 +0000

    call broken desperate block damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
