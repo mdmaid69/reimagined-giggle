fdfc0cfda9b972332f3c9f40c0ac5a70e908f1b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:12 +0000

    habit raw noisy window proper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def reverse_array(array):
+        array.reverse()
