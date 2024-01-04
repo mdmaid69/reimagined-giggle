585a0f09fe1e50bf751e2c7de1c711e6125dd978
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:19:45 +0000

    broad people cross work union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_current_directory():
+        return os.getcwd()
