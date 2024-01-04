afb3201348018f91b0ae9dcbeeaa1fac4e4a5b16
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:38 +0000

    attempt middle unite quit passenger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_user_string():
-        return collections.UserString()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
