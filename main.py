8d2e9b58cb06d76ed92a937ca67dc29833165557
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:06:10 +0000

    568energy rare cruelty defeat hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
