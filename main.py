2838192e84ee28fb0bb307fde4e68c28a71f5b03
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:35:29 +0000

    tough condition unlikely success person

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
