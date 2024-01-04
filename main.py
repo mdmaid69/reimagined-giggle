5421cd6173843475783c3e0a1413a2c3f1680085
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:09 +0000

    nearly down deep start receive

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
