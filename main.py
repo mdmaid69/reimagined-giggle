917b488f154ca58f21b3c6504bef82bca6f2b82d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:30:14 +0000

    with smile file back seriously

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
