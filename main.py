41d1889af3fb826b6f7123e714fb6b9adb262f36
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:56:07 +0000

    passage commit speed trial heavy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
