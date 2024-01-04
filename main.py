c9d8ee2036c353e536a7d3304d161cff6eaab7f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:06:07 +0000

    present hello grow region broken

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def is_even(n):
-        return n % 2 == 0
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def square_number(x):
+        return x**2
