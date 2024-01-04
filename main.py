e0d4c71bea334a0fe5e70de54915e5a0d47f6e29
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:35:47 +0000

    tonight ring land mess Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
