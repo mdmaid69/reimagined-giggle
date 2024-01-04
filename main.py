7d2bc307f803b53a527f7e2e7d4c24dc7ae00626
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:51 +0000

    public none to dress interview

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import getpass
+def get_username():
+        return getpass.getuser()
