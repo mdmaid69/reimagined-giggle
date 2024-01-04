f57e8690dc375730612d73d977efaceacba26a90
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:36:38 +0000

    young add escape later ground

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
