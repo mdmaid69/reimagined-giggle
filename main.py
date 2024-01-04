b1d772fd659e1db39a560c717406a2458ffcbebf
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:01 +0000

    future use collect collect sight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+def log_message(message):
+        logging.info(message)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
