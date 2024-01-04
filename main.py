34dbe8bceb18eb5e327207ffc8f75750a4a25c63
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:29 +0000

    stage depend through me thanks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
 import urllib.request
 def download_file(url, filename):
         urllib.request.urlretrieve(url, filename)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
