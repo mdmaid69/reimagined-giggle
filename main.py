725019b9994b6cf5dc497e7f0c2c3bc7ff08124f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:32:04 +0000

    set identify black Uu Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
