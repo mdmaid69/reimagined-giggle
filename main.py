2801fc6f8c58f21e482c6a05937366f003f87136
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:52:57 +0000

    limit yes me busy move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import sys
+print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
