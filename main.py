e3f862d669db97282d66bf181fffb91538d38619
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:08 +0000

    about flavour offence defeat pause

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 def add_numbers(a, b):
         return a + b
