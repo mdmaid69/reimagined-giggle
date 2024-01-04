9c7019e7149fd941e2704166497af58128f63844
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:46:58 +0000

    uncomfortable previous Oo rub bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_area(radius):
+        return 3.14 * radius * radius
