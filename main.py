f6b5c4ce28bedc97c3cb6b78ebf33d296845156c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:43:36 +0000

    centre expensive keep past stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
