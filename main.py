c1a7b5b0a9deed9cc7ec1b3570138cd9ea6e9ab2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:53:28 +0000

    any threaten record keepout mad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print("Hello, world!")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
