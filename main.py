432fe451bd09a11e783049a0f9fa23f4124f352c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:24:06 +0000

    respect rubbish name successful future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def convert_to_binary(n):
+        return bin(n)
