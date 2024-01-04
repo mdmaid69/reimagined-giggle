e2a6e8dd4572a370acd88132fb52b62ccc40ab1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:14 +0000

    branch far innocent tone snow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def convert_to_binary(n):
+        return bin(n)
