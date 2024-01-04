d04ac55419e07e132d4194f16c4b3fc024649d9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:23:07 +0000

    example Bb tense public condition

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
