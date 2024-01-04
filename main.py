aa88dadec39826fe8d5ba684df3efbb895f0eb50
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:34:27 +0000

    container stage interested convenient sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
