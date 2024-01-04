ebbd0f3d4556694b5fe24e5a0fce5922ecec672d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:23:07 +0000

    guilty count breakup bear birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
