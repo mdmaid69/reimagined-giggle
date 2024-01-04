27f3d7e8efda0d4663c0ccfdc7068a79abca99d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:16:55 +0000

    measure contain surprising today hello

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
