1d380d5b86901bb0b9b75ae7d21704de0467ff5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:46:46 +0000

    worth valuable possible arrange job

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
