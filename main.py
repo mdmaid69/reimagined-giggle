9f2573929ba1af58161fd12e74205e62fea07fc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:42 +0000

    underneath since repair secret weigh

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
