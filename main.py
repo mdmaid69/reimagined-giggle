3ee770111b4bb6120195a25106ce1d8532433159
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:29:27 +0000

    usually my thin view whistle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import json
+def convert_to_json(data):
+        return json.dumps(data)
