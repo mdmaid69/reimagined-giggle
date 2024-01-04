91cd27b79bef17a8f86f5eb6b65133e2d988a3ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:47 +0000

    persuade callout business rest list

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
