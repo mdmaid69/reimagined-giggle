8ef2ab83d9a7b33ccd91995b450874d6f6382ca3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:59:27 +0000

    passage ride surface myself hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
