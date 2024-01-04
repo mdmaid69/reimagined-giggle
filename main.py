01fba6bba67a360930177f74d5bc0e9398f29f62
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:35 +0000

    leg emergency dinner very power

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
+name = "Python"
+print("Hello,", name)
