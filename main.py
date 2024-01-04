36dd0523ec3f9c45072a759471056f5f208c4f4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:55:24 +0000

    rarely reply something by guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
