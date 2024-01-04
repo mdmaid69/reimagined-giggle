903f30193e20c2d5f1e89be2fdf226e77593a42f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:41 +0000

    shadow provide lock fold exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_item(array, i):
+        return array[i]
