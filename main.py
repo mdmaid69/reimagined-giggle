05f2911740da55713d5ba502f59d028349aeeabe
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:44 +0000

    apparently call snow normal case

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.time()
-import os
-print(os.getcwd())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
