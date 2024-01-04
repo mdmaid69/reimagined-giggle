386081ef2e5b35a3d2c8ee5257ba95634904b501
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:28 +0000

    together card dangerous badly determined

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
