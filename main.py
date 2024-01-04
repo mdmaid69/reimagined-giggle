c5ac202938cb8a38dc40814ef77d66fb4afc07d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:23:48 +0000

    circumstance minute I connected shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
