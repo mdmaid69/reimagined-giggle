6bb8778453e3ea1dd5429ecc9adf30c7a73131a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:45:30 +0000

    spray positive friendly trouble pronounce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
