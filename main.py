89197dd6ebab80ec9bf09ecc876e5e60cfc35a8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:42 +0000

    typical constantly across evening advanced

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def convert_to_hex(n):
+        return hex(n)
