6f31da5afa11fc5fed1f8e26dcb18a79f214b83d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:46:56 +0000

    time able thick Ee land

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+print("Hello, world!")
 import csv
 def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
