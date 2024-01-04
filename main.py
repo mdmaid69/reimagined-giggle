8bf138990214f98ed1b8d1a250a2fae28bb438b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:46:21 +0000

    rich corner brain runaway sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
