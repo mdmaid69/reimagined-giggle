d9d017b550dba7a51ad4d4c120194b27795d26bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:17:52 +0000

    listen favourite crime love famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
