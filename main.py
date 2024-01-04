1b26105a67afaaf966d5e5b5c420c7d15586fbdd
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:49:00 +0000

    trial card back go might

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
