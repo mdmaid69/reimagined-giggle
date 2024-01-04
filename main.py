be50a34fdba9bd5cd124cb860569717fb32f0713
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:27 +0000

    pay learn gap backwards crash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import sys
+print(sys.version)
