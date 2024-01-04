94fc5b3d52d437f5daf1937506b5436f743ae0f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:22:33 +0000

    race attention down worth hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
