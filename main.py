75e6f0d04052f790b6a382909daced5f2bd994ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:49:57 +0000

    Yy operation science married later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
