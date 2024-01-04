36c0bb605eb7728b899839d75f236f1f98a8e051
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:40:05 +0000

    Dd maximum rather perfect sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
