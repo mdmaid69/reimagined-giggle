aa02d1f830e10e122d043780ab082d2d2f2087a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:33:26 +0000

    callout actual check leave principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
