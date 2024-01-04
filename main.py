371f3f8c18d61c78567a56ec164676fc848fdd70
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:47:22 +0000

    cope praise sweep until after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def is_odd(n):
+        return n % 2 != 0
+def find_min(numbers):
+        return min(numbers)
