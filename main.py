5af6ce109d7fd1cf355f44df24ca0785dde60c57
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:23:42 +0000

    preparation trip argument bringup which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
