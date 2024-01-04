1de9743eb4a54e9f0644ca1fa4cc858a49278c7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:55:59 +0000

    embarrassing play area post boat

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
