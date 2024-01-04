e5dda24bfdbaa5f6c29d0410f136dab9cbfbb6e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:25:21 +0000

    crash rank smoke gather manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
