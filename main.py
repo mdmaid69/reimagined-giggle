6b168ca167b26ed4a4ebb05fbdd6fdcfda0af1d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:00 +0000

    wealth my mother shape advise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
