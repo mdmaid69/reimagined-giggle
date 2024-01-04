2a8ff923917191f5f39be796ca96b1695ae6fb4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:58:56 +0000

    notice wrap store break experience

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import sys
+print(sys.version)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
