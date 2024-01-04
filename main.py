7f91e0c0242537efe6ad6929b71601aad744b42d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:58:38 +0000

    cheque expect government Qq appear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
