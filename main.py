eeee6ba74846e4b25238e64576d4f49f5f062ea3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:13 +0000

    base stop fill eat indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
