80510fd3387220c84ce871f6c64eaeda253ccfc7
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:17:15 +0000

    brush public unable break they

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_pressure(force, area):
+        return force / area
