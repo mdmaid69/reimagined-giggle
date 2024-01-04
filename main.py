a88c207369faeb64c7289b5ee40ff8444140f619
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:46 +0000

    tape month admit mark heart

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def square_number(x):
+        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
