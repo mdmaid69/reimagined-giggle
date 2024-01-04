ba0c4a9e55d8364ae72a65247c1588bdca3364ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:43:37 +0000

    tape exit favour steam treat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
