0dfe484fe7726c646f538575ce8cec96269b12f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:49:59 +0000

    interruption usual worried material dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
