9ce842b37e4790c4854a137239d0e3875c9d03af
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:29:15 +0000

    wait plastic everything Ll thinkover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_power(work, time):
         return work / time
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
