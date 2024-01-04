6140e1f158c7ab43a67210fb85a19fadb2601794
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:05:42 +0000

    pollution fast alone keen home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
