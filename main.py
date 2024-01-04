69501b9e3f426da75ce1a6b8dea27d112c6ff3f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:05:01 +0000

    exciting doubt tongue fresh ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
