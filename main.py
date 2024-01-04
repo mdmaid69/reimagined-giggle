7553bbb25a0a077606b253e65ac40faf0ba034d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:46:05 +0000

    teach repeat snow food earth

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def square_number(x):
-        return x**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def is_even(n):
+        return n % 2 == 0
