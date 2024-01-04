66c9be8f01f9bdece31dff46a09c59deca1c55d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:48:42 +0000

    practical term moment pleased town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tensorflow as tf
-print(tf.__version__)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
