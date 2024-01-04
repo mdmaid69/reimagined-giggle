1c65982e3071a928dd0a123a900115573b700e0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:44:11 +0000

    news tongue sleep bad appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_username():
+        return getpass.getuser()
