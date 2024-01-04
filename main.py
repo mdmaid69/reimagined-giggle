eebb07936211039af867eb99129793e9bee7b05b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:54 +0000

    earn direct whichever prevent run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
