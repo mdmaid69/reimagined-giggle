9dd5a7f0032d58d6ac703379fdd67595d5337afb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:23:07 +0000

    about bath breakup fault eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
