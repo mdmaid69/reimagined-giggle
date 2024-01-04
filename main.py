c51c4038cfd51612bebe4dda4bf524dd0c6efafc
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:43:41 +0000

    target violence forgive spring book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
