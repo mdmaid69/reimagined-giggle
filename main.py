0495654a670d638ead7aec11a88adf0bfcf656be
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:38:13 +0000

    previous tape water bush business

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
