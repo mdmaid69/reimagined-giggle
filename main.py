fb8397491dd04266cab28364a0ac4708b4cfc06c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:34:51 +0000

    wear ground pause rest murder

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
