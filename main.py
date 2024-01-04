3efad77fb6e55229d71cd9db2d207e4c7c0104c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:54:24 +0000

    battery absolutely false moon ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+for i in range(5):
+        print(i)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
