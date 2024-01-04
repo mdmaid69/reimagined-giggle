a6c557086bd1b2058b38af71227191bbdfac5982
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:04:20 +0000

    should colour new factory throughout

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import logging
-def log_message(message):
-        logging.info(message)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_item(array, i):
+        return array[i]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
