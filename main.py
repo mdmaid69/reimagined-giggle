3379fbadb110a5983251fd1f1b6e41e2fa576783
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:25:22 +0000

    eat face black from accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
