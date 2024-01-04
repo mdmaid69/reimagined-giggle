d6e9b3299c0e19ba90cef6423f5ab1075ec3197c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:00:00 +0000

    refuse prefer soon continue bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
