ba5687f7b16345dd347d5e03d519c5378465516f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:14:30 +0000

    thank relative finally whole music

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
