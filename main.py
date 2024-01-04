32e98e67eac4e8847152c0aeb8dfe0e0dc58e211
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:40 +0000

    ignore comfort fight spot unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
