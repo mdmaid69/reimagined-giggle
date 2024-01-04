566824d3c513c907d5260c74816a4609df717ccd
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:36:47 +0000

    keepoff blow develop act ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
