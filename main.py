69fa44dcc2326951c56b552dddbc0ec976cc357c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:14:46 +0000

    patience part pattern rain wing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def is_odd(n):
+        return n % 2 != 0
