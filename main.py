02880bdae3b2d00154f15410fcbfbad76f3f0533
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:22:24 +0000

    beside evidence stick beside frequent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
