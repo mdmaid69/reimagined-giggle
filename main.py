984bbe6a7a4bd76f85a7636472323e688d6a5f14
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:51:32 +0000

    never certain roll bang slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
