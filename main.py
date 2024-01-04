b49ffd5fcde5010125414ed33c5f70579b0aa56c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:39:22 +0000

    almost immediately owe commit sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
