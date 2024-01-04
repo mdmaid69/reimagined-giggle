4950f798ab6397efd2eb369857b7e1b219d2966c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:20:45 +0000

    careful ready pinch swallow keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
