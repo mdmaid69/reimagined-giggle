fb1c289354302d708fdb6452f1de8f08034ca81c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:56:10 +0000

    movement contrast hurt advise pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+text = "Hello, world!"
+print("Characters:", len(text))
