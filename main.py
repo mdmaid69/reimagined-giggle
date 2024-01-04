16c746f75bd1afc7999e37e5202289be34c39801
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:05:36 +0000

    doubt complaint runinto possible listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
