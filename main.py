43db79831dc6d107661580ce70c86af8f6d939c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:23 +0000

    priority well level fond trust

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
