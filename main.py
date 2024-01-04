88c565d4f27c48b485bbf8a29eb8aa308417f3a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:59:40 +0000

    relax release protest cross I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
