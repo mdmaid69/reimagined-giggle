6be6b351861cc4e5d7ffd898679c5b84accdd315
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:52 +0000

    before Hh plastic correct set

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
