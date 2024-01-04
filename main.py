e308ff99fd917a26df4feb92424b680b4aef34c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:13:20 +0000

    town crash correct recent practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def divide_numbers(x, y):
+        return x / y
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
