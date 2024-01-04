95f0854955edfda91a01376945fd150efe0833c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:45:27 +0000

    one account scrap basic by

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def square_number(x):
+        return x**2
