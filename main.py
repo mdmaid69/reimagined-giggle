6dc1736ea131ad4abeb3de9286766c4837460ede
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:00 +0000

    make period right no rarely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_power(work, time):
+        return work / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
