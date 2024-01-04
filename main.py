9705a218d356e53a5f5667aa633b32c285a2220b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:20 +0000

    apply suffer rest yours breakup

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
