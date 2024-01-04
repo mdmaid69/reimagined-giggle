fe42f714d48314f928a1610bf006814d8ab97f91
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:25:08 +0000

    everybody experiment steer because can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-name = "Python"
-print("Hello,", name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
