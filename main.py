c98500de6ccfd9febf1793236bbf5f4776e23189
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:41:13 +0000

    regional cold vote open country

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
 def get_array_as_format(array, format_spec):
         return format(array, format_spec)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
