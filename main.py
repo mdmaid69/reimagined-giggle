d4b8d1f31f24b167b6f79f61b4961480b2e82853
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:15:21 +0000

    talk scare skirt change willing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import datetime
+print(datetime.datetime.now())
