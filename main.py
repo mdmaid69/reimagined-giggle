16a0d13ddf6516244794b3b4dd283dcd59e671fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:24:50 +0000

    will suffer twist church attract

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
