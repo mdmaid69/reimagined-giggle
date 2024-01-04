528d6fe92679ca937fa6f1c724314bba3fb65e6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:50:17 +0000

    collect weather window light corner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import sys
+def print_python_version():
+        print(sys.version)
+import platform
+def get_python_version():
+        return platform.python_version()
