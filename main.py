33e489b020293b7c589135e60a4719ce2b3dfc75
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:51:43 +0000

    judge hurt shut or shoot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_work(force, distance):
-        return force * distance
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
