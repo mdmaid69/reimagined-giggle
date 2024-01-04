441c9025e695672771ca869dcf644c3cb77e00c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:06 +0000

    hang borrow have country miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def remove_duplicates(lst):
+        return list(set(lst))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
