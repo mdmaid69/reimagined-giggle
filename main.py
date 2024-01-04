fefbbe01decc1cdeda16ec175ffb275ab5532b4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:00 +0000

    although brother open hurry company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
