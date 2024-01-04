ca22ec003b920876be7ce07e7e376967396b5c34
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:38:58 +0000

    behaviour official car reject cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
