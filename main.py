775016409cd0dce6a2855d9c6ff33c4b437efe88
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:44 +0000

    case range knock fun insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
