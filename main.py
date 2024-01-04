7177091adbabdfd67bab292c006e827d06d8db44
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:49:03 +0000

    related mail thanks polite receive

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_power(work, time):
+        return work / time
