2c112c4e0ecf7d329365bef5953997a7cb11fd5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:22:44 +0000

    generous risk building detail government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
