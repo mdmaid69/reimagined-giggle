2e775899823d62be97218b69c9b583b1283924e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:07:50 +0000

    apparently reduction flow persuade produce

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def count_elements(lst):
-        return len(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
