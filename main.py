d41549720b50ebaa26f968059654cf49c590b122
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:24:04 +0000

    slide ring mother destroy needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
