efc1104423e6f11cad73a07e0e57d6dca055030a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:49:49 +0000

    afraid consult possession fix post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
