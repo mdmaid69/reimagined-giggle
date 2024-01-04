b3d08f5f658c5a042d1fe1fe926e60a230ba8456
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:53:29 +0000

    sorry rent drop 565end period

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def remove_duplicates(lst):
+        return list(set(lst))
