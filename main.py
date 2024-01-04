360d0023edafe119d9b8142384ef12273800a5f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:16:31 +0000

    embarrassed respect spin difficulty remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
