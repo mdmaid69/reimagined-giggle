1be9a15be36598513baadc8f37f828ad3550d93c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:46:16 +0000

    let sure ask black cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_as_list(array):
+        return list(array)
