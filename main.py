5b1bb941098a894685ccab39d6c4824dab696f49
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:32:48 +0000

    glad breakfast comfortable plan many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
