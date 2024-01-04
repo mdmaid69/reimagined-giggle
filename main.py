a6dd988d4b8d6e6659b0450bea7117d449450751
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:09 +0000

    straight tragedy same use swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
