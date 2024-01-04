1d9fce4a1943ee2ea5e93a87341bc3f252a29ab8
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:18:51 +0000

    somebody force warm shape quiet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
