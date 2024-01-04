da117617f21b098517d0de9a4d69c11b98cc6330
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:50 +0000

    reaction sick delicate laugh spread

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import tensorflow as tf
+print(tf.__version__)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
