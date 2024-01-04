f8625c301f14a709f56b65f1229fe20f303f953f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:27:51 +0000

    every seat punish before mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def subtract_numbers(x, y):
+        return x - y
