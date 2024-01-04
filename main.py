097788fd3fe73cd21c10a2ac7d14f63b623ace56
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:52:18 +0000

    false imagination 564encourage party obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
