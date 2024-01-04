2ddd09655cb2ba77056b3155373cb23a0d0b5fbd
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:16:39 +0000

    bringup avoid hope do broad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
