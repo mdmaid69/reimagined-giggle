cd6b89d5f166c902c889bcf7465544367a501755
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:53:10 +0000

    intelligent fail ourselves collect knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import platform
+def get_os_info():
+        return platform.uname()
