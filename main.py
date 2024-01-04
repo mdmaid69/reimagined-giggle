2c64f7f5b61ce6ea49efadff46317fe5dc28a176
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:05 +0000

    deep set everyone arrive period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
