982102afa89ebed44f34d8429518298bb977f1e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:08:33 +0000

    only keepout food search fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
