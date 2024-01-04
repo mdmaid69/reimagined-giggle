d2b655262d7830669af4d9e41e0ddab44d497d2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:46:04 +0000

    always shade suppose score same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
