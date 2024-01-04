d3d154f28e9fab9a7ba9f584a9ecd0efc9e27b32
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:40:18 +0000

    target fairly goback(to) complain year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
