92d27c2b4da908cacc12056b577ad9bab758b32c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:48:32 +0000

    world hardly where best impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_stack():
-        return collections.deque()
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
