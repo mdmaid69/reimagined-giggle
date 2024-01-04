e3d470068a09fc33a050fb585c49415c33978d7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:56:37 +0000

    shut normal uponon creature year

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
