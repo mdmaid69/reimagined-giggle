8ed3c9ad713f5c580197e0d824e7d6a854adc267
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:10:28 +0000

    shoot forward sometimes last proper

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ def print_hello():
 thread = threading.Thread(target=print_hello)
 thread.start()
 thread.join()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
