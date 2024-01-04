f7ef8753dfd7b28721bfde4d6c76139d79447b96
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:46:13 +0000

    apart space fill name emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+from collections import Counter
+print(Counter("hello world"))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
