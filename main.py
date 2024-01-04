702e7f752fb9468a2783d9d6b23a7fb404ab66c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:17:47 +0000

    cut join centimetre climb power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-print([x**2 for x in range(10)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
