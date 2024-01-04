27a645a5a51a5c85a21e5f64bb730222974c7dc6
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:45:23 +0000

    peaceful hungry trade winter somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
