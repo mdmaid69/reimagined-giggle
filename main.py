c9fa8971685dbdbebf12da13c9a9d27790cbf6a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:23 +0000

    try oil sharp other hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
