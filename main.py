60c0a6dee164a3b1d3d84e29c23d0ef09785f743
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:07:47 +0000

    sweep back sea difficulty take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
