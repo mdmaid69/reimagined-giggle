86a917f0a7f1138956da820c64c953f6994e021d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:02 +0000

    willing uniform getback mind case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def roll_die():
+        return random.randint(1, 6)
