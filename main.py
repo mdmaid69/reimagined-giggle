2db0fced1fa2e8f0774f7ddaa65c705689721bbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:59:34 +0000

    hollow region friend possession finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
