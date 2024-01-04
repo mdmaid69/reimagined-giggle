c0120919060987d1215094e18d2a358b45cb9468
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:58 +0000

    face sink begin basis shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
