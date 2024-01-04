ce96e3cd226082380190ac7eb53cd30602e22f52
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:30:36 +0000

    shop birth plate expensive maintain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_int(array):
+        return int(array[0])
