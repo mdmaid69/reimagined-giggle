882f741ae1d5eac673200d0b8b06d62beef7e9bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:44:40 +0000

    system bored fast she ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
