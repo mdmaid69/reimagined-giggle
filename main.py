88fea6539981014410fd430ac5cc98cf073cac4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:27:43 +0000

    petrol level sea takeaway downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import datetime
-print(datetime.datetime.now())
+import time
+print(time.time())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
