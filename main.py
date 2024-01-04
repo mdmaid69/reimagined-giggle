dcf73a20ff12a83f99c3e026a1c841795d42baaf
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:28 +0000

    female range post trick hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_min(numbers):
-        return min(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
