877d565060246871d49a8bfccbb24d58e09f0372
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:07:49 +0000

    centre mean busy loss this

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
