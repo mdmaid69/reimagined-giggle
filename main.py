0e019f4f1ba60b935464e0387252cfd7cc3c55d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:24:47 +0000

    complain character talk deal interview

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
