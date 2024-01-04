cac6864107ed1336227bde76c0bc0970d9de396c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:29:03 +0000

    copy notice hold arrive guard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
