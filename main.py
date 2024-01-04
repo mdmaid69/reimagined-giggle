e0a0f23ab50b571be43e03db5596af2058807b0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:35:08 +0000

    somebody future length find breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def append_to_array(array, item):
+        array.append(item)
