318b5ec0d7ffe62da777d36aa764c24a091eb293
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:18:36 +0000

    pattern concern undo whistle afford

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
