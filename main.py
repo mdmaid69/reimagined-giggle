209265f9247742b29a20b12e960dbb98c43ee341
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:54:59 +0000

    giveup protect call opposite progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
