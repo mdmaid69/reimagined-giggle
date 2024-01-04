125c5d9142d524269b4498c13c2a170c45e42945
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:16:31 +0000

    ilovedicks appearance clean from point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
