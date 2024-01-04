1c510196f9511012a4b5094d75a37faa87123cb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:01:11 +0000

    after typical makeup somebody excellent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
