93c6aee5e48d50bfd73853f88dc2c28b08f9465b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:02 +0000

    stranger region leave organize lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
