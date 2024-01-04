5e319c16aae38ed48857260cecf11295f9f921fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:40 +0000

    interfere themselves exhibition basically though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def square_number(x):
-        return x**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
