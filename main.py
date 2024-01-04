08782ba1d97f57e89b042a0622f2a863451c8187
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:10 +0000

    connection fierce mess large clear

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
