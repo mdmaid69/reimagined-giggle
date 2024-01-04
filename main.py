74a1aaa3bac3ebf17bb41902a70f8121701eaf3d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:47:27 +0000

    patient typical little ought about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import platform
+def get_python_version():
+        return platform.python_version()
