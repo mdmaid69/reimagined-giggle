e647fd4145d13d014c491125d6eebb8ea7d44ffe
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:32:24 +0000

    provide study knowledge attention obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import random
-print(random.randint(0, 100))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
