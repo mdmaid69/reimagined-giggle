774aa8ea5203e236487e8339a979d7b4a112e8ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:59:07 +0000

    matter dance any top OKorokay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  def multiply_numbers(x, y):
-        return x * y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
