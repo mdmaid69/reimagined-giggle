0952e08c25e8f167fbcaaa4dd19aca9ed3167573
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:21:21 +0000

    eager top whisper replace instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
