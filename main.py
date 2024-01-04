87381b559c00856f5c425a2c222a972f77e90069
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:44:00 +0000

    slide style knock feeling breakinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
