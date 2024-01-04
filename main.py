f7ec6634182981be3f330e454323b2c0dedf6045
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:59:06 +0000

    content die defend debt simple

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
