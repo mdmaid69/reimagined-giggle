c928216b645ae990cf35d0402df6e23fc4b6bf68
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:16 +0000

    avoid murder environment apart as

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-for i in range(5):
-        print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
