444b09c9b59a1b4201271a105b803b0cb3c93313
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:04:31 +0000

    last his tongue preparation second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
