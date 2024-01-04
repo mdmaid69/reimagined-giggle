87b5572ab1dddefc293e9a995f87178c5c26458f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:47:09 +0000

    somehow bang traffic deliberate church

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
