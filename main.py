b04bc6b32fec192814d8614f3719a8cc0df4bbf4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:53:30 +0000

    discovery clear surprised tie twist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print("Hello, world!")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
