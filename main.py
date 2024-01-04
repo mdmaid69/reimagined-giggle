80758e5a573abf79bfcb2ebcd51fd1aa44d5c4b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:25:28 +0000

    whatever arrange strike artificial wave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
