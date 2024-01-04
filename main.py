a40153e027d6e4d0f5172d1f2657ae284952527b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:30:47 +0000

    used region or inform tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import getpass
-def get_username():
-        return getpass.getuser()
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
