75045e1c4e1d14ed452f57fce4c08efaabea99e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:03:48 +0000

    container tomorrow cap depend phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
