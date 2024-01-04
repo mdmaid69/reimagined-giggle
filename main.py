a0828b9bc8f83ac42563bc809db32635e422fbcc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:25:27 +0000

    gather arrange call container sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
