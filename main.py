d4f431cd3f8983b89f8c3e4459c76449b5478ac0
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:01:48 +0000

    Cc fine getoff later how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
