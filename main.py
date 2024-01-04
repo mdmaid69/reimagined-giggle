3e272763094417ca2c62a2276eedf5e4d97cbeb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:47:26 +0000

    deserve height accident free advanced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+print([x**2 for x in range(10)])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
