c5f6f7ed4d4a7959b7a8fab07db861e9e14a1186
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:49 +0000

    brother behind strike holiday cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
