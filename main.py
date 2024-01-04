10824cb016e9a45d7caa5b210f732a2caeae4830
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:26:36 +0000

    roof odd stretch shame means

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
