a35084620a0d9ec009d03b6972c7279d2b2dc497
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:51:54 +0000

    cope wing estimate ill kind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
