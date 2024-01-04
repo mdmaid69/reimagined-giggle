795a73fd8a62564228141fd01552f87e50518788
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:36:25 +0000

    careless invitation pretty noise phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
