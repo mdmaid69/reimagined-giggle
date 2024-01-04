7b8d2ba4d02b1048eb135ca90c62135578f96e78
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:07:35 +0000

    last trip trick cover supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
