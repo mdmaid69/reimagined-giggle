f8314b000435999d00480f9d03a593e08b691d7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:45:33 +0000

    extremely waste match float estimate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import socket
+import unittest
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
