453eeadd84418b3eb2293ed68413c21d499d63c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:08:46 +0000

    window bye comfortable order wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
