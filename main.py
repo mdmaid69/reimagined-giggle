eb4dea81fd53fe8ba3520d910e7a872c4592e65c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:04:10 +0000

    unlikely bye clear fetch more

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print("Hello, world!")
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
