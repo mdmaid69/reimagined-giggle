896bc184b75b7dc0f2882cf8988e4f01fdba011d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:28:14 +0000

    that strike train criticism flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print(sum(range(10)))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def is_odd(n):
+        return n % 2 != 0
