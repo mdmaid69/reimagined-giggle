e311bf10107953f23bb0cf491eb62ec1732f1f28
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:16:27 +0000

    mix earn nearly dig price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
