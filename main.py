2aa7d4c0a8c674fb9664343db36e363f1be9de4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:18 +0000

    exist than Ff belief damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
