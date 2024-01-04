fe4ebd911cb0c5c47e7621b05f152ac9be75906b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:09 +0000

    unit discuss progress trousers somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
