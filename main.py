53d488cf104a110837c61fcd2a31d6cf377f90e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:08:45 +0000

    possession turnaway juice permission happen

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
