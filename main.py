838ca38dd62e5034bbc6ace38b6cae092df13baf
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:58:09 +0000

    accordingto instead gift herself act

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
