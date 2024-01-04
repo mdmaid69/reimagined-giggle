71b99a509c1f5a07436608228f3df0517939d71a
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:50:09 +0000

    partner serve process upset switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
