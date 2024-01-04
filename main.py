90b3f02cbbbce7bf73d854d43c55efd8caa22996
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:16 +0000

    voice team thanks steer shock

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+for i in range(5):
+        print(i)
+import math
+print(math.pi)
