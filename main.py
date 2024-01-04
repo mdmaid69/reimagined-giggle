87dd288de8fd1d6c4da95cc050db779c2e365c54
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:12 +0000

    interview similar wish near available

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
