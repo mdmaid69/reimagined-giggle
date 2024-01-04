f8eafe75c1bb2e38534d6574511e89edc443e658
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:03 +0000

    alive morning wide blame squash

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
