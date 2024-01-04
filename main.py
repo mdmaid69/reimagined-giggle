911a2a46a2027296198b49807097109caf7aca28
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:00 +0000

    come still suggestion this let

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def reverse_string(s):
-        return s[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def remove_duplicates(lst):
+        return list(set(lst))
