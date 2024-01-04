014de2410906f0a601ba4d658acf7509c5c833ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:36:15 +0000

    always still hold listen borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
