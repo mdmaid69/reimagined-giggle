84777500aed37bb1ea6218768151e1fdc93db993
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:51 +0000

    reserve offend proud breakthrough put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
