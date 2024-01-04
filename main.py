31a4820e5cedd25dc160af9d3feabe7f31ecc7d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:04:21 +0000

    narrow centimetre store lock mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
