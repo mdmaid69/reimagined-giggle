8487374a7593e3f892b71aa5713c6681708fe8ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:48 +0000

    object exercise match lesson hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
