39c4bc5d0e4968d47fdeed495390dedb1905797b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:48 +0000

    shoot load possibly money generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
