70be4a70fd0996bcb4b8cce196e84c81c0e98087
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:42:52 +0000

    call miss error strength loss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
