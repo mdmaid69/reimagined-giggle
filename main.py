81bbe6a55f2cea59d9d5d7276e48a123bb261374
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:13:30 +0000

    worried smell negative my meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
