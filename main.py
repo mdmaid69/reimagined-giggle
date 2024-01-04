a59d3c48c7f30beeb47f072b3c37994404d7a522
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:11:23 +0000

    tooth tear event soon Qq

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import platform
+def get_os_info():
+        return platform.uname()
