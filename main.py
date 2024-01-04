12cccd984a50e42edafda29dc1946e423ab2f08f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:45:55 +0000

    realize secret opposite count shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
