6b59ec2c44123e82e72fc27eed2b3cb6614cc093
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:36:41 +0000

    there original store zero post

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
