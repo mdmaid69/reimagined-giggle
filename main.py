6213e8a36b8dc102ac66c039b383528189f653c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:51:17 +0000

    all Yy breakout sail there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
