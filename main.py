6b9a0f9a72e6e7aa43ce69ce096220780b642634
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:48:20 +0000

    path couple with exciting offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def convert_to_binary(n):
+        return bin(n)
