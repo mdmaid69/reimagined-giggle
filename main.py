24121b8edea4755390643a383fedde595949ba02
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:43:49 +0000

    phone hungry by risk adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_item(array, i):
+        return array[i]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
