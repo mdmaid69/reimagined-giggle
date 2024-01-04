e2a61c2b8f86ce5e9dcfc246186d83639f1c462d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:27 +0000

    store suit dream drop man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
