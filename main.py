993bad34af6ecf4f95c4dcb5dc9bb2b82b744a0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:09:44 +0000

    wake callup empty game ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_typecode(array):
+        return array.typecode
