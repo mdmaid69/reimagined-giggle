69458c9fba710c55a9dc5bfcf67ae8232fd9e846
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:09:38 +0000

    sensitive lead medium low name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def count_characters(sentence):
-        return len(sentence)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
