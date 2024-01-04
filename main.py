cac3d340fcfc80b3372c8f342c39036fe9f7f07f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:09:12 +0000

    arm arm sort influence show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
