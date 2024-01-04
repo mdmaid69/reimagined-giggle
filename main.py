3a5d4f49015bad4ba6b2244acc6d0130ebbe2332
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:25:24 +0000

    cope bite limit frightened what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
