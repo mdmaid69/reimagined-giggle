fffef2b97aa3cfd5153e1279f11cf395ee280027
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:01 +0000

    trade slight height trust belief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
