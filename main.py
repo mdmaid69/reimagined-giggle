5f134033bbd9d59815e9983727d883b771902f14
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:25 +0000

    sign any 565end path hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sys
+print(sys.version)
