a49ea7485b42e6988f3261e9ec22cec817c964bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:50 +0000

    talkinto disagree decision keepoff whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
