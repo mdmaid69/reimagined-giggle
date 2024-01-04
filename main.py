db7be22daa5ba93f03047ef43bad26929ec3c9fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:13:39 +0000

    rich through ground wave hurry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
