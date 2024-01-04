56109b763947b27413323183d793454dca3a8a12
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:19:12 +0000

    fold into secret past crash

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def reverse_array(array):
+        array.reverse()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
