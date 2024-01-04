2efd7fa17eb772df118bb648e8fb3b2fabaa25d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:56 +0000

    very ago each cut Ww

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_density(mass, volume):
+        return mass / volume
+def subtract_numbers(x, y):
+        return x - y
