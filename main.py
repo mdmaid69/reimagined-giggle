68ab3f48a09b6ea3a1d29194cd84a51587eab6ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:17:24 +0000

    somewhere unite interesting knowledge move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
   def check_if_file_exists(file_name):
         return os.path.isfile(file_name)
