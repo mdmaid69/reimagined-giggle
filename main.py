d188e66400ffa18d01fbf98f4f28bcc481e1aa9c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:43:32 +0000

    law correct runaway clear legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
