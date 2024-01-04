95d35133d19167df8b7cefc1af34447deb81d227
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:12:32 +0000

    cap police juice attempt easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
