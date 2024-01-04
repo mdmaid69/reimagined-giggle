b16a0fbdce85cff09a12ad0da58d58216cc5047c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:54:37 +0000

    special public quality standard Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
