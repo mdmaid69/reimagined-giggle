247373f4cc4af76ef01dc77fd3e91fddec323c10
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:49:12 +0000

    conduct acceptable price turn sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
