ec002f7a13d25c8f7a741b571662d704cf99dc3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:23:47 +0000

    threaten keepup match rush feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
