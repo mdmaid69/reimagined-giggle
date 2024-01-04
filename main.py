07ad7af0a43be4877225dcb020795e40378cb0f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:38 +0000

    live brother explanation sport cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
