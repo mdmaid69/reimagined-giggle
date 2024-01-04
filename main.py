b2e1629c02fa2476c3a7f4a12cb33d952a72dcc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:47:55 +0000

    satisfactory makeinto confused school century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
