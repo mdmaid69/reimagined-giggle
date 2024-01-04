07cc426489de4eff63c20f54b3406134b8297774
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:00:40 +0000

    tax opinion close way enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
