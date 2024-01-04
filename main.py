4e5fa1bdd343163d63f3239d72b3af644673aaa4
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:37 +0000

    morning blame OKorokay 565end measure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
