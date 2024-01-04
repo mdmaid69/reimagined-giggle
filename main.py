a791c828571aab184e8c21ef99fecd045bdd0244
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:27:51 +0000

    Vv call sick square draw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
 def calculate_power(work, time):
         return work / time
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
