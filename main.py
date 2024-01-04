563485af82089c7772528176f788c851a943f32f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:54 +0000

    then rain discovery impossible way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
