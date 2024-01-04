5995deb88e864987c042e87071891dfa1fd98fe3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:24 +0000

    inside tread sensitive explain deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(numbers):
-        return max(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
