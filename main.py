0e29718d4ce2d8a1ff1d47c4710011a415401d96
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:10:41 +0000

    conversation contrast bite ill left

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
