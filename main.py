d8a245de8595b5ba285ce87698db4d22c9776d1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:07:52 +0000

    job hit rain transparent force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
