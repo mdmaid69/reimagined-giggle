5df53c408707bcf2bfef7104d3003c04f41fc7c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:22:20 +0000

    block turnaway choice independent apart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_words(sentence):
+        return len(sentence.split())
