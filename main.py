da6dfd767c9e9bc0b8560b0421ed7f0ac4a81e2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:55:36 +0000

    deliberate star gas sorry offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
