d05ac66772a8eaa053bc2cd177807c66773035bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:03:58 +0000

    sign part mean if surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def sort_numbers(numbers):
+        return sorted(numbers)
