b6c89ee297e4db1fb209ae1ec9af012a79f9c90a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:48:24 +0000

    tour brush terrible left increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import random
+print(random.randint(0, 100))
