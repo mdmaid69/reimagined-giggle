fb32b97fa747f4947b74346537fe2f387329e8ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:43 +0000

    direction safe hide bar often

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def remove_duplicates(lst):
+        return list(set(lst))
+def is_odd(n):
+        return n % 2 != 0
