4f9a878e7eaa6c083a92dad8ac19ce2bbe50bfd1
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:23:16 +0000

    recently delight nerve brown peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
