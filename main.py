a6fabb9bbd5edb94726de558b847e5f351cda476
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:45:02 +0000

    Zz question journey themselves charge

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
