fedd59549f2749525b5b746372b2854bbf887584
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:42 +0000

    environment range left never clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import platform
+def get_os_info():
+        return platform.uname()
