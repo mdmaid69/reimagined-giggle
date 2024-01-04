e26408953bd55086a7193362b5d22a60c853c438
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:46 +0000

    breakinto provide bag turninto and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
