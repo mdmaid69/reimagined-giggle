31bcca7bd574c94332ae700ab12f95da7c485731
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:07 +0000

    dark bush visit pattern failure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
