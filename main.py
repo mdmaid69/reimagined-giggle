abddab95ea342a9dc295f989cbee3c623bf44733
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:30 +0000

    fortunate vote black unlucky cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
