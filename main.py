2753788c09428438c19b69dfe4e036f0fe1385d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:13:06 +0000

    matter short children bet however

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
 def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def count_words(sentence):
-        return len(sentence.split())
+        return math.factorial(n)
