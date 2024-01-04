5a4d8061609d3b661eb919ff7d8af8272e6a303a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:08:53 +0000

    Kk definite cold passenger lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def convert_to_octal(n):
-        return oct(n)
+def change_working_directory(path):
+        os.chdir(path)
