ec3c41d73c1d7f7aebb6ca4b179fab8e0d99e64c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:26:29 +0000

    complaint sort blame emergency frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
