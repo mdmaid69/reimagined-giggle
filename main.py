4357d6b87b4f0ac6efc7cfa1d03f4ce2962a372b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:11 +0000

    inquire will national fault match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
