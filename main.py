d0fb28e6feefdec1cb598615953956cd13c2f0ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:10:52 +0000

    cheap contact ear issue quality

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
