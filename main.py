98124e2c50f312f58b9713333afff8f18e717c60
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:11:58 +0000

    hate middle undo fortunate check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_absolute_value(x):
+        return math.fabs(x)
