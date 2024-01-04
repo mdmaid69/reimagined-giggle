954995b764fab88561b90fa9152a02a6cbad57ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:43:37 +0000

    which rather spend deliver pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_item(array, i):
+        return array[i]
