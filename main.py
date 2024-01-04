fd9e436d0884798e082c2d6baacef3a5e989e948
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:31:53 +0000

    luck pay tune hardly train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
