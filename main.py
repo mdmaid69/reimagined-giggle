4dc63095e005b96829e95f0ec8da9b746a8d96e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:45:15 +0000

    bye geton past machine spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  def convert_to_hex(n):
+        return hex(n)
