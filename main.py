6312441ad50dce1c4bfea703d116f15db3b41c09
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:49:57 +0000

    chance dream police round fear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
