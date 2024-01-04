e2ccb917f4d9697ebc1a2c4a1b0c8d11c05570c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:52 +0000

    month Rr doctor fetch exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_rectangle(l, w):
+        return l * w
