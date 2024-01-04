6a20b4d6a9ee53bf062e4841a42646b4b5cdc9aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:33:53 +0000

    blame everybody rest night wind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
