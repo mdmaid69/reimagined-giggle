81018199a784a56ac2251cd60a603355b8419509
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:11 +0000

    imagination formal clue knock term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
