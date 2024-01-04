b539cf62efba5d1a1bae4c20656243f91f1d186d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:02:35 +0000

    dance earth butter store door

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
