cc90253151bc572f5a4a2e3e6dfb78273503eb6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:51:53 +0000

    shallow alone shop female software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
