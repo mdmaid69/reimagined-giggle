830f4e6b9cbb25cdbf615f5d823fcac470e24b4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:23:48 +0000

    bear minor personal apologizeapologise walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
