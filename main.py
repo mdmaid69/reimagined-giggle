f1908cce9cb2f0d099abf7732d2be217dce58822
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:51:13 +0000

    persuade broad history will else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
