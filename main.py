03809ae6b6ec284e8ccd0de46f528daab4313d47
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:16:59 +0000

    case argument opposite sensible impressed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import random
+print(random.randint(0, 100))
