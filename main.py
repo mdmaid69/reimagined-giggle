1374f1a2b8516988d60741a0e0d7ceea10470dda
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:36:40 +0000

    wash Uu describe payment introduce

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_pressure(force, area):
-        return force / area
+import os
+print(os.getcwd())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
