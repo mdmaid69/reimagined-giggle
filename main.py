76f89013d416c3e439b0eb68b79e4a21b2266b95
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:16:17 +0000

    scene odd content godown beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
