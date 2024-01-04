c4f243f7cebf274e65d7da6b4f0eec5462cf6bb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:40:41 +0000

    one trip threaten walk divide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
