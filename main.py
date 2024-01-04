0018b640a0af938ef0ada852ff1c42d93a080cae
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:25:07 +0000

    pupil children window theirs kind

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+print(sum(range(10)))
