ae80e7f9424cc62bcdb103a955a0adf844d4ec93
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:09:05 +0000

    demand limit performance waste pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
