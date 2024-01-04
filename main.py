d9c0e8d8e3323aa7e991ae0b0736ee0c5f395d87
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:14:28 +0000

    entrance complain after near possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
