c186a0f3e14bbe8b8f3f2e154d3c5d41d2adb03e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:26:32 +0000

    class skirt attention consider blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
