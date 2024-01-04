a8cd1c97e3a24a4f433d9c93ffe92096e6858459
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:45:41 +0000

    unlucky advice declaration floor pull

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
