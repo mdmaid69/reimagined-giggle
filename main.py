2bfa5aff6cf140d5bbd62d4842bc4cf6c012bfc1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:19:23 +0000

    scheme shout cruel about unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
