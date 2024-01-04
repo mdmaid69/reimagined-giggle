03bfc784dc6503aa73e6081b3f680f2b91cf8c5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:08:45 +0000

    still government aim gather cheap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
