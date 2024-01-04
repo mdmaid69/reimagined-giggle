d21daa45bba835ed80ea5cb5e7da687e18efa68c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:12 +0000

    failure like herself comeup objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
