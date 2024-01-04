c8d642452b2ef726d21e43a720b7fa551670a438
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:11:55 +0000

    factory hair ignore last surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
