d40d0f9f4b79354fb6f2da25212b424026abf617
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:11:43 +0000

    sign crisis long wonder something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
