61b936e40d923b9b247923af85e810286001b812
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:49:39 +0000

    label before the regularly minimum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def find_min(numbers):
+        return min(numbers)
