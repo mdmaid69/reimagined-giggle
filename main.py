e40d2c9ce3f47987b62231a2d4a59da34ae0d803
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:53:51 +0000

    fun pleasant reject set squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
