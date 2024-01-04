e78770b4bbd07b00993ec6dc7a01b53f5df7f921
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:24:49 +0000

    snow pretend insist present protest

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
