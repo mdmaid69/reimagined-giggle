cd85e5e0ccc6f7592bae19b28c7004cf4257c48f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:41 +0000

    in record thought someone punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
