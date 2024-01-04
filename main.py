46dbb806e2c38a1642f4b03aff2dc0d611902793
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:36 +0000

    me squash shallow steady oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def remove_directory(path):
+        os.rmdir(path)
