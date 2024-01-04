9be3740d5818849914ee2d0b1cc2dc609671f3b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:45:30 +0000

    university cheek table scale unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
