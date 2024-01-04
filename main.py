7f26d8fa7f3c180f7aedea0d7cfd99553891de3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:58:11 +0000

    century grow rubbish limit sharp

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def reverse_list(lst):
-        return lst[::-1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
