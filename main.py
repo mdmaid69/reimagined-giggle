3885dafbe72baa8513c5e870f2036b2949f9604a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:25:01 +0000

    comeover loan upwards combine possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
