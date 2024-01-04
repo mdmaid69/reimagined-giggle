97deae8058f682cb7cf0b71cbdc531ece6a18937
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:00 +0000

    tidy storm later point chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
