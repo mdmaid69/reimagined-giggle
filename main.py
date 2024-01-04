70c32d0e9e0bbb3080088f86133045336612e0ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:54:53 +0000

    walk trick list rely sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_set(array):
+        return set(array)
