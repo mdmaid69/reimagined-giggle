6b4992c1ab56f10ae47ed82b38a66facaeb4ebbb
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:10:55 +0000

    workout because Hh exchange party

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
