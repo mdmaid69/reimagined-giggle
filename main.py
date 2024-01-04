c54c9b380db5b0cf5d28d8e45ae5694944c1cbdd
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:23 +0000

    error correct in stage protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
