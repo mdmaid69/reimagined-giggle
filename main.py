134e52c80a3915a655f4058523cc6df49d5eb089
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:26 +0000

    getout although part pool getup

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
