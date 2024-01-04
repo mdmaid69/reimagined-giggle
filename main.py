26a2b37986718b74fcbf6957fa982e9915ffe5d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:17:38 +0000

    goup store weak cover knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
