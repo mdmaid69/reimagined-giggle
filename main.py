4f7d8658182d7d20078c3647b4b3cd12c2af10f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:22 +0000

    572enter dress something near steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
