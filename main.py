2fadfb37c33529a4addcc9c74e90d061eb7b61ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:19:50 +0000

    concerned band they band temper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import tensorflow as tf
+print(tf.__version__)
