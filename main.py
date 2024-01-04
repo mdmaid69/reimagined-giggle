3ea72e46cb26314ffc18baca4c38c434ee1326ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:44:36 +0000

    he total mean cover fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
