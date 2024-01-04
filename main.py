680086de059d42b40cb57b5fc1a94832c87ff151
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:25:10 +0000

    description promise pretend straight close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_pressure(force, area):
+        return force / area
