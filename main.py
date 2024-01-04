b436467871a2093a2292d5c6458cf6fe68f00063
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:35:59 +0000

    centimetre right language beneath dish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def sort_list(lst):
+        return sorted(lst)
