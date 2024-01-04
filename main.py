80cdeab8ba11a06a6168932ac8aec4b16e79f73e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:33:03 +0000

    runaway occur keepup beneath arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
