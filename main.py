4922abc46d6e6735eff09003b9e1d56484eef6c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:02 +0000

    build excuse walk most let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
