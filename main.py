c19a66d9b0c8f946776d431c741ac59cd3cc319d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:51:07 +0000

    personal estimate schedule show tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
