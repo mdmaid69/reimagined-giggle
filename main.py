e525ff5a46e7309592e388564d897968d7c91e89
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:02:06 +0000

    extra politics fashion company not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def square_number(x):
+        return x**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
