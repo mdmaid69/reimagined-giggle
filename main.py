f448977ae33257b1cee0840e7aab2146552ebfaa
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:48:59 +0000

    kind 566end best steer can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
