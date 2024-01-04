73ef81ded3b046628d07eff15061a5c91c4b8156
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:45:51 +0000

    affect interest somebody pipe along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
