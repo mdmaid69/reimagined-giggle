6a06a70feda9404567a495c1272d02c2fb65942c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:59 +0000

    moral send march feeling yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
