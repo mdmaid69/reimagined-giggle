e34cddeb7364eace5fb5bff600381a9cb7faf283
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:54:05 +0000

    achievement fight short attach breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
