3814fb8a5564835a39ded3ab56467b7ee58309b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:15 +0000

    calmly bright influence rent autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
