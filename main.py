43f166ed963b0a6a5dd193748344f6977f25aea3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:32:26 +0000

    flat machine argument active ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
