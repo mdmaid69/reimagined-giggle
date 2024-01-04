4532d88639bc22fba4d6993c39406e295a8f6285
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:11:42 +0000

    rescue while height sign admire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_acceleration(speed, time):
+        return speed / time
+def square_number(x):
+        return x**2
