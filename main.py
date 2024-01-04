7082b9c8cbaedbec82d013611c654cab7f91c4d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:44:38 +0000

    motion motion risk I trip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_length(array):
-        return len(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
