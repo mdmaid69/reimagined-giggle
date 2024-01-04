1e4569b94de663d6fde6a5b9a4302754cfab2e9e
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:44:09 +0000

    card genuine appeal commit release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
