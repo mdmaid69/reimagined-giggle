697ab06935a29c7607e8af41536d55957c583050
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:17:13 +0000

    spite waste present stiff wind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-for i in range(10): print(i)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+print(json.dumps({"name": "John", "age": 30}))
