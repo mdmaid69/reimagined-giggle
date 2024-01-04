84fd691543cb364a6f6a2690f81d272f523639b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:36 +0000

    autumn bin likely drop raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
