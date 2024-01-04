88a838e5f710316718a6ff2e6e3e7e86ef440b90
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:26:33 +0000

    towards could face side urgent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
