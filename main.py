cdb7487cba1eca7ce5f9bcf71b0af8ae51cb3580
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:32:01 +0000

    run taste than quantity fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
