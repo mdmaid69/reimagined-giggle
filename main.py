a98a06d723333cecb703c1a8c2fe002549431867
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:54:34 +0000

    listen positive spread takeoff fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_speed(distance, time):
+        return distance / time
