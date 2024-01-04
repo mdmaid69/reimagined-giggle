b664f217fc7414bd7faacd696fc38b19274570f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:06:01 +0000

    surprised split reject yes face

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
