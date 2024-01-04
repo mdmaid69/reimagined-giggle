514dadfbdc84213ff79b8e4cb5ac5c64e699a0cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:35:32 +0000

    drop pleasant bet her promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
