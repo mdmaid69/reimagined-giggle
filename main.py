d4e5fa0b239efd1e778ef2c886111be8a42d07cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:04 +0000

    pump downstairs television afraid lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
