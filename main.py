c79d051851de884eaf1d2ea549d8ad402e69591e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:29:03 +0000

    border any anywhere closely expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
