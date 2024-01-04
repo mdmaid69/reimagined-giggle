ded457ed15df6135213351a50694b350e367a755
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:01:01 +0000

    correct sound pretend OKorokay relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
