85511d3af1ae6730e6080eac5a223992158fae66
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:46:28 +0000

    little steep before communication fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
