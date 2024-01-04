09628e1d2f6c9d936121da378abcbccb3b8b8c0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:57:03 +0000

    couple callback stop violence interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
