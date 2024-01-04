7cc3e7a0eca45c34117426e180104b7186dbbda5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:56:16 +0000

    rid burn apparently anywayanyhow public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
