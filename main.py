abdf0d2419c6f4dda69fc30d274d7f7ab988fe5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:43:40 +0000

    top cost certain environment adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
