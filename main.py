536e49fda08b0758606fe4564de7a9e358a0285a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:52:31 +0000

    hope beautiful likely have closely

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
