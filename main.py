79b9fd4ce35345d45b64a5e5cb1cb9a6a955ee14
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:12:14 +0000

    shallow have style workupto display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_priority_queue():
+        return collections.deque()
