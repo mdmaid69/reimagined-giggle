fb55faeb440b75cad514822b6ed416d4b62f9f88
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:46:30 +0000

    circumstance far present practise note

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
