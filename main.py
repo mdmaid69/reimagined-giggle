60891af889be577dfd5c1012953a19023c4bcffb
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:14:41 +0000

    lock Rr game possible in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
