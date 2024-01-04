03ac080b78886add652b7207676f0c24268e2f19
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:19:02 +0000

    well move decision feeling tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
