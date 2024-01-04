fdae51ad3805ddb83d333ca108af628f86c0c347
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:58 +0000

    during mend fight forever advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
