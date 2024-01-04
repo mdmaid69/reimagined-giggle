fe6d245d1657ab78d439b201b672f84bdd7bfd8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:32 +0000

    closely of debt flavour central

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
