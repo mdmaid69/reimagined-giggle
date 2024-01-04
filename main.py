77367db85200ad194c1f611d6e05b416fb8f40c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:22:32 +0000

    second please without brother interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import collections
-def create_queue():
-        return collections.deque()
