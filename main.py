4f4adf2c548936caa082b44632eed4297f900dcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:42 +0000

    curious sharp wheel bill forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
