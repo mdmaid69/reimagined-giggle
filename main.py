8ad057a251949aad79e0751a243f158f7f32a4d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:21:59 +0000

    upstairs strength will attempt move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def is_odd(n):
+        return n % 2 != 0
