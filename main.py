f5269959904c58bf3789d5755669b261150859dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:55:08 +0000

    split raw clearly extra bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
