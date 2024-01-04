fdeff4c118fee13fc33294d61e0957ac51505fe2
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:51:11 +0000

    plenty deliberate go flood properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
