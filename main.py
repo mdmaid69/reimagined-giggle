0456e0efa248acfddfd05427adc4d72682bc54b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:55 +0000

    travel spoil slight emotion poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
