be29a3e10a780a5525bc254dcaa82ae03df756f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:58:16 +0000

    between evening tired let exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
