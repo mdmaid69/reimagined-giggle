f68dc9a62b3398d4cec7d6b60265df3baa0656b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:39:35 +0000

    constantly tap force work opportunity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import datetime
+def get_current_date():
+        return datetime.date.today()
