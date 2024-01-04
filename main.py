629325a986f47226762af22ca32531b46c551bb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:46:16 +0000

    classroom satisfied various damp accidentally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
