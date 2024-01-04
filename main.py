5299384d231c288809b5872a1af141e2abebf35c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:27 +0000

    left traffic discussion complete impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
