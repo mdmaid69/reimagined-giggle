a2951fa0352eb5062b24967b398c7c0bc76e00da
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:58:58 +0000

    occur stay closed progress who

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
