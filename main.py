3ba295e79749b5eca9bff1e3ba39babc668c21ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:47:19 +0000

    there roll you he all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
