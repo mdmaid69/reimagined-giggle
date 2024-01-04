6ee520f14927be621341bf32df2d14f606e5ca45
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:35:08 +0000

    different open than spring discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_tangent(x):
+        return math.tan(x)
