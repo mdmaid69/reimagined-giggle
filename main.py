1283f75733432e37d326b696d45fdb86b609baa2
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:39:31 +0000

    die increase prefer better risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
