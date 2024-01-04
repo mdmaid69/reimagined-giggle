19a6501f12f665ee47c4793b6aaedd83740fdcba
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:45:48 +0000

    tight union family 571enough long

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
