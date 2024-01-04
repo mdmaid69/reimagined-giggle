5e091479cc06a62f51c00b3546dc0557ec69160a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:47:19 +0000

    brief ball scene hospital tough

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
