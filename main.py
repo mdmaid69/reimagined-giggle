81592e5dd6079e4395de50f8aa85121822559115
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:04 +0000

    mix speech charge until squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
