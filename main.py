75f3f90a22357f71aedf870be75b5dd2364473be
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:40 +0000

    disappointed silent minimum breakthrough minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
