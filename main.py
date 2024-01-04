fb113c6252ad913643bec1dd328e980bbfe63b61
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:15:43 +0000

    shoulder uncertain dull eat embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
