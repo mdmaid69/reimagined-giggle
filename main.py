0fb8ff355b04d5ebd00e77f70a8e570377be78fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:00:33 +0000

    suggest film sort promise tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
