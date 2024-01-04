919422a5d5fa763fc21a27c1b3671e0244f0d298
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:13:00 +0000

    correct behind combine connect deaf

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
