88e7cdfae116a2d5ed8233f0b7187af886972d72
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:31:08 +0000

    stone shadow sort this tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
