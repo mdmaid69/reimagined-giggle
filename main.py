41286452a22fa66acbbcd6029bdd6960078b1e04
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:59:03 +0000

    goodbye guard behind journey refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
