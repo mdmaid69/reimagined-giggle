8dfd6d0aaff73d6c613655bd58a30f410f5746d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:35:41 +0000

    their declare alone close manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
