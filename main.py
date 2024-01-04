7bdc490e85a7201023a99ff11329e34284a16ac7
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:09:26 +0000

    cold pollution bang limit satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_typecode(array):
+        return array.typecode
