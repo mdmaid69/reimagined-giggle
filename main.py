d1598f8754ab2c191d73f32a6070f469959cca7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:53:49 +0000

    always survive progress rank neither

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
