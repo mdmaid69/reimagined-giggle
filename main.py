eb0d5c544d65ae0b29f69f6f7f28b213c5fed4c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:45:14 +0000

    offence main grateful quit male

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
