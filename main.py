4040d407cf75725853bf174bbe6df00e3c86fd35
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:36:11 +0000

    search hold score part shelter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
