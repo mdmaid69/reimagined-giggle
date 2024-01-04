a75aef92100d2d150c46186c70d838000564b0ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:41 +0000

    travel interested last truth class

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
