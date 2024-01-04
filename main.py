5cd26c86df650e9c8cf09dc354b80f1e83bbd787
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:33 +0000

    field question skin dead stay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
