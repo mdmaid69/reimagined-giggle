d7c5238d218e65573f11546770a3fa36f12c7cfa
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:29:14 +0000

    twist surface twist today delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
