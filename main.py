19e39f4fe717c971cdef3f9d8d902080733b0df7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:27:49 +0000

    unkind later everybody programme consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
