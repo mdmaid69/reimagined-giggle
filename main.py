62b3fdd1de2e25ba4e9f077648c6330b600591f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:10 +0000

    form secret serve dry everything

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
