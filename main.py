64a88fd0f2ad447674a0ae98e0f89c6521a3b7b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:28:12 +0000

    guess wide stay impression telephone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
