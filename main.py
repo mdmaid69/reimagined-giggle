a1ebb2ea2e4777b2474565e43a92dafda009b6b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:44:16 +0000

    yes takeback achieve explanation twist

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def multiply_numbers(x, y):
+        return x * y
