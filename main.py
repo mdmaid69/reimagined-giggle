27298b8cab666b80cd3687266ef7dad95e3577ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:09:39 +0000

    confident worse expect either please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
