920363120bc74b5a08fb076410f5cdfbcfa3a22c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:14:19 +0000

    important Pp false connection well

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+for i in range(10): print(i)
