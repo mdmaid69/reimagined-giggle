18b926da6f7bb40316c046d70c4a93d7bf311449
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:23:19 +0000

    sing rub peaceful extraordinary trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
