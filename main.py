0af820e9b4db224c2ec0cbf9ba0f8f6d42e984da
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:40:05 +0000

    style winter boil themselves spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 text = "Hello, world!"
-print("Characters:", len(text))
+print("Words:", len(text.split()))
