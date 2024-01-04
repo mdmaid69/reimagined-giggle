64343ab19b2d784e90663a728e62d77bfa5024f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:27:07 +0000

    control especially silent lunch value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
