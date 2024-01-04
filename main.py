d417041208c54135fbef589a77548a2ffffd597a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:30:16 +0000

    complaint whoever else clothes greeting

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
