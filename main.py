3c3e33dfcc788df30f70b647ed4bc176cda567df
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:26:05 +0000

    national show though future cut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
