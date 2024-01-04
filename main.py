8b690ab0c7694f5ccaa364bffc904c9d559c591e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:46 +0000

    welcome care cheat judge anybody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print(sum(range(10)))
