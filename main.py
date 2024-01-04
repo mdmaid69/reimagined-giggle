18dfd3c476db8e8f9941c042f2cc5830cecfbcc8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:56:45 +0000

    plant play tight shout tie

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_queue():
+        return collections.deque()
