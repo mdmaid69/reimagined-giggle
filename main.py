0cbd84a99153b933fcb4daf597909fad52a700df
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:03:59 +0000

    lie admit necessary pretty book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
