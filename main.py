0b7307b6514a3206ba54d3bc1103c0dd8f7269a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:00:09 +0000

    transport reduction happen brave eye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def sort_list(lst):
-        return sorted(lst)
+def find_min(lst):
+        return min(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
