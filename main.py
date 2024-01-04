fe23d5a26ff0a98c9243eaa575512b5501f08121
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:02 +0000

    age rubbish thinkof he cough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
