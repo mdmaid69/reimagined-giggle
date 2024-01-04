efc87b5680def60111567f448a6b8a4b9510debf
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:34:15 +0000

    warning money damp bunch next

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
