2949c7dae9c24c9a8572dc0c32fe7bbf788bbcd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:27:29 +0000

    worth touch previous pure respect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+print(re.match("h.*o", "hello world"))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
