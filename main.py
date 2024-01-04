631f1d34e7e342c912c7ecd08d2c0f9fd5ada5b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:04:10 +0000

    share minor slightly hang keep

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
