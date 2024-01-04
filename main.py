75fe20d8b82ba48988ea977a10ad5f927ab5cd92
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:29:17 +0000

    record radio near waste bang

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
