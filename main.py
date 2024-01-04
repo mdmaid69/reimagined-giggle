20b3154161e18280a9cbf559ece7788f32ef6c57
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:39:19 +0000

    middle waste relationship guess people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
