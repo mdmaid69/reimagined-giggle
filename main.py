439e4aa629d0273f7d052ce01d5238762c1e3257
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:46 +0000

    doctor correction about payment face

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
