2f1de36cf14b139ab47c2b4469b087a37c2db2e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:45:49 +0000

    design soft strike safety murder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
