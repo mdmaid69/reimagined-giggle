ecde28b4173b36b7bb1f5d5baae2fd68fffa3b56
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:46:21 +0000

    himself tap mainly her thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
