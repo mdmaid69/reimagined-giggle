ab2a06bca4198ecf20f420e48e2b9606c97c2cfe
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:10:19 +0000

    disturbing risk chief side progress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
