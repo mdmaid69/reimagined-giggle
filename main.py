6f8fcb1d45aff6d5613a329595f877b44c04497b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:39:20 +0000

    eat protest stick recent gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
