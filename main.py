36ceffe894e6dc50ccc5d69f3cf7b213f766f346
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:10:16 +0000

    interfere impressive niggaacademy busy danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+for i in range(5):
+        print(i)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
