1aeb6c5ded63c41e51f3e5a034314c3461583d39
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:50:51 +0000

    stranger doctor final marry mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
