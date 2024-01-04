df77994e8d0735b2b158da0897022b53b6c4f709
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:28:27 +0000

    advanced silent finger clever pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
