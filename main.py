927e44f17594f0e111e9cd9ebbedbf63c8ef2ec0
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:02:05 +0000

    contrast comeout warm roll over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
