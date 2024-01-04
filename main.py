fed5f884b6476f40f9ba0df9a9ac9fda60fedb92
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:18 +0000

    top practical Ff attempt bringback

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
