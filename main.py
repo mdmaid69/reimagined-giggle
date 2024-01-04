07569c95c33ba209c30eb8ef19e90cb2153dba92
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:13:17 +0000

    detail hang care past invite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def greet(name):
+        print(f"Hello, {name}!")
