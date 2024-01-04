4db7124e7743a8b0e898f8abe027f39174541c5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:46 +0000

    congratulate treat accommodation press outside

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
