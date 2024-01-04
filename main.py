3c52bc53e7a8c230e33ad84488d3ef1a42df4d72
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:06 +0000

    well risk their clothes head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def check_if_directory_exists(dir_name):
         return os.path.isdir(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
