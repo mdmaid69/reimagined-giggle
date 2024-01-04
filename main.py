0962f02bb236fbc3bd30595f2964ae6c59e3a9cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:09:42 +0000

    high receive convenient talkoutof smell

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
