fc94a3fc28877fe96a838ee3edc02cad9721201e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:24 +0000

    tip road gooff half story

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import re
+print(re.match("h.*o", "hello world"))
