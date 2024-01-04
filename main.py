a6bfef19c86d1c4f3bf1c54773790b483d64196c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:42:42 +0000

    left between business art transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
