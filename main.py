68da753cccab3df722727ac218254846c9ed6f4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:27:17 +0000

    forget loss interview race story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
