c167d2f21ceb13d54ba72409b485e478e79fe9b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:30:21 +0000

    art firm walk even quantity

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
