7d7c8325f01d45fc9c08cf874618586e81d4d14c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:29:14 +0000

    public cry feature major interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+        return os.path.getsize(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
