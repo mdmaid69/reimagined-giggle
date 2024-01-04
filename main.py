10042b95950c8fd7cb29bfa9d69f3f10de97a006
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:16:34 +0000

    problem fail joke reduce small

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
