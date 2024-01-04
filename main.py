c4c2b9df6e4abf1217ba781b36199fcd0164d432
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:15 +0000

    upwards hurt Vv appeal laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_bytearray(array):
+        return bytearray(array)
