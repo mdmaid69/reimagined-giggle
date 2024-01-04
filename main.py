d7f8bd1b3aabbd760c5a4645d74c9c991e83eab9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:25:18 +0000

    phone delight drink cap measure

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
