f893d52e17042eb3afca9e6ed1020564c790b0dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:16 +0000

    callat stupid crash excellent hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
