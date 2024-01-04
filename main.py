5af3fd9912a9187333c34e9790584e1a773f20ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:54 +0000

    protection mind turnup large workon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
