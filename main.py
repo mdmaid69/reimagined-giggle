9368f8cd09dd38c52d3041217cb3e72cea53a521
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:32:53 +0000

    communicate long flight worth honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
