39284652eaf97b2da675b0d3d844e2c22d4e5454
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:32 +0000

    very Mm body price visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
