62e258e74429d63fd2b21a754d62001a2d39d8b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:54:26 +0000

    grand thumb attractive centimetre appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_min(numbers):
+        return min(numbers)
