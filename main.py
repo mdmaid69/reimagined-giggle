ea8f071e6501e84854bfe32ab55f1e919fa343b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:16:19 +0000

    dress by sink earth pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
