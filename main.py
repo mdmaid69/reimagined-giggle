cf322bf822fc8e3ecbf3329e3348b8462b6c6eec
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:20:15 +0000

    run sit story now give

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def reverse_string(s):
+        return s[::-1]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
