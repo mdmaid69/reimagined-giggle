b8dd9a39c9cc1292ff930b210d42a9301152a627
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:39:22 +0000

    time wall battery takeaway sister

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
