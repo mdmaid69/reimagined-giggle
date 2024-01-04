3f382378f9a7e1a095f7f07f4ab30f236cbf012d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:05:34 +0000

    risk Rr minute tonight disgusting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(5):
+        print(i)
