683cd27b919d780cdd995282fdcd60ae0cdb8657
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:11:43 +0000

    protest band underneath its within

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
