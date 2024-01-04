08e9656985cbf7244d04e056ea376583ef9e936c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:39:38 +0000

    ashamed hold might wish recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import numpy as np
+print(np.array([1, 2, 3]))
