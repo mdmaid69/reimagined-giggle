53eef92376f41bbfc5b1742aceb11f6a20108c0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:29:33 +0000

    study position juice tax callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
