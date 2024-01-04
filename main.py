87c72eafda7e61d8fa88ff1863332d10b1662f0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:47 +0000

    geton survive naturally object anybody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
