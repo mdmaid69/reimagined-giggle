bc58a6219162397eb2c608a2b1510844fdc31677
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:14:44 +0000

    difference glad sting shade temperature

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def count_elements(iterable):
+        return collections.Counter(iterable)
+print([x**2 for x in range(10)])
