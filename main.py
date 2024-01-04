4aa79b4ccea70bab4832720a06673ecaa2f0c653
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:25:30 +0000

    material plate lunch able apparently

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+print("Hello, world!")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
