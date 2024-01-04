786c77c2df26988772e346c46ce185e608d226ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:50 +0000

    behave church report minimum stone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_itemsize(array):
+        return array.itemsize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
