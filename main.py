ae0a99dfac16deab3bfa1150f9905c605d9da9c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:23 +0000

    back straight shame variety minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
