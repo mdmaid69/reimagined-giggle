fc8b9940e6f525bfb9330bb9f6823bcdb71cb09f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:17 +0000

    challenge lucky permission Oo married

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_as_str(array):
+        return str(array)
