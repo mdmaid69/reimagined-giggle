7aa89a0b1300bbb61719f9f204848698b150be46
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:46:25 +0000

    movement rate occasion nor workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
