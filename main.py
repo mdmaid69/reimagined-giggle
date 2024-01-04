2d077943a284188fa679572a3c90114aa7e8898d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:34:47 +0000

    traffic manner thumb Bb include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
