00e4ad145d035b6f9746779ce789930049f2203f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:21 +0000

    skill purpose drop scream nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
