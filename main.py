5197841142f05832b3471f10b1785dd60d7f081e
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:18:51 +0000

    really bread tape matter dry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_min(numbers):
+        return min(numbers)
