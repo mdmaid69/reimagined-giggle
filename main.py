6b1503607b8420c5df39255a43ad862e587fa133
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:01:30 +0000

    touch friendly complain wander more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
