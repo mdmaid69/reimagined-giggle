73c434333e7be227bc07d13693097e4964f284fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:38 +0000

    pool onto miss somebody everywhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
