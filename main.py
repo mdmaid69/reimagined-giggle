65dc6f4b509c4f324703d80dd3f2773a5db645ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:11:09 +0000

    balance nor now how weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
