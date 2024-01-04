70321bc5afcefb4503bc9e6d4ffda3d9e668219b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:37:11 +0000

    second confident any table single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
