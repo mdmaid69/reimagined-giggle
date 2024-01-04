1dc760c3c67cff81a14e705fe48a720810be249c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:20:18 +0000

    average Tt finish callat before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
