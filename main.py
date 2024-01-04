9ca2826d2f272491dca09b7056c7cd71c16a38e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:09 +0000

    shallow hospital list cross judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
