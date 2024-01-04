3db4d814575164bcc1fd466a1713e486919130bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:26:31 +0000

    doctor set cheat justice kind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
