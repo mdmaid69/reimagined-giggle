79dae2c47338ec2d01ad72296925724b31dc7f54
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:20:34 +0000

    steady true nerve sure nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import collections
+def create_stack():
+        return collections.deque()
+import os
+def remove_directory(path):
+        os.rmdir(path)
