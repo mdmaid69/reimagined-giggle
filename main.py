b5d91e9faa861aa4e82def96d406070694ca25b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:35:12 +0000

    government tell turnover free copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def count_words(sentence):
-        return len(sentence.split())
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
