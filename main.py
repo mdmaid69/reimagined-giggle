c0d8c033324c7add96f1e0181a872abd2c1f7678
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:14:07 +0000

    snow practical emphasize ashamed month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def convert_to_hex(n):
+        return hex(n)
