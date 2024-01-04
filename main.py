7ff81c590970d94bcf63435ee381d0a24f99ca3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:10:02 +0000

    lookback(on) raw special scene constantly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
