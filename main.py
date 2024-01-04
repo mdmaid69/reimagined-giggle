f89d7c5abaa127b1468c33c20b37ccc05981885a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:41 +0000

    great instrument holdon so thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
