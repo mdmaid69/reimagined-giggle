16dc5b7d59d52865dc549efab892dd99888ed1f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:33:10 +0000

    record air deliberately declaration lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
