15f0447dcc989c8c86838edcc6b2009999787f6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:08:47 +0000

    market contrast squash mark record

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
