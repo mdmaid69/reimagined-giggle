935a2508157a59776b183c9d0e20265381e985bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:19 +0000

    card physical arrange connect thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_os_info():
-        return platform.uname()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import time
+def get_time_since_epoch():
+        return time.time()
