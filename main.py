8bebf776f6ca898fa165f5d42be8c3ecd1c19d25
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:16 +0000

    this deaf project cut practical

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
