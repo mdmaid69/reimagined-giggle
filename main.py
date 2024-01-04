d5df034ecb7b550c13cd24e897b8a95337ffe4ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:52:58 +0000

    unkind walk imagine steal safety

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_error_function(x):
+        return math.erf(x)
