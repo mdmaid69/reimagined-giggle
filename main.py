f59ffc179a4c121001cbd62fd4f3707e87d8eaa4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:27:03 +0000

    port upset towards leg Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
