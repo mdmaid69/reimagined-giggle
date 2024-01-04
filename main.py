e621ca608bb27b92e9bbf0cd9bc19f5b0d944f3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:10:04 +0000

    before punishment provide report Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_index(array, item):
+        return array.index(item)
