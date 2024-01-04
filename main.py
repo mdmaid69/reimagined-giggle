8167339199d818ce2c1e8c176fac68def5011519
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:09 +0000

    always hole act bother comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
