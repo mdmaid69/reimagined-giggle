d12ffaeb3fb2a19a57bb32245af5ae5b5cde455f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:33 +0000

    ability keepup talkoutof provide joke

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
