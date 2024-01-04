83c939fb9d38be2e4154ac379d63290b1db27661
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:46 +0000

    visit Vv count unlucky far

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
