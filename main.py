7ce98fe77fb6f1f145e9c1e3526cf6db2bf51d16
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:26:15 +0000

    turnover relative lend limit scream

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
