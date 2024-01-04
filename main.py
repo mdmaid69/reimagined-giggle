138ff6d0ff5f0cd91712e5b6599f38c5c20c6124
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:00:01 +0000

    ground 567enemy moral too place

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-for i in range(5):
-        print(i)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def reverse_list(lst):
+        return lst[::-1]
