fc11d509d0072a2a2e6cfcc37d73cf81f35c3779
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:58 +0000

    today voice office light sing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
