3dd85cb62d746a79f2f6fcf6f1e2e2a2575020d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:39:22 +0000

    complain measure remain him towards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
