af91f6df6569ec584f2c092a947a7ea18a282e7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:07:23 +0000

    most leave reach imagination happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-print(os.getcwd())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+name = "Python"
+print("Hello,", name)
