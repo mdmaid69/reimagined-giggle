4fabf379798e13ce115d0c70d4863f96a464a0e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:40:28 +0000

    star bet only often noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import tensorflow as tf
-print(tf.__version__)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
