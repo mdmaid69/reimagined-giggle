cdd6d612abf2363ded4ab0ab32cb03e9ce8c44ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:41:39 +0000

    ground independent welcome size repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+print(math.pi)
+import array
+def get_array_length(array):
+        return len(array)
