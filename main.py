fc6a76cb69f63ab284e106fa84cf99f170feee42
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:27:17 +0000

    hour class rub obvious firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import getpass
+def get_username():
+        return getpass.getuser()
