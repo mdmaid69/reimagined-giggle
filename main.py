4e2797ce1ef0e88ee8dd39247d99eef24fc79e48
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:48 +0000

    responsibility average design sell thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
