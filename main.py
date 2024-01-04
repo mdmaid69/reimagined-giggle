cefe82dc7e000932018d0b73c8e8ab425580899b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:17:53 +0000

    thumb replace practise file pride

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
