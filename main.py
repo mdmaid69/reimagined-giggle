a499952a86a99eb6e08dabd36e2c6ab9f058b593
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:29:19 +0000

    fight period mention wish surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
