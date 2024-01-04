c42d79e0811ad1bf32dff313622c8935ab7e026e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:04:03 +0000

    talkback bringback letter decision please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_directory_name(path):
+        return os.path.dirname(path)
