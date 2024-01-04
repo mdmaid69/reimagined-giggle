fbc0e377730895cbbc4acec9f95897257905612d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:28:42 +0000

    keep most respect 570enjoyable blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_factorial(n):
+        return math.factorial(n)
