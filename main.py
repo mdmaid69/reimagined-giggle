870f86a68ecd1563f36cea0631425898803bbc11
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:00 +0000

    reliable once run actually discover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
