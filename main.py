7e1020f23ed5d40b3aa081c34e9afbf8b4992ead
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:19 +0000

    low no attract dance create

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
