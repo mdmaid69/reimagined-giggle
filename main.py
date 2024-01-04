9f764784ada9f9b17b1ad6126c9a4c5e74ee7beb
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:23:09 +0000

    yourself relax describe warm progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
