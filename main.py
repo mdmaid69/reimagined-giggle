471a408acff5a079fdffaa734e00dd8c2ab1599e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:29 +0000

    sort curious Mm permission harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
