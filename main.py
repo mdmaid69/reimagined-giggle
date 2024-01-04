67c105113ced59b4f73e26e1ef5e5235f98f34ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:49:10 +0000

    exciting Ff deliberately keep gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_max(numbers):
+        return max(numbers)
