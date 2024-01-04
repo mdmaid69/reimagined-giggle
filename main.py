7b2f186e156dd3cf366427b4bd0f62e65486f2df
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:10 +0000

    forgive score keepfrom plain question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
