4d5b79769c53a5a9fa192b3d0378aedf76755708
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:54:27 +0000

    crazy remove frequent service extra

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_array_typecode(array):
-        return array.typecode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
