fb11daf6b0f31bf51de4a9bb517c466a344c921f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:43:35 +0000

    welcome pretty opponent soil sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
