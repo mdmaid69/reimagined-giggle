ebaeb8088d6399604170dd33d1a0e911bf494aa4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:16 +0000

    try search let contrast old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
