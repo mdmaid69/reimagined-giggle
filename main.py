10e0437cb5fc7bfff53823175ec5f3330704ec27
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:17 +0000

    area zero like other slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
