2f6e68115098f657c478a21956d09faf4fed93ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:54:45 +0000

    black once pay anybody deliberately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_power(work, time):
-        return work / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
