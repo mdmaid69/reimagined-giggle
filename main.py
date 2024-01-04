a767e7140e90af9ebd3eb07b00e6934390938929
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:24:58 +0000

    steer inform commit search control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
