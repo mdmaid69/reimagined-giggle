9abacf76885113d77514dc2578125a4cf67bd270
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:25:28 +0000

    turnon crowded shallow office myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
