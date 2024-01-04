f78915ee097e724d0f43543c72cc6db92d9978e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:44 +0000

    lean positive danger shadow instrument

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-print(sum(range(10)))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import time
+print(time.time())
