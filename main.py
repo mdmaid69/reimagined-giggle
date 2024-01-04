22109ac15b3f042576c71f35c3bb6b091c994a46
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:56:21 +0000

    regret schedule shoulder pause bus

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_max(numbers):
+        return max(numbers)
