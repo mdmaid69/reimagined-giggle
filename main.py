0eec7ebfe8ab96d1addf8bf1fb54ec9ac09a1aee
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:29 +0000

    furniture detail error suckme guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
