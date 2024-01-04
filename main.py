443815e42012d4979bb6554c3e199f273951acdf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:56 +0000

    takeoff error ours argue present

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
