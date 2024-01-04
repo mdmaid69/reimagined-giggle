3804b30e8583f1af4e137a2b4f100d16ec1a1f9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:33:01 +0000

    middle side off string shut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import time
+print(time.time())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
