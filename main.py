16bbe7dc7afd05e390aa2eaee51c900d6bdf43fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:07:52 +0000

    hurry mad hole community hollow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
