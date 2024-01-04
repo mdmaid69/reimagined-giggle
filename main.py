8123f1a1895fbe05ef6a83549cb5695712b5b489
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:35:51 +0000

    handle cheap keepup kill form

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
