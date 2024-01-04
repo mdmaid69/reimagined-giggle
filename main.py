b14b9b46ea5a141f119f95816b3b7e78c3839a7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:13 +0000

    television please Hh usually school

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
