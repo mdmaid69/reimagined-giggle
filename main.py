d3660e4ce3dee4d040b7201ea465f8c4f30b6b7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:30:25 +0000

    still ever goon object understand

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
