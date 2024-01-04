23879743f633b386a474db8628c09faa1e17d714
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:24:42 +0000

    family phone support language polite

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
