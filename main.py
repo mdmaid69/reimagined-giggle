e0f4aaca37d3c6ab8c9b983f706cde1fe6ed539b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:34:33 +0000

    fit hurt tip prepare turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
