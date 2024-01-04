b1f5a68c2167c5e7738fc273527be7ee6d5b5718
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:44:25 +0000

    lookup bear yours correction sweep

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
