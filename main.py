51ab4176047979299d88cadbe0ce681d80342b78
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:28:24 +0000

    anything meanwhile scare once pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
