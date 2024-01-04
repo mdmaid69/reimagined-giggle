f2b1c84cd8dd2dc20f09ace9ce95af6fed400e48
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:24 +0000

    regional Ww battery impressive later

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
