327336ae6893cc31646aa51d377e85fb4648f751
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:09:38 +0000

    mention fine secret practice favour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def count_elements(lst):
+        return len(lst)
+  def count_elements(lst):
+        return len(lst)
