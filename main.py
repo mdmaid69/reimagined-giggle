d135d0b8a3fc990ef3a16cd507b903eb889aaf0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:55:53 +0000

    crowded agree clean achieve meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
