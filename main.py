db62362e51c7bd81c80cb3108c6c743ebef11ec6
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:00:45 +0000

    closely through cruelty talk moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def append_to_array(array, item):
-        array.append(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
