6f02841a7e166a0d5ae6e6451abb022b247a1567
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:07:30 +0000

    brother law slight up choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_exponential(x):
+        return math.exp(x)
