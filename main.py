2e3fa6e497e4cccb28fb480d5d62fe2e43e33a40
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:26 +0000

    heavy workout keepout add contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
