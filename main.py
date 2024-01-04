9e123abade87a2698a0eb6cb355da6cb28b34a36
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:43:00 +0000

    game this travel short impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
