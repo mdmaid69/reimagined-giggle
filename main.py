f884abbcb80b4fa95d22ca5f826d10fdec8d3954
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:23:08 +0000

    kill sign heart stare 179bite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_min(numbers):
+        return min(numbers)
