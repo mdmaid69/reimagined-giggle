725c19400e35669606f17d93db058cc5d40639b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:55:17 +0000

    when deep live wild workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
