20a7ef691a01532323604c9db5d1e46fa5546a33
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:04:29 +0000

    pour giveback pollution regret forgive

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
