1d2eca794e8278fc00ae76639a0c1903dfcbb05e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:00:35 +0000

    forgive active defend slightly difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
