796af6a45ffe278a017d61d15781f3e5beb04ecb
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:33:32 +0000

    carry evening match moment journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_current_directory():
+        return os.getcwd()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
