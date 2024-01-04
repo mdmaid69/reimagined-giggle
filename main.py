17c2818af9c4b8ec4ee11e88db094c63e99ebc74
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:39:45 +0000

    Ww news long straight pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
