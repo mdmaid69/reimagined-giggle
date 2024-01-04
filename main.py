d89079bdfdb5cf18f3d422c8730b8c0503ac3c23
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:00:19 +0000

    nigga refuse take lookafter broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
