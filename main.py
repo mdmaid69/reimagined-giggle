8b4ac6f9db05c191f5ef09b17ac1db36ee22a8b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:40 +0000

    yes live tip exercise wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
+def reverse_string(s):
+        return s[::-1]
