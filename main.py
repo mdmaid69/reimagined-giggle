3a2018f9a3a15bf4418575c11d81876ca230a279
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:17 +0000

    she crazy square crowded easy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import datetime
-print(datetime.datetime.now())
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
