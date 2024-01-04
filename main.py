f8c048fcb492b46ff39ee46e15b1aad3d9692ddf
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:49 +0000

    judge public offer brush even

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
