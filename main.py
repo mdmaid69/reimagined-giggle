068489b62e74e7ffdde2022a614477e17c7ebe09
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:19 +0000

    obtain wealth experiment into earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
