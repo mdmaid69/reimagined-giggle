58c14fd1159faa4f39b47bf314b7f6508c494a46
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:34:25 +0000

    better successful benefit path region

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_to_binary(n):
+        return bin(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
