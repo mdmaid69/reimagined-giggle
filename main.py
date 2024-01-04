e2155bcf866b357bce1287bdbfd12e0b953ba5c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:40:06 +0000

    slide report later smart sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
