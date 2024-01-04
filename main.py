49d51d7e3d3c435bdbea7066c15517c61a1ec495
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:11:01 +0000

    tread clear advanced Vv remove

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
