cf40c4d9ca0e2dff2009467d88092de2f65c85cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:12 +0000

    consist bored term sight fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
