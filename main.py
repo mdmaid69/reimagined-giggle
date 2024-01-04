1b25a44d6b9b448f7a823dcc8453aa762485bc9b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:50:19 +0000

    right cross bit genuine contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def reverse_array(array):
+        array.reverse()
