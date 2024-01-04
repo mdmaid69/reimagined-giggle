f45d0b818f6d4de1661df41bc36c04e92f9e0e54
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:08:35 +0000

    common discussion fire her dick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
