75631470238bf232bf15e569bcbf8c61b4dff03c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:08:41 +0000

    here permission challenge week see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_as_bytearray(array):
+        return bytearray(array)
