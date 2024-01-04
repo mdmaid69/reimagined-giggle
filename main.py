7674d18f0d9b64af6ff676ea49656b988187a3d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:02:10 +0000

    listenfor about lesson face Jj

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+print("Hello, world!")
