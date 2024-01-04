d67c71b1b7f70be21fa18761b85240a73611a1f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:51:50 +0000

    really land Zz runaway consult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
