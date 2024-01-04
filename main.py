d42da7c2281e09fc86c69c9340d1eb59bd0e4955
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:18:34 +0000

    quick whoever this make regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
