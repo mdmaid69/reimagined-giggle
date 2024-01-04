241ab1b242806e78bb2c7737929bbb458bd8d65f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:11:29 +0000

    stranger them soil solve cover

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def find_max(numbers):
+        return max(numbers)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
