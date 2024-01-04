a80e34baa0b6fa25ad3059fed15365834b301a9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:55 +0000

    somewhere open open deserve regret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
