b77c90bcd5f9e1a53ab49ff8d056dba7e80d284c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:16 +0000

    forget approve workon circle job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
