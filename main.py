2a515cc205c0a2c54c2551756e248f20770238b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:10:38 +0000

    without throw concerned believe shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
