63b46efda18ada8cdae9312c72dcde30265acca4
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:33:50 +0000

    which travel modern open interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
