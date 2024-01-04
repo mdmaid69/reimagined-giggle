866fd63b7f224505f74dcc99549c8dac0ff35446
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:02 +0000

    happy occur business above church

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 def reverse_list(lst):
         return lst[::-1]
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import time
+print(time.time())
