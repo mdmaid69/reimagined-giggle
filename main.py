2edcf3ba3c6a8dd3fd8a382e40188af19bb42166
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:16:13 +0000

    count past respect mess press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
