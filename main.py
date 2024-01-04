8f1ed8e08652e0f9cc4bd755f948164fbb6843b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:23:32 +0000

    make nerve because address threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
