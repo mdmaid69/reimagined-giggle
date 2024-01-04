6620cda2070dd32a4131988004dfa708b018ce32
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:22:47 +0000

    have hurt only direct hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
