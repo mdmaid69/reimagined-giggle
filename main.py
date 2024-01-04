2ed3cd28c1636ca287e28943e8abd0b3c7852d15
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:24:19 +0000

    feel reduce dress delicate average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
