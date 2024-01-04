61dee981aa578179ec41735672a35f3f0afa7b2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:31:36 +0000

    niggaacademy education balance opposite appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
