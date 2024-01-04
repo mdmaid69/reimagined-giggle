d7583efc75000455e346a77de0f89c389e5f3d4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:30:43 +0000

    turndown course stranger thought large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
