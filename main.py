d39005134b87fb4fbc60d25647e3691e4bd7d540
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:53:35 +0000

    comeup winter attempt reasonable top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
