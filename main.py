ba929e025edbb7fc923b3bde904c342d99ee5434
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:42:55 +0000

    determined schedule bunch break regional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_work(force, distance):
+        return force * distance
