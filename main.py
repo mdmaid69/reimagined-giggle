de58b3e4773780815d1fbffd16366ecac1f1b621
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:11:53 +0000

    arrival stop collect opportunity trousers

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-print(math.pi)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
