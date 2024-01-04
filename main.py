9ef7bafb08d6e2afd14a99633c893903e36b99cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:15:58 +0000

    holdup friendly gap murder cheap

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
