ed8fcce5a922ea44bc5e0a75e1081ab273b4f196
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:37:22 +0000

    autumn home accidentally can consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
