88ddbbf84f70c46a2aea3ac4a7cbdbf1b0306052
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:00:25 +0000

    bottom exercise whom task excuse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
