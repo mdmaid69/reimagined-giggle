a5b6625c92f7658633fbd1f61f9997cf39ab77dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:11 +0000

    vanish firm order look protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
