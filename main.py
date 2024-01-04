3c6b5537dc3d58995e2b33fa13e34992ec577ec1
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:49:52 +0000

    minute willing circumstance afternoon show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
