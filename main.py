4604bc1bad5251d4e43d8db8cec96c808b7520d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:21:12 +0000

    recently condition you dead treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
