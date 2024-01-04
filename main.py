4f63b95ee9f4c58f9fb217165e3405df6b3b6810
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:36:08 +0000

    judge nonsense storm shoot picture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+print(os.getcwd())
+text = "Hello, world!"
+print("Reversed:", text[::-1])
