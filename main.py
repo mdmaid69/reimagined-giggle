cc0e7a3f1227fe2ed64d93bd2c8be38c41ead472
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:38:36 +0000

    hurt mystery home kick easy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
