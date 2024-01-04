0f110e102ff47737cdacf7a8c836d17275fa2272
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:00 +0000

    sit recognize mind borrow bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
