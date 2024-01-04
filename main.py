8f746341677124c9155b4c4402af230844b84c42
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:24:33 +0000

    minute hide light attach further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
