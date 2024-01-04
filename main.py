baa587aa708008a380ef4bfb14499240e47af8d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:59 +0000

    appearance left possibly talk exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_list(array):
+        return list(array)
