f2946093b9e858735295e73f6b8aec4c590ea80b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:57 +0000

    estimate lesson head spill moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def append_to_array(array, item):
-        array.append(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
