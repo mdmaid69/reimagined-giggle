7e8ba0f6fab91c98efc6407f1747c0c8187acab3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:16:05 +0000

    conversation relationship forever please satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
