1d94c02f4c9531822df54a1b39995407fde224fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:01:08 +0000

    that time thinkover everywhere escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
