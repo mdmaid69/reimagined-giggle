6b4e5bfb303f5a3b1a78ab1f0b859d40e8650a2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:42:44 +0000

    luck argue wipe die off

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
