2df971e65313c79207f4156b16a6a779430ae973
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:48:22 +0000

    formal square watch fast partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def convert_to_binary(n):
-        return bin(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
