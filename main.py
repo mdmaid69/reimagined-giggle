6a355a0f4750594e8349951752cde645d37723fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:27 +0000

    bet place everyone stop duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
