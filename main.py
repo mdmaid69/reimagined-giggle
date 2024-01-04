59f1cbcb8eb04ab64796cac7439d7b0ed41f081a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:58 +0000

    flow trick bringforward train deal

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
