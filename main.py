30ad2c67e929b9458d5ba3cf6ae56cde016453ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:44:33 +0000

    once form believe mad marry

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
