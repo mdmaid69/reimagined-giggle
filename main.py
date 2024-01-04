bbecd15e76c92784e82a63a5d1223685cb1e648d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:56:08 +0000

    probably condition such practical hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
