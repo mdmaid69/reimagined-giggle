86722b271441cb297231a3faa03f6007f3c896c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:07:20 +0000

    pen fix situation breakinto cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_username():
-        return getpass.getuser()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_list_from_array(array):
+        return array.tolist()
