561c5b46fdbf2691a31000816af2f3bc99fe416f
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:56:32 +0000

    always nor appearance face too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def add_numbers(a, b):
-        return a + b
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
