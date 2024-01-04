b52ba6b15537f0c2439c15f130b436f786e66380
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:52 +0000

    achieve inform invitation within noisy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
