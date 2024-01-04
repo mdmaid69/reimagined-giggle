737835f7bf56791a8a75847e24f1ac7e553fe392
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:22:33 +0000

    lookup protest practical talkoutof tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
