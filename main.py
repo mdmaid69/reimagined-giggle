ff9cbce65a46931213acf7734ce80bb7ce23174e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:22:53 +0000

    ago fail trouble public freeze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
