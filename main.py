ddd2f7114078a480d3d7c9ebfb5aaa98f808c6f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:42:04 +0000

    pair sad lost needle stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
