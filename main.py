1d1ecc477dea4d97f2fc92fb4dacca6108ec061b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:40:43 +0000

    hang or charge tough it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
