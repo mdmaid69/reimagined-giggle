d3fde7702c41682f3079fb49f54476c81ce053f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:38 +0000

    yet Mm seat only thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
