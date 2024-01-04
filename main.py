82f7fd9c4f7b135252db37a83d6d0a30ee84f87b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:40 +0000

    mystery drop talk price turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
