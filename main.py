125deb5f07defc82687b19681c00ebd370b08bdf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:54:18 +0000

    display though borrow thing belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
