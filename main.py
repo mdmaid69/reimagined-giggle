ce1165eb47079829c76cc034042d3ee5d9f42f70
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:31 +0000

    reason fix view popular equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def find_min(numbers):
+        return min(numbers)
