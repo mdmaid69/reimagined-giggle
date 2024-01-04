c8ed473d410dddcc107fa3cf8beb9370a7a9b6ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:26 +0000

    male noone hollow recent close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
