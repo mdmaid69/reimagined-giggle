bdc54acf33d878ec53d61448a183a4208883203c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:56:05 +0000

    threaten clean easy practice attach

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
