b258a7e120ccae6e4ebf3510940ee8d087028565
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:45:12 +0000

    exit limit imagination bath laugh

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
