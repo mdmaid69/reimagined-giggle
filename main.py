07da9267cb00c58c75607ff9be8019fbdf813419
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:33 +0000

    special spend judge late close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
