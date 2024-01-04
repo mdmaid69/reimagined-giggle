54eac92c8da6283f4ff23c1fb594a6a1ca0b9fb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:14:27 +0000

    tip tool us stop well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_str(array):
+        return str(array)
