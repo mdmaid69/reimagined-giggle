3f40f31124aa937de16ba06929cb4c67e733d6e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:15:51 +0000

    peace shopping stone offer his

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
