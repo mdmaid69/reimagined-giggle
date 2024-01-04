eb8370bd54a104e56579bb7e3e590abd96dff34e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:02 +0000

    talkinto damage punish computer persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
