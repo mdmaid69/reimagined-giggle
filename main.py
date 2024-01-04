40f363b0ca69ea9233a5ae4f5a53e64d81c437f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:36:36 +0000

    together excuse power pen table

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print([x**2 for x in range(10)])
-for i in range(10): print(i)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
