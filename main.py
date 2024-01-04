5f3cdd6296131f86aeef8960c567d65a15b4649b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:58:52 +0000

    main discussion reliable congratulate sound

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_palindrome(s):
-        return s == s[::-1]
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
