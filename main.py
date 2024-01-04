4ec0090e8b1c711f2786e880ed09c09ce91a3963
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:28 +0000

    moment allow though he run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import datetime
-print(datetime.datetime.now())
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
