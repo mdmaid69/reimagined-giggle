cbcbbf3945c183b7bfa5498595b4bd1872684453
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:14:15 +0000

    second hold friendly extremely juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
