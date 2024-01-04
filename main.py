fa90b771b096cc4755efe5fbce429d78529a6d67
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:00:39 +0000

    across dance fence blind his

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
