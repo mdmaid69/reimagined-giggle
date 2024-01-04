35fde695d864f5146fab9de790d9d137c8992e73
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:36:20 +0000

    crash 568energy sink great dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
 def calculate_npv(rate, cash_flows):
         return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
