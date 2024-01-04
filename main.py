4fdb5a1394a0201800cf5fb13200f7c7d7924f19
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:49:14 +0000

    central mixture in coat train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
