1bb58e0575600bdc2aac0e6429f6c07fb683f917
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:58:49 +0000

    day direct judge scream sleep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
