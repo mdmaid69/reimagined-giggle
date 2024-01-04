5a1ac459445157d8c415feaeff1b527fb1e9b727
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:58:21 +0000

    impressed exactly switch division difference

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 def find_min(numbers):
         return min(numbers)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
