7c569cd5f944eed64b6109731a0a40b15cb90c0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:40:29 +0000

    calm male not flow none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
