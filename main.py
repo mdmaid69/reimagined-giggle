91d3793c6e96f1465a7b84b0fa1ba35a533383fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:23:59 +0000

    convince tax process circle wherever

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
