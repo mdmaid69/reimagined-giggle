2a11d296d8ad6daee22b963a395369ae93a1eebc
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:49:00 +0000

    variety respect even criticism after

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_list(array):
+        return list(array)
