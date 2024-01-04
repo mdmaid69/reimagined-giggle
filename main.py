9b5c050aad148941a0c7bf0d5945d40ef2af8bae
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:49:24 +0000

    within makeup thinkof cheek range

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
