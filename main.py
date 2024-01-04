c7d7943aa15f7868566a23881cab112abfe9543c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:39:45 +0000

    swimming contain rest forever payment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
