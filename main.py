4976e037deeedca8a831d9c79900e9329ca795ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:54:43 +0000

    rescue period prefer also careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
