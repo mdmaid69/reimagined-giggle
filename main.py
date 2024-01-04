d549bac0686d491303cfe52ea5c20723be9c7c3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:06:42 +0000

    window offence slice next risk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_speed(distance, time):
+        return distance / time
