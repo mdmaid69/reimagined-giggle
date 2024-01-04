9c5bdeb7135b27a414597663cfab8f5ac825d529
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:42 +0000

    discover shut creature usual down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
