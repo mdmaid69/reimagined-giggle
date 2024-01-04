9b11e370cbae7e57ec9c1f3d68bf57c8d2815633
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:51:08 +0000

    turnon tune be negative stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
