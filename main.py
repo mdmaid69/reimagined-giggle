6326fc75cafe4a2593d2ba3167c040532f35fdbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:07 +0000

    area male motion none conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
