efd1a7bf6993c88c63335c70aa8fc0b200d6dfe0
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:19 +0000

    everywhere important error Uu offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.time()
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
