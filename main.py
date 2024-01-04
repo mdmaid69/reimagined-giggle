eb7f164f8d6da232c472c5d44f7cfa3ea3ab1001
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:13:24 +0000

    tidy introduce interview dead repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import time
+def get_current_time():
+        return time.time()
