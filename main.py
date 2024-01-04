c4dc7ad330af75e9799696fc411b5821103e1c04
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:35 +0000

    more week clue correction complicated

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
