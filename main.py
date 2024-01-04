b1034c806e6774dde75b0f3c66496bd0763c5dd1
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:00:28 +0000

    evening up disgusting possibility ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
