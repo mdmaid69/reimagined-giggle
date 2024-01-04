d2ef234cd076fe40cfc19da2774becea1b176eed
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:35:43 +0000

    lost touch free breakfast boil

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def convert_to_octal(n):
+        return oct(n)
