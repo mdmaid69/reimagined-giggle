50cfe90238f35fe358cb1b7000e17281502226c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:16 +0000

    stop operation intend drive year

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
