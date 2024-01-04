57bf3cc7f4fbbb8f95f9e145c2972444356842ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:35:52 +0000

    tape balance perform report answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
