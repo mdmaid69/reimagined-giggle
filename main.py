eba9a752210859af7c52508227839185d635366b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:51:17 +0000

    beside eager public capital attention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import time
-print(time.time())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
