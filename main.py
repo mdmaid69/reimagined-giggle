e82cf1af36081ebf0338949800b0f4036c62cbcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:56 +0000

    available interrupt lunch save confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
