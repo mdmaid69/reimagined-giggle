a28bf1387a757780c27ef7deb979cf8a6c4db6a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:24:47 +0000

    tragedy reaction great tone advise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
