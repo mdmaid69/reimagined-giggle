0043a203518ffdc6e6bae71f298ab5345bfe7b56
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:35:29 +0000

    ability right scale reduce book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
