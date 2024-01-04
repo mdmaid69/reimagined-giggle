273f96bd4c4fb0f8faa3eabc1db6473d572b6ec3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:49:04 +0000

    drink blame breath may aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
