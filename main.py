301b5b68f8365d44acaaaedd22b85ffaafc12159
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:27:23 +0000

    rise on measure beginning doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
