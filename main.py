f447685b1a197ab37cb6b11dd4bf5593cb2e58f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:28:51 +0000

    save our figure strict bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
