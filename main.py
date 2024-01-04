a5b3c3282568c2ab947dee1a76fa52a4edce386f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:28 +0000

    keen skirt regard suffer knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def convert_array_to_list(array):
+        return array.tolist()
