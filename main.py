915676ce8302378dc0fc2b0e6e5ed95244b6b32b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:36:27 +0000

    claim reduce contain open this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
