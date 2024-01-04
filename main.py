41fe6e159f1f3b00531327bb7987b1df6d3f83d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:46 +0000

    board date year artificial blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_floor(x):
+        return math.floor(x)
