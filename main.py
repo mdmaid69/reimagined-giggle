117faa99503725b4e906abdc94b800cbd69950d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:23:37 +0000

    hope strict runover thirsty experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_set(array):
-        return set(array)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
