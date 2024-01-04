39d83000dfa5714e424e5f9d7af46c5f7c9a08a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:15:49 +0000

    block run remove criticism minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
