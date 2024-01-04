927a53d2e21cd83849724b71ef5fd4c496a5f1ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:09:37 +0000

    line mistake person traffic feel

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def square_number(x):
-        return x**2
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+print(math.pi)
