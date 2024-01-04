c4eb3d689864923a931b5edfc7ec9eb8fb0679df
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:17:33 +0000

    wear half decision happy mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def square_number(x):
-        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import datetime
+print(datetime.datetime.now())
