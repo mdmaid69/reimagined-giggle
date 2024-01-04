7c44c462ec28fdad300c9696ced85b92b9ccfc70
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:51:51 +0000

    keepup offence average along expect

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
