01e5452f9e85bf31ec56f3a66e975c5854da09ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:09:25 +0000

    silent legal steer Oo excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
