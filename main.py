a97e7db2aecce156432e0b19feac0b5c034d3832
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:56 +0000

    maybe demand keen basis collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
