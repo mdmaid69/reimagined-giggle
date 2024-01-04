1dc59ce5020c1c6d2d08d13fa806b8471f5af03c
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:32 +0000

    comment anywhere pinch near score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
