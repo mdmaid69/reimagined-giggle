27bbdaa64e5965e319815bd845b5d7e9cfcffdb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:35:10 +0000

    yourself approve interesting hope geton

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
