8dd284885e3a1bc55618a9e1229a083c54c1c9cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:01:20 +0000

    fire space shock of strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
