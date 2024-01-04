a6275a86100d37cb768cd5595b53f2f35ca99ee0
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:08:37 +0000

    file dish case performance even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
