e21aea2f4a5cecdea683df985291fe9ed596af18
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:42:29 +0000

    eat walk hour commit accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
