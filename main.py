48f81b9ba52de96ab74e9eff6eaef1e23ce3ed2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:02:02 +0000

    final patience also embarrassing compare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+for i in range(10): print(i)
