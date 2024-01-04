dad9456311b3361061c064d248e8f9ddba9e92dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:38:54 +0000

    kind square polite noone almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
