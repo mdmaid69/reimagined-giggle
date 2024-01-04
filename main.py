533339f64d6f8600673a23110b6b6ffabd270c42
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:51:09 +0000

    hour once heat protest error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
