5cfbd23ba3ea90a66548f8325cc476572d3100b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:56:25 +0000

    notice draw contact after mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+import platform
+def get_python_version():
+        return platform.python_version()
