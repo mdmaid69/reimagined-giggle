907bc0500d20027fb1419a02ae60301dee6bd2bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:49:25 +0000

    report surface pleased Ii respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
