0c6f7b90d8ef2755931e62322d68579e2eed30cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:57:31 +0000

    patience few afterwards heart perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
