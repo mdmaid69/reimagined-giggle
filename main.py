b0014e6560bca87a469003a1ff447f28d9f6ba1c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:34:18 +0000

    sell commit goback(to) home characteristic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
