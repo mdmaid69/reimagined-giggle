a7b01f587f777cf1569e7ccd9fd4e3be356ce34c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:08 +0000

    memory confused consult rob help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
