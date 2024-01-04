5eafaff90946e7ef5c747cdb45a59f38ef743e6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:41:10 +0000

    leave him solve message recent

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
