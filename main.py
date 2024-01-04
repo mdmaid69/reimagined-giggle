bfe797893000179f510278ae303c5737e77bfaa2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:40 +0000

    wrong instruction dance getup true

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
