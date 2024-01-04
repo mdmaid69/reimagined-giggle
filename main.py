cb4a0bbb647f5f6602bea676bd2891349de2b9f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:43:15 +0000

    invitation paper raise goup oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
