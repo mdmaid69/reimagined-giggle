457bb728fc551deff6ba1931015f8c2ba47b9bbc
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:43 +0000

    appearance increase fix believe deal

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
