5814762a69933ff6916ae5388cd19d06c45ee742
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:59 +0000

    lean quantity rest edge nor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
