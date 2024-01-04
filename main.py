6c0082b0cfd8053129a65cde66b2c941c13b79bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:45:30 +0000

    middle exhibition wash cover stir

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
