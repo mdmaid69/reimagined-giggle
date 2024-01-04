ddafa3f18fb8910c0129e2ef64eda0e54fedd8fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:45:35 +0000

    future active approval lead fit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
