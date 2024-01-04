8db0f70578542602f82c0e908b839f7121e34afd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:50 +0000

    towards pull independent wind exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
