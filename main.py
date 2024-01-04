c3672371e41e52f19c62e31f4c8aaaf72da53e27
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:35 +0000

    strike yet classroom pattern disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
