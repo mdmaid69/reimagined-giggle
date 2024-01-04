84f2dff2b95ff5a35125f00173fa9621d77e1cac
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:30:11 +0000

    popular correct mess lost central

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
