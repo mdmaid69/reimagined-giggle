4a54d0c420e3a572567bbda0b458e436af01fd2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:59:30 +0000

    dry us building rush recover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def subtract_numbers(x, y):
+        return x - y
+def cube_number(x):
+        return x**3
