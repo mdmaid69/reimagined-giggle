2fd468f24682d999d5e6543f3f9ac2323f7f8f86
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:02:40 +0000

    watch mood spare keepup brave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
