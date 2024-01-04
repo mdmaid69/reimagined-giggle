870fa8db9ce4cd51456562c1f3df5e32a7face7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:41:05 +0000

    coffee press lend without expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
