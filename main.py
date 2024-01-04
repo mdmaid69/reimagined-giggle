bc6f67e532dbfef942b4cd199f6030ecc75b56ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:31 +0000

    trick mouth giveback must independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
