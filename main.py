3bdcf5d256cb5d72a4b83a516e5434d2f5af6d68
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:57:21 +0000

    special him rent repair exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
