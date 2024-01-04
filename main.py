fc85828ff52508adfdaf652e2bca5600bb1b7f94
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:47:54 +0000

    with sea inside quarter watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def count_elements(lst):
-        return len(lst)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
