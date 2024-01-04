cc5ed1720f656bf4efaedac8ee3e36772a53d7d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:25:35 +0000

    matter interview benefit exchange type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
