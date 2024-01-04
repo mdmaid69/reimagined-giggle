05d027ccc539f319d1a8318897a33ca986f79211
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:58:12 +0000

    something Dd gift clear birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
