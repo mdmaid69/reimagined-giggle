60881b1f9381b60731e3c5301bdb932a22317c1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:56:07 +0000

    speak some sight party through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
