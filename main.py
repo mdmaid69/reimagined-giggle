8afa2608f1f24ce4f2e1ec8d7d397af3b5ba3e2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:08:24 +0000

    measure comefrom right than issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
