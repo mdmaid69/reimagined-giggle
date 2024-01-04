87f56b949e84820a601aa0c95f40c399534df3d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:21:40 +0000

    quick wherever wealth at bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_pressure(force, area):
+        return force / area
