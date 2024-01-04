c61cf36581317ca4b653122ddb88a3628126c033
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:56 +0000

    pocket wait breakinto sky forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
