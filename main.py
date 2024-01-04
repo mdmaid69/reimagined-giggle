ddc297352d14d55b0fa6cdfaf2f438c529eadf0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:22:12 +0000

    smell pay married pass responsibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
