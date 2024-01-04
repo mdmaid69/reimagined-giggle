fb6627e1562dde30670308b95159bf82e9e13283
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:42:40 +0000

    settle me furniture wear interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_min(numbers):
+        return min(numbers)
