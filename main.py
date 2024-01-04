b1eed62ee0cca031c5a7313ac692a9cb20026c6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:02:08 +0000

    scale responsible improve belong active

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+print("Hello, world!")
