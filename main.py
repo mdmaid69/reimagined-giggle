243069365d8fe45b79bbb8b6018c15b2876e641b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:20:51 +0000

    keen gate means dead fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
