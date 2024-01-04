5f0920c75cfec5c77970eca7550e5e5496b81b34
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:21 +0000

    steal because instant everything dish

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print(sum(range(10)))
+def is_palindrome(s):
+        return s == s[::-1]
