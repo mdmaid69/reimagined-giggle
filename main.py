4c03acf63be98acf6410f3eb9dc442db67cb39e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:04 +0000

    matter slow description loan interrupt

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def sort_list(lst):
+        return sorted(lst)
