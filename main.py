4e7eab6178a37804c0c34b6676e4dc2862fc1780
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:41 +0000

    ring forward understand list experience

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
