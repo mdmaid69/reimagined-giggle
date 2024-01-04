ee4680d0678633db9729d055539dc8900488f21a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:33:53 +0000

    calmly tap clean remark consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
