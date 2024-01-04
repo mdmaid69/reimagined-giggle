cbc4ff639c06fb28ec7a9a56d4654f45e9ba8387
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:20:47 +0000

    error side task safety strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
