40a309a7779a9d1e75adc9194f4edf0008296282
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:34 +0000

    callup combination emergency history congratulations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
