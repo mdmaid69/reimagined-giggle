5eccf1268288b79104441d341d6a39694589df4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:28:52 +0000

    progress dare aim art machine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
