d451f432623451a12f1d9051f1899dccd1cb99b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:33:18 +0000

    unusual exact wave realize fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
