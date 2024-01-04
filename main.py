e17e801105dada6a49532979034179950a75bf46
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:13:47 +0000

    many hole cry harm false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
