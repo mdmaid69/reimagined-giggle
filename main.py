f0f958a22ed7d557e05d88e7974cfa2eadaa00b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:05:04 +0000

    single pass pinch spare train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
