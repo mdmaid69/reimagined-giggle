a16b6a0951ba51b42d93690d8d6dd57106bc36c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:05:27 +0000

    much fix friend schedule alone

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
