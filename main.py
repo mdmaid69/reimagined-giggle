93de32835170534348ad74ee7061a61036e88563
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:40:51 +0000

    top order meaning roof callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
