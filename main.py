480e77f216a5606c921d0c4c41e838a179059794
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:15:53 +0000

    Oo light thank hungry love

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
