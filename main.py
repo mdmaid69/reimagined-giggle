b6ee50409edb76b7c43c4145c6cd8c942d38fc8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:29:40 +0000

    good tool uponon nowhere sudden

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
