8f8ff2c33fa4a7b498f5364cb1e13abe2d0d158c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:08 +0000

    sort close search another themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
