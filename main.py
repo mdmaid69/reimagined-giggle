2f70c00ffe6f4d784ceb51c0dbfdf1f96ac00d2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:11:24 +0000

    touch remember lost invitation try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
