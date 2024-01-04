4f7ffe8c036b45c617e8c39220343d8873c0597b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:47:41 +0000

    bringup chase breakinto jump original

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
