4a44f0bbb876a149ed65764e4ade3a713d60c86c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:25:45 +0000

    hot complaint quality heart about

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_characters(sentence):
+        return len(sentence)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
