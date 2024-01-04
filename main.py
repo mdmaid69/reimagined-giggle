aacee7246019b1570fff77c3327b1a3d63d31c33
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:54:07 +0000

    for measure bottom trick total

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
