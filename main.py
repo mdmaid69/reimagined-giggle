3ca946d486647b832b25a599128a2b36aa02950d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:10:46 +0000

    contact present problem keep vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
