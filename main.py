705707c2dc9c562a81e41b808cc768d636b759ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:23:27 +0000

    aim sick tool enthusiastic negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
