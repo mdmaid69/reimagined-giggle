658065c2a12ad4656421bd881dfb5b7f889d7a7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:10:18 +0000

    pride extremely dream job rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def count_elements(lst):
+        return len(lst)
