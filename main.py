2732abde3bc987af1a9d9fef8fc91a56bbfa4029
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:41:43 +0000

    centre bridge right sensible cost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
