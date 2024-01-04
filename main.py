fe8bfe2d36839d7dcb5703b67b7d5913a5635ebc
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:06:30 +0000

    shock distance arrival strange goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
