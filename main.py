f14fa86e9a36df22e1d1d42d5abd5ac97fe86f40
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:51:33 +0000

    greeting float embarrassing lay education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+for i in range(5):
+        print(i)
