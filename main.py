fec3d3d581b9f3c20e11c70ec681af6069f8f8c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:08 +0000

    Itself middle label fetch like

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
