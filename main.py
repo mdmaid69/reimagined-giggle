687f233a4a3c833e544352f4d596df12d489626d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:33:24 +0000

    song visit conversation proud nerve

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
