c93a6a6e1f37ab618363804293adb89f7e2a1bc1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:30 +0000

    acceptable turnon lower pleasant building

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def square_number(x):
+        return x**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
