7b7475b21a80dde947569101c3fd0556e50b41f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:15:57 +0000

    shock square prepare occur water

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
