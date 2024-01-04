8e6a8951a5829307a4429169d129489779e78ab6
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:34:43 +0000

    fight crime rank cruel question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
