887d689c48c17310848f3401565bc5dcb3cf6760
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:02 +0000

    than create from woman model

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
