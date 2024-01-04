c54ae8923960d99fd33e0ef09700aea43c77cbb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:09:22 +0000

    delicate discovery accommodation pile tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_characters(sentence):
+        return len(sentence)
