d544a74115a3ea4ab57ed4f324893400b7e2906b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:24 +0000

    identify confident temperature pocket congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_typecode(array):
+        return array.typecode
