caaf9cbef38af60e5a55a1db51dc542d9923d7f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:01:33 +0000

    noisy within half unless recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
