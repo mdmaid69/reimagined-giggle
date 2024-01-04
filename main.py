8cb16384309cc8f7f748474aec69c3a7066cb12f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:26 +0000

    beautiful secret environment around live

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
