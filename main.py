7562b0e9ea2226852f3d5b5b8872703d165aa414
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:18:13 +0000

    makeinto concern guess argue bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_stack():
+        return collections.deque()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
