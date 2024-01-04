b5e9ea8679a0f9b3a06d35d21ae236dc77ad8368
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:43:33 +0000

    who past too power willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
