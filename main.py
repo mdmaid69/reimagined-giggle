bf5a7edfa1295329cf9f941020e514d1817fc323
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:49 +0000

    beginning keepfrom later border fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import platform
+def get_os_info():
+        return platform.uname()
