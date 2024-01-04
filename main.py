b068ab9893e52015093ef11822adb13ce79a5562
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:57 +0000

    inform swimming ordinary twist regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
