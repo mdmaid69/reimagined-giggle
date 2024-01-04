b451d9c40511b31800a1f2dc30884e2dafcc03cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:43:17 +0000

    standard sure quantity check improvement

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
