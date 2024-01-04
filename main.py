be77abafca0bedd1a91125449cb20e72fd0af619
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:03 +0000

    debt birth weekend lucky around

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
