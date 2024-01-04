fe9bd58e7ffec833a484f4023f086bb12dcdb30b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:11:45 +0000

    doctor meal seem pot jump

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import numpy as np
+print(np.array([1, 2, 3]))
