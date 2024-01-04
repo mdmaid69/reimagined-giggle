940ac8838ea69a66754ce9d0e37b5fe2b9cfa941
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:47:25 +0000

    base power clear wrong project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
