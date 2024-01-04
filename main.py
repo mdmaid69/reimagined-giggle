2a382d08a5c6f9bbada4794d1b917e2532f1c622
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:11:52 +0000

    pity muscle address opportunity fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
