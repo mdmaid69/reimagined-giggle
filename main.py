23c1732e6c048d19601bce82950801bde3a4fdbd
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:11:57 +0000

    modern traffic poison workupto bush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
