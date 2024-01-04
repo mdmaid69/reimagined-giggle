7a150bb1d74b800c0ab14e26d37e13e033cc5750
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:57 +0000

    Kk notice tidy crash learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
