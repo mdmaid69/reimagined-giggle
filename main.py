b86da3070abda648d26a312f43fdc8464f04a232
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:28:18 +0000

    under impression accept giveback swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
