c1d5fc551293e64d9e6286e999bbd43e620e858f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:14:50 +0000

    swim children correction search giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
