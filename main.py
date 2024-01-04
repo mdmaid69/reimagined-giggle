100556f8b3ca16db206403b8156cd455664009cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:11:07 +0000

    lead bet such address copy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
