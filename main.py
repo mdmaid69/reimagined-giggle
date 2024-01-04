ce18d538ddb39a63cf42af469910e1c24790525a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:04:20 +0000

    shine regard coffee afraid moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
