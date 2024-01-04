76c78f12d8ffc34c40dc126c52ba9d0cf816b0ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:36:34 +0000

    legal clear bang callat workupto

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
