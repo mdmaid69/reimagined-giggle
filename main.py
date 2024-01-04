3025bbb921334a01451f42aa7ccdc00572a84364
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:44 +0000

    guide callout underneath sit live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import random
+def roll_die():
+        return random.randint(1, 6)
