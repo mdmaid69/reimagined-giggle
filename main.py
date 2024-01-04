a009f3de0147461c9c03d905457a15b7a082b781
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:27:30 +0000

    possession collect any sad emotion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
