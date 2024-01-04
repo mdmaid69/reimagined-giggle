954341e523706e3fe59dc48cb37a7d91463b72ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:59:29 +0000

    contents maximum comefrom bridge long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+print(os.getcwd())
