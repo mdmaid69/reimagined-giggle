d4c01622d6482caa5561138dc8d00a656dda0c84
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:12:46 +0000

    in mind wave make hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def count_words(sentence):
+        return len(sentence.split())
