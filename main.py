9e3044ee81a8b45c7e028d7fb84d7b1d1f6e55e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:35 +0000

    habit part deliver fault oil

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_speed(distance, time):
+        return distance / time
