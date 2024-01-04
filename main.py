d48cfe6ac6e09ed3e35eedbca932b2f7c05c2366
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:42:47 +0000

    whom crazy 564encourage thank stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
