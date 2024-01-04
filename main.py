205a7605fadc9c6331ffbb21e66d8fac18c6a61a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:13:05 +0000

    fight active butter turnon relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def reverse_list(lst):
+        return lst[::-1]
