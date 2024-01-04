c62fe037ff1ae044e17ab1601be451f1f28f12df
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:21:54 +0000

    needle beneath moment ilovedicks tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
