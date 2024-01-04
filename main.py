b8046d29b89620eaced8f568951b215f4e2d2066
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:38:08 +0000

    stop slip throughout support relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
