8cffd6f979b3d03bf2b2061f40c0851478bfa51d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:11:17 +0000

    suckme bit bet minor afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
