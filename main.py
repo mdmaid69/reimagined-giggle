b03760b9217f2523ab6a6638f9b1fade07d44030
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:34:28 +0000

    effect warn yesterday strength cool

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_max(numbers):
+        return max(numbers)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
