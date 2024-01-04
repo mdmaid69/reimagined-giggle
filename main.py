96b296d2e403082fb2cf744d74db236c11f9e973
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:30 +0000

    yes bank brush range guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
