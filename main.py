69a4063ec77b6091822e2218e3285d55d2745550
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:04:09 +0000

    balance hurt clear exist judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
