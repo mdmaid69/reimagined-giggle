6aba678f984228c5c9ebd161e3053cc6409bb661
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:10:36 +0000

    workupto straight same or pinch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
