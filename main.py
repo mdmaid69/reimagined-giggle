4e0f7d3a90408f19739c20f7d038b7a856fdca50
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:02:55 +0000

    print rare order satisfied depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_length(array):
+        return len(array)
