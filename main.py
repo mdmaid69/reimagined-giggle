2263f63258b90e3ec9f17e73a22897eb375c0602
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:34:38 +0000

    shoot takeback fight sort generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def count_elements(lst):
-        return len(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
