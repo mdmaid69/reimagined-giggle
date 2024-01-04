69c0fb80c16486ccf367c367254d02d0ae80f3b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:51:08 +0000

    dance cover underneath consist store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
