19a59e3a8e87787807afde254d6bc4f7600fc314
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:21:39 +0000

    return drop again mend people

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
