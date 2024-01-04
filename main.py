f13f77ee60c406617d370e8758f167fa03bfb40e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:49 +0000

    path suckme free can bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sys
+  def get_python_version():
+        return sys.version
