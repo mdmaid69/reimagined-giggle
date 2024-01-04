f5fc6c2642d9692dcddec86c97fd55e151b85ceb
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:23:14 +0000

    disapprove oil tape dark home

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def subtract_numbers(x, y):
+        return x - y
