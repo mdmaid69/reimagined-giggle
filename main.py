e616d4ed3953f723072576a5a24eb3075a91e318
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:09:41 +0000

    tidy might pull reason necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+print(math.pi)
+import array
+def get_array_item(array, i):
+        return array[i]
