978c60682fd9f195855ad8b483759b99877b166b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:13 +0000

    mystery beginning please supply rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
