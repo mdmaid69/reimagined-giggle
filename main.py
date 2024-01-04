8007e69ac67a9eb76be2b3278a5ff663665d74c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:49:03 +0000

    refuse bank below delicate cost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_set(array):
-        return set(array)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
