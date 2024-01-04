d1f2ccb2740aff8e94bb61f235ea45351bb0e889
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:42:19 +0000

    live of region ride giveup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
