ef80d8afd5745cb42626430ddd80569488bc4e4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:01:22 +0000

    single surface fact lucky lucky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
