e22f3ae594aec020070ea33f52e06b7c77b954a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:58:17 +0000

    play Ss warning release anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
