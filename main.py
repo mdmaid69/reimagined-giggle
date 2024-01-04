d0c7dfcc2915bd20605d2024e4c40a651d1f6af9
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:56 +0000

    circumstance tip nurse object true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
