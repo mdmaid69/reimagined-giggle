6d02c475d1a51b3f8f497c8c5050aba128788c3d
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:09:17 +0000

    circle guess week before old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
