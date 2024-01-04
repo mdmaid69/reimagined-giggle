eb4215bbf2ecc9fa8dc5513425969d462e871cc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:00:41 +0000

    thanks scheme type recovery odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
