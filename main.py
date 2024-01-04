45454b0f255bb61c072e36ce89c7ca8f651032d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:02:50 +0000

    giveback awake peaceful trick over

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import platform
+def get_python_version():
+        return platform.python_version()
