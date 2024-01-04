35a228dafab838289cc124e51cd5353bfd23cef0
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:30 +0000

    address passage real goback(to) type

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def square_number(x):
-        return x**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+name = "Python"
+print("Hello,", name)
