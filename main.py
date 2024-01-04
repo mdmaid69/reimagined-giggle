f7ef6c23fafcdcdec76ac08546239412f006b4bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:52 +0000

    amount powerful Ii pause month

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
