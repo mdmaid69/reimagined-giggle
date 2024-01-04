d4ecc08b18e044f24432cf0d240cbd4fa5a99260
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:35:39 +0000

    upwards shadow can ahead someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
