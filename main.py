9558ea1dee5db3b21943669d4e7ea922d8a6e86a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:45:10 +0000

    guess grand body convenient angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-print(random.randint(0, 100))
+import array
+def reverse_array(array):
+        array.reverse()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
