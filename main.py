ec3fa073e4af36e511c5fd1fbfd46d04c5df531c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:22 +0000

    manner cough teach serve keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
