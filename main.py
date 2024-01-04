e71c4777ae74439db4ba024e54cd8295590cbcb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:55:34 +0000

    tend cut fear after time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
