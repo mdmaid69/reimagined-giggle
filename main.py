2b44bf79286dc6b7154369c0f236c0fabd3ce469
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:36 +0000

    refuse lift between powerful shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
