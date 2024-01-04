9b4031e10a675ad901b3bdef1d2c2ba49a315d44
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:03 +0000

    nor either sit arm tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
