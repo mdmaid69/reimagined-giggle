011e97559b902db941dfe1a1d94bdd758ce4b3a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:07:56 +0000

    fill especially object behind stone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
