8fe927276facd88ba109016fa0ac7953f05a1be4
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:49:16 +0000

    wave beautiful apply crowded thankyou

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print(sum(range(10)))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
