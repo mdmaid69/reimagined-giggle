8211cfd115841338af3a62e54adcaacbe7d907e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:15:36 +0000

    disgusting ring go frighten getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
