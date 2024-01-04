90c6c0410050064a90c20308d44d2754142b8f12
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:23:58 +0000

    quiet connect unkind square stretch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
