cad4f765071900e32ba6d892a8e26131713a42e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:21:50 +0000

    married spare now sort breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
