a0825726baf9b1a02793654e8ebcfac60826f68c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:23 +0000

    whether score satisfactory yes connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sys
+print(sys.version)
