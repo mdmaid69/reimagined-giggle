74bd6dddf0ff6b90e2a5479cd55783e1d4a2aa8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:33:22 +0000

    smooth feeling turn punish score

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
