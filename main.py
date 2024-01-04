7974bc76eb7be7a2dcf538e1d8555bd021371a40
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:39:56 +0000

    damage refuse progress increase drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
