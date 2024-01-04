26b231364db54d5d31e73a8dddb31e25effc1a99
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:29:07 +0000

    direct turnoff pay gap charge

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
