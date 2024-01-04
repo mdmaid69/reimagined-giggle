11beb80b249d18421f781d55edf956db717ff9f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:48:03 +0000

    unfortunate distant tread nothing medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def count_elements(lst):
-        return len(lst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
