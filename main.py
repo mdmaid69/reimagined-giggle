ae18fca048d143ca13b4dfe65a923df112ad3d74
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:31:01 +0000

    zero male genuine rarely face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
