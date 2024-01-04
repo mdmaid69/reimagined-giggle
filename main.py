c98c6bef4ad26d131cb132397050fb3a14f90e7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:16:17 +0000

    I remember professional sure ilovedicks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
