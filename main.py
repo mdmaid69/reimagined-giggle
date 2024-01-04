84e042cfc3395c0cfdaafa3ad9bc0070721f955e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:49:49 +0000

    sky shoulder describe subject machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_list_from_array(array):
+        return array.tolist()
