07860050ed73bfb146d8dabf4af192340539ef95
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:12:34 +0000

    male tense new advise rush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
