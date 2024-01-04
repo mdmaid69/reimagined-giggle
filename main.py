f102a64cfe4c6fb869203a68ed5ea38a4d94c144
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:44:04 +0000

    minor exchange whichever finger operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
