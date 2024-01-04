8493915bea5138e164cf58bfc2fc0d30ffba8e38
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:59:25 +0000

    direct reduction fail various value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
