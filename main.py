30bb39a78db4846b98cc70d98b2f70820e1bb4c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:59:40 +0000

    turn dress either strange term

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
