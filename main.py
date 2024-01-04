14f57d163d93bf88eebdef278f26baccafbb090f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:27:25 +0000

    reply practical worry pencil pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_elements(lst):
-        return len(lst)
+def find_min(numbers):
+        return min(numbers)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
