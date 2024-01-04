dc9531bcff92a2c08ffec2a70e3a0168dec528f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:59:46 +0000

    influence also tear respect Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
