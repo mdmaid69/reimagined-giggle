8c0ebd6f8ef3bb1b536c9ce92960caa370dd08d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:51:52 +0000

    watch pronunciation be past quarter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
