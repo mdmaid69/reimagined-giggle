82eefd40005c746449d5287e962945349eef32eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:58:48 +0000

    spare himself that weak collect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
