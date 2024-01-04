a5f43acb0961e12578c4e79765cd3e2ab6e42409
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:56:32 +0000

    label range maximum fix story

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
