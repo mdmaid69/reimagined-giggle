c4800fe3480429253370a5d18e048d6d0e1b0af9
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:59 +0000

    basic visit aim attach inform

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+print("Hello, world!")
