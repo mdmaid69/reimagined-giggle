5c7a68bab9176ec4661fcb79b7bfa17468b0be63
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:17:30 +0000

    duty black check public building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
