093472a2e425b51dc76ff8bed02488c581003eb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:45:13 +0000

    exaggerate office frightened generally book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def change_working_directory(path):
+        os.chdir(path)
