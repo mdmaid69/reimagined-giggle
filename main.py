9813ba7a2d81f0d8b74a81d50bc6415765c49138
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:25 +0000

    know bother improve benefit against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_acceleration(speed, time):
+        return speed / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
