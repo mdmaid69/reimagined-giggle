3735c6b0cc0ea3eacb5d75353852414135c7d023
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:20 +0000

    approve before city change relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_list_from_array(array):
+        return array.tolist()
