2afd8fe7d5682563cdcba96f66384fcaeb6f7d9c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:15:07 +0000

    reach combine furniture star spray

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
