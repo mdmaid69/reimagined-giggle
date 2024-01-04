8d0070e832d13f3231b27fb4f35a6d3eeab9f3fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:32 +0000

    excellent frequent giveout closely value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
