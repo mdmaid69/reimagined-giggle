20cf3287153d02ae95451f8465b3db29369992b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:33:55 +0000

    government see board fat contribute

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_counter():
+        return collections.Counter()
