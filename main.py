32d5cb5046d65b063d2919c7b0f403605230202a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:22:08 +0000

    disagree free flood tour to

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import tensorflow as tf
+print(tf.__version__)
