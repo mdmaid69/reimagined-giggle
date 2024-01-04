60f842ad0ebd99fdf3e5be5616e8054a97f5a439
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:30:34 +0000

    key listenfor OKorokay like phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
