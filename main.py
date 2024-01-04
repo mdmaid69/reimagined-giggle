ea2ff0489c0105ce3423fb59bfcb664881f8f661
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:51:36 +0000

    round sell 568energy interruption under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
