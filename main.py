b909d4a0e382ebf89f6995873087dd4082eb53b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:17:02 +0000

    beside crash reliable tend turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
