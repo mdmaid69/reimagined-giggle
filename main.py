d393a439312ca189049056be5247ea8bee636d5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:58:00 +0000

    lot clue criticism workout among

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
