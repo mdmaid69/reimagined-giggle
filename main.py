6dbf32093768f79ee8280728deeb4deb2a06e56c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:32:18 +0000

    either call shelter duty common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
