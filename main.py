1aa714acae4b595ec29f2825c2626d94748691b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:55:31 +0000

    maybe learn hospital question embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-print(datetime.datetime.now())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
