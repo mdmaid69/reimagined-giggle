abfe807d1d85f576ac612e2a8a41e4ee1dfb3c14
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:26:33 +0000

    too approve breakin unite callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
