d74bc7cfb6f29d2d2e2fe8e1dc52db88018074fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:33:43 +0000

    fire from understand defeat message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
