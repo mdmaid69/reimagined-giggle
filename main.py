5bd3b2b0cf6584f72069c014c442a4c60d33fad2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:40:48 +0000

    snow start medium charge window

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_float(array):
+        return float(array[0])
