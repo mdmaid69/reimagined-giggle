816f1092e42c07ff96dad898a1169744318db1a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:07:09 +0000

    burn closed straight off straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
