29d456a60ce6c4a0f36ece4c988a84e43e7322b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:19:22 +0000

    moment somehow section tired past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
