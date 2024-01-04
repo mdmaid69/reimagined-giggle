5e4af3ae878d3328a46e5dd0966aebdd299e1138
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:01 +0000

    stupid obtain rent mystery shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
