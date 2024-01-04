d393460b1584822efe689fc8bb59eba49eb7d840
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:15:49 +0000

    raise thinkthrough level whole Ff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
