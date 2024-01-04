2e0034f40b4c26c679e6a2e92f2bcc5cb403f29b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:38:38 +0000

    getoff wash meal board report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def sort_list(lst):
+        return sorted(lst)
