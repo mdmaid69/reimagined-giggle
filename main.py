e7ebec398810280d2e6e08800fbde4a91e5278ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:44:44 +0000

    tone runaway distance press burst

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def convert_to_octal(n):
+        return oct(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
