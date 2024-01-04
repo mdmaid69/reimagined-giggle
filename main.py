9e83d8c5f3bc9f19478c83b810df2a323c6676b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:02:13 +0000

    sticky bill my like flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
