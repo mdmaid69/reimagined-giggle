1ec4660c3b8adbef2737cc3c8dc2cfadd0db22b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:01:48 +0000

    even Cc ask turnon confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
