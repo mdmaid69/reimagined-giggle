fd9b9da356a1cff6500ea8b27ecfbf80ab4fdee8
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:43:35 +0000

    group 569enjoy bottom operate complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
