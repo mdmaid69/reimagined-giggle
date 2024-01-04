2fdfa1b574a49222cb36efe27c722429affd2fb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:49:18 +0000

    straight escape respect it earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
