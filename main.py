d985dfaf0fe34c49c46fb2342f164eb8c9da41d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:52:21 +0000

    bound drive complicated adjective of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
