27bf9249584d26fad620a03759cccf5cd5ef61b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:20:06 +0000

    scheme image afterwards lie correction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
