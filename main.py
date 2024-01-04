467706f1b22fbdf0e056f777d6118e20938209f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:53 +0000

    forget although record rest region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def get_time_since_epoch():
-        return time.time()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
