b1e4a3734162476ff621be1227440b1b24ad6451
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:55:40 +0000

    reach takeover owe know noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
