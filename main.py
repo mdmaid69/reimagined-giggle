a43071c2392038c1a321de75baee75662a846a37
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:24:43 +0000

    opinion type calm house yesterday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-print(sum(range(10)))
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
