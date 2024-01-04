338da9039048eeb9e8ddbabff2ddfa9ab58517bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:23:27 +0000

    about hit accommodation copy general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def reverse_array(array):
+        array.reverse()
+def find_max(numbers):
+        return max(numbers)
