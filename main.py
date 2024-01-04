fecb8c9685adb0d9dea5e1f3cc83934413e2d5dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:29:16 +0000

    bye give temper image creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
