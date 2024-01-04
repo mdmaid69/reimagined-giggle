3966dce71100d9684e72e4df376f18dd1c1eedb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:12:15 +0000

    amount nerve woman lookafter appearance

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
 import re
-def split_string(pattern, string):
+def split_by_pattern(pattern, string):
         return re.split(pattern, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
