4f10527ff324faf7891198cdb43268a1e0d03cf1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:11:09 +0000

    fair memory process command software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
