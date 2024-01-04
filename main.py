a65d7acc733d9b692f52973d74b875574885fda6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:08:40 +0000

    steam off upset fast none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
