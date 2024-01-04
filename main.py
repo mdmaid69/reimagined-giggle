609bf65e742069fc5757b1874485c9eae6955b85
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:12:11 +0000

    wealth view turndown sick proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import random
-print(random.randint(0, 100))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
