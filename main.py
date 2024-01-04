ff2d45ca91d9d467763651455e4e41260d46283c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:26:24 +0000

    motion favourite fight common through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_density(mass, volume):
+        return mass / volume
