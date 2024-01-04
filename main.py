7157cd8d5db162ae4e7a5bde47b34a868fea1522
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:37:45 +0000

    off grow convenient rent water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
