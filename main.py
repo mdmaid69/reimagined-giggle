5754a61339a51285ddf75556f9abf88b98e1d42c
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:13:59 +0000

    skill party separate address steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
