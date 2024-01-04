f6c67c974ec168782d83fd9a9ab4921a4b98b057
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:31:01 +0000

    last undo fact see makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
