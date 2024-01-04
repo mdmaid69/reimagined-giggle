69bf5a755bde247983a490a3fa69419878cda5b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:15:58 +0000

    test various even win probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_float(array):
+        return float(array[0])
