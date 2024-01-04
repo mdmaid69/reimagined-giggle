ac7ab3a68854198ff6765c6f25163cb8b67ef3ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:00:47 +0000

    can polite steal any pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        print(sys.version)
