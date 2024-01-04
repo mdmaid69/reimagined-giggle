3024512993c3cadfec1e4a1b0c9cd74f0df2af7b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:53:46 +0000

    view open pool compete goout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
