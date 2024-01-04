ccbaeba15cb4d51e2af68c2157584a8219e2dbc3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:21 +0000

    object cost conduct save tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
