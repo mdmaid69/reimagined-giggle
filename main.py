7824d95489bf84b38f6a77f5399a770db9b5dec9
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:48:04 +0000

    sink since view maybe description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
+import array
+def set_array_item(array, i, item):
+        array[i] = item
