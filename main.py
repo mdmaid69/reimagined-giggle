34160282ed7e49c2e79026b2c390ec0d7f935b55
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:03:34 +0000

    curious wave defeat second stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
