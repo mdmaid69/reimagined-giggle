33e0074a58e89ac9656151b9b732cadd77e9194f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:39:24 +0000

    promise blind computer accidental principle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_str(array):
+        return str(array)
