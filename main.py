3018e62782ec42d1b9f9a2fe979142d7866cf947
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:44:36 +0000

    type free sure major around

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
