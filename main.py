a98936b2c5a0b7eafc325faf2eb136d9134da02b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:18:11 +0000

    bar pity concerning objection lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
