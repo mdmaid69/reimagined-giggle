875d3dd8d0593ef3a8bea318b9ea106e5135f6e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:33 +0000

    appearance process search order bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
