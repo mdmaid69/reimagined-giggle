6e6179f809339cbd9f01bf60965244326a929fec
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:12:29 +0000

    cap whoever wealth experience Qq

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_set(array):
+        return set(array)
