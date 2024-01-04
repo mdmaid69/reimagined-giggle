2b75f39bdcf4c8f4f8648c6a3ad6f5471fb4492e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:37:35 +0000

    muscle flavour aim beat again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
