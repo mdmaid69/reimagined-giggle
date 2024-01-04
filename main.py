70a835f5a324e0d1c6266b8f94b8eebc95b4f272
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:29:37 +0000

    soak generous too some write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
