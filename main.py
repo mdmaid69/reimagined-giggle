7b4368c6b1ec47f5ae61786318faa8e68d53450b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:25 +0000

    except happen influence pure age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
