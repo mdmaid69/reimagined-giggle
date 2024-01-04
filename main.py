e0b7c325f6a28e0e94cc6f87249a7e9d98eb652f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:07:37 +0000

    what talkback slow call star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
