641aabf41ed9c0f080e84102a9462372b52d3d47
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:04 +0000

    pretend strange sleep from cross

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+print([x**2 for x in range(10)])
+name = "Python"
+print("Hello,", name)
