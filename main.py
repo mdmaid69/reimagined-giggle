165793224fc1c99f6acc5404b183d8d3ff7b7488
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:04:01 +0000

    this not feed better memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-print(math.pi)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
