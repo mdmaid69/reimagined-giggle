75e88311cbdace09c1fdedddefce7a5cca0bc2b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:06:01 +0000

    government stop rare through whole

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
