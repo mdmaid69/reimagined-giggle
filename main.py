1a7f517a2f73a678ca239968a33fabb2db2eba8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:08 +0000

    what boil nonsense actually table

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
