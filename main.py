3afbc79668e00982b7b844d4eb957e43a2f9d193
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:30 +0000

    point ordinary close show emotion

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
