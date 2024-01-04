ef4e6e1d34f6746de49b1487049c466d72713fab
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:38:41 +0000

    thorough sail frightened available area

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
