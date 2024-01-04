248cf8fc177d1d8e1df3c2e4caebffbeb8f01745
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:30 +0000

    pure head mess do used

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
