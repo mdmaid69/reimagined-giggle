fa6b8b3a8731236221304ffd4cc4caad3321b64b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:09:48 +0000

    law legal marry Hh talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
