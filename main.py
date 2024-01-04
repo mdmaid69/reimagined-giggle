aae8b67545c0ec2713dc2d910a79bf8ec8ee8da1
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:06:01 +0000

    floor left nearly gather swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
