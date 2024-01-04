855d2eff6d677a946160432b802886398662c062
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:12:09 +0000

    collection grade our union connection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
