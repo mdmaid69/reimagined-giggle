e86478aa7c81b1992c9ee6d4c60868c80a8cfb0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:27:51 +0000

    forever pretty pin definite balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
