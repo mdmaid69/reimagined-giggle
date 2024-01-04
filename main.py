8216b03616dd991eef7fe1713f538806085c5429
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:13:28 +0000

    once shut say relief still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
