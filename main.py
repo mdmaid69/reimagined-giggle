a1cdaef3f52d5698e93cb79f07d1acc64a786d89
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:13:38 +0000

    bread cry favourite contribute bag

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def get_array_as_repr(array):
+        return repr(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
