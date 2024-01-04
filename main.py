941b168a3b2452f44f944dfc8e4c182d2a683841
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:30:05 +0000

    smart mistake original no professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
