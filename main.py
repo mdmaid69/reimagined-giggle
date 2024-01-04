8ec933a99a51dd3cbbcef9e2999149ccc8859490
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:28:41 +0000

    here stamp lock doubt clue

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
