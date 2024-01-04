748e5c53a0a9452ec7f6ccddc7931e6f9c097cde
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:19:03 +0000

    work name disappointed stone try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
