dc495fde85aad1a10160c297b1d4b462154be903
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:48:09 +0000

    achievement to perfect harm only

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
