1832420587bfb3c1fa352c6b4e4a6941efd85041
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:32:20 +0000

    cry determined like jump mind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_min(numbers):
+        return min(numbers)
