1f9680bdb57ed980b7dd4b047aa501f17501c498
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:30:05 +0000

    spring throughout thin comfort declaration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
