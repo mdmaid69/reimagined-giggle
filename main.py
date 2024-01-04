bf83d649caa4e6b7b4758e63e47345abf8079e41
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:01 +0000

    interrupt hard hole trick conversation

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
