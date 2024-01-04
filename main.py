9a962a02d6be5aacf1bffdabff5ee2a0f3c1bd32
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:53 +0000

    nice heat principle bother winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
