656ebc48bdc919400fb1c84b397eb120d0489699
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:25 +0000

    father tongue bang worth yet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+name = "Python"
+print("Hello,", name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
