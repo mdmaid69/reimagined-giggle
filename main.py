ab301bc379cdcb575c9d98b484022a3dda4f661a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:57 +0000

    order mind shake might comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
