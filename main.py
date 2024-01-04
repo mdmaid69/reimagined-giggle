dad2c07bda129eba1fe28e2a6fba2336cfa9a6be
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:45:58 +0000

    through breakin anybody right birth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
