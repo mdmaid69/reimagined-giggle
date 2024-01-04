bd55ccf4df95a8c01248b824943ee00b0df1a8ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:45 +0000

    soil speed trial measure sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
