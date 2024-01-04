2ff3fb7147e192734b62925bfa4eb4c0b3b25ed7
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:30:39 +0000

    usually look land consider scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
