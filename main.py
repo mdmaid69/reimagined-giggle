e198a5db09239eccdb0e5f1d525871c39ce36a2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:53:56 +0000

    themselves community idea aim fence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def multiply_numbers(x, y):
+        return x * y
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
