052f98f5554173525abae8191ecbaed9c194df25
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:51 +0000

    active recognize successful neither nervous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
