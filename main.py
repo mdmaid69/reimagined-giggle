b2ae14fd9ddc192c61287523facaceae957d9e8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:49:05 +0000

    male intend sport size trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
