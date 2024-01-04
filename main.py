f9a43c603a5b019c0516f2988ee5a080504dcf0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:14:48 +0000

    place language left well speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
