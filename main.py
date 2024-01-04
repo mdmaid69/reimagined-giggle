0ef109a282cf42bc35c3190f688ffb66609bcc4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:14:06 +0000

    community cut wherever show earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
