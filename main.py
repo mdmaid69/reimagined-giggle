ae2e0fce2df9c68d2733591fc4b24aa1a9441f07
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:05:41 +0000

    bottom maintain hang few clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
