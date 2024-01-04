59cf96f086c00392271f3e29833040951976bf12
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:07:55 +0000

    reason own tip worth transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
