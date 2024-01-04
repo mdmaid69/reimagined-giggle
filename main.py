4da69aa8de62ca7fc2b273c8461dae1b111f743d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:38:48 +0000

    thumb mention process approval substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
