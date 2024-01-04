fd8ab0ef5f99e4640cd303c13de95eea658dd8b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:36:21 +0000

    block damage loud noisy form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import sys
+  def get_python_version():
+        return sys.version
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
