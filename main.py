9254a3fb22873369d95309f1d1ef3b8f0e62af23
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:08 +0000

    reduce past style big exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
