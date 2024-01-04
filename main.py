96cca746cf30305f6dc0b842d6d0b53fae970ecd
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:08 +0000

    doubt satisfied always church whoever

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
