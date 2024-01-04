5f0417e403ee768334a086aa13168075ca9adad9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:27:13 +0000

    condition lost church scream best

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-print(os.getcwd())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
