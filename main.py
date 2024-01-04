b857f726f280ca8bad5389746792045653fbf1c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:34:47 +0000

    record exactly actually programme turninto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def count_elements(lst):
+        return len(lst)
