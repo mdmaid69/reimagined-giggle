6d8840f02415c1dacec9c4a9ba16bcc4f6506589
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:38:48 +0000

    discovery suit sting lookup copy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
