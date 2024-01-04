fadb3a2b618ccb072199691e4d69d91b76abf07f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:13:56 +0000

    busy geton Bb edge to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
