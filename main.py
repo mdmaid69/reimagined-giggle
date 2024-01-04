67fbdc8053d7789cc0707faf4036e46a227d3670
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:13:03 +0000

    collect basically start cook than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def square_number(x):
+        return x**2
