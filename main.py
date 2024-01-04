b87d746c857718336c334c153a630cc8abde1d3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:59:00 +0000

    the pump rush sing however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_typecode(array):
+        return array.typecode
