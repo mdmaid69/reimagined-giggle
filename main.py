7ac170efb85b9eedc38cbcf94dde5b8091b9686c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:40:13 +0000

    easy finally skirt certain tip

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
