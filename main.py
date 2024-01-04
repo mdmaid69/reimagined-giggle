eeb298bd9a72a5320123aff98525477b5ae51d2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:08 +0000

    aware mouth raise spite simple

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import random
-print(random.randint(0, 100))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_cosine(x):
+        return math.cos(x)
