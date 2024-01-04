11a1b51e1a7f9701802524d16a482308684419a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:39:46 +0000

    escape wait responsibility left confidence

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
