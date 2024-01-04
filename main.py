403f5b9e016d7b189a35fdeb6d2d38bbe9188eba
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:02 +0000

    pleasure quarter introduce apparently key

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
