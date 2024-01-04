36f260589027b604673f554cc881fba0fef84df3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:06:12 +0000

    pair comein 564encourage doctor harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
