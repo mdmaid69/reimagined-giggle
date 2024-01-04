0a3f5416a9bfe43ff8f3c56480e3c0610306529d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:58:18 +0000

    long aim exchange work failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
