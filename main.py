7573db4242cbb1017c64fe93650b30019d1d886b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:59:49 +0000

    easy pause train far Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def get_array_as_repr(array):
+        return repr(array)
