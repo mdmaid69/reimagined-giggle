2f04067c3bd97e278846d37ae24460d3792c8753
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:33:02 +0000

    wise whichever improve dance against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
