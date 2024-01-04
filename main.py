dede88d0f08599954c91acaea65e5060b577c5a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:31 +0000

    doubt hurt address whistle country

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
