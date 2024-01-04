cdc13266776d5fbca5a7557583e3d014e1864155
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:19:27 +0000

    habit lookaround tidy trick visit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
