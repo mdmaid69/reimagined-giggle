ac0389f734208ff53584bbc3d294a97ccd21eb55
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:35:36 +0000

    fire achieve sentence shrink learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
