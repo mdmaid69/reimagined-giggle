d0f6123828037fac45715f8d77715feca902fd4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:59 +0000

    chat beat feel rough near

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
