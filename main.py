e80947989a1b982c6cd0e080a7a0ceb49725d8df
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:16:46 +0000

    keepaway separate finally wake sentence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
