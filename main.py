63b330ca347da3e2ba0c7743822e95b0006f0b42
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:07:52 +0000

    pair hurry period experiment child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
