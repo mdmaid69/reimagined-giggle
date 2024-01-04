dfb6ab4d586f5f6e2b23b10d34d64f203f418a97
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:03:00 +0000

    play write lesson Hh development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
