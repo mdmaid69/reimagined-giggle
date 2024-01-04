f34fab01f4fa1bb5c9180c5932b1e6bc2d6c34ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:43:39 +0000

    cut traffic offence lower aim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
