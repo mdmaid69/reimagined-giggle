ca9739f5c7119951964fcd1e22da2abfcba3e1a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:57:42 +0000

    realize repeat off help slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sys
+def print_python_version():
+        print(sys.version)
+import datetime
+def get_today_date():
+        return datetime.date.today()
