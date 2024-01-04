94c592fec5a1f3059fa735b4ac395be65b2b43a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:43:13 +0000

    fight fact bath calloff creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
