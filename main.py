e6dbc45425c26f506fb71a40195011b64cd34764
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:13:21 +0000

    head for profit praise little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
