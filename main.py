e68ef714e58a04dac8eaa504987276c80312c2f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:41:02 +0000

    jump spite moral stand taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def get_array_as_complex(array):
+        return complex(array[0])
