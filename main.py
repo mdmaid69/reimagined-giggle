450c7c2d9a0a5c516fab557ecc860bd2c9cef45b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:02:52 +0000

    careful risk tend to slip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
