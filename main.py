2aca5f2dba17f22807a12f7a122f6849e6fe260a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:00:54 +0000

    past dry roll rate tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
