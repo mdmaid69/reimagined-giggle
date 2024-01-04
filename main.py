a1c1884c20ccdd846f72cf98e2f227e2bd732012
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:42:44 +0000

    whistle education offend easy weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
