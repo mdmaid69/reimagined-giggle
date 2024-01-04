a36fd254674a3996e3fc47864bdea6499a23ad8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:41:18 +0000

    praise by disappear independent visit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
