9cdcc6b5bada95403020799fcb33be7f01a22dab
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:49 +0000

    bringdown but confuse she order

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_list(array):
+        return list(array)
