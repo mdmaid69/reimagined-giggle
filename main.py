b6cda6a18ca86ba46a72ee5494e572bb54bd798f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:17 +0000

    tomorrow own my drawing better

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def cube_number(x):
-        return x**3
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
