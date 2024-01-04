970aca023be86d040f21b5d5cf75869dc45bf333
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:43 +0000

    Mm weak defend strange number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
