5596da9c79b9e77c7a78a67f778f7b4eac05cfb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:18:22 +0000

    mysterious meet emotion priority likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
