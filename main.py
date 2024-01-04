6cc8112fb81e84f6fcd8bb05866bb42fb0b39024
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:41 +0000

    advice breakin silent afternoon high

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
