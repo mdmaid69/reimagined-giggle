36397eccc4f0bb8e994f74eb465978eb885763e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:06:06 +0000

    evidence with that bet suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def multiply_numbers(x, y):
-        return x * y
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
