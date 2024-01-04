e8a393fa25592096908803f70cb0afca755711c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:47:03 +0000

    however touch by earth professional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
