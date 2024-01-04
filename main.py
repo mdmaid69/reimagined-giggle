956a3db308725e1b1c51220b1f6231f769f8ceef
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:05:11 +0000

    invitation explain shine side many

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
