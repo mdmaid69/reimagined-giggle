7e8b8fcf66dad8837ac8f5a752752cd1ae823939
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:25 +0000

    nice study may practical smooth

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
