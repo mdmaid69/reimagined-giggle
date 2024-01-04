ac60e1a116d0f1456549b086cc9595530cd4988e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:43:31 +0000

    bet space lookafter left over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def convert_to_hex(n):
-        return hex(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
