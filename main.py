df7032c7e4f262c6ba0768fcfd6125e1f019e700
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:12 +0000

    tidy measure confuse waste exciting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
