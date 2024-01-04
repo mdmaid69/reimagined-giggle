a411adcb460c155ea0a53799efb59c11e549fded
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:30 +0000

    through trial yours tense cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
