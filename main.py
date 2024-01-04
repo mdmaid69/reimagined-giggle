75324968607285ffd1da2cd688523ff4c809abce
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:26:50 +0000

    upset plastic pleasure professional mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
