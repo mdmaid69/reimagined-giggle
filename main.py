7a62819c15c1e759478273b1db0ee4b2b200da05
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:54 +0000

    wheel welcome top idea finger

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
