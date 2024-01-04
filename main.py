7cfc102618e6983855c9af17bf6da88b9bea836c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:45:06 +0000

    bother bottom rush stranger before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
