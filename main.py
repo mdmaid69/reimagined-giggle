3a12c609f1a146a54dc782b3a93ea9d0ea0bf64e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:24 +0000

    time Dd side deliver important

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def convert_to_binary(n):
+        return bin(n)
