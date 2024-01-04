ecdfef05871f63d90ebf511f28ac18c2927867fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:49:40 +0000

    anything drop just father jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
