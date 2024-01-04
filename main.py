a7b0b45341c5ad9da1d87fe29a0be1e3a81abd4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:26:18 +0000

    573entertainment continue money circle contrast

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def convert_to_binary(n):
-        return bin(n)
+import getpass
+def get_username():
+        return getpass.getuser()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
