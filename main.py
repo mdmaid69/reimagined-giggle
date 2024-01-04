caac1306eed32cdfb621d82be2c0ea9118d1dc04
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:29:25 +0000

    wise protest memory disappear work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def split_path(path):
+        return os.path.split(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
