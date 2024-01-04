2427e2ab441ab11cd9731515af758f1f671a9b55
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:12:30 +0000

    book badly sound disturb split

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
