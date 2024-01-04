88b0ae81525f5d5d95b13130acb9473e69e41a6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:58:31 +0000

    will shrink confusing write listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
