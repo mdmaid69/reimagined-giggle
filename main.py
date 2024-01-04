eca3bf2ff6f034263408f32ffde821cee3c6910b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:35:20 +0000

    happy ahead twist snow entry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
