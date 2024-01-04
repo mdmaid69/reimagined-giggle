fa50cc0ccf5fa953f31d99a1a6f40db61102852c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:28:07 +0000

    getaway love branch form demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
