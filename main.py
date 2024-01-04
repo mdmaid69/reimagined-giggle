960145ef9136e4ff228e0867d13bac21fc278929
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:38:08 +0000

    except march before heart impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
