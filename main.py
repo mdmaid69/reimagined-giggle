3a10020ecdc8da485ebe53445b2d1a9af8fb4287
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:44:23 +0000

    themselves Tt pause neither confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
