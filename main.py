c9b95537639393415c5aa601339e06639bb0aee4
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:58:40 +0000

    score fortunate punishment story payment

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
