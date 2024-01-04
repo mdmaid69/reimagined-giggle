f843d68ee6deed861ee8fa82e24d48e14bb75a58
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:34 +0000

    lost row mend holiday for

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
