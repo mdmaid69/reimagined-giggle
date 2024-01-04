cf0bfa9215d333535afeadc52fdcced57cdc01e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:06:08 +0000

    excellent no frequent afraid every

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
