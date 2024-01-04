283c4266ca8f8ad31d2fc4eb9b364baecccd9df5
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:14:45 +0000

    nor operation priority complicated workon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import sys
-def exit_program():
-        sys.exit()
+import tensorflow as tf
+print(tf.__version__)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
