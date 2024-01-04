d122965f23c217ddda7e9156f2450b1cbf8e0955
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:59 +0000

    furniture his post image however

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
