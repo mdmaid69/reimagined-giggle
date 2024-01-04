54af7b723a320e262a2caade89e59865598d91ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:09:50 +0000

    firm press suggest side touch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
