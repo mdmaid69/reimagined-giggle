0460da140c12f4093b4dd83ec71e2af6ed197555
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:28 +0000

    Ff thank guess but rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
