881377adf92be493c3556abdf05987a1c5bdc446
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:09:58 +0000

    children address congratulations solve runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
