9f644ce72557b9f33d8f3bd9848d5bea7934dbce
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:13:16 +0000

    represent themselves afternoon defend deliberately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
