725093cbb77637448e2d77c34d06131f9b231e87
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:11:35 +0000

    case fit mother look responsibility

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
