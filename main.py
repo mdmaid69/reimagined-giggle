85e5332b0a64d4da7328d3c8b421719e16f570ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:30 +0000

    recovery bury brother in road

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
