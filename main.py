41a088a4cc723fefa98f67df259bd9d443e481a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:07:42 +0000

    organization even finally accordingto ready

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+print([x**2 for x in range(10)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
