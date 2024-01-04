8c09ad89574c392f8b429450a41f0934b6085613
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:33 +0000

    crowd poison second point past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
