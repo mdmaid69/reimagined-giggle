3e620ba1f80156fe9c7289df32812fe1c041ba19
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:28:50 +0000

    attach dare anywhere variety nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
