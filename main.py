ad7432e7530614345bbf04533d1a02e2763a2273
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:04 +0000

    waste obtain exploration copy mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
