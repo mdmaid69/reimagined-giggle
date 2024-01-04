5bd4498937982caec63f7d669629cb5453c8f7b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:08:08 +0000

    hate row trust live terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_floor(x):
+        return math.floor(x)
+  def is_odd(n):
+        return n % 2 != 0
