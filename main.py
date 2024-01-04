a7f10a792943d8527923e66e5a0c9f4f701ddf41
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:51:14 +0000

    loud easy handle mine left

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
