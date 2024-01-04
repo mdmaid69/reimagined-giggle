7965e53d743130d71dafa6dbe7d4b5246beb230e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:23 +0000

    frighten consider interrupt theirs great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
