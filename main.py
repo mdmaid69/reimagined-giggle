63e329c4071a5741379f66d7fe81ae8f003b86ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:01:33 +0000

    definite receive almost polite frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
