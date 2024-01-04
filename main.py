157ecb5b6f574d50d8a8531067caac0d4dd5dc68
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:06:27 +0000

    trap surprise month cheek pot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_priority_queue():
+        return collections.deque()
