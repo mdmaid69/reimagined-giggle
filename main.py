8807d59f7676d61b099fcbc0b6ba1da93ed5e067
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:18:37 +0000

    laugh away twist away once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
