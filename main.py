e8e269a55f12d03351a4c714c6f2d2c4584611e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:16:48 +0000

    bring curious office strict unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def subtract_numbers(x, y):
+        return x - y
