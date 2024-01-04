2db0e607502e5617a6266f3fd909855c36e750c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:54:41 +0000

    still fire brief check turnup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
