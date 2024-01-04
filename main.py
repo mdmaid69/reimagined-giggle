a227880958889c9eb7445b9ed9faa50ee12a9a38
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:27:08 +0000

    pronunciation above through bottom drop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
