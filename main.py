6020250b3ae834f9848a5b262a5e0f0ce36b2e99
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:19:28 +0000

    factory scene negative main drive

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
