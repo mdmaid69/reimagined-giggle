a418c80561c3d0bc1fd0e588ee94aa4fdcba8d87
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:13:00 +0000

    complete order sensible anywhere bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
