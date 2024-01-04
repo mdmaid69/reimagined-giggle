af584ee72ca1fb4d8309a27cd37dfaec0671c766
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:47:06 +0000

    shade tired law push beside

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
