9b6ee0afa87d3969fd488800197e0efd45fa60f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:44 +0000

    definite considering group storm serve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
