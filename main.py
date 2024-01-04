e30f193b11bed7d687487476e4aa366412269955
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:15:03 +0000

    confused wrong way regular reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
