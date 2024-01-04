dd6cc42ddc720a84a3cc45884825a6b6a60ae771
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:51 +0000

    achievement workupto name balance bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
