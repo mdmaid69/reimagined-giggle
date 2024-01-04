abdbd1dde218313616aae8b8af93248c1db1e5e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:02:03 +0000

    proper we high exercise mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import random
+print(random.randint(0, 100))
+import array
+def get_array_as_float(array):
+        return float(array[0])
