ce6932407b1b36d951fa210f6229c540fb009f50
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:37 +0000

    still mysterious sharp fail public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
