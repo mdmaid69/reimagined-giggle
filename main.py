04b5407e591b815fe1c3b8f3b60afaa24379a8e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:10:20 +0000

    you turnover suggestion wash push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
