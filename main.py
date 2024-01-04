8b014d3b0eaebae3b96bd073d37d248be2f848b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:35:36 +0000

    generous connected rob connected union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
