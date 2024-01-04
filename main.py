6c18d5791e8c309818ce539650be752e1112019b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:51:49 +0000

    flat present uniform burn fairly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def set_array_item(array, i, item):
+        array[i] = item
