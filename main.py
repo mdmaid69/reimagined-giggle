ae27934a7c9b877d50fb80fecdc84d3f0e5d4c09
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:53:20 +0000

    can major pinch section us

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
