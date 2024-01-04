b3b63b04dd6df97be88254a52314c29adc3f951d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:42 +0000

    tired tooth lesson upset attempt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
