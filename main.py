7cc2b6f6fad2f78c16e02de4cc8347657ede3748
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:30:53 +0000

    corner operation complicated sleep deal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def convert_to_binary(n):
+        return bin(n)
+import os
+print(os.getcwd())
