1597aa7b9e7ff62540f6b462af49fe85cdc4dcfb
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:22:31 +0000

    dig size fair inquire supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
