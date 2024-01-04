22f0f0f9ec0e874d1cfe9ae9b2d3655373eca294
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:35 +0000

    light convincing knowledge show recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
