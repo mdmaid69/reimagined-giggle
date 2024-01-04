8b1256552c22c780495ff1188b209f51a96e1741
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:56:20 +0000

    separate television intend negative run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-print(math.pi)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
