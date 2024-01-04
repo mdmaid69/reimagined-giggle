bdc250f02c5aad65d598b27d36ccd8a5feb16a66
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:17:12 +0000

    door snow pronounce impressed quickly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
