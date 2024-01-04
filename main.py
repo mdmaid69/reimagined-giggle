963b6b77d67d18450100935eca86958c524cc072
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:23:28 +0000

    evening damp name light place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def remove_duplicates(lst):
+        return list(set(lst))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
