951e18e9e12ed14e8be1d0513befd02aa855933c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:07:56 +0000

    key replace convenient shut talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
