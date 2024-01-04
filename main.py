ef3c26e4ad06bbc3111dd1435032ed43987cf731
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:38 +0000

    wise cover split develop because

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import datetime
-print(datetime.datetime.now())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
