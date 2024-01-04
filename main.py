3ad553444c462c4854a444de6785c65401269c7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:55:13 +0000

    little flavour instant opinion collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_counter():
+        return collections.Counter()
