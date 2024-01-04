160416e909c68cb90799f7faa8f5ca8e312ba986
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:17:21 +0000

    promise unfortunate now also Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import datetime
+def get_current_date():
+        return datetime.date.today()
