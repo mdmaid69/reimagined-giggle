6b0ee7969a8699181087de63b5502cf20bb7110b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:48:53 +0000

    commit dead worry either move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def get_time_since_epoch():
+        return time.time()
