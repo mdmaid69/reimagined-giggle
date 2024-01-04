faf5a390cb920b5f77e7de7c580e78718b23f84f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:30:40 +0000

    survive along content care distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
