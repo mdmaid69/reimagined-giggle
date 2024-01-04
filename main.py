b16c654d9a28e9c55420b75fec65cab13eabe576
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:01:58 +0000

    pronounce lucky have rather join

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-print(sys.version)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
