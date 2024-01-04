20268d375e7c407339ad410578fa86f7901d38d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:15 +0000

    such complicated work section behave

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
