b6208b535ea6a6165613ab982fe8acab2da54bee
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:06:08 +0000

    right exchange friendly crowd few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
