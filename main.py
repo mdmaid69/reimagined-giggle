b0703359fd3275402c6eb5e0a1edf2290b79d0d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:34:24 +0000

    feature later keep sweet when

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
