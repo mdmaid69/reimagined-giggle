75a2875a66f4b49595c1a7030d1b68423b252c11
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:05 +0000

    morning bear progress roof circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
