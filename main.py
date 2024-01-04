c4fd445c705a1d2076432f2ebdb37cf617c46096
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:54:38 +0000

    still cheek another shut previous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import time
+def get_current_time():
+        return time.time()
