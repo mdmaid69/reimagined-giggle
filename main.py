7254f3ad24f2cfa336b27d6dac2d53691081ec7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:07:34 +0000

    warn she wall godown last

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
