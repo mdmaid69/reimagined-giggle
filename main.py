67846a616cf194cb832a7fa332f4449ac1517de1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:24:31 +0000

    mistake shine team lift develop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
