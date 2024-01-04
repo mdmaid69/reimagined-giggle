16bb7286475c29e8feaf91ccb7d5bde4e0b84a66
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:35:15 +0000

    wake blame pleased direction quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
