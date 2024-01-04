8d82dd5f201a3fcae30082d2c835e9697a71aebc
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:15 +0000

    slow recommend phone start afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
