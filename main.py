a8da4d4a13c49d4aa751384d0489abbbf34726a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:55:29 +0000

    increase goout development rare divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
