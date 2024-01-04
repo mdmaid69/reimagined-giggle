e40115e1b586c62fb00136e47f2ae6308e4d091d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:38:11 +0000

    sensible delight regret play may

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+for i in range(5):
+        print(i)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
