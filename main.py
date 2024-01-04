af96635d66f860cce9068399c431ede7ea4e3ac4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:34 +0000

    herself represent theirs suit march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
