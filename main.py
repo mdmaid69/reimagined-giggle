1f923227c5bec0907a02cbb7dac2d75a7fca9ab0
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:45:18 +0000

    objection react after Nn tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
