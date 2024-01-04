0610c6492d03d7f9eeb842e961df328da42c91c9
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:45:29 +0000

    shoulder include behind full mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
