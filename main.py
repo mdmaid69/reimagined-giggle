98fdb572cbe0418eb78ea0dd6e4ee82287709285
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:35:58 +0000

    there hang disturb outside ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def is_odd(n):
+        return n % 2 != 0
