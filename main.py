ad98e0588ffc803fbf8da8851d696cfdf34dce5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:56:29 +0000

    571enough unpleasant gather interested temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
