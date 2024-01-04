229f1fb8f02129bf41083e0cd4e8c622ad3059de
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:56:41 +0000

    imagination runover workon sink fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
