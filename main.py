372bf7bd61b71899e7cc8e653719815e05cebed3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:28 +0000

    key away spill soak injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
