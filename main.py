dc554884d140c0dc0460d9112463981c7769a03d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:18:39 +0000

    tell cruel ring feeling if

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
