77956a054c98ecbe0c9592ccf4e50e08f471ffb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:40:39 +0000

    who fork experience trousers treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
