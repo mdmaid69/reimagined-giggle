91f7ebb70b7caa47ac0a8efaad77f03bba4c223b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:50:35 +0000

    count stone such range Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
