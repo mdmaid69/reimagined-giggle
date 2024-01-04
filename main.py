210bb0c81d26f2f874f3c1e2e65248b721b2e937
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:42:30 +0000

    reduce beautiful consider reason send

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
