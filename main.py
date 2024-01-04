2cb832812df4c68791f4a256157be4f3b90d99b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:03:59 +0000

    show lesson ring boil majority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
