786b4d8e5af298d8f3832a0ff9f695fbeeb83c37
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:21:58 +0000

    introduction remove rush deaf decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
