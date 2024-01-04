a729027a4cb57e75cdc365d8623ee2d51f7daa0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:39:33 +0000

    pocket type breakup excuse goon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
