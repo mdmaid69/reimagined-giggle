58de3fd0ac83ef435166bd2d82684d4275f5e7b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:44 +0000

    active face pressure give pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
