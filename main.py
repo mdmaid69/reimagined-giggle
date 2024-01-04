6acb5385f498fc0687e96831e173a2738ab91ee8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:35:29 +0000

    accommodation goup summer grow rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_item(array, i):
+        return array[i]
+def calculate_density(mass, volume):
+        return mass / volume
