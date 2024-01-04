45a503c670eab98098d9b487fef7a3f17366cfd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:32:29 +0000

    suggest sick everything either bye

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
