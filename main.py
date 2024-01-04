be41986d3fbb381e6acaba2c0b752b604dca879d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:08:13 +0000

    act poison fine modern dear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
