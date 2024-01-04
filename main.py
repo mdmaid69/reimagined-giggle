596201f29b4094f6fab131303e2ea305f346ed56
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:21:09 +0000

    whole divide food colour outside

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
