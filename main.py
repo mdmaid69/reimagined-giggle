b7e3b7b45b32575dc1af759264b3a5586c96c001
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:59:47 +0000

    blind bye copy believe moment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
