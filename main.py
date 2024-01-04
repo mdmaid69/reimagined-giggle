94c5176a90ed81f19e71082ba2501791df980842
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:07:08 +0000

    substance week guess possibly least

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
