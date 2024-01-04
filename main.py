988a4d41638c4b1a277d11fc20ac637f9e6c9fcb
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:49:53 +0000

    finish blue takeaway quickly style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
