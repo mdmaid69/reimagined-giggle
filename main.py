ebe612ea4d1c335b1482b35ede5aaebd9c9d1349
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:42:06 +0000

    strong journey smoke beginning meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
