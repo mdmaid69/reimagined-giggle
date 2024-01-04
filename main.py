c3d6f6b51d79207fd5c17f1d7ea59992b9bd37b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:45:51 +0000

    judge male block hurry between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
