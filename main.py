09581332cbdd6a74ea39cb6256e0f2700e5f0f31
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:14:52 +0000

    middle tread stop own necessary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-for i in range(10): print(i)
+def count_words(sentence):
+        return len(sentence.split())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
