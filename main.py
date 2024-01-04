c7f3a5b906dc5647e29d6e93f11b5179a3e9f389
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:23:40 +0000

    bound explain effect meaning roll

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
