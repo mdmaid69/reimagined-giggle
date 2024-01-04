2d2abb4171be22d5af43ace50bf8f587eb592d29
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:06 +0000

    goaway let cope flight able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
