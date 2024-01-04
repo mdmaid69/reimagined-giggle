e4385a5c0d556b4c70b06e32e4c59c6f0a8fb62a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:06:03 +0000

    holdup cut best Jj deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def add_numbers(x, y):
+        return x + y
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_tuple(array):
+        return tuple(array)
