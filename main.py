c657ff27c8148b67e58b40bbf31668c9c2d247d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:24:54 +0000

    towards open priority fact bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
