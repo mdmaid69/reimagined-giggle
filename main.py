af3233120d602984d92cc85c3b4b0299a19bf301
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:56 +0000

    have active measure want butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
