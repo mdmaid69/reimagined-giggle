e0c7ae49bd00ce67d49609e728e041984a7d8ae5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:01 +0000

    feeling control advice inform fire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.time()
+import platform
+def get_os_info():
+        return platform.uname()
