8dc6634486bcbf0bd7527359dc2605257f3d8c94
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:02 +0000

    statement knowledge spot people whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
