17d63aa5d990e51fb1aa80d810a0b7c5b9b60d96
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:51:56 +0000

    goout down neat heavy buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
