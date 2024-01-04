fa87a465e1e8cc5114aaa66011a91d02ff8f791a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:13 +0000

    help call odd risk concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
