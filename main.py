690298f7758e74caac9cf567b301df2c75c2a0da
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:05 +0000

    tour pick satisfactory open classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
