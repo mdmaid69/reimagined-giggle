f6b43af5c0e565de12ea6cfaaf5b6347055a481d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:40:43 +0000

    thin foot style pressure convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
