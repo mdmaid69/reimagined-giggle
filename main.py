51bdd1d331d17f8d5380ff47c293915ddb39910e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:59 +0000

    season wind from burn machine

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
