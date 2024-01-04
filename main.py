343cef4086fc030ff268411af6ae88005adfcab3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:57:02 +0000

    damage error car minute strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
