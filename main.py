a7c49aa294bb240db32cb34a563c9f66e54efeef
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:30:46 +0000

    space later relief Qq fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
