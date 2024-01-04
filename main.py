d473092540aa76315e34bb6c8dbe21d7fd3fc19e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:45:43 +0000

    art unusual system takeover laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
