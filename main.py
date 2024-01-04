792172cf14af1cce48e51effd914748d1f85f896
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:19 +0000

    develop steal else goodbye emergency

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
