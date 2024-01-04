29cff11e0ca9fbc039c3986bf6ead5a316eb2954
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:04:37 +0000

    scream stamp tear sing basically

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def print_python_version():
+        return sys.version
