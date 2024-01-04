345171e76597089711fdec89f0f0ce92ee3e62bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:23:34 +0000

    rank glass connect constantly plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
 print(math.pi)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
