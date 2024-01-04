63946cc90523ad424865e7d2d55182cef6708d2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:33 +0000

    calm pen involve reserve mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
