81d253c9f5d2fbe1a5c1693fcf60528b9e4520f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:58:34 +0000

    wash reasonable fact mess already

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+print(sys.version)
