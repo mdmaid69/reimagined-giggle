b52382c8c68ee98495c143bad6271e0cbe1a94ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:49 +0000

    cry arrangement flow second defend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def clear_array(array):
-        array *= 0
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
