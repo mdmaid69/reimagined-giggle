d17bde4572cac598a37f1e5f713e7dca1d3a572a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:52:39 +0000

    beside pollution goout how compete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
