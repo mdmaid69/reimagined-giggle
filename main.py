2eb21cbcbbd19302f6198b7f0d1ce2d4527c5292
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:43:30 +0000

    grateful healthy bringforward stranger private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
