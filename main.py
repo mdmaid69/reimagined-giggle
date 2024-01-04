57ddc9623b650de1f942359cb017b0616ac8885a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:48:04 +0000

    better empty bread walk announce

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_current_directory():
+        return os.getcwd()
