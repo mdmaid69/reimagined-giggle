2ac7e2105461c3c525c31437d8a1dc8b21e4f4b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:27:45 +0000

    march thirsty drink wave blow

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def sort_list(lst):
+        return sorted(lst)
+def square_number(x):
+        return x**2
