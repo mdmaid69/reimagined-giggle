676f993260c88b9447d9894939b32f113e5f260a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:11 +0000

    reply block precise file surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def get_array_item(array, i):
+        return array[i]
