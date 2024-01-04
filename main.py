7db21eb73a7e19fad8d9242e98849024437a1589
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:24:42 +0000

    almost precise share enthusiastic rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def find_unique_words(sentence):
+        return set(sentence.split())
