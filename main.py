36503327c77dbe1887e597ffab9aa9608fa0800f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:05 +0000

    truth enthusiastic shut cough waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
