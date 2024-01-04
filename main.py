692c48a2f6032a4c4d69187fe3082535da706584
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:30 +0000

    continue case characteristic difficulty set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
