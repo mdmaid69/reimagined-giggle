b6ad46e17693fb77e271fbc6496a6c006409cf49
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:14 +0000

    scale improve row avoid holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
 def calculate_circle_circumference(radius):
         return 2 * math.pi * radius
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
