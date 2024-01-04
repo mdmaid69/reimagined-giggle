cd70f06cb3343995464283814606e646a6e45783
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:41:54 +0000

    fence mine involve remain even

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
