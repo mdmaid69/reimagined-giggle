d47cbc0529849daf8aa7e36fdd014db404338182
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:03 +0000

    remark cost sometimes intend extra

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
