c8f35c946da872cbd75f65d85badda908327f1ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:52:56 +0000

    party container write taste foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
