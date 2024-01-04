99a247860f5ac9f8eed91193d80591d896ed8f3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:27:43 +0000

    payment imagine giveback may truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
