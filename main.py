3f5648e09ddcc2ab9a8839678bb95d578d2198fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:06:28 +0000

    contact piece suit bill persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+  def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_str(array):
+        return str(array)
