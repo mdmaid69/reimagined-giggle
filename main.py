140f32e453d566b5e5dbc8c247fda03383d06de9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:01:46 +0000

    eye slightly long bother open

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
