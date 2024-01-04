74a4830a3e750760c080aeeb39e912f4473fd3a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:06 +0000

    possession dinner turn often satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
 import array
-def append_to_array(array, item):
-        array.append(item)
+def reverse_array(array):
+        array.reverse()
+import array
+def get_list_from_array(array):
+        return array.tolist()
