0bfcfe4e9a7843c7f29f3a298b191b76005f8fe1
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:34:51 +0000

    lunch uponon wild stage rare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def convert_to_binary(n):
-        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
