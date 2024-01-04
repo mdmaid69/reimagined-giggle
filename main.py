b6fa06c5c34f03cc5d2af33ee972e5d3ec602ef1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:53:42 +0000

    close spring sure rare tap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
