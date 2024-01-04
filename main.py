9f9799565f327d0c7513f0fe9b8719afbf8a199c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:53:19 +0000

    flat spot comfortable round light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
