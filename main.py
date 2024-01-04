d8e030728f3ae2de0040178f718d97aea2eaaf59
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:49 +0000

    callup wind uniform hardly definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
