4ca3362c7fa43cbe2f91802d45852b2a73d010c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:32:34 +0000

    news interrupt unhappy touch raise

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
