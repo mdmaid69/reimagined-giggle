84e9635e825119ddebd5c4ddeff0d5f72ced5d56
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:38 +0000

    now design fat hate warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
