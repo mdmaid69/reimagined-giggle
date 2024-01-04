41e30c33b896a0cbb550dd87179e722396da17bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:43:22 +0000

    return run influence reply conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
