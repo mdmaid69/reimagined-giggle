2ed1445f863d50eac07ccf25152d1a033c82115b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:44:30 +0000

    instant neck used wealth church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
