e164efb9b97348579de75538995c400f42c30edb
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:17:29 +0000

    build connected except roof nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
