8cc19b46e8c837fbbc83cc716e23c617b3225a7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:59 +0000

    separate walk confuse morning it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import getpass
+def get_username():
+        return getpass.getuser()
+def greet(name):
+        print(f"Hello, {name}!")
