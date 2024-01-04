44ddf673f01765f23fc551aaf21dcf4f1cc9819d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:15:01 +0000

    stage maintain far bright 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
