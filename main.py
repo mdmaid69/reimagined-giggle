a702ab3c82eb7d1e432c418210d50ae849adf630
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:29 +0000

    so female transport trick black

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
