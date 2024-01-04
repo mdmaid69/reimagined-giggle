2703011df93492fbc35c920dc15e79535523334d
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:54:56 +0000

    out no guess twist possible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
