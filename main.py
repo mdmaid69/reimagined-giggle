155f96c0ae42ab0fdb77b56b493ca5f1c7e9805e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:03:50 +0000

    direct workout protest decide narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_area(radius):
+        return 3.14 * radius * radius
