540b12123b660015b67977a8e48e7f9e365fa5cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:30:10 +0000

    activity model shop insist possibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
