9c627b1af3b46408d50905c278cdc901f6307c81
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:00:07 +0000

    matter before jump file be

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
