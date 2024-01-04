f4914fac4c4abb64adf5e156c1dde7b910a29152
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:26:30 +0000

    detail once copy reply bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
