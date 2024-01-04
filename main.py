7d9f98e9de17b2e0d3e1dc6a40c9ae4120b04b7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:46 +0000

    fear tip mystery past demand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
