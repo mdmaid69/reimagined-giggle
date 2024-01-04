a500af5d4719fa301a3de9ee56b946f3bd9ad181
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:29:44 +0000

    repeat wet surprise Jj public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def is_even(n):
+        return n % 2 == 0
