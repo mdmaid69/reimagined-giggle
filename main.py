1d90c459a917ae474b423317e1d0439c2ae94233
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:06:07 +0000

    centimetre unlikely crash lay letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def change_working_directory(path):
+        os.chdir(path)
