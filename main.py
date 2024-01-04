45d6cb0efe6c147e1cc97b5549b7ebb5e69adbc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:01:31 +0000

    but fasten listen store could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
