183065a4c7bec1591bc07e53f57bae4383723d71
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:19:20 +0000

    steam handle strike succeed tool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import numpy as np
+print(np.array([1, 2, 3]))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
