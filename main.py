62d95b4d528a8c180faa2b8ccdf41af3f88fa3de
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:26:34 +0000

    appearance my news pure exchange

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
