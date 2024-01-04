9d7a6bde280e70387bcfaa7b2191143b21eef172
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:10:29 +0000

    than minimum meal extremely shape

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+print([x**2 for x in range(10)])
