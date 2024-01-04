cc6814bd375a3bc7f8fd4805915122c2f0fddcf4
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:13:14 +0000

    closed official unless age nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
