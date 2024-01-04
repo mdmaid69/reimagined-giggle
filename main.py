f99142c2a68b6aa678daab8eff297189f83dc616
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:32:42 +0000

    keep prove makeup sale no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
