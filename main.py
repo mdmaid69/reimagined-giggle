2fc74000dbfa0432e2be27281c9177da0bcefb5b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:38:59 +0000

    question mark black exit get

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
