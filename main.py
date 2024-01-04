47564d09ef5d7611103cf22146fcdb7040b5cc0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:17 +0000

    tax dream physical press herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
