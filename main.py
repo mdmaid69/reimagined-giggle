2f136a2084cf812fb84d62f749ded574a58a1113
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:47:31 +0000

    preparation hurt surprised sort try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
