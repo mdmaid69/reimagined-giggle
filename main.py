c2ea9ce172ceeffe7cf2b41e6428a8fa48d8d65d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:55:48 +0000

    basically embarrassed mark future air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
