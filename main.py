3e6ab94bb6cc051232b1ee439ba3c1e32386d80d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:16:10 +0000

    holdout prove spring exaggerate music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
