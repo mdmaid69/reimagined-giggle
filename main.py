c939bbc294670314f6b0cfe26a61de5c74ba4c1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:42:32 +0000

    partner standard slip degree class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
