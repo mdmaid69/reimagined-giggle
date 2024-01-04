50c0b017633a9262c30bfb2356da78c28dc40cf6
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:05:41 +0000

    trust confidence destroy muscle vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
