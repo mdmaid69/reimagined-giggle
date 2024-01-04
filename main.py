ae357aed0270cc832d8649965999637e4251e62d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:52 +0000

    justice turnover majority noisy worth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
