a9ced8ac863402d6694f6d7e6780a0f85cf480f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:23:51 +0000

    lucky over accidentally hold forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
