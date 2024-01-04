9d16b4f03debe7f08ad7ae3de060b54b7d4f59f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:22:43 +0000

    rescue judge practice lend risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
