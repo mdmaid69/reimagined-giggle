0d82f074e4a111a61c7ea5bbd3a5b4c249e7dfb9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:55:55 +0000

    single window warn fight statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
