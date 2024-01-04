0ac7d21fb46c615dee59e06ecbc3b3937a1b6ea5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:02:07 +0000

    fall favourite frightened turnon club

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
