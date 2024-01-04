0b1e81ed8155c7c2b5831f1428693ed077934376
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:32:43 +0000

    hurt think fine duty comeup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
