57b8086ff211a84be4eca0359ea9aebbe62b3ea4
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:17:18 +0000

    key consult fond develop butter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import array
-def reverse_array(array):
-        array.reverse()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
