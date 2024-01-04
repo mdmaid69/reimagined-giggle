29b88d0ce083b7440c58ef4d7e58497370f670b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:24:46 +0000

    guess learn uniform instant smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
