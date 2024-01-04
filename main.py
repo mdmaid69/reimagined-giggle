3cfb555e018d8ab48f595cd6b6f85b807cb3c6ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:33:56 +0000

    at relation guide relationship regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_current_date():
+        return datetime.date.today()
