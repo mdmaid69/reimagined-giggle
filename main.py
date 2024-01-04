29b230607ae08be6f5394951ae727183b87c7236
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:48:37 +0000

    free splash so gate exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
