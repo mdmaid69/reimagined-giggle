02254fcd2fe870e72af06adaa8b88a01e91828c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:32:39 +0000

    tone favour fine address hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
