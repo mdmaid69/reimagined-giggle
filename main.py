c32fcb576fd833784c62ef3e5e5b90204ca395f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:35:08 +0000

    569enjoy wire discussion married young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
