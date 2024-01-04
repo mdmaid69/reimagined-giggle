cb7eff271a5783edfa1dcc559d8fca81d182b8d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:06:39 +0000

    everyone push clean mysterious negative

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-print(sum(range(10)))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
