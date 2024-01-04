f41f1b140731f015bd0b7d18d163736a81585681
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:10 +0000

    snow similar experience suffer control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def roll_die():
-        return random.randint(1, 6)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
