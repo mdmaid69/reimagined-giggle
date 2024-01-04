c757056abe6c91f69b637505e27c556c1a3a165b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:23 +0000

    lesson do freeze wait evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
