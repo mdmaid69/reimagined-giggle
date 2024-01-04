e88fba24d7fb74beeb803d62168f9ea79355c03a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:19 +0000

    urgent interested splash failure disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
