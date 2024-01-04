6aa428acf4b28a24084836725ab5d0d9d12bbb07
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:18:21 +0000

    care change friend choose extremely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
