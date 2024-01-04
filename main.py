8610281cca08284e49922689b97bb2518e62580f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:46:39 +0000

    mood Ll Ff very defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 sentence = "Hello, world!"
 print("Unique words:", len(set(sentence.split())))
