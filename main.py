c153ab6d246a27c4b93c717d2a01f2724d305819
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:02:00 +0000

    boring lunch include open sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
