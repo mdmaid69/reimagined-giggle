bcefdac0a26a20a697492b5a25296f683534a6bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:19:18 +0000

    answer feature female save exercise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
