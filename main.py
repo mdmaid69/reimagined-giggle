297fb03797a847dfc65a1214b87689d7fd58b323
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:03:10 +0000

    birthday instrument finish giveup hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def divide_numbers(x, y):
+        return x / y
