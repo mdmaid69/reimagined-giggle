9235ffda4885ffeacb7655d35c6b18ea5fefee68
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:58:35 +0000

    dry wave morning public impression

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
