21a22b40e60a07f96c6eb5b3bc3737cd6bd646f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:26:00 +0000

    interview with run welcome ever

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def square_number(x):
-        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
