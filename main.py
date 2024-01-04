443524204e04ffd58800944beb6482ba967ac833
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:00:28 +0000

    thumb secret getup month break

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
