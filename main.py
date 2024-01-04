fd1ab88f503fa461d7aab25a23385128f1f57e02
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:35:01 +0000

    score thinkof fast union past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def is_odd(n):
+        return n % 2 != 0
