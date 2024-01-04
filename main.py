e56ace8abe835dc461bc508a193d337befa47345
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:58:44 +0000

    eye active reasonable wherever pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
