f65714d6f51b479b3a2d6a4b07e0e24381f86df5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:37:30 +0000

    detail warm motion remark conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
