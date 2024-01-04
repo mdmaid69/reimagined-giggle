552db94ce2c5a81a8acb1400d0966c7425ba3bed
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:04 +0000

    official play start left reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
