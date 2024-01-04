76fa8303ceb6e9f3a9f140b8dd68081c73a1371b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:50:50 +0000

    grow common to negative charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
