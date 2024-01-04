5cac32cb55e7065f44f1eacb73fd4be10f35d724
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:58:29 +0000

    explain group fault careless settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
