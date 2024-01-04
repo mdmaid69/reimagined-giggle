3f41ab3b2d587ea62f058262f0a515d4e89aa814
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:33:26 +0000

    trousers compare learn risk appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
