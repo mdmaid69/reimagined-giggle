f56fc8abb38ab0beead48c5bbebbc274b4a66062
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:27:54 +0000

    pollution concentrate bite instruction art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
