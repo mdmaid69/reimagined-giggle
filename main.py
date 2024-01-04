48ff90d6c7ce464f337df8217d4fc9abbc61af1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:50:27 +0000

    runaway improve flood regularly healthy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
