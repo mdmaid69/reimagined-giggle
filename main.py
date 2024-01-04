b13bd14f4b7906176e7ec845210453edf85ce693
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:08:59 +0000

    matter fall around pick record

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_speed(distance, time):
+        return distance / time
+def multiply_numbers(x, y):
+        return x * y
