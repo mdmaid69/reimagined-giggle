c34734c1b3530f35747658a0246e1d95f2c8754c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:14:46 +0000

    project 569enjoy behaviour glad plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import datetime
+def get_today_date():
+        return datetime.date.today()
