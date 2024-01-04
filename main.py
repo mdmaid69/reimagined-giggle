602c742cc389d305c6a4ed94cb228d5f27801703
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:23:44 +0000

    clear balance scared letter outside

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
