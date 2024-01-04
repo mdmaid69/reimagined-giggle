69711e25dd7b08a89af2a63a977754ca31d5e860
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:08:15 +0000

    hurry talk rubbish bringdown charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
