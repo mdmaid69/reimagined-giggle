5af76960c1037438a4bc1f535bd90e37decf68a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:11:06 +0000

    slide clue entrance knowledge squeeze

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
