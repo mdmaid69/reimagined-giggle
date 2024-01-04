4ed6c4efbf7de85c91d081579d95698dcdde1946
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:25:25 +0000

    keep eye damage safe regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
