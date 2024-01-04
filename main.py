0a79e2392e85dc033618112b1671389f6061d872
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:33:00 +0000

    rely cause disappointed public compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
