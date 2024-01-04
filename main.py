cfd91fe7fac63e7b33f5afc5421bc9668d676d51
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:26:28 +0000

    basically show Ll boil sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
