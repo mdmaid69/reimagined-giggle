77ab91f6a007e996ffdd6c39bded39114bc6ca70
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:43:05 +0000

    control brush expensive surprise grow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import sys
-def print_python_version():
-        print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
