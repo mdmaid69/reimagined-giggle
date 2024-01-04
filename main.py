2e588d4eccb7ee0b4543d313ff666278d5bbbd63
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:28:40 +0000

    beside sting out convincing whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
