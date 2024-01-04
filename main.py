be2bb5b8d3caa288b1a3a7197ea6b0a08bdf7082
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:40:25 +0000

    might tell classroom wash getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
