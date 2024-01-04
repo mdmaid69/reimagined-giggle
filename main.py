90b7faa9904627daecfffdf1ebc62df067b48ec5
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:21:30 +0000

    strange pool control really win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def count_words(sentence):
+        return len(sentence.split())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
