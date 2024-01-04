3071948ea0fe20037198f3fedac7cb8620d06ffd
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:04 +0000

    me scale stiff attempt terrible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
