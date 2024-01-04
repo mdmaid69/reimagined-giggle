67acd7fe58a97ffe3c2aabf388b657993d13f030
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:23:36 +0000

    level late whisper pencil taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
