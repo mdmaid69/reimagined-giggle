cf79ac31c5f8b9cedf0a8a1d0cd90b89c98c9a36
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:00:25 +0000

    past normal surprising key arrange

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
