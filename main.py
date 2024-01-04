7d31601a3b21332ce39fbbd7db5c072fd863edd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:04 +0000

    near dry Ee calloff water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
