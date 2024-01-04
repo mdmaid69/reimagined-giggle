54cab5d0e3d03572e60243a1ea6afea41a980509
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:15:34 +0000

    study combine negative achievement mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
