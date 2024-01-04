e961b86319aa716d8b51f93d5949222ce9150a1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:26:39 +0000

    office window shoulder learn square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
