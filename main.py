0f86503a30c3fe5f673aefe497c30aaeecbd2d08
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:04:06 +0000

    keepout so understand begin hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
