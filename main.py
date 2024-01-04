9304e55474d1d2cbc549440cf8eb5cb1f8c5bdc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:04 +0000

    simple promise march month worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
