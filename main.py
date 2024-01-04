904cb047dbe9eec6fb2671657a337ff425d4ef0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:38 +0000

    gap exhibition pronounce treat interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
