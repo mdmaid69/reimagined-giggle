1d06c62502609f352196abdae2e2e77459c27503
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:46:21 +0000

    lock realize hour finish decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def subtract_numbers(x, y):
-        return x - y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
