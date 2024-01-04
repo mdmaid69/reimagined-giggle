d9b4103e8c311b18f293e48b9786d7fdbe5acdff
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:57 +0000

    tour interesting lunch dream reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
