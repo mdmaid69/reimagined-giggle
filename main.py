4b01c2fba71256aa40ab4e76c600f8928bb9620b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:03:39 +0000

    future better difficulty present positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
