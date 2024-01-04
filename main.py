6748759c803e4fd9218c4f981263b4090b3164ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:27:19 +0000

    behind trial year minimum free

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def reverse_list(lst):
+        return lst[::-1]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
