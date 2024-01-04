7e844a0f1f6a045cec8845f604cd64c33b8b000e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:45:39 +0000

    comeround you move act short

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def add_numbers(x, y):
+        return x + y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
