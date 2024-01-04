5cddabb57373f99a52515e4b9116698e4ff840ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:39:15 +0000

    turnback print although pocket official

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
