305c227d432b8889f8940da488f5f4b163eb85ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:20:06 +0000

    crisis call join moon old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
