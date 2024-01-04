9b5cd1eca44eadcf15c4ef356d358d65e8dcce4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:39 +0000

    tense experience invitation fresh determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
