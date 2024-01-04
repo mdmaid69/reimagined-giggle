66d1b4deeab658514314e9af91b9c1566b805ecb
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:33:15 +0000

    extraordinary sight man trust mine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
