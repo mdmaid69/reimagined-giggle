838840bad00d4e48630aae0c1b249a517069acb6
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:01:46 +0000

    pride ashamed set battery closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
 import sys
 def print_python_version():
         print(sys.version)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
