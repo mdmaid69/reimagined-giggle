829550037e59b7f0baefb7c75516d73cc3fc7863
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:59:09 +0000

    face unit road side away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
