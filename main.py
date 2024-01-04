02ca4f49ddcfba5af74b3bf63529360c9df466f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:21:35 +0000

    fall receive grade over 571enough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_work(force, distance):
+        return force * distance
