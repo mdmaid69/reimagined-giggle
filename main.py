271becc7b23b8be442a15270c367e864bdd18b50
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:46:49 +0000

    artificial ours keepoff female record

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import getpass
-def get_username():
-        return getpass.getuser()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
