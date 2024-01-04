3e14867867f75d0fb756f6e08a7d2a368c6bf522
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:24 +0000

    boat takeoff pick second reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
