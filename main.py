b652281127ec81469a364b78531a55d5c9a6b8db
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:26:11 +0000

    have figure threaten cut guilty

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_density(mass, volume):
+        return mass / volume
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
