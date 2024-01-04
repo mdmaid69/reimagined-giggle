9b910ac8eefdbd65f8d397de1aec3f27d8eddcee
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:29:58 +0000

    bet answer she discover to

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
