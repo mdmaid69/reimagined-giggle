5088cda2f2168dafbc2212aaeceb573a10d9e0c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:09:52 +0000

    change exam statement open hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
