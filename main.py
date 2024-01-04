45ed7d7454a8035682f76d92a39b0ebcabde416f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:38:23 +0000

    regard right dare bath embarrassing

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
