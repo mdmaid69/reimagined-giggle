33549a689ef4c57e1c2ecb8d611a8ad952a9bbec
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:48:36 +0000

    next sure worse escape write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
