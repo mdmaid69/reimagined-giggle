2127385c94dbd06f35ffb4f4796b6326415a1aec
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:46 +0000

    difficulty more tread workon goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
