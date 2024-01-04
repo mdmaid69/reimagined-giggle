90393889b163688df0afef5bb9700c2d4c274c60
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:56:51 +0000

    care aware neat used nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
