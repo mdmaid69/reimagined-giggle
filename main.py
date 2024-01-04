ab139b0bccfa409870e5d1674c5f59c2c27de24a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:39 +0000

    peaceful pinch of unkind bottom

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def convert_to_octal(n):
-        return oct(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
