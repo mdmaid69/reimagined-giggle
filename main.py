9722df5911337a7219ce9a33ccc2237ad504ce7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:22:40 +0000

    forget raw regional level aim

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def sort_list(lst):
+        return sorted(lst)
