2ff1b9b48d6b0fd3a84428e14e01b4e7ae7ebdf7
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:28:31 +0000

    direction them past determined display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
