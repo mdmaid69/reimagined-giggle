fd5df7037b8c1200744f86a6e8dcd2638d972550
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:01 +0000

    considering squeeze giveaway stage ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
