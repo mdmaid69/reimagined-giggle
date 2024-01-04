8ccb58ce9692670669c9c3501d900fa76f8de472
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:09:31 +0000

    shrink prevent consist cheat last

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_even(n):
+        return n % 2 == 0
