26f3f0308b286fcaaeb7f604aba50308f1c7a95f
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:08 +0000

    colour where familiar since often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
