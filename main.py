9aec4ed09a0a27900795b60ed33b960256a12a4e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:20:38 +0000

    try breakup fight want favourite

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
