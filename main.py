757052eab97c028fec8428a68abaec8bd637e014
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:39 +0000

    sure occasion contribute live lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        return sys.version
