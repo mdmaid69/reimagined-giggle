44aebc0acb8766038db2bd8b561771bece124c45
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:06 +0000

    will strike transport fairly reserve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
