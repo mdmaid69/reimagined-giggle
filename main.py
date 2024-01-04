deb04c67249368e247b11a40c9990c838e7f6071
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:16:20 +0000

    tear by row range one

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
