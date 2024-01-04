7718c38447d3c290098dcf249fc1882d4d025177
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:43:10 +0000

    entrance mysterious chief definite breakinto

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
