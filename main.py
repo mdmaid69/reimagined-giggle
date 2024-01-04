38fa9464b7046061e1686f7f6b88eaa8f6e93d59
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:52:00 +0000

    slight wise yet aware gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
