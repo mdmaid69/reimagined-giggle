81febd397393114d1fe406cb7c424c18bbcf5dce
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:53:48 +0000

    really generous really separate positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
