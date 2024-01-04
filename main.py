3d0097a5c84ab1a5ab0c4b51e66f76b5081852b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:56 +0000

    accommodation boring kind yes detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
