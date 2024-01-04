b8f51392ae75c5bd08b9263bf94ee0ddafeec88f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:48:08 +0000

    feeling force impression shop minimum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
