57ed86b1f9a1d35e48f58870e50558940f44e2c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:45:56 +0000

    thinkof match urgent say fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
