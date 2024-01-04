19456c0834ddfacce4e83045c84dd634b926fe3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:03:01 +0000

    Rr dear lock song even

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def is_palindrome(s):
-        return s == s[::-1]
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def subtract_numbers(x, y):
+        return x - y
