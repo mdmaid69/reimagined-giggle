eff08b38d077f9f5ab73b813cdc3d6709f31e70a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:22:03 +0000

    quarter only keepup importance sentence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
