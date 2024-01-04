ab7cdba532e2133145a3f5ba037a86eadc1fc3d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:31 +0000

    congratulations thorough suddenly long interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
