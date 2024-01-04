dcdaa8302b6049a9d26ab8a08615a580749d3c3d
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:11:45 +0000

    believe boat touch over record

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def clear_array(array):
+        array *= 0
+def square_number(x):
+        return x**2
