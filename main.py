d315aa3d943b902620dab60938756e7fd9f8dc28
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:32:53 +0000

    hardly vision contact culture there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
