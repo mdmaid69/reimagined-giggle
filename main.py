5e3047f1f7b762a85c84001eb6b038637ea109d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:18:21 +0000

    break compare flavour load expression

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
