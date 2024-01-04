6b68bdd893757173ca9338b3d37457ee7b5fe179
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:04:26 +0000

    dress stare should type first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_string():
+        return collections.UserString()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
