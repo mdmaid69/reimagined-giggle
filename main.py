8cf92c6887c8af47a394caf4bf7bb91f1d2d330a
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:52:08 +0000

    support present lookback(on) patient related

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
