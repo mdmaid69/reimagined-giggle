b40e3a73cb5e44d2957592dc947233c18ea79084
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:42:12 +0000

    contents rest congratulate offer press

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def convert_to_binary(n):
+        return bin(n)
