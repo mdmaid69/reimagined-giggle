1f475f2f5a3c9eecf32bc0ac99410fbfce2dd1ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:28 +0000

    view in bang plant mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
