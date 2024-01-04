96585d4d991aec60a5f7330908f4a9308bac0a55
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:44:45 +0000

    protect destroy responsible sleep turnover

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
