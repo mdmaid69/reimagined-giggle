04fee87658a22e126e37582a3c9ed1954a10435e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:54:11 +0000

    rate sport fun calmly fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def subtract_numbers(x, y):
+        return x - y
