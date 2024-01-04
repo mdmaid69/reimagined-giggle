57f0244b577e77d4b660054916534a533f13d130
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:53:20 +0000

    my broken display hope help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
