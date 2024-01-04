f57101db367d72a85a7f4a4120e90962558eef91
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:22 +0000

    fence chin violence bridge drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
