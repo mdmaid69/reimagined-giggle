7194d76a41adf3f9f5f07dbdeb567ca155d8577f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:40:49 +0000

    try female you classroom air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
