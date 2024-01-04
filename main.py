40be3ac17a31a86e2e6356290320679902b014ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:28 +0000

    light ring store strong odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_max(lst):
+        return max(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
