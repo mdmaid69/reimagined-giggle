0c070b101bf448a5e1c1ed3531d72c11ccd1c6f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:56:52 +0000

    force while aim concerned talkinto

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
