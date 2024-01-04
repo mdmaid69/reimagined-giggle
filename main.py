a64cdfbcbc39f7b5488187807e20b692234e516f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:02:04 +0000

    scared opposite probably steep finish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_index(array, item):
-        return array.index(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
