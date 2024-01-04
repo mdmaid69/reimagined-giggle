06a4dd2ea5b97093b7301b7d895705f7f13a070a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:14 +0000

    emphasize each later division and

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_pressure(force, area):
+        return force / area
+def calculate_perpetuity(payment, rate):
+        return payment / rate
