25dc114b5043341103a2b9bcb0cc47d51ff700ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:59:28 +0000

    certainly yet flood cross aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
