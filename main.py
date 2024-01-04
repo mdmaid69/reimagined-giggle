442cc9bb6e23cd1d70b2ff76cd51c3b4bcb0edf6
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:59:44 +0000

    explain theirs club usually dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
