9d41eab396f6d2bf01b3d13eec910811df2dadbd
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:17:03 +0000

    sure laugh aim repair brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
