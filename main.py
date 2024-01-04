0113948e4ca004f8dca9f47ee0de85a1671337b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:02 +0000

    giveup area last soon kick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
