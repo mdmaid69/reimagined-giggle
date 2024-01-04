51d82a3bbc48cb2a604c8f36f02fafdee3b6dcb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:58:52 +0000

    bad thorough arrangement lead offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
