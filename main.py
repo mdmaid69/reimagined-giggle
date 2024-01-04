a3635474f91e2763678c3ac9995cbfe821ba8941
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:28:54 +0000

    brush build mark pen variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
