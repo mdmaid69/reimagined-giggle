e5511a9fab1ae99b47d87f7fa0826c8b6549c28a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:16 +0000

    cross satisfactory throughout dance nothing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+print(math.pi)
