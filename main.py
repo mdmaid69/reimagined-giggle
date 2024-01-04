b607804bd113ddcfa00aea51e7c1e4b0ecf635b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:53:52 +0000

    involve middle rank lend demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
