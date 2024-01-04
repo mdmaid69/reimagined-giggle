4f136f46323da04e29d76d58b61c56a6120a2684
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:41:53 +0000

    find conclusion unfair runover record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
