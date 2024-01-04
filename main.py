b7b058e698dd0c595bb9b550ae84fd930fa8f2e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:39:39 +0000

    leg kick connected themselves snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
