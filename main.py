10d7d5f5d6394427d0afdc5fc4606eccf63993b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:38:41 +0000

    ashamed rush watch spring object

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
