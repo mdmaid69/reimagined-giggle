8b795a7ac40c4baa9cc10da0206641377e1851a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:02:40 +0000

    tap soil interested could but

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import sys
-print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
