ba9d0480eba491657ef67d2509dc90bf89fa3334
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:20:36 +0000

    land reach sign appeal she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
