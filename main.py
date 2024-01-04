29f8c5843e6a5cc74b16ea31b6fa656d64b33ecb
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:35 +0000

    art official set it present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
