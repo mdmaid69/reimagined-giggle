ff5c8020392c8c7742f2f7392f73d547e2721293
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:18:42 +0000

    nice phone my narrow home

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
