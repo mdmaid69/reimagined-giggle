e48001bcd220ef2016d1a2616754db9ce7dbc77b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:36:06 +0000

    modern goup estimate sport fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
