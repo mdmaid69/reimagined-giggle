161dcbf6f8b385bc571179dba1c6e575c1223e63
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:03:01 +0000

    war OKorokay mad quickly single

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def create_user_dict():
+        return collections.UserDict()
