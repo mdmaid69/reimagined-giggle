2b8baccba76d465ecc89a012bf679980bd05ea94
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:48:46 +0000

    eat figure favourite talkback crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-print(os.getcwd())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
