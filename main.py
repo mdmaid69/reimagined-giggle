5bdeb547a00d7b6b50e366b9b741e5070af9c76c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:58:25 +0000

    slice circle lead shake call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
