53550f1f7d073634dfd7b6148b2c42f00ee7f2b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:53 +0000

    before vote education remember admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import json
+print(json.dumps({"name": "John", "age": 30}))
