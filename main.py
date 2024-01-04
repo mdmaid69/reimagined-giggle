dc92d02b0f24ff302242fd30cfc2cb8cfbd79edb
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:16:24 +0000

    formal aware test she takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
