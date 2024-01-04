391a31edd9fd1338d5f031d4cd78ba31e7625c43
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:38 +0000

    cause fortunately doubt rush store

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
