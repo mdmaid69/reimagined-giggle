34df1db7054e17372f2447bcb707c0c9c7bf7b03
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:32:58 +0000

    agreement innocent service temper describe

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
