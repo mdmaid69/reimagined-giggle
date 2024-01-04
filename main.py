925a9d3ff2b2f5cd71a90b0c8d03afdf674e4fc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:32 +0000

    want wish wild kill married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
