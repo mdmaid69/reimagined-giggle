5c84b6a68936527a50f2b6cc0b0cab347817fbb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:19:56 +0000

    bin means target pool add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import sys
+  def get_python_version():
+        return sys.version
