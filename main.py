1f499943cb4a446453d4eb0238a7ea5ecfb30233
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:22 +0000

    air worth pressure callout secret

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
