44d86f9fbb19dd25204903f2cff3ad8b06fde9b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:45:06 +0000

    cost thirsty giveout still task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
