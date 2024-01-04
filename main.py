421a1b92f11e62c0bcccccaf082c0f0140195a2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:08:48 +0000

    anxious fast little state material

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import json
   def convert_dict_to_json(d):
         return json.dumps(d)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
