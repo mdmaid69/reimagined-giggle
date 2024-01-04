4b3c2731b6f7b84285c12b6370b3bd3a0d80052f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:08 +0000

    pause prize will chat over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
