25b399abdb7a356cfd4abc0f3edf5f4f3beb714d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:40:48 +0000

    behave takeover welcome not communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
