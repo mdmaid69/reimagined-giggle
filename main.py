a722c7a043eeea1f22be1335348536b97c08696c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:29 +0000

    plate vision seriously rule laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_item(array, i):
+        return array[i]
