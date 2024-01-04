b5576f8ca33a69831f057a083ce552d222cb48ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:06:48 +0000

    physical reaction education immediately borrow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+print(json.dumps({"name": "John", "age": 30}))
