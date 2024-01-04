56377d9f3e38d4896dae6f067cc2f7e34c1401a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:11:20 +0000

    message reaction piece situation promise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
