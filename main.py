5091872bd2377807b57d3e2bdc515a4f29992837
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:04:51 +0000

    fortunately excuse rise goup search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
