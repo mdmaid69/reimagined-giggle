ea7b928020e6faf761a800963cf7e4ea1a12314e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:00:40 +0000

    cook opposite suddenly standard work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_max(numbers):
+        return max(numbers)
