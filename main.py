b416e339435650f04f1c34b5de9e0d020b058eb3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:26 +0000

    win shape when sail special

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
