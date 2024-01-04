c28ce6afc1140bfec344406a7fc990c4e8120a37
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:05:33 +0000

    final fat reduce prefer concentrate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-name = "Python"
-print("Hello,", name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
