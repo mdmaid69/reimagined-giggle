10de0ee928fb1d3a6d5c197f10845dbfad789edd
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:04 +0000

    swallow fast friendly future sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
