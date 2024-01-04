1e202b76a6296e220cfdf016ab219d6e3b97fe99
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:39:24 +0000

    threaten so wait neither but

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
