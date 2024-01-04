9efb1dc70cfcff839afb331076ad6212ed43dd55
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:42:21 +0000

    guilty call lookup fine successful

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
