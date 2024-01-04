84abae191a07c1285dbc3da476386f6dc4c4f36a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:38 +0000

    mend line simple stop children

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+from collections import Counter
+print(Counter("hello world"))
