599a72a02e4ff935d5e3e83d5a70033a1e588736
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:04 +0000

    loss this 572enter dream about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
