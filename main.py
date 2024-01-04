285644f225a616bfbb9dc59b0c7d180fa9c34eae
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:21:14 +0000

    store up ticket hot tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
