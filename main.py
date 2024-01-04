061468fe86a534d2a02732f4746fe594728df209
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:56:12 +0000

    like use Ll Uu pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+print(math.pi)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
