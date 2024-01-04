d8567c4ff5c9da80d58fc4208a8fd5fb62707ddc
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:39 +0000

    rarely question strength telephone part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
