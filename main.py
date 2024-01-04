fb4859331aa379b025f3c69cafba2e4536fb3a11
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:07 +0000

    bet wind hit remove damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(numbers):
+        return min(numbers)
