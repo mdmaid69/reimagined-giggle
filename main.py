eeceb1f8d95113438afa9e7810183e2038a64bc1
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:59:45 +0000

    great polite noise story chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
