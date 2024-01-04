c3f81b8d90a4fc90493e58239f295beaa201f22b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:51:49 +0000

    row wear command ours female

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_area_rectangle(l, w):
+        return l * w
