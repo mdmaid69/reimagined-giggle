7cb1ccab9d35b6fbdf678a122d9486f6bc28cf61
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:55:38 +0000

    suggest forward hope change song

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
