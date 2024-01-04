728b934f1ebc2fb896fca3aae74791e123a46ee8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:13:31 +0000

    cough Dd tip pick ourselves

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import numpy as np
+print(np.array([1, 2, 3]))
