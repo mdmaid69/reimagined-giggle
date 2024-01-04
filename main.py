792ea39174f7fbdc4842ebe7968640ca649be221
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:12:22 +0000

    room approval themselves pride opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
