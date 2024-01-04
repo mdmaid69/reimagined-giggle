db488976caffab6f766c02755135f24746fee3b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:49:58 +0000

    neither apologizeapologise boil left fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
