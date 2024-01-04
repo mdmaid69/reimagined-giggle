33373b6903753d0d3e31453dd97d901c309e89e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:49:29 +0000

    heat fold father lie loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(lst):
+        return min(lst)
