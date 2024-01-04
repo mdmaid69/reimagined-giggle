d97f0ebd88e0cb95d9f2158f0b91b6347db5e6a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:32:06 +0000

    attention comfortable steep damage upwards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
