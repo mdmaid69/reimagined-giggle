2214d0a2f4e3b58308858951c21d070e533c00d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:18:52 +0000

    press light hand crisis substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
