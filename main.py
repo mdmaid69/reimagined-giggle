574d6d5542a0ba93039a842417a4b638a190d6d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:28:58 +0000

    coat unit Gg concerning however

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_as_bool(array):
-        return bool(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_density(mass, volume):
+        return mass / volume
