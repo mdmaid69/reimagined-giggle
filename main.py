0eb1cbe6ac5c0f25fbc73dca1617f83729b5629c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:13:22 +0000

    blame intend low welcome open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
