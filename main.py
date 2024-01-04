213af71aed5ba8c40c55cf82b163b0e16592c229
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:58:10 +0000

    demand late I finger Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
