009c0304e7144b5fc189420085dbef6e1982974f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:55:49 +0000

    fit plain service succeed get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
