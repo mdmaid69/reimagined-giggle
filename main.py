af1de6ece8185055d27779f5bb3654b1fab6c21e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:00 +0000

    among completely dance average wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def square_number(x):
-        return x**2
+import datetime
+print(datetime.datetime.now())
+import time
+def get_current_time():
+        return time.time()
