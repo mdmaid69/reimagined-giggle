95f41505e8108646a575592e05c4c49f14108394
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:33 +0000

    distance path uncertain unfortunate for

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import sys
+  def get_python_version():
+        return sys.version
   import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
