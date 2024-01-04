3f7840ab02a49cbb4a277b0f2b49a714961a4faf
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:49:20 +0000

    cum Cc help music wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
