779fb057c17f8c19a699eaf5901ce4a749cc8b9c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:46 +0000

    collect subject sell anyone nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def multiply_numbers(x, y):
+        return x * y
