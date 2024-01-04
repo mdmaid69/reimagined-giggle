62b85341ba13ecf7e36f5367d3c7dbaec534b69b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:09 +0000

    preparation state trouble priority car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
