8aa71480de6fa9ab8a9689142489646991933358
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:52:01 +0000

    fit treat tour dance promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
