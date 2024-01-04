2563d0ec307f8e2cc46de48d3121671bb8eb5f40
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:29:57 +0000

    glad shock face generally impatient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_as_list(array):
-        return list(array)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
