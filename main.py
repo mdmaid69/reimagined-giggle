81e4e244787be1b8a7b1a394b9c1a3b9308ddd87
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:03 +0000

    makeinto wrong meet achieve behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
