d8521dc21d49aed1b8ef5cfa697ad3d2798c0698
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:38:07 +0000

    oil organize measure regional shoot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_list_from_array(array):
+        return array.tolist()
