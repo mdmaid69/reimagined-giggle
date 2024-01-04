72f9b1edada6781e101e3fff72251e7896ce9b1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:25:10 +0000

    price myself act trap lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
