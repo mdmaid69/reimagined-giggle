33dabf558b568a91c0464aefe99e6b1b267e60a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:11:23 +0000

    surprise niggaacademy statement run produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def reverse_array(array):
+        array.reverse()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
