1aaaf582965b10577680e121830193a71a0c4caa
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:35:43 +0000

    container zero insist poor hair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def count_elements(lst):
+        return len(lst)
