a9aba8ea684c56ab23e9c6d8a059f96eae112abd
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:05:19 +0000

    price behind remember collect owe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
