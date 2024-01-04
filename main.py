9c2d0fa6cf07a456b7b9bc54f96e65b87a14af6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:25:59 +0000

    we force Ee shopping sort

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-for i in range(5):
-        print(i)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def add_numbers(x, y):
+        return x + y
