8310306588812805010d5ab1dc3c9f72330f3bf5
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:33:23 +0000

    plain base reaction never payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
