f42fb1d6cf82439c2f2d326a9c641ebb93c35a1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:37 +0000

    word dress from chance welcome

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
