064f70d831cb968e2b59fd9b0fd57302bb36bd91
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:17 +0000

    fail achievement talkback bush continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
