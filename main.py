7824929a3ed75f8c1b9ffe6b5cf65474d2a559d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:11:44 +0000

    keen repeat phone nor even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
