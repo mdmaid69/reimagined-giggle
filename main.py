0653ebad08732d85d6ac30d4fa2cfbe96743dc79
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:47:51 +0000

    clothes protest organize him occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
