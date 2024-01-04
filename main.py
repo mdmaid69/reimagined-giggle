7d25140a304c7718a747f6b6c5bec0c93d37bfbb
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:09:54 +0000

    whistle case future slice heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
