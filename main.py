1ee743f7161f7400c78d2d2072483793b562e97c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:12:07 +0000

    turninto release turnaway build cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
