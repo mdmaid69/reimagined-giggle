38c3457570d54ba6abe6fbbac726dc07c2312325
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:06:18 +0000

    mystery round bar sharp convenient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
