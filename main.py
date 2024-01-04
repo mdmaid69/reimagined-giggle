783d7b69b800724a1e2087f99a52b28293e77c63
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:04:44 +0000

    instruction bottom such exploration bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
