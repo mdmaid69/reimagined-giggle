a269d935e513b121bb84958e5a0221ec08995bd2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:50:28 +0000

    section cold yet only negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_item(array, i):
+        return array[i]
