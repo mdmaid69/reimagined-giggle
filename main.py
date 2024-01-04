e87ef33dab093c3b202672d4768bac0657fde367
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:08:48 +0000

    report impressed bet half hungry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+print(os.getcwd())
+def convert_to_hex(n):
+        return hex(n)
