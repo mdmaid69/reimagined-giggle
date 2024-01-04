983d8cb2b5b86e2087e7910afeda8bacbc4d91d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:44:12 +0000

    couple prepare could it communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
