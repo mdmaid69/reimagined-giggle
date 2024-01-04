a9d3ec826cd5b5dc8d5d7499d026ea1058870e66
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:34:28 +0000

    put really serious event office

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
