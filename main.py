b96a0185cf52072cee9f55a86fbe9476caed8d20
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:44:23 +0000

    direct difference smell fast slide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
