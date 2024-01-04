ee7d17c1dbd05d99e77764be1016845dfeca8855
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:35:41 +0000

    gooff try lookup range peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
