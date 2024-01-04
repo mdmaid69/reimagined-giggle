defac6449b4a6b55c6156a6d10b794261a777fe7
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:44 +0000

    conduct last past term attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
