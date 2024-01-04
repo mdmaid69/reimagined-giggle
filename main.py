377a951d80f210e4ed42c4439be8054da0583407
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:05:44 +0000

    independent illness range stretch tidy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
