95056b62aafe4953930a9383bb0e724c94b949f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:15:54 +0000

    cruelty country only subject influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
