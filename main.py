8ada487ba7de5876754ebe04fb8b746393e5a025
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:17:12 +0000

    event total past explanation considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
