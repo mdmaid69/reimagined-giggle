07eb4c1917f8cc54003e6275a4a860c4f2cf39c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:55:13 +0000

    dull freedom strong increase shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
