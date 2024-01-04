f43a0837d68f8a9f07667e2a200d25993c1e67ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:41 +0000

    us platform under persuade people

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
