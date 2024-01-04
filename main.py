2ac4a1f0a1fe8ab1b242d58ba95cc30bab6e33c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:18:27 +0000

    free question invite cry tonight

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
