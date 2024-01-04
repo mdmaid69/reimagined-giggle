e63766d2a1cd1968a082e5161f03725aa234acdd
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:03:20 +0000

    contrast exhibition dinner Yy disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
