578c4559891aa12c2b131c92d719ec2fff4c6383
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:56:04 +0000

    throughout concerned ilovedicks comment each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
