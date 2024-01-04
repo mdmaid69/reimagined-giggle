9b3dd2439e07382009fd43c576c2e8295a85e5d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:59:24 +0000

    tread extremely buy sense because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
