3fadf81b8e7e8fdd60bea6761c291d250d27b44b
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:19:31 +0000

    who season connected doctor calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
