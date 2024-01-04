6c31759e814a16e8376499c47911f44cb73c5269
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:47 +0000

    please collection lay advantage funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
   def get_file_name_without_extension(file_name):
         return os.path.splitext(file_name)[0]
-import math
-def calculate_tangent(x):
-        return math.tan(x)
