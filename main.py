981aa2787426ec54f6032d708d7f8c3190af1915
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:58:35 +0000

    comeout shut aware congratulate uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def convert_to_hex(n):
+        return hex(n)
