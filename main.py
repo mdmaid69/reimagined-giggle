e74be666cb56b429c9c3a64f090b2e59ec9049b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:07:54 +0000

    debt their hope bet lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
