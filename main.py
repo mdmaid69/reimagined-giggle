edac490cf69ed95b2e7725ef4fd6f2967a64eb3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:28:53 +0000

    string actually convenient inquire operate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
