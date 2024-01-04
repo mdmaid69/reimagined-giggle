8443f19aded11e497f7d469fe83c3c531e110489
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:38:55 +0000

    whole notice surprising future shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
