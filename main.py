a48e7546d141348448b2c5ace08bee2b3361d909
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:54:06 +0000

    urgent left total warn honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
