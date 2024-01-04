0e97a78ebb8a5fd352b2008b9dda484206e709a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:45:28 +0000

    bang listen detail national field

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
