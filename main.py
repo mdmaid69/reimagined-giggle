c25534b4ba61ce45067b74c318cd51e4050fca8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:45:02 +0000

    centre tough unfair yourself pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def multiply_numbers(x, y):
-        return x * y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def sort_numbers(numbers):
+        return sorted(numbers)
