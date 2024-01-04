c38a7a0b58df0e31ef7f845ad18f39b889935c20
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:33:51 +0000

    receive excuse inquire answer good

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
