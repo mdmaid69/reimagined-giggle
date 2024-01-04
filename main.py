aa89e1d1caa16c8c2e40afa0b50427b201fa47ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:55:34 +0000

    damage choice obvious anybody splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_force(mass, acceleration):
+        return mass * acceleration
