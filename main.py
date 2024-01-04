175b9f888a009893b4c82bcf6fff6b9e75a50bd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:13:57 +0000

    question float worse sticky still

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
