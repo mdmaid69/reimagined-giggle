c88a1dbf76da4435c60ceb0730b552d8357b6697
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:58:19 +0000

    coffee intelligent scare all petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
