435f2a3363f7f6aa5c6e986c6a9694124a994080
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:41:16 +0000

    custom nothing offence together positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
