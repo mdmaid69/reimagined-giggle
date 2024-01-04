31b53772929cf80c429dcc529a7df5cd508a9dff
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:28:59 +0000

    whatever for gate since smart

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
