b3dc98926ac6e5a3af242e9826b46b5c097c61c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:51:18 +0000

    settle since turninto maximum consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
