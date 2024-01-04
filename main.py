129aa28e861dbefac193fa56e73a3d48d2bea4a0
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:57:26 +0000

    instrument shock promise body conduct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
