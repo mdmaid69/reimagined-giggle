5f12f383b8efbd15b382ecec5ea3364f872837f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:37:49 +0000

    black broken future test ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(5):
-        print(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
