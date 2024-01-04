ed872a7e220f1e2fa58d346c59776a1c319f60fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:10:26 +0000

    minute brave represent flat spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
