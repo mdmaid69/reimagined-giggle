c981ba845d8d50d8809f6e8c72e5812f93985ea7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:35 +0000

    always typical before shoulder wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_list(array):
+        return list(array)
