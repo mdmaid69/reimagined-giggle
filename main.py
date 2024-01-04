61458c22227617e5887a09a8cc523c0eac8d471b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:25:38 +0000

    cough why rest dull sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
