a1728d5b537c9e7c1ba8ebc207fb35908541b3db
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:40:30 +0000

    chase morning at mind task

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
