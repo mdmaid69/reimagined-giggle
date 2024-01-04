90543c01c22a05602ae8ed01a1eb1f3627b10e3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:40 +0000

    breakfast comeoff few concentrate tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
