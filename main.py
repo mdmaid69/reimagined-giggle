e22e8fd4bd381c74ffab6fea7fc464607cb985b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:22:57 +0000

    special relation increase let anyone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+for i in range(5):
+        print(i)
