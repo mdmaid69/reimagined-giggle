fcc3ea075bad65f6e98d9b3f8101132e44ea9042
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:45:26 +0000

    you find afraid tool preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
