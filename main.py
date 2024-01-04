e40f318c7a43906d5bc11d1d4119e472be3abb2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:29 +0000

    part lean Ll ordinary free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+def print_python_version():
+        print(sys.version)
