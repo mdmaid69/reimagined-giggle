8663595cf317975e88e530cc9fef7769e60d5c11
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:31:21 +0000

    pass character name future himself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_min(numbers):
+        return min(numbers)
