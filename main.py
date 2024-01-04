086dd135a6e4ce4f5cd166b376f72f79c738c9f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:14:01 +0000

    minute and breakup language brush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
