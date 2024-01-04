411388ea7b8c11f3b8a68f982b36706d5cfa53f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:04 +0000

    famous precise flavour practical holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
