1f4b344500e73dfaf82a8ac5a9a6c2c7af509940
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:06:20 +0000

    street determined where search dig

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_density(mass, volume):
-        return mass / volume
+import datetime
+print(datetime.datetime.now())
+def calculate_power(work, time):
+        return work / time
