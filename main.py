8c6d2fde2cb73c2211645a4787608945d96b1965
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:17:27 +0000

    stamp thin church friendly only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
