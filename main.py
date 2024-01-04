0d0121e9f60d61573ec6cf944a88c78c04c09c95
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:02:49 +0000

    aim grand beneath thinkthrough bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def list_files_in_directory(path):
+        return os.listdir(path)
