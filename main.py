d2ddd92ef923edb421711d5f81c16b46085b225f
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:08:15 +0000

    steer indeed description react repeat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+print(sum(range(10)))
