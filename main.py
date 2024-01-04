21d625c307bb6c53ccbcaf1f35a3c7e738290ae8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:46:50 +0000

    pipe adjective breakup solve separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
