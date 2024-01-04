f78ea708675e863e85855bb0ef7574f0580d8e5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:52:13 +0000

    comeup human similar cruel preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_area(radius):
+        return 3.14 * radius * radius
