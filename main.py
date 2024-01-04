bfdede41f746f727fad834e43d372e2d58cd2397
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:45:00 +0000

    apparently thinkof bear alone exam

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
