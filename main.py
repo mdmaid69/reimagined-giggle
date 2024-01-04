e93d557cf19593bd446ee153ba4181622f6898dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:27:53 +0000

    suck quality connection seat tend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
