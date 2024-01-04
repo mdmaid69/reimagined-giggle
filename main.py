761d4969361d1920b9889da374cabc8791a0956f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:53:20 +0000

    surprised all track together read

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
