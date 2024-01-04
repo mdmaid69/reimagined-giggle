046a609b2788b9330e156f4527e27b777f2d4a41
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:31:02 +0000

    our persuade purpose takeback deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
