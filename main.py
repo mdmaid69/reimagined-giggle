42dd080e4a24b91e63a4fe6441c60fb6377b89f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:02 +0000

    dig along phone score convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_str(array):
+        return str(array)
