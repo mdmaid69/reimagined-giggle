48a632bc89592bc366228a125c02fce532553237
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:00:22 +0000

    shopping violent stupid steep opposite

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
