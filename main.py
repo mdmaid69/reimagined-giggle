28e8ec6cea444ad95d9f18db83600d31af57a184
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:00 +0000

    depend turnup unite worth check

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import getpass
+def get_username():
+        return getpass.getuser()
