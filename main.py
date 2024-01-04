a43dfe6a9b639508726c4f8b088f978fcb5cb0af
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:54 +0000

    prepare path part feed result

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def cube_number(x):
-        return x**3
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
