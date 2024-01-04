cf92835b043ab0651088c9695136ed9c07bc1973
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:12:18 +0000

    whistle hear collect event calm

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
