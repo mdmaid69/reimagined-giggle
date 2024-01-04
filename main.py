f97e311094ce6ff704d6a20f5df71c6750acafef
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:03 +0000

    concerning warn circumstance part goout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
