42a60f7f9eacdb0952347322c91d2c191313db65
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:37:19 +0000

    organization situation train heat power

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
