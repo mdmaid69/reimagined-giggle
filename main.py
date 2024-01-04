2c368602c3b7d295c3f7feabc9ac3517c2318c57
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:19:25 +0000

    idea argument die know responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
