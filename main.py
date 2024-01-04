6880eb7377aa16a0adc5ce09d0c83dab1fa98998
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:07:59 +0000

    idea Zz hear exaggerate one

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-print(os.getcwd())
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
