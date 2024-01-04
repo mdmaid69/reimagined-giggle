874c7b9b2f0a2f72a0f915b9ceec92afd017d2da
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:24:10 +0000

    teach later immediately organize reserve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
