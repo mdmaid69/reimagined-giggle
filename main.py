34c846d1249323c69dab08568ee5e2eaa4001072
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:50:45 +0000

    arrange glad right consider makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
