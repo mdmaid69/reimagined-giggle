697c6ec3af6672cc6b642590550b9c818e774898
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:29:08 +0000

    crazy arm bang towards become

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def clear_array(array):
+        array *= 0
