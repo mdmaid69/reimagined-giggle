2acdfa98af181ae59a12c2d27a89d5b5e281ae9b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:57:57 +0000

    aware invitation shut crowded little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
