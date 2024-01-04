bb5f0fa71c0dd4deb34875f78a12fa9b83f70b32
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:23:37 +0000

    in suggest load on circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_typecode(array):
+        return array.typecode
