b3d8500a83dc101fe3e3197a21d630f93af089f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:43:10 +0000

    backwards holdup tidy Cc doubt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_str(array):
+        return str(array)
