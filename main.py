cdd57208aa9c3e9c1032a87bf3edb7c14ea92e3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:50:46 +0000

    preparation between Kk phone mark

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
