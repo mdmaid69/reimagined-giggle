9f3311459a650fafd0c606d87bab51fdeb808960
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:26:09 +0000

    awake unfair whenever broad as

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import tensorflow as tf
+print(tf.__version__)
