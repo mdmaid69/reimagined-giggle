d468e3cd078654cabadbc664e3d2ae3a146832a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:40:54 +0000

    watch factory sport price goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
