cf85f3bc2668fcab46f221bb330a9b525e00cfa7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:49:56 +0000

    rely his vision tape stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
