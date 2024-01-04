d5e5f77e55020a8b04d1dbd1143e3112878d6816
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:06:53 +0000

    large cough yours space trade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_repr(array):
+        return repr(array)
