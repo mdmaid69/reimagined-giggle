3eaf6e6ae4ad3b284382bbed21f7bc64951ff159
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:53 +0000

    laugh Nn final disagree noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_from_array(array, item):
+        array.remove(item)
