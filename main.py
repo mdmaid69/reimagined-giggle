c074640da8f46426756893aab76dbf69e2e45bb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:07:07 +0000

    fit minimum shadow neat consider

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
