3f68b883847b94fa976d55dacd97f0af45e04ada
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:35:55 +0000

    scale reply 565end properly narrow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def get_array_as_float(array):
+        return float(array[0])
