30068cc427538f159ed38e3d0197928db7907f7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:20 +0000

    discover comeback flow brave plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
