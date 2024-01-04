14169a41ada4382d878349f802d558fa70010c68
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:14:00 +0000

    comedown thinkover group bar company

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(10): print(i)
