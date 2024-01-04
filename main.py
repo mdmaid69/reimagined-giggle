bab18678a670fbb1ba81132d376f8873c6c103fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:40:22 +0000

    odd test tool joke journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import time
-print(time.time())
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
