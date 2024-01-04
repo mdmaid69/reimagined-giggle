7170651c6fba2d5c635d6d8d10ec7624740b056f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:56:32 +0000

    development bye put meet will

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
