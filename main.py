1c235f19d0b4f1d797dcca3686b3e7eb1ffc8b9f
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:15:49 +0000

    thinkover raw boil attempt soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
