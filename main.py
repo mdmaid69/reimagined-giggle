7d6ec710b2db35c2092ed6af7c053ef9b51e5235
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:42 +0000

    refuse swimming aim urgent means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
