0b261dfdc1b39e6719cc0c4d64d70ec07b2943ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:10:58 +0000

    help cold attract may guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
