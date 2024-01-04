ff96824346e9c056d6bd6c4d49304747a483bc5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:37:20 +0000

    secret worth reasonable shut ride

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def convert_to_octal(n):
+        return oct(n)
