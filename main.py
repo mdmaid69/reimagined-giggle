b9e18aded28f75cafc44610b2ba204d04567f5b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:09 +0000

    action towards negative theirs forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import numpy as np
   def calculate_mean(arr):
         return np.mean(arr)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def reverse_list(lst):
+        return lst[::-1]
