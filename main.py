1a61f646603ae028ba267ed95c2944e8ff95dfde
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:06:21 +0000

    success picture convinced than shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
