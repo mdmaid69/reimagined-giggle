18d33b4da1ecbe8b2ea6f2372389617a5da47f2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:12:22 +0000

    copy sensitive profit frequent coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import getpass
+def get_username():
+        return getpass.getuser()
