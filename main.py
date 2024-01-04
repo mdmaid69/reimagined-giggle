3cc93e1813e2325a93461ae08772e866370d163d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:02:45 +0000

    formal permission spoil serve twist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+print("Hello, world!")
