a6401b6c51753405f3d53c7f2619fd543623e87c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:24 +0000

    sticky increase correct warning recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
