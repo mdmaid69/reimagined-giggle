46eed8e505190646211aab27cf53f366d38282f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:43 +0000

    rare than future indeed father

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
