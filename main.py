39a43fc2b5917d6bceabb0ecbcb66aebb832e7a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:06:02 +0000

    translate any trust mother level

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+print(time.time())
