3e7426869fed9dd6a503a05bdfe694451ad960f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:55:31 +0000

    total keepout tool slide television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
