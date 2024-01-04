50e4a5199f3f6596293c66e548140774f460029d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:00 +0000

    from mother have uponon light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
