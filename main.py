1302a9767061ccd9de5e4d197f20adbc7468137c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:03:28 +0000

    whisper lock Itself soak reply

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
