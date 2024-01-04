7366943fe41bd28ae64c707183596bb09aa1a80d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:09 +0000

    564encourage boat view holiday decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def reverse_array(array):
-        array.reverse()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
