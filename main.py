6503a0f4a8c074393d9133e577dcb80ce1bb6c15
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:30 +0000

    translate evening check keepfrom miss

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
