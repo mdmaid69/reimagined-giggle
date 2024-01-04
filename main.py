6c0263fcb31ce3c198da9bb75d0916393594c1a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:02:06 +0000

    lay cheek they hard classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def subtract_numbers(x, y):
+        return x - y
