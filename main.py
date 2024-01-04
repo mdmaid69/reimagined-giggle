0b5ca3c4eca14e5c511ff3c5aa344190143f1423
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:20 +0000

    special brief either inquire between

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import os
-print(os.getcwd())
+def square_number(x):
+        return x**2
+import datetime
+def get_today_date():
+        return datetime.date.today()
