cdda33bed3bdc65ba4c6b66f09ede757950d2715
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:30:38 +0000

    fair report lunch sad fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
