f134237d9a5743b3e3ac993a7a6e3285979eee95
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:15:42 +0000

    stare understand whom 569enjoy short

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
