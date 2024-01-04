a989eab54d9b8ec5a9727a457d509d7ee0e9b4a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:18 +0000

    never plan pipe face under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
