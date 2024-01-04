9389e3ef6fa652507fd8272d4480dd84b492b99d
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:54:29 +0000

    cause flat comeover bus exchange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-name = "Python"
-print("Hello,", name)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
