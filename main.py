e1fed149a6c4181ce80e6b6f47b586058ef3dbe7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:03:27 +0000

    advantage number recognize either pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
