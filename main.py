f76aaed1fec96127b81a1cf3eefb809b462ded03
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:03:34 +0000

    left cruelty pair easy dance

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
