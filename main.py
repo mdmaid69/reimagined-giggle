5dcec396d37698e3fe4197921df99b1e19ff1249
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:21 +0000

    wrong gap even possibly frequent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
