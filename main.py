3f40acdba64433b0b77c46c7c1649ff7047605f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:29 +0000

    smell Rr crash sort fire

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def multiply_numbers(x, y):
-        return x * y
+print("Hello, world!")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
