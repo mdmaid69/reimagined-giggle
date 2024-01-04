48b594c8dbda2950e9ebaba9f1d6d845337e3503
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:04 +0000

    by work history compete like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
