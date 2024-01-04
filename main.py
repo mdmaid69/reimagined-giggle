0a4011b6beeed63482453539cf9517e8e495fff9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:02 +0000

    light its provide loan spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
