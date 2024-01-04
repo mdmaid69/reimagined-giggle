160b94536d4f77b1c9d3427b3e0b8fd16eea04cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:01:30 +0000

    would narrow late than hospital

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print([x**2 for x in range(10)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
