6a6b000148ba412e9755fbc32b5553605192215b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:03:15 +0000

    workout but row introduce fail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_max(lst):
-        return max(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
