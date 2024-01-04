b84e4b99f218d69f26c8298a12e66c5a3260e70f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:48 +0000

    contact need cry sorry turndown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
