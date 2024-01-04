d5c5f3d3be84b58906323ab30c41a43d0b6fe4c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:13:32 +0000

    cover unusual contribute about tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
