549ce0684099b1c7ce3f54e66ae4218431251f24
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:10 +0000

    attention middle actually beat phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_volume(length, width, height):
-        return length * width * height
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
