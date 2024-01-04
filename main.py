6f6b5c2f79460b42a68f57f1c6d87af7c65450ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:54:28 +0000

    cut bringback soil admit clothes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
+def convert_to_octal(n):
+        return oct(n)
