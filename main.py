aa68eb963d3be7457af01a910525c1acf5f9d927
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:18:20 +0000

    discuss history store reply behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
