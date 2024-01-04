b9ba0efc7461bd231da61e03be3816049a67c685
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:02:18 +0000

    fair boring interview wrong patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def multiply_numbers(x, y):
-        return x * y
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
