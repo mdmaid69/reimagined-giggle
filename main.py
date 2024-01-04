b2595e7b7f24bd4a28f4c7104df403921ede35ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:56 +0000

    mix shut hour keepout organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
