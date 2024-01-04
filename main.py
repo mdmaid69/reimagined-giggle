c7591a15a3db5d71032f866aaeae60d8fd5d985f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:00:52 +0000

    wave frightened favourite myself drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
