6b4db07c7491722006d387808685c5118b93b433
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:14:01 +0000

    trip thankyou decide team will

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_power(work, time):
+        return work / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
