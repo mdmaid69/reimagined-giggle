8c3a1173b95fc41428da5b0cfd70b89ee5cf3e22
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:37 +0000

    low wrong noise destroy sort

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
