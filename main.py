ab9ce148f02cad3210f042e0b2d443570298c38a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:51:14 +0000

    tune considering lead along please

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
