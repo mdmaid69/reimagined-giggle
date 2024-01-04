a2fb7e839713cdcbbef407eae96e5215c9a3c986
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:15:33 +0000

    solve that heavy confusing division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
