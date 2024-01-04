7ff37a71c4316cbbd938546c23b2ef702d8b262e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:37:32 +0000

    choice furniture play music rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import numpy as np
+print(np.array([1, 2, 3]))
