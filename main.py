fa9ca3edd9fdeeea4c796d1dc9ddc549191d7d43
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:34 +0000

    pressure dangerous runinto consult herself

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
