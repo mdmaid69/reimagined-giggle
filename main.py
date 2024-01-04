e52a005fdcf95986b4b8dcc361378a31a7c3bd49
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:43:56 +0000

    bear heat terrible least dull

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+  def reverse_list(lst):
+        return lst[::-1]
