ccaf8d9e6277a68866a3d8d97ed4cad2b7dbc064
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:41:22 +0000

    joke run official frightened spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
