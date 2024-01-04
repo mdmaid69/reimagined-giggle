da467f7035cf5542239ea3fea493244d9c95af81
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:08:47 +0000

    standard solve giveout load check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def sort_numbers(numbers):
+        return sorted(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
