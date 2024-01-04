af324e5a11cea8d30e3718e656e1469e90b2f7d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:46:52 +0000

    stage computer temperature contrast turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import time
+print(time.time())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
