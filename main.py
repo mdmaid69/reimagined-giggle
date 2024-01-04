4e85663ae8986b1d71d5e632ffea04d2928e3142
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:58 +0000

    season force pollution second passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
