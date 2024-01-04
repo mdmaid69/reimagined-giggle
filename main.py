f115c9f995ecd31687434a7fc0c205a07343f314
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:17 +0000

    insist Tt involve have suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
