432b1b78a419d2c71e9a531d034ff2b34c7110f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:19:58 +0000

    frame greeting lunch burst believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_list(array):
-        return list(array)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
