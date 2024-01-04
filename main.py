9c55dcf6a6715104b402d3d49290b991dfced8d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:08:03 +0000

    necessary concentrate wash possible pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
