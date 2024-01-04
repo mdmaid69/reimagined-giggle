0a8e5456b02abdc6159cdbfee75a0b9cd3122173
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:16:06 +0000

    amount her takeaway meanwhile confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
