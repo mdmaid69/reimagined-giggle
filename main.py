503771ab2f88c7ee221d8293114d34805f82db62
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:19:44 +0000

    print back by tie change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_power(work, time):
+        return work / time
