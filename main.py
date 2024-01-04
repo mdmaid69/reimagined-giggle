16de138fffaf61db7c05bbd64a9fb8a4be376638
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:15:23 +0000

    considering now determined thin react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
