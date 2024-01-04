5aeb93eef714871d2f8cb16d762ee0dc61840304
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:21:19 +0000

    develop them soft represent base

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
