279eb1e59244007dcfc4e5b6eece5546b645e2a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:47:13 +0000

    cost male callback cross poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_tangent(x):
+        return math.tan(x)
