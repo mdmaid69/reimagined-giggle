68a14d8c914eaecccb2c08baad6a55b7dd090d52
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:36:15 +0000

    length soft price fast impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
