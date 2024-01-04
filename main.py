8a63aa38c0b4f11ce98a21e2c5916bbee28c29e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:56:56 +0000

    under rush angry first frightened

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def factorial(n):
-        if n == 0:
-        return 1
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
         else:
-        return n * factorial(n-1)
-  def sort_list(lst):
-        return sorted(lst)
+        return -1
+import datetime
+print(datetime.datetime.now())
