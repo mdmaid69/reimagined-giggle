971762b46a8acf434e3733fa005a22e2dbab0b36
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:05 +0000

    comeround situation course fall love

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
