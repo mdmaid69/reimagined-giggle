fd051e2750e4436ae964bf819036c77d7353cbde
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:09:56 +0000

    television month wall temperature young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
