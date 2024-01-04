0f5efc51904d0b73e67fc5bc89695446dacfd2d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:38 +0000

    history activity open start figure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
