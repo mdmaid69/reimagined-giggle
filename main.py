00c541dd1f3767d49e0dc78bea0bff6e43c75913
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:51:22 +0000

    in workupto bring turnover come

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
