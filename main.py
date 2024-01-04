8a0d2f596e45d27d8a26690b00bf5db9d009c81c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:06:59 +0000

    sting wash confused about reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
