724ee9158e99f45cb7b31cbc35e8e2c1e10ef23b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:13:29 +0000

    force to minimum loud value

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
