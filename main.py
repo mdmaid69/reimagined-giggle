b71ac685db72d3e52bb743ae630c3941e6096173
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:06:01 +0000

    minor hospital trap insist scream

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
