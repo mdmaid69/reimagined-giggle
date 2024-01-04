401af635e8ebe76c7d5cc2a907e74a0f36ff4052
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:56:44 +0000

    culture light deaf cope can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
