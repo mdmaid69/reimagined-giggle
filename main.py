1607189ab6b54efdbfd808ecaaf45ff44d319f29
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:48:06 +0000

    year label again concerning cover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_floor(x):
+        return math.floor(x)
