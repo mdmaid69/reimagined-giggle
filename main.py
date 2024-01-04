2f01a5fa881ece3e4cc370dea3afb734f80314b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:34 +0000

    write only deliberate relief practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_queue():
-        return collections.deque()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
