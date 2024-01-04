82012166bb155f2d9c061ec7567e457527dc04f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:46 +0000

    learn concern characteristic surface aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
