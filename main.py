676fec1bb05666433196cfe133326279d173f8a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:50:55 +0000

    recovery bringback equipment gather Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
