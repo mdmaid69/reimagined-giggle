fd37e3d5d31de3a9ba3d18e016e47156b04d371c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:12 +0000

    getaway ought record press would

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os
 def remove_directory(path):
         os.rmdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
