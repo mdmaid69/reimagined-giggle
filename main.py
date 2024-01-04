8fa32a70d963af3b1e88650ca5641d03e72593d0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:32 +0000

    connect raw variety trouble blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_current_directory():
-        return os.getcwd()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
