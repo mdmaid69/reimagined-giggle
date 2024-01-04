a62ebfb5dd1e0f8df6bb0516ca4b083c8fb39198
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:09:37 +0000

    know rate too platform rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def is_odd(n):
+        return n % 2 != 0
