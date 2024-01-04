717ba30d3aa638fc129ee385500cd9abfa3070c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:26:58 +0000

    calloff possible fashion embarrassing field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
