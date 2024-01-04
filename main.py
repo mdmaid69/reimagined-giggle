0ee65ded03ed39cb031b495b59025cab80158c52
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:13:02 +0000

    classroom charge cross sick guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
