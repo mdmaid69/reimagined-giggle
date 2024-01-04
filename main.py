ed2b299eaa57ad295edd5d4e3067dc8897ce03ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:16:40 +0000

    pride happy stop expression thing

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
