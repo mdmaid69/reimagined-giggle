6ca2a0622f232b03155566791f85350bf81c6072
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:47:28 +0000

    leg wash prepare callout object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print([x**2 for x in range(10)])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
