708a74496b629b7c11db4754eaeb61e18d086e35
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:40:17 +0000

    clear early brown Ss check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
