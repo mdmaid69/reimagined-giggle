7730f2a586603dc55eaaf317890f40646b38d50d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:57 +0000

    stage level lie light defeat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+print([x**2 for x in range(10)])
