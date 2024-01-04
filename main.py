9d7a01ed5e75d844fb2853fd69b8372f7c207247
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:05 +0000

    coat affect worth sweet by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
