696a98e5e5b3ba995e912829f28dca2537339deb
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:20:20 +0000

    than dish emphasis narrow score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
