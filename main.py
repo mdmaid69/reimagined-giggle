40d435f97eaef2e2f6e823543a6774ea0a6e3778
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:52 +0000

    threaten print height identify butter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def multiply_numbers(x, y):
-        return x * y
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
