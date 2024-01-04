b147d357da8babf7ad1da753e0c5a1d0fbed0b0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:10 +0000

    rough quick smooth switch but

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
