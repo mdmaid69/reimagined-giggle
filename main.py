9feb1f0926ba1ad7d4f86efd8a2d60bf5d8fda16
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:59:15 +0000

    from lie forget positive medium

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
