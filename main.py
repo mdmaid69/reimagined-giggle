f6b94b1a360de091f2b8be3147f69992beb23d40
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:42:45 +0000

    deaf do later off prefer

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def reverse_list(lst):
+        return lst[::-1]
+print([x**2 for x in range(10)])
