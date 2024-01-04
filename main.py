52546a71aaf82c34a85ca2632b2791ad6140ab35
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:46:27 +0000

    life guide software discovery afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
