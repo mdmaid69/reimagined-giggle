c741428d7d960018146808f96691a3d0afd830fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:05:37 +0000

    pretty combination detail ball Ee

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import time
+print(time.time())
