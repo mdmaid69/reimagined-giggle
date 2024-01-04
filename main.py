7a424a7cb6463d88f279a3f3e5c9763c5b966a2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:40 +0000

    or check bottom add detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_permutations(n, k):
+        return math.perm(n, k)
