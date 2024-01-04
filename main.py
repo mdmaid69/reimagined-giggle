784fd096ee24ae68f9169fcab1bf576dafb71a28
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:51:45 +0000

    front performance relax interested feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_set(array):
+        return set(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
