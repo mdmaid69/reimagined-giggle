7fb3b3d1467a0dc7fbdc85518b880178e5a2ce1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:00 +0000

    treat keepoff concentrate average deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
