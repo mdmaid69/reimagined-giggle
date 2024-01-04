3ae98330fbea32306f000fe230a1d8432a0b7801
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:19:32 +0000

    factory nonsense in black film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
