9ba143f3f95210c6ce67d1ac714d2d7d25bc9135
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:47:57 +0000

    male remove nonsense idea variety

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
