5499b41a63845eae7d3088496a1b4c1b0f19ae37
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:27:30 +0000

    connect guess react Kk suddenly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
