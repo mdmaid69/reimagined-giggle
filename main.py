fba696af2490e6f699bd5a3e40719a52b5dcd958
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:39:41 +0000

    front quantity everybody creature skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
