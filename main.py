aabb0c26701b0186ce1fcc258d8f151944b41dc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:40:24 +0000

    similar note church cheap figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
