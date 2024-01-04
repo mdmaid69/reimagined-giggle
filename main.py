22769b1ef6ad3628780ea801a75c741c1e093b9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:49:26 +0000

    564encourage season around raise government

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
