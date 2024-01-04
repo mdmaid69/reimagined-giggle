95f69831a2530f5fa3b5e4c0dc610c5266eefaf6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:01 +0000

    new young morning doubt model

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
