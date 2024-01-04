89e70f491b6be7d376669dd3792bee84e25200bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:47:50 +0000

    lie generally production underneath foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import datetime
+def get_today_date():
+        return datetime.date.today()
