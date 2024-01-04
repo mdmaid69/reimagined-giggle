08745c1f9ec286cc90f8c4774c3369faaf1cecd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:22:13 +0000

    runout emotion help when trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_work(force, distance):
+        return force * distance
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
