269b5f94bcd4bca6a657b5db3545664ac2337b59
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:17:02 +0000

    search without surprising class tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
