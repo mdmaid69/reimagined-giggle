141aecd19f92d2e6ebef1061eb9d02e0afd28590
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:40:35 +0000

    instant most neither nigga top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
