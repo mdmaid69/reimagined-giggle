391f20fbb57664b3e5927380dacdcd63148e45d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:48:14 +0000

    confidence attractive brother lesson opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
