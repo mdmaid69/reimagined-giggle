560ce45e1a7df9309177a7ee2f89f16d4e084743
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:03:27 +0000

    present peaceful goon walk follow

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
