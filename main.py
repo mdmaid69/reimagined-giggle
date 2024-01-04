2a06e80b1cd65d709364eebf5972591644fca72c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:52:16 +0000

    trip mysterious sick knock peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
