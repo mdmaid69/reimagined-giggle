23c7814e3abc23a8d3ba49933eae34df081f7c8e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:31:23 +0000

    play emotion broad whoever back

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_characters(sentence):
+        return len(sentence)
