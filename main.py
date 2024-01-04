11318a9192df41fcbd9ecbddd218fc9320c542fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:49 +0000

    yes hate holdon rely belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
