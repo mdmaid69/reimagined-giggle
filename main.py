e5e6ab608303cb9d05679bd5d99091c6b806e599
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:03:14 +0000

    soil beside out breathe thirsty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def cube_number(x):
+        return x**3
+import array
+def get_array_as_float(array):
+        return float(array[0])
