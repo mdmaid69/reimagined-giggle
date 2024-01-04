cbc0cd215690c984c41806d4038ec39ef31f2484
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:19:04 +0000

    before breakinto guess once content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
