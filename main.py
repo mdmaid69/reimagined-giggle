eb26f2b2c8c7ae301ba506ac3ccc435b14b05d82
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:23:15 +0000

    check unite moral throw godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
