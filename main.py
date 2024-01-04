d39a48910ad88cab9e0c8a81daa5fdfdd9383973
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:05:52 +0000

    cross satisfied talk name phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
