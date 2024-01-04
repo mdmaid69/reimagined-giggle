953a1ea24e38263614316a27cd77d4d8f0634a6f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:50 +0000

    back goup run connection bend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
