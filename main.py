bd9fba1a6b1f0915a866b694611d068b679d2e0c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:06:36 +0000

    ordinary careless term female unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
