c077a7f13ca8514865799f6f7b1134b3bef2f349
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:34:47 +0000

    workon imagine connected me strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
