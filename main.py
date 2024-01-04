1f0c3a5b62f7453ddc64a1ce5c7cc3a00e121a66
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:50:17 +0000

    call community cope ought edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
