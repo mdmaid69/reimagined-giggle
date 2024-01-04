6cd59ea857ffc871711814b062053ca782c6f9a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:27:05 +0000

    comfortable satisfied uncomfortable town water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
