c0ba99a15040d3b2da0a17a90bc66b15d902a570
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:38:13 +0000

    round thinkthrough angry tidy big

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
