c41f5c7604855d82fafbc1a1312e3df4e330d70a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:25:52 +0000

    introduction there push file argue

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_min(lst):
+        return min(lst)
