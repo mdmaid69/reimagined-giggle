52198544afd7e1186ac4d1f882c142fbc256dff4
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:46:02 +0000

    better hope discussion supply name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
