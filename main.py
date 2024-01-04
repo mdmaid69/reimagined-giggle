9f7030f2a72b0646739c7cab21e34d743ddb4da3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:55:11 +0000

    wet unlikely Ss gift whichever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
