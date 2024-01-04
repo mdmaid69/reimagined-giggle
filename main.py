4cc454778dbad0ec1ceb2882b64eebf2b58ec07d
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:52:31 +0000

    important offend belong fat breakin

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def convert_to_octal(n):
+        return oct(n)
