238ea955617da7292cf0104623e1a710ffaf0c47
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:16 +0000

    plan snow wear really decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
