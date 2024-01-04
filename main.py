5664e91fb37099140aae84ab80df4e4234e8f2f4
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:24:29 +0000

    holdout fly suckme 179bite realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_index(array, item):
+        return array.index(item)
