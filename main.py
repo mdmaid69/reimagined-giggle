96bd636dc5854781dcf862da0fdb4582f47d7751
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:39:31 +0000

    decide long tour 571enough confidence

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+name = "Python"
+print("Hello,", name)
