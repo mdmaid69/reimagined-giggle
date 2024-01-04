60bf1a859ad1f27ced19c8c5cd220dd4dc9575d7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:16:08 +0000

    wing lonely radio reaction avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
