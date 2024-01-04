8119512d23f8d9e784cd9318ce75c1e338079dfe
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:19:18 +0000

    disturb accordingto achievement thanks speech

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
