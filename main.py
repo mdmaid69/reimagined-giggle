b6295c62221c5ee36bcfdc81f9a444270a784c8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:51:59 +0000

    middle lesson best fasten back

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
