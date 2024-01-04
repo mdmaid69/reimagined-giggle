d0445f14ba3a635e85d206cfd261622e8abf11e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:35:10 +0000

    flavour bet point next offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_str(array):
+        return str(array)
+def count_words(sentence):
+        return len(sentence.split())
