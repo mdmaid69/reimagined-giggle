2892a5543f5527b9c666d543c136ffacfcb92f71
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:46:08 +0000

    creature ilovedicks mention ahead ill

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
