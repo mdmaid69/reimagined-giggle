2038dd98f914d72e5fd3453a43f4529b4ab3dd14
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:43:24 +0000

    importance bet crime trip sting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def get_current_working_directory():
+        return os.getcwd()
