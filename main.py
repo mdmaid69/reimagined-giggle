02a0bc69dd0a70e2ad1325e2bc405416d9ff1618
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:46:00 +0000

    keepoff callat cost depend food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
