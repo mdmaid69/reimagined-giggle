1d886afd2fc29377c65724335fe2976d8f611d07
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:58:59 +0000

    cry drink used lookaround world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
