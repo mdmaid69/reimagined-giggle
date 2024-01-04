7a17b6a7c6677007310826a9239077e61c95e6c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:09:03 +0000

    cruelty city represent calmly up

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+for i in range(10): print(i)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
