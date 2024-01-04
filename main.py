62235a4f7595d799d490deff9b0967baf81190a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:05 +0000

    exist accommodation away father laugh

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-name = "Python"
-print("Hello,", name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
