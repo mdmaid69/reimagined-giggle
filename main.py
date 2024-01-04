5130c965ba14ebc313fb6c69b01fa17018ed8dcc
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:17:01 +0000

    coffee disappear clean further wild

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import time
-def get_time_since_epoch():
-        return time.time()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
