cc39de09d2354e7d1779afb0af692aa439b5588c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:10:56 +0000

    detail satisfied stranger suddenly beneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
