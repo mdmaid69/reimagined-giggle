3c4b018b48cda97390b387d778c0b425b1267eef
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:13:41 +0000

    forgive project talkback finish theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import getpass
-def get_username():
-        return getpass.getuser()
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
