299d9974bc6a387262128a8da9dedfd04b77cadf
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:43:36 +0000

    whisper comein drink punish wind

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
