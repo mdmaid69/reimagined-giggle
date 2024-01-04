cf9a1f28994a3666bf53d802c2981c7103a6a332
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:09:40 +0000

    harm hard formal passage wide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_speed(distance, time):
-        return distance / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
