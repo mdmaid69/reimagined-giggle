2ebbb7d50179022c9907daa1c33792e195555be3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:52:08 +0000

    recently type possibly although lay

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+print(sys.version)
