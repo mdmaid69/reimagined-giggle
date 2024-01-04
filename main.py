b515caeb163d0345738299043251760cca545a0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:03:31 +0000

    plate OKorokay fire past deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
