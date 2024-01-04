2484aebdf8e85dd0363364dce92a0fec6cd5b33f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:49:12 +0000

    confuse several display stranger comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
