a095d054ff1db325a20b25ce1cdd1d17bba1983f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:52:02 +0000

    without strange want 569enjoy steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def multiply_numbers(x, y):
+        return x * y
