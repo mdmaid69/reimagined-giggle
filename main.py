b6ed58ff76f5c3fe4e46c345d81c9e7be8294b06
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:01:54 +0000

    reach machine fork jump burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_set(array):
+        return set(array)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
