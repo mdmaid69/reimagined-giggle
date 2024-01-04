0345565442f144fa0a44e67e136873dd82abe584
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:27:12 +0000

    every watch before Rr interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
