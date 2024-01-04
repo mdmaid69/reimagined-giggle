c11127c333302848f1a538024ac278f33b9a4896
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:14:44 +0000

    suggestion 567enemy rarely shape success

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def find_min(numbers):
-        return min(numbers)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_palindrome(s):
+        return s == s[::-1]
