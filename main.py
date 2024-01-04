2a283f2e2243cae3908549d7ff11512c51402a72
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:39:56 +0000

    trial niggaacademy turnover group discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
