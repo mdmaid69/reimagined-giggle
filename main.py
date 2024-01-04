260cf53f4923de3cf07a75a8c0c884cca454da8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:06 +0000

    hospital used thing food mend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+name = "Python"
+print("Hello,", name)
