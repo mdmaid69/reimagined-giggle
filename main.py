1c9fa03161d538dc83c79e072276e07f947be4d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:42:48 +0000

    subject weigh eye better bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
