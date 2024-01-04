470a9610741a583da63a80f324c0542c8b68ce31
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:49:32 +0000

    pair lonely stand visit moral

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
