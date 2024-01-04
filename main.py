47eaed310b12489607fa620d3abe4652918ed8e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:41:40 +0000

    build success beat guilty debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
