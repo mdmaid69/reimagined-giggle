94268889be8d28f64e1524a74b634da276fdf713
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:52:56 +0000

    promise suppose roof conclusion handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
