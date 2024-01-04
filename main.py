1bbed4d56cc953eba138a240cdb35ec0cb7cd587
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:25:42 +0000

    evidence describe contain cough or

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
