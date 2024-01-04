ff790d54ed3a3eff0cc375ef296adb0080eea6da
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:43:38 +0000

    bang hear scream separate beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
