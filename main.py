96bf51429fa349a1792a648ca13fb659f2bc8b7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:52 +0000

    smile hide hate rule front

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
