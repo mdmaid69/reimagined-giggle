feb83e34795797f126cc92203feaef6b055b565a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:23:53 +0000

    butter print workon preparation keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
