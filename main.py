0c090fe403b1dfd20864e9aa70f959e669ccb445
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:28:49 +0000

    welcome convincing tape wise before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
