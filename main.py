89cd9eab5cd6186e4473f4feeaa9ed5d0f51b029
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:24:26 +0000

    mind middle flavour exercise giveback

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
