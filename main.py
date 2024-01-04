4168feebb8eb50983c6b618395544e244228ea3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:21 +0000

    reply light image same amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_speed(distance, time):
+        return distance / time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
