97508dc60ee5b543290ac20ca558ae03fc46309b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:23:07 +0000

    other doubt lead past describe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
