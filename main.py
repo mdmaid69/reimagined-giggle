099816f468b34b235823f4d20f6a2c805f58a6f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:41:15 +0000

    cry roll makeinto correct mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
+  def remove_duplicates(lst):
+        return list(set(lst))
