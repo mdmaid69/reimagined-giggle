537bc6a1b34ff0c505a5ac71fdbfb26afdee1d1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:24:20 +0000

    save cheap forward add recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item(array, i):
+        return array[i]
