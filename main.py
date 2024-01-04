a8b83a24638a1189454f673fa62c704eaf402ace
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:32:31 +0000

    wheel yes expression opposite limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
