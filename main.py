a25a401d8029dbddf6c55b86e75e114c56919b00
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:06 +0000

    excuse deal dish fast calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
