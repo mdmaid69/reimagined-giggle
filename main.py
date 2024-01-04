2f6e5567ffc25f61a9f067585dc1838b11a953ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:11 +0000

    crime promise today punish excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
