23f2fb8ea94b152d0991308d893fe3a9f507c124
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:39:05 +0000

    several unpleasant collection door otherwise

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
