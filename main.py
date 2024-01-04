897179f9f264411a0bea2a5b2326118abe82c92f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:54:40 +0000

    mistake science grateful match education

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
