6e2ccd709c5a3e17121d7abe1755272c5aedb5d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:15 +0000

    geton introduce bridge fill comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
