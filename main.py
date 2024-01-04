4fe1e3009cc84987ff099a2247dcbb4efb03697c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:12 +0000

    start arrival godown narrow accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
