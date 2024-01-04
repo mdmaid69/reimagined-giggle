53fcc2de57bfefdd92e27e60c829c0d541054326
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:38:02 +0000

    object table union weekend cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
