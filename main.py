e00f1b599a24cfecafd8c50e3bbab13c43adbf2a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:33 +0000

    worth him announce they decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+import os
+def remove_directory(path):
+        os.rmdir(path)
