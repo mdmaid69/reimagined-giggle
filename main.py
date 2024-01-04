904cd5c70498851125dbd80d5ecf527078ceb4f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:07:20 +0000

    Ss waste unlikely basic sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
