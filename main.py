ddf8fbd40579649b7e56bb927be19f30582bd9c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:04 +0000

    determined someone throw either foot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-print(sum(range(10)))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
