0fc2834abf7bc95d4678bfc19a1196ac4708e212
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:39:34 +0000

    straight case sure behaviour may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
