b34b20580b63b2227ffa025112c6bfaa7bf4d287
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:03:39 +0000

    furniture shadow duty congratulations afford

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print(sum(range(10)))
