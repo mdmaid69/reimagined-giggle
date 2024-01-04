c19ec215e9c53e3b615e26e352269b9be978adae
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:14:48 +0000

    ill OKorokay single term ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
