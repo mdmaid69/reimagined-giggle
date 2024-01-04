888fd5c9441b4185f436711c828044991bd7be87
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:17:34 +0000

    nor score statement history relationship

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
