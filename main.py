ba1df2cc86b0f73961a591cd50badefc9563ae81
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:11:39 +0000

    shoot geton report tidy touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
