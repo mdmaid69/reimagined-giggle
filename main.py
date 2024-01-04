f659c57b908d2d305e2f7a84bd0b7bf55f4f34d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:33:33 +0000

    plan rain he the service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
