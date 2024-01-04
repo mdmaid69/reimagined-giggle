cb6107d2b80490a3ad3b8344f1885dda84d31d74
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:08:25 +0000

    adjective scheme square debt politics

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
