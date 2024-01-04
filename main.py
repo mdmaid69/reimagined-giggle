21c15fdb173c34c0c6796e22971dfc04a091661a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:13 +0000

    cool group small common in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
