8841a9707a43ac383acb5aa46d549374e93992a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:58:13 +0000

    national prepare persuade fun government

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
