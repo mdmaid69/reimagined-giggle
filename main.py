8f47d2a90091bb0926e259c803f4b39c0b8f8e83
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:49:39 +0000

    walk similar quarter certain exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
