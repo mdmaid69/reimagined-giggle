f590f4ee540e9470754eae4c476adfa51acb9f40
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:51 +0000

    fear argue fall wait machine

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
