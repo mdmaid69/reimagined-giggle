2a23c0640bede6ae52fd3a4391873b5b45a36327
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:53:48 +0000

    object Uu reject police actually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
