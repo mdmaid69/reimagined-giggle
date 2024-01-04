4cc29900d5326ce7aa1c81722062b289aaea041a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:30:20 +0000

    afterwards body division whatever capital

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import collections
-def create_queue():
-        return collections.deque()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
