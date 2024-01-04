75a34fdc3f42123bc1e74fb5da5d90aa0684fb51
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:51:57 +0000

    goout comedown there support shine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
