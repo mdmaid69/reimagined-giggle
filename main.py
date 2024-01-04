e6a485f04c153a8b5a592ebc85a76a1f0fa702c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:53:30 +0000

    disturbing callup proof ahead mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
