c3be045ca7693cd72502c02ab2fa79714d1dca0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:28 +0000

    help or seem breathe dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
