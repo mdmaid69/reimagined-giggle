7d71b1a64e84da5e04c50a56f1458877e04e17b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:28:04 +0000

    strike term nearly increase 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
