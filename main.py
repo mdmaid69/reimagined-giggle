b24faece02a6b5c50c6ca3fc9b1f3702fcf1fea4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:43 +0000

    somewhere school defeat passage illness

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
