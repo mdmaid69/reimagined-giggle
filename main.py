9dd2b19de6e545d3e3533e3059f780b23bfae284
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:25:01 +0000

    priority container label 179bite it

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import getpass
+def get_username():
+        return getpass.getuser()
