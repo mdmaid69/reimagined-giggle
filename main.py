8e0bb6c5a6819b64dc1d53669f963eeb298b14c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:47:33 +0000

    interview cope objection complete sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
