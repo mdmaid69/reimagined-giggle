e2953a1a5b05595907fdf12ad4b38b6d6f3dd3c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:05 +0000

    circle attractive size Ww evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
