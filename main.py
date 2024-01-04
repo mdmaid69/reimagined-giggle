cbd6e5d43666a35a7db302b7b8ba08f241b39168
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:46 +0000

    tune form admit habit decision

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
