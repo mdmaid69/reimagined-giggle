8069318c4839933f658eed06fba3e705c8e31201
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:00 +0000

    activity cold key hungry because

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
