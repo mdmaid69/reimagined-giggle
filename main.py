62c0c1736ab2df2eb54399f7a41ba9ddea29cbfc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:11:34 +0000

    dull suggestion eager wherever game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
