4f87839855fba5c452ac1d5bf08d2bfdc6fc688d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:10:04 +0000

    freedom chief debt runout name

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
