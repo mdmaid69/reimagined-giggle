51a9fbaf47b0536adcc17a596b125b07c4c263fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:50:39 +0000

    back guess by sister phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
