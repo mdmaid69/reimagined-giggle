b1d717b740658224acfc1a56da7bd4126c972dd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:52:51 +0000

    young downstairs juice reach kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
