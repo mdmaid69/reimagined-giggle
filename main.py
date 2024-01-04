3c6ff85109beaab191ac7cbc8b833a564c94ace8
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:16:00 +0000

    thought lock climb distance long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
