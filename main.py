f3a739d2dbc7c8874605f520a2f9bcd40bd8c16e
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:25:27 +0000

    shock respect basic charge future

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+print(os.getcwd())
