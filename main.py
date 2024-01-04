0ede4480114ed11368e58628e0ba6f54dc61e482
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:50:02 +0000

    light write miss child pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tensorflow as tf
+print(tf.__version__)
