1c257271edd62ce67a627d5c1a3ac4c16e614213
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:18 +0000

    she bit bringback combine by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
