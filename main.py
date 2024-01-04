9e2197206463a140a383d88d9511a600774faf85
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:27:22 +0000

    comment broken work custom trap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
