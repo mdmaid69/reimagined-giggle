6c7aad28b40a45b986cf1bb86802694d40830f48
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:47:10 +0000

    drive before undo fill waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
