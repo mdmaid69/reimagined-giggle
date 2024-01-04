1e734ea291bd030c75ccb9f313395df1bb0ce5eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:20:08 +0000

    cheap split question division knowledge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
