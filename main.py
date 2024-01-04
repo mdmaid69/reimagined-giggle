7baad854f00ae9721884e66bf442d0e68c0fb966
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:51:13 +0000

    pupil among muscle here spare

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
