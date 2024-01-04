f04089b75fe9b5a76238b29a607c38e8417a0f3a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:02:59 +0000

    matter Gg once love through

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
