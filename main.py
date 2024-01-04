dcce62e902c5970d82c7387f5d951ad152796684
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:35 +0000

    study really information criticize learn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
