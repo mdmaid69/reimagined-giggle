c9a5ba0a191b42f6b81ef2cd75cc5948fe80a0aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:03:53 +0000

    notice formal positive less knowledge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_hex(n):
+        return hex(n)
+def square_number(x):
+        return x**2
