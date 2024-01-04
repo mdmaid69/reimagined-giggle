0f96e06c1ef258b287841db7f6750027f8704275
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:08:09 +0000

    over mass giveout public like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
