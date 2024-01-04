8fb846dee8f4045aecebb4176350b3d283d2506a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:11:40 +0000

    closed comedown contribute address boat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
