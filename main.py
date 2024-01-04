4dc710e338059e13acb064c0b88d33790e345642
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:02:40 +0000

    see occasional introduction somewhere Nn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def reverse_string(s):
+        return s[::-1]
