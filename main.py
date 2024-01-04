2f102d9a1f518a4d37ac66fe940d4e25760d16a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:12:04 +0000

    secret bath use discover possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
