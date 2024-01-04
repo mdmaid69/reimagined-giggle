35d4d29d88807ded952c4adc2bdbb26aa18d3907
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:16 +0000

    fence fair occur dream responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
