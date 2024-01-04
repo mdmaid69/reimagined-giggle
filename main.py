81d17b551ad442b163587f8510c12a23313d9573
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:19:06 +0000

    why thirsty match back cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
