7f9bd87b954955aa66162838690a9ab73f5531d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:46:10 +0000

    sentence gap process sign comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_length(array):
+        return len(array)
