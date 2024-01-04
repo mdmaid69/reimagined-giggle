d3470a34b9c21cd3c718eff6f02d0976ad9b9270
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:11:54 +0000

    address present bite lock some

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
