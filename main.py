5f678e00b5cfa01ef04a3657cefadc18063acf99
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:26:28 +0000

    sort spot mouth thick consult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_speed(distance, time):
+        return distance / time
