28fadbb998eb6c5ee2141f610d89bedfc10a85ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:21 +0000

    mistake inquire among lonely accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
