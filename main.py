0b40fabc108dbfb93f118f1a56df8aa8fe0d7631
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:14 +0000

    ashamed try finish manage delight

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
