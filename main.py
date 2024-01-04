02ade579a7bea6d072981d28db7646d552f2aa81
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:48:55 +0000

    black either innocent finally body

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
