8b59b2dd2d1c81bc26b2c60cdc1608c9235885f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:21:30 +0000

    tax shoulder switch wise make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
