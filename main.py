2d2c62e053cf109394b4dc21404c57a904b168af
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:55:21 +0000

    conclusion shoulder mix thought yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
