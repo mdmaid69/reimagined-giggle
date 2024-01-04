c5d373988d6602dfbc44a8a45000ccd2f1491d32
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:45:44 +0000

    live benefit whether me tonight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
