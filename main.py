5fd3530bc7833bbf92a3e824805c268eb8456635
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:17:26 +0000

    land quality pride slice responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
