afd843bbcdb892f84313d21c9dcc72fb82c10e3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:50 +0000

    mess case load mean I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
