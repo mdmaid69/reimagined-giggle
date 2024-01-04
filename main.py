59ec86563b171fc496fe7ec4ded798a4efa19c5c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:53:17 +0000

    569enjoy exploration prevent wealth onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
