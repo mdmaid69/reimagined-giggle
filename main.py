472d35853fec66374f29f42d4ed77815a29c7cd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:09 +0000

    somebody path project pity play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
