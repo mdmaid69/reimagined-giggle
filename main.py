f05f930150124cc78a44787d9f73b3fc29f20460
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:15:13 +0000

    getout beautiful 570enjoyable taste OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
