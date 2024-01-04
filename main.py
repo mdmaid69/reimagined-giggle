143f7d843ac4c9c15c6e96359331471ec72a2cc8
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:17 +0000

    type present gooff past out

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import platform
-def get_os_info():
-        return platform.uname()
+def find_min(lst):
+        return min(lst)
+import time
+def get_current_time():
+        return time.time()
