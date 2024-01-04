99308a8623af5859db1f331588f1d6ed4d8f7001
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:04 +0000

    sound confuse organization drink anything

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
