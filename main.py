3117509086dc2db0796d09606c5d40b96b2777f8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:25:09 +0000

    herself shock that treatment notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
