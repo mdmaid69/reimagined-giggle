18001a06887a7c75c42ea02572aa115483d7d2bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:49:48 +0000

    fat walk accordingto wrap achievement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
