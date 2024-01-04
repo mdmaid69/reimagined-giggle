e6508f57062be5102973e713dc89cc70fdfeece9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:56 +0000

    union offence contain OKorokay pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
