179c0e7b3cb80f22b5451106a546c9768396a2e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:11:00 +0000

    male produce cost anywayanyhow breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
