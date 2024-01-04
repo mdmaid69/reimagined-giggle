df5d9c116500fd4d61668b5e082c230c827b3826
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:46 +0000

    why store cost cum shall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def reverse_array(array):
+        array.reverse()
