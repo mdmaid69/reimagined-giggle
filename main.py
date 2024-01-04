75cb2e65e8522bb9345e31321c9002c7d482a506
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:38 +0000

    else outside hour ordinary name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def convert_to_hex(n):
-        return hex(n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
