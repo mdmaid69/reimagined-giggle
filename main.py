51c8fa1ff77f264d68fcd3ce1e485dd590a59e73
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:48:09 +0000

    appeal rarely ticket cruelty give

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
