6fd907813ac57f3804ddcb482ded8a0a56ce8df2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:07:31 +0000

    name yesterday limit comedown Rr

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
