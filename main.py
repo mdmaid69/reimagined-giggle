b066f982754bab067c0f122792b116c56902e2e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:11 +0000

    bath plenty comein lie tragedy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
