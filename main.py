b8b9006d93ff0f879f3434ca736e4b3fae04b9da
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:29:47 +0000

    steady minor freedom impression swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
