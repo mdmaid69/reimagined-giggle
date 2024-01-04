e704b7669731ab32aa156af92b664f9590fdb87d
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:20 +0000

    similar plan major effect fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
