7c7ba0fb98f20f5b8adf8f597694e2e7f641ee99
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:11:08 +0000

    proud limit occasion imagine glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
