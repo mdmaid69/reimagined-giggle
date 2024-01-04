f8019cef520cf329e8c86c78b6746297a4f511d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:26 +0000

    variety trouble doubt slide splash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
