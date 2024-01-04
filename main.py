7982af1f09789ef31fdb5604841a5e98e7215d0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:40:31 +0000

    anxious operate shop sleep 567enemy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
