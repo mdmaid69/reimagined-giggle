c39d24e04e0457b5e274e921d2732345e2f8c659
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:36 +0000

    effect confusing easy man pile

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
