d3ee0a6713d9b2d3a5a756dd7437581505a76c86
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:31:22 +0000

    owe any kick war furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
