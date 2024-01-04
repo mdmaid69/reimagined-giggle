8f627edff727835862121bfda6ecf9d14299dedb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:16:52 +0000

    so field target emergency conclusion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import sys
-print(sys.version)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
