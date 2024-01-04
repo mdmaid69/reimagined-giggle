3a8f4eb95df4b79ad0b9eabb22c706f00778e914
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:23:37 +0000

    face save issue off dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
