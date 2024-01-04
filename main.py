c7372f3d6cdd106c71c75ad8a16949a3b84109ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:01:08 +0000

    profit Ss nonsense anywhere cool

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
