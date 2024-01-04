53f56437f6e5bc7127ffb9c3288178546b4ae2d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:28 +0000

    concerning practise miss scrap partner

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def reverse_array(array):
+        array.reverse()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
