3c4e8aca548add6dadf5c86ca1ea42af9c2f445f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:42:20 +0000

    shelter answer value who communication

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
