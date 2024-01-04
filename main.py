f88e374d5cdf42cb59cc4ad65e6a8ba3611c8361
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:27 +0000

    frequent body dress hard emergency

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
