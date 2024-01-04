94e4b7bbd788c38a28dabdf6b2f4a34fd1a42bea
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:37:58 +0000

    best disagree better bound taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+print(re.match("h.*o", "hello world"))
+def reverse_string(s):
+        return s[::-1]
