f9a0b777aa4ba349b037d2becb8e1d5c81c1d9fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:30:39 +0000

    breakup desperate shame stretch walk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
