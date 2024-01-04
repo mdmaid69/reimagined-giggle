aa8fd4623c5a3fd015291b906ed4a827541eff01
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:15:56 +0000

    after limit flow complete Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_current_working_directory():
+        return os.getcwd()
