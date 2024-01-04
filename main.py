be534a98950bab74f0c8c88eaff3cbc912e06a99
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:27 +0000

    bus female exciting how getback

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
