eb875dab5559a73b706e25d2c096547c0b0f88b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:10:43 +0000

    shape apologizeapologise makeup command both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
