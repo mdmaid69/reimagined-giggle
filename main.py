f1cd0f0d1d71dab676b6dafa17475cabb772f536
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:05:23 +0000

    then giveup evidence release year

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-name = "Python"
-print("Hello,", name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
