25111bb5d21bca95688bf078edf04f0eb12b1a2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:06 +0000

    act happy goodbye sort pass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
