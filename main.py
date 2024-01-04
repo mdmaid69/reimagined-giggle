54f2fc1520f89aa1c353f09b9fea6e7d5d61dff4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:09:09 +0000

    production development importance produce allow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
