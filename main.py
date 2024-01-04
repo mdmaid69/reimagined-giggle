48c5e17e12e0a8b6860e5944b16d0e51285e6408
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:25:34 +0000

    callup rush official soon concentration

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-print(sys.version)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
