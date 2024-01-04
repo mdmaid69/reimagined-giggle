4294bdd4cbd0390f958848060ff18adfbec7f45c
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:53:00 +0000

    eager appeal spot object sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
