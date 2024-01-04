8613d9b31dd3a93dd2da761c567c7aa1902bee41
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:54:23 +0000

    smoke do interest offer this

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
