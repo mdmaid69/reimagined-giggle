863df134f662665c767632b672f224d51dca406d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:05 +0000

    plan true new form out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
