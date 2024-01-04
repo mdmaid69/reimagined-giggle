a5c918066facd7536b79600523c0888e364c7d44
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:27 +0000

    lend shop balance yes callback

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
