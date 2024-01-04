bbd01a771b15b1c86a5f0fbe2fccc5fb7757653c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:51:49 +0000

    mother space small land cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
