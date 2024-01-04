205e513d08605a5eacb52213159028120cc3dda6
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:13:21 +0000

    thirsty tell murder cover sleep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_min(numbers):
+        return min(numbers)
