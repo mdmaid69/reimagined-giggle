f25ab142e0ceb3356f5cedbf67aa516286ea601b
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:26 +0000

    rush human anywhere pipe identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
