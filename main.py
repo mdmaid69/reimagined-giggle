a3614b3cd184e7c0ed2eae407aea81cc06ee6a7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:57:43 +0000

    temper transport bad floor for

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def reverse_list(lst):
+        return lst[::-1]
