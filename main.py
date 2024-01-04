66e479695a332664d38dcb1423d245b5113f92e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:15:53 +0000

    size cut female path different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
