017487ab9860b3d4ecc617224da428679eff52c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:12 +0000

    Tt star evidence house pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-def print_python_version():
-        return sys.version
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
