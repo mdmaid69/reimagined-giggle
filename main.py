02ed26da3bbbfe66ae6fa7816322ffa4dd490a23
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:12:59 +0000

    possession silent introduce distance spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
