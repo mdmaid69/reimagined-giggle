f42a24f122e9ac3a68910019ccdb85013e0200db
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:18:34 +0000

    bringforward seem month tough die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
