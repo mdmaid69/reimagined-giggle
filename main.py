1080d8be8547d7ad6671df344eb2c4b17e2891cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:52:40 +0000

    admit bath agreement finger single

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-for i in range(5):
-        print(i)
+print([x**2 for x in range(10)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
