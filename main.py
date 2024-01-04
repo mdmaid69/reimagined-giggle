e3286bd02d19bcc0874a0275e35fa4b5b7281671
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:09:49 +0000

    doctor transparent pile sign that

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
