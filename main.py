48e628315a89f3bff1c959c68f95ba96497b106f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:15:50 +0000

    physical public reaction terrible play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-print(math.pi)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
