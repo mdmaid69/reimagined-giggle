b1d37ef5fefcdfaf45c2539c61980355efb7b838
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:03:27 +0000

    Vv surprising visit pair mystery

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
