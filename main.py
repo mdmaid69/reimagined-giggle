79980f29fe2fd3f2a5f3f44ef07a9bea637a612b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:24 +0000

    business point recover attach under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def divide_numbers(x, y):
-        return x / y
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
