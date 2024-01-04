4c716bdd53d1a6ef8e713e7db1f1e9940a3ac834
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:50 +0000

    ago sticky fetch 179bite noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
