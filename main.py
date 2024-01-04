bc9053ee2c4f951791405693bb81bc9cca8a57fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:42 +0000

    below clear drawing pool confidence

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
+def factorial(n):
+        if n == 0:
+        return 1
         else:
-        return -1
+        return n * factorial(n-1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
