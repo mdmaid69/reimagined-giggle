6e59c23a9a32721de1af5093ef60aa5bc25a95aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:44:36 +0000

    criticize long shame available completely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
