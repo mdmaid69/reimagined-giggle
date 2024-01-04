bef87404530b9364155a75fe90a10299c1205e13
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:04 +0000

    right lonely wide hurt criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def calculate_area_circle(r):
+        return 3.14 * r**2
