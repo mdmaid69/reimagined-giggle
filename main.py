d9deec5b02115cfd80eb447c605a8669ed7453c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:56:02 +0000

    various Ii wish lot object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def find_min(numbers):
-        return min(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
