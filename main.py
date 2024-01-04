3c716e321b3d49423f3025c19b8332b991fb2982
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:51 +0000

    squash family cook oil invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
