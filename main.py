1fb9c48589e2dc79757320f24736ca50b75c7b93
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:58:54 +0000

    spill worry dull thinkthrough hold

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
