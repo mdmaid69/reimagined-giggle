fadb5dd8e84caa63fb22b1c802f2322088cee946
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:37:18 +0000

    either benefit stretch send in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
