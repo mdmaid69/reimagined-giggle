ae7c207f1119c15a4ca35da231737979f53591d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:09:50 +0000

    me bus your Vv dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
