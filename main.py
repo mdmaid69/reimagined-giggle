61dcc0650efb478b165c86f1c239f1c4da5a9659
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:40:17 +0000

    unpleasant platform flow possible adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
