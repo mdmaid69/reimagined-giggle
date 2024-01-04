8e1dbc0200cd25cef983f7228fe709f8c5e7763d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:30:47 +0000

    turnup tool gradually my flood

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_stack():
+        return collections.deque()
