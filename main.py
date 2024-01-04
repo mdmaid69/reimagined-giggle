aae2515e90f7fadddfce7f0585613b87ce074f0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:03 +0000

    government part project face roll

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
