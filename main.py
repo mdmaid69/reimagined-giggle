9b2e12e2124ede110d0fceb903b2d4416bb26832
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:41 +0000

    565end chase less identify ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
