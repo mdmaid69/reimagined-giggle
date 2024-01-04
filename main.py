eb68bd228965ba07f8d36fbf78df7189a7eac6a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:54:45 +0000

    getout temper everywhere maybe straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
