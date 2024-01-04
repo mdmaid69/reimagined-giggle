c505f677bfdcab558f858959e84dbac5bae2b9be
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:31:52 +0000

    visit reason trip pattern yesterday

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
