e00b59704f7d7dfdbf3c7111f1bf9acf7adca83a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:44:10 +0000

    team answer straight spin heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
