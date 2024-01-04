08a0c5ab53fa949cd98a7192065e8824cc3fb3e0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:03:42 +0000

    wish fill anywayanyhow tonight near

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
+def calculate_acceleration(speed, time):
+        return speed / time
