32229e71928958e5765dc44ee73267486a6090db
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:53 +0000

    somehow imagination tend notice meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
