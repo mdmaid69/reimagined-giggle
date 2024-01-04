4e1e30733a6a3277b5cc97f45cf87bfa13d4ee4a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:09 +0000

    concentrate tragedy seriously workon breakfast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def cube_number(x):
+        return x**3
