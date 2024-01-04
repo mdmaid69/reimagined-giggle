cfd06937e40cfd5896a8a563262a0f56a5bf80dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:28 +0000

    there trouble surprised angry shelter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
