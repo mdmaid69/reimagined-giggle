2e19859074ef8bbec43ce870be505858b54b6c8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:34 +0000

    snow 570enjoyable maintain goup take

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def add_numbers(a, b):
+        return a + b
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
