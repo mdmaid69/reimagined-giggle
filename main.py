98e8c3112446436bd64b62f6444f69727e475aec
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:35:26 +0000

    gentle convinced help level skill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
