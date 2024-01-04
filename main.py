2aeafe78fef5cb2e920f30cea9168ad7f50aa7e7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:41:59 +0000

    cross little 566end comeup smart

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
