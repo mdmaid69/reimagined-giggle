7ed3a0e5d86ca8c7ec0d7ad62f618d0bff99859a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:28:35 +0000

    aim variety degree motion Oo

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
