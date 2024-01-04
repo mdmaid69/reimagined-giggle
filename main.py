13a83a4f5637a5269fd4e1c0679891a54f3be462
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:09:42 +0000

    pure solve direction concentration depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
