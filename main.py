c177c9742519d21a24af2b33ba2bba2720a38b7e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:06 +0000

    inside attach natural invite worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
