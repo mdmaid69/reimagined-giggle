5ebcfcc08e816cf5df2c17fc0ba46b385d1bd944
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:20 +0000

    second very exchange control anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
