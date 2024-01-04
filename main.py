dd40e938ccb03215e7713d405fea0b8e636f65f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:34:23 +0000

    relief himself affect makeinto alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
