8c7b6dec008b02a1d33515cd0aa3dd8353c14017
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:15:19 +0000

    man book drop detail crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
