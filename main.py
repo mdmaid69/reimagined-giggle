14746fe9ac7d714e3a738faeac9aed04be900c22
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:18 +0000

    extremely precise rid open moment

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
