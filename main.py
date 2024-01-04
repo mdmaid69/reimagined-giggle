71aaa5d63c856ab5ec3237d699d941ba60c4bee3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:04:14 +0000

    boat copy breath makeinto case

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+print(sum(range(10)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
