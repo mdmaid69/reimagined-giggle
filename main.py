009f6d5648f1952e1158d22a22e31fd4b306dd5a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:53:11 +0000

    present it sick acceptable range

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def subtract_numbers(x, y):
+        return x - y
