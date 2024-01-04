b6f2af3f932c238ba2b8137c79fb63117b84237f
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:16:49 +0000

    occasional mend next sport rely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_current_directory():
+        return os.getcwd()
