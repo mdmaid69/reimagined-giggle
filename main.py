4daf84f3e5d5bc3fcf0c579fa99d68eca0702c51
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:12:48 +0000

    pump measure ours sensitive power

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+  def square_number(x):
+        return x**2
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def get_array_item_count(array, item):
+        return array.count(item)
