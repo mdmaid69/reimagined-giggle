acf24d4097f655fa4c0356394d3938c8e05aa417
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:26:29 +0000

    deliver imagine summer about last

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def is_odd(n):
-        return n % 2 != 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
