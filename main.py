2ff382007418d86d1eebbc1fcee97d2fc2d3302a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:42 +0000

    pour book comefrom glass throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
