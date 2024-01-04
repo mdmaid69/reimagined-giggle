13947929f9ae13be0e4cd57712be1481857bb959
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:59 +0000

    importance sensible secret his answer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
