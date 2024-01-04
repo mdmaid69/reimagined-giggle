42ce62a32bb0ea25f73ca28cdae14b70d5ab8a95
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:19:54 +0000

    such meanwhile off confidence fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_as_set(array):
+        return set(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
