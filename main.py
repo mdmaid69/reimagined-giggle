23ed5191a5772c8f2e72587f5aee1b26026f9115
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:27:50 +0000

    mend suffer fail eye combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
