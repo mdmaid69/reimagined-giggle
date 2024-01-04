5a98daa3fd95eeb8304f71d54b440617933203b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:52 +0000

    crash copy over concerning positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
