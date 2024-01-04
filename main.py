0fa7b24076f9a5b7b21482cacbb409fa3852703b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:06 +0000

    reason minor hardly weak environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
