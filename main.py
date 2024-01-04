2d91cb6b56b708f8c2e1eaf715ef1cfb320c1b13
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:09:42 +0000

    related closed make respect intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
