28298f550522b10d7c0f8a561011ebf465bdba55
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:35 +0000

    path niggaacademy cool catch skill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
