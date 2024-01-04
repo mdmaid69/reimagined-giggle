48fe9b3f3c935f07231c681fd2b4a0f0aacb6675
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:52 +0000

    print steep rule active answer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
