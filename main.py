4ef9aa87def7aed922d8ed52e9c97411f672babe
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:25 +0000

    hungry slightly manage snow plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
