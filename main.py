31b7bd9798ce1150c5ca75802298444a09a2ac01
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:05:07 +0000

    uponon wave cause wash attract

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import random
 def generate_random_number(start, end):
         return random.randint(start, end)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
