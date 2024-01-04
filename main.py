29a238cd7582f64dd810accceb921bd42eda8278
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:07 +0000

    early receive uponon style back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def reverse_array(array):
+        array.reverse()
