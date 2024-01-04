58ef049442e191b010cdacfb0f9d96e7620bc026
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:57:47 +0000

    punishment face refuse interview tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
