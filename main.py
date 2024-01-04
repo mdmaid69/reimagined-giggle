1253c682573747d458a46a3fade7caab4a48821e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:26 +0000

    run several relative perhaps level

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
