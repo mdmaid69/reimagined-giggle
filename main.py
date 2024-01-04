e1f144611a14830c728dd39130d099b37d1a34c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:44 +0000

    boat mark passage place furniture

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
