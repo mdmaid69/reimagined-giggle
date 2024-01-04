ba33c74272cc6f2a00ad375c51f08441e5a6afd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:35:24 +0000

    herself somehow sink congratulations thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
