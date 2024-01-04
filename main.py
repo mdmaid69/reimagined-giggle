3a207304b208dd3155b0d36777150405f85388ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:07:36 +0000

    frightened supply dark case replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
