fd4ec5400efb8d54666e3c786b68713520254aa2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:47:43 +0000

    lead anything suffer 564encourage slice

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_length(array):
+        return len(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
