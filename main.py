27805b133be1ab52ce85d93f5f4c363b644767fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:33:59 +0000

    check runinto floor Gg threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+from collections import Counter
+print(Counter("hello world"))
