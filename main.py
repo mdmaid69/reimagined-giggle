9090e399e1abb8b86358d33537d24eb88b0e228f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:52:16 +0000

    voice quantity related interest hold

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_as_list(array):
-        return list(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
