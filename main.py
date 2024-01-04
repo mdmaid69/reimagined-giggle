3d87723821c223a39c8068ef7772aa1f6237da09
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:47:06 +0000

    reply heart impressed deliberate blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+  def convert_to_octal(n):
+        return oct(n)
