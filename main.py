838f1442e9ef576287f8ee74ad235fe767dec6b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:23 +0000

    quarter straight during not closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
