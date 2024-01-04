258d5fcd6c06b07cfac54f8a442bf34653ffdaa6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:14:30 +0000

    upwards degree disagree complete valuable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import numpy as np
+print(np.array([1, 2, 3]))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
