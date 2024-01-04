bbb22b94507c5f53580ba66c49e961d23704f37d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:03:15 +0000

    explain standard because only well

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
