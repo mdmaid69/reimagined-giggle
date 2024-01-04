d45cdaa7aa79be7bce1574628cd97180d705b85f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:16:09 +0000

    safety work Uu carry familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
