713bb88b6e5d00bc87ead3375f899709a2ba1d08
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:47:31 +0000

    building tidy exist powerful surprised

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
