0646cd19f0183e3c838b31509dcc1381305c1e75
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:56:51 +0000

    argue anywayanyhow neither apply takeoff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Cube numbers:", [x**3 for x in range(n)])
