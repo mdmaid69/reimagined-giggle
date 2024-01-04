f921c9c893fe522fd8e3019cc6d6ff5f21f6d28f
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:54:28 +0000

    fun divide achieve like female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import random
+print(random.randint(0, 100))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
