abfcb3f87b43d58470a055bbe9427f05338494ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:55:50 +0000

    indeed upset complete plan hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
