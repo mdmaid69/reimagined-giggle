942021315901d2d48ac1efa9b8a6f30d1d1ffa50
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:25:42 +0000

    obtain calm Cc getoff anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
