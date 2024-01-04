b70c45afda4fd6ea39b8f130923028396a2685ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:23:30 +0000

    Yy nature forgive busy body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
