60e046c952e9fe90122e66d06e05a47677b68a25
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:59 +0000

    reasonable boil branch flow storm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def clear_array(array):
-        array *= 0
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
