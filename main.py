d1437889414f3b06ba7fe1cfa9f1eb30236d7b20
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:33:19 +0000

    passenger steady might during maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
