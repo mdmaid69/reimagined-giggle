6eb45334161b1e791ca1206ad77bbeb701c67784
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:41:23 +0000

    unit mention thinkthrough file run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_pressure(force, area):
+        return force / area
