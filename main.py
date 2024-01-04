28e309d742f894007684881b3049e5b26706de7d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:48 +0000

    nigga experience here instead transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.time()
