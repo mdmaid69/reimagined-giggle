5bbd0d6df6cda4739f07fabd1704199e72494132
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:59:08 +0000

    join justice mystery spite count

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
