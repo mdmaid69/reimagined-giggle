bc7775ff647983d9d2b894a01da1c2d90dfb0adc
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:30 +0000

    admire short sport opposite spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
