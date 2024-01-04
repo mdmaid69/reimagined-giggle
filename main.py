fb570efd5194df1f4f06a7d208221e6c9683ac42
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:42 +0000

    second stamp voice here lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
