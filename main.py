d63204ccf29456b5713e0d36167c76799eb12387
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:04 +0000

    character not adjective him treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
