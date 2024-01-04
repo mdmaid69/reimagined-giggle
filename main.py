b959bc8d5d59d0dbcd28752200eae66e835fe249
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:32:31 +0000

    secret independent official large even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
