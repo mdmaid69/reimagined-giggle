a97c7ba1eb11ac04c81449694d55d6fa8ae551a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:07 +0000

    cross favourite point emphasis calloff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def add_numbers(x, y):
-        return x + y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
