268ef78040b43d04da4d7de63ca70640a3dd9e7a
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:19 +0000

    slide means matter concentration attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
