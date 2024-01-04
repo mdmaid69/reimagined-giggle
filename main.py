3b67eb3635c4b2752851e5f8a82e43cae880d49a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:30:04 +0000

    trust attach dream takeoff mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
