4fcb0583c6da1f4114243449848b0384c64ac2b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:23:32 +0000

    problem definite hurry become together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
