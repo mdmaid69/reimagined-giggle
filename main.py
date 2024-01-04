51a453e7206deb44ee89690ca2fcd9d894e27e38
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:53 +0000

    temperature draw law now song

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
