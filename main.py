a8963ccedb1b23854e558e1d05ba02be6980eadc
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:24:34 +0000

    organize sweep method keepout back

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
