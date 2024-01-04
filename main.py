8244294f53cef3907d1a615aca74a5f4ea3afda6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:35:28 +0000

    slowly wash collect protect seem

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
