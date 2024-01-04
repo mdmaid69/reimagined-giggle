8594184082e2aacf50902b6d60b411751afd4dc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:34:49 +0000

    importance example party sweet disagree

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def clear_array(array):
-        array *= 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
