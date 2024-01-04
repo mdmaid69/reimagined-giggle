70ed1dc0b4e5d59d5048d396b25114cf763eb8ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:35:10 +0000

    far shout communication scale furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
