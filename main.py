df4b1f6f794f5d168ecb3abc2d29cf169d166243
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:27:32 +0000

    floor sink dress ability scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
