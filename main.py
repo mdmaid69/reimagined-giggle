f6dee6bcce06654062f7f5e86553167ea597913a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:14:35 +0000

    shop punish mainly scream slice

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def sort_list(lst):
+        return sorted(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
