8d7ab0733e59d78089b9bf8c0b4c644adedb1927
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:48:14 +0000

    thin term fact unfortunate behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
