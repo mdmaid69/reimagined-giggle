f1fda7837c0b6a0d09bca446647afc9d36cdf6b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:17:17 +0000

    nothing increase range holdout word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
