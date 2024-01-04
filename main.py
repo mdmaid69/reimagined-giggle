386853d483ea743376d1017be98e29e1f219bd04
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:44:31 +0000

    place rest Yy light week

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
