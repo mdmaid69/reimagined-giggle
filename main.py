245c72f0055a07072a1ee9c713252f73216dddac
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:54:53 +0000

    uponon size get supply result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-for i in range(5):
-        print(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
