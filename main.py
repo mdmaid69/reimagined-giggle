89167eefd0a6aceec86fbb72ac7c64877013d5ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:31:34 +0000

    yourself until experiment conclusion mass

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
