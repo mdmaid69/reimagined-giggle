71c8ead9e2f7a31f6a0e48ab8decb4c0aa0eeb7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:17:55 +0000

    nearly case runinto for breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
