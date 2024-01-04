dd42e63b9db90b77479fea69482a35c9b4c357b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:06:29 +0000

    yours proud influence trade splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def reverse_list(lst):
+        return lst[::-1]
