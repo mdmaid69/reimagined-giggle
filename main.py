cd3da72636db9b717bd1e2ee3a506dcd426c6a5a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:31:13 +0000

    model put delight near move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
