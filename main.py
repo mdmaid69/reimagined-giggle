fcf521b5834b369810bd4edd44657a429ca08f7c
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:11:29 +0000

    pull study talk meaning study

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_index(array, item):
+        return array.index(item)
