59365dd07c8713c7374fca4bc4fb24197f10a13b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:20:23 +0000

    surprise when sleep impression near

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def is_odd(n):
+        return n % 2 != 0
+for i in range(5):
+        print(i)
