a59efcaee2612b0db8c2fbeb282bcd391b91b87a
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:13:22 +0000

    unusual how track opinion active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
