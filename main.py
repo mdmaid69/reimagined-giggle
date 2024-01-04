a75f509283d7d95c8828b60ab718b017bff8127e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:24 +0000

    sharp fast wing survive study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
