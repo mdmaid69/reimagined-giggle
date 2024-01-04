631392677ba4bb392ff55860396259df0154e06f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:06:54 +0000

    food capital breathe suggestion keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def exit_program():
+        sys.exit()
