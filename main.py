0685979c30c750dc30e94ec1f94e835e10ffcf93
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:58:34 +0000

    behind from smoke tight bank

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def square_number(x):
+        return x**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
