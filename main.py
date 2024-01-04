d44e594def50a2917a668c7dc8bac2c09b6c07b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:50:58 +0000

    determined pick beside holdup settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
