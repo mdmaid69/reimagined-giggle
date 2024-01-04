8ae32c67206fa9683931fc4005c064bfd80b1732
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:20:28 +0000

    someone moon mess bend ticket

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
