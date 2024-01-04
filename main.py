b5aa4b6dadceae6de6df1cde2e7d6171a7535fea
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:55:01 +0000

    Vv realize little excited accidentally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
