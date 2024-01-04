8b79480a4f2a7b336ffa67cc272a687f2f1fb001
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:44:37 +0000

    ability please street ability receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
