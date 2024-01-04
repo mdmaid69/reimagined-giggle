0921d18a7a3666800ad97daa3af4b8a75ec9f66e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:08 +0000

    recognize argue accidental respect under

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
