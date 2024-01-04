13282e4a88bc0a6edda4e78edb4541c0d62e055d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:06 +0000

    unfair turnaway since friend loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
