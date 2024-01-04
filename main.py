9fd7dc45d0b75b0752d3af450d6c360e3926e5b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:41:39 +0000

    load discuss plan noone suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
