4db16df8f2b77152649f51573f54c60c8ef2c009
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:34:57 +0000

    complaint calm transport soft joke

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
