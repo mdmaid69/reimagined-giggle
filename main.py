598f0035858ffe86b653dcec9bc97c8cbc71d554
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:36 +0000

    but tragedy appeal somebody Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
