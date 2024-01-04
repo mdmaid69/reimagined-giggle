c854ed7368aa4717a4caf8bc3c3a2f5d05fe71ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:24:52 +0000

    choose generous interested reserve bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
