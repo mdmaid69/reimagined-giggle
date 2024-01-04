7a7e5e09f1f6fbf7e2626d03c09b0799e9df8095
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:36:14 +0000

    must from often place good

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def sort_list(lst):
+        return sorted(lst)
