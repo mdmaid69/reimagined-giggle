fea4635e20e722899e4913f6aa2707db4bd2d9e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:59 +0000

    sink holdon least he use

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
