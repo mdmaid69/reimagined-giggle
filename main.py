91dc6742bde904f53e5364e9a9cd1cf03657b897
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:27:46 +0000

    sell just reply emotion its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
