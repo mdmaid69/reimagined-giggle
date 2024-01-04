0f549a6583ff0cedec64fa7c1d12caf602c9071b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:50:21 +0000

    responsible quality stiff division cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
