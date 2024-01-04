fd0165aafed1d79a70a738aee4e61b3154df2325
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:25:03 +0000

    system turn cross answer quality

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
