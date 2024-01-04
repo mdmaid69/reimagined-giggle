a34a1a2b47538b5e8c0c9225e952297b2b25e74f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:53:09 +0000

    ring provide where during range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
