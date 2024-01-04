12d6eb478abb35adc671fc8225abab6d969cf09e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:07:15 +0000

    weigh comeround design able sorry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def remove_duplicates(lst):
+        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
