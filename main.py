aaa3c9b839ecb9c45e5e379f70aafda4a0c5df8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:48:19 +0000

    at turnoff woman effect wonder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
