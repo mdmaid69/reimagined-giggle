9a5ddf19c9e5c35c155464e00a69b2384cb60b4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:40:01 +0000

    dead slightly paper considering stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
