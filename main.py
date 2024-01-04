be645a226151475e63ae4c2b469bcca3f5be756b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:26:02 +0000

    OKorokay calmly discovery situation unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
