61063500ee9c9b811273e73a7317b366d0ab9b9a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:14:30 +0000

    fly accidentally tread everywhere phone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Squared:", [n**2 for n in numbers])
