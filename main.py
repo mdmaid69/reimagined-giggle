4345cb9061ad3ae9d3dc3b8073a0efd89e4cc552
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:01:35 +0000

    shine late permission through open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
