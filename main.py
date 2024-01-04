83e2fa9ff6985f4644a5a77cf46ad5c9fe199851
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:53:52 +0000

    music congratulations mail excuse serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
