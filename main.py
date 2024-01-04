4874e34bba86c9e33c92d11bc2f69220710464cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:56:24 +0000

    practise murder callat clearly worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
