9cf33db11f70118249bc97ea18263403e95baf92
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:55 +0000

    unfortunate remain dish exploration explanation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
