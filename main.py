f6de62d33162e029a6c0e9e96cda5fb50f5f104a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:41:23 +0000

    rain display careful arrival snow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
