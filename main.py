144c95bcbef47d36f5fb59cb262ba43d9b7bc090
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:39:56 +0000

    cruelty excellent free lookaround beside

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def convert_to_binary(n):
+        return bin(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
