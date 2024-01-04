56ea9b0d5af49db84edaa4564af409dbc2579469
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:08 +0000

    professional shade reasonable surprised loan

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
