52d46537272e443be124b8701edaa897b67abced
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:05:25 +0000

    square minute load evening concerning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_list(array):
+        return list(array)
+def reverse_list(lst):
+        return lst[::-1]
