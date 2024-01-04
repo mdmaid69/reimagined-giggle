fd1ba71a8bb4ffe6e7a4d25daab2568ffdf1b7e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:03:29 +0000

    sweep yourself hurry slip law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
