a2eba9e70f8924b2fa530f928662ebb8b02bfe64
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:17:04 +0000

    goup loan distance deliberately home

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+print(random.randint(0, 100))
