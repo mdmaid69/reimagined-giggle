a0db8bd4013ef28f42a8d6cda8c93c6302c58b2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:54:38 +0000

    bringforward often exchange clean Rr

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
