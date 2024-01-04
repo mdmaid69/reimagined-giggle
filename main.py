ceee04e749e64f4ec5fa2e7cfb9da3335e7a9545
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:56 +0000

    loose offer performance distant left

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
