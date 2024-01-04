a4967848dbd37b28f7a7c6b6f6fed5154f87ff1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:41:59 +0000

    fun level tap rather city

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
