2e1268b5f9b9d08c77516284972daed05b4b1276
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:50:03 +0000

    disturbing generous and string area

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
