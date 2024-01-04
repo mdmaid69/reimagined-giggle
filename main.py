48db7b2dba66c941c2a28baccf52d3ce815da235
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:01:34 +0000

    job early average damage 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_rectangle(l, w):
-        return l * w
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
