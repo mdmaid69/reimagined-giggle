a3903e8f9b92d0f130a1528c2c0453dbb2db0d16
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:35:17 +0000

    emotion glad high turnup foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
