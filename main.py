0e595f2c306e4118fc0bc15d91929f535f240a83
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:30:39 +0000

    pretend suggestion trade consist pure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
