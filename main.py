2e4736769d3b515a9d1c4fe33263770a6a685105
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:12:24 +0000

    somehow treat greeting face wish

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
