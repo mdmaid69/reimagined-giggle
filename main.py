a32e612709d051c157dc196b39b93ea4caad63de
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:29 +0000

    near accept holdup squash loan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def convert_to_octal(n):
-        return oct(n)
+def find_max(numbers):
+        return max(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
