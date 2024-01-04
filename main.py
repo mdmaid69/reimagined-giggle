1f43310276754fe4ba42bfa649fa284a533eaebb
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:44 +0000

    rush ilovedicks rise unless contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
