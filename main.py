6c0aacc4dc460a9e3945519cfa1ac5890ba878ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:07:18 +0000

    now wise increase draw ring

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
