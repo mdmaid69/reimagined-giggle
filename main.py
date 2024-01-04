866d3501bc33f840e130a8667e38a6aa200a74ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:42:10 +0000

    talkoutof union goback(to) coffee class

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+print(os.getcwd())
