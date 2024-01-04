8c456d16013eeddc9dd73a84ed8702dd4d2df5d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:47:44 +0000

    lunch clue rubbish description tell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
