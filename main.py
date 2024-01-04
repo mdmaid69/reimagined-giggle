5b01b152679ad2b0f340f561896ec3b87371e4bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:13:45 +0000

    climb include achievement condition minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def clear_array(array):
+        array *= 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
