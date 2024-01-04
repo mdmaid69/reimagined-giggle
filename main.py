f170f062313b24d0811b3a192cc020e88043c76e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:07:09 +0000

    crowd past excited pattern weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-print(re.match("h.*o", "hello world"))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
