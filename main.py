4451be67684a12bc14ff5e116eb5529775b4ad35
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:06:53 +0000

    fill worth so science sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
