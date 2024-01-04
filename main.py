249cf492ad27f5b8a70ec7767826cc770ef10f04
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:49 +0000

    somewhere radio tooth choose hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def count_elements(lst):
-        return len(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
