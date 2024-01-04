4c9a664d1bc606f69842461a72aea0d93b671949
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:59 +0000

    second print smoke ring produce

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
