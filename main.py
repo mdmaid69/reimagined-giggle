4c2d505a114526ad51ebbc8a6e548719b58a222f
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:50:12 +0000

    march noone flow race once

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
