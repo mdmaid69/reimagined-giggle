af30b0040d41fb058a66ab3608e2078492a7289a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:35:33 +0000

    thank Ii shallow loose regular

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def is_even(n):
-        return n % 2 == 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_int(array):
+        return int(array[0])
