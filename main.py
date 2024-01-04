ead576c57be9c2e8a8a190ac0a4cc260ab7f175e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:13 +0000

    plate cum sad season bridge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
