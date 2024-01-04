84965d88a68fd072afd55b55332d987b6b81f581
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:48:38 +0000

    lock set their church measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
