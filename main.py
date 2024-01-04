a63a3abebd6db876a0d869123a1e6811624bcbb9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:54:19 +0000

    careless trousers secret exchange compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
