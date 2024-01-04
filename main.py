3953faac7f73aa32c39d19667484d8398dca53e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:45:31 +0000

    such health together shame container

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def square_number(x):
+        return x**2
+def is_odd(n):
+        return n % 2 != 0
