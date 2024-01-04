eeee18675bf37b534975be5ba3f60a7863a8d921
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:30:07 +0000

    unpleasant immediate then simple shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
