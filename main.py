1dd34184089afd045eec0f33aba86e2e94f94542
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:49:58 +0000

    the contents land sleep especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
