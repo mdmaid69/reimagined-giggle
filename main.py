323ea73374b37cda9853e6857bb62214cc29968f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:24:40 +0000

    sound clean comeback tap agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
