d6c4be7332299fd36e94778c097930ed4b48a64f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:24 +0000

    simple obtain concerned abroad miss

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
