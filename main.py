d90a713b98b1d4be87125bb631d0171a7ed44b07
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:18:37 +0000

    hate operate measure provide through

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_power(work, time):
+        return work / time
