631afa352de150a63c9bfaf5b9fbcde46776d5db
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:40:20 +0000

    snow relief upset unusual university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
