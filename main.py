e031132a18c10e505db909396ad0b9b365d67df3
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:57 +0000

    compete circumstance suppose vision aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
