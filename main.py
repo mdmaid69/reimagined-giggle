9fffc9481571e68990dc5adc4f53fefd87d136f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:39 +0000

    afternoon protect keepfrom deep much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
