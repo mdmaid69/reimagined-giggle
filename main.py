fcd5c6b3cd3c45457a7cc609777ef2c95c67fbcc
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:02:55 +0000

    show force correct takeoff calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
