003650bb91fa2026a9477b9dcc08e2580236cd21
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:21 +0000

    before priority surprised announce agree

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
