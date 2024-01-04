62c103908256b9d5390a8ff6b72cc65aabaf6530
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:49:06 +0000

    disturb many fair whenever secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
