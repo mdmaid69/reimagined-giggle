0279d627d7b71d5841e66e71a89738b91e8e54c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:57:38 +0000

    here whichever large impressed slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
