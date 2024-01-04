fa27dba743681fee1aa0b200c2ab87857831c7cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:08:09 +0000

    piece bright peace treatment move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
