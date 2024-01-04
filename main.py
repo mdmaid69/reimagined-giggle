c89582374a2f1faa34c15a7ecd5c2d8f0c315c5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:21 +0000

    sad division pull exaggerate Ss

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
