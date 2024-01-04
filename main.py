ac5021a85e348b9c594c02133d841421bf108185
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:44:56 +0000

    stranger room past male sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 def calculate_perimeter_triangle(a, b, c):
         return a + b + c
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
