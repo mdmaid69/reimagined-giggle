1e78cbb7e67e674904145e2784fedcab2e0a1a95
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:29 +0000

    few pass breakout break file

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
