00fb564ec1e79bca5552a5eaa08fa983900002ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:48 +0000

    danger lookafter friendly aim surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
