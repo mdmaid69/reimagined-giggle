99644eba34eb2014b65d4779c74dbfad99e35568
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:19:22 +0000

    tend neither develop male move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
