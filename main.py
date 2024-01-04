dc75641c7ea9d21eb1e38ffbdc7c326a9b6c6e65
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:40:23 +0000

    ignore attach switch even back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
