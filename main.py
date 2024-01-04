20e56c4168419cd70e7094dbaab2c79a53ffb767
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:26:09 +0000

    effort meet forever fit around

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
