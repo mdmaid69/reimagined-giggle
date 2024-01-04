4493c312ba40ac72df3cdf5611d4ef7bf34d1c49
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:27:23 +0000

    basis branch cheat cross remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def multiply_numbers(x, y):
+        return x * y
