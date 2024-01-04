ba3dc042745e71ec0085bd08806b15ab4e405ed9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:27 +0000

    beside takeout concentration partner ability

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print("Hello, world!")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
