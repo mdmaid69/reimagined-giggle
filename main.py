9138c61ae93256f3e75e581035b85bda7dc4b857
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:10:52 +0000

    release pick keepfrom exhibition identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
