1d9cac585f49f5d2662bdcd5cbe96cc7cd9aa078
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:12:47 +0000

    valuable as bus clear somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def square_number(x):
-        return x**2
+import sys
+def exit_program():
+        sys.exit()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
