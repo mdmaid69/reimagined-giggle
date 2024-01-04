6cb8a70827ae5c54accf5bdce0b4f3f992322b34
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:08:44 +0000

    notice have hold off keep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
