c32cc170a3a7e7bb7382855cb04fcda7838c6f39
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:13:56 +0000

    cause anything new advice promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
