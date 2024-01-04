341fc35137a9366322a4f4028e39d25b5de74520
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:23:30 +0000

    illness contact smile immediate Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
