edbc19b2ee997a103f527ee18c4ff19f7114770f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:22:59 +0000

    better slow whoever receive nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def convert_array_to_string(array):
+        return array.tostring()
