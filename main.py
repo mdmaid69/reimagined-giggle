b459ef7a91e9608b19f3f85c6ae8752b784a0178
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:32:04 +0000

    lonely favourite region slide plain

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
