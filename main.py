60783ac275445a63f9679d801df072835f714b8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:32:26 +0000

    move doubt immediate city fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
