8f8a60550531d925e914261162498fbb4ee41f74
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:17:56 +0000

    exciting sad thanks wash turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
