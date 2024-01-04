75ab800f11267b89628fa93ae67e89880e4b30cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:51:56 +0000

    better case gift ignore say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def remove_duplicates(lst):
+        return list(set(lst))
