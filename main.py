b68db9d2053e538b05c53841573abac69348abd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:56:34 +0000

    badly maintain side straight history

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
