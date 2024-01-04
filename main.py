ea60adc973a60aaec3878338694c8b9c9a400e0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:21:39 +0000

    building bend able lead violence

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import time
+def get_current_time():
+        return time.time()
