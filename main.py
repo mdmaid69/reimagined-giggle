7987ed813587a680abeec5721cab1fbc49d32f08
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:53:18 +0000

    introduction cause image waste carry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
