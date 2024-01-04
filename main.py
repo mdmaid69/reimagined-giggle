8ec2f5be394ab48ebf1d68637e27eac3a114197e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:19:03 +0000

    unlucky trip contents handle which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_circle(r):
+        return 3.14 * r**2
