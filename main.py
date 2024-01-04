d407f6f76b4e4582d0dbf82bfd623031be1af087
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:57:04 +0000

    progress realize light much wide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
