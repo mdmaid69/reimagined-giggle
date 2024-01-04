ac87e038defd3f21e3a6490526fd9d76c314ae70
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:25:40 +0000

    anything except rough disagree quick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
