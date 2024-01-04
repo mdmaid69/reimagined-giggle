d499158b61c2626725dcf95ce253117227293842
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:40:22 +0000

    after breakin ball opposite education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
