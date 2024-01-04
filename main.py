08c491cb6cfee4f62a33cad07e4831ff19eb9a6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:43 +0000

    difficult else fork match except

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_logarithm(base, x):
+        return math.log(x, base)
