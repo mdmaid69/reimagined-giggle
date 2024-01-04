28dced6fcd874b4ef92fdc757a2c43ecf18b644a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:28:06 +0000

    shine giveout bin everybody by

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
