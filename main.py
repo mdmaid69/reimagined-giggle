ef04d840a87b8052f53e2c2f7928910e883f1c06
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:27:21 +0000

    room land add muscle keepup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
