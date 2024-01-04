af33e81c9420ba9e113b6de5e0f726c9ae929e86
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:37:35 +0000

    method market lie recognize pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
