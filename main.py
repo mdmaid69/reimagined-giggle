cf8f30be7b387bb3d6f6342c925b46906319fdfd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:04:12 +0000

    star shoulder sweet favourite profit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import sys
+def print_python_version():
+        return sys.version
