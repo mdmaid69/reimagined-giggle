8f63e5f53dc020a65498d5c396e61f4bab21441c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:02:13 +0000

    calm advise people pen commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
