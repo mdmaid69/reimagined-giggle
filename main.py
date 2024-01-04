e4065aa9f12a18ab115f9d3e14e450e673e83f77
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:11 +0000

    keepout mind suit job waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_exponential(x):
+        return math.exp(x)
