26ac93316786d27d1fe120f802aa8960473087c0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:17:34 +0000

    bright quality track home sleep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
