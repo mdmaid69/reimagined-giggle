f62c82b62b03cb3abf60440dc74f5243c3df1829
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:20:59 +0000

    turnout tense science cause write

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
