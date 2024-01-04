1e5571868470b50f0cdf68cd950a75f032f8231a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:18:54 +0000

    into swimming slice naturally transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
