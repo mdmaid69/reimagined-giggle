8dd4e043f3bf2d8a76ceabb754eeb79bccfab8a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:31:50 +0000

    small board what choose pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
