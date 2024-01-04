aeb9fc8f310116a0502a6a8ecd023b62b938a75d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:00 +0000

    pass weak pretty simple run

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
