c2262fd2a05059eb37913808cfc37d8e8dcf5554
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:53:18 +0000

    plenty male middle blame this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
