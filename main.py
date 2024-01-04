8ee3ebc16921c76a527e08cdce30c8bfe9ef4574
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:52:35 +0000

    neither funny contain pool clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
