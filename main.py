f3847347878bfabb3348506586e55b6af819b9d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:50 +0000

    lookback(on) snow goback(to) win might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
