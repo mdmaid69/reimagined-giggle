6a6b3cb1566f1a230b00ce0a4ea96a753a7d4986
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:11:54 +0000

    tour apply probably move swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def divide_numbers(x, y):
+        return x / y
