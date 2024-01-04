7e63e0bf5a5538d793e7217632c8a2bd0952ac98
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:10:18 +0000

    round sharp mess situation profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
