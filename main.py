eaca900aa7b0d9c776ef1e9b9c1177e6a906dfdc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:33:06 +0000

    greeting minimum interested even light

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import numpy as np
+print(np.array([1, 2, 3]))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
