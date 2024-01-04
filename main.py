142816f936c6fce0af2f361168877056433c0216
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:56:12 +0000

    active complain count wet rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
