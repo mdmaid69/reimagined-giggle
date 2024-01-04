37872b17ecf609a4a16196050909c850e7e10302
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:03:53 +0000

    relief rent someone takeoff practise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-print(sum(range(10)))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
