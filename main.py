91081745c379330b17fadabbadab1d4c9918c200
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:10:59 +0000

    brave conduct quality truth anyone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
