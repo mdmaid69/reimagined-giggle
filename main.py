8d478374e93b09ddbbbcee8f3ed48caddf8e42d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:17:24 +0000

    when door beneath nonsense shop

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
