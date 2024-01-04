aefee6e29a70824ba0bc0358dc3ba4032a3aa5ec
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:05:37 +0000

    shelter 571enough temper lesson load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
