3ee61309701ecd4a5a926a161622791390e36ab4
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:56:46 +0000

    up cover valuable doubt ashamed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
