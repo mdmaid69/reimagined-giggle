ba6d9b117b1b21d51c56d16013634536dfa45e86
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:10:28 +0000

    whom heart before example easy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
