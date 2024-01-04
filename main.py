8cfe46c721bdcf584850969710a7c31c8eccd486
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:34:38 +0000

    proof charge sad decision runinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
