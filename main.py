5a08148dffce51c1dcdaba85316246412a8527e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:16:46 +0000

    money sort operate image interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
