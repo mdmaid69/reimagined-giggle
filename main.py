d149dfb20b6d9e9b20179fb669ccb55ec985f1cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:02:30 +0000

    statement accident surprising disturbing interested

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sys
+  def get_python_version():
+        return sys.version
