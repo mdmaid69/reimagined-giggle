0f4d82b94d2d0973e8269bcb8d2f74821957f05e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:04:51 +0000

    respect sail why guilty respect

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_index(array, item):
+        return array.index(item)
