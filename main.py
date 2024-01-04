bd6ad40cc1f2eab3e578729dd66f234ebb27da94
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:52 +0000

    tool woman somehow slow nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def add_numbers(x, y):
+        return x + y
