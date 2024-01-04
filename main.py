eb6943bc059a09f467e18bb03b3ef9c251a5ab1b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:52:20 +0000

    other week support black giveaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
