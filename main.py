6d5e849249fdce7b7e222b64e16f5fe79d30d5cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:04:02 +0000

    last run also definite delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_as_repr(array):
+        return repr(array)
