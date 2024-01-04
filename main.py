ade4e85d8f30211e943a96e5a11874ca05ae4c6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:30:13 +0000

    cover label complicated freeze somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-print(math.pi)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
