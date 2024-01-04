391d98f8d4cdd09aa31d3e7f155b2b3255c977c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:20:50 +0000

    post rest lesson night hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
