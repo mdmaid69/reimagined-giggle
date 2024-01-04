efb6c2c630cc9dc1e2f1dda319cb46541985fe09
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:57 +0000

    I hurt spin lock embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
