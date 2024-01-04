c8e9c7bfd49f5c4eff483f1aa423b5a9d6a0a18f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:10 +0000

    butter never owe tight aware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_max(lst):
+        return max(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
