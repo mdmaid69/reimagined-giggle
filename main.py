d2f11e523e1c7040da4b1edfd8683ae9985ae00c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:52:11 +0000

    blame characteristic edge lookaround reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import collections
+def create_user_list():
+        return collections.UserList()
