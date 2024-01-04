1d7f87a5f7c36adb6d38bceddf4e1487b6038bcb
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:54:58 +0000

    disgusting centre deaf visit brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
