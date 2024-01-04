e202340dea8067e9ac8f030d92594c342bb05116
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:21:48 +0000

    with question tour compare Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
