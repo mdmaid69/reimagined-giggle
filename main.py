6c3a8459a0fdb9fe54456ddbc28818fdb754c38e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:04:19 +0000

    tell hard talkoutof think couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
