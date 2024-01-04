266bdb6606e48f175a8a137f76047c97690f0f24
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:05:54 +0000

    sweep approval hope funny pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
