e1b4f22423d39be1999c6d14ac6c6f0364ea2d15
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:31:09 +0000

    interested respect tough giveup square

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
