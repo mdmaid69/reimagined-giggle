0a7b7f03347e016a11492a41c517633ea641c0a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:21:43 +0000

    enthusiastic bother capital tend into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
