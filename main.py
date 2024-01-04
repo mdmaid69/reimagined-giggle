1dae80b437e24057b8c88caa434455c5bb1486c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:12:38 +0000

    deliberate mix help mouth joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
