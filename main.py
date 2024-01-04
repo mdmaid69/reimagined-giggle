7814c7b734613caa8bee9836cf5c7a9c99e9a2bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:51:28 +0000

    of remember have flow runout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
