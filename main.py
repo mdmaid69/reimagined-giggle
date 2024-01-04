199991ab926d14a3a9fc840d6a43091385c41808
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:55:20 +0000

    sit our information apparently watch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 numbers = [1, 2, 3, 4, 5]
 print("Sum:", sum(numbers))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
