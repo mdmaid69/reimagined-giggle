f7cd42f64721d849f56387bab7f1b77b35872829
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:51:29 +0000

    temperature takeoff plan whatever bend

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
