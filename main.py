ec9abc4c864c08c9350582e2ad985ca4ef8510e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:11 +0000

    same congratulations love beginning yet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
