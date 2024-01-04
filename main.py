0441d9a17fb9ae0042039832986859b4a7450502
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:59:35 +0000

    pay dry land lookafter professional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
