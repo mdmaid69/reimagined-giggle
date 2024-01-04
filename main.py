b54fe8516041e0096a0a221fb21e9328072138e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:25:53 +0000

    official support roof phone edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
