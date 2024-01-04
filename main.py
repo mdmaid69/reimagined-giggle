3ce1ffc66d8fde7ff13400256fafa1821055a846
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:28:52 +0000

    smile number maximum unit risk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
