dc7bcd2cf90992a352f13232d9e66c0473192160
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:24:56 +0000

    apply comfortable female condition upstairs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def add_numbers(x, y):
+        return x + y
