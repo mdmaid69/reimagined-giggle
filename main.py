0e74bdba033d0b3ea72d7c9e29fd6547a1b27528
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:50:31 +0000

    pause pinch responsibility food pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
