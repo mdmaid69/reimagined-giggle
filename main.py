7e048540f1656805148aef1cc5e679716c155a97
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:55:45 +0000

    especially apologizeapologise rich effect happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
