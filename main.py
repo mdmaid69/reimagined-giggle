826379c5f1e77e3e61db5d834959299cadf432cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:33:21 +0000

    sure loan issue suddenly naturally

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
