23a084101af8d80a9bfcd214580a5e1ffc4580dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:45:11 +0000

    steal talk familiar along alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import sys
-print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
