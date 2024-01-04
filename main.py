6cb39a7bcafde27bedd62b3ba250eacd6c328ecc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:08:15 +0000

    name physical plastic wise pile

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
