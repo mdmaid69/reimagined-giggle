5b622378be2db6be2f9d892888343132748fa0f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:43:20 +0000

    rely recover train practise transparent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
