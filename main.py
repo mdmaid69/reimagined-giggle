6856321e803da5063927da223b542d57d96c6b00
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:29:45 +0000

    freedom actual hot party persuade

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
