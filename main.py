1d955a1755b1690b2b5b6567caa47701a8f5b937
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:15:11 +0000

    Ww tomorrow similar male thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
