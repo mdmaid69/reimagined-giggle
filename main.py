9a96af48223bb85c7650f197b3216cf0088e8729
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:37:29 +0000

    turnback stiff cheek smoke worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def convert_to_binary(n):
+        return bin(n)
