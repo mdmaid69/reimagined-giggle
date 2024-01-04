1cc98c0c34cb78bf1be022c75aebdc62e2c64fb1
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:11:24 +0000

    earth because matter bend test

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
