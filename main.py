eb3835a73cdecadeef86e157cb9f2c9537fd2c07
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:46:45 +0000

    onto put best limit cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
