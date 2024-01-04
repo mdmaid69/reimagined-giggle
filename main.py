ea819e9273431e2c50fac4ff4dcefe47ab1adef0
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:11:26 +0000

    estimate ignore final tall answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
