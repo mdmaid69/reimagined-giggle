55f8e85e5cd4e7244d9c5779a4c5a7e635afd195
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:19:28 +0000

    place point people explanation spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
