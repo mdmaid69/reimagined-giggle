6f80a5cf9bb649789cf3ab551e1922c7711b509d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:23:50 +0000

    manner where agreement consult safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
