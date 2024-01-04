dab1ac6ae7c652e94cfe34f659c05bba70585c27
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:31:28 +0000

    connect wrong sort circle money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(5):
-        print(i)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
