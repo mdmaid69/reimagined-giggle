ccd6cc30948b84772368255928a18b12d33b144d
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:06:31 +0000

    expensive moral act extra whoever

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
