58556972399b38214de98dcaa873c7c8e59a1400
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:26 +0000

    row greeting suckme pause intelligent

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
