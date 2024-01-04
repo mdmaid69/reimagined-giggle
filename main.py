cf92b2111875af9a390f2fc57a7839579fa1cb5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:06:15 +0000

    direction hurry about may put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
