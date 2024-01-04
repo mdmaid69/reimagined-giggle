23a32acbeefea7207a16811fd0047d16e1d3ea8b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:31:46 +0000

    watch division mine worst transparent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
