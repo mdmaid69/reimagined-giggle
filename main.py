9b621848a96bf653a62d2b331adbccd59a6addf2
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:26:30 +0000

    remain up acceptable block notice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def multiply_numbers(x, y):
+        return x * y
