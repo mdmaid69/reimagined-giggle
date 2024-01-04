da2cadb8cc578a73f24054693b2a9e11a7bb121f
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:48:15 +0000

    tall alive steer true mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def sort_list(lst):
-        return sorted(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
