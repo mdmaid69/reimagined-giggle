83f83171fa3f96356bf94e5aa9db882348af041a
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:34:22 +0000

    completely something number by involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
