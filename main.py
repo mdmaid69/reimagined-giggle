3e646770bef37373fa414f2f1b944485c63a3d87
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:35 +0000

    story shop human among first

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def print_python_version():
-        return sys.version
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def multiply_numbers(x, y):
+        return x * y
