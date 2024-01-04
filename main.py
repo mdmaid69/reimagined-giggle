7884f2eec65e51c7ba90892a0165a313e29e07d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:31:58 +0000

    swim colour fasten trousers best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
