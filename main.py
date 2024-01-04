11631df04c99b0b3890fd8c70a30bbb73677dbb6
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:13:06 +0000

    runaway emphasis rush operation minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
