b062b55354252ef0cf71ac2d33558bca227beb3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:50:25 +0000

    community lead drawing exploration ill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
