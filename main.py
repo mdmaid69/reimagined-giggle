8ee3d59f5402a1a344cbc2b479f780b701560e31
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:47:17 +0000

    system fill excuse guide relation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def square_number(x):
-        return x**2
+def count_elements(lst):
+        return len(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
