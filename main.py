7e4974f64992cd2db640f5b9a89aa6c5880d4e78
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:54:46 +0000

    move home during dead beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
