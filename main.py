d348aef261c1dd677447170d5a5231952bdd1f03
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:18:56 +0000

    shop behave ought hurry estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def count_elements(lst):
-        return len(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
