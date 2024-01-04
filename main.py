65d006acdb73ba19e802387195fb331d650aaf89
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:00:11 +0000

    load declaration bush refuse importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
