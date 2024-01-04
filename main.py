fe4035fe233e4c48fa33c16f1578808fc696dcd5
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:17:50 +0000

    contents after part when convinced

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import random
+def roll_die():
+        return random.randint(1, 6)
 def calculate_work(force, distance):
         return force * distance
