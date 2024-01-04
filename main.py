85fc74e419d3c11508d328e9bd2b991253603a7e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:32:57 +0000

    promise operate away imagine accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
