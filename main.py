af9ea5b2d53e7548de1aac5eb856e85f10192ebc
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:22 +0000

    this knowledge discover communication food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
