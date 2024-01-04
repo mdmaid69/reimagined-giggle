e1c2b2d86896114af9dde0190a036ba389d046bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:15:07 +0000

    house sort forget hit frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
