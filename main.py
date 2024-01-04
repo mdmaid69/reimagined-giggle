3de90b22767ddfa77d8347e6ae05162952a7a38c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:47:08 +0000

    hit flat row complain operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
