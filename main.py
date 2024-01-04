42c9aada42a7a1efa0f0850752289908dd3c3f24
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:17:11 +0000

    fine sign life different workon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
