cba00a3aa00eab60cd0c51aa33f4fdc4547f4497
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:36:13 +0000

    guess teach combine calloff task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+print(math.pi)
