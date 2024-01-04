dcc25f22f371e7525bf448f5b480ded21d5f8578
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:24:12 +0000

    kind bear great before passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
