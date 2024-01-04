08609e3e44c04d51d95bc1fdd36c26315983de49
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:29:54 +0000

    serious worse guess murder reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_directory():
+        return os.getcwd()
