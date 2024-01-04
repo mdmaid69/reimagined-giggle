311000e25bcc2ce409685501a0e4853e13c9cb3e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:57 +0000

    more success nerve nigga makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
