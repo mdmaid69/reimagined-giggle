42581df57309ae6da4f3e6f0ed99e7e49063c6d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:56:39 +0000

    wish kind error slide lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
