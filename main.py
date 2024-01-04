6809ff540895361af7b7e74a52f40ef058c26c0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:04:35 +0000

    so without turn emphasize crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
