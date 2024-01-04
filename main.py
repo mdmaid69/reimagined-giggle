316177a14a35260c5ecc9c6fd7b2e1c2eb984e87
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:32:49 +0000

    pressure count advantage feel themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
