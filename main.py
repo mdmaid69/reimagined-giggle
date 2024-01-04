56e5dcca661675a16efce640104935dfc740ff01
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:08:04 +0000

    woman report quantity ignore score

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def convert_to_hex(n):
-        return hex(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
