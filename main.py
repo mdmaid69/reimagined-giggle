607a0320d41640bf2b957d07b09cac6a99f87402
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:46:08 +0000

    talk peace notice collection wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
