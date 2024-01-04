1f19e5d17f9c4d276a06a7a2c245803e59803c41
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:58:32 +0000

    away dark scrap principle language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
