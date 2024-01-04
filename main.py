9294d268dcf9c8f42aa4908baa47b2fe13f458dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:46:20 +0000

    generous reasonable fit warm flight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+for i in range(10): print(i)
