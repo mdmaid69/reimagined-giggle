16160e39c9567dfe5334e2a165387e42228befbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:20:43 +0000

    spoil brown post bring cheat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+print(datetime.datetime.now())
