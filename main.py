85d505bf673c1699b6e56f61c47d597fb6385c1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:59 +0000

    news attention skill natural Bb

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
