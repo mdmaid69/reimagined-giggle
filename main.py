2ce7375e3df73402c0aa5b0e6c197fa00184b4b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:02:06 +0000

    call sensible lookaround clue disappointed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
