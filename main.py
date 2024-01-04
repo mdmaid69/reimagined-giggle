f05ceee87b77febf2f6a4815e65d449307392f29
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:31 +0000

    certainly expect curious container wrap

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
