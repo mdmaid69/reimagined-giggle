a930ccd9d3c2175b4a5c1a2f11bd28989584a00e
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:41:58 +0000

    fall public repeat characteristic crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
