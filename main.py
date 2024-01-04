45f6264037e6b68d4bc71211ea21d2e00b4168c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:33:09 +0000

    criticism proper part try hello

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_counter():
-        return collections.Counter()
+import tensorflow as tf
+print(tf.__version__)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
