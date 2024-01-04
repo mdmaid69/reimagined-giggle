fee999bf79aa5dd18982c5d49c56355a3d10eb5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:36:28 +0000

    keepoff persuade wash also godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
