9543a095dce94b9ea706b2107cfe67d9d91e7db1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:04:19 +0000

    decide sign turndown worst mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
