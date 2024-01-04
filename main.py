550eddda19918f61845cf292143a78b110fe2075
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:48:31 +0000

    actually pay formal dream disgusting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
