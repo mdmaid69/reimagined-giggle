ff5f89c4b53ae45bc9d8fab9dd99cc2e46836be6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:45:04 +0000

    weather winter hurry cover ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
