ca3b795d0d587a92d035d6b53d1bb7226bb270c5
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:11:14 +0000

    lead hang stop temper reserve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
