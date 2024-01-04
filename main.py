fbffba9db2288512878de0b610ac14ab1ffdfa73
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:22:59 +0000

    ordinary most week glass willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_factorial(n):
+        return math.factorial(n)
