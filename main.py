5fdd728aca2f12d758d1b4477bf0a93f4797cd89
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:21:36 +0000

    thinkover agree comment contact bus

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+def cube_number(x):
+        return x**3
+  def convert_to_octal(n):
+        return oct(n)
