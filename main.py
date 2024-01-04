3a5f84da3224aec3a9ffd5be4a78dd68107a1276
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:35:35 +0000

    natural bored switch nice goout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
