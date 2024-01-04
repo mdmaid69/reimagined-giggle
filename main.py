fdbcb302cefbdcd0715e65a9bca145944df05c12
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:51:38 +0000

    lunch club black peaceful somewhere

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
