428d09d6dc0b9d2ee58b1e845d2143bb798c008c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:31:00 +0000

    attach night react play turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
