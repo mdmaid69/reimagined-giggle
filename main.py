621c3b269440bfc6b6e6ca4dfd759ff7bf4b38e8
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:46:40 +0000

    defend amount scare left and

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def divide_numbers(x, y):
+        return x / y
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
