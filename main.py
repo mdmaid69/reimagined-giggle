37b299f46d636d0e42a5994ec5be323c02e9538b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:10:51 +0000

    record roof release this opposite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
