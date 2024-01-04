1720860f52e0a016cfaac31e3db4739e7efca65d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:55:58 +0000

    relief gap rather whichever ought

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
