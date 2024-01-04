a90882a1ab9609cece822e56965c22c136bf0516
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:00:40 +0000

    morning complete poor lower female

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
