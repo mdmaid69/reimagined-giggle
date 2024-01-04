8437341957ed93de15814efef5bb67b34f2b54aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:04:36 +0000

    exactly shallow cover chase cruel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_int(array):
+        return int(array[0])
