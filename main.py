cf84d0fe92596b0490765b4136f3f3a946a28a9b
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:28:50 +0000

    centre eye fire watch Dd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+name = "Python"
+print("Hello,", name)
