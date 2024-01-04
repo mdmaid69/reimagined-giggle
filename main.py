c22ed77188180ac865c81e8e8cf914c35e175da8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:56 +0000

    question use certain murder lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
