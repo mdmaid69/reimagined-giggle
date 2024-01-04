2de8e83275e4e18f0168512f6a6d89c37805c960
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:31:10 +0000

    married mind lift familiar deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_item(array, i):
+        return array[i]
