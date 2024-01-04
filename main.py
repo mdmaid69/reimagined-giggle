41eea8ac808ac1f5920becb6462ff23a8db586ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:07:46 +0000

    find still development range on

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 text = "Hello, world!"
 print("Is palindrome:", text == text[::-1])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
