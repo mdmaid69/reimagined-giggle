7fb9e000c21cf2532145ddfd51efa149811916a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:43:21 +0000

    turninto brave trouble meeting unit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_speed(distance, time):
+        return distance / time
