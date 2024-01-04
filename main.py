50f1407cd2e7fbb1803a24529dfd95576caf0bab
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:18:13 +0000

    ear before explain its different

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
