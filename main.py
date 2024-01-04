619099fee62a44cc7e87ecb43eb3ad06c9cbd5d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:41:25 +0000

    sense succeed brush steal claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
