7b932cabb2b300eb34701065c55338eeaec785cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:23:22 +0000

    complete indeed storm before giveaway

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import os
-print(os.getcwd())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def divide_numbers(x, y):
+        return x / y
