34c0e066b729c9bc76d167326e64a2e3d0e87ff5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:03:37 +0000

    ilovedicks comeover organization news advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
