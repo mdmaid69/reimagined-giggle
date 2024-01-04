294ff5e8fd3b3543ed653cd22b784cd05bafc6d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:22:16 +0000

    thick embarrassed Vv closed through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
