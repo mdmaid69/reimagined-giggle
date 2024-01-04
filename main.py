977e07193cade92add214089fb20ab922c28cdf7
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:19 +0000

    rubbish sea early declare hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
