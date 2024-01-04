8407d60edaf4cc7a9fe63f729b0aadf1ba4877a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:52:08 +0000

    against unusual judge afford wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
