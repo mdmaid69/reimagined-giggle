768dc94bfe0da629cc6ad9530467442270c8e031
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:22:57 +0000

    uponon deep this anywayanyhow mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
