3e06308469017028915feb61b7324ba2022e2299
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:06 +0000

    formal 567enemy under increase imagination

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
