f790bc16a0c0dffcfd944bbfbde3e7abbb473e81
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:04:14 +0000

    tell exaggerate clear shine secret

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
