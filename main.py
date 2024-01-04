1c37920bb53c39c16c1687c6e8be2cc7ecbb1d32
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:54:46 +0000

    tidy mind copy action let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def sort_list(lst):
-        return sorted(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+def remove_directory(path):
+        os.rmdir(path)
