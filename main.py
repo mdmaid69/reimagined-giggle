2e75a3c2abf82a22e27797a1ce89669116148015
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:36 +0000

    big sign young 571enough protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_length(array):
+        return len(array)
