9951881d6afdd27e34c8ec9ce02ea1c285cc8768
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:17:41 +0000

    watch even wise concentration report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
