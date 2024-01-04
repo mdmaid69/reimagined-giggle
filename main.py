0f825a2c4e45ee4e094a60cba9741d5000fdba90
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:57:46 +0000

    often object lock once interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
