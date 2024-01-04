6bc7ac836f80b169dac904c2d3275907e72b1c90
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:44:54 +0000

    listen business our row my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
