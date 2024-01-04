b20bc1fe85e28086d4b74e1b26258ab621822e58
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:09:48 +0000

    never home Kk instrument closed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
