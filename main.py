c6de1648cd97af57bd87b2b811ab8e4bb40b04b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:05:35 +0000

    memory bus odd to news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
