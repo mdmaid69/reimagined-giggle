01d485bbc767e44bf051550aa648c9f3b5d66485
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:11:50 +0000

    advice business accidental equipment smell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
