ba51f8395bbd0b0e06263ca17c0301f5ec8e3c00
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:12:48 +0000

    taste advise agree deliberate mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
