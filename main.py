b1d371b5510767afbd8924ef0edb6fafd79547dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:20:33 +0000

    fix regard worst passage voice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def divide_numbers(x, y):
+        return x / y
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
