58cdefbfde122c73a12acd947641e06d9ab5b36f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:43:38 +0000

    definite pipe tradition minor expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
