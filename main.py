a39e3afd06048999bcb8488955413eb6f709b4f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:43:47 +0000

    Jj recovery anxious guide yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
