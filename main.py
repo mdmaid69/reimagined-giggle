7f56ca02039acfbace73cf45cde19718b49cc0ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:09:53 +0000

    hit shout persuade call violent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def count_characters(sentence):
+        return len(sentence)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
