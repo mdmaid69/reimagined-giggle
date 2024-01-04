4f233a0e3287a6c2fa8408a77348c5916513aea8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:06:50 +0000

    beneath begin share unkind safe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
