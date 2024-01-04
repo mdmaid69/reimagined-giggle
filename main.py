2bd1fd16a6de65bed853c0e05008be919b21588d
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:29 +0000

    boat increase approval switch beside

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
