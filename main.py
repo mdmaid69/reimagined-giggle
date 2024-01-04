d76233c747cfab51332aeabfa3e37013f8843ce1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:20:08 +0000

    comeround strict mend out branch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import platform
-def get_python_version():
-        return platform.python_version()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
