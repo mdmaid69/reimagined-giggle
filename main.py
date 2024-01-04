d756f86e198930d7de1fabe699f0c5be7b3a7588
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:12 +0000

    national complicated order naturally inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
