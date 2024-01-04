be59d8d739ee529c15b93779caf385a8fa689307
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:02:18 +0000

    old exercise uponon common Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_power(work, time):
+        return work / time
