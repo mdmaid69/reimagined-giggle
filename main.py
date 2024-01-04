3db9729b505342451dd5b03fb8c34b9f9168ee0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:32:02 +0000

    improve mistake trip match border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
