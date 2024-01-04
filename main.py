823a4f5d939333b9af461d6018b08724cff7cce4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:44:59 +0000

    whistle unit direct bag later

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
