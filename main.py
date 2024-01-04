2106ba0321c1f8ee49127019e3eeeb244bcdd5b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:33 +0000

    correct callout Bb 571enough many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
