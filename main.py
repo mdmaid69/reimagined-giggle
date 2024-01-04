972fd387786e09585c2bb95575234376c9ae60cd
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:41:34 +0000

    apart male maybe bet stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
