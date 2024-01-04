da66fc9b70312daf12a0452b19d2a019fc23b72e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:08:59 +0000

    arrange to themselves from add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-print(os.getcwd())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
