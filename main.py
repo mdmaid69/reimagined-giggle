49cdb5e51e360a208b867168dba3197554711d4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:48:07 +0000

    translate reject during winter herself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
