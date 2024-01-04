03ea48122d670415aad183a052c9c42b9da546b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:03:12 +0000

    discussion container people poison like

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
