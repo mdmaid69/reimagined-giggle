d643c0b1f5885d263ee9eafe5335c4ad6d72efed
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:20:30 +0000

    steer measure belief day future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
