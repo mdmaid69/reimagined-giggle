9f256fd14c06ea413dadb35dc34197981176cf2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:50:41 +0000

    embarrassing slice run modern between

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+name = "Python"
+print("Hello,", name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
