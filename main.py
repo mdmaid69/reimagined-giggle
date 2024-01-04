83054a1287be6be77101f5bc1da70df25c69b471
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:13 +0000

    love nonsense hurry please stage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
