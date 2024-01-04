9489987f64fff4dc921878bbf1447b7bd4666369
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:48:18 +0000

    gradually unite search house switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_typecode(array):
+        return array.typecode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
