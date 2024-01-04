21a6499fa7002df33b00bbc082c0e9db470fb8ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:24:26 +0000

    present find more share complicated

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
