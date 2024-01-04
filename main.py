1f26eafebf5bd8cc6c8f0f5313d7842a6b825d3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:53:40 +0000

    close correct connect period Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
