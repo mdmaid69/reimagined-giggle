c3ff6dbd4de0210270ce492a0a452f76d1750997
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:35:02 +0000

    different language bottom nor free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-print([x**2 for x in range(10)])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
