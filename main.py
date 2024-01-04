8f77b5d0556f2df322f0d12889f8ce1fb23a42c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:03:10 +0000

    history spring communication weak gift

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
