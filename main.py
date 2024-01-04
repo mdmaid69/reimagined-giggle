cac7a4fb5f14512e582bf068f1c0032d6c86f19a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:46:32 +0000

    public organize born natural flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
