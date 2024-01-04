6e2a1c077a565942e1a96e62c549a624734a1b35
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:21 +0000

    slice see first properly snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
