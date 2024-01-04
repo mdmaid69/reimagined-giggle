6831f337eedcb0fde73c9a065818417c50b13feb
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:21:20 +0000

    bin 179bite identify less importance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_max(numbers):
+        return max(numbers)
