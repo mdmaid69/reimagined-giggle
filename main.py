56a66f3b981a4877b2ed3d10f198c31afd8d7d83
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:22 +0000

    Qq issue guess upwards pretend

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_length(array):
+        return len(array)
