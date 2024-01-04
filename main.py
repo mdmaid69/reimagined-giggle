f45091559ff0bc9e95b7cee7ececd54bd662a447
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:30:51 +0000

    record bus doubt drop 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
