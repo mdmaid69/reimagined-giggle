357c8883e9d5ae298dffb1866cf5a763bd68909b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:47:47 +0000

    course bring star problem illness

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
