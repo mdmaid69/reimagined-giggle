6cfb13e5745e2549c05f48310eef5ac1d5bd5e37
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:52:29 +0000

    play treat glad slide protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
