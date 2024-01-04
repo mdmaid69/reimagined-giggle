38cf8a8ed145ecea971dbfb6869a60d2f1512e1a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:11:44 +0000

    uncertain anywhere guide transport summer

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
