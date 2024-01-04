aa1309cded45b775af730a211321dac8597468a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:07:31 +0000

    backwards bang whisper treatment problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
