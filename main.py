70423c82d4784211a8c7e279f44b8ec8c3864aba
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:51 +0000

    spend measure zero cap round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
