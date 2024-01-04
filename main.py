3ef7954f921e64d63d19f89f37ce7daa75d05fc7
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:38 +0000

    treat already match cross moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
