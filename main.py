2416eaaa2a8a52515f569c2025236c2e23148550
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:50:11 +0000

    perhaps limit quick pencil afraid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
