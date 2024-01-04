2fb83ecae5c5d8a497b6405e81c0079d5750d4fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:23:26 +0000

    chin near interview stretch telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
