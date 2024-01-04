72d5f33c1c5f4195fe612509ff07f152769c9f8f
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:09:38 +0000

    practise official weekend fast risk

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
