16b0ecc53346916300c31fec2d4d8f3fcb80b59e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:56:05 +0000

    boat laugh burn smell rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
