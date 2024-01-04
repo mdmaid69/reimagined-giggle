663c1921407abe1ff3dad28a474eb8a488b36e8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:40:37 +0000

    kick fierce introduction bush know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
