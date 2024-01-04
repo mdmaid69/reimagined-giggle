6ff81d80908f1967d4f39a098cb9b55361f09cf9
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:22:02 +0000

    related Bb some mess trousers

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def cube_number(x):
+        return x**3
 import array
-def get_array_from_string(string, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
