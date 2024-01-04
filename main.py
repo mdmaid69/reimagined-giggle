f27aa7b93ac7eb3d50b27941d37995b82ac7d568
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:14:02 +0000

    passage second know kind influence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
