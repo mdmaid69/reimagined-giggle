cf232cb9a0061e6544fde7d5f6fcdcb074f37939
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:42:44 +0000

    gather clever separate accommodation century

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_min(numbers):
+        return min(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
