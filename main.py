807285bd2f2d2fdb68199e1d7f92442179854d3b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:46:32 +0000

    gap centre recovery raw separate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
