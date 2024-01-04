1339e61df2c5da492867fdafc35edd0993ff9d11
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:41:45 +0000

    move shoulder contain correct formal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_itemsize(array):
+        return array.itemsize
