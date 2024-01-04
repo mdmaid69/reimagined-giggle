68f9e3a32eb24fa9f34896b0ae5354792117f0b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:31:15 +0000

    present godown little getback accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
