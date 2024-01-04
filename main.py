51b505b5f34e078251a94e11a833ac4c98a36607
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:47:10 +0000

    away whoever fasten stop receive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_set(array):
+        return set(array)
+import time
+def get_current_time():
+        return time.ctime()
