ca5a0cb0fffdbfd65e327e9e22d044e1c30b7c4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:29:27 +0000

    modern success common event conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
