0e45295c4ef0b4f19a170aeae2e1ac91299781b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:15:43 +0000

    message level lean full feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
