6317bdb64a56051159f8e8f224db0f1f9f37bf66
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:29:02 +0000

    add base Pp average enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
