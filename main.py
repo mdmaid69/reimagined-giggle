f08fe6a0ee7ed9db8281d0c94e38ec25279403ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:44:45 +0000

    draw cope callup charge travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
