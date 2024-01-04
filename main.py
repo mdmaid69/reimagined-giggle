133be49c8df4d27edb65a8b46ea5e58b52d82799
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:25:47 +0000

    Ll motion act intelligent practical

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
