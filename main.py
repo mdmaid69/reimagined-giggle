ca02ba11bf76238d0060b73134efc4d4fee8fa46
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:08:46 +0000

    Mm thirsty concerning distance false

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
