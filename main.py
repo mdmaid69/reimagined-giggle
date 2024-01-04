1a66bdba896deb4eb9c370384e2e436e1369c85b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:29:13 +0000

    challenge exist cost along surprise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import collections
 def create_counter():
         return collections.Counter()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
