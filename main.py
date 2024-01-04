f56610107e49460ba8d84e9d81a0b2730db2668f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:17:53 +0000

    paper woman 564encourage bite raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
