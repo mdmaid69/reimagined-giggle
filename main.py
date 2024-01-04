6183d2bd00ea7973c9e8b8e8879f373f025aa8d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:12 +0000

    curious rich turn theirs foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
