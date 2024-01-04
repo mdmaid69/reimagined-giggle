d19674952c528e1feb1395ddba0d988a433a8970
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:43:30 +0000

    force admire area intend plan

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_speed(distance, time):
+        return distance / time
