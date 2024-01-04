9130b8264f9d9100c1b6cc9938307f57cb9aed45
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:08:21 +0000

    stop colour complete guard fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_work(force, distance):
+        return force * distance
