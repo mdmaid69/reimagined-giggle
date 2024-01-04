338e131e49a300cd09a17cde1a392074492ac54e
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:31:19 +0000

    too peaceful very speed usual

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
