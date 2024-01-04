57f6303626422e68f65bfd6f42be84e72e1a187d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:34 +0000

    press children exciting holdon march

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
