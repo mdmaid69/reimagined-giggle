3c49d2e0b212fd6a8173efb78d1d67d717a19740
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:19:48 +0000

    top crowd remark purpose address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
