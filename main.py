8341e7a89f850e3b1dd959a50fc5332cd5322283
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:00 +0000

    themselves continue thank clue except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
