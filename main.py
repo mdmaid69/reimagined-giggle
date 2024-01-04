020df808a7ae638cc073391a3198d092673981bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:46:23 +0000

    shallow Ee point cross bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
