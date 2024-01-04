bcef44d34045cd326c8cb6bad0b332c6390f2b99
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:45:56 +0000

    brother post danger minimum difficult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
