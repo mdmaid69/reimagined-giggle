0c5120d4f62c4ced1b276703c833cbedc52eb5ef
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:11:09 +0000

    hear warn bad level manage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
