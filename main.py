9f54336606315a89658692a8f3b8370732168cad
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:20:16 +0000

    week straight time left tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
