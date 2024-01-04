4ee1016a1d14cdeb3468394d172a1b63cbe723c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:40:28 +0000

    tap manner road mark discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
