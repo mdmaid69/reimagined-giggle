f5a225cfc2bb55830f55e114ff0585bf053daab2
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:44:48 +0000

    571enough meaning safety workupto borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_int(array):
+        return int(array[0])
