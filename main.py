34a7057727d749304123445da8632397ae2f504e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:35:45 +0000

    label early mad crowd respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import sys
+def exit_program():
+        sys.exit()
