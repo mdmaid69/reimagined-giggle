cdccb7c1a34eb28c1d6a77a51ac6c78d9cac1aeb
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:51:22 +0000

    Oo rubbish eat else certainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def cube_number(x):
-        return x**3
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
