a3ebe9878d3661c242a5a8a6bbce3a05e2ae7444
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:07:51 +0000

    recognize unable keepup strength instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
