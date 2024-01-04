4f52cb9c8e14e0454457114995fba7b98b24973a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:59:12 +0000

    almost shout comeoff stop picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import getpass
+def get_username():
+        return getpass.getuser()
+name = "Python"
+print("Hello,", name)
