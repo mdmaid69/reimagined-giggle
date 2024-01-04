dd36b426b31ea3464c922a02149b9a00718ba76f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:58 +0000

    slow exact spare turn support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import logging
-def log_message(message):
-        logging.info(message)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
