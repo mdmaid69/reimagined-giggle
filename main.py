7763600ec9e03e48050505d592a018eb212b393e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:48:24 +0000

    slow share interesting second high

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-for i in range(5):
-        print(i)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
