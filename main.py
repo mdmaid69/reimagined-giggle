643d081d634a162dd7bc9daafcbd842c01b8dc9c
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:11:52 +0000

    side during search birthday admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
