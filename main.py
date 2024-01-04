370740e8a5d7f9852471b5b2f0887d9966227c65
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:35:07 +0000

    turninto nothing smooth fall whoever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
