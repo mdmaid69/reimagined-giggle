52cde0d53a78cd743b63c614796724ab8b9fb9cc
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:03:59 +0000

    often disgusting indeed everywhere care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
