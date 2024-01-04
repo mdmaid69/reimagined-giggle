11b7eded27fde13338d49cf699c427c9d0b41df6
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:57:03 +0000

    fix mistake wide clean enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
