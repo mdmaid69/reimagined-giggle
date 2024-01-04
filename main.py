7636435d6ddd87e7a703971d21845166544de39f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:24:43 +0000

    undo suddenly bringdown scared light

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
