33d752ad5acd3348074dd3868912e948eb3c0268
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:14:18 +0000

    classroom certain test perfect shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def subtract_numbers(x, y):
+        return x - y
