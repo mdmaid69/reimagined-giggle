3a77cd7f9b6c6aee9b449aaf5d62be6a4b9181d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:19:20 +0000

    club jump much back begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
