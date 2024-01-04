c17515970081a8d6b77fd4c117c3688ce37a95d1
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:21 +0000

    go tap hope stamp people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
