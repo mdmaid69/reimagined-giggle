e3283a33a4a829340fd05ff8d2a92edb4938ac30
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:31:02 +0000

    almost fetch represent upset push

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
