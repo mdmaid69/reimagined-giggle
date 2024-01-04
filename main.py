ccd50597a6cfa9b6e57b7c087b4dff3c36e91a68
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:58:07 +0000

    cool begin adjective autumn takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
