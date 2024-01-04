dcc1ccdef0395efd3858d8c341ab225e3a2302ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:41:25 +0000

    later invite exchange combination apply

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+  def multiply_numbers(x, y):
+        return x * y
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def subtract_numbers(x, y):
-        return x - y
+print("Powers of 2:", [2**x for x in range(n)])
