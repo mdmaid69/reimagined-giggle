5f3dbd11ebad5bfbdcaa4475f68f85f8c3250277
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:31:07 +0000

    sentence benefit cause deserve hurt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
