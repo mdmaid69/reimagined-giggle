04c937e5005c9569b6ce18ec04cda66b92421b29
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:00:05 +0000

    tired name getback move themselves

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
