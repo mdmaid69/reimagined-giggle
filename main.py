323ec25d33f097fee6ea50e7f71b3dabd8e56377
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:52:07 +0000

    makeinto suggestion food safety improve

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
