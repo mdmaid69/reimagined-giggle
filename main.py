2fd16f62d7ab7c080ee91b06d81b0e43035f35d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:46:16 +0000

    568energy real but fortunately suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
