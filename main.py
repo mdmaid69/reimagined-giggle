84c1d7695f02bd6067bbe3f921f588ebeb3f4559
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:07 +0000

    near part tomorrow factory during

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import random
-print(random.randint(0, 100))
-def multiply_numbers(x, y):
-        return x * y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
