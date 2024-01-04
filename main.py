991c49c06bcae95aa9fae0f24fd71b62507be0a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:34 +0000

    way quickly bringup acceptable cheat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
