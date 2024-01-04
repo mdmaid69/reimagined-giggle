fcbda47a8c4e530e17150e2ab9fdc071b5736e5a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:11 +0000

    machine measure many 565end report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
