7640c456ecbd975dd2b28d51a6e5321c26cd34bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:10:54 +0000

    test Ss image job violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
