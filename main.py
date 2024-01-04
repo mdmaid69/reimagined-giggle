942f1dcad6f3e85b682d823b8e5a996d2f5fd449
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:22:06 +0000

    runaway hard more refuse further

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
