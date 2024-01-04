0b228c3b43cf852bdd88aed159964402180bc4fe
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:08 +0000

    scrape sky rather creature weekend

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
