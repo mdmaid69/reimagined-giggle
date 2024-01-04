fc76fdb88f5432e5cf9df26e78c0b284502dc4c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:49:03 +0000

    song cut takeout succeed view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
