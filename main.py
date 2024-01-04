2c18d17095fdc0824601984a857126d70e643fba
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:13:44 +0000

    table though independent mention find

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
