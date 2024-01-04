8b67a0669e0975065a7146d509fa55a09e62d5ce
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:18:21 +0000

    rid several secret pile damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
