b5c882dbbef3b02f25f655858adc3214711ebd1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:22 +0000

    boring rub talkback ready ago

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_time_since_epoch():
+        return time.time()
