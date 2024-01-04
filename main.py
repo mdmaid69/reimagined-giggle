b892e10021bcbfda4d4e8d9313cd902b6dfad4d3
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:59:20 +0000

    eager behave float shoulder notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
