ac7999e5ebd0f6f87cf5cf6cb803f72555dd2685
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:59:23 +0000

    deep gap tooth sticky brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
