0448b0b7f42fdef41e88ed499ad88e168212be36
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:11:49 +0000

    ground simple mean treatment piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
