c39d99de7d41735ec79ee5c362c59b2f2c2f3ea3
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:42:21 +0000

    challenge friend soak present emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.ctime()
