fd35a373c3c69b96e6d2a78232896375c50f6c80
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:14:56 +0000

    slow during prepare wash plastic

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import collections
-def create_counter():
-        return collections.Counter()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
