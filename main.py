2d799e3f1c01749a928df8dfc4249e09164b3656
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:48:36 +0000

    crime father journey dark pronounce

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_max(lst):
+        return max(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
