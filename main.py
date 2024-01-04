328307ba3757935fe118a4dd44dde628f4ee284b
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:09:27 +0000

    all will 572enter boat trap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import random
-def roll_die():
-        return random.randint(1, 6)
+def is_odd(n):
+        return n % 2 != 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
