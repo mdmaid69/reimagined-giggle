74b6c53a509466f0ffd5967c8b8ec0d978d8996a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:04:12 +0000

    spite involve hour wrong noisy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_acceleration(speed, time):
+        return speed / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
