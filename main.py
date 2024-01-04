97b1fd3a7171ce1534b156d621810c7cc87f3f98
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:56:14 +0000

    Ss reaction neither coffee way

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
