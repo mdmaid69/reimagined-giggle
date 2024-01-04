3ded840bad4fb9595718cd96a1993c1347def9e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:44:18 +0000

    nowhere declare responsible thin spare

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-print(sum(range(10)))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
