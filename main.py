029ff0da251f90e1f49bd89f9a6ef36d2883eaa3
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:57:15 +0000

    comeround great on strict load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
