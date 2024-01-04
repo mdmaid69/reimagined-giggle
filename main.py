32fe41e3776127c64164339757e63d4304460e51
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:12:11 +0000

    giveup brief although cut miss

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
