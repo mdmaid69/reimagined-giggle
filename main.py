6dbbd300aae359c383b83fe6bf736ebfed31de1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:20:06 +0000

    honest fit board comeback it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def divide_numbers(x, y):
+        return x / y
