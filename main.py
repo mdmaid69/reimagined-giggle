cfd9e505a5fd449200dfc0d5c8472ed7dfe1223e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:27:51 +0000

    shock brother ask really recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
