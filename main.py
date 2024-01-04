b9ef34c9f5ee33f56f3e3388895c00af50c22ba3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:12:53 +0000

    know twist taste record split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
 numbers = [1, 2, 3, 4, 5]
 print("Squared:", [n**2 for n in numbers])
