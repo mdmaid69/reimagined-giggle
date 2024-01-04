79f3ab023db04f77bbb002980be218bfc81277bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:32:01 +0000

    fairly type that morning development

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
