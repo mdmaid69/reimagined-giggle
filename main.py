a874afbee5b93d57cbc14dee91a4df8eae93d899
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:54 +0000

    never onto open soon people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
