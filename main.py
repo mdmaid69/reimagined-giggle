235439f884c423216522158da4fd07c625f062ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:28:20 +0000

    black borrow rubbish row bear

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def cube_number(x):
-        return x**3
+def find_min(lst):
+        return min(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
