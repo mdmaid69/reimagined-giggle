b825e07a609cc75d67be08025eb259b259a92531
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:00 +0000

    right rain runover good wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def convert_to_octal(n):
+        return oct(n)
