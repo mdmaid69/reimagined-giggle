b57b254367f58e7217f54ed2842c73c76c80d131
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:39:38 +0000

    dry tip culture reserve advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
