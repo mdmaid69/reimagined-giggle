9171e5dd22e65ed5d6c28a6575b8ce08a265601b
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:05:34 +0000

    post level bend bet murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
