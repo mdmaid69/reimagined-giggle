cdc5d61767180e5a6dda791605d751aa77bc49df
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:53:13 +0000

    easy how apologizeapologise yesterday arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
