2a471e6369f583757415cc860359bab06da6d1ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:51:13 +0000

    activity onto bang broad hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_density(mass, volume):
+        return mass / volume
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
