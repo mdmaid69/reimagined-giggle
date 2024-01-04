be814c8001b7825b0f743a6b772683c29802a745
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:52:33 +0000

    culture space low exercise movement

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
