581e26773dbc9078e8b50664edab3db061232d69
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:57:10 +0000

    trick pump react disturb comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
