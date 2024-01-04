575749731b52ed9454074e8adb7bcf0fd64345ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:19:40 +0000

    interested secret bottom minimum party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
