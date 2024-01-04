554ccfb6f244ae8c916d614c27e1c55259d0a78f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:24:43 +0000

    scare car tape instant seem

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
