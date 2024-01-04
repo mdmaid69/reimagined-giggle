33300a28cf6ab7bdb1a3e2149771ab08159958af
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:14:52 +0000

    frequent section moment surprised cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
