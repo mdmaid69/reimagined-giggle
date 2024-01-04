087b015ef0cc32b7b79356962f538c98c2fbb368
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:29:49 +0000

    issue fault likely memory where

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_min(numbers):
-        return min(numbers)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
