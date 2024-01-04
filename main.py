2e88ce559cce3a2b54cd86e4519e559a8811b4bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:34:48 +0000

    type arm key ground tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
