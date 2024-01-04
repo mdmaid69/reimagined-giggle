c34ba2c699156591a2948ac34c1f7430c3221c2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:58:40 +0000

    computer cruelty event lend post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
