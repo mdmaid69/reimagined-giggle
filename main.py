4d57e8750b84ad5d7d8a6d93d16b9f2bbc9bc078
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:31:05 +0000

    ready school development there many

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
