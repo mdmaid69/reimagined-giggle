6117c23281bfda28cc94a79ba0e749f16d605dc3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:01:21 +0000

    centimetre Ii meal certain sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
