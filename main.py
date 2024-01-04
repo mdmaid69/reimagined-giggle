8a6688da8b90a5500cebf9d71036b818f0962530
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:11:41 +0000

    defeat own worried autumn sure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
