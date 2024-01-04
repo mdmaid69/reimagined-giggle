191c7304471e92b01daf3d83c087fa1ef043671e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:29:39 +0000

    woman most experiment gas whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
