f7b2c1d7dde2724ca9238ad39724dc62398594fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:03:58 +0000

    reach hit innocent wrap last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_current_directory():
-        return os.getcwd()
-def reverse_string(s):
-        return s[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
