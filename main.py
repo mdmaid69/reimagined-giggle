c90453599c69ac5b601e550c8a4ffbb655258433
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:32:55 +0000

    opposite frightened hit distant spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
