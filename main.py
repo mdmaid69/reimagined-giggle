d44694f58fcb187542048bd6265bd91ecfaa5a6a
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:08:08 +0000

    fast communication problem trust smell

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
