9ae1c5df2a8251f1742cff925fe335cf9af139ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:20:47 +0000

    boat practice slight project unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
