cd84b01b8d5e0e63c9479d0ad09ce07a0ff2876f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:18 +0000

    sound branch refuse middle fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
