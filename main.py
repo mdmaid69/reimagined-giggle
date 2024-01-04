db402f6c96e178523db2e65f3f0bd90469cb907f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:54:10 +0000

    yourself contents objection band rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
