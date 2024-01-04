e03b4e046ae2814c91725f65421d61a96161d593
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:14:57 +0000

    bright frame later related late

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
