6ee02d2e87469dfcdfe5b6578ccd0f4510b8f34b
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:45:19 +0000

    control win yes keepup maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
