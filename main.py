182adf42f6645a83c0becc5f790686ccf923c41e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:15:13 +0000

    bag generous somebody claim or

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_elements(lst):
+        return len(lst)
