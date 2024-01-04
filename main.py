770fe1aa3d78f5864deda5eb9f0563b2a021c335
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:31:15 +0000

    everything pile then right flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
