a859f4218c233d97b9043bfb5434f9ec46582b2f
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:08 +0000

    exam programme unlikely female holiday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_max(numbers):
+        return max(numbers)
