c442b8363acdd2659d4d820934d552b9773b6eaa
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:53:16 +0000

    listenfor near bottom talkback scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
