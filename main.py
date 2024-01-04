214e390f842f63ec68a4debf3f84b7f8fb24c349
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:52 +0000

    normal comfortable clear tap suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
