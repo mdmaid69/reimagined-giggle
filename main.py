1c39ae1124432bbc397a8e1b4d7438bbc07d0751
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:38:04 +0000

    watch considering beat central truth

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def clear_array(array):
+        array *= 0
