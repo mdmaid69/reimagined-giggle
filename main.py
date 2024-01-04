8885748f9ed20398ff4241624da05e9362f6c6a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:21:12 +0000

    this it lookback(on) space mysterious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
