32b805895f0af41aca3c5d9d6d071aa93b5b3040
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:39:41 +0000

    smile connection strong fond subject

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
