565c853b3e401aa3f5d0f73aeb0e0d55a8114ffa
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:52:13 +0000

    stiff conversation along we record

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
