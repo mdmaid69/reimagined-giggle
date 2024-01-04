b198dc21fc1f53a3e88380248bf9f87535f7bede
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:18:30 +0000

    trust test few reaction ring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
