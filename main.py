e9a7927e6bec33d732d168b1cb69ac68cc41084c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:14:30 +0000

    listenfor anybody may arrangement she

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
