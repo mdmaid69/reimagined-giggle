2bc44b4c55996e55dace326058af0718b5c0d38d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:47:39 +0000

    report calm main weekend bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def is_odd(n):
+        return n % 2 != 0
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
