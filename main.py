4307cf347ca0b7ade1e102635351b92af9c50375
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:01:07 +0000

    suit behind valuable wipe everywhere

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
