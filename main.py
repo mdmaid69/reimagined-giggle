fb3817a6c9105a36f001cb7858287f96b6efe4e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:59:19 +0000

    turnover sink may workon platform

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
