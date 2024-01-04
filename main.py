95698746c87885d1fbbcc4ab52107d986a15c443
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:04:21 +0000

    worth odd burst sorry bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
