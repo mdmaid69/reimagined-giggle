95a1c67aa801ba7757a4a7d4d3af437b841db48f
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:28:45 +0000

    list takeover whole whisper squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
