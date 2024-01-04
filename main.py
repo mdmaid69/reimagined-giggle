86313a12d614c2a3bc4aaa37f187f5e91263a170
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:41:07 +0000

    snow fire shout praise tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_tuple(array):
+        return tuple(array)
