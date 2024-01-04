83b73c6a297c93d913fcdba655e98550026f6fd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:15 +0000

    furniture next grand Ww emphasis

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import datetime
 def get_current_datetime():
         return datetime.datetime.now()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
