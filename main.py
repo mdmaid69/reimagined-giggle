cafb179bd9a41a39df03861e713c43150837df4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:28:48 +0000

    own mind find damp right

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
