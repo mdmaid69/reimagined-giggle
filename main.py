7de1be61f0806394c88dea6a01a9cb25e8248fa8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:46:17 +0000

    goaway objection rely store sound

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+for i in range(10): print(i)
