3ecf52f45c90383bf7787bb18a183634d6a17f58
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:01:19 +0000

    partner Itself confusing private doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
