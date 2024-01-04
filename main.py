3afc5313966f7169592b4fd59ff2275c6aabef75
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:38:02 +0000

    next not yours once stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
