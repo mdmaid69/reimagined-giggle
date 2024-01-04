12909eacb0e6b52b03267cc68f6b1ada4a28127c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:24 +0000

    whistle amount murder there pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
