a79a5ced12766ae1ccfa7f2b567f6247911f0222
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:20:07 +0000

    insist include identify object talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
