037154aae4cb5f30110199fe803df032aa33e727
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:04:37 +0000

    loud flood prefer lean line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
