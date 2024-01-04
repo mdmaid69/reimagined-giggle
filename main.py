82c7cef3299950a136e371ecfbe200b26b661e0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:09:20 +0000

    tight realize joke oil copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
