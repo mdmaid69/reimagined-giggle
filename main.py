c64f6b69021b009682cfc80a5c2cc899e5b75483
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:14:18 +0000

    crowded past thick anywhere beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
