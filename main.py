4e15b9c71baaf12bf863495e49f7fa7d06afb238
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:28 +0000

    progress plastic dull answer same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_length(array):
+        return len(array)
