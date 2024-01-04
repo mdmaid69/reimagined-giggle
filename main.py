0051872ecb7d950d9152e8e20391b4eac88362fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:42:39 +0000

    duty little chase 567enemy but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_list_from_array(array):
+        return array.tolist()
