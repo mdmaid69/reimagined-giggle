cf77baf779e8aa937b6e6c634b3440ceee1b9dc8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:37 +0000

    uponon active strict thirsty serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
