df7e1505ee91075f9248c6d068c7af74ecb5f5b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:35:41 +0000

    consist row fast convinced government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
