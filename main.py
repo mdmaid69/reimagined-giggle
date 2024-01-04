2e4624ec9975ff20054f0efffcb1c0575f50b222
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:21:06 +0000

    excuse continue regional touch already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
