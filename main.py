84ee4f50d7c8655de0c59845316efe83999b9b73
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:14:09 +0000

    improve pay Ff telephone reliable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
+def calculate_pressure(force, area):
+        return force / area
