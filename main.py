4b7c53eeae2d7f4c9d8933bba13185424e7fd24b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:18:37 +0000

    happy world classroom soak exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
