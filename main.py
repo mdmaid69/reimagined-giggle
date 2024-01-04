1dc8ca9b77cfb23a4ffa9ae56361516450b72fcc
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:56:58 +0000

    hospital whisper normal difference hurt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
