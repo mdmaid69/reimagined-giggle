4e05bfde89f0306b14c77f737831452f6a215253
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:39:39 +0000

    job pick supply general kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_speed(distance, time):
+        return distance / time
