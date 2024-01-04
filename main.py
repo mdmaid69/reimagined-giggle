8d0a404a786094a2810e707f7c2287de6a4b1d82
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:54:28 +0000

    crash news stare fail boat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
