849574518f61912f064a3bf59ff88a4ffaade0a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:35:35 +0000

    present declaration attitude cross geton

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
