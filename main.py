0e5990b60d48b9aa189acced3efa39b34d26ce0b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:09 +0000

    prepare car key kill avoid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
