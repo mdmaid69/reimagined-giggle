9c7bca8550b801d356c12edf250593d47f005ff9
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:48:14 +0000

    violence be acceptable date cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
