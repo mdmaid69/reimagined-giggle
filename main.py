70bbc8d1d4cf78eef319937db7f134e38d0b44ab
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:28:00 +0000

    various boring properly base comeround

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
