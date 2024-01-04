4e35f82a14f33b5c80044b4450a751a35614ff19
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:03:10 +0000

    statement room giveback major foreign

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
