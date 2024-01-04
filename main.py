e02b94401b810aa79321a48db01c8369e0bdbb59
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:15:38 +0000

    store dull fault excellent famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
