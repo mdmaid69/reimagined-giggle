425a644db39eb3eab496c28ec36ab8957d538509
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:46 +0000

    worse past cost uniform boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def find_max(lst):
-        return max(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
