cff2c32799f5e67f213abbd21cd6f7b81e520d82
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:21:50 +0000

    machine uniform capital sharp report

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def reverse_list(lst):
-        return lst[::-1]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
