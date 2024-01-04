e03c35a75061f67554f06595240ec816a9beac1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:26:50 +0000

    flow priority new on related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
