0e7c95d8b49466027a2dee3fa65f7442e6daf6c1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:32:00 +0000

    best perfect term show music

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
