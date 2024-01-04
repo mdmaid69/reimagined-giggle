aa2abae65163ad52afb8a638a8309a1510082eee
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:41:45 +0000

    fortunate pick mood steady record

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
