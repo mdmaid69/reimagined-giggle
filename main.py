69eec9c386bc31506a95c6abfbbdf0ffb81e1bb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:22:34 +0000

    make block human original open

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_max(numbers):
+        return max(numbers)
