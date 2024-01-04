ce9e25bae304fc7593d5c9cc2cc12c160f2dab63
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:47:53 +0000

    duty comedown black match recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
