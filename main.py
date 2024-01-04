b603d88d41ca62d44674ca941e7c8cf4f0e5da44
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:38:52 +0000

    spring otherwise himself for beside

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
