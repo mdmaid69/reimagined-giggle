1a03ccaff12c1a9f67e7320a817a1a120ea630b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:18:01 +0000

    serve release lookback(on) shape human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def convert_array_to_bytes(array):
+        return array.tobytes()
