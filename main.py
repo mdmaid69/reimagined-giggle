96e2220fb652263ac5fb3c7461239f04a01be84e
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:32:55 +0000

    train government taste properly tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
