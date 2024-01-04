3362020c1f40d362e7db5197f713d421ece000e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:17 +0000

    sorry Oo ahead computer doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
