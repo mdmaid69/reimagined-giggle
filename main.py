e9a193094d725286a0b5e9dc00e02922cf10c18f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:24:29 +0000

    uncertain behave used holdout need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def greet(name):
+        print(f"Hello, {name}!")
