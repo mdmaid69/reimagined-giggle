5f4ba96e2606dab425cb29fb387338e052f1b441
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:03:12 +0000

    nervous slightly pretty accidental cross

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_density(mass, volume):
+        return mass / volume
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
