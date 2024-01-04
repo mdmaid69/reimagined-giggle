c893ca467fe6ec7b654a6444bbb057b2f1415d27
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:22:19 +0000

    need join medium road almost

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
