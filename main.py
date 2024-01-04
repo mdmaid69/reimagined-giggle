384acf513932a768f554c2fb621b7c597f3e54d8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:59:51 +0000

    callback transparent 565end sure steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
