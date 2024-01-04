995d0b99bba3a89be0d6e5a1e2349b86666dfeab
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:53:28 +0000

    roll dream tell stiff free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
