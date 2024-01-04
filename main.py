1d9bd1233b0b70aa9d77dd53d98df2d4a7bfc09b
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:16:18 +0000

    news slide Cc advantage pleasant

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-print("Hello, world!")
+def is_odd(n):
+        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
