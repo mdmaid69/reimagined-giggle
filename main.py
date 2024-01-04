d3f39f700ab295079f46f52173217e6cdde9aacc
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:25:33 +0000

    worse whenever stamp communication ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
