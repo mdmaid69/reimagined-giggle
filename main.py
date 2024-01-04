1d72253835d677e93a54d922d940017f9a21fbd6
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:24:02 +0000

    finally floor butter worth mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
