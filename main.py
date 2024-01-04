832441c617038ad45fe1cf69164fe068b1034b23
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:52:48 +0000

    during sweet comeround ordinary valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
