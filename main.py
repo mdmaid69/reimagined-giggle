005b7d980984a64b93399e55f85f71ff59d0c5fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:13:38 +0000

    small experiment I shadow surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import sys
   def get_python_version():
         return sys.version
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
