08cae97c7066e0f19020af1882a00bb0fc2a6b27
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:02 +0000

    holdon war operation floor mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def divide_numbers(x, y):
-        return x / y
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
