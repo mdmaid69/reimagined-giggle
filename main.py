dcb640d78c91d13758b1e00991cc2e23b8e12823
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:32 +0000

    occasion here meet heavy door

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def divide_numbers(x, y):
-        return x / y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def sort_list(lst):
+        return sorted(lst)
