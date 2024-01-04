13d1d96f3b898d47e6bb2b555d4d13a4848f4b18
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:53:54 +0000

    meal no join force too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def convert_array_to_list(array):
+        return array.tolist()
