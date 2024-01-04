29ed6b15f9dd00be2067decef12e1f45e02321a4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:48:27 +0000

    education settle shoot weigh punish

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
