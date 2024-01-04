32f108279785e10b7a0cdb1a42432780a1e3eb39
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:21:42 +0000

    reserve yet raise branch pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
