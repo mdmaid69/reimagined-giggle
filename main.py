3bc60c39b1504ddf890337e7f33dda3315c9bd0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:33:03 +0000

    squash solve general ever hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+print(math.pi)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
