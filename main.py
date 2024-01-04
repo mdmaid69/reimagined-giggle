565616c9cc9a9a789ca2c2d7aab18238ebe9698a
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:42:26 +0000

    deal emotion perhaps smile spring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
