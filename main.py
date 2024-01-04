081733d8ca2d185233ada7aa5dbbcbd03cadd672
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:14:26 +0000

    never centre safety store main

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def convert_to_binary(n):
+        return bin(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
