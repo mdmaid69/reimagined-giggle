53c562ac20f0635fc3b922c66ec758174cdbf7ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:07:00 +0000

    oil miss rest exactly than

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
