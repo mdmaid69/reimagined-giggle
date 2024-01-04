1aca41b6c2df6673433fbccf52bd275e0bc0b343
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:32:22 +0000

    trip turn report follow complete

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
