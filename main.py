f3c765a43e6b7530c5e399b7b20384581e6ebc5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:07:09 +0000

    shake judge scrape previous cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
