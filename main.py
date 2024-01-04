e04d4ccb14e750ced4705ea9922eb651fb34af16
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:52:10 +0000

    agree throw hers down doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def sort_numbers(numbers):
+        return sorted(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
