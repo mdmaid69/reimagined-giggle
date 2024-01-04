5469fff9fb64402fb31224941e2c5681ad703320
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:29:15 +0000

    properly normal laugh person safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_block_size(file_name):
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
