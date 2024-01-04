6e8ec90c25c99c700dc9a1173586e2146095acf2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:31:57 +0000

    list unfortunate congratulations immediately dark

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
