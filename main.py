84730b36bd059e8ddf2926b3ab3524797893d09e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:26:35 +0000

    pencil tear can perform easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import platform
+def get_os_info():
+        return platform.uname()
