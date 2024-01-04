97a15080e066114be5ce911f0ea79fff2b163b76
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:10:41 +0000

    justice next declaration account wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
