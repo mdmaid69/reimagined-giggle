b33f1fb41c9bb9a004b170ea06878109a44c4366
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:46:40 +0000

    whistle song through subject interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
