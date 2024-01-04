5bf774ce4db549bfaa2cda61d2b07631ad49e782
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:41:32 +0000

    produce development before table insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
