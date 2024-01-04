068715f3b0658a1301408fee03b9a54343752678
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:07:09 +0000

    careful go open truth improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
