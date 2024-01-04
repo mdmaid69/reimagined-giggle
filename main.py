e26c9e90d78287e710cdda6c4c2a2c1c4cab41e4
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:14:25 +0000

    sink development welcome breakinto dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
