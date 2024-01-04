f158ec4278de9b5929fb07fe0cb19cb84d31b672
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:16:59 +0000

    keep expensive typical old scared

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
