ad9db752c026bf2604d2f5f4d6062f036e6ce859
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:10:26 +0000

    use kind shut tired field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import sys
-def exit_program():
-        sys.exit()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
