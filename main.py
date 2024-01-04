0669c2efffd9a1359a21d23b1cb1e4f426b035cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:19 +0000

    prize divide provide passenger position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
