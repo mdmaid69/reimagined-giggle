17b67e2049d2b0290ae2e3638096b046eec1059a
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:53:48 +0000

    arrangement matter practise colour neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def reverse_list(lst):
+        return lst[::-1]
