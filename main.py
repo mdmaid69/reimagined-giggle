4cacc888c1e241dc1afe8390e6e02477d5cb4c9d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:17 +0000

    by drive exact scrap scheme

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_power(work, time):
+        return work / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
