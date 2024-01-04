a1d80be5944d603fd7ed52c0df528f17afa8ce10
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:19 +0000

    pretty mood snow force influence

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_work(force, distance):
-        return force * distance
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
