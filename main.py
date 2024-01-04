d3043daab4c543f2cf56b1e57a74a264c64b068d
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:22:24 +0000

    getoff town preparation sensitive bye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
