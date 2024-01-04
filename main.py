1510ab522132cfba027a54082ca862da9922fa0f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:21:00 +0000

    history problem so emphasize start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import time
+def get_time_since_epoch():
+        return time.time()
+import array
+def get_array_as_repr(array):
+        return repr(array)
