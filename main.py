fa841f749d1b5cf512f8b0a68d20214f6030a648
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:07 +0000

    head agreement create your telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
