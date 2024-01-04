8838cad7c45655d10532c252113972fd1824f7bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:13:27 +0000

    of improvement never gooff comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
