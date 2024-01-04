af82200cf9499b0fd19a8152ac8fd4395260ea2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:30 +0000

    want on objection activity cap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def count_words(sentence):
+        return len(sentence.split())
+import numpy as np
+print(np.array([1, 2, 3]))
