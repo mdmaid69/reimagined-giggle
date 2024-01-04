0d2e0cdb0be94d7d5a40e7028c9eec274785c1de
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:35:50 +0000

    flat invite dig giveback negative

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
