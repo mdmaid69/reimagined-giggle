c6d15afbafe82f3fca1ee0ea5250220f113d3f30
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:15:25 +0000

    prevent free however tight medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_sine(x):
+        return math.sin(x)
