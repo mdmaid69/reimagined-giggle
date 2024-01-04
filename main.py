58cce90631f4c6ce670cd016e499a50caa18d0af
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:58:59 +0000

    back result purpose confidence refuse

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def find_max(lst):
-        return max(lst)
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_str(array):
+        return str(array)
