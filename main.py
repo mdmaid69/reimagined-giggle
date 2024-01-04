8ad8a422dc37ce28bf34342e457ce6ae97e7c443
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:41:53 +0000

    cost defend process thanks what

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
