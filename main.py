6494c17b4536a92b9cf35db396c9ceeb40cf7e67
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:22:36 +0000

    his 572enter voice post innocent

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
