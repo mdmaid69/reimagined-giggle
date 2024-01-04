6d7b5119dc43d161e4760e405766dcbeaa4004aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:35:27 +0000

    fly finally joke equal conversation

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sys
+def exit_program():
+        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
