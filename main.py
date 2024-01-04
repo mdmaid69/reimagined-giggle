d7962d9e32b0d717df145fc6d1a9af5b95f14317
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:36:36 +0000

    doctor patience gift related makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import time
+print(time.time())
