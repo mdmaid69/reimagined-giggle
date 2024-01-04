0844f2d54732b014ae5537d58aee06e89e0e795b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:01:22 +0000

    talkinto doubt joke belief shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import collections
+def create_user_list():
+        return collections.UserList()
