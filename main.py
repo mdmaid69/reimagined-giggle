119bc811eeb59427726815eea4eb896483103364
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:33:36 +0000

    short thought inquire shall persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_repr(array):
+        return repr(array)
