85568bfadf868212d68f9e1d9acc580aae468276
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:52:49 +0000

    holdon blame soon grow petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
