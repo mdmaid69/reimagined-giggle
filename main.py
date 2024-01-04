b297d2737d05ed49e76ee75c63d601be1cd7ba37
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:15:25 +0000

    neither now 572enter talkinto involve

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import sys
-def exit_program():
-        sys.exit()
+import os
+print(os.getcwd())
+  def sort_list(lst):
+        return sorted(lst)
