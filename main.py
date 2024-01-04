50c7ceb7fe55fbd17dd07cbb945103606b938341
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:50:21 +0000

    cross effect catch purpose soon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_area_rectangle(l, w):
+        return l * w
