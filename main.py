f227974f85621cc860b3ba3dcc1bccf03a3443f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:08:34 +0000

    spread practice concern sudden friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
