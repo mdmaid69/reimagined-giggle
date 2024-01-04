9eafb7133b454776b153ed94794e622dd0b1bdc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:03:23 +0000

    seriously brush announce during degree

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
