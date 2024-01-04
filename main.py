3ce2db2d2c80cf60b7288cb5391027c2fef2e316
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:47:11 +0000

    report once practice dream step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
 import array
-def get_array_typecode(array):
-        return array.typecode
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
