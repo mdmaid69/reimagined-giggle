68d2f2576ac868a06c628e923f0727163164a445
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:36:42 +0000

    lead all somehow slowly wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
