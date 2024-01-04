2c689ed1824d896d32f7c5cf0a23eaadb6f4296f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:19:06 +0000

    activity round taste area plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_set(array):
+        return set(array)
