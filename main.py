78e90bb80694971a2f709e6fe1ea7f754b5913f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:47:55 +0000

    geton Kk hurry window press

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
