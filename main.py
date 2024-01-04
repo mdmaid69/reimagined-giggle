9a0a28bef0a6f94949c13b438b9650f82d25749a
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:03:29 +0000

    noone load she single formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
