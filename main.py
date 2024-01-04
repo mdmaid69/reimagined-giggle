45e4fbeee97cf6d88da71675101bbdacea7a7d2e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:31:45 +0000

    to determined near play slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_max(lst):
+        return max(lst)
