c185cb028856d14544334eb544bddb53edfd3b8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:22:46 +0000

    embarrassing principle come thought secret

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def subtract_numbers(x, y):
+        return x - y
