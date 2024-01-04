56d88e74ac557c52dee6805c8a509d80073d3fb5
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:35:56 +0000

    feel autumn start embarrassed example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
