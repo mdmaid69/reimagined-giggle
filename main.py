cc3e966a8597cfb503b29a8e72c32aecc20a456f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:34:59 +0000

    crowd allow walk like but

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
