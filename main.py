3f44ddb66a2e8808f39f9aba3f6b6164158c3e6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:37:41 +0000

    keep already large keepup guilty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
