b7ec12837326ff02f2436bffc35483326881ba30
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:11:48 +0000

    church clean front trousers exchange

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_octal(n):
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def convert_to_octal(n):
         return oct(n)
