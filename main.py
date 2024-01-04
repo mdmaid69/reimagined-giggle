01836c98df2907b40c70cbe1e73b93183e11280b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:11:27 +0000

    aim earn fire person while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
