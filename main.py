504828643ab4a8788661802bd5e5a30db306fb8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:28:42 +0000

    news future ride watch motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_bytes_from_array(array):
+def convert_array_to_bytes(array):
         return array.tobytes()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
