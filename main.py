3792cd0f94f3e83de1bfd4f68a1713001a663fc6
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:55:14 +0000

    left meeting respect finger maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_palindrome(s):
+        return s == s[::-1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
