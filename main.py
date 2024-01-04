9f912a7c75e5d28c9a10ef08e04856f758329d8a
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:37:33 +0000

    tough different thumb pour first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
