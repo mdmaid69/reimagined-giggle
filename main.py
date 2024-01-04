e1215516b6f8b3da5e1fa0b90e142ac7c84d12dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:22:04 +0000

    attention takeback all soon gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
