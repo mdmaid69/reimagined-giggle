fbd9514fbcfdb3e9631c9dcf7d7b26d9f0224882
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:14:30 +0000

    kind warning rent mention exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
