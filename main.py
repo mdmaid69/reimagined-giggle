d1d42936f2c7fcd9429961f1c4cfab702f2c8dea
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:33:53 +0000

    wonder dead because bang with

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_speed(distance, time):
-        return distance / time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
