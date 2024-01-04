073385581a2930c9a037180e36160b3fb7e8a19f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:42:14 +0000

    kill fear dangerous container amount

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import tensorflow as tf
+print(tf.__version__)
