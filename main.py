bcd413ff010e34620fd69d8fc8b0a1f80798b1df
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:43 +0000

    brown unless true contain dream

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
