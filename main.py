65c4c6e784f0f425000fbdadbf1849a5ba4fb9f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:30 +0000

    bend some tone normal since

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
