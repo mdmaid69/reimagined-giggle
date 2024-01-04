1208ea6fa7db533e644d89a52023b07473ab810b
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:09 +0000

    butter actual shoulder furniture cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
