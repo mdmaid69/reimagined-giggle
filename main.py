f70a94a909f100ce83ef9636c20ecc8ad57ffb4b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:39:03 +0000

    pretty breathe goout we history

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
