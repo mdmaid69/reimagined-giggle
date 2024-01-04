eab5a24461cde96a9a4d293a1d73cc89538ce6b2
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:03:09 +0000

    upstairs bringforward proof direction bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def multiply_numbers(x, y):
+        return x * y
