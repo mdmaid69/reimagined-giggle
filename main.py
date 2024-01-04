14c57fd0f9fedcd4453271ae7f44433c9a719d2d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:18:18 +0000

    generous surface accordingto yesterday telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def convert_to_hex(n):
+        return hex(n)
