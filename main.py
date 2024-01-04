3a177ad04fb486df5791e3986efd8eb0ba7249d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:40:01 +0000

    Dd impossible alone play office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
