897757de3da711ca6bf725e1ab8f25830453f74c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:29:00 +0000

    offence land blame strike sharp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_density(mass, volume):
-        return mass / volume
+  def sort_list(lst):
+        return sorted(lst)
+def count_characters(sentence):
+        return len(sentence)
