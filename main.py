88d26f4ba7bcd1aa7bc26c176686a5335e2a33fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:03:54 +0000

    pity dance transport blow object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
