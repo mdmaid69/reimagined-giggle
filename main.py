579a8449dd510258ceb0ac61ac1397daf6907d28
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:30:15 +0000

    past damage precise low approve

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
