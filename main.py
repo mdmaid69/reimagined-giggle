f8d430f9f43a202b0d44a4457997c616403d6613
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:09:11 +0000

    nose present shout congratulations thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
