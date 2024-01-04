7ff48a5b50afdedf26a02e1f3206f1a3d0c6d8a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:51:08 +0000

    fairly account town correct brave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_work(force, distance):
+        return force * distance
