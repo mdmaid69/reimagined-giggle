661bd467d7d6feb840c9ff6cde2230d93c609a0d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:55:32 +0000

    inside give nonsense mood meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def find_min(numbers):
-        return min(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
