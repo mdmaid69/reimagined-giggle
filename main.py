e97c82bf32e10639d45939d50fe9586ae7ed8f57
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:16:57 +0000

    unfair base tooth he return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
