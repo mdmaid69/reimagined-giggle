93eab4f300f8252fe60d4b8f15c1ea55cba82dd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:09 +0000

    turndown come clean slice control

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
