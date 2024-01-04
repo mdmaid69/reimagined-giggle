37f4dbf3a3c273916ed48271e280b2d67d8db862
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:40:01 +0000

    achievement bang tune left top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
