331ec29d6ba9e540bfc4f3613c5c1d7b8a2bc16b
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:02:43 +0000

    inside theirs broken possibility picture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_str(array):
+        return str(array)
