2b47787496b565db280314be2bf36a49547792e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:18:15 +0000

    appearance great but direct direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
