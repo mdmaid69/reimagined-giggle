2eab75416c8072e6f4f11398bd5cb663ea878cc0
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:39:00 +0000

    invitation write rich provide frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
