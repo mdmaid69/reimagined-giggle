28c827613621e69e8c31520efdd66922551d9268
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:20:37 +0000

    breakout thumb film manage lie

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
