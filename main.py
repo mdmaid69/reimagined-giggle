bf360775be3c20a1521273978159be615a7426d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:12:39 +0000

    treatment equipment spoil regard murder

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
