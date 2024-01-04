43c14f9a1d49e95406b33c234bb08a720e8259af
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:22:47 +0000

    estimate straight expect include use

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
