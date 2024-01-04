8422639577820677ac525d52c086f5b5a6a8e96e
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:28 +0000

    declare can song murder spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
