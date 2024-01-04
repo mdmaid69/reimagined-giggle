ce26c46d0d4717169678eeef52bdeebcc1750ff5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:57:38 +0000

    clear hard temper bill active

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
