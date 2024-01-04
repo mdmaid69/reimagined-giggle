8b5e1a62d001af102458a9e243b80abaab6cab7e
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:42 +0000

    wall tense bringup neither production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
