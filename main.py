1803f35fa5b03b75fbd28d3fb2d9d5ad3a0393a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:04:43 +0000

    take by deep such wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
