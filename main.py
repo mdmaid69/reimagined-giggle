80e16519983b6576732e17ca918f19ffc4430591
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:25:53 +0000

    ordinary combine ability crowd bunch

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def split_path(path):
+        return os.path.split(path)
