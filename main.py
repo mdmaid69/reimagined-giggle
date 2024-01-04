ab3360af0c176906f183d90b28b068afc494f585
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:00:06 +0000

    mood leg activity comeover suggestion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import os
-print(os.getcwd())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
