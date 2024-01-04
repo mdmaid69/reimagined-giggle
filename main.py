cc97b09a9fca386bc4d6d9dbd67973de8da0dee8
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:08 +0000

    study gather fortunate bound bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
