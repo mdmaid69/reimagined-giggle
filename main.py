55ce833f13e166c7eb8f5083ff84bcc4596e9186
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:06:10 +0000

    terrible lot lose attention train

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
