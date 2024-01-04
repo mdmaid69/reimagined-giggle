1c709c59931d57fd12d4bfbf9011fb36f28dff6b
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:08:29 +0000

    production rarely present peaceful protest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
