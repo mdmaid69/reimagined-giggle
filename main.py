ee6e25b37603cad0e79b5236c7fba81aaf5f5600
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:02:48 +0000

    snow kill reply 570enjoyable period

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+from collections import Counter
+print(Counter("hello world"))
