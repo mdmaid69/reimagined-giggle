f6c0109296cc18b46e8bfba2d4a639751714482d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:04:31 +0000

    operate dig petrol peace muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
