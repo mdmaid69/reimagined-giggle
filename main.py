2dbf1e5613a8acf81b91bde596cfbdffaaa1d833
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:56:51 +0000

    cry standard brother advise Ee

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
