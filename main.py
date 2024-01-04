d46500e455b641bfcf6def37e1e8277cdeba599b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:01:16 +0000

    promise service unless sensible badly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def convert_to_hex(n):
-        return hex(n)
+import time
+print(time.time())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
