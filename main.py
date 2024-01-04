9407d07e27b089ae91ad708645ed53f613918eae
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:16:13 +0000

    order guard forward probably mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
