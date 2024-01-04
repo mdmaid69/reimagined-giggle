20cb1659eb5a308d13317b1732b30bf45cd333a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:23:36 +0000

    peaceful you crazy Oo half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
