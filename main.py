83842467c746882b0776253bcefe776278f8b545
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:50:15 +0000

    cost left lookafter tense polite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
