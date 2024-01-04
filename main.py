1168798759a71e67b9fac4ba5036f812e5eb1a65
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:03:35 +0000

    mainly suck since practice sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
