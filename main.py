d0b4393722167dd2d63488706ea49ba444c294ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:43:13 +0000

    567enemy release loss remark correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+def get_today_date():
+        return datetime.date.today()
