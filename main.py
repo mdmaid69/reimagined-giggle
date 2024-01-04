2fbdb15bf1ecbcdaf4194a1eef1a11fc3b4eacfa
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:46:00 +0000

    escape look accidentally silent extremely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
