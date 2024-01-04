8aac0e0086c8732e648c76b8d5bea48c0875a1d4
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:17:31 +0000

    black path just better normal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import re
+print(re.match("h.*o", "hello world"))
+  def add_numbers(x, y):
+        return x + y
