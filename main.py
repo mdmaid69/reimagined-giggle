aa8e67ea2c607133e67235afbd007db9fabc0cbf
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:51:46 +0000

    before centimetre win bath spare

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
