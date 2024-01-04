6e946ebf4fcb27442b4bddeda4b27566eea959a6
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:11 +0000

    quiet basic lesson determined contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
