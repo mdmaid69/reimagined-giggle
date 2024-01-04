2b5a908f52d268ad641ad140b9f2636c2b9fdd72
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:32 +0000

    determined neither still safe new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
