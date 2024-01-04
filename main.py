7a49825c281a24cf02b91a0778d1d98e5180bf35
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:47:27 +0000

    speak achieve blame watch in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
