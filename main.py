04f02fa0f76cef8e4a844a65a8a845a8033e0504
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:59 +0000

    available blame to vanish turnover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
