7e6bbcf2c427a06257198b17912f3b18e53096e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:19:56 +0000

    look birth mad fall contribute

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print([x**2 for x in range(10)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
