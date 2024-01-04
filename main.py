9cf5814928f7f7ee2586946bf007a84c1042f14c
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:24:57 +0000

    real rule material broad ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
