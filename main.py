e0e51593882f23983b7052ecb534181074a7e04a
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:57:00 +0000

    remain suck short broken damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
