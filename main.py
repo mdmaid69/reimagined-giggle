41e00ccc54c6cb51add1f643695ad5e1b019bdab
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:30:22 +0000

    warning confident row estimate twist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
