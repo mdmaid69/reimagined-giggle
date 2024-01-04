bbc8512524cdcedc8fda7fde8e25cf3686524297
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:41:49 +0000

    software by accept nigga certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_cosine(x):
+        return math.cos(x)
