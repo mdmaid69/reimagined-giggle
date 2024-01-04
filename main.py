2353ad1ddecbb62674be14927191a60e905db43c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:00:11 +0000

    set politics benefit thing early

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
