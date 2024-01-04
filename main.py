e045d7f3cb4bfa34a55e9d169070e36592adcb97
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:56 +0000

    classroom rare stay anyone condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_time_since_epoch():
+        return time.time()
