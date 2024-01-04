50185822e1beeb59204695350f8b1d88a34c2571
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:55:33 +0000

    release new sink passenger importance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_list(array):
+        return list(array)
