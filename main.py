986be678a1093485c63904a985032457b62bcd43
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:02:58 +0000

    afraid lot improve someone failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def reverse_list(lst):
+        return lst[::-1]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
