6b969cbc57576f04c9f137056db4a7714059fa24
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:40:13 +0000

    excellent account rush sentence priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
