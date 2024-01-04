e7f574f36b1b3e735a9785af581c0690d0f4d0aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:01:34 +0000

    sight memory length quarter mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
