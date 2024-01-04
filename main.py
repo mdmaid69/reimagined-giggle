2b77d6dcc09b30816ac92c4e6af234c0c37c310e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:07:35 +0000

    any medium takeout fairly many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
