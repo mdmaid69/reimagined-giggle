2a9bebd1d4bcabd2a04bbd6331993357ae5f0ff1
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:42:31 +0000

    lose complete anxious ordinary exercise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
