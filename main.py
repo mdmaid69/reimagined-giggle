f4f6fa3322167adc643611ca0d7da51236c71360
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:52:43 +0000

    damp equal similar maintain invitation

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+print([x**2 for x in range(10)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
