6b38676381f76b2bd1774827da265d629c3695ba
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:08:49 +0000

    breakout woman succeed attractive upwards

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
