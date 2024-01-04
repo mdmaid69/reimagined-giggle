be2a064d466a1aef6004b4f3775ebf05a62d9809
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:05:42 +0000

    turnon fond discovery 567enemy thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
