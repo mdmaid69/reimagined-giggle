dea67f14410922408e4e5836e1339b86a08c469e
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:46:32 +0000

    matter about sad guard breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
