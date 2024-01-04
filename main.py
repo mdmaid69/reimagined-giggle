8f771e397502df026f407414c4ef382e5f254d64
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:12:27 +0000

    crowd whom write cover foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def print_python_version():
+        return sys.version
+name = "Python"
+print("Hello,", name)
