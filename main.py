f61b15d0ffc49e6c1ff06ec45f154cc4a1ee5698
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:39:47 +0000

    train clean geton thing gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-print(math.pi)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
