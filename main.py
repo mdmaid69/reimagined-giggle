7be97a7320e2b8cd3b48efcc64a10254cea1edef
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:26:55 +0000

    butter try tap across love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
