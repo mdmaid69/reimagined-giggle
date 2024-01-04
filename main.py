782a8a39992f2d0f56c37f431e89acde84ef061a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:17:48 +0000

    turnout bring quantity possibility sign

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
