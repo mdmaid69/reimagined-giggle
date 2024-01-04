7a73ae24d3eaa2f2de8ba694bc59c078fccb335a
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:50:40 +0000

    quit Cc direction test backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
