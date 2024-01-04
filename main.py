bbc2c8a4be7342bd20278e9f50efb1c432708b5a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:22:50 +0000

    need 571enough run listen issue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
