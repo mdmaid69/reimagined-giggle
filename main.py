a45ce59f1195344fae736eee4922688f72178573
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:13:21 +0000

    figure squeeze get stupid limit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
