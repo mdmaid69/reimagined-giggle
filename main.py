03af70b84d5c5f2bce27e8f19be76d689109abd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:38:55 +0000

    before regional instruction listen weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
