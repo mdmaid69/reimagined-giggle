e07225a1d624b0cb4825fd7e0ef2f16839d6766e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:36:32 +0000

    safe wash sensible smile congratulate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def add_numbers(x, y):
-        return x + y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
