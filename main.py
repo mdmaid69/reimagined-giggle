480e25558d312a348ecdef87b6d263da8e01c4cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:11:38 +0000

    old favour bag hear artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import sys
+  def get_python_version():
+        return sys.version
