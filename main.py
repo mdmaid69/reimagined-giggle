b2a4c7129fde494ee63423e2e9c1d8e4312977db
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:08:25 +0000

    nigga about over accident present

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
