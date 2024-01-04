7b0621d659321ecb646e0353acd290c37dbe6658
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:39 +0000

    exist Uu process swing prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
