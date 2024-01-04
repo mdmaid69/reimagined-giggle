92a601ca493961ee94f2c2460b0a63727bc4183b
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:52 +0000

    secret wrong shape top danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
