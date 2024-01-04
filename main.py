dac416657afe5867623002be86d183219a185801
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:55:29 +0000

    Rr money unlucky responsible dream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
