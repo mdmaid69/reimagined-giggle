d03af01f3f3853ac914b6f5b1216dec11663880e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:35:32 +0000

    team schedule calmly occasional extraordinary

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
