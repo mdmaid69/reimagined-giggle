dc4b06298873d36b4df567c1160419f868e31b00
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:50:02 +0000

    star trick spring sound city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
