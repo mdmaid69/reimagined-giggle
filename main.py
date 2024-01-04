f4f52a7f699c44b686039857953114897c582598
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:32:16 +0000

    late hurt walk sign accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
