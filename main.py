c204d56c2e27c742c302cfc67a16ec733cbdf594
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:16:29 +0000

    minute open case exhibition report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
