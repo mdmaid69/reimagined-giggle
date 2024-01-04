679a9bc87d9252a53d81b4324904b479579e6982
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:34:52 +0000

    hard run rain middle nurse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def square_number(x):
-        return x**2
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
