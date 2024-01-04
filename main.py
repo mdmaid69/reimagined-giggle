e96ffc29e8dbb1cf299b2de9104cf30e56b6ab35
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:14:35 +0000

    workon quick across popular address

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
