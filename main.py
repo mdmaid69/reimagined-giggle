a2c6f5b03c0ff14283f0c625ae42bc2e40644474
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:28:52 +0000

    post payment combine substance wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
