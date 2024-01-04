421f623482716278c498a3936890149ea85b5fcb
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:28:45 +0000

    nose fall compare contrast only

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
