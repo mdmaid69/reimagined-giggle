4837c623697d4c4982b264867777d2292f786f42
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:55:26 +0000

    closed high star be answer

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
+import array
+def get_array_as_repr(array):
+        return repr(array)
