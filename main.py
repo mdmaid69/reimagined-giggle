5d1dede0e5b3a9ac80045aacb8e7ef9a394db7c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:21:49 +0000

    surprising friend switch compare situation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
