c56fb949ef968f0fe93dbea89dd6d18b31775322
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:34:26 +0000

    open keen live listenfor beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def remove_duplicates(lst):
+        return list(set(lst))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
