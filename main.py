8c8b6b0ed4f7f015a672b3df74154773b923adb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:53 +0000

    television record sure dance drawing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+for i in range(10): print(i)
