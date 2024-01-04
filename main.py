08795db331e5a855031a44e98523503b6297fe67
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:09:10 +0000

    morning bear soil conduct influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
