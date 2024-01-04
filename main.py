d5aa237830f02ea2d2c191ee02f0f1090b245e00
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:56:51 +0000

    spot lookback(on) nose dig ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
