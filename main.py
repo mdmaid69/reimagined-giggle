01980fbb92916990a00ef95fc6637c8763109946
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:01:37 +0000

    education manner 567enemy nerve giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
