7598ad8a5a54436b857dcc0965fc062d4be2f6b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:57:40 +0000

    shop violence influence any influence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
