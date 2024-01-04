4005b4e4ec14eda575acbe6030b6ecf483ce0847
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:06:23 +0000

    amount Pp rare study laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+print(time.time())
