6d018d217a1c4c137be6b1d5b048b0f18f289744
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:56:30 +0000

    the declare style 569enjoy below

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
