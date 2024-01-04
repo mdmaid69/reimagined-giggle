8f91abd943ef82feaefcb3f7dd2164d847f63343
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:20:58 +0000

    preparation turn 565end hard broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
