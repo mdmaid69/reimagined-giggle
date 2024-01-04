403e317d7b9eebf0c49abc71e96538bc564d3ebb
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:49:38 +0000

    count power lead develop honest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
