152e93b3766e6936e0a3100d2ff5450f1231f098
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:39 +0000

    blind used worry would cover

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def calculate_area_rectangle(l, w):
-        return l * w
+print("Hello, world!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
