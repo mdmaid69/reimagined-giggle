7f4a40558fb5f7dfe0ed091f745a373a71717c88
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:45:30 +0000

    this criticism regard dream hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
