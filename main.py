6b86e38b108e9382d6e95134b9cc5cec646780df
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:03:38 +0000

    official either dress harm cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
