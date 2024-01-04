1b3ec8b27a4f56163ff0993d5398e0088a3b8a36
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:41:31 +0000

    accommodation sharp attempt smell mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
