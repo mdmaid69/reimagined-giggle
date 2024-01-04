5170d7c5e3232ce1005b0cfaec294ff835d39568
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:29:08 +0000

    lower comeoff debt holdon minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
