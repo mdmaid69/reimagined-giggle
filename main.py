367d229f0bd0954d7bc72327aa6d0091f6438c51
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:11 +0000

    moral weekend short software my

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def convert_to_hex(n):
-        return hex(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
