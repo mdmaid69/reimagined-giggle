6f2f9e27e0e1d7f25a67ac14869ab52763dcabc3
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:06:47 +0000

    year date wheel everything mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def is_palindrome(s):
+        return s == s[::-1]
