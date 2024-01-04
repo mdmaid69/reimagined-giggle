b132d3fa1a6151e0ec83531f3e3143a2ec7de0ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:31:47 +0000

    between promise project bridge rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
