824c7e1b9255425e937137f9d12f6444ec755b8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:09:18 +0000

    error match behave Gg willing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
