e0cdabc5d06baa306563284c39d420b3a3154358
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:43:53 +0000

    temper result before pick fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_set(array):
-        return set(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
