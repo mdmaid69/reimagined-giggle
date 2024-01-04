887719b51d7489df23a4ba06f370776d09067048
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:18:34 +0000

    worst straight for Zz stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def convert_to_binary(n):
+        return bin(n)
