b9004862bdbfb9b96dd8a9846742168ad3220405
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:31:01 +0000

    callout protect closed about responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
