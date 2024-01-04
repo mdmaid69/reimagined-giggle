38f64101c3d86935f72928ec805f3d14eb99a09c
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:17:15 +0000

    pronounce along strength pencil imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
