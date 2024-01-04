3212d1bdecdffc86c07b771621e795dbecf8468d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:36:09 +0000

    instruction skin emergency mood firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
