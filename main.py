23a0a1f56799855535bce4849440de28259d1e51
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:23:18 +0000

    culture lead talkinto gather healthy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
