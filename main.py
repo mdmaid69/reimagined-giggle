47a9b0c69359682d575459ab2755088e9f21ef50
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:34:47 +0000

    improve telephone who absolutely fat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
