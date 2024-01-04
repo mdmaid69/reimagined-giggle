a249b88c50ac2874ed519fad7870cbc9c1af6287
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:22:30 +0000

    pinch future shock pronunciation nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
