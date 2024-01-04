e766f35343ddde22e2ca742ab615963920359466
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:26:52 +0000

    distant turnout awake doubt race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
