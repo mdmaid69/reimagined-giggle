cc495bc8038f5c0af480b2520c99d998a9876f2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:00 +0000

    crisis law quantity keepfrom occasion

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
