e55e9adb4ee780fc4a475d78f2b411164287e91b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:35:06 +0000

    tread skin muscle criticism shoot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
