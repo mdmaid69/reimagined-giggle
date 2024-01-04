0a284ce63c6fae6063b8d40dfa380f93d7623269
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:43:08 +0000

    pump suddenly anything stamp reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
