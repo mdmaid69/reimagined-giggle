0638b3c3730308005521b361773a53c6bec63ef0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:36:22 +0000

    Zz wrong about legal respect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+print(os.getcwd())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
