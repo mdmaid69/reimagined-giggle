7e55213364745d0385dcdd5d3b06010a3f335b8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:37:24 +0000

    heart different ordinary keen then

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
