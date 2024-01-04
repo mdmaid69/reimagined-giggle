625eb7dbdf47057bd1c958faf647046fc8c60d63
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:21:55 +0000

    exercise ball concentrate switch fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
