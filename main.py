842589b843ce954f487559f521f31bcbb30c2b6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:52:37 +0000

    attempt central use sore breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
