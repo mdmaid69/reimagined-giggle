b4b87edbc440f0f749a4e4d8475112ef348b9de2
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:27:20 +0000

    understand have communication standard charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
