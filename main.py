7b2128516e7c7729e565bb9d715d8602ca942d19
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:41:06 +0000

    reduce trip nor sweep sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
