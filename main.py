24d9dbac8ea82f7b54cf79edb130229ed50f9fea
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:06:29 +0000

    justice impression quick pile certainly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
