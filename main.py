e2296b42db8068a960b2dfab0b156c82d54f8115
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:13:06 +0000

    line wrong once shop century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
