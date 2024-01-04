a6775a841cdc120dd94cae82aa80b91f5596eeab
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:26:14 +0000

    tear figure interrupt no minority

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
