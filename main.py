e3e1ff30d9ad4ac9329f66054e9ee923f03cac66
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:47:22 +0000

    worse listen minority weekend total

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
