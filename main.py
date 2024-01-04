3dfaaabfc4c8f85bfeb75cee7fdd01dcd270a551
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:42:01 +0000

    knock other beside pronounce bag

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
