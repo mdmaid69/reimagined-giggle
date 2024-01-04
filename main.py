700912defc36338d52094eb9ac557f3a653aed8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:26:09 +0000

    culture week scare welcome city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
