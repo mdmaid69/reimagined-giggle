d0abd3cc2b9a7d193b522e95587ff4b7d9b71d09
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:12:19 +0000

    holdout downstairs feature accommodation office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
