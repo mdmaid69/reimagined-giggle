2bda8f07a60b0476be5ee0a98b60f9faba4751fc
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:46:38 +0000

    almost comeround home similar means

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
