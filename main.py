da015a701b63ff2c3327e5e9cd2350e835352c38
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:52:29 +0000

    pot track sale safety pump

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_repr(array):
+        return repr(array)
