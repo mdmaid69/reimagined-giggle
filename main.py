1974f16b30257891c380b18a033ad106bed0e00a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:53:31 +0000

    worse length read stiff interest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
