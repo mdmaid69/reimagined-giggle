7c084a40729e71103196265c8085ced7e86b80db
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:45:02 +0000

    suckme weak slow know appearance

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_current_directory():
+        return os.getcwd()
