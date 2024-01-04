e22334650efa647f513de55dde28f9ddbbfb9b33
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:00:10 +0000

    to past seat later meal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
