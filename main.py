c1497552c2f86e16d7d114c0e4f79bd63127339e
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:13 +0000

    excuse professional Dd shop would

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
