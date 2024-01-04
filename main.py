72b65578ac83dd57c75f661f90246400e93ae13a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:29:21 +0000

    official approval ordinary welcome book

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
