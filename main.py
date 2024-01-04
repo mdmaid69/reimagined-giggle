6bd883a1a2882f8d5379efbb62f879c075cb364b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:17:03 +0000

    holdout operate carry operation compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
