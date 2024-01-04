dcb783e102fd987d395bb19c3e542c68db294ac0
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:53:37 +0000

    wrong present active repeat entry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import sys
-print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
