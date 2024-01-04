a1f8080b44a32a6b3c165385357f8858053356b4
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:30:17 +0000

    concentrate quantity port 564encourage part

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
