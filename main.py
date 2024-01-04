c7ea09d0047c427b89bf16a9522f66aee1b7bbdc
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:57 +0000

    level trick block post positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
