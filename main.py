5b92934b13556a69942484bec43534737760b4bb
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:47:38 +0000

    announce suddenly ordinary environment miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
