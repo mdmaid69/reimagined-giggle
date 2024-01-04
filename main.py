90148f6c56e4e417dd964cf136396ad30871c5fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:55:42 +0000

    active regularly bringforward treat pair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
