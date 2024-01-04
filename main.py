b1e4c24eb6217838658a1a93ab1455b5a353238b
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:06:33 +0000

    sore thorough birth approve ring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
