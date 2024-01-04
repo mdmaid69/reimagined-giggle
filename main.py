fd6ea1a4734acf652eafefc34aacefd7abc3b309
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:42:04 +0000

    ever breakfast dry equal such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
