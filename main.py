fb8fccd8a8ae960fffcb1cb2b88734d32e63dc1d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:21:05 +0000

    keen embarrassed punish combination reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_list(array):
+        return array.tolist()
