29ed1cd8bf4861038abcb59347ff28f4210e451c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:10 +0000

    sweep coffee minimum offer load

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
