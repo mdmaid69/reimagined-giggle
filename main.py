17dacfc6a4117d8abf06d0169a4b3e19ab8d502f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:43:15 +0000

    block punishment design cope seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
