5ca0a5787461d491c5463c15bf10fbd81f6815e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:09:26 +0000

    goback(to) sense sick seriously study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
