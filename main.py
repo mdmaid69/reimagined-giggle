e328cbcc960242aa652412e57573bf2e204013bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:48:42 +0000

    Bb hold turnout solve case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_acceleration(speed, time):
+        return speed / time
