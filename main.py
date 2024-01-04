8defe56fa8fa4ddbe1263048205a5e020851cc73
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:22:40 +0000

    area trap total explain calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
