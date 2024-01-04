7d06b1d68e72aa4b0c485ac85114e45a5823b153
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:47:28 +0000

    indeed problem importance punish character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
