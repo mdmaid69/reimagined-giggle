c692f4af94596d48ba6dd3f0bad3b33a8d3bad96
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:54:17 +0000

    fortunate own success allow concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
