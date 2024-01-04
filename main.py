f0d1f1ee3260c322a2af8eec0ce4576ea59a99de
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:14:02 +0000

    few legal themselves stamp juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import sys
+print(sys.version)
