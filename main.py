f6d227eaffbc0539d10e4c788ad5677a2fd46866
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:02:10 +0000

    phone either comeround favourite shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import sys
+def print_python_version():
+        print(sys.version)
