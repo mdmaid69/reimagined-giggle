51816b79e83b257a5c027c5c7fcef6c2703dcf2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:42:02 +0000

    announce another emotion uncomfortable factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
