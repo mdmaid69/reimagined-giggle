41de5bf2c3770bfd9d3a96108e40ae87fc5507f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:14:50 +0000

    perform doubt bang callback loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_elements(lst):
-        return len(lst)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
