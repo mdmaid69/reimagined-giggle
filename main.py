b30a46d776d8dfce66a87adc014be89fa7e1e769
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:34:03 +0000

    wave wise lock laugh copy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-for i in range(10): print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
