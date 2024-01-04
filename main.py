2945f0e8a15f360c05ee9aef8b0d3e5482781ba9
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:31:00 +0000

    fun follow somehow become fetch

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print(sum(range(10)))
