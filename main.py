5c0ee633338d16471ad79d35950a80044371ad71
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:19:20 +0000

    neither safe goon walk service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
