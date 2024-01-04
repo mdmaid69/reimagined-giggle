4d96cf2fdd0725107d08a5b5a8790ee3d3c8b123
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:34:11 +0000

    band so offer 572enter phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
