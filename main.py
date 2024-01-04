8daa14e502a572c9b1a0543eea18d157a82f9aa3
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:51:31 +0000

    innocent length ashamed whom herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
