bbd6f7975b9bff8629b38076bba0b4b77321ab0a
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:39 +0000

    variety collect last day obvious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
