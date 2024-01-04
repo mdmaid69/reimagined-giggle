dd718bddfd61e31fe6cec9b45f7ca4f2cb15b6c6
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:57:45 +0000

    surprising conclusion opposite alone really

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+print("Hello, world!")
