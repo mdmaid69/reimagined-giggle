926935581a22197d31a8cc734d11a1d23d657778
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:46:56 +0000

    agree negative protest happen myself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
