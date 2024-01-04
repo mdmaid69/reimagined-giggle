5233d577e643dcc989cd913770ae980d8e2372f5
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:01:38 +0000

    summer ground visit waste share

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
