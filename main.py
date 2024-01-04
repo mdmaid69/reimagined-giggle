5c73da99bcbabe58f4848387a410e2907607354d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:36:32 +0000

    complain board count my dull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
