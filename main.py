e627741129373a4d70081b12f8fcd4ed337e7791
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:23:29 +0000

    fence guard very great prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
