3e8e99c54873f2b4aa899b76982cd66d3dbae7ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:06:41 +0000

    slight fierce weigh warn weekend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
