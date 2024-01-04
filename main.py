424944710829e9a50f899b32e85c081a73a55afd
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:30:53 +0000

    music separate emphasis tap splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_directory():
+        return os.getcwd()
