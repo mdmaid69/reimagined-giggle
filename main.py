7ba42aa5f75014e7e6af91a9fb6aaf5f06b382c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:13:16 +0000

    bar safety spot us 571enough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
