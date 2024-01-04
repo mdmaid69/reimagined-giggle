367fb81087f336809c607d5f667b7677751adb11
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:26:27 +0000

    Qq film persuade related turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
