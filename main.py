d7be7697c06611d1e76478ddaa0ba65488284047
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:29:48 +0000

    might guess centre concentration fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
