701e980b96ebf7d6118ef2518f4b418a2220fe6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:54:51 +0000

    anywayanyhow prefer mad my stupid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+name = "Python"
+print("Hello,", name)
