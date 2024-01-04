8d67157d298a8c4b5b28feaf5a357f9cc96cb3ff
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:45:22 +0000

    stiff organization secret zero deep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import os
+print(os.getcwd())
