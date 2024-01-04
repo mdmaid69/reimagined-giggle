b5610380abc69535c9ee84129414c54d9a87c11e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:17:25 +0000

    shrink mysterious obvious home doctor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
