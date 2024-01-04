833ed6707d705bf604bcc1097e9bdfc2844cb075
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:40:05 +0000

    hardly contents runaway match bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
