10a978d11dfd5fb1d18866973d3a39eedd77efc7
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:55:34 +0000

    dinner lower pride contrast yet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import tensorflow as tf
+print(tf.__version__)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
