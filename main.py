2f39707985706efa57298bb9d9063f007a50cdbc
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:23:39 +0000

    reject wild unlikely neither square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
