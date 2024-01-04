e32b385b28b056c6e1db9d1b1a52df9c01f79abd
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:00:10 +0000

    care judge actually grateful through

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def is_odd(n):
+        return n % 2 != 0
+def find_max(lst):
+        return max(lst)
