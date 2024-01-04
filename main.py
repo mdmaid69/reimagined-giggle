060766701b468aa4c95a9b4aaf6f4cb04337b43e
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:23:37 +0000

    opposite different boring busy firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
