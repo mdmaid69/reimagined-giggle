8abaff7940abbacafd5dcafd0dac28fbf88ecf11
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:10:48 +0000

    possible weigh concentrate operation how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
