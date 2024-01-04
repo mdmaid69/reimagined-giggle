91048b38236e329e5a4b5dc5d2b7f4ae8272a535
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:14:50 +0000

    tomorrow tape home constantly several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_elements(lst):
+        return len(lst)
