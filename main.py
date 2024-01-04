74ded8750580662010607d653334ec1901463457
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:05:21 +0000

    shall whom scheme favourite especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
