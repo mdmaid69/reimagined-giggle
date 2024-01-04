c2710d8babf5a95784a346a50f9a2c2ed68a7689
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:30:37 +0000

    bit fence land plate normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
