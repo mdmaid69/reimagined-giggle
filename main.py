de3aea377e07a1f186f37aec374eba840b9ba76b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:06:32 +0000

    better split curious pretty buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
