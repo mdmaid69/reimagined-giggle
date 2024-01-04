287bf526d4334d08306c2c46666e627a944ee19a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:28:56 +0000

    confuse tear both promise top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_list(lst):
+        return lst[::-1]
