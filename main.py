962de37bc9afa2c22d03b369b2c5ca5ef4870cca
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:17:51 +0000

    trouble manner dance mother seem

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
