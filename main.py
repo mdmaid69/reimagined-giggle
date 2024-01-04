29c3de47dbd209784124b7a31dbd26d120e404e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:02:04 +0000

    talk them top neither embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
