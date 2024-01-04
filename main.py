08d067bfc3277af8a6c3b074172873d54975793b
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:36:43 +0000

    telephone regularly reject flow catch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_area_circle(r):
+        return 3.14 * r**2
