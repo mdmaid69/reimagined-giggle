8961a270621212a2131a6f36759a630aae4f1d27
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:50:24 +0000

    material clearly so pattern knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
