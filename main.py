9e0f767969c8d2a2972b5a0f26b2fb63cfc21d75
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:10:02 +0000

    relationship hate object that determined

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import getpass
+def get_username():
+        return getpass.getuser()
