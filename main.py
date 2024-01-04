ccf4d1e4307bf1b081fd02dcc37a5306c94d8d05
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:45:28 +0000

    frame Uu embarrassed think Cc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def count_characters(sentence):
+        return len(sentence)
