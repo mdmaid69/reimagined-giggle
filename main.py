7e2f1414b608205974707041b561dbcf7d77339d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:53:10 +0000

    565end street far man watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
