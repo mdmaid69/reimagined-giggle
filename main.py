49b6f33614407edf153bae17ede2ada119b8c691
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:52:14 +0000

    replace male rest begin grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
