0b430e02260f6d0820ffeeb9de6e487b419159ad
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:41:58 +0000

    petrol lesson there picture interruption

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
