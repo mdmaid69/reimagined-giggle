75621af00935d59e9482526144361c9cb706a6e9
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:42:30 +0000

    mixture admit such pleasant range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
