be281a6d48d8ce8cc796fe49a39f39f2b7ea6d57
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:41:30 +0000

    several address attempt frequent marry

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
