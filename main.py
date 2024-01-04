ca75f33852534389f50cc5f50b7e6ee305fb5920
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:14:45 +0000

    culture winter play false sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def reverse_list(lst):
+        return lst[::-1]
+import sys
+def print_python_version():
+        return sys.version
