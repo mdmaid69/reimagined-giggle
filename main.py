050e72a0d5c9a384370fa52af207d27ecfe1e98d
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:54:02 +0000

    dark pair majority getup question

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
