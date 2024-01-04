e877d054917e1db09c6ba72d08608b542dac2ec3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:20:47 +0000

    platform pool discussion shout whom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
