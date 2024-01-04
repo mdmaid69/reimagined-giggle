b651cef7f46347ec07e3e4623747c6c9ff7b2d72
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:38:39 +0000

    skin confused equal bunch quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
