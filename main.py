a445d916b14c95c31dedf3e7f0a160cf771b2059
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:19:57 +0000

    this deaf winter through borrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_slice(array, i, j):
+        return array[i:j]
