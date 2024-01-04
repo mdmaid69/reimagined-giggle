74b088f4a6e26217b848a317a030e903810e2517
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:33:07 +0000

    recover walk harm obtain shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
