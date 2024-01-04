59e0ad84784715fc3f64099c8be89a49c80465c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:04:42 +0000

    language pick region operate test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_dict():
+        return collections.UserDict()
