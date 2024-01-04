7e74267c307a9639d258b763cc08f68559cb710e
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:59:27 +0000

    will otherwise lean operation noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
