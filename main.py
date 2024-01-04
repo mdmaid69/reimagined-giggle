d0e10efb92f2b392fddeb3221b0373326edba00d
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:50:42 +0000

    rare train earn paper perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
