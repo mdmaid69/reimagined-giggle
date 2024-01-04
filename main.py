e7bc7a6557b1c96ff464cce6295f408cec6ce10a
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:03:41 +0000

    score difficulty Bb suddenly maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_volume(length, width, height):
+        return length * width * height
