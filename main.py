f18bb77c306af3fc11dcb7218ef2997a2399dc26
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:57:28 +0000

    uniform heat commit recent true

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+print(math.pi)
