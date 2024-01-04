abe5b6744e5460c86bf7a3eb9ec0938f1c35b24a
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:08:24 +0000

    confidence reason school pile near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
