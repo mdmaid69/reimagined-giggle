ecf56f27b4e0822e554b094ce766ae410b22eefb
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:40:43 +0000

    soil whether instant meal or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_index(array, item):
+        return array.index(item)
