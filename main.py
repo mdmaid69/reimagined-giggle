6524c4309cce9f4432d2632c30661bdb7e08eb03
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:02:02 +0000

    thought reach communication Ff unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
