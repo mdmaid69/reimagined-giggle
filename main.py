8f3052fded1c584d9c27d41ece1423111015af62
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:48:46 +0000

    shopping workon immediate dead flat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
