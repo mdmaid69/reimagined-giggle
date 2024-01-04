838419c98eb969cb3ae700287f98e681b19657fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:23:48 +0000

    calmly concern reasonable stage morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
