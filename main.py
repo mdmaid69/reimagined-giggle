bc5abd9a560ea05ee06c6e6c653702b11f7f97bd
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:34:14 +0000

    getaway score furniture travel far

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
