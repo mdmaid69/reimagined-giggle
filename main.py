e8b2baa4fd36e2f1af5525555183f4e01b382a73
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:30:33 +0000

    move proud responsibility offence open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
