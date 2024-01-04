ecd2357032991781558f94e0d710be73a43232ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:11:37 +0000

    claim comeout possession pronounce mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
