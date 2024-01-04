5c016cd69be3f2955bb6fdb36b38b6c50fe3a437
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:03 +0000

    bury team myself perhaps emergency

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
