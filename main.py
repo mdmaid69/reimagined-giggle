1b13dbc86b620b765650613b40b1ddfd06539459
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:20:12 +0000

    seriously fix noone formal mainly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
