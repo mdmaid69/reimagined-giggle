15f71e25702a5e62e70169d6ce91ae506073298b
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:18 +0000

    point march bottom effort knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
