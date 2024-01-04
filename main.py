f887e2b2eae6c73f926bb8352cc3dd485fe4ea7f
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:54:07 +0000

    shut point 567enemy introduce brush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
