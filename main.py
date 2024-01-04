771bc005464a30fbf0cba6d3acc7ae11141dead7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:22:10 +0000

    wear change manage reserve behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
