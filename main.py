6a39c721b7cbcbb067785078dfebfdce3f0c1fe0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:16:52 +0000

    callat breakdown fall similar well

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
