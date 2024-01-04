bac5d727b77a2872707eed092f7b4f61dff2ee6c
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:12:24 +0000

    schedule objection further container opposite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
