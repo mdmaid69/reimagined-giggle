213733b6a37369180c2ea04bfe99b6e0fa476c76
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:08:28 +0000

    sight push drink hate send

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
