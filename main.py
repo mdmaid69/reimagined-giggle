3d843dde40e0b2b8f2fde21da2b98e32dbce4b27
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:18:17 +0000

    her number direct split reach

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_area_rectangle(l, w):
+        return l * w
