450999091a62b453ec17f175e996e896c5473019
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:57:29 +0000

    short serious bend whatever reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
