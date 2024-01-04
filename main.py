46352e8292a8f1a6db7397c473ab58b0459c5d22
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:57:00 +0000

    warning must clothes under generally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_work(force, distance):
+        return force * distance
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
