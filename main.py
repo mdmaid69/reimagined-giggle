b2a42c3cf9afb06f7a12175d235fce70ee27baab
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:18:32 +0000

    uniform concentration dangerous zero talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
