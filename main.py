5145ee3b9ee5bf93dee3bb4b3da93d079336699f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:05:40 +0000

    takeover rare her failure forever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
