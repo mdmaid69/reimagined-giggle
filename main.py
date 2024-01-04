281b4006ee52deb47c942ade3fbc43a88e46df87
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:12:10 +0000

    already set kill guard needle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import platform
+def get_python_version():
+        return platform.python_version()
