500f39ccd1ce1a39d499cbd911c3e0244af45531
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:34:07 +0000

    please 573entertainment rest ashamed course

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
