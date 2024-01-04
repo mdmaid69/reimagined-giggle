62974ac3e0b3839e2cf0f9e59c62c15a891e92e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:08:11 +0000

    taste Kk track dry lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
