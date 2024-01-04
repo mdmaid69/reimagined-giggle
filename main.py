46319438bb75af945785a468ab835631c01bac60
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:48:13 +0000

    afford write shrink fierce thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
