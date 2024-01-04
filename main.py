1c139287a14f1736a95597b385c1e8dd7470f69a
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:44:58 +0000

    round note minor related succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
