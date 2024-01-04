350a1d833ef9320a53f85cbcdbac048f4c851680
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:00:41 +0000

    lunch Pp mix dream demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
