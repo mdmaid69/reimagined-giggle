903cfeaa45b3c56144db762bb397c90233f4a7f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:03 +0000

    getback march as mention try

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
