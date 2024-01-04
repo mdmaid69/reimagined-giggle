3f2fdb6ad825b94925671a7379eb401eac9ccb71
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:36:31 +0000

    expensive goaway for tip course

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_work(force, distance):
+        return force * distance
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
