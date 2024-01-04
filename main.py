4aeefe7ce3035a41976f6839db8b1f5ebce2f432
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:22:03 +0000

    fast still high repeat brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
