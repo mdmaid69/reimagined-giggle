178a41464e744d936df007f15edd9f3d4248d504
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:59:11 +0000

    dance bag probably computer top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
