118a2bfce7affa00b7c970b1d2805c91d6fbdd18
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:52:23 +0000

    friend government pretend love turnout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def convert_to_octal(n):
-        return oct(n)
+def reverse_list(lst):
+        return lst[::-1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
