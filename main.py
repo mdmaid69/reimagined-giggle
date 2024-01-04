b45a5165d2aade0112afa724607688d238feb5b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:03:56 +0000

    actually breakdown memory interview result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
