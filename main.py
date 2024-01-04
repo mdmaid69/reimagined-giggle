0abf6b3830152b2bf72a19578075ee5516712476
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:38:38 +0000

    message chief sort cost voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+print(math.pi)
