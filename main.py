ed11065f127a0a767992187cd7c4bd377bbe783f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:12 +0000

    past rule turn ocean go

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_user_string():
+        return collections.UserString()
