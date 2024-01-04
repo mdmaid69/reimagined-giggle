8ff344c921fd6458676c124a48ec136a8bfbec54
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:42:37 +0000

    arm dig mass piece separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
