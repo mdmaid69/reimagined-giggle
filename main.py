cf91726c79dad60f79d88cc70c61737e223deb29
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:15:20 +0000

    stand great above business truth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
