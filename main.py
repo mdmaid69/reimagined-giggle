4e69c92838fa3408a7babe832a5682c01d0e1dd7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:02 +0000

    only month cry preparation right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
