0a157dce9b2d1c2e7a8a3c6ccbb7d18ccd4d004b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:22:20 +0000

    return relation any try purpose

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-print([x**2 for x in range(10)])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
