a445583ca864bbd2910d626d6b748a5e98498b57
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:57:03 +0000

    interested form wing grade likely

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
