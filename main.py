64c89e2de1d424e0864deafe2fd32b23f128ec90
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:37:13 +0000

    claim twist very narrow soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
