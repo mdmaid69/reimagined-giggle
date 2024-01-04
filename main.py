a4f6d6136cff0049e6a424c8ed65a044ccc9f518
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:37:23 +0000

    machine shopping than goup Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
