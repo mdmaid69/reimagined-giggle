9a52f5030f8590f0919a9ff0155e7eee37bb09e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:21:07 +0000

    exist hurt might them listen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
