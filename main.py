232bb311c6a5b078d48b32458ecb94bac03be5f6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:19:58 +0000

    deaf bottom star material promise

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+print(os.getcwd())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
