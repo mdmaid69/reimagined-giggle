c43e4bdce5a7a526acbc3f43ed5aba7d4ac78a22
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:46:41 +0000

    Bb like influence inform set

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
