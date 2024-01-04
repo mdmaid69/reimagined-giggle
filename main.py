6eb9ce241fdefe128a8c7d5200a60d5d2ac00166
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:30:41 +0000

    sorry murder correct flat will

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
