0cd8391a31c562160dc237446af0ac10fc05b897
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:38:46 +0000

    kill chase along shout autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
