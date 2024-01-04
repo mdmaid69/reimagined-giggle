5c5b5968a0f9edf15a112c4741bab3cce338d977
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:10:59 +0000

    improve school moral contents breakfast

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
