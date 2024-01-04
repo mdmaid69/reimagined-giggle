a97a7e8ea8f21db38ccbe270770972cce768d14c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:33:15 +0000

    accidental means swing apart attract

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
