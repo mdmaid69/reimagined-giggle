d8195175e1332600840501bc749f8f96bb91a502
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:19:26 +0000

    them modern medium somewhere twist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_mean(arr):
+        return np.mean(arr)
