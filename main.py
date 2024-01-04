e48caafb4fc8ab79151e9332fdc3c5a910ddb84e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:18:04 +0000

    against interesting sensitive dead smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
