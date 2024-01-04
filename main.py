ae716e57d021ca5ff2d545f5e363e3d9051fc053
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:29:57 +0000

    late weekend expression keepout order

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
