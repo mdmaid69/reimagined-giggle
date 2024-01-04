91da1a690d0a952c00ba1072b02fa7522d11d4bf
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:46:49 +0000

    phone think half freeze card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
