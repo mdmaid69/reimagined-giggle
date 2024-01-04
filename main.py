31688c6a61c80e418d2b4703737856bdc1aeebc5
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:48:56 +0000

    murder nature promise miss talk

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
