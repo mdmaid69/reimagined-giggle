0120113e29298216f6603ce016b6f63848f598c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:34:08 +0000

    need sail them getup anyone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
