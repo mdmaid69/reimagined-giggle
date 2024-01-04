b074cdd74efe9be7264e0e810a18fbfd21cf6399
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:07:50 +0000

    raw disgusting next shop workout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-print(random.randint(0, 100))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
