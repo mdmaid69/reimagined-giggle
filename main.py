236b4c8dbbc075f3b370ba1d68e8d0d3b3e400d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:08:17 +0000

    defend rich clean include sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
