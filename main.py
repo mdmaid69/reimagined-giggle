5a98f5eaeb32c52d3391672f2d9f739eac8772b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:15:55 +0000

    wave expensive modern tired welcome

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
+import datetime
+print(datetime.datetime.now())
