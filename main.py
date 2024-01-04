ec76559682778be6343f7a1f9c6fc802c96933f1
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:18:08 +0000

    tear influence leg will difference

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
