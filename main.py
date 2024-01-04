5002c7636a3c26f19402dac2efd6cdc8dfb3c28a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:31:33 +0000

    improve she on even declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
