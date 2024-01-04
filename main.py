255572da8bb5f1aada9a2d31a180663d6166d1a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:10:59 +0000

    match holiday concern respect constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
