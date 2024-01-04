57df0d2babbb446727f0bc07841697c2a5a114c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:16:11 +0000

    makeup branch present method gentle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
