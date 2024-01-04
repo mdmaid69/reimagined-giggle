bd433d49a3c172258752dc781fa2b27cafc51c4c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:08:19 +0000

    ticket trust low limit steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
