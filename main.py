dfce082d388c29c05b286b14b4ca09dd9cafb3fa
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:18:42 +0000

    attention eye equal violent whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
