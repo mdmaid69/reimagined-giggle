af24c2ef95653dc9195463237268685248792b68
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:36:27 +0000

    leg mix extremely system proud

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
