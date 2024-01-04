ca049040e5d78e1ad7d12338ddd88cd310d0e63e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:01:00 +0000

    active threaten question Ee read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
