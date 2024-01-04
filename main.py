06496067e7b197e5578d00d804080b5127e9ba09
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:18:58 +0000

    love turnback scrap smile treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
