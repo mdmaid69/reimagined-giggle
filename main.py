25b35107b034de20fc11a35f8776a98bf911db53
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:55:27 +0000

    development justice lookback(on) clue story

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
