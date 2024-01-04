1c06f07fbb8cd6a6edfc09976f60b9563c2b748e
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:27:41 +0000

    point spoil violence usual likely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
