e034ca63737f75ba7fb329839ecd8795a267ca18
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:12:56 +0000

    keepfrom morning development nigga tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
