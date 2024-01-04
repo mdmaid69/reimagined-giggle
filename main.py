94c8bf2a4bb421ee4cac87a58a3b5b974051d20b
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:15:18 +0000

    best above choose cool positive

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def sort_list(lst):
+        return sorted(lst)
