493e05652d73c6cf1ae6fefc096ccb87f541139a
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:42 +0000

    aware breathe difficulty feature agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
