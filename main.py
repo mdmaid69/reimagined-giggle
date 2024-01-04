029aee66a4a750119a0f0cb416bfe4401441d2b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:08:24 +0000

    right platform embarrassed low hope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
