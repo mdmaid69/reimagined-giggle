dc875071bf9623730820c9005860f4f23a6aabd9
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:06:37 +0000

    whistle free actual sail trust

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
