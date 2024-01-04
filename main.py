9152d9f2a713f959a67cd552406ea2b575fa04dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:39:49 +0000

    ear report slide loud instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
