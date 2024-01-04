13ab82cc8d7c77fdfa74105b2f5084f488745cb7
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:08 +0000

    back guilty false platform protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
