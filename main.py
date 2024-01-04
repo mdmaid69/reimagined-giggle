1987537ced236d8f5d0cb262e9e2fcb6f4f4537a
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:18:06 +0000

    each reduce bridge figure hospital

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
