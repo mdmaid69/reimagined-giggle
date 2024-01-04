92c3aae4ac9cc3cb75b5c2d02d14da6b968555fb
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:55:34 +0000

    build commit sensitive nor only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
