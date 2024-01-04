715d233494b1336f0af30ad79c605a6e3d238d28
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:54:47 +0000

    nearly plate apply support start

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
