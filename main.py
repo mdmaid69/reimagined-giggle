ef11043effba2d005fc90b835b5424e7048cf436
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:21 +0000

    bye ability theirs runinto nearly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
