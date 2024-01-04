9906a0cf508a154b681ae6ca9ea6667703002d26
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:02:01 +0000

    embarrassing book offend sink appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
