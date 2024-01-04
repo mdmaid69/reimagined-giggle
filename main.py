1cec58a1b8f7db8f3a2b24e68bb84268b23b299a
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:58:18 +0000

    nor first guess safety week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
