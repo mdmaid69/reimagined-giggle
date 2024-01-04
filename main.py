ebaf8efb9551e33155a3cd45f1ae925c18aca90c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:02:24 +0000

    crash choice action means into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_time_since_epoch():
+        return time.time()
