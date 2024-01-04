689450aae1ecb2cbcb6353aa8286418942e989f0
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:56:57 +0000

    paper crash house bored please

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
