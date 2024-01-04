6430feb927d3c7370032e6560248b9eae65651dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:30:02 +0000

    strict safety sink thirsty there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
