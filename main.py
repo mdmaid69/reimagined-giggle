efe69d1b16eee60c77ce1d4a66f33592b9d16cce
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:52:27 +0000

    disappointed payment march list with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
