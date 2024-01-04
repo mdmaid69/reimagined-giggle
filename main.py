101b7405916f43a805c7216068631d2ea1c2415a
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:56 +0000

    flavour in reaction might bad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_volume(length, width, height):
+        return length * width * height
