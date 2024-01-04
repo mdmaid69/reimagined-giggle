b34cba8f02276f7eafa42a380f19953b58664aa8
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:17:17 +0000

    flow anybody before pump paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
