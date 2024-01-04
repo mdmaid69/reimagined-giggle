04b07d152023dea48e78367836b2c046f069f202
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:42:24 +0000

    paper takeaway perfect amount print

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
