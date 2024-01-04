4fc571f4a21b526c41a1356b6a8aaaf93cd6c617
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:49:14 +0000

    trap say because sleep schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
