417de1491eea276eedd432e9689bd299f1db8ced
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:48:34 +0000

    care hurt Gg believe steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
