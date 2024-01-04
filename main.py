4c832a7e0316155b7d7618a86f08420178c1b9a3
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:39:23 +0000

    gate example find live at

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+x = 10
+y = 20
+print("Sum:", x + y)
