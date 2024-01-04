4e8e3f5e630dfcc7c1fc5daaebb8d224d9416cd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:46:53 +0000

    eager brother wet afternoon make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
