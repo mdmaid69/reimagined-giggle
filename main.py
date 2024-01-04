4944d27b1c6454322fd5a7887d112e326b7bae6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:43:43 +0000

    healthy bread steer undo refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
