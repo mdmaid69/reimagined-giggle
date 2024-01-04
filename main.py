3e4aab31b6a8dd96ae5fabc17d44828e02d8c509
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:58:37 +0000

    vote rely everyone completely avoid

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_floor(x):
+        return math.floor(x)
