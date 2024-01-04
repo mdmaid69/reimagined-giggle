1aa569df03b50afdf68d89e29a5acd46c812520f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:48:06 +0000

    evening dress advanced appeal impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
