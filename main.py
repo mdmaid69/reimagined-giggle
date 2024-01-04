51353f1f12120381b0dcce9ee81981f27a65a87a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:11:29 +0000

    borrow total react because raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
