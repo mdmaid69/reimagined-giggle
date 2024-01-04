65fd40159d8d2df96a643cf778b36e7abd093ca2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:55:43 +0000

    once thanks strike remember cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
