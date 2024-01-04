bbbce9c827a584e294e20223e14be60a69ad3ce7
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:54:59 +0000

    lookback(on) murder whom peace only

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+print("Hello, world!")
+for i in range(10): print(i)
