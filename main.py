835e0955acf3652c13582a0111581aef724fa9b1
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:10:07 +0000

    564encourage power row keepout modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
