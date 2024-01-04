2621dc08f5fba819ac3fbc66aa3487b108bb993a
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:24:54 +0000

    mark disappear if break similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def check_if_array_contains_item(array, item):
         return item in array
-def cube_number(x):
-        return x**3
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
