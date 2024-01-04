8467169a63b170fe72736b6a1c3e724c5c04e88c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:45:15 +0000

    edge will story treat insist

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import sys
-print(sys.version)
-print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
