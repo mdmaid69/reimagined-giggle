0e0dbd2789cf622b78e20ecf0105a0bd138ea1d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:17:30 +0000

    just calm mine giveup occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
