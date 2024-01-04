86f511d264338f97fa4ac3024ca8678a825e7605
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:04:57 +0000

    top between about any slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_priority_queue():
-        return collections.deque()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
