90459035b3e7db7bd93700ee5302a47d9cccf40d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:13:31 +0000

    trousers mess vote official defeat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
