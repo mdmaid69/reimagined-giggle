bac9c2d2f6a9627976ffc433adaee3d525213976
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:37:37 +0000

    passage giveup takeover cross spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
