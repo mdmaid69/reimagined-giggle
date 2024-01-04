fb4d7c665fbe880a34cd0af781106a12ad419cf6
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:28:06 +0000

    572enter pen some turn advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
