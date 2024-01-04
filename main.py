e896614660acda8e904e657fe762530c3539603b
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:56:18 +0000

    awake everybody Itself within illness

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+def cube_number(x):
+        return x**3
+  def remove_duplicates(lst):
+        return list(set(lst))
