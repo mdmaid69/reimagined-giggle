d6bf06f9d3472b6b94167c9dc41bd8e2eedb74f2
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:44:36 +0000

    shout anywhere character rough Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
