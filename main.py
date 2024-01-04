fa62f0b57e61f42e781cb8522b816e9bcc8c5e07
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:06:33 +0000

    Hh bringforward patient century contact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
