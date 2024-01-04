43033b394c52838f82ac8adc63da681d5fffb204
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:20:56 +0000

    never number justice wait listenfor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
