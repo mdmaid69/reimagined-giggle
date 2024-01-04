f84f435a8b87f4801f1049b0d3eacf702d61f5c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:13 +0000

    favourite roll wonder pupil increase

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def convert_array_to_string(array):
+        return array.tostring()
