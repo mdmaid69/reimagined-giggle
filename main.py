626da0b02b9e2035671457c1788348543715ae0e
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:11:03 +0000

    nowhere Rr well shock before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import time
+print(time.time())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
