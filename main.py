3f73754497ee5e246e2b84b094517cbd8cd451d5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:01:17 +0000

    sting 569enjoy friend community store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
