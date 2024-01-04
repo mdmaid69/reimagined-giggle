5ed53f22346a2d1115b2414adbb961fb747e7b16
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:12:02 +0000

    gas tall cause poison cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_current_working_directory():
+        return os.getcwd()
