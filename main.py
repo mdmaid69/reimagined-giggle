1ad4360caf1219ff6ba52161a7ee2b3575d5b091
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:21:48 +0000

    it callup glass female strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
