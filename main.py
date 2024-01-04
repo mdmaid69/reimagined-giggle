7a276a29baaf561bd55f39e911ff2c1bf2ca98b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:46:33 +0000

    identify call past every yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
