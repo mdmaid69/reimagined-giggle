7ff32ead1953e9eb2e56b9e8f80521bd2548819f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:52:22 +0000

    long traffic search boring which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
