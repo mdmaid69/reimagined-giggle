f2ecf1d6ecb7dc145c12490740e0bf9c6034daed
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:52 +0000

    bread long history exam serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
