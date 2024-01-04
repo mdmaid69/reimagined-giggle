219777c124524f4498a004de41c1a5c12fcadf58
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:38:48 +0000

    connection ride news breath official

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def square_number(x):
-        return x**2
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
