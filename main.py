23c0c53d21159649ebc5ebf2d237ae72ee159257
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:37:47 +0000

    command course occasion breakdown money

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
