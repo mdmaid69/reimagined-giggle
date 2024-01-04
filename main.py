63b57a9198a427f3ac5e443e2435959cab6c9fc6
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:59:11 +0000

    excellent strike while leg trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
