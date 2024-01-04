fd4c0539d97b08a106b072fa128e3df2b41dd97e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:26:56 +0000

    accommodation contain violence back angry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import sys
+print(sys.version)
