d02a847d332f5a23c9080055991e3a0108d5c470
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:10:13 +0000

    eat whom support range funny

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
