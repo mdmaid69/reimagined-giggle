16140ca835d7274745e146176653312cf589633c
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:55:28 +0000

    alone Mm upwards repeat sad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
