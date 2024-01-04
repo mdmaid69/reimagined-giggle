9703fb16562a91db9f17488451a4ac74db8eeaf3
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:53:09 +0000

    makeup part split mark regularly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import time
-print(time.time())
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def is_odd(n):
+        return n % 2 != 0
