0209024b36d7ee1f889df4a6fc871c31a5d8ac2b
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:24:27 +0000

    along delicate bringback right than

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+print([x**2 for x in range(10)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
