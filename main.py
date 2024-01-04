39c01b1a79aabb016f4bfeb8f3738129d693e9d9
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:27:45 +0000

    interested turnon file relative description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
