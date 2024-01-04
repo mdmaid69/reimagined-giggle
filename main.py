be8491a1df17760b4d91625c79766db3c59758a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:16:04 +0000

    lock middle advice software own

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-from collections import Counter
-print(Counter("hello world"))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
