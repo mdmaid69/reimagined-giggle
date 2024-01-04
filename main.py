b07c54dfb6febd685490375a643ca7bacbdf56b7
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:54:21 +0000

    eager married comeup leave love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
