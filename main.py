0c7e0bae97ef28f50fbcff7bb10555e48aa6123f
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:42:41 +0000

    birth acceptable vision body serve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
