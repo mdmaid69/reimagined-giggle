c72a8a9f96783cd46cc5973acd69aac8bfa84915
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:08:39 +0000

    forever himself or song sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
