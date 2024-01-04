daad3840155be246f48d1310cf9f1c48ebb0c4b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:46:47 +0000

    pipe abroad care damage foreign

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
