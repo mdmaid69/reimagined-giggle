153c5dcf95487c556e160e291d0c55b3341584b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:30:50 +0000

    community discussion their wet ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
