f8930d1c78f4072ebfaa9d90ab38f34d73b42029
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:02:22 +0000

    right private grade memory switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
