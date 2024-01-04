3d543241c0df978bbf7709691f48350b1fa89c3d
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:38:06 +0000

    play trick expect effect morning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
