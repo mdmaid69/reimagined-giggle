66da3ceb917749dfcac94cc76cb905ebf3824bad
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:21:23 +0000

    or stage purpose nowhere zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import tensorflow as tf
+print(tf.__version__)
