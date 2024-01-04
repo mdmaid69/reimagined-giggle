e5b20ecdfc04376a73b6f05e44476aa908ed4d07
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:34:34 +0000

    return often OKorokay far plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
