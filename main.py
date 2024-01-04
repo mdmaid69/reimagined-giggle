2dd0fbc61f399c7d28488d581d85a5e7c2200f92
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:55:50 +0000

    suggestion thinkover only organization transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def clear_array(array):
+        array *= 0
