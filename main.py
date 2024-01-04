cf33896b4d65f1ddb4c1b1a361edef4576b04f87
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:17:45 +0000

    effect post burst shut passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
