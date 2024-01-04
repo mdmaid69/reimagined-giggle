ebb82d1f02655c4d9685b230718dd202b9a119c7
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:39:31 +0000

    down tooth replace active autumn

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
