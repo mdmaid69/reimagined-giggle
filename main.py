58a319405c4084432722c3c16402f7ec28159adb
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:14 +0000

    themselves of violence office rescue

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_acceleration(speed, time):
+        return speed / time
