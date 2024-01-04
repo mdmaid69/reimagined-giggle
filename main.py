f84196f72ab73ec20dbb321d671ea449d7a5bd44
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:22:34 +0000

    solve clearly eye minor work

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def add_numbers(a, b):
+        return a + b
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
