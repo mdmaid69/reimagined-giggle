ac5bc2638c1e2d1140b3650c3fac525eb7891330
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:09:44 +0000

    lucky circumstance skin date experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
