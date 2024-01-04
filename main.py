295d0e8b9132606430cc73e68c86aa98865da015
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:38:37 +0000

    available calm throw general uponon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_floor(x):
+        return math.floor(x)
