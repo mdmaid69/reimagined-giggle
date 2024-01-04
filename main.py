e46ada3912b90d955051d82d6f3b8aaf5f024296
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:37:39 +0000

    along object home influence eat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
