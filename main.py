baf68a5098453902bd6a9a02f8827a351927b9ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:19:09 +0000

    prize charge phone steady acceptable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
