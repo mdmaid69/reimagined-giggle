d9d94ad2f9613c4946439e23685fd5faac04ec79
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:48:44 +0000

    interview communicate similar winter danger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def count_elements(lst):
+        return len(lst)
