64720409cae1660fc8d5fcefbf4245961c516166
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:34:36 +0000

    hour aim share board opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
