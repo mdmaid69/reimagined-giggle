2544c24b410b230fd62c87148c771e641ff2506e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:52:48 +0000

    steam present taste benefit duty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
