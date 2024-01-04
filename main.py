c72206c49721bd81e116ce2ace16733c481e58c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:06:21 +0000

    surprising complain lift badly interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def count_words(sentence):
-        return len(sentence.split())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
