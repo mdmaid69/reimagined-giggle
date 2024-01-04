bc4d7b1581a74f671d040c11731cb88050ac8eb6
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:52:53 +0000

    slice ill go herself seriously

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_tangent(x):
+        return math.tan(x)
