17fa3999577c6f7cfa9c26f286802d20ca60a28a
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:49:26 +0000

    knock make hope later maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item(array, i):
+        return array[i]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
