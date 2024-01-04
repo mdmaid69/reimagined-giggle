9897a665c739bb925cff4f50276e9242a53fd02d
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:30:57 +0000

    from pin key state cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import sys
+def exit_program():
+        sys.exit()
