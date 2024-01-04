29728c24ce66a800079d8930fcfabae7cd70606b
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:29:51 +0000

    bad shall declaration sleep and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
