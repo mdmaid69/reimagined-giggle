d6c110ab300aa05e11d03580fe3cb84120db616c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:05:46 +0000

    rarely mess steady wash information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_elements(lst):
-        return len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
