99f8dae722d1d2b02092bf5d43cbc64a6b0a3023
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:21:27 +0000

    trip discussion treat finish your

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
