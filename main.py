3436105c49ef30d3905cb101391e804f6fb8e8e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:07:22 +0000

    condition consult tight quality train

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
