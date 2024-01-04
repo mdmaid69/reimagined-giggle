1c47fca1ad20dcf3875790dae97aecffa4f4a663
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:53 +0000

    obvious who Cc drive fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
