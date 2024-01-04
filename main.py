da4f306aca9ce5fe984538b4374cd48f64b6fe8c
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:33:07 +0000

    shop tight accidentally takeout direction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
