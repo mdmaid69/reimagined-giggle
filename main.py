69edd20727f0a9306b8fcd48d2d18421c71307a1
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:45:59 +0000

    goout official journey ever choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
