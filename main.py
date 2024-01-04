debc2662630a9c246f51e892f868b4ad89ad54e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:26:53 +0000

    hollow dinner minimum firm nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
