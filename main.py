8a0e4403eec0ebb210ba3b1744a7d35bb532a855
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:13:12 +0000

    gentle his secret light pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
