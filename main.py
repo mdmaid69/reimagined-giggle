669dfd8b351e32ac5f0e482cf5ad68ee66b8f3ea
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:25:10 +0000

    year office rule run together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
+import datetime
+def get_today_date():
+        return datetime.date.today()
