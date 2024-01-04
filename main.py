1537631a08ccddaf78a11c56648963cb1008b6d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:08:19 +0000

    type immediately bound order neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
