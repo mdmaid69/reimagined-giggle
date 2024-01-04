fdd635199d823e4e82fa52c152ae575831d41efb
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:43:21 +0000

    married record motion price confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
