6e0f0a274e226bb9ff62ab9092b31fa9eae1429f
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:55:13 +0000

    few police brain begin cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
