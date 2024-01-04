d769494ac9598636051df004828fc4d7fd03ce44
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:45:53 +0000

    clue report dance black confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
