81e05dfa929ebe3694efdc574e2171330ed824df
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:30:24 +0000

    condition experiment greeting city whistle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
