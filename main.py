de2034f9e9771f4b81961d23bd82f411bb83d7c8
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:17:23 +0000

    effect each church present bother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
