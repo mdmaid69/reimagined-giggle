de8f88ba6c2c9eaac71a16af4730ea6d84d33655
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:34:19 +0000

    once middle shock which in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
