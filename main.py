9ed57e30c04e0caea32cf562fd06286dcadfb4bc
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:37:35 +0000

    be reach extremely that confuse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def convert_to_binary(n):
-        return bin(n)
+print("Hello, world!")
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
