bd325976bd811d7f31d79be00fd0d4bbcba45553
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:13:32 +0000

    mention great car justice sentence

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-print(math.pi)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
