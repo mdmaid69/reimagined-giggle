415857a01a4573dc8ae68881761baeda86cf8ba7
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:43:55 +0000

    interested right order feed keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
