13193b1cdf619422d354c366e71bb2fa3c2cdb94
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:34:24 +0000

    respect bridge comment centre quickly

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def is_odd(n):
+        return n % 2 != 0
