61584356ee882133e83fb4df561df6fff5e5ed19
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:25:47 +0000

    ring hello mind Kk close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_max(lst):
+        return max(lst)
