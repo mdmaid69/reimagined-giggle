614dd5838b1d1011c8a9a3cd831667fd66691209
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:16:29 +0000

    careful like goout bin stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
