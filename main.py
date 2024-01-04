5921e1dfb058fc9cd9ae0766a9ac651b0b30d89e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:56:56 +0000

    crisis list tragedy Jj announce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import sys
+def exit_program():
+        sys.exit()
