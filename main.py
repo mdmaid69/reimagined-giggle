091bcfa5e3e49bdf8493ef66135d2376eb3fa306
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:11:23 +0000

    sleep hold fair off basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_list(lst):
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def reverse_list(lst):
         return lst[::-1]
