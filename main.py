8f8b5e82b68fe1dea806b42495ebfe29cc4d5ec7
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:44:07 +0000

    original one bet in giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
