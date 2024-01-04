0b04961daf7c482bc550f819aae1f9b0e8d6f17f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:54:36 +0000

    character read reduction height off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
