8dec18627263e320119cf10dfba238985a71e934
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:58:53 +0000

    smooth call offence church on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def sort_list(lst):
-        return sorted(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
