fd31f8f109dc6ea321482f327d2983db0061427d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:38:11 +0000

    relative reply how 565end fun

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_area_rectangle(l, w):
+        return l * w
