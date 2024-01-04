a28330db4dcb8ffe6685fbcb07e5e765cdfea6a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:12:07 +0000

    whistle disturb admit lock love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
