bf784ea9aa7e5232e101cdb9cd9608ac65f80bec
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:13:56 +0000

    fond suggest narrow how physical

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
