b3e967bcd8d77798fb1d0e93b18e68fe826534f7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:41:06 +0000

    age record suppose rare goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+import datetime
+print(datetime.datetime.now())
