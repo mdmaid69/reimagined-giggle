77c7c47a343d9b909a5177575119db8dabe7cea2
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:39:37 +0000

    often please whistle surprising below

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
