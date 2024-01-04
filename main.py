a0decddc4faaf73152d5241de2dedaba9de5f1de
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:34:29 +0000

    branch sight smell war often

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
