f374101ceda2fa8084f09d96af8241e9c6c36b41
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:48:43 +0000

    distance hot breathe see exhibition

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+print(sum(range(10)))
