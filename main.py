97058211cee933790853480b52d3c8561d183d41
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:06:38 +0000

    sad inform afford majority drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
