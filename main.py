4294dd2d7d16d12ec37f57e1e72a9c99727c83ed
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:35:01 +0000

    drop tend similar through culture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def add_numbers(x, y):
+  def add_numbers(x, y):
         return x + y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
