4af3c305b925509574282c8bd73d58887f4987f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:31:10 +0000

    strike doctor comment scream soil

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
