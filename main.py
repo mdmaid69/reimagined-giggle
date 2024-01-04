a6802ba5c92027efc8b2c5b4ebd249089a0e2ad4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:19:42 +0000

    television care trousers frightened maintain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
