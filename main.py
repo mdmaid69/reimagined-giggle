0023d08058dc9b299aac2ce9db223a15d3e7544d
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:36:54 +0000

    569enjoy guess night thinkof recognize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
