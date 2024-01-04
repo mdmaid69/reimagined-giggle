f74849a207876bb1250f82662c1dcf605ec8625b
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:01:07 +0000

    perhaps owe wait deliberate obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
