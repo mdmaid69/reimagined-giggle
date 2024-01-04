8d2ef9bc5411a752a8e7b6e67ae7d2e3951c54da
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:31:09 +0000

    hold fit run team half

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def convert_to_hex(n):
+        return hex(n)
