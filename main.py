b207b8da2756c72653ed758ff239a12bfaac0c8d
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:13:28 +0000

    but public square truth few

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+def exit_program():
+        sys.exit()
