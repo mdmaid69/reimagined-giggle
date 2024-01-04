117acce4b9ed636d4d24408944636448c1ce3534
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:59:43 +0000

    carry pressure object with pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
