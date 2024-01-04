052c4c0b931e8b7d488264c3c3e229fbb523fca5
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:41:51 +0000

    still interfere tour lookafter still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
