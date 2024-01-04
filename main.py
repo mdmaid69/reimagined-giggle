bb4c78902dd27aa0cb65363b0892cb4eea6c24e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:44:58 +0000

    coffee wake lock certain peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
