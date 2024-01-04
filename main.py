48764f5e0684cccfa3bab1207a0acd8630ebdf25
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:04:31 +0000

    smart nervous chance offer later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def convert_to_octal(n):
+def convert_to_octal(n):
         return oct(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
