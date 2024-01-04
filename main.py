8cb2823d568d96303ac49ed5991676eac2342a71
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:00:58 +0000

    right search ago turnover married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
