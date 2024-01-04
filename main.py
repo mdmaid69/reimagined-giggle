e2eec77b516efc998370d335658e7e78979cd124
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:12:59 +0000

    paper positive since count surprised

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
