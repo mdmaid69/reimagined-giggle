c8a61a154c73a7603a8ab190e6b20e5ac152e325
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:56:01 +0000

    deliberate represent force interested content

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_acceleration(speed, time):
-        return speed / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
