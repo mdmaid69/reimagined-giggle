7c3d17e8c6a8252f62545081bd1606dbba731e54
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:49:16 +0000

    comedown recovery choose top answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
