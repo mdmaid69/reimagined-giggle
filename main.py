32f0fdd00a2d8373e2ab13c7f53070d8d30a9943
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:19:18 +0000

    many travel black quantity getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
