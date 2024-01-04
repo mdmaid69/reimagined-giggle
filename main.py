37a55456bab71d1fedb7eaf04ac0ec566f9e2c4d
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:46:17 +0000

    interest busy shock present quantity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
