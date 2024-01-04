a06e4f3274339b2ec154afb0e727a5a0df06807e
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:32:12 +0000

    slice team hello negative profit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
