9dc824db5c75480c34170f1d8d448b7d38546a51
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:24:02 +0000

    performance before birth down autumn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def convert_to_octal(n):
+        return oct(n)
