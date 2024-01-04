6ad23ba884842f1decb9c35bc79418cb5397f19c
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:23:50 +0000

    movement turnout famous height brush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
