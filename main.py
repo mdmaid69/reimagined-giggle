205dbb9389b57c5e6fd50e69474b01b9b22248e3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:13:21 +0000

    discovery basically settle typical opposite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
