e709e656a38ca2467c9721bb3768c23c5370c16e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:09:17 +0000

    touch concentration defend risk would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
