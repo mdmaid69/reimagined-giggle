b9f8ca14e50cce9e0e6ff01b7be85fca72c4acc4
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:39:01 +0000

    false dig adjective think appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
