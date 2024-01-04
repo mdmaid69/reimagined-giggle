945935c9fd397eb93fb973b464559152df569b07
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:53:49 +0000

    reply sale reduction spring honest

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_max(lst):
-        return max(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
