72af8fba710c67cca6e0e6c78adb7776c2b82f70
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:56:20 +0000

    show special near willing must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-print(sys.version)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
