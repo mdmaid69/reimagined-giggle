0f00e3cae2390b2332f0cb16f124b3058887068b
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:46:28 +0000

    drink ticket doubt refuse system

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def calculate_area_rectangle(l, w):
-        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
