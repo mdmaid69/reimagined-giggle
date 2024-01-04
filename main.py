83e807c76b7b9d7c34e901f4d5c0aeca79827b1e
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:23:15 +0000

    recover respect connection principle have

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 def calculate_area(radius):
         return 3.14 * radius * radius
