54c28cdcf76ade7751af9686c2e5b9ac4705b00c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:26:45 +0000

    allow take onto raise skirt

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
