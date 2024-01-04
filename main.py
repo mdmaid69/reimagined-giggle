5d3e5ed18bd09f683342f5b30f1d83b23153da91
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:23:11 +0000

    proud bye left calmly finger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
