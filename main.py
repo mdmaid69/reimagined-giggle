44bb6f477de4a95023e721835ed8247ac482e606
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:43:43 +0000

    anybody name nowhere wide opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
