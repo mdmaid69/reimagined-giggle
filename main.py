4a56152808ec9986f49ddd2f1ee6538cbeefedaa
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:17 +0000

    butter separate except tip hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
