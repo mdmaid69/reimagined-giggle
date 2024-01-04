2f64a6b937263bed37e4f3ea5903caa43fc67628
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:17:10 +0000

    accidental Uu otherwise getout row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
