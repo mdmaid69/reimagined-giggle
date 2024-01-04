c53d46683fdf7f35afe143ab1b9de10986ca3009
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:34:05 +0000

    story behind label achievement choice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
