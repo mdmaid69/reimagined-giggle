35790d10cea61ac9f2b35b7694c4df6d40297ed4
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:59:26 +0000

    more better actual talk skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
