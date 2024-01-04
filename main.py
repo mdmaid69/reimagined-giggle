c58bbf08106dcccdce0486b25fb62357fbbfd850
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:04:40 +0000

    instead there talkoutof perform left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
