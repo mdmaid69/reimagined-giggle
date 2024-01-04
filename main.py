c013b5811d7941f0ee50416d5645416a7d752c40
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:22:29 +0000

    stone power use although failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import collections
-def create_stack():
+def create_queue():
         return collections.deque()
