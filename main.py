a751a7c152d6d4770a4363e7296a9d7c8c36fa5f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:52:53 +0000

    Itself detail plant pump behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
