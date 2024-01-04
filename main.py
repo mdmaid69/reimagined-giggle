93464908d2a9ca00b58f157aaea9cdd3c5dbe135
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:32:38 +0000

    beyond forever fun price load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
