bcb272bc2876fb4d46e9c49af97d81746129f1b0
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:16:58 +0000

    few Jj insist exit relief

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def square_number(x):
+        return x**2
+  def subtract_numbers(x, y):
+        return x - y
