7abc39d4889edf2c5b1d53ed12510fe53982680f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:06:34 +0000

    minimum finally speed along development

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
