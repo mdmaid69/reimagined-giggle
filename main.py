7ed8297534ac1abaa2792ddf8fa039fa261736ee
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:08:21 +0000

    clear way temper write getaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
