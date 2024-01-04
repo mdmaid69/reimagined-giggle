1e9cd39b49b100b8457bd04fa2e325f2d72eac89
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:43:44 +0000

    culture test takeoff crime news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
