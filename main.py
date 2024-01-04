c24f22a6f906caf32f262cad13d8a54570294aac
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:29:30 +0000

    display basically stand tell depend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
