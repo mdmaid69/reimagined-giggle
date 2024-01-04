595b8d93c9c02e8e5d4e2c858fe213d9989cde5d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:58:52 +0000

    store afterwards convinced protect pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
