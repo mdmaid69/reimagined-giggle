7cba1411e6962e6840181be1f292211aea384b3c
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:36:01 +0000

    criticism unusual crash pay meeting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
