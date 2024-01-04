a4361c544354a4f730206678d2a1974885a84cc2
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:19:19 +0000

    spring likely wait takeback although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
