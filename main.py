95916853af10bd94250bc891964d96dfd25a27a2
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:17:04 +0000

    movement little right workupto habit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
