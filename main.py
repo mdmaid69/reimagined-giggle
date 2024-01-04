e7d5b1621927292f9cb4ad74ceedaee4deb1534c
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:04:39 +0000

    deep pretty thanks temperature mixture

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
