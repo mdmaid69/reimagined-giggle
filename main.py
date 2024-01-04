a4932fb2ca2311d40e3869a1425799b637e2ce53
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:55:43 +0000

    back sad hurry Oo study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
