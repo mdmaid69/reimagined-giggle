5a5efe4111b36cb11a885a8feb49d8ba0b7877dd
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:05:27 +0000

    accident face pick could office

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_max(numbers):
+        return max(numbers)
