8cb8324aea5fd71064b4afd935bfc194c9f57292
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:26:47 +0000

    myself class seem breakup funny

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_length(array):
+        return len(array)
