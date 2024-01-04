66c41962f82302d701ec1d0759727fb42499d1e1
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:37:39 +0000

    hear delicate ago lucky recommend

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
