343c3ade79c356feb11722f92d0662dddea211c4
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:41:59 +0000

    ours study bottom welcome height

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Uppercase:", text.upper())
