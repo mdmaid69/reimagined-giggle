39b22a97793ff1637f5c290db3b689606b2f198b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:27:23 +0000

    now equal noisy zero mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
