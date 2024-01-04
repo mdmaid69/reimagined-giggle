5908db854b7a4598cd18e093817605efbc663fde
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:42:40 +0000

    mess hold speech maximum skirt

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
