aecfdcf73d96dfefa94879398c61fe448cc25728
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:21:12 +0000

    cause question call sit young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
