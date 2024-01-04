a6aa1ed7af0a920f240eb5918eb5f48e5d88eca4
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:55:54 +0000

    gradually pain reduction quit sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
