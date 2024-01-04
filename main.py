41f6c363e54bacdb5ea4af1a4b1ac31cf4935320
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:46:47 +0000

    please previous unhappy ask juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
