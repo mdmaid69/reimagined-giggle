dc1d2dc6ac6aee71f0e62c37aac883a71da374b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:56:07 +0000

    favourite wave without season comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
