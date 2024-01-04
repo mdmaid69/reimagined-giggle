4f8a25660b3492f2413e6bbeea7552432c105087
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:49:28 +0000

    city mistake comfort extremely flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
