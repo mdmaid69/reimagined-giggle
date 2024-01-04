db3b96d335e7560c787d6a448a4211f4e4e05548
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:19:56 +0000

    after argument now recently act

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_current_time():
-        return time.time()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
