df924a673aceb60c21a8ea8534e4c665c3c6d56b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:05:34 +0000

    Bb apart swallow side practice

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
