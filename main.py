1f59d05ca8e24c396bd0ade8c448a8da5b6440f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:51:04 +0000

    strange professional 179bite continue out

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
