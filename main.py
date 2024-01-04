2b52381f34edeaf3ca88b2681c68755d5bff7506
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:22:32 +0000

    strike tidy minor less vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
