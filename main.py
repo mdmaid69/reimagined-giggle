ed99cd2183d7bd3d2ef62210f7cdcfffa42255b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:09:52 +0000

    lose accommodation urgent people uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
