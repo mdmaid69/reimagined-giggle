625ad352c96a5b2ad153e909a8b042e3de84556c
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:30:10 +0000

    Kk listenfor back stamp doctor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
