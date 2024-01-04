b079fd95e6e5c6e049587386dcbcf779cf450fd0
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:50:00 +0000

    affect fun leave worse shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
