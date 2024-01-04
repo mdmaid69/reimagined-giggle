798c9d6753b27f0c49248da4cece25ee06cf0af0
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:00:19 +0000

    and Cc journey us last

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
