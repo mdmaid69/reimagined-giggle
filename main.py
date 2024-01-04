e33fca33da5c44d5325a616825b9d814ca7d8416
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:48:50 +0000

    only whichever week possession suppose

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+name = "Python"
+print("Hello,", name)
