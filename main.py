139b262710b5bf19c009fbc0e861eb12bb30f5b8
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:36:10 +0000

    copy manage there goodbye object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
