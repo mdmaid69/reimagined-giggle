a145668b462e640948736b4a3e4cf988859bbac0
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:04:59 +0000

    storm disturb single pay trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
