6901bdf8a07edd0cbcd254df680ec2d714c4830d
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:59:16 +0000

    once software slide over trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
