62fec5183f37b61fe4bda8217d0694cbb26fd422
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:05:06 +0000

    full black save still fast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
