2328301688f4c52b1894e0807e6e2f7cf0fed630
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:39:36 +0000

    ready fill community loss secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+print(os.getcwd())
