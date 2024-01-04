cebbb602a71cb1f3d277ece6dc81bca7dc6e809e
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:09:01 +0000

    private now fit cut file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def find_max(lst):
+        return max(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
