1ce6fe48451a428e8c6aec3ec0a9483eecce6d69
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:09:14 +0000

    traffic disturb cover flavour no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
