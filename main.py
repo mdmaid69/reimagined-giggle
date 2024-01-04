3c6e00a01c9f14fb7d7138d598a018e2b055a674
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:08:17 +0000

    backwards trap top giveaway persuade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_binary(n):
-        return bin(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
