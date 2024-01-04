0415f79801e0e15cbaa970909e5d00bd8ed09c28
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:15:58 +0000

    born clothes search sorry measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
