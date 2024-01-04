09bb538ef094babcf48bd917f7336f493b8c183b
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:00:51 +0000

    typical needle post that coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
