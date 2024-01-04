ad8a5dec66613cd2d957408662ef87b7945adcb8
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:27:54 +0000

    inform challenge slice actually authority

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def multiply_numbers(x, y):
+        return x * y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
