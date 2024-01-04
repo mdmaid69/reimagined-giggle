7c5b62b8f60335ac57e06aebd65fcb3c1cd2832c
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:01:02 +0000

    stir professional scream succeed ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
