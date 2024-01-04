a9737eeebd08d697253b086e4d2f122e10cb599c
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:38:02 +0000

    defeat judge advanced bin flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
