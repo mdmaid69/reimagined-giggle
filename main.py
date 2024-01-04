3457c0fb176aedfeb77431bd2daa4811f677283d
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:09:44 +0000

    hers complaint argue square brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
