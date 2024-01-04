12214fd10bcc96741b35758ed57b3571ac1a2ffc
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:00:28 +0000

    turn brave goaway whisper expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
