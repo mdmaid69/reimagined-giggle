bf7f5a1e59fad0192929c0f0d29a027f14cfb13f
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:25:52 +0000

    take discovery make breakinto regard

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
