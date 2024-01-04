ae8462dc46413c129bc6b7c1a7c2a1ba84cb6d44
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:52:09 +0000

    afraid object progress woman table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
