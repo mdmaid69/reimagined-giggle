054d5d108ad7f84584ed9a9842e89e7d1e6e6a4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:35:06 +0000

    some food bound lonely up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
