8f5b1d9ec78d97e60370789b6f2a650b05c15786
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:50:52 +0000

    care needle comfort ocean less

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
