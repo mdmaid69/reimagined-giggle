325d4156df04a2f700181c900da8997dbc11152f
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:33:59 +0000

    transport conversation relief long mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def convert_to_octal(n):
+        return oct(n)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
