5a0eb8bbd60f1c3ac3a65e3b347d67ba2e087494
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:59:49 +0000

    confused please comeover past group

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def is_odd(n):
+        return n % 2 != 0
