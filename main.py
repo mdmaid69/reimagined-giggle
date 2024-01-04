88693e420265b15ffdb28704f1cda5d6649f0c98
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:23:56 +0000

    term morning error below right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
