30f6a68c9bb8206fd73a39fb0bd70b4d2439eb35
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:38:53 +0000

    arrival forever ability detail write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
