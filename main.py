253d5639100024b172866b45c14c8af331168bae
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:11:14 +0000

    proud improvement afraid shake breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import random
+print(random.randint(0, 100))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
