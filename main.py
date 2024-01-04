d26ffba9d42f6d0c8e8e6809787157a08c542b28
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:23:53 +0000

    move pride mood notice funny

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import itertools
 print(list(itertools.permutations([1, 2, 3])))
-def reverse_list(lst):
-        return lst[::-1]
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
