3315d06858c40a6df7afc184d4f6d3a6c63c8ce9
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:55:32 +0000

    base to apply connection godown

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def append_to_array(array, item):
+        array.append(item)
