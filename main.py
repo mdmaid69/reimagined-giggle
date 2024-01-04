83d45e44af0e973834e5648aabde0174a6858c89
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:56:07 +0000

    turndown impossible squeeze genuine pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
