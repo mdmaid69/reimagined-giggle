a80dba0f101c3605f8ac0a7d9631175deb57ee04
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:08:45 +0000

    market wet lead suggest check

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import logging
+def log_message(message):
+        logging.info(message)
