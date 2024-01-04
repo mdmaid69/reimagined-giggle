084ef206370b19511e79aec5ac03d6b9c44e65df
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:58:28 +0000

    expect contact raise you prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
