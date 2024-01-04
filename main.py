a17b872f04061a540aaa15df98f8f710cf3600a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:00:50 +0000

    walk address visit stage organize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_list(array):
+        return list(array)
