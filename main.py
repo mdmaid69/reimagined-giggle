17384a6e5923e0f475306b9fd138b0bd158fe241
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:33:27 +0000

    yesterday even fierce correct shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
