77f27c35deceb5cf349fc9ca8bf480c1609740dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:38:50 +0000

    rough attempt mention now exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
