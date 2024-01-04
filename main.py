0768b84486587213576b3e653ea9b0083eb3e6b6
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:55:36 +0000

    wear practical belief day bus

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
