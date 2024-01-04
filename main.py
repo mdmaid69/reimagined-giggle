e624ce9ee59191e72e4a3a7e8062de713101eb33
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:58:53 +0000

    floor extra neither drink none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
