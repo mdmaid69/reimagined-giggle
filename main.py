e4109418d311f238e96028749bfb6481ae304671
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:05:50 +0000

    pull admit dry base range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
