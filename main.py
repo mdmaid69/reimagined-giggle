2dd53a7096eb4adc22fe00c3846ff14255cc3b07
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:03:31 +0000

    declaration soft slight maybe vote

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_current_time():
-        return time.time()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
