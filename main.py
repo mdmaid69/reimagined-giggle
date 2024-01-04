67d17eb2ed2afd3dc66064c50702c8fffd520d4f
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:50:08 +0000

    basically plan along mass contact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
