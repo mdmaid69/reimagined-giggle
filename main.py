232425a229a76d7f84691458a94162fc76deb321
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:23:52 +0000

    please back challenge pinch criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_counter():
+        return collections.Counter()
