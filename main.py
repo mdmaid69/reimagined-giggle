4e8d16c78301fb0b490785ceb487bef8ea7cf8cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:35:34 +0000

    connection song black replace trip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
