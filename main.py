caea81b1636acbdc24ed0816c6da440240054a05
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:32:20 +0000

    wind separate real second attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
