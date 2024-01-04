8c00eabd39bbc5c8d9dea76c252a00e3128e7ed9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:07:57 +0000

    model murder lay up creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
