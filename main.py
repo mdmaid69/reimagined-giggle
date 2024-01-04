3981a86b627a09a1fd716dd5dc829da0a1ca9e69
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:40:37 +0000

    keepfrom getout necessary pull sound

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
