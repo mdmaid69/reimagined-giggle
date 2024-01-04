9e282ee9daa11cdb8c5273f2cbff66fc522e8629
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:06:57 +0000

    before gate frequent priority still

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_pressure(force, area):
+        return force / area
+  def convert_to_binary(n):
+        return bin(n)
