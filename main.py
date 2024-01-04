fe3b44e02759894fbeb84605fb705d0e1fd67edf
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:53:52 +0000

    anyone moral talkback extra yet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_length(array):
+        return len(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
