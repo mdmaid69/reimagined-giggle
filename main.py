71241dc80d9d56c200dbcab8e684334e0437c235
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:00:31 +0000

    country getback clean them contact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
