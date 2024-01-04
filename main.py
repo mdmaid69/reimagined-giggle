432d2aba33313de35f453f97479fa8090d38ad95
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:06:48 +0000

    corner visit lookaround different change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
