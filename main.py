6c77d576f8346ef9daa1872521581d59187a0bd3
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:07:32 +0000

    bill right communicate load original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import os
+print(os.getcwd())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
