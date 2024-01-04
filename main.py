37a8ab37be46218193a4fc6e4c56dfaa99aade18
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:49:01 +0000

    too autumn feed catch physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
