c3ac28ab775abdefe4a3984c693008b8bf6b6565
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:31:43 +0000

    comment win formal comein lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
