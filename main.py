1089727576a2b4775363ac949b8af4dc22470720
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:12:30 +0000

    boil example combine paper towards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
