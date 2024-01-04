0b34584e447e8d6a2d71f7547c2b931f3dca2fe9
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:03:40 +0000

    angry special satisfactory old purpose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
