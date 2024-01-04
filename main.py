5cdc7d983e41ec7764baa3c3d11e36a55082e004
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:46:26 +0000

    can breakout talk phone today

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def calculate_area_rectangle(l, w):
+        return l * w
