e660e2eac32b4fd927dae08c236ee3218a3f00a5
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:56:56 +0000

    trip temper you attempt several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
