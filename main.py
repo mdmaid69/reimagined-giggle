e59405c989d82e2e08028cb0f0e8195c81732973
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:55:10 +0000

    born tour rescue field later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
