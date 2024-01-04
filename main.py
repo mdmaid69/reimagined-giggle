cb06879db5ef60c699cdf69f4c72f0a1c55e35cf
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:45:25 +0000

    tell tone border active than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
