062a18a863489ea08fd525cb1fd982350758d4b9
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:18:44 +0000

    swim much slightly cool delight

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
