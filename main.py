23190cbf9f9608a454add2510d27a952ca696c80
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:31:07 +0000

    spoil test accept threaten record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
