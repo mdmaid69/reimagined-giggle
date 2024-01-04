5dd2d01fcd2904edfa405c7330a28fc4e29715ae
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:01:51 +0000

    nigga angry Ee set black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
