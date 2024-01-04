e78e433f55041606a8ac8e1a7ad6ef258c0f5e95
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:19:16 +0000

    wander politics standard allow sister

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
