4ced403e88f46a22197c5108cc8df8d8c9727d08
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:13:56 +0000

    shoot carry Rr wind worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
