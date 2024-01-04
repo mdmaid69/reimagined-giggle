274ecd24ccbb95d034c7321d91076052651f666f
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:15:17 +0000

    pass several reach on jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def convert_to_octal(n):
+        return oct(n)
