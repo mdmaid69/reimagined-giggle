a5857f55d975dbabb3bc38b008ba4acc1844c67d
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:11:03 +0000

    black soft ride oil takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
