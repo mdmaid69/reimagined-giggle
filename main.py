b0d59237658ac1f2643e8d8b68329665f0ddf49e
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:56:48 +0000

    secret city wire calmly forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
