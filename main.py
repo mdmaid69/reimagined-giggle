935cff2666c4c02c9e6be3324d3482293bfc48aa
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:32:19 +0000

    turnover best breathe concerned street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
