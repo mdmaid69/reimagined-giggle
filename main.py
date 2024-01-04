889bdf0de996a516e81e5c2485f13119fc7810c3
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:29:25 +0000

    comein against basically clearly generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
