bb7307273dfac230f499d35a1de105789e5e8ce7
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:27:28 +0000

    switch level calloff difficult thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
