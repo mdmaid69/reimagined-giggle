4c2d4709da4a588a3821f996c3e9598e4e50a66d
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:55:30 +0000

    effect cover ourselves rely add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_volume(length, width, height):
+        return length * width * height
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
