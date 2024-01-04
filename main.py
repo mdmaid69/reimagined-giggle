36c7b78ff3f59377625dbc7cebdbc50b442590a7
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:10:30 +0000

    view already tidy lend nonsense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
