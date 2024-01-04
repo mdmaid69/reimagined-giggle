287c8be2f6a3b409504908ffc5ea11adaaba675b
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:04:39 +0000

    view phone whistle fire above

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
