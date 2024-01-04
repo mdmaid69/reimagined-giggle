a4dcdc70e2813907484eb72d948d3e4b98070900
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:07:02 +0000

    disappointed shallow thought prove middle

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
