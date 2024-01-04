50fed84c0dc0d6b025a4f3f072873b086a8f870b
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:24:12 +0000

    takeaway inquire consist comment radio

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
