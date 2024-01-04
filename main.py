53ef0f6dcd5703a5c171fb01e1c24bbf7bf5786f
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:42:42 +0000

    objection around left strong strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
