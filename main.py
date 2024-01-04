665a1a2ad5f9d0a765a1b39d66e1229995dba132
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:03:33 +0000

    evidence fairly nurse 570enjoyable declaration

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
