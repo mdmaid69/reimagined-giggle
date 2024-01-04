17e7bc2a6af04c9d2a1c5204aa901c67af6966b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:53:20 +0000

    wind face march minimum failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
