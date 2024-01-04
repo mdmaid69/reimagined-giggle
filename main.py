4655d138e7841b05cf3f357eda908d77b5584539
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:07:12 +0000

    mean broken admit approval hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
