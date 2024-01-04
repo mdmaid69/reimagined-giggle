edc98e3c48e57d67e48229b75e7ddee6c783b290
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:25:04 +0000

    violence excellent until smell hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
