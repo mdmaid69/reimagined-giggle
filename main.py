2722b89c88702a4ab52c4c16605a2229fd96e334
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:50:38 +0000

    wrap transparent nigga far visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
