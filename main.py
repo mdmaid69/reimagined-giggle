9c08ad139c878e9b7a069158bded3365a3882949
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:43:19 +0000

    there maximum reasonable air break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
