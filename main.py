940c64fc9b9bedd9f3fa3c0c6c5a7dcc28ca3f6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:51:29 +0000

    rare wave us conversation what

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
