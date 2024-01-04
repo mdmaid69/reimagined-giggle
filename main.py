d81d9b2f19ff48e86a80cb9594600dec6baf2495
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:54:29 +0000

    regional at frightened mark brown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
