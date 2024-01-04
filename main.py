cb5324485a85daeb4b83aec51729bb12f064b439
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:02:07 +0000

    climb advantage load before believe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
