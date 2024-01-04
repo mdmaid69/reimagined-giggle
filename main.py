0042e09538f92b6a0940527ebc53648a0e160ac5
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:17:30 +0000

    defend combination form coffee black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
