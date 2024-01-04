d9878e8fef895a6f9caca47142175da0ec260e2c
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:06:40 +0000

    mail look sort influence either

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
