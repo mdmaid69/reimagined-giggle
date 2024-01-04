dc37c26fc0c947f0f507696b18463ddb51805053
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:04:28 +0000

    trade chance them purpose behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
