ce041f6d879c83822d062e7934e29efee744e4e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:24:27 +0000

    geton kick public benefit street

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-from collections import Counter
-print(Counter("hello world"))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
