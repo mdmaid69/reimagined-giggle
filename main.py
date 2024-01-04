fb54de494f35e60550747a2e6148a5b39c88c400
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:03:23 +0000

    gas smoke circumstance constantly thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
