3b78e7ba191842b39678d752a54d21c904a11c5e
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:23:17 +0000

    task perform education uniform inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
