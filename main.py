cc765689d7258169a12b6bdac030ca553b1c3e06
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:18:50 +0000

    fire basic beautiful trouble solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
