5c7c1edecb8e7f2c746e03b39d4ac5dcf3461147
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:50:23 +0000

    office both debt agree apparently

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
