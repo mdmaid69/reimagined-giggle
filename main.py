269895236e204df417e00f175053482af28b5295
mdmaid69 <robot@alexsyw.me>
2024-01-04 17:34:59 +0000

    notice dress work variety we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(x, y):
+        return x + y
