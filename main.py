d0bc1c4c660cc7823eb7c97bdd66280f5d5f09e6
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:54:05 +0000

    gate wait 565end regional finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
