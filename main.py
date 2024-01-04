8f9fe69d0f6ce138506d2a801163501cec3f305c
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:03:05 +0000

    evidence smile fresh lunch you

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
