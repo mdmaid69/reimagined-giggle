3465c6c02337858057f98dbabaf923b23facd256
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:43:03 +0000

    promise down mark give proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
