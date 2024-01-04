5057949efb3d9c7c1b069713660a959a2cf0a716
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:46:08 +0000

    contain book priority try surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
