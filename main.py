014e6aceb38d0ad6942d5e13e408229d356410c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:02:58 +0000

    recent regular strike appeal proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
