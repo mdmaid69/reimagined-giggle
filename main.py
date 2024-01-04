1abd094277fadc19b35389cbf5de6bcbc9da8909
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:33:44 +0000

    train recommend maximum worst suckme

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
