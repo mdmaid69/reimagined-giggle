5a1327e126ddad8c130f4ca49e4f953d81ba2dbe
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:42:30 +0000

    which suggest swim instant narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
