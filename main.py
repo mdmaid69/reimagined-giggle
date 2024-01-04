9eee64a06ebbba554bf390984238206df0596ecd
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:07:26 +0000

    shout grade belong but convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
