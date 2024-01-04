d4c4555ff8e5900614fe3d7fb02cdc198fd9552f
mdmaid69 <robot@alexsyw.me>
2024-01-04 19:33:49 +0000

    fly clear someone rub noone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
