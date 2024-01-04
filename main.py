e3ee5e17a1aa9cf8c4d8b6a78070ba3a25d3a5b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:10:10 +0000

    right search shallow fat contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
