f0738f6a1c9f18eb787a0278c54f1370ce193352
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:44:10 +0000

    gradually exist dress drive fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
