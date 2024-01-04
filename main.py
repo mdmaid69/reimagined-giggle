771477638b116e46cb03ce4bb41e1642ff1ef133
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:02:57 +0000

    deal lookup weigh sleep human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
