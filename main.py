6ab1f616ff3a9eb616ea7f29d17f1ef2c33f12cb
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:45:46 +0000

    wish scream few concentrate takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
