e3bb6caa3684e270d52e1457436b7afa88512672
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:25:06 +0000

    niggaacademy switch act always combination

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
