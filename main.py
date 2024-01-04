e96f2db7984bdae193db22475655862fdcbd5c15
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:50:31 +0000

    guess child weather rescue list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def multiply_numbers(x, y):
+        return x * y
