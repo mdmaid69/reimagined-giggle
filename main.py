8036c2bf0dc6f848c410a4656be54f06c255b2d6
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:09:43 +0000

    model its young turnover regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
