8f7af54016da3ef406521a8f1081d16b95098bb2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:59:31 +0000

    hour introduction interrupt short positive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
