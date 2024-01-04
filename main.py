4ac1ab1c6bf2c049bd67866fb973f5efce2b1279
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:03:22 +0000

    wipe dream under future room

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
