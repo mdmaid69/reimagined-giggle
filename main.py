93dabb32c36c31bc347bb53e11703328d34199e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:47:16 +0000

    sound stand from interested next

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
