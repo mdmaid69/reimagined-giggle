efd7093a2aed789dfdb5c109e007dcc1ada6cfde
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:43:13 +0000

    offer sick process tight quickly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def cube_number(x):
-        return x**3
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
