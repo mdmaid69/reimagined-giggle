b29226b8c5560939fd0080fcd027f16b560d60e2
mdmaid69 <robot@alexsyw.me>
2024-01-04 12:41:11 +0000

    reduce recently stamp gradually fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
