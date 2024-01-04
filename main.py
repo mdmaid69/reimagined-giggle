8a3ae6c338209ec6f85c3826a130281516c53a45
mdmaid69 <robot@alexsyw.me>
2024-01-04 13:49:42 +0000

    whenever below sentence tooth talkback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
