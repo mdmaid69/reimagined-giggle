bf562f07bf0058fa7415e0a81a8b6f9cd81f3293
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:32:39 +0000

    aim recent ahead mouth with

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
