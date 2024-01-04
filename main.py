eeea2297bd6e03d3f6739c0dfd187d5cca6ee7b5
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:13:42 +0000

    picture pair free before range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def add_numbers(x, y):
+        return x + y
