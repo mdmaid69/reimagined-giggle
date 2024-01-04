4ab43572ec8dc5e73b538f4f1b4b4cbf7a2781c2
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:41:24 +0000

    advantage occasion float pot lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
