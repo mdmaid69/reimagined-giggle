9678a57ffe45bba3cc59a3720725bec1344f2b6e
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:10:22 +0000

    dig invitation smile throughout slice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
