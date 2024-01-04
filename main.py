845c0b0de7be28efde9d880618c1fa4e430408ca
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:15:49 +0000

    stamp contact start advantage progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  def is_odd(n):
-        return n % 2 != 0
+def find_max(numbers):
+        return max(numbers)
