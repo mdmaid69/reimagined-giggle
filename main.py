04db6ec68ad5ae23ea24577e13ac7eccbc82dd56
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:09:47 +0000

    yourself wash experiment callout 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
