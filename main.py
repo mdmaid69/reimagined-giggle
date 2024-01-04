1f2a10af7985fd6a3f45eb83619b285d14bb850f
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:57:12 +0000

    team cry art position run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
