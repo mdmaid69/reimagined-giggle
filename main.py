a7bed7d1879cbbb000b07676f25cbc957c2acbcd
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:06:20 +0000

    contain upwards pay patience feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def subtract_numbers(x, y):
-        return x - y
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
