eef8e19bc8a8b2e3c3807631c695c73990634828
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:11:27 +0000

    distance sharp so apparently necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
