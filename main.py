c441182e5b2617c6b3336c2c09c00019e30107e5
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:13:21 +0000

    autumn dream against no bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
