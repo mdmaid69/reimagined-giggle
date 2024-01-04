68875684f3a43d54b34bc0a3c16ea5a97bcb3f6d
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:46:10 +0000

    holdup demonstrate embarrassing consider ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
