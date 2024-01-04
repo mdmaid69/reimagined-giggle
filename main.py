6c9345f3ffad029869d83405291d190e00a9749e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:23:49 +0000

    blue her declare quarter takeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
