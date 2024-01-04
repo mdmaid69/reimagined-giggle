4dd56f0c09a42b35965cb73a2600a9f9a45a7d35
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:36:17 +0000

    finally consult deaf not yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import platform
+def get_python_version():
+        return platform.python_version()
