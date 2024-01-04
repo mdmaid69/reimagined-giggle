7c8c99e1b00ff6063f466739c75757249b0700eb
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:00:52 +0000

    wipe impressive branch comeround mine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
