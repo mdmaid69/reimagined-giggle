cf97c36db6b09d5c41771b1f4cffba8674dcb343
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:22 +0000

    because real left talkinto control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def convert_to_octal(n):
-        return oct(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_str(array):
+        return str(array)
