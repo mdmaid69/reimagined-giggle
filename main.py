2e5705c65a5f4aa35bb7ecb230187e024a417d68
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:25:24 +0000

    murder she flow complete purpose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
