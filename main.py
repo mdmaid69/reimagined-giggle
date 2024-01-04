fc2d457e17956b7c03405a2cbd3689aec0f83993
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:10:13 +0000

    receive left breakout practical practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
