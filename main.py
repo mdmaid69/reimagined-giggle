6d6c7def3c408b3242d78f120736ceffb0659968
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:49:41 +0000

    afford yet every problem window

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def multiply_numbers(x, y):
+        return x * y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
