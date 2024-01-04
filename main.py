6e049f68bbba13947c8984b2a1d775c1388a0e65
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:45:38 +0000

    open spring concentration disapprove hurry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
