f45b4656482ba9912a7641e5d4c6ee1a636e89ac
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:48 +0000

    mainly hers beginning care myself

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import tensorflow as tf
+print(tf.__version__)
+def reverse_list(lst):
+        return lst[::-1]
