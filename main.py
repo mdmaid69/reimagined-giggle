abdbd619dc3099b671d6bcb91559b60e8b87b34a
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:35:10 +0000

    empty crowd ago Pp but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
