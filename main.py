2dad3fc0af3f42fdcf1b67429a8602d44a14aff6
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:16:26 +0000

    matter frightened mail hard relative

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
