73b25b57819d796e51b130f37646d98e21c3d894
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:40:07 +0000

    depend mix seem spin emergency

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_pressure(force, area):
+        return force / area
