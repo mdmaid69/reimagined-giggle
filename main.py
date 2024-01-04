7f1b3ba70fd8f9f55d4b58c2ee57601ea235172e
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:23:00 +0000

    distant Ii expensive runout dark

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
