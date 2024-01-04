30a26787504d6229f11eb579b429451c011c63d2
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:24:22 +0000

    position generous false space rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import sys
+def exit_program():
+        sys.exit()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
