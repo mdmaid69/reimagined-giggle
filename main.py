71d5b14ec44c46a4768193863494564cfa1ae904
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:31:56 +0000

    feel brief lead front since

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
