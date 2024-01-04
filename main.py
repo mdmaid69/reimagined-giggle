5500134f0d4bac77dd13e6748f1ff19641caf3dc
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:35:48 +0000

    activity basic straight design join

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import time
-def get_current_time():
-        return time.ctime()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_max(lst):
+        return max(lst)
