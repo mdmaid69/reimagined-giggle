c1b4083345e0cd0dbd83bb9f2b4fab66d9436ed3
mdmaid69 <robot@alexsyw.me>
2024-01-04 09:57:59 +0000

    succeed song culture main workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
