b2c3005560194a47a998a0155d797a42988df14e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:30:33 +0000

    abroad open fasten luck behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-print(os.getcwd())
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
