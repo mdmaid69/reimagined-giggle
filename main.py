724fea1ec8ad5258ff1aed6f26eed33a50a2ae08
mdmaid69 <robot@alexsyw.me>
2024-01-04 23:10:55 +0000

    ill depend load wave change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
