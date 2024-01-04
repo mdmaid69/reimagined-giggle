43e6d0da82eced766f1a476af94209b31d069803
mdmaid69 <robot@alexsyw.me>
2024-01-04 22:13:50 +0000

    total weigh break recovery raw

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
