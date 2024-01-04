5041f126e20b6cbc1384104b711ca60a28c85376
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:15:06 +0000

    undo guess visit anybody rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_float(array):
-        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
