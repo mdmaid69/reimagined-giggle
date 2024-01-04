f6fe7e471d8fad796d01fb9788b6e09901691e3f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:30:56 +0000

    both wish load sit matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
