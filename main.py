d2efed39d87ae495370b58b9716a334e48550a02
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:23:03 +0000

    Ss control instead busy together

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
