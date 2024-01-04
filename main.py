8c9ea754faa42e6f9f19181732d0a6275db6b86f
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:27:30 +0000

    Ii tradition rarely train rate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
