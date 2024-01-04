ab24f49b52371ef2d5fd242e23662be3e7349057
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:11:24 +0000

    slide worth illness idea wipe

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-for i in range(10): print(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
