240e2ce8c3e4503ed8f2903d4d9c5d12e40c0669
mdmaid69 <robot@alexsyw.me>
2024-01-04 05:52:19 +0000

    risk word decide win what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
