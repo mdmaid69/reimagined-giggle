e50f74a68cdbff4f76a6624e7fc122f4a7e946fd
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:26:44 +0000

    equipment home hit public urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
