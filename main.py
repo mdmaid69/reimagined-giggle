a85f74aaf57ecb37dd4d75db8e6a89c8ba99890c
mdmaid69 <robot@alexsyw.me>
2024-01-04 08:58:46 +0000

    sort minimum responsible settle slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
