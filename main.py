43c0e1fdc8c8db98db397923cd61d7c8a0584098
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:33:05 +0000

    most lookafter generally hang Ff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
