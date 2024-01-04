457f2e33793f9883e4165a22696262a39ca2e48d
mdmaid69 <robot@alexsyw.me>
2024-01-04 04:15:05 +0000

    giveup beneath dark Kk plenty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_density(mass, volume):
+        return mass / volume
