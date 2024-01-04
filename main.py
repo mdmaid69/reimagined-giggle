8d28ebb0d767016d95c029ef0b1fb61ab3df9e73
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:12:07 +0000

    wet Ww test write aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import time
-print(time.time())
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
