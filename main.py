6c51332267eedf487cf47b9d7bad4259a41736a9
mdmaid69 <robot@alexsyw.me>
2024-01-04 01:20:39 +0000

    buy talkoutof hold amount along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
