edbc2e6bd763ee41b60d4d6c5fe73051acfff317
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:05:39 +0000

    still talk natural direct group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
