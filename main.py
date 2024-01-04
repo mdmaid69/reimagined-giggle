558ce7f2bfa3465cb041084fc205ad074c48a98a
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:58:30 +0000

    rich clearly absolutely breakinto shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
