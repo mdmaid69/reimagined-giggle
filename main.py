3b5de9854f68092bbc83e18f9921eb5a6eec1a58
mdmaid69 <robot@alexsyw.me>
2024-01-04 14:53:36 +0000

    claim use and home skill

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import sys
+def print_python_version():
+        print(sys.version)
