b6711fb76505b4c12af13b88dad5991acefe5507
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:08:23 +0000

    through recognize object scale protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
