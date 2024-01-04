6267c4dd14988d4a9a8f73aa6101036f27361a83
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:49:58 +0000

    declare complicated peaceful complicated grateful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
