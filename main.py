dedd2325c7a89bb51bb0b066a538557e8cdf466e
mdmaid69 <robot@alexsyw.me>
2024-01-04 03:43:50 +0000

    front seem such now kill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
