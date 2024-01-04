f1f43596dbfde3443d5ee9170fe667a5cd8b83b3
mdmaid69 <robot@alexsyw.me>
2024-01-04 15:01:46 +0000

    somebody new excuse regional in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from collections import Counter
-print(Counter("hello world"))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
