64c52d27bdc48d1a88616685ade3e2688d624861
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:14:57 +0000

    broken original must purpose trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_item(array, i):
-        return array[i]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
