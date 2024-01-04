5d413db5d0ca98d31f4499cbe004e8fc07fd42f9
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:51:31 +0000

    back whom smile female beside

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
