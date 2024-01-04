f9c57313d7b8630b25adca9243ea080d55050e09
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:05:10 +0000

    style intend dream firm nigga

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
