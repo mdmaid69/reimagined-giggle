d5ea837387d17dc780bcb9218bf71baf48e196a8
mdmaid69 <robot@alexsyw.me>
2024-01-04 10:28:46 +0000

    along improve prevent object mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
