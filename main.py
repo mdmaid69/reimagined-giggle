6866effc7fdb9c19409084e221b06747417c0e97
mdmaid69 <robot@alexsyw.me>
2024-01-04 11:18:38 +0000

    certain belong this guess escape

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
