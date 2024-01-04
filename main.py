feb71dcb3282181dd16a1267cb2800e7030cc432
mdmaid69 <robot@alexsyw.me>
2024-01-04 02:21:09 +0000

    reserve set insist operation wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
