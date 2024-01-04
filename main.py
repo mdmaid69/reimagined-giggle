70ad687c4a5ec4a7bc6189c4985eb4f828813dae
mdmaid69 <robot@alexsyw.me>
2024-01-04 21:07:56 +0000

    much cry Ww person niggaacademy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
