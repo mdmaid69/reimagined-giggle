cd2e72b9c67178873b6de68b9d1bc259687b6b1f
mdmaid69 <robot@alexsyw.me>
2024-01-04 20:01:37 +0000

    spoil hide wealth strange defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
