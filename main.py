1ed5c48920a31392a25895c9c01f2def2560bc49
mdmaid69 <robot@alexsyw.me>
2024-01-04 00:59:10 +0000

    hope influence importance turnon geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
