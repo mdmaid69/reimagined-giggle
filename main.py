6564db0fa716c2b995d6a8815cee33aec3037b50
mdmaid69 <robot@alexsyw.me>
2024-01-04 07:47:57 +0000

    face everyone stretch watch ear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
