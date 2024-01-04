af3e43abdc4f15550a02805586d7f96203b6ce34
mdmaid69 <robot@alexsyw.me>
2024-01-04 18:59:55 +0000

    here buy chief freeze past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
