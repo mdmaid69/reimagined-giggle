e87614ef78f284063bdb03635e4bd2946d2382f3
mdmaid69 <robot@alexsyw.me>
2024-01-04 16:26:32 +0000

    body shout tip traffic cruel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def multiply_numbers(x, y):
-        return x * y
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
