1d507fad22a3b4e892a268e1103ecce3644d5e81
mdmaid69 <robot@alexsyw.me>
2024-01-04 06:31:30 +0000

    fill expensive gift inquire emotion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def convert_to_octal(n):
+        return oct(n)
