653d20c0754b24b626f4121962d38d953bf130dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:30:54 +0000

    activity powerful thin test live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import sys
-def exit_program():
-        sys.exit()
+import time
+print(time.time())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
