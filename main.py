fb5322c8f74b92b5d50d98cfb81f143181bbeaf1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:47:03 +0000

    part treat 567enemy complain exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
