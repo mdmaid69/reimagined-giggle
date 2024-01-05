2b5d3c86bb112fb23d2b303735aca39e666405b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:17:40 +0000

    record frightened push authority importance

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
