ae9a86ca9a02ebdfc0099e6251ef05ee889bb8c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:11:53 +0000

    tonight determined impressive string amount

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+print(math.pi)
