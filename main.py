32b7969823cbbe298403bcc4792e2b19ce930790
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:31:37 +0000

    repair ahead excited control people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def convert_to_binary(n):
+        return bin(n)
