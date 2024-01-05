add7512e82eeeb79a3680ff850f5f3c6826bd43d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:00:28 +0000

    modern schedule 571enough Rr card

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
