5daf607751644adebce8f9d5c3e62f945999614c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:35:52 +0000

    hello name acceptable lookaround amount

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_floor(x):
+        return math.floor(x)
