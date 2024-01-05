10ad36b7512dd2f300d6875462595a2bc306f8d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:58:44 +0000

    relax street about arrangement keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
