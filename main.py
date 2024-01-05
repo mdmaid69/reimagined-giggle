fb0cd1dc4034978d569eb6a9695e259ee72a942f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:22:40 +0000

    grand interfere if repeat goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
