03dfc3fced00d0cd3bd23d0f1cb1f8583a1db690
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:45:52 +0000

    idea acceptable belong its keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
