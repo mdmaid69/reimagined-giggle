a7754fe5609c5e97451e3eac9a23f884ecec456f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:08 +0000

    tired thanks cross complain phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
