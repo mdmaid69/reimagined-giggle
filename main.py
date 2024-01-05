c4f4c9714e3c53fa2eeb6e234201de2f1fa30552
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:45:17 +0000

    supply within unpleasant far tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
