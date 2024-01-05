a8a5585a1e55782522bc2e6f96d09af28e0d19f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:46:42 +0000

    minimum minor reply statement breakup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
+def find_max(numbers):
+        return max(numbers)
