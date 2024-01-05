455eee71b82b38b651acd87ed68432aa4aab7184
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:57:42 +0000

    war temperature borrow unusual operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+print("Hello, world!")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
