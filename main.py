abc8574ec944e2f9019601c90768dd0fe53116ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:51:44 +0000

    bit slow treat introduction account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def sort_numbers(numbers):
-        return sorted(numbers)
+print("Common elements:", set(list1) & set(list2))
