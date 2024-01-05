e0cff61220c70641b16d94c4f31214fe378309b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:59:47 +0000

    blind home mix satisfactory appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
+  import re
+  def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
