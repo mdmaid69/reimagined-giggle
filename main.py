d75816a9cf05f95995ecf72019971b46bad17337
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:44:05 +0000

    free within set sign with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
