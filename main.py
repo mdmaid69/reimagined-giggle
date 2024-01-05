1ff1cfec51ac6f39fa449a14b375d9308236a168
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:50:41 +0000

    accidental who row disappear confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
