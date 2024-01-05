5632aaeeecb882c07262c26c99e9a96227108e36
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:37:14 +0000

    sort pretty whistle occasion glass

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def reverse_array(array):
-        array.reverse()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
