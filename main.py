eb5f638d91ed8f152bf22ce4df0c7aad7f4fc221
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:45 +0000

    half phone flat pronunciation part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
