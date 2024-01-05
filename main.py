46c128568695e86498573be70064a28e416fe750
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:19:46 +0000

    anybody breath ought issue thought

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
