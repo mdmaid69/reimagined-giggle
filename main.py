4a62e342b97f5032f660d203e9e754ece14078fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:07:00 +0000

    development necessary cry goon pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
