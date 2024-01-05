7cf22664c02683b4796ea33a35981d33cf269277
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:25:08 +0000

    balance building myself crash measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
