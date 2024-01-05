f130eadc381306af21c0789eaadda1104bb2ca05
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:06:25 +0000

    live work weather desperate noise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
