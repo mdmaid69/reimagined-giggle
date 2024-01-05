33756a37d8bff26fd3f0154fe6c42a8a88ec20b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:48:13 +0000

    goodbye mess door lock class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
