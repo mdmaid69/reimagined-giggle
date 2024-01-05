1ef32b96012ef6f80662027b756328861c555490
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:00:35 +0000

    offer love thank scared introduce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
