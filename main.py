7762dd4a7ab312991aae58c59a191f0060b4fa66
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:15:20 +0000

    old tone guard transport problem

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def square_number(x):
+        return x**2
