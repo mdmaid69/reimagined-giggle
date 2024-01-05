337cbcfc3af6e714c0ad062df586a375fa0435e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:42:51 +0000

    talk sale error battery ours

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
