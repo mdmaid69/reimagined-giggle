411bab2c645c2d82796ce0f98a3be11a0f0ddd25
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:16:36 +0000

    pity see talk question where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
