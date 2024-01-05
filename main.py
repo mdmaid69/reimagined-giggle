7ea103c5be6a2d40d92d094f8a8800a092dbe7ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:44:43 +0000

    lift dear really target close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
