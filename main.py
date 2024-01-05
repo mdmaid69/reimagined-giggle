c7abdf13cfe38924fc1cd9daa8f427b2e752cc5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:55 +0000

    distant separate send impressed book

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
