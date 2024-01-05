0dd778f37ac00b033cfc441b224d7f506414979f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:56:32 +0000

    once home suit film generous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
