efb964c9994b180c7870ab353d39bb79b856a779
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:11:15 +0000

    claim awake opportunity rate sort

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_min(lst):
-        return min(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
