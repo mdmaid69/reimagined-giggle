13f1ae1a5c4b0613664b984e2ceb9d9c7f8627b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:50 +0000

    refuse connect scrape getout patience

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
