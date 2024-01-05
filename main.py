d1cedb49a2c9a171fcddf38f3d742a6fd05251ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:37:59 +0000

    birth list Ll command thin

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
