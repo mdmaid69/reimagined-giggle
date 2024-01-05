74637ce1d7c7126e15add60ce47ae40cafda01cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:18:32 +0000

    harm any turnon discover curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
