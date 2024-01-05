8630faa03a9f662127fd01e8cf4c76e888e497f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:17:39 +0000

    colour whisper along tour able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def is_odd(n):
+        return n % 2 != 0
