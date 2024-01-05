41e0fb10dc01f9482ce21dd83e210e064ec0b109
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:58:04 +0000

    only valuable second trip risk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
