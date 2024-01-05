76eb8a0d65e911041190b2a2ed7bd457edb21b5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:51:50 +0000

    have container rubbish minimum clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
