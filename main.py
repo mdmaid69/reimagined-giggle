2d1c6bc7a42204bc5e1d6f76f788784f0b6c3d79
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:42:56 +0000

    exchange long sky light uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
