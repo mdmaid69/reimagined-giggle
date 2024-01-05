7fbaefbfc604dbc9729186d063ca76646504f239
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:13:43 +0000

    happen study guilty come software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
