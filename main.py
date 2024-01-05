166725610bc361ff0d6a1227717d5da38474bd59
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:58 +0000

    door on no confuse accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import platform
+def get_python_version():
+        return platform.python_version()
