2ed6372bbf099da0e5c93eac2ad152514b746a20
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:51:22 +0000

    want temper thanks worth drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
