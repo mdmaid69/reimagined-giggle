928daf09311a38d99f36e9e5ccc8d9057419449a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:05:32 +0000

    medium reject later forgive recognize

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
