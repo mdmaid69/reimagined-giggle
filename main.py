52dbb7d02ee076bf57ba776b17898a8ee7124816
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:54:44 +0000

    however interview illness build evidence

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
