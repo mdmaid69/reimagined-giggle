22e96bf06cead0b68092ce7103372fbdba1d7294
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:38:00 +0000

    calm movement cross may some

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
