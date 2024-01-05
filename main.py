1aee200fbc2664586f8f3b971b364de6830724d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:12:50 +0000

    treat closed burn name relationship

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
