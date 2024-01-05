e9832a8ffbccb8e72fb0faecd3cf1d65faa84590
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:48:48 +0000

    since sound appearance towards deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
