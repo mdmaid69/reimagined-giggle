5210607dd2046ee51f69358be738ac5f5ae6f9a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:54:33 +0000

    service job bush proper cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def sort_list(lst):
-        return sorted(lst)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
