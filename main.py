9129d1e7a87586ee3e4bfb20fc3f70a71528bd6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:38:40 +0000

    bottom properly scrap control suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
