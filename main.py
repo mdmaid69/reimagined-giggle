b238316cd788ed2d400f096377883384fc4e19f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:44:41 +0000

    hardly few minority tongue near

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
