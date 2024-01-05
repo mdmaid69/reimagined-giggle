223d2b9335c829200391aad7a231fbf9c9dd56bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:14:47 +0000

    upset arrange home apply hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+import sys
+def print_python_version():
+        return sys.version
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
