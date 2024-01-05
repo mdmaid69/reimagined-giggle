a4bb4ff65c161d0ea1c9ecdee225576d7ea68363
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:58:42 +0000

    polite board then afraid run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
