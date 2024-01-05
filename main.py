a5c2cc781e55d627b5fe4b139bb0a74a9dbf4919
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:44:37 +0000

    point suck crash goon split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
