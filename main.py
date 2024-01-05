e4683013b8ad93b29f7733ac86fd2a75fe328ba9
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:51:45 +0000

    past trick fence keen closely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
