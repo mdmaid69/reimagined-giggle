9dbfa3dc438bba6d03f3b0a595a8422176f9a9ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:13:49 +0000

    degree flavour all cough add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def reverse_array(array):
+        array.reverse()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
