68dfc71c5750d60186c33b10475d2cab036b9988
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:14 +0000

    borrow time disappear cheat impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+print(sum(range(10)))
