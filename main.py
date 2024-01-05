4e87bc773fc3b0587e9b4585956cf14f1d4f9b8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:45:53 +0000

    fun doubt tell ill pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
