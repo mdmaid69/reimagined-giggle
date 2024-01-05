2ae75f6f4bb5fae7aba98078cec95bbddbab7aed
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:00 +0000

    grow makeup force food foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
