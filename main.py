3bce495c67cdafe2a69dadc2c63a6486683ab4f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:38:30 +0000

    feature cover until sleep rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
