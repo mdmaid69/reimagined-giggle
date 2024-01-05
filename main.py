d97809fbf8908b90fb058a2fd77923121dbd41a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:38:49 +0000

    sleep bang backwards bad wet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import tensorflow as tf
-print(tf.__version__)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+print(os.getcwd())
