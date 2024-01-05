827b93223ce6b8d6f8087ae04586e62ddc82a3ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:41:04 +0000

    first arrangement father shape obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
