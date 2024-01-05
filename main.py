3ed86efc3de1228cacf9a8627bdc3c8cebf8acf3
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:54:40 +0000

    excuse occasion equal report light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
