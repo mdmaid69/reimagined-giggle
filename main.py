782948018f29706d6e0896ff8c36d9d1fdc4ff22
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:12:26 +0000

    car use skill more achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
