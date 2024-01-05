73df513e797825b7159fb5f7e6eb392ebf07151b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:13:56 +0000

    nose honest foreign protect underneath

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
