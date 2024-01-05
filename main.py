5fa629cc7cc8a8084918caa3955939a42b86d01a
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:24:42 +0000

    drive during file basic dance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
