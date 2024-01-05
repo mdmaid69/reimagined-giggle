bb0034c3d3d93358b194ace6e39d8c84ba8d6304
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:46:51 +0000

    mean trousers programme stone somebody

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def convert_to_hex(n):
+        return hex(n)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_array_to_list(array):
+        return array.tolist()
