4cf1c93575df3c39497046d0b70c22b7bcfe0f48
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:38:45 +0000

    through season deep exactly serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
