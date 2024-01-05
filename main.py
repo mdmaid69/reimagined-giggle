f5fbdca2597f2bae51bf3a4f132623f61ab036f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:21:11 +0000

    when important birth head straight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def greet(name):
-        print(f"Hello, {name}!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
