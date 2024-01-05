ad5d03034d1d686d8c56da23c39f2ceb056776c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:28:16 +0000

    smart takeoff press clearly fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
