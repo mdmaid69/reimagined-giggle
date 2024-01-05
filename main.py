8bfc66017438806e154339b7e5223fe876be170d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:19 +0000

    when before admire seem free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
