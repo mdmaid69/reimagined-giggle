608a6179723f90ca0eb0465255e52762534a6c74
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:03:20 +0000

    effort accept by height mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
