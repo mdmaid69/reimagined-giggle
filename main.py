9bf26d186fb2541d909496a04f1fd4ff1d59051a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:22 +0000

    present flat giveback bringdown in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_elements(lst):
-        return len(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
