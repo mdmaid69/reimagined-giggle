f17b4faabeea28d96140ad132381d10bd5b48d0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:05 +0000

    inside remember either preparation lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def divide_numbers(x, y):
-        return x / y
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
