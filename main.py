4c4bebc6c9813bc3c778820db618c602022aef53
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:40:14 +0000

    doubt lookup mark love suck

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
