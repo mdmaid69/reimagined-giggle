3746dc57cd8cda8845ef308c4087e3e364160b44
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:00:47 +0000

    leg knowledge bite push this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
