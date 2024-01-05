487c5799a7fd578d1c8292317224270ff0c24bfa
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:24:21 +0000

    emergency base thirsty them dress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
