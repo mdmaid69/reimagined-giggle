b87b67dd9e38ebe7a1b75c3e6b71e62b678834d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:21:09 +0000

    unfortunate into field debt plan

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_odd(n):
+        return n % 2 != 0
