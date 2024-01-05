9c9d5d978827b0d951ebd5921b14f11bb98b3c80
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:06 +0000

    power disapprove sweep break interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_floor(x):
+        return math.floor(x)
