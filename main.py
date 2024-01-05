a7ed769ebd633f5b7070f72b411b57686cd53ee0
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:12:23 +0000

    behave squeeze please lonely doctor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
