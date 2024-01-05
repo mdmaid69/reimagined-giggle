8d3dfa8359989cf53ce7ccca3110a61574bb37c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:52:01 +0000

    back occasional plenty say unlikely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def count_words(sentence):
+        return len(sentence.split())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
