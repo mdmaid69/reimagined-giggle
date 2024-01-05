f7015006c19d6980384dc58941b05b1090c3c596
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:47:02 +0000

    sign inside necessary division suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
