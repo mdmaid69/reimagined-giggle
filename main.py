c3553853f5f095993e4af04c520ca907cf99bfaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:38:58 +0000

    date old field organize pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
