fa62e3fe5e8fcc5c90c39ded68e2fc670fb9760e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:27:58 +0000

    mind mean blame ring busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
