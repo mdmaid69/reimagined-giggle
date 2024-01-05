7852425afbbad4cfc5e34ed78b361e172d66c96e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:39:29 +0000

    back beginning urgent cut hers

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
