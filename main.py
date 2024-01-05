e537e3f641ff26c150599690f513693c6dc89627
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:37:30 +0000

    need wash hurry schedule challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
