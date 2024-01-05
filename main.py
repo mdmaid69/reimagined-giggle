a602c168b14c9bdfc3b035e1bd5c3bc4b4c08758
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:56:29 +0000

    speed hole Pp all passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
