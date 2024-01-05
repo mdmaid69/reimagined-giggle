439b96d101711bfe144fa5434b4f92d15c10c651
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:51 +0000

    573entertainment hope popular giveback approve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def count_words(sentence):
-        return len(sentence.split())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
