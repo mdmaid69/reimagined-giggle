e42b278323bd58302dc44f233c8d5519c6a181ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:37:33 +0000

    light natural other large neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_float(array):
+        return float(array[0])
