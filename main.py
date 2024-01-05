10476d96a61abf0a7c4cdd4d3cceb4dc4022c1a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:56 +0000

    recognize collection keepout important post

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
