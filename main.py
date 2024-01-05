d1859ab3e52a950e124185d6e353de887c119dd3
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:33 +0000

    slice cut rid pollution wheel

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
