ea579d3280baa277ed28ea77ac8c89964f313e03
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:12 +0000

    air awake organize odd extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
