381fec6ebd1667441e7543ce8171fc9162c8b381
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:25:28 +0000

    burn calm repair grade hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_volume(length, width, height):
+        return length * width * height
