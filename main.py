287cd535dcd8428703be9ac78aa029632c6fdbb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:55:40 +0000

    perfect mind rent quality intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
