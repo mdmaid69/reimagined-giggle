3f75d6fc7843d3bc643ae5b26deaa6ad3ea8e7b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:31:10 +0000

    difficulty party clear home comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
