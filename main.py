a540a64456f8d6cfb89dc842894944eb4f102622
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:29:18 +0000

    sort wind moral satisfactory under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
