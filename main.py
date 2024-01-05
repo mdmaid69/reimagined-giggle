f7f910fee86d5e5f039887d1cff869393b4e3293
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:06:47 +0000

    climb grade speak class until

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def remove_duplicates(lst):
+        return list(set(lst))
