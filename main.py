a651e3697ffb644771095a1acbc768e9545f3ff8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:04:36 +0000

    aware pin neck pot money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
