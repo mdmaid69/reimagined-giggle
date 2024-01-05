2c4f7ede9b4542e0c343b75a709ab46bd0c96c2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:00:04 +0000

    leave draw base play silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
