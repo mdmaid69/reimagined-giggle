45e539a4c5845cac1f750ad1420142860cd5cfaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:06:22 +0000

    closely discovery bush pride still

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
