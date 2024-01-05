20b50583bbf29f2eb73384f66de4aa42d0eaacb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:56:59 +0000

    else limit experiment search strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def calculate_area_rectangle(l, w):
+        return l * w
