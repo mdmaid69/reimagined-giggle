7f9dedaec40333d5126cc5e19c9148262bc369a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:51:47 +0000

    clear last usual delicate disagree

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_stack():
-        return collections.deque()
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
