d0ca03734d7de7b84fad4fd257046f62f4f90a25
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:45:59 +0000

    know conversation mend boring heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
