3995b34cf9f1cf5d89f3c63d4df2aa3c71b69835
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:41:01 +0000

    into depend only talkoutof direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
