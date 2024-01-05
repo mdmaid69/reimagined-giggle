94163e84812631b7c465be1227b5c45508417947
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:29:36 +0000

    listenfor unless hand loss arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
