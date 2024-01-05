75bba32ce006451b2be1c8282bd2d8c4786a2270
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:42:00 +0000

    score owe health flow steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def move_file(src, dst):
+        shutil.move(src, dst)
