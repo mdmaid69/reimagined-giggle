cf199825a8dbf84ff8dc59809476f7f1e91c71eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:46:56 +0000

    team at getback walk temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
