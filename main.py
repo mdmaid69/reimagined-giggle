f5fb3966bebc61fe6ad84a365cebbc88ab40f9e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:49 +0000

    soil tooth arrive worst deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
