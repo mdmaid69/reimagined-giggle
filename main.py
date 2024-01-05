a927249141bd260c046b3f2ae80d8f34ae1fcc06
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:55:32 +0000

    declare proof obtain swallow man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
