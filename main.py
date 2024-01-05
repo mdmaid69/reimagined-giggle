826986aff0d44c67145b328cbb6e9a5a713d8798
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:00:25 +0000

    defeat hold mark during group

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
