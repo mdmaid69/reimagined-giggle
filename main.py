2fa4ac0f651752b5376d99b391cfdada52af30b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:12:02 +0000

    bound nor fat concerning most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
