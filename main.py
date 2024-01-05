3055978504e66fa2b4ad0a9fb43d868301d1dbe4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:52 +0000

    test alone check people friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
