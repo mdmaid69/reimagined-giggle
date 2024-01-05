b328272afcc234a292150429b3bf866a4f672c14
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:18:45 +0000

    back disturb want moon announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
