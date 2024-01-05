7775b235a2ba580974dfee6c7a106a63e1940600
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:08 +0000

    pronounce choose desperate preparation wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
