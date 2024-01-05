4dbe4d7e6e02f6d6102cea98c0f5c8a90df49d3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:52:15 +0000

    bar empty calmly kill bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
