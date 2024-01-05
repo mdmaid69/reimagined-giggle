554e89e4dc22d732ab19127a5f2fbe9c60ce9b31
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:48:46 +0000

    yesterday protest uponon this lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
