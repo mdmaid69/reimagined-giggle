1b8ad3052087cf7efbac9b81d62dbd3bc83271c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:09:09 +0000

    complain spray spring breakinto with

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-print(sum(range(10)))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
