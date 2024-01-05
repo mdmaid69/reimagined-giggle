770c75668e1e8e3729983f86ba701c2c52102b4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:25 +0000

    better whether praise press pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
