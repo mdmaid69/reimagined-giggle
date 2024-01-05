23ff8cb98574428a8869c45e6de99d7366a93cb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:50:35 +0000

    spray bang may thirsty replace

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import collections
+def create_counter():
+        return collections.Counter()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
