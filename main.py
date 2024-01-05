f9d66d71f09c2c2e5f5e163a66e63247504fef32
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:12:30 +0000

    poison benefit achievement male surface

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
