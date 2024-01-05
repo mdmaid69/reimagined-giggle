6558b8b3accc82889139c8438db00669611e43aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:42:15 +0000

    think work check interruption reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
