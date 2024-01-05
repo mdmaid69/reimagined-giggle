129b276508af99772a62023518b29ca54a05ee35
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:15:07 +0000

    never Oo bend easy letter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_min(numbers):
+        return min(numbers)
