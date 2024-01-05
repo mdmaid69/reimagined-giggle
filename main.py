fc6e34236f33faf21cd094cfef8e280b422e6c6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:02:18 +0000

    steady nervous unite more into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
