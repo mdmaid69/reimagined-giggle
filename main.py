5e8726fdd55ef2a1225f284f5900445410438fcd
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:11 +0000

    sound Vv both draw distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
