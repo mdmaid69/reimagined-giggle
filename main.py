28bd76061b1719e78a2098e1edf811d41da2824a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:32 +0000

    564encourage else display odd centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def multiply_numbers(x, y):
+        return x * y
