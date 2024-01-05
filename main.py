883b77ccfb945f468d06f4c20c05014e3448b7f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:21:57 +0000

    start present example right hurry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
