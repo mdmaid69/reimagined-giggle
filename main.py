84ac635af6f61e1fb03c0bb67c0ac671a735b21e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:31:37 +0000

    cheek copy neither offer able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
