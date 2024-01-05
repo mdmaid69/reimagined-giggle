3a972c32a63987e07830b9eeb3a2dbdf169e1342
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:30 +0000

    rank press time yet pull

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_density(mass, volume):
+        return mass / volume
