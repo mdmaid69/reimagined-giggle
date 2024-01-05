9dd358d74397578c7546e66fa9d6bdbab1b8625e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:37:40 +0000

    allow that offer grade platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
