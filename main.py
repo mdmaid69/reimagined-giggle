db436b9417f13557402cc9a4d047a7f7f50e3ce0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:40:40 +0000

    invitation afford blame introduction breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
