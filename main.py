7f92e84e8bd8835b33d0093ff0de53bfac23b3d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:39:57 +0000

    equipment approve back entry respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
