1c09d9c82054534c6fd866be07f264e0ea68f112
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:45:57 +0000

    hurry high squeeze possession dream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
