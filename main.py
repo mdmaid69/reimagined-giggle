4b4107e4ff3d32a66d546e1ef363ac710c667d51
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:26 +0000

    praise square neither another mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
