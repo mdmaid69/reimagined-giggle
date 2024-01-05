618bd75db162a8ec88868b8140a2c5f9a3500c23
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:42 +0000

    taste odd shopping fail attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
