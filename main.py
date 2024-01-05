607dd25c23418080bc315b79d23adc925ecb832c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:31:25 +0000

    hang favourite contrast turn ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import platform
+def get_os_info():
+        return platform.uname()
