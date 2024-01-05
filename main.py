0f0d2dc2a069e8abea13a1887680a07a54e7d637
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:51:24 +0000

    hope cool basis convenient mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
