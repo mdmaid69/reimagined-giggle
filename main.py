06fa6a343d65a7583cc470d22057d9dc321897bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:02 +0000

    scream lonely off open goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
