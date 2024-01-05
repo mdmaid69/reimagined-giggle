6f991aa517345f9c95e379b1a32748d69b961aca
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:23:36 +0000

    climb accidentally empty certainly pool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
