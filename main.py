7fa17d7814d202e055022d33bf8088d65297734f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:23 +0000

    inside cause discovery sense freeze

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
-def get_today_date():
+def get_current_date():
         return datetime.date.today()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
