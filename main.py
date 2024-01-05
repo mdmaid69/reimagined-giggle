0671ba56ea8f0bfa3dbbd24bbb73d54754ef6cd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:10:49 +0000

    total nothing clearly crime replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
