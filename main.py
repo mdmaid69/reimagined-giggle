0c45e6d1676f2427ca79d4765c1968ba58df97b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:17:51 +0000

    interrupt experience better escape charge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
