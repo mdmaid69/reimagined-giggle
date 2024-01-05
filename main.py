31523ec91b953063eaa7406884ac44ecb7638a30
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:01:21 +0000

    basis stop art smoke nice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
