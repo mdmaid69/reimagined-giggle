586ee06288c3d24d112a25be2161fa0e88979dd0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:05:58 +0000

    conduct crime possibility break delicate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-  import sys
-  def get_python_version():
-        return sys.version
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
