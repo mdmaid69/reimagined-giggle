8f66a1e0a3d4c6d9d65991356a1d9dac51448323
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:24:10 +0000

    comefrom joke persuade post peaceful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def convert_to_octal(n):
-        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
