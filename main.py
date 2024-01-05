1bc55908cef2b4c4564c59d4131e05d92dd755c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:22 +0000

    medium deep improvement fear last

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
