cfc8de45069000de9e5036247f80b2ec8cb4dcc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:20:17 +0000

    purpose old ill key pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def square_number(x):
+        return x**2
