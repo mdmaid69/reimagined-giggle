7deed670e0be02e84ecfd4ee31176be21eabed06
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:54:50 +0000

    will its nothing tongue people

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def reverse_list(lst):
+        return lst[::-1]
