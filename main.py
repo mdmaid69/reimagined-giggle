7bf2b43ed0e29dc53ea4563ffeafdbbf3229f4b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:25:03 +0000

    note fairly bet odd follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_list_from_array(array):
+        return array.tolist()
