affbce448a8e611ff70626adeaff694c5c2300ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:37 +0000

    cover feel conversation may around

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
