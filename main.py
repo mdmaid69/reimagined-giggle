7cda0a5f0834cff01b4807c4dd14bb51bf55fe43
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:47:32 +0000

    wrap offend difficult blame various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
