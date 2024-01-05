5af3303a92de17e268ad7709a8cdb85374c4c5ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:12:43 +0000

    university thing achievement Ss intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+print(math.pi)
