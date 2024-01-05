ac3cfeb42020b3b22290a8cfb75ec5181eed87c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:10:27 +0000

    brain lookaround listen telephone accept

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
