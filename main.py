f700318e7b5d429059b7f63a76599423afed6a09
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:17:33 +0000

    total nearly properly rise though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
