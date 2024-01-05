98e7c088d19a5edbdc2c77d64a45f7f3988ebdf6
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:52 +0000

    anxious punish form reduction regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
