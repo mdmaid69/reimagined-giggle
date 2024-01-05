5d6a912485650aa45b5a08e56e042df2bd6aa0f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:22 +0000

    later that chance love grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import json
+print(json.dumps({"name": "John", "age": 30}))
