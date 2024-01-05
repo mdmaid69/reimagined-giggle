c34fb0c24ce1cf5d543fa3d98ae9282916236a2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:29:44 +0000

    beyond fall attractive anybody anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
