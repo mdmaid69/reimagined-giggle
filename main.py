8246a7bb0b8ac281a9ae62800e5a2a529ea38f41
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:01:23 +0000

    normal increase bear fear stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
