e95801858051d41aaf44d50088d9467af5461c64
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:38:07 +0000

    appeal direction far usual thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def multiply_numbers(x, y):
+        return x * y
