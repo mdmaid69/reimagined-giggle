1d30abd300191f71a3985b78051736817e8e2221
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:35:34 +0000

    but remember power vote hardly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
+def find_max(lst):
+        return max(lst)
