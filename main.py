d355414b5ce146681303f6627f5e8b3ed88c05b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:34 +0000

    fact account education off lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def convert_to_hex(n):
-        return hex(n)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
