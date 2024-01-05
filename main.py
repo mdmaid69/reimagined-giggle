3492a7f19ad640c6d7233388aa8e353d676d8c51
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:42:08 +0000

    cry wind obtain passenger bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
