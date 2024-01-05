7eb0b4f9f3d424fe701424681b54a8061e2657eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:02 +0000

    right image their smooth knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
