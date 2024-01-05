aa9e84951d1cdacefb9243185b1aed831de69ebd
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:45 +0000

    goout sister equal slice step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
