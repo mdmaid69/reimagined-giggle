576fc0da56e08073ec8aa5f0d678a266d899e59f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:07 +0000

    provide through repair school flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print("Difference:", set(list1) - set(list2))
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_memoryview(array):
+        return memoryview(array)
