ad52ab5b03e1c7d85152ad558e48d920f0685b4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:33:17 +0000

    person home next fact takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
