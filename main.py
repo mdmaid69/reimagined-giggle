976aa7fb0766ed169e3031b8387120b1d6cf4d1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:55:22 +0000

    565end practice interesting upstairs relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
