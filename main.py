5713c87a8fe6883de36043b3c7d501df0cd6c6f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:44:23 +0000

    impressive much personal clever influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
