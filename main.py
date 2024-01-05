869d4ce467810e4236aac564ff7c87bc86746abb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:44:00 +0000

    dry will man good practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
