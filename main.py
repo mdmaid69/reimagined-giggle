6cfcdd839a29b62ab7e2371f50160e6d49830244
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:29:58 +0000

    chin free unpleasant trick touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
