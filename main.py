0099ceb59ebbd7cd9491850cdb78814698b04341
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:50:09 +0000

    human brain minimum fail unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
