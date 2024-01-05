29d7753601a61b92f0a19e3b0485cccf1e75af22
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:26:08 +0000

    comedown motion cruel interruption stage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
