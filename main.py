b47c6b8c8a85017c040a4c0f100facb9dd1b6041
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:08:04 +0000

    within shape wide large sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
