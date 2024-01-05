ee57ae8ab0b2a2ca16178e514a89b488e79ae98d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:45:37 +0000

    Bb height turnover rise convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
