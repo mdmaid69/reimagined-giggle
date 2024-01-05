3c066091cbdd393fb62bd59d642cf515d46e0293
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:46:09 +0000

    drink permission taste let fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
