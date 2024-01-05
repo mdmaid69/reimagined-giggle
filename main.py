36d0ce0ae2c2ee092e4b66185bd9274c34c1823a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:47:55 +0000

    wonder sensitive obvious human how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
