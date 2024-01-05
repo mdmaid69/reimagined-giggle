760be430881c625c9e8fcfa4aa9d2e019d26cfb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:11 +0000

    sky plenty whichever welcome ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_stack():
-        return collections.deque()
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
