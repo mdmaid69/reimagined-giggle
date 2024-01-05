20debb1b2c744bf2cc98f909525d88e155ea8bf5
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:37:08 +0000

    interview mark mood old measure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_volume(length, width, height):
+        return length * width * height
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
