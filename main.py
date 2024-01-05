f35cd0b023ea61484076de35904061d3ee3a704f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:57:30 +0000

    near circle difference upstairs workon

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import numpy as np
-print(np.array([1, 2, 3]))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
