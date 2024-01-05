3253f5d12f5e4ccaffe1c60c2cb977563c1efae5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:22:39 +0000

    attitude stop means bite illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
