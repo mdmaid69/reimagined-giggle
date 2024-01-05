b4ad31e4b8d95710d144cbbe571f019c033d42dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:06:39 +0000

    569enjoy accept disappointed himself nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
