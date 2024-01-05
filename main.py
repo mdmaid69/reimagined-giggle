fe8cfeeffb3d4114636ed923ae1d9980eed04304
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:02:58 +0000

    usual do identify order entrance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
