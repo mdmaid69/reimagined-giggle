6896a1491e5b27cb54c07e9de99bf961ac492e1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:50:02 +0000

    practical type lay worth borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def add_numbers(a, b):
+        return a + b
