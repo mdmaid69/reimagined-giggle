fb86473ee19942ab48de71e05a26f4870b8075fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:23:58 +0000

    generous practise zero coffee doctor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_volume(length, width, height):
+        return length * width * height
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
