5f3b0318f5ced37fe80a6ab17028ea0c59af27fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:27:00 +0000

    true record care experiment neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
