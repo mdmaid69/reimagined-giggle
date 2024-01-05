3af583d2c81bf295117935eb33edba4045d4db2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:14:02 +0000

    situation somehow floor represent react

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
