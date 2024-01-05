a15121e699001e4b90ef3cf2a227d62b4256e49d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:30 +0000

    each bringup tax illness organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def remove_duplicates(lst):
+        return list(set(lst))
