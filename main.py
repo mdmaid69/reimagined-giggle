878dac6b44bd2312b7267cb457cfa10b70f64634
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:17 +0000

    discover tense breakinto like receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
