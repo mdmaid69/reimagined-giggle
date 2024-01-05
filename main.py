1a2f92e8ba6e27ce3f6e6c4f6e348ed0be8bda62
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:59:15 +0000

    above interested become people mine

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
