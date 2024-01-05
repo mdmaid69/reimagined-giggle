fd72023a1e7db367c56d813127f083389ef9c4a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:30:40 +0000

    the whistle disagree beyond believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def convert_to_binary(n):
+        return bin(n)
