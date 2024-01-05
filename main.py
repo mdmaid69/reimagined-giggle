bcaeee8ef938d68188af91fb5685566fa51f01c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:14:06 +0000

    field importance fast interview fold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
