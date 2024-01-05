b042738b18224596be0592cc5af557b070ddbdb7
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:17:15 +0000

    famous achieve goout patient but

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_as_set(array):
-        return set(array)
+def set_array_item(array, i, item):
+        array[i] = item
