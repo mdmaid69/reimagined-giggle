049e6de146acd6348a314f4b41d999cf734a9264
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:17:53 +0000

    excuse quality possibly myself wish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import re
-print(re.match("h.*o", "hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
