caa2785309a6af4ac12976c27e46da0e781fb801
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:05:24 +0000

    order heavy family threaten appeal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
