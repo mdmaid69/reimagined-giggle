bbaf3870243ec231ae22bc4b15d4b0b494243c5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:45:21 +0000

    bear consult least plan discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
