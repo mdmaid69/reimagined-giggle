44ec0890195b66f6bacfb929cac86ae27d915820
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:12:19 +0000

    Cc reserve Mm large complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_date():
+        return datetime.date.today()
