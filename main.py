8f0e7bfb65b5d0859f216ef2becce4bbf1794503
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:09 +0000

    rent several whistle phone we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
