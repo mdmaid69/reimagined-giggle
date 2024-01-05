973a8c1d42093c69b3e0d4d352ff5c9808a8fe4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:55:02 +0000

    miss ready gap prefer corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
