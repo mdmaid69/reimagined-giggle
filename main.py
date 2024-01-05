b4045ec8cfee6bf5cdf1b773acfe27f48acb7b49
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:26:59 +0000

    opposite work height lead their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
