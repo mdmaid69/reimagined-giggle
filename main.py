f2c8d2be568a876e44187048de08a9a617f1ff1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:26 +0000

    mouth radio position exciting Ll

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
