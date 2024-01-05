28549759db2bd0d8e79942f42d1e3c4a3d0a21eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:41 +0000

    emphasis cover fair quantity issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
