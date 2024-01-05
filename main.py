b8c8e3c75e5028c46a26e849f9fa171cffa35f21
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:06:35 +0000

    danger concerning emergency turnout 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
