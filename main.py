806e7ec197bb88841cba7827270ecdc241457e30
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:54:30 +0000

    she argument history move sister

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import platform
-def get_os_info():
-        return platform.uname()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def add_numbers(a, b):
+        return a + b
