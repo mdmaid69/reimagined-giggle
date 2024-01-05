1549494f51ae98960b8dd5ad056e542a840f9e64
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:19:18 +0000

    happy fat especially spin smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
