d008554f5317403f08627f30cd13ea7e935892cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:03:50 +0000

    inform from thinkof family demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
