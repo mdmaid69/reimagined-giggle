f2e9033b2c11abb3e55db4c767d5e051f9db2da1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:00:31 +0000

    earn consist through lesson well

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def sort_list(lst):
+        return sorted(lst)
