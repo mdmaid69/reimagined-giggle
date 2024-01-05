fbe01d648f535ba08c3a1251ff07ff014a0eb95d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:05:47 +0000

    sometimes part possibility fight belief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
