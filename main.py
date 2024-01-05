aa24a21dfca10c9da610e66bdf86362a5f4f8597
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:14:54 +0000

    stick situation whatever pass lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_current_directory():
-        return os.getcwd()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
