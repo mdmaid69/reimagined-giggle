5c29c6b084e9dfcc7e2c2ea4b10949a8efb5e032
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:46:40 +0000

    line priority today room run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
