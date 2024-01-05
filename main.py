f0890395add1d873c17a14848bb0a14373530d8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:52:33 +0000

    exaggerate typical section account confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def is_palindrome(s):
+        return s == s[::-1]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
