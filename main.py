dfb7dd713d451fe40044f884f006270128cd4104
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:20:12 +0000

    variety exaggerate advice computer hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
