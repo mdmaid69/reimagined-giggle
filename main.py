93340a17f4bdda745d3f4610f1845e7c204143a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:48:12 +0000

    furniture beneath admire takeout busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
