30364ae51a2900c18afc78d25efdae4f00bc88de
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:10:10 +0000

    unable keepoff accident neither luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
