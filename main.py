8c4cad0ed05b1d3a1ecda580fa6fece0b9b4e038
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:25:00 +0000

    offer later there tread run

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
