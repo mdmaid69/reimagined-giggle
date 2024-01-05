9c0981b35370ff33c7646672706cddbf55b2f6af
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:38:54 +0000

    pinch keepoff meeting crash visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
