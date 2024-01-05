fd038ca44cb3c4aa1acbb4b8ec2e56544f02d792
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:38:58 +0000

    pair goback(to) mind immediately embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
