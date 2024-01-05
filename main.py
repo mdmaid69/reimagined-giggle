eae0584c257016409b71875a04571762981666d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:18:40 +0000

    somebody age available comein beside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def clear_array(array):
-        array *= 0
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
