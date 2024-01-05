7e6dd5f3c0f6043654fe56b438ce1e9f1e40a986
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:07 +0000

    region breath no error travel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
