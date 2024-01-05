e72ba7486c0805ac8b4b51b22aa5224d4e706b76
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:27:17 +0000

    cross giveaway passage roll heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
