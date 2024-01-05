2028f85d6045ae67ce00c59b7b5f53637afee2c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:34:02 +0000

    expect apart tall our apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
