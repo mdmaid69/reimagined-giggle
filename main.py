bfeda4705b828def0df14938930f498ba9962a24
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:26 +0000

    whistle battery regular unpleasant convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
