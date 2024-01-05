f9ba05680fff0c4f8f801d18e51ebc89b382180b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:48:51 +0000

    favour judge area exciting measure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
