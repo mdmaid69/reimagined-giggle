4fa14b6283cecaf485021a399584850a0ce28033
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:17:32 +0000

    range sport relationship equal wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
