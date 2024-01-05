8f6d724fb76b8257df9f013b9d6c92c67936ab7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:16:02 +0000

    danger scale attempt slow target

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
