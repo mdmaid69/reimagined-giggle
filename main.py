77d0c6f0bd4968eb41e7a5304f9a22e92f1f705f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:13:10 +0000

    recovery sudden see funny happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
