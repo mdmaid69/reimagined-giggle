c04e8254d7aab78ea19d6606e76406e66a7531ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:52:19 +0000

    press fall sharp split relationship

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
