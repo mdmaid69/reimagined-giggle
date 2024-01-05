749bc9ee77399d272663d3c5bed6c2105952aad3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:18:18 +0000

    manage man quick public pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
