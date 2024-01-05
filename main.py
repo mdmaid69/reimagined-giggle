466cde1f5754b29e0fa4593daf0a01801a15dc08
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:20:09 +0000

    survive branch look object contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
