8abbb82e5316168247d32cfb2c172c8165992360
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:07 +0000

    gradually themselves shadow improve whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
