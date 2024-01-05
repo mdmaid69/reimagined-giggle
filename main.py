62135c2bf72071f406a69978ab3f329a8e131ce4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:03:07 +0000

    peace rise arrival morning possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+def exit_program():
+        sys.exit()
