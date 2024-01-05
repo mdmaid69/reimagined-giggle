c8ec8dc20098e2b17110eea6a3389130ed4af6d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:55:28 +0000

    person rain twist press private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
