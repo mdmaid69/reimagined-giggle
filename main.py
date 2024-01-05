f0ca0649a8502e3b3fc3d412fd6d8cb52f07c7ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:46:09 +0000

    ride have takeaway tap wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
