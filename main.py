7a2b1cffa4c1de014eb057bb7af679a00f3b90cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:01:17 +0000

    thankyou runaway his though system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
