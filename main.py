08966adaae8a233bcc52bf51a18b2555a4f0260e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:17:13 +0000

    police judge completely back seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+name = "Python"
+print("Hello,", name)
   import numpy as np
   def calculate_mean(arr):
         return np.mean(arr)
