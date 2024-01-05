0cb73ce84190910bf404f7c0418619b48ac87aa7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:36 +0000

    pretend payment split cap doctor

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
