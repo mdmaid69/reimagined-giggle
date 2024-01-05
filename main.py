6be19e1b49bc76322eef6621711af101a546b92e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:23:42 +0000

    blame food tooth taste silent

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
