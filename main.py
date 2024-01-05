da33088fb5a3dd9e1ba6d7f2410895333b38ec8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:37:53 +0000

    bank Cc opinion owe point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
