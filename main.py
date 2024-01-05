2b7865d2295e3842f795e2f675bd41aa201fad74
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:51:19 +0000

    future by worry tired past

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
