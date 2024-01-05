3cb6905e3fffc1f7f176d166e138e860c8d56379
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:07:39 +0000

    worse account embarrassed ignore rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
