6c8322406fb9eb8b88b717f310007120300bb7ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:01:23 +0000

    bottom pencil autumn objection indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
