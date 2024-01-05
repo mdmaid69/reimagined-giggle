ad99a4e926b4f6db2bdcf93e739b21db6bda0adb
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:04:55 +0000

    mine will memory report announce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
