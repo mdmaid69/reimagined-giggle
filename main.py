5aad787a46b81b15d261f50fd872f8b65bdbda55
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:15:39 +0000

    concentration herself after anxious giveup

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
