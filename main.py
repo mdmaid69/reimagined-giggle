d145977af1684065a016257fab1bfb5197d2868c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:54:44 +0000

    difficulty confuse radio day woman

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
