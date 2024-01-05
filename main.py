39dfd6034803da090e4d107d5e706b8f73f3502b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:46:52 +0000

    wear grow to laugh obvious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
