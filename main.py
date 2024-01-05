a2d11dc938965971d511c63e2e89c5471bed8c2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:49:17 +0000

    backwards recently field like criticism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
