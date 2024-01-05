e0f2791528829cde9971df61811ec631a6b0fb44
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:21 +0000

    wind talkoutof he change always

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
