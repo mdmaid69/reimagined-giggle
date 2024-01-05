6770d5ab389f0e86c997ee6b81b9af57b9383550
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:04:17 +0000

    programme able bye character rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
