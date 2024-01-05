ca174edc97d97aa7f0204150f7657cb09fe52997
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:24:48 +0000

    search introduction many switch provide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def count_elements(lst):
+        return len(lst)
