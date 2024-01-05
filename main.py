24c7497e87a73b46bd5cb75d6743628e8bd0b59e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:30:41 +0000

    time sore in yesterday label

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
