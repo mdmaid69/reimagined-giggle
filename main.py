c309e8ca12a6990408f981f789294075719b2371
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:43:02 +0000

    peaceful bringdown variety year practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
