0d6f9b181309a7e9410d9bf75e5e8b9f542a3be0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:20:48 +0000

    command treat guilty bound worry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_min(lst):
+        return min(lst)
