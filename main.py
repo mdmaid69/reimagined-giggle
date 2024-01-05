13319713031066a112c3c910484dc6db48b597aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:36 +0000

    model try quarter sort deaf

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def reverse_array(array):
+        array.reverse()
