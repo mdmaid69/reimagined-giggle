3b8bf380da4aaa429be2ef9e5f6e8b474649e2f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:39 +0000

    fire exchange good fear shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
