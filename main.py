55de82c5d8ca945167d5e3da15302ef5e6d219f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:45:46 +0000

    push balance community match pride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def reverse_list(lst):
+        return lst[::-1]
