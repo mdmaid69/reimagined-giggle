a2ba34b51b402e18c08e0cafbb572036901148ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:29:39 +0000

    generous backwards destroy blind sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
 def calculate_root(x, n):
         return math.pow(x, 1/n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
