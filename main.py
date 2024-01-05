586fea83e755b314dfcb3ac65f3856bc03ccf81d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:29:28 +0000

    lead from crowd reply most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
