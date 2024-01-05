09fe0424dcd0edd7a600edb65f5405ff2e7cf2e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:18:51 +0000

    habit speak sport song early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
