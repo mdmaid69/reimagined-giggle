280e3038dceaf639b77b8be3aba38c31c302efe9
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:29:38 +0000

    object small sister available cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
