2d1c714ed8f82eeddf41b830a2580842705e9026
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:49:21 +0000

    exaggerate crazy once nerve wait

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
