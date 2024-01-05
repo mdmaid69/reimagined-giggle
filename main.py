eebc11629d2475e6f006797c9ea54c19c2e51254
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:29:42 +0000

    load hard justice yesterday line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
