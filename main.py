8257d8cde7195a328dd5e2d13dbd05160dedb382
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:19:29 +0000

    recommend bottom fair part major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
