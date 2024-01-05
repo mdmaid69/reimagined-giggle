55a544667f5df96a1bca3b3409c21725ac2ddf09
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:16:49 +0000

    telephone talk Nn reliable memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
