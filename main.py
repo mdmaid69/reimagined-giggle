c1e0add9273fac74b416d7e0a4c140168ea555da
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:07:14 +0000

    education aim passenger main content

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def remove_duplicates(lst):
+        return list(set(lst))
