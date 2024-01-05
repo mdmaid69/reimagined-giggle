7d927d0b298a85693fbd6b109a1ea0c791ff443b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:21:23 +0000

    scrap translate keepout some pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
