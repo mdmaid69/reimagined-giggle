9e7f1988f8ebae378fdc75f1eda24591eab28b40
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:06:22 +0000

    compete do medium seat neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
