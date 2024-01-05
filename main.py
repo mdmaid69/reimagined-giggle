7ac420687e1819b4105f45b9b50a70ccd2b5be79
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:14:15 +0000

    suit actually turnoff loss build

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
