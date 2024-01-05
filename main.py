f210f0ad15e24e2dd7c3ee207dbb5a639edcf7c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:31:45 +0000

    sweet play old unusual somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
