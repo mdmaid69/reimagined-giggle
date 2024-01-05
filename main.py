9c1abe6454fb541c7ef30cb52fdbf4b9e5533165
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:41:14 +0000

    life mouth frighten exaggerate early

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_list(lst):
-        return lst[::-1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_unique_words(sentence):
+        return set(sentence.split())
