29d0b7831a584387e147e4526671dcac8505f9d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:45:07 +0000

    separate wait ability careless also

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
