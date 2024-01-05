6dbfd325b00b67f3a23f6c0b2b626c63902db6c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:51:44 +0000

    kick agreement meeting address excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
