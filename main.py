b54ce25631ebd8de96066d1dfe03f7a2ea963f6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:42:41 +0000

    hurt themselves priority opinion cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
