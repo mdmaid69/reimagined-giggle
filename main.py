dceac74a9d9328892b631770753fcd4356dfca7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:36:00 +0000

    reduce geton note sore downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
