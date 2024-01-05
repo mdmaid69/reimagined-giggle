779d489143a0e1667aef762041a06ee1413917e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:33:51 +0000

    ashamed closed impatient visit bread

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_priority_queue():
+        return collections.deque()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
