7c1a493e6de5366a7ac9fd7f0b1e703b5022cd48
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:02:37 +0000

    hardly increase search warning same

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
