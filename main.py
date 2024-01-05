9f9212af4ee399a72480eb26af488b2fb97ef8ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:01:19 +0000

    base this cut male bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(x, y):
+        return x + y
