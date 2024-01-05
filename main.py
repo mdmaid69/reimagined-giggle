d58f25459730382930925e2ceb6d83be52ca826d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:40:42 +0000

    be discovery branch software name

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_power(work, time):
-        return work / time
+def find_max(lst):
+        return max(lst)
+import sys
+def print_python_version():
+        print(sys.version)
