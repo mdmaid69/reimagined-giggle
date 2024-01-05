eb60e908e40a769d27d5a8ca959acc9d6b4fede1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:55:02 +0000

    harm centre might early sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
