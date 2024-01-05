a2c5fd1ca7124516d56d12b53bd61daf8b07effb
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:44:15 +0000

    later sky cut force effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
