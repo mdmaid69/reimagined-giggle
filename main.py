d1717bc106349735579a6f3e33edb985d37f0446
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:09:56 +0000

    trick she visit Ee tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_array_to_list(array):
+        return array.tolist()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
