e31d2afd47720a681297ffbf559fe2a8886347f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:14:14 +0000

    public off present insist land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
