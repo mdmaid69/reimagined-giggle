e4cab956600c0313b957bc993a947ef4dcf664cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:48 +0000

    pin cross wealth Ff soon

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
