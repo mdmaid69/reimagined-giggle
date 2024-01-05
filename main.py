e69e4e7658a3e27f4de6426d40f8eabc99bb2972
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:08:37 +0000

    no contrast peace confusing sensitive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
