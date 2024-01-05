ae9abade15220959ebb8ce48b208737cfef9137b
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:33:37 +0000

    line from point boring in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
