0117e01a09d5f12f67e2d25425db58fc28fde07b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:34:42 +0000

    reserve problem disappear warn prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
