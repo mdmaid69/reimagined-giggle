aaf733d20507eee376a6a94780e1d950a6ca0668
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:07:30 +0000

    scheme raw towards lean compare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import datetime
-print(datetime.datetime.now())
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
