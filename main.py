523e4c2ffda73a6e95a67639c0dbf1c9042b1590
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:05:12 +0000

    bunch design such delight pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
