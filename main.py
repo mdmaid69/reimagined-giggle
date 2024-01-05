71916b79f29c25c6455e617140601ec1f296a9f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:24:02 +0000

    recent difficulty university single organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
