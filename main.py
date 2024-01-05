ffcf82188bd8287d41a1c389ebeca12ffe70f1a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:08:11 +0000

    test mass century strong form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+for i in range(10): print(i)
