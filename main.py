6b4e0b537bbb0bfa2b92ef9e3468811a62312bb5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:17:34 +0000

    importance turnout talk able feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
