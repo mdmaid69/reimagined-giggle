b463035d269e6f258dfbb7f007f04a6522f0b90a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:00:53 +0000

    principle brain country house save

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
