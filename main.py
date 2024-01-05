3fa134d70a4658a187247af5413bbc94dc5b224e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:14:54 +0000

    confused blow especially sense either

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
