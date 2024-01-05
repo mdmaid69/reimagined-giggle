d7d8348d1884df2e72b1a041a670ae2404908aa9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:51 +0000

    up check rent split bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_priority_queue():
+        return collections.deque()
