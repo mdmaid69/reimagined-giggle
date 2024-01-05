37aef5247b2ef6f2d578568bd69f88a13acebcb9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:20:13 +0000

    police read present same pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
