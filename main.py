223ece340f3b9b01fc4bf7283e4d18045d144168
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:32 +0000

    trick demand discovery tend offend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_min(numbers):
+        return min(numbers)
