f97a6f817d8de21b493bbe915bb8f62cd6d22c01
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:31:23 +0000

    store guilty properly 573entertainment offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
