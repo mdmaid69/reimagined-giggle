ef8cc7ce52a1223ddef834353282fb14ddd465e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:17:28 +0000

    stay independent variety trial post

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
