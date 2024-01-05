f5a2c2745d8b3e85740b73d2412d57e0c77e0661
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:57:10 +0000

    independent judge take workon deliberate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import pandas as pd
 print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
