5a2c26106fa669e00236b16ec22c27927ea79b0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:17:39 +0000

    safe wall join start before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
