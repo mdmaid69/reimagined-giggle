f7cbbde8ef13dfb5776df30f1e58cb7126553341
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:01:13 +0000

    warn nurse city lost be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
