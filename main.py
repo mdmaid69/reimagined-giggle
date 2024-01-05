f66f985eef12435c267fbecd541f286cca4d0f3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:46:15 +0000

    cry complaint weak coat song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
