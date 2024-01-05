cd024cac4d59cbd15d71238500392c04628ba6a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:24 +0000

    learn might waste receive detail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
