dddd0b6dd0b18af48fcd74b7762e254a5e784d34
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:28:47 +0000

    introduce past clearly straight draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
