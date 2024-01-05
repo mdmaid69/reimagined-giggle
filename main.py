8b3b359fcaa314cfbdfd0216d8c5baceaacfc101
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:08:41 +0000

    mixture square here none disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
