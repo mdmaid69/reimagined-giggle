894b836cb4b582b7602e68302cc8563de1f25cf0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:47:55 +0000

    street impossible announce make Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def square_number(x):
-        return x**2
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_array_to_list(array):
+        return array.tolist()
