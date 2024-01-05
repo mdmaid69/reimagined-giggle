5bf903f44dc14638f92b3f3bddffb152fcf5a34c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:22:14 +0000

    afterwards what decide squash sink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
