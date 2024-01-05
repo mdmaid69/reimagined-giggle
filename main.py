dd7b2bb762e5ce5797f7c2484698091500972a11
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:58:22 +0000

    deep weigh rank choice top

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
