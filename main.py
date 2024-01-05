9f746a566f6657fb0d50d6ae844ea885d647769f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:05:53 +0000

    tragedy contact relationship during behind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def delete_file(file_name):
+        os.remove(file_name)
