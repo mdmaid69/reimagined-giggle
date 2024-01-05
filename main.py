fd3372da0110d76718b07c587c0fae95f9d24e87
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:04:43 +0000

    discovery agree head swim crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
