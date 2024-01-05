7624ef632a05973dcb06ddf282877d7cfdfde487
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:19:19 +0000

    middle lead door swallow left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.ctime()
