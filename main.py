a28e6fdd57a4da2e84aa805096a17e822cbf4e9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:19:48 +0000

    season definite official you goodbye

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
