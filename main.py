c6dfea57cae23c917abf30284209120abdeb2378
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:49 +0000

    proof shock confident month conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+for i in range(5):
+        print(i)
