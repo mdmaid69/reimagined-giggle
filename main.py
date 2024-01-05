03b99c4f78289b05423dd196947343e192d81bf1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:35:34 +0000

    of excellent know scream bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
