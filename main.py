9bd2b7f71f7a695f04e0c7c26b9bd27dccadc2b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:05:04 +0000

    deep unable guard last vanish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
