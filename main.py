fb42d0552f1c95d2030591025b327467aee4f6c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:13:28 +0000

    uponon stare taste ground freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
