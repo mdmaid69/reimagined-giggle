7027fab899392f5cf9571868209ff1a0e85ccad0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:38:24 +0000

    favour wish special print knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_unique_words(sentence):
+        return set(sentence.split())
