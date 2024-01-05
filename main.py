7c99b60f6fa61b0893397ed976e66b862fc5d46d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:26:11 +0000

    lay far start few prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
