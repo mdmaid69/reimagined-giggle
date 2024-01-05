2bd8d54f4cd625ade43db7c9c3ac9ca004f1a288
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:06:30 +0000

    skill extraordinary someone defeat set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
