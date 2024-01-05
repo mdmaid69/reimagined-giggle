ddd648ef85f892f0d7f98aae6ef36cdea8e2ca27
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:30:43 +0000

    space passenger trust pocket afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
