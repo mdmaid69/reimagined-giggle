5f07d1d6d7dae0015be19d0f73f42a744d935e74
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:51:04 +0000

    thank amount design land drop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print([x**2 for x in range(10)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def sort_numbers(numbers):
+        return sorted(numbers)
