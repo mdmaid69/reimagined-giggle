b5bb42e9346ac85893eeec2a4b48872a7f181543
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:04:11 +0000

    affect eye listenfor who mess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(10): print(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def cube_number(x):
+        return x**3
