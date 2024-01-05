3c8ebdd51ff9fc8366334d2aed7934ff9df9469a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:58:14 +0000

    food reject ordinary law pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
