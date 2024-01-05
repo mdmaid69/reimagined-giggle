46ac9c2e0c1cd23860ba7b007ee4b5bc38df3082
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:42:02 +0000

    use add spare lose region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
