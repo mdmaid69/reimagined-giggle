17688880ad8f14d5d53a5178f749befbd1d9b668
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:06:32 +0000

    road minimum pipe off callat

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
