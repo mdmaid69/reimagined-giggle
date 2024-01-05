65c3edc0802f72edc9d550821d0162210fb2f147
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:54:59 +0000

    exactly attract tread delicate traffic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
