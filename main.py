e8adbfd3b2e2111718a965f7264ee7d2a06c580b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:51:27 +0000

    free fond earn relationship zero

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
