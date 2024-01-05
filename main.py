fdfa2ffaacb4722cf0ff0215edadeb0fd6b11457
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:15:47 +0000

    remove night half splash message

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
