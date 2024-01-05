34ce59bfe08a54b84574fad93f7fcb17b38729d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:23:01 +0000

    train trap unite us definite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
