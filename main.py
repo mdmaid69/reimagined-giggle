ae1c9b92bc6a45ef715e03b2b3cb7aee6676c0e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:21 +0000

    influence vanish suffer between getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_typecode(array):
-        return array.typecode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def reverse_list(lst):
+        return lst[::-1]
