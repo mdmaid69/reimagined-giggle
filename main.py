0ac7e8b65a0d4a60ef55c15529dc6bcb0e5eab12
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:46:46 +0000

    flat opportunity keepout dead sink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
