25257945af313992a43dccc41ba3346a24fb36e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:36:38 +0000

    uncertain past independent choose date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
