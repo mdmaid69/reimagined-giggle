d75fc391013ca1013cea113111cefd251a51423a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:44 +0000

    support container finger die succeed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
