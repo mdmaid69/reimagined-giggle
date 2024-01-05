f259ac591fcb17a0a6dfc9b6aaa1356cadcd9700
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:46:49 +0000

    everyone often inquire area spray

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
