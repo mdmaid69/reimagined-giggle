3a6812a894308eb36e39f5bc8f3fcdbec1903929
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:37:51 +0000

    hour fasten burn feature introduction

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def convert_to_binary(n):
-        return bin(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
