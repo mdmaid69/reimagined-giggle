ffd8fa5e8bba93463fd14070ff5661add0d3f176
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:56:02 +0000

    surface beneath bear unfair else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
