3c38b2f3a0c4fa0a6c7b9cd97168bcf92b4668c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:25:31 +0000

    tear table weigh instant private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
