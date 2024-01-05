e7f1ab98f3badccf0217e60384ef4f55405f358b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:56:53 +0000

    pleasant education copy recovery breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
