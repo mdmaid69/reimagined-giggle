21a28cc1d7f4bcdabde4f48c435ba82f663e856d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:47:43 +0000

    shock announce occasional public comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
