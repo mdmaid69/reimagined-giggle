ce97d5ac9bbc46cf7a078f3991222502d6061e02
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:49:59 +0000

    Ii handle slow sore owe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
