e6e393dfdf9a242653ac5bbde6dcc799fea05ad4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:32:56 +0000

    clean forget any past shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
