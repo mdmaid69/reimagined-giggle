5c8ab38c047673582df8ba3452a141591202f872
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:54:10 +0000

    Ff run effect limit bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  def multiply_numbers(x, y):
-        return x * y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
