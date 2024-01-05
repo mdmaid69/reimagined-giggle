7c8430d7dfaef638b439fd0b81c7e0b348a24b54
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:49:57 +0000

    Ww winter harm usual group

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
