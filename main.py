b1033c5643d5683de410467f8cd6edeec51e521d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:30:42 +0000

    relax little holdon size relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
