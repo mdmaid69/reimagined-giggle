0a1024d70b5612a09b34e12b9d666d4b2d8243d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:54:32 +0000

    funny rather 569enjoy expect food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
