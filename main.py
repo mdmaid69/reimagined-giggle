75f3c94fabd152ea2abc41713d5647ceef7fee3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:46:11 +0000

    poor mouth control takeaway this

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_directory_name(path):
+        return os.path.dirname(path)
