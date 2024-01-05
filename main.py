8bcc05c93e357ccce2c7f7950b52c6d16bf63ade
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:12:47 +0000

    keepout label so hope birthday

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
