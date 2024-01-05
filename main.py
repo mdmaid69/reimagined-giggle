61c11f8b64aea5930ade4b28450306cbd2b33629
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:11:36 +0000

    anxious central cost him state

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
