0e42a74e815d237fa92cb9792dbb462b1217c4b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:23 +0000

    guard about achievement relief rarely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+i = 0
+while i < 5:
+        print(i)
+        i += 1
