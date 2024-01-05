14a6e2fcd3c79f326c8b439ea2ce3099b1e60872
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:43:27 +0000

    odd cheek suddenly ready trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
