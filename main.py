3972cd5c2339ea943627ac7b32ab950dcd454e85
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:26:51 +0000

    laugh excuse obtain rank protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
