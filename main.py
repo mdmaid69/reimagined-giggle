9cd429321e3b312b886d2717ae5252ac67e81b69
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:05 +0000

    health roll gooff lucky firm

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
