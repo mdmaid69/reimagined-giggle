6eb6f436dd59609fa8836f636f1139db28b7dbdd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:27:07 +0000

    couple pull other telephone medium

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
