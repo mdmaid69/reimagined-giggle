35eceb03efe76f0842f18095f38d26c7605af4c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:45:32 +0000

    discovery closed suck surprise stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
