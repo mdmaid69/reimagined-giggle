562efb8d1c7cc3f2a4bfa55fec39842eadf96c4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:28:27 +0000

    talkoutof so period next stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
