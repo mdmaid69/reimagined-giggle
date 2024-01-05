2581d05d822e8aa5e56136cc5de4dd8f678d4061
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:54:35 +0000

    edge sound runinto direct apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
