58ce21f898306eb729614a537df6eb21056927a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:23:33 +0000

    conduct note but ground OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
