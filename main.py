db0d46a419cbc99d526b6f279139c6919f274334
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:31:46 +0000

    beyond welcome rubbish habit cinema

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
