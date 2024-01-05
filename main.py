65b70002060074ba745fa14ec4303cdd75e80613
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:24:15 +0000

    there false ordinary grateful bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import time
-def get_current_time():
-        return time.time()
+print(time.time())
