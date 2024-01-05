a0803d4f30c79e38774d0bdbf99e1cc318c38dfd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:50:21 +0000

    holiday ride unlucky near slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
