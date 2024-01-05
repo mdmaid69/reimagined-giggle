1a7b4581b26787c921df074b4ed614fa23fdc5a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:32:24 +0000

    mass hold frighten knock criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
