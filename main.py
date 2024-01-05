efab70b3b543c047bf89178478c6cb3f4dcea2d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:23:06 +0000

    run born front themselves favourite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
