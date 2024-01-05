0a56476d9363acca95ebe1d9903cf16e0ea590ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:50:46 +0000

    like progress patient curious present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
