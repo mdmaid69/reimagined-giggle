51d48b716348b1bdc0e80e67433f0f9f860f5378
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:22:38 +0000

    burst skin regard shout pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
