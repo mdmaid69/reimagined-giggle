26624ff43e72a68a70f7b4b0552b485e260ddd17
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:54:57 +0000

    shopping hate on powerful safe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
