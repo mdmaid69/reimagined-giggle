74773c3c6a7bcd6f728253b8f92e1501e75363cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:37:59 +0000

    unit only extra around tomorrow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
