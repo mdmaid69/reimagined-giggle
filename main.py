61eb282461f5d1bdc5e15de993b077b33d5e9ffd
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:10:26 +0000

    square there whistle about room

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
