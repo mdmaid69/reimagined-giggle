26a858631e3713ec3d1255d5c39400cebf3a61c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:45 +0000

    ground unusual level actually improve

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
