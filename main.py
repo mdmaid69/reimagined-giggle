50650a99e40e9436f65f78b7001965bb660cc7a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:49:11 +0000

    tradition attract left get about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
