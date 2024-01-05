0e59369e18b5306b055313f3c8b2c06a63f2499d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:35 +0000

    off keep course cheat ticket

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
