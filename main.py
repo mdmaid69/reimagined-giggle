849cc16200d402e7acae1da837053ca8c38d5495
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:57:45 +0000

    contents acceptable table exactly strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
