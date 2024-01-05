c2f7caa38090def85925c2071d9732f8eff08524
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:30:55 +0000

    worry police port love practice

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
