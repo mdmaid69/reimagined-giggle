2697439c1da951d66f30dca848147814fff272cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:12:28 +0000

    television somewhere square politics closely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
