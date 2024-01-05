8ba46aa04a3a9710ca23ea6c92fd565587ce41a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:43:41 +0000

    eat into gift none powerful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
