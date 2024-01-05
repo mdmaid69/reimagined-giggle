031f33d2272b7f584b5e38c9249156db0e7b78f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:22:11 +0000

    whistle hang abroad quarter eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import sys
+print(sys.version)
