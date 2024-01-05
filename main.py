342dc7fefd0436e8cecd3ec36d427937778bb330
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:55:07 +0000

    tap interrupt judge best takeout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-print(math.pi)
+import sys
+def exit_program():
+        sys.exit()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
