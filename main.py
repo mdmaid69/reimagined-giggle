793cb9afd8e50cf157fdc34e6bbcb81beee3b8d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:22:53 +0000

    check hold during play proper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
