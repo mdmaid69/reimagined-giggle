72616977c4a9044b8c1e767bd0e3e5ccb2b15a1a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:57:43 +0000

    soft cheap exact runinto clear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
