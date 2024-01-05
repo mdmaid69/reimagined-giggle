47831fb86a64f876e8515637203025e7226f060f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:55:36 +0000

    listen violent plan backwards flow

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
