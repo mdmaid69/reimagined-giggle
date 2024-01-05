983862ef415f526d776675c44a9d5c6ef7b8fc0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:24:43 +0000

    rain nice firm advise steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_bool(array):
+        return bool(array)
