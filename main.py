dd0737c7b8f1f2a76218597cd7ee8bad3595399c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:36:24 +0000

    throughout keepup bottom crisis waste

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
