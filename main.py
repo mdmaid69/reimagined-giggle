8d219770f3e368d5269cc0b909fac1d18af5a40a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:33:28 +0000

    chance let mean most boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
