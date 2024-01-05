e7a54a3a165e1903f1bf66ed7e7142ae89eaf579
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:09:17 +0000

    season typical turn bunch Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
