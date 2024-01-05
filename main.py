2441954bbb9683fbddd418401f98b3b2df713301
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:18:36 +0000

    phone clothes suit geton safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import sys
-print(sys.version)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
