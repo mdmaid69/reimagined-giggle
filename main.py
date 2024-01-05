b60e958d6f9933ecb19f529750b0be09b6b2df13
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:27:10 +0000

    opportunity swimming model lookafter Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
