5174eb4b2630339d34ef85fc25e14c209327179b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:29:12 +0000

    regularly conduct accident cinema motion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
