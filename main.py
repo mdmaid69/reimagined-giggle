39b2d4d1686a64478c28d650c5cb098e955d3584
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:02:45 +0000

    train extremely under view calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
