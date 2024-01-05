690951cc8a726ddcc4319d54de9f74751469af67
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:40:43 +0000

    often dress related left home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def is_even(n):
+        return n % 2 == 0
