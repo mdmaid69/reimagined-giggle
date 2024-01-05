ba4a099ac3675cb8f49202216c9e02f293dd6ff7
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:40:42 +0000

    still twist emphasis mix far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def is_even(n):
+        return n % 2 == 0
