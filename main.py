916b4ec6b82e664f0b1cfa607c44907022a49b04
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:36:46 +0000

    wave generous wise this watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
