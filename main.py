90d0fb6d2ebad6c0ff51adfb972a2293310c2e44
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:46:13 +0000

    wash who Ii speed later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_characters(sentence):
-        return len(sentence)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
