538fdbad5b995c4ed018a6f95065bdbb86f22cdd
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:53:15 +0000

    card foot hurry appeal become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
