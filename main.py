3d903ef50424d7e60210d1ea9fd5e2f6955601f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:43:08 +0000

    manage no married giveup indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 text = "Hello, world!"
-print("Words:", len(text.split()))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+print("Uppercase:", text.upper())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
