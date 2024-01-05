9f11a9a4745f10da91a53c268ae9bf477b695fd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:27:46 +0000

    war hurt needle need average

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
