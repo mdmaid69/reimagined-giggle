5a98e1bbfd70a7713e20753fcfe66e8085adf066
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:42:58 +0000

    use basic mysterious bar which

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import time
+print(time.time())
