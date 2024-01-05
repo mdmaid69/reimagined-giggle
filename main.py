125e9768db63cfdf1700a75d82b85fe7001c61b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:31 +0000

    towards floor negative kind zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
