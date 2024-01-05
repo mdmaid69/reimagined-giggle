556deed78c54865aa7392281907a8a655291b461
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:36:33 +0000

    value telephone estimate careful wise

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
