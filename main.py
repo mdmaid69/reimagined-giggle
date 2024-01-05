4c517328a4a1f8e53e3d386942ba825163138be1
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:48:41 +0000

    cum getaway uniform cum illness

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
