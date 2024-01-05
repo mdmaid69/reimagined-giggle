59f60d4f86ab2d7dd1f6bcb3d9dc62f8d562109f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:47:09 +0000

    shrink free land happy tap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print(sum(range(10)))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
