11ddc872fc37dc506e40a2af21c96dfb8a1345c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:19:17 +0000

    public situation bury right far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
