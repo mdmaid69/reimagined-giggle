1c92359afaee71728f13c9dd4fdf20542d938e4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:58:17 +0000

    choose match force at chin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Powers of 2:", [2**x for x in range(n)])
