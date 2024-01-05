0b7cc0f3df025b4833e4d4615018a4957c00bdab
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:12:55 +0000

    sit voice provide far I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
