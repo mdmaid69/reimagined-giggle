9500628b72bed7bc47412db9390a9b7e4dac9dc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:49:29 +0000

    control tell your send stamp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
