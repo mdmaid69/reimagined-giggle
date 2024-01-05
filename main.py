6b64a98f2e3ee03c8adb77cf736550906e3b70d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:38:11 +0000

    shrink of key speak choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
