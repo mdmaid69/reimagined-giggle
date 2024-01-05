36cb42b5b4c72c7cea3cdcae911d56c38d3c7db7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:51 +0000

    run rule part listenfor awake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
