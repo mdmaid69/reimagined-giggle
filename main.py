4bd1c79bfed04b99da239220d660c5e29edec76e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:19 +0000

    risk calloff Ee getout schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
