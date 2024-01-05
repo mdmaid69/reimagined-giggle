805bb77c9d88b1e5cdb669853f15e5a820c9b92a
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:13:01 +0000

    nervous getoff weigh knock stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
