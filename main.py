da6ca3b28d62c85a2ebf1eb739e6c4eaf0a4c3db
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:10:13 +0000

    pronunciation scrap regularly holiday get

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
