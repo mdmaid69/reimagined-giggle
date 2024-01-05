0def35137c09c54724f4641076c384ef1130fb31
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:32:05 +0000

    unfortunate fierce regard dear suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
