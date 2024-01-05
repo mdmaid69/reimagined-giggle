e43c129216cdc36bf0867fc63d5dae54665fa6eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:49:08 +0000

    flow plant priority show great

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_characters(sentence):
+        return len(sentence)
