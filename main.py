0334948f0946ab79ff937c01babf38644c465d2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:54:30 +0000

    easy stupid accidentally commit getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
