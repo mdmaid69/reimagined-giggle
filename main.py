5d1c97e4ee1c925544bdb137fa83beeb936c02f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:31:25 +0000

    pocket lean discussion sharp danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_dict():
+        return collections.UserDict()
