0e4aa94ce648e75ec10d1739e0699f43a9900ebf
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:27:30 +0000

    clearly careful pipe shrink weigh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_speed(distance, time):
+        return distance / time
