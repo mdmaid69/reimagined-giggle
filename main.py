1035de9538e42178fbe4362fb684762614bf7d99
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:48:45 +0000

    live survive field dead crisis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
