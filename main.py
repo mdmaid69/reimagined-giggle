dd9e8d4233ad7267c970b4791e31d44d3655e81b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:05:19 +0000

    seem comfortable skirt answer number

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_acceleration(speed, time):
+        return speed / time
+print([x**2 for x in range(10)])
