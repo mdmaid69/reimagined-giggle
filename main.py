acc28c9d62e17b7ec2dc03ea32c1de17b768863e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:08 +0000

    speak fat smooth sleep burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
