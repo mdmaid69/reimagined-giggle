aca897d53206eaf419a757798af498338e390d7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:11 +0000

    bound aim suggest card open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
