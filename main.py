3108aa06a533c99dd4d52348d515c57ddd2e645d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:57 +0000

    achievement round front perfect sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
