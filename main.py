0eda55dd61b28dfc8dc84209b308caa8f973da02
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:41:01 +0000

    top basic use mark hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_speed(distance, time):
+        return distance / time
