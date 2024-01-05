ee11462dd93f76d831f1dbe1bcfac86c8f45d7c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:06:53 +0000

    around luck law downstairs chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
