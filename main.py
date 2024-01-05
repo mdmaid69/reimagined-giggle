800dc6908d9ed2f8378964c0763612ae406144f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:24:59 +0000

    less hello mind bath match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
