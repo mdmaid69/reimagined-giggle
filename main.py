41bffe85bf1045d3a67caa4fd60d68e4c183900c
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:54:57 +0000

    workon side bank succeed same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
