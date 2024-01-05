970aec185da578bc529b576e686573a3687e764f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:47:47 +0000

    work bring goout blame visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
