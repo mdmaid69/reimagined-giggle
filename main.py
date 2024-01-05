a7153c2a785d42012024459955aaf37ed7e7b203
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:51:07 +0000

    rather cinema explain tap next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
