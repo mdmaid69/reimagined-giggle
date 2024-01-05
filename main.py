1336eb6cc997a80cb75a0ccff17a75664da44b70
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:02:58 +0000

    unit bus down rain spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_floor(x):
+        return math.floor(x)
+x = 10
+y = 20
+print("Sum:", x + y)
