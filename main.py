64bebcdb132508810256a9f9d577c33db2205cc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:37:30 +0000

    take continue claim miss sing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
