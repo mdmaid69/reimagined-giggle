b8ed92f8826be23024313bda62ab22575e66e067
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:41:20 +0000

    ill past his leg inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
