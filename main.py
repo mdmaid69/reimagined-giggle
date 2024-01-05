8433af66dbd8405440b07f628a7897901708015e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:46:16 +0000

    safety criticize proud fly hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
