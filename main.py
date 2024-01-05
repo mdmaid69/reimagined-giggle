874a5109a0b0c2aa05fb1b67202c42c021fb6738
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:03:30 +0000

    case raise fall talk new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
