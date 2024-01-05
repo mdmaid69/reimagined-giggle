3ec62da843e32b8b3fbc516aeef0c4adcc1a7c54
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:34:15 +0000

    experience voice rather float rate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
