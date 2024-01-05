5976d9d0a18bebb2bb182826610abcb30cdb0ab4
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:37:23 +0000

    might laugh branch face bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
