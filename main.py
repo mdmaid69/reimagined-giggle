b2fe48b6565aef985d21c9459f885b730547323e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:11:01 +0000

    estimate rush turnback direct mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
