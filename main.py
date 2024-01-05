b17f0b0d4c47e0e96adb90fbdcf7aeaa24573337
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:30 +0000

    appear hospital suddenly swallow react

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
