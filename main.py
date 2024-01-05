1ec0c6d9ecffa912c17e3cadf1387b59458e60b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:52:41 +0000

    Dd someone grand majority uncomfortable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
