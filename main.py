68025d7988892de0b73532a0545a4fcbca91100b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:53:24 +0000

    ought apologizeapologise Ee note strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
