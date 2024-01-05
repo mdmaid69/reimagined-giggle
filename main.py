29bec0557856c4d86e82348d1f2bacc1872914ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:13:45 +0000

    point board bag he path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
