d5f117534bc17e85f6ecfd8ecbc106b5d90cddd3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:44:41 +0000

    label consist juice spring temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
