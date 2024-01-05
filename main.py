8cf473041367065582a3b3795b637b2dedd63ec8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:34 +0000

    help needle practical shape but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
