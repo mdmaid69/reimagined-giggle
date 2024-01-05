b6a4c82b7ee57efdab371007fdbc087e385801a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:50:51 +0000

    standard rise tone movement want

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def cube_number(x):
-        return x**3
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
