ead8ca3d51a82e8fd24e19025a47ea62e3219e97
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:11:30 +0000

    occasional game glass his Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
