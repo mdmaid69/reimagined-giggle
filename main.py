dde3bf34a84f792bb8cb35e548006862201b7809
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:46:27 +0000

    pass charge piece after will

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
