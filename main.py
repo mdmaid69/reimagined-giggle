f4cc0c68d0cbdaa7950e221f1cba5142716da2d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:00:19 +0000

    them minimum alive thinkover control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def is_odd(n):
-        return n % 2 != 0
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
