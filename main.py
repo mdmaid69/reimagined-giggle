f0df0b17f1cbb73a017fb98ad7763a1d238cd0c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:49:37 +0000

    empty shape post class undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
