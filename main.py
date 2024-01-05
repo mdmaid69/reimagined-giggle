029e4eae285d7788e42712459632b269f281952a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:44:19 +0000

    tune sense hair properly earn

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_min(numbers):
+        return min(numbers)
