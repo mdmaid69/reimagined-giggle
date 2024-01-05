d3f627f52cc58295d6dc1229f4242dde1f841812
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:37 +0000

    nor law determined glad prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
