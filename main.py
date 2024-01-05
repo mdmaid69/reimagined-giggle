f6a954b81c45e670c8ec8183829b59f90867d962
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:11:22 +0000

    involve prepare none influence embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
