e395a6e36e3880748762959bcb5d60937ff976dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:07 +0000

    acceptable seriously allow health him

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
