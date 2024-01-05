81c2ebd970f6ea65ed03a5e2ba92d6faf0ed0a37
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:42:01 +0000

    contribute together rise behaviour callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
