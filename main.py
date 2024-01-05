6e225c0b8432a701f4e507935705b9c6a3e7ec29
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:40:47 +0000

    help consult blue subject some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
