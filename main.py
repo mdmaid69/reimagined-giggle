74f228c4fe752795b1c2ee9f41dc1f859c072ab1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:06:10 +0000

    branch company run clever usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
