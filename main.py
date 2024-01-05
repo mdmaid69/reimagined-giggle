9bb79221edcf2d13a6216431e3d5c2df0a6f6639
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:29:55 +0000

    cruelty worried whisper face here

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
