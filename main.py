b6e4442cd4af208fd9f02d39d1d2ada8eff23894
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:49:13 +0000

    set tie not shall bus

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
