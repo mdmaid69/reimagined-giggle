95f5edcd6368d7614061eb779fde4ae7534959e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:24:02 +0000

    temper peaceful name they protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
