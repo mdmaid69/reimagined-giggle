3cf9592a79c623105ea623c1419073197b830694
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:09:53 +0000

    might love expensive fast unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
