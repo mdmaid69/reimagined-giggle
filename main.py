470b8532fdcdae28e09dbd3aecef55149b7857a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:26:03 +0000

    catch tread punish reaction wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
