748dbbb0eca08d802a1b6b969ed845bc67bf5919
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:04 +0000

    cause straight instead risk post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
