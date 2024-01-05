04ea6ea5725ceef4564035171616a9517243ade6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:37:00 +0000

    damage unless makeup share perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
