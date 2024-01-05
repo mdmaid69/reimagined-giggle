7f824d8510ef9a5270680bd4b99952768a2d6e20
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:16:20 +0000

    567enemy curious family computer board

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def count_elements(lst):
+        return len(lst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
