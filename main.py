453e9b97148b2584cb3bf6fe98e6fc7b2e8dcfd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:47:32 +0000

    bin experiment blow repair breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
