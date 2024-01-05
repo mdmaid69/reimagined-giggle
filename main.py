b0ce6dc724a9ec75414ee98aa208754dfd618782
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:59:38 +0000

    possibility clear same leg power

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
