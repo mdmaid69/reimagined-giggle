9c0f423760085ae43a2ebae3b071ca6e9e6da9f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:51:08 +0000

    excited whoever push design label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_float(array):
+        return float(array[0])
