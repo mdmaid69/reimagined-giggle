d6c39f1eabb03d78bf2e0682e5977c3fc997e626
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:36:58 +0000

    money whoever tune pass means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
