3ab8a7b378acf22c6bf99e2553abdcd4d999ea52
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:57:23 +0000

    put until each complain display

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
