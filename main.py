857fed5f4e3a2e596f5cfb41e3800aeae6af5f43
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:57:34 +0000

    married still generally conclusion than

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import unittest
+import socket
 
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
