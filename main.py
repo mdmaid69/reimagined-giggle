55b7d1ab2d12fa5234f2fae6972d006fe30e5fc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:13:32 +0000

    myself Rr plan strike recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
