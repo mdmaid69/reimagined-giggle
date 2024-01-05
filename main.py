2e8faac666961d7ee0ef411d8640ab4b41057c36
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:45:53 +0000

    easy sink male decide collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
