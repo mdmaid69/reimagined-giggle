96ee53636f6bf7323b312b403253645871bc8e48
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:51:30 +0000

    fun mixture bend high attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
