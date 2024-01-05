c0ed7e5af5aadd4c05caac7a0d866dae47c324db
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:11:07 +0000

    order movement worry suddenly relative

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import platform
-def get_os_info():
-        return platform.uname()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
