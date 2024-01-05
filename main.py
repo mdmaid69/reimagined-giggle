247408cc7eb04e33e18047d123548445cc6d58b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:43:03 +0000

    Ll black afterwards wear this

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+print([x**2 for x in range(10)])
