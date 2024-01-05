30b2449ad87db8b19042358eb3de2d93aaaf7dca
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:31:36 +0000

    anyone this afterwards price number

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def is_odd(n):
+        return n % 2 != 0
