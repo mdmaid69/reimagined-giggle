314c32ac42d75c1c1cad0c0ab56da311f9df012f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:59:40 +0000

    extraordinary rob spoil Mm pile

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
