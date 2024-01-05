aa3fcda9d2e90dd956d17313fb0ed156ac7bce9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:17:13 +0000

    union habit inside exit rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
