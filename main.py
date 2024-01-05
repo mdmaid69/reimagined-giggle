e002be4a0f297e618bba472342bee58054f8747b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:57:05 +0000

    excited embarrassed pair guard value

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def convert_to_hex(n):
+        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
