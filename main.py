aa68035f8e62d6cf98b835ab07367eb6290a16c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:09:22 +0000

    wide further pupil wall wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
