22572eeeaa586dc74be988bd259b01d0c830e77c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:24:54 +0000

    phone transport valuable instruction message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
