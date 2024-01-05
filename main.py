750381aeaa6a84ecffb058bc280073ca09d34f93
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:41:17 +0000

    sign in produce ear powerful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
