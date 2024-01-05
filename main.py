c5c364ba61b50a938139fc51f0cceee2ec85d319
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:16:11 +0000

    once university left occasional pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import random
-print(random.randint(0, 100))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
