9c85836e1c1d7e19c0666b70e53f0b8969a007ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:11:20 +0000

    start trial priority create refuse

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
