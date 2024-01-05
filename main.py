ad751fe514ce30ba44d9ae5b1ecd250d9e421d0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:14:18 +0000

    crowded 566end bang even pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
