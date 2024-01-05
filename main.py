c3d9817bc2e2dd3aaa452d69163f5daaf4326a26
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:11:50 +0000

    cook fold survive nonsense underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
