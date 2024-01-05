772f45a4df38aaa2af7111c0e16858b888c26833
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:01:16 +0000

    warn taste sure label create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
