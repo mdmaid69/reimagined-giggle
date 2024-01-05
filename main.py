6dc56e7d0a7251a841a19c296066edc40cf3bc60
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:37 +0000

    better hate cheap sell trust

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
