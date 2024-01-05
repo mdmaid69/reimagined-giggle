d8a8dfa2419d2b6a02495f2b774d2f44328bc171
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:26 +0000

    worth sink rid block before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
