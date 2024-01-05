780f6e16c62d47adba666731d1d8ecb2d814c3dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:09:11 +0000

    operation different regularly accidentally holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
