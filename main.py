4ae735ef316a2c5118d9ff276cf3ac8965173d35
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:39:25 +0000

    worst of conclusion she 568energy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
