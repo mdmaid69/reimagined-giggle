33a1a0aa9c12817ed30d2b3471083389d0caf9bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:02:30 +0000

    modern meanwhile reasonable Rr fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(a, b):
-        return a + b
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
