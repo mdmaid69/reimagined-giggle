ddc6d6b627121d8fce9da3b676c5b98468619ec2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:32:33 +0000

    control urgent introduce conduct cheat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
