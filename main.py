434f81d46343e8f4ea1038991f8f36d7bf402965
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:51 +0000

    bit copy common any might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
