72e2f436ed4e89a94b70b96ff06139db468dff0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:59:33 +0000

    proof used nigga bin skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
