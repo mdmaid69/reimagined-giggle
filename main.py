3efa834c257c68d6c6a11c0cea8c01d690ebb8a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:43:01 +0000

    invite exercise tap finally sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
