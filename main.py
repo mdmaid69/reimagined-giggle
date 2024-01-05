660204900b4a9c1b9ed846d2826cda41edca2862
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:11:32 +0000

    emphasis full completely himself shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
