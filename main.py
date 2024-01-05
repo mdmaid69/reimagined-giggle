31c0555349ae3e44febfc2f6564a3eccad6bd48f
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:45:50 +0000

    approval introduce change demonstrate chin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
