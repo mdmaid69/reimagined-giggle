16a7a68ffdf4b716c886502dd884eb57cbd08445
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:03:05 +0000

    goon keepfrom partner party spray

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
