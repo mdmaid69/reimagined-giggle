e343312e82b825a1a1542945e42e0f7156a8a7ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:32:37 +0000

    warm bath pull speed disapprove

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def sort_list(lst):
+        return sorted(lst)
