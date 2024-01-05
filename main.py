3a7a1d1015da6307f59ed4610d2b9cd06f2dacdc
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:47:59 +0000

    in near hurt tip Cc

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
