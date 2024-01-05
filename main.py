77511f6a75ff14a09801573cab2a078559fe17f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:03:26 +0000

    university hollow difficulty walk sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+name = "Python"
+print("Hello,", name)
