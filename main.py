56fae351fa90889e647f1d0a6d54593f883d338d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:27:20 +0000

    comfort city drop near division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
