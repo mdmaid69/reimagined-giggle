138ac436db1fa124f490d9341b06fc7ebcd4c77b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:41:36 +0000

    threaten stage agreement Rr bored

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
