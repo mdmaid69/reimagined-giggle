e7391f8593b55a1db21bcff2aae10f7e0e70f34b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:59:41 +0000

    bet instrument report quick complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
