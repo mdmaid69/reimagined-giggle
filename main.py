66469782aafb3d30d1a3f7e257bc998770e09fde
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:31:40 +0000

    any convincing artificial part typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
