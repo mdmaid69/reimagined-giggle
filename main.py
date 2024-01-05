eb7a230d9ce4e9f4fa09b325274a9041fa2b47b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:11 +0000

    thinkover appeal drink advice shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
