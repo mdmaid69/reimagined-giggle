4b40ef5278864c856829b175cee662e4721fefde
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:15:49 +0000

    change city lunch suckme stage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
