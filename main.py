ac34c7f36f07e3177823512940b1e335362afe99
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:17:44 +0000

    thanks cook punish but knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def find_min(lst):
+        return min(lst)
