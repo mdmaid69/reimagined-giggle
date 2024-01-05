a273d2fd7bfe7a22d01ffcf7f8d887ea9176d488
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:48:05 +0000

    squeeze form correct light he

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
