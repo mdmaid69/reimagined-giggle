06cc62ff9a744b71d6ef0d56c08fe2fb24ec5026
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:32:03 +0000

    opportunity can strange off chin

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_list(array):
+        return list(array)
+import random
+def roll_die():
+        return random.randint(1, 6)
