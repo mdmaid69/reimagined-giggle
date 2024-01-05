31e29d798a7650717c94426b8a0633677f889ce4
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:20:11 +0000

    close edge discover design connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def is_even(n):
-        return n % 2 == 0
+def set_array_item(array, i, item):
+        array[i] = item
