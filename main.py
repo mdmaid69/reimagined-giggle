e2de1bd9e6b6b9e1f5d76f035b1bacab18abefb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:22:18 +0000

    trial contact runover but finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
