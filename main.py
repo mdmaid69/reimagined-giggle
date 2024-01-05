652bb008558b7a5aac6b9485495d12f065ef9456
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:28 +0000

    bringup takeout 565end opposite wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item(array, i):
+        return array[i]
