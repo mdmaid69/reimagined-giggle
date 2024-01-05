6467ab7b7e046f57f28970f6433017c87f7fb14b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:52:47 +0000

    onto expect trap feed order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
