e7d1c08d7d38dd52e7d8d21bee2b18fe5e0e74b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:55:17 +0000

    direction goback(to) combination excuse turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
