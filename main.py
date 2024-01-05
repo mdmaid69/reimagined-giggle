10f25cb2b7ca684732cf6922019b95777d96577a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:36:37 +0000

    change stare public over remain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def find_max(numbers):
+        return max(numbers)
+import os
+print(os.getcwd())
