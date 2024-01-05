b867e26009b497d9b17fb803fa652e8327f3d13a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:52 +0000

    delicate leg threaten typical bother

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(sum(range(10)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
