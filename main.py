b85ae08f7c77662b72515fffb2b2978b71794686
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:43:48 +0000

    threaten thorough sorry combine disappear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def convert_to_binary(n):
-        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_area_circle(r):
+        return 3.14 * r**2
