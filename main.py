5ddd3a399413435d4454f623c5ae41bccc3a981a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:32:07 +0000

    thankyou recently knock cross thumb

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def convert_to_octal(n):
+        return oct(n)
