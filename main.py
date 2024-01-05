8f3c01b499ad29e7124c87255e20e3ebe8ddab88
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:40 +0000

    finish file century between account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
