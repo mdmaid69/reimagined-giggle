9e89aa423fca6f3de3fd71ce02a5b33d1639b115
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:26:52 +0000

    colour quick road explanation consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
