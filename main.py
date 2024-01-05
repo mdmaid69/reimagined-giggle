48c0dc516a87c65d0d7b2d12ed479a2cc9f00ad6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:21:52 +0000

    bad successful rich shoulder knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
