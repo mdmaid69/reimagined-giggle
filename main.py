3e65ebedbb762942aa91efdd1a8c82e7ddcef9b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:14:39 +0000

    allow prove anything peace damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import time
+print(time.time())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
