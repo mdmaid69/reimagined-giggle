15866ffdd5350d9272fc107d4e1fe3c41834cfe5
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:24:01 +0000

    vision divide sight somebody collect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
