c932ec32a9de17c52f53f56359025b7f82f7ec42
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:43:21 +0000

    abroad challenge quickly judge would

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_power(work, time):
-        return work / time
+  def remove_duplicates(lst):
+        return list(set(lst))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
