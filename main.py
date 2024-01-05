426c86c0ab420bbe14e00429779db2fb9c38af2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:13:17 +0000

    mention slide visit aware load

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
