0c21db9a0abb9f19309768de7618cc16b564331d
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:39:44 +0000

    heavy back surprised church fight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def cube_number(x):
-        return x**3
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
