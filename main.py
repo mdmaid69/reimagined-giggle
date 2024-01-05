31bc251ddbfefe75d39d3c2af774df8e2b8698a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:24:26 +0000

    mad be arm dance flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
