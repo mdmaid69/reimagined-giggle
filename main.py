ffa73ad61a151b6e8a22edc85924fc5985f3240c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:51:08 +0000

    concerned different available nor holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
