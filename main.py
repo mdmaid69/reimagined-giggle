0fc38d631007e5d0bb88ec8ac0e4187addc21a3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:02:21 +0000

    just clever bottom congratulate right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
