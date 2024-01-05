434107bb113862958b7a34fb724afdbfbcc1edf1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:37:21 +0000

    development mad fault their create

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
