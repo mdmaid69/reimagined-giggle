31550901c12192e46a7a16f1e5f3d1c8b3349f72
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:42 +0000

    send worst that meet after

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
