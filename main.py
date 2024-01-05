f412ddf2a80d10c688ce61f20850ab743e97617b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:50:30 +0000

    apart relation smart disturb change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def subtract_numbers(x, y):
-        return x - y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
