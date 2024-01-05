efdcc07f477324c02aa01a47479e8ef3533a5d30
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:11 +0000

    means complain shop feel meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
