9bc4a194e75e6661a02710ef126906ac84a6eaa8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:52:52 +0000

    illness boat sort bend guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
