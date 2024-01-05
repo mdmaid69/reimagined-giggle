42d3db7dfddb4b474ab9a9fa4bcf59adbbb6e996
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:54 +0000

    post pull switch calmly possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import collections
+def create_stack():
+        return collections.deque()
