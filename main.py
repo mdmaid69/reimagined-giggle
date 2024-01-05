367a56077964aeb5e9bc48871a9370cbeb15bb7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:43:44 +0000

    aim mistake sweep pinch remark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
