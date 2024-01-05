7f290a5283649c0b7568c1dc61012933ce6a83ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:40:02 +0000

    quit within guess delicate knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def change_working_directory(path):
-        os.chdir(path)
-def reverse_list(lst):
-        return lst[::-1]
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
