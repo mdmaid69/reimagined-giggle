a117efcc61c9fc05ecaa2ae3baf10d37d7812d40
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:51:24 +0000

    wave getout before film common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
