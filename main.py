616f287fc7c4a62d738e67d51dd17f6d453934a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:14:41 +0000

    fire official listen possibly someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
