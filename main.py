7bb2bef22e8190db095c5f4ac64e7a690f0c7b84
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:25:52 +0000

    whom no right expensive necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def divide_numbers(x, y):
+        return x / y
