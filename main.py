097ba9f30121a48dac40c0d7003b0efd3e0475e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:32 +0000

    bad turnout perfect muscle season

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_bool(array):
+        return bool(array)
