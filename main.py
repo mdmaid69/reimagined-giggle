94b846b867e7e9700ed7fe6b21f69fa70fa54591
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:42:40 +0000

    frightened door ride uponon over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
