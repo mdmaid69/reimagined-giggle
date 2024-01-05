ce9f302bb3e0a52d692c42adb206294deb472dd0
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:57:06 +0000

    hand wear mistake want well

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
