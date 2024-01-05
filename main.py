fcc5d98620e28b5058179358465140642be60d7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:01:42 +0000

    unusual ring move comfort finally

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+print(sum(range(10)))
+def calculate_work(force, distance):
+        return force * distance
