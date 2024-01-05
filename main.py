727f8de80a175ac82234dd1c44ad9463b378a93f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:01:54 +0000

    lookback(on) difficulty wash eager also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
