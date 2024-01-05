c993839e4a0aac47c536f677af0b9bab72e7bb9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:02:07 +0000

    gentle exciting hope straight everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def reverse_array(array):
+        array.reverse()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
