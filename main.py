5090d65e7e7245b8dff0296bb4d775e61030c126
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:03:54 +0000

    apparently off fall above market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
