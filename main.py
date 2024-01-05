51a2decd8344c092070ebdc2efae2321bc024eae
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:21:39 +0000

    lower game die best fire

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_max(lst):
+        return max(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
