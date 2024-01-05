fd8485ec638b396128d8b189ef73cc2891dc7175
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:17:51 +0000

    another lose sticky character breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import numpy as np
-print(np.array([1, 2, 3]))
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
