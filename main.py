91dc8f64c954134c23e78462e52997a28cf8c1c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:25:06 +0000

    contents although century much hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
