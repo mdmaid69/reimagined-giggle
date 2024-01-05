83f574b21c7650ac2404f013307d39bfbafac9ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:34:01 +0000

    will raise gift you ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
