2c7e61772e130b0764ef00aa4883e0190b8bb5c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:27:50 +0000

    surface patience wind minority sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_volume(length, width, height):
+        return length * width * height
