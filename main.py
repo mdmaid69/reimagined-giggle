03ea693b88c2e50f482d33ee0c212bd140cb9a1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:46:37 +0000

    equipment noise also show action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
