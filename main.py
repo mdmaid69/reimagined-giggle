dba4d1946bd8a0c6d47e8b76c94ba17bd40ad2b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:47:21 +0000

    interesting glad whichever cool deliberate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def multiply_numbers(x, y):
-        return x * y
+print([x**2 for x in range(10)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
