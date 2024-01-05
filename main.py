e3e8fcc7db789e7dacc42ba9341ea3ef28b70352
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:17:30 +0000

    row murder tape bridge he

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
