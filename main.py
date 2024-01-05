af97dcfde12b66459817335ec374ea7bef061791
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:12:22 +0000

    wish afraid calloff where pause

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sys
-print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
