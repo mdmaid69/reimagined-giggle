09851e81156db23fbdbf3c7f46a4197f457b0633
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:03:13 +0000

    meaning personal strange thorough pocket

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
