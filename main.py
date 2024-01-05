bb447d312e5163bb000869934b5724939e297987
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:23:28 +0000

    judge contain correct interfere operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+x = 10
+y = 20
+print("Sum:", x + y)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
