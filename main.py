fdcb041a750abf1bf2b33ea34540b5a13ea91ccb
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:50:46 +0000

    politics how several dry winter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
