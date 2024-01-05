59408eabec2f789abe5b2ff1f51439fe011348f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:48:48 +0000

    burn count temperature ourselves weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def count_elements(lst):
+        return len(lst)
