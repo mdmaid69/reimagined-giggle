6aa19e06098473e97c81011b32447805b881d7b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:38:01 +0000

    Hh wise use thick comment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 def calculate_volume(length, width, height):
         return length * width * height
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_error_function(x):
+        return math.erf(x)
