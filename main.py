7aececb12210fdc0d6fe76c671de6dfba3aba2e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:51:42 +0000

    active gradually will gap union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
