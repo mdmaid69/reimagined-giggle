16af341ed3918143646b712a702dfc45befbff17
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:33:47 +0000

    because copy many ready constantly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
