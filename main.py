1efae6bd6cc9a3d252003b90507de80ebf1ad081
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:49:38 +0000

    fall after bit consult chin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_pressure(force, area):
+        return force / area
