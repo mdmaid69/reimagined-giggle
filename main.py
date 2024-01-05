e7c51fb82fbcb78d8b3e4db9e15fb6110dae29da
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:36:02 +0000

    kind dead deaf strange himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-print(os.getcwd())
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def is_odd(n):
+        return n % 2 != 0
