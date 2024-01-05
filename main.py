c953cc0600125b25b75b7ae3ac079d90d4e5bf37
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:38 +0000

    whistle slight cover refer available

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_length(array):
+        return len(array)
