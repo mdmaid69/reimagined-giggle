103267c286ae2346688d788117860f5cf339e898
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:45:23 +0000

    impressive tune lead receive practice

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+print(math.pi)
