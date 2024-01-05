9383b2e545744b12c9ba7f9d90eaa5f6b948ddf8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:24:10 +0000

    base quick juice delight hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
