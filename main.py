f91b8132c39675ffec8fbdb31b8068609e9dd406
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:24:46 +0000

    whichever home why record cry

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import sys
+  def get_python_version():
+        return sys.version
