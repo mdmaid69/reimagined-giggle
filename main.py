815be564b6b29e8f2f71eda0ad04b26838d688bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:52:19 +0000

    justice group neither thankyou certain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
