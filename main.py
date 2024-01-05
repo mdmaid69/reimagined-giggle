05a8ad5f7f4f8d863ebc1108897ce8bfa65ad307
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:50:21 +0000

    rain union makeup circumstance please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
