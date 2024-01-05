4974ada3ae309f0aeb0eea07c40f8ac0fe259a10
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:22:24 +0000

    reduction jump attract instant intend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
