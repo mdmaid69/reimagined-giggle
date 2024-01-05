e2f226ba75f4acc42ab85fa34a96de3f109747fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:57:18 +0000

    interview obtain recognize serve bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
