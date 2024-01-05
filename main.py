b8a3d2b43b315e3037edf19a93c69a5a1b17e666
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:33:05 +0000

    quality report fork even female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
