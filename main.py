65bed3cb6e65a8e2775d0bc3ba0bea78d51c38ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:17:49 +0000

    tidy apparently likely commit convincing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
