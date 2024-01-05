34899621a5336618cb9f192a9281753e5a5845f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:42 +0000

    thinkof meanwhile want trust week

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
