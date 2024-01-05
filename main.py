000ae5c65a3b4afd265a61abbd47b34a5bf345f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:06:02 +0000

    survive interview demonstrate character exam

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+print(sys.version)
