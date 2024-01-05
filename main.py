2f9e8e8199aa17bf245801cb0badfd40485770bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:20:32 +0000

    reasonable wave sink group getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def add_numbers(x, y):
-        return x + y
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
