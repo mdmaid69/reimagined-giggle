64b997959fc7786780ce85e964ddc6c1c369972a
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:01:13 +0000

    comeround handle have target long

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+print("Hello, world!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
