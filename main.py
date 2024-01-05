28f9a0868844d103933710ab220c3b0cd9a41ad2
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:47 +0000

    race total often remove theirs

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
