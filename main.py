2e523e2ff6b1d1aaea39a3ce070a7b23afb4d8f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:58:39 +0000

    scrap medium so repeat stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
