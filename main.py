c686f6238a57cd205d23b0236ac9d4946596d1e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:30:44 +0000

    voice left Ii settle realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+x = 10
+y = 20
+print("Sum:", x + y)
