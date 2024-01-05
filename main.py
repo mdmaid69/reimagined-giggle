b3b3bafe93a9623c67516ca3277ce4089df28e35
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:06:21 +0000

    father touch save under future

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
