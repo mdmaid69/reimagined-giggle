edfe6a45be13b39f3584fd0c1cd9e96611c02361
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:20:44 +0000

    honest congratulations them unit lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
