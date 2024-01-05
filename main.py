cdf0dee7680f7b263ad29c14e4e314765a875598
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:34:09 +0000

    original culture shopping busy rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
