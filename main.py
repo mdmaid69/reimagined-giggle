6c6b4a5df9fb0f0b01695bfa9b7f53a5ac152a9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:35 +0000

    keen suck getoff obtain several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def square_number(x):
+        return x**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
