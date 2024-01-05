18f0af24203e0cbebe50afe0405b7b6f19c44950
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:06:27 +0000

    finish male train will inquire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
