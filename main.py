4ede44fe6b88d726aca778ee6db713bd99871e18
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:04:47 +0000

    undo loose tap platform sight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
