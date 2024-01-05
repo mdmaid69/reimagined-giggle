5a20f5b80d00d5786e84de3549b119ae37dbd9c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:28:52 +0000

    sound front please sing out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
