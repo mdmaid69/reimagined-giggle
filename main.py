f8fa0d8193ae3ea774ca04d42f7612e0d28ad42c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:12 +0000

    quit classroom black hope tired

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def is_palindrome(s):
-        return s == s[::-1]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
