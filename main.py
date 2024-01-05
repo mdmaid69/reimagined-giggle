4342af2ef38ff8a31d8931eb2b4bdffcbd176279
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:14:55 +0000

    himself front free opponent familiar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_list_from_array(array):
+        return array.tolist()
