84e79fd8268f740c8bd522a319f4da44cc9e26bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:52:25 +0000

    off past circle middle survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
