fc84a97cb824eb3bad6386a27507ac7fac1a41f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:13 +0000

    tired spin effort impressed blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
