b804205726bfd8a72d7df370f8c14dfde9f74ca5
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:36:47 +0000

    concentration desperate time die thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
