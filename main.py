10121e1f1d001ee63343da93ae5222e3df38f0f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:33:44 +0000

    extremely difficult accordingto please better

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import getpass
+def get_username():
+        return getpass.getuser()
