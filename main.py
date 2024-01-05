e9d0c7f921cb26b7bcefe40c14adeded725d8fe2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:03:29 +0000

    dick school government admit soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
