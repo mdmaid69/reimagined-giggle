0aa9a45c0eed9e6cf7ade339e4bcbafc897aedaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:04:56 +0000

    lock push coat difficulty risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def is_even(n):
+        return n % 2 == 0
