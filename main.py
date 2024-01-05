82bbc5ee12764f6311b14aa25ec5b5ba3edb1229
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:07:29 +0000

    declare drawing scale large mother

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import platform
+def get_os_info():
+        return platform.uname()
