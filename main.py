1415e7ecc3f21620ac4f801febeb1c24db9c3894
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:32 +0000

    hello period song nothing switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
