3ddbd32d4d1b8725a554ec7f946cb4d18830502a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:30:44 +0000

    dance score mail mother final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
