ba731ca74764b11d4e3ae4dd545e5704939087fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:19 +0000

    fat before slip profit emergency

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
