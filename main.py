dc89a8142d10371f8765b548287a85fd7eef789a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:52:18 +0000

    without circumstance flow fairly nonsense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
