40ee6cd00c204925d4f7468524bc8c6ee8f461a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:06:51 +0000

    ashamed temper actually road agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
