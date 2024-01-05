a524dbeeb482f30948d3007f3e752f92e581a301
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:31:32 +0000

    sound the damage so temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
