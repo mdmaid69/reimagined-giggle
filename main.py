bdd6bb53afe8aba2d19e4b0a35ff9cc2700b4974
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:03:10 +0000

    gradually love total cool fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_exponential(x):
+        return math.exp(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
