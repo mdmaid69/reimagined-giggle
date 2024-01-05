1ab44958d1ad66f48b55e83af10184ca54d61f14
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:20:33 +0000

    authority warning beyond intend inside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
