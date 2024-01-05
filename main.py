6fadb0f1c21c05306b9108a32a8488c58a99e15c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:34:25 +0000

    hardly law illness press prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
