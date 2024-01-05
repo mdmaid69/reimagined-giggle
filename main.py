9fc692bc678e2255bc660d3581c66159ac1e992f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:02:23 +0000

    interested quality blame justice score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_area_rectangle(l, w):
+        return l * w
