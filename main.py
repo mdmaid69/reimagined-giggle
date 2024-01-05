a05ed8a0fe5e49e614ff75526617342ec0e48aaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:06 +0000

    beneath walk tap else spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-for i in range(5):
-        print(i)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import sys
+def exit_program():
+        sys.exit()
