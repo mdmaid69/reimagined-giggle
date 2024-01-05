8eefb57ce18b0cc6ea3c16d41c6f1691fe938ed6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:20:38 +0000

    holdup turnup flat across love

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_to_binary(n):
+        return bin(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
