6dc974825ce52638e52518b56c33df4ccf12f6ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:36:04 +0000

    fill save breakinto flow border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
