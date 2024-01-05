46ad0d8128307e116c00b0cc516203ad5e728786
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:01:04 +0000

    character somewhere before many punish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
