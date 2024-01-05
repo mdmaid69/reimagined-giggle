6c6ba6b76c9ac9631635ca3cf9fb94c5c87188e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:28:39 +0000

    criticize break bringforward distant satisfied

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
