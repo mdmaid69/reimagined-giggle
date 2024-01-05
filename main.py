933b7318de833f513264be2ada74a4d350a43b05
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:24:13 +0000

    feeling identify proud failure greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
