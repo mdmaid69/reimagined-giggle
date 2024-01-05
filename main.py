b5ae38c8a6cf5a3a1b68a017eb6530f9d8d844af
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:48:42 +0000

    share sorry determined answer light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import os
-print(os.getcwd())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
