97f3e8d8c08a5d2fd5c02ef7b5b091c651863b4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:43:41 +0000

    answer choice pour passage back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
