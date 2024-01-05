aeb0fcdd9f91ba97b37726bbb593fa457746825f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:46:54 +0000

    run mend tax spare only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
