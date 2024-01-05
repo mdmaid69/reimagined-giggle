674a94f66d34e26109a43d32a036e035482ed1be
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:44:58 +0000

    blind announce guide spring lean

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def divide_numbers(x, y):
-        return x / y
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_str(array):
+        return str(array)
