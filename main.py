a2117ac3d0434b1e27f48a7559bfd14c6ca7d6da
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:26:38 +0000

    age whenever neither chief trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
