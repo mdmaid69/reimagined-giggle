6ece240987fc50949562585c3e025c7dfed1e6f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:54:32 +0000

    fit switch once positive waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
