43e06051d8c5e4284eb943f6c6879593885d9002
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:06:01 +0000

    Bb recover listenfor prevent measure

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
