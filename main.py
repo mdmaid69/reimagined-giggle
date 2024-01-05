ecda76e8c21dd8e4a88408b2304013745afaa1c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:37:08 +0000

    front trade heavy straight hold

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_float(array):
+        return float(array[0])
