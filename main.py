521a874fb6c6b12aadc61083fe000a86996f096d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:48:54 +0000

    change needle change far he

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
