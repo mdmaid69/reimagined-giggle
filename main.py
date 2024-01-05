e15dbc0a40421e2820b666501ec13cdc0e8b0f6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:59:21 +0000

    threaten only comedown difficulty brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import itertools
 def flatten(iterable):
         return list(itertools.chain.from_iterable(iterable))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
