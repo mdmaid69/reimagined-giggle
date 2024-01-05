280b2362d532ae3e7b263ae52a996760e79f398a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:02:15 +0000

    street shut towards slice entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
