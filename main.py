10dcd05565aa8f8de16292fee86aee767383a958
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:26:29 +0000

    party kind murder count mouth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
