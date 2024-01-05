6e0dbccdaec1909bdf2a73e55a0ab23f2338b13f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:01:34 +0000

    independent slow minimum oil collection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
