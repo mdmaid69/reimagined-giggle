389e1a13506cc1d3a07ef5ef419ca6d652d01f9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:25 +0000

    small owe murder rub always

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
