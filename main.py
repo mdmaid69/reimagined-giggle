1e1cdd1796f8040fb4db94ec02943e2452a0b0c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:01:49 +0000

    check spread return even calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
