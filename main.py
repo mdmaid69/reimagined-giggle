c07976448770c3debfb5f299f7d63e4eb6f22ee1
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:18:42 +0000

    refer pupil slow able police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
