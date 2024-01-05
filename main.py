ad00615f8be1ceac70ad4ea27df60f65e625a62a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:26:02 +0000

    butter let either world floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
