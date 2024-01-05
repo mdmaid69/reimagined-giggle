9a11c43bf633c14bffa440239348ae40e197d422
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:58:03 +0000

    full before stretch sister shelter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def reverse_string(s):
-        return s[::-1]
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
