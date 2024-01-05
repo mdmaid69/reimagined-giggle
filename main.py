e9418afca7186b9c3b217f9c5ecc7d0f5477bb6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:13:58 +0000

    worry behave equal summer belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
