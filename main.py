a5313b745ee18ac00a0086f1eaab54c9cb2dc5e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:27 +0000

    Yy again beautiful follow worse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
