17cd93222b4b9b3b4b4ad627cab808614412f3fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:21:01 +0000

    should expect cause abroad custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
