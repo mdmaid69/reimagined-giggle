4e7a914fe86105e2c03b124568c08de29e9f99de
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:51 +0000

    ill regard entrance choice best

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+name = "Python"
+print("Hello,", name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
