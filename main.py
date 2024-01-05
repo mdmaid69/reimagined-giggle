dd6853345f493df4b80c8f389d58327b2fbb4362
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:01:32 +0000

    funny getout Tt intelligent scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
