764ca7097afa8390f045fdcc65599c061d7e4234
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:17:32 +0000

    attract spoil nor previous switch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
