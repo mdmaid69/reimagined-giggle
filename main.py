782cbff03cbaa7fa40e6205ef9afb577262645d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:00:56 +0000

    middle turninto drive tooth just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
