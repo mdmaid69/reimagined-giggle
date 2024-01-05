6171c28343fac41307cd436b48693f42105d2fee
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:31:54 +0000

    weigh legal usually really full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
