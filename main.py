d41f28c967812680e34592f3f1b4aa068b010d59
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:20:40 +0000

    OKorokay on expression several fight

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
