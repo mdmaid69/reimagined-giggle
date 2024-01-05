e5b94c4ee47f1ec3ba34b61bd1193814b8e4c768
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:57:45 +0000

    strong disappointed error introduce above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def multiply_numbers(x, y):
-        return x * y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
