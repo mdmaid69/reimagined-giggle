2ecb8885f161bb9e756053bf999d5d3f46b05efc
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:07 +0000

    towards by kill fast spring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
