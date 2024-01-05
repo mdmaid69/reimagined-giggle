14299d3f9f6b8c450a6ce8a6df32f0225f9ee327
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:38:30 +0000

    language cold character human spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
