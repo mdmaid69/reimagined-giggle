c4e6d7e8c3c64c0c73413ee0f281ef9573f0c3f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:45 +0000

    justice cause value fix herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
