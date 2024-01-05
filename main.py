0887c61da814843c3ab02cc7294d3cf0c16fcd20
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:02 +0000

    bother fit sometimes lead train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
