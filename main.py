6124e2455009c1b5389fae852ff67ad48e45b632
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:06:31 +0000

    drive loss compete decide appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
