819e66b63278c2b38cb825fa835109e0b381b419
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:57:28 +0000

    height platform injury meaning turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-print(datetime.datetime.now())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
