036ecce0910711b036808944d0433c3ae0d400bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:44:39 +0000

    would hole its film picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
