8d6f2487fc91676babbc2971be15bee19e2795d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:27:10 +0000

    story careful straight burn lookaround

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
