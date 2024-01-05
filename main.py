2b4fa742ed8d76c0854231cf623e3b75ca604059
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:00:39 +0000

    radio traffic state recommend minute

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def remove_duplicates(lst):
+        return list(set(lst))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
