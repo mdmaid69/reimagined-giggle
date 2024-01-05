51709883be5f3aba261f9fab795da1cdd7e54ad2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:48 +0000

    introduction front section stir slice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
