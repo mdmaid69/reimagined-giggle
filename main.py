2d8a6522a8ebb4996c4f68524d2e909bd6ee8fa9
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:29:15 +0000

    gather politics care sell line

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def read_csv_file(filename):
+import json
+def load_json(filename):
         with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+        return json.load(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
