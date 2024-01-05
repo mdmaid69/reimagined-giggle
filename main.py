b65795627cc23cdb88e1e00ab2f718d6438695eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:32:17 +0000

    discuss most at question top

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_stack():
-        return collections.deque()
-from collections import Counter
-print(Counter("hello world"))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
