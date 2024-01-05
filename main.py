2dfc293657675fb2b1b1bcb67ab44f0c5efa654d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:49:56 +0000

    average boil yourself splash violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_itemsize(array):
+        return array.itemsize
