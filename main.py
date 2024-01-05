7f5fa8ac76a55146c29b8e23fef391d90f456720
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:59 +0000

    maximum level study appeal pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
