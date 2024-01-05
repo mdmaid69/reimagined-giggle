058ce211f8257aad46d36ec059524b8d338d4740
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:39:08 +0000

    callout test feeling even limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
