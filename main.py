e0bb49a4277a10d8873f59d16a7d28e2aaa2b92a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:50:31 +0000

    both herself on speak wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
