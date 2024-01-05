498fb0e042e79845db3b81014558a0f015078f0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:46:31 +0000

    sad enthusiastic convince lay gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
