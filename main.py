b04bae9388eaaed38167008a7b2ff495648a8e2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:27:22 +0000

    teach fault hospital style comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
