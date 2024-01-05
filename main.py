4958f893a75a10f564d2260ae4e9f9a4e10f6fbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:57:49 +0000

    project runinto bunch several tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
