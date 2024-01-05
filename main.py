8241c744a4f11f9c7685643f9387f6ea86f7a934
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:19:24 +0000

    tonight feature smooth hate thin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
