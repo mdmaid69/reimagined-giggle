aeafc10dcedf600c27def3a0d818c220122a455f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:45:06 +0000

    travel promise continue plastic world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
