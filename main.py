6a00065fb8ab1f1a9e79eb91fec591a7ea78ecad
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:23:34 +0000

    temper chin instead search adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
