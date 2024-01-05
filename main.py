46734bbc333a546a93f0774498cc3a037abe4609
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:47:50 +0000

    colour discovery none increase my

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
