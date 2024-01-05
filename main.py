6b70afac368b508a7fe35955e6b75b15297e4333
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:39:26 +0000

    blame doctor square communication season

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
