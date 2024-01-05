8b84575b138d4a7c780b5ed753716e6d45b32973
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:51 +0000

    splash bringdown plenty afternoon straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
