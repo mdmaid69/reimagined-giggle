37bb3b3ba60a86eca070bc69f86264f7bdf96007
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:39 +0000

    subject positive such various must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
