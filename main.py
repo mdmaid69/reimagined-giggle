64f08a93e4aec82f73108b7213cf3482dbeed497
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:27:03 +0000

    because difficulty phone project yesterday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
