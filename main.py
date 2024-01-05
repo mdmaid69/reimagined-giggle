ffac746cd150ef11643360d68466087d53dda516
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:45:36 +0000

    yesterday suit uncomfortable community introduction

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
