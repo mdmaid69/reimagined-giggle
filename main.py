aebcde0d50ef3f8698d58e7fffca3e6cdc2f0351
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:35:47 +0000

    Rr mysterious turnup uncertain search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
