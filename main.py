b64e9a868c7b807c18675c4991668657962a8410
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:01:27 +0000

    responsible what issue model mail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
