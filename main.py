b87e5eaf8d8637c8f80dfbad12b6abce9157419d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:00:30 +0000

    embarrassed themselves reason fortunate backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
