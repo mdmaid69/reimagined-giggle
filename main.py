e0ad5c0de507ef114adf0e2c07e83a9bf7f4361c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:20:37 +0000

    damp trouble why dream deaf

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import json
+print(json.dumps({"name": "John", "age": 30}))
