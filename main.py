09de10817a33f1d4d4f77cedc6e5ca61d61ed123
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:39:14 +0000

    swimming advise mouth bus question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_density(mass, volume):
+        return mass / volume
