558e8797a893303aa04aabd5319770ddb85e4592
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:36:01 +0000

    immediately holiday gas meanwhile complete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+from collections import Counter
+print(Counter("hello world"))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
