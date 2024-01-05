5a1cbc775f5e70a6483f065c7b7118e4bcfab996
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:46:17 +0000

    evidence gooff able free glass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
