9397072b9dc3e21e3cf22a7275a004094a7986f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:27:55 +0000

    unable attention sharp whatever hate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
