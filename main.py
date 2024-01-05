262810dec04f91e076f8e8b9e18f0d3aa21781ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:31 +0000

    law sight why violence special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 def get_file_modification_time(filename):
         return os.path.getmtime(filename)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
