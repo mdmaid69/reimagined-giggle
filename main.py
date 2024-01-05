612ce014c2dc6f82bbe48b44e5eca5c72d4a7208
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:25:04 +0000

    write air note compare uncertain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
