345f50bc2f42dcc22a340cb454af9f61090f6942
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:18:36 +0000

    by apply catch holiday month

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
