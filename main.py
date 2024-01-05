fd6d7942893f22ef605d5e7a53073964edcea630
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:29:08 +0000

    hospital land floor stone uponon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
