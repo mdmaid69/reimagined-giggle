3933f0adc4dfe07929a3834ca53e54d77ca1456f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:40:16 +0000

    comfortable think still prevent her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
