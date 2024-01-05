7ffc6272ca953e34beeb4b1355138aeb845317a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:26:53 +0000

    bush breathe open emotion aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_queue():
+        return collections.deque()
