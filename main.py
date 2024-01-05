7e213c55b8b20ac993658c03e730dc433d35f507
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:37:32 +0000

    slip protest one pay otherwise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
