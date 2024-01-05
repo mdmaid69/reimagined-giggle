afcf4a6e901cdfc59f29f5dc31d4c5f515c2faf2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:34:36 +0000

    doctor contain date doubt smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
