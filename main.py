f314d8030cc5987daaa9bff74986f207f0b98297
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:12:58 +0000

    brief extraordinary clothes press reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
