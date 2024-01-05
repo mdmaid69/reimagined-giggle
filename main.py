2708d066ef6b7b006513f139f2f17dd588de7c07
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:10:05 +0000

    sudden 179bite bottom create stare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def is_even(n):
+        return n % 2 == 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
