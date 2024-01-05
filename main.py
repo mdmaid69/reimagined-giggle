842bb092354d9777479eb4fbb401f7d28a0bda1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:02:10 +0000

    collection estimate tell popular either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
