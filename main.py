91740a9ee168cc8b22eb4b5d2aed5a96940f726b
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:39:14 +0000

    thankyou art emergency name we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
