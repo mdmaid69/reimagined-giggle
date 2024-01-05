82349fcdd873266eb728b54c4b3bb5c4c3ce78ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:06:52 +0000

    taste declaration walk note related

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
