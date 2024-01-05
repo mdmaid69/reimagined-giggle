059a4d66b0549c13a7c92720bd7545c0a40ae91d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:18:38 +0000

    throw ours interview miss that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def cube_number(x):
-        return x**3
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_exponential(x):
+        return math.exp(x)
