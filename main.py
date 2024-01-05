9b08f95e88cb5bb0cf17b96d6f1885098898eb61
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:45:13 +0000

    difficulty describe broad tour concentrate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
