0d033f23757cf6f3fb9a2e22f81ac8af9f076e41
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:34:19 +0000

    worry boil broken immediate bush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
