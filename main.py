4633ed8e167229de8aafc656c4438cf8701df508
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:18:25 +0000

    work possibly injury loud include

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
