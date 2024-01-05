44dbb39d28387aa05828c6151e56f721146be1a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:54 +0000

    occasion invite workon usually confusing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
