63a6368e0ecc4d58f7884b76d6ae0820fa3b1c42
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:19:11 +0000

    clue connection union slight opportunity

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def count_elements(lst):
+        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
