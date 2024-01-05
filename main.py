2ecb9f7a944d28dc82c14fcbc38e9a4b3a238f49
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:30:39 +0000

    even determined file takeoff again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
