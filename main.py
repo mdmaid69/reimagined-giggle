86dfb1d3673886860996cffe3a0356709fbfe8fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:09:31 +0000

    meaning nice custom shout hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
