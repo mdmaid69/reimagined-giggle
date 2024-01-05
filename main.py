4f3943758f5547127b26a843f6ebbaeed7e327af
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:54:09 +0000

    prepare desperate confident closely prize

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
