12b4516974f2a5c3a0dd55bb4093656a8384e012
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:38:39 +0000

    no matter roll through pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
