213f5906bfc1ecd7d73774e2b947c5cc939f03da
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:47:57 +0000

    satisfactory interruption answer shout television

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
