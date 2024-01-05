8485d9b028f38d4e0bfb9296473a1a4fcb4a896c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:11 +0000

    improve confused soft hollow brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
