1a95d593065d130268105e6f3fec7b6516aebf0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:39:37 +0000

    repeat entry excuse fair begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
