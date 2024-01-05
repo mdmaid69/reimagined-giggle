3b6943dba146a08fd5d0543f846b0f854fc75af8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:17 +0000

    pencil Dd material runinto bored

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
