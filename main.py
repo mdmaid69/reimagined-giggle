44342040c8fd72207dd30fb14711c45ec667b3b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:37:52 +0000

    thought speed constantly rid protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
