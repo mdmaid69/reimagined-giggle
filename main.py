3b8893e19529723c57739ffd7e2c4c5a1de23155
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:51:34 +0000

    smart butter lose awake obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_max(lst):
+        return max(lst)
