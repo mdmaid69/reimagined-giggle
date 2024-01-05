8a9984021a8b806369d9b134950a2aedaad59f7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:40:59 +0000

    house quantity combine may careless

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
