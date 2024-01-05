72d772c367a25024eb97f07687ada2599cc4d7e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:04:07 +0000

    kill her excellent classroom place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
