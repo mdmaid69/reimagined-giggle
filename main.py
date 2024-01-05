118f9f47072382fae4e1f55fabcd40d5a8ee5787
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:36:37 +0000

    serious suggestion shame go forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
