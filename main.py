301de0e6cc73d94e43670c0c5ea3949ba449cda5
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:52:23 +0000

    admit ought night and copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
