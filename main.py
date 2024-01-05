2f7a7a9f7b3e8dc08a1fe9154e4bda8911afcc62
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:26:58 +0000

    custom solve this deliberate ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
