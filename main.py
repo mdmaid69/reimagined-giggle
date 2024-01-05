092c40b78eeae9c770c146aa50e63ad238732102
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:40:54 +0000

    guilty without market owe shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import collections
+def create_user_list():
+        return collections.UserList()
