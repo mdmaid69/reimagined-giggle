2c6592c4df3bb200cb500b5fe55131a91f8a0d16
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:18 +0000

    block workupto spare careless mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
