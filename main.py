05a6c085dcaf47e3bbdb4190680ed1297131df30
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:47 +0000

    creature brief accidentally exchange stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_floor(x):
+        return math.floor(x)
