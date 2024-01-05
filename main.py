c1ccbb9d7e772a8cb51a7c285896b71904ff45ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:26:34 +0000

    bringforward spoil react language hardly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
