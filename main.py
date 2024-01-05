6720218cc270299e74af599f78dbe32f8e94dbe4
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:48:55 +0000

    then term house me keep

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
