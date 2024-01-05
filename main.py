3ed58964d63a6d3bc1a268b613d23fcdf00848a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:12:11 +0000

    bother communication stone argument size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
