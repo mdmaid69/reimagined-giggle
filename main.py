5218e291abac47dae729df010d1385d3362bb463
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:30:05 +0000

    lend further favourite stare unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_error_function(x):
+        return math.erf(x)
