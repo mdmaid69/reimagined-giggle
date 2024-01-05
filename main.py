1adb7ddfd1a57c012f83e5fa9735704db1c3451c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:17:33 +0000

    spring building increase boil few

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
