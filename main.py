1349912187fa1f04e90d9c0daa94be2f9db25f50
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:56 +0000

    middle afterwards occur kick pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
