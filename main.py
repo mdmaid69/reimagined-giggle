6b5264f7c4e9595411396fa81bc8be27df881962
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:19:15 +0000

    funny actual behave trouble direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def divide_numbers(x, y):
+        return x / y
