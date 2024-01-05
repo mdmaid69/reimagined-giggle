0086b1e1ca72300e2533cfa06d18c77240cf6eb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:15:20 +0000

    poison such wherever pattern release

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import logging
+def log_message(message):
+        logging.info(message)
