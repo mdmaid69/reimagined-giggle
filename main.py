5bd93bd3c28589a6b199e2ecad691e50633e41e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:31:35 +0000

    equal flat pencil contrast clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
