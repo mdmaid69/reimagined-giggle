718391a393dae0ddb711359f5be21d222b08a511
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:40:13 +0000

    back behind scared pot begin

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+print(sum(range(10)))
+import os
+print(os.getcwd())
