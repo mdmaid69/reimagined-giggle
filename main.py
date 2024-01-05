6dc89351b9915aa59527380d121ac841c8e587f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:12:03 +0000

    compare wise getaway pin force

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_complex(array):
+        return complex(array[0])
