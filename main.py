bdfac842043d3264734666fca1bad03949562ebd
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:25 +0000

    beginning high only collection cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
