b3a6c97d815b99bf56f88bde2bda93aa18d0c4fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:52:36 +0000

    wild serious expect surprised manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
