385a684f8e94cfb39825d8d50e81d892a690d171
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:32:05 +0000

    design passenger several port importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
