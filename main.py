a9dd6961b54d393b6b2bc29aa22598f4d3966ebf
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:53:52 +0000

    problem anywayanyhow building Uu contents

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
