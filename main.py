e9f299287b5a788d94ed63967ba90fa6d50d2c30
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:58:48 +0000

    claim emphasize refuse report refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
