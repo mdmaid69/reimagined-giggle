e646d3077325a0118504e184367313cdc5d766e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:22 +0000

    popular repair fun grateful reject

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_today_date():
+        return datetime.date.today()
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
