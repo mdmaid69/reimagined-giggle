81a0a6d534e5952793e43ca60520fe311d32baf7
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:57 +0000

    bring variety again offence event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
