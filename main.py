fe55655b88ee093222ce93d70528453ab84953c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:39:44 +0000

    tear praise takeout owe ticket

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_list(array):
+        return list(array)
