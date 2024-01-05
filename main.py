3f3bf4dae0d475275a3e0925f8d759d51c333b29
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:33:50 +0000

    voice mystery speed pollution today

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.ctime()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
