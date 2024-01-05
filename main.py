0948da78044dea32eb6fe0cb8547e9c8dc883ac5
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:57:53 +0000

    go might trial bend thumb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import platform
+def get_os_info():
+        return platform.uname()
