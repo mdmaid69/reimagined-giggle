eacbc7c1129637f0d4f2c28f47228f204b67ef4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:24 +0000

    suckme thumb country keepfrom shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
