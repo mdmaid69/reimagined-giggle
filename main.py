e4a1d8b36ff00277e9769411d23318f0b1548333
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:34:28 +0000

    sight excuse walk country wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
