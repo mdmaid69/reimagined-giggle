04f21119ed9ef3c51ebf8981e2bf1faa59f96f60
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:21:12 +0000

    direct neither apart smile mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
