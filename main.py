5a056f0f4288e2c9fedc36bbe75b3ecb045ff1fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:18:45 +0000

    benefit who inform out medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
