327904b9ee1dff4ae10906705128aba1769b3bb9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:44:55 +0000

    split government Oo long cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
