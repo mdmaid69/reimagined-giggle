25d2e559d14ccc11be945e6be96314fa7ec5d12b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:06:10 +0000

    themselves extra destroy sink confused

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
