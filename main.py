9ac1819edd9c1ca245c6184371c6162bfdf9b59c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:39:07 +0000

    rate too defeat feed circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def clear_array(array):
+        array *= 0
