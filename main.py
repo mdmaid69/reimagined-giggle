5a020c5d35551738f5c7af11f244aea6360556a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:57:50 +0000

    whistle stir constantly sign thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
