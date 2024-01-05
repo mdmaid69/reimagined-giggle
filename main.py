59c4ddf5a327419ee77b03d6181d572c3a3334f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:01:07 +0000

    immediately loss escape tear study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
