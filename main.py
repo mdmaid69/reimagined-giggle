7856516230499dd986e494d280f3e82cb5b5c953
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:04:01 +0000

    rain basic concentrate comeup hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
