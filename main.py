2d25dfc837647f7cf81ca254ba005b40010635e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:16:28 +0000

    however punish my direction left

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+print([x**2 for x in range(10)])
