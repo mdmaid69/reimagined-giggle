a72c12f95f344b6aeca9aeae845c0cdd57a9beb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:27:16 +0000

    drop bringdown cost rest design

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_floor(x):
+        return math.floor(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
