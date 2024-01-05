98cee045c29d096d29e11fd5b03226064eb3cb3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:53:57 +0000

    apologizeapologise church only teach maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
