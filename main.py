d3ee3ea0044efeb1bd785e5f489298bfa1ce8e4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:28:14 +0000

    sad opponent phone turn situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
