303331a5c76bdebcbb4628d65fe7d7beead56875
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:07:06 +0000

    Cc happen onto keepout top

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
