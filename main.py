f97e1f2eac7d6bcd2806966db879dea9f8adcbfd
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:44 +0000

    extremely under worse feature immediately

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def clear_array(array):
+        array *= 0
