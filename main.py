105a89cae40cc69d322f730675bb679de5151b32
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:37:02 +0000

    cross short where freeze design

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import time
+def get_time_since_epoch():
+        return time.time()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
