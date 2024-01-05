43d2cc1402447ea63911b1a272899528f0088fe1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:36:03 +0000

    scheme sharp later area someone

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
