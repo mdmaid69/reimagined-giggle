7feb20b8990b60263060f712e964ef23eb0c3626
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:15:49 +0000

    contact comein job moon clear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  def is_even(n):
+        return n % 2 == 0
