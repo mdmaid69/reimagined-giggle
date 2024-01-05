4c69ae2b72cfb64e990625a0e341f64b4ed5cf15
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:37:50 +0000

    experiment frame possibility giveaway collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
