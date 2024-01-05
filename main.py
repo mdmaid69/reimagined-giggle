606bb5702fb5cacc7b0afaf592328163091dae76
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:55:53 +0000

    giveout OKorokay upset claim dream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+def get_today_date():
+        return datetime.date.today()
