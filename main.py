09896a9fe6d686612cfc89db9184f74f8209ea5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:36 +0000

    address interfere emphasis before weekend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
