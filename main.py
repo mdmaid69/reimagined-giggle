368103cce162c1a029dc5e7e3d54502222269bf1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:34:19 +0000

    occur combination kind damp workupto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
