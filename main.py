ec3a9dbf9a879e26a0cefd39c889e629dcea2553
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:14:05 +0000

    ahead funny suggest explain similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import array
-def get_string_from_array(array):
-        return array.tobytes()
