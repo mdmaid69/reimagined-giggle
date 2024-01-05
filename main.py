691e1a52413ff255a3346cefc370321e3700c533
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:31:20 +0000

    whom imagine tap warn tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
