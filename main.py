1dbae107b1fc03716b3ebebbd74b9e0dad2ebb3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:04:50 +0000

    makeinto impatient leg emergency repair

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
