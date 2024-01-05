8151c0be93981d9628a6e63f0ed94d044a8284c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:52:21 +0000

    container meeting left treat club

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
