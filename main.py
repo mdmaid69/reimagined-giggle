80f1e7095bc0e58cc9b91b39220055d71dcaf984
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:42:57 +0000

    arrive contents sure should effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
