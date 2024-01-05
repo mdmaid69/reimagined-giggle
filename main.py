309790be4aa9a46299690d0ba81d0fe59c62d1f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:12:30 +0000

    apart there treat temper actually

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
