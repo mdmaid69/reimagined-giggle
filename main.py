a98024ab0eec74393662ebc46dc7227f2b6f894b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:23 +0000

    natural uncertain murder summer criticism

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
