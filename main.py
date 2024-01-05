7bb43a955b331ef639f6472e55d78ad7ba4935dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:23:31 +0000

    month set delight worth likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
