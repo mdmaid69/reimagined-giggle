459a4180cab9c3e006032aa8e54eff52159fbd77
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:43:00 +0000

    really no outside disgusting plenty

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_odd(n):
-        return n % 2 != 0
-def sort_numbers(numbers):
-        return sorted(numbers)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
