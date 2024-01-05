79e5fceb5d1775c9ecb4a8434f81a16ae5a177ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:14:56 +0000

    treat dry translate lose operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
