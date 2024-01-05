2e92cbd8c30c8a174ca34611993eca30b06808e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:33:47 +0000

    telephone fork ahead forever arrange

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import sys
+def print_python_version():
+        print(sys.version)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
