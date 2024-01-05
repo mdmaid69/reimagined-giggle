6509428a2b1d6fd15e9a43e6a92718b474f3aa2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:27:49 +0000

    pen money sky hurt delicate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
