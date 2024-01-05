ffa9b907e3b004ff8836e3550136b5fecd394048
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:08:54 +0000

    refer doubt tape dress month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
 def get_array_as_memoryview(array):
         return memoryview(array)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
