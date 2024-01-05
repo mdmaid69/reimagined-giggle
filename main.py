e6dd025e0ececc313108fe420bfafb3790e36fff
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:08:22 +0000

    drop simple butter deliberately smell

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
