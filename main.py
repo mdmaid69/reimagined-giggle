9935d8aec57ed1d98104509a4302205df84bf8c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:43:23 +0000

    fair fond busy obvious favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
