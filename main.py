501847aa8129a15eb068a8564627b3cb30df6dee
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:32:36 +0000

    platform turn suggestion squeeze dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_current_working_directory():
+        return os.getcwd()
