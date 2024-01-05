4b3ba8c3eac34510d94130bb746f1c6a281d4b6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:46:49 +0000

    sail home separate home path

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
