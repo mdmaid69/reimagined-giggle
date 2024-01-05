71338ca8a1956c493d83e9a572d96892f81c568c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:17:29 +0000

    correction upset contrast present spend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
