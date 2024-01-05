36ba3948143cb46d450398b696beccb5866e3aae
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:19:50 +0000

    stranger pay cold rob runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
