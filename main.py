87396d8dfb02f612df3ab927b6b28b5dc1103599
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:38:24 +0000

    contain attention sad little genuine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def sort_list(lst):
-        return sorted(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import datetime
+print(datetime.datetime.now())
