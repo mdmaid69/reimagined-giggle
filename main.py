128a8b51a5a3bf19933f64cae61ec4045995b2f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:24:42 +0000

    tight face lead extraordinary board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
