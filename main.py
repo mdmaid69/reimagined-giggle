db7ca7a9a730c5ed19686e6df7c7474dfbe027bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:12:35 +0000

    hair tune news more tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
