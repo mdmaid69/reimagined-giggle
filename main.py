41d0f440993cc928c0f470cf239302b21a95907b
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:08:20 +0000

    forward talkoutof none to paper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
