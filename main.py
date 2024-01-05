5cd0e4de99e470f52ee3cf4275ae714f059c68cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:30:25 +0000

    fact concerned switch getaway circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def reverse_array(array):
+        array.reverse()
