ffd08c990ca7a9e63b13d88ba37623c771e9ac4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:46:59 +0000

    pleasure although cause once race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
