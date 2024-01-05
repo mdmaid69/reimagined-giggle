adb1183c8abdb490faa2b36dcc55dbead64e568e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:22:40 +0000

    themselves few gift gather knock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
