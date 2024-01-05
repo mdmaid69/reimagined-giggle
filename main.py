8e81f6a198982849d70001cbffdbe2105212701f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:17 +0000

    pay open deliver abroad kick

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
