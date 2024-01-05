fa1d241e83f7d872db2ed188d0f393854909b75c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:22:36 +0000

    remain fun neat worry basically

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
