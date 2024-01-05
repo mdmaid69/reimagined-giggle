9d73fc03541b32604128cc7b24e08fae040efa72
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:37:59 +0000

    turnon check rest conversation cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
