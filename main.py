4caef8aafc2c0a474f438e81eec972430e22f014
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:51:30 +0000

    notice makeinto so challenge cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
