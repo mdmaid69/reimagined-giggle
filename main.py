d9bf86d07f4221dba26b4c80e2c00dceca4eb7b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:01:35 +0000

    turnoff but while satisfactory able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 def is_palindrome(s):
         return s == s[::-1]
-import datetime
-def get_today_date():
-        return datetime.date.today()
