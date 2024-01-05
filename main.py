e061a5272dad4decb6998c9a940f2dc903744906
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:12:50 +0000

    rough damp paper because here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def count_characters(sentence):
-        return len(sentence)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
