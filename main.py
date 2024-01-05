aa4ae9484a9a2d2ee06d5c8f7d31b1539cbad4b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:57:28 +0000

    press strength unlucky uncertain move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
