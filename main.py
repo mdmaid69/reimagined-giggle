132298eccc17af351334fd2d681c825b28780b69
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:28:39 +0000

    release shelter large benefit fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
