404c6d3fd44c63b744d4672068d55630e8e487c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:09:00 +0000

    war turninto last run send

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import sys
-def exit_program():
-        sys.exit()
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
