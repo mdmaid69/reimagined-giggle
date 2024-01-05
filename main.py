93ae25e01c177ed657eee5ca15107127ae5f061e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:12:11 +0000

    confuse border radio march word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def clear_array(array):
+        array *= 0
