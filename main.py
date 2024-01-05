aeefe51f8dfb28130d1faaced887614687bfb31d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:50:26 +0000

    genuine comeback opposite behave clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
