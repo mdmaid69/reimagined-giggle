420f7e307f4e3b6a28df13a268ee16954b392f23
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:34:06 +0000

    yourself prize Rr tax birth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
