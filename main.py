e12cb21accff22a4b5de10c4f86a59784d8cb4db
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:20:45 +0000

    measure wake question declare correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_floor(x):
+        return math.floor(x)
