2e16f9eafe5324ae4a1b069897bdb1e543c77498
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:20:40 +0000

    skirt left shock influence watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
