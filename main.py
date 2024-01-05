dcf4e2608d471f708f7ac07c748959f84c586977
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:29:01 +0000

    autumn trip ring motion pattern

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
