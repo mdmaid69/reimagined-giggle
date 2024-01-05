0658e6fb77c5183c017f85c6c9d7ff95c25458a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:51:30 +0000

    question party build petrol wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
