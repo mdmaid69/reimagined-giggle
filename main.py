238da8e703caa2342f8d7e8ced834bdc5ecc4403
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:26 +0000

    public thick slide if recognize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
