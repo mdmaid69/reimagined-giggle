2db4af38fd0b5f51c9ea482da34514d47a74fa10
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:41:39 +0000

    order hold attract accept extra

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
