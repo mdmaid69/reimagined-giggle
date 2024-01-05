211f451659251c49e463db7724fbe09a85fdad01
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:40:40 +0000

    several club schedule dream trouble

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
