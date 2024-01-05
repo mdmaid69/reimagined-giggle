57b7b5279c82d3dfc91b24d2ac80c9fec883b711
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:07:30 +0000

    search shock slice prevent fire

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
