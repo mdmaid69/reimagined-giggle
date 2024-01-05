d7e8a7671e7ae1f90772681d31d262b94ba68c1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:02:48 +0000

    basis exact lesson Ee wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
