942d58f316d1cfafcbfa0f744991a07511ca3bd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:42:58 +0000

    leave wave other keepaway pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
