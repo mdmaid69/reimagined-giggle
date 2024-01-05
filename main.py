f9464f3d4aa776ddd2878d21539721555eb8b80a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:44:07 +0000

    pipe subject correct week near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_power(work, time):
+        return work / time
