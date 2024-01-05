5cfc57391a61fefce85fad5f5bda7061cdb0f908
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:57:29 +0000

    Gg react judge what bit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
