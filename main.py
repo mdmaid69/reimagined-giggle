0cba4a2c9573fe98a48de8096b79efdc6a1cba78
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:37:18 +0000

    exhibition middle shame Ii shop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
