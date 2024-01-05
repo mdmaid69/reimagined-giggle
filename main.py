37d0c50566745ca21b07e5675360a814f4c40d4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:16:47 +0000

    safety plant correct danger possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
