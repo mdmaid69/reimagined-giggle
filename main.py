e6afc8e6acb9b383ef4332cf507d71fa4f620517
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:23 +0000

    face complaint into callup makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
