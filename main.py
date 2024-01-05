a74916fe22f02ecbac2b730d03e3b29360bd8492
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:17:15 +0000

    careful proud exactly pause now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
