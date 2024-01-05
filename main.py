fe24fd70faa04f2247b375645c880b7c9ab8a6c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:10:20 +0000

    dig shoulder foot desperate provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
