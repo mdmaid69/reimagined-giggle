1ad56d87c624cd2c92d82fa38699e23fabfd822d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:11:31 +0000

    nervous crime actual mainly warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
