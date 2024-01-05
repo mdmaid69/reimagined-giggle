0ce23d20ff2a5b0b2ea5c633003aa0d5542f13bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:07:51 +0000

    the bridge getaway rid medium

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
