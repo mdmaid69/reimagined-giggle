7af8cd7d1b8293c6a976241cc60d3af65751cf18
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:47:38 +0000

    protection before bound keep usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
