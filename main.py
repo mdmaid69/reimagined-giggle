1a2fb5c56a0566c2ff7e8698ffb7edd9d1ce6e43
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:48:03 +0000

    greeting slice heart understand crowd

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def add_numbers(x, y):
-        return x + y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
