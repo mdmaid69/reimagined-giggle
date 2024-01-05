2ed0d742b56552ab7b1a8b72ea783dcca211d6fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:19:32 +0000

    part slip visit depend upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
