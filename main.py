28049ecb51a400bbbbe47e5696c7b2653518cfe2
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:26 +0000

    miss heat bury polite travel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
