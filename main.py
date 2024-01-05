594bee9979790132c7380fd4167f0df1056e1c08
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:46 +0000

    Cc belong alive may say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def remove_duplicates(lst):
+        return list(set(lst))
