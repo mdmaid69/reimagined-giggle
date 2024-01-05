0633a2154c50d068ad357fb42e779676edbfcab5
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:42:11 +0000

    along thinkthrough rest genuine goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
