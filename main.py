889e0134f012d00e264df79435a49a92013ae9d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:09:51 +0000

    confuse Ii production no touch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def convert_to_hex(n):
+        return hex(n)
