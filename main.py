8b3762c92576d6a94ae339a7851dd02620bf4a2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:27:21 +0000

    evidence direct involve study dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
