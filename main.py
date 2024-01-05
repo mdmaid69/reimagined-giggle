9549647ecce00e57849407da0b89fb8ce449c37d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:10:28 +0000

    study allow with pride official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import platform
+def get_os_info():
+        return platform.uname()
