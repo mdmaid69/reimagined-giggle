45aa015ad3de585d20255c2dd56aae63e5571449
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:42:59 +0000

    moral silent meal wherever free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-print(os.getcwd())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
