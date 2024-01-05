e8b3d0ec7c6120d237334499e81201156869187d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:07:17 +0000

    angry consult professional generous Hh

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+print(math.pi)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
