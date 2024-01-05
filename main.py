4474d3807d1317cf271a41b3592a145ae58c35ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:19 +0000

    content makeup low step separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
