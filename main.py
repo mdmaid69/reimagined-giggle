7779d68ea771f0d2dd734756670c327a516a1d28
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:03:13 +0000

    trip behind Hh crash factory

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
