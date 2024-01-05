b762d7444cd3144ce2c183c7271f1fb3e42e3fa4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:21:22 +0000

    start check act consider communicate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_area(radius):
+        return 3.14 * radius * radius
