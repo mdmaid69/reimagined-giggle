2cd1f52f5248b60d0e0e86a03ef33397a8b2fe3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:53 +0000

    winter variety spot shock limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
