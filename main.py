e2aaa32a9c88628facfd8de2ed0b55ae3878f4fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:23:38 +0000

    where experiment break often message

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print([x**2 for x in range(10)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
