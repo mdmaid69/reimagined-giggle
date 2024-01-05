c271765872fdf06eb23f2f897cfea8c6b294c6c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:44 +0000

    except switch official equal dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
