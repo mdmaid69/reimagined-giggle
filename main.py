629087faa4f425b55bfb694ae9e48cd0468d90ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:10:30 +0000

    sit yesterday train ill our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
