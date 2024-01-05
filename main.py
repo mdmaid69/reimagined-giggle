e10c4b0031ebb90e95977f97a5391a9cc0638136
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:53:40 +0000

    port square worried keepout recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
