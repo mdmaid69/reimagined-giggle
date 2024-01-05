770c12ebaa9506ca4d81c5646653625e2ae5d4d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:49:59 +0000

    giveaway almost bet 566end plate

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
