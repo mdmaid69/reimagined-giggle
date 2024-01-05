0c2d0bcb39e0d737bf35d06614be72ee1d46cd9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:48:56 +0000

    convinced ahead mean city voice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
