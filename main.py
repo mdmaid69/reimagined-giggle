908b42b4d8652bd475d655eee2de6adb3ed5855f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:44:45 +0000

    fall mad before lookback(on) divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+        return os.path.getsize(file_name)
