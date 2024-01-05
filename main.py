edb93885ef44fca868df3de563d42731b4df05d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:50 +0000

    onto adjective everywhere odd state

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
