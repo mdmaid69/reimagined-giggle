47bb6e6227f70b36c6082e05fe3188d82e94bfe5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:10 +0000

    bridge new home delight position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
