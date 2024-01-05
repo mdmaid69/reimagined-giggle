413548fe8acffb21fa85806098f66127db2d0968
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:18:51 +0000

    light brush push Mm allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
