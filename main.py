6a80dbe814588d7fc716d164c1db37a214e5665c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:24:56 +0000

    stamp let get trap principle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
