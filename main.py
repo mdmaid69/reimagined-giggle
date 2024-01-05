4c90e9a932c535d8ec54fa088e67115574963c7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:05 +0000

    shop 568energy soft hair win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_current_directory():
+        return os.getcwd()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
