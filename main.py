273bc55c47cd28307e9129bf7cd95d71bb5c4a10
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:25:03 +0000

    either runinto possibly will weak

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-print("Hello, world!")
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_work(force, distance):
+        return force * distance
