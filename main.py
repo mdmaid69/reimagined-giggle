61006ba1e9c7c91d676e56f0ca8283fd7988c80c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:33:56 +0000

    Ll bringback since comeround test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
