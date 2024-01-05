61233e4ca69ac63e9448db0ad711bc11833a8c49
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:58:48 +0000

    tend port taste disturb mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import os
-print(os.getcwd())
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
