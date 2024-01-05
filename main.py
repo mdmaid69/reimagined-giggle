e9a2e7b0d208bd09e05108f37ccf712d3d317952
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:05:16 +0000

    everyone later charge thumb sister

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
