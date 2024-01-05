bdd36416d14a50e75b205c05459e4e067909e578
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:28:26 +0000

    check sure among artificial through

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_work(force, distance):
+        return force * distance
