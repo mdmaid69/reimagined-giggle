dbd131f296fba49842b3e9fe713eb43cca672e7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:02:57 +0000

    train shine equipment rely mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
