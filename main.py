d239d64e077b518fabfed0e1fe6e1221ee741eab
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:16 +0000

    raise late so fact back

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def find_max(lst):
+        return max(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
