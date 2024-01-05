a5b923370a45c4a88a8a933e9ea9ab23499c3852
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:03 +0000

    morning pity so impressive even

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_power(work, time):
+        return work / time
