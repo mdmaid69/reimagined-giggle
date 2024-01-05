3dd011db243f74c21cb1e1623e6f11878ddc15a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:47:37 +0000

    could identify subject whom promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
