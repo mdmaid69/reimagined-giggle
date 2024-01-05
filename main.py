2845a13c941c56cabd21c1abcefdf1a1f267b6b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:12:18 +0000

    relief bring clear high supply

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
