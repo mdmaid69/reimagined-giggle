06084a97e095e3f07fed84a1cf1e933d3d4725ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:48:43 +0000

    pour tax spot hard date

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
