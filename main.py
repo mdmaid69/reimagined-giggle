1e5b3f79d05644859d4afa38d8c185b2543786db
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:52:35 +0000

    plain blame body behaviour return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
