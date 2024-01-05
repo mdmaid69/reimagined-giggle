8bfe3f167a1fa10ddd106aaf537f45626fa0923d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:17:35 +0000

    peaceful smooth recognize town scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_slice(array, i, j):
+        return array[i:j]
