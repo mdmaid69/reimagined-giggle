51c9bd880e0ee0f9015c935bb574c2446237889c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:21:35 +0000

    upset only juice round surface

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
