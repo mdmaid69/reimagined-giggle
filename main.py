98206e3d01b3ed59e6faab142899e56c75164313
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:48:23 +0000

    breakout dark future disturb news

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import datetime
+print(datetime.datetime.now())
