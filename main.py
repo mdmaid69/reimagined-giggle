6b93b8c776e4c5a2c3f789e9c044ff19931ea92e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:07 +0000

    trust clean surface stop estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
