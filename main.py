d20050cde3d80cf856cb498a9840dcf03e2a3b9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:41:52 +0000

    remain container ashamed reliable cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
