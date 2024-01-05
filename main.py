f7a3a13135c5175c8845ee802565c8491d6319bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:40:46 +0000

    comedown stop Ee film study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
