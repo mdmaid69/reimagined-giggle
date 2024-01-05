6fd11bf0d107af4f478695c07872737f7f740d56
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:15:49 +0000

    work man bottom climb goaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
