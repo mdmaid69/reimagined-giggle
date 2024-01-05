b8a5fe87616c41dbbe7520ceda3113c88cb99e22
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:33:29 +0000

    party 571enough fierce needle complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
