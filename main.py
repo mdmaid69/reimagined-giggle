381b1b9f261c83189855e6291ab1d9638ba2642c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:43:27 +0000

    worry street scheme inquire admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
