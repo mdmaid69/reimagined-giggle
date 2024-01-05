8a7858417c3352f0fe182ad1df9843dea7ad1f3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:35 +0000

    strike comefrom corner dream brother

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
