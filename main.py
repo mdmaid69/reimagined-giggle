04a355dadd2cf4308efac70c0a1627a1002aa300
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:17 +0000

    team sick pretend holiday breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
