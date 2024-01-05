38477cafcc511eedfd5f753bbf503e7c1d584298
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:29:16 +0000

    exam aim just sudden my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_counter():
+        return collections.Counter()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
