4c054fbbb6d82d9faa7191d404a93b64a2540ab1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:56:51 +0000

    plenty clear grateful clue return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_volume(length, width, height):
+        return length * width * height
