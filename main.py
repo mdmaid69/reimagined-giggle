a6362c838b5d0d846d8fa7904cea20c6dcfcd69e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:19:44 +0000

    keepoff because greeting dick vanish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
