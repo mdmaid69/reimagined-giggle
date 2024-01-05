ead25c081da15c7f3927b28045bd6562f1d9125c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:53:02 +0000

    display apply between kill wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
