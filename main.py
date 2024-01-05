300605ecff0c30817aad9807d44a5e4c21a306cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:58:34 +0000

    floor danger keepoff increase extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
