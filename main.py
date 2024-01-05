ecb6633d89ce2b295190e9b0f7b0b112e8d6387f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:38:09 +0000

    black mind steam disappear find

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def is_even(n):
+        return n % 2 == 0
