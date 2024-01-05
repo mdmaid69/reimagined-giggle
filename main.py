3ba9f1edd42e04cef527bbc7ed1e01bf04aa1ad3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:31 +0000

    may cruelty serious improvement common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
