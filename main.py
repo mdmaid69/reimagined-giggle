7ec29ef0c6f9ccde353ed238b0408d5ad16a98f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:11:02 +0000

    crash ride complaint rid react

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def subtract_numbers(x, y):
+        return x - y
+  def remove_duplicates(lst):
+        return list(set(lst))
