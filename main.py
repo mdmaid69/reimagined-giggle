086b8d7ac276812ed179dfca7bbb6dffaa2848fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:40 +0000

    twist thumb choice summer most

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
