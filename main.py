8d24aeec919aa45818f642206c21997efb87eb94
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:28:03 +0000

    shoot careless juice day opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
