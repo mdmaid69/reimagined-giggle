6e67af0c8267825349a1ca5715fa469414bb8e1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:58 +0000

    count Ii capital 179bite we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
