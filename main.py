08db29d5b37257e67eed2cea3025e2305f37e2c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:28:25 +0000

    brave professional squeeze awake warning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
