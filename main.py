5e3d73af1c51d62e7867334c3dd35262e0da860a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:40:55 +0000

    shoulder dry anything must as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import random
+print(random.randint(0, 100))
