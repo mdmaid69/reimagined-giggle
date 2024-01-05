06d301ae7d59b2c0db59bf470abe214e7496ceb9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:25:01 +0000

    tough song already hour runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
