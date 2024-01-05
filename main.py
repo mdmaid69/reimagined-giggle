181df389bf4a36f32e8e7542411a7eabbd9916ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:26:26 +0000

    compare exist experiment bin holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.time()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
