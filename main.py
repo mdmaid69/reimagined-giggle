90528a9689197b26ba7debf8787a593b4dbb7b99
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:08:17 +0000

    subject lock Tt hard address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
