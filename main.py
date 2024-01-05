23383aba0100c81ddb1000d462448feb1efc8912
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:41:44 +0000

    murder lead walk sleep smooth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_length(array):
+        return len(array)
