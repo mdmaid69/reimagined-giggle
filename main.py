cc2e8e01a000275fd88023146768cc03c0f740f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:18:18 +0000

    charge find meeting let fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
