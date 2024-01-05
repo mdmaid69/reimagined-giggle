364ee7b1ce27b34d4ef22ea1aba619adcad4a510
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:27:16 +0000

    trick beneath disapprove patient ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
