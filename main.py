c7048e6ca77faa122d1aef95aa91e2c0a6830102
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:47:57 +0000

    pretty fit feed argue depend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def convert_to_hex(n):
-        return hex(n)
+import sys
+print(sys.version)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
