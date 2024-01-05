7703e41d3fac43b66e7bd7caf3367a2c313d398b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:03:02 +0000

    dare cope piece way ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
