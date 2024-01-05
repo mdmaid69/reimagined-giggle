83b82b3fa34d9a54fa905ebbd3c044300a588375
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:16:05 +0000

    far anything bother another wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
