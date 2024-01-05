40978199779ecf3fc41d904e26e584844f9ab94e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:04:44 +0000

    club their study murder commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
