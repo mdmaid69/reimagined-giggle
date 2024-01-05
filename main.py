9b3c4313f1f9d0feb7378345c98c230351bae28c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:46:44 +0000

    illness place bath public excuse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_length(array):
+        return len(array)
