10037afde7b176c72129f51a121b871c234076dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:22:19 +0000

    every remove ask turnoff shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
