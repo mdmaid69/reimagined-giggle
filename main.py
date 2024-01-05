b6562bfc6a4049051fbe5c1d641c6eb96888665e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:26 +0000

    past Gg hollow apart same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
