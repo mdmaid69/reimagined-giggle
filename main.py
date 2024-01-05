50ea2594c7a37a45e9a277f7e48331faeee4474a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:40:05 +0000

    ill unite sticky correct rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
