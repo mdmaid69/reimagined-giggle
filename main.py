7d7b69798dc25d08589b9e512d3dfd02f5fab661
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:16:42 +0000

    collect busy crash violence getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
