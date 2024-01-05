bf1f6712ca6ee49d7dbb5df21506016a5cc61e24
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:38:04 +0000

    beneath by knowledge scrape wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
