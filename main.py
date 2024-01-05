a838134abbd2443460af69476c5068ac3afa485c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:21:59 +0000

    rarely lost consist fit any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_as_int(array):
+        return int(array[0])
