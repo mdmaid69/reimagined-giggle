3ade030eb14ea20af316c04ce9849c478b358176
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:31:45 +0000

    about die explanation damage trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def create_counter():
-        return collections.Counter()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
