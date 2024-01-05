06beac59492ac686e08f5843910f53ca4d203c73
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:25 +0000

    music value various turnover its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
