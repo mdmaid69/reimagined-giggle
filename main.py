9d9a7110ea92a8650452b770c122cbfe70840109
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:19:27 +0000

    disturbing second design summer takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
