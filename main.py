67f0eccb4d6521ee7d72c8d2fc45abdd00cb5844
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:05:44 +0000

    damage undo accident straight centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
+import platform
+def get_os_info():
+        return platform.uname()
