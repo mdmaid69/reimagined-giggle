efe60ace0d1ee8b2a823e5b8719ec124a2f75861
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:14:04 +0000

    tell set meal pair soon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
