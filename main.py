c68ed9766c4c040a838bb2ef60d3ffaf11eb3e68
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:07:39 +0000

    port acceptable wealth could person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
