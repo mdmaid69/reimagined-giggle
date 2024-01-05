1cb3d5b22f110e8526128233f9252ae74b0207aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:18 +0000

    follow something survive ticket post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+print(time.time())
