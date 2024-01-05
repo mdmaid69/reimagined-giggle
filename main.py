8e96bfe398178a1274c448950d7635fd0f41b23b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:29:03 +0000

    habit bridge wire path steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
