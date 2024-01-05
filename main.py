7731ab73e2bcc9743e8ba55278d136ddda0de8f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:37:04 +0000

    card equal match degree use

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
