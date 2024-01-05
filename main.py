f44102f3a8d5357fa95e199d48eba6525b4e2677
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:54 +0000

    Kk short argument undo make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
