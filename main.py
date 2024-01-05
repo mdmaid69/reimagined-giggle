a76da32882f792ab9cc49a2d881561a1a4db9efc
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:37:54 +0000

    knock warn challenge yourself drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def square_number(x):
+        return x**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
