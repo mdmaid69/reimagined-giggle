57eff737d312e5a57f243f6ba9d5c5b9cb015547
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:57:47 +0000

    figure badly considering flood worry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def convert_to_octal(n):
+        return oct(n)
