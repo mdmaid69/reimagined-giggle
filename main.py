80dd2171fe836bd301a77ea2415d37b268d52452
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:27:59 +0000

    rule fair passage in port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_current_time():
+        return time.ctime()
