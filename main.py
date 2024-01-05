c55277fb95d738e35dc18f4bc9dd398336f3234c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:36:49 +0000

    far success sleep close ordinary

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def is_even(n):
+        return n % 2 == 0
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
