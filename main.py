649ec245711cc603673d3fc8bbdf469bad3df342
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:28:37 +0000

    keepaway blame small woman maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
