660507a362eae77fca72a79c776b97545b5a546b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:45:45 +0000

    question university regularly opponent temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
