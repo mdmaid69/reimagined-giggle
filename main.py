ac80f31a6f61f5b2dbeefc1b8218360a82d5fb26
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:46:50 +0000

    tonight name thick design summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
