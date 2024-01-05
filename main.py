b3dd2f58707c34746c9981fedc977c0adca1b20e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:31:37 +0000

    exciting able commit advanced group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
