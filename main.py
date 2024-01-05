3422ff99cef8d1b115909fea4ef9ec88df7aecf5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:03:31 +0000

    quantity tap classroom free except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def sort_list(lst):
+        return sorted(lst)
