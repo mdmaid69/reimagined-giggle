11677540f342340f1af7d1230aa2bd1b029c2110
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:03:17 +0000

    picture school which branch office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
