dc3ead5f9258f3751092d04a0541e5934db3bbaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:44:59 +0000

    unkind obvious all yes image

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def find_min(numbers):
+        return min(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
