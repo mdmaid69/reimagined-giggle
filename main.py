8729dcf5fd60cacc79cb242049ad53f230275cd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:52:57 +0000

    there unfair proud wipe shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
