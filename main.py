ddf620c914010dcc1bfbe94ff01ea22237ac5983
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:11:06 +0000

    strange cry feeling willing shoulder

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
