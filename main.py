d535d98d344da4c0a8f6e843446d4268dec4e881
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:02:37 +0000

    concerned easy remove whistle when

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_even(n):
+        return n % 2 == 0
