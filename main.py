b836f5e49105c037d62d6ee7ff5f94aaad8b1470
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:47:18 +0000

    naturally only movement weekend bridge

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
