e144a1a0d1dcdd94c6842c3608cb4186cebce7b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:01:00 +0000

    creature improvement swallow pride yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
