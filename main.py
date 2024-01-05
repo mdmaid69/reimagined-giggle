00331e5a80ebb697f32d411653a23d7912dadb13
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:23:08 +0000

    Ii warning value travel pain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print([x**2 for x in range(10)])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
