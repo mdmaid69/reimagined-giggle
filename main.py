2b54610a126f8106aad4f697add9e7cac1186f28
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:17:15 +0000

    later takeoff dream person eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
