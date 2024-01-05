34fc1d62edff9d1bdd54384df0defea26737b52e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:44:27 +0000

    group bad class worried tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
