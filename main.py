2147cfda96c120371532895383af734538363d12
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:17 +0000

    cum communicate 567enemy produce can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
