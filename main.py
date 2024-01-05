1336ac71419bd0624492a18d6227e80329d052b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:21:51 +0000

    statement hope emphasis argue no

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
