9d38e46f63c332ce22aa8b5fadbfe456f7e5ef51
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:57:23 +0000

    angry voice everyone such as

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-print(sum(range(10)))
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
