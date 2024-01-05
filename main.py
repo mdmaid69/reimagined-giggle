9fbfbd24ea7b7bc26afdaef6ab63bf73768a78a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:45:10 +0000

    like attach shoot afraid medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
