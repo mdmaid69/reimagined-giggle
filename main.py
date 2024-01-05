e1b51f8f1bbbed040353991dfaf4d90617b92d42
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:18:03 +0000

    moon surface possibility survive language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sys
-  def get_python_version():
-        return sys.version
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
