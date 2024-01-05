d6d5b6b304110186d41699dc394a7d25b67057e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:28:06 +0000

    if when shadow never bury

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
