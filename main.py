167a4063a0cac4d85184f820c6d58d678ff8554d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:16:17 +0000

    embarrassing march walk move gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
