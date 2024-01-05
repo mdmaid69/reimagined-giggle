c653b3041311ac8bacb829fa724183ae4966767f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:46 +0000

    through habit world thankyou content

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_max(numbers):
+        return max(numbers)
