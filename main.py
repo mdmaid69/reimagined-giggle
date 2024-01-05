c189ee3c6dac72e0306ad37b3f053f11e8ddbfcc
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:58:08 +0000

    proof born answer race official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
