945ac48df4efb4c69852e6d24368917ad1e4f6f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:35:00 +0000

    keep neck crime determined scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def convert_to_octal(n):
+        return oct(n)
 import array
 def get_array_as_tuple(array):
         return tuple(array)
