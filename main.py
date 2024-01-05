071cc5ae005cb196b1a94ed86d44d4267e6f836d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:56 +0000

    cope blue drink lower rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
