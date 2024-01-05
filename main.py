2321e6483d37eb64112bb9da8d21d2af412428fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:18:21 +0000

    Uu swim stop recommend beyond

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
