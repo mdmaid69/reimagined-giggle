8924b297adadfa1164a031c6b18026619c6cc97f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:06:24 +0000

    contact instrument impression do load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
