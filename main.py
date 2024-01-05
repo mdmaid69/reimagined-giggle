64bdcd98bbb7cc7659997f935423f2974c334994
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:46:57 +0000

    easy could same plan nonsense

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
+text = "Hello, world!"
+print("Characters:", len(text))
