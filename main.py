e0f96ecbb96205fbcd3b835772a5a62135369588
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:40:52 +0000

    gas drop sound just power

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
