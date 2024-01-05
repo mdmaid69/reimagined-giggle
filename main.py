70af4ffeaec5a66e97c4be95ce88c0cca9e24710
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:27 +0000

    suckme thinkthrough put frame relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
