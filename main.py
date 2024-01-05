a81b2d91b4a10a2709333d1d36ceb4aefeca3361
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:35:32 +0000

    improve basis upstairs sensible reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def create_user_list():
+        return collections.UserList()
