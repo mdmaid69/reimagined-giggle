710f6e0e6c9d89e2d211a6e7e389ec4e46c7b6ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:25:02 +0000

    her backwards influence cover nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
