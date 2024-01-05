2a3ba7335058d1cfcbd3cadd8f39e868f8d66121
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:53:51 +0000

    noone invite ear visit possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
