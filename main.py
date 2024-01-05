eca596a1ab5bf40702aa8bdc5085c95de8997438
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:21:38 +0000

    strong exactly area complain fresh

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
