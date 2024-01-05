0073e09611bd8188943d9c3b5625bffaef8945b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:45:38 +0000

    reduce whenever object nerve same

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print([x**2 for x in range(10)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
