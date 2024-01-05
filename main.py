2a56384b83a284ffe0571dd05f2da4ce09583f48
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:40 +0000

    payment quick welcome workupto return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
