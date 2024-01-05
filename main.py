6253f22c656866c5928fbff124ed50798f2bdf3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:29:33 +0000

    how among spray lot somehow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
