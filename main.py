6310504f615b516561f38d97e6e8e2c5522400a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:34:19 +0000

    only exactly share trick repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
