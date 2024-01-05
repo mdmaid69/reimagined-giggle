3fb87177e4ebb0c0c89fa210b2b788477a7e6ab4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:57:05 +0000

    Yy bunch impressive doctor watch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def is_odd(n):
+        return n % 2 != 0
