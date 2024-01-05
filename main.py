f67083e5c265a39eeb83a80eb94ca957b8897029
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:02 +0000

    reply block noone contents rank

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_floor(x):
+        return math.floor(x)
