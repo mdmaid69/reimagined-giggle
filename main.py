f3a05a1663ba08ac5b04a259d11ff725b211e2fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:58:53 +0000

    star distance turnup environment bag

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
