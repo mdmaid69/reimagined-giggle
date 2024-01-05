830d05397aacea1a8912890f11964979b0ae42a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:49:55 +0000

    they regularly whether part polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_volume(length, width, height):
+        return length * width * height
