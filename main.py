9de30cc88c5baafafb8868648222f94b5b386acc
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:54:15 +0000

    bye challenge Yy inform niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
