e5a06c132cec9244ff2b934922283384e9168593
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:26:04 +0000

    lose emotion at smart ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
