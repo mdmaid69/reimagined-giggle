e42e92927bd25849500894901f1958193e3320d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:37:26 +0000

    would surface appear street satisfactory

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
