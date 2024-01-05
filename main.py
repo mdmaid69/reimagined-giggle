ee18cb804f0eebef0b621d33e1fdbcb6187fe5b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:29 +0000

    quiet suffer cough free wall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
