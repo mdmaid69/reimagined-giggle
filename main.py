66c6113f234c9f23912bb9c1a20a6a2c9626d0d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:02:45 +0000

    bang university me increase break

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
