f52ebd40509c31ac3f3f5862381ea3d7f15fae56
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:49:11 +0000

    although herself buy port keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def sort_list(lst):
+        return sorted(lst)
