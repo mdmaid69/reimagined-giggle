569467cffb7848b0e9f53cb220ed9c1ef69709a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:13:17 +0000

    measure promise freeze everywhere likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
