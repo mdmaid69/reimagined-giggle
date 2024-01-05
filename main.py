324dfa9cee23cd6fe37f590292eeab0e54d2589f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:36:34 +0000

    Ll report everyone partner commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
