d7c1e9a3235b9df844959ffd0a966d42f70a5813
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:40:33 +0000

    please influence lean learn shoulder

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def find_min(numbers):
+        return min(numbers)
