405567e9a7194ebf528e3b53bf6c00bed9ee3151
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:43:29 +0000

    slow production at arm chance

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
