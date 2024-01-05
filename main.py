e45ac71e889bf193a39f9892b7ad10f21468ff2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:30 +0000

    ball old criticism yesterday question

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
