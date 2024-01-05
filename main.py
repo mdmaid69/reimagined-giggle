89ecbc2e8fd9d993a441c5b8ee73877bbd607f8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:16:51 +0000

    ignore low hungry spite guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
