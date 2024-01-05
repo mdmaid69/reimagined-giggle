51888b578dab555ce2bfdbf85c9e7f65d859026a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:14:07 +0000

    Cc trial Gg compare major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
