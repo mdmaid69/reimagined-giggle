775c920a6cea4099464de922c9a9ff035c14db4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:18:36 +0000

    company Vv share unite holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
