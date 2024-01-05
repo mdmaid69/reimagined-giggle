203dc8688d5e57edeab697a645f4bb7f5e48857f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:03:01 +0000

    sick discover mind might like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
