29d6125303f4a5543865b4e5a4c3c5d253b45fbe
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:06:29 +0000

    special down trade mine unkind

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
