b723d974989b15098cdae412515ab2968b8ebc00
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:26:59 +0000

    anything pool scream load pollution

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
