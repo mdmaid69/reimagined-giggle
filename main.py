708dc1d90b30800e9a3ee47c9e7708ac8a6967a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:08:35 +0000

    draw gradually but somehow seat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
