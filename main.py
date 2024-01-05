70e8707b20e5c8ba5abc73af6d64b24f2da0246a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:45:51 +0000

    care thing friend wash new

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
