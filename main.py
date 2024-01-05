a29d7e976307eca8e01422e7ed21ca18b58ce5cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:34:50 +0000

    onto pinch programme afford wind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-for i in range(10): print(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
