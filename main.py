83687894e16adf1a3a9ad1e853402364891f2774
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:20:07 +0000

    cut unpleasant class single you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import collections
+def create_user_string():
+        return collections.UserString()
