fe225bbae6a48042bbae7933d5b75ef52509c668
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:39 +0000

    test comefrom impression content lost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
