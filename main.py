760e65d230bae375d9bd3c2420f00140784024f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:35:18 +0000

    supply play opportunity start eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def reverse_array(array):
+        array.reverse()
