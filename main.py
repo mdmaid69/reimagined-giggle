f03695edbbae4712b0fbd1a3a59c56afe1c15746
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:07:45 +0000

    fight ear skill deal mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
