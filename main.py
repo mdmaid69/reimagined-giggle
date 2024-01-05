df0726df987f5bb05ae90e726cdc9ec211c2e640
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:23:17 +0000

    tip play honest evidence base

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
