fa5eede4c79ab1ad6c2717328883551725188801
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:40:12 +0000

    care thought bit judge use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
+  import sys
+  def get_python_version():
+        return sys.version
