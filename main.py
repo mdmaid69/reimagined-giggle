04c5f645a4c39805b436d33be277ee47e60458d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:08:37 +0000

    joke block profit move post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
