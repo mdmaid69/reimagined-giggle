12baa67a7edb90492fec6ac4b4d39d15924f03cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:48:39 +0000

    purpose minimum basically sell desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
