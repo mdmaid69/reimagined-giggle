13ec38641b89113c82fd849deac8d492d2043a8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:34:54 +0000

    pipe hair hold successful attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
