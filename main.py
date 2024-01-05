5a70503bf2255c2158af237baf20e405907b78d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:58:49 +0000

    quick consult punish neither worth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
