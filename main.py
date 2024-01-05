2a2f5f44c1db0ee8188e5f135abd0b920e0cb5d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:25 +0000

    boil insist yourself score trousers

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def sort_numbers(numbers):
+        return sorted(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
