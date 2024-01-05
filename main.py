fdbfdd9e21b91878ac03876294db903e6f786075
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:54:48 +0000

    shelter suit letter message weather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def subtract_numbers(x, y):
-        return x - y
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
