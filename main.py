6aeae2f219f3f02fe8b478991ed7fd598de40d31
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:43:38 +0000

    whoever floor quickly profit sweet

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def reverse_array(array):
+        array.reverse()
