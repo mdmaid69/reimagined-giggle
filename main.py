7d92e2a53eba8e59a83333d6268c44cb33a4f033
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:57 +0000

    hard journey argument dry clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
