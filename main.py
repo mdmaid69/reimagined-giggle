a78886abfc70056e96f40bbd960b43a0e90f2823
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:37:27 +0000

    plan concerning describe squeeze power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
