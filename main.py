11d32ce9d21c3cd677b008ff847090c38c5c9687
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:00:49 +0000

    touch want female better willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
