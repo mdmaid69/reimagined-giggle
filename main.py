f9fea4ab72a02873c49ee91c6e0eb7927ce3c1ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:50:17 +0000

    transport scream shine complain take

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
