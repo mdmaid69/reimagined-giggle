f7931786ec819e1849714f6a22e24a79cc460070
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:28:50 +0000

    scheme cry store upstairs ought

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
