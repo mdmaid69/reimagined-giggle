27e002b07becb924e2cfaf80376125212e5a4266
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:02:34 +0000

    stick joke everywhere noone delicate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
