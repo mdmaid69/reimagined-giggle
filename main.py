d37e6330abc89e10a8a8560669cc6c70bc318f13
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:42:54 +0000

    Mm run pronunciation cold success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
