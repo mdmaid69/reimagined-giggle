aa0a34888df9202148f2560148261f17c1f9b314
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:49:32 +0000

    sister pool platform plan pride

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
