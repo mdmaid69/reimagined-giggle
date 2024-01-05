49451d45b865ad095a3e1e161a5a36937342a438
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:58 +0000

    share creature greeting line computer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        print(sys.version)
