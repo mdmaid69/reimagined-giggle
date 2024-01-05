b575ac350caf9af0e2b1c72d8a050a4446059be7
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:04:15 +0000

    generous study seat season somehow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def add_numbers(x, y):
-        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
