905c87776d2dddde4fbb4cfb23e3b0c3cf8763aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:28:06 +0000

    surprising last accidentally choice amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
