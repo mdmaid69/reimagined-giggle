22b242796ba454a4e97f54f670687f19cd18ab83
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:09:57 +0000

    minor city scream unite ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
