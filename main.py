aa380d2ade32dcb9b6bab7f511906ae823d9af2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:30:34 +0000

    office slice give shout important

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-print(sum(range(10)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
