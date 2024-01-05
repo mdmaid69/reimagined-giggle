7a92b1c11be94b682f923c61d83d3583b7b99732
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:37:52 +0000

    Ww trip treat her foreign

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import datetime
-print(datetime.datetime.now())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
