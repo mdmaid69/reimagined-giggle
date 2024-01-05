a283f82a53fff7556f41f02030aa3f965f0e1580
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:02:02 +0000

    chin appearance over normal to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
