524b2d0026bf1bec789247bac43a524d75bd8555
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:59:55 +0000

    better stranger impression weigh hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
