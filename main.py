5a85cc4c5da609fb25d47b5ed101e38593b1df0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:21:34 +0000

    march dry rush back present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
+  import sys
+  def get_python_version():
+        return sys.version
