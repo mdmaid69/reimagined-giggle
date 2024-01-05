cc39570a88668a0e568616ec80a71480517bb954
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:46:41 +0000

    example whistle comfortable plain blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
