56c178de5c3da6e8d53ded09ff5d3358d1452ba4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:38:38 +0000

    black deep tread tidy pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
