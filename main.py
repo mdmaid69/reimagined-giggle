14881fab356bea9a2313db071221cfdec5581a4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:01 +0000

    to dead date criticize plain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
