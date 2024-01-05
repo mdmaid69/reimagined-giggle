3a7496b4864885961f0715508573753d76b20316
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:09:46 +0000

    street famous beneath will safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
