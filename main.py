cf3cb334d41252dabf7b26911d90530088b06c99
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:22:37 +0000

    display poor somebody threaten able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
