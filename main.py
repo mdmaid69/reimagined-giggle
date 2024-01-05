376f0fff4039c5b6018a438794dc9fec75585f20
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:02:16 +0000

    search exactly use second news

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
