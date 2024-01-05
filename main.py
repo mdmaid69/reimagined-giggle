d7cef88880b72ca6308dc0a2e0f24822bfa2f6a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:17:33 +0000

    go pause adjective ever delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
