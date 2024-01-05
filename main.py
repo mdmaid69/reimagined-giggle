0b48c6d2e7b891150beec1af77af51a5c05fba31
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:41:17 +0000

    drink range second account close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
