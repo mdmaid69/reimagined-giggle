ccbc372b44ee14a1358952657400fa04c7611530
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:10:50 +0000

    us level frightened comein once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
