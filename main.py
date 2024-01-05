543e8713aa7895b476bebe40587ea0fb82058947
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:57:08 +0000

    name thanks smell race somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
