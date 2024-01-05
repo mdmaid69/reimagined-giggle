3c947639fb9c6667866be56e2297852e61494206
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:41:19 +0000

    separate design already mother fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
