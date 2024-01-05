04ae5c6b8a3ff3da8ec6743c9a4baf269932a781
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:21:55 +0000

    accommodation bringdown justice dream mark

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_repr(array):
+        return repr(array)
