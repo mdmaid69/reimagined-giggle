b01f0cb453aa9f875900fac437088776fb7293b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:37:20 +0000

    fix among ought loose avoid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import platform
+def get_python_version():
+        return platform.python_version()
