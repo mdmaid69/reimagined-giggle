9626f26c946cc6cc0d6714b50bfd5594570f88ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:16:16 +0000

    form kind runaway test man

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
