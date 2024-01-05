6936ca5173a565c0060ac9da4e7329f3ab0acfd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:43:48 +0000

    no from near fly splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
