59eaaf258feaef1064b1fb75b143d9110e9b2f78
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:40 +0000

    force angry close firm mess

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
