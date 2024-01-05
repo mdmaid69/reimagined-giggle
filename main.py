c7336c55f3b24b6362f64b11bc049e1acd48f1be
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:43:47 +0000

    keepoff conversation story edge ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
