7a6c20222de2044b419cf4054f87c7d268d57568
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:40:52 +0000

    male church her put debt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
