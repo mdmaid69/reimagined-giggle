e62a34ffb15d81ea3049ae7ae45960cfe01cbe56
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:33:00 +0000

    process obtain cold innocent cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
