d5595360bb0ac991b2ddb54e404182d15890d08f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:57:01 +0000

    pretty sensitive normal disturb difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
