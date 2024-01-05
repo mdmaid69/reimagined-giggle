3891cd9aa4d3978cdd7b8062fec981abaffa33b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:54:00 +0000

    anybody dare downstairs period design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
