6e3200d0bc6e8bdc5c346af7c8855e339578623e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:41:24 +0000

    check instead main getup own

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def find_min(numbers):
+        return min(numbers)
