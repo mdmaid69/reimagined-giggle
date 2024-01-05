1b363b62ce40ca12ddf334e98648b122adc44c5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:13:28 +0000

    always word switch accept weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
