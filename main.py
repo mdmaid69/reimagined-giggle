2e53e48489f54b3e6569a4af1ae3d7c106492b77
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:38:03 +0000

    name that close hang cum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-name = "Python"
-print("Hello,", name)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
