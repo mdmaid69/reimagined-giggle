f691fc7d7b6dc48c7a778d289bf3678b1f21cdd4
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:20:08 +0000

    head pin reliable butter frightened

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
