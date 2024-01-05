67e1e68731fd6ade7d08e1eea4769bd15a194afb
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:13:04 +0000

    blame true nearly excuse careless

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
