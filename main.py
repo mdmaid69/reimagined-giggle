44efeb75ef450aa364e666fca99cb578430a0a12
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:05:17 +0000

    after realize reduce emergency family

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
