71c9af1924f0563e23265b420bd1e6faa079b5f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:26:24 +0000

    exercise breakout bin shrink bridge

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
