197f29083a8fcf717af1fdadab52b021c9483a51
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:35:05 +0000

    demonstrate teach turnback and miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import collections
-def create_stack():
-        return collections.deque()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def create_counter():
+        return collections.Counter()
