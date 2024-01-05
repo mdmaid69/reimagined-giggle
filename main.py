2943bc91531586bd9a23ef91bad78456525a11b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:19:16 +0000

    squeeze please characteristic use poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_length(array):
-        return len(array)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
