13961d0578ab2a266c1864e114c115e6af0cca69
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:39:54 +0000

    division arrive person any welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
