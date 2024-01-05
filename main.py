da970725d97fb33394fefece140f922aaf84a598
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:38:56 +0000

    scream as instruction fire very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
