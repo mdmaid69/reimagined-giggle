bd0d7771030f4cd28d3ebeecea2f3874648371ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:24:15 +0000

    new Qq area pleasant remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_octal(n):
+        return oct(n)
