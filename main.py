541696eba343bad6cf501c5535629a62eaca3baf
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:07:45 +0000

    yesterday cum worse operate hospital

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
