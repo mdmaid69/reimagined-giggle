5d8282fa4dce9f58f6be83cdd25f93bbadf52d6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:00:39 +0000

    unable will whole look Vv

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
