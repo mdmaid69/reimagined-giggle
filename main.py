f250bcf201eda99fe562563633d7a604735b67f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:04:44 +0000

    would suddenly realize consist please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
