6ca99ad8fd15a52d7abceaf725aec97c53e664fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:59:42 +0000

    gentle light foot hair word

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
