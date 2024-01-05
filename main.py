21598ce7b26f5aa07c3e49b7eec86a8c448f3bae
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:31:28 +0000

    spring bringup woman joke turnout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
