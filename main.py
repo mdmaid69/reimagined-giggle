7db2f63189ee5a9bf2e3a7bbf31857dec46f1dc9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:59:20 +0000

    exact successful male sore 573entertainment

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
