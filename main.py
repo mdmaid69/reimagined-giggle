36de5a307c08ab739cf475c196c72672976e873f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:23:04 +0000

    home imagine thirsty careful activity

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
