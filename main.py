0480243afc840ddd6463fb3cf0355b327d676d65
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:42:37 +0000

    typical less conclusion touch main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def reverse_array(array):
+        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
