b7d65f0c03672d7ef5518f61880119e734e14056
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:29:44 +0000

    pain agreement organization track build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def count_elements(lst):
+        return len(lst)
