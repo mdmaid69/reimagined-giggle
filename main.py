e0d1898aa081a6d09331c14bd7d1876a90a1aca3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:56:42 +0000

    should passage cruelty embarrassed tear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
