9bbdf2b877c5c7369c25b046189bf221e81ed163
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:17:01 +0000

    seriously lead wind workupto somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
