087e983c0653335c6f0d2b8468d5299d7095c39f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:20:03 +0000

    dress unpleasant exactly whenever risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
