0f3037cc3ff84a2dad2c43c4d322268abf7a2cbc
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:40:46 +0000

    some trousers preparation ordinary praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_speed(distance, time):
+        return distance / time
