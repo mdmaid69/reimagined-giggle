df067c2606e445bb243a0241b2f6692e78705450
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:43:17 +0000

    ear bank protection almost direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
