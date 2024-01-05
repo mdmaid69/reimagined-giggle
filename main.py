a13fe6402097e49498f8e75a38bfc9f3285a6946
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:37:10 +0000

    concentration where to high scream

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def find_max(numbers):
-        return max(numbers)
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
