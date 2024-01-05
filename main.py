a205e3133d86dcce49b84b1368475c6603884fff
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:41 +0000

    thin harm book right impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_dict():
+        return collections.UserDict()
