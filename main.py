d22700fe147bd65f1f5c897e8f7a8348123fab0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:56:21 +0000

    breakup positive understand wind conclusion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
