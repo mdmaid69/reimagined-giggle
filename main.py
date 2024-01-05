b36ddab2aa61496657a199d39d165d9c88fd8268
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:24:39 +0000

    ready clear pile damage yesterday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
 import array
-def get_array_length(array):
-        return len(array)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
