fa9bbc284eb847c6bfa4e8e23689581c84ce58e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:48:14 +0000

    peace trial free backwards fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
