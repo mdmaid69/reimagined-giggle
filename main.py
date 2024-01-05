d60e1d60dfc82b3b5522c6558118d563ab056803
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:50:52 +0000

    tonight shoot squash someone Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
