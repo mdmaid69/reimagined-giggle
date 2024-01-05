8d60f1b635e6ba0f19692b0964846434bd58fca1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:33:19 +0000

    calm progress support regret fair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def count_elements(lst):
-        return len(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
