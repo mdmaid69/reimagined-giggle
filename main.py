602930a2f722b13440645a491190afbb80a9ef38
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:52:49 +0000

    stiff yourself division length we

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
