7a45e26995b2c9f145ebe37886fbaef9a2740602
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:14:09 +0000

    pleasant fit shall change bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
