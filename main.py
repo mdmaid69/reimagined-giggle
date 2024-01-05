581b273436332723f954c0ad97b2976f7db50564
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:17:08 +0000

    communication fierce breakin time tour

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
