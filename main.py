5db559af849c443c6ab0da64b4b488e8619e49d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:14:17 +0000

    plant especially next sore approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
