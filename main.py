7a182d147ad912e097e7f8be574574f44fc4e6c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:44:24 +0000

    part usual car cross Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
