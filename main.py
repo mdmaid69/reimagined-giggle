84c9cc416c00b16053ddd3ac03e0033faab2196c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:28:21 +0000

    comeup unkind description difference serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
