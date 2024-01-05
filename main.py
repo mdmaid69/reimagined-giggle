e7f7e8949eafceaf0bf69a38298ef1eb9cea303c
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:22:38 +0000

    quickly market charge extremely mainly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
