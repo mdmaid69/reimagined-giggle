cba68e990de7c3a2b8f5236f59ec2a848f58a07f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:07 +0000

    succeed voice among it slide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
