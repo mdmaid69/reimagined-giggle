293e56a55e6b36c2c5c898b35bae7acf40afcadb
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:29:35 +0000

    check choose conversation us for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
