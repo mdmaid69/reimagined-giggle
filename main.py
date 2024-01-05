b7cdb87f6f2bba43a131b109256459fde6f5eb54
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:06:37 +0000

    wild short takeover breakout our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
