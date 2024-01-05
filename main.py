9f4e41a5889c136dba014c3e04b94a956149336e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:19:12 +0000

    cost transport explain gate list

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
