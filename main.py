ccf245d12197f87a2b6d2dcc93d3b2567df84c74
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:57 +0000

    necessary sweep top practical recent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
