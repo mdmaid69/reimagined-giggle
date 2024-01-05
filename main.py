c14fee99f4a9c84e5fc5f5a956e588b2966c854b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:44:23 +0000

    spite fetch sit brother downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
