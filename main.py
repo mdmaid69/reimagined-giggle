34d6f88a4105213f1b56965353c741defc76436f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:02:24 +0000

    attitude defend order talk aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
