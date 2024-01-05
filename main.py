4bcd6fef40ad07cb96233047c3d401e8827bb66a
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:51:40 +0000

    action birth invite variety sail

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def change_working_directory(path):
-        os.chdir(path)
+x = 10
+y = 20
+print("Sum:", x + y)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
