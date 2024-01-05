1235f94ebe0abb9399ee1f2f11694eeb037aadaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:58 +0000

    maintain suit watch about pleasure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def reverse_array(array):
+        array.reverse()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
