ea9f59a11718c6df7517f77c1aa901c76b303422
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:15:46 +0000

    little themselves myself anybody hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import platform
+def get_python_version():
+        return platform.python_version()
