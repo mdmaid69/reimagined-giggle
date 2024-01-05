34f03864906205ad874b90d9d724cecb533ade7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:53:58 +0000

    tap grade ought commit finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
