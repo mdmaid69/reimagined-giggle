4ff9eb9ae012ccab4ef97f5537528fcf453ce804
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:39:08 +0000

    young bring position remember train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_force(mass, acceleration):
+        return mass * acceleration
