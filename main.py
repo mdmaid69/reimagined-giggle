b74d0aea64a8d9b7e31c95772bff9a50b2e43b93
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:46:04 +0000

    list excuse rate goback(to) so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
