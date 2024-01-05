68ef312cdc3634daf4671ba193ef20ea9525edbc
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:47:54 +0000

    borrow than invitation string turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
