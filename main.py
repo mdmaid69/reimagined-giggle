d4c9ec5fef7ad9825b0534a83a4f93460fde5d50
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:55:43 +0000

    sport obvious else final fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
