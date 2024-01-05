e8a2f870af992e34ce33a14a7fc55fa3fcc51242
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:56:29 +0000

    grade settle slide door thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
