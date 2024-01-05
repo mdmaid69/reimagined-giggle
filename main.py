6e167c4b5c16244f9add49687c43bb0d17a1b6f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:36:21 +0000

    side embarrassing deep bang obtain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
