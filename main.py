f24ab9ac4e3845c2162b7cba3d854a7154f72864
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:46:17 +0000

    accept pass pain cum walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
