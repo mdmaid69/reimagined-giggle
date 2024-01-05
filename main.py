708997f690201ad642cd17695da9ffbad7f7ac61
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:35:58 +0000

    towards genuine fall fire perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
