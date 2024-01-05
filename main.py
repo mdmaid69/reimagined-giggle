111fd9749bdd6deb106cf7f5e95440b88925c3c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:51:17 +0000

    Pp town poor find produce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def calculate_area_rectangle(l, w):
+        return l * w
