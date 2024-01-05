12d1c8b41cea7ff4319e13232001e958375c2aca
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:13:29 +0000

    surface advantage plan track swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
