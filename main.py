69bfa8ec4cb4710335935fcbd92b315e45203e80
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:01:04 +0000

    pay so ticket content pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
