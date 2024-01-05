5601621a7d7563b5c6e3ccab92507f21c00a9c1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:19:56 +0000

    defend choose no claim room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
