cc258cd171f7088592b1e49e9d76b286edeb309e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:17:45 +0000

    especially address refuse private satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
