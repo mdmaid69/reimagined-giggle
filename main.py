997450e232e1e1164ab96565d839baa855fb720a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:25:39 +0000

    precise cost anxious makeup leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
