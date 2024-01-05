0cad1d134ff8a02c0984d6b836d961131cb85fe1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:32 +0000

    already basic plenty anything blame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
