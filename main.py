1e61942185b72705776a1649bb13f85237cb26b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:11:29 +0000

    trip project price wash half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
