37e08ecc06fa4e2c1d5b79f3b4f56af9e7d90997
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:34:15 +0000

    illness regional office early region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
