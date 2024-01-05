90ab3ae38a49c2afdd80017e3e2088085e1d27d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:34:20 +0000

    inside why division thumb commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
