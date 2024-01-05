bbd8fd65b40f5b80aa79608517d7b7a539a865ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:07:22 +0000

    file year for ball badly

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_work(force, distance):
+        return force * distance
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
