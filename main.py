811ee73dc2bff1b134acbd2a59e31b6d1f2cbbaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:04:39 +0000

    accidentally Ss frequent crazy morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
