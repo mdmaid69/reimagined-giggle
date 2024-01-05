3d1a88021478b2d48c408addeb38887bc5c47b03
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:31:46 +0000

    not agreement plenty afternoon urgent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-print([x**2 for x in range(10)])
+import array
+def clear_array(array):
+        array *= 0
