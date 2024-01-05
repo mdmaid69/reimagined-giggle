8c6c02fade14f1057fcb8ce8fda40c445e93216f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:08:05 +0000

    straight convinced successful row mean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
