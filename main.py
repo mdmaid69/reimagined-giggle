96d297074035d68b8aaa282493eef85cc2c218e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:23:45 +0000

    write couple card image entrance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
