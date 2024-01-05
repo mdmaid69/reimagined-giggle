f065d1a100ca2b94f9c5d63c624eb2aff36b0109
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:03:15 +0000

    satisfied return be evening impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
