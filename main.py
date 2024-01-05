1cff99c11094c405cad2d98a193591fffa61d4ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:03:12 +0000

    further judge neither spend wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_base_name(path):
+        return os.path.basename(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
