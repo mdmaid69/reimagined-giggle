4fd213a89ec20330937f6dc2e18eeca8b44c17de
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:11:43 +0000

    Ee taste figure ignore pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import os
+print(os.getcwd())
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
