b3282caa43bf21915012d5bd72003a6644681e9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:50:59 +0000

    should Ee all glass regional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
