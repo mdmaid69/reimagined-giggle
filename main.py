bce9b07ca9fb2bed37f596a92fe3e66d70710574
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:44:06 +0000

    report still whether always quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
