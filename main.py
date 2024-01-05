4481a6c9d2bc96330731922eaad5928a2c58b87c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:50 +0000

    form lean immediate smell spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
