6174c23c22d72bc42802a5ab9b9bfd0e0119b871
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:39:21 +0000

    combination round wealth store price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
