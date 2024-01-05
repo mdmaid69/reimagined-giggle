7a29357bb1ff222d947c2959bdbce25a01e2d767
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:16:23 +0000

    act represent feature material create

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+print(math.pi)
+def calculate_work(force, distance):
+        return force * distance
