b0f402283c90e762df698fa55f03147ecd09d40f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:35 +0000

    cross everybody whether brush pour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
