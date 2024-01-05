7ab36cc609373f1bca071dcb886c3a2a269cce99
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:19:07 +0000

    strange dead care pull notice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def find_max(numbers):
+        return max(numbers)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
