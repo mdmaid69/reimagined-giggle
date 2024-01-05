7a253e4a42771cea33707760fec1db11c534a1ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:42:49 +0000

    hold worst once afford any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
