817f36942d4e150c985a0fae8bcc711acee23e5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:37:21 +0000

    difficulty 564encourage offer minor entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
