7d33d9718393a7823510d7144dca21a9084ea8f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:50:40 +0000

    air early talk swallow breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
