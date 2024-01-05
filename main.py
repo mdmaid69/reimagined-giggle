683714817219c6f3a1af155a0fe243fe14cc9413
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:51:11 +0000

    office principle Gg natural play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import logging
 logging.basicConfig(level=logging.INFO)
 logging.info("This is an info message")
