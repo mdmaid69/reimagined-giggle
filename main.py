837c794998435e0e803d45355c41e525fd40a82d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:28:54 +0000

    large bill discussion complaint awake

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
