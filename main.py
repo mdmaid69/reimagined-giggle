25528da9b9c0f18c0c8151d94c54e82f9e0c5992
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:13:39 +0000

    absolutely second form opportunity suckme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
