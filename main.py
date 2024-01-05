9150ad41cff2a57d88821b2d1e248a1255ab4b7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:25:37 +0000

    different view fat summer rather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
