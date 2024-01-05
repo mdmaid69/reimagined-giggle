4e6259771c38b674a5aaa9e988957a16c10e4e3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:57:32 +0000

    feeling direct sport reduction style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
