4d2fae8a644373d51cb7c7aa095bab9dbfa9447a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:36:28 +0000

    responsible behaviour next pocket juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
