2186d0f54ef4f87f718270e5d3dc44f5ee996fac
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:52:54 +0000

    conduct common someone lay test

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+def exit_program():
+        sys.exit()
