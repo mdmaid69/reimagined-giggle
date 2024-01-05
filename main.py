f005525cb90f22ffc86e1aea3223c6863609de8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:37:02 +0000

    stick wherever takeout new sit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
