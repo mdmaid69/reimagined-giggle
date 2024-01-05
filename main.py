c6f521c536a05fa15c8c36cbad49e411f1ce3316
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:43:37 +0000

    rely stare concerning Ff chase

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
