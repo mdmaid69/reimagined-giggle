fdbfac793082077ad976b6832f3bd3a932925249
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:42:31 +0000

    ever offence involve intelligent over

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
