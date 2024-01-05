c8c6af51441b73666524e0928e1366781054cd19
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:29 +0000

    certain sky gate long stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
