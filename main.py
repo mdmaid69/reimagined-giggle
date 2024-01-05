f7f509ad5fcca0906f29f7fb397d9f4a76c47400
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:50:14 +0000

    invite distance Kk yet upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
