bd07f1616d95ea8ae518d6c2291d996b69ed8bce
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:16:57 +0000

    wherever wall ride Itself autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
