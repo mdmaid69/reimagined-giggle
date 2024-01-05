f256d712e058577e9935b7fa42a1953236bca212
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:55:27 +0000

    keepoff separate cheap pause consult

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
