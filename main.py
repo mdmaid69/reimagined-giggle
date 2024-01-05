9c462b05b9e84cf1fddf51fcc62160940720d979
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:50:43 +0000

    understand flood form giveup touch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sys
-print(sys.version)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
