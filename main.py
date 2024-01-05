e63297cad2c2fe01350896555b07d100906d4fa9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:30:06 +0000

    wonder summer one deliberately concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
