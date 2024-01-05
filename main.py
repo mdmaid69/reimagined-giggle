16f15f4af03b1bd3caab3e4249880da736acbcc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:24:28 +0000

    illness 565end unpleasant near soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
