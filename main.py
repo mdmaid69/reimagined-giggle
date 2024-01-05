079b0423de1e01b0c5a2a013efe698906c9a026b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:30:36 +0000

    hold unpleasant approval taste final

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
