e01378306bfc6fd4505ef651287ac7b511882dde
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:55:35 +0000

    pretty spite obtain takeback brother

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def convert_to_octal(n):
+        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
