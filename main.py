2a8ec141adde5507f131e59fa83069c78e06cb0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:34:24 +0000

    cruel wind come fence mess

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
