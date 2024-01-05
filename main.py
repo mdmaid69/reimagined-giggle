20475aad9f61497f49fbdb2ce0269cdada36f068
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:50:56 +0000

    die pronunciation copy down search

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
