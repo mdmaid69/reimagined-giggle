e81dc75fe7fd09d893f99a40b93d82f629f29e43
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:43 +0000

    further giveaway suggestion build autumn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
