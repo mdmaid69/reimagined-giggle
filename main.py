20789924b98386c66a145f116e3f5879de22da56
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:33 +0000

    breakthrough talk estimate phone while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
