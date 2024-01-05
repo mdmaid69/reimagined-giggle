b97db1eaa1bb2a9faee8bb178d2aa8dc5f67c67d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:19 +0000

    comfortable defend pleased central short

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
