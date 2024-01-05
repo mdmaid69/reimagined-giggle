e79eb1624652aa5c958c1d4b3f8348a2887f8a15
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:08:38 +0000

    save birth brave reject thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
