1f52cd93815008da5d454b1137d2a052a074d441
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:57:22 +0000

    principle force rub on make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
