dbbf51ef199f13242438f5d4706cdf57b4638373
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:51:38 +0000

    fault term name music pot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
