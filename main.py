acac8c53d932388eac60b9136a1c654145b6d01d
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:58:53 +0000

    sure getoff behave watch spend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
