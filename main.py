8d4805d8990205dd6189c154056b8773a992891f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:43:44 +0000

    interview instruction recognize weak juice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
