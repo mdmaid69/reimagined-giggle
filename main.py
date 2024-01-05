77b8e3636cf2bc975bf53f6831e9c14a4e35fac2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:13:48 +0000

    degree square loose computer so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_bool(array):
+        return bool(array)
