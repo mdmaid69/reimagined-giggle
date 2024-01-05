deb3e3fa7e21a1ca564cea4f9277d8d9ca32adbc
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:40:48 +0000

    become possession its realize brush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
