1e113a40d4804b7afb518244aa22ebdff383983b
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:20:12 +0000

    gate switch whom matter slide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
