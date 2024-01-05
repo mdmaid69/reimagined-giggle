781b39a3cf4fd9fd857c16befcf652db2810a11c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:50:22 +0000

    live check increase birth Oo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+print("Hello, world!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
