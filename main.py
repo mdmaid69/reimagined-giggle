f6dbce1e0cd75c20d473f4cbb4922b395e727bb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:38:29 +0000

    sign precise cook strike wrap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def reverse_list(lst):
+        return lst[::-1]
