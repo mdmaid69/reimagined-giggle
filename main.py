aa943a3c0b08ddfa0302a4503262eba1de77f551
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:48:25 +0000

    large surprising lookup never deliberately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 def subtract_numbers(x, y):
         return x - y
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
