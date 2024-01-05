ab7fcdd2017c87bb94b517774d390ad2125a4fa7
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:14:49 +0000

    arm maximum meet failure fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
