6960c2d73bcd653a2649b13ef20749e9c33ac0c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:40:19 +0000

    round care born art damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
