1e339e56e8cf2aa9c8c31f7318c2d9236d92b1bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:00:53 +0000

    moon along but pump search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
