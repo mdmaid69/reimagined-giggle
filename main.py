f71aef08ed5077406294b419fe57b79efad60bb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:13:44 +0000

    could develop unable manage list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_length(array):
+        return len(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
