c70447e8ef6b598c83d8d73788ec2149de2aa296
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:24 +0000

    design eat church yourself pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
