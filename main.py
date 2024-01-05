68d5ccc01b357960cd056d8d7d3e45c127160499
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:43:35 +0000

    let thirsty safety dish rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
