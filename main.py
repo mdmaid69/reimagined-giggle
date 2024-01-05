838e85d8c865ccaf95d5247188181162a500d8a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:38 +0000

    measure small method trick who

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def multiply_numbers(x, y):
+        return x * y
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
