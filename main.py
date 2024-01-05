89c4e0bb13d47ffce62eda67f46465df3575f0e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:31 +0000

    immediate familiar include speed rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
