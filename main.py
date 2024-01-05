01ff60d7600da0ca34b53f00062f53cf74a15d68
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:11:24 +0000

    regret refer detail hole spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
