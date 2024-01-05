ec0532e22251a85be5fa371691a0407414b752bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:00:06 +0000

    comefrom friendly already container child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
