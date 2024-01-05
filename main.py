e6e89334c35ab37514c823cdcd15e5bb9fad80e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:53:53 +0000

    bag gap cap pass last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
