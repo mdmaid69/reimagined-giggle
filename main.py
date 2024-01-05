e27e07e1bb0014f22b790a5363c767781526071c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:49:12 +0000

    cap regularly last pressure comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Words:", len(text.split()))
