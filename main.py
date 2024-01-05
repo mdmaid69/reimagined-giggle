56e0fa7aa4caff988acb495e3ebc76ef35a57646
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:32:27 +0000

    plate term accidentally shout late

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def count_words(sentence):
-        return len(sentence.split())
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
