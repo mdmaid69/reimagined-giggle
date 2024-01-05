a73ee2eca1e934966a41278667c3779da50e1281
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:43:53 +0000

    tool definite lend mine relation

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def create_stack():
-        return collections.deque()
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
