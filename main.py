fba1481857814e2d61391356364f6aae1bcb968b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:41:06 +0000

    pump convenient 569enjoy fairly will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
