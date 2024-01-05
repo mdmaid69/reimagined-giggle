fd7d975f53e55a8f8b47d702de5807a940b00f97
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:17:42 +0000

    repair zero water outside programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
