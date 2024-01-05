7e3047e3c978754b6689b15ffad3014cbcb27481
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:34:44 +0000

    shout glad right equipment tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
