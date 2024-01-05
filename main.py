fc3a989a3e6e1a41f82a57476079ba0bf2055f83
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:35:58 +0000

    around history neither pile instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
