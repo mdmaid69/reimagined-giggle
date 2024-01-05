a1406541d9aa7b798ceba249fe163396075fec29
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:28:35 +0000

    false poison freedom unkind without

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
