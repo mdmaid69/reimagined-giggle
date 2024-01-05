d4f98aa80f262a1229c65349966fb78f97cb32ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:59:25 +0000

    explanation deep surface trade admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
