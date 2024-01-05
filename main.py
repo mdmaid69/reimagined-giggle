a65b4c639a85f89b57c5733eeb3f0e638dcc1ed1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:39:55 +0000

    language way majority talkoutof please

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
