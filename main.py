a56da7dca9211dd8e39a13bf4e85564ddb69bf44
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:55:14 +0000

    prove connected worse fit apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
