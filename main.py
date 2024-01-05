693aea8cab2b02c6ccdb2798c4bf44a5ebf1cdf3
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:20:11 +0000

    cause feel autumn fast whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
