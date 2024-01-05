63c4abec96f2b049a12927398913fea8551bb87f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:43 +0000

    history deliberately spill talk care

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def add_numbers(a, b):
-        return a + b
+name = "Python"
+print("Hello,", name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
