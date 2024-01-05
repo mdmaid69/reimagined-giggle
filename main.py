b9b848931db3d8429e7ca39ffcebb0148f9e7c63
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:59 +0000

    about black failure make broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
