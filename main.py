4f065d1b6d144dd2bc4fc18902fa60141c6720d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:50:39 +0000

    mark pin progress thin pass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
