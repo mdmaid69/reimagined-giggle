7c9647ae594c9caebdd6307f970a05c08e9a82fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:27:07 +0000

    seat on worst else sister

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_area_rectangle(l, w):
+        return l * w
