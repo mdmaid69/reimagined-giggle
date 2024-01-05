c7578aeb448c7b7ba4da69ba1398d0409e5f4725
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:49 +0000

    announce 568energy story by future

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-  def cube_number(x):
-        return x**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_acceleration(speed, time):
+        return speed / time
