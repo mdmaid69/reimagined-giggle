88e96d2d1d81eed63c7531c5d91dd55023c9c38d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:52:46 +0000

    concerned argue no total hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
