7baac091a54397189c6900691ada80bf6468ad1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:38:38 +0000

    brief guess firm turninto achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def count_elements(lst):
-        return len(lst)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import os
+print(os.getcwd())
