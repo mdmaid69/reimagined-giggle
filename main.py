f0134f985d3c1cb3718455dfc2409d7f5d26d64e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:42:57 +0000

    shine dear tear opposite mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
