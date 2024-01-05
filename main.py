99c4736da5c803669e9c0d64368cbb70e5c8f9f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:20:33 +0000

    tense surprise bar weigh trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
