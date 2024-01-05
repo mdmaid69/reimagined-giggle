b3fccdc1ba4ae3278972f6baeb0b468ca893af06
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:41:06 +0000

    past look dear treat home

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
