279a2e6daf942db169e88cf741be0c7ee12cb89d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:06:57 +0000

    thanks fear opportunity runinto belief

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
