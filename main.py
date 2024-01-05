c74087fe61fb378f2a6b6bc1e318fcb3cce7cfcf
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:56:09 +0000

    sensible take handle late first

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
