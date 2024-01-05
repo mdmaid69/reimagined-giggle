8c84d1a7eec23d83b60b572ecd927c21e1547246
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:43:15 +0000

    price phone home reduction taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
