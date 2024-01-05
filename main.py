a8ec9ed63fc36a8f18ef252e2bd78949263576bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:37:47 +0000

    half translate during pool today

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
