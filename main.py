b332815ef1ef1d0b673c3ff6d634461aadad015e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:26:36 +0000

    car card upset scrape organize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import platform
-def get_python_version():
-        return platform.python_version()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
