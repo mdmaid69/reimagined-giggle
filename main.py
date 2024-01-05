3237c4bde592ebad57d9e2ab71c9ad29da9e3ed9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:48:21 +0000

    recognize race damp introduce succeed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def sort_list(lst):
+        return sorted(lst)
