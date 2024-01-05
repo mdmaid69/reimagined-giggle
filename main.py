6d0e0e82253d0007bc2d523baacbc05ab57ba9a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:49:13 +0000

    sale reasonable treat needle in

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
