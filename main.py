bb76e1ae55a5e8a091024baad8b1d6d2385369b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:27:59 +0000

    whoever which proud phone congratulate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
