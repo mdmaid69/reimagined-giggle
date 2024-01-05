ed009b0f7db21bc11b53bcdab27e450f5096fa07
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:43:14 +0000

    string relief mistake duty present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
