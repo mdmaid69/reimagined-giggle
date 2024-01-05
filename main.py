17f418d0effb299658924838a3dd3f4a26624794
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:26:23 +0000

    satisfied bank bar talkoutof trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
