639a4cbfd2df3f153d10a1f065fa029985e58c75
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:07:28 +0000

    professional bringback crazy small hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
