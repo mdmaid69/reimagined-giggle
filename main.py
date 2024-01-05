d9b4016c91f02ac67f4d16bd1df6a8691717f1ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:12:45 +0000

    damp sweet child valuable love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
