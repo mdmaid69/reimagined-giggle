66b565d9951ada11b14e58266c8190ac01545b54
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:00:18 +0000

    list instruction share show difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
