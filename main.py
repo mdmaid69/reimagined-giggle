f9f62c054580201b67167325bb14c17028fc0403
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:57:40 +0000

    switch public reduce and really

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_work(force, distance):
-        return force * distance
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import random
+print(random.randint(0, 100))
