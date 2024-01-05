287e4b466631f6aafcdc8aac6cf3b14bb9b747cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:25 +0000

    argument born suggest hear ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
