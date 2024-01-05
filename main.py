7d763cd7b1db6caf757e7fd76eedb934ebcefc3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:22:26 +0000

    later really undo excuse stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
