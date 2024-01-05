5d3048dadabbc967e673662cba9a0f1d961d8190
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:39:28 +0000

    crash plastic inform floor tongue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
