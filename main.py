d7881e5623ea7270d53108ed34028a47641f49fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:26:42 +0000

    road television rush arrive usually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def get_array_as_repr(array):
+        return repr(array)
