e24a2f58887d70bef210b0e4c9ab624ae8942856
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:24:40 +0000

    command school dangerous cost himself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
