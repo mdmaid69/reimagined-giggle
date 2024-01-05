41002f4f2d72c707ff980eaf1a9f676781e22c24
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:21:43 +0000

    niggaacademy pity machine secret twist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def check_if_array_contains_item(array, item):
+        return item in array
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
