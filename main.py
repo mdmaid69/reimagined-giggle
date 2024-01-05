2baa01f4425791520e1c5687a4b6be979bc76f8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:23:25 +0000

    steam less land final certain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
