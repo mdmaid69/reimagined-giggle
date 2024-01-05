6a55269632fb98ec2331fcb8cae9d0fb6be59f67
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:22:28 +0000

    remember game emergency sound stand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
