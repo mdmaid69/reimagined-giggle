f07054a05973e11f6c8a4cc6cd1941140fd9442d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:57:03 +0000

    to dangerous crowd birthday common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def divide_numbers(x, y):
+        return x / y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
