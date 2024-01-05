f9c96dcc4f055727051f9b8a64d5365070cfb84d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:43:06 +0000

    shine leave offence occasion describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
