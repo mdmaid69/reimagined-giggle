9d3dda5da381376030088f7b01a78c33020290d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:30 +0000

    intend health world region suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
