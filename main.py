ca0bc31671bb6dffa7ba8bce50a1231b89e97148
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:47:40 +0000

    in nonsense hope temperature sale

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
