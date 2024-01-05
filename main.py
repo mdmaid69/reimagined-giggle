b449ebb78bf6d81a5890a27f00ad2cdbfc2d331e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:08:18 +0000

    ring afraid leave OKorokay warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
