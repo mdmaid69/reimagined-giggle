c3f214d182ff4d178fdf02306931491d78245cdf
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:34:37 +0000

    plate practise related extraordinary interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
