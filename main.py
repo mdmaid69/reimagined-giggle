21900b2a2d9c2e71e548e42110456434389f9310
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:47:56 +0000

    strict embarrassed far warm swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import random
-print(random.randint(0, 100))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
