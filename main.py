17f150cc2a66dc6e2c89c93f615d05078cc592e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:37:38 +0000

    rarely I worried piece gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_stack():
+        return collections.deque()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
