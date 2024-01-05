cb3e8bd506e94474eef4af08414ddeef67fb649a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:34:26 +0000

    kill pump seem range public

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.time()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
