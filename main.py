0c511754c2d1447eadec033bdc3360d69e7d6cbd
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:28:05 +0000

    trip discuss pollution serious use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
