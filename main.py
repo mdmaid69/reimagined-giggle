3f270ad07a44e83e1c797fe3aa4acc61be06734d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:32:39 +0000

    trust behave bar bend convinced

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(10): print(i)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
