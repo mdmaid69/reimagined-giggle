18418bbe694f156d1f277a3ee6d3c3b912b71042
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:43:43 +0000

    past room sign height deep

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_current_directory():
-        return os.getcwd()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
