214d7c8268159cbe7761b501cb6fc6fcda20d197
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:13:17 +0000

    flow only though whom rise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def multiply_numbers(x, y):
+        return x * y
