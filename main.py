ce67ea82481987ec368d5991fd1b61ac2ac02190
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:27:55 +0000

    future today drive claim meeting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
