9f57b0d831a717a7805a50d9f952cac5fd447b8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:34:06 +0000

    handle sale tread possible Rr

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
