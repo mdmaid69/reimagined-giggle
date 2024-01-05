2b9eb6a9598a74901a573e8ecbaa4b1e7b6d4cb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:11:27 +0000

    declare family instant likely father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
