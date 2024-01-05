0781b5c7e373386a481e180ded6533d7b924e740
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:31:12 +0000

    team distance rarely everybody study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def create_user_list():
+        return collections.UserList()
