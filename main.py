9c60ae95fbf9e631e6b1e29ba5dfc755af06ea9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:38:59 +0000

    frightened himself sentence more crazy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
