36bba1f75ad49d538d7e7c8fe8bf8b9d958672dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:21:16 +0000

    dear care check form calmly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
