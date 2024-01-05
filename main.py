79a4852d2785e69b88e57ae9a9c3d1390209c6a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:35:20 +0000

    sit fall acceptable difficulty unlucky

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
