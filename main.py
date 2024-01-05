da1184c20d037e7299549ed980dea0acf2c62d66
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:39:43 +0000

    top emphasis suggestion interview swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def multiply_numbers(x, y):
+        return x * y
