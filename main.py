b7d43e7aa8bf609da78f39b9a905ff7f28dba87d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:46 +0000

    fetch band often warm system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
