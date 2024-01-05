2eb387a9096c57aa261588f52189470dfb54b23a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:44:16 +0000

    second late sure guide near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_density(mass, volume):
+        return mass / volume
