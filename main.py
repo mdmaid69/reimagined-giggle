9ad8bf6a3fb50cebbe2491ab0f44b3854b077ef2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:21:54 +0000

    Kk forgive remove sport drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
