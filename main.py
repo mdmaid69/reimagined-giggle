4cc2b9738c16beef95cf9a6bb61b22d10551f7ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:54:35 +0000

    usual touch whole exam quit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
