785cc3eb99a1945fe8a89b64fa62ca514d0dd88b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:01:13 +0000

    to heavy bag about today

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
