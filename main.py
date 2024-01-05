3e50fa8524a07f0aec717ff48c277f32891ff620
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:33:24 +0000

    accordingto Qq shall mass sea

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def sort_list(lst):
+        return sorted(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
