35e7589baef5e30c69ea173d69e351e4dd272dae
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:08:37 +0000

    mark free laugh abroad well

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
