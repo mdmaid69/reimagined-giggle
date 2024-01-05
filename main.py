ef9a5e69ef6de932e47eafef63d3d3f8ae6cd847
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:00:26 +0000

    port rule suit squeeze failure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def add_numbers(a, b):
-        return a + b
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
