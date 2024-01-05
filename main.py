88492efb28669692de208f70fa3ef39a1f964226
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:24:41 +0000

    strict Kk tap will thank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-for i in range(5):
-        print(i)
+  def add_numbers(x, y):
+        return x + y
+  def subtract_numbers(x, y):
+        return x - y
