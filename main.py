0dc2caa94c2db4305f79cb4b30c8891897cf3876
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:29:41 +0000

    takeoff Ii pin compete sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
