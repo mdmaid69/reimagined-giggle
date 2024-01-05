5a00942b7f2bbdba8c8ff49190aaff71210dcf2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:57:52 +0000

    apply learn bright goup anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
