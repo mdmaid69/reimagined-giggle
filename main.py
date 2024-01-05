7769b19f0c10050c78123b76a7026d2efd3255e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:49:54 +0000

    style circumstance smell famous other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import numpy as np
+print(np.array([1, 2, 3]))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
