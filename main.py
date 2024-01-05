2c078958cb88ca8b427ad312ea8497431b46234e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:45 +0000

    twist late hurry explain rain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
