c07cac19a56a4913e44cf523adaf5b0955e8a07b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:41:55 +0000

    characteristic wander over used too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
