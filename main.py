f670659ededd03ef43dff936fe62cdf921588d84
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:11:58 +0000

    water eager wise exciting level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
