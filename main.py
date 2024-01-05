100230c0368a5c1495737a70cd2c55579bcd1b31
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:58:37 +0000

    promise extra basis solve window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
