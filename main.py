8f5be4c874c5f2f345751706bcb1be9395787d27
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:32:09 +0000

    development chance block pollution build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
