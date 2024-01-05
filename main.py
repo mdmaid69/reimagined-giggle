bef688e8ae7314d36c9ef052ddca893b80fe827d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:42:33 +0000

    crash broken body keepup standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_speed(distance, time):
+        return distance / time
