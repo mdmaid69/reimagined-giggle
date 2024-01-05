1a064b0934dde6ccb1e21202293a9e2c5fbb2825
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:09 +0000

    entry hope think while listen

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
