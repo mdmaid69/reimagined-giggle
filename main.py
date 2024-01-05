aa272bac3658caa3b33497f2aa1b5906180cc593
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:41:39 +0000

    welcome recover successful public total

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-import collections
-def create_user_string():
-        return collections.UserString()
+name = "Python"
+print("Hello,", name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
