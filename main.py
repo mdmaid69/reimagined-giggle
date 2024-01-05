fb812984c89617326b70e0588da31c4dcd735176
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:28:27 +0000

    trade appeal future touch world

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
