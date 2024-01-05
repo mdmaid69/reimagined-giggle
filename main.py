cc28efa7f9b8d320e2e79ef2176c2d6167fb1e15
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:49:20 +0000

    false pick lock knock turnout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
