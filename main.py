b658454e296ed25dbb7098bfa178e86202c01b7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:07:24 +0000

    apparently last burn lead aim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import datetime
-print(datetime.datetime.now())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
