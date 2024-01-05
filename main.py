8eda8bc467eab04636b3fa481a834bd638216f80
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:13:38 +0000

    lead high would takeoff emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import sys
+def exit_program():
+        sys.exit()
