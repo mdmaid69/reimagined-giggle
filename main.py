f7e2fa764212d697a61a33cfa8cd445f308e93a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:02:58 +0000

    consult mainly succeed example entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
