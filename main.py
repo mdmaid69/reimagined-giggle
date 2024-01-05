56400db019f85ebae7ea7f0a7643de950fb2eacf
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:54:01 +0000

    promise contribute at beneath enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
