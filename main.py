8122ba4bddd7e7b3141ef5450dd725329df80d48
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:21:02 +0000

    science branch radio hard taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def is_odd(n):
+        return n % 2 != 0
+  def is_even(n):
+        return n % 2 == 0
