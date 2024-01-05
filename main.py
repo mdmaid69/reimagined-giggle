21239cfe94f4fc65aff0489d51dbe37e6a702c42
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:43:10 +0000

    runinto feed rank young complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
