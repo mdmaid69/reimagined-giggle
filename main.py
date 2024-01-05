66e97135fb765581de24af640a18550f925c85e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:11:28 +0000

    interest disgusting immediate towards stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
