513d998f2c36a9aba69d43c2ecc0b9cc592fc4a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:46:36 +0000

    software bag lost flat turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
