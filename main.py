da75a44d0b62fd796e89cd5570f3d148716919cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:05:04 +0000

    story rate pipe programme Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
