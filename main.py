86262883d7e5276761569f52abef35ede7cd6c8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:08 +0000

    obvious comeover obtain majority mark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
