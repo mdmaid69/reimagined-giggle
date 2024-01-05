b05e7ea6bdb723eeacfa43b8fc128f40d5770ebb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:21:18 +0000

    build display more during spring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
