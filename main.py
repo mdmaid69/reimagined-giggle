cd08726e1b6ecccfd8de070d3bfdf65e0ff4a2e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:06:10 +0000

    final similar spin precise wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
