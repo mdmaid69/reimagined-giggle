e7f60268050b08fb46e91399ab066a4a428b0262
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:10:17 +0000

    match model behind discussion peace

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
