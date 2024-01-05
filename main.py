ef2418d492e7f8415626192a3d8382fc0ad50961
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:13:24 +0000

    considering worried possibly sort relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_item_count(array, item):
+        return array.count(item)
+def is_even(n):
+        return n % 2 == 0
