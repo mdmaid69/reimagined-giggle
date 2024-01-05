74255ec1363d30d52f498b71974312339f0398fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:22:01 +0000

    getup matter instruction guess flavour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
