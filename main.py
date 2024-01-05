5653158065e5d3349d117565fe16f76bac57a220
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:33:10 +0000

    burst soft influence light normal

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
