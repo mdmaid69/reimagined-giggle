71057f39f75e8c44ce1d0d4c14d3eb47e0cfbd98
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:41:23 +0000

    sweet pronunciation slice team right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
