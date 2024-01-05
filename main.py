16f1250e74acb3ec290fe5a866a16e24e3c8186b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:55:08 +0000

    dance OKorokay strike contact search

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
