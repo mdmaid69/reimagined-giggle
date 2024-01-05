316811e8a715b3af7272b1e0dd479d3ee409abf7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:35:09 +0000

    light smile eye thankyou satisfied

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
