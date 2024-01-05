a517780ae478f90244168f49f6900b302399fc3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:22:11 +0000

    summer table sharp wait hear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_density(mass, volume):
+        return mass / volume
