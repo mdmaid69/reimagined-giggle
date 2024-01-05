c1b9e9e4de34b4fdae5d59267f0e88304fe78bd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:25 +0000

    turnup show contrast snow godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
