2215c1219c10e98a178ea507aed4a44034877c1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:11:05 +0000

    open offer what shoulder exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def square_number(x):
-        return x**2
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
