268a4c5a5a1cca9c27071aa1358d58c8c06a724d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:41:03 +0000

    worst suckme safety truth tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
