86b89c787bb95850de510286d48b8ba481d1f0d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:25:55 +0000

    disappear proud male could occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
