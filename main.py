bbfabccaed2a49a5e6584eae6361d785d5706423
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:05:20 +0000

    strange holiday because physical getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
