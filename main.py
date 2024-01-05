0e7d6ff7d2099f459e938dc7d3e110f1185a05ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:00:21 +0000

    under Itself sticky spend along

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
