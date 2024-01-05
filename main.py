0f9a8ad1cd2b99a1f30fef726e1ebb815d7f6929
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:10:08 +0000

    surprised stretch private where regional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
