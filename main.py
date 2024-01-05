6cd1088138647667ea5da66b4d90971cf810514e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:15:34 +0000

    turn keepaway tip embarrassed cost

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
