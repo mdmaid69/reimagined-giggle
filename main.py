803559c5dab5f3d40cc4aacf2acc5489867b4be1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:29:25 +0000

    dare fit reply everybody upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
